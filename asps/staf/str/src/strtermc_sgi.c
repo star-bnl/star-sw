6081a61aa8ac81348b3b3b0759ce9cd4e77e0ef6
Robert W. Hackenburg <hack>
1993-06-08 20:42:51 +0000

    'commit from within installib'

--- asps/staf/str/src/strtermc_sgi.c
+++ asps/staf/str/src/strtermc_sgi.c
@@ -29,25 +29,39 @@ some platforms, such as the DECstation 5000 running ULTRIX;  alphabetical order
 doesn't work.
 
 	FORTRAN-callable routines ("void" ==> no argument or no return value)
-
-	void strterm_get_char_(long int *i1)
-	void strterm_get_char_from_line_(long int *i1)
-	void strterm_set_char_(void)
-	void strterm_set_line_(void)
-	void strterm_set_normal_(void)
-	void strterm_set_nowait_(void)
-	void strterm_set_wait_(void)
-
 */
+
+	void strterm_get_char_(long int *);
+	void strterm_get_char_from_line_(long int *);
+	void strterm_set_char_(void);
+	void strterm_set_line_(void);
+	void strterm_set_normal_(void);
+	void strterm_set_nowait_(void);
+	void strterm_set_wait_(void);
+
+/*
+	Non-FORTRAN-callable (internal-usage) routines:
+*/
+
+	char strterm_get_next_char(void);
+
 
 
 
 
 
-/*	File-wide "global" definitions:         */
+/*
+	File-wide "global" definitions:
+*/
 
-#include <termio.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <termio.h>
+#include <unistd.h>
+
+	int	ioctl( int, int, ... );
+	int	msg_enabled_trace_( char *, int *, size_t );
+	void	message_( char *, int *, int *, size_t );
 
 static struct termio str_buf_save;
        struct termio str_buf;
@@ -69,154 +83,10 @@ static int str_term_mode=0;
 */
 
 
-
-
-
-/*	The remaining routines are FORTRAN-callable.     */
-
-
-
-
-
-void strterm_get_char_(long int *i1)
-{
-/*
-  FORTRAN callable:
-
-	CALL STRTERM_GET_CHAR( Ichar )
-*  Input:
-	INTEGER Ichar !Integer-value of character read from the terminal
-	              !in either "wait" or "nowait" mode, as set by the
-	              !routines STRTERM_SET_WAIT, STRTERM_SET_NOWAIT,
-	              !STRTERM_SET_NORMAL STRTERM_SET_LINE, and STRTERM_SET_CHAR.
-	              !If in "nowait" mode, and if nothing is in the input
-	              !buffer when this routine is called, then Ichar
-	              !returns set to zero.  Unfortunately, a Unix bug
-	              !renders the same effect on EOF (^D).
-
-  Output argument:
-	integer i1 -- passed by reference (pointer or address).
-
-  Return value:  none ("void")
-
-  Functional description:
-	Get a single character from the terminal, w/o waiting, and without
-	waiting for a complete line (ie, no EOT character needed to make
-	characters available).
-	Return with the null byte as the character if no
-	character is ready at the terminal (ie, don't wait).
-
-  Warning:
-	A Unix ambiguity fails to distinguish between EOF, sometimes
-	indicated with a ^D from an input stream, but otherwise being
-	a specific signal from the input device indicating that there
-	is no (more) data available, and a "no character avaiable"
-	condition.
-
-	This will result in an unpleasant surprise if a program is exitted
-	while in nowait mode:       The Unix shell begins receiving
-	null bytes from the terminal, instead of waiting for real characters.
-	This being indistiguishable under Unix from an EOF, or ^D, the session
-	will be immediately ended, and the user logged off!!!
-*/
-
-	char c;
-	int n;
-
-	if (str_term_mode != (str_term_mode_char_P | str_term_mode_nowait_P) )  /*Need to set the correct mode*/
-	{
-	  (void)strterm_set_nowait_();    /* Set terminal I/O waiting off.*/
-	  (void)strterm_set_char_();      /* Set terminal into single-char. mode*/
-	}
-
-
-	switch(c=strterm_get_next_char())    /* Try to read a character (or get next one in buffer) */
-	{
-	case -1:                                 /* Some kind of low-level read-error  */
-	  (void)printf("strterm_get_char-E1 read error\n");
-	  (void)strterm_set_normal_();
-	  *i1 = -1;      /* Return an error indicator  */
-	case 0:
-	  *i1 = 0;       /* Return a null byte */
-	default:
-	  *i1 = c & 255; /* Prevent sign extension */
-	}
-/*	end switch(c=strterm_get_next_char())  */
-
-	return;
-
-} /* void strterm_get_char_(long int *i1) */
-
-
-
-
-
-void strterm_get_char_from_line_(long int *i1)
-{
-/*
-  FORTRAN callable.
-
-  Output argument:
-	integer i1 -- passed by reference (pointer or address).
-
-  Return value:  none ("void")
-
-  Functional description:
-	Get a character from the terminal, w/o waiting.
-	Wait for a complete line before transmitting.
-	Return with the null byte as the character if no
-	character is ready at the terminal (ie, don't wait).
-
-  Warning:
-	A Unix ambiguity fails to distinguish between EOF, sometimes
-	indicated with a ^D from an input stream, but otherwise being
-	a specific signal from the input device indicating that there
-	is no (more) data available, and a "no character avaiable"
-	condition.
-
-	This will result in an unpleasant surprise if a program is exitted
-	while in nowait mode:       The Unix shell begins receiving
-	null bytes from the terminal, instead of waiting for real characters.
-	This being indistiguishable under Unix from an EOF, or ^D, the session
-	will be immediately ended, and the user logged off!!!
-*/
-
-	static char c;
-	static int n;
-
-	(void)strterm_set_nowait_();      /* Set terminal I/O waiting off.*/
-
-	n=read(0, &c, 1);           /*Try to read a character "c" from a line*/
-
-	if (n < 0)        /* This is some kind of low-level read error.  */
-	{
-	  (void)printf("strterm_get_char_from_line-E1 read error\n");
-	  *i1 = -1;       /*  Send back an error indication  */
-	}
-	else if (n == 0)  /*  This is either no-character, or EOF (^D).  */
-	{
-	  *i1 = 0;        /*  Send back a null byte */
-	}
-	else              /*  A character is available -- return it.     */
-	{
-	  *i1 = c & 255;  /* Prevent sign extension */
-	}
-/*	end if (n < 0)    */
-
-	(void)strterm_set_wait_();   /* Set terminal I/O waiting on. */
-
-	return;
-
-} /* void strterm_get_char_from_line_(long int *i1) */
-
-
-
-
-
 char strterm_get_next_char(void)
 {
 /*
-  FORTRAN callable.
+  Not FORTRAN callable.
 
   No arguments
 
@@ -280,6 +150,148 @@ char strterm_get_next_char(void)
 
 
 
+
+
+
+
+/*	The remaining routines are FORTRAN-callable.     */
+
+
+
+void strterm_get_char_( long int *i1 )
+{
+/*
+  FORTRAN callable:
+
+	CALL STRTERM_GET_CHAR( Ichar )
+*  Input:
+	INTEGER Ichar !Integer-value of character read from the terminal
+	              !in either "wait" or "nowait" mode, as set by the
+	              !routines STRTERM_SET_WAIT, STRTERM_SET_NOWAIT,
+	              !STRTERM_SET_NORMAL STRTERM_SET_LINE, and STRTERM_SET_CHAR.
+	              !If in "nowait" mode, and if nothing is in the input
+	              !buffer when this routine is called, then Ichar
+	              !returns set to zero.  Unfortunately, a Unix bug
+	              !renders the same effect on EOF (^D).
+
+  Output argument:
+	integer i1 -- passed by reference (pointer or address).
+
+  Return value:  none ("void")
+
+  Functional description:
+	Get a single character from the terminal, w/o waiting, and without
+	waiting for a complete line (ie, no EOT character needed to make
+	characters available).
+	Return with the null byte as the character if no
+	character is ready at the terminal (ie, don't wait).
+
+  Warning:
+	A Unix ambiguity fails to distinguish between EOF, sometimes
+	indicated with a ^D from an input stream, but otherwise being
+	a specific signal from the input device indicating that there
+	is no (more) data available, and a "no character avaiable"
+	condition.
+
+	This will result in an unpleasant surprise if a program is exitted
+	while in nowait mode:       The Unix shell begins receiving
+	null bytes from the terminal, instead of waiting for real characters.
+	This being indistiguishable under Unix from an EOF, or ^D, the session
+	will be immediately ended, and the user logged off!!!
+*/
+
+	char c;
+	int n;
+
+	if (str_term_mode != (str_term_mode_char_P | str_term_mode_nowait_P) )  /*Need to set the correct mode*/
+	{
+	  (void)strterm_set_nowait_();    /* Set terminal I/O waiting off.*/
+	  (void)strterm_set_char_();      /* Set terminal into single-char. mode*/
+	}
+
+
+	switch(c=strterm_get_next_char())    /* Try to read a character (or get next one in buffer) */
+	{
+	case -1:                                 /* Some kind of low-level read-error  */
+	  (void)printf("strterm_get_char-E1 read error\n");
+	  (void)strterm_set_normal_();
+	  *i1 = -1;      /* Return an error indicator  */
+	case 0:
+	  *i1 = 0;       /* Return a null byte */
+	default:
+	  *i1 = (int) c & 255; /* Prevent sign extension */
+	}
+/*	end switch(c=strterm_get_next_char())  */
+
+	return;
+
+} /* void strterm_get_char_(long int *i1) */
+
+
+
+
+
+void strterm_get_char_from_line_(long int *i1)
+{
+/*
+  FORTRAN callable.
+
+  Output argument:
+	integer i1 -- passed by reference (pointer or address).
+
+  Return value:  none ("void")
+
+  Functional description:
+	Get a character from the terminal, w/o waiting.
+	Wait for a complete line before transmitting.
+	Return with the null byte as the character if no
+	character is ready at the terminal (ie, don't wait).
+
+  Warning:
+	A Unix ambiguity fails to distinguish between EOF, sometimes
+	indicated with a ^D from an input stream, but otherwise being
+	a specific signal from the input device indicating that there
+	is no (more) data available, and a "no character avaiable"
+	condition.
+
+	This will result in an unpleasant surprise if a program is exitted
+	while in nowait mode:       The Unix shell begins receiving
+	null bytes from the terminal, instead of waiting for real characters.
+	This being indistiguishable under Unix from an EOF, or ^D, the session
+	will be immediately ended, and the user logged off!!!
+*/
+
+	static char c;
+	static int n;
+
+	(void)strterm_set_nowait_();      /* Set terminal I/O waiting off.*/
+
+	n=read(0, &c, 1);           /*Try to read a character "c" from a line*/
+
+	if (n < 0)        /* This is some kind of low-level read error.  */
+	{
+	  (void)printf("strterm_get_char_from_line-E1 read error\n");
+	  *i1 = -1;       /*  Send back an error indication  */
+	}
+	else if (n == 0)  /*  This is either no-character, or EOF (^D).  */
+	{
+	  *i1 = 0;        /*  Send back a null byte */
+	}
+	else              /*  A character is available -- return it.     */
+	{
+	  *i1 = (int) c & 255;  /* Prevent sign extension */
+	}
+/*	end if (n < 0)    */
+
+	(void)strterm_set_wait_();   /* Set terminal I/O waiting on. */
+
+	return;
+
+} /* void strterm_get_char_from_line_(long int *i1) */
+
+
+
+
 
 void strterm_set_char_(void)
 {
@@ -303,8 +315,6 @@ void strterm_set_char_(void)
 
 	int static id = 0;
 	int nlines = 1;
-	int msg_enabled_trace_( char *prefix, int *id, size_t sprefix );
-	void message_( char *prefix, int *nlines, int *id, size_t sprefix );
 
 	if ( (msg_enabled_trace_( "strterm_set_char-t1", &id , (sizeof("strterm_set_char-t1")-1) ) ) != 0 )
 	{
@@ -365,8 +375,6 @@ void strterm_set_line_(void)  /* Set terminal to wait-for-line mode  */
 */
 	int static id = 0;
 	int nlines = 1;
-	int msg_enabled_trace_( char *prefix, int *id, size_t sprefix );
-	void message_( char *prefix, int *nlines, int *id, size_t sprefix );
 
 	if ( (msg_enabled_trace_( "strterm_set_line-t1", &id, (sizeof("strterm_set_line-t1")-1) ) ) != 0 )
 	{
@@ -446,8 +454,6 @@ void strterm_set_nowait_(void) /* Turn terminal I/O waiting off */
 
 	int static id = 0;
 	int nlines = 1;
-	int msg_enabled_trace_( char *prefix, int *id, size_t sprefix );
-	void message_( char *prefix, int *nlines, int *id, size_t sprefix );
 
 	if ( (msg_enabled_trace_( "strterm_set_nowait-t1", &id, (sizeof("strterm_set_nowait-t1")-1) )) != 0 )
 	{
@@ -514,8 +520,6 @@ void strterm_set_wait_(void) /* Turn terminal I/O waiting on */
 
 	int static id = 0;
 	int nlines = 1;
-	int msg_enabled_trace_( char *prefix, int *id, size_t sprefix );
-	void message_( char *prefix, int *nlines, int *id, size_t sprefix );
 
 	if ( (msg_enabled_trace_( "strterm_set_wait-t1", &id, (sizeof("strterm_set_wait-t1")-1) ) ) != 0 )
 	{
