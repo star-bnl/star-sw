74e5569a39f9bd1570128e7234392aa7224d168e
olson <>
1995-12-05 07:54:46 +0000

    reorganize and add sun4os5

--- pams/tls/src/tu_intrpt_handler.c
+++ pams/tls/src/tu_intrpt_handler.c
@@ -7,21 +7,15 @@ void tu_intr_reset();
 void tu_intr_sighandler();
 int tu_interrupt();
 
-#ifndef VMS
+/****** irix  *****************************************************/
+#ifdef irix
+
 
 void tu_intr_set_();
 void tu_intr_reset_();
 int tu_interrupt_();
 
 #include <signal.h>
-
-#else
-
-#include descrip
-#include iodef
-static int i_chan1 = 0;
-
-#endif /* VMS */
 
 static char *old_sig_func;
 static int old_sig_mask = 0;
@@ -30,9 +24,133 @@ static int was_interrupted = 0;
 
 void tu_intr_set() 
 {
-#ifndef VMS
+/*
+    old_sig_func = ( char * ) sigset( SIGINT, tu_intr_sighandler );
+*/
+    old_sig_func = ( char * ) signal( SIGINT, tu_intr_sighandler );
+    was_interrupted = 0;
+}
 
-#ifndef IRIX
+
+void tu_intr_set_() 
+{
+    tu_intr_set();
+}
+
+
+void tu_intr_reset() 
+{
+/*
+    (void) sigset( SIGINT, (void(*))old_sig_func );
+*/
+    was_interrupted = 0;
+}
+
+
+void tu_intr_reset_() 
+{
+    tu_intr_reset();
+}
+
+void tu_intr_sighandler( sig ) 
+     int sig;
+{
+    was_interrupted++;
+    sigrelse( sig );
+
+}
+
+int tu_interrupt() 
+{
+    return was_interrupted;
+}
+
+int tu_interrupt_() 
+{
+    return tu_interrupt();
+}
+
+
+#endif /* irix */
+
+/****** sun4os5  *****************************************************/
+#ifdef sun4os5
+
+
+void tu_intr_set_();
+void tu_intr_reset_();
+int tu_interrupt_();
+
+#include <signal.h>
+
+static char *old_sig_func;
+static int old_sig_mask = 0;
+static int was_interrupted = 0;
+
+
+void tu_intr_set() 
+{
+    old_sig_func = ( char * ) signal( SIGINT, tu_intr_sighandler );
+    was_interrupted = 0;
+}
+
+
+void tu_intr_set_() 
+{
+    tu_intr_set();
+}
+
+
+void tu_intr_reset() 
+{
+    was_interrupted = 0;
+}
+
+
+void tu_intr_reset_() 
+{
+    tu_intr_reset();
+}
+
+void tu_intr_sighandler( sig ) 
+     int sig;
+{
+    was_interrupted++;
+    sigrelse( sig );
+
+}
+
+int tu_interrupt() 
+{
+    return was_interrupted;
+}
+
+int tu_interrupt_() 
+{
+    return tu_interrupt();
+}
+
+
+#endif /* sun4os5 */
+
+/****** aix  ******************************************************/
+#ifdef aix
+
+
+void tu_intr_set_();
+void tu_intr_reset_();
+int tu_interrupt_();
+
+#include <signal.h>
+
+
+static char *old_sig_func;
+static int old_sig_mask = 0;
+static int was_interrupted = 0;
+
+
+void tu_intr_set() 
+{
     int newmask, newestmask;
     
     old_sig_mask = sigblock( sigmask(SIGINT) );
@@ -40,20 +158,191 @@ void tu_intr_set()
     
     newmask = sigsetmask( newestmask );
 
-#ifdef DEB
-    printf(" SIGINT %x, sigmask(SIGINT) %x\n", SIGINT, sigmask(SIGINT));
-    printf(" newestmask %x\n",newestmask);
+    old_sig_func = ( char * ) signal( SIGINT, tu_intr_sighandler );
+    was_interrupted = 0;
+}
+
+void tu_intr_set_() 
+{
+    tu_intr_set();
+}
+
+
+void tu_intr_reset() 
+{
+    sigsetmask( old_sig_mask );
+    (void ) signal( SIGINT, old_sig_func );
+    was_interrupted = 0;
+}
+
+void tu_intr_reset_() 
+{
+    tu_intr_reset();
+}
+
+void tu_intr_sighandler( sig ) 
+     int sig;
+{
+    was_interrupted++;
+}
+
+int tu_interrupt() 
+{
+    return was_interrupted;
+}
+
+int tu_interrupt_() 
+{
+    return tu_interrupt();
+}
+
+#endif /* aix */
+
+/****** sun4  *** should be like aix **********************************/
+#ifdef sun4
+
+
+void tu_intr_set_();
+void tu_intr_reset_();
+int tu_interrupt_();
+
+#include <signal.h>
+
+
+static char *old_sig_func;
+static int old_sig_mask = 0;
+static int was_interrupted = 0;
+
+
+void tu_intr_set() 
+{
+    int newmask, newestmask;
     
-    printf("old mask %x,  new mask %x\n", old_sig_mask, newmask);
-#endif /* DEB */
+    old_sig_mask = sigblock( sigmask(SIGINT) );
+    newestmask = old_sig_mask & ~sigmask( SIGINT);
+    
+    newmask = sigsetmask( newestmask );
 
     old_sig_func = ( char * ) signal( SIGINT, tu_intr_sighandler );
+    was_interrupted = 0;
+}
 
-#else /*  IRIX */
-    old_sig_func = ( char * ) sigset( SIGINT, tu_intr_sighandler );
-#endif /* IRIX */
+void tu_intr_set_() 
+{
+    tu_intr_set();
+}
 
-#else /* VMS */
+
+void tu_intr_reset() 
+{
+    sigsetmask( old_sig_mask );
+    (void ) signal( SIGINT, old_sig_func );
+    was_interrupted = 0;
+}
+
+void tu_intr_reset_() 
+{
+    tu_intr_reset();
+}
+
+void tu_intr_sighandler( sig ) 
+     int sig;
+{
+    was_interrupted++;
+}
+
+int tu_interrupt() 
+{
+    return was_interrupted;
+}
+
+int tu_interrupt_() 
+{
+    return tu_interrupt();
+}
+
+#endif /* sun4 */
+
+/****** hpux  *** should be like aix **********************************/
+#ifdef hpux
+
+
+void tu_intr_set_();
+void tu_intr_reset_();
+int tu_interrupt_();
+
+#include <signal.h>
+
+
+static char *old_sig_func;
+static int old_sig_mask = 0;
+static int was_interrupted = 0;
+
+
+void tu_intr_set() 
+{
+    int newmask, newestmask;
+    
+    old_sig_mask = sigblock( sigmask(SIGINT) );
+    newestmask = old_sig_mask & ~sigmask( SIGINT);
+    
+    newmask = sigsetmask( newestmask );
+
+    old_sig_func = ( char * ) signal( SIGINT, tu_intr_sighandler );
+    was_interrupted = 0;
+}
+
+void tu_intr_set_() 
+{
+    tu_intr_set();
+}
+
+
+void tu_intr_reset() 
+{
+    sigsetmask( old_sig_mask );
+    (void ) signal( SIGINT, old_sig_func );
+    was_interrupted = 0;
+}
+
+void tu_intr_reset_() 
+{
+    tu_intr_reset();
+}
+
+void tu_intr_sighandler( sig ) 
+     int sig;
+{
+    was_interrupted++;
+}
+
+int tu_interrupt() 
+{
+    return was_interrupted;
+}
+
+int tu_interrupt_() 
+{
+    return tu_interrupt();
+}
+
+#endif /* hpux */
+
+/****** VMS *******************************************************/
+#ifdef VMS
+
+#include descrip
+#include iodef
+static int i_chan1 = 0;
+
+
+static char *old_sig_func;
+static int old_sig_mask = 0;
+static int was_interrupted = 0;
+
+
+void tu_intr_set() 
+{
     int i_stat;
     int i_func, i_mask[2];
     short i_iosb[4];
@@ -69,74 +358,25 @@ void tu_intr_set()
     i_stat = sys$qiow(0,i_chan1,i_func,i_iosb,0,0,
 		    tu_intr_sighandler,i_mask,
 		    0,0,0,0,0);
-
-#endif /* not VMS elif VMS */
     was_interrupted = 0;
 }
 
-#ifndef VMS
-
-void tu_intr_set_() 
-{
-    tu_intr_set();
-}
-
-#endif /* not VMS */
 
 void tu_intr_reset() 
 {
-#ifndef VMS
-
-#ifndef IRIX
-    sigsetmask( old_sig_mask );
-    (void ) signal( SIGINT, old_sig_func );
-
-#else /* IRIX */
-
-    (void) sigset( SIGINT, (void(*))old_sig_func );
-#endif /* IRIX */
-
-#endif /* not VMS */
     was_interrupted = 0;
 }
 
-#ifndef VMS
-
-void tu_intr_reset_() 
-{
-    tu_intr_reset();
-}
-
-#endif /* not VMS */
 
 void tu_intr_sighandler( sig ) 
      int sig;
 {
     was_interrupted++;
-
-#ifdef IRIX
-    sigrelse( sig );
-#endif /* IRIX */
-
-#ifdef DEB
-    printf("was_interrupted = %d\n",was_interrupted);
-#endif /* DEB */
-
 }
 
 int tu_interrupt() 
 {
-#ifdef DEB
-    printf("tu_interrupt %d\n",was_interrupted);
-#endif /* DEB */
     return was_interrupted;
 }
 
-#ifndef VMS
-
-int tu_interrupt_() 
-{
-    return tu_interrupt();
-}
-#endif /* not VMS */
-
+#endif /* VMS */
