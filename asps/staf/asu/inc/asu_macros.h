9ca0316b4314bf6b3ce4559c3d67bf21b3de2ff0
Craig E. Tull <tull>
1996-05-24 00:45:58 +0000

    macro definitions for ASU

---
+++ asps/staf/asu/inc/asu_macros.h
@@ -0,0 +1,32 @@
+/*:Copyright 1995, Lawrence Berkeley Laboratory
+*:>---------------------------------------------------------------------
+*:FILE:        asu_macros.h
+*:DESCRIPTION: cpp Macros fro ASP
+*:AUTHOR:      cet - Craig E. Tull, cetull@lbl.gov
+*:BUGS:        -- STILL IN DEVELOPMENT --
+*:HISTORY:     20may96-v002a-cet- split off EML,CORBA,SUT,etc.
+*:HISTORY:     25nov95-v001c-cet- rename from ASP.h to asp_macros.h
+*:HISTORY:     20nov95-v001b-cet- incorporate SOC.h MACROS
+*:HISTORY:     27jul95-v001a-cet- recreation (from *_object)
+*:<---------------------------------------------------------------------
+*/
+#ifndef ASU_MACROS_H
+#define ASU_MACROS_H
+
+#define ASU Analysis Service Utilities
+
+/*-------------------------------------------- INCLUDES             --*/
+/*** NONE ***/
+
+/*-------------------------------------------- MACROS               --*/
+/*- Don't use CORBA. #define CORBA -*/
+
+#include "stafGeneric.h"
+
+/*- Maximum number of objects in system. -*/
+#define OBJ_MAX_COUNT 2048
+
+/*-------------------------------------------- CORBA                --*/
+
+#endif /*ASU_MACROS_H*/
+
