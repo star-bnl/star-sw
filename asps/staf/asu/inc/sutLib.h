761f55fe3c60112a9d260b7dc06c216d247c2b2f
tull <>
1996-05-24 01:01:32 +0000

    String UTilities header file

---
+++ asps/staf/asu/inc/sutLib.h
@@ -0,0 +1,18 @@
+#ifndef SUTLIB_H
+#define SUTLIB_H
+
+#ifdef __cplusplus
+#define CC_P "C"
+#endif
+
+extern CC_P int sutMatchWild(char *pattern, char* string);
+extern CC_P int sutMatchReg(char *pattern, char* string);
+extern CC_P char* strntok(const char * str, const char * del
+		, const int n);
+extern CC_P int strsplit(const char * str, const char * del, char*** a);
+extern CC_P int strbracket(const char *str, const char * od
+		, const char * cd, char *** a);
+extern CC_P int isInteger(char *c);
+
+#endif /*SUTLIB_H*/
+
