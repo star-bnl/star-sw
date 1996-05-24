6624ec584b4204f7e4f1097bde9494fdde87f2c0
Craig E. Tull <tull>
1996-05-24 01:00:27 +0000

    generic macros and typedefs for STAF

---
+++ asps/staf/asu/inc/stafGeneric.h
@@ -0,0 +1,22 @@
+/*- stafGeneric.h - generic macros for STAF -*/
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef				  CC_P
+#ifdef __cplusplus
+#define CC_P "C"
+#else
+#define CC_P
+#endif
+#endif				/*CC_P*/
+
