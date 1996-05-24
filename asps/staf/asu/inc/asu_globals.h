cb93b8d0867dc0332497e2c878654c7347dcafe9
Craig E. Tull <tull>
1996-05-24 00:46:37 +0000

    global variables for ASU

---
+++ asps/staf/asu/inc/asu_globals.h
@@ -0,0 +1,19 @@
+/*:Copyright 1995, Lawrence Berkeley National Laboratory
+*:>---------------------------------------------------------------------
+*:FILE:         asu_globals.h
+*:DESCRIPTION:  Global variables for ASU
+*:AUTHOR:       cet - Craig E. Tull, cetull@lbl.gov
+*:BUGS:         -- STILL IN DEVELOPMENT --
+*:HISTORY:      14nov95-v000a-cet- creation
+*:<---------------------------------------------------------------------
+*/
+#ifndef ASU_GLOBALS_H
+#define ASU_GLOBALS_H
+
+/*-------------------------------------------- INCLUDES             --*/
+/*-------------------------------------------- TYPEDEFS             --*/
+/*-------------------------------------------- GLOBALS              --*/
+
+
+#endif /* ASU_GLOBALS_H */
+
