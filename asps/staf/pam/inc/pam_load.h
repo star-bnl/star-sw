b86aeb22a4b2b1a696d6f6fce8043993e3fcdc4a
Craig E. Tull <tull>
1996-05-28 23:06:19 +0000

    include files for loading pam PAMs

---
+++ asps/staf/pam/inc/pam_load.h
@@ -0,0 +1,9 @@
+/* pam_load.h */
+/*- includes for pam functions. -*/
+#include "pamc.h"
+#include "pamf.h"
+#include "pam0.h"
+#include "pam1_gen_tbl1.h"
+#include "pam2_fill_tbl2.h"
+#include "pam3_show_tbls.h"
+
