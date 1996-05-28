294e5b2ba96607cc4e6d343bcac17d7b199137c8
tull <>
1996-05-28 22:22:25 +0000

    full header file for TDM

---
+++ asps/staf/tdm/inc/tdmLib.h
@@ -0,0 +1,10 @@
+#include "asuLib.h"
+#include "emlLib.h"
+#include "socLib.h"
+#include "tdm_macros.h"
+#include "tdm_types.h"
+#include "tdm_codes.h"
+#ifdef __cplusplus
+#include "tdmClasses.hh"
+#endif
+#include "tdm_globals.h"
