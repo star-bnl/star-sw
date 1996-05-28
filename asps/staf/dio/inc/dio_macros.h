4ea496e0ff6dc25332103f5c6429947ac3681ca8
tull <>
1996-05-28 22:45:22 +0000

    macro definitions for DIO

---
+++ asps/staf/dio/inc/dio_macros.h
@@ -0,0 +1,25 @@
+/* Copyright 1995, Lawrence Berkeley National Laboratory
+*:>---------------------------------------------------------------------
+*:FILE:		dio_macros.h
+*:DESCRIPTION:	Analysis Service Package template header file
+*:AUTHOR:	cet - Craig E. Tull, cetull@lbl.gov
+*:BUGS:		-- STILL IN DEVELOPMENT --
+*:HISTORY:	12dec95-v000a-cet- creation
+*:<---------------------------------------------------------------------
+*/
+#ifndef DIO_MACROS_H
+#define DIO_MACROS_H
+
+#define DIO Dataset Input/Output
+
+/*-------------------------------------------- INCLUDES             --*/
+#include "soc_macros.h"
+
+/*-------------------------------------------- MACROS               --*/
+/*- Don't use CORBA (by default).
+#define CORBA -*/
+
+#define DIOFILESTREAM(OBJ) ((dioFileStream*)(OBJ->ptr()))
+
+#endif /* DIO_MACROS_H */
+
