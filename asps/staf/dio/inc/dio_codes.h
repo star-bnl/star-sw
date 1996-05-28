acb28ee7346efcf5e5b727cd021d652d67b21c64
Craig E. Tull <tull>
1996-05-28 22:45:37 +0000

    error codes for DIO

---
+++ asps/staf/dio/inc/dio_codes.h
@@ -0,0 +1,14 @@
+/*:Copyright 1995, Lawrence Berkeley National Laboratory
+*:>---------------------------------------------------------------------
+*:FILE:         dio_codes.h
+*:DESCRIPTION:  Error codes for DIO
+*:AUTHOR:       cet - Craig E. Tull, cetull@lbl.gov
+*:BUGS:         -- STILL IN DEVELOPMENT --
+*:HISTORY:      12dec95-v000a-cet- creation
+*:<---------------------------------------------------------------------
+*/
+#ifndef DIO_CODES_H
+#define DIO_CODES_H
+
+#endif /* DIO_CODES_H */
+
