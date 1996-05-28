8e2ad442face8277245bd1e17c0e03e0a4e41104
tull <>
1996-05-28 22:15:56 +0000

    full header file for SPX

---
+++ asps/staf/spx/inc/spxLib.h
@@ -0,0 +1,9 @@
+#include "asuLib.h"
+#include "socLib.h"
+#include "spx_macros.h"
+#include "spx_types.h"
+#include "spx_codes.h"
+#ifdef __cplusplus
+#include "spxClasses.hh"
+#endif
+#include "spx_globals.h"
