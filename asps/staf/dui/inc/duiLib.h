c7a49225fb08f1853a2ec6a02023bdd9ffd94122
tull <>
1996-05-28 22:33:25 +0000

    full header file for DUI

---
+++ asps/staf/dui/inc/duiLib.h
@@ -0,0 +1,10 @@
+#include "asuLib.h"
+#include "socLib.h"
+#include "tdmLib.h"
+#include "dui_macros.h"
+#include "dui_types.h"
+#include "dui_codes.h"
+#ifdef __cplusplus
+#include "duiClasses.hh"
+#endif
+#include "dui_globals.h"
