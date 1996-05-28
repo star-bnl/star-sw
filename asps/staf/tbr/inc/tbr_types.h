a8c5b861bbc6d928de63e69253f4301fb3fc3f4b
tull <>
1996-05-28 23:00:25 +0000

    variable type definitions for TBR

---
+++ asps/staf/tbr/inc/tbr_types.h
@@ -0,0 +1,56 @@
+/*:Copyright 1996, Lawrence Berkeley National Laboratory
+*:>---------------------------------------------------------------------
+*:FILE:         tbr_types.h
+*:DESCRIPTION:  Variable types for TBR
+*:AUTHOR:       cet - Craig E. Tull, cetull@lbl.gov
+*:BUGS:         -- STILL IN DEVELOPMENT --
+*:HISTORY:      11mar96-v000a-cet,hjw- creation
+*:<---------------------------------------------------------------------
+*/
+#ifndef TBR_TYPES_H
+#define TBR_TYPES_H
+
+/*-------------------------------------------- INCLUDES             --*/
+#include "dstype.h"
+#include "tdmClasses.hh"
+#include "tbr_macros.h"
+
+#ifdef CORBA
+#include "tbr_i.hh"
+#endif
+
+/*-------------------------------------------- CORBA                --*/
+#ifndef CORBA
+
+#endif /*CORBA*/
+
+/*-------------------------------------------- TYPEDEFS             --*/
+#ifndef CORBA
+
+#endif /*CORBA*/
+
+/*-------------------------------------------- GLOBALS              --*/
+/*-------------------------------------------- PROTOTYPES           --*/
+extern CC_P int tbr_init();
+extern CC_P int tbr_start();
+extern CC_P int tbr_stop();
+extern CC_P void tbr_def_();
+
+#ifndef NOKUIP
+extern CC_P void kam_tbr_count_();
+extern CC_P void kam_tbr_list_();
+extern CC_P void kam_tbr_motif_viewdataset_();
+extern CC_P void kam_tbr_ascii_setvalue_();
+extern CC_P void kam_tbr_ascii_getvalue_();
+extern CC_P void kam_tbr_ascii_print_();
+
+extern CC_P int kam_tbr_count();
+extern CC_P int kam_tbr_list();
+extern CC_P int kam_tbr_motif_viewdataset();
+extern CC_P int kam_tbr_ascii_setvalue();
+extern CC_P int kam_tbr_ascii_getvalue();
+extern CC_P int kam_tbr_ascii_print();
+#endif /*NOKUIP*/
+
+#endif /* TBR_TYPES_H */
+
