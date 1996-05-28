c59e0832da5e52685d3a8d3407f460e9f8645254
tull <>
1996-05-28 22:26:18 +0000

    init, start, and stop routines for TDM

---
+++ asps/staf/tdm/src/tdm_init.cc
@@ -0,0 +1,63 @@
+//:Copyright 1995, Lawrence Berkeley National Laboratory
+//:>--------------------------------------------------------------------
+//:FILE:	tdm_init.C
+//:DESCRIPTION:	Functions  to initialize TDM 
+//:AUTHOR:	cet - Craig E. Tull, cetull@lbl.gov
+//:BUGS:	-- STILL IN DEVELOPMENT --
+//:HISTORY:	29nov95-v000a-cet- creation
+//:<--------------------------------------------------------------------
+
+#include "tdmLib.h"
+
+tdmFactory *tdm;
+
+//:>--------------------------------------------------------------------
+//:ROUTINE:	int tdm_init()
+//:DESCRIPTION:	Initialize TDM
+//:ARGUMENTS:	-- NONE --
+//:RETURN VALUE:-- NONE --
+//:<--------------------------------------------------------------------
+int tdm_init()
+{
+   EML_MESSAGE("Initializing TDM.");
+
+/*- Define the TDM KUIP commands. -*/
+   tdm_def_();
+
+   return TRUE;
+}
+
+//:>--------------------------------------------------------------------
+//:ROUTINE:	int tdm_start()
+//:DESCRIPTION:	Start TDM
+//:ARGUMENTS:	-- NONE --
+//:RETURN VALUE:-- NONE --
+//:<--------------------------------------------------------------------
+int tdm_start()
+{
+   EML_MESSAGE("Starting TDM.");
+
+/*- Create the TDM Factory. -*/
+   tdm = new tdmFactory("tdm");
+
+   return TRUE;
+}
+
+//:>--------------------------------------------------------------------
+//:ROUTINE:	int tdm_stop()
+//:DESCRIPTION:	Stop TDM
+//:ARGUMENTS:	-- NONE --
+//:RETURN VALUE:-- NONE --
+//:<--------------------------------------------------------------------
+int tdm_stop()
+{
+   EML_MESSAGE("Stopping TDM.");
+
+/*- Delete the TDM Factory.
+   delete tdm;
+unecessary -- soc will do it. -*/
+
+   dsDatasetAllocStats();		/* show allocation stats */
+   return TRUE;
+}
+
