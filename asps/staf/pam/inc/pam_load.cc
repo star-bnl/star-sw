8d75fef1d99181380d5f702bbee8fedc685bdf12
tull <>
1996-05-28 23:06:19 +0000

    include files for loading pam PAMs

---
+++ asps/staf/pam/inc/pam_load.cc
@@ -0,0 +1,9 @@
+/* pam_load.c */
+/*- load calls for pam functions. -*/
+   pamc_load_ami(broker);
+   pamf_load_ami(broker);
+   pam0_load_ami(broker);
+   pam1_gen_tbl1_load_ami(broker);
+   pam2_fill_tbl2_load_ami(broker);
+   pam3_show_tbls_load_ami(broker);
+
