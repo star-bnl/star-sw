4e4b9758fe830c08550b8d2c5cf42e45b41214aa
tull <>
1996-05-28 22:33:07 +0000

    error codes for DUI

---
+++ asps/staf/dui/inc/dui_codes.h
@@ -0,0 +1,14 @@
+/*:Copyright 1995, Lawrence Berkeley National Laboratory
+*:>---------------------------------------------------------------------
+*:FILE:         dui_codes.h
+*:DESCRIPTION:  Error codes for DUI
+*:AUTHOR:       cet - Craig E. Tull, cetull@lbl.gov
+*:BUGS:         -- STILL IN DEVELOPMENT --
+*:HISTORY:      29nov95-v000a-cet- creation
+*:<---------------------------------------------------------------------
+*/
+#ifndef DUI_CODES_H
+#define DUI_CODES_H
+
+#endif /* DUI_CODES_H */
+
