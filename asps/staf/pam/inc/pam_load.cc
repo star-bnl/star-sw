bdca37fd8bc77e6d7d93f386e36e278d252f41ad
Craig E. Tull <tull>
1996-09-24 18:24:32 +0000

    SL96b - Release Version -

--- asps/staf/pam/inc/pam_load.cc
+++ asps/staf/pam/inc/pam_load.cc
@@ -2,8 +2,4 @@
 /*- load calls for pam functions. -*/
    pamc_load_ami(broker);
    pamf_load_ami(broker);
-   pam0_load_ami(broker);
-   pam1_gen_tbl1_load_ami(broker);
-   pam2_fill_tbl2_load_ami(broker);
-   pam3_show_tbls_load_ami(broker);
 
