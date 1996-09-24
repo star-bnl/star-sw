ba60fc6ba9abc8a0a0b7d96f2c035c9cf779bb1e
tull <>
1996-09-24 23:32:40 +0000

    Initialization file generated by pamiGen

---
+++ asps/staf/pam/src/pam_init.cc
@@ -0,0 +1,10 @@
+/* automatically generated file -- DO NOT EDIT */
+#include "pamc.h"
+#include "pamf.h"
+extern "C" int pam_init(void), pam_start(void), pam_stop(void);
+int pam_init() { return 1; }
+int pam_start() {
+	pamc_load_ami(ami);
+	pamf_load_ami(ami);
+return 1; }
+int pam_stop() { return 1; }
