de3a5fede56655886ccb0de9a04ecc28b7b84d13
Craig E. Tull <tull>
1996-09-25 20:55:41 +0000

    sun4os5

--- asps/staf/asu/SLM_DIR/sun4os5.mk
+++ asps/staf/asu/SLM_DIR/sun4os5.mk
@@ -69,7 +69,8 @@ SUNW_CRTN := /vol/lic/SUNWspro-3.0/SC3.0/lib/crtn.o
 SUNW_CRTN := /opt/SUNWspro/SC4.0/lib/crtn.o
 endif
 export FOR_LIBS += $(addprefix -L,$(SUNW_LDIRS)) \
-	-lM77 -lF77 -lsunmath -lm -lc -L/usr/ucblib -lucb \
+	-lM77 -lF77 -lsunmath -lm -lc \
+	-R/usr/ucblib -L/usr/ucblib -lucb \
 	$(SUNW_CRTN)
 #
 # X-Motif Libraries
