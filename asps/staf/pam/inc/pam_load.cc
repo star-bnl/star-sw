5f11592b439a3e2f8a548395988d6f45fe4b2171
tull <>
1996-10-24 19:23:53 +0000

    OBSOLETE load

--- asps/staf/pam/inc/pam_load.cc
+++
@@ -1,5 +0,0 @@
-/* pam_load.c */
-/*- load calls for pam functions. -*/
-   pamc_load_ami(broker);
-   pamf_load_ami(broker);
-
--- asps/staf/pam/inc/pam_load.h
+++
@@ -1,5 +0,0 @@
-/* pam_load.h */
-/*- includes for pam functions. -*/
-#include "pamc.h"
-#include "pamf.h"
-
