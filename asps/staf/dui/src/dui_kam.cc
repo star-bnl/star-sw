91c9890ce94926b650cd9065370af795898d0587
Craig E. Tull <tull>
1997-01-07 01:32:29 +0000

    DEV: pre-edit checkin

--- asps/staf/dui/src/dui_kam.cc
+++ asps/staf/dui/src/dui_kam.cc
@@ -35,12 +35,15 @@
 *:* DUI/CD [ PATH ]
 *:<---------------------------------------------------------------------
 */
-void kam_dui_cd_(){kam_dui_cd();}
-int kam_dui_cd()
+void kam_dui_cd_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  path = ku_gets();	/* path */
 
+	STAFCV_T status = dui_cd(path);
+}
+STAFCV_T dui_cd(char* path)
+{
    if( !dui->cd(path) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
@@ -56,13 +59,16 @@ int kam_dui_cd()
 *:* DUI/CP FROM TO
 *:<---------------------------------------------------------------------
 */
-void kam_dui_cp_(){kam_dui_cp();}
-int kam_dui_cp()
+void kam_dui_cp_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  fromPath = ku_gets();	/* path */
    char*  toPath = ku_gets();	/* path */
 
+	STAFCV_T status = dui_cp(fromPath, toPath);
+}
+STAFCV_T dui_cp(char* fromPath, char* toPath)
+{
    if( !dui->cp(fromPath,toPath) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
@@ -78,13 +84,15 @@ int kam_dui_cp()
 *:* DUI/LS [ PATH ]
 *:<---------------------------------------------------------------------
 */
-void kam_dui_ls_(){kam_dui_ls();}
-int kam_dui_ls()
+void kam_dui_ls_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  path = ku_gets();	/* path */
 
-
+	STAFCV_T status = dui_ls(path);
+}
+STAFCV_T dui_ls(char* path)
+{
    char * result = (char*)ASUALLOC(2048);
    strncpy(result,"",1);
    if( !dui->ls(path, result) ){
@@ -103,12 +111,15 @@ int kam_dui_ls()
 *:* DUI/MKDIR PATH
 *:<---------------------------------------------------------------------
 */
-void kam_dui_mkdir_(){kam_dui_mkdir();}
-int kam_dui_mkdir()
+void kam_dui_mkdir_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  path = ku_gets();	/* path */
 
+	STAFCV_T status = dui_mkdir(path);
+}
+STAFCV_T dui_mkdir(char* path)
+{
    if( !dui->mkdir(path) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
@@ -124,13 +135,16 @@ int kam_dui_mkdir()
 *:* DUI/MV FROM TO
 *:<---------------------------------------------------------------------
 */
-void kam_dui_mv_(){kam_dui_mv();}
-int kam_dui_mv()
+void kam_dui_mv_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  fromPath = ku_gets();	/* fromPath */
    char*  toPath = ku_gets();	/* toPath */
 
+	STAFCV_T status = dui_mv(fromPath, toPath);
+}
+STAFCV_T dui_mv(char* fromPath, char* toPath)
+{
    if( !dui->mv(fromPath,toPath) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
@@ -146,11 +160,14 @@ int kam_dui_mv()
 *:* DUI/PWD
 *:<---------------------------------------------------------------------
 */
-void kam_dui_pwd_(){kam_dui_pwd();}
-int kam_dui_pwd()
+void kam_dui_pwd_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
 
+	STAFCV_T status = dui_pwd();
+}
+STAFCV_T dui_pwd()
+{
    char* result=NULL;
 
    if( !dui->pwd(result) ){
@@ -169,12 +186,15 @@ int kam_dui_pwd()
 *:* DUI/RM PATH
 *:<---------------------------------------------------------------------
 */
-void kam_dui_rm_(){kam_dui_rm();}
-int kam_dui_rm()
+void kam_dui_rm_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  path = ku_gets();	/* path */
 
+	STAFCV_T status = dui_rm(path);
+}
+STAFCV_T dui_rm(char* path)
+{
    if( !dui->rm(path) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
@@ -190,12 +210,15 @@ int kam_dui_rm()
 *:* DUI/RMDIR PATH
 *:<---------------------------------------------------------------------
 */
-void kam_dui_rmdir_(){kam_dui_rmdir();}
-int kam_dui_rmdir()
+void kam_dui_rmdir_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char*  path = ku_gets();	/* path */
 
+	STAFCV_T status = dui_rmdir(path);
+}
+STAFCV_T dui_rmdir(char* path)
+{
    if( !dui->rmdir(path) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
@@ -211,14 +234,17 @@ int kam_dui_rmdir()
 *:* DUI/MKTABLE NAME SPEC ROWCOUNT
 *:<---------------------------------------------------------------------
 */
-void kam_dui_mktable_(){kam_dui_mktable();}
-int kam_dui_mktable()
+void kam_dui_mktable_()
 {
    long npars = ku_npar();      /* number of KUIP parameters */
    char* name = ku_gets();      /* table name */
    char* spec = ku_gets();      /* table row specifier */
    long rowcount = ku_geti();   /* rows to allocate */
 
+	STAFCV_T status = dui_mktable(name,spec,rowcount);
+}
+STAFCV_T dui_mktable(char* name,char* spec,long rowcount)
+{
    if( !dui->mkTable(name,spec,rowcount) ){
       EML_ERROR(KAM_METHOD_FAILURE);
    }
