96d612485a9031fc5a8b4bef9c80cb1ac289c4d9
olson <>
1997-01-16 19:51:20 +0000

    add sun4os5pc for Solaris Intel

--- asps/staf/asu/SLM_DIR/env_v.mk
+++ asps/staf/asu/SLM_DIR/env_v.mk
@@ -5,11 +5,11 @@ ifndef ENV_V_MK
 export ENV_V_MK += DONE
 #
 export ENVDIRS := bin cdf doc idl inc lib solib src srcm wrk \
-	aix hpux irix sun4 sun4os5 osf1 \
-	bin/aix bin/hpux bin/irix bin/sun4 bin/sun4os5 bin/osf1 \
-	lib/aix lib/hpux lib/irix lib/sun4 lib/sun4os5 lib/osf1 \
+	aix hpux irix sun4 sun4os5 sun4os5pc osf1 \
+	bin/aix bin/hpux bin/irix bin/sun4 bin/sun4os5 bin/sun4os5pc bin/osf1 \
+	lib/aix lib/hpux lib/irix lib/sun4 lib/sun4os5 lib/sun4os5pc lib/osf1 \
 	solib/aix solib/hpux solib/irix solib/sun4 solib/sun4os5 \
-			solib/osf1
+			solib/sun4os5pc solib/osf1
 export ENVFILES := Makefile.control Makefile.user
 export ENVMAKES := Makefile \
 	bin/Makefile cdf/Makefile doc/Makefile idl/Makefile \
