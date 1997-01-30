c52682a946df2033d3a6db254736f58dc2e4e710
Craig E. Tull <tull>
1997-01-30 23:18:17 +0000

    DEV: pre-SL97b: asu soc spx tdm dui dio ami pam (dsl dsu)

--- asps/staf/pam/src/pam_init.cc
+++ asps/staf/pam/src/pam_init.cc
@@ -1,10 +1,12 @@
 /* automatically generated file -- DO NOT EDIT */
 #include "pamc.h"
+#include "pamcc.h"
 #include "pamf.h"
 extern "C" int pam_init(void), pam_start(void), pam_stop(void);
 int pam_init() { return 1; }
 int pam_start() {
 	pamc_load_ami(ami);
+	pamcc_load_ami(ami);
 	pamf_load_ami(ami);
 return 1; }
 int pam_stop() { return 1; }
