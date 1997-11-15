9b8ffc59c6ee7f8889c758c7d9f00fa5c3cdca11
tull <>
1997-11-15 02:55:17 +0000

    shortname added

--- asps/staf/soc/src/soc_utils.c
+++ asps/staf/soc/src/soc_utils.c
@@ -1,7 +1,11 @@
 #include "asuAlloc.h"
+#include <math.h>
+#include <strings.h>
 
 extern char *id2name(char *base, long id);
+extern char *shortname(char *longname, size_t length);
 
+/*--------------------------------------------------------------------*/
 char *id2name(char *base, long id)
 {
    char *name;
@@ -16,4 +20,27 @@ char *id2name(char *base, long id)
       sprintf(name,"%s%d",base,id);
       return name;
    }
+}
+/*--------------------------------------------------------------------*/
+char *shortname(char *longname, size_t length)
+{
+	char *n=(char*)MALLOC(length +1);
+	size_t l1=(length-1)/2;
+	size_t l2=(length-l1-1);
+	size_t ll=(strlen(longname)-l2);
+	char *nn;
+	char *b;
+
+	n[length] = NULL;
+	if( length < strlen(longname) ){
+		strncpy(n,longname,l1);
+		nn = n; nn += (l1+1);
+		b = longname; b += ll;
+		strncpy(nn,b,l2);
+		n[l1] = '~';
+	}
+	else {
+		strncpy(n,longname,length);
+	}
+	return n;
 }
