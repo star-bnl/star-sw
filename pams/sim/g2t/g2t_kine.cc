4cdf5d8b5834f1e9a940f11195ac23a414b4df72
nevski <>
1998-01-03 23:46:37 +0000

    new g2t pam-module prototype

--- pams/sim/g2t/g2t_kine.cc
+++ pams/sim/g2t/g2t_kine.cc
@@ -1,32 +1,42 @@
 /*:>--------------------------------------------------------------------
 **: FILE:       g2t_kine.cc
 **: HISTORY:
-**:             00jan93-v000a-hpl- Created by stic Version
-**:  Id: idl.y,v 1.26 1997/11/10 19:59:13 ward Exp  
+**:             15dec97-v000a-Yuri Fisyak
+**:             31dec97-v001a-Pavel Nevski
 **:<------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "g2t_kine.h"
+#include "g2t_hits.h"
 #include "geant_gcnum.h"
 long g2t_kine_()
 {
-   long npars = 2;      /* number of tables  */
-   char*  pname = "g2t_get_kine";     /* PAM name */
-   long ntrack = Gcnum.ntrack;
-   long nvertx = Gcnum.nvertx;
-   char *names[] = {"g2t_vertex","g2t_track"}; /*  array of TABLE names */
-   char *tnames[2]; 
-   printf (" Got Ntrack=%d, Nvertx=%d \n", Gcnum.ntrack,Gcnum.nvertx);
-   tnames[0] = names[0]; 
-   tnames[1] = names[1]; 
+   long  npars   = 3;                   /* number of tables  */
+   char* pname   = "g2t_get_kine";      /* first PAM name    */
+   char* hname   = "g2t_get_hits";      /* second PAM name   */
+   long  ntrack  = Gcnum.ntrack;
+   long  nvertx  = Gcnum.nvertx;
+   char* names[] = {"g2t_vertex","g2t_track","g2t_hits"}; /* TABLE names */
+   STAFCV_T status;
+
+   printf (" in g2t got Ntrack=%d, Nvertx=%d \n", Gcnum.ntrack,Gcnum.nvertx);
+
    /*             name      spec      Nrow  */
    tdm_newtable (names[0], names[0], nvertx);
    tdm_newtable (names[1], names[1], ntrack);
+   tdm_newtable (names[2], names[2], 100000);
+
    tdmtable_maxrowcount(names[0], nvertx);
    tdmtable_maxrowcount(names[1], ntrack);
-   printf (" tables ready, Calling g2t_kine \n"); 
-   STAFCV_T status = ami_call(pname,npars, tnames);
+   tdmtable_maxrowcount(names[2], 100000);
+
+   printf (" -> Calling g2t_kine \n"); 
+   status = ami_call(pname,2,names);
+
+   printf (" -> Calling g2t_hits \n"); 
+   status = ami_call(hname,3,names);
+
    return STAFCV_OK;
 }
 
