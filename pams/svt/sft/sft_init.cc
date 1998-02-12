869d919d41f08f430dfb37b5b1c0ee8fee814c91
Yuri Fisyak <fisyak@bnl.gov>
1998-02-12 16:32:13 +0000

    Remove sft, NO module idl-file

--- pams/svt/sft/sft_init.cc
+++
@@ -1,8 +0,0 @@
-/* automatically generated file -- DO NOT EDIT */
-#include "sft_am.h"
-extern "C" int sft_init(void), sft_start(void), sft_stop(void);
-int sft_init() { return 1; }
-int sft_start() {
-	sft_am_load_ami(ami);
-return 1; }
-int sft_stop() { return 1; }
