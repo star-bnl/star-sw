6de1edc0678931684a1b3b557fb69f078124158d
David Morrison <dave>
1998-05-18 16:06:17 +0000

    clean up dlfcn.h include logic

--- asps/staf/soc/inc/cdl.h
+++ asps/staf/soc/inc/cdl.h
@@ -1,48 +1,41 @@
-#ifndef _CDL_INCLUDED
-#define _CDL_INCLUDED
+#ifndef __CDL_H__
+#define __CDL_H__
 
-#ifdef HPUX
+#if defined(HPUX)
 #include <stdlib.h>
 #include <dl.h>
 void perror();
-#endif
-#if defined(SUN) || defined(SGI) || defined(ALPHA_OSF)
+#elif defined(AIX)
+#include "dlfcn.h"
+#else
 #include <dlfcn.h>
 #endif
-#ifdef AIX
-#include "dlfcn.h"
-#endif
-
-#include <string.h>
-#include <stdio.h>
 
 typedef enum {
-      UNDEF_T,
-      VOID_T,
-      VOID_P,
-      CHAR_T,
-      CHAR_P,
-      LOGICAL_T,
-      LOGICAL_P,
-      INTEGER_T,
-      INTEGER_P,
-      REAL_T,
-      REAL_P,
-      DOUBLE_T,
-      DOUBLE_P,
-      COMPLEX_T,
-      COMPLEX_P,
-      LAST_T
-    } ArgType;
+  UNDEF_T,
+  VOID_T,
+  VOID_P,
+  CHAR_T,
+  CHAR_P,
+  LOGICAL_T,
+  LOGICAL_P,
+  INTEGER_T,
+  INTEGER_P,
+  REAL_T,
+  REAL_P,
+  DOUBLE_T,
+  DOUBLE_P,
+  COMPLEX_T,
+  COMPLEX_P,
+  LAST_T
+} ArgType;
 
 typedef 
-#ifdef HPUX
+#if defined(HPUX)
    shl_t 
-#endif
-#if defined(SUN) || defined(SGI) || defined(SGI64)
+#elif defined(SUN) || defined(SGI) || defined(SGI64)
    int *
-#endif
-#if defined(ALPHA_OSF) || defined(AIX)
+#else
    void *
 #endif
   FileHandle;
@@ -51,7 +44,7 @@ typedef struct files {
    struct files  *next;
    char          *filename;
    FileHandle     file_handle;
- } Files, *FilesPtr;
+} Files, *FilesPtr;
 
 typedef void (*GenFuncPtr)();
 
@@ -63,34 +56,29 @@ typedef struct FuncDesc {
    ArgType            type;
    short              narg;
    ArgType           *arglist;
- } FuncDesc, *FuncPtr;
-
-static FuncPtr procList = NULL;
-
-static FilesPtr fileList = NULL;
-
-static int debug_level = 0;
+} FuncDesc, *FuncPtr;
 
 struct cdldefs{
-    char path[72], f77[72], cc[72], CC[72], ld[72], lang[4];
-  };
-#ifdef __cplusplus
-     extern "C" {
-#endif
-
- extern void  cdl_init(void);
- extern void  cdl_set_opt(char *text, char *chvar);
- extern int   cdl_create(char *filename);
- extern int   cdl_load(char *lib_name);
- extern int   cdl_unload(char *lib_name);
- extern void *cdl_func_addr(char *func_name);
- extern char *cdl_get_lib(int *ns );
- extern void  cdl_symbols(char *lib_name, int *ns, char *symbol);
- extern FuncPtr cdl_get_func(char *func_name);
- extern int cdl_proto(char *func_proto);
+  char path[72], f77[72], cc[72], CC[72], ld[72], lang[4];
+};
 
 #ifdef __cplusplus
-     }
+extern "C" {
 #endif
 
+  extern void  cdl_init(void);
+  extern void  cdl_set_opt(char *text, char *chvar);
+  extern int   cdl_create(char *filename);
+  extern int   cdl_load(char *lib_name);
+  extern int   cdl_unload(char *lib_name);
+  extern void *cdl_func_addr(char *func_name);
+  extern char *cdl_get_lib(int *ns );
+  extern void  cdl_symbols(char *lib_name, int *ns, char *symbol);
+  extern FuncPtr cdl_get_func(char *func_name);
+  extern int cdl_proto(char *func_proto);
+  
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* __CDL_H__ */
