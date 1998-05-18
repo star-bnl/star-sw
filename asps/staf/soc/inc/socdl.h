94452284322a0431c7fc03bce8c84f2e51b702ba
David Morrison <dave>
1998-05-18 17:31:57 +0000

    add decl for soc_dl_load

--- asps/staf/soc/inc/socdl.h
+++ asps/staf/soc/inc/socdl.h
@@ -1,9 +1,17 @@
+#ifndef __SOCDL_H__
+#define __SOCDL_H__
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+int soc_dl_load(char *pkgName);
 int soc_dl_init(char *pkgName);
 int soc_dl_start(char *pkgName);
 int soc_dl_stop(char *pkgName);
+
 #ifdef __cplusplus
 }
 #endif
+
+#endif /* __SOCDL_H__ */
