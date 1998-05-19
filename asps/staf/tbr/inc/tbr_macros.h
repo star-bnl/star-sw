7083c7f1649d917cc335cdad7f986d4d7d8b6267
David Morrison <dave>
1998-05-19 15:58:24 +0000

    do not use C++ style comments in header file

--- asps/staf/tbr/inc/tbr_macros.h
+++ asps/staf/tbr/inc/tbr_macros.h
@@ -1,13 +1,5 @@
-//:Copyright 1996, Lawrence Berkeley National Laboratory
-//:>--------------------------------------------------------------------
-//:FILE:        tbr_macros.h
-//:DESCRIPTION: TBR macros header file
-//:AUTHOR:      cet - Craig E. Tull, cetull@lbl.gov
-//:BUGS:        -- STILL IN DEVELOPMENT --
-//:HISTORY:     11mar96-v000a-cet,hjw- creation
-//:<--------------------------------------------------------------------
-#ifndef TBR_MACROS_H
-#define TBR_MACROS_H
+#ifndef __TBR_MACROS_H__
+#define __TBR_MACROS_H__
 
 #define TBR Table BRowser
 
@@ -18,5 +10,4 @@
 /*- Don't use CORBA (by default).
 #define CORBA -*/
 
-#endif /* TBR_MACROS_H */
-
+#endif /* __TBR_MACROS_H__ */
