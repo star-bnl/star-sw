e5274dedab55da03030e34e680bc13ad4596902f
fisyak <>
1998-05-26 18:32:47 +0000

    fix missing sys/systeminfo.h for Linux

--- asps/staf/msg/src/msgPublic.c
+++ asps/staf/msg/src/msgPublic.c
@@ -42,7 +42,7 @@ static const char sccsid[] = "@(#)"__FILE__"\t\t1.55\tCreated 3/8/98 03:34:14, \
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
-#ifndef _AIX
+#if !defined(_AIX) && !defined(Linux) && !defined(hpux)
 #include <sys/systeminfo.h>
 #endif
 
