7894008a93bf34664ffb0d1630c16a555feb049b
ward <>
1998-06-15 21:10:46 +0000

    Reinstall correct MakeArch.mk

--- asps/staf/MakeArch.mk
+++ asps/staf/MakeArch.mk
@@ -1,6 +1,6 @@
 #  $Log: MakeArch.mk,v $
-#  Revision 1.11  1998/06/15 21:08:32  ward
-#  break up big string in function soc_list
+#  Revision 1.12  1998/06/15 21:10:46  ward
+#  Reinstall correct MakeArch.mk
 #
 #  Revision 1.10  1998/05/19 16:36:38  perev
 #  Makefiles
@@ -23,7 +23,7 @@
 #  Revision 1.1.1.1  1997/12/31 14:35:23  fisyak
 #  Revision ?.?.?.?  1998/02/07           perev
 #
-#             Last modification $Date: 1998/06/15 21:08:32 $ 
+#             Last modification $Date: 1998/06/15 21:10:46 $ 
 #. default setings
 
 
@@ -214,8 +214,8 @@ endif
 ifneq (,$(findstring $(STAF_ARCH),sun4x_55 sun4x_56))
 
   OSFID := sun SUN SOLARIS solaris Solaris
-  CC :=  insure
-  CXX := insure
+  CC :=  /opt/SUNWspro/bin/cc
+  CXX := /opt/SUNWspro/bin/CC
   LD  := $(CXX)
   SO  := $(CXX)
   FC  := /opt/SUNWspro/bin/f77
@@ -233,8 +233,8 @@ endif
 
 ifneq (,$(findstring $(STAF_ARCH),sunx86_55))
   OSFID := sun SUN SOLARIS SOLARISPC
-  CC       :=  insure
-  CXX      := insure
+  CC       :=  /opt/SUNWspro/bin/cc
+  CXX      := /opt/SUNWspro/bin/CC
   SO       := $(CXX)
   FC       := /opt/SUNWspro/bin/f77
   LD       := $(CXX)
