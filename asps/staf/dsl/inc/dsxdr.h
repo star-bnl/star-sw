1146184a6874817068bb12f1485de442b6fc0657
dave <>
1998-06-17 17:32:18 +0000

    Linux has a broken version of XDR_DESTROY

--- asps/staf/dsl/inc/dsxdr.h
+++ asps/staf/dsl/inc/dsxdr.h
@@ -26,6 +26,17 @@ bool_t xdr_dataset(XDR *xdrs, DS_DATASET_T **ppDataset);
 bool_t xdr_dataset_type(XDR *xdrs, DS_DATASET_T **ppDataset);
 bool_t xdr_dataset_data(XDR *xdrs, DS_DATASET_T *pDataset);
 
+  /* Linux has a broken version of xdr.h - x_destroy doesn't take any
+     arguments, but the macro XDR_DESTROY passes it one.  */
+#if defined(linux)
+#ifdef XDR_DESTROY
+#undef XDR_DESTROY
+#endif
+#define	XDR_DESTROY(xdrs)				\
+	if ((xdrs)->x_ops->x_destroy) 			\
+		(*(xdrs)->x_ops->x_destroy)()
+#endif
+
 #ifdef DS_PRIVATE
 /******************************************************************************
 *
