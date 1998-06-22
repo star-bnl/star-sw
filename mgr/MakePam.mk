5dd936272beb7cf05cd5f20baf0177b4bff72f13
Lidia Didenko <didenko@bnl.gov>
1998-06-22 19:03:50 +0000

    Remove libraries

--- mgr/MakePam.mk
+++ mgr/MakePam.mk
@@ -185,7 +185,7 @@ ifneq ($(STAR_PATH),$(OUT_DIR))
     LIBRARIES += $(wildcard  $(STAR_LIB)/lib$(PKG).a)
   endif                           
 endif                           
-LIBRARIES += -L$(STAR)/asps/../.$(STAR_HOST_SYS)/lib -ltls -lmsg
+####LIBRARIES += -L$(STAR)/asps/../.$(STAR_HOST_SYS)/lib -ltls -lmsg
 endif                          
 ifeq (,$(strip $(LIB_PKG) $(SL_PKG)))
 all:
@@ -205,9 +205,8 @@ $(LIB_PKG):$(OBJS)
 endif                          
 ifneq ($(strip $(FILES_SL) $(FILES_OG) $(FILES_init)),)   
 $(SL_PKG): $(FILES_SL) $(FILES_OG) $(FILES_init) $(LIB_PKG)
-#	$(SO) $(SOFLAGS) $(FILES_SL) $(FILES_OG)  $(FILES_init)  -o $(SL_PKG) \
-	$(LD) $(LDFLAGS) $(FILES_SL) $(FILES_OG)  $(FILES_init)  -o $(SL_PKG) \
-        $(LIBRARIES) $(CERN_LIBS)  $(FLIBS) $(CLIBS)
+	$(SO) $(SOFLAGS) $(FILES_SL) $(FILES_OG)  $(FILES_init)  -o $(SL_PKG) \
+        $(LIBRARIES)  
 	@echo "           Shared library " $(SL_PKG) " has been created"   
 #--------- module --------- 
 ifneq ($(NAMES_IDM),)           
