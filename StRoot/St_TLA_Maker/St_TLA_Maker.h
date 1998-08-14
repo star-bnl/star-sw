a0548dda9d499732a4a78d7e293370dd6087defb
fisyak <>
1998-08-14 15:25:58 +0000

    add options

--- StRoot/St_TLA_Maker/St_TLA_Maker.h
+++ StRoot/St_TLA_Maker/St_TLA_Maker.h
@@ -1,5 +1,8 @@
-// $Id: St_TLA_Maker.h,v 1.3 1998/08/10 02:32:07 fisyak Exp $
+// $Id: St_TLA_Maker.h,v 1.4 1998/08/14 15:25:58 fisyak Exp $
 // $Log: St_TLA_Maker.h,v $
+// Revision 1.4  1998/08/14 15:25:58  fisyak
+// add options
+//
 // Revision 1.3  1998/08/10 02:32:07  fisyak
 // Clean up
 //
@@ -17,11 +20,14 @@
 #ifndef StMaker_H
 #include "StMaker.h"
 #endif
-
+//class St_stk_stkpar;
 class St_TLA_Maker : public StMaker {
  private:
-               Bool_t drawinit;
-// static Char_t m_VersionCVS = "$Id: St_TLA_Maker.h,v 1.3 1998/08/10 02:32:07 fisyak Exp $";
+   Bool_t drawinit;
+// static Char_t m_VersionCVS = "$Id: St_TLA_Maker.h,v 1.4 1998/08/14 15:25:58 fisyak Exp $";
+// Int_t         m_mode;        // mode 1 = primaries;
+// St_stk_stkpar m_stk_stkpar;  //! pointer to stk parameters
+ 
  protected:
  public: 
                   St_TLA_Maker();
@@ -32,6 +38,7 @@ class St_TLA_Maker : public StMaker {
    virtual void   Init();
    virtual Int_t  Make();
    virtual void   PrintInfo();
+// virtual void Set_mode       (Int_t   m =      2){m_mode       = m;} // *MENU*
    ClassDef(St_TLA_Maker, 1)   //StAF chain virtual base class for Makers
 };
 
