c3890b0571db8ad1f2f566c4c1b6828188f29af0
cvs2git <cvs2git>
1998-09-07 22:51:07 +0000

    This commit was manufactured by cvs2git to create branch 'VPdsl'.

--- StRoot/macros/rootlogon.C
+++
@@ -1,8 +0,0 @@
-{
-//   gSystem.Load("Root_Tree.DLL"); // We have to load this DLL "by hand" to manage NTuples
-   printf("\nWelcome to the ROOT tutorials\n\n");
-   printf("\nType \".x STAR_Demos.C\" to get a toolbar from which to execute the STAR demos\n");
-   printf("\nType \".x demos.C\" to get a toolbar from which to execute the demos\n");
-   printf("\nType \".x demoshelp.C\" to see the help window\n\n");
-}
- 
