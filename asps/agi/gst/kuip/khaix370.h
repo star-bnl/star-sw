5ef6c47916a10a947fffa20e39e2e1542445fbe3
fisyak <>
1998-09-18 00:29:16 +0000

    Add break off for Linux

---
+++ asps/agi/gst/kuip/khaix370.h
@@ -0,0 +1,15 @@
+/*
+ * $Id: khaix370.h,v 1.1 1998/09/18 00:29:08 fisyak Exp $
+ *
+ * $Log: khaix370.h,v $
+ * Revision 1.1  1998/09/18 00:29:08  fisyak
+ * Add break off for Linux
+ *
+ * Revision 1.1.1.1  1996/03/08 15:33:00  mclareni
+ * Kuip
+ *
+ */
+#ifndef AIX370
+#  define AIX370
+#endif
+
---
+++ asps/agi/gst/kuip/khibmvm.h
@@ -0,0 +1,15 @@
+/*
+ * $Id: khibmvm.h,v 1.1 1998/09/18 00:29:09 fisyak Exp $
+ *
+ * $Log: khibmvm.h,v $
+ * Revision 1.1  1998/09/18 00:29:09  fisyak
+ * Add break off for Linux
+ *
+ * Revision 1.1.1.1  1996/03/08 15:33:00  mclareni
+ * Kuip
+ *
+ */
+#ifndef IBMVM
+#  define IBMVM
+#endif
+
