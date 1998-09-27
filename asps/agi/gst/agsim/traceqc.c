8eec318cc125baf864d2b564f18b82e2dd58d942
nevski <>
1998-09-27 19:29:48 +0000

    break treatment refined

--- asps/agi/gst/agsim/traceqc.c
+++ asps/agi/gst/agsim/traceqc.c
@@ -1,7 +1,10 @@
 /*
- * $Id: traceqc.c,v 1.6 1998/09/24 08:04:41 nevski Exp $
+ * $Id: traceqc.c,v 1.7 1998/09/27 19:29:48 nevski Exp $
  *
  * $Log: traceqc.c,v $
+ * Revision 1.7  1998/09/27 19:29:48  nevski
+ * break treatment refined
+ *
  * Revision 1.6  1998/09/24 08:04:41  nevski
  * verbose interrupt handler
  *
@@ -37,67 +40,38 @@ void    U_STACK_TRACE();
 printf (" interrupt trace routine (traceqc) not awailable yet \n"); 
 #endif
 }
-  
-typedef struct _KmCommand {     /*                                         */ \
-  struct _KmCommand *next;      /* link to next command                    */ \
-  char         *path;           /* command path                            */ \
-  char         *name;           /* command name                            */ \
-  int           hidden;         /* flag if command is invisible            */ \
-  int           level;          /* depth of submenus                       */ \
-  int           total;          /* total number of parameters              */ \
-  int           mandatory;      /* number of mandatory parameters          */ \
-  int         **par;            /* list of total parameter descriptions    */ \
-  int           list_par;       /* index+1 of parameter taking a list      */ \
-  int           xcount;         /* count number of action calls            */ \
-  int          *action_F;       /* action routine                          */ \
-  int          *action_C;       /* action routine                          */ \
-  int          *user_help_F;    /* user help routine                       */ \
-  int          *user_help_C;    /* user help routine                       */ \
-  int          nguidance;       /* number of lines in guidance text        */ \
-  char        **guidance;       /* help text                               */ \
-  int          nkeyword;        /* number of lines for keywords            */ \
-  char        **keyword;        /* list of keywords                        */ \
-  int          nhlink;          /* number of lines for links               */ \
-  char        **hlink;          /* list of links                           */ \
-  int           argc;           /* number of arguments entered             */ \
-  char        **argv;           /* argc argument values                    */ \
-  char         *argline;        /* argument line as entered                */ \
-  int          *argoffs;        /* argc offsets into argline for KUGETE    */ \
-} KmCommand;                    /*                                         */ \
+
+/*  emulate kuip kuexec member function to get command handle */
+#define   SUBROUTINE int
+#define   IntFunc    int
+#define   pCharFunc  int
+#include  "kuip/kmenu.h"
  
 void reset_arg_list( KmCommand *cmd )
 {
   extern KmCommand *cmd_current_;
   cmd_current_ = cmd;
- 
   /* clear current values */
   if( cmd->argc > 0 )
-  {
-    int i;
-    for( i = 0; i < cmd->argc; i++ )
-      free( cmd->argv[i] );
-    cmd->argc = 0;
-    free( (char*)cmd->argv );
-    cmd->argv = 0;
-    free( cmd->argline );
-    cmd->argline = 0;
-    free( (char*)cmd->argoffs );
+  { int i;
+    for(i=0; i<cmd->argc; i++ )  free( cmd->argv[i] );
+    cmd->argc = 0;               free( (char*)cmd->argv );
+    cmd->argv = 0;               free( cmd->argline );
+    cmd->argline = 0;            free( (char*)cmd->argoffs );
     cmd->argoffs = 0;
-  }
-}
+} }
  
 void dump_arg_list_ ( )
 {
   extern KmCommand *cmd_current_;
   static KmCommand *cmd;
- 
   cmd = cmd_current_;
   if( cmd->argc > 0 )
-  {
-    int i;
+  { int i;
+    printf (" *** Last command: %s \n",cmd->name);
     for( i = 0; i < cmd->argc; i++ )
-    printf (" argument %d: %s \n",i,cmd->argv[i] );
-    printf(" argument line as entered: \n %s \n", cmd->argline );
-  }
-}
+    printf (" *** argument %d: %s \n",i,cmd->argv[i] );
+/*  printf (" *** argument line as entered: \n %s \n", cmd->argline ); */
+    printf (" ****************************************************** \n");
+} }
  
