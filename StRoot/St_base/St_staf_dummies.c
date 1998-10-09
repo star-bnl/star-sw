f9a8471dc075a8c80fe2bc32fdce69580f820b67
perev <>
1998-10-09 22:47:16 +0000

    Add printout in dummies

--- StRoot/St_base/St_staf_dummies.c
+++ StRoot/St_base/St_staf_dummies.c
@@ -1,15 +1,16 @@
-void ami(){};
-void hbook1_(){};
-void hbook2_(){};
-void hbookn_(){};
-void hfill_(){};
-void hfn_(){};
-void hrend_(){};
-void hropen_(){};
-void hrout_(){};
-void sig_die(){};
+void ami(){}
+void hbook1_(){printf("***DUMMY HBOOK1****");}
+void hbook2_(){printf("***DUMMY HBOOK2****");}
+void hbookn_(){printf("***DUMMY HBOOKN****");}
+void hfill_(){printf("***DUMMY HFILL****");}
+void hfn_(){printf("***DUMMY HFN****");}
+void hrend_(){printf("***DUMMY HREND****");}
+void hropen_(){printf("***DUMMY HROPEN****");}
+void hrout_(){printf("***DUMMY HROUT****");}
+void sig_die(){printf("***DUMMY SIG_DIE****");}
 void gufld_ (float *x, float *b){
+printf("***DUMMY GUFLD 0 0 5****");
   b[0] = 0.;
   b[1] = 0.;
   b[2] = 5.;
-};
+}
