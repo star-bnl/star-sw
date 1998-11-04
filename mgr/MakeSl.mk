43c07b19e0e18f1b7c0dd692dcad685d4ca2d220
Victor Perevoztchikov <perev@bnl.gov>
1998-11-04 20:24:47 +0000

    Cleanup

--- mgr/MakeSl.mk
+++ mgr/MakeSl.mk
@@ -5,48 +5,44 @@ include $(STAF_MAKE_HOME)/MakeEnv.mk
 include $(STAF_MAKE_HOME)/MakeArch.mk
 
 CPPFLAGS += -DSTAF -DCERNLIB_DZDOC -DCERNLIB_NONEWL -DCERNLIB_SHL -DCERNLIB_HADRON 
-GEA := $(FOR72) $(FFLAGS)   $(CPPFLAGS)  
+INCL  =  -I$(STAR)/inc -I$(STAR)/.share/tables
+CPPFLAGS += $(INCL)
+
 FOR := $(FOR72) $(FFLAGS)   $(CPPFLAGS)  
-GST := $(FOR72) $(FFLAGS)   $(EXEFLAGS)
-GSC := $(CXX)   $(CXXFLAGS) $(EXEFLAGS)
+F77 := $(FOR)   $(FEXTEND)
 CPP := $(CXX)   $(CXXFLAGS) $(CPPFLAGS)  
 CC  := $(CC)    $(CFLAGS)   $(CPPFLAGS)   
-FSL := $(FOR72) $(FFLAGS)   $(CPPFLAGS)   $(SOFLAGS)
 LDS := $(SO)    $(SOFLAGS)
 
-CMDS  = atlsim starsim gstar staf staf+ staf++ staf+++
-SIZE  = 1 2 4 6 8 10 12 14 16 18 20 22 24 28 32 34 36
-INCL  = -Iinc  -I$(STAR)/asps/staf/inc
+INCL  =  -I$(STAR)/inc -I$(STAR)/.share/tables
 SHELL := /bin/ksh
-LIBSL :=
 RMF   := /bin/rm     -f
-LIB   = -L/usr/dt/lib -L/usr/SUNWspro/lib -ldl  -lM77 -lF77 -lsunmath -lmalloc
 .SILENT:
 .SUFFIXES:
 .SUFFIXES: .sl .o .g .f .F .c .cc .cdf .rz
 #
 .g.sl:; geant3    $*   -o  $*.F
-	$(FSL) -c $*.F -o  $*.o     $(INCL)
-	$(LDS)    $*.o -o  $*.sl    $(LIBSL)
+	$(FOR) -c $*.F -o  $*.o     
+	$(LDS)    $*.o -o  $*.sl    
 	$(RMF)    $*.F     $*.o
 #
-.F.sl:; $(FSL) -c $*.F -o  $*.o     $(INCL)
-	$(LDS)    $*.o -o  $*.sl    $(LIBSL)
+.F.sl:; $(F77) -c $*.F -o  $*.o    
+	$(LDS)    $*.o -o  $*.sl    
 	$(RMF)             $*.o
 #
-.f.sl:; $(FSL) -c $*.f -o  $*.o
-	$(LDS)    $*.o -o  $*.sl    $(LIBSL)
+.f.sl:; $(F77) -c $*.f -o  $*.o
+	$(LDS)    $*.o -o  $*.sl    
 	$(RMF)             $*.o
 #
-.c.sl:; $(CC)  -c $*.c -o  $*.o     $(INCL)
-	$(LDS)    $*.o -o  $*.sl    $(LIBSL)
+.c.sl:; $(CC)  -c $*.c -o  $*.o    
+	$(LDS)    $*.o -o  $*.sl    
 	$(RMF)    $*.o
 #
-.cc.sl:;$(CPP) -c $*.cc -o $*.o     $(INCL)
-	$(LDS)    $*.o -o  $*.sl    $(LIBCC)
+.cc.sl:;$(CPP) -c $*.cc -o $*.o     
+	$(LDS)    $*.o -o  $*.sl    
 	$(RMF)    $*.o
 #
-.o.sl:;	$(LDS)    $*.o -o  $*.sl    $(LIBCC)
+.o.sl:;	$(LDS)    $*.o -o  $*.sl    
 #
 .cdf.o:;kuipc     $*
 	$(FOR) -c $*.f -o  $*.o
@@ -55,32 +51,13 @@ LIB   = -L/usr/dt/lib -L/usr/SUNWspro/lib -ldl  -lM77 -lF77 -lsunmath -lmalloc
 	$(FOR) -c $*.F -o  $*.o     $(INCL)
 	$(RMF)    $*.F
 #
-.F.o:;  $(FOR) -c $*.F -o  $*.o     $(INCL)
-.f.o:;  $(FOR) -c $*.f -o  $*.o
+.F.o:;  $(F77) -c $*.F -o  $*.o     $(INCL)
+.f.o:;  $(F77) -c $*.f -o  $*.o
 .c.o:;  $(CC)  -c $*.c -o  $*.o
 .cc.o:; $(CPP) -c $(INCL)  $*.cc -o $*.o
 #
 #
-geant3: geant3.f; $(GEA) -o geant3 geant3.f `cernlib kernlib`
 #
-$(CMDS):ccsim.o fgsim.o agsim.o kgsim.o ggsim.o comisf.o comisc.o agdummy.o
-	$(GSC) -o $@                   \
-                      -DSTAF $(INCL)   \
-                      ccsim.cc fgsim.o \
-	agsim.o kgsim.o ggsim.o comisf.o comisc.o agdummy.o \
-     -L./ -ltdm -lspx -lsoc -lasu -ltop -ltnt -lami -ldio -ldui -ldsl -ldsu \
-	`cernlib geant321 pawlib graflib/Motif \
-         packlib mathlib kernlib`  \
-        $(LIB)
-#
-	echo '#!'$(PWD)'/$@'  > import.map
-#	mv  /tmp/$@  ./$@
- 
-$(SIZE):
-	sed -e '5,15s/8 000/$@ 000/; 5,15s/2 000/500/' fgsim.f > simsize.f
-	$(FOR) -c simsize.f -o  fgsim.o
-normal:
-	$(FOR) -c fgsim.f   -o  fgsim.o
 detm:
 	$(RMF) detm.rz; echo "createdoc dzdoc.bank detm.rz; quit;" | dzedit
 cleanup:
@@ -88,9 +65,6 @@ cleanup:
 	/bin/rm -f agsim.g agdummy.g kgsim.cdf comisc.* ccsim.cc
 	/bin/rm -f cmzsave.dat paw.metafile dzedit.las
 	ls -ltr
-atlas:	geant3 atlsim detm cleanup
-star:	geant3 gstar  detm cleanup
-setup:
 show:
 	@echo LDS = $(LDS) 
 	@echo FSL = $(FSL) 
