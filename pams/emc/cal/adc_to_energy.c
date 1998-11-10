761e59fb9d610ef4f3026f166de9c99583f16f5b
Akio Ogawa <akio@bnl.gov>
1998-11-10 21:54:04 +0000

    A major re-structuring pams and tables

--- pams/emc/cal/adc_to_energy.c
+++ pams/emc/cal/adc_to_energy.c
@@ -9,9 +9,11 @@
 #include "emc_def.h"
 
 long type_of_call adc_to_energy_(
-  TABLE_HEAD_ST    *emc_pedestal_h,   EMC_PEDESTAL_ST     *emc_pedestal ,
-  TABLE_HEAD_ST    *emc_adcslope_h,   EMC_ADCSLOPE_ST     *emc_adcslope ,
-  TABLE_HEAD_ST         *emc_adc_h,        EMC_ADC_ST          *emc_adc )
+  TABLE_HEAD_ST      *ped_header_h,EMC_CALIB_HEADER_ST       *ped_header ,
+  TABLE_HEAD_ST    *emc_pedestal_h,    EMC_PEDESTAL_ST     *emc_pedestal ,
+  TABLE_HEAD_ST      *slp_header_h,EMC_CALIB_HEADER_ST       *slp_header ,
+  TABLE_HEAD_ST    *emc_adcslope_h,    EMC_ADCSLOPE_ST     *emc_adcslope ,
+  TABLE_HEAD_ST        *emc_hits_h,        EMC_HITS_ST         *emc_hits )
 {
 /*:>--------------------------------------------------------------------
 **: ROUTINE:    adc_to_energy_
@@ -24,45 +26,67 @@ long type_of_call adc_to_energy_(
 **:       emc_adcslope    - slope data
 **:      emc_adcslope_h   - header Structure for emc_adcslope
 **:    INOUT:
-**:            emc_adc    - adc channel is output, energy is output 
-**:           emc_adc_h   - header Structure for emc_adc
+**:            emc_hits    - adc channel is output, energy is output 
+**:           emc_hits_h   - header Structure for emc_hits
 **:      OUT:
 **: RETURNS:    STAF Condition Value
 **:>------------------------------------------------------------------*/
 
-    long i, det, eta, phi, nok_p, nok_s;
+    long i, j, det, module, eta, sub, nmod, neta, nsub, nse;
+    float slp;
 
-    if(emc_adc_h->nok <= 0 ) {
-      puts("adc_to_energy : emc_adc_h->nok is less equal zero. No input.\n");
-      return STAFCV_OK;
-    }
-    nok_p=emc_pedestal_h->nok-1;
-    nok_s=emc_adcslope_h->nok-1;
-    det = (long)emc_adc[0].det;
+    det = (long)emc_hits[0].det;
     switch(det){
-    case BEMC: case EEMCR: case EEMCL:
-    case BPRS: case EPRSR: case EPRSL:
+    case BEMC: case BPRS: case BSMDE: case BSMDP:
+    case EEMC: case EPRS: case ESMDE: case ESMDP:
       break;
     default:
-      puts("***adc_to_energy: emc_adc[0].det is invalid. Unknown detector.\n");
+      puts("***adc_to_energy: emc_hits[0].det is invalid. Unknown detector");
       return STAFCV_BAD;
     }
-    if(emc_pedestal[nok_p].det != det){
-      puts("***adc_to_energy: Pedestal data is not for this datector.\n");
+    if(ped_header[0].det != det){
+      puts("***adc_to_energy: Pedestal data is not for this datector");
       return STAFCV_BAD;
     }
-    if(emc_adcslope[nok_s].det != det){
-      puts("***adc_to_energy: Adcslope data is not for this detector.\n");
+    if(slp_header[0].det != det){
+      puts("***adc_to_energy: Adcslope data is not for this detector");
       return STAFCV_BAD;
     }
-    for(i = 0; i < emc_adc_h->nok; i++){
-      eta = emc_adc[i].eta_bin;
-      phi = emc_adc[i].phi_bin;
-      emc_adc[i].energy = (emc_adc[i].adc - emc_pedestal[nok_p].ped[phi][eta])
-	                / emc_adcslope[nok_p].p0[phi][eta];
-      if(emc_adc[i].energy < 0.0){
-	emc_adc[i].energy = 0.0;
+    nmod = ped_header[0].nmodule;
+    neta = ped_header[0].neta;
+    nsub = ped_header[0].nsub;
+    nse=nsub*neta;
+    if(nmod != slp_header[0].nmodule ||
+       neta != slp_header[0].neta    ||
+       nsub != slp_header[0].nsub    ){
+      puts("***adc_to_energy: Pedestal & Adcslope data have different # of module/eta/sub");
+      return STAFCV_BAD;
+    }
+    switch(slp_header[0].func){
+    case 1:
+      for(i = 0; i < emc_hits_h->nok; i++){
+	if(emc_hits[i].adc>-1){   /* if adc=-1, leave energy as it is */
+	  module = emc_hits[i].module;
+	  eta    = emc_hits[i].eta;
+	  sub    = emc_hits[i].sub;
+	  j = sub-1+(eta-1)*nsub+(module-1)*nse;
+	  slp    = emc_adcslope[j].p0;      
+	  if(slp > 0.0){
+	    emc_hits[i].energy = (emc_hits[i].adc-emc_pedestal[j].ped)/slp;
+	    if(emc_hits[i].energy < 0.0){
+	      emc_hits[i].energy = 0.0;
+	    }
+	  } else {
+	    puts("***adc_to_energy: Adcslope data is less equal 0.0");
+	    printf("***adc_to_energy: module,eta,sub= %d %d %f\n",module,eta,sub);
+	    emc_hits[i].energy = -1.0;
+	  }
+	}    
       }
+      break;
+    default:
+      puts("***emc_adc_sim: Function has not been implimented yet!");
+      return STAFCV_BAD;
     }    
     return STAFCV_OK;
 }
--- pams/emc/cal/fake_adcslope.c
+++ pams/emc/cal/fake_adcslope.c
@@ -13,7 +13,8 @@ extern float ran0(long*);
 long type_of_call fake_adcslope_(
   TABLE_HEAD_ST     *ems_control_h,  EMS_CONTROL_ST          *ems_control ,
   TABLE_HEAD_ST *ems_cal_control_h,  EMS_CAL_CONTROL_ST  *ems_cal_control ,
-  TABLE_HEAD_ST   *fake_adcslope_h,  EMC_ADCSLOPE_ST       *fake_adcslope )
+  TABLE_HEAD_ST *fake_slp_header_h,  EMC_CALIB_HEADER_ST *fake_slp_header ,
+  TABLE_HEAD_ST        *fake_slp_h,  EMC_ADCSLOPE_ST            *fake_slp )
 {
 /*:>--------------------------------------------------------------------
 **: ROUTINE:    fake_adcslope_
@@ -27,71 +28,63 @@ long type_of_call fake_adcslope_(
 **:      OUT:
 **: RETURNS:    STAF Condition Value
 **:>------------------------------------------------------------------*/
-    long i, j, n, func, nok_c, nok_cc, nok_fp, det;
-    long iseed=999; 
+    long i, j, k, l, nok_c, nok_cc, nok_fp;
+    short func, det, nmod, neta, nsub, tot;
+    long iseed=9999; 
     float bits;
 
-    if(fake_adcslope_h->maxlen <= 0 ) {
-      puts("maxlen is less equal zero\n");
-      return STAFCV_BAD;
-    }
-    if(fake_adcslope_h->nok > fake_adcslope_h->maxlen){
-      puts("nok is more than maxlen\n");
-      return STAFCV_BAD;
-    }
-  
-    n=fake_adcslope_h->nok;
     nok_c=ems_control_h->nok-1;
     nok_cc=ems_cal_control_h->nok-1;
     det = ems_cal_control[nok_cc].det;
     if(ems_cal_control[nok_cc].iseed != 0) iseed=ems_cal_control[nok_cc].iseed;
     switch (det){
-    case BEMC:
-      fake_adcslope[n].nphi = ems_control[nok_c].bemc_phi_nbin;
-      fake_adcslope[n].neta = ems_control[nok_c].bemc_eta_nbin;
-      bits = (float)ems_control[nok_c].bemc_adcbits;
-      break;
-    case EEMCR: case EEMCL: 
-      fake_adcslope[n].nphi = ems_control[nok_c].eemc_phi_nbin;
-      fake_adcslope[n].neta = ems_control[nok_c].eemc_eta_nbin;
-      bits = (float)ems_control[nok_c].eemc_adcbits;
-      break;
-    case BPRS:
-      fake_adcslope[n].nphi = ems_control[nok_c].bemc_phi_nbin;
-      fake_adcslope[n].neta = ems_control[nok_c].bemc_eta_nbin;
-      bits = (float)ems_control[nok_c].bprs_adcbits;
-      break;
-    case EPRSR: case EPRSL:
-      fake_adcslope[n].nphi = ems_control[nok_c].eemc_phi_nbin;
-      fake_adcslope[n].neta = ems_control[nok_c].eemc_eta_nbin;
-      bits = (float)ems_control[nok_c].eprs_adcbits;
+    case BEMC: case BPRS: case BSMDE: case BSMDP:
+    case EEMC: case EPRS: case ESMDE: case ESMDP:
+      nmod = ems_control[nok_c].nmodule[det-1];
+      neta = ems_control[nok_c].neta[det-1];
+      nsub = ems_control[nok_c].nsub[det-1];
+      bits = (float)ems_control[nok_c].adcbits[det-1];
       break;
     default:
-      puts("ems_cal_control.det is invalid. Specify the detector\n");
+      puts("***fake_adcslope: ems_cal_control.det is invalid. Specify the detector");
       return STAFCV_BAD;
     }
-    if(fake_adcslope[n].neta < 1 || fake_adcslope[n].nphi < 1){
-      puts("neta or ephi is less than 1, need inputs in ems_control\n");
+    printf("fake_adcslope: det,nmod,neta,nsub=%d,%d,%d,%d\n",det,nmod,neta,nsub);
+    if(nmod < 1 || neta < 1 || nsub < 1 ){
+      puts("***fake_adcslope: nmod, neta or nsub is less than 1, need inputs in ems_control");
       return STAFCV_BAD;
     }
-    fake_adcslope[n].flag = -1; /* flag=-1: fake data */
-    fake_adcslope[n].det = det; /* flag=-1: fake data */
+    tot=nmod*neta*nsub;
+    if(fake_slp_h->maxlen < tot ) {
+      puts("***fake_adcslope: Need more space on fake_slp.");
+      return STAFCV_BAD;
+    }
     func=ems_cal_control[nok_cc].slope_fun;
+    fake_slp_header[0].det    = det;
+    fake_slp_header[0].nmodule= nmod;
+    fake_slp_header[0].neta   = neta;
+    fake_slp_header[0].nsub   = nsub;
+    fake_slp_header[0].flag   = -1;       /* flag=-1: fake data */
+    fake_slp_header[0].func   = func;     /* func from ems_cal_control */
     switch(func){
     case 1:
-      for( i = 0; i < fake_adcslope[n].nphi ; i++ ){
-	for( j = 0; j < fake_adcslope[n].neta ; j++ ){
-	  fake_adcslope[n].p0[i][j]= ems_cal_control[nok_cc].slope_ave *
-	    (1.0 + ems_cal_control[nok_cc].slope_var*(ran0(&iseed)-0.5));
+      l=0;
+      for( i = 0; i < nmod ; i++ ){
+	for( j = 0; j < neta ; j++ ){
+	  for( k = 0; k < nsub ; k++ ){
+	    fake_slp[l].p0 = ems_cal_control[nok_cc].slope_ave *
+	      (1.0 + ems_cal_control[nok_cc].slope_var*(ran0(&iseed)-0.5));
+	    l++;
+	  }
 	}
       }
       break;
     default:
-      puts("Function is not implimented yet!");
+      puts("***fake_adcslope: Function has not been implimented yet!");
       return STAFCV_BAD;
     }    
-    fake_adcslope[n].fun=func;
-    fake_adcslope_h->nok++;
     ems_cal_control[nok_cc].iseed=iseed;
+    fake_slp_header_h->nok=1;
+    fake_slp_h->nok=l;
     return STAFCV_OK;
 }
