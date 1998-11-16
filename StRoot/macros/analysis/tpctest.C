d7b5479237fe57820e706765c2a22089cb9668bf
fisyak <>
1998-11-16 02:43:26 +0000

    new tpc

--- StRoot/macros/analysis/tpctest.C
+++ StRoot/macros/analysis/tpctest.C
@@ -1,5 +1,8 @@
-// $Id: tpctest.C,v 1.2 1998/09/16 17:58:45 love Exp $
+// $Id: tpctest.C,v 1.3 1998/11/16 02:43:26 fisyak Exp $
 // $Log: tpctest.C,v $
+// Revision 1.3  1998/11/16 02:43:26  fisyak
+// new tpc
+//
 // Revision 1.2  1998/09/16 17:58:45  love
 // tpctest slice plotting added
 //
@@ -23,10 +26,25 @@
 // Define globals over this package
 
 Int_t ievt=0;  //local event counter 
-  StChain   *chain = 0;
-
+#include "iostream.h"
+#pragma includepath "/afs/rhic/star/packages/dev/StRoot/StChain"
+class  StChain;
+StChain *chain = 0;
+void Load(){
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+  gSystem->Load("xdf2root");
+  gSystem->Load("St_Tables");
+  gSystem->Load("libmsg");
+  gSystem->Load("libtls");
+  gSystem->Load("tpc");
+  gSystem->Load("St_tpc");
+  gSystem->Load("St_TLA_Maker");
+  gSystem->Load("St_xdfin_Maker");
+  gSystem->Load("St_tpctest_Maker");
+  gSystem->Load("St_params_Maker");
+}
 void start(){
-
 #ifndef __CINT__
 #include "Rtypes.h"
 #include "St_XDFFile.h"
@@ -36,7 +54,7 @@ void start(){
 #endif
 // The following file is 1039 full 42+ track laser events.
 // Char_t *filename="/star/mds/data/SD97/cosmic/Sept/log-p274-t23-f6-las.xdf";
-   Char_t *filename="/scr20/love/star/test/log-p274-t23-f6-las.xdf"; 
+   Char_t *filename="/disk1/star/SD97/cosmic/Sept/log-p274-t23-f6-las.xdf"; 
 // The following file is 44 events of central membrane pattern only.
 // Char_t *filename="/star/mds/data/SD97/cosmic/Sept/log-p285-t28-f6-las.xdf";
 //  Char_t *filename="/scr20/love/star/test/log-p285-t28-f6-las.xdf";
@@ -46,16 +64,18 @@ void start(){
  
   St_XDFFile *xdffile_in = new St_XDFFile (filename,"r");
 // Create the main chain object
-  chain= new StChain("StChain");
+  chain = new StChain("StChain");
 //  Create the makers to be called by the current chain
-  St_xdfin_Maker *xdfin=new  St_xdfin_Maker ("xdfin_Maker","event/raw_data/tpc");
+ 
+  St_params_Maker *param = new St_params_Maker;
+  St_xdfin_Maker *xdfin=new  St_xdfin_Maker;
   chain->SetInputXDFile(xdffile_in);
-  St_run_Maker *run_Maker = new St_run_Maker ("run_Maker","run/params");
-  St_tpctest_Maker *tpctest_Maker = new St_tpctest_Maker ("tpctest_Maker","event");
+  St_TLA_Maker *mini_daq  = new St_TLA_Maker("tpc_raw","event/raw_data/tpc");   
+  St_tpctest_Maker *tpc = new St_tpctest_Maker ("tpctest","event/data/tpc");
   // Set slice selector.   nslice <0 plots all z in one view. 
   // 0 < nslice < 7 plots only the slice selected.  
   // nslice =7 plots seven slices in succession.
-   tpctest_Maker->Set_nslice(7);
+   tpc->Set_nslice(7);
   //
   chain->PrintInfo();
    // Create a root file to hold the ntuple.
@@ -82,4 +102,11 @@ void skip(Int_t nskip=1){
   }
   ievt += nskip;// keep the count of events straight.
 }
-void end(){ chain->Maker("tpctest_Maker")->Histograms()->Write();}
+void end(){ chain->Maker("tpctest")->Histograms()->Write();}
+void tpctest(){
+  Load();
+  start();
+  skip(5);
+  loop(5);
+  end();
+}
