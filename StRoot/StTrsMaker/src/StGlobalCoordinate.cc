481a8dd1cbe9e8d34fd9562358b4c6c5cf87dd96
Brian Lasiuk <lasiuk>
1998-11-16 19:41:58 +0000

    constructor do not use reference for double&

--- StRoot/StTrsMaker/src/StGlobalCoordinate.cc
+++ StRoot/StTrsMaker/src/StGlobalCoordinate.cc
@@ -1,6 +1,6 @@
 /*************************************************************************
  *
- * $Id: StGlobalCoordinate.cc,v 1.1 1998/11/10 17:12:20 fisyak Exp $
+ * $Id: StGlobalCoordinate.cc,v 1.2 1998/11/16 19:41:58 lasiuk Exp $
  *
  * Author:  brian May 20, 1998
  *
@@ -12,8 +12,8 @@
  ***************************************************************************
  *
  * $Log: StGlobalCoordinate.cc,v $
- * Revision 1.1  1998/11/10 17:12:20  fisyak
- * Put Brian trs versin into StRoot
+ * Revision 1.2  1998/11/16 19:41:58  lasiuk
+ * constructor do not use reference for double&
  *
  * Revision 1.1  1998/11/10 17:12:20  fisyak
  * Put Brian trs versin into StRoot
@@ -30,10 +30,8 @@
 
 StGlobalCoordinate::StGlobalCoordinate() {/**/}
 
-StGlobalCoordinate::StGlobalCoordinate(const double& x, const double& y, const double& z)
-{
-    StGlobalCoordinate(StThreeVector<double>(x,y,z));
-}
+StGlobalCoordinate::StGlobalCoordinate(const double x, const double y, const double z)
+    : mPos(x,y,z) { /**/ }
 
 StGlobalCoordinate::StGlobalCoordinate(const StThreeVector<double>& x)
     : mPos(x) {/**/}
