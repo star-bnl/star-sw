7b8e691c63661ba763bd9afa93b61c91dab6c53a
Yuri Fisyak <fisyak@bnl.gov>
1998-11-22 01:03:57 +0000

    Add name

--- StRoot/macros/ana.C
+++ StRoot/macros/ana.C
@@ -1,13 +1,11 @@
-// $Id: ana.C,v 1.6 1998/11/22 00:59:18 fisyak Exp $
+// $Id: ana.C,v 1.7 1998/11/22 01:03:57 fisyak Exp $
 // $Log: ana.C,v $
+// Revision 1.7  1998/11/22 01:03:57  fisyak
+// Add name
+//
 // Revision 1.6  1998/11/22 00:59:18  fisyak
-// Add name
-//
-// Revision 1.5  1998/11/22 00:53:47  fisyak
-// Add name
-//
 // $Name:  $  
-// $Tag:  $  
+
 void ana(const Char_t *Path="/disk1/star/auau200/hijing135/default/",const Char_t *root_file="all2.root")
 {
   gROOT->Reset();
