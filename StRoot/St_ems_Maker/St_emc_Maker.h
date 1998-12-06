369ba2adb6dc4e4bf666847cd691acb53396ecde
Akio Ogawa <akio@bnl.gov>
1998-12-06 09:57:14 +0000

    put histgrams

--- StRoot/St_ems_Maker/St_emc_Maker.h
+++ StRoot/St_ems_Maker/St_emc_Maker.h
@@ -1,29 +1,10 @@
-// $Id: St_emc_Maker.h,v 1.1 1998/12/03 22:27:26 akio Exp $
+// $Id: St_emc_Maker.h,v 1.2 1998/12/06 09:57:13 akio Exp $
 // $Log: St_emc_Maker.h,v $
+// Revision 1.2  1998/12/06 09:57:13  akio
+// put histgrams
+//
 // Revision 1.1  1998/12/03 22:27:26  akio
 // New emc maker
-//
-// Revision 1.1  1998/11/30 21:18:31  akio
-// emc calibration & hit Maker
-//
-// Revision 1.7  1998/10/31 00:25:45  fisyak
-// Makers take care about branches
-//
-// Revision 1.6  1998/10/06 18:00:31  perev
-// cleanup
-//
-// Revision 1.5  1998/08/26 12:15:13  fisyak
-// Remove asu & dsl libraries
-//
-// Revision 1.4  1998/08/14 15:25:58  fisyak
-// add options
-//
-// Revision 1.3  1998/08/10 02:32:07  fisyak
-// Clean up
-//
-// Revision 1.2  1998/07/20 15:08:15  fisyak
-// Add tcl and tpt
-//
 #ifndef STAR_St_emc_Maker
 #define STAR_St_emc_Maker
 
@@ -50,6 +31,7 @@ class St_emc_Maker : public StMaker {
   void MakeHistograms();             // Histograms
  protected:
    TH1F *m_emc_etot;    //!
+   TH2F *m_emc_energy;  //! 
    TH2F *m_emc_nhit;    //! 
    TH2F *m_bemc_hit;    //!
    TH2F *m_bprs_hit;    //!
