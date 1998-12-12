53e36f7214d76a2887e6ee15bdae40f15cc4c508
Yuri Fisyak <fisyak@bnl.gov>
1998-12-12 02:37:53 +0000

    fix evr

--- StRoot/St_dst_Maker/St_glb_Maker.h
+++ StRoot/St_dst_Maker/St_glb_Maker.h
@@ -1,5 +1,8 @@
-// $Id: St_glb_Maker.h,v 1.5 1998/11/01 16:42:27 fisyak Exp $
+// $Id: St_glb_Maker.h,v 1.6 1998/12/12 02:37:53 fisyak Exp $
 // $Log: St_glb_Maker.h,v $
+// Revision 1.6  1998/12/12 02:37:53  fisyak
+// fix evr
+//
 // Revision 1.5  1998/11/01 16:42:27  fisyak
 // dst analysis
 //
@@ -50,85 +53,88 @@ class St_mft_control;
 class St_particle_dst_param;
 class St_dst_Maker : public StMaker {
  private:
-   Bool_t drawinit;
-// static Char_t m_VersionCVS = "$Id: St_glb_Maker.h,v 1.5 1998/11/01 16:42:27 fisyak Exp $";
-   // egr
-   Int_t         m_scenario;   //#1: Real TPC Stand-Alone Tracking: Use this when running the TPC only.  
-                               //    EGR will refit the TPC tracks, reproducing the parameters in tptrack.
-                               //#2: Perfect TPC Stand-Alone Tracking: Use this when running the TPC only.  
-                               //    EGR will perform a direct copy of tptrack into globtrk.
-                               //#3: Real SVT Stand-Alone Tracking: Use this when running the SVT only.  
-                               //    EGR will refit the SVT tracks, reproducing the parameters in svt_track, sort of.
-                               //#4: Perfect SVT Stand-Alone Tracking: Use this when running the SVT only.  
-                               //    EGR will copy the svt_track table directly into globtrk.
-                               //#5: Fit TPC tracks to the event vertex: Will refit the TPC tracks including 
-                               //    the event vertex point.
-                               //#6: Real SVT+TPC Matched Tracking: Refits the SVT and TPC tracks matched 
-                               //    by the svm matcher. Copies only tracks with a TPC track in it to globtrk.
-                               //#7: Perfect SVT+TPC Tracking: Copies SVT and TPC tracks matched using the 
-                               //    Monte Carlo ID. Copies only tracks with a TPC track in it to globtrk. Mtm from
-                               //    tpc track, angles from svt track
-                               //#8: Improved Real SVT+TPC Matched Tracking: Refits the SVT and TPC tracks 
-                               //    matched by the svm matcher. Fills globtrk with all matched SVT+TPC tracks, 
-                               //    followed by all eftover TPC tracks, followed by all leftover SVT tracks with a
-                               //    chi-square(1) fit better than epar.svtchicut
-                               //#9: Improved Perfect SVT+TPC Tracking: Refits the SVT and TPC tracks matched using 
-                               //    the Monte Carlo ID. Fills globtrk with all matched SVT+TPC tracks, followed by 
-                               //    all leftover TPC tracks, followed by all leftover SVT tracks with a chi-square(1) 
-                               //    fit better than epar.svtchicut
-                               //#10:Refits the SVT and TPC matched tracks followed by unmatched tracks
-                               //    Fills globtrk with all matched SVT+TPC tracks, followed by all
-                               //    leftover TPC tracks copied from tpctrack.
-                               //    The matched tracks are straight copies of svt_track but with
-                               //    the 1/pt value taken from tptrack.
-                               //#11:Description Perfect SVT/TPC matching (M.C.). Only tracks w/ TPC into 
-   Int_t         m_svtchicut;  // = 0 all unmatched svt tracks copied
-   Int_t         m_useglobal;  // = 1 - Perfect matching between tpc and svt (checks mc_ids)
-                               // = 2 - Refit tracks
-                               // = 3 - Copies info from svt tracks + 1/invpt from tptrack
-   Int_t         m_usesvt;     // = 0 Dont' copy unmatched svt tracks
-                               // = 1 Copy unmatched tracks with chisq>svtchicut, no refit 
-                               // = 2 Copy unmatched tracks with chisq>svtchicut Refit
-   Int_t         m_usetpc;     // = 0 Don't copy unmatched tracks
-                               // = 1 Copy unmatched tracks, no refit
-                               // = 2 Copy unmatched tracks, refit
-
-   Int_t         m_usevert;    // 
-   St_svm_ctrl    *m_svm_ctrl;    // ! 
-   St_evr_privert *m_evr_privert; //!
-   St_evr_evrpar  *m_evr_evrpar;  //!
-   
-   St_ev0_ev0par  *m_ev0par;      //!
-   St_mft_control *m_magf;        //!
-   St_egr_egrpar  *m_egr_egrpar;  //!
-   St_particle_dst_param *m_particle_dst_param; //!
-   // Histograms
-   static const Int_t nxpT;
-   static const Int_t nyeta;
-   static const Float_t xminpT;
-   static const Float_t xmaxpT;
-   static const Float_t ymineta;
-   static const Float_t ymaxeta;
-
+  Bool_t drawinit;
+  // static Char_t m_VersionCVS = "$Id: St_glb_Maker.h,v 1.6 1998/12/12 02:37:53 fisyak Exp $";
+  // egr
+  Int_t         m_scenario;   //#1: Real TPC Stand-Alone Tracking: Use this when running the TPC only.  
+  //    EGR will refit the TPC tracks, reproducing the parameters in tptrack.
+  //#2: Perfect TPC Stand-Alone Tracking: Use this when running the TPC only.  
+  //    EGR will perform a direct copy of tptrack into globtrk.
+  //#3: Real SVT Stand-Alone Tracking: Use this when running the SVT only.  
+  //    EGR will refit the SVT tracks, reproducing the parameters in svt_track, sort of.
+  //#4: Perfect SVT Stand-Alone Tracking: Use this when running the SVT only.  
+  //    EGR will copy the svt_track table directly into globtrk.
+  //#5: Fit TPC tracks to the event vertex: Will refit the TPC tracks including 
+  //    the event vertex point.
+  //#6: Real SVT+TPC Matched Tracking: Refits the SVT and TPC tracks matched 
+  //    by the svm matcher. Copies only tracks with a TPC track in it to globtrk.
+  //#7: Perfect SVT+TPC Tracking: Copies SVT and TPC tracks matched using the 
+  //    Monte Carlo ID. Copies only tracks with a TPC track in it to globtrk. Mtm from
+  //    tpc track, angles from svt track
+  //#8: Improved Real SVT+TPC Matched Tracking: Refits the SVT and TPC tracks 
+  //    matched by the svm matcher. Fills globtrk with all matched SVT+TPC tracks, 
+  //    followed by all eftover TPC tracks, followed by all leftover SVT tracks with a
+  //    chi-square(1) fit better than epar.svtchicut
+  //#9: Improved Perfect SVT+TPC Tracking: Refits the SVT and TPC tracks matched using 
+  //    the Monte Carlo ID. Fills globtrk with all matched SVT+TPC tracks, followed by 
+  //    all leftover TPC tracks, followed by all leftover SVT tracks with a chi-square(1) 
+  //    fit better than epar.svtchicut
+  //#10:Refits the SVT and TPC matched tracks followed by unmatched tracks
+  //    Fills globtrk with all matched SVT+TPC tracks, followed by all
+  //    leftover TPC tracks copied from tpctrack.
+  //    The matched tracks are straight copies of svt_track but with
+  //    the 1/pt value taken from tptrack.
+  //#11:Description Perfect SVT/TPC matching (M.C.). Only tracks w/ TPC into 
+  Int_t         m_svtchicut;  // = 0 all unmatched svt tracks copied
+  Int_t         m_useglobal;  // = 1 - Perfect matching between tpc and svt (checks mc_ids)
+  // = 2 - Refit tracks
+  // = 3 - Copies info from svt tracks + 1/invpt from tptrack
+  Int_t         m_usesvt;     // = 0 Dont' copy unmatched svt tracks
+  // = 1 Copy unmatched tracks with chisq>svtchicut, no refit 
+  // = 2 Copy unmatched tracks with chisq>svtchicut Refit
+  Int_t         m_usetpc;     // = 0 Don't copy unmatched tracks
+  // = 1 Copy unmatched tracks, no refit
+  // = 2 Copy unmatched tracks, refit
+  
+  Int_t         m_usevert;    // 
+  St_svm_ctrl    *m_svm_ctrl;    // ! 
+  St_evr_privert *m_evr_privert; //!
+  St_evr_evrpar  *m_evr_evrpar;  //!
+  
+  St_ev0_ev0par  *m_ev0par;      //!
+  St_mft_control *m_magf;        //!
+  St_egr_egrpar  *m_egr_egrpar;  //!
+  St_particle_dst_param *m_particle_dst_param; //!
+  // Histograms
+  static const Int_t nxpT;
+  static const Int_t nyeta;
+  static const Float_t xminpT;
+  static const Float_t xmaxpT;
+  static const Float_t ymineta;
+  static const Float_t ymaxeta;
+  
  protected:
-   TH2F     *m_pT_eta_rec;  // pT versus eta Spectra for reconstructed
-   TH2F     *m_pT_eta_gen;  // pT versus eta Spectra for generated
-   TH1F     *m_pT;      // pT  recostructed
-   TH1F     *m_eta;     // eta recostructed
-
+  TH2F     *m_pT_eta_rec;  //! pT versus eta Spectra for reconstructed
+  TH2F     *m_pT_eta_gen;  //! pT versus eta Spectra for generated
+  TH1F     *m_pT;          //! pT  recostructed
+  TH1F     *m_eta;         //! eta recostructed
+  TH1F     *m_tlength;     //! dst track length
+  TH1F     *m_chi2xd;      //! x chisq/degf
+  TH1F     *m_chi2yd;      //! y chisq/degf
+  TH1F     *m_lameffm;     //! Lambda effective mass
  public: 
-                  St_dst_Maker(const char *name="dst", const char *title="event/data/global/dst");
-   virtual       ~St_dst_Maker();
-   virtual Int_t Init();
-   virtual Int_t  Make();
-   virtual void   PrintInfo();
-   virtual void   Set_scenario (Int_t m = 8){m_scenario  = m;} // *MENU*
-   virtual void   Set_svtchicut(Int_t m = 0){m_svtchicut = m;} // *MENU*
-   virtual void   Set_useglobal(Int_t m = 2){m_useglobal = m;} // *MENU*
-   virtual void   Set_usesvt   (Int_t m = 1){m_usesvt    = m;} // *MENU*
-   virtual void   Set_usetpc   (Int_t m = 1){m_usetpc    = m;} // *MENU*
-   virtual void   Set_usevert  (Int_t m = 0){m_usevert   = m;} // *MENU*
-   ClassDef(St_dst_Maker, 1)   //StAF chain virtual base class for Makers
-};
+  St_dst_Maker(const char *name="dst", const char *title="event/data/global/dst");
+  virtual       ~St_dst_Maker();
+  virtual Int_t Init();
+  virtual Int_t  Make();
+  virtual void   PrintInfo();
+  virtual void   Set_scenario (Int_t m = 8){m_scenario  = m;} // *MENU*
+  virtual void   Set_svtchicut(Int_t m = 0){m_svtchicut = m;} // *MENU*
+  virtual void   Set_useglobal(Int_t m = 2){m_useglobal = m;} // *MENU*
+  virtual void   Set_usesvt   (Int_t m = 1){m_usesvt    = m;} // *MENU*
+  virtual void   Set_usetpc   (Int_t m = 1){m_usetpc    = m;} // *MENU*
+  virtual void   Set_usevert  (Int_t m = 0){m_usevert   = m;} // *MENU*
+  ClassDef(St_dst_Maker, 1)   //StAF chain virtual base class for Makers
+    };
 
 #endif
