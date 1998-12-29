d7eb7b012bbb5c81d8a5c8504ddafb10e5b84e6c
Valeri Fine <val.fine@gmail.com>
1998-12-29 19:37:40 +0000

    St_NodeView:  new class to create refs topology of the "main" St_Node object has been introduced

---
+++ StRoot/St_base/St_NodeView.h
@@ -0,0 +1,26 @@
+//*-- Author :    Valery Fine(fine@bnl.gov)   25/12/98 
+// 
+// 
+#ifndef STAR_St_NodeView
+#define STAR_St_NodeView
+
+#include "St_Node.h"
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// St_NodeView                                                         //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+class St_NodeView : public St_ObjectSet {
+public:
+  St_NodeView():St_ObjectSet(){;}
+  St_NodeView(St_Node &pattern,const St_NodePosition *nodePosition=0,EDataSetPass iopt=kStruct);
+  virtual ~St_NodeView(){}
+  virtual void Browse(TBrowser *b);
+  virtual St_NodePosition *GetPosition(){ return (St_NodePosition *)GetObject();}
+  virtual St_Node         *GetNode();
+  ClassDef(St_NodeView,1)
+};
+#endif
+
--- StRoot/St_base/St_ObjectSet.cxx
+++ StRoot/St_base/St_ObjectSet.cxx
@@ -1,14 +1,37 @@
 //*-- Author :    Valery Fine(fine@bnl.gov)   25/12/98 
+// 
+// 
 #include "St_ObjectSet.h"
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// St_ObjectSet                                                         //
+//                                                                      //
+// St_ObjectSet class is a special kind of St_DataSet with one extra    //
+// pointer to wrap any TObject onto St_DataSet object                   //
+//                                                                      //
+//  BE CAREFUL !!!                                                      //
+//  One has to use it carefully no conrol over that extra object        //
+//  is perfomred. This means: the onject m_Obj data-member points to can//
+//  be destroyed with no this kbject notifying.                         //
+//  There is no tool /protection to check whether m_Obj is till alive.  //
+//  It is one's  code responsilitiy                                     //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
 ClassImp(St_ObjectSet)
 //_____________________________________________________________________________
-St_ObjectSet::St_ObjectSet(const Char_t *name, TObject *obj):St_DataSet(name),m_Obj(obj){;}
+St_ObjectSet::St_ObjectSet(const Char_t *name, TObject *obj):St_DataSet(name,obj?obj->GetTitle():"St_ObjectSet"),m_Obj(obj){;}
 //_____________________________________________________________________________
 St_ObjectSet::St_ObjectSet(TObject *obj) : St_DataSet(obj?obj->GetName():"unknown",obj?obj->GetTitle():"St_ObjectSet")
               ,m_Obj(obj){;}
-
+//_____________________________________________________________________________
+ St_ObjectSet::St_ObjectSet(St_ObjectSet &set,TObject *obj): St_DataSet(set),
+  m_Obj(obj?obj:set.GetObject()){}
 //_____________________________________________________________________________
 St_ObjectSet::~St_ObjectSet(){;}
+//______________________________________________________________________________
+TObject *St_ObjectSet::Clone() {
+   return new St_ObjectSet(*this,GetObject());
+}
 
-//_____________________________________________________________________________
-TObject *St_ObjectSet::GetObject() { return m_Obj;}
