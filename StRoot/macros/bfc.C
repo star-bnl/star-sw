6cba7de3422e09a9339b619567692aa4c76608c7
fisyak <>
1999-01-11 18:29:33 +0000

    More HTML correction from Gene

--- StRoot/macros/bfc.C
+++ StRoot/macros/bfc.C
@@ -1,5 +1,8 @@
-// $Id: bfc.C,v 1.33 1999/01/08 21:39:52 fisyak Exp $
+// $Id: bfc.C,v 1.34 1999/01/11 18:29:33 fisyak Exp $
 // $Log: bfc.C,v $
+// Revision 1.34  1999/01/11 18:29:33  fisyak
+// More HTML correction from Gene
+//
 // Revision 1.33  1999/01/08 21:39:52  fisyak
 // Add Gene Van Buren bfc description
 //
@@ -11,13 +14,22 @@ at the beginning of the line so that, as in normal C/C++, they are
 not interpretted by Root.<br>
 
 The comments in this macro file follow the outline of the steps
-for writing a macro described in <a href="/STAR/html/comp_l/train/root/RootMacros.html">Understanding Root Macros</a>.
+for writing a macro described in <a href="/STAR/html/comp_l/train/root/RootMacros.html">Understanding Root Macros</a>.<br>
+
+The code in this macro begins with the definitions of some pointers we
+will use later. The macro then defines two functions: Load and bfc.
+The bfc function definition allows some input parameters to be specified,
+and then determines whether to call Load.
 </font> End_Html -------------------------------------*/
+
 TBrowser *b = 0;
 class StChain;
 StChain  *chain=0;
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
-<b>STEP 1:</b> Next in the macro are a series of system load calls.
+<b>STEP 1:</b> Here in the macro are a series of system load calls. In
+this example, they enumerated in a function which may or may not get
+called depending on whether the libraries have already been loaded.
 This is where Root picks up what it needs to know to run our programs.
 These are calls which load the code libraries containing the classes we
 will use. A few loads are of particular interest here. <a
@@ -40,6 +52,7 @@ End_Html St_Table Begin_Html itself is in the <a href="http://www.rhic.bnl.gov/c
 One can see many individual libraries loaded for each analysis module
 among the load calls as well.
 </font> End_Html -------------------------------------*/
+
 void Load(){
     gSystem->Load("St_base");
     gSystem->Load("StChain");
@@ -84,16 +97,14 @@ void Load(){
     gSystem->Load("St_dst_Maker");
     gSystem->Load("St_run_summary_Maker");
 }
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 2:</b> Next, we must specify input files we want to process.
-Here, any input/output files are given a reference pointer and a class.
+The actual file names are specified in the bfc function call. Then,
+any input/output files are given a reference pointer and a class.
 In particular, xdf files need the End_Html St_XDFFile Begin_Html class loaded in the
 <a href="http://www.rhic.bnl.gov/cgi-bin/star-cgi/cvsweb.pl/StRoot/xdf2root/"><i>xdf2root</i></a> library mentioned above, while Root files need
-simply the End_Html TFile Begin_Html base class.<br>
-
-You will see many lines in macro files which call End_HTML gBenchmark->Start() Begin_HTML
-and End_Html gBenchmark->Stop(). Begin_Html These allow processing to be timed, and their use is
-clear enough not to warrant further discussion here.
+simply the End_Html TFile Begin_Html base class.
 </font> End_Html -------------------------------------*/
 
 bfc(const Int_t   Nevents=1,
@@ -113,16 +124,21 @@ bfc(const Int_t   Nevents=1,
   TFile       *root_out  = 0; 
   if (FileOut) root_out  =  new TFile(FileOut,"RECREATE");
   //TFile      *root_tree= new TFile("auau_central_hijing.tree.root","RECREATE");
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
-<b>STEP 3:</b> You may instantly recognize this as the construction of a <i>chain</i>.
-The fact that it is actually End_Html StChainSpy, Begin_HTML and not End_Html StChain, Begin Html may make you
-worry a bit at first. But the difference is minimal: End_Html StChainSpy Begin_Html allows all
-running chains to be monitered across Root sessions and across the network.
+<b>STEP 3:</b> You may instantly recognize this next bit as the construction of a
+<i>chain</i>.  We first check to see if an old chain exists and delete it if it
+does. The new chain is given the name "bfc".<br>
+
+One could easily substitute the End_Html StChainSpy Begin_Html class for the
+End_Html StChain Begin_Html class. End_Html StChainSpy Begin_Html allows all running chains to
+be monitered across Root sessions and across the network.
 </font> End_Html -------------------------------------*/
 
   // Create the main chain object
   if (chain) delete chain;
   chain = new StChain("bfc");
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 4:</b> Next it's time to construct our <i>makers</i>. Each maker
 puts its name on a list to which the chain has access. This allows the
@@ -133,6 +149,7 @@ some basic knowledge about the analysis module of interest. You can generally
 get by with copying it from a macro like this one. Note that xdf input
 files require a maker to read in the data file.
 </font> End_Html -------------------------------------*/
+
   //  Create the makers to be called by the current chain
   St_params_Maker  *params = new St_params_Maker("params","params");
   St_TLA_Maker       *geom = new St_TLA_Maker("geom","run/geant/Run");
@@ -166,6 +183,7 @@ files require a maker to read in the data file.
   
   // Create HTML docs of all Maker's involved
   //  chain->MakeDoc();
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 5:</b> The PrintInfo() member function of chain just prints version
 numbers and any other log-worthy information about the makers in the chain.
@@ -173,16 +191,23 @@ The Init() member function goes down the list of makers and calls any
 initiallization routines each maker may have. If any returns a non-zero
 value (failure), then Init() returns a non-zero value as well.
 </font> End_Html -------------------------------------*/
+
   chain->PrintInfo();
   // Init the chain and all its makers
   int iInit = chain->Init();
   if (iInit) chain->Fatal(iInit,"on init");
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
-In this macro, the xdfin maker for the xdf file will read
-in the first record of the file, which contains run header
+In this macro, the xdfin maker for the xdf file will read in the
+first record of the file during Init, which contains run header
 information including paramaters. These parameters are
-subsequently written to the output files with statements like:
+subsequently written to the output files here.<br>
+
+You will see many lines in macro files which call End_HTML gBenchmark->Start() Begin_HTML
+and End_Html gBenchmark->Stop(). Begin_Html These allow processing to be timed, and their use is
+clear enough not to warrant further discussion here.
 </font> End_Html -------------------------------------*/
+
   //  chain->MakeTree("StChainTree","Title");
   //  chain->SetBranches();
   // Prepare TCanvas to show some histograms created by makers
@@ -202,21 +227,24 @@ subsequently written to the output files with statements like:
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 6a:</b> Now it's time to loop over the events. A simple <i>for</i>
 loop suffices. The key ingredient in the event loop is the call to
-chain.Make(). Again, chain goes through the list of makers, calling their
+chain->Make(). Again, chain goes through the list of makers, calling their
 individual Make() member functions and returning a zero if all goes OK.
 It is in these makers' Make() routines that analysis code for each event
 is processed. Any new tables these routines create and fill are placed in
 their maker's branch of the overall data tree structure.
 </font> End_Html -------------------------------------*/
+
   gBenchmark->Start("bfc");
   Int_t i=0;
   for (Int_t i =1; i <= Nevents; i++){
     if (chain->Make(i)) break;
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 6b:</b> The data is now available for any further processing, such
 as histogramming or writing to an output file. Below, the entire "dst"
 branch of the data structure is output to disk:
 </font> End_Html -------------------------------------*/
+
     St_DataSet *dst = chain->DataSet("dst");
     if (dst) {
       if (xdf_out){
@@ -237,27 +265,31 @@ branch of the data structure is output to disk:
     //    chain->FillTree();
     //  histCanvas->Modified();
     //  histCanvas->Update();
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 6c:</b> The data for each event must be cleared from memory before the
-next event is read in, and that is performed with the call to chain.Clear(). 
+next event is read in, and that is performed with the call to chain->Clear(). 
 An <i>if</i> statement here prevents the chain from clearing on the final
 event in this particular example, permitting the data from the last event to
 persist in memory after the macro finishes.
 </font> End_Html -------------------------------------*/
+
     if (i != Nevents) chain->Clear();
     printf ("===========================================\n");
     printf ("=========================================== Done with Event no. %d\n",i);
     printf ("===========================================\n");
   }
+
 /*------------------------------------- Begin_Html <font color="#800000" size="-1">
 <b>STEP 7:</b> After the event loop, the macro then makes a call to
-chain.Finish() to perform post-event loop routines in the makers.
+chain->Finish() to perform post-event loop routines in the makers.
 You should note that the file pointers are all deleted, essentially
-closing the files. In this macro, chain.Finish() is called only if
-more than one event is analyzed. Otherwise, a broswer is called up
+closing the files. In this macro, chain->Finish() is called only if
+more than one event is analyzed. Otherwise, a browser is called up
 to provide simplified access to the data structures, a useful tool
 when debugging/testing your code on a single event.
 </font> End_Html -------------------------------------*/
+
   if (Nevents > 1) {
     chain->Finish();
     delete xdf_in;
