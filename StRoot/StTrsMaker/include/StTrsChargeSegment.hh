f1974840fb65a76f2a8571f4b0d7341c7e92e89c
Brian Lasiuk <lasiuk>
1999-01-15 11:02:26 +0000

    remove g2t pointer add pid member; add systemofunits; mv access fcts to .hh add ostream operator

--- StRoot/StTrsMaker/include/StTrsChargeSegment.hh
+++ StRoot/StTrsMaker/include/StTrsChargeSegment.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTrsChargeSegment.hh,v 1.1 1998/11/10 17:12:09 fisyak Exp $
+ * $Id: StTrsChargeSegment.hh,v 1.2 1999/01/15 11:02:26 lasiuk Exp $
  *
  * Author: brian May 18, 1998
  *
@@ -11,8 +11,10 @@
  ***************************************************************************
  *
  * $Log: StTrsChargeSegment.hh,v $
- * Revision 1.1  1998/11/10 17:12:09  fisyak
- * Put Brian trs versin into StRoot
+ * Revision 1.2  1999/01/15 11:02:26  lasiuk
+ * remove g2t pointer
+ * add pid member; add systemofunits; mv access fcts to .hh
+ * add ostream operator
  *
  *
  * Revision 1.2  1999/01/15 11:02:26  lasiuk
@@ -38,14 +40,15 @@
  * Revision 1.2  1998/05/21 21:27:39  lasiuk
  * Initial revision
  *
-
+#include "iostream.h"
  * Initial Revision
  *
+ **************************************************************************/
 #ifndef ST_TRS_CHARGE_SEGMENT_HH
 #define ST_TRS_CHARGE_SEGMENT_HH
 #include <iostream.h>
 #include <list>
-#include "g2t_tpc_hit.hh"
+
 #include "SystemOfUnits.h"
 #include "StGlobals.hh"
 #include "StThreeVector.hh"
@@ -56,7 +59,12 @@
 #include "StTpcElectronics.hh"
 #include "StMagneticField.hh"
 #include "StTrsDeDx.hh"
-    StTrsChargeSegment(StThreeVector<double>&, StThreeVector<double>&, g2t_tpc_hit*);
+#include "StTrsMiniChargeSegment.hh"
+
+class StTrsChargeSegment {
+
+public:
+    StTrsChargeSegment();
     StTrsChargeSegment(StThreeVector<double>& pos,
 		       StThreeVector<double>& mom,
 		       double de,                  // energy deposited
@@ -65,34 +73,45 @@
 		       float  ne  = -1);           // numberOfElectrons
 
     ~StTrsChargeSegment();
-    StThreeVector<double>& position();
-    StThreeVector<double>& momentum();
-    double                 dE();
-    double                 ds();
+    //StTrsChargeSegment(const StTrsChargeSegment&);
+    //StTrsChargeSegment& operator=(cont StTrsChargeSegment&);
+
+    //
+    // access functions
+    //
     const StThreeVector<double>& position()          const;
     const StThreeVector<double>& momentum()          const;
     double                 dE()                const;
     double                 ds()                const;
-    void  split(StTrsDeDx*, StMagneticField*, int, double, list<StTrsMiniChargeSegment>*);
+    void  rotate(StTpcGeometry*, StTpcSlowControl*);
     float                  numberOfElectrons() const;
-        void  split(StTrsDeDx*, StMagneticField*, int, double, list<StTrsMiniChargeSegment, allocator<StTrsMiniChargeSegment> >*);
+    
     void  rotate(StTpcGeometry*, StTpcSlowControl*, StTpcElectronics*);
 
 #ifndef ST_NO_TEMPLATE_DEF_ARGS
-    // NOTE: This should be a wrapped table so we don't have to copy...
-    //       For now copy the vectors...
-    g2t_tpc_hit*  mG2tTpcHit ;
     void  split(StTrsDeDx*, StMagneticField*, int, list<StTrsMiniChargeSegment>*);
 #else
         void  split(StTrsDeDx*, StMagneticField*, int, list<StTrsMiniChargeSegment, allocator<StTrsMiniChargeSegment> >*);
 #endif
 
+private:
     StThreeVector<double> mPosition;
     StThreeVector<double> mSector12Position;
     StThreeVector<double> mMomentum;
     double                mDE;
+    double                mDs;
+    int                   mPid;
+    
     float                 mNumberOfElectrons;
+    int                   mSectorOfOrigin;
+
+    // NOTE: This should be a wrapped table so we don't have to copy...
+    //       For now copy the vectors...
+    //g2t_tpc_hit*  mG2tTpcHit ;
+};
 inline const StThreeVector<double>& StTrsChargeSegment::position() const {return mPosition;}
+inline const StThreeVector<double>& StTrsChargeSegment::momentum() const {return mMomentum;}
+ostream& operator<<(ostream&, StTrsChargeSegment&);
 inline double StTrsChargeSegment::ds() const {return mDs;}
 inline int StTrsChargeSegment::pid() const {return mPid;}
 inline float StTrsChargeSegment::numberOfElectrons() const {return mDs;}
