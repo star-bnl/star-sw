4ad0aaccd50bea6867c38a82126b02fad2e01047
wenaus <>
1999-01-21 20:51:42 +0000

    Fix for Sun compiler peculiarity

--- StRoot/StEvent/StEvent.cc
+++ StRoot/StEvent/StEvent.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StEvent.cc,v 1.2 1999/01/15 22:53:39 wenaus Exp $
+ * $Id: StEvent.cc,v 1.3 1999/01/21 20:51:41 wenaus Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  *
@@ -14,19 +14,23 @@
  ***************************************************************************
  *
  * $Log: StEvent.cc,v $
- * Revision 1.2  1999/01/15 22:53:39  wenaus
- * version with constructors for table-based loading
+ * Revision 1.3  1999/01/21 20:51:41  wenaus
+ * Fix for Sun compiler peculiarity
+ *
+ * Revision 1.4  1999/01/30 23:03:10  wenaus
+ * table load intfc change; include ref change
  *
  * Revision 1.3  1999/01/21 20:51:41  wenaus
  * Fix for Sun compiler peculiarity
+ *
  * Revision 1.2  1999/01/15 22:53:39  wenaus
-static const char rcsid[] = "$Id: StEvent.cc,v 1.2 1999/01/15 22:53:39 wenaus Exp $";
+static const char rcsid[] = "$Id: StEvent.cc,v 1.3 1999/01/21 20:51:41 wenaus Exp $";
  *
 #if !defined(ST_NO_NAMESPACES)
-static const char rcsid[] = "$Id: StEvent.cc,v 1.2 1999/01/15 22:53:39 wenaus Exp $";
+static const char rcsid[] = "$Id: StEvent.cc,v 1.3 1999/01/21 20:51:41 wenaus Exp $";
 #endif
 
-static const char rcsid[] = "$Id: StEvent.cc,v 1.2 1999/01/15 22:53:39 wenaus Exp $";
+static const char rcsid[] = "$Id: StEvent.cc,v 1.3 1999/01/21 20:51:41 wenaus Exp $";
 StEvent::StEvent(StRun* run, dst_event_header_st* hdr, dst_event_summary_st* sum) {
 StEvent::StEvent()
     mType = hdr->event_type;
@@ -36,6 +40,8 @@ StEvent::StEvent()
     mTime = hdr->time;
     mTriggerMask = hdr->trig_mask;
     mBunchCrossingNumber = hdr->bunch_cross;
+    mId.first = hdr.n_event[0];
+    mRunNumber = hdr.n_run;
     mTriggerMask = hdr.trig_mask;
     mBunchCrossingNumber = hdr.bunch_cross;
     string dummyString;
