46e25911662781e127bae5e2f60b4b0f4beff085
Pavel Nevski <nevski>
1999-01-26 16:02:38 +0000

    first g2r attempt

--- StRoot/St_geant_Maker/St_geant_Maker.h
+++ StRoot/St_geant_Maker/St_geant_Maker.h
@@ -1,38 +1,5 @@
-// $Id: St_geant_Maker.h,v 1.4 1999/01/05 01:37:02 fisyak Exp $
-// $Log: St_geant_Maker.h,v $
-// Revision 1.4  1999/01/05 01:37:02  fisyak
-// Intermeidate version with St_Node
-//
-// Revision 1.3  1999/01/03 20:56:36  fisyak
-// Remove St_geom_Maker
-//
-// Revision 1.4  1998/12/25 21:02:14  nevski
-// Add Set/Get method
-//
-// Revision 1.3  1998/12/16 20:56:24  fisyak
-// Add gstar to ROOT
-//
-// Revision 1.2  1998/12/04 19:36:48  fisyak
-// Add Pavel/Ruben gstar interface
-//
-// Revision 1.1  1998/10/31 00:28:31  fisyak
-// Makers take care about branches
-//
-// Revision 1.6  1998/10/06 18:00:31  perev
-// cleanup
-//
-// Revision 1.5  1998/08/26 12:15:13  fisyak
-// Remove asu & dsl libraries
-//
-// Revision 1.4  1998/08/14 15:25:58  fisyak
-// add options
-//
-// Revision 1.3  1998/08/10 02:32:07  fisyak
-// Clean up
-//
-// Revision 1.2  1998/07/20 15:08:15  fisyak
-// Add tcl and tpt
-//
+// $Id: St_geant_Maker.h,v 1.5 1999/01/26 16:01:44 nevski Exp $
+
 #ifndef STAR_St_geant_Maker
 #define STAR_St_geant_Maker
 
@@ -46,7 +13,7 @@
 class St_Node;
 
 #endif
-
+class TRotMatrix;
 class St_geant_Maker : public StMaker {
  private:
   Bool_t drawinit;
@@ -72,8 +39,12 @@ class St_geant_Maker : public StMaker {
    virtual void   SetIwtype  (Int_t n=      0) {iwtype  = n;} // *MENU
 
    virtual void   Work();
-   St_Node* GetNode() { return fNode; }
+   virtual TRotMatrix *GetMatrix(float theta1, float phi1,
+                                 float theta2, float phi2,
+                                 float theta3, float phi3);
 
+   St_Node* GetNode() { return fNode; }
+   //----------------------------------------------------------------------
    ClassDef(St_geant_Maker, 1)   //StAF chain virtual base class for Makers
 };
 
