a4696d28e1df59c11b20ac66c9e8d018908c9ace
Valeri Fine <val.fine@gmail.com>
1999-01-26 16:04:53 +0000

    Add() can set the custom file name

--- StRoot/St_io_Maker/St_io_Maker.h
+++ StRoot/St_io_Maker/St_io_Maker.h
@@ -12,14 +12,14 @@
 #ifndef StMaker_H
 #include "StMaker.h"
 #endif
-
+ 
 class  TBranch;
 class  TTree;
 
 class St_io_Maker : public StMaker {
  private:
    Bool_t drawinit;
-// static Char_t  m_VersionCVS = "$Id: St_io_Maker.h,v 1.3 1999/01/20 23:45:58 fine Exp $";
+// static Char_t  m_VersionCVS = "$Id: St_io_Maker.h,v 1.4 1999/01/26 16:04:53 fine Exp $";
 // Int_t          m_mode;        // mode 1 = primaries;
    TObjArray     *m_ListOfBranches; //!
 
@@ -28,9 +28,9 @@ class St_io_Maker : public StMaker {
             St_io_Maker(const char *name="output", const char *title="io_something",TTree *tree=0);
    virtual  ~St_io_Maker();
    virtual  TObjArray *GetListOfBranches(){return m_ListOfBranches;}
-   virtual void Add(const Char_t *dataName);
-   virtual void Add(TString &dataName);
-   virtual void Add(TBranch *branch,const Char_t *dataName=0);
+   virtual void Add(const Char_t *dataName,const Char_t *fileName=0);
+   virtual void Add(TString &dataName,const Char_t *fileName=0);
+   virtual void Add(TBranch *branch,const Char_t *dataName=0,const Char_t *fileName=0);
    virtual TObjArray *CreateBranchList() {if (!m_ListOfBranches) m_ListOfBranches = new TObjArray; return m_ListOfBranches;}
    virtual St_DataSet *DataSet(const Char_t *set);
    virtual Int_t  Finish();
