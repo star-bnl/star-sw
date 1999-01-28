7a162e236098504f62e5f89c0984263927630afe
lasiuk <>
1999-01-28 02:48:59 +0000

    include sector coordinates

--- StRoot/StTrsMaker/include/StCoordinates.hh
+++ StRoot/StTrsMaker/include/StCoordinates.hh
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * $Id: StCoordinates.hh,v 1.1 1998/11/10 17:12:04 fisyak Exp $
+ * $Id: StCoordinates.hh,v 1.2 1999/01/28 02:48:59 lasiuk Exp $
  *
  * Author: brian May 19, 1998
  *
@@ -11,6 +11,9 @@
  ***********************************************************************
  *
  * $Log: StCoordinates.hh,v $
+ * Revision 1.2  1999/01/28 02:48:59  lasiuk
+ * include sector coordinates
+ *
  * Revision 1.1  1998/11/10 17:12:04  fisyak
  * Put Brian trs versin into StRoot
  *
@@ -23,6 +26,7 @@
 #define ST_TPC_COORDINATES_HH
 
 #include "StTpcPadCoordinate.hh"
+#include "StTpcLocalSectorCoordinate.hh"
 #include "StTpcLocalCoordinate.hh"
 #include "StGlobalCoordinate.hh"
 #include "StTpcCoordinateTransform.hh"
