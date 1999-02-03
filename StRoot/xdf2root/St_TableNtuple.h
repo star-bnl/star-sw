40e91a606dddcc2d3062e2c11e0f035c741e2910
fine <>
1999-02-03 22:37:49 +0000

    a new class St_TableNtuple by  Gene Van Buren has been introduced

---
+++ StRoot/xdf2root/St_TableNtuple.h
@@ -0,0 +1,75 @@
+// $Id: St_TableNtuple.h,v 1.1 1999/02/03 22:37:49 fine Exp $
+// $Log: St_TableNtuple.h,v $
+// Revision 1.1  1999/02/03 22:37:49  fine
+// a new class St_TableNtuple by  Gene Van Buren has been introduced
+//
+//
+// Revision 1.1 1999/01/27 10:28:29 genevb
+//
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// St_TableNtuple                                                       //
+//                                                                      //
+// St_TableNtuple is a class to convert STAR Tables into ntuples        //
+// The class inherits from TTree, so it can be used just as a           //
+// TTree would (Draw(), etc.). Columns are not made for table           //
+// entries which are not basic numerical entities. Table entries        //
+// which are arrays are spread into multiple columns with names         //
+// given by the entry and index (e.g. chisq[3] => chisq0, chisq1,       //
+// and chisq2). The class constructor defines the TTree and its         //
+// TBranches. You must use the Fill() member function to fill the       //
+// TTree from a table.                                                  //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef STAR_St_TableNtuple
+#define STAR_St_TableNtuple
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class St_TableNtuple;
+#endif
+
+#include "TTree.h"
+
+class St_Table;
+class TFile;
+class St_XDFFile;
+class TBrowser;
+
+class St_TableNtuple : public TTree {
+ private:
+  TString   fTableClass; //  Name of table class
+    Int_t   fNvar;       //  Number of columns used
+     void **fArgs;       //! Array of pointers to variables
+   TClass  *fClassPtr;   //! StAF table class used in the table
+     enum   NumType {
+    kNAN, kFloat, kInt, kLong, kShort, kDouble, kUInt, kULong, kUShort
+  }        *fType;       //! Number type of table columns
+    Int_t  *fOffset;     //! Offset values for pointers to table columns
+
+ protected:
+   virtual    void LearnTable(const St_Table &table, Bool_t buildTree=kFALSE, Int_t bufsize=8000);
+
+ public:
+                   St_TableNtuple();
+                   St_TableNtuple(const St_Table &table, Int_t bufsize=8000);
+                   ~St_TableNtuple();
+   virtual   Int_t AddTFile(const Char_t *file, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
+   virtual   Int_t AddTFile(TFile &f, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
+   virtual   Int_t AddXDFFile(const Char_t *file, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
+   virtual   Int_t AddXDFFile(St_XDFFile &f, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
+   virtual    void Browse(TBrowser *b);
+   virtual   Int_t Fill(const St_Table &table, Int_t firstRow=0, Int_t nRows=-1);
+   virtual   Int_t GetNvar() const { return fNvar; }
+   virtual const Char_t* GetStTableClassName() const { return fTableClass.Data(); }
+   virtual TClass* GetTableClass() const { return fClassPtr; }
+   virtual    void PrintInfo();
+   ClassDef(St_TableNtuple,1)
+};
+
+#endif
