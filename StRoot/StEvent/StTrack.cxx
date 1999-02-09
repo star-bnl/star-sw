4c5d5ea9df3baa76545e8c05dd284c30f89331d6
fisyak <>
1999-02-09 21:19:37 +0000

    Import new Torre staff

--- StRoot/StEvent/StTrack.cxx
+++ StRoot/StEvent/StTrack.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTrack.cxx,v 1.1 1999/01/30 03:58:08 fisyak Exp $
+ * $Id: StTrack.cxx,v 1.2 1999/02/09 21:19:37 fisyak Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  *
@@ -13,8 +13,8 @@
  ***************************************************************************
  *
  * $Log: StTrack.cxx,v $
- * Revision 1.1  1999/01/30 03:58:08  fisyak
- * Root Version of StEvent
+ * Revision 1.2  1999/02/09 21:19:37  fisyak
+ * Import new Torre staff
  *
  * Revision 1.3  1999/02/12 02:01:19  wenaus
  * New track constructor to load helix params independently of table
@@ -22,19 +22,19 @@
  * Revision 1.2  1999/01/15 22:54:02  wenaus
  * version with constructors for table-based loading
  *
-static const Char_t rcsid[] = "$Id: StTrack.cxx,v 1.1 1999/01/30 03:58:08 fisyak Exp $";
+static const Char_t rcsid[] = "$Id: StTrack.cxx,v 1.2 1999/02/09 21:19:37 fisyak Exp $";
  * New decoding for dst_track::method. New enum added.
 #ifdef __ROOT__
  *
-static const Char_t rcsid[] = "$Id: StTrack.cxx,v 1.1 1999/01/30 03:58:08 fisyak Exp $";
+static const Char_t rcsid[] = "$Id: StTrack.cxx,v 1.2 1999/02/09 21:19:37 fisyak Exp $";
 #endif
 StTrack::StTrack() : mHelix(0, 0, 0, StThreeVectorF())
 ClassImp(StTrack)
 ClassImp(StTrack)
 StTrack::StTrack() : mHelix(0, 0, 0, StThreeVectorD())
-static const char rcsid[] = "$Id: StTrack.cxx,v 1.1 1999/01/30 03:58:08 fisyak Exp $";
+static const char rcsid[] = "$Id: StTrack.cxx,v 1.2 1999/02/09 21:19:37 fisyak Exp $";
     mStartVertex = 0;
-StTrack::StTrack(dst_track_st* trk) : mFitTraits(trk), mHelix(0, 0, 0, StThreeVectorF())
+StTrack::StTrack(dst_track_st* trk) : mFitTraits(trk), mHelix((double )0 , (double )0, (double )0 , StThreeVectorF())
 StTrack::StTrack(dst_track_st* trk) : 
     mStartVertex = 0;
     mStopVertex   = 0;
@@ -52,12 +52,11 @@ const StTrackGeometry*
 StTrackGeometry*
 Int_t StTrack::operator!=(const StTrack& t) const
 
-void StTrack::setHelix(const StPhysicalHelixD& val) { mHelix = val; }
+void StTrack::setHelix(const StPhysicalHelix& val) { mHelix = val; }
 {
     return pid(*this, mPidTraitsVec);
 void StTrack::setHelix(const StPhysicalHelixD& val) { mHelix = val; }
 StTrackNode*
-//______________________________________________________________________________
 void StTrack::setStartVertex(StVertex* val) { mStartVertex = val; }
 void
 void StTrack::setStopVertex(StVertex* val) { mStopVertex = val; }
