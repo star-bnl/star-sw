2d041fb37067c12dfbaffbbcd25fc4aebbee3199
druss <>
1999-02-11 19:46:33 +0000

    MWC maker for mdc2.  Removed print statements, set parameters

--- StRoot/St_mwc_Maker/St_mwc_Maker.cxx
+++ StRoot/St_mwc_Maker/St_mwc_Maker.cxx
@@ -1,5 +1,8 @@
-// $Id: St_mwc_Maker.cxx,v 1.2 1999/01/29 22:55:19 druss Exp $
+// $Id: St_mwc_Maker.cxx,v 1.3 1999/02/11 19:46:33 druss Exp $
 // $Log: St_mwc_Maker.cxx,v $
+// Revision 1.3  1999/02/11 19:46:33  druss
+// MWC maker for mdc2.  Removed print statements, set parameters
+//
 // Revision 1.2  1999/01/29 22:55:19  druss
 // corrected bugs, included new parameters from mwc parameter table
 //
@@ -40,8 +43,7 @@ ClassImp(St_mwc_Maker)
 
 //_____________________________________________________________________________
 St_mwc_Maker::St_mwc_Maker(const char *name, const char *title):StMaker(name,title){
-   printf(" ----- Welcome to St_mwc_Maker -----\n");
-   printf(" d.e. russ:1/29/99 \n");
+
    drawinit=kFALSE;
 }
 //_____________________________________________________________________________
@@ -49,10 +51,6 @@ St_mwc_Maker::~St_mwc_Maker(){
 }
 //_____________________________________________________________________________
 Int_t St_mwc_Maker::Init(){
-// Create tables
-//     first Create parameter tables:
-
-   printf(" ----- Welcome to St_mwc_Maker::Init -----\n");
 
    // set pointers to table wrappers
 
@@ -68,9 +66,8 @@ Int_t St_mwc_Maker::Init(){
 
    // setting table values
 
-   printf("setting geom table\n");
    geom.init  = 0;
-   geom.neta  = 16;
+   geom.neta  = 4;
    geom.nphi  = 12;
    geom.r1max = 118.669;
    geom.r1min = 54.669;
@@ -78,11 +75,11 @@ Int_t St_mwc_Maker::Init(){
    geom.r2min = 125.488;
    m_geom->AddAt(&geom,0);
 
-   printf("setting cal table\n");
    cal.cc     = 1.00;
+   cal.pd     = 0.00;
+   cal.ps     = 0.00;
    m_cal->AddAt(&cal,0);
 
-   printf("setting param table\n");  
    mpar.gain            = 11.0;     
    mpar.de_thresh_in    = 2.0e-8;     
    mpar.de_thresh_out   = 3.0e-8;
@@ -91,22 +88,14 @@ Int_t St_mwc_Maker::Init(){
    mpar.num_wires_count = 80.0;
    mpar.el_noise_width  = 0.00;
    mpar.min_ion         = 0.00;
-   mpar.wires           = 1;
-   m_mpar->AddAt(&mpar,0);
 
-   // check tables
-   
-   m_geom->ls("*");
-   m_cal ->ls("*");
-   m_mpar->ls("*");
-   
+   m_mpar->AddAt(&mpar,0);
 
 // Create Histograms    
    return StMaker::Init();
 }
 //_____________________________________________________________________________
 Int_t St_mwc_Maker::Make(){
-   printf(" ----- Welcome to St_mwc_Maker::Make -----\n");
 
 //  PrintInfo();
 
@@ -129,12 +118,7 @@ Int_t St_mwc_Maker::Make(){
      St_DataSetIter geant(gStChain->DataSet("geant"));
      St_g2t_mwc_hit *g2t_mwc_hit = (St_g2t_mwc_hit *) geant("g2t_mwc_hit");
 
-     printf ("checking some values \n");
-     g2t_mwc_hit_st *g2t_mwc = g2t_mwc_hit->GetTable();
-     printf("id: %i, p[0]: %f, p[1]: %f, p[2]: %f \n",g2t_mwc->id,
-            g2t_mwc->p[0],g2t_mwc->p[1],g2t_mwc->p[2]);
 
-     printf ("checking if the tables exist \n");
      if (!g2t_mwc_hit) {printf("g2t_mwc_hit does not exist\n");return kStErr;}
      if (!m_geom)      {printf("m_geom does not exist\n")     ;return kStErr;}
      if (!m_mpar)      {printf("m_mpar does not exist\n")     ;return kStErr;} 
@@ -142,7 +126,6 @@ Int_t St_mwc_Maker::Make(){
      if (!sector)      {printf("sector does not exist\n")     ;return kStErr;} 
      if (!raw)         {printf("raw does not exist\n")        ;return kStErr;} 
 
-     printf ("------------ Calling mws -------\n");
      Int_t mwc_result = mws(
                             g2t_mwc_hit,
                             m_geom,
@@ -162,7 +145,7 @@ Int_t St_mwc_Maker::Make(){
 //_____________________________________________________________________________
 void St_mwc_Maker::PrintInfo(){
   printf("**************************************************************\n");
-  printf("* $Id: St_mwc_Maker.cxx,v 1.2 1999/01/29 22:55:19 druss Exp $\n");
+  printf("* $Id: St_mwc_Maker.cxx,v 1.3 1999/02/11 19:46:33 druss Exp $\n");
 //  printf("* %s    *\n",m_VersionCVS);
   printf("**************************************************************\n");
   if (gStChain->Debug()) StMaker::PrintInfo();
