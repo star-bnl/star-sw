03907f22b96935f2bbfda984c804d13f5774fdd0
Torre Wenaus <wenaus>
1999-02-12 02:01:20 +0000

    New track constructor to load helix params independently of table

--- StRoot/StEvent/StTrack.cc
+++ StRoot/StEvent/StTrack.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTrack.cc,v 1.2 1999/01/15 22:54:02 wenaus Exp $
+ * $Id: StTrack.cc,v 1.3 1999/02/12 02:01:19 wenaus Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  *
@@ -13,25 +13,39 @@
  ***************************************************************************
  *
  * $Log: StTrack.cc,v $
- * Revision 1.2  1999/01/15 22:54:02  wenaus
- * version with constructors for table-based loading
+ * Revision 1.3  1999/02/12 02:01:19  wenaus
+ * New track constructor to load helix params independently of table
+ *
+ * Revision 1.3  1999/02/12 02:01:19  wenaus
+ * New track constructor to load helix params independently of table
  *
  * Revision 1.2  1999/01/15 22:54:02  wenaus
  * version with constructors for table-based loading
  *
-static const char rcsid[] = "$Id: StTrack.cc,v 1.2 1999/01/15 22:54:02 wenaus Exp $";
+static const char rcsid[] = "$Id: StTrack.cc,v 1.3 1999/02/12 02:01:19 wenaus Exp $";
 #include "StEvent/StTrack.hh"
 
-static const char rcsid[] = "$Id: StTrack.cc,v 1.2 1999/01/15 22:54:02 wenaus Exp $";
+static const char rcsid[] = "$Id: StTrack.cc,v 1.3 1999/02/12 02:01:19 wenaus Exp $";
 
 StTrack::StTrack() : mHelix(0, 0, 0, StThreeVector<double>())
 {
     mStartVertex = 0;
-StTrack::StTrack(dst_track_st* trk) : mFitTraits(trk), mHelix(0, 0, 0, StThreeVector<double>())
+    mStopVertex   = 0;
+}
 
 StTrack::StTrack(dst_track_st* trk) : mFitTraits(trk),
   mHelix(0, 0, 0, StThreeVector<double>())
 {
+    mStartVertex = 0;
+    mStopVertex   = 0;
+                 double& curvature,
+                 double& dip,
+                 double& phase,
+                 double curvature,
+                 double dip,
+                 double phase,
+                 StThreeVector<double>& origin) : mFitTraits(trk),
+  mHelix(curvature, dip, phase, origin)
 {  
 }
 
