22926c6e544a8acd8fea7f8c1d8fd8ad7a816f85
genevb <>
1999-02-17 22:54:15 +0000

    Fixed errors when no tables/datasets found in St_TableNtuple

--- StRoot/xdf2root/St_TableNtuple.h
+++ StRoot/xdf2root/St_TableNtuple.h
@@ -1,7 +1,7 @@
-// $Id: St_TableNtuple.h,v 1.1 1999/02/03 22:37:49 fine Exp $
+// $Id: St_TableNtuple.h,v 1.2 1999/02/17 22:54:15 genevb Exp $
 // $Log: St_TableNtuple.h,v $
-// Revision 1.1  1999/02/03 22:37:49  fine
-// a new class St_TableNtuple by  Gene Van Buren has been introduced
+// Revision 1.2  1999/02/17 22:54:15  genevb
+// Fixed errors when no tables/datasets found in St_TableNtuple
 //
 //
 // Revision 1.1 1999/01/27 10:28:29 genevb
@@ -43,21 +43,21 @@ class TBrowser;
 
 class St_TableNtuple : public TTree {
  private:
-  TString   fTableClass; //  Name of table class
-    Int_t   fNvar;       //  Number of columns used
-     void **fArgs;       //! Array of pointers to variables
-   TClass  *fClassPtr;   //! StAF table class used in the table
+  TString   mTableClass; //  Name of table class
+    Int_t   mNvar;       //  Number of columns used
+     void **mArgs;       //! Array of pointers to variables
+   TClass  *mClassPtr;   //! StAF table class used in the table
      enum   NumType {
     kNAN, kFloat, kInt, kLong, kShort, kDouble, kUInt, kULong, kUShort
-  }        *fType;       //! Number type of table columns
-    Int_t  *fOffset;     //! Offset values for pointers to table columns
+  }        *mType;       //! Number type of table columns
+    Int_t  *mOffset;     //! Offset values for pointers to table columns
 
  protected:
-   virtual    void LearnTable(const St_Table &table, Bool_t buildTree=kFALSE, Int_t bufsize=8000);
+   virtual    void LearnTable(const St_Table &table, Bool_t buildTree=kFALSE, Int_t bufsize=25000);
 
  public:
                    St_TableNtuple();
-                   St_TableNtuple(const St_Table &table, Int_t bufsize=8000);
+                   St_TableNtuple(const St_Table &table, Int_t bufsize=25000);
                    ~St_TableNtuple();
    virtual   Int_t AddTFile(const Char_t *file, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
    virtual   Int_t AddTFile(TFile &f, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
@@ -65,9 +65,9 @@ class St_TableNtuple : public TTree {
    virtual   Int_t AddXDFFile(St_XDFFile &f, Char_t *dataset, Char_t *tname, Int_t firstEvent=1, Int_t nEvents=-1);
    virtual    void Browse(TBrowser *b);
    virtual   Int_t Fill(const St_Table &table, Int_t firstRow=0, Int_t nRows=-1);
-   virtual   Int_t GetNvar() const { return fNvar; }
-   virtual const Char_t* GetStTableClassName() const { return fTableClass.Data(); }
-   virtual TClass* GetTableClass() const { return fClassPtr; }
+   virtual   Int_t GetNvar() const { return mNvar; }
+   virtual Char_t* GetStTableClassName() const { return mTableClass.Data(); }
+   virtual TClass* GetTableClass() const { return mClassPtr; }
    virtual    void PrintInfo();
    ClassDef(St_TableNtuple,1)
 };
