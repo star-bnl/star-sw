07157e85df63998d0cc4e16930e50eefa028bb3e
Thomas Ullrich <thomas.ullrich@bnl.gov>
1999-02-18 15:42:09 +0000

    Momemta of daughter tracks added.

--- StRoot/StEvent/StV0Vertex.cc
+++ StRoot/StEvent/StV0Vertex.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StV0Vertex.cc,v 1.3 1999/01/27 13:05:05 ullrich Exp $
+ * $Id: StV0Vertex.cc,v 1.4 1999/02/18 15:42:09 ullrich Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  *
@@ -13,9 +13,8 @@
  ***************************************************************************
  *
  * $Log: StV0Vertex.cc,v $
- * Revision 1.3  1999/01/27 13:05:05  ullrich
- * Renamed data member and access functions: xxxToV0 into xxxToPrimaryVertex.
- * This is the right meaning according to P. Jones.
+ * Revision 1.4  1999/02/18 15:42:09  ullrich
+ * Momemta of daughter tracks added.
  *
  * Revision 1.3  1999/01/27 13:05:05  ullrich
  * Renamed data member and access functions: xxxToV0 into xxxToPrimaryVertex.
@@ -27,15 +26,16 @@
  **************************************************************************/
 #include <iostream.h>
 #include "StEvent/StV0Vertex.hh"
-#include "StEvent/StEnumerations.hh"
 
-static const char rcsid[] = "$Id: StV0Vertex.cc,v 1.3 1999/01/27 13:05:05 ullrich Exp $";
+static const char rcsid[] = "$Id: StV0Vertex.cc,v 1.4 1999/02/18 15:42:09 ullrich Exp $";
 
 StV0Vertex::StV0Vertex()
 {
     mType = V0;			// always
-    mDcaDaughtersToPrimaryVertex[0] = 0;
-    mDcaDaughtersToPrimaryVertex[1] = 0;
+    mDcaDaughtersToPrimaryVertex[negativeTrack] = 0;
+    mDcaDaughtersToPrimaryVertex[positiveTrack] = 0;
+    mMomentumOfDaughters[negativeTrack] = StThreeVector<float>();
+    mMomentumOfDaughters[positiveTrack] = StThreeVector<float>();
     mDcaDaughters = 0;
     mDcaParentToPrimaryVertex = 0;
 }
@@ -43,18 +43,28 @@ StV0Vertex::StV0Vertex()
 StV0Vertex::StV0Vertex(dst_v0_vertex_st* v0vtx)
 {
     mType = V0;			// always
-    mDcaDaughtersToPrimaryVertex[0] = v0vtx->dcan;
-    mDcaDaughtersToPrimaryVertex[1] = v0vtx->dcap;
+    mDcaDaughtersToPrimaryVertex[negativeTrack] = v0vtx->dcan;
+    mDcaDaughtersToPrimaryVertex[positiveTrack] = v0vtx->dcap;
+    mMomentumOfDaughters[negativeTrack].setX(v0vtx->neg_px);
+    mMomentumOfDaughters[negativeTrack].setY(v0vtx->neg_py);
+    mMomentumOfDaughters[negativeTrack].setZ(v0vtx->neg_pz);
+    mMomentumOfDaughters[positiveTrack].setX(v0vtx->pos_px);
+    mMomentumOfDaughters[positiveTrack].setY(v0vtx->pos_py);
+    mMomentumOfDaughters[positiveTrack].setZ(v0vtx->pos_pz);
     mDcaDaughters = v0vtx->dcapn;
     mDcaParentToPrimaryVertex = v0vtx->dcav0;
 }
 
 StV0Vertex::~StV0Vertex() { /* noop */ }
 
-void StV0Vertex::setDcaDaughtersToPrimaryVertex(unsigned int i, float val)
+void StV0Vertex::setDcaDaughterToPrimaryVertex(StTrackSign sign, float val)
 {
-    if (i < 2)
-	mDcaDaughtersToPrimaryVertex[i] = val;
+    mDcaDaughtersToPrimaryVertex[sign] = val;
+}
+
+void StV0Vertex::setMomentumOfDaughter(StTrackSign sign, const StThreeVector<float>& v)
+{
+    mMomentumOfDaughters[sign] = v;
 }
 
 void StV0Vertex::setDcaDaughters(float val) { mDcaDaughters = val; }
