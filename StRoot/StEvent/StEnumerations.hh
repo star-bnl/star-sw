655cfdb888b5a5fdecbaa9da032cfd7034ac48dd
Thomas Ullrich <thomas.ullrich@bnl.gov>
1999-02-18 15:40:06 +0000

    Added CINT link pragmas, detector enumeration changed but official version still pending, added enum StTrackSign.

--- StRoot/StEvent/StEnumerations.hh
+++ StRoot/StEvent/StEnumerations.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StEnumerations.hh,v 1.2 1999/01/15 22:53:38 wenaus Exp $
+ * $Id: StEnumerations.hh,v 1.3 1999/02/18 15:40:06 ullrich Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  ***************************************************************************
@@ -10,8 +10,9 @@
  ***************************************************************************
  *
  * $Log: StEnumerations.hh,v $
- * Revision 1.2  1999/01/15 22:53:38  wenaus
- * version with constructors for table-based loading
+ * Revision 1.3  1999/02/18 15:40:06  ullrich
+ * Added CINT link pragmas, detector enumeration changed but official
+ * version still pending, added enum StTrackSign.
  *
  * Add Xi vertex type
  *
@@ -20,7 +21,16 @@
  * version still pending, added enum StTrackSign.
  *
  * Revision 1.2  1999/01/15 22:53:38  wenaus
-enum StDetector {tpc, svt, emc, smd, ftpc, tof, rich};
+ * version with constructors for table-based loading
+ *
+ **************************************************************************/
+#ifndef StEnumeration_hh
+#define StEnumeration_hh
+
+#ifdef __CINT__
+#pragma link C++ enum StDetector;
+enum StDetector {tpc, svt, ssd, ftpcEast, ftpcWest, rich, tof,
+		 emcTower, emcPreShower, smdEta, smdPhi};
 		 barrelEmcTower, barrelEmcPreShower, barrelSmdEta, barrelSmdPhi,
 		 endcapEmcTower, endcapEmcPreShower, endcapSmdEta, endcapSmdPhi,
                  ctb, ssd, zdcWest, zdcEast, mwpcWest, mwpcEast};
@@ -29,6 +39,8 @@ enum StDetector {tpc, svt, emc, smd, ftpc, tof, rich};
 #endif
 
 		   pileUpPrimary, V0};
+
+enum StBeamPolarizationAxis {sideways, vertical, longitudinal};
 
 enum StVertexType {undefined, primary, kink,
 		   twoBody, threeBody, nBody,
