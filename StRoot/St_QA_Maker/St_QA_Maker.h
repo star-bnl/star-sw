16e6cd86fca0b672d69b134e182aa6a4e7ace181
Kathleen R. Turner <kathy>
1999-02-25 19:25:39 +0000

    fix up histograms

--- StRoot/St_QA_Maker/St_QA_Maker.h
+++ StRoot/St_QA_Maker/St_QA_Maker.h
@@ -1,5 +1,8 @@
-// $Id: St_QA_Maker.h,v 1.4 1999/02/24 21:15:04 kathy Exp $
+// $Id: St_QA_Maker.h,v 1.5 1999/02/25 19:25:39 kathy Exp $
 // $Log: St_QA_Maker.h,v $
+// Revision 1.5  1999/02/25 19:25:39  kathy
+// fix up histograms
+//
 // Revision 1.4  1999/02/24 21:15:04  kathy
 // fixed histograms and added a few new ones
 //
@@ -60,14 +63,67 @@
 class St_QA_Maker : public StMaker {
  private:
    Bool_t drawinit;
-// static Char_t m_VersionCVS = "$Id: St_QA_Maker.h,v 1.4 1999/02/24 21:15:04 kathy Exp $";
-   // Histograms
+// static Char_t m_VersionCVS = "$Id: St_QA_Maker.h,v 1.5 1999/02/25 19:25:39 kathy Exp $";
+   // Histograms booking constants
    static const Int_t nxpT;
    static const Int_t nyeta;
    static const Float_t xminpT;
    static const Float_t xmaxpT;
    static const Float_t ymineta;
    static const Float_t ymaxeta;
+
+     static const Int_t nchisq;
+     static const Int_t nmass;
+     static const Int_t ntau; 
+     static const Int_t ndedx;  
+     static const Int_t npnt;   
+     static const Int_t nleng;  
+     static const Int_t npsi;   
+     static const Int_t knpsi;  
+     static const Int_t ntrk;   
+     static const Int_t nvrt;   
+     static const Int_t nmnpt;  
+     static const Int_t nmneta; 
+     static const Int_t nxyz;   
+     static const Int_t knyeta; 
+     static const Int_t knid;   
+     static const Int_t cnp; 
+     static const Int_t cndedx; 
+
+     static const Float_t kminnid;  
+     static const Float_t kmaxnid; 
+     static const Float_t minpsi;   
+     static const Float_t kminpsi;  
+     static const Float_t maxpsi;   
+     static const Float_t minchisq; 
+     static const Float_t maxchisq; 
+     static const Float_t minmass;  
+     static const Float_t maxmass;  
+     static const Float_t mindedx;  
+     static const Float_t maxdedx;  
+     static const Float_t minpnt;   
+     static const Float_t maxpnt;   
+     static const Float_t minleng;  
+     static const Float_t maxleng;  
+     static const Float_t mintau;   
+     static const Float_t maxtau;   
+     static const Float_t mintrk;   
+     static const Float_t maxtrk;   
+     static const Float_t minvrt;   
+     static const Float_t maxvrt;   
+     static const Float_t minmpt;   
+     static const Float_t maxmpt;   
+     static const Float_t minmeta;  
+     static const Float_t maxmeta;  
+     static const Float_t kmineta;  
+     static const Float_t kmaxeta;  
+     static const Float_t minxyz;   
+     static const Float_t maxxyz;   
+     static const Float_t cminp; 
+     static const Float_t cmaxp; 
+     static const Float_t cmindedx; 
+     static const Float_t cmaxdedx; 
+
 
  protected:
 
@@ -140,13 +196,20 @@ class St_QA_Maker : public StMaker {
    virtual       ~St_QA_Maker();
    virtual Int_t Init();
    virtual Int_t  Make();
-   virtual void   MakeEvSum();
-   virtual void   MakeGlob();
-   virtual void   MakeDE();
+   virtual void   MakeHistEvSum();
+   virtual void   MakeHistGlob();
+   virtual void   MakeHistDE();
    virtual void   MakeHistPrim();
    virtual void   MakeHistGen();
    virtual void   MakeHistV0();
    virtual void   MakeHistPID();
+   virtual void   BookHistEvSum();
+   virtual void   BookHistGlob();
+   virtual void   BookHistDE();
+   virtual void   BookHistPrim();
+   virtual void   BookHistGen();
+   virtual void   BookHistV0();
+   virtual void   BookHistPID();
    virtual void   PrintInfo();
    ClassDef(St_QA_Maker, 1)   //StAF chain virtual base class for Makers
 };
