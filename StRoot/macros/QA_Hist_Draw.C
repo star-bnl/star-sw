55da0c612f5638b58da8d4403817c23bb8477b1d
Valeri Fine <val.fine@gmail.com>
1999-02-26 15:49:41 +0000

    QA_Hist_Draw.C havs been improved, one may select histogra to draw now

--- StRoot/macros/QA_Hist_Draw.C
+++ StRoot/macros/QA_Hist_Draw.C
@@ -1,10 +1,21 @@
 //read root histogram file and send all histograms to a postscript file
-void QA_Hist_Draw(const Char_t
-*fileName="/diskA/star/kathy/output/psc0049_08_40evts_3EV.root",
-const Char_t *psFile="QA_hist.ps")
-{
-   printf(" Usage: QA_Hist_Draw( const Char_t *fileName = \"%s\",\n\t\tconst Char_t *psFile=\"%s\")\n\n\n"
-          , fileName, psFile);
+void QA_Hist_Draw(
+     const Char_t *firstHistName="*",const Char_t *lastHistName="*",
+     const Char_t
+     *fileName="/disk00001/star/auau200/hijing135/jetq_on/b3_6/year_1b/hadronic_on/tfs/set0050_04_24evts.root",
+     const Char_t *psFile="QA_hist.ps")
+{ 
+    cout << endl   
+         << " Usage:  QA_Hist_Draw( " << endl
+         << "                        const Char_t *firstHistName=\"" << firstHistName << "\","   << endl
+         << "                        const Char_t *lastHistName=\""  << lastHistName  << "\""    << endl
+         << "                        const Char_t *fileName =\""     << fileName      << "\","   << endl
+         << "                        const Char_t *psFile=\""        << psFile        << "\","   << endl
+         << "                      );" << endl 
+         << " Note: firstHistName = \"*\" is by default and means ALL histograms from the file should be drawn" << endl
+         << " ----- lastHistName  = \"*\" is by default and means ALL histograms by the end of file should be drawn" << endl
+         << endl ;
+
   TFile file1("/diskA/star/kathy/output/psc0049_08_40evts_3EV.root");  
   // file1.ls();
   TList *keys = file1.GetListOfKeys();
@@ -14,16 +25,24 @@ const Char_t *psFile="QA_hist.ps")
     TIter nextKey(keys);
     TKey *key = 0; 
     Int_t histCounter = 0;
+    Int_t histReadCounter = 0;
+    Bool_t started = kFALSE;
     while (key = (TKey *)nextKey()) {
       TObject *obj = key->ReadObj();
-      if (obj && obj->InheritsFrom("TH1")) {
-          printf(" Drawing ... %s::%s; Title=\"%s\"\n", obj->ClassName(),obj->GetName(), obj->GetTitle());
-          obj->Draw(); 
-          histCounter++;
-          if (gPad) {
-            gPad->Modified();
-            gPad->Update();
-          }
+      if (obj && obj->InheritsFrom("TH1")) { 
+          histReadCounter++;
+          printf(" %d. Reading ... %s::%s; Title=\"%s\"\n",histReadCounter,obj->ClassName(),obj->GetName(), obj->GetTitle());
+          if (! started && (strcmp("*",firstHistName)==0 || strcmp(obj->GetName(),firstHistName)==0 ))  started = kTRUE;
+          if (started) {
+            if (strcmp(obj->GetName(),lastHistName)==0) started = kFALSE;
+            histCounter++;
+            printf("  -   %d. Drawing ... %s::%s; Title=\"%s\"\n",histCounter,obj->ClassName(),obj->GetName(), obj->GetTitle());
+            obj->Draw();   
+            if (gPad) {
+              gPad->Modified();
+              gPad->Update();
+           }
+         }
       }
     } 
   }
