91b1530cea7e6c6b0fc3f1d51c6678b8e628fb96
caines <>
1999-02-26 18:05:07 +0000

    no longer fill debug histograms

--- StRoot/St_srs_Maker/St_srs_Maker.cxx
+++ StRoot/St_srs_Maker/St_srs_Maker.cxx
@@ -46,30 +46,53 @@ Int_t St_srs_Maker::Init(){
    m_srs_srspar  = (St_srs_srspar  *) local("svt/srspars/srs_srspar");
    m_srs_direct  = (St_srs_direct  *) local("svt/srspars/srs_direct");
 // Create Histograms    
+   m_x_vs_y = new TH2F("si_x_vs_y","X vs Y of Si space points",
+                           300,-30,30,300,-30,30);
+   m_x_vs_y->SetYTitle("y cm");
+   m_x_vs_y->SetXTitle("x cm");
+
    return StMaker::Init();
 }
 //_____________________________________________________________________________
 Int_t St_srs_Maker::Make(){
-// Create output tables
-   if (!m_DataSet->GetList())  {
-     St_scs_spt    *scs_spt    = new St_scs_spt("scs_spt",20000);       m_DataSet->Add(scs_spt);
-     St_srs_result *srs_result = new St_srs_result("srs_result",20000); m_DataSet->Add(srs_result);
-     St_DataSetIter geant(gStChain->DataSet("geant"));
-     St_g2t_svt_hit *g2t_svt_hit = (St_g2t_svt_hit *) geant("g2t_svt_hit");
-     if (g2t_svt_hit ) {
+  // Create output tables
+  if (!m_DataSet->GetList())  {
+    St_scs_spt    *scs_spt    = new St_scs_spt("scs_spt",20000);       m_DataSet->Add(scs_spt);
+    St_srs_result *srs_result = new St_srs_result("srs_result",20000); m_DataSet->Add(srs_result);
+  }
+  
+  St_DataSetIter mine(gStChain->DataSet("svt_hits"));
+  St_srs_result *srs_result  = (St_srs_result *) mine("srs_result");
+  srs_result->ReAllocate(20000);
+  St_scs_spt *scs_spt = (St_scs_spt *) mine("scs_spt");
+  
+  St_DataSetIter geant(gStChain->DataSet("geant"));
+  St_g2t_svt_hit *g2t_svt_hit = (St_g2t_svt_hit *) geant("g2t_svt_hit");
+  if (g2t_svt_hit ) {
+    
+    Int_t res =  srs_am (srs_result,g2t_svt_hit,scs_spt,
+			 m_geom,m_config,m_shape,m_srs_srspar,m_srs_direct,m_srs_activea);
+  }
+  
+  //Fill histograms
 
-       Int_t res =  srs_am (srs_result,g2t_svt_hit,scs_spt,
-                          m_geom,m_config,m_shape,m_srs_srspar,m_srs_direct,m_srs_activea);
-     }
-   }
-   if (gStChain->Debug()) m_DataSet->ls("*");
+  if( scs_spt){
+
+    scs_spt_st  *spc   = scs_spt->GetTable();
+    for (Int_t i = 0; i < scs_spt->GetNRows(); i++,spc++){
+      m_x_vs_y->Fill(spc->x[0],spc->x[1]);
+    }
+  }
+  
+  
+  if (gStChain->Debug()) m_DataSet->ls("*");
   //  PrintInfo();
- return kStOK;
+  return kStOK;
 }
 //_____________________________________________________________________________
 void St_srs_Maker::PrintInfo(){
   printf("**************************************************************\n");
-  printf("* $Id: St_srs_Maker.cxx,v 1.13 1999/01/02 19:08:20 fisyak Exp $\n");
+  printf("* $Id: St_srs_Maker.cxx,v 1.14 1999/02/26 18:05:06 caines Exp $\n");
   //  printf("* %s    *\n",m_VersionCVS);
   printf("**************************************************************\n");
   if (gStChain->Debug()) StMaker::PrintInfo();
