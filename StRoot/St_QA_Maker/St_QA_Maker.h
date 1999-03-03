81d6c64ba1c7b8052fd1e87dd813c929d1edb84b
kathy <>
1999-03-03 23:34:30 +0000

    fixes to histograms

--- StRoot/St_QA_Maker/St_QA_Maker.h
+++ StRoot/St_QA_Maker/St_QA_Maker.h
@@ -1,57 +1,60 @@
-// $Id: St_QA_Maker.h,v 1.6 1999/02/26 18:42:34 kathy Exp $
-// $Log: St_QA_Maker.h,v $
-// Revision 1.6  1999/02/26 18:42:34  kathy
-// added vertex histograms
+//! $Id: St_QA_Maker.h,v 1.7 1999/03/03 23:34:30 kathy Exp $
+//! $Log: St_QA_Maker.h,v $
+//! Revision 1.7  1999/03/03 23:34:30  kathy
+//! fixes to histograms
+//!
+//! Revision 1.6  1999/02/26 18:42:34  kathy
+//! added vertex histograms
 //
-// Revision 1.5  1999/02/25 19:25:39  kathy
-// fix up histograms
+//! Revision 1.5  1999/02/25 19:25:39  kathy
+//! fix up histograms
 //
-// Revision 1.4  1999/02/24 21:15:04  kathy
-// fixed histograms and added a few new ones
+//! Revision 1.4  1999/02/24 21:15:04  kathy
+//! fixed histograms and added a few new ones
 //
-// Revision 1.3  1999/02/23 22:22:22  kathy
-// changes to histograms: titles changed so they'll be in order and redundant ones removed
+//! Revision 1.3  1999/02/23 22:22:22  kathy
+//! changes to histograms: titles changed so they'll be in order and redundant ones removed
 //
-// Revision 1.2  1999/02/22 21:27:18  kathy
-// moved hist from St_glb_Maker to St_QA_Maker and had to rename some etc
+//! Revision 1.2  1999/02/22 21:27:18  kathy
+//! moved hist from St_glb_Maker to St_QA_Maker and had to rename some etc
 //
-// Revision 1.1  1999/02/08 19:28:33  didenko
-// fixed directory level
+//! Revision 1.1  1999/02/08 19:28:33  didenko
+//! fixed directory level
 //
-// Revision 1.3  1999/01/22 22:21:07  didenko
-// header file for  QA maker
+//! Revision 1.3  1999/01/22 22:21:07  didenko
+//! header file for  QA maker
 //
-// Revision 1.2  1998/12/21 19:43:19  fisyak
-// Move ROOT includes to non system
+//! Revision 1.2  1998/12/21 19:43:19  fisyak
+//! Move ROOT includes to non system
 //
-// Revision 1.1  1998/11/01 16:42:26  fisyak
-// dst analysis
+//! Revision 1.1  1998/11/01 16:42:26  fisyak
+//! dst analysis
 //
-// Revision 1.4  1998/10/31 00:26:13  fisyak
-// Makers take care about branches
+//! Revision 1.4  1998/10/31 00:26:13  fisyak
+//! Makers take care about branches
 //
-// Revision 1.3  1998/10/06 18:00:34  perev
-// cleanup
+//! Revision 1.3  1998/10/06 18:00:34  perev
+//! cleanup
 //
-// Revision 1.2  1998/09/08 22:43:11  fisyak
-// Modify St_QA_Maker to account new calling sequence
+//! Revision 1.2  1998/09/08 22:43:11  fisyak
+//! Modify St_QA_Maker to account new calling sequence
 //
-// Revision 1.1  1998/08/18 14:06:07  fisyak
-// Add to bfc dst
+//! Revision 1.1  1998/08/18 14:06:07  fisyak
+//! Add to bfc dst
 //
-// Revision 1.3  1998/08/10 02:32:07  fisyak
-// Clean up
+//! Revision 1.3  1998/08/10 02:32:07  fisyak
+//! Clean up
 //
-// Revision 1.2  1998/07/20 15:08:15  fisyak
-// Add tcl and tpt
+//! Revision 1.2  1998/07/20 15:08:15  fisyak
+//! Add tcl and tpt
 //
 #ifndef STAR_St_QA_Maker
 #define STAR_St_QA_Maker
 
 //////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// St_QA_Maker virtual base class for Maker                            //
-//                                                                      //
+//!                                                                      //
+//! St_QA_Maker virtual base class for Maker                            //
+//!                                                                      //
 //////////////////////////////////////////////////////////////////////////
 #ifndef StMaker_H
 #include "StMaker.h"
@@ -66,8 +69,8 @@
 class St_QA_Maker : public StMaker {
  private:
    Bool_t drawinit;
-// static Char_t m_VersionCVS = "$Id: St_QA_Maker.h,v 1.6 1999/02/26 18:42:34 kathy Exp $";
-   // Histograms booking constants
+//! static Char_t m_VersionCVS = "$Id: St_QA_Maker.h,v 1.7 1999/03/03 23:34:30 kathy Exp $";
+   //! Histograms booking constants
    static const Int_t nxpT;
    static const Int_t nyeta;
    static const Float_t xminpT;
@@ -131,40 +134,45 @@ class St_QA_Maker : public StMaker {
  protected:
 
 // for method MakeEvSum - from table event_summary
-   TH2F     *m_trk_tot_gd;    // number of good global tracks versus total
-   TH1F     *m_glb_trk_tot;   // # tracks total from globtrk
-   TH1F     *m_glb_trk_gd;    // # tracks good from globtrk
-   TH1F     *m_glb_trk_plus;  // number of (+) global tracks    
-   TH1F     *m_glb_trk_minus; // number of (-) global tracks
-   TH2F     *m_trk_pls_mns;   // number of (+) global tracks versus (-) 
-   TH1F     *m_vert_total;    // total number of vertices
-   TH1F     *m_vert_V0;       // number of V0 vertices
-   TH1F     *m_vert_La;       // number of La vertices 
-   TH1F     *m_vert_Ala;      // number of Ala vertices
-   TH1F     *m_vert_K0;       // number of K0 vertices
-   TH1F     *m_mean_pt;       // mean pt value
-   TH1F     *m_mean_eta;      // mean eta value 
-   TH1F     *m_prim_vrtx0;    // primary vrtx x position
-   TH1F     *m_prim_vrtx1;    // primary vrtx y position
-   TH1F     *m_prim_vrtx2;    // primary vrtx z position
-   TH1F     *m_vrtx_chisq;    // primary vrtx chisq
+   TH1F     *m_trk_tot_gd;    //! number of good global tracks divided by total
+   TH1F     *m_glb_trk_tot;   //! # tracks total from globtrk
+   TH1F     *m_glb_trk_gd;    //! # tracks good from globtrk
+   TH1F     *m_glb_trk_plusminus; //! # trks pos/neg. 
+
+   TH1F     *m_vert_total;    //! total number of vertices
+   TH1F     *m_vert_V0;       //! number of V0 vertices
+/*    TH1F     *m_vert_La;       //! number of La vertices  */
+/*    TH1F     *m_vert_Ala;      //! number of Ala vertices */
+/*    TH1F     *m_vert_K0;       //! number of K0 vertices */
+   TH1F     *m_mean_pt;       //! mean pt value
+   TH1F     *m_mean_eta;      //! mean eta value 
+   TH1F     *m_prim_vrtx0;    //! primary vrtx x position
+   TH1F     *m_prim_vrtx1;    //! primary vrtx y position
+   TH1F     *m_prim_vrtx2;    //! primary vrtx z position
+   TH1F     *m_vrtx_chisq;    //! primary vrtx chisq
 
 // for method MakeGlob - from table globtrk
-   TH1F     *m_pT;            // pT  recostructed
-   TH1F     *m_eta;           // eta recostructed
-   TH2F     *m_pT_eta_rec;    // pT versus eta Spectra for reconstructed
-   TH1F     *m_point;         // number of points on the track
-   TH1F     *m_fit_point;     // number of track points used for fitting
-   TH1F     *m_length;        // length of track
-   TH1F     *m_chisq0;        // chi square [0]
-   TH1F     *m_chisq1;        // chi square [1]
-   TH1F     *m_psi;           // psi reconstructed
-   TH1F     *m_det_id;        // detector id of track 
+   TH1F     *m_pT;            //! pT  recostructed
+   TH1F     *m_eta;           //! eta recostructed
+   TH2F     *m_pT_eta_rec;    //! pT versus eta Spectra for reconstructed
+   TH2F     *m_mom_trklength; //! mom vs. trk length
+   TH1F     *m_point;         //! number of points on the track
+   TH1F     *m_fit_point;     //! number of track points used for fitting
+   TH1F     *m_length;        //! length of track
+   TH1F     *m_chisq0;        //! chi square [0]
+   TH1F     *m_chisq1;        //! chi square [1]
+   TH1F     *m_psi;           //! psi reconstructed
+   TH1F     *m_det_id;        //! detector id of track
+   TH2F     *m_npoint_length; //! num points vs length
+   TH2F     *m_fpoint_length; //! num fit points vs length
+   TH2F     *m_chisq0_mom;    //! chisq0 vs momentum
+   TH2F     *m_chisq1_mom;    //! chisq1 vs momentum
+
 
 // for method MakeDE - from table dst_dedx
-   TH1F     *m_ndedx;         // number of point to find dE/dx
-   TH1F     *m_dedx0;         // dE/dx [0]
-   TH1F     *m_dedx1;         // dE/dx [1] 
+   TH1F     *m_ndedx;         //! number of point to find dE/dx
+   TH1F     *m_dedx0;         //! dE/dx [0]
+   TH1F     *m_dedx1;         //! dE/dx [1] 
 
 // for method MakeHistPrim - from table primtrk
    TH1F     *m_prim_pT;          //! pT  recostructed
@@ -175,8 +183,13 @@ class St_QA_Maker : public StMaker {
    TH1F     *m_prim_chi2yd;      //! y chisq/degf
    TH1F     *m_prim_point;       //! # points on track
    TH1F     *m_prim_fit_point;   //! # fitted points
-   TH1F     *m_prim_psi;         //! psi angle
-   TH1F     *m_prim_det_id;      //! detector id
+   TH1F     *m_prim_psi;         //! psi angle_ 
+   TH1F     *m_prim_det_id;        //!
+   TH2F     *m_prim_mom_trklength; //!
+   TH2F     *m_prim_npoint_length; //!
+   TH2F     *m_prim_fpoint_length; //!
+   TH2F     *m_prim_chisq0_mom;    //!
+   TH2F     *m_prim_chisq1_mom;    //!
 
 
 // for method MakeHistGen - from table particle
@@ -194,10 +207,8 @@ class St_QA_Maker : public StMaker {
 
 
 // for method MakeHistVertex - from table dst_vertex
-   TH1F     *m_v_flag;  //! bitmask of e.g. quality information
    TH1F     *m_v_detid; //! detector id where vertex was found 
    TH1F     *m_v_vtxid; //! vertex type
-   TH1F     *m_v_ndau;  //! number of charged daughter tracks
    TH1F     *m_v_x;     //! vertex coordinates in
    TH1F     *m_v_y;     //!  STAR reference 
    TH1F     *m_v_z;     //!   system
