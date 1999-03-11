404fd822ad3a9ed3e38591cde0760f0780dddbbb
Kathleen R. Turner <kathy>
1999-03-11 17:04:42 +0000

    changed directory to find default input files

--- StRoot/macros/plotTable.C
+++ StRoot/macros/plotTable.C
@@ -4,6 +4,6 @@ gSystem->Load("St_Tables");
 gSystem->Load("xdf2root");
 St_ev0_aux temp;
 St_TableNtuple myNtuple(temp);
-myNtuple.AddXDFFile("/disk00000/star/test_data/year2a_psc079_01_46evts_dst.xdf","dst","ev0out");
+myNtuple.AddXDFFile("/disk00000/star/mdc1_test_data/mdc1_year2a_psc079_01_46evts_dst.xdf","dst","ev0out");
 myNtuple.Draw("theta");
 }
