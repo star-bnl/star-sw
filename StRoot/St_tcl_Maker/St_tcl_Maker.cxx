7c1796f9efcbf58e7436ed17a702ba0169512a90
Herbert John Ward <ward@physics.utexas.edu>
1999-03-11 20:40:20 +0000

    Add code for cluster morphology.

--- StRoot/St_tcl_Maker/St_tcl_Maker.cxx
+++ StRoot/St_tcl_Maker/St_tcl_Maker.cxx
@@ -1,5 +1,8 @@
-// $Id: St_tcl_Maker.cxx,v 1.27 1999/03/04 14:05:59 sakrejda Exp $
+// $Id: St_tcl_Maker.cxx,v 1.28 1999/03/11 20:40:18 ward Exp $
 // $Log: St_tcl_Maker.cxx,v $
+// Revision 1.28  1999/03/11 20:40:18  ward
+// Add code for cluster morphology.
+//
 // Revision 1.27  1999/03/04 14:05:59  sakrejda
 // table of sequences increased
 //
@@ -185,6 +188,210 @@ Int_t St_tcl_Maker::Init(){
   
   return StMaker::Init();
 }
+Int_t St_tcl_Maker::FillOneRowOfMorphTable(int iClusterTbl,
+        St_tcc_morphology *morph,
+        int padrow,int sector,int nseq,int npix,int npad,
+        unsigned int totChargeEq1,
+        int maxCharge,float averageCharge,float meanPadPos,float meanTimePos,
+        float padSigma1Eq5,float timeSigma1Eq6,
+        float padTimeSigma1Eq7,float padSigma2Eq12,
+        float timeSigma2Eq13,float padTimeSigma2Eq14,
+        float ecc1Eq15,float ecc2Eq16,
+        float linEcc1Eq8,float linEcc2Eq9) {
+  tcc_morphology_st singlerow;
+  singlerow.clusterId=iClusterTbl+1;
+  singlerow.rowNumber=padrow;
+  singlerow.sectorNumber=sector;
+  singlerow.numberOfSequences=nseq;
+  singlerow.numberOfPixels=npix;
+  singlerow.numberOfPads=npad;
+  singlerow.numberOfHits=0;  // will be filled later
+  singlerow.totalCharge=totChargeEq1;
+  singlerow.maxCharge=maxCharge;
+  singlerow.averageCharge=averageCharge;
+  singlerow.meanPadPosition=meanPadPos;
+  singlerow.meanTimePosition=meanTimePos;
+  singlerow.padSigma1=padSigma1Eq5;
+  singlerow.timeSigma1=timeSigma1Eq6;
+  singlerow.padTimeSigma1=padTimeSigma1Eq7;
+  singlerow.padSigma2=padSigma2Eq12;
+  singlerow.timeSigma2=timeSigma2Eq13;
+  singlerow.padTimeSigma2=padTimeSigma2Eq14;
+  singlerow.ecc1=ecc1Eq15;
+  singlerow.ecc2=ecc2Eq16;
+  singlerow.linEcc1=linEcc1Eq8;
+  singlerow.linEcc2=linEcc2Eq9;
+  singlerow.meanX=0;
+  singlerow.meanY=0;
+  singlerow.meanZ=0;
+  singlerow.clusterFlag=0;
+  morph->AddAt(&singlerow,iClusterTbl);
+  return 0; // no error
+}
+// #define TCC_PAD 182
+// #define TCC_BIN 512
+#define CENTIMETERS_PER_TIME_BIN 0.4082
+#define LINEARIZATION 1.2
+Int_t St_tcl_Maker::CalculateQuadrupoleMoms(
+        int padrow,int npad,int *pads,unsigned short charge[TCC_PAD][TCC_BIN],
+        unsigned int *totChargeEq1,float *meanPadEq3,float *meanTimeEq4,
+        float *padSigma1Eq5,float *timeSigma1Eq6,float *padTimeSigma1Eq7,
+        float *padSigma2Eq12,float *timeSigma2Eq13,float *padTimeSigma2Eq14,
+        float *ecc1Eq15,float *ecc2Eq16,
+        float *linEcc1Eq8,float *linEcc2Eq9) { // Eq #s refer to SN0357
+  int ii,jj;
+  float alpha,beta,qq,centimetersPerPad,tanLinearizationFactor;
+
+  tanLinearizationFactor=tan(LINEARIZATION);
+  if(padrow<=13) centimetersPerPad=0.335; else centimetersPerPad=0.67;
+
+  *totChargeEq1=0; *meanPadEq3=0; *meanTimeEq4=0;
+  for(ii=npad-1;ii>=0;ii--) {
+    for(jj=TCC_BIN-1;jj>=0;jj--) {
+      *totChargeEq1+=charge[ii][jj];
+      *meanPadEq3+=centimetersPerPad*pads[ii]*charge[ii][jj];
+      *meanTimeEq4+=(jj+1)*CENTIMETERS_PER_TIME_BIN*charge[ii][jj];
+    }
+  }
+  if((*totChargeEq1)>0) *meanPadEq3/=(*totChargeEq1);  else *meanPadEq3=0;
+  if((*totChargeEq1)>0) *meanTimeEq4/=(*totChargeEq1); else *meanTimeEq4=0;
+
+  *padSigma1Eq5=0; *timeSigma1Eq6=0; *padTimeSigma1Eq7=0;
+  *padSigma2Eq12=0; *timeSigma2Eq13=0; *padTimeSigma2Eq14=0;
+  for(ii=npad-1;ii>=0;ii--) {
+    for(jj=TCC_BIN-1;jj>=0;jj--) {
+      alpha=centimetersPerPad*pads[ii]-(*meanPadEq3);
+      beta=(jj+1)*CENTIMETERS_PER_TIME_BIN-(*meanTimeEq4);
+      qq=charge[ii][jj];
+      *padSigma1Eq5      += alpha        * alpha        * qq ;
+      *timeSigma1Eq6      += beta         * beta         * qq ;
+      *padTimeSigma1Eq7  += alpha        * beta         * qq ;
+      *padSigma2Eq12     += (alpha+beta) * (alpha+beta) * qq ;
+      *timeSigma2Eq13     += (alpha-beta) * (alpha-beta) * qq ;
+      *padTimeSigma2Eq14 += (alpha+beta) * (alpha-beta) * qq ;
+    }
+  }
+  if((*totChargeEq1)>0) *padSigma1Eq5/=(*totChargeEq1);  else *padSigma1Eq5=0;
+  if((*totChargeEq1)>0) *timeSigma1Eq6/=(*totChargeEq1);  else *timeSigma1Eq6=0;
+  if((*totChargeEq1)>0) *padTimeSigma1Eq7/=(*totChargeEq1); else *padTimeSigma1Eq7=0;
+  if((*totChargeEq1)>0) *padSigma2Eq12/=(*totChargeEq1);  else *padSigma2Eq12=0;
+  if((*totChargeEq1)>0) *timeSigma2Eq13/=(*totChargeEq1);  else *timeSigma2Eq13=0;
+  if((*totChargeEq1)>0) *padTimeSigma2Eq14/=(*totChargeEq1); else *padTimeSigma2Eq14=0;
+  *padSigma2Eq12/=2;      // This represents the sqrt(2) in Eqs 10 and 11.
+  *timeSigma2Eq13/=2;      // This represents the sqrt(2) in Eqs 10 and 11.
+  *padTimeSigma2Eq14/=2;  // This represents the sqrt(2) in Eqs 10 and 11.
+
+  *padSigma1Eq5=sqrt(*padSigma1Eq5);
+  *timeSigma1Eq6=sqrt(*timeSigma1Eq6);
+  // sometimes this is sqrt(-): *padTimeSigma1Eq7=sqrt(*padTimeSigma1Eq7);
+  *padSigma2Eq12=sqrt(*padSigma2Eq12);
+  *timeSigma2Eq13=sqrt(*timeSigma2Eq13);
+  // sometimes this is sqrt(-): *padTimeSigma2Eq14=sqrt(*padTimeSigma2Eq14);
+
+  if(*padSigma1Eq5!=0 && *timeSigma1Eq6 !=0) {
+    *ecc1Eq15=(*padTimeSigma1Eq7)/((*padSigma1Eq5)*(*timeSigma1Eq6));
+  } else *ecc1Eq15=0;
+  if(*padSigma2Eq12!=0 && *timeSigma2Eq13!=0) {
+    *ecc2Eq16=(*padTimeSigma2Eq14)/((*padSigma2Eq12)*(*timeSigma2Eq13));
+  } else *ecc2Eq16=0;
+  *linEcc1Eq8=tan(LINEARIZATION*(*ecc1Eq15))/tanLinearizationFactor;
+  *linEcc2Eq9=tan(LINEARIZATION*(*ecc2Eq16))/tanLinearizationFactor;
+
+  return 0; // OK.
+}
+Int_t St_tcl_Maker::cluster_morphology( Int_t sectorNumber,
+      St_type_shortdata *pixel_data_in,
+      St_type_shortdata *pixel_data_out,
+      St_tcl_tpcluster *tpcluster,
+      St_tcl_tp_seq *tpseq,St_tcc_morphology *morph) {
+  type_shortdata_st *pixTbl;
+  unsigned short charge[TCC_PAD][TCC_BIN];
+  float meanPadEq3,meanTimeEq4,padSigma1Eq5,timeSigma1Eq6,padTimeSigma1Eq7,
+      padSigma2Eq12,timeSigma2Eq13;
+  float padTimeSigma2Eq14,ecc1Eq15,ecc2Eq16,linEcc1Eq8,linEcc2Eq9;
+  tcl_tp_seq_st *seqTbl = (tcl_tp_seq_st*) tpseq->GetTable();
+  int pixBeg,pixEnd,nseq,padrow,sector,seqCnt;
+  int iPixTbl,nCluster,iClusterTbl,iSeqTbl;
+  int maxCharge,whichPad,whichTimeBin,npad,pads[TCC_PAD];
+  int ipad,numberOfPixels,ii,jj;
+  unsigned int totChargeEq1;
+  static int lastRowPrevTime=-1;
+  nCluster=tpcluster->GetNRows();
+  tcl_tpcluster_st *clusterTbl = (tcl_tpcluster_st*) tpcluster->GetTable();
+  for(iClusterTbl=lastRowPrevTime+1;iClusterTbl<nCluster;iClusterTbl++) {
+    if(iClusterTbl%473==0) {
+      printf("St_tcl_Maker::cluster_morphology Sector %2d, %3d percent done\n",
+             sectorNumber,(100*(iClusterTbl-lastRowPrevTime))/(nCluster-lastRowPrevTime));
+    }
+    sector=clusterTbl[iClusterTbl].tpc_row/100;
+    if(sector!=sectorNumber) { printf("cluster table may be corrupted.\n"); return 1; }
+    padrow=clusterTbl[iClusterTbl].tpc_row%100;
+    if(padrow<1||padrow>45) { printf("padrow (%d) out of range.\n",padrow); return 2; }
+    iSeqTbl=clusterTbl[iClusterTbl].jseq-1;
+    nseq=clusterTbl[iClusterTbl].nseq;
+    if(padrow<=13) pixTbl= pixel_data_in->GetTable();
+    else           pixTbl=pixel_data_out->GetTable();
+    npad=0; // count number of pads without assuming one sequence per pad
+    memset((void*)charge,(char)0,(size_t)(sizeof(unsigned short)*TCC_PAD*TCC_BIN));
+    maxCharge=0; numberOfPixels=0;
+    for(seqCnt=0;seqCnt<nseq;seqCnt++) { // seqCnt is not table index, iSeqTbl is.
+
+      // Calculate whichPad, index into pads[]
+      for(ipad=npad-1;ipad>=0;ipad--) { if(pads[ipad]==seqTbl[iSeqTbl].secpad) { break; } }
+      if(ipad>=0) whichPad=ipad;
+      else { // create a new entry in pads[]
+        if(npad>=TCC_PAD) return 55;
+        whichPad=npad; pads[npad++]=seqTbl[iSeqTbl].secpad;
+      }
+
+      pixBeg=seqTbl[iSeqTbl].jpix-1;
+      pixEnd=pixBeg+seqTbl[iSeqTbl].tdc_hi-seqTbl[iSeqTbl].tdc_low;
+
+      if(seqTbl[iSeqTbl].tpc_row!=clusterTbl[iClusterTbl].tpc_row) return 3;
+
+      for(iPixTbl=pixBeg;iPixTbl<=pixEnd;iPixTbl++) {
+        whichTimeBin=seqTbl[iSeqTbl].tdc_low+iPixTbl-pixBeg;
+        if(whichTimeBin<0||whichTimeBin>=TCC_BIN) return 81;
+        charge[whichPad][whichTimeBin]=pixTbl[iPixTbl].data;
+        if(maxCharge<pixTbl[iPixTbl].data) maxCharge=pixTbl[iPixTbl].data;
+        numberOfPixels++;
+      }
+      iSeqTbl=seqTbl[iSeqTbl].next-1;
+    }
+    if(iSeqTbl!=-1) return 69;
+    if(CalculateQuadrupoleMoms(
+             padrow,npad,pads,charge,
+             &totChargeEq1,&meanPadEq3,&meanTimeEq4,
+             &padSigma1Eq5,&timeSigma1Eq6,&padTimeSigma1Eq7, &padSigma2Eq12,
+             &timeSigma2Eq13,&padTimeSigma2Eq14, &ecc1Eq15,&ecc2Eq16,
+             &linEcc1Eq8,&linEcc2Eq9)) return 111;
+    if(FillOneRowOfMorphTable(iClusterTbl,morph,padrow,sector,nseq,numberOfPixels,
+       npad,totChargeEq1,maxCharge,(float)((1.0*totChargeEq1)/numberOfPixels),
+       meanPadEq3,meanTimeEq4,
+       padSigma1Eq5,timeSigma1Eq6,padTimeSigma1Eq7,padSigma2Eq12,
+             timeSigma2Eq13,padTimeSigma2Eq14,ecc1Eq15,ecc2Eq16,
+             linEcc1Eq8,linEcc2Eq9
+    )) return 102;
+  }
+  lastRowPrevTime=nCluster-1;
+  return 0; // OK.
+}
+/*=====================================================================================
+                   NOTES FOR FUNCTION cluster_morphology()
+---------------+----------------+--------------+---------------------+-----------------
+type           | wrapper name   | tableName    | range               | indexStep,
+               |                |              |                     | index name
+---------------+----------------+--------------+---------------------+-----------------
+tcl_tpcluster  | tpcluster      | clusterTbl   | 1 to nok            | ++, iClusterTbl
+---------------+----------------+--------------+---------------------+-----------------
+tcl_tp_seq     | tpseq          | seqTable     | clusterTbl->jseq,   | tpseq[].next,
+               |                |              | clusterTbl->nseq    | iSeqTbl
+---------------+----------------+--------------+---------------------+-----------------
+type_shortdata | pixel_data_out | pixTbl       | tpseq->jpix,        | ++,
+               | or             |              | tpseq->tdc_hi-      | iPixTbl
+               | pixel_data_in  |              | tpseq->tdc_low      |
+---------------+----------------+--------------+---------------------+-----------------
+=====================================================================================*/
 //_____________________________________________________________________________
 Int_t St_tcl_Maker::Make(){
   //  PrintInfo();
@@ -195,6 +402,7 @@ Int_t St_tcl_Maker::Make(){
     St_tcl_tphit_aux *tphitau   = new St_tcl_tphit_aux("tphitau",1);   local.Add(tphitau);
     //    St_tcl_tpc_index *index     = new St_tcl_tpc_index("index",max_hit);     local.Add(index);
     St_tcl_tpcluster *tpcluster = new St_tcl_tpcluster("tpcluster",max_hit); local.Add(tpcluster);
+    St_tcc_morphology *morph = new St_tcc_morphology("morph",max_hit); local.Add(morph);
     St_tcl_tp_seq    *tpseq     = new St_tcl_tp_seq("tpseq",5*max_hit);      local.Add(tpseq);
     St_DataSet       *sector;
     St_DataSet       *raw_data_tpc = gStChain->DataSet("tpc_raw");
@@ -222,15 +430,23 @@ Int_t St_tcl_Maker::Make(){
 	  St_type_shortdata  *pixel_data_in  = (St_type_shortdata *) sect("pixel_data_in");
 	  St_type_shortdata  *pixel_data_out = (St_type_shortdata *) sect("pixel_data_out");
 	  // tcl
+          printf("Starting %20s for sector %2d.\n","tcl",indx);
 	  Int_t tcl_res = tcl(m_tpg_pad_plane, m_tcl_sector_index, raw_sec_m,
                               raw_row_in, raw_pad_in, raw_seq_in, pixel_data_in,
                               raw_row_out,raw_pad_out,raw_seq_out,pixel_data_out,
                               tpcluster,tpseq);
+
+          printf("Starting %20s for sector %2d.\n","cluster_morphology",indx);
+          Int_t tcc_res = cluster_morphology( indx, pixel_data_in, pixel_data_out,
+              tpcluster, tpseq, morph);
+          if(tcc_res) { printf("ERROR %d, tcl maker\n",tcc_res); return kStErr; }
+
 	  sector_tot++;
 	  // tph
 	  Int_t k = indx;
 	  if (sector_tot == 1) k = -k;
 	  tcl_sector_index->CurrentSector = k;
+          printf("Starting %20s for sector %2d.\n","tph",indx);
 	  Int_t tph_res =  tph(m_tcl_sector_index,m_tclpar,m_tsspar,
 			       m_tpg_pad_plane,
 			       pixel_data_in,pixel_data_out,
@@ -282,7 +498,7 @@ Int_t St_tcl_Maker::Make(){
 //_____________________________________________________________________________
 void St_tcl_Maker::PrintInfo(){
   printf("**************************************************************\n");
-  printf("* $Id: St_tcl_Maker.cxx,v 1.27 1999/03/04 14:05:59 sakrejda Exp $\n");
+  printf("* $Id: St_tcl_Maker.cxx,v 1.28 1999/03/11 20:40:18 ward Exp $\n");
   //  printf("* %s    *\n",m_VersionCVS);
   printf("**************************************************************\n");
   if (gStChain->Debug()) StMaker::PrintInfo();
--- StRoot/St_tcl_Maker/St_tcl_Maker.h
+++ StRoot/St_tcl_Maker/St_tcl_Maker.h
@@ -1,5 +1,8 @@
-// $Id: St_tcl_Maker.h,v 1.7 1999/03/01 18:53:33 sakrejda Exp $
+// $Id: St_tcl_Maker.h,v 1.8 1999/03/11 20:40:20 ward Exp $
 // $Log: St_tcl_Maker.h,v $
+// Revision 1.8  1999/03/11 20:40:20  ward
+// Add code for cluster morphology.
+//
 // Revision 1.7  1999/03/01 18:53:33  sakrejda
 // hit eveluation switchable
 //
@@ -24,6 +27,9 @@
 #ifndef STAR_St_tcl_Maker
 #define STAR_St_tcl_Maker
 
+#define TCC_PAD 182
+#define TCC_BIN 512
+
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
 // St_tcl_Maker virtual base class for Maker                            //
@@ -47,6 +53,11 @@ class St_tfs_fsctrl;
 
 class TH1F;
 
+#include "St_type_shortdata_Table.h"
+#include "St_tcl_tpcluster_Table.h"
+#include "St_tcl_tp_seq_Table.h"
+#include "St_tcc_morphology_Table.h"
+
 class St_tcl_Maker : public StMaker {
  private:
                void                  MakeHistograms();// Histograms for tpc clustering
@@ -65,6 +76,26 @@ class St_tcl_Maker : public StMaker {
 	                                        // correlations for tpc evaluations
                St_tfs_fspar          *m_tfs_fspar;   //! TFS parameter table 
                St_tfs_fsctrl         *m_tfs_fsctrl;  //! TFS control switches
+    Int_t cluster_morphology( Int_t sectorNumber,
+          St_type_shortdata *pixel_data_in, St_type_shortdata *pixel_data_out,
+          St_tcl_tpcluster *tpcluster, St_tcl_tp_seq *tpseq,St_tcc_morphology *morph);
+Int_t FillOneRowOfMorphTable(int iClusterTbl,
+        St_tcc_morphology *morph,
+        int padrow,int sector,int nseq,int npix,int npad,
+        unsigned int totalChargeEq1,
+        int maxCharge,float averageCharge,float meanPadPos,float meanTimePos,
+        float padSigma1Eq5,float bucSigma1Eq6,
+        float padTimeSigma1Eq7,float padSigma2Eq12,
+        float bucSigma2Eq13,float padTimeSigma2Eq14,
+        float ecc1Eq15,float ecc2Eq16,
+        float linEcc1Eq8,float linEcc2Eq9);
+    Int_t CalculateQuadrupoleMoms(
+        int padrow,int npad,int *pads,unsigned short charge[TCC_PAD][TCC_BIN],
+        unsigned int *totalChargeEq1,float *meanPadEq3,float *meanTimeEq4,
+        float *padSigma1Eq5,float *timeSigma1Eq6,float *padTimeSigma1Eq7,
+        float *padSigma2Eq12,float *timeSigma2Eq13,float *padTimeSigma2Eq14,
+        float *ecc1Eq15,float *ecc2Eq16,
+        float *linEcc1Eq8,float *linEcc2Eq9);
 
 protected:
 
