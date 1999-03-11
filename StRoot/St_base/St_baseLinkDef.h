2efad9fef50b6340f0c8e2c16c70184ba354f330
Victor Perevoztchikov <perev@bnl.gov>
1999-03-11 00:43:29 +0000

    new linkDef file

---
+++ StRoot/St_base/St_baseLinkDef.h
@@ -0,0 +1,9 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class StTree;
+#pragma link C++ class StBranch;
+#pragma link C++ class StIOEvent-;
+#pragma link C++ class StIO-;
+#endif
