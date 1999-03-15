891f191d4384a0390ee9e4827553c84a7f6f007f
fisyak <>
1999-03-15 19:52:18 +0000

    New Makers scheme

--- StRoot/macros/bfcz.C
+++ StRoot/macros/bfcz.C
@@ -1,4 +1,14 @@
-// $Id: bfcz.C,v 1.8 1999/02/24 19:45:46 fisyak Exp $
+// $Id: bfcz.C,v 1.9 1999/03/15 19:52:18 fisyak Exp $
+//#define TRS
+//#define TSS
+#define FTPC
+//#define FSS
+#define SVT
+//#define EMC
+#define CTF
+//$define StMagF
+#define GEANT
+//#define XDF
 TBrowser *b = 0;
 class StChain;
 StChain  *chain=0;
@@ -12,23 +22,42 @@ void Load(){
     gSystem->Load("St_Tables");
     gSystem->Load("libmsg");
     gSystem->Load("libtls");
-    gSystem->Load("St_params_Maker");
-    gSystem->Load("St_calib_Maker");
+    gSystem->Load("St_db_Maker");
+#ifndef StMagF
     gSystem->Load("geometry");
-    gSystem->Load("St_g2r");
+#else
+    gSystem->Load("StMagF");
+#endif
+#ifdef GEANT
+    gSystem->Load("St_g2r"); 
     gSystem->Load("St_geant_Maker");
-    gSystem->Load("St_TLA_Maker");
     gSystem->Load("St_tpc");
-    gSystem->Load("St_ftpc");
+#endif
+#ifdef TRS
+    gSystem->Load("StarClassLibrary");
+    gSystem->Load("StTrsMaker"); 
+    gSystem->Load("St_tpcdaq_Maker");
+#else
+#ifdef  TSS
     gSystem->Load("St_tss_Maker");
-    gSystem->Load("St_fss_Maker");
+#endif
+#endif
     gSystem->Load("St_tcl_Maker");
     gSystem->Load("St_tpt_Maker");
+#ifdef FTPC
+    gSystem->Load("St_ftpc");
+#ifdef  FSS
+    gSystem->Load("St_fss_Maker");
+#endif
     gSystem->Load("St_fcl_Maker");
     gSystem->Load("St_fpt_Maker");
+#endif
+#ifdef EMC
     gSystem->Load("St_emc");
     gSystem->Load("St_ems_Maker");
     gSystem->Load("St_emc_Maker");
+#endif
+#ifdef CTF
     gSystem->Load("St_ctf");
     gSystem->Load("St_ctf_Maker");
     gSystem->Load("St_mwc");
@@ -37,111 +66,166 @@ void Load(){
     gSystem->Load("St_trg_Maker");
     gSystem->Load("St_l3");
     gSystem->Load("St_l3t_Maker");
-    gSystem->Load("StRchMaker");
+#endif
+    //    gSystem->Load("StRchMaker");
     gSystem->Load("St_svt");
+#ifdef SVT
     gSystem->Load("St_srs_Maker");
     gSystem->Load("St_stk_Maker");
-    gSystem->Load("St_strange");
+#endif
     gSystem->Load("St_global");
     gSystem->Load("St_dst_Maker");
-    gSystem->Load("St_run_summary_Maker");
+    //    gSystem->Load("St_run_summary_Maker");
     gSystem->Load("St_QA_Maker");
-    gSystem->Load("St_io_Maker");
+    gSystem->Load("StTreeMaker");
 }
 
-bfcz(const Int_t Nevents=100,
+void bfcz (const Int_t Nevents=1000000,
      const Char_t *fzfile ="/disk1/star/test/psc0049_08_40evts.fzd",
      TString* FileOut=0)
 {                              
+  //set I/O for crs
+  TString InFile = "$input_file";
+  gSystem->ExpandPathName(InFile);
+  if (!strcmp("$input_file",InFile.Data())) {InFile = fzfile;}
+  Int_t NoEvents = atoi(strrchr(InFile.Data(),'_')+1);
+  if (NoEvents <=0) {NoEvents = Nevents;}  
+  if (NoEvents > Nevents) {NoEvents = Nevents;}  
+  printf("Input file name = %s with No.Events to process = %i \n",InFile.Data(),NoEvents);
   // Dynamically link some shared libs
   if (gClassTable->GetID("StChain") < 0) Load();
   if (!FileOut){
-    FileOut = new TString(gSystem->BaseName(fzfile));
+    FileOut = new TString(gSystem->BaseName(InFile.Data()));
     FileOut->ReplaceAll(".fzd",".root");
     FileOut->ReplaceAll(".fz",".root");
     FileOut->Strip();
   }
-  cout << "File for chain " << FileOut.Data() << endl;
+  printf("File for chain %s\n",FileOut.Data());
   TFile       *root_out  =  new TFile(FileOut->Data(),"RECREATE");
-
+#ifdef XDF
+  St_XDFFile  *xdf_out = 0;
+  TString *XdfFile = new TString(FileOut->Data());
+  XdfFile->ReplaceAll(".root","_dst.xdf");
+  xdf_out = new St_XDFFile(XdfFile->Data(),"wb"); 
+  printf ("Open xdf file  = %s \n +++++++++++++++++++++++++++++++++++++++++++++++\n",XdfFile->Data());
+#endif
   // Create the main chain object
   if (chain) delete chain;
   chain = new StChain("bfc");
+  chain->SetDebug();
+  chain->SetInput("EvtHddr",".make/geant/.const/EvtHddr");    
 
   //  Create the makers to be called by the current chain
-  St_params_Maker     *params = new St_params_Maker("params","params");
-  St_geom_Maker        *geom = new St_geom_Maker("geom","run/geant/Run");
-  geant = new St_geant_Maker("geant","event/geant/Event");
-  geant->SetNwGEANT(40 000 000);
-  //  geant->SetNwPAW(1000000);
-  TString cmd("gfile p ");
-  cmd += fzfile;
-  geant->Do(cmd.Data());
-  //  geant->Do("mode tpce prin 1 digi 2");   // make tpc_hit in local coordinates
-  St_calib_Maker       *calib = new St_calib_Maker("calib","calib"); 
-  //  St_evg_Maker     *evgen = new St_evg_Maker("evgen","event/evgen");
-  St_fss_Maker         *ftpc_raw = new St_fss_Maker("ftpc_raw","event/raw_data/ftpc");
-  St_tss_Maker         *tpc_raw = new St_tss_Maker("tpc_raw","event/raw_data/tpc");
-  // Set parameters
-  //  tpc_raw->adcxyzon();
-  St_ems_Maker         *emc_raw = new St_ems_Maker("emc_raw","event/raw_data/emc");
-  St_emc_Maker         *emc_hits = new St_emc_Maker("emc_hits","event/data/emc/hits");
-  St_tcl_Maker         *tpc_hits = new St_tcl_Maker("tpc_hits","event/data/tpc/hits");
-  St_srs_Maker         *svt_hits = new St_srs_Maker("svt_hits","event/data/svt/hits");
-  St_fcl_Maker         *fcl_hits = new St_fcl_Maker("ftpc_hits","event/data/ftpc/hits");
-  StRchMaker           *rch      = new StRchMaker("rch","event/raw_data/rch");
-  St_tpt_Maker         *tpc_tracks = new St_tpt_Maker("tpc_tracks","event/data/tpc/tracks");
-  St_stk_Maker         *stk_tracks = new St_stk_Maker("svt_tracks","event/data/svt/tracks");
-  St_fpt_Maker         *ftpc_tracks = new St_fpt_Maker("ftpc_tracks","event/data/ftpc/tracks");
-  St_glb_Maker         *global = new St_glb_Maker("global","event/data/global");
-  St_ctf_Maker         *ctf      = new St_ctf_Maker("ctf","event/data/ctf");
-  St_mwc_Maker         *mwc      = new St_mwc_Maker("mwc","event/data/mwc");
-  St_trg_Maker         *trg      = new St_trg_Maker("trg","event/data/trg");
-  St_l3t_Maker         *l3Tracks   = new St_l3t_Maker("l3Tracks","event/data/l3/tracks");
-  St_run_summary_Maker *summary = new St_run_summary_Maker("run_summary","run/dst");
-  St_dst_Maker         *dst     = new St_dst_Maker("dst","dst");
-  //  dst_Maker->Save();
+  const char *mainDB = "$STAR/StDb";
+  St_db_Maker *dbMk = new St_db_Maker("db",mainDB);
+  chain->SetInput("params","db:StDb/params");
+  dbMk->SetDebug();  
+  const char *calibDB = "$STAR_ROOT/calib";
+  St_db_Maker *calibMk = new St_db_Maker("calib",calibDB);
+  chain->SetInput("calib","calib:calib");
+  calibMk->SetDebug();  
+
+  geant = new St_geant_Maker("geant");
+  geant->SetNwGEANT(10 000 000);
+  geant->SetDebug();
+  geant->SetInputFile(InFile.Data());
+  chain->SetInput("geom","geant:geom");
+#ifdef FSS
+//		fss
+  St_fss_Maker *fssMk 	= new St_fss_Maker("ftpc_raw");
+  fssMk->SetDebug();
+#endif  
+#ifdef TRS
+//		trs
+  StTrsMaker   *trs = new StTrsMaker;
+  St_tpcdaq_Maker *tpc_raw = new St_tpcdaq_Maker;
+#else
+#ifdef TSS
+//		tss
+  St_tss_Maker *tssMk 	= new St_tss_Maker("tpc_raw");
+  tssMk->SetDebug();
+#endif
+#endif
+#ifdef EMC
+//		emc
+  St_ems_Maker  *emsMk = new St_ems_Maker("emc_raw" );
+  emsMk->SetDebug();
+  St_emc_Maker  *emcMk = new St_emc_Maker("emc_hits");
+  emcMk->SetDebug();
+#endif
+//		tcl
+  St_tcl_Maker *tclMk = new St_tcl_Maker("tpc_hits");
+  tclMk->SetDebug();  
+
+#ifdef SVT
+//		svt
+  St_srs_Maker *srsMk 	= new St_srs_Maker("svt_hits");
+  srsMk->SetDebug();  
+#endif
+#ifdef FTPC
+//		fcl
+
+  St_fcl_Maker *fclMk 	= new St_fcl_Maker("ftpc_hits");  
+  fclMk->SetDebug();
+#endif
+//		tpt
+  St_tpt_Maker *tptMk 	= new St_tpt_Maker("tpc_tracks");
+  tptMk->SetDebug();
+#ifdef CTF
+//		l3t
+//  St_l3t_Maker  *l3tMk  = new St_l3t_Maker("l3Tracks");
+//  l3tMk->SetDebug();
+#endif
+#ifdef SVT
+//		stk
+  St_stk_Maker *stkMk 	= new St_stk_Maker("svt_tracks");
+  stkMk->SetDebug();
+#endif
+#ifdef FTPC
+//		fpt
+  St_fpt_Maker *fptMk 	= new St_fpt_Maker("ftpc_tracks");
+  fptMk->SetDebug();
+#endif
+GLOBAL:
+//		global
+  St_glb_Maker *glbMk = new St_glb_Maker("global");
+  glbMk->SetDebug();
+//		dst
+  St_dst_Maker *dstMk = new St_dst_Maker("dst");
+  dstMk->SetDebug();
   St_QA_Maker          *qa         = new St_QA_Maker;  
-  St_io_Maker          *out  = new St_io_Maker("Output","all");
+  goto CHAIN;
+//		Tree
+  StTreeMaker *treeMk = new StTreeMaker("tree",InFile.Data());
+  treeMk->SetIOMode("w");
+  treeMk->SetDebug();
+  treeMk->SetInput("dst","dst");
+  //  treeMk->SetInput("global","global");
+//treeMk->SetInput(".default","Others");
+  
+  
+CHAIN:
   // Create HTML docs of all Maker's involved
   //   chain->MakeDoc();
 
   chain->PrintInfo();
 
   // Init the chain and all its makers
-  chain->SetDebug(1);
+  //  chain->SetDebug(1);
   int iInit = chain->Init();
   if (iInit) chain->Fatal(iInit,"on init");
 
-  //  chain->MakeTree("StChainTree","Title");
-  //  chain->SetBranches();
-  // Prepare TCanvas to show some histograms created by makers
-  
-  FileOut->ReplaceAll(".root","_dst.root");
-  cout << "File for dst " << FileOut.Data() << endl;
-  out->Add(dst->GetName(),FileOut.Data());
-  if (root_out) {chain->Write();}
-  gBenchmark->Start("bfc");
   Int_t i=0;
-  for (Int_t i =1; i <= Nevents; i++){
-    if (chain->Make(i)) break;
-    if (i != Nevents) chain->Clear();
-    printf ("===========================================\n");
+  for (Int_t i =1; i <= NoEvents; i++){
+    if (chain->Make(i)==2) break;
+    //    gSystem->Exec("ps ux");
+    if (i != NoEvents) chain->Clear();
     printf ("=========================================== Done with Event no. %d\n",i);
-    printf ("===========================================\n");
   }
 
-  if (Nevents > 1) {
+  if (NoEvents > 1) {
     chain->Finish();
-    if (root_out){
-      root_out->Write();
-      root_out->Close();   
-      delete root_out;
-      gBenchmark->Print("root i/o");
-    }
-    gBenchmark->Print("bfc");
+    printf ("Run completed ");
+    gSystem->Exec("date");
   }
-  else {if (!b)   b = new TBrowser;}
-  cout << "Run completed ";
-  gSystem->Exec("date");
 }
