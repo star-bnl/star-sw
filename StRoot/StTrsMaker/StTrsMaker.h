b6c5c4b7bc3f54ead8bc756d3fa38bc66b098945
lasiuk <>
1999-03-16 02:01:45 +0000

    add Finish() which frees the memory allocated in Init()

--- StRoot/StTrsMaker/StTrsMaker.h
+++ StRoot/StTrsMaker/StTrsMaker.h
@@ -1,6 +1,9 @@
-// $Id: StTrsMaker.h,v 1.5 1999/03/15 02:52:26 perev Exp $
+// $Id: StTrsMaker.h,v 1.6 1999/03/16 02:01:45 lasiuk Exp $
 //
 // $Log: StTrsMaker.h,v $
+// Revision 1.6  1999/03/16 02:01:45  lasiuk
+// add Finish() which frees the memory allocated in Init()
+//
 // Revision 1.5  1999/03/15 02:52:26  perev
 // new Maker schema
 //
@@ -58,15 +61,10 @@ class StTrsUnpacker;
 
 class StTrsMaker : public StMaker {
  private:
-// static Char_t  m_VersionCVS = "$Id: StTrsMaker.h,v 1.5 1999/03/15 02:52:26 perev Exp $";
+// static Char_t  m_VersionCVS = "$Id: StTrsMaker.h,v 1.6 1999/03/16 02:01:45 lasiuk Exp $";
 // Int_t          m_mode;        // mode 1 = primaries;
 // St_stk_stkpar *m_stk_stkpar;  //! pointer to stk parameters
 
-#ifndef ST_NO_TEMPLATE_DEF_ARGS
-   //vector<StTrsChargeSegment>  *mData; //
-#else
-   //vector<StTrsChargeSegment,allocator<StTrsChargeSegment> >  *mData; //
-#endif
 
     // DataBases
     StTpcGeometry               *mGeometryDb; //!
@@ -89,14 +87,22 @@ class StTrsMaker : public StMaker {
     StTrsUnpacker               *mUnPacker;//!
     StTrsRawDataEvent           *mAllTheData;//!
 
+    // Calculation and Initialization Done Internally in the Maker
+    double                       mMiniSegmentLength;
+    int                          mFirstSectorToProcess;
+    int                          mLastSectorToProcess;
+    // should be a boolean
+    int                         mProcessPseudoPadRows;
+    
 protected:
 
 public: 
     StTrsMaker(const char *name="Trs");
-    virtual       ~StTrsMaker();
-    virtual Int_t Init();
-    virtual Int_t  Make();
-    virtual void   PrintInfo();
+    ~StTrsMaker();
+    Int_t  Init();
+    Int_t  Make();
+    Int_t  Finish();
+    void   PrintInfo();
     ClassDef(StTrsMaker, 1)   //StAF chain virtual base class for Makers
 
 private:
