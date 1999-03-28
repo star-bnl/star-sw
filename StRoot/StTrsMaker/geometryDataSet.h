600cddb332dbf2f97ab1fe0116680f6a229170d4
perev <>
1999-03-28 02:59:11 +0000

    Put interfaces to .const area

--- StRoot/StTrsMaker/geometryDataSet.h
+++ StRoot/StTrsMaker/geometryDataSet.h
@@ -56,6 +56,6 @@ class geometryDataSet : public St_DataSet {
     double outerSectorEdge;
     int padsInRow[45];
     /* DANGER HARD CODE  88 96 104 112 118 126 134 142 150 158 166 174 182 98 100 102 104 106 106 108 110 112 112 114 116 118 120 122 122 124 126 128 128 130 132 134 136 138 138 140 142 144 144 144 144*/
-    ClassDef(geometryDataSet,1)
+    ClassDef(geometryDataSet,1) //geometryDataSet class
 };
 #endif
