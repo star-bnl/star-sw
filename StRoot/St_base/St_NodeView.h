971cc6706397cd7f6a042098e6f97140bdaba61c
fine <>
1999-03-30 16:24:03 +0000

    Corrections towards GEANT manager

--- StRoot/St_base/St_NodeView.h
+++ StRoot/St_base/St_NodeView.h
@@ -1,6 +1,9 @@
 //*-- Author :    Valery Fine(fine@bnl.gov)   25/12/98 
-// $Id: St_NodeView.h,v 1.5 1999/03/29 19:25:26 fine Exp $
+// $Id: St_NodeView.h,v 1.6 1999/03/30 16:24:02 fine Exp $
 // $Log: St_NodeView.h,v $
+// Revision 1.6  1999/03/30 16:24:02  fine
+// Corrections towards GEANT manager
+//
 // Revision 1.5  1999/03/29 19:25:26  fine
 // Visibility flag have been activated. Some working correction
 //
@@ -24,10 +27,13 @@ public:
   virtual void Browse(TBrowser *b);
   virtual void Draw(Option_t *option); // *MENU* 
   virtual Int_t DistancetoPrimitive(Int_t px, Int_t py);
+  virtual Bool_t IsMarked();
   virtual St_NodePosition *GetPosition(){ return (St_NodePosition *)GetObject();}
   virtual St_Node         *GetNode();
   virtual void             Paint(Option_t *option="");
   ClassDef(St_NodeView,1)
 };
+
+inline Bool_t St_NodeView::IsMarked(){ return TestBit(kMark); }
 #endif
 
