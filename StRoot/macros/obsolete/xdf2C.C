310de50338dd1ea6b33f2fe9de72dec411644856
Victor Perevoztchikov <perev@bnl.gov>
1999-04-01 00:55:17 +0000

    xdf to .C converter

--- StRoot/macros/obsolete/xdf2C.C
+++ StRoot/macros/obsolete/xdf2C.C
@@ -1,89 +1,89 @@
-// $Id: xdf2C.C,v 1.1 1998/12/12 02:38:45 fisyak Exp $
+// $Id: xdf2C.C,v 1.2 1999/04/01 00:55:17 perev Exp $
 // $Log: xdf2C.C,v $
-// Revision 1.1  1998/12/12 02:38:45  fisyak
-// Clean up
+// Revision 1.2  1999/04/01 00:55:17  perev
+// xdf to .C converter
 //
-// Revision 1.25  1998/11/25 21:58:36  fisyak
-// Cleanup
 //
-// Revision 1.24  1998/11/07 02:45:05  fisyak
-// cleanup analysis
-//
-// Revision 1.23  1998/11/06 01:42:18  fisyak
-// Add ana.C
-//
-// Revision 1.22  1998/11/01 16:42:28  fisyak
-// dst analysis
-//
-// Revision 1.21  1998/10/31 00:26:26  fisyak
-// Makers take care about branches
-//
-// Revision 1.20  1998/10/21 20:30:56  fine
-// makedoc macro creates "gif" directories and fill it up
-//
-// Revision 1.19  1998/10/18 21:20:49  fisyak
-// typo
-//
-// Revision 1.18  1998/10/12 00:53:02  fisyak
-// Add parameters for bfc
-//
-// Revision 1.17  1998/09/27 01:24:22  fisyak
-// bfc.C for whole file
-//
-// Revision 1.16  1998/09/26 00:35:31  fisyak
-// Add real files
-//
-// Revision 1.15  1998/09/26 00:17:27  fisyak
-// Add SetWrite
-//
-// Revision 1.13  1998/09/23 20:23:23  fisyak
-// Prerelease SL98h
-//
-// Revision 1.12  1998/09/18 14:35:33  fisyak
-// Fix makers
-//
-// Revision 1.11  1998/09/15 20:55:35  fisyak
-// Split St_DataSet -> St_DataSet + St_DataSetIter
-//
-// Revision 1.10  1998/08/26 12:15:15  fisyak
-// Remove asu & dsl libraries
-//
-// Revision 1.9  1998/08/20 12:33:32  fisyak
-// Splitted base libraries
-//
-// Revision 1.8  1998/08/18 14:05:08  fisyak
-// Add to bfc dst
-//
-// Revision 1.7  1998/08/10 02:35:13  fisyak
-// add laser
-//
-xdf2C(const Char_t *fileinp = "geant.xdf")
+void xdf2C(const char *Input="$STAR/params",const char *Output="qweDB")
 {
+  int ierr;
   gSystem->Load("St_base");
   gSystem->Load("xdf2root");
   gSystem->Load("St_Tables");
-  //  StChain chain("bfc");
-  const Char_t *Path="/afs/rhic/star/packages/SL98k/StdB/";
-  St_XDFFile   *xdf_in   = new St_XDFFile(fileinp,"r");
-  // Init the mai chain and all its makers
-  St_DataSet *set=xdf_in->NextEventGet();
-  St_DataSetIter param(set);
-  St_DataSet *t = 0;
-  //  St_tss_tsspar *tsspar = (St_tss_tsspar *) param("params/tpc/tsspars/tsspar");
-  //  tsspar->Print(0);
-  TString path = Path;
-  while (t = param()){
-    path  = Path;
-    path += t->Path();
-    if (t->HasData()){ // Table
-      path += ".C";
-      ofstream *out = new ofstream(path.Data());
-      cout << "Open " << path.Data() << endl;
-      t->SavePrimitive(*out,"");
-      delete out;
-    }
-    else { // DataSet
-      if (gSystem->mkdir(path.Data())) {cout << "Directoty " << path << " creation failed" << endl;}
-    }
-  }
+
+  ofstream out; 
+  St_DataSet *TOP = new St_DataSet("TOP");
+  St_DataSet *Top = new St_DataSet("Top");
+  TString TSInp = Input;
+  TString TSOut = Output;
+  gSystem->ExpandPathName(TSInp);
+  gSystem->ExpandPathName(TSOut);
+
+
+//  const char *Ipref = "/afs/rhic/star/packages/SL99c/";
+  TString Ipref = gSystem->DirName((const char*)TSInp);Ipref +="/";
+  const char *params =  "params" ;
+  const char *params = gSystem->BaseName((const char*)TSInp);
+
+//  const char *Ipref = "$ww/";
+//  const char *Opref = "/afs/rhic/star/packages/SL99d/StDB/";
+  TString Opref = TSOut; Opref +="/";
+  gSystem->MakeDirectory((const char*)TSOut);
+
+  TString OuputFile,CWD ;
+  TString InputFile = Ipref; InputFile += params;
+
+  St_FileSet *fs = new St_FileSet((char*)InputFile);
+  TOP->Add(fs);
+  St_DataSet *set;
+  char path[100];
+//  fs->ls(0);  
+
+  St_DataSetIter next(TOP,9999);
+  while (set = next()) { //loop over DIR 
+    strcpy(path, (char*)(set->Path())+4);
+    if (strstr(path,"CVS")) continue;
+    InputFile = Ipref; InputFile.Append(path);
+    OuputFile = Opref; OuputFile += path;
+
+    printf("InputFile=%s\n",(const char*)InputFile);
+    printf("OuputFile=%s\n",(const char*)OuputFile);
+
+
+    if (strcmp("directory",set->GetTitle())==0) {	// directory
+      ierr = gSystem->MakeDirectory((char*)OuputFile);
+      printf("MakeDir=%s %d\n",(char*)OuputFile,ierr);
+      CWD = OuputFile;
+////      gSystem->ChangeDirectory((char*)OuputFile);
+    } else {						// file
+      if (!strstr((char*)InputFile,".xdf")) continue;
+      printf("Process file %s -> %s\n",(char*)InputFile);
+      St_XDFFile   *xdf_in   = new St_XDFFile((char*)InputFile,"r");
+
+      St_DataSet *zet= xdf_in->NextEventGet();
+      Top->Add(zet);
+      St_DataSetIter Param(Top,9999);
+      St_DataSet *t;
+
+      while ((t = Param()))   {// xdf loop
+        strcpy(path,(char*)t->Path()+4);
+
+        TString FileC = CWD; FileC += "/"; FileC += path;
+        printf("FileC=%s\n",(char*)FileC);
+        if (t->HasData()){ // Table
+          FileC +=".C";
+          out.open((char*)FileC);
+          printf("OpenOut %s \n",(char*)FileC);
+          t->SavePrimitive(out,"");
+          out.close();
+        } else { // DataSet
+          ierr=gSystem->MakeDirectory((char*)FileC);
+          printf("MakeXdfDir=%s %d\n",(char*)FileC,ierr);
+        }
+ 
+        printf("%s\n",path);
+      }// end of xdf loop
+    delete zet;
+    }// end if (DIR/FILE)
+  }// end oop over DIR
 }
