3fe6121bc379ff2556213234d15dc823703bec2d
cvs2git <cvs2git>
1999-04-03 01:03:19 +0000

    This commit was manufactured by cvs2git to create branch 'snelling'.

--- StRoot/macros/LdStaf.C
+++
@@ -1,6 +0,0 @@
-Int_t LdStaf(Char_t *libs)
-{
-  Char_t buffer[300]="GEN/lib/";
-  strcat(buffer,libs);
-  return    gSystem.Load(buffer);
-}
--- StRoot/macros/plotTable.C
+++
@@ -1,9 +0,0 @@
-{
-gSystem->Load("St_base");
-gSystem->Load("St_Tables");
-gSystem->Load("xdf2root");
-St_ev0_aux temp;
-St_TableNtuple myNtuple(temp);
-myNtuple.AddXDFFile("/disk00000/star/mdc1_test_data/mdc1_year2a_psc079_01_46evts_dst.xdf","dst","ev0out");
-myNtuple.Draw("theta");
-}
