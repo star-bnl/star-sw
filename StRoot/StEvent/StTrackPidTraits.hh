5678138ae23a58db543f7398cbc77d50d08ac5a8
ullrich <>
1999-04-08 15:01:18 +0000

    Added dE/dx PID from TPC.

--- StRoot/StEvent/StTrackPidTraits.hh
+++ StRoot/StEvent/StTrackPidTraits.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTrackPidTraits.hh,v 1.2 1999/01/15 22:54:10 wenaus Exp $
+ * $Id: StTrackPidTraits.hh,v 1.3 1999/04/08 15:01:18 ullrich Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  ***************************************************************************
@@ -10,8 +10,8 @@
  ***************************************************************************
  *
  * $Log: StTrackPidTraits.hh,v $
- * Revision 1.2  1999/01/15 22:54:10  wenaus
- * version with constructors for table-based loading
+ * Revision 1.3  1999/04/08 15:01:18  ullrich
+ * Added dE/dx PID from TPC.
  *
  * Revision 1.2  1999/01/15 22:54:10  wenaus
  * version with constructors for table-based loading
@@ -20,16 +20,21 @@
 #ifndef StTrackPidTraits_hh
 #define StTrackPidTraits_hh
 
+class StGlobalTrack;
+class StDedxPid;
+
 class StTrackPidTraits {
 public:
-//     StTrackPidTraits();
-//     StTrackPidTraits(const StTrackPidTraits&);
-//     ~StTrackPidTraits();
-//     const StTrackPidTraits & operator=(const StTrackPidTraits&);
-//     int operator==(const StTrackPidTraits&) const;
-//     int operator!=(const StTrackPidTraits&) const;
-protected:
+    StTrackPidTraits(const StGlobalTrack&);
+    ~StTrackPidTraits();
+
+    const StDedxPid* tpcDedxPid() const;
+    
 private:
+    StDedxPid *mTpcDedxPid;
+    // StDedxPid *mSvtDedxPid;  
 };
+
+inline const StDedxPid* StTrackPidTraits::tpcDedxPid() const {return mTpcDedxPid;}
 
 #endif
