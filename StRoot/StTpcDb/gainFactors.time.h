b5333d7b329525687495554a34eee8b7dc0ef3e5
hardtke <>
1999-04-15 17:06:42 +0000

    interfaces for gain factors

---
+++ StRoot/StTpcDb/gainFactors.time.h
@@ -0,0 +1,17 @@
+//:Description: 
+//:Synonyms::::
+//:Source:
+//:Update:
+//:Update frequncy:
+//:Reminder:
+//:Recall frequency:
+//:Size of Data:
+//:Pointer to data:  gainFactors.time:
+struct gain_factors {
+
+  //  type varnam;    //Units : Comments
+
+  float innerSectorGainFactors[1750];
+  float outerSectorGainFactors[3942];
+
+};
