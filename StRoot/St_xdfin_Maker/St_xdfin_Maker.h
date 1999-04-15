2e1bc7501959ff9901e9323f63c030ed6b6aabf9
fisyak <>
1999-04-15 15:14:25 +0000

    Restore function Init_Done

--- StRoot/St_xdfin_Maker/St_xdfin_Maker.h
+++ StRoot/St_xdfin_Maker/St_xdfin_Maker.h
@@ -21,6 +21,7 @@ public:
    St_xdfin_Maker(const char *name,const char *inputFile=0);
    virtual       ~St_xdfin_Maker();
    virtual Int_t  Init();
+   virtual void   Init_Done (Bool_t k=kFALSE){m_InitDone = k;} // *MENU*
    virtual Int_t  Make();
    virtual void   PrintInfo();
    virtual void   Skip(Int_t Nskip=1);
