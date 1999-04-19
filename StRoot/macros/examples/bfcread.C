983bb894cdadb971df421c36219a39e1000ec6c7
Lidia Didenko <didenko@bnl.gov>
1999-04-19 18:10:41 +0000

    readDST macro in new scheme

--- StRoot/macros/examples/bfcread.C
+++ StRoot/macros/examples/bfcread.C
@@ -2,6 +2,7 @@ class StChain;
 class St_DataSet;
 St_DataSet *dst;
 StChain *cnain;
+TBrowser *brow=0;
 
 void bfcread(Int_t nevents=1, const char *MainFile="gtrack.dst.root")
 {
@@ -10,6 +11,7 @@ void bfcread(Int_t nevents=1, const char *MainFile="gtrack.dst.root")
     gSystem->Load("StChain");
     gSystem->Load("St_Tables");
     gSystem->Load("StTreeMaker");
+    gSystem->Load("St_QA_Maker");
 
 //	TOP maker
     chain = new StChain("bfc");
@@ -20,16 +22,22 @@ void bfcread(Int_t nevents=1, const char *MainFile="gtrack.dst.root")
   treeMk->SetIOMode("r");
   treeMk->SetDebug();
 
-  chain->SetInput("dst","bfc/dst");
-
+  //  chain->SetInput("dst","bfc/dst/dst");
+   chain->SetInput("dst","bfc/dst");
+   St_QA_Maker  *qa  = new St_QA_Maker;
+  
   chain->Init();
-     
+ 
+
   for (int iev=0;iev<nevents; iev++)
   {
     chain->Clear();
     int iret = chain->Make();
     if (iret) break;
     dst = chain->GetDataSet("dst");
-    if (dst) dst->ls(9);    
+    if (dst) {
+          dst->ls(9);
+          brow = new TBrowser("dst",dst);    
+    }
   } 
 }
