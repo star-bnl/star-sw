70329bb32772126efd31a988d25dccf219344e0f
fisyak <>
1999-04-20 12:56:08 +0000

    Add ctf/mwc

--- StRoot/macros/bfc_tfs.C
+++ StRoot/macros/bfc_tfs.C
@@ -1,5 +1,8 @@
-// $Id: bfc_tfs.C,v 1.12 1999/04/19 13:43:19 fisyak Exp $
+// $Id: bfc_tfs.C,v 1.13 1999/04/20 12:56:03 fisyak Exp $
 // $Log: bfc_tfs.C,v $
+// Revision 1.13  1999/04/20 12:56:03  fisyak
+// Add ctf/mwc
+//
 // Revision 1.12  1999/04/19 13:43:19  fisyak
 // Take out L3
 //
@@ -361,6 +364,14 @@ void bfc_tfs (const Int_t Nevents=1000,Char_t *infile=0, Char_t *outfile=0)
   St_fpt_Maker *fptMk 	= new St_fpt_Maker("ftpc_tracks");
   fptMk->SetDebug();
 #endif /* FTPC */
+#ifdef CTF
+  St_ctf_Maker         *ctf      = new St_ctf_Maker("ctf");
+  St_mwc_Maker         *mwc      = new St_mwc_Maker("mwc");
+  St_trg_Maker         *trg      = new St_trg_Maker("trg");
+#endif
+#ifdef L3
+  St_l3t_Maker         *l3Tracks   = new St_l3t_Maker("l3Tracks");
+#endif
   St_dst_Maker *dstMk = 0;
 #ifdef GLOBAL
 //		global
--- StRoot/macros/bfcz.C
+++ StRoot/macros/bfcz.C
@@ -1,9 +1,12 @@
-// $Id: bfcz.C,v 1.13 1999/04/19 13:43:20 fisyak Exp $
+// $Id: bfcz.C,v 1.14 1999/04/20 12:56:08 fisyak Exp $
 // $Log: bfcz.C,v $
-// Revision 1.13  1999/04/19 13:43:20  fisyak
+// Revision 1.14  1999/04/20 12:56:08  fisyak
+// Add ctf/mwc
+//
+// Revision 1.12  1999/04/19 13:43:19  fisyak
 // Take out L3
 //
-// Revision 1.12  1999/04/18 23:45:05  fisyak
+// Revision 1.11  1999/04/18 23:45:05  fisyak
 // New schema
 //
 // macro to read fz files, xdf files and minidaq data 
@@ -361,6 +364,14 @@ void bfcz (const Int_t Nevents=1000,Char_t *infile=0, Char_t *outfile=0)
   St_fpt_Maker *fptMk 	= new St_fpt_Maker("ftpc_tracks");
   fptMk->SetDebug();
 #endif /* FTPC */
+#ifdef CTF
+  St_ctf_Maker         *ctf      = new St_ctf_Maker("ctf");
+  St_mwc_Maker         *mwc      = new St_mwc_Maker("mwc");
+  St_trg_Maker         *trg      = new St_trg_Maker("trg");
+#endif
+#ifdef L3
+  St_l3t_Maker         *l3Tracks   = new St_l3t_Maker("l3Tracks");
+#endif
   St_dst_Maker *dstMk = 0;
 #ifdef GLOBAL
 //		global
