bff5bb69bfbe6fdb91c7ca349b44c689bed90a85
David Hardtke <hardtke@uclink4.berkeley.edu>
1999-04-29 14:44:38 +0000

    add electronics interface class

---
+++ StRoot/StTpcDb/TPCElectronics.time.h
@@ -0,0 +1,26 @@
+//:Description: 
+//:Synonyms::::
+//:Source:
+//:Update:
+//:Update frequncy:
+//:Reminder:
+//:Recall frequency:
+//:Size of Data:
+//:Pointer to data:  TPCElectronics.time:
+struct tpcelectronics {
+
+  //  type varnam;    //Units : Comments
+
+    int    numberOfTimeBins;
+    double nominalGain;
+    double samplingFrequency;
+    double tZero;
+    double adcCharge;
+    double adcConversion;
+    double averagePedestal;
+    double shapingTime;
+    double tau;
+   
+};
+
+
