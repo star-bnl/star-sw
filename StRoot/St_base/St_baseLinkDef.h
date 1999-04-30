88e09fdc9ff0230ce0235e8b83dca186c2e6ea3e
perev <>
1999-04-30 21:10:13 +0000

    add StFile to LinkDef.h

--- StRoot/St_base/St_baseLinkDef.h
+++ StRoot/St_base/St_baseLinkDef.h
@@ -4,6 +4,7 @@
 #pragma link off all functions;
 #pragma link C++ class StTree;
 #pragma link C++ class StBranch;
+#pragma link C++ class StFile;
 #pragma link C++ class StIOEvent-;
 #pragma link C++ class StIO-;
 #pragma link C++ class StArray;
