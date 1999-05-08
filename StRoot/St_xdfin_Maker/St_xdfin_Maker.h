84c4d1adaa1cb28d579d1df2aa0d80b60f84e905
Yuri Fisyak <fisyak@bnl.gov>
1999-05-08 02:38:58 +0000

    Add fEvtHddr

--- StRoot/St_xdfin_Maker/St_xdfin_Maker.h
+++ StRoot/St_xdfin_Maker/St_xdfin_Maker.h
@@ -14,23 +14,24 @@
 #include "St_XDFFile.h"
 
 class St_xdfin_Maker : public StIOInterFace {
-private:
-St_XDFFile fXdfin;
-int m_InitDone;
-public:
-   St_xdfin_Maker(const char *name="xdfin",const char *inputFile=0);
-   virtual       ~St_xdfin_Maker();
-   virtual Int_t  Init();
-   virtual Int_t  Open(const char *file=0);
-   virtual void   Close(Option_t *opt=0);
-   virtual void   Init_Done (Bool_t k=kFALSE){m_InitDone = k;} // *MENU*
-   virtual Int_t  Make();
-   virtual void   PrintInfo();
-   virtual void   Skip(Int_t Nskip=1);
+ private:
+  St_XDFFile fXdfin;
+  int m_InitDone;
+  StEvtHddr *fEvtHddr;//! pointer to Event Header
+ public:
+  St_xdfin_Maker(const char *name="xdfin",const char *inputFile=0);
+  virtual       ~St_xdfin_Maker();
+  virtual Int_t  Init();
+  virtual Int_t  Open(const char *file=0);
+  virtual void   Close(Option_t *opt=0);
+  virtual void   Init_Done (Bool_t k=kFALSE){m_InitDone = k;} // *MENU*
+  virtual Int_t  Make();
+  virtual void   PrintInfo();
+  virtual void   Skip(Int_t Nskip=1);
 //	for compatability with StIOInterFace
-   void SetBranch(const Char_t *brName,const Char_t *file=0,const Char_t *iomode="r"){};
+  void SetBranch(const Char_t *brName,const Char_t *file=0,const Char_t *iomode="r"){};
 
-   ClassDef(St_xdfin_Maker, 0)   //StAF chain virtual base class for Makers
+  ClassDef(St_xdfin_Maker, 0)   //StAF chain virtual base class for Makers
 };
 
 #endif
