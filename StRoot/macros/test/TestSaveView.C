352e98b4b3b41c19997c8bcd9a1c2d3bf82a6ae9
Yuri Fisyak <fisyak@bnl.gov>
1999-05-10 19:16:25 +0000

    remove bfcread.C

--- StRoot/macros/test/TestSaveView.C
+++ StRoot/macros/test/TestSaveView.C
@@ -1,22 +1,48 @@
-TestSaveView(St_NodeView *view)
+TestSaveView(St_NodeView *viewFull)
 {
 //  gSystem->MakeDirectory(GetName());
 //  gSystem->ChangeDirectory(GetName());
-  if (!view) return;
+  if (!viewFull) return;
   
-  St_DataSetIter next(view);
+  St_DataSetIter next(viewFull,0);
   St_NodeView *view = 0;
   Int_t iCounter = 0;
-  Char_t  buffer[100];
+//  Char_t  buffer[100];
   TString fileName;
+  TString topDirectory="./";
+  TString curDirectory = topDirectory;
+  Char_t *fullPath = gSystem->ConcatFileName(curDirectory.Data(),viewFull->GetName());
+  gSystem->MakeDirectory(fullPath);
+  delete [] fullPath;
+
   while( (view = (St_NodeView *)next()) )
   {
-       fileName = view->GetName();
-       sprintf(buffer,"%d",iCounter);
-       fileName += buffer;
+
+       UInt_t numPosition = view->GetPosition()->GetId();
+//       sprintf(buffer,";%d",numPosition);
+
+       curDirectory = topDirectory;
+       curDirectory += view->PathP();
+//       curDirectory += buffer;
+
+//       fileName  = view->GetName();
+//       fileName += buffer;
+       fileName = "Position";
+       fileName += ".C.";
+       fileName += "year_1b";
+
+       // Create directory 
+       cout << "Dir: " << curDirectory.Data() << endl;
+       if (gSystem->MakeDirectory(curDirectory.Data())) {
+          printf(" can not create %s for %s \n",curDirectory.Data(), view->GetName());
+          return;
+       }
+
+       fullPath = gSystem->ConcatFileName(curDirectory.Data(),fileName.Data());
        ofstream out;
-       out.open(fileName.Data());
-       cout << iCounter+1 << ". " << fileName << endl;
+       out.open(fullPath);
+//==       cout << iCounter+1 << ". " << fullPath << endl;
+       delete [] fullPath;
        view->SavePrimitive(out);
        out.close();
        iCounter++;
