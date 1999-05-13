eb3759a6e5f6ea8a9b96f1ee067e957bba197cf2
perev <>
1999-05-13 21:24:10 +0000

    Aliases + SetOff

--- StRoot/St_db_Maker/St_db_Maker.h
+++ StRoot/St_db_Maker/St_db_Maker.h
@@ -28,14 +28,15 @@ enum DBConst {kMinTime = 19950101, kMaxTime = 20380101};
 class St_db_Maker : public StMaker {
 private:
 
-  Bool_t drawinit;
-  TObjArray  *m_DBList;        // List of the nodes containing the active validation tables
+  St_DataSet *m_DataBase;       // DB structure
   
-  TString     m_MainDir;       // The main root directory for the calibrarion data
-  TString     m_UserDir;       // The user root directory for the calibrarion data
-  TString     m_CurrentDir;    // The current root directory for the calibrarion data
+  TString     m_MainDir;        // The main root directory for the calibrarion data
+  TString     m_UserDir;        // The user root directory for the calibrarion data
+  TString     m_CurrentDir;     // The current root directory for the calibrarion data
+  Int_t       fIsDBTime;	//! flag to use owb time stamp
+  TDatime     fDBTime;		//! Own DB time stamp
 
-//  static Char_t m_VersionCVS = "$Id: St_db_Maker.h,v 1.2 1999/03/11 01:32:56 perev Exp $";
+//  static Char_t m_VersionCVS = "$Id: St_db_Maker.h,v 1.3 1999/05/13 21:24:10 perev Exp $";
  protected:
  public: 
                    St_db_Maker(const char *name,const char *maindir,const char *userdir=0);
@@ -43,11 +44,17 @@ private:
    virtual void    Browse(TBrowser *b);
    virtual TString GetMainDir(){ return m_MainDir;}
    virtual TString GetUserDir(){ return m_UserDir;}
+   virtual TDatime GetDateTime();
+   virtual void    SetDateTime(int idat,int itim);
+   virtual void    SetDateTime(const char *datalias);
    virtual Int_t   Init();
    virtual Int_t   Make(){return kStOK;};                                                               // *MENU*
    virtual void    PrintInfo();
    virtual void    SetMainDir(const Char_t *db);
    virtual void    SetUserDir(const Char_t *db);
+   virtual void    SetOff(const Char_t *path);
+   virtual void    SetOn (const Char_t *path);
+   virtual void    OnOff();
    virtual St_DataSet* UpdateDB (St_DataSet* ds);
    static EDataSetPass UpdateDB (St_DataSet* ds,void *user );
    static EDataSetPass PrepareDB(St_DataSet* ds,void *user );
