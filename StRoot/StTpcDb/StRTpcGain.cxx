05da01a84c111f6a11b61f25b4893f06dceb4364
hardtke <>
1999-05-17 22:05:12 +0000

    source code for accessing Gain Tables

---
+++ StRoot/StTpcDb/StRTpcGain.cxx
@@ -0,0 +1,33 @@
+#include <StRTpcGain.h>
+
+ClassImp(StRTpcGain)
+
+
+float StRTpcGain::getGain(int row, int pad)   const {
+  if (row>0&&row<=padplane->numberOfInnerRows()){
+    return mGain.innerSectorGainFactors[padplane->indexForRowPad(row,pad)];
+  }
+  else if (row>padplane->numberOfInnerRows()&&row<=padplane->numberOfRows()){
+    return mGain.outerSectorGainFactors[padplane->indexForRowPad(row,pad)];
+  }
+} 
+  
+float StRTpcGain::getOnlineGain(int row, int pad) const {return 0;}
+  
+float StRTpcGain::getNominalGain(int row, int pad) const {return 0;}
+  
+float StRTpcGain::getRelativeGain(int row, int pad) const {return 0;}
+  
+float StRTpcGain::getAverageGainInner(int sector) const {return 0;}
+  
+float StRTpcGain::getAverageGainOuter(int sector) const {return 0;}
+ 
+
+
+
+
+
+
+
+
+
