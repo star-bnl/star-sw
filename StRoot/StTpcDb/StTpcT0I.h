433f339d9d27ca554d5ced87554eb7dc5212fe7e
David Hardtke <hardtke@uclink4.berkeley.edu>
1999-05-19 21:58:10 +0000

    interface class for time offsets (T0)

---
+++ StRoot/StTpcDb/StTpcT0I.h
@@ -0,0 +1,32 @@
+#ifndef __STTPCT0I__
+#define __STTPCT0I__
+#include <TObject.h>
+#include <StTpcPadPlaneI.h>
+
+class StTpcT0I : public TObject {
+
+  //Abstract base class defining accessors
+public:
+
+  virtual float getT0(int row, int pad)   const = 0;
+  virtual void SetPadPlanePointer(StTpcPadPlaneI* ppin) = 0;
+
+  ClassDef(StTpcT0I,0)
+
+};
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ StRoot/StTpcDb/timeOffsets.time.h
@@ -0,0 +1,16 @@
+//:Description: 
+//:Synonyms::::
+//:Source:
+//:Update:
+//:Update frequncy:
+//:Reminder:
+//:Recall frequency:
+//:Size of Data:
+//:Pointer to data:  timeOffsets.time:
+struct time_offsets {
+
+  //  type varnam;    //Units : Comments
+  long innerSectorTimeOffsets[1750];
+  long outerSectorTimeOffsets[3942];
+
+};
