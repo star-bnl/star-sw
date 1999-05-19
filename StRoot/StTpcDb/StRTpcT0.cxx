f310a3ace2184a1596e8868a2a7678f8f3304d89
hardtke <>
1999-05-19 21:58:10 +0000

    interface class for time offsets (T0)

---
+++ StRoot/StTpcDb/StRTpcT0.cxx
@@ -0,0 +1,28 @@
+#include <StRTpcT0.h>
+
+ClassImp(StRTpcT0)
+
+  void StRTpcT0::SetPadPlanePointer(StTpcPadPlaneI* ppin){
+      padplane = ppin;
+  }
+
+
+float StRTpcT0::getT0(int row, int pad)   const {
+  if (row>0&&row<=padplane->numberOfInnerRows()){
+    return mT0.innerSectorTimeOffsets[padplane->indexForRowPad(row,pad)];
+  }
+  else if (row>padplane->numberOfInnerRows()&&row<=padplane->numberOfRows()){
+    return mT0.outerSectorTimeOffsets[padplane->indexForRowPad(row,pad)];
+  }
+} 
+  
+ 
+
+
+
+
+
+
+
+
+
