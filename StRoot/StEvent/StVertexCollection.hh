159e6842b92499bee165963d636865768778e147
Victor Perevoztchikov <perev@bnl.gov>
1999-05-22 18:34:37 +0000

    Lists replaced by vectors

--- StRoot/StEvent/StVertexCollection.hh
+++ StRoot/StEvent/StVertexCollection.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StVertexCollection.hh,v 1.4 1999/03/04 18:17:49 ullrich Exp $
+ * $Id: StVertexCollection.hh,v 1.5 1999/05/22 18:34:37 perev Exp $
  *
  * Author: Thomas Ullrich, Jan 1999
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StVertexCollection.hh,v $
+ * Revision 1.5  1999/05/22 18:34:37  perev
+ * Lists replaced by vectors
+ *
  * Revision 1.4  1999/03/04 18:17:49  ullrich
  * Namespace std not used if ST_NO_NAMESPACES defined
  *
@@ -30,11 +33,11 @@ using namespace std;
 #endif
 
 #ifdef ST_NO_TEMPLATE_DEF_ARGS
-typedef list<StVertex*, allocator<StVertex*> >            StVertexCollection;
-typedef list<StVertex*, allocator<StVertex*> >::iterator  StVertexIterator;
+typedef vector<StVertex*, allocator<StVertex*> >            StVertexCollection;
+typedef vector<StVertex*, allocator<StVertex*> >::iterator  StVertexIterator;
 #else
-typedef list<StVertex*>            StVertexCollection;
-typedef list<StVertex*>::iterator  StVertexIterator;
+typedef vector<StVertex*>            StVertexCollection;
+typedef vector<StVertex*>::iterator  StVertexIterator;
 #endif
 
 #endif
