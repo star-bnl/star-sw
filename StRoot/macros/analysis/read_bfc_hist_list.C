711771e6ad9057ed8d4bc99e788e3f2eb7f126b8
Kathleen R. Turner <kathy>
1999-06-03 16:12:22 +0000

    putting in new default files for some macros, remove unneeded macro dedx.C and put in new read_bfc_hist_list.C macro

---
+++ StRoot/macros/analysis/read_bfc_hist_list.C
@@ -0,0 +1,67 @@
+//  
+//  
+
+//=======================================================================
+// owner: Kathy Turner
+// what it does: reads the *.hist.root file produced from bfc.C and
+//               then lists all histogram branches and the names and
+//               titles of the histograms in the branches
+//=======================================================================
+
+
+class StChain;
+class St_DataSet;
+St_DataSet *Event;
+StChain *chain;
+TBrowser *brow=0;
+
+void Example_pmj(Int_t nevents=1, const char
+*MainFile="/disk00000/star/test/new/tfs_Solaris/year_1b/psc0069_01_40evts.hist.root")
+
+{
+//
+    gSystem->Load("St_base");
+    gSystem->Load("StChain");
+    gSystem->Load("St_Tables");
+    gSystem->Load("StTreeMaker");
+    gSystem->Load("StarClassLibrary");
+    gSystem->Load("StRootEvent");
+
+//  Input Tree
+  StTreeMaker *treeMk = new StTreeMaker("treeRead",MainFile);
+  treeMk->SetIOMode("r");
+  treeMk->SetDebug();
+  treeMk->SetBranch("*",0,"0");  		//deactivate all branches
+  treeMk->SetBranch("histBranch",0,"r");	//activate histBranch
+
+
+// --- now execute chain member functions
+  treeMk->Init();
+  treeMk->Clear();
+  treeMk->Make();
+
+// Now look at histograms:
+    Event = treeMk->GetDataSet("hist");
+      if (Event) {
+	cout << "Here is list of all histograms: " << endl;
+          Event->ls(9); 
+      }
+
+   St_DataSetIter nextHistList(Event);
+   St_ObjectSet *histContainer = 0;
+   TList *dirList = 0;
+   while (histContainer = (St_ObjectSet *)nextHistList()) {
+     dirList = (TList *) histContainer->GetObject();
+     TIter nextHist(dirList);
+     TObject  *o = 0;
+     cout << " --- " << histContainer->GetName() << endl;
+     while (o= nextHist()) {
+       cout << " Hist name: " << o->GetName() << " ==> Title: " 
+	    << o->GetTitle() << endl; 
+     }
+   }
+
+}
+
+
+
