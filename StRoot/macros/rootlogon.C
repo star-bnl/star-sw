77a17c644443a7687efb0cab97fe56fbb532d650
perev <>
1999-06-11 23:01:56 +0000

    cleanup

--- StRoot/macros/rootlogon.C
+++ StRoot/macros/rootlogon.C
@@ -1,5 +1,8 @@
-// $Id: rootlogon.C,v 1.4 1999/05/21 15:34:00 kathy Exp $
+// $Id: rootlogon.C,v 1.5 1999/06/11 23:01:56 perev Exp $
 // $Log: rootlogon.C,v $
+// Revision 1.5  1999/06/11 23:01:56  perev
+// cleanup
+//
 // Revision 1.4  1999/05/21 15:34:00  kathy
 // made sure Log & Id are in each file and also put in standard comment line with name of owner
 //
@@ -16,5 +19,11 @@
    printf("\nType \".x demoshelp.C\" to see the help window\n\n");
 #pragma includepath "./StRoot"
 #pragma includepath "/afs/rhic/star/packages/dev/StRoot"
+
+// 	Assign bif size of hashtable for STAR I/O
+TBuffer::SetGlobalWriteParam(2003);
+printf("fgMapSize=%d\n",TBuffer::GetGlobalWriteParam());
+gROOT->SetStyle("Plain");
+
 }
  
