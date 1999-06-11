5939d457b8176766b5e7ce58483c115a202b8171
Valeri Fine <val.fine@gmail.com>
1999-06-11 18:18:41 +0000

    Help for 3D demos

--- StRoot/macros/graphics/EventDemosHelp.C
+++ StRoot/macros/graphics/EventDemosHelp.C
@@ -4,7 +4,7 @@
    c1 = new TCanvas("c1","Help to run demos",200,10,700,500);
  
    welcome = new TPaveText(.1,.8,.9,.97);
-   welcome->AddText("Welcome to the ROOT demos");
+   welcome->AddText("Welcome to the STAR event demos");
    welcome->SetTextColor(4);
    welcome->SetFillColor(24);
    welcome->Draw();
@@ -12,7 +12,7 @@
    hdemo = new TPaveText(.05,.05,.95,.7);
    hdemo->SetTextAlign(12);
    hdemo->SetTextFont(61);
-   hdemo->AddText("- Run demo Hsimple first. Then in any order");
+   hdemo->AddText("- Run either \"Read StEvent\" or any \"Make StEvent\" first. Then some \"draw\" operation");
    hdemo->AddText("- Click left mouse button to execute one demo");
    hdemo->AddText("- Click right mouse button to see the title of the demo");
    hdemo->AddText("- Click on 'Close Bar' to exit from the demo menu");
