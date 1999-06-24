4116d1e0bdb789cb763592d724949f67cb731ce3
kathy <>
1999-06-24 18:58:49 +0000

    example macro added to read a flat root histogram file (not star specific) and list all histograms

---
+++ StRoot/macros/examples/Example_read_hist_file_list.C
@@ -0,0 +1,39 @@
+
+// Kathy 24June99
+//
+//  - adapted from macro taken from roottalk digest 
+//           http://root.cern.ch/root/roottalk/roottalk99/1228.html
+// 
+//=========================================================================
+
+void draw_hist(const char* filein="/star/u2e/liq/rootc/beam_gas_1.hist.root")
+{
+TFile* fin = new TFile(filein) ;
+
+if (!fin->IsOpen()) {
+printf("<E> Cannot open input file %s\n",filein) ;
+exit(1) ;
+}
+
+TList* list = fin->GetListOfKeys() ;
+if (!list) { printf("<E> No keys found in file\n") ; exit(1) ; }
+TIter next(list) ;
+TKey* key ;
+TObject* obj ;
+
+while ( key = (TKey*)next() ) {
+obj = key->ReadObj() ;
+if ( (strcmp(obj->IsA()->GetName(),"TProfile")!=0)
+ && (!obj->InheritsFrom("TH2"))
+ && (!obj->InheritsFrom("TH1")) ) 
+  {
+  printf("<W> Object %s is not 1D or 2D histogram : "
+  "will not be converted\n",obj->GetName()) ;
+  }
+
+ printf("Histo name:%s title:%s\n",obj->GetName(),obj->GetTitle());
+ }
+
+ cout << " finish draw_hist " << endl;
+}
+
