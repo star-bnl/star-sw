4233f1fd9801a35675307ae7a8f9c1b8373cb2ec
kathy <>
1999-06-28 20:17:41 +0000

    updated version - cleaned up

--- StRoot/macros/analysis/QA_bfcread_dst_tables.C
+++ StRoot/macros/analysis/QA_bfcread_dst_tables.C
@@ -1,5 +1,8 @@
-// $Id: QA_bfcread_dst_tables.C,v 1.2 1999/06/28 16:53:20 kathy Exp $
+// $Id: QA_bfcread_dst_tables.C,v 1.3 1999/06/28 20:17:41 kathy Exp $
 // $Log: QA_bfcread_dst_tables.C,v $
+// Revision 1.3  1999/06/28 20:17:41  kathy
+// updated version - cleaned up
+//
 // Revision 1.2  1999/06/28 16:53:20  kathy
 // updated version - now checks to see how many tables are really there, not just ones we know about
 //
@@ -27,8 +30,8 @@ class St_DataSet;
 St_DataSet *Event;
 
 void QA_bfcread_dst_tables(const char 
-*MainFile="/afs/rhic/star/data/test/dev/tfs_Linux/Thu/year_2a/psc0208_01_40evts.dst.root",
-const char *fname="QA_dst.txt")
+*MainFile="/afs/rhic/star/data/test/dev/tfs_Solaris/Mon/year_1b/psc0050_01_40evts.dst.root",
+const char *fname="qa_tables.txt")
 
 {
 //
@@ -70,7 +73,6 @@ const char *fname="QA_dst.txt")
 // --- now execute chain member functions
   chain->Init();
  
-  Int_t numtables=0;
   St_DataSet *ds=0;
   St_Table *tabl=0;
   St_DataSet *obj=0;
@@ -89,7 +91,6 @@ const char *fname="QA_dst.txt")
     ds=chain->GetDataSet("dst");
     St_DataSetIter tabiter(ds);
 
-    numtables=0;
     tabcntr=0;
 
       if (ds) {
@@ -98,9 +99,9 @@ const char *fname="QA_dst.txt")
       ds->ls(2);
 
       cout << "QA-> Tester: " << endl;
-      cout << "QA-> Date tested: " << endl << endl;
-      cout << "QA-> DST file: " << MainFile << endl;
-      cout << "QA-> Date DST created: " << endl << endl;
+      cout << "QA-> Date tested: " << endl;
+      cout << "QA-> Date DST created: " << endl;
+      cout << "QA-> " << MainFile << endl << endl;
       cout << "QA-> table name";
       cout.width(18);
       cout << "# rows";
@@ -114,264 +115,32 @@ const char *fname="QA_dst.txt")
 
       while (obj = tabiter.Next()) {
 //.. count all tables that exist:
-	    if (obj->InheritsFrom("St_Table")) {
-               tabcntr++;
-//	       cout << "object  = " << obj->GetName() << endl;
-//             cout << "tabcntr = " << tabcntr << endl;
-            }
-      }
+        if (obj->InheritsFrom("St_Table")) {
+          tabcntr++;
+          //cout << "object  = " << obj->GetName() << endl;
+          //cout << "tabcntr = " << tabcntr << endl;
 
-//.. go to event_header
+          tabl = (St_Table *)tabiter.Find(obj->GetName());
+          if (tabl) {
+            cout << "QA-> " << obj->GetName();
+            cout.width(28-strlen(obj->GetName()));
+            cout << tabl->GetNRows() << endl;
 
-        tabl = (St_Table *)tabiter.Find("event_header");
-        
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(16);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(16);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to event_summary
-
-        tabl = (St_Table *)tabiter.Find("event_summary");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(15);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(15);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to globtrk
-
-        tabl = (St_Table *)tabiter.Find("globtrk");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(21);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(21);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to globtrk_aux
-
-        tabl = (St_Table *)tabiter.Find("globtrk_aux");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(17);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(17);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to vertex
-
-        tabl = (St_Table *)tabiter.Find("vertex");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(22);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(22);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to globtrk2
-
-        tabl = (St_Table *)tabiter.Find("globtrk2");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(20);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(20);
-          fout << tabl->GetNRows()<< endl;
- 
-        }
-
-//.. go to primtrk
-
-        tabl = (St_Table *)tabiter.Find("primtrk");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(21);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(21);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to primtrk_aux
-
-        tabl = (St_Table *)tabiter.Find("primtrk_aux");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(17);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(17);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to dst_v0_vertex
-
-        tabl = (St_Table *)tabiter.Find("dst_v0_vertex");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(15);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(15);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to dst_xi_vertex
-
-        tabl = (St_Table *)tabiter.Find("dst_xi_vertex");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(15);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(15);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to dst_dedx
-
-        tabl = (St_Table *)tabiter.Find("dst_dedx");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(20);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(20);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to point
-
-        tabl = (St_Table *)tabiter.Find("point");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(23);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(23);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to monitor_soft
-
-        tabl = (St_Table *)tabiter.Find("monitor_soft");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(16);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(16);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to particle
-
-        tabl = (St_Table *)tabiter.Find("particle");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(20);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(20);
-          fout << tabl->GetNRows()<< endl;
-
-        }
-
-//.. go to g2t_rch_hit
-
-        tabl = (St_Table *)tabiter.Find("g2t_rch_hit");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(17);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(17);
-          fout << tabl->GetNRows()<< endl;
-        }
-
-
-//.. go to dst_TriggerDetectors        
-
-       tabl = (St_Table *)tabiter.Find("dst_TriggerDetectors");
-
-        if (tabl) {
-          numtables++;
-          cout << "QA-> " << tabl->GetName();
-          cout.width(8);
-          cout << tabl->GetNRows()<< endl;
-          fout << "QA-> " << tabl->GetName();
-          fout.width(8);
-          fout << tabl->GetNRows()<< endl;
-
+            fout << "QA-> " << obj->GetName();
+            fout.width(28-strlen(obj->GetName()));
+            fout << tabl->GetNRows()<< endl;
+          }
         }
       }
 
 // ------------------------------------------------------------
 
-      cout << endl <<"QA-> total # tables = " << tabcntr << endl;
+      cout << endl << "QA-> total # tables = " << tabcntr << endl;
       fout << endl << "QA-> total # tables = " << tabcntr << endl;
 
-      cout << "QA-> total # tables checked = " << numtables << endl;
-      fout << "QA-> total # tables checked = " << numtables << endl;
 
       fout.close();
       chain->Finish();    
+      }
     }
 }
