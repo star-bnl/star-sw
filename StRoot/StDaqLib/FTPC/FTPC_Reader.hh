37441c612e3d64f6e5f8d4a1bffd77b2a5807b68
levine <>
1999-07-02 22:21:11 +0000

    dummy FTPC behavior - replace methods with real code

---
+++ StRoot/StDaqLib/FTPC/FTPC_Reader.hh
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *  
+ * Author: M.J. LeVine
+ ***************************************************************************
+ * Description: common definitions for FTPC (dummy placeholder)
+ *      
+ *
+ *   change log
+ *
+ ***************************************************************************
+ *  
+ *
+ **************************************************************************/
+#ifndef FTPC_READER_HH
+#define FTPC_READER_HH
+#include "StDaqLib/GENERIC/EventReader.hh"
+
+// Detector Reader Virtual Class
+
+struct  Bank_FTPCP;
+
+class FTPC_Reader : public DetectorReader
+{
+  friend class EventReader;
+
+public:
+  FTPC_Reader *getFTPCReader(int sector){};
+  ZeroSuppressedReader *getZeroSuppressedReader(int sector){};
+  ADCRawReader *getADCRawReader(int sector){};
+  PedestalReader *getPedestalReader(int sector){};
+  PedestalRMSReader *getPedestalRMSReader(int sector){};
+  GainReader *getGainReader(int sector){};
+  CPPReader *getCPPReader(int sector){};
+  BadChannelReader *getBadChannelReader(int sector){};
+  FTPC_Reader(EventReader *er){};
+  ~FTPC_Reader(){}; 
+
+  int MemUsed(){};
+
+
+protected:
+
+  // copy of EventReader pointer
+  EventReader *ercpy;
+
+  // Bank Pointers
+  Bank_DATAP *pBankDATAP;
+  Bank_FTPCP *pBankFTPCP;
+
+
+  // Useful functions
+  int InformBuffers(ZeroSuppressedReader *, int sector) { return FALSE; };
+  int InformBuffers(ADCRawReader *,int sector) { return FALSE; };
+  int InformBuffers(PedestalReader *,int sector) { return FALSE; };
+  int InformBuffers(PedestalRMSReader *,int sector) { return FALSE; };
+  int InformBuffers(GainReader *,int sector) { return FALSE; };
+  int InformBuffers(CPPReader *,int sector) { return FALSE; };
+  int InformBuffers(BadChannelReader *,int sector) { return FALSE; };
+  int InformBuffers(ConfigReader *,int sector) { return FALSE; };
+
+  int AttachBuffers(ZeroSuppressedReader *, int sector) { return FALSE; };
+  int AttachBuffers(ADCRawReader *, int sector) { return FALSE; };
+  int AttachBuffers(PedestalReader *, int sector) { return FALSE; };
+  int AttachBuffers(PedestalRMSReader *, int sector) { return FALSE; };
+  int AttachBuffers(GainReader *, int sector) { return FALSE; };
+  int AttachBuffers(CPPReader *, int sector) { return FALSE; };
+  int AttachBuffers(BadChannelReader *, int sector) { return FALSE; };
+  int AttachBuffers(ConfigReader *, int sector) { return FALSE; };
+};
+
+#endif
