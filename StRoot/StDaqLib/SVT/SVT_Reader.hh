aeeaecb7d6cc0374a7254eb102c5ab9f87101608
levine <>
1999-07-03 03:23:26 +0000

    added code to navigate to SVTP bank

--- StRoot/StDaqLib/SVT/SVT_Reader.hh
+++ StRoot/StDaqLib/SVT/SVT_Reader.hh
@@ -6,6 +6,7 @@
  *      
  *
  *   change log
+ * 02-Jul-99 MJL add navigation code to get to SVTP bank
  *
  ***************************************************************************
  *  
@@ -14,17 +15,23 @@
 #ifndef SVT_READER_HH
 #define SVT_READER_HH
 #include "StDaqLib/GENERIC/EventReader.hh"
+#include "StDaqLib/GENERIC/RecHeaderFormats.hh"
 
 // Detector Reader Virtual Class
 
-struct  Bank_SVTP;
+
+struct  Bank_SVTP: public Bank
+{
+  Pointer dummy[6];   //**** PLACE HOLDER for real bank definition ******
+  // look at TPC/TPCV2P0.cxx, TPCV2P0.hh for further details
+};
 
 class SVT_Reader : public DetectorReader
 {
   friend class EventReader;
 
 public:
-  SVT_Reader *getSVTReader(int sector){};
+  SVT_Reader *getSVTReader(int sector){cout <<"DUMMY implementation"<<endl;};
   ZeroSuppressedReader *getZeroSuppressedReader(int sector){};
   ADCRawReader *getADCRawReader(int sector){};
   PedestalReader *getPedestalReader(int sector){};
@@ -32,7 +39,34 @@ public:
   GainReader *getGainReader(int sector){};
   CPPReader *getCPPReader(int sector){};
   BadChannelReader *getBadChannelReader(int sector){};
-  SVT_Reader(EventReader *er){};
+  SVT_Reader(EventReader *er){
+    cout <<"DUMMY implementation"<<endl;
+    ercpy = er; // squirrel away pointer eventreader for our friends
+  // Fix up DATAP
+    pBankDATAP = (Bank_DATAP *)er->getDATAP();
+
+    if (!pBankDATAP->test_CRC()) ERROR(ERR_CRC);
+    if (pBankDATAP->swap() < 0) ERROR(ERR_SWAP);
+    pBankDATAP->header.CRC = 0;
+
+    // position independent pointers to lower banks, variable DATAP length
+    int len = pBankDATAP->header.BankLength - sizeof(Bank_Header)/4;
+    Pointer *ptr = &pBankDATAP->SVT;
+    for (int i=0; i<len; i++, ptr++) {
+      if (ptr->length==0) continue;//invalid entry
+      pBankSVTP = (Bank_SVTP *)(((INT32 *)pBankDATAP)+ (ptr->offset)); 
+      if(!strncmp(pBankSVTP->header.BankType,"SVTP",4)) break;
+    }
+    if(strncmp(pBankSVTP->header.BankType,"SVTP",4)) {
+      printf("detector SVT not found in DATAP\n");
+      exit(0);
+    }
+
+    if (!pBankSVTP->test_CRC()) ERROR(ERR_CRC);
+    if (pBankSVTP->swap() < 0) ERROR(ERR_SWAP);
+    pBankSVTP->header.CRC = 0;
+  };
+
   ~SVT_Reader(){}; 
 
   int MemUsed(){};
