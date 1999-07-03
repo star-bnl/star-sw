45b108b1dcb969b950ba76dcb6d2b0750e039357
Micheal J. LeVine <levine@bnl.gov>
1999-07-03 03:21:10 +0000

    added code to navigate to EMCP bank

--- StRoot/StDaqLib/EMC/EMC_Reader.hh
+++ StRoot/StDaqLib/EMC/EMC_Reader.hh
@@ -6,6 +6,7 @@
  *      
  *
  *   change log
+ * 02-Jul-99 MJL add navigation code to get to EMCP bank
  *
  ***************************************************************************
  *  
@@ -14,17 +15,23 @@
 #ifndef EMC_READER_HH
 #define EMC_READER_HH
 #include "StDaqLib/GENERIC/EventReader.hh"
+#include "StDaqLib/GENERIC/RecHeaderFormats.hh"
 
 // Detector Reader Virtual Class
 
-struct  Bank_EMCP;
+
+struct  Bank_EMCP: public Bank
+{
+  Pointer dummy[6];   //**** PLACE HOLDER for real bank definition ******
+  // look at TPC/TPCV2P0.cxx, TPCV2P0.hh for further details
+};
 
 class EMC_Reader : public DetectorReader
 {
   friend class EventReader;
 
 public:
-  EMC_Reader *getEMCReader(int sector){};
+  EMC_Reader *getEMCReader(int sector){cout <<"DUMMY implementation"<<endl;};
   ZeroSuppressedReader *getZeroSuppressedReader(int sector){};
   ADCRawReader *getADCRawReader(int sector){};
   PedestalReader *getPedestalReader(int sector){};
@@ -32,7 +39,34 @@ public:
   GainReader *getGainReader(int sector){};
   CPPReader *getCPPReader(int sector){};
   BadChannelReader *getBadChannelReader(int sector){};
-  EMC_Reader(EventReader *er){};
+  EMC_Reader(EventReader *er){
+    cout <<"DUMMY implementation"<<endl;
+    ercpy = er; // squirrel away pointer eventreader for our friends
+  // Fix up DATAP
+    pBankDATAP = (Bank_DATAP *)er->getDATAP();
+
+    if (!pBankDATAP->test_CRC()) ERROR(ERR_CRC);
+    if (pBankDATAP->swap() < 0) ERROR(ERR_SWAP);
+    pBankDATAP->header.CRC = 0;
+
+    // position independent pointers to lower banks, variable DATAP length
+    int len = pBankDATAP->header.BankLength - sizeof(Bank_Header)/4;
+    Pointer *ptr = &pBankDATAP->EMC;
+    for (int i=0; i<len; i++, ptr++) {
+      if (ptr->length==0) continue;//invalid entry
+      pBankEMCP = (Bank_EMCP *)(((INT32 *)pBankDATAP)+ (ptr->offset)); 
+      if(!strncmp(pBankEMCP->header.BankType,"EMCP",4)) break;
+    }
+    if(strncmp(pBankEMCP->header.BankType,"EMCP",4)) {
+      printf("detector EMC not found in DATAP\n");
+      exit(0);
+    }
+
+    if (!pBankEMCP->test_CRC()) ERROR(ERR_CRC);
+    if (pBankEMCP->swap() < 0) ERROR(ERR_SWAP);
+    pBankEMCP->header.CRC = 0;
+  };
+
   ~EMC_Reader(){}; 
 
   int MemUsed(){};
