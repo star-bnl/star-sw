8a3302d2ca69112b364bbdc901860b21cfa4e948
levine <>
1999-07-10 21:05:49 +0000

    Classes reorganized so that TPC, SVT, SSD, FTPC classes are derived from a single set of interfaces; remianing detectors each have their own interface (defined by the detector group).
    The following applies to EMC, RICH, TRG:
    The xxx_Reader constructor is passed a pointer to its top-level pointer
    bank (xxxP). All code which existed inside the detector-specific classes
    which served to locate this top-level pointer bank has now been extracted and
    moved to the EventReader::findBank() method.
    
    Existing user code for the TPC, SVT, FTPC, SSD should not be changed by this
    re-organization. Other detectors now have a new API. The API for each of
    these "other" detectors is now completely free of constraints, with the
    exception of the arguments passed to the constructor xxx_Reader::xxx_Reader()

--- StRoot/StDaqLib/EMC/EMC_Reader.hh
+++ StRoot/StDaqLib/EMC/EMC_Reader.hh
@@ -7,6 +7,8 @@
  *
  *   change log
  * 02-Jul-99 MJL add navigation code to get to EMCP bank
+ * 08-Jul-99 MJL completely change definition - EMC_Reader is independent 
+ *               class which is handed a pointer at the constructor invocation
  *
  ***************************************************************************
  *  
@@ -26,50 +28,27 @@ struct  Bank_EMCP: public Bank
   // look at TPC/TPCV2P0.cxx, TPCV2P0.hh for further details
 };
 
-class EMC_Reader : public DetectorReader
+class EMC_Reader
 {
   friend class EventReader;
 
 public:
-  EMC_Reader *getEMCReader(int sector){cout <<"DUMMY implementation"<<endl; return FALSE;};
-  ZeroSuppressedReader *getZeroSuppressedReader(int sector){cout<<sector<<endl; return FALSE;};
-  ADCRawReader *getADCRawReader(int sector){cout<<sector<<endl; return FALSE;};
-  PedestalReader *getPedestalReader(int sector){cout<<sector<<endl; return FALSE;};
-  PedestalRMSReader *getPedestalRMSReader(int sector){cout<<sector<<endl; return FALSE;};
-  GainReader *getGainReader(int sector){cout<<sector<<endl; return FALSE;};
-  CPPReader *getCPPReader(int sector){cout<<sector<<endl; return FALSE;};
-  BadChannelReader *getBadChannelReader(int sector){cout<<sector<<endl; return FALSE;};
-  EMC_Reader(EventReader *er){
-    cout <<"DUMMY implementation"<<endl;
+  //  move the constructor guts {...} to a .cxx file
+  EMC_Reader(EventReader *er, Bank_EMCP *pEMCP){
+    pBankEMCP = pEMCP; //copy into class data member for use by other methods
     ercpy = er; // squirrel away pointer eventreader for our friends
-  // Fix up DATAP
-    pBankDATAP = (Bank_DATAP *)er->getDATAP();
-
-    if (!pBankDATAP->test_CRC()) ERROR(ERR_CRC);
-    if (pBankDATAP->swap() < 0) ERROR(ERR_SWAP);
-    pBankDATAP->header.CRC = 0;
-
-    // position independent pointers to lower banks, variable DATAP length
-    int len = pBankDATAP->header.BankLength - sizeof(Bank_Header)/4;
-    Pointer *ptr = &pBankDATAP->EMC;
-    for (int i=0; i<len; i++, ptr++) {
-      if (ptr->length==0) continue;//invalid entry
-      pBankEMCP = (Bank_EMCP *)(((INT32 *)pBankDATAP)+ (ptr->offset)); 
-      if(!strncmp(pBankEMCP->header.BankType,"EMCP",4)) break;
-    }
-    if(strncmp(pBankEMCP->header.BankType,"EMCP",4)) {
-      printf("detector EMC not found in DATAP\n");
-      exit(0);
-    }
-
-    if (!pBankEMCP->test_CRC()) ERROR(ERR_CRC);
-    if (pBankEMCP->swap() < 0) ERROR(ERR_SWAP);
+    if (!pBankEMCP->test_CRC())  {
+    printf("CRC error in EMCP: %s %d\n",__FILE__,__LINE__) ;
+  }
+    if (pBankEMCP->swap() < 0) {
+    printf("swap error in EMCP: %s %d\n",__FILE__,__LINE__) ;
+  }
     pBankEMCP->header.CRC = 0;
+    //do whatever else needs to be done
   };
 
-  ~EMC_Reader(){}; 
 
-  int MemUsed(){return FALSE;};
+  ~EMC_Reader(){}; 
 
 
 protected:
@@ -81,25 +60,8 @@ protected:
   Bank_DATAP *pBankDATAP;
   Bank_EMCP *pBankEMCP;
 
-
-  // Useful functions
-  int InformBuffers(ZeroSuppressedReader *, int sector) { return FALSE; };
-  int InformBuffers(ADCRawReader *,int sector) { return FALSE; };
-  int InformBuffers(PedestalReader *,int sector) { return FALSE; };
-  int InformBuffers(PedestalRMSReader *,int sector) { return FALSE; };
-  int InformBuffers(GainReader *,int sector) { return FALSE; };
-  int InformBuffers(CPPReader *,int sector) { return FALSE; };
-  int InformBuffers(BadChannelReader *,int sector) { return FALSE; };
-  int InformBuffers(ConfigReader *,int sector) { return FALSE; };
-
-  int AttachBuffers(ZeroSuppressedReader *, int sector) { return FALSE; };
-  int AttachBuffers(ADCRawReader *, int sector) { return FALSE; };
-  int AttachBuffers(PedestalReader *, int sector) { return FALSE; };
-  int AttachBuffers(PedestalRMSReader *, int sector) { return FALSE; };
-  int AttachBuffers(GainReader *, int sector) { return FALSE; };
-  int AttachBuffers(CPPReader *, int sector) { return FALSE; };
-  int AttachBuffers(BadChannelReader *, int sector) { return FALSE; };
-  int AttachBuffers(ConfigReader *, int sector) { return FALSE; };
 };
+
+EMC_Reader *getEMCReader(EventReader *er);
 
 #endif
