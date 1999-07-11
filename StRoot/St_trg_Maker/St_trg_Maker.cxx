128591115891c4b2879af6a89a56d5e681046d06
druss <>
1999-07-11 19:32:42 +0000

    Change DST table from dst_TriggerDetectors to dst_TrgDet. The classname can only be 19 letters long.
    also put the m_Dataset->add line before the PAM is called.
    SPIROS is checking in the change to the actual idl file (part of global)

--- StRoot/St_trg_Maker/St_trg_Maker.cxx
+++ StRoot/St_trg_Maker/St_trg_Maker.cxx
@@ -1,5 +1,12 @@
-// $Id: St_trg_Maker.cxx,v 1.3 1999/06/24 18:02:46 druss Exp $
+// $Id: St_trg_Maker.cxx,v 1.4 1999/07/11 19:32:42 druss Exp $
 // $Log: St_trg_Maker.cxx,v $
+// Revision 1.4  1999/07/11 19:32:42  druss
+// Change DST table from dst_TriggerDetectors to dst_TrgDet.
+// The classname can only be 19 letters long.
+//
+// also put the m_Dataset->add line before the PAM is called.
+// SPIROS is checking in the change to the actual idl file (part of global)
+//
 // Revision 1.3  1999/06/24 18:02:46  druss
 // add a line to make sure that the table exists...
 //
@@ -50,22 +57,21 @@ St_trg_Maker::~St_trg_Maker(){
 Int_t St_trg_Maker::Init(){
 // Create tables
 // Create Histograms    
-   return StMaker::Init();
+  return StMaker::Init();
 }
 //_____________________________________________________________________________
 Int_t St_trg_Maker::Make(){
 
+// Get CTB and MWC Tables
   St_DataSet *ctf = GetInputDS("ctf");
   St_DataSet *mwc = GetInputDS("mwc");
-
   if (!ctf || !mwc) return kStWarn;
-
   St_ctu_cor   *ctu_cor  = (St_ctu_cor   *) ctf->Find("ctb_cor");
   St_mwc_raw   *mwc_raw  = (St_mwc_raw   *) mwc->Find("raw"    );
   if (!ctu_cor ||  !mwc_raw) return kStWarn;
 
-  St_dst_TriggerDetectors *dst = new St_dst_TriggerDetectors("dst_TriggerDetectors",1);
-
+// Create 1 row table dst add it to the dataset and run the PAM
+  St_dst_TrgDet *dst = new St_dst_TrgDet("TrgDet",1);
   if (!dst) return kStWarn;
   m_DataSet->Add(dst);
   Int_t Res = trg_fillDst (ctu_cor,mwc_raw,dst);
@@ -76,7 +82,7 @@ Int_t St_trg_Maker::Make(){
 //_____________________________________________________________________________
 void St_trg_Maker::PrintInfo(){
   printf("**************************************************************\n");
-  printf("* $Id: St_trg_Maker.cxx,v 1.3 1999/06/24 18:02:46 druss Exp $\n");
+  printf("* $Id: St_trg_Maker.cxx,v 1.4 1999/07/11 19:32:42 druss Exp $\n");
 //  printf("* %s    *\n",m_VersionCVS);
   printf("**************************************************************\n");
   if (Debug()) StMaker::PrintInfo();
