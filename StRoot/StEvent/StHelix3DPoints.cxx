66416e5fe08bac74d509d68250bdc3a8363ba744
Valeri Fine <val.fine@gmail.com>
1999-07-14 01:38:04 +0000

    Previous version has been restored

--- StRoot/StEvent/StHelix3DPoints.cxx
+++ StRoot/StEvent/StHelix3DPoints.cxx
@@ -1,8 +1,11 @@
 //*-- Author :    Valery Fine   21/05/99  (E-mail: fine@bnl.gov)
-// $Id: StHelix3DPoints.cxx,v 1.2 1999/05/31 18:37:44 fine Exp $
+// $Id: StHelix3DPoints.cxx,v 1.3 1999/07/14 01:38:03 fine Exp $
 // $Log: StHelix3DPoints.cxx,v $
-// Revision 1.2  1999/05/31 18:37:44  fine
-// 3D graphics interface clean up
+// Revision 1.3  1999/07/14 01:38:03  fine
+// Previous version has been restored
+//
+// Revision 1.3  1999/07/14 01:38:03  fine
+// Previous version has been restored
 //
 // Revision 1.2  1999/05/31 18:37:44  fine
 // 3D graphics interface clean up
@@ -39,8 +42,9 @@ StHelix3DPoints::StHelix3DPoints(StHelixD *trackHelix,Float_t step,Int_t lastPos
 {
   m_Helix = 0;
   if (!track) return; 
-    SetLastPosition(lastPosition);
-    SetStep(length/Size());
+  m_Helix = &(track->helix());
+  if (m_Helix) 
+  {
     if (lastPosition) SetLastPosition(lastPosition);
     else              SetLastPosition(1);
     SetStep(length/GetLastPosition());
@@ -54,7 +58,7 @@ StHelix3DPoints::StHelix3DPoints(StHelixD *trackHelix,Float_t step,Int_t lastPos
 
 //________________________________________________________________________________
 StHelix3DPoints::~StHelix3DPoints(){;}
-  return m_N;
+ 
 //________________________________________________________________________________
 Int_t  StHelix3DPoints::GetLastPosition()const {
   return Size()-1;
@@ -92,7 +96,7 @@ Option_t *StHelix3DPoints::GetOption()      const {return "";}
 //________________________________________________________________________________
 void StHelix3DPoints::PaintPoints(Int_t , Float_t *,Option_t *){}
 //________________________________________________________________________________
-  m_N = lastPosition+1;
+Int_t StHelix3DPoints::SetLastPosition(Int_t lastPosition)
 {
   Int_t previousPosition = GetLastPosition();
   m_N = lastPosition+1;  // compute the size
@@ -101,16 +105,32 @@ void StHelix3DPoints::PaintPoints(Int_t , Float_t *,Option_t *){}
 //________________________________________________________________________________
 void StHelix3DPoints::SetOption(Option_t *){;}
 //________________________________________________________________________________
- 
+Int_t StHelix3DPoints::SetPoint(Int_t , Float_t, Float_t , Float_t ){return 0;}
 //________________________________________________________________________________
-Float_t *StHelix3DPoints::GetXYZ(Float_t *xyz,Int_t idx, Int_t num)const
+Int_t StHelix3DPoints::SetPoints(Int_t , Float_t *, Option_t *) {return 0;}
 
+//______________________________________________________________________________
+Float_t *StHelix3DPoints::GetXYZ(Float_t *xyz,Int_t idx,Int_t num)  const
+{
+  //
+  // GetXYZ(Float_t *xyz,Int_t idx,Int_t num=1) fills the buffer supplied
+  // by the calling code with the points information.
+  //
+  //  Input parameters:
+  //  ----------------
+  //   Float_t *xyz - an external user supplied floating point array.
+  //   Int_t    num - the total number of the points to be copied
+  //                  the dimension of that array the size of the
+  //                  array is num*sizeof(Float_t) at least
+  //   Int_t    idx - The index of the first copy to be taken.
+  //
   //  Return: The pointer to the buffer array supplied
   //  ------
 
-    for (Int_t i=idx;i<size;i++) {
-      xyz[j++] = GetX(i);
-      xyz[j++] = GetY(i);
+  if (xyz) {
+    Int_t size = TMath::Min(idx+num,Size());
+    Int_t j=0;
+    Int_t i = 0;
     for (i=idx;i<size;i++) {
       xyz[j++] = GetX(i); 
       xyz[j++] = GetY(i); 
