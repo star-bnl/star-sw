7c12140a4710b2a3f1c05d78551e98b0d0fb8462
cvs2git <cvs2git>
1999-07-17 21:46:36 +0000

    This commit was manufactured by cvs2git to create branch 'cons'.

--- StRoot/StDbLib/Conditions/StDbTpcCondTables.hh
+++
@@ -1,8 +0,0 @@
-#ifndef STDBTPCCONDTABLES_HH
-#define STDBTPCCONDTABLES_HH
-
-
-#include "StDb_tpcGas.h"
-#include "StDb_tpcVoltages.h"
-
-#endif
--- pams/global/inc/StVertexId.h
+++
@@ -1,9 +0,0 @@
-#include "StVertexDefinitions.h"
-enum StVertexId {
-  kEventVtxId  = kEventVertexIdentifier,
-  kV0VtxId     = kV0DecayIdentifier,
-  kXiVtxId     = kXiDecayIdentifier,
-  kKinkVtxId   = kKinkDecayIdentifier,
-  kOtherVtxId  = kOtherTypeIdentifier
-};
-
