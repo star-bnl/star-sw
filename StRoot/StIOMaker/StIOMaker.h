853f9699fd56854acd552c0daebe6c1824c458f4
perev <>
1999-07-19 13:53:52 +0000

    workaround for CINT

--- StRoot/StIOMaker/StIOMaker.h
+++ StRoot/StIOMaker/StIOMaker.h
@@ -32,24 +32,9 @@ public:
    virtual void Clear(Option_t *opt);
    virtual void   SetFile(const char *file);   
            void   SetMaxEvent(Int_t mx=10000000){fMaxEvent=mx;fNumEvent=0;};
-
-   StFile         *fFileSet;    //!Chain of files
-   const char     *fNextFile;	//!next file from file set
-   StIOInterFace  *fCurrMk;	//!Pointer to Current Maker
-   StIOInterFace  *fFmtMk[9];	//!Pointers to TreeMaker,xdfin_Maker,St_io_Maker,StDAQMaker
-
-
-   Int_t  fMaxEvent;		//! for debug only
-   Int_t  fNumEvent;    	//! for debug only
-
-
    void   SetFileSet(StFile *fileSet){fFileSet = fileSet;};
 
 protected:
-
-
-   Int_t  fMaxEvt;
-   Int_t  fNumEvt;
 
 void Build(StFile *fileSet,const char *treeName);
 StIOInterFace *Load();
@@ -57,10 +42,17 @@ StIOInterFace *Load();
 
 //	Data members
 
-Int_t  fCase    ;		//! case 1=root,2=xdf,3=mdc2,4=daq
+
+   StFile         *fFileSet;    //!Chain of files
+   const char     *fNextFile;	//!next file from file set
+   StIOInterFace  *fCurrMk;	//!Pointer to Current Maker
+   StIOInterFace  *fFmtMk[9];	//!Pointers to TreeMaker,xdfin_Maker,St_io_Maker,StDAQMaker
+   Int_t  fMaxEvent;		//! for debug only
+   Int_t  fNumEvent;    	//! for debug only
+   Int_t  fCase    ;		//! case 1=root,2=xdf,3=mdc2,4=daq
 
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StIOMaker.h,v 1.4 1999/07/19 01:57:54 perev Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StIOMaker.h,v 1.5 1999/07/19 13:53:52 perev Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
    ClassDef(StIOMaker, 1)   //StAR chain virtual base class for Makers
 };
