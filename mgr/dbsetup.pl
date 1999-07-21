4284e6a6b67a665aca69e81a9f32ae1cb033fa57
Torre Wenaus <wenaus>
1999-07-21 09:19:08 +0000

    Add STAR environment variables

--- mgr/dbsetup.pl
+++ mgr/dbsetup.pl
@@ -1,8 +1,11 @@
 #!/opt/star/bin/perl -w
 #
-# $Id: dbsetup.pl,v 1.2 1999/07/07 13:22:11 wenaus Exp $
+# $Id: dbsetup.pl,v 1.3 1999/07/21 09:19:08 wenaus Exp $
 #
 # $Log: dbsetup.pl,v $
+# Revision 1.3  1999/07/21 09:19:08  wenaus
+# Add STAR environment variables
+#
 # Revision 1.2  1999/07/07 13:22:11  wenaus
 # incorporate run log
 #
@@ -20,7 +23,47 @@
 #
 
 use DBI;
+use File::Basename;
+use lib "/star/u2d/wenaus/datadb";
+use lib "/afs/rhic/star/group";
 
+## STAR environment setup
+$STAR_ROOT="/afs/rhic/star";
+$GROUP_DIR="$STAR_ROOT/group";
+$STAR_PATH="$STAR_ROOT/packages";
+if ( ! $STAR_LEVEL ) {$STAR_LEVEL="pro"}
+$STAR_VERSION = (fileparse(readlink("$STAR_PATH/$STAR_LEVEL")))[0];
+$STAR="$STAR_PATH/$STAR_VERSION";
+require "STAR_SYS.pl";  # define $STAR_HOST_SYS
+$STAR_LIB="$STAR/.$STAR_HOST_SYS/lib";
+$MINE_LIB=".$STAR_HOST_SYS/lib";
+$STAR_BIN="$STAR/.$STAR_HOST_SYS/bin";
+$STAR_MGR="$STAR/mgr";
+$STAR_PAMS="$STAR/pams";
+$STAR_DATA="$STAR_ROOT/data";
+$STAR_PARAMS="$STAR/params";
+$STAR_CALIB="$STAR_ROOT/calib";
+$STAR_PROD="$STAR/prod";
+$STAR_CVSROOT="$STAR_PATH/repository";
+$ROOT_LEVEL='';
+if ( -e "$STAR/mgr/ROOT_LEVEL" ) {$ROOT_LEVEL=`cat $STAR/mgr/ROOT_LEVEL`}
+$CERN_LEVEL='';
+if ( -e "$STAR/mgr/CERN_LEVEL" ) {$CERN_LEVEL=`cat $STAR/mgr/CERN_LEVEL`}
+$STAR_PATH="/usr/afsws/bin:/usr/afsws/etc:/opt/star/bin:/usr/sue/bin:/usr/local/bin:$GROUP_DIR:$STAR_MGR:$STAR_BIN";
+$CERN="/cern";
+$CERN_ROOT="$CERN/$CERN_LEVEL";
+
+my $systype = substr($STAR_HOST_SYS,0,4);
+if ( $systype eq 'sun4' ) {
+} elsif ( $systype eq 'i386' ) {
+    $STAR_PATH.=":/usr/local/bin/ddd";
+    $LD_LIBRARY_PATH="/usr/lib:/usr/local/lib:$MINE_LIB:$STAR_LIB:/usr/dt/lib:/usr/openwin/lib";
+} else {
+    $LD_LIBRARY_PATH="/opt/SUNWspro/lib:/usr/openwin/lib:/usr/dt/lib:/usr/local/lib:/afs/rhic/star/packages/ObjectSpace/2.0m/lib:$MINE_LIB:$STAR_LIB:/usr/lib";
+}
+$LD_LIBRARY_PATH.=":/usr/ccs/lib:/opt/star/lib";
+
+## Database setup
 $dbuser="root";
 $dbpass="octopus3";
 $dbname="";
