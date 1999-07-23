12e73c61af0ae33a7bb7f18da461cd58b25f7ff8
fine <>
1999-07-23 20:59:37 +0000

    Display Filter classes have been moved from StEventDisplayMAker to StEvent

---
+++ StRoot/StEvent/StVirtualEventFilter.cxx
@@ -0,0 +1,28 @@
+#include "StVirtualEventFilter.h"
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// StVirtualEventFilter virtual base class                              //
+//                                                                      //
+//                                                                      //
+//  Submit any problem with this code via begin_html <A HREF="http://www.star.bnl.gov/STARAFS/comp/sofi/bugs/send-pr.html"><B><I>"STAR Problem Report Form"</I></B></A> end_html
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+ClassImp(StVirtualEventFilter)
+//_____________________________________________________________________________
+Int_t StVirtualEventFilter::GetFlag(){ return m_ActiveFlag;}
+
+//_____________________________________________________________________________
+Int_t StVirtualEventFilter::Filter(StGlobalTrack *,Width_t &,Style_t &)
+{
+   static Int_t colorTrack = kRed;
+   colorTrack++;
+   if (colorTrack > 20) colorTrack = kRed;
+   return GetFlag() ? colorTrack : 0 ;
+}
+//_____________________________________________________________________________
+Int_t StVirtualEventFilter::Filter(const StObjArray *,Width_t &,Style_t &)
+{ return GetFlag()? kYellow : 0 ;}
+
+
