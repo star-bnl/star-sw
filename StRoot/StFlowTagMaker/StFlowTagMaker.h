2cbe5e919709baf988d0bf000cd6450b1197bbf0
Raimond J.M. Snellings <raimond.snellings@nikhef.nl>
1999-07-26 23:39:36 +0000

    added histograms for FlowTag QA

--- StRoot/StFlowTagMaker/StFlowTagMaker.h
+++ StRoot/StFlowTagMaker/StFlowTagMaker.h
@@ -17,9 +17,18 @@
 // History:
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <iostream.h>
+#include <iostream>
+#include <stdlib.h>
 #include "StMaker.h"
 #include "tables/FlowTag.h"
+#include "StEvent.h"
+#include "StGlobalTrack.h"
+#include "StChain/StChain.h"
+#include "TMath.h"
+#include "TString.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
 
 class StEvent;
 
@@ -29,25 +38,56 @@ class StFlowTagMaker : public StMaker
 public:
 
   StFlowTagMaker(const Char_t *name="FlowTag", const Char_t *title="FlowTag");
-  ~StFlowTagMaker();
-  Int_t  Make();
-  void   PrintInfo();
+  virtual ~StFlowTagMaker();
+  virtual Int_t Make();
+  virtual void PrintInfo();
+  virtual Int_t Init();
 
   FlowTag_st* tag(); // returns pointer to the tag table
 
-  // output Tag info to screen
-  void printTag(ostream& = cout);
 
 protected:
 
-  void fillFlowTag();
+  void  fillFlowTag();
   Int_t phiRapidityWeight(float PhiAngle, float PseudoRapidity, 
 			  float Pt, float *weight);
   Int_t eventPlane(long Multiplicity, float *mPseudoRapidity, float *mPhiAngle, 
 		   float *mPt, double *mQx, double *mQy, double *mEventPlaneAngle,
 		   int OrderParameter=2, int PhiYWeigt=0);
-  void SWAP(long &a,long &b);
-  void indexx(long n,float arr[], long indx[]);
+  void  swap(long &a,long &b);
+  void  indexx(long n,float arr[], long indx[]);
+  Int_t makeHistograms();
+  // output Tag info to screen
+  void  printTag(ostream& = cout);
+
+  // Histograms to check if code is working
+
+  // C++ way to define constants
+  enum {nHarmonics = 5, nSubEvents = 4};
+
+  // limits histograms
+  enum {nPhiBins = 100, nEtaBins = 100, nPtBins = 100}; 
+  enum {nPsiBins = 100, nSumPtBins = 100, nMultBins = 100, 
+	n_qBins = 100}; 
+
+  struct histHarmonic {
+    TH1F *mHistPsi;
+    TH1F *mHistFlowTagSumPt;
+    TH1D *mHistFlowTagMult;
+    TH1F *mHist_q;
+  };
+
+  struct histSubEvent;
+  friend struct histSubEvent;
+  struct histSubEvent {
+    TH1F *mHistPhi;
+    TH1F *mHistPseudoRapidity;
+    TH1F *mHistPt;
+    TProfile *mHistResolution;
+    struct histHarmonic histHarmonics[nHarmonics];
+  };
+
+  struct histSubEvent histSubEvents[nSubEvents]; //!
 
 private:
 
@@ -59,3 +99,12 @@ private:
 };
 
 #endif
+
+
+
+
+
+
+
+
+
