16184fb20a20f4c317eab327f1381f408a51a77c
Raimond J.M. Snellings <raimond.snellings@nikhef.nl>
1999-08-09 21:41:30 +0000

    added README file

--- StRoot/StFlowTagMaker/StFlowTagMaker.h
+++ StRoot/StFlowTagMaker/StFlowTagMaker.h
@@ -3,7 +3,8 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// StFlowTagMaker
+// StFlowTagMaker.h
+// $Id: StFlowTagMaker.h,v 1.1.1.2 1999/08/09 21:41:28 snelling Exp $
 //
 // Description: 
 //  Maker to access and analyze StEvent and fill Tag for flow analysis
@@ -15,39 +16,91 @@
 //  Raimond Snellings, LBNL, 6/99
 //
 // History:
+// $Log: StFlowTagMaker.h,v $
+// Revision 1.1.1.2  1999/08/09 21:41:28  snelling
+// added README file
+//
 //
 ///////////////////////////////////////////////////////////////////////////////
-#include <iostream.h>
+#include <iostream>
+#include <stdlib.h>
 #include "StMaker.h"
 #include "tables/FlowTag.h"
+#include "StEvent.h"
+#include "StGlobalTrack.h"
+#include "StChain/StChain.h"
+#include "TMath.h"
+#include "TString.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TProfile.h"
 
-class StEvent;
+// defined it here because I would like to have these
+// cuts in a header file however, I would like to have the
+// name only defined in this class (I do not know how to do this right)
+#ifdef onlyforStFlowTagMaker
+static const double trackquality[3][2] = {{10, 0}, 
+					  { 0, 0},
+					  { 0, 0}};
+#endif
 
 class StFlowTagMaker : public StMaker 
 {
 
 public:
 
-  StFlowTagMaker(const Char_t *name="FlowTag", const Char_t *title="FlowTag");
-  ~StFlowTagMaker();
-  Int_t  Make();
-  void   PrintInfo();
+  StFlowTagMaker(const Char_t *name="FlowTag");
+  virtual ~StFlowTagMaker();
+  virtual Int_t Make();
+  virtual void PrintInfo();
+  virtual Int_t Init();
 
   FlowTag_st* tag(); // returns pointer to the tag table
 
-  // output Tag info to screen
-  void printTag(ostream& = cout);
 
 protected:
 
-  void fillFlowTag();
+  void  fillFlowTag();
   Int_t phiRapidityWeight(float PhiAngle, float PseudoRapidity, 
 			  float Pt, float *weight);
   Int_t eventPlane(long Multiplicity, float *mPseudoRapidity, float *mPhiAngle, 
 		   float *mPt, double *mQx, double *mQy, double *mEventPlaneAngle,
 		   int OrderParameter=2, int PhiYWeigt=0);
-  void SWAP(long &a,long &b);
-  void indexx(long n,float arr[], long indx[]);
+  void  swap(long &a,long &b);
+  void  indexx(long n,float arr[], long indx[]);
+  Int_t makeHistograms();
+  // output Tag info to screen
+  void  printTag(ostream& = cout);
+
+  // Histograms to check if code is working
+
+  // C++ way to define constants
+  enum {nHarmonics = 5, nSubEvents = 4};
+
+  // limits histograms
+  enum {nPhiBins = 100, nEtaBins = 100, nPtBins = 100}; 
+  enum {nPsiBins = 100, nSumPtBins = 100, nMultBins = 100, 
+	n_qBins = 100}; 
+
+
+  struct histHarmonic {
+    TH1F *mHistPsi;
+    TH1F *mHistFlowTagSumPt;
+    TH1D *mHistFlowTagMult;
+    TH1F *mHist_q;
+  };
+
+  struct histSubEvent;
+  friend struct histSubEvent;
+  struct histSubEvent {
+    TH1F *mHistPhi;
+    TH1F *mHistPseudoRapidity;
+    TH1F *mHistPt;
+    TProfile *mHistResolution;
+    struct histHarmonic histHarmonics[nHarmonics];
+  };
+
+  struct histSubEvent histSubEvents[nSubEvents]; //!
 
 private:
 
@@ -59,3 +112,12 @@ private:
 };
 
 #endif
+
+
+
+
+
+
+
+
+
