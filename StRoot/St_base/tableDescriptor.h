a16a6e1e5f16fd88ab744e84608f3f5706f5b22d
Valeri Fine <val.fine@gmail.com>
1999-08-11 00:40:12 +0000

    new class St_tableDescriptor

---
+++ StRoot/St_base/tableDescriptor.h
@@ -0,0 +1,27 @@
+/* tableDescriptor.h */
+#ifndef TABLEDESCRIPTOR_H
+#define TABLEDESCRIPTOR_H
+#define TABLEDESCRIPTOR_SPEC   \
+"struct tableDescriptor {      \
+    char         *m_ColumnName;\
+    unsigned int m_Offset;     \
+    unsigned int m_Size;       \
+    unsigned int m_TypeSize;   \
+    unsigned int m_Dimensions; \
+    unsigned int* m_IndexArray;\
+    EColumnType  m_Type;       \
+};"
+
+enum EColumnType {kNAN, kFloat, kInt, kLong, kShort, kDouble, kUInt
+                      ,kULong, kUShort, kUChar, kChar };
+
+typedef struct tableDescriptor_st {
+    char        *m_ColumnName;  /* The name of this data-member                                          */
+    unsigned int m_Offset;      /* The first byte in the row of this column                              */
+    unsigned int m_Size;        /* The full size of the selected column in bytes                         */
+    unsigned int m_TypeSize;    /* The type size of the selected column in bytes                         */
+    unsigned int m_Dimensions;  /* The number of the dimensions for array                                */
+    unsigned int *m_IndexArray; /* The array of the sizes for each dimensions m_IndexArray[m_Dimensions] */
+    EColumnType  m_Type;        /* The data type of the selected column                                  */
+} TABLEDESCRIPTOR_ST;
+#endif /* TABLEDESCRIPTOR_H */
