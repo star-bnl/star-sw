3dc64ae8c537ed0a6740d0d3e6dd7a766a63fedb
Valeri Fine <val.fine@gmail.com>
1999-08-13 16:35:53 +0000

    The artificial restrictions for tableDescriptor have been introduced since database want that

--- StRoot/St_base/tableDescriptor.h
+++ StRoot/St_base/tableDescriptor.h
@@ -16,13 +16,27 @@
  * enum EColumnType {kNAN, kFloat, kInt, kLong, kShort, kDouble, kUInt
  *                     ,kULong, kUShort, kUChar, kChar };
  */
+
+/*  This is to introduce an artificial restriction demanded by STAR database group
+ *
+ *    1. the name may be 19 symbols at most
+ *    2. the number the dimension is 2 at mots
+ *
+ *  To lift this restriction one has to provide -DNORESTRICTIONS CPP symbol and
+ *  recompile code.
+ */
 typedef struct tableDescriptor_st {
-    char        *m_ColumnName;  /* The name of this data-member                                          */
+#ifdef NORESTRICTIONS
+    char         *m_ColumnName; /* The name of this data-member                                          */
+    unsigned int *m_IndexArray; /* The array of the sizes for each dimensions m_IndexArray[m_Dimensions] */
+#else
+    char         m_ColumnName[20];  /* The name of this data-member                                          */
+    unsigned int m_IndexArray[2];   /* The array of the sizes for each dimensions m_IndexArray[m_Dimensions] */
+#endif
     unsigned int m_Offset;      /* The first byte in the row of this column                              */
     unsigned int m_Size;        /* The full size of the selected column in bytes                         */
     unsigned int m_TypeSize;    /* The type size of the selected column in bytes                         */
     unsigned int m_Dimensions;  /* The number of the dimensions for array                                */
-    unsigned int *m_IndexArray; /* The array of the sizes for each dimensions m_IndexArray[m_Dimensions] */
     Int_t        m_Type;        /* The data type of the selected column                                  */
 } TABLEDESCRIPTOR_ST;
 #endif /* TABLEDESCRIPTOR_H */
