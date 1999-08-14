474ed20583688938354a74db55e076493d76f85d
Yuri Fisyak <fisyak@bnl.gov>
1999-08-14 00:41:04 +0000

    fix typo

--- StRoot/StEventDisplayMaker/StEventDisplayMakerLinkDef.h
+++ StRoot/StEventDisplayMaker/StEventDisplayMakerLinkDef.h
@@ -2,6 +2,6 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class SEventDisplayMaker;
+#pragma link C++ class StEventDisplayMaker;
 #pragma link C++ enum  EDisplayEvents;
 #endif
