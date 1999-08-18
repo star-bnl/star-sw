45006773062ba9bc42860068d4f16719d0dd1466
porter <>
1999-08-18 15:53:43 +0000

    initial tpc calibration structs in db

---
+++ StDb/include/Calibrations/tpcGainFactors.h
@@ -0,0 +1,18 @@
+//:Description: 
+//:Synonyms::::
+//:Source:
+//:Update:
+//:Update frequncy:
+//:Reminder:
+//:Recall frequency:
+//:Size of Data:
+//:Pointer to data:  gainFactors.time:
+struct tpcGainFactors {
+
+  //  type varnam;    //Units : Comments
+
+  int  sectorID;
+  long innerSectorGainFactors[1750];
+  long outerSectorGainFactors[3942];
+
+};
