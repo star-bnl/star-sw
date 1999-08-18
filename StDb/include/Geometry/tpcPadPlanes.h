366d3fd3225d232607fb2b198f5d779137e73f6f
porter <>
1999-08-18 15:54:14 +0000

    initial tpc geometry structs in db

---
+++ StDb/include/Geometry/tpcPadPlanes.h
@@ -0,0 +1,46 @@
+//:Description: 
+//:Synonyms::::
+//:Source:
+//:Update:
+//:Update frequncy:
+//:Reminder:
+//:Recall frequency:
+//:Size of Data:
+//:Pointer to data:  TPCPadplanes.time:
+struct tpcPadPlanes {
+  //  type varnam;    //Units : Comments
+
+
+    int    padRows;
+    int    innerPadRows;
+    int    innerPadRows48;
+    int    innerPadRows52;
+    int    outerPadRows;
+    int    superInnerPadRows;
+    int    superOuterPadRows;
+
+    double innerSectorPadWidth;
+    double innerSectorPadLength;
+    double innerSectorPadPitch;
+    double innerSectorRowPitch1;
+    double innerSectorRowPitch2;
+    double firstPadRow;
+    double firstOuterSectorPadRow;
+    double lastOuterSectorPadRow;
+    double firstRowWidth;
+    double lastRowWidth;
+    double outerSectorPadWidth;
+    double outerSectorPadLength;
+    double outerSectorPadPitch;
+    double outerSectorRowPitch;
+    double outerSectorLength;
+    double ioSectorSeparation;
+    double innerSectorEdge;
+    double outerSectorEdge;
+ 
+    int    innerPadsPerRow[13];
+    int    outerPadsPerRow[32];
+    double innerRowRadii[13];
+    double outerRowRadii[32];
+
+};
