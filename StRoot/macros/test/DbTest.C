102d823194f9a5e714b217460f6f11af7fb2ea29
R. Jefferson (Jeff) Porter <rjporter@lbl.gov>
1999-08-24 20:50:28 +0000

    Added & updated to the Db access example macros

--- StRoot/macros/test/DbTest.C
+++ StRoot/macros/test/DbTest.C
@@ -1,16 +1,16 @@
-#include "StRoot/StDbLib/Calibrations/tpcDriftVelocity.h"
-//
 void DbTest(){
   gSystem->Load("St_base"); 
+  gSystem->Load("StUtilities");  
   gSystem->Load("StChain");  
   StChain* mchain = new StChain("StDbTest");
   gSystem->Load("StDbLib.so"); 
   gSystem->Load("StDbMaker.so");  
+  gSystem->Load("StTpcDb.so"); 
 //
 // Construct with configuration="dbtest1"
 // that is a key in the StarDb::StarDbKeys table
 //
-  StDbMaker* mk = new StDbMaker("dbtest1","StarDb");
+  StDbMaker* mk = new StDbMaker("TrsES99","StarDb");
   mk->SetTime(1);
   mk->Init();
   mk->Make();
@@ -20,23 +20,38 @@ void DbTest(){
   St_DataSet* set2 = mk->GetData("StarDb");
   St_DataSet* set3 = set2->Find("Calibrations"); 
   St_DataSet* set4 = set3->Find("tpc");
-  St_DataSet* set5 = set4->Find("tpcDriftVelocity");
+  St_DataSet* set5 = set4->Find("tpcSlowControlSim");
 //
 // for purpose of handling with St_DataSet,
 // StDb_tpcDriftVelocity inherits from TObject
 // & can be put into an St_ObjectSet..
 //
-  StDb_tpcDriftVelocity* table=(StDb_tpcDriftVelocity*)set5->GetObject();
+  StDbTableI* table=((StDbDataSet*)set5)->GetDbTable();
 //
 // any user code must compile with the header file
-//  ... where to put it? now Calibrations subdir of StDbLib
+// tpcSlowControlSim* tpv = (tpcSlowControlSim*)table->GetTable();
 //
-  tpcDriftVelocity* tpd = table->getTable();
+  //
+  // Or write to an ascii file
+  // 
+ofstream of("mytpcSlowControlSim.xml");
+StDbXmlWriter* writer = new StDbXmlWriter(of);
+writer->streamTableName(table->getTableName());
 
-  cout << tpd->laserDriftVelocityEast << endl;
-  cout << tpd->cathodeDriftVelocityEast << endl;
-  cout << tpd->cathodeDriftVelocityWest << endl;
-//
+ // optional ... provides the db-address
+
+writer->streamAccessor();
+table->StreamAccessor(writer); 
+writer->endAccessor();
+
+table->dbStreamer(writer);
+writer->streamEndTableName();
+
 }
+
+
+
+
+
 
 
