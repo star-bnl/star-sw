cbf69354660320e65f5fc98b02cbfa97d8860351
Raimond J.M. Snellings <raimond.snellings@nikhef.nl>
1999-09-03 16:07:37 +0000

    Added method to write out histogrmas and TNtuple

--- StRoot/StQACosmicMaker/StQACosmicMaker.h
+++ StRoot/StQACosmicMaker/StQACosmicMaker.h
@@ -2,13 +2,16 @@
 #define StQACosmicMaker_HH
 /***************************************************************************
  *
- * $Id: StQACosmicMaker.h,v 1.5 1999/08/26 03:36:58 snelling Exp $
+ * $Id: StQACosmicMaker.h,v 1.6 1999/09/03 16:07:37 snelling Exp $
  *
  * Author: Raimond Snellings, LBNL, Jun 1999
  * Description:  Maker to QA the Cosmic data (hitfinding, tracking, 
  *               geometry etc.)
  *
  * $Log: StQACosmicMaker.h,v $
+ * Revision 1.6  1999/09/03 16:07:37  snelling
+ * Added method to write out histogrmas and TNtuple
+ *
  * Revision 1.5  1999/08/26 03:36:58  snelling
  * Added Q versus pad
  *
@@ -31,30 +34,64 @@
 #include "TH2.h"
 #include "TNtuple.h"
 #include "TProfile.h"
+#include "TString.h"
 
 class StQACosmicMaker : public StMaker {
+
+ public:
+
+  StQACosmicMaker(const char *name="QACosmics");
+  virtual        ~StQACosmicMaker();
+
+  virtual Int_t  Init();
+  virtual Int_t  Make();
+  virtual Int_t  Finish();
+  virtual void   PrintInfo();
+
+  virtual void   setSector(const Int_t sectorNumber);
+  virtual void   setNrXbins(const Int_t bins) {nXBins = bins;}
+
+  virtual void   WriteTNtupleOn() {WriteTNtuple(kTRUE);}
+  virtual void   WriteTNtupleOff(){WriteTNtuple();}
+  virtual void   WritePostscriptOn() {WritePostscript(kTRUE);} 
+  virtual void   WritePostscriptOff(){WritePostscript();}
+  virtual void   WriteHistogramsOn() {WriteHistograms(kTRUE);} 
+  virtual void   WriteHistogramsOff(){WriteHistograms();}
+
+  virtual const char *GetCVS() const
+  {static const char cvs[]="Tag $Name:  $ $Id: StQACosmicMaker.h,v 1.6 1999/09/03 16:07:37 snelling Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
  private:
 
   Int_t SelectedSector;
   Bool_t bSectorSelectionOn;
   Bool_t bWriteTNtupleOn;
+  Bool_t bWriteHistogramsOn;
   Bool_t bWritePostscriptOn;
   Int_t nXBins;
+  TString MakerName;
 
-  virtual void   SectorSelection(Bool_t flag=kFALSE){bSectorSelectionOn=flag;}
-  virtual void   SectorSelectionOn() {SectorSelection(kTRUE);} 
-  virtual void   SectorSelectionOff(){SectorSelection();}
   virtual Int_t  initResHistograms();
   virtual Int_t  fillResHistograms();
   virtual Int_t  calcResHistograms();
   virtual Int_t  initChargeHistograms();
   virtual Int_t  fillChargeHistograms();
   virtual Int_t  calcChargeHistograms();
+  virtual Int_t  writeOutHistograms();
+  virtual Int_t  writeOutPostscript();
+
   virtual Int_t  initTNtuple();
   virtual Int_t  fillTNtuple();
+  virtual Int_t  writeOutTNtuple();
+
+  // bool selection
   virtual void   WritePostscript(Bool_t flag=kFALSE){bWritePostscriptOn=flag;}
   virtual void   WriteTNtuple(Bool_t flag=kFALSE){bWriteTNtupleOn=flag;}
+  virtual void   WriteHistograms(Bool_t flag=kFALSE){bWriteHistogramsOn=flag;}
+  virtual void   SectorSelection(Bool_t flag=kFALSE){bSectorSelectionOn=flag;}
+  virtual void   SectorSelectionOn() {SectorSelection(kTRUE);} 
+  virtual void   SectorSelectionOff(){SectorSelection();}
+
 
  protected:
 
@@ -94,41 +131,14 @@ class StQACosmicMaker : public StMaker {
 
   struct ChargeHist {
     TH2F *mQdist;
+    TProfile *mQprof;
     struct FitQHist FitQHists; 
   };
 
   // histograms for q versus x,y,z
   struct ChargeHist ChargeHists[nChargeHist]; //! 
 
- public:
-
-  StQACosmicMaker(const char *name="QACosmics");
-  virtual        ~StQACosmicMaker();
-
-  virtual Int_t  Init();
-  virtual Int_t  Make();
-  virtual Int_t  Finish();
-  virtual void   PrintInfo();
-
-  virtual void   setSector(const Int_t sectorNumber);
-  virtual void   setNrXbins(const Int_t bins) {nXBins = bins;}
-
-  virtual void   WriteTNtupleOn() {WriteTNtuple(kTRUE);}
-  virtual void   WriteTNtupleOff(){WriteTNtuple();}
-  virtual void   WritePostscriptOn() {WritePostscript(kTRUE);} 
-  virtual void   WritePostscriptOff(){WritePostscript();}
-
-
-  virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StQACosmicMaker.h,v 1.5 1999/08/26 03:36:58 snelling Exp $ built "__DATE__" "__TIME__ ; return cvs;}
-
   ClassDef(StQACosmicMaker, 1) //macro for rootcint
 };
 
 #endif
-
-
-
-
-
-
