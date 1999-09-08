b814f72c1fee1b4c4d97c8faaa6cbff93d97b54c
Yuri Fisyak <fisyak@bnl.gov>
1999-09-08 00:44:20 +0000

    CleanUP

--- mgr/stic.pl
+++ mgr/stic.pl
@@ -1,5 +1,6 @@
 #! /usr/local/bin/perl
 #use File::Basename;
+use Cwd;
 use File::Copy;
 #print "stic @ARGV\n";
 my $dir = shift;
@@ -9,10 +10,13 @@ while (my $par = shift) {
   if ($par =~ /Root|ROOT|ObjectSpace/) {next;} 
   push @pars, $par;
 }
+my $Dir = cwd();
+my $idl = pop @pars;# print "get $idl\n";
+if ($idl !~ /^\//) {$idl = $Dir . "/" . $idl;}# print "Full name $idl\n";
 my $tmpdir = "/tmp/" . $$;# print "tmpdir = $tmpdir \n";
 if (! -d $dir    && !mkdir ($dir,    0755)) {die "$0:Can't create directory $dir\n";};
 if (! -d $tmpdir && !mkdir ($tmpdir, 0755)) {die "$0:Can't create directory $tmpdir\n";};
-my $com = "cd $tmpdir && stic @pars";# print "=========================\n$com\n";
+my $com = "cd $tmpdir && stic @pars $idl";# print "=========================\n$com\n";
 my $exec = `$com`; if ($exec) { exit $exec; }
 my @files = ($stem . ".h",$stem . ".inc","St_" . $stem . "_Module.h","St_" . $stem . "_Module.cxx");
 foreach my $file (@files){
