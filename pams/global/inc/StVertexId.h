3639adc6251cb76f62d8fa761e1f558f2a26e62f
margetis <>
1999-09-09 14:54:45 +0000

    Add kUndefinedVertexIdentifier=0 after Thomas request

--- pams/global/inc/StVertexId.h
+++ pams/global/inc/StVertexId.h
@@ -1,9 +1,10 @@
 #include "StVertexDefinitions.h"
 enum StVertexId {
-  kEventVtxId  = kEventVertexIdentifier,
-  kV0VtxId     = kV0DecayIdentifier,
-  kXiVtxId     = kXiDecayIdentifier,
-  kKinkVtxId   = kKinkDecayIdentifier,
-  kOtherVtxId  = kOtherTypeIdentifier
+  kUndefinedVtxId = kUndefinedVertexIdentifier,
+  kEventVtxId     = kEventVertexIdentifier,
+  kV0VtxId        = kV0DecayIdentifier,
+  kXiVtxId        = kXiDecayIdentifier,
+  kKinkVtxId      = kKinkDecayIdentifier,
+  kOtherVtxId     = kOtherTypeIdentifier
 };
 
