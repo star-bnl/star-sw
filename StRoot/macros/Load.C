fd2e309b8a37ad2c35f7a558cb4fdeff9cdeafdd
Yuri Fisyak <fisyak@bnl.gov>
1999-09-09 13:52:38 +0000

    Add StBFChain

--- StRoot/macros/Load.C
+++ StRoot/macros/Load.C
@@ -1,5 +1,8 @@
-// $Id: Load.C,v 1.6 1999/05/21 15:33:50 kathy Exp $
+// $Id: Load.C,v 1.7 1999/09/09 13:52:38 fisyak Exp $
 // $Log: Load.C,v $
+// Revision 1.7  1999/09/09 13:52:38  fisyak
+// Add StBFChain
+//
 // Revision 1.6  1999/05/21 15:33:50  kathy
 // made sure Log & Id are in each file and also put in standard comment line with name of owner
 //
@@ -11,6 +14,7 @@
 void Load(){
     gSystem->Load("St_base");
     gSystem->Load("StChain");
+    gSystem->Load("StBFChain");
     gSystem->Load("xdf2root");
     gSystem->Load("St_Tables");
 }
