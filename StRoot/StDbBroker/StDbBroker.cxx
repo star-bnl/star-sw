e886c3ad10c6a2472ed5f7388df83478b139362d
Victor Perevoztchikov <perev@bnl.gov>
1999-09-14 15:16:38 +0000

    Low-level interface to MySQL params database

---
+++ StRoot/StDbBroker/StDbBroker.cxx
@@ -0,0 +1,130 @@
+#include <iostream.h>
+#include <iomanip.h>
+#include <stdlib.h> 
+
+#include <strstream.h>
+
+#include "TString.h"
+
+//needed for GetComments
+#include "TROOT.h"
+#include "TBuffer.h"
+#include "TClass.h"
+#include "St_Table.h"
+#include "TRealData.h"
+#include "TDataMember.h"
+#include "TDataType.h"
+
+#include "Api.h"
+#include "StDbBroker.h"
+
+ClassImp(StDbBroker)
+//______________________________________________________________________________
+//the only remaining St_Table dependence is in this function 
+char **StDbBroker::GetComments(St_Table *parentTable)
+{
+  char **ElementComment = new char*[m_nElements]; 
+  if (!parentTable) {
+    //    MakeZombie();
+    return NULL;
+  }
+  
+  TClass *classPtr = parentTable->GetRowClass();
+  if (!classPtr) return NULL;
+  
+  if (!classPtr->GetListOfRealData()) classPtr->BuildRealData();
+  
+  TIter next(classPtr->GetListOfDataMembers());
+  TDataMember *member = 0;
+  UInt_t i=0, j=0;
+  while ( (member = (TDataMember *) next()) ) {
+    ElementComment[i] = strdup(member->GetTitle());
+ // strip trailing blanks from Comments (they are stripped in mysql anyway)
+    j=strlen(ElementComment[i]);
+    while (j != 0  && ElementComment[i][j-1] == ' ') j--;
+    ElementComment[i][j]='\0';
+    i++;
+  }
+  return ElementComment;
+}
+//______________________________________________________________________________
+// int StDbBroker::Init(const char *dbname)
+// {
+//   return DbInit(dbname);
+// }
+//______________________________________________________________________________
+
+void StDbBroker::Fill(void * pArray, const char **Comments)
+{
+  if ( m_nElements==0 ) return;
+  //char **Comments = new char*[m_nElements]; 
+  //TString Comment;
+
+  UInt_t i;
+  for (i=0;i<m_nElements;i++) {
+    
+    if(m_descriptor[i].dimensions>1)
+      {
+	cerr<<"dim>1, can't handle yet"<<endl;
+	return;
+      }
+    
+    m_descriptor[i].name[19]='\0';
+  }
+  
+  UInt_t date, time;
+  //VP TDatime::GetDateTime(m_DateTime, date, time);
+  date = m_DateTime[0]; time = m_DateTime[1];
+  uint datetime[4]={0,0,0,0};
+  datetime[0]=date;
+  datetime[1]=time;
+  
+  ::DbFill(datetime, (const char*) m_tableName, (const char*) m_structName, m_nElements,m_descriptor,Comments,m_nRows,m_sizeOfStruct,pArray);
+  
+  delete [] Comments;
+}  
+//______________________________________________________________________________
+void * StDbBroker::Use()
+{
+//convert event datetime to date and time
+  UInt_t date, time;
+//VP TDatime::GetDateTime(m_DateTime, date, time);
+  date = m_DateTime[0]; time = m_DateTime[1];
+  uint datetime[4]={0,0,0,0};
+  datetime[0]=date;
+  datetime[1]=time;
+  uint nRows=0;
+
+  UInt_t i;
+  for (i=0;i<m_nElements;i++) {
+      m_descriptor[i].name[19]='\0';
+  }
+
+void *pDbData = ::DbUse(&nRows, datetime, (const char*)m_tableName,(const char*)m_structName,m_nElements,m_sizeOfStruct,m_descriptor);
+  
+//cout << "found nRows "<<nRows<<" of " << m_structName<<" structure"<<endl;
+
+  if (pDbData==NULL)
+    {
+      SetNRows(0);
+      SetBeginDate(19950101);
+      SetBeginTime(0);
+      SetEndDate(20380101);
+      SetEndTime(0);
+    }
+  else
+    {
+      SetNRows((UInt_t)nRows);
+      SetBeginDate((UInt_t)datetime[0]);
+      SetBeginTime((UInt_t)datetime[1]);
+      SetEndDate((UInt_t)datetime[2]);
+      SetEndTime((UInt_t)datetime[3]);
+    }
+
+  return pDbData;
+}
+//______________________________________________________________________________
+
+
+
+
---
+++ StRoot/StDbBroker/StDbBroker.h
@@ -0,0 +1,119 @@
+//  
+
+#ifndef STAR_StDbBroker
+#define STAR_StDbBroker
+
+#include "Rtypes.h"
+
+/* needed for GetComments only */
+class St_Table;
+/*this is a temporary quick-and-dirty class for db access*/
+class StDbBroker  {
+  public:
+      enum EColumnType {kNAN, kFloat, kInt, kLong, kShort, kDouble, kUInt
+                             ,kULong, kUShort, kUChar, kChar };
+     struct Descriptor{
+     char name[20];		//variable name 
+     int firstDimension;		//first dimension, if this is an array
+     int secondDimension;	//second dimension
+     int offset;			//variable offset
+     int size;			//total size of element
+     int typeSize;		//unit size
+     int dimensions;		//number of dimensions
+     EColumnType type;		//type of element
+     };
+
+  protected:
+    Descriptor  *m_descriptor;
+    Char_t *     m_structName;  //name of the struct type used in this St_Table
+    Char_t *     m_tableName;   //name of this instance of St_Table
+    UInt_t       m_sizeOfStruct;// byte size of this struct
+    UInt_t       m_nElements;   // Number of variables in the structure
+    UInt_t       m_nRows;       // number of rows in the table
+
+    UInt_t       m_DateTime[2]; // Current DateTime
+
+    UInt_t       m_BeginDate;   // begin date: 2021 05 31
+    UInt_t       m_BeginTime;   // begin date: HH MM SS
+    UInt_t       m_EndDate;     // end date
+    UInt_t       m_EndTime;     // end time
+
+  public:
+    StDbBroker(){}
+
+    virtual ~StDbBroker(){}
+    
+    //    int Init(const char *dbname);// return 0 if OK
+
+    void * Use();
+
+    char  **GetComments(St_Table *parentTable);
+    void   Fill(void * pArray, const char **ElementComment);
+
+    UInt_t GetNRows()                {return m_nRows;       }
+    UInt_t GetBeginDate()            {return m_BeginDate;   }
+    UInt_t GetBeginTime()            {return m_BeginTime;   }
+    UInt_t GetEndDate()              {return m_EndDate;     }
+    UInt_t GetEndTime()              {return m_EndTime;     }
+
+    static const Char_t * GetTypeName( EColumnType type) {
+      switch (type)
+	{
+	case kFloat:  return "float";
+	case kInt:    return "int";
+	case kLong:   return "long";
+	case kShort:  return "short";
+	case kDouble: return "double";
+	case kChar:   return "char";
+	  //temporarily
+	case kUInt:   return "int";
+	case kULong:  return "long";
+	case kUShort: return "short";
+	case kUChar:  return "char";
+
+	case kNAN:    return "";
+	default:      return "";
+	} 
+    };
+
+    void   SetDateTime(UInt_t date,UInt_t time)
+                                   {m_DateTime[0] = date; m_DateTime[1]= time;}
+
+    void   SetDictionary(UInt_t nElements, Descriptor *D)
+                                     {m_nElements=nElements; m_descriptor = D;}
+
+    void   SetTableName(const Char_t *table_name)
+                                          {m_tableName=strdup(table_name);};
+    void   SetStructName(const Char_t *struct_name)
+                                          {m_structName=strdup(struct_name);};
+    void   SetStructSize(UInt_t size)     {m_sizeOfStruct=size;    };
+    void   SetNRows(UInt_t nRows)         {m_nRows = nRows;        }
+    void   SetBeginDate(UInt_t BeginDate) {m_BeginDate = BeginDate;}
+    void   SetBeginTime(UInt_t BeginTime) {m_BeginTime = BeginTime;}
+    void   SetEndDate(UInt_t EndDate)     {m_EndDate = EndDate;    }
+    void   SetEndTime(UInt_t EndTime)     {m_EndTime = EndTime;    }
+
+    ClassDef(StDbBroker,0)
+};
+
+extern "C" void DbFill(uint *,         //datetime[4]
+		       const char *,  //tableName
+		       const char *,  //StructName
+		       uint,           //nVar
+		       StDbBroker::Descriptor *d,
+		       const char **,       //Comments
+		       uint,           //nRows
+		       uint,           //sizeOfStruct
+		       void *);       //pData
+		       
+extern "C" void *DbUse(uint*,           //&nRows,
+		       uint *,         //datetime[4]
+		       const char *,  //tableName
+		       const char *,  //StructName
+		       uint,           //nVar
+		       uint,           //sizeOfStruct
+		       StDbBroker::Descriptor *d);
+
+extern "C"  int DbInit(const char *);  //dbName
+
+#endif
