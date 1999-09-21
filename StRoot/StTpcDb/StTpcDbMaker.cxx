7f20d16484458ffe804146fabdd1c35a5fa8f2ae
hardtke <>
1999-09-21 20:14:35 +0000

    create tpg_pad_plane from database

--- StRoot/StTpcDb/StTpcDbMaker.cxx
+++ StRoot/StTpcDb/StTpcDbMaker.cxx
@@ -9,6 +9,7 @@
 #include "StChain.h"
 #include "St_DataSetIter.h"
 #include "StTpcDb.h"
+#include "tpc/St_tpg_main_Module.h"
 ClassImp(StTpcDbMaker)
 
 //_____________________________________________________________________________
@@ -27,15 +28,48 @@ Int_t StTpcDbMaker::Init(){
    StDbDataSet* DB = (StDbDataSet*)temp->Find("StarDb");
    assert(DB);
    m_TpcDb = new StTpcDb(DB);
-// Create Histograms    
+// Create Needed Tables:    
+   m_tpg_pad_plane = new St_tpg_pad_plane("tpg_pad_plane",1);
+   m_tpg_pad_plane->SetNRows(1);
+   AddConst(m_tpg_pad_plane);
+   m_tpg_detector = new St_tpg_detector("tpg_detector",1);
+   m_tpg_detector->SetNRows(1);
+   AddConst(m_tpg_detector);
+//
    return StMaker::Init();
 }
 //_____________________________________________________________________________
 
 Int_t StTpcDbMaker::Make(){
- return kStOK;
+  Update_tpg_pad_plane();
+  Update_tpg_detector();
+  return kStOK;
 }
 
-StTpcDb* StTpcDbMaker::tpcDbInterface(){
- return m_TpcDb;
+void StTpcDbMaker::Update_tpg_pad_plane(){
+ tpg_pad_plane_st *pp = m_tpg_pad_plane->GetTable();
+  m_tpg_pad_plane->SetNRows(1);
+  pp->nrow_in = tpcDbInterface()->PadPlaneGeometry()->numberOfInnerRows();
+  pp->nrow_out = tpcDbInterface()->PadPlaneGeometry()->numberOfOuterRows();
+  pp->pad_len_in = tpcDbInterface()->PadPlaneGeometry()->innerSectorPadLength();
+  pp->pad_len_out = tpcDbInterface()->PadPlaneGeometry()->outerSectorPadLength();
+  pp->pad_sep_in = tpcDbInterface()->PadPlaneGeometry()->innerSectorPadPitch();
+  pp->pad_sep_out = tpcDbInterface()->PadPlaneGeometry()->outerSectorPadPitch();
+  pp->pad_wid_in = tpcDbInterface()->PadPlaneGeometry()->innerSectorPadWidth();
+  pp->pad_wid_out = tpcDbInterface()->PadPlaneGeometry()->outerSectorPadWidth();
+  pp->nsect = tpcDbInterface()->Dimensions()->numberOfSectors();
+  for (int i=1;i<=tpcDbInterface()->PadPlaneGeometry()->numberOfRows();i++){
+   pp->npads[i-1] = tpcDbInterface()->PadPlaneGeometry()->numberOfPadsAtRow(i);
+   pp->rad[i-1] = tpcDbInterface()->PadPlaneGeometry()->radialDistanceAtRow(i);
+  }
 }
+
+void StTpcDbMaker::Update_tpg_detector(){
+ tpg_detector_st *pp = m_tpg_detector->GetTable();
+ m_tpg_detector->SetNRows(1);
+ 
+    pp->nsectors = tpcDbInterface()->Dimensions()->numberOfSectors();
+ //    m_tpg_detector->drift_length = 0.
+   pp->clock_frequency = tpcDbInterface()->Electronics()->samplingFrequency();
+ }
+
--- StRoot/StTpcDb/StTpcDbMaker.h
+++ StRoot/StTpcDb/StTpcDbMaker.h
@@ -12,21 +12,29 @@
 #include "StMaker.h"
 #endif
 class StTpcDb;
+class St_tpg_pad_plane;
+class St_tpg_detector;
 class StTpcDbMaker : public StMaker {
  private:
-  StTpcDb* m_TpcDb;               //! tpc database class 
+  StTpcDb* m_TpcDb;               //! tpc database class
+  St_tpg_pad_plane* m_tpg_pad_plane; //!
+  St_tpg_detector* m_tpg_detector; //! 
  protected:
  public: 
                   StTpcDbMaker(const char *name="TLA");
    virtual       ~StTpcDbMaker();
    virtual Int_t Init();
    virtual Int_t  Make();
-   virtual StTpcDb* tpcDbInterface();    //! return m_TpcDb
+   virtual void Update_tpg_pad_plane();
+   virtual void Update_tpg_detector();
+   virtual StTpcDb* tpcDbInterface() const;    //! return m_TpcDb
 // virtual void Set_mode       (Int_t   m =      2){m_mode       = m;} // *MENU*
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StTpcDbMaker.h,v 1.1 1999/09/16 18:47:24 hardtke Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StTpcDbMaker.h,v 1.2 1999/09/21 20:14:35 hardtke Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
    ClassDef(StTpcDbMaker, 1)   //StAF chain virtual base class for Makers
 };
+
+inline StTpcDb* StTpcDbMaker::tpcDbInterface() const {return m_TpcDb;}
 
 #endif
