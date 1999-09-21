0f7b0505e880f51719576bd0aa10a159291dfb2e
Torre Wenaus <wenaus>
1999-09-21 12:18:14 +0000

    Data area moved to RCF NFS

--- mgr/stats.pl
+++ mgr/stats.pl
@@ -1,8 +1,11 @@
 #!/opt/star/bin/perl
 #
-# $Id: stats.pl,v 1.1 1999/08/18 13:07:03 wenaus Exp $
+# $Id: stats.pl,v 1.2 1999/09/21 12:18:14 wenaus Exp $
 #
 # $Log: stats.pl,v $
+# Revision 1.2  1999/09/21 12:18:14  wenaus
+# Data area moved to RCF NFS
+#
 # Revision 1.1  1999/08/18 13:07:03  wenaus
 # Computing statistics web page
 #
@@ -40,15 +43,15 @@ print "</ul>\n";
 
 print "<h3>Environment setups</h3>\n<table border=0 cellspacing=12><tr>";
 print "<td valign=top><h4>By level</h4><pre>\n";
-print `cat /star/datapool/web/swstat-envlevel.txt`.
+print `cat /star/starlib/doc/www/html/comp-nfs/swstat-envlevel.txt`.
     "</pre></td>";
 
 print "<td valign=top><h4>By version</h4><pre>\n";
-print `cat /star/datapool/web/swstat-envver.txt`.
+print `cat /star/starlib/doc/www/html/comp-nfs/swstat-envver.txt`.
     "</pre></td>";
 
 print "<td valign=top><h4>By site</h4><pre>\n";
-print `cat /star/datapool/web/swstat-envsites.txt`.
+print `cat /star/starlib/doc/www/html/comp-nfs/swstat-envsites.txt`.
     "</pre></td>";
 
 print "<td valign=top><h4><a href=\"/webdata/swstat-envhosts.txt\">By machine</a></h4></td>\n";
@@ -59,15 +62,15 @@ print "</tr></table>\n";
 
 print "<h3>root4star invocations (since early Aug '99)</h3>\n<table border=0 cellspacing=12><tr>";
 print "<td valign=top><h4>By level</h4><pre>\n";
-print `cat /star/datapool/web/swstat-rootlevel.txt`.
+print `cat /star/starlib/doc/www/html/comp-nfs/swstat-rootlevel.txt`.
     "</pre></td>";
 
 print "<td valign=top><h4>By version</h4><pre>\n";
-print `cat /star/datapool/web/swstat-rootver.txt`.
+print `cat /star/starlib/doc/www/html/comp-nfs/swstat-rootver.txt`.
     "</pre></td>";
 
 print "<td valign=top><h4>By site</h4><pre>\n";
-print `cat /star/datapool/web/swstat-rootsites.txt`.
+print `cat /star/starlib/doc/www/html/comp-nfs/swstat-rootsites.txt`.
     "</pre></td>";
 
 print "<td valign=top><h4><a href=\"/webdata/swstat-roothosts.txt\">By machine</a></h4></td>\n";
@@ -76,7 +79,7 @@ print "<td valign=top><h4><a href=\"/webdata/swstat-rootusers.txt\">By user</a><
 print "</tr></table>\n";
 
 print "<h3>Source code statistics</h3>\<blockquote><pre>";
-print `cat /star/datapool/web/swguide-stats.txt`."</pre></blockquote>\n";
+print `cat /star/starlib/doc/www/html/comp-nfs/swguide-stats.txt`."</pre></blockquote>\n";
 
 print "<h3><a href=\"http://www.star.bnl.gov/cvs/userCommits.html\">Commits by user</a></h3>\n";
 
