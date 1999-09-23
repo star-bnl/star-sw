c43f640fc3047a0f5a11944b6ea11457b71bde9d
Valeri Fine <val.fine@gmail.com>
1999-09-23 18:31:32 +0000

    CVS id adjusted

--- StRoot/St_base/St_NodeViewIter.h
+++ StRoot/St_base/St_NodeViewIter.h
@@ -1,6 +1,9 @@
 //*-- Author :    Valery Fine(fine@bnl.gov)   25/01/99  
-//  
-// 
+// $Id: St_NodeViewIter.h,v 1.8 1999/09/23 18:31:32 fine Exp $
+// $Log: St_NodeViewIter.h,v $
+// Revision 1.8  1999/09/23 18:31:32  fine
+// CVS id adjusted
+//
 
 #ifndef STAR_St_NodeViewIter
 #define STAR_St_NodeViewIter
