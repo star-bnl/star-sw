ac27b387b3cf455e44af0a593a93c06fb6a816b9
fine <>
1999-09-24 17:25:04 +0000

    Missing StMicky.h

---
+++ StRoot/St_baseTest/StMicky.h
@@ -0,0 +1,20 @@
+//*-- Author :    Valery Fine(fine@bnl.gov)   25/09/99  
+//  This class is to provide "micky" test for RMath.h methods
+//  derived from CERNLIB 
+//  http://wwwinfo.cern.ch/asdoc/shortwrupsdir/f110/top.html 
+// $Id: StMicky.h,v 1.1 1999/09/24 17:25:04 fine Exp $
+// $Log: StMicky.h,v $
+// Revision 1.1  1999/09/24 17:25:04  fine
+// Missing StMicky.h
+//
+// Revision 1.1  1999/09/23 18:33:10  fine
+// test system for RMath class has been introduced
+//
+
+// #include "Rtypes.h"
+
+ class StMicky {
+   public:
+     StMicky();
+ };
+
