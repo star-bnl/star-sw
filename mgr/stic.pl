592643884cd307b44cf1217ecb082bc54ab19a5a
fisyak <>
1999-09-24 01:11:04 +0000

    Reduced Include Path

--- mgr/stic.pl
+++ mgr/stic.pl
@@ -1,15 +1,16 @@
 #! /usr/local/bin/perl
-#use File::Basename;
+use File::Basename;
 use Cwd;
 use File::Copy;
 #print "stic @ARGV\n";
-my $dir = shift;
-my $stem = shift;
+my $target = shift;
 my @pars = ();
 while (my $par = shift) {
   if ($par =~ /Root|ROOT|ObjectSpace/) {next;} 
   push @pars, $par;
 }
+my $file = basename($target); #print "file = $file\n";
+my $dir = dirname($target);   #print "dir = $dir\n";
 my $Dir = cwd();
 my $idl = pop @pars;# print "get $idl\n";
 if ($idl !~ /^\//) {$idl = $Dir . "/" . $idl;}# print "Full name $idl\n";
@@ -18,10 +19,6 @@ if (! -d $dir    && !mkdir ($dir,    0755)) {die "$0:Can't create directory $dir
 if (! -d $tmpdir && !mkdir ($tmpdir, 0755)) {die "$0:Can't create directory $tmpdir\n";};
 my $com = "cd $tmpdir && stic @pars $idl";# print "=========================\n$com\n";
 my $exec = `$com`; if ($exec) { exit $exec; }
-my @files = ($stem . ".h",$stem . ".inc","St_" . $stem . "_Module.h","St_" . $stem . "_Module.cxx");
-foreach my $file (@files){
-  my $sfile = $tmpdir . "/" . $file;
-  my $dfile = $dir    . "/" . $file;
-  if (-f $sfile) {copy ($sfile,$dfile);}
-}
+my $sfile = $tmpdir . "/" . $file;# print "sfile $sfile\n";
+if (-f $sfile) {copy ($sfile,$target);}
 exit `rm -rf $tmpdir`;
