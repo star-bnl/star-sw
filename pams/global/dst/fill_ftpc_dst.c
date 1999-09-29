fa3bd1bbbaa984ed9fd118ba3d0bf8fe7fb5b356
Spyridon Margetis <smargeti@kent.edu>
1999-09-29 15:42:17 +0000

    commit Janets changes in fill-ftpc-dst.c and .idl

--- pams/global/dst/fill_ftpc_dst.c
+++ pams/global/dst/fill_ftpc_dst.c
@@ -4,6 +4,8 @@
 #include <math.h>
 /* ------- STAF/ROOT generated includes ------- */
 #include "fill_ftpc_dst.h"
+#include "StDetectorId.h"
+#include "StVertexId.h"
 
 #define TWO4   16                    /* 2**4  */
 #define TWO10  1024                  /* 2**10 */
@@ -31,7 +33,6 @@
 long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptrack,
   TABLE_HEAD_ST  *fppoint_h,           FCL_FPPOINT_ST        *fppoint,
   TABLE_HEAD_ST  *dst_track_h,         DST_TRACK_ST          *dst_track,
-  TABLE_HEAD_ST  *dst_track_aux_h,     DST_TRACK_AUX_ST      *dst_track_aux,
   TABLE_HEAD_ST  *dst_point_h,         DST_POINT_ST          *dst_point,
   TABLE_HEAD_ST  *dst_vertex_h,        DST_VERTEX_ST         *dst_vertex,
   TABLE_HEAD_ST  *dst_dedx_h,          DST_DEDX_ST           *dst_dedx)
@@ -56,8 +57,6 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
    *:       OUT:
    *:             dst_track             - DST tracks table       
    *:             dst_track_h           - Header Structure for dst_track
-   *:             dst_track_aux         - DST auxillary tracks table       
-   *:             dst_track_aux_h       - Header Structure for dst_track_aux
    *:             dst_point             - DST points table       
    *:             dst_point_h           - Header Structure for dst_point
    *:             dst_vertex            - DST vertex table
@@ -69,7 +68,9 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
    *: RETURNS:    STAF Condition Value
    *:
    *: HISTORY:    
-   *:      Dec 18, 1998    Janet Seyboth  Original
+   *:      July 27, 1999    Janet Seyboth  remove dst_track_aux table
+   *:                                      use StVertexId.h and StDetectorId.h
+   *:      Dec  18, 1998    Janet Seyboth  Original
    *:
    *:>-------------------------------------------------------------------- 
    */
@@ -89,7 +90,7 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
   /* Locate primary vertex  */
   ivtx_prim = -1;
   for (i=0; i<dst_vertex_h->nok; i++) {
-    if (dst_vertex[i].vtx_id = 1 ){
+    if (dst_vertex[i].vtx_id == kEventVtxId  && dst_vertex[i].iflag == 1){
        ivtx_prim = i;
        break;
     } 
@@ -102,10 +103,13 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
           return STAFCV_BAD;
     }
     else {
+
 /*  Primary key */
     dst_track[dst_track_h->nok].id      = dst_track_h->nok + 1;
+
 /* bitmask quality information */
     dst_track[dst_track_h->nok].iflag   = 0;
+
 /*  Locate first hit on current track and determine detector id  */
      for (ihit=0; ihit<MAXHITS; ihit++) {
         if (fptrack[itrk].hitid[ihit] > -1){
@@ -117,31 +121,57 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
                            fppoint[iPoint].y;
            dst_track[dst_track_h->nok].x_first[2]    = 
                            fppoint[iPoint].z ;
-           /*   Rows 1->10 FTPC West  det_id=4  */
+           /*   Rows 1->10 FTPC West  det_id= kFtpcWestId  */
            if (fppoint[iPoint].row >= 1 && fppoint[iPoint].row <= 10) {
-               dst_track[dst_track_h->nok].det_id  = 4;    /* West */
+               dst_track[dst_track_h->nok].det_id  = kFtpcWestId;    /* West */
            }
-           /*   Rows 11->20 FTPC East  det_id=5  */
+           /*   Rows 11->20 FTPC East  det_id=kFtpcEastId  */
            else if (fppoint[iPoint].row >= 11 && fppoint[iPoint].row <=20) {
-               dst_track[dst_track_h->nok].det_id  = 5;   /* East */
+               dst_track[dst_track_h->nok].det_id  = kFtpcEastId;   /* East */
            }
            break;
         }
       }
+
+/*  Track finding and track fitting method */
+/*  (Currently undefined )                 */
+    dst_track[dst_track_h->nok].method = 0;
+
+/*  Geant particle ID number for mass hypothesis used in tracking */
+/*   (Currently not set for FTPC)                                 */
+    dst_track[dst_track_h->nok].pid    = 0;
+
 /*  Number of points  */
     dst_track[dst_track_h->nok].n_point = fptrack[itrk].nrec;
+
 /*  Maximum number of points */
     dst_track[dst_track_h->nok].n_max_point  = MAXHITS;
+
 /*  Number of points used in fit */
     dst_track[dst_track_h->nok].n_fit_point  = fptrack[itrk].nfit;
+
 /*  Charge */
     dst_track[dst_track_h->nok].icharge      = fptrack[itrk].q;
-/*  x-coordinate at start of track  */
-    dst_track[dst_track_h->nok].x0           = fptrack[itrk].v[0];
-/*  y-coordinate at start of track  */
-    dst_track[dst_track_h->nok].y0           = fptrack[itrk].v[1];
+
+/*  Initialized to zero by FTPC dst filler module   */
+    dst_track[dst_track_h->nok].id_start_vertex  = 0;
+
+
+/*  map  (currently not set for FTPC)                        */
+    dst_track[dst_track_h->nok].map[0]                = 0;
+    dst_track[dst_track_h->nok].map[1]                = 0;
+
+/*  radius at start of track (cm) */
+/*  (currently not set for FTPC)  */
+    dst_track[dst_track_h->nok].r0           = 0.;
+
+/*  azimuthal angle at start of track (deg) */
+/*  (currently not set for FTPC)  */
+    dst_track[dst_track_h->nok].phi0           = 0.;
+
 /*  z-coordinate at start of track  */
     dst_track[dst_track_h->nok].z0           = fptrack[itrk].v[2];
+
 /*  momentum angle at start */
     dst_track[dst_track_h->nok].psi = 
         atan2(fptrack[itrk].p[2],fptrack[itrk].p[1]);
@@ -149,24 +179,39 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
        dst_track[dst_track_h->nok].psi = 
                     dst_track[dst_track_h->nok].psi*C_2_PI;
     }
+
 /*  tan(dip) = pz/pt at start  */
     dst_track[dst_track_h->nok].tanl  = fptrack[itrk].p[2]/
                    sqrt(fptrack[itrk].p[1]*fptrack[itrk].p[1]
                         + fptrack[itrk].p[3]*fptrack[itrk].p[3]);
+
 /*  1/pt at start */
     dst_track[dst_track_h->nok].invpt =  
       1./sqrt(fptrack[itrk].p[1]*fptrack[itrk].p[1]
                 +fptrack[itrk].p[2]*fptrack[itrk].p[2]);
-/*  covariance matrix, diagonal elements */
-    dst_track[dst_track_h->nok].covar_diag[0] = 0;
-    dst_track[dst_track_h->nok].covar_diag[1] = 0;
-    dst_track[dst_track_h->nok].covar_diag[2] = 0;
-    dst_track[dst_track_h->nok].covar_diag[3] = 0;
-    dst_track[dst_track_h->nok].covar_diag[4] = 0;
-    dst_track[dst_track_h->nok].covar_diag[5] = 0;
+
+/*  covariance matrix */
+/*  (currently not set for FTPC)  */
+    dst_track[dst_track_h->nok].covar[0] = 0;
+    dst_track[dst_track_h->nok].covar[1] = 0;
+    dst_track[dst_track_h->nok].covar[2] = 0;
+    dst_track[dst_track_h->nok].covar[3] = 0;
+    dst_track[dst_track_h->nok].covar[4] = 0;
+    dst_track[dst_track_h->nok].covar[5] = 0;
+    dst_track[dst_track_h->nok].covar[6] = 0;
+    dst_track[dst_track_h->nok].covar[7] = 0;
+    dst_track[dst_track_h->nok].covar[8] = 0;
+    dst_track[dst_track_h->nok].covar[9] = 0;
+    dst_track[dst_track_h->nok].covar[10] = 0;
+    dst_track[dst_track_h->nok].covar[11] = 0;
+    dst_track[dst_track_h->nok].covar[12] = 0;
+    dst_track[dst_track_h->nok].covar[13] = 0;
+    dst_track[dst_track_h->nok].covar[14] = 0;
+
 /*  chi-sqare fit */
     dst_track[dst_track_h->nok].chisq[0]      = fptrack[itrk].chisq[0];
     dst_track[dst_track_h->nok].chisq[1]      = fptrack[itrk].chisq[1];
+
 /*  Locate last hit on current track     */
      for (ihit=MAXHITS-1; ihit>=0; ihit--) {
         if (fptrack[itrk].hitid[ihit] > -1) {
@@ -181,53 +226,14 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
           break;
         }
      }
+
     xsq = (dst_track[dst_track_h->nok].x_last[0] - dst_track[dst_track_h->nok].x_first[0]) *
           (dst_track[dst_track_h->nok].x_last[0] - dst_track[dst_track_h->nok].x_first[0]);
     ysq = (dst_track[dst_track_h->nok].x_last[1] - dst_track[dst_track_h->nok].x_first[1]) *
           (dst_track[dst_track_h->nok].x_last[1] - dst_track[dst_track_h->nok].x_first[1]);
     dst_track[dst_track_h->nok].length  = sqrt(xsq + ysq);
+
     dst_track[dst_track_h->nok].impact  = 0;
-    dst_track[dst_track_h->nok].ndegf   = 0;              
-    dst_track[dst_track_h->nok].id_global_pid         = 0; 
-    dst_track[dst_track_h->nok].id_hypo_pid           = 0;
-/*  COMMENT OUT FOR MDC2   */
-/* CURRENTLY treating ALL ftpc tracks as if they are primary tracks */
-/*    if (ivtx_prim >= 0 ) {                               MDC2 */
-/*       dst_track[dst_track_h->nok].id_start_vertex       =   MDC2 */
-/*                    dst_vertex[ivtx_prim].id;            MDC2 */
-/* Increase number of charged daughter tracks for primary vertex */
-/*       dst_vertex[ivtx_prim].n_daughters = dst_vertex[ivtx_prim].n_daughters++;                                                          MDC2 */
-/*    }                                                    MDC2 */
-/*    else {                                               MDC2 */
-        dst_track[dst_track_h->nok].id_start_vertex  = 0;
-/*    }                                                    MDC2 */
-/*  COMMENT OUT FOR MDC2                                   MDC2 */
-    dst_track[dst_track_h->nok].id_stop_vertex        = 0;
-    dst_track[dst_track_h->nok].id_emc                = 0;
-    dst_track[dst_track_h->nok].id_smd                = 0;
-
-/*  Fill dst auxillary track table */
-
-    if (dst_track_aux_h->nok>dst_track_aux_h->maxlen-1) {
-      fprintf(stderr,"FILL_FTPC_DST: Too many auxilliary dst tracks...exiting.\n");
-          return STAFCV_BAD;
-    }
-    else {
-      dst_track_aux[dst_track_aux_h->nok].id_track  = dst_track_h->nok + 1;
-      dst_track_aux[dst_track_aux_h->nok].residuals[0] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].residuals[1] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[0] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[1] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[2] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[3] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[4] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[5] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[6] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[7] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[8] = 0.0;
-      dst_track_aux[dst_track_aux_h->nok].covar_off_diag[9] = 0.0;
-      dst_track_aux_h->nok++;
-     }      /* End of filling dst auxillary track table */
 
 /*  Fill dst point table for current track */
 
@@ -240,7 +246,7 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
          /* hitid array filled by FORTRAN routine, must -1 for C routine */
          iPoint =  fptrack[itrk].hitid[ihit] - 1;
 /*       hw_position  (32 bits)                                             */
-/*                    bits  0-3   det_id  (4 = FTPC West, 5 = FTPC East     */
+/*                    bits  0-3   det_id                                    */
 /*                    bits 4-10   FTPC pad plane (1-20)                     */
 /*                    bits 11-20  Sector number within pad-plane (1-6)      */
 /*                    bits 21-24  number of pads in cluster                 */
@@ -319,7 +325,7 @@ long  type_of_call fill_ftpc_dst_(TABLE_HEAD_ST *fptrack_h, FPT_FPTRACK_ST *fptr
     else {
       dst_dedx[dst_dedx_h->nok].id_track = dst_track[dst_track_h->nok].id;
       dst_dedx[dst_dedx_h->nok].det_id = dst_track[dst_track_h->nok].det_id;
-      dst_dedx[dst_dedx_h->nok].iflag = 0;
+      dst_dedx[dst_dedx_h->nok].method = 0;
       dst_dedx[dst_dedx_h->nok].ndedx = fptrack[itrk].ndedx;
       dst_dedx[dst_dedx_h->nok].dedx[0] = fptrack[itrk].dedx;
       dst_dedx[dst_dedx_h->nok].dedx[1] = 0;
