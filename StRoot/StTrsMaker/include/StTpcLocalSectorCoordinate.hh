cbb9f47263cb7dc2b835ff4b5b6541ffea765b4d
Hui Long <long@physics.ucla.edu>
1999-10-04 16:05:59 +0000

    change mVolumeId to mFromSector

--- StRoot/StTrsMaker/include/StTpcLocalSectorCoordinate.hh
+++ StRoot/StTrsMaker/include/StTpcLocalSectorCoordinate.hh
@@ -1,6 +1,6 @@
 /*********************************************************************
  *
- * $Id: StTpcLocalSectorCoordinate.hh,v 1.1 1999/01/28 02:48:12 lasiuk Exp $
+ * $Id: StTpcLocalSectorCoordinate.hh,v 1.2 1999/10/04 16:05:59 long Exp $
  *
  * Author: brian Jan 26, 1999
  *
@@ -11,6 +11,12 @@
  **********************************************************************
  *
  * $Log: StTpcLocalSectorCoordinate.hh,v $
+ * Revision 1.2  1999/10/04 16:05:59  long
+ * change mVolumeId to mFromSector
+ *
+ * Revision 1.2  1999/10/01 17:15:00 Hui Long
+ * replace mVolumeId by fromSector
+ *
  * Revision 1.1  1999/01/28 02:48:12  lasiuk
  * Initial Revision
  *
@@ -35,18 +41,22 @@ public:
     
     // access functions provided by StThreeVector
     const StThreeVector<double>& pos()  const;
-    int   volumeId()                    const;
+    //  int   volumeId()                    const;
+     int  fromSector()                       const;//HL
     // To Modify Coordinates
     StThreeVector<double>& pos();
 
 private:
     StThreeVector<double> mPos;
-    int                   mVolumeId;
+  // int                   mVolumeId;
+     int                   mFromSector;
+    
 };
 
 inline const StThreeVector<double>& StTpcLocalSectorCoordinate::pos() const { return(mPos); }
 inline StThreeVector<double>& StTpcLocalSectorCoordinate::pos() { return(mPos); }
-inline int StTpcLocalSectorCoordinate::volumeId() const { return(mVolumeId); }
+//inline int StTpcLocalSectorCoordinate::volumeId() const { return(mVolumeId); }
+inline int StTpcLocalSectorCoordinate::fromSector() const { return(mFromSector); }//HL
 // Non-member
 ostream& operator<<(ostream&, const StTpcLocalSectorCoordinate&);
 #endif
