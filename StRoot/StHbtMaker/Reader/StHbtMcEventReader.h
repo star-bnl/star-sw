f1eedb18f3bcd5ec683001c5c0f2dcd1d2ba2783
lisa <>
1999-10-05 11:37:42 +0000

    Helens realistic V0Cut and Franks memory-sealed McReader

--- StRoot/StHbtMaker/Reader/StHbtMcEventReader.h
+++ StRoot/StHbtMaker/Reader/StHbtMcEventReader.h
@@ -2,32 +2,66 @@
 #define StHbtMcEventReader_hh
 
 #include <ctime>
-#include "StHbtMaker/Base/StHbtEventReader.hh"
-//#include "StChain.h"
 #include "StMaker.h"
+#include "StHbtMaker/Base/StHbtEventReader.hh"
+#include "StV0MiniDstMaker/StV0MiniDstMaker.h"
+#include "StHbtMaker/Base/StHbtEventCut.hh"
+#include "StHbtMaker/Base/StHbtTrackCut.hh"
+#include "StHbtMaker/Base/StHbtV0Cut.hh"
 
+class TOrdCollection;
 class StHbtMcEventReader : public StHbtEventReader{
-
-private:
+  
+ private:
+  // pointers to other makers
   StMaker* mTheMcEventMaker;      //! this is the chain where the StEventReaderMaker is
-  time_t timeStamp;
+  StV0MiniDstMaker* mTheV0Maker;  //! this is the chain where the StV0MiniDstMaker is
 
-public:
+  // pointers to front-loaded cuts
+  StHbtEventCut* mEventCut; //!
+  StHbtTrackCut* mTrackCut; //!
+  StHbtV0Cut*    mV0Cut;    //!
+
+  long              mV0;        //! Number of v0s looked at to date
+  time_t timeStamp; // to display the time/event
+
+ protected:
+  TOrdCollection *mCollection; //!
+  
+ public:
   StHbtMcEventReader();
-  //  ~StHbtMcEventReader();
-
+  ~StHbtMcEventReader();
+  
   virtual StHbtEvent* ReturnHbtEvent();
   virtual StHbtString Report();
-
-
-  void SetTheMcEventMaker(StMaker*);
+  
+  // sets and gets for the other makers
+  void SetTheMcEventMaker(StMaker*); 
+  void SetTheV0Maker(StV0MiniDstMaker*);
   StMaker* TheMcEventMaker();
+  StV0MiniDstMaker* TheV0Maker();
 
-  ClassDef(StHbtMcEventReader, 1)
+  // sets and gets for the front-loaded cuts
+  void SetEventCut(StHbtEventCut*);
+  void SetTrackCut(StHbtTrackCut*);
+  void SetV0Cut(StHbtV0Cut*);
+  StHbtEventCut* EventCut();
+  StHbtTrackCut* TrackCut();
+  StHbtV0Cut*    V0Cut();
 
+  //  ClassDef(StHbtMcEventReader, 1)
 };
-
+    
 inline void StHbtMcEventReader::SetTheMcEventMaker(StMaker* mcMaker){mTheMcEventMaker=mcMaker;}
+inline void StHbtMcEventReader::SetTheV0Maker(StV0MiniDstMaker* maker){mTheV0Maker=maker;}
 inline StMaker* StHbtMcEventReader::TheMcEventMaker(){return mTheMcEventMaker;}
+inline StV0MiniDstMaker* StHbtMcEventReader::TheV0Maker(){return mTheV0Maker;}
+
+inline void StHbtMcEventReader::SetEventCut(StHbtEventCut* ecut){mEventCut=ecut;}
+inline void StHbtMcEventReader::SetTrackCut(StHbtTrackCut* tcut){mTrackCut=tcut;}
+inline void StHbtMcEventReader::SetV0Cut(StHbtV0Cut* v0cut){mV0Cut=v0cut;}
+inline StHbtEventCut* StHbtMcEventReader::EventCut(){return mEventCut;}
+inline StHbtTrackCut* StHbtMcEventReader::TrackCut(){return mTrackCut;}
+inline StHbtV0Cut*    StHbtMcEventReader::V0Cut(){return mV0Cut;}
 
 #endif
