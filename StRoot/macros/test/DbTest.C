dcf5379b796610373578ef96126bb4a483331c89
porter <>
1999-10-19 14:50:37 +0000

    Modified example to use St_db_Maker with StDbLib & StDbBroker

--- StRoot/macros/test/DbTest.C
+++ StRoot/macros/test/DbTest.C
@@ -1,52 +1,35 @@
 void DbTest(){
-  gSystem->Load("St_base"); 
-  gSystem->Load("StUtilities");  
-  gSystem->Load("StChain");  
-  StChain* mchain = new StChain("StDbTest");
-  gSystem->Load("StDbLib.so"); 
-  gSystem->Load("StDbMaker.so");  
-  gSystem->Load("StTpcDb.so"); 
-//
-// Construct with configuration="dbtest1"
-// that is a key in the StarDb::StarDbKeys table
-//
-  StDbMaker* mk = new StDbMaker("TrsES99","StarDb");
-  mk->SetTime(1);
-  mk->Init();
-  mk->Make();
-//
-// could do this with an St_DataSetIter....
-//
-  St_DataSet* set2 = mk->GetData("StarDb");
-  St_DataSet* set3 = set2->Find("Calibrations"); 
-  St_DataSet* set4 = set3->Find("tpc");
-  St_DataSet* set5 = set4->Find("tpcSlowControlSim");
-//
-// for purpose of handling with St_DataSet,
-// StDb_tpcDriftVelocity inherits from TObject
-// & can be put into an St_ObjectSet..
-//
-  StDbTableI* table=((StDbDataSet*)set5)->GetDbTable();
-//
-// any user code must compile with the header file
-// tpcSlowControlSim* tpv = (tpcSlowControlSim*)table->GetTable();
-//
-  //
-  // Or write to an ascii file
-  // 
-ofstream of("mytpcSlowControlSim.xml");
-StDbXmlWriter* writer = new StDbXmlWriter(of);
-writer->streamTableName(table->getTableName());
 
- // optional ... provides the db-address
+  // Baseline shared libraries
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+  gSystem->Load("St_Tables");
 
-writer->streamAccessor();
-table->StreamAccessor(writer); 
-writer->endAccessor();
+  // DB-specific libs
 
-table->dbStreamer(writer);
-writer->streamEndTableName();
+  gSystem->Load("StDbLib");
+  gSystem->Load("StDbBroker"); 
+  gSystem->Load("St_db_Maker");
 
+  // create makers connecting to databases RunParams & Geometry
+
+  St_db_Maker *dbMk = new St_db_Maker("RunParams","MySQL:RunParams");
+  St_db_Maker *dbMk2 = new St_db_Maker("Geometry","MySQL:Geometry");
+
+  // Initial with [database]_hierarchy structure known to St_db_Maker
+
+  dbMk->Init();
+  dbMk2->Init();
+
+  // Make reaquests for data
+
+  p = dbMk->GetDataBase("RunParams/tpc");
+  p2 = dbMk2->GetDataBase("Geometry/tpc");
+
+  // Browse these requests
+
+  TBrowser *b = new TBrowser("RunParams",p);
+  TBrowser *b2 = new TBrowser("Geometry",p2);
 }
 
 
