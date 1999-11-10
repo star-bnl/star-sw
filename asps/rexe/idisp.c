8b5b35ebbf4889fafae5d5a29862187482b44d35
fisyak <>
1999-11-10 23:57:26 +0000

    tidy

--- asps/rexe/idisp.c
+++ asps/rexe/idisp.c
@@ -1,9 +1,9 @@
 /*-- Author :    Pavel Nevski   01/09/99*/
-int idisp0_ (a,b)   char  *a,*b;     { return  b-a;    }
-int idisp1_ (a,b)   char  *a,*b;     { return (b-a)+1; }
-int idisp2_ (a,b)   short *a,*b;     { return (b-a)+1; }
-int idisp4_ (a,b)   int   *a,*b;     { return (b-a)+1; }
-int iponter_(a,b)   int   *a,**b;    { return (*b-a);  }
+int idisp0_ (char  *a, char  *b) { return  b-a;    }
+int idisp1_ (char  *a, char  *b) { return (b-a)+1; }
+int idisp2_ (short *a, short *b) { return ((b-a)) + 1; }
+int idisp4_ (int   *a, int   *b) { return ((b-a)) + 1; }
+int iponter_(int   *a, int  **b) { return (*b-a); } 
 
 #ifdef PrivateMalloc
 #include <stdlib.h>
