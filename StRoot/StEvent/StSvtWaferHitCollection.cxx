c161c7e736fc0daa307b503e39689489570fc352
Thomas Ullrich <thomas.ullrich@bnl.gov>
1999-11-11 21:19:38 +0000

    Delete hits explicitly in destructor

--- StRoot/StEvent/StSvtWaferHitCollection.cxx
+++ StRoot/StEvent/StSvtWaferHitCollection.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StSvtWaferHitCollection.cxx,v 2.1 1999/10/13 19:45:22 ullrich Exp $
+ * $Id: StSvtWaferHitCollection.cxx,v 2.2 1999/11/11 21:19:35 ullrich Exp $
  *
  * Author: Thomas Ullrich, Sep 1999
  ***************************************************************************
@@ -10,22 +10,35 @@
  ***************************************************************************
  *
  * $Log: StSvtWaferHitCollection.cxx,v $
- * Revision 2.1  1999/10/13 19:45:22  ullrich
- * Initial Revision
+ * Revision 2.2  1999/11/11 21:19:35  ullrich
+ * Delete hits explicitly in destructor
  *
  * Revision 2.1  1999/10/13 19:45:22  ullrich
  * Initial Revision
  *
  **************************************************************************/
 #include "StSvtWaferHitCollection.h"
+#include "StSvtHit.h"
 
-static const char rcsid[] = "$Id: StSvtWaferHitCollection.cxx,v 2.1 1999/10/13 19:45:22 ullrich Exp $";
+static const char rcsid[] = "$Id: StSvtWaferHitCollection.cxx,v 2.2 1999/11/11 21:19:35 ullrich Exp $";
 
 ClassImp(StSvtWaferHitCollection)
 
 StSvtWaferHitCollection::StSvtWaferHitCollection() { /* noop */ }
 
-StSvtWaferHitCollection::~StSvtWaferHitCollection() { /* noop */ }
+StSvtWaferHitCollection::~StSvtWaferHitCollection()
+{
+    //
+    // Usually this wouldn't be necessary but mHits
+    // is a polymorphic container and StTpcHit  
+    // provides its own new/delete operator.
+    //
+    for (unsigned int i=0; i<mHits.size(); i++) {
+	delete mHits[i];
+	mHits[i] = 0;
+    }
+}
+
 
 const StSPtrVecSvtHit&
 StSvtWaferHitCollection::hits() const { return mHits; }
