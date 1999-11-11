ebac4e64aec3d3fa0fcf03c3a38b336be3350c5a
ullrich <>
1999-11-11 21:19:38 +0000

    Delete hits explicitly in destructor

--- StRoot/StEvent/StFtpcSectorHitCollection.cxx
+++ StRoot/StEvent/StFtpcSectorHitCollection.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StFtpcSectorHitCollection.cxx,v 2.1 1999/10/13 19:44:42 ullrich Exp $
+ * $Id: StFtpcSectorHitCollection.cxx,v 2.2 1999/11/11 21:19:33 ullrich Exp $
  *
  * Author: Thomas Ullrich, July 1999
  ***************************************************************************
@@ -10,23 +10,34 @@
  ***************************************************************************
  *
  * $Log: StFtpcSectorHitCollection.cxx,v $
- * Revision 2.1  1999/10/13 19:44:42  ullrich
- * Initial Revision
+ * Revision 2.2  1999/11/11 21:19:33  ullrich
+ * Delete hits explicitly in destructor
  *
  * Revision 2.1  1999/10/13 19:44:42  ullrich
  * Initial Revision
  *
  **************************************************************************/
 #include "StFtpcSectorHitCollection.h"
+#include "StFtpcHit.h"
 
-static const char rcsid[] = "$Id: StFtpcSectorHitCollection.cxx,v 2.1 1999/10/13 19:44:42 ullrich Exp $";
+static const char rcsid[] = "$Id: StFtpcSectorHitCollection.cxx,v 2.2 1999/11/11 21:19:33 ullrich Exp $";
 
 ClassImp(StFtpcSectorHitCollection)
 
 StFtpcSectorHitCollection::StFtpcSectorHitCollection() { /* noop */ }
 
 StFtpcSectorHitCollection::~StFtpcSectorHitCollection()
-{ /* noop */ }
+{
+    //
+    // Usually this wouldn't be necessary but mHits
+    // is a polymorphic container and StTpcHit  
+    // provides its own new/delete operator.
+    //
+    for (unsigned int i=0; i<mHits.size(); i++) {
+	delete mHits[i];
+	mHits[i] = 0;
+    }
+}
 
 const StSPtrVecFtpcHit&
 StFtpcSectorHitCollection::hits() const { return mHits; }
