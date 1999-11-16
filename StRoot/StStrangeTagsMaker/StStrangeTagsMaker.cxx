4569f1dfad04f33daec7977f456938aa88b5c961
Gene Van Buren <gene@bnl.gov>
1999-11-16 19:20:44 +0000

    Modified for StEvent 2.0

--- StRoot/StStrangeTagsMaker/StStrangeTagsMaker.cxx
+++ StRoot/StStrangeTagsMaker/StStrangeTagsMaker.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StStrangeTagsMaker.cxx,v 1.11 1999/09/24 01:23:26 fisyak Exp $
+ * $Id: StStrangeTagsMaker.cxx,v 1.12 1999/11/16 19:20:44 genevb Exp $
  *
  * Author: Gene Van Buren, Feb 1999
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StStrangeTagsMaker.cxx,v $
+ * Revision 1.12  1999/11/16 19:20:44  genevb
+ * Modified for StEvent 2.0
+ *
  * Revision 1.11  1999/09/24 01:23:26  fisyak
  * Reduced Include Path
  *
@@ -49,7 +52,7 @@
 #include "StV0Vertex.h"
 #include "StXiVertex.h"
 #include "StChain.h"
-#include "TMath.h"
+#include <math.h>
 #include "PhysicalConstants.h"
 
 ClassImp(StStrangeTagsMaker)
@@ -97,7 +100,10 @@ StrangeTag_st* StStrangeTagsMaker::tag()
 
 void StStrangeTagsMaker::fillTag()
 {
-    Int_t v0tot = 0;
+    StSPtrVecV0Vertex& v0Vertices = mEvent->v0Vertices();
+    Int_t v0tot = v0Vertices.size();
+    StSPtrVecXiVertex& xiVertices = mEvent->xiVertices();
+    Int_t castot = xiVertices.size();
     Int_t nbelowK0 = 0;
     Int_t nK0 = 0;
     Int_t naboveK0 = 0;
@@ -111,63 +117,62 @@ void StStrangeTagsMaker::fillTag()
     Int_t nXi = 0;
     Int_t naboveXi = 0;
 
-    for (StVertexIterator vertices = mEvent->vertexCollection()->begin();
-                    vertices != mEvent->vertexCollection()->end(); vertices++) {
-      if ( (*vertices)->type() == V0 ) {
-        v0tot++;
-        StV0Vertex *vertex = (StV0Vertex *) *vertices;
-        const StThreeVectorF& nMom = vertex->momentumOfDaughter(negativeTrack);
-        const StThreeVectorF& pMom = vertex->momentumOfDaughter(positiveTrack);
+    unsigned int i;
+
+    for (i=0; i<v0tot; i++) {
+        StV0Vertex *vertex = v0Vertices[i];
+        const StThreeVectorF& nMom = vertex->momentumOfDaughter(negative);
+        const StThreeVectorF& pMom = vertex->momentumOfDaughter(positive);
         StThreeVectorF vMom = nMom + pMom;
         Float_t pN2 = nMom.mag2();
         Float_t pP2 = pMom.mag2();
         Float_t pV2 = vMom.mag2();
-        Float_t eNpi = TMath::Sqrt(pN2 + mMasspi2);
-        Float_t eNpr = TMath::Sqrt(pN2 + mMasspr2);
-        Float_t ePpi = TMath::Sqrt(pP2 + mMasspi2);
-        Float_t ePpr = TMath::Sqrt(pP2 + mMasspr2);
+        Float_t eNpi = sqrt(pN2 + mMasspi2);
+        Float_t eNpr = sqrt(pN2 + mMasspr2);
+        Float_t ePpi = sqrt(pP2 + mMasspi2);
+        Float_t ePpr = sqrt(pP2 + mMasspr2);
         Float_t eK0 = eNpi + ePpi;
         Float_t eLa = eNpi + ePpr;
         Float_t eLb = eNpr + ePpi;
-        Float_t maK0 = TMath::Sqrt(eK0*eK0 - pV2);
-        Float_t maLa = TMath::Sqrt(eLa*eLa - pV2);
-        Float_t maLb = TMath::Sqrt(eLb*eLb - pV2);
+        Float_t maK0 = sqrt(eK0*eK0 - pV2);
+        Float_t maLa = sqrt(eLa*eLa - pV2);
+        Float_t maLb = sqrt(eLb*eLb - pV2);
 
         Float_t perc = (maK0/kaon_0_short_mass_c2) - 1.;
-        if (TMath::Abs(perc) < mRange) nK0++;
-        else if (TMath::Abs(perc + m2Range) < mRange) nbelowK0++;
-        else if (TMath::Abs(perc - m2Range) < mRange) naboveK0++;
+        if (abs(perc) < mRange) nK0++;
+        else if (abs(perc + m2Range) < mRange) nbelowK0++;
+        else if (abs(perc - m2Range) < mRange) naboveK0++;
 
         perc = (maLa/lambda_mass_c2) - 1.;
-        if (TMath::Abs(perc) < mRange) nLa++;
-        else if (TMath::Abs(perc + m2Range) < mRange) nbelowLa++;
-        else if (TMath::Abs(perc - m2Range) < mRange) naboveLa++;
+        if (abs(perc) < mRange) nLa++;
+        else if (abs(perc + m2Range) < mRange) nbelowLa++;
+        else if (abs(perc - m2Range) < mRange) naboveLa++;
 
         perc = (maLb/antilambda_mass_c2) - 1.;
-        if (TMath::Abs(perc) < mRange) nLb++;
-        else if (TMath::Abs(perc + m2Range) < mRange) nbelowLb++;
-        else if (TMath::Abs(perc - m2Range) < mRange) naboveLb++;
+        if (abs(perc) < mRange) nLb++;
+        else if (abs(perc + m2Range) < mRange) nbelowLb++;
+        else if (abs(perc - m2Range) < mRange) naboveLb++;
 
-      } else if ( (*vertices)->type() == Xi ) {
+    }
+    for (i=0; i<castot; i++) {
 
-        StXiVertex *vertex = (StXiVertex *) *vertices;
+        StXiVertex *vertex = xiVertices[i];
         const StThreeVectorF& pMom = vertex->momentumOfBachelor();
         StThreeVectorF lMom = vertex->momentumOfV0();
         StThreeVectorF xMom = lMom + pMom;
         Float_t pP2 = pMom.mag2();
         Float_t pL2 = lMom.mag2();
         Float_t pX2 = xMom.mag2();
-        Float_t epi = TMath::Sqrt(pP2 + mMasspi2);
-        Float_t ela = TMath::Sqrt(pL2 + mMassla2);
+        Float_t epi = sqrt(pP2 + mMasspi2);
+        Float_t ela = sqrt(pL2 + mMassla2);
         Float_t eXi = ela + epi;
-        Float_t maXi = TMath::Sqrt(eXi*eXi - pX2);
+        Float_t maXi = sqrt(eXi*eXi - pX2);
 
         Float_t perc = (maXi/xi_minus_mass_c2) - 1.;
-        if (TMath::Abs(perc) < mRange) nXi++;
-        else if (TMath::Abs(perc + m2Range) < mRange) nbelowXi++;
-        else if (TMath::Abs(perc - m2Range) < mRange) naboveXi++;
+        if (abs(perc) < mRange) nXi++;
+        else if (abs(perc + m2Range) < mRange) nbelowXi++;
+        else if (abs(perc - m2Range) < mRange) naboveXi++;
 
-      }
     }
 
     mTagTable->NV0 = v0tot;
