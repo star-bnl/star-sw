c263060f74f95c717f4ba34268cf3d2814a05686
fine <>
1999-11-24 18:51:22 +0000

    all StVirtual::Filter have been renamed to Channel

--- StRoot/St_base/StVirtualEventFilter.cxx
+++ StRoot/St_base/StVirtualEventFilter.cxx
@@ -27,7 +27,7 @@ ClassImp(StVirtualEventFilter)
 Int_t StVirtualEventFilter::GetFlag(){ return m_ActiveFlag;}
 
 //_____________________________________________________________________________
-Int_t StVirtualEventFilter::Filter(StGlobalTrack *,Width_t &,Style_t &)
+Int_t StVirtualEventFilter::Channel(StGlobalTrack *,Width_t &,Style_t &)
 {
    static Int_t colorTrack = kRed;
    colorTrack++;
@@ -35,17 +35,17 @@ Int_t StVirtualEventFilter::Filter(StGlobalTrack *,Width_t &,Style_t &)
    return GetFlag() ? colorTrack : 0 ;
 }
 //_____________________________________________________________________________
-Int_t StVirtualEventFilter::Filter(const StObjArray *,Width_t &,Style_t &)
+Int_t StVirtualEventFilter::Channel(const StObjArray *,Width_t &,Style_t &)
 {return GetFlag()? kYellow : 0 ;}
 
 //_____________________________________________________________________________
-Int_t StVirtualEventFilter::Filter(const St_TableSorter *,Int_t index,Width_t &,Style_t &)
+Int_t StVirtualEventFilter::Channel(const St_TableSorter *,Int_t index,Width_t &,Style_t &)
 { return GetFlag()? (kGreen+index)%20 : 0 ;}
 //_____________________________________________________________________________
-Int_t StVirtualEventFilter::Filter(const StVertex *,Width_t &,Style_t &)
+Int_t StVirtualEventFilter::Channel(const StVertex *,Width_t &,Style_t &)
 { return GetFlag()? kBlue : 0 ; }
 
 //_____________________________________________________________________________
-Int_t StVirtualEventFilter::Filter(const St_Table *,Int_t rowNumber,Width_t &,Style_t &)
+Int_t StVirtualEventFilter::Channel(const St_Table *,Int_t rowNumber,Width_t &,Style_t &)
 { return GetFlag()? (kGreen+rowNumber)%20 : 0 ;}
 
