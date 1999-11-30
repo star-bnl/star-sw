168ac7e738acda90ac6ed386157c9841f7ffeaf7
Kathleen R. Turner <kathy>
1999-11-30 20:04:17 +0000

    fix Example macros so that they work from .dst.root files or .dst.xdf files & update documentation; also had to change which values printed in *read_dst_print_tables* macro since the names have changed in dst tables

--- StRoot/macros/examples/Example_read_hist_file_min_list_draw_ps.C
+++ StRoot/macros/examples/Example_read_hist_file_min_list_draw_ps.C
@@ -1,5 +1,8 @@
-// $Id: Example_read_hist_file_min_list_draw_ps.C,v 1.3 1999/11/03 19:03:06 kathy Exp $
+// $Id: Example_read_hist_file_min_list_draw_ps.C,v 1.4 1999/11/30 20:04:17 kathy Exp $
 // $Log: Example_read_hist_file_min_list_draw_ps.C,v $
+// Revision 1.4  1999/11/30 20:04:17  kathy
+// fix Example macros so that they work from .dst.root files or .dst.xdf files & update documentation; also had to change which values printed in *read_dst_print_tables* macro since the names have changed in dst tables
+//
 // Revision 1.3  1999/11/03 19:03:06  kathy
 // changes to default input files and output file names - needed by perl script for testing
 //
@@ -16,8 +19,8 @@
 //   Minimal commands to read hist file and list,draw & send to ps
 //   histogram with known name.
 //
-//   You must first run  Example_read_xdf_makehist.C to create 
-//     an output Kathy_hist.root file (flat file).  
+//   You must first run  Example_read_dst_makehist.C to create 
+//     an output *hist.root file (flat file).  
 //   This macro then reads in the histogram file and draws 
 //     histogram h1.
 //   This will not work for *.hist.root files produced from bfc.C since
@@ -26,7 +29,7 @@
 //
 void Example_read_hist_file_min_list_draw_ps()
 {
-TFile f1("Example_read_xdf_makehist.root");
+TFile f1("Example_read_dst_makehist.root");
 f1.ls();
 TCanvas MyCanvas("CanvasName","Canvas Title",800,600);
 gStyle->SetOptStat(111111);
