3417a9a053d4ac307cc1dac5af13fb4d5e6739fd
Manuel Calderon de la Barca Sanchez <calderon@physics.ucdavis.edu>
1999-12-03 00:55:21 +0000

    Completely revised for StMcEvent 2.0 Using StDbUtilities for coordinate transformations. Tested g2t_event table is read properly (when available). Added messages for diagnostics. Tested in Linux, Solaris 4.2  and HP.

--- StRoot/StMcEventMaker/StMcEventMaker.h
+++ StRoot/StMcEventMaker/StMcEventMaker.h
@@ -1,7 +1,14 @@
 /**********************************************
  *
- * $Id: StMcEventMaker.h,v 1.2 1999/07/28 20:27:43 calderon Exp $
+ * $Id: StMcEventMaker.h,v 1.3 1999/12/03 00:55:21 calderon Exp $
  * $Log: StMcEventMaker.h,v $
+ * Revision 1.3  1999/12/03 00:55:21  calderon
+ * Completely revised for StMcEvent 2.0
+ * Using StDbUtilities for coordinate transformations.
+ * Tested g2t_event table is read properly (when available).
+ * Added messages for diagnostics.
+ * Tested in Linux, Solaris 4.2  and HP.
+ *
  * Revision 1.2  1999/07/28 20:27:43  calderon
  * Version with SL99f libraries
  *
@@ -16,38 +23,37 @@
 #endif
 
 class StMcEvent;
+class StMcTrack;
 
 class StMcEventMaker : public StMaker {
-  
-private:
-    Bool_t drawinit;
-    StMcEvent* mCurrentMcEvent; //!  This tells CINT not to parse it.
-    
-    
-
-protected:
-
-  
 public:
 
-    StMaker* currentChain;
-    StMcEventMaker(const char* name = "MCEvent", const char* title = "event/McEvent");
+    StMcEventMaker(const char* name = "MCEvent", const char* title = "");
     virtual ~StMcEventMaker();
+
     virtual void  Clear(const char* opt="");
-    virtual void PrintInfo();
     virtual Int_t Init();
     virtual Int_t Make();
     virtual Int_t Finish();
-  
 
-    // if need to make histograms or print stuff, look at St_QA_Maker
-
-    //method to return the read event like in StEventMaker 
     StMcEvent* currentMcEvent() { return mCurrentMcEvent;}; 
 
     virtual const char* GetCVS() const
-    {static const char cvs[]="Tag $Name:  $ $Id: StMcEventMaker.h,v 1.2 1999/07/28 20:27:43 calderon Exp $ built "__DATE__" "__TIME__; return cvs;}	
+    {static const char cvs[]="Tag $Name:  $ $Id: StMcEventMaker.h,v 1.3 1999/12/03 00:55:21 calderon Exp $ built "__DATE__" "__TIME__; return cvs;}	
     
+public:
+
+    Bool_t  doPrintEventInfo;      //! lots of screen output
+    Bool_t  doPrintMemoryInfo;     //! 
+    Bool_t  doPrintCpuInfo;        //! 
+
+protected:
+    void   printEventInfo();
+    void   printTrackInfo(StMcTrack*);
+
+private:
+    Bool_t drawinit;
+    StMcEvent* mCurrentMcEvent; //!  This tells CINT not to parse it.
     ClassDef(StMcEventMaker, 1)
 
 };
