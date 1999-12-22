30daaa851f4967e294e6e8ef1974fdf50a0b0220
posk <>
1999-12-22 21:43:58 +0000

    Reduced the line widths to compensate for the increase in postscript line width which started in ROOT 2.23/08.

--- StRoot/macros/graphics/videoStyle.C
+++ StRoot/macros/graphics/videoStyle.C
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// $Id: videoStyle.C,v 1.1 1999/10/28 21:28:07 posk Exp $
+// $Id: videoStyle.C,v 1.2 1999/12/22 21:43:58 posk Exp $
 //
 // Author: Art Poskanzer, LBNL, Oct. 1999
 // Description:  Style file for video presentation histograms.
@@ -10,9 +10,12 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
 // $Log: videoStyle.C,v $
+// Revision 1.2  1999/12/22 21:43:58  posk
+// Reduced the line widths to compensate for the increase in postscript
+// line width which started in ROOT 2.23/08.
+//
 // Revision 1.1  1999/10/28 21:28:07  posk
 // Style file for making histograms for video presentations.
-//
 //
 //
 ///////////////////////////////////////////////////////////////////////////////
@@ -29,14 +32,14 @@ TStyle* myStyle = new  TStyle("videoStyle", "Video Style");
 myStyle->SetPalette(1,0);
 myStyle->SetCanvasColor(10);
 myStyle->SetCanvasBorderMode(0);
-myStyle->SetFrameLineWidth(4);
+myStyle->SetFrameLineWidth(3);
 myStyle->SetFrameFillColor(10);
 myStyle->SetPadColor(10);
 myStyle->SetPadTickX(1);
 myStyle->SetPadTickY(1);
-myStyle->SetPadBottomMargin(0.17);
+myStyle->SetPadBottomMargin(0.2);
 myStyle->SetPadLeftMargin(0.2);
-myStyle->SetHistLineWidth(15);
+myStyle->SetHistLineWidth(8);
 myStyle->SetHistLineColor(kRed);
 myStyle->SetLabelSize(0.06,"X");
 myStyle->SetLabelSize(0.06,"Y");
@@ -49,9 +52,10 @@ myStyle->SetTitleSize(0.08,"Y");
 //myStyle->SetTitleOffset(0.,"X");
 //myStyle->SetTitleOffset(0.,"Y");
 myStyle->SetTitleColor(10);
+myStyle->SetTitleTextColor(kBlue);
 //myStyle->SetOptStat(1110111);
 myStyle->SetStatColor(10);
-myStyle->SetFuncWidth(10);
+myStyle->SetFuncWidth(8);
 myStyle->SetFuncColor(kGreen);
 
 gROOT->ForceStyle();
