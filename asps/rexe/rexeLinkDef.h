b09999bddbfe66871293d767dcffe94d2ccc53ea
fisyak <>
2000-01-04 16:11:42 +0000

    move TGeant3 to root4star executable

---
+++ asps/rexe/rexeLinkDef.h
@@ -0,0 +1,15 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ global gMC;
+#pragma link C++ class StarMC;
+#pragma link C++ class TGeant3;
+#pragma link C++ class StGeant3GeometryGUI-;
+#pragma link C++ class StGuiGeomMain;
+#pragma link C++ class StGuiGeomDialog;
+#pragma link C++ class StGUISliders;
+#pragma link C++ class StDrawVolume-;
+#pragma link C++ class THIGZ;
+#pragma link C++ class TPaveTree;
+#endif
