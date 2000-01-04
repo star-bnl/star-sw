c62f4ce9fff432a34d28362fe41f7488eb9072f5
cvs2git <cvs2git>
2000-01-04 16:04:08 +0000

    This commit was manufactured by cvs2git to create branch 'alice'.

--- mgr/find_dead_link.pl
+++
@@ -1,8 +0,0 @@
-#!/opt/star/bin/perl
-use Env;
-if (defined($AFS)) {$File::Find::dont_use_nlink;}
-require "find.pl";
-&find (\&wanted,'.');
-sub wanted {
-  -l and not -e and print "bigus link: $File::Find::name\n";
-}
