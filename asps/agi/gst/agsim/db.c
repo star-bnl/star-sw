f48c22501d182126cf6393ad067cfe92654105f0
Pavel Nevski <nevski>
2000-01-06 23:31:11 +0000

    db is c, not c++

--- asps/agi/gst/agsim/db.c
+++ asps/agi/gst/agsim/db.c
@@ -1,8 +1,8 @@
 /*CMZ :  2.00/00 24/11/99  00.33.54  by  Pavel Nevski*/
 /*-- Author :    Sasha Vanyashin  28/06/99*/
 /*DECK,DBDUMMY,T=CC,IF=-MYSQL.*/
-
-extern "C" void  dbset_(){};
-extern "C" void  dbuse_(){};
-extern "C" void  dbfill_(){};
+/* extern "C" for c++ */
+void  dbset_(){};
+void  dbuse_(){};
+void  dbfill_(){};
  
