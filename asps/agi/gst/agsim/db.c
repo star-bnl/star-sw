d3fa139a9aefb012822d41d2216f846ed82aec55
nevski <>
2000-01-06 14:53:09 +0000

    agi interface version 2.0

---
+++ asps/agi/gst/agsim/db.c
@@ -0,0 +1,8 @@
+/*CMZ :  2.00/00 24/11/99  00.33.54  by  Pavel Nevski*/
+/*-- Author :    Sasha Vanyashin  28/06/99*/
+/*DECK,DBDUMMY,T=CC,IF=-MYSQL.*/
+
+extern "C" void  dbset_(){};
+extern "C" void  dbuse_(){};
+extern "C" void  dbfill_(){};
+ 
