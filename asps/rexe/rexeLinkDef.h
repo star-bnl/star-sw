aacf5a6a0b1d3e5cadddf2ae29c80de9fc87ff45
Yuri Fisyak <fisyak@bnl.gov>
2000-01-07 00:13:43 +0000

    Remove Alice Higz stuff

--- asps/rexe/rexeLinkDef.h
+++ asps/rexe/rexeLinkDef.h
@@ -5,11 +5,5 @@
 #pragma link C++ global gMC;
 #pragma link C++ class StarMC;
 #pragma link C++ class TGeant3;
-#pragma link C++ class StGeant3GeometryGUI-;
-#pragma link C++ class StGuiGeomMain;
-#pragma link C++ class StGuiGeomDialog;
-#pragma link C++ class StGUISliders;
-#pragma link C++ class StDrawVolume-;
-#pragma link C++ class THIGZ;
 #pragma link C++ class TPaveTree;
 #endif
