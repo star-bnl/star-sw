882940b9daaf951a3e982215db32250c09b3b763
genevb <>
2000-01-20 17:03:26 +0000

    Need to cast reference pointer now???

--- StRoot/macros/examples/Example_plotTable.C
+++ StRoot/macros/examples/Example_plotTable.C
@@ -1,5 +1,8 @@
-// $Id: Example_plotTable.C,v 1.6 2000/01/06 19:35:48 kathy Exp $
+// $Id: Example_plotTable.C,v 1.7 2000/01/20 17:03:26 genevb Exp $
 // $Log: Example_plotTable.C,v $
+// Revision 1.7  2000/01/20 17:03:26  genevb
+// Need to cast reference pointer now???
+//
 // Revision 1.6  2000/01/06 19:35:48  kathy
 // change to use available xdf file as input
 //
@@ -26,7 +29,7 @@ gSystem->Load("St_Tables");
 gSystem->Load("xdf2root");
 gSystem->Load("StUtilities");
 gSystem->Load("StAnalysisUtilities");
-St_TableNtuple myNtuple(St_dst_v0_vertex());
+St_TableNtuple myNtuple((St_Table&) St_dst_v0_vertex());
 myNtuple.AddXDFFile("/star/rcf/test/dev/tfs_Linux/Mon/year_1b/hc_lowdensity/gstar.dst.xdf",
 "dst","dst_v0_vertex");
 myNtuple.Draw("pos_py:pos_px");
