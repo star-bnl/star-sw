6f78aaae2046e47074e4026981f049425505d678
fine <>
2000-01-24 21:12:23 +0000

    new comments

--- StRoot/St_base/StVirtualEventFilter.cxx
+++ StRoot/St_base/StVirtualEventFilter.cxx
@@ -5,7 +5,7 @@
 // StVirtualEventFilter virtual base class is to create the variuous    //
 // user-defined filters                                                 //
 //                                                                      //
-// $Id: StVirtualEventFilter.cxx,v 1.9 2000/01/24 20:57:22 fine Exp $   //
+// $Id: StVirtualEventFilter.cxx,v 1.10 2000/01/24 21:12:23 fine Exp $   //
 //                                                                      //
 // This class defines the intreface to define                           //
 // a graphics attribute:                                                //
@@ -14,17 +14,19 @@
 //                                                                      //
 // and a trivial selection as well                                      //
 //  to reply whether the object does satisfy some user                  //
-//  defined conditions.                                                 //                                                                      //
+//  defined conditions.                                                 //
 //  For that the return value = 0 means the event supplied              //
 //  did not pass the filter and should not be drawn at all              //
 //                                                                      //
+//     Channel       Parameters                                         //
+//    -------------  -----------------------------                      //
 // 1. StGlobalTrack  *globTrack                                         //
 // 2. StObjArray     *hitCollection                                     //
 // 3. St_TableSorter *tableObject,Int_t index                           //
 // 4. StVertex       *vertexObject                                      //
 // 5. St_Table       *tableObject, Int_t rowNumber                      //
 //                                                                      //
-// User should derive his own class and overload the "filter" methods   //
+// User should derive his own class and overload the "Channel" methods  //
 // with the signature those fit his/her current needs                   //
 //                                                                      //
 // Submit any problem with this code via begin_html <A HREF="http://www.star.bnl.gov/STARAFS/comp/sofi/bugs/send-pr.html"><B><I>"STAR Problem Report Form"</I></B></A> end_html
@@ -60,6 +62,9 @@ Int_t StVirtualEventFilter::Channel(const St_Table *,Int_t rowNumber,Size_t &,St
 
 //_____________________________________________________________________________
 // $Log: StVirtualEventFilter.cxx,v $
+// Revision 1.10  2000/01/24 21:12:23  fine
+// new comments
+//
 // Revision 1.9  2000/01/24 20:57:22  fine
 // new comments
 //
