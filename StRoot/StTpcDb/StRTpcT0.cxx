f0876e3af4c9add8e1957daffbb199dae7cb2125
David Hardtke <hardtke@uclink4.berkeley.edu>
2000-01-24 15:31:31 +0000

    change to use new gain and t0 tables

--- StRoot/StTpcDb/StRTpcT0.cxx
+++ StRoot/StTpcDb/StRTpcT0.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StRTpcT0.cxx,v 1.6 1999/12/16 22:00:53 hardtke Exp $
+ * $Id: StRTpcT0.cxx,v 1.7 2000/01/24 15:31:31 hardtke Exp $
  *
  * Author:  David Hardtke
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StRTpcT0.cxx,v $
+ * Revision 1.7  2000/01/24 15:31:31  hardtke
+ * change to use new gain and t0 tables
+ *
  * Revision 1.6  1999/12/16 22:00:53  hardtke
  * add CVS tags
  *
@@ -22,9 +25,9 @@ float StRTpcT0::getT0(int row, int pad)   const {
   float padT0 = 0;
   if (row > 0 && padplane->indexForRowPad(row,pad)>=0) {
     if (row<=padplane->numberOfInnerRows())
-      padT0 =  (*mT0)[0].innerSectorTimeOffsets[padplane->indexForRowPad(row,pad)];
+      padT0 =  (*mT0IS)[0].offset[padplane->indexForRowPad(row,pad)];
    else if (row>padplane->numberOfInnerRows()&&row<=padplane->numberOfRows())
-      padT0 =  (*mT0)[0].outerSectorTimeOffsets[padplane->indexForRowPad(row,pad)];
+      padT0 =  (*mT0OS)[0].offset[padplane->indexForRowPad(row,pad)];
   }
   return padT0;
 } 
--- StRoot/StTpcDb/StRTpcT0.h
+++ StRoot/StTpcDb/StRTpcT0.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StRTpcT0.h,v 1.6 1999/12/16 22:00:53 hardtke Exp $
+ * $Id: StRTpcT0.h,v 1.7 2000/01/24 15:31:31 hardtke Exp $
  *
  * Author:  David Hardtke
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StRTpcT0.h,v $
+ * Revision 1.7  2000/01/24 15:31:31  hardtke
+ * change to use new gain and t0 tables
+ *
  * Revision 1.6  1999/12/16 22:00:53  hardtke
  * add CVS tags
  *
@@ -17,22 +20,30 @@
 #ifndef __STRTPCT0__
 #define __STRTPCT0__
 #include "StTpcT0I.h"
-#include "tables/St_tpcTimeOffsets_Table.h"
+#include "tables/St_tpcISTimeOffsets_Table.h"
+#include "tables/St_tpcOSTimeOffsets_Table.h"
 
 class StRTpcT0 : public StTpcT0I {
 
 private:
 
-  St_tpcTimeOffsets* mT0;
+  St_tpcISTimeOffsets* mT0IS;
+  St_tpcOSTimeOffsets* mT0OS;
   int mSector;
   StTpcPadPlaneI* padplane;
 
 public:
 
-  StRTpcT0(St_tpcTimeOffsets* T0In=0){AddData(T0In);}
+  StRTpcT0(St_tpcISTimeOffsets* T0In=0,St_tpcOSTimeOffsets* T0Out=0){
+   AddData(T0In);
+   AddData(T0Out);
+  }
   ~StRTpcT0(){}
-  void AddData(St_tpcTimeOffsets* T0In) {
-      mT0 = T0In;
+  void AddData(St_tpcISTimeOffsets* T0In) {
+      mT0IS = T0In;
+   }
+  void AddData(St_tpcOSTimeOffsets* T0Out) {
+      mT0OS = T0Out;
    }
 
   //Implements Abstract Interface 
