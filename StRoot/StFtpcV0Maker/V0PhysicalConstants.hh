eb667318eb9c99dbc83c72b9eb629c370ffc3f69
fisyak <>
2000-01-25 15:32:45 +0000

    Add namespace for CC5

--- StRoot/StFtpcV0Maker/V0PhysicalConstants.hh
+++ StRoot/StFtpcV0Maker/V0PhysicalConstants.hh
@@ -1,6 +1,9 @@
-// $Id: V0PhysicalConstants.hh,v 1.2 2000/01/03 13:16:21 jcs Exp $
+// $Id: V0PhysicalConstants.hh,v 1.3 2000/01/25 15:32:45 fisyak Exp $
 //
 // $Log: V0PhysicalConstants.hh,v $
+// Revision 1.3  2000/01/25 15:32:45  fisyak
+// Add namespace for CC5
+//
 // Revision 1.2  2000/01/03 13:16:21  jcs
 // Add CVS Id strings
 //
@@ -15,6 +18,9 @@
 #define V0_PHYSICAL_CONSTANTS
 
 #include "SystemOfUnits.h"
+#ifndef ST_NO_NAMESPACES
+using namespace units;
+#endif
 
 static const HepDouble charged_pion_mass_c2 = 139.56995 * MeV;
 static const HepDouble neutral_pion_mass_c2 = 134.9764  * MeV;
