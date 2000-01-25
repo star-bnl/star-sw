4719be65746dceb8f2227082c1d02b09d86322e7
Valeri Fine <val.fine@gmail.com>
2000-01-25 22:36:40 +0000

    Linux warning has been removed

--- StRoot/St_base/St_tableDescriptor.cxx
+++ StRoot/St_base/St_tableDescriptor.cxx
@@ -1,37 +1,5 @@
 //*-- Author :    Valery Fine   09/08/99  (E-mail: fine@bnl.gov)
-// $Id: St_tableDescriptor.cxx,v 1.10 2000/01/25 22:25:46 fine Exp $
-// $Log: St_tableDescriptor.cxx,v $
-// Revision 1.10  2000/01/25 22:25:46  fine
-// the number of dimensions for table arrays became 3 instead of 2
-//
-// Revision 1.9  2000/01/25 02:17:04  fine
-// CreateLeafList and dtor have been fixed
-//
-// Revision 1.8  2000/01/24 04:02:53  fine
-// CreateLeafList(): some extra protection
-//
-// Revision 1.7  2000/01/24 03:55:47  fine
-// new nethod CreateLeafList() to create text descriptor compatible with TBranch ctor
-//
-// Revision 1.6  1999/12/03 20:51:27  fine
-// change the data-member name size from 20 to 32 to be in line with dstype.h
-//
-// Revision 1.5  1999/09/13 13:28:27  fine
-// One cast int -> uint introduced to avoid compiler warnings
-//
-// Revision 1.4  1999/08/13 16:35:53  fine
-// The artificial restrictions for tableDescriptor have been introduced since database want that
-//
-// Revision 1.3  1999/08/12 18:53:49  fine
-// clash between St_tableDescriptor::GetSize and St_Table::GetSize resolved
-//
-// Revision 1.2  1999/08/11 14:44:39  fine
-// name clash with ROOT over enum resolved
-//
-// Revision 1.1  1999/08/11 00:40:11  fine
-// new class St_tableDescriptor
-//
-
+// $Id: St_tableDescriptor.cxx,v 1.11 2000/01/25 22:36:40 fine Exp $
 #include <stdlib.h> 
 #include "St_tableDescriptor.h"
 #include "St_Table.h"
@@ -106,7 +74,7 @@ TString St_tableDescriptor::CreateLeafList() const
          const Char_t *colName = GetColumnName(i);
          Char_t digBuffer[100];       
          for (Int_t j=0;j<nDim;j++) {
-	   for (Int_t l=0;l<indx[j];l++){
+	   for (UInt_t l=0;l<indx[j];l++){
 	     if (l) string += ":";
 	     sprintf(digBuffer,"%s_%d",colName, l);
 	     string += digBuffer;
@@ -205,9 +173,10 @@ void St_tableDescriptor::LearnTable(TClass *classPtr)
 #ifdef NORESTRICTIONS
                                               elementDescriptor.m_IndexArray = new UInt_t(dim);
 #else
-       if (UInt_t(dim) > sizeof(elementDescriptor.m_IndexArray)/sizeof(UInt_t *)) {
+       UInt_t maxDim = sizeof(elementDescriptor.m_IndexArray)/sizeof(UInt_t *);
+       if (UInt_t(dim) > maxDim) {
                 Error("LearnTable","Too many dimenstions - %d", dim);
-                dim =  3;
+                dim =  maxDim;
        }
 #endif
         for( Int_t indx=0; indx < dim; indx++ ){
@@ -271,3 +240,40 @@ St_Table::EColumnType St_tableDescriptor::GetColumnType(const Char_t *columnName
   if (indx >= 0 ) indx = GetColumnType(indx);
   return EColumnType(indx);
 }
+
+//____________________________________________________________________________
+// $Log: St_tableDescriptor.cxx,v $
+// Revision 1.11  2000/01/25 22:36:40  fine
+// Linux warning has been removed
+//
+// Revision 1.10  2000/01/25 22:25:46  fine
+// the number of dimensions for table arrays became 3 instead of 2
+//
+// Revision 1.9  2000/01/25 02:17:04  fine
+// CreateLeafList and dtor have been fixed
+//
+// Revision 1.8  2000/01/24 04:02:53  fine
+// CreateLeafList(): some extra protection
+//
+// Revision 1.7  2000/01/24 03:55:47  fine
+// new nethod CreateLeafList() to create text descriptor compatible with TBranch ctor
+//
+// Revision 1.6  1999/12/03 20:51:27  fine
+// change the data-member name size from 20 to 32 to be in line with dstype.h
+//
+// Revision 1.5  1999/09/13 13:28:27  fine
+// One cast int -> uint introduced to avoid compiler warnings
+//
+// Revision 1.4  1999/08/13 16:35:53  fine
+// The artificial restrictions for tableDescriptor have been introduced since database want that
+//
+// Revision 1.3  1999/08/12 18:53:49  fine
+// clash between St_tableDescriptor::GetSize and St_Table::GetSize resolved
+//
+// Revision 1.2  1999/08/11 14:44:39  fine
+// name clash with ROOT over enum resolved
+//
+// Revision 1.1  1999/08/11 00:40:11  fine
+// new class St_tableDescriptor
+//____________________________________________________________________________
+
