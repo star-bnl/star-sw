5b8cb2621d5fddf77664e0d5ceb1ed5a2784a94f
cvs2git <cvs2git>
2000-01-29 16:23:08 +0000

    This commit was manufactured by cvs2git to create tag 'v_00'.

--- StRoot/StRrsMaker/StRichRrsMacros.h
+++
@@ -1,8 +0,0 @@
- * Revision 1.2  2000/02/08 16:30:23  lasiuk
- *
- *********************************************************************/
-#define uSE_MEMORY_INFO 1
-#define RRS_DEBUG 0
-#define rICH_COORDINATE_BOUNDS_CHECK 1
-#define rICH_WITH_VIEWER 1
-#define rICH_WITH_MONITOR 1
