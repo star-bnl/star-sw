3f7827dfefe8c969f23f56e5fc25290439ea2281
cvs2git <cvs2git>
2000-02-05 03:15:22 +0000

    This commit was manufactured by cvs2git to create tag 'V2'.

--- pams/global/inc/StDedxMethod.h
+++
@@ -1,9 +0,0 @@
-#include "StDedxDefinitions.h"
-enum StDedxMethod {
-  kUndefinedMethodId         = kUndefinedMethodIdentifier,
-  kTruncatedMeanId           = kTruncatedMeanIdentifier,
-  kEnsembleTruncatedMeanId   = kEnsembleTruncatedMeanIdentifier,
-  kLikelihoodFitId           = kLikelihoodFitIdentifier,
-  kWeightedTruncatedMeanId   = kWeightedTruncatedMeanIdentifier,
-  kOtherMethodId             = kOtherMethodIdentifier
-};
