eb70dc05eaac6241265e074a83e3449ddd7d050b
Brian Lasiuk <lasiuk>
2000-02-08 16:26:04 +0000

    rmove vector and StGlobals from Interface. allocate space for survey parameters calculate sector origins and pad corner positions

--- StRoot/StRrsMaker/StRichGeometryDb.cxx
+++ StRoot/StRrsMaker/StRichGeometryDb.cxx
@@ -1,12 +1,14 @@
-/******************************************************
- * $Id: StRichGeometryDb.cxx,v 1.2 2000/01/25 22:02:20 lasiuk Exp $
+/*******************************************************************
+ * $Id: StRichGeometryDb.cxx,v 1.3 2000/02/08 16:26:01 lasiuk Exp $
  *
  * Description:
  *
- ******************************************************
+ *******************************************************************
  * $Log: StRichGeometryDb.cxx,v $
- * Revision 1.2  2000/01/25 22:02:20  lasiuk
- * Second Revision
+ * Revision 1.3  2000/02/08 16:26:01  lasiuk
+ * rmove vector and StGlobals from Interface.
+ * allocate space for survey parameters
+ * calculate sector origins and pad corner positions
  *
  * Revision 1.4  2000/02/12 21:55:44  lasiuk
  * Wire position adjustment
@@ -14,14 +16,7 @@
  * Revision 1.3  2000/02/08 16:26:01  lasiuk
  * rmove vector and StGlobals from Interface.
  * allocate space for survey parameters
- ******************************************************/
-
-//#include "TInterpreter.h"              // ROOT
-//#include "TSystem.h"
-//#include "St_Table.h"
-//#include "St_DataSet.h"
-//#include "rich_rich.h"
-
+ * calculate sector origins and pad corner positions
  *
  * Revision 1.2  2000/01/25 22:02:20  lasiuk
  * Second Revision
@@ -39,9 +34,9 @@
 
 //RRS
 #ifndef ST_NO_NAMESPACES
-    : quads(5) 
+using namespace units;
 #endif
-    StRichGeometryDb::p2Db = this;   // access to current instance
+
 #include "StRichGeometryDb.h"
 
 StRichGeometryDb* StRichGeometryDb::p2Db = 0;
@@ -51,14 +46,6 @@ StRichGeometryDb::StRichGeometryDb()
 {
     //StRichGeometryDb::p2Db = this;   // access to current instance
     
-    quads[1].z0       =   1.9 * centimeter;          // verified
-    quads[2].z0       = -65.1 * centimeter;          // verified
-    quads[3].z0       = -65.1 * centimeter;          // verified
-    quads[4].z0       =   1.9 * centimeter;          // verified
-    quads[1].x0       =  41.4 * centimeter;          // verified
-    quads[2].x0       =  41.4 * centimeter;          // verified
-    quads[3].x0       = -1.92 * centimeter;          // verified
-    quads[4].x0       = -1.92 * centimeter;          // verified
 StRichGeometryDb::~StRichGeometryDb()
     quad_gap_z        =  30.0 * millimeter;          
     delete p2Db;
@@ -68,69 +55,77 @@ void StRichGeometryDb::my_fill()
     
     pad_side_x        = 7.9   * millimeter;          // verified
     pad_side_z        = 7.5   * millimeter;          // verified
+    quad_gap_x        =  30.0 * millimeter;          // verified          
+    quad_gap_y        =  30.0 * millimeter;          
 	
+    wire_spacing      = 4.2   * millimeter;          // verified
+    number_of_wires   = 192;                         // verified
     n_pad_x           = 48;                          // verified
     n_pad_z           = 80;                          // verified
+    pad_pitch         = 8.0   * millimeter;
+    row_pitch         = 8.4   * millimeter;
+    pad_spacing       = 0.5   * millimeter;          // verified
     row_spacing       = 0.5   * millimeter;
+
+    n_pad_x           = 80;                          // verified
     n_pad_y           = 48;                          // verified
 
     mNumberOfPadsInaColumn = 96;
-    ampl_factor       = 1.0e4;                       // verified      
-}
     mNumberOfPadsInaRow    = 160;
+    
+    number_of_pads    = 15360;                       // verified
+
+    mRadialDistanceToRich   = 242.6655311 * centimeter;
+    length            = 1310  * millimeter;          // verified
+    width             = 836   * millimeter;          // verified
+    height            = 2     * millimeter;          // verified
+    mRadialDistanceToRich   = 242.925 * centimeter;
+	
+    //mRadialDistanceToRich   = 242.925 * centimeter;
+    // change from gjk (May 15,2000)
+    mRadialDistanceToRich   =	243.130 *centimeter;
+
+    //
+    // Calculate the positions of the uppermost left had
+    quads[1].z0 = quadrantGapInZ()/2. + padPitch()*0.5;
+    quads[4].z0 = quads[1].z0;
+
+    //
+    quads[2].z0 = -quadrantGapInZ()/2. - padPitch()*(numberOfPadsInAQuadrantRow()-0.5);
+    quads[3].z0 = quads[2].z0;
+    
+    //1.9 * centimeter;          // verified
+    quads[1].x0 =
+	quadrantGapInX()/2 + rowPitch()*(numberOfRowsInAQuadrantColumn()-0.5);
+    quads[2].x0 = quads[1].x0;
+    // -65.1 * centimeter;          // verified
+    quads[2].x0 = -quadrantGapInX()/2. - padPitch()*(numberOfPadsInAQuadrantRow()-0.5);
+    quads[3].x0 = -quadrantGapInX()/2. - rowPitch()*0.5;
+    quads[4].x0 = quads[3].x0;   
+    // 41.4 * centimeter;          // verified
+    quads[1].y0 =
+	quadrantGapInY()/2 + rowPitch()*(numberOfRowsInAQuadrantColumn()-0.5);
+    quads[2].y0 = quads[1].y0;
+    quadsOrigin[1].z0 =
+	quadrantGapInZ()/2. + padPitch()*(numberOfPadsInAQuadrantRow()/2.);
+    quadsOrigin[4].z0 = quadsOrigin[1].z0;
+    quads[4].y0 = quads[3].y0;   
+
+    quadsOrigin[2].z0 =
+	-quadrantGapInZ()/2. - padPitch()*(numberOfPadsInAQuadrantRow()/2.);
+    quadsOrigin[3].z0 = quadsOrigin[2].z0;
+    quadsOrigin[1].x0 =
+	quadrantGapInX()/2. + padPitch()*(numberOfPadsInAQuadrantRow()/2.);
+    quadsOrigin[1].x0 =
+	quadrantGapInX()/2 + rowPitch()*(numberOfRowsInAQuadrantColumn()/2.);
+    quadsOrigin[2].x0 = quadsOrigin[1].x0;
+    quadsOrigin[2].x0 =
+	-quadrantGapInX()/2. - padPitch()*(numberOfPadsInAQuadrantRow()/2.);
+    quadsOrigin[3].x0 =
+	-quadrantGapInX()/2. - rowPitch()*(numberOfRowsInAQuadrantColumn()/2.);
+    quadsOrigin[4].x0 = quadsOrigin[3].x0;   
     quadsOrigin[1].y0 =
-/*
- *  A fill from a central DB works the following way. 
- *  A STAR_Table that can hold the values is created. It
- *  should be found in the central repository of such tables.
- *  A macro, located in the central repository of DB macros
- *  fills the table with numbers. Then I copy those numbers
- *  to my DBs.
- */
-
-// void StRichGeometryDb::star_fill()
-// {    
-//     gInterpreter->ProcessLine(".L rich_rich.C");
-//     St_DataSet * r = (St_DataSet *) gInterpreter->Calc("CreateTable()");
-//     rich_rich_st * s=(rich_rich_st*)((St_Table*)r)->GetArray();
-//     if ( !s ) cerr << "Error reading from STAR_TABLE!!!\n";
-    
-//     version           = s->version;
-    
-//     quads[1].z0       = s->quad1_z0;
-//     quads[2].z0       = s->quad2_z0;
-//     quads[3].z0       = s->quad3_z0;
-//     quads[4].z0       = s->quad4_z0;
-//     quads[1].x0       = s->quad1_x0;
-//     quads[2].x0       = s->quad2_x0;
-//     quads[3].x0       = s->quad3_x0;
-//     quads[4].x0       = s->quad4_x0;
-//     quad_gap_x        = s->quad_gap_x;
-//     quad_gap_z        = s->quad_gap_z; 
-    
-//     wire_spacing      = s->wire_spacing; 
-//     wire_x0           = s->wire_x0;  
-//     number_of_wires   = s->number_of_wires;  
-    
-//     pad_side_x        = s->pad_side_x;    
-//     pad_side_z        = s->pad_side_z;   
-//     pad_spacing       = s->pad_spacing;   
-//     n_pad_x           = s->n_pad_x;           
-//     n_pad_z           = s->n_pad_z;          
-//     number_of_pads    = s->number_of_pads;  
-//     length            = s->length;           
-//     width             = s->width;            
-//     height            = s->height;           
-//     ampl_factor       = s->ampl_factor;  
-    
-// }
-
-
-/*
- *  Function handles the access to the
- *  only instance by using a static
- *  pointer. 
- */
+    //-1.71 * centimeter;          // verified
     wire_y0[1] = -quad_gap_y/2. - (0.5)*row_pitch + wire_spacing/2.;
 
     mRadiatorDimension = StThreeVector<double>(131./2.,83.6/2.,1.)*centimeter;
@@ -142,40 +137,54 @@ void StRichGeometryDb::my_fill()
 
 StRichGeometryDb* StRichGeometryDb::getDb()
 {
-    os << "** StRichGeometryDb::print() **" << endl;
+    if(!p2Db)
 	p2Db = new StRichGeometryDb();
     
-    os << "Detector Length=   " << (detectorLength()/centimeter) << " cm" << endl;
-    os << "Detector Width=    " << (detectorWidth()/centimeter)  << " cm" << endl;
+    return p2Db;
+}
 
-    os << "Xo (1)=            " << (quadrantX0(1)/centimeter)    << " cm" << endl;
-    os << "Xo (2)=            " << (quadrantX0(2)/centimeter)    << " cm" << endl;
-    os << "Xo (3)=            " << (quadrantX0(3)/centimeter)    << " cm" << endl;
-    os << "Xo (4)=            " << (quadrantX0(4)/centimeter)    << " cm" << endl;
+    os << " (Zo, Xo) cm  (Corner Pad)" << endl;
+    os << "  (" << (quadrantZ0(2)/centimeter) << "," << (quadrantX0(2)/centimeter) << ")      ";
+    os << " (" << (quadrantZ0(1)/centimeter) << "," << (quadrantX0(1)/centimeter) << ")" << endl;
+    os << "  (" << (quadrantZ0(3)/centimeter) << "," << (quadrantX0(3)/centimeter) << ")      ";
+    os << "(" << (quadrantZ0(4)/centimeter) << "," << (quadrantX0(4)/centimeter) << ")" << endl;
+    os << " Detector Length=   " << (detectorLength()/centimeter) << " cm" << endl;
+    os << " Detector Width=    " << (detectorWidth()/centimeter)  << " cm\n" << endl;
+    os << "  (" << (quadrantZOrigin(2)/centimeter) << "," << (quadrantXOrigin(2)/centimeter) << ")    ";
+    os << " (" << (quadrantZOrigin(1)/centimeter) << "," << (quadrantXOrigin(1)/centimeter) << ")" << endl;
+    os << "  (" << (quadrantZOrigin(3)/centimeter) << "," << (quadrantXOrigin(3)/centimeter) << ")    ";
+    os << "(" << (quadrantZOrigin(4)/centimeter) << "," << (quadrantXOrigin(4)/centimeter) << ")\n" << endl;
+    os << "  (" << (quadrantX0(3)/centimeter) << "," << (quadrantY0(3)/centimeter) << ")      ";
+    os << "(" << (quadrantX0(4)/centimeter) << "," << (quadrantY0(4)/centimeter) << ")" << endl;
+    os << "Quad Gap (z)=      " << (quadrantGapInZ()/millimeter) << " mm" << endl;
+    os << "\nQuadrant Origins" << endl;
+    os << "  (" << (quadrantXOrigin(2)/centimeter) << "," << (quadrantYOrigin(2)/centimeter) << ")    ";
 
-    os << "Zo (1)=            " << (quadrantZ0(1)/centimeter)    << " cm" << endl;
-    os << "Zo (2)=            " << (quadrantZ0(2)/centimeter)    << " cm" << endl;
-    os << "Zo (3)=            " << (quadrantZ0(3)/centimeter)    << " cm" << endl;
-    os << "Zo (4)=            " << (quadrantZ0(4)/centimeter)    << " cm" << endl;
+    os << endl;
+    os << "radiator Dimension:  " << (radiatorDimension()/centimeter) << " cm" << endl;
+    os << "quartz Dimension:    " << (quartzDimension()/centimeter)   << " cm" << endl;
+    os << "pad Plane Dimension: " << (padPlaneDimension()/centimeter) << " cm" << endl;
+    os << "Proximity Gap:       " << (proximityGap()/centimeter)      << " cm" << endl;
+    
+    os << endl;
+    os << "Total # of Pads:   " << (numberOfPads())                       << endl;
+    os << " Complete Row:     " << (numberOfPadsInARow())                 << endl;
+    os << " Complete Column:  " << (numberOfRowsInAColumn())              << endl;
+    os << " Quadrant Row:     " << (numberOfPadsInAQuadrantRow())         << endl;
+    os << " Quadrant Column:  " << (numberOfRowsInAQuadrantColumn())      << endl;
 
-    os << "Quad Gap (x)=      " << (quadrantGapsInX()/centimeter) << " cm" << endl;
-    os << "Quad Gap (z)=      " << (quadrantGapsInZ()/centimeter) << " cm" << endl;
-    // Extra here
-
-    os << "No of Pads:        " <<  (numberOfPads())                       << endl;
-    os << " Row:              " << (numberOfPadsInRow())                   << endl;
-    os << " Column:           " << (numberOfPadsInColumn())                << endl;
-
-    os << "Pad Pitch=         " << (padPitch()/centimeter)        << " cm" << endl;
-    os << "Pad Width=         " << (padWidth()/centimeter)        << " cm" << endl;
-    os << "Pad Length=        " << (padLength()/centimeter)       << " cm" << endl;
-
-    os << "Wire Pitch=        " << (wirePitch()/centimeter)       << " cm" << endl;
+    os << endl;
+    os << "Pad Width=         " << (padWidth()/millimeter)        << " mm" << endl;
+    os << "Pad Length=        " << (padLength()/millimeter)       << " mm" << endl;
     os << "Wire OffSet (x)=   " << (firstWirePositionInX()/centimeter) << " cm" << endl;
-    os << "Number of Wires:   " << (numberOfWires())                       << endl;
     os << "Pad Spacing=       " << (padSpacing()/millimeter)      << " mm" << endl;
 
-    os << "Gas Gain=          " << (gasGainAmplificationFactor())            << endl;
+    os << endl;
+    os << "Number of Wires:   " << (numberOfWires())                       << endl;
+    os << "Wire Pitch=        " << (wirePitch()/millimeter)       << " mm" << endl;
+    os << "Wire OffSet (+y)=   " << (firstWirePositionInY(1)/centimeter) << " cm" << endl;
+
+    os << endl;
     os << "AnodeToPadSpacing= " << (anodeToPadSpacing()/centimeter) << " cm" << endl;
 
     os << "\nSurvey Geometry:" << endl;
