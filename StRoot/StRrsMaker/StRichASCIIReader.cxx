b9a84c6fdc41abdcaf8ac48b1a9fcbff0073ed73
lasiuk <>
2000-02-08 16:20:20 +0000

    for standalone __ROOT__ defn'd. switch order of arguments, remove quadrant algebra

--- StRoot/StRrsMaker/StRichASCIIReader.cxx
+++ StRoot/StRrsMaker/StRichASCIIReader.cxx
@@ -1,11 +1,12 @@
 /******************************************************
- * $Id: StRichASCIIReader.cxx,v 1.2 2000/01/28 20:35:46 lasiuk Exp $
+ * $Id: StRichASCIIReader.cxx,v 1.3 2000/02/08 16:20:20 lasiuk Exp $
  *
  * Description:
  *******************************************************
  * $Log: StRichASCIIReader.cxx,v $
- * Revision 1.2  2000/01/28 20:35:46  lasiuk
- * inline removed from .cxx files
+ * Revision 1.3  2000/02/08 16:20:20  lasiuk
+ * for standalone __ROOT__ defn'd.
+ * switch order of arguments, remove quadrant algebra
  *
  * Revision 1.3  2000/02/08 16:20:20  lasiuk
  * for standalone __ROOT__ defn'd.
@@ -14,7 +15,7 @@
  * Revision 1.2  2000/01/28 20:35:46  lasiuk
  * inline removed from .cxx files
  *
-
+ * Revision 1.1  2000/01/27 17:06:00  lasiuk
  * Initial Revision
  *
  ******************************************************/
@@ -44,7 +45,7 @@ StRichASCIIReader::StRichASCIIReader(string& file)
 }
 
 int StRichASCIIReader::operator()(StRichGHit& hit )
-		  mdE, mdS, mHitID , mGV );
+{
     if ( read() == 0 ) {
 	hit.fill( mX.x(), mX.y(), mX.z(), mQuadrant,
 		  (mP.x()/abs(mP)), (mP.y()/abs(mP)), (mP.z()/abs(mP)),
@@ -96,36 +97,19 @@ int StRichASCIIReader::read()
     float p[3];
     mIfs >> x[0] >> x[1] >> x[2] >> xx[0] >> xx[1];
     mIfs >> xx[2] >> p[0] >> p[1] >> p[2] >> mTof;
-//     PR(mX);
-//     PR(mP);
-//     PR(mTof);
-//     PR(mdE);
-//     PR(mdS);
-//     PR(mVolume);
+    mIfs >> mdE >> mdS >> mVolume;
+    mX = StThreeVector<double>(x[0],x[1],x[2]);
+    mXX = StThreeVector<double>(xx[0],xx[1],xx[2]);
+    mP = StThreeVector<double>(p[0],p[1],p[2]);
+     PR(mX);
+     PR(mP);
      PR(mTof);
      PR(mdE);
      PR(mdS);
      PR(mVolume);
-
-void StRichASCIIReader::switchQuads(int q)
-{
-    //
-    //  Changes the quadrants disposition
-    //  It also changes any undefined quad value to 0
-    //
-    
-    switch(q) {
-    case 1: { mNvl = 3; break;}
-    case 2: { mNvl = 2; break;}
-    case 3: { mNvl = 4; break;}
-    case 4: { mNvl = 1; break;}
-	
-    default: mNvl = -1;                 // if undefined
-    }
-    
-}
     mQuadrant = whichVolume(mVolume);
     return 0;
+}
     
 #ifndef ST_NO_NAMESPACES
 //}
