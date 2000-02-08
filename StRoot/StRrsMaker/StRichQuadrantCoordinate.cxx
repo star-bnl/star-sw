71ac6b6552bfa3589bd847b284ae4446b837bf37
Brian Lasiuk <lasiuk>
2000-02-08 23:45:48 +0000

    Default constructor initializer changed for CC4.2

--- StRoot/StRrsMaker/StRichQuadrantCoordinate.cxx
+++ StRoot/StRrsMaker/StRichQuadrantCoordinate.cxx
@@ -1,6 +1,6 @@
 /*********************************************************************
  *
- * $Id: StRichQuadrantCoordinate.cxx,v 1.1 2000/02/08 16:34:10 lasiuk Exp $
+ * $Id: StRichQuadrantCoordinate.cxx,v 1.2 2000/02/08 23:45:48 lasiuk Exp $
  *
  * Author: brian Jan 20, 2000
  *
@@ -11,13 +11,17 @@
  **********************************************************************
  *
  * $Log: StRichQuadrantCoordinate.cxx,v $
+ * Revision 1.2  2000/02/08 23:45:48  lasiuk
+ * Default constructor initializer changed for CC4.2
+ *
  * Revision 1.1  2000/02/08 16:34:10  lasiuk
  * Initial Revision:  eventually for StUtilities
  *
  **********************************************************************/
 #include "StRichQuadrantCoordinate.h"
 
-StRichQuadrantCoordinate::StRichQuadrantCoordinate() {/**/}
+StRichQuadrantCoordinate::StRichQuadrantCoordinate()
+    : mPos(0,0,0) {/**/}
 
 StRichQuadrantCoordinate::StRichQuadrantCoordinate(const double x, const double y, const double z, const int q)
     : mPos(x,y,z), mQuadrant(q) { /* nopt */}
