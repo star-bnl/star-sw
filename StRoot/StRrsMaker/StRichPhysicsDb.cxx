2f6b12e5de47dab4a5ae932e07e2e267f179a202
Brian Lasiuk <lasiuk>
2000-02-08 16:29:37 +0000

    include gasGainAmplification factor here instead of geometry

--- StRoot/StRrsMaker/StRichPhysicsDb.cxx
+++ StRoot/StRrsMaker/StRichPhysicsDb.cxx
@@ -1,13 +1,13 @@
-/******************************************************
- * $Id: StRichPhysicsDb.cxx,v 1.2 2000/01/25 22:02:22 lasiuk Exp $
+/*******************************************************************
+ * $Id: StRichPhysicsDb.cxx,v 1.3 2000/02/08 16:29:33 lasiuk Exp $
  *
  * Description:
  *  Implementation of the two databases modules
  *
- ******************************************************
+ *******************************************************************
  * $Log: StRichPhysicsDb.cxx,v $
- * Revision 1.2  2000/01/25 22:02:22  lasiuk
- * Second Revision
+ * Revision 1.3  2000/02/08 16:29:33  lasiuk
+ * include gasGainAmplification factor here instead of geometry
  *
  *
  * Revision 1.3  2000/02/08 16:29:33  lasiuk
@@ -15,19 +15,13 @@
  *
  * Revision 1.2  2000/01/25 22:02:22  lasiuk
  * Second Revision
- ******************************************************/
-
-//#include "TInterpreter.h"              // ROOT
-//#include "TSystem.h"
-//#include "St_Table.h"
-//#include "St_DataSet.h"
-//#include "rich_rich.h"
+ *
  * Revision 1.1  2000/01/18 21:32:03  lasiuk
  * Initial Revision
  *
  *******************************************************************/
 
-#if defined (__SUNPRO_CC) && __SUNPRO_CC >= 0x500
+// STL
 #include <algorithm>
 #include <iostream.h>
 
@@ -41,141 +35,104 @@ using std::max_element;
 using namespace units;
 #endif
 
-    //RRS
+#ifndef ST_NO_NAMESPACES
 //namespace StRichRawData {
 #endif
-    StRichPhysicsDb  *StRichPhysicsDb::p2Db  = 0;
+
 //RRS
-    StRichPhysicsDb::StRichPhysicsDb()
-	: e_distribut(21)
-    {    
-	//star_fill();
-	my_fill();
-	common_fill();
-    }
+#include "StRichPhysicsDb.h"
+
+StRichPhysicsDb  *StRichPhysicsDb::p2Db  = 0;
+    
+StRichPhysicsDb::StRichPhysicsDb()
+    : e_distribut(21)
+{    
+    //star_fill();
     my_fill();
+    common_fill();
+}
+
+
+void StRichPhysicsDb::my_fill()
+    avg_n_inter     = 25/centimeter;
+    mGasGainAmplification = 1.0e4;
+    
+    mPolia           = 0.3;
+    avg_n_inter     = 15.3/centimeter;
+    mGasGainAmplification = 1.0e5;
+    
+    phot2elec       = 0.27;
+    phot2pad        = 0.75;
+    avl2phot        = 7.7e-6;
+    
+    electric_noise  = 800.0;
+    pedestal        = 50;
     adc_factor      = 0.16556*(1.e-15*coulomb);      // adc/femtocoulomb
-    void StRichPhysicsDb::my_fill()
-    {
-	mVersion         = 1;
+    adc_threshold   = 5;
+    channel_width   = 10;           // bits
+    //e_charge        = 1.602e-19;
     
-	mPolia           = 0.3;
-	avg_n_inter     = 25;
-	
-	phot2elec       = 0.27;
-	phot2pad        = 0.75;
-	avl2phot        = 7.7e-6;
-	
-	electric_noise  = 800.0;
-	pedestal        = 50;
-	adc_factor      = 0.16556;      // femtocoulomb/adc_channel
-	adc_threshold   = 5;
-	channel_width   = 10;           // bits
-    }
-    
+}
+
+void StRichPhysicsDb::common_fill()
+{
+    e_distribut[0]  = 79.4;
+    e_distribut[1]  = 91.4;
+    e_distribut[2]  = 94.8;
+    e_distribut[3]  = 96.4;
+    e_distribut[4]  = 97.35;
+    e_distribut[5]  = 97.95;
+    e_distribut[6]  = 98.39;
+    e_distribut[7]  = 98.73;
+    e_distribut[8]  = 99.00;
+    e_distribut[9]  = 99.21;
+    e_distribut[10] = 99.38;
+    e_distribut[11] = 99.51;
+    e_distribut[12] = 99.61;
+    e_distribut[13] = 99.69;
+    e_distribut[14] = 99.75;
+    e_distribut[15] = 99.80;
+    e_distribut[16] = 99.842;
     e_distribut[17] = 99.879;
-
-    void StRichPhysicsDb::common_fill()
-    {
-	e_charge        = 1.602e-19;
-	
-	e_distribut[0]  = 79.4;
-	e_distribut[1]  = 91.4;
-	e_distribut[2]  = 94.8;
-	e_distribut[3]  = 96.4;
-	e_distribut[4]  = 97.35;
-	e_distribut[5]  = 97.95;
-	e_distribut[6]  = 98.39;
-	e_distribut[7]  = 98.73;
-	e_distribut[8]  = 99.00;
-	e_distribut[9]  = 99.21;
-	e_distribut[10] = 99.38;
-	e_distribut[11] = 99.51;
-	e_distribut[12] = 99.61;
-	e_distribut[13] = 99.69;
-	e_distribut[14] = 99.75;
-	e_distribut[15] = 99.80;
-	e_distribut[16] = 99.842;
-	e_distribut[17] = 99.879;
-	e_distribut[18] = 99.912;
-	e_distribut[19] = 99.941;
-	e_distribut[20] = 99.941;
-	
     e_distribut[18] = 99.912;
-	e_max = *(max_element( &e_distribut[0], &e_distribut[20] ));
+    e_distribut[19] = 99.941;
     e_distribut[20] = 99.941;
-    }
+    
     // find max of e_distribut
+    e_max = *(max_element( &e_distribut[0], &e_distribut[20] ));
+    
+}
 
-    /*
-     *  Function handles the access to the
-     *  only instance by using a static
-     *  pointer. 
-     */
-
-    StRichPhysicsDb* StRichPhysicsDb::getDb()
-    {
-	if(!p2Db)
-	    p2Db = new StRichPhysicsDb();
-	return p2Db;
-    }
-
-/*
- *  A fill from a central DB works the following way. 
- *  A STAR_Table that can hold the values is created. It
- *  should be found in the central repository of such tables.
- *  A macro, located in the central repository of DB macros
- *  fills the table with numbers. Then I copy those numbers
- *  to my DBs.
- */
-
-// void StRichPhysicsDb::star_fill()
-// {
-//     gInterpreter->ProcessLine(".L rich_rich.C");
-//     St_DataSet * r = (St_DataSet *) gInterpreter->Calc("CreateTable()");
-//     rich_rich_st * s=(rich_rich_st*)((St_Table*)r)->GetArray();
-    
-//     if ( !s ) cerr << "Error reading from STAR_TABLE!!!\n";
-    
-//     version         = s->version;
-    
-//     polia           = s->polia;
-//     avg_n_inter     = s->avg_n_inter;
-    
-//     phot2elec       = s->phot2elec;
-//     phot2pad        = s->phot2pad;
-//     avl2phot        = s->avl2phot;
-    
-//     electric_noise  = s->electric_noise;
-//     pedestal        = s->pedestal;
-//     adc_factor      = s->adc_factor;      // femtocoulomb/adc_channel
-//     adc_threshold   = s->adc_threshold;
-//     channel_width   = s->channel_width; 
-// }
+StRichPhysicsDb* StRichPhysicsDb::getDb()
+{
     if(!p2Db)
 	p2Db = new StRichPhysicsDb();
     return p2Db;
 }
 
 void StRichPhysicsDb::print(ostream& os) const
+{
     os << "** StRichPhysicsDb::print() ** " << endl;
-    os << "averageNumberOfInteraction= " << averageNumberOfInteractions() << endl;
+    os << " version: " << version() << endl;
+    os << "maximumElectronEnergy=     "  << maximumElectronEnergy() << endl;
+    os << "Ionization:" << endl;
+    os << "polia= " << polia() << endl;
+    os << "averageNumberOfInteraction= " << (averageNumberOfInteractions()/centimeter) << " /cm" << endl;
+    os << "maximumElectronEnergyProbability=     "  << maximumElectronEnergyProbability() << endl;
     os << "gas Gain Amplification=    "  << gasGainAmplification()  << endl;
-    os << "photonToPadEfficiency= " << photonToPadEfficiency() << endl;
-    }
-
-    os << "maximumElectronEnergy= " << maximumElectronEnergy() << endl;
     for(int i=0; i<e_distribut.size(); i++) {
 	os << "electronDistribution[" << i << "]= " << electronDistribution(i) << endl;
     }
+
     os << "\nEfficiency:" << endl;
-    os << "Electronics" << endl;
+    os << "photonToPadEfficiency=     " << photonToPadEfficiency() << endl;
+    os << "photoConversionEfficiency= " << photoConversionEfficiency() << endl;
+    os << "feedBackPhotonProbability= " << feedBackPhotonProbability() << endl;
+
+    os << "\nElectronics:" << endl;
     os << "averagePedestal= "      << averagePedestal()               << " channels" << endl;
-    os << "electronCharge= " << electronCharge() << endl;
-    os << "averagePedestal= " << averagePedestal() << endl;
-    os << "adcConversion= " << adcConversion() << endl;
-    os << "adcThreshold= " << adcThreshold() << endl;
-    os << "adcChannelWidth= " << adcChannelWidth() << endl;
+    os << "adcConversion= "        << (adcConversion()/(1.e-15*coulomb)) << " ADC/fC"   << endl;
+    os << "adcThreshold= "         << adcThreshold()                  << " channels" << endl;
     os << "adcChannelWidth= "      << adcChannelWidth()               << " channels" << endl;
     os << "electronicNoiseLevel= " << electronicNoiseLevel() << endl;
     //os << "electronCharge= " << electronCharge() << endl;
