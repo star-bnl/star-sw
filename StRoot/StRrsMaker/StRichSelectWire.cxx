c4cc7c82a700690fd7a96cc1d986685d60acd0cb
lasiuk <>
2000-02-12 21:55:13 +0000

    take into account gap between top and bottom of chamber

--- StRoot/StRrsMaker/StRichSelectWire.cxx
+++ StRoot/StRrsMaker/StRichSelectWire.cxx
@@ -1,5 +1,5 @@
 /*********************************************************************
- * $Id: StRichSelectWire.cxx,v 1.3 2000/02/08 16:31:54 lasiuk Exp $
+ * $Id: StRichSelectWire.cxx,v 1.4 2000/02/12 21:55:13 lasiuk Exp $
  *
  * Description:
  *
@@ -15,9 +15,13 @@
  *
  *********************************************************************
  * $Log: StRichSelectWire.cxx,v $
- * Revision 1.3  2000/02/08 16:31:54  lasiuk
- * use dbs
+ * Revision 1.4  2000/02/12 21:55:13  lasiuk
+ * take into account gap between top and bottom of
+ * chamber
  *
+ * Revision 1.4  2000/02/12 21:55:13  lasiuk
+ * take into account gap between top and bottom of
+ * chamber
  *
  * Revision 1.3  2000/02/08 16:31:54  lasiuk
  * use dbs
@@ -41,32 +45,43 @@
 #include "StRichViewer.h"
 #endif
 
-    static double first_wire_pos = geoDB->firstWirePositionInX();
+wirePosition StRichSelectWire::operator()( const StRichGHit& hit ) const 
 {
     static StRichGeometryDb* geoDB     = StRichGeometryDb::getDb();  // locals
     static MyRound round;
-	
-    int wire = round(  (first_wire_pos-hit.position().x()) / wire_spacing  ) + 1;
-    //PR(wire);
-	
-    // now taking possible errors in account 
-    // condition given by fiducial cut
-    // +5 is added here because of the possible wires
-    // between quadrants. For now we suppose there are 
-    // 5 additional wires in the gap, but this line should
-    // be rewritten as soon as more information on geometry
-    // is availaible.
-    // if (wire < 1 || wire > geoDB->number_of_wires+5) do smth;
+    //static double first_wire_pos = geoDB->wire_x0;
+    //static double first_wire_pos = geoDB->firstWirePositionInX();
+    //static double wire_spacing   = geoDB->wire_spacing;
+    static double wire_spacing   = geoDB->wirePitch();
+
+    if(hit.position().x() >= 0) {
+    int wireNumber;  // wire Number starts at zero
+	    round((geoDB->firstWirePositionInX(1)-hit.position().x())/geoDB->wirePitch());
+    
+    if(hit.position().y() >= 0) {
+	wireNumber =
+	    nearestInteger((geoDB->firstWirePositionInY(1)-hit.position().y())/geoDB->wirePitch());
+	if(wireNumber<0)
+	    geoDB->firstWirePositionInX(1)-(wireNumber)*geoDB->wirePitch();
+	else if (wireNumber>(geoDB->numberOfWires()/2-1))
+	    wireNumber = geoDB->numberOfWires()/2-1;
+	wirePosition =
+	    round((geoDB->firstWirePositionInX(-1)-hit.position().x())/geoDB->wirePitch())+geoDB->numberOfWires()/2.;
+    }
+    else {
+	wireNumber =
+	    nearestInteger((geoDB->firstWirePositionInY(-1)-hit.position().y())/geoDB->wirePitch())+geoDB->numberOfWires()/2.;
+	if(wireNumber>(geoDB->numberOfWires()-1))
+	    geoDB->firstWirePositionInX(-1)-(wireNumber-geoDB->numberOfWires()/2)*geoDB->wirePitch();
+	else if(wireNumber<geoDB->numberOfWires()/2)
 	    wireNumber = geoDB->numberOfWires()/2;
 	wirePosition =
 	    geoDB->firstWirePositionInY(-1)-(wireNumber-geoDB->numberOfWires()/2)*geoDB->wirePitch();
-	StRichViewer::getView()->mWhichWire->Fill(wire);
-    
-    double wire_position =  first_wire_pos - (wire-1) * wire_spacing;
-#ifdef RICH_WITH_VIEWER
-	return (  wire_position ); 
-	
     }
+    
+#ifdef RICH_WITH_VIEWER
+    if (StRichViewer::histograms )
+	StRichViewer::getView()->mWhichWire->Fill(wireNumber);
 #endif
 	
 	return (wirePosition); 
