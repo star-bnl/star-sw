f707d56846cc276f1a2d7cb71467a15a704731f4
David Hardtke <hardtke@uclink4.berkeley.edu>
2000-02-15 22:21:47 +0000

    Add effective drift distances

--- StRoot/StTpcDb/StRTpcDimensions.h
+++ StRoot/StTpcDb/StRTpcDimensions.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StRTpcDimensions.h,v 1.7 1999/12/16 22:00:52 hardtke Exp $
+ * $Id: StRTpcDimensions.h,v 1.8 2000/02/15 22:21:47 hardtke Exp $
  *
  * Author:  David Hardtke
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StRTpcDimensions.h,v $
+ * Revision 1.8  2000/02/15 22:21:47  hardtke
+ * Add effective drift distances
+ *
  * Revision 1.7  1999/12/16 22:00:52  hardtke
  * add CVS tags
  *
@@ -17,21 +20,32 @@
 #ifndef __STRTPCDIMENSIONS__
 #define __STRTPCDIMENSIONS__
 #include "StTpcDimensionsI.h"
+#include "StTpcPadPlaneI.h"
+#include "StTpcWirePlaneI.h"
 #include "tables/St_tpcDimensions_Table.h"
+#include "tables/St_tpcEffectiveGeom_Table.h"
 
 class StRTpcDimensions : public StTpcDimensionsI {
 
  private:
 
   St_tpcDimensions* mTpc; //!
-
+  St_tpcEffectiveGeom* mEffGeo; //!
+  StTpcPadPlaneI* pp; //!
+  StTpcWirePlaneI* wp; //! 
 
 public:
 
-  StRTpcDimensions() : mTpc(0){;}
-  virtual ~StRTpcDimensions(){;}
+  StRTpcDimensions() : mTpc(0), mEffGeo(0) {;}
   void AddData( St_tpcDimensions* TpcIn);
+  void AddData( St_tpcEffectiveGeom* GeoIn);
   StRTpcDimensions(St_tpcDimensions* TpcIn){ AddData(TpcIn); }
+  StRTpcDimensions(St_tpcDimensions* TpcIn,St_tpcEffectiveGeom* GeoIn){ 
+  AddData(TpcIn); AddData(GeoIn);}
+  virtual ~StRTpcDimensions(){;}
+  void SetPadPlanePointer(StTpcPadPlaneI* in){pp = in;}
+  void SetWirePlanePointer(StTpcWirePlaneI* in){wp = in;}
+ 
 
   //accessors
 
@@ -55,12 +69,22 @@ public:
     float cathodeOuterRadius()  const;
     float cathodeThickness()    const; 
 
+  //TPC distances
+    float innerEffectiveDriftDistance()  const;
+    float outerEffectiveDriftDistance()  const;
+    float gatingGridZ()                  const;
+    float zInnerOffset()                 const;
+    float zOuterOffset()                 const;
+
+
     ClassDef(StRTpcDimensions,0)
 
 };
 
 #ifndef __CINT__
 inline  void StRTpcDimensions::AddData( St_tpcDimensions* TpcIn){ mTpc = TpcIn;}
+
+inline  void StRTpcDimensions::AddData( St_tpcEffectiveGeom* GeoIn){ mEffGeo = GeoIn;}
 
 inline int StRTpcDimensions::numberOfSectors() const {
 return (*mTpc)[0].numberOfSectors;
@@ -109,6 +133,15 @@ return (*mTpc)[0].cathodeOuterRadius;
 inline float StRTpcDimensions::cathodeThickness() const {
 return (*mTpc)[0].cathodeThickness;
 } 
+
+inline float StRTpcDimensions::zInnerOffset() const {
+  return (*mEffGeo)[0].z_inner_offset;
+}
+
+inline float StRTpcDimensions::zOuterOffset() const {
+  return (*mEffGeo)[0].z_outer_offset;
+}
+
 #endif
 #endif
 
