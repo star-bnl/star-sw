c104cb3dd0a3970acd8c3b6b21145a2db34e7ac4
pfachini <>
2000-02-22 20:27:07 +0000

    *** empty log message ***

--- StRoot/StMixerMaker/StMixerEmbedding.cc
+++ StRoot/StMixerMaker/StMixerEmbedding.cc
@@ -15,7 +15,6 @@
 
 #include <utility>
 #include "StMixerEmbedding.hh"
-#include "StDaqLib/TPC/trans_table.hh"
 
 StMixerEmbedding* StMixerEmbedding::mInstance = 0; // static member
 
@@ -58,85 +57,56 @@ void StMixerEmbedding::doEmbedding()
       //}
       currentPad1 = mSector1->timeBinsOfRowAndPad(irow,ipad);
       currentPad2 = mSector2->timeBinsOfRowAndPad(irow,ipad);
-      if (currentPad1.size()>0 && currentPad2.size()==0) {
-	size1 = static_cast<int>(currentPad1.size());
+      if (!currentPad1.size() && !currentPad2.size()) continue;
+      if (currentPad1.size() && !currentPad2.size()) {
 	for(mTimeSequenceIterator1  = currentPad1.begin();
 	    mTimeSequenceIterator1 != currentPad1.end();
 	    mTimeSequenceIterator1++) {
 	  bin1 = static_cast<int>(mTimeSequenceIterator1->time());
-	  conversion1 = log8to10_table[static_cast<int>(mTimeSequenceIterator1->amplitude())];
+	  conversion1 = static_cast<float>(mTimeSequenceIterator1->amplitude());
+	  //if (irow==3 && ipad==54) cout << irow << ' ' << ipad << ' ' << bin1 << ' ' << conversion1 <<endl;  
 	  StMixerAnalogSignal padSignal(bin1,conversion1);
 	  mSector->addEntry(irow,ipad,padSignal);
-	  //if (irow==14) cout << "1nomixer" << ' ' << irow << ' ' << ipad << ' ' << bin1 << ' ' << conversion1  << endl;
 	}
       }
-      if (currentPad1.size()==0 && currentPad2.size()>0) {
-	size2 = static_cast<int>(currentPad2.size());
+      if (!currentPad1.size() && currentPad2.size()) {
 	for(mTimeSequenceIterator2  = currentPad2.begin();
 	    mTimeSequenceIterator2 != currentPad2.end();
 	    mTimeSequenceIterator2++) {
 	  bin2 = static_cast<int>(mTimeSequenceIterator2->time());
-	  conversion2 = log8to10_table[static_cast<int>(mTimeSequenceIterator2->amplitude())];
+	  conversion2 = static_cast<float>(mTimeSequenceIterator2->amplitude());
 	  StMixerAnalogSignal padSignal(bin2,conversion2);
 	  mSector->addEntry(irow,ipad,padSignal);
 	  //if (irow==14) cout << "2nomixer" << ' ' << irow << ' ' << ipad << ' ' << bin2 << ' ' << conversion2  << endl;
 	}
       }
-      if (!currentPad1.size() && !currentPad2.size()) continue;
-      size2 = static_cast<int>(currentPad2.size());
-      size1 = static_cast<int>(currentPad1.size());
-      int begin1;
-      int begin2;
-      bool first=true;
-      for(mTimeSequenceIterator1  = currentPad1.begin();
-	  mTimeSequenceIterator1 != currentPad1.end();
-	  mTimeSequenceIterator1++) {
+      if (currentPad1.size() && currentPad2.size()) {
+	for(int bin=0; bin<mTimeBins; bin++) {
+	  accum[bin]=0;
+	}
+	for(mTimeSequenceIterator1  = currentPad1.begin();
+	    mTimeSequenceIterator1 != currentPad1.end();
+	    mTimeSequenceIterator1++) {
+	  bin1 = static_cast<int>(mTimeSequenceIterator1->time());
+	  conversion1 = static_cast<float>(mTimeSequenceIterator1->amplitude());
+	  accum[bin1]=conversion1;
+	}
 	for(mTimeSequenceIterator2  = currentPad2.begin();
 	    mTimeSequenceIterator2 != currentPad2.end();
 	    mTimeSequenceIterator2++) {
-	  if (first) {
-	    //PR(first);
-	    begin1 = static_cast<int>(currentPad1.begin()->time());
-	    begin2 = static_cast<int>(currentPad2.begin()->time());
-	    first=0;
-	  }
-	  bin1 = static_cast<int>(mTimeSequenceIterator1->time());
 	  bin2 = static_cast<int>(mTimeSequenceIterator2->time());
-	  conversion1=log8to10_table[static_cast<int>(mTimeSequenceIterator1->amplitude())];
-	  conversion2=log8to10_table[static_cast<int>(mTimeSequenceIterator2->amplitude())];
-	  if (bin1==bin2) {
-	    conversionmixer=conversion1+conversion2;
-	    if (conversionmixer>1015) conversionmixer=1015;
-	    //if (irow==14) cout << "2mixer" << ' ' << irow << ' ' << ipad << ' ' << bin2 << ' ' << conversionmixer  << endl;
-	    StMixerAnalogSignal padSignal(bin1,conversionmixer);
-	    mSector->addEntry(irow,ipad,padSignal);
-	    break;
-	  }
-	  if(bin1<bin2) {
-	    //if (irow==14) cout << "1<" << ' ' << irow << ' ' << ipad << ' ' << bin1 << ' ' << conversion1  << endl;
-	    StMixerAnalogSignal padSignal(bin1,conversion1);
-	    mSector->addEntry(irow,ipad,padSignal);
-	    break;
-	  }
-	  if(((begin1+size1-1)==bin1) && bin2>bin1) {
-	    //if (irow==14) cout << "2" << ' ' << irow << ' ' << ipad << ' ' << bin2 << ' ' << conversion2  << endl;
-	    StMixerAnalogSignal padSignal(bin2,conversion2);
-	    mSector->addEntry(irow,ipad,padSignal);
-	  }
-	  if((begin1==bin1) && bin2<bin1) {
-	    //if (irow==14) cout << "2" << ' ' << irow << ' ' << ipad << ' ' << bin2 << ' ' << conversion2  << endl;
-	    StMixerAnalogSignal padSignal(bin2,conversion2);
-	    mSector->addEntry(irow,ipad,padSignal);
-	  }
-	  if(((begin2+size2-1)==bin2) && bin2<bin1) {
-	    //if (irow==14) cout << "1" << ' ' << irow << ' ' << ipad << ' ' << bin1 << ' ' << conversion1  << endl;
-	    StMixerAnalogSignal padSignal(bin1,conversion1);
-	    mSector->addEntry(irow,ipad,padSignal);
+	  conversion2 = static_cast<float>(mTimeSequenceIterator2->amplitude());
+	  accum[bin2]+=conversion2;
+	  if (accum[bin2]>1015) accum[bin2]=1015; 
+	}
+	for(int bin=0; bin<mTimeBins; bin++) {
+	  if (accum[bin]>0) {
+	  //if (irow==3 && ipad==54) cout << irow << ' ' << ipad << ' ' << bin << ' ' << accum[bin] <<endl;  
+	  StMixerAnalogSignal padSignal(bin,accum[bin]);
+	  mSector->addEntry(irow,ipad,padSignal);
 	  }
 	}
       }
     }
   }
 }
-
-
