66c533cf1929d315197e3e4a40a00655f0cc1a00
caines <>
2000-02-24 16:58:39 +0000

    Change srs histogram names to srs*

--- StRoot/St_srs_Maker/St_srs_Maker.cxx
+++ StRoot/St_srs_Maker/St_srs_Maker.cxx
@@ -1,12 +1,15 @@
-//$Id: St_srs_Maker.cxx,v 1.20 2000/02/17 23:23:03 caines Exp $
+//$Id: St_srs_Maker.cxx,v 1.21 2000/02/24 16:58:39 caines Exp $
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
 // St_srs_Maker class for Makers                                        //
 // Author : Anon                                                       //
 //////////////////////////////////////////////////////////////////////////
 //$Log: St_srs_Maker.cxx,v $
+//Revision 1.21  2000/02/24 16:58:39  caines
+//Change srs histogram names to srs*
+//
 //Revision 1.20  2000/02/17 23:23:03  caines
-//Adjust for using new geom files using hardware ids
+// Adjust for using new geom files using hardware ids
 //
 //Revision 1.19  1999/12/23 16:38:48  caines
 //Added cvs ID strings
@@ -37,6 +40,7 @@ Int_t St_srs_Maker::Init(){
 // 		Create tables
    St_DataSetIter       local(GetInputDB("params/svt"));
 
+
 // 		geometry parameters
    m_shape       = (St_svg_shape   *) local("svgpars/shape");
    m_config      = (St_svg_config  *) local("svgpars/config");
@@ -56,42 +60,42 @@ Int_t St_srs_Maker::Init(){
    m_srs_direct  = (St_srs_direct  *) local("srspars/srs_direct");
 
 // 		Create Histograms    
-   m_x_vs_y = new TH2F("si_x_vs_y","X vs Y of Si space points",
+   m_x_vs_y = new TH2F("srs_x_vs_y","X vs Y of Si space points",
                            300,-30,30,300,-30,30);
    m_x_vs_y->SetYTitle("y cm");
    m_x_vs_y->SetXTitle("x cm");
 
-   m_waf_no1 = new TH2F("si_layer1"," Si z vs ladder no.",
+   m_waf_no1 = new TH2F("srs_layer1"," Si z vs ladder no.",
                            100,-25.,25.,20,0.5,8.5);
    m_waf_no1->SetYTitle("ladder no");
    m_waf_no1->SetXTitle("Z cm");
 
-   m_waf_no2 = new TH2F("si_layer2"," Si z vs ladder no.",
+   m_waf_no2 = new TH2F("srs_layer2"," Si z vs ladder no.",
 			100,-25.,25.,20,0.5,8.5);
    m_waf_no2->SetYTitle("ladder no");
    m_waf_no2->SetXTitle("Z cm");
 
-   m_waf_no3 = new TH2F("si_layer3"," Si z vs ladder no.",
+   m_waf_no3 = new TH2F("srs_layer3"," Si z vs ladder no.",
 			100,-25.,25.,20,0.5,12.5);
    m_waf_no3->SetYTitle("ladder no");
    m_waf_no3->SetXTitle("Z cm");
 
-   m_waf_no4 = new TH2F("si_layer4"," Si z vs ladder no.",
+   m_waf_no4 = new TH2F("srs_layer4"," Si z vs ladder no.",
 			100,-25.,25.,20,0.5,12.5);
    m_waf_no4->SetYTitle("ladder no");
    m_waf_no4->SetXTitle("Z cm");
 
-   m_waf_no5 = new TH2F("si_layer5"," Si z vs ladder no.",
+   m_waf_no5 = new TH2F("srs_layer5"," Si z vs ladder no.",
 			100,-25.,25.,20,0.5,16.5);
    m_waf_no5->SetYTitle("ladder no");
    m_waf_no5->SetXTitle("Z cm");
 
-   m_waf_no6 = new TH2F("si_layer6"," Si z vs ladder no.",
+   m_waf_no6 = new TH2F("srs_layer6"," Si z vs ladder no.",
 			100,-25.,25.,20,0.5,16.5);
    m_waf_no6->SetYTitle("ladder no");
    m_waf_no6->SetXTitle("Z cm");
 
-   m_waf_no7 = new TH2F("si_layer7"," Si z vs ladder no.",
+   m_waf_no7 = new TH2F("srs_layer7"," Si z vs ladder no.",
 			100,-40.,40.,100,0.5,20.5);
    m_waf_no7->SetYTitle("ladder no");
    m_waf_no7->SetXTitle("Z cm");
