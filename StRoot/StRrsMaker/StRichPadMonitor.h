5946a3092fd7ba17e77fae1f4f52a9e6474f2476
lasiuk <>
2000-02-29 18:19:38 +0000

    Split needed classes into individual files

--- StRoot/StRrsMaker/StRichPadMonitor.h
+++ StRoot/StRrsMaker/StRichPadMonitor.h
@@ -1,80 +1,47 @@
-/******************************************************
- * $Id: StRichPadMonitor.h,v 1.1 2000/02/12 21:57:57 lasiuk Exp $
+/***************************************************************
+ * $Id: StRichPadMonitor.h,v 1.2 2000/02/29 18:19:38 lasiuk Exp $
  * Description:
  *  First aTtempt at a simple Pad Monitor.
  *  Runs only in ROOT
  *
- * Auxiliary classes are kept here for now as well
+ ***************************************************************
  *
  * $Log: StRichPadMonitor.h,v $
- * Revision 1.1  2000/02/12 21:57:57  lasiuk
- * Initial revision
+ * Revision 1.2  2000/02/29 18:19:38  lasiuk
+ * Split needed classes into individual files
  *
- ******************************************************/
+ * Revision 1.2  2000/02/29 18:19:38  lasiuk
+ * Split needed classes into individual files
+ *
  * Revision 1.1  2000/02/12 21:57:57  lasiuk
  * Initial revision
  ***************************************************************/
 #ifdef __ROOT__
+//#include <vector>
+using std::vector;
+#endif
 
+#include "TObject.h"
 #include "TCanvas.h"
-#include "TBox.h"
+#include "TObjArray.h"
+//#include "St_Node.h"
+#include "TText.h"
+#include "TPaveLabel.h"
 
-#include "StRichGeometryDb.h"
-#include "StRichCoordinateTransform.h"
-///////////////////////////////////////////////////////////////////////
-// Object which is produced by the Slow simulator
-//
-class StRichSinglePixel  {
-public:
-    StRichSinglePixel();
-    StRichSinglePixel(int p, int r, int adc);
-    
-    ~StRichSinglePixel();
+#include "TColor.h"
 
-    //StRichSinglePixel(const StRichSinglePixel&) {/*use default*/|
-    //StRichSinglePixel& operator=(const StRichSinglePixel&) {/*use default*/|
-    
-public:
-    double mLength;
-    double mWidth;
-    double mPad;
-    double mRow;
-    int    mAdc;
-};
-
-////////////////////////////////////////////////////////////////////////////
-// Object which we want ROOT to draw
-//
-class StRichDrawableTPad : public TBox {
-public:
-    StRichDrawableTPad();
-    StRichDrawableTPad(double xl, double yl, double xu, double yu, int adc);
-    
-    virtual ~StRichDrawableTPad();
+class StRichPadMonitorText;
+class StRichGeometryDb;
 class StRichCoordinateTransform;
-    //StRichDrawableTPad(const StRichDrawableTPad&) {/*use default*/|
-    //StRichDrawableTPad& operator=(const StRichDrawableTPad&) {/*use default*/}
-    
-    void draw();
-    void setPosition(int p, int r);
-
-public:
-    int mAdc;
-    double mLength;
-    double mWidth;
-    double mPad;
-    double mRow;
-};
-
-///////////////////////////////////////////////////////////////////////////////////////
-// the pad monitor which is a canvas with some boundaries drawn
-//
 class StRichSinglePixel;
 class StRichDrawableTPad;
 
 class StRichPadMonitor : public TObject {
 public:
     static StRichPadMonitor* getInstance(StRichGeometryDb*);
+    
+    ~StRichPadMonitor();
+
     //StRichPadMonitor(const StRichPadMonitor&) {/*nopt*/}
     //StRichPadMonitor& operator=(const StRichPadMonitor&) {/*nopt*/}
 
@@ -83,12 +50,21 @@ public:
     void update();
     void addOuterRingPoint(double x, double y);
     void drawRing();
+    
+    StRichPadMonitor(StRichGeometryDb*);
 
-    //This better be private lateer!
-public:
+    void    drawColorBox();
     Color_t GetColorAttribute(double amp);
     
+// #ifndef ST_NO_DEF_TEMPLATE_ARGS
+//     vector<StRichDrawableTPad*>   mAllFilledPads;//!
+// #else
+//     vector<StRichDrawableTPad*, allocator<StRichDrawableTPad*> >   mAllFilledPads;//!
+// #endif
+    vector<double>     mYPoints;
     vector<double>     mXOPoints;
+    vector<double>     mYOPoints;
+    //St_Node *mHall;
 
     StRichPadMonitorText*      mTextWindow;
     
