45d3e650c7872918cc36337c16e306dc73e69f45
Thomas Ullrich <thomas.ullrich@bnl.gov>
2000-03-02 12:43:51 +0000

    Method can be passed as argument to constructor. Default method is truncated mean.

--- StRoot/StEvent/StTpcDedxPidAlgorithm.h
+++ StRoot/StEvent/StTpcDedxPidAlgorithm.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTpcDedxPidAlgorithm.h,v 2.5 1999/12/21 15:09:14 ullrich Exp $
+ * $Id: StTpcDedxPidAlgorithm.h,v 2.6 2000/03/02 12:43:51 ullrich Exp $
  *
  * Author: Thomas Ullrich, Sep 1999
  ***************************************************************************
@@ -10,8 +10,9 @@
  ***************************************************************************
  *
  * $Log: StTpcDedxPidAlgorithm.h,v $
- * Revision 2.5  1999/12/21 15:09:14  ullrich
- * Modified to cope with new compiler version on Sun (CC5.0).
+ * Revision 2.6  2000/03/02 12:43:51  ullrich
+ * Method can be passed as argument to constructor. Default
+ * method is truncated mean.
  *
  * Revision 2.5  1999/12/21 15:09:14  ullrich
  * Modified to cope with new compiler version on Sun (CC5.0).
@@ -33,6 +34,7 @@
 #define StTpcDedxPidAlgorithm_hh
 #include <vector>
 #include "StFunctional.h"
+#include "StEnumerations.h"
 #if !defined(ST_NO_NAMESPACES)
 using std::vector;
 #endif
@@ -41,19 +43,21 @@ class StDedxPidTraits;
 
 class StTpcDedxPidAlgorithm : public StPidAlgorithm {
 public:
-    StTpcDedxPidAlgorithm();
+    StTpcDedxPidAlgorithm(StDedxMethod = kTruncatedMeanId);
     
     StParticleDefinition*  operator() (const StTrack&, const StSPtrVecTrackPidTraits&);
-    const StDedxPidTraits* traits() const;    
+
+    const  StDedxPidTraits* traits() const;
     double numberOfSigma(const StParticleDefinition*) const;
     double meanPidFunction(const StParticleDefinition*) const;
     double sigmaPidFunction(const StParticleDefinition*) const;
-   
+
 private:
     const StDedxPidTraits*        mTraits;       //!
     const StTrack*                mTrack;        //!
+    StDedxMethod                  mDedxMethod;   //!
 #if defined(ST_NO_TEMPLATE_DEF_ARGS)
-    vector<StParticleDefinition*, 
+    vector<StParticleDefinition*,
            allocator<StParticleDefinition*> > mParticles; //!
 #else
     vector<StParticleDefinition*> mParticles;    //!
