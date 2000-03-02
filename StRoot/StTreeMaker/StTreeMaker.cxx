e53375df25f23366da2fd3949d5346597a690895
perev <>
2000-03-02 18:59:14 +0000

    Again write as default for branch

--- StRoot/StTreeMaker/StTreeMaker.cxx
+++ StRoot/StTreeMaker/StTreeMaker.cxx
@@ -220,7 +220,8 @@ void StTreeMaker::UpdateTree(Int_t flag)
         continue;}
         
     br = (StBranch*)fTree->Find(updName);
-    if (!br && fIOMode!="r" && fIOMode!="0") br = new StBranch(updName,fTree,"w");
+    if (!br && fIOMode!="r" && fIOMode!="0") {
+      br = new StBranch(updName,fTree); br->SetIOMode("w");}
       
     if (!br) 				continue;
     if (!updMode.IsNull() || !updFile.IsNull()) br->SetFile(updFile,updMode);  
