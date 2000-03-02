f98781c4622efe10fc59e36571abab8106d8a640
Thomas Ullrich <thomas.ullrich@bnl.gov>
2000-03-02 13:13:19 +0000

    Modified such that code compiles on Linux without warnings.

--- StRoot/StAnalysisMaker/countPrimaryPions.cc
+++ StRoot/StAnalysisMaker/countPrimaryPions.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: countPrimaryPions.cc,v 2.3 2000/02/27 20:50:39 ogilvie Exp $
+ * $Id: countPrimaryPions.cc,v 2.4 2000/03/02 13:13:19 ullrich Exp $
  *
  * Author: Craig Ogilvie, MIT, Dec 199
  ***************************************************************************
@@ -8,53 +8,53 @@
  * Description:  Demonstrates a simple use of PID information in StEvent
  *
  ***************************************************************************
-
+ *
+ * $Log: countPrimaryPions.cc,v $
+ * Revision 2.4  2000/03/02 13:13:19  ullrich
+ * Modified such that code compiles on Linux without warnings.
+ *
  **************************************************************************/
-#include "StEventTypes.h"
-#include "StParticleDefinition.hh"
-#include "StParticleTable.hh"
+#include "StEventTypes.h"    // includes all basic StEvent classes
+#include "StPionPlus.hh"
 #include "StTpcDedxPidAlgorithm.h"
 
-static const char rcsid[] = "$Id: countPrimaryPions.cc,v 2.3 2000/02/27 20:50:39 ogilvie Exp $";
+static const char rcsid[] = "$Id: countPrimaryPions.cc,v 2.4 2000/03/02 13:13:19 ullrich Exp $";
 
 long countPrimaryPions(StEvent& event)
 {
     long counter = 0;
-    StParticleDefinition* particle;  
-    particle =  StParticleTable::instance()->findParticle("pi+") ;
 
     // return if no primary vertex
-    if (event.primaryVertex()==0) {
-       return counter;
-    }
+    if (!event.primaryVertex()) return counter;
+
     const StSPtrVecPrimaryTrack& tracks = event.primaryVertex()->daughters();
+    
     // return if no primary tracks
-    if (tracks.size() < 0) {
-      return counter;
-    }
+    if (!tracks.size()) return counter;
 
     // collection of primary tracks
-    StPrimaryTrackIterator iter;
-    StPrimaryTrack *track;
+    StPrimaryTrack         *track;
 
-    for (iter = tracks.begin();
-       iter != tracks.end(); iter++) {
-       track = *iter;
-       if (track==0) continue;
+    for (StPrimaryTrackIterator iter = tracks.begin();
+	 iter != tracks.end(); iter++) {
+
+	track = *iter;
+	if (track==0) continue;
       
-       StTpcDedxPidAlgorithm tpcDedxAlgorithm;
-       //
-       // now apply pid algorithm tpcDedxAlgorithm, which finds the tpc dedx
-       // object in the collection of pidTraits, and initializes
-       // the data members of the algorithm
-       // which can then be accessed through member functions
-       // of the algorithm e.g. numberOfSigma
-       // guess is currently not used
-       //
-       const StParticleDefinition *guess = track->pidTraits(tpcDedxAlgorithm);
-       double deviant = tpcDedxAlgorithm.numberOfSigma(particle); 
-       if (fabs(deviant) < 3) counter++;
-	       // counts for both charge signs of particle          
+	StTpcDedxPidAlgorithm tpcDedxAlgorithm;
+
+	//
+	// Now apply pid algorithm tpcDedxAlgorithm, which finds the tpc dedx
+	// object in the collection of pidTraits, and initializes
+	// the data members of the algorithm
+	// which can then be accessed through member functions
+	// of the algorithm e.g. numberOfSigma
+	// guess is currently not used
+	//
+	track->pidTraits(tpcDedxAlgorithm);	// ignore return value
+	double deviant = tpcDedxAlgorithm.numberOfSigma(StPionPlus::instance()); 
+	if (fabs(deviant) < 3) counter++;
+	// counts for both charge signs of particle          
     }
     return counter;
 }
