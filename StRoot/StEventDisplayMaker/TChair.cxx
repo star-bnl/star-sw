8e82530ecc7304c38e1c1e9edeba730a66300a48
fine <>
2000-03-09 21:57:03 +0000

    TChair class to be moved to ROOT later

---
+++ StRoot/StEventDisplayMaker/TChair.cxx
@@ -0,0 +1,12 @@
+#include "TChair.h"
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+//  TChair                                                              //
+//                                                                      //
+//  It is a base class to create a custom interface for St_Table objects//
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+ClassImp(TChair)
+
