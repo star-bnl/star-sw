85e2e8dfbbf8b7c106112188594deaf82ef405d8
cvs2git <cvs2git>
2000-03-10 23:03:53 +0000

    This commit was manufactured by cvs2git to create tag 'MDC3'.

--- asps/agi/gst/agsim/db.c
+++
@@ -1,8 +0,0 @@
-/*CMZ :  2.00/00 24/11/99  00.33.54  by  Pavel Nevski*/
-/*-- Author :    Sasha Vanyashin  28/06/99*/
-/*DECK,DBDUMMY,T=CC,IF=-MYSQL.*/
-/* extern "C" for c++ */
-void  dbset_(){};
-void  dbuse_(){};
-void  dbfill_(){};
- 
