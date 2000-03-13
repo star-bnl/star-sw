495a95a3dfd08b6a9c1a4ae69a3326462be6c6bf
R. Jefferson (Jeff) Porter <rjporter@lbl.gov>
2000-03-13 22:18:10 +0000

    added RunLog with MagFactor in database for all running

--- StRoot/macros/test/DbTest.C
+++ StRoot/macros/test/DbTest.C
@@ -15,21 +15,27 @@ void DbTest(){
 
   St_db_Maker *dbMk = new St_db_Maker("RunParams","MySQL:RunParams");
   St_db_Maker *dbMk2 = new St_db_Maker("Geometry","MySQL:Geometry");
+  St_db_Maker *dbMk3 = new St_db_Maker("RunLog","MySQL:RunLog");
 
-  // Initial with [database]_hierarchy structure known to St_db_Maker
-
+ 
   dbMk->Init();
   dbMk2->Init();
+  dbMk3->Init();
 
   // Make reaquests for data
 
+  // choose timestamp 
+  dbMk->SetDateTime(19990101,10000);
+
   p = dbMk->GetDataBase("RunParams/tpc");
   p2 = dbMk2->GetDataBase("Geometry/tpc");
+  p3 = dbMk3->GetDataBase("RunLog");
 
   // Browse these requests
 
   TBrowser *b = new TBrowser("RunParams",p);
   TBrowser *b2 = new TBrowser("Geometry",p2);
+  TBrowser *b3 = new TBrowser("RunLog",p3);
 }
 
 
