c4d430c674bf926a8e7054e6a69bc70a6f3b4ee3
ullrich <>
2000-03-17 14:52:23 +0000

    Method helix() now checks for q=0 and sets curvature = 0 (which it should be anyhow).

--- StRoot/StEvent/StHelixModel.cxx
+++ StRoot/StEvent/StHelixModel.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StHelixModel.cxx,v 2.2 1999/10/28 22:25:42 ullrich Exp $
+ * $Id: StHelixModel.cxx,v 2.3 2000/03/17 14:52:23 ullrich Exp $
  *
  * Author: Thomas Ullrich, Sep 1999
  ***************************************************************************
@@ -10,8 +10,9 @@
  ***************************************************************************
  *
  * $Log: StHelixModel.cxx,v $
- * Revision 2.2  1999/10/28 22:25:42  ullrich
- * Adapted new StArray version. First version to compile on Linux and Sun.
+ * Revision 2.3  2000/03/17 14:52:23  ullrich
+ * Method helix() now checks for q=0 and sets
+ * curvature = 0 (which it should be anyhow).
  *
  * Revision 2.2  1999/10/28 22:25:42  ullrich
  * Adapted new StArray version. First version to compile on Linux and Sun.
@@ -28,7 +29,7 @@
 
 ClassImp(StHelixModel)
 
-static const char rcsid[] = "$Id: StHelixModel.cxx,v 2.2 1999/10/28 22:25:42 ullrich Exp $";
+static const char rcsid[] = "$Id: StHelixModel.cxx,v 2.3 2000/03/17 14:52:23 ullrich Exp $";
 
 StHelixModel::StHelixModel() : mModel(helixModel)
 {
@@ -97,10 +98,35 @@ StHelixModel::momentum() const {return mMomentum;}
 StPhysicalHelixD
 StHelixModel::helix() const
 {
-    int h = mCharge > 0 ? -1 : 1;  // -sign(q*B)
+    //
+    //  No charge no curvature.
+    //  Agreement is to use q=0 as equivalent
+    //  to saying curvature = 0. It should be
+    //  0 but we better make sure.
+    //
+    double curvature = mCurvature;
+    if (mCharge == 0) curvature = 0;
+
+    //
+    //  h = -sign(q*B)
+    //  Here we assume B > 0. Here we run into
+    //  problems if STAR ever switches polarisation.
+    //  For B=0, h is ill defined and we can use
+    //  +1 or -1. Both work as long as the phase
+    //  is calculated correctly. Here we use the
+    //  +1 convention.
+    //
+    int h;
+    if (mCharge == 0)
+	h = 1;
+    else if (mCharge > 0)
+	h = -1;
+    else
+	h = 1;
+    
     double phase = mPsi-h*pi/2;
     
-    return StPhysicalHelixD(mCurvature,   // 1/cm
+    return StPhysicalHelixD(curvature,    // 1/cm
                             mDipAngle,    // radian
                             phase,        // radian
                             mOrigin,      // cm
