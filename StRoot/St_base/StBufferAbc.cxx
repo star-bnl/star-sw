4b26bbe7cfe2531359e404611ca95023bda5f22d
cvs2git <cvs2git>
2000-03-21 16:35:19 +0000

    This commit was manufactured by cvs2git to create tag 'preV0'.

--- StRoot/St_base/StBufferAbc.cxx
+++
@@ -1,2 +0,0 @@
-#include "StBufferAbc.h"
-ClassImp(StBufferAbc)
--- asps/rexe/rexeLinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ global gMC;
-#pragma link C++ class StarMC;
-#pragma link C++ class TGeant3;
-#pragma link C++ class TPaveTree;
-#endif
