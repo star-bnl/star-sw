cf3faca498e24d837b74ec126b0e315d975d2db2
ogilvie <>
2000-03-21 16:35:18 +0000

    implementing some base class functions

---
+++ StRoot/StSpectraMaker/StSpectraCut.cc
@@ -0,0 +1,9 @@
+#include "StSpectraCut.h"
+
+StSpectraCut::StSpectraCut(double lowLimit, double highLimit){
+
+  mLowLimit = lowLimit;
+  mHighLimit = highLimit;
+
+  // place to construct diagnostics
+}
