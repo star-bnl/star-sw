fd1dfc0ba07c36c4eeee155e35762a79ee3335c2
Valeri Fine <val.fine@gmail.com>
2000-03-23 14:56:18 +0000

    Adjusted to libSTAR and ROOT 2.24

--- StRoot/St_db_Maker/St_db_Maker.h
+++ StRoot/St_db_Maker/St_db_Maker.h
@@ -1,5 +1,8 @@
-// $Id: St_db_Maker.h,v 1.10 2000/02/26 01:45:04 fine Exp $
+// $Id: St_db_Maker.h,v 1.11 2000/03/23 14:55:56 fine Exp $
 // $Log: St_db_Maker.h,v $
+// Revision 1.11  2000/03/23 14:55:56  fine
+// Adjusted to libSTAR and ROOT 2.24
+//
 // Revision 1.10  2000/02/26 01:45:04  fine
 // CVS id has been introduced
 // 
@@ -17,13 +20,12 @@
 #ifndef StMaker_H
 #include "StMaker.h"
 #endif 
-#include "St_Table.h"                      
+#include "TTable.h"                      
 
 #include "TDatime.h"
 #include "StDbBroker/dbConfig.h"
 
-class St_FileSet;
-class St_DataSet;
+class TFileSet;
 class TList;
 class TBrowser;
 class StDbBroker;
@@ -36,7 +38,7 @@ enum DBConst {kMinTime = 19950101, kMaxTime = 20380101};
 class St_db_Maker : public StMaker {
 private:
 
-  St_DataSet *fDataBase;       	//! DB structure
+  TDataSet *fDataBase;       	//! DB structure
   StDbBroker *fDBBroker;	//!MySql broker 
   St_dbConfig *fHierarchy; //!
   TString     fMainDir;        	//! The main root directory for the calibrarion data
@@ -46,7 +48,7 @@ private:
   TDatime     fDBTime;		//! Own DB time stamp
   Int_t       fUpdateMode;	//! 
 
-//  static Char_t fVersionCVS = "$Id: St_db_Maker.h,v 1.10 2000/02/26 01:45:04 fine Exp $";
+//  static Char_t fVersionCVS = "$Id: St_db_Maker.h,v 1.11 2000/03/23 14:55:56 fine Exp $";
  protected:
  public: 
                    St_db_Maker(const char *name,const char *maindir,const char *userdir=0);
@@ -66,35 +68,36 @@ private:
    virtual void Clear(Option_t *opt=""){if(opt){/*unused*/}};
 
 protected:
-   virtual St_DataSet* UpdateDB (St_DataSet* ds);
-   virtual int UpdateTable(UInt_t parId, St_Table* dat, TDatime val[2]);
-   virtual St_DataSet *LoadTable(St_DataSet* left);
-   virtual St_DataSet *FindLeft(St_ValiSet *val, TDatime vals[2]);
-   virtual St_DataSet *OpenMySQL(const char* dbname);
+   virtual TDataSet* UpdateDB (TDataSet* ds);
+   virtual int UpdateTable(UInt_t parId, TTable* dat, TDatime val[2]);
+   virtual TDataSet *LoadTable(TDataSet* left);
+   virtual TDataSet *FindLeft(St_ValiSet *val, TDatime vals[2]);
+   virtual TDataSet *OpenMySQL(const char* dbname);
 
-   static EDataSetPass UpdateDB (St_DataSet* ds,void *user );
-   static EDataSetPass PrepareDB(St_DataSet* ds,void *user );
+   static EDataSetPass UpdateDB (TDataSet* ds,void *user );
+   static EDataSetPass PrepareDB(TDataSet* ds,void *user );
 public:
    static TDatime  Time(const char *filename);
    static int      Kind(const char *filename);
 
    virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: St_db_Maker.h,v 1.10 2000/02/26 01:45:04 fine Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: St_db_Maker.h,v 1.11 2000/03/23 14:55:56 fine Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
    ClassDef(St_db_Maker, 0)   //StAF chain virtual base class for Makers
 };
 
-class St_dbConfig : public St_Table   
+class St_dbConfig : public TTable   
 {                                          
 protected:                                 
-  static St_tableDescriptor *fgColDescriptors;    
-  virtual St_tableDescriptor *GetDescriptorPointer() const { return fgColDescriptors;}       
-  virtual void  SetDescriptorPointer(St_tableDescriptor *list) const { fgColDescriptors = list;}  
+  static TTableDescriptor *fgColDescriptors;    
+  virtual TTableDescriptor *GetDescriptorPointer() const { return fgColDescriptors;}       
+  virtual void  SetDescriptorPointer(TTableDescriptor *list) const { fgColDescriptors = list;}  
 public:                                    
-  St_dbConfig() : St_Table("dbConfig",sizeof(dbConfig_st)) {SetType("dbConfig");}           
-  St_dbConfig(Text_t *name) : St_Table(name,sizeof(dbConfig_st)) {SetType("dbConfig");}          
-  St_dbConfig(Int_t n): St_Table("dbConfig",n,sizeof(dbConfig_st)) {SetType("dbConfig");}   
-  St_dbConfig(Text_t *name,Int_t n): St_Table(name,n,sizeof(dbConfig_st)) {SetType("dbConfig");} 
+
+  St_dbConfig() : TTable("dbConfig",sizeof(dbConfig_st)) {SetType("dbConfig");}           
+  St_dbConfig(Text_t *name) : TTable(name,sizeof(dbConfig_st)) {SetType("dbConfig");}          
+  St_dbConfig(Int_t n): TTable("dbConfig",n,sizeof(dbConfig_st)) {SetType("dbConfig");}   
+  St_dbConfig(Text_t *name,Int_t n): TTable(name,n,sizeof(dbConfig_st)) {SetType("dbConfig");} 
   dbConfig_st *GetTable(){ return (dbConfig_st *)GetArray();}                                            
                                            
   ClassDef(St_dbConfig,0) //  
