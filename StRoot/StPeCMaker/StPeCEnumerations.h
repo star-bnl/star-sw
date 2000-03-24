94e7975f9304f3a3851817cd8288b107f2234ac6
nystrand <>
2000-03-24 22:38:11 +0000

    First version

---
+++ StRoot/StPeCMaker/StPeCEnumerations.h
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////
+//
+// $Id: StPeCEnumerations.h,v 1.1 2000/03/24 22:38:11 nystrand Exp $
+// $Log: StPeCEnumerations.h,v $
+// Revision 1.1  2000/03/24 22:38:11  nystrand
+// First version
+//
+// Revision 1.0  2000/03/20 23:28:50  nystrand
+//
+//////////////////////////////////////////////////////////////////////
+#ifndef StPeCEnumerations_hh
+#define StPeCEnumerations_hh
+enum StPeCParticle {pion, kaon, proton};
+#endif
