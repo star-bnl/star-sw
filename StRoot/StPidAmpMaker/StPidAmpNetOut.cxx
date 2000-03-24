2783a7f7a74c042cf2c38dd0fde678a067ae980e
aihong <>
2000-03-24 15:11:14 +0000

    add PrintContent()

--- StRoot/StPidAmpMaker/StPidAmpNetOut.cxx
+++ StRoot/StPidAmpMaker/StPidAmpNetOut.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StPidAmpNetOut.cxx,v 1.1.1.1 2000/03/09 17:48:33 aihong Exp $
+ * $Id: StPidAmpNetOut.cxx,v 1.2 2000/03/24 15:10:44 aihong Exp $
  *
  * Author: Aihong Tang & Richard Witt (FORTRAN Version),Kent State U.
  *         Send questions to aihong@cnr.physics.kent.edu
@@ -17,6 +17,9 @@
  ***************************************************************************
  *
  * $Log: StPidAmpNetOut.cxx,v $
+ * Revision 1.2  2000/03/24 15:10:44  aihong
+ * add PrintContent()
+ *
  * Revision 1.1.1.1  2000/03/09 17:48:33  aihong
  * Installation of package
  *
@@ -50,7 +53,14 @@ StPidAmpNetOut::StPidAmpNetOut(Text_t* name,Text_t* title,Int_t id,TArrayD bandP
   mResoParArray=linrParAry;
   mCalibConst=0.0;
   }
+StPidAmpNetOut::StPidAmpNetOut(const char* name,const char* title,Int_t id,TArrayD bandParAry, TArrayD ampParAry, TArrayD linrParAry) : TNamed(name,title) {
 
+  mGeantID=id;
+  mBandParArray=bandParAry;
+  mAmpParArray=ampParAry;
+  mResoParArray=linrParAry;
+  mCalibConst=0.0;
+  }
   StPidAmpNetOut::~StPidAmpNetOut(){}
 
 void StPidAmpNetOut::SetBandParArray(TArrayD bandParAry){
@@ -71,14 +81,120 @@ void StPidAmpNetOut::SetGeantID(Int_t id){
 void StPidAmpNetOut::SetCalibConst(Double_t cal){
   mCalibConst=cal;
 }
+void StPidAmpNetOut::PrintContent(){
+  int i;
+  cout<<GetName()<<" Geant ID: "<<mGeantID<<endl;
+  cout<<"Calibration constant: "<<mCalibConst<<endl;
+  cout<<"band Parameters: { ";
 
 
-TArrayD   StPidAmpNetOut::GetBandParArray(){ return mBandParArray;}
-TArrayD   StPidAmpNetOut::GetAmpParArray(){return mAmpParArray;}
-TArrayD   StPidAmpNetOut::GetResoParArray(){return mResoParArray;}
-Int_t     StPidAmpNetOut::GetNBandPars(){return mBandParArray.GetSize();}
-Int_t     StPidAmpNetOut::GetNAmpPars(){return mAmpParArray.GetSize();}
-Int_t     StPidAmpNetOut::GetNResoPars(){return mResoParArray.GetSize();}
+  for (i=0; i<mBandParArray.GetSize();i++){
 
-Int_t     StPidAmpNetOut::GetGeantID(){return mGeantID;}
-Double_t  StPidAmpNetOut::GetCalibConst(){return mCalibConst;}
+         cout<<mBandParArray.At(i);
+
+       if ( i!=(mBandParArray.GetSize()-1))    cout<<", ";
+       else cout<<" ";
+  }
+
+  cout<<"}"<<endl;
+ 
+   cout<<"amplitude Parameters: { ";
+
+
+
+  for (i=0; i<mAmpParArray.GetSize();i++){
+
+         cout<<mAmpParArray.At(i);
+
+       if ( i!=(mAmpParArray.GetSize()-1))    cout<<", ";
+       else cout<<" ";
+  }
+
+  cout<<"}"<<endl;
+  
+  cout<<"resolution Parameters: { ";
+
+
+  for (i=0; i<mResoParArray.GetSize();i++){
+
+         cout<<mResoParArray.At(i);
+
+       if ( i!=(mResoParArray.GetSize()-1))    cout<<", ";
+       else cout<<" ";
+  }
+
+  cout<<"}"<<endl;
+
+ 
+  cout<<endl;
+
+ 
+}
+
+
+
+
+
+TArrayD   StPidAmpNetOut::GetBandParArray() const{ return mBandParArray;}
+TArrayD   StPidAmpNetOut::GetAmpParArray() const{return mAmpParArray;}
+TArrayD   StPidAmpNetOut::GetResoParArray() const{return mResoParArray;}
+Int_t     StPidAmpNetOut::GetNBandPars() const{return mBandParArray.GetSize();}
+Int_t     StPidAmpNetOut::GetNAmpPars() const{return mAmpParArray.GetSize();}
+Int_t     StPidAmpNetOut::GetNResoPars() const{return mResoParArray.GetSize();}
+
+Int_t     StPidAmpNetOut::GetGeantID() const{return mGeantID;}
+Double_t  StPidAmpNetOut::GetCalibConst() const{return mCalibConst;}
+
+ostream& operator<<(ostream& s, const StPidAmpNetOut& netOut){
+  int i;
+  s<<netOut.GetName()<<" Geant ID: "<<netOut.GetGeantID()<<endl;
+  s<<"Calibration constant: "<<netOut.GetCalibConst()<<endl;
+
+  s<<"band Parameters: { ";
+
+  TArrayD theBandParArray=netOut.GetBandParArray();
+
+  for (i=0; i<theBandParArray.GetSize();i++){
+
+         s<<theBandParArray.At(i);
+
+       if ( i!=(theBandParArray.GetSize()-1))    s<<", ";
+       else s<<" ";
+  }
+
+  s<<"}"<<endl;
+ 
+   s<<"amplitude Parameters: { ";
+
+  TArrayD theAmpParArray=netOut.GetAmpParArray();
+
+
+  for (i=0; i<theAmpParArray.GetSize();i++){
+
+         s<<theAmpParArray.At(i);
+
+       if ( i!=(theAmpParArray.GetSize()-1))    s<<", ";
+       else s<<" ";
+  }
+
+  s<<"}"<<endl;
+  
+  s<<"resolution Parameters: { ";
+
+  TArrayD theResoParArray=netOut.GetResoParArray();
+
+  for (i=0; i<theResoParArray.GetSize();i++){
+
+         s<<theResoParArray.At(i);
+
+       if ( i!=(theResoParArray.GetSize()-1))    s<<", ";
+       else s<<" ";
+  }
+
+  s<<"}"<<endl;
+
+ 
+  s<<endl;
+
+  return s;
+}
