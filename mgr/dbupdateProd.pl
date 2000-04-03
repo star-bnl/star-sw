964ff9807209c2280d758061c95db53c0b3ab8fb
didenko <>
2000-04-03 17:56:49 +0000

    add dataset

--- mgr/dbupdateProd.pl
+++ mgr/dbupdateProd.pl
@@ -32,9 +32,7 @@ my $topHpssReco  =  "/home/starreco/reco";
 my @SetG = (
              "auau200/nexus/default/b0_3/year_1h/hadronic_on",
              "auau200/mevsim/vanilla/central/year_1h/hadronic_on",
-             "auau200/mevsim/cascade/central/year_1h/hadronic_on",
              "auau200/mevsim/vanilla/flow/year_1h/hadronic_on", 
-             "auau200/mevsim/vanilla/fluct/year_1h/hadronic_on",
              "auau200/mevsim/vanilla/resonance/year_1h/hadronic_on", 
              "auau200/mevsim/vanilla/trigger/year_1h/hadronic_on", 
              "auau200/vni/default/b0_3/year_1h/hadronic_on",
@@ -53,7 +51,20 @@ my @SetG = (
              "auau200/starlight/vmeson/none/year_1h/hadronic_on",
              "auau200/dtunuc/two_photon/halffield/year_1h/hadronic_on",
              "auau200/dtunuc/two_photon/none/year_1h/hadronic_on",
-              
+             "auau200/hemicosm/default/none/year_1h/hadronic_on",
+             "pau200/hijing/b0_7/gam15/year_1h/hadronic_on",
+             "pau200/hijing/b0_7/jet15/year_1h/hadronic_on", 
+             "auau200/mevsim/vcascade/central/year_1h/hadronic_on",       
+             "auau200/mevsim/vcascade/flow/year_1h/hadronic_on", 
+             "auau200/mevsim/vcascade/fluct/year_1h/hadronic_on",
+             "auau200/mevsim/vcascade/resonance/year_1h/hadronic_on", 
+             "auau200/mevsim/vcascade/trigger/year_1h/hadronic_on",
+             "auau200/mevsim/cascade/central/year_1h/hadronic_on",
+             "auau200/mevsim/vanilla/fluct/year_1h/hadronic_on",
+             "auau200/hijing_quark/b0_3_jetq_off/jet05/year_1h/hadronic_on",
+             "auau200/hijing_quark/b0_3_jetq_on/jet05/year_1h/hadronic_on",
+             "auau200/hijing_antinuc/b0_3_jetq_off/jet05/year_1h/hadronic_on",
+             "auau200/hijing_antinuc/b0_3_jetq_on/jet05/year_1h/hadronic_on",
 );
 
 my $SetD = "dst/prod5/1999/12";
@@ -180,15 +191,15 @@ my $inext =scalar(@SetG);
 #  $diskRecoDirs[$ll] = $DISK1 . "/" . $SetG[$ll] . "/tfs_6";
 #  print "diskRecoDir: $diskRecoDirs[$ll]\n" if $debugOn;
 #}
-for( $ll = 0; $ll<scalar(@SetG); $ll++) { 
+for( $ll = 0; $ll< 29; $ll++) { 
   $diskRecoDirs[$ll] = $DISK2 . "/" . $SetG[$ll] . "/tfs_6";
-  print "diskRecoDir: $diskRecoDirs[$ll]\n" if $debugOn;
+  print "diskRecoDir: $diskRecoDirs[$ll]\n";
 }
-$diskRecoDirs[$inext] = $DISK1 . "/" . $SetG[2] . "/tfs_6";
- $inext++;
-$diskRecoDirs[$inext] = $DISK1 . "/" . $SetG[4] . "/tfs_6";
- $inext++;
-#$diskRecoDirs[$inext] = $DISK1 . "/" . $SetG[5] . "/tfs_6";
+for( $ii = 29; $ii< 35; $ii++) { 
+$diskRecoDirs[$ii] = $DISK1 . "/" . $SetG[$ii] . "/tfs_6";
+  print "diskRecoDir: $diskRecoDirs[$ii]\n";
+}
+
 
 print "\nFinding reco files in disk\n";
  
