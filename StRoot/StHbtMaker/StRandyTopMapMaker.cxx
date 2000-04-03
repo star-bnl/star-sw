83ade74a94d40a9c50c09376d95295fa6cd3b960
Randall C Wells <rcwells>
2000-04-03 23:18:36 +0000

    Improved speed in RandyTopMapMaker

--- StRoot/StHbtMaker/StRandyTopMapMaker.cxx
+++ StRoot/StHbtMaker/StRandyTopMapMaker.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StRandyTopMapMaker.cxx,v 1.3 2000/04/03 20:28:10 rcwells Exp $
+ * $Id: StRandyTopMapMaker.cxx,v 1.4 2000/04/03 23:18:36 rcwells Exp $
  *
  * Author: Torre Wenaus, BNL,
  *         Thomas Ullrich, Nov 1999
@@ -14,6 +14,9 @@
  ***************************************************************************
  *
  * $Log: StRandyTopMapMaker.cxx,v $
+ * Revision 1.4  2000/04/03 23:18:36  rcwells
+ * Improved speed in RandyTopMapMaker
+ *
  * Revision 1.3  2000/04/03 20:28:10  rcwells
  * Removed remnant Pt tag in code from StAnalysisMaker.
  *
@@ -35,7 +38,7 @@
 #include "StTrackGeometry.h"
 #include "StTrackTopologyMap.h"
 
-static const char rcsid[] = "$Id: StRandyTopMapMaker.cxx,v 1.3 2000/04/03 20:28:10 rcwells Exp $";
+static const char rcsid[] = "$Id: StRandyTopMapMaker.cxx,v 1.4 2000/04/03 23:18:36 rcwells Exp $";
 
 ClassImp(StRandyTopMapMaker)
 
@@ -75,37 +78,47 @@ StRandyTopMapMaker::Make()
     StEvent* mEvent;
     mEvent = (StEvent *) GetInputDS("StEvent");
     if (! mEvent) return kStOK; // If no event, we're done
-    StEvent& ev = *mEvent;
     
     // OK, we've got the event. Let's fix that Topology Map
     // This only fixes TPC pad rows and the turnaround flag
     StTrack* rTrack;
     int mult = mEvent->trackNodes().size();
-    unsigned long mask2 = 2147483648;
+    unsigned long mask2 = 0x80000000;
     unsigned long temp1;
+    int pad, ipad;
+    bool padRow[46];
+    unsigned long map1, map2;
+    StPtrVecHit myHitVector;
+    StPtrVecHitIterator myHitIterator;
+    StTpcHit* myHit;
+    // For speed we try to remove the turnAround flag
+    //    bool turnAround;
     for (unsigned long int icount=0; icount<(unsigned long int)mult; icount++){
       rTrack = mEvent->trackNodes()[icount]->track(global);
+      // Changed code
       // This checks bits that should not be set
       temp1 = rTrack->topologyMap().data(1) & mask2;
       // If FTPC track, don't do anything
       if ( int(temp1) ) continue;
-      // End check of bits that should not be set
-      int numHits = rTrack->detectorInfo()->hits(kTpcId).size();
-      bool padRow[46];
-      bool turnAround = false;
-      int pad;
-      int ipad;
-      unsigned long map1 = 0;
-      unsigned long map2 = 0;
-      StTpcHit* myHit;
+      //      numHits = rTrack->detectorInfo()->hits(kTpcId).size();
+      //      turnAround = false;
       // Zero pad rows
-      for (int ipad=0; ipad<46; ipad++) padRow[ipad] = false;
-      for (int ihit=0; ihit<numHits; ihit++) {
-	myHit = (StTpcHit *)rTrack->detectorInfo()->hits(kTpcId)[ihit];
+      for (ipad=0; ipad<46; ipad++) padRow[ipad] = false;
+      myHitVector = rTrack->detectorInfo()->hits(kTpcId);
+      for (myHitIterator=myHitVector.begin(); myHitIterator!=myHitVector.end(); myHitIterator++) {
+	myHit = (StTpcHit *)(*myHitIterator);
 	pad = myHit->padrow();
-	if ( padRow[pad] ) turnAround = true;
 	padRow[pad] = true;
       }
+      /*
+      for (ihit=0; ihit<numHits; ihit++) {
+	myHit = (StTpcHit *)(rTrack->detectorInfo()->hits(kTpcId)[ihit]);
+	pad = myHit->padrow();
+	//	if ( padRow[pad] ) turnAround = true;
+	padRow[pad] = true;
+      }
+      */
+      map1 = map2 = 0;
       // Fill the map
       for (ipad=1; ipad<=24; ipad++) {
 	if ( padRow[ipad] ) {
@@ -117,7 +130,7 @@ StRandyTopMapMaker::Make()
 	  map2 |= 1UL<<(ipad-25);
 	}
       }
-      if ( turnAround ) map2 |= 1UL<<(30);
+      //      if ( turnAround ) map2 |= 1UL<<(30);
       StTrackTopologyMap newMap( map1,map2 );
       rTrack->setTopologyMap( newMap );
     }
