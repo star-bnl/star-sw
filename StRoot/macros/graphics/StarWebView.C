f8deb3ce77da37cd7f4531d42145e6842be82617
fine <>
2000-04-07 17:07:09 +0000

    adjusted to the ROOT 2.24

--- StRoot/macros/graphics/StarWebView.C
+++ StRoot/macros/graphics/StarWebView.C
@@ -1,5 +1,8 @@
-// $Id: StarWebView.C,v 1.3 1999/05/21 15:33:54 kathy Exp $
+// $Id: StarWebView.C,v 1.4 2000/04/07 17:07:08 fine Exp $
 // $Log: StarWebView.C,v $
+// Revision 1.4  2000/04/07 17:07:08  fine
+// adjusted to the ROOT 2.24
+//
 // Revision 1.3  1999/05/21 15:33:54  kathy
 // made sure Log & Id are in each file and also put in standard comment line with name of owner
 //
@@ -17,16 +20,21 @@
  
   gROOT->Reset();
   Bool_t NT=kFALSE;
-  if (strcmp(gSystem.GetName(),"WinNT") == 0 ) NT=kTRUE; 
+  if (strcmp(gSystem.GetName(),"WinNT") == 0 ) NT=kTRUE;
+  if (NT) gSystem->Load("ROOT_STAR");
+  else  gSystem->Load("libSTAR");
+  if (!gGeometry) new TGeometry;
   TCanvas Star("Star","Star",400,600);
   // Pick the geomtery file from the remote Web site at CERN: http://root.cern.ch
   gBenchmark->Start("pick");
-  TWebFile f("http://root.cern.ch/files/star.root");
+  TWebFile f("http://www.star.bnl.gov/~fine/star_year_2a.root");
   gBenchmark->Show("pick");
-  // Create an TPad view of this geometry
-  year2a_hadron.Draw();
+  // List first 3 levels of the geometry
+  HALL->ls(3);
+  // Create an TPad view of "ECAL" from this geometry if any
+  HALL->FindByName("ECAL")->Draw();
   // Now create ROOT browser to inspect the geometry
-  TBrowser b;
+  TBrowser b("STAR",HALL);
   // Begin_Html <P ALIGN=CENTER> <IMG SRC="gif/browser.gif"> </P> End_Html //
   //  Call TPad::x3d() method to create 3D view with the "default" 3D viewer
   //  Under UNIX it is X3D package
