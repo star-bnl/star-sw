cf772d0fc9d4c4281220fb7e0513827046c14f31
aihong <>
2000-04-09 16:38:31 +0000

    nhitsDcaDependent added

--- StRoot/StPidAmpMaker/Include/StPidAmpEnum.hh
+++ StRoot/StPidAmpMaker/Include/StPidAmpEnum.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StPidAmpEnum.hh,v 1.1.1.1 2000/03/09 17:48:34 aihong Exp $
+ * $Id: StPidAmpEnum.hh,v 1.2 2000/04/09 16:38:31 aihong Exp $
  *
  * Author: Aihong Tang & Richard Witt (FORTRAN Version),Kent State U.
  *         Send questions to aihong@cnr.physics.kent.edu
@@ -11,6 +11,9 @@
  ***************************************************************************
  *
  * $Log: StPidAmpEnum.hh,v $
+ * Revision 1.2  2000/04/09 16:38:31  aihong
+ * nhitsDcaDependent added
+ *
  * Revision 1.1.1.1  2000/03/09 17:48:34  aihong
  * Installation of package
  *
@@ -19,7 +22,7 @@
 #ifndef StPidAmpEnum_hh
 #define StPidAmpEnum_hh
 
-enum StPidAmpNetType {noDependent, ptDependent, nhitsDependent, ptNhitsDependent};
+enum StPidAmpNetType {noDependent, ptDependent, nhitsDependent,nhitsDcaDependent, ptNhitsDependent};
 
 
 #endif
