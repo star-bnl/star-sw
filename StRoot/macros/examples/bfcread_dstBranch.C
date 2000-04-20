46ef1074b43e9d4bb8d24c5114590d30fe95edf2
Kathleen R. Turner <kathy>
2000-04-20 22:05:24 +0000

    update to print out more detailed info

--- StRoot/macros/examples/bfcread_dstBranch.C
+++ StRoot/macros/examples/bfcread_dstBranch.C
@@ -1,5 +1,8 @@
-// $Id: bfcread_dstBranch.C,v 1.7 2000/04/18 20:37:26 kathy Exp $
+// $Id: bfcread_dstBranch.C,v 1.8 2000/04/20 22:05:17 kathy Exp $
 // $Log: bfcread_dstBranch.C,v $
+// Revision 1.8  2000/04/20 22:05:17  kathy
+// update to print out more detailed info
+//
 // Revision 1.7  2000/04/18 20:37:26  kathy
 // St_DataSet,St_DataSetIter,St_Table classes are nowchanged to TDataSet,TDataSetIter,TTable
 //
@@ -38,16 +41,25 @@ StChain *chain;
 void bfcread_dstBranch(
  Int_t nevents=2, 
  const char *MainFile=
- "/afs/rhic/star/data/samples/gstar.dst.root")
+ "/afs/rhic/star/data/samples/gstar.dst.root",
+  const char *fname="qa_dst.out")
 {
 //
+  cout << " events to process  = " << nevents << endl;
+  cout << " Input File Name = " << MainFile << endl;
+  cout << " Output file containing printouts = " << fname << endl;
+
+  ofstream fout(fname);
+
+  fout << " Running: bfcread_dstBranch.C " << endl;
+  fout << " events to process  = " << nevents << endl;
+  fout << " Input File Name = " << MainFile << endl;
+  fout << " Output file containing printouts = " << fname << endl;
+  fout << endl << endl;
+
     gSystem->Load("St_base");
     gSystem->Load("StChain");
-
-  gSystem->Load("libglobal_Tables");
-  gSystem->Load("libgen_Tables");
-  gSystem->Load("libsim_Tables");
-
+    gSystem->Load("libglobal_Tables");
     gSystem->Load("StIOMaker");
 
 //  Setup top part of chain
@@ -67,13 +79,28 @@ void bfcread_dstBranch(
   TTable   *tabl=0;
   TDataSet *obj=0;
 
-// Event loop
-  int istat=0,i=1;
 
-EventLoop: if (i <= nevents && !istat) {
-    cout << "============================ Event " << i << " start" << endl;
+  Float_t tottabcntr=0;
+  Float_t totobjcntr=0;
+
+
+  int istat=0;
+  int i=0;
+  int countev=0;
+  int countevdds=0;
+  int countevobj=0;
+  int countevtab=0;
+
+// Event loop
+EventLoop: if (i < nevents && !istat) {
+
     chain->Clear();
     istat = chain->Make(i);
+    
+//  count # times Make is called
+    i++;
+
+    cout << " Call Make # " << i << endl; 
     cout << "     istat value returned from chain Make = " << istat << endl;
 
 // Now look at the data in the event:
@@ -81,49 +108,86 @@ EventLoop: if (i <= nevents && !istat) {
     int countTable=0;
 
     if (!istat) {
+
+    countev++;
+
+    cout << " start event # " << countev << endl;
+
       ds=chain->GetDataSet("dst");
       TDataSetIter tabiter(ds);
       if (ds) {
+        countevdds++;
 //        ds->ls(2);  
         while (obj = tabiter.Next()) {
-//	  cout << " I have found an object! " <<endl;
-          countObj++;
 
+	  cout << " QAInfo: found object: " << obj->GetName() << endl;
+	  fout << " QAInfo: found object: " << obj->GetName() << endl;
+
+          countObj++;
+          totobjcntr++;
 
 //.. count all tables that exist:
           if (obj->InheritsFrom("TTable")) {
             tabl = (TTable *)tabiter.Find(obj->GetName());
             if (tabl) {
               countTable++;
-              cout << " Found Object (Table) "<< endl;
-              cout << "   Name = " <<  obj->GetName() << 
-                      "    # rows =  " << tabl->GetNRows() << endl;
-             }
-          }
+              tottabcntr++;
+              cout << " QAInfo: it's a table with #rows = " 
+                        << tabl->GetNRows() << endl;
+              fout << " QAInfo: it's a table with #rows = " 
+                        << tabl->GetNRows() << endl;
+
+
+	    } // tabl
+          }  // obj
 //.. end of counting all tables that exist
+	}  // while
+           if (countObj) countevobj++;
+           if (countTable) countevtab++;
+      }   // ds
 
-	}      
-      }
-    }
-    cout << " End of Event " << i << endl;
-    cout << "  # objects found = " << countObj << endl;
-    cout << "  # tables found = " << countTable << endl;
+    cout << " QAInfo: event # " << countev << ", # objects found = " 
+         << countObj << ", # tables found = " << countTable << endl << endl;
+    fout << " QAInfo: event # " << countev << ", # objects found = " 
+         << countObj << ", # tables found = " << countTable << endl << endl;
 
+    }  // istat
 
-    if (istat) {
+    else   // if (istat)
+      {
       cout << "Last event processed. Status = " << istat << endl;
     }
 
-    i++;
     goto EventLoop;
-   }
 
-  cout << " bfcread DST Branch: passed event loop " << endl;
+}  // EventLoop
+     
+  tottabcntr /= countev;
+  totobjcntr /= countev;
+ 
+  cout << endl;
+  cout << "QAInfo: End of Job " << endl; 
+  cout << "QAInfo: # times Make called = " << i << endl;
+  cout << "QAInfo:  # events read = " << countev << endl;
+  cout << "QAInfo:   # events with dst dataset = " << countevdds << endl;
+  cout << "QAInfo:     # with objects = " << countevobj << endl;
+  cout << "QAInfo:     # with tables  = " << countevtab << endl;
+  cout << "QAInfo: avg # tables per event  = " << tottabcntr << endl;
+  cout << "QAInfo: avg # objects per event = " << totobjcntr << endl << endl;
+
+  fout << endl;
+  fout << "QAInfo: # times Make called = " << i << endl;
+  fout << "QAInfo:  # events read = " << countev << endl;
+  fout << "QAInfo:   # events with dst dataset = " << countevdds << endl;
+  fout << "QAInfo:     # with objects = " << countevobj << endl;
+  fout << "QAInfo:     # with tables  = " << countevtab << endl;
+  fout << "QAInfo: avg # tables per event  = " << tottabcntr << endl;
+  fout << "QAInfo: avg # objects per event = " << totobjcntr << endl << endl;
 
  chain->Finish();   
+
 }
  
-
 
  
 
