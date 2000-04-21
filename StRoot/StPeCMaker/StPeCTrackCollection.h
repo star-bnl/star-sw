5ed91a2c472a293d39b3431cf900e8b6576885db
nystrand <>
2000-04-21 19:11:19 +0000

    Include collection of StPeCPair classes

--- StRoot/StPeCMaker/StPeCTrackCollection.h
+++ StRoot/StPeCMaker/StPeCTrackCollection.h
@@ -1,7 +1,10 @@
 //////////////////////////////////////////////////////////////////////
 //
-// $Id: StPeCTrackCollection.h,v 1.1 2000/03/24 22:37:42 nystrand Exp $
+// $Id: StPeCTrackCollection.h,v 1.2 2000/04/21 19:11:19 nystrand Exp $
 // $Log: StPeCTrackCollection.h,v $
+// Revision 1.2  2000/04/21 19:11:19  nystrand
+// Include collection of StPeCPair classes
+//
 // Revision 1.1  2000/03/24 22:37:42  nystrand
 // First version
 //
@@ -12,13 +15,16 @@
 #define StPeCTrackCollection_h
 #ifndef __CINT__
 #include <vector>
+#include "StPeCPair.h"
 #ifndef ST_NO_NAMESPACES
 using std::vector;
 #endif
 #include "StPrimaryTrack.h"
-typedef vector<StTrack*>           StPeCPrimaryTrackCollection;
-typedef vector<StTrack*>::iterator StPeCPrimaryTrackIterator;
-typedef vector<StTrack*>           StPeCNonPrimaryTrackCollection;
-typedef vector<StTrack*>::iterator StPeCNonPrimaryTrackIterator;
+typedef vector<StTrack*>             StPeCPrimaryTrackCollection;
+typedef vector<StTrack*>::iterator   StPeCPrimaryTrackIterator;
+typedef vector<StTrack*>             StPeCNonPrimaryTrackCollection;
+typedef vector<StTrack*>::iterator   StPeCNonPrimaryTrackIterator;
+typedef vector<StPeCPair*>           StPeCPairCollection;
+typedef vector<StPeCPair*>::iterator StPeCPairIterator;
 #endif /*__CINT__*/
 #endif
