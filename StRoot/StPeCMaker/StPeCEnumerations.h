bf95d7fe658db29ed36be868117f6ba068d432ea
Joakim I. Nystrand <nystrand>
2000-04-21 19:11:47 +0000

    Add muons, electrons

--- StRoot/StPeCMaker/StPeCEnumerations.h
+++ StRoot/StPeCMaker/StPeCEnumerations.h
@@ -1,7 +1,10 @@
 //////////////////////////////////////////////////////////////////////
 //
-// $Id: StPeCEnumerations.h,v 1.1 2000/03/24 22:38:11 nystrand Exp $
+// $Id: StPeCEnumerations.h,v 1.2 2000/04/21 19:11:47 nystrand Exp $
 // $Log: StPeCEnumerations.h,v $
+// Revision 1.2  2000/04/21 19:11:47  nystrand
+// Add muons, electrons
+//
 // Revision 1.1  2000/03/24 22:38:11  nystrand
 // First version
 //
@@ -10,5 +13,5 @@
 //////////////////////////////////////////////////////////////////////
 #ifndef StPeCEnumerations_hh
 #define StPeCEnumerations_hh
-enum StPeCParticle {pion, kaon, proton};
+enum StPeCParticle {pion, kaon, proton, electron, muon};
 #endif
