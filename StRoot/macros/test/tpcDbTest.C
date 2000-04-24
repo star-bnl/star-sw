4b0ae5180550188ad1b96aeefdbc348917aa6041
hardtke <>
2000-04-24 19:39:20 +0000

    fix to run with updated shared libraries

--- StRoot/macros/test/tpcDbTest.C
+++ StRoot/macros/test/tpcDbTest.C
@@ -3,22 +3,23 @@
     gSystem->Load("StChain");
     gSystem->Load("St_Tables");
     gSystem->Load("StUtilities");
+    gSystem->Load("StDbUtilities");
     gSystem->Load("StDbLib");
     gSystem->Load("StDbBroker");
     gSystem->Load("St_db_Maker");
     gSystem->Load("StTpcDb");
 
   //  Create the makers to be called by the current chain
-  const char *mainDB =  "/afs/rhic/star/users/fine/public/StDb";
-  const char *userDB = ""; // "MyDb/params";
+  const char *mainDB =  "MySQL:StarDb";
+  const char *userDB = "StarDb";
   StChain *chain =  new StChain();
-  St_db_Maker *dbMk = new St_db_Maker("db",mainDB,userDB);
+  St_db_Maker *dbMk = new St_db_Maker("db",mainDB);
   StTpcDbMaker *tpcDbMk = new StTpcDbMaker("tpcDb");
   dbMk->SetDebug();
   tpcDbMk->SetDebug();
+  dbMk->SetDateTime(19970101,10000);
   chain->SetDebug();
   chain->Init();
   chain->Make();
-  new TBrowser ("TPC",tpcDbMk->FindByName(".const"));
 }
 
