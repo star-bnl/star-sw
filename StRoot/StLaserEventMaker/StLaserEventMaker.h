437dc90738068988f533c766d7f5cd27f360f411
William A. Love <love@bnl.gov>
2000-04-24 14:36:34 +0000

    Write clock, drivel, tzero on Event Header.  truncate psi angles to 0-180 range Expand doca to do straight tracks and do 12 laser sectors, add z cut. Expand to 2000 possible track numbers.

--- StRoot/StLaserEventMaker/StLaserEventMaker.h
+++ StRoot/StLaserEventMaker/StLaserEventMaker.h
@@ -1,5 +1,10 @@
-// $Id: StLaserEventMaker.h,v 1.3 2000/02/15 21:01:26 love Exp $
+// $Id: StLaserEventMaker.h,v 1.4 2000/04/24 14:36:34 love Exp $
 // $Log: StLaserEventMaker.h,v $
+// Revision 1.4  2000/04/24 14:36:34  love
+// Write clock, drivel, tzero on Event Header.  truncate psi angles to 0-180 range
+// Expand doca to do straight tracks and do 12 laser sectors, add z cut.
+// Expand to 2000 possible track numbers.
+//
 // Revision 1.3  2000/02/15 21:01:26  love
 // Maximum tracks upped to 1000
 //
@@ -34,12 +39,15 @@ class St_tpt_pars;
 class TTree;
 class StLaserEvent;
 
-enum { maxNofTracks = 1000}; 
+enum { maxNofTracks = 2000}; 
 
 class StLaserEventMaker : public StMaker {
 private:
   Int_t m_runno;          //Run number to put on events (derive from filename)
   Int_t m_date;           //date to put in event header
+  Float_t m_tzero;        // record tzero etc. in the event header
+  Float_t m_drivel;
+  Float_t m_clock;
   Int_t m_rowmin ; Int_t m_rowmax ;  //Range for the pixel branch.
   Bool_t m_mklaser;   	          //control flag for laser tree production
   St_tpg_pad_plane      *m_tpg_pad_plane;  //! Constants that describe TPC
@@ -69,7 +77,7 @@ public:
   virtual void   Set_laser(Bool_t m=kTRUE){m_mklaser = m;}
 
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StLaserEventMaker.h,v 1.3 2000/02/15 21:01:26 love Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StLaserEventMaker.h,v 1.4 2000/04/24 14:36:34 love Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
 ClassDef(StLaserEventMaker, 1)   //StAF chain virtual base class for Makers
 };
