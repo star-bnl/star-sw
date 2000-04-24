ae8155a1b74bbcfb35da6c7fc34dd7363789b955
cvs2git <cvs2git>
2000-04-24 15:37:37 +0000

    This commit was manufactured by cvs2git to create tag 'V3_03a'.

--- StRoot/StSpectraMaker/StSpectraAxesEnumeration.h
+++
@@ -1,7 +0,0 @@
-#ifndef StSpectraAxesEnumeration_hh
-#define StSpectraAxesEnumeration_hh
-
-enum StSpectraAbscissa {kRapidity, kPseudoRapidity};
-enum StSpectraOrdinate {kTransverseMass, kPperp};
-
-#endif
--- mgr/badfiles.csh
+++
@@ -1,8 +0,0 @@
-#
-#uncomment for total test
-#set x = ( `find $1 -name '*.root' ` )
-
-set x = ( $1/*.root )
-foreach FIL ( $x )
-$STAR/mgr/lsFile.csh $FIL |& grep Error
-end
