3fad97a3dbb3e6dc58542037fac0ce3fdb8a28be
porter <>
2000-04-25 18:27:04 +0000

    added flavor test in example

--- StRoot/StDbLib/examples/ReadConfig.cc
+++ StRoot/StDbLib/examples/ReadConfig.cc
@@ -4,24 +4,34 @@
 #include "StDbTable.h"
 #include "StDbXmlReader.h"
 #include "StDbXmlWriter.h"
+#include <time.h>
 
 int main(){
 
 
   StDbManager* mgr = StDbManager::Instance();
-  StDbConfigNode* node = mgr->initConfig(dbGeometry,dbTpc,"reconV0");
-  mgr->setRequestTime("19991210010101");
-  mgr->fetchAllTables(node); 
-  StDbTable* table = node->findTable("tpcDimensions");
+  mgr->setVerbose(true);
+  StDbConfigNode* node = mgr->initConfig(dbCalibrations,dbTpc,"reconV0");
 
-  ofstream of("tpcDimensions.xml");
+  node->setFlavor("onl");
+  unsigned int rTime = (unsigned int)time(NULL);
+  node->setProdTime(rTime);
+  mgr->setRequestTime(rTime);
+  mgr->fetchAllTables(node); 
+  StDbTable* table = node->findTable("tpcElectronics");
+
+  ofstream of("tpcElectronics.Now.xml");
   StDbXmlWriter writer(of);  
   writer.ioTable(table);
+
+  //  mgr->setStoreTime(rTime);
+  //  mgr->storeAllTables(node);
 
   delete node;
   delete mgr;
 
 } 
+
 
 
 
