9aef8e2998e7206cc12d4b8ae56bb4feca830ada
lasiuk <>
2000-04-26 18:58:03 +0000

    change list to vector for mc info storage. allow change of allocation methodology

--- StRoot/StRrsMaker/StRichPadPlane.cxx
+++ StRoot/StRrsMaker/StRichPadPlane.cxx
@@ -1,11 +1,15 @@
 /******************************************************************
- *  $Id: StRichPadPlane.cxx,v 1.3 2000/04/14 22:38:09 lasiuk Exp $
+ *  $Id: StRichPadPlane.cxx,v 1.4 2000/04/26 18:58:03 lasiuk Exp $
  *
  * Description:
  *   StRichPadPlane is a special container specialized to store 
  *   RICH Raw Data. 
  ********************************************************************
  *  $Log: StRichPadPlane.cxx,v $
+ *  Revision 1.4  2000/04/26 18:58:03  lasiuk
+ *  change list to vector for mc info storage.
+ *  allow change of allocation methodology
+ *
  *  Revision 1.3  2000/04/14 22:38:09  lasiuk
  *  add print diagnostic for crash
  *  extra careful on clearing the dataset
@@ -17,8 +21,10 @@
  *  Initial Revision
  *
  ********************************************************************/
+#define NORMAL_ALLOCATION
 
 #include "StRichPadPlane.h"
+#include "StGlobals.hh"
 
 ostream& operator<<(ostream& os, const StRichID& id)
 {
@@ -26,55 +32,98 @@ ostream& operator<<(ostream& os, const StRichID& id)
 	       << " q: " << id.mAmount << " type: " << static_cast<int>(id.mSignalType));
 }
 
-StRichPadPlane::StRichPadPlane()
-    : v() { /*nopt*/ }
+StRichPadPlane::StRichPadPlane() { /*nopt*/ }
     
 StRichPadPlane::StRichPadPlane(size_t rows, size_t cols)
 {
-    v.resize(rows);
-    for (unsigned int i=0; i<rows; i++ )
-	v[i].resize(cols);
+    //cout << "StRichPadPlane::StRichPadPlane() " << endl;
+
+    unsigned int ii, jj;
+//  PR(rows);
+//  PR(cols);
+
+    //
+    // How a normal person would do this
+    //
+#ifdef NORMAL_ALLOCATION
+     StRichPad tmp;
+     cout << "NORMAL_ALLOCATION" << endl;
+     v.resize(rows);
+//      PR(v.size());
+//      PR(v.capacity());
+     for (unsigned int i=0; i<rows; i++ ) {
+	 v[i].resize(cols,tmp);
+	 if(v[i].size() != cols) {
+	     cout << "StRichPadPlane::StRichPadPlane()\n";
+	     cout << "\tv[i].size()= " << v[i].size() << "\n";
+	     cout << "\tAborting...Error in allocation" << endl;
+	     abort();
+	 }
+     }
+#else
+     cout << "NOT NORMAL_ALLOCATION" << endl;
+
+     aDetectorRow aRow;
+     for(ii=0; ii<cols; ii++) {
+	 aRow.push_back(StRichPad());
+     }
+     
+     PR(aRow.size());
+     
+     for(jj=0; jj<rows; jj++) {
+	v.push_back(aRow);
+     }
+#endif
+
+//      PR(v.size());
+//      PR(v.begin()->size());
+//      PR(this->row_size());
+//      PR(this->col_size());
 }
 
 StRichPadPlane::~StRichPadPlane() { /* nopt */ }
     
-// row_type&
-// StRichPadPlane::operator[](int n)
-// { return v[n]; }
-
-// const_row_iter StRichPadPlane::begin() const
-// { return v.begin(); }
-
-// row_iter StRichPadPlane::begin()
-// { return v.begin(); }
-
-// const_row_iter StRichPadPlane::end() const
-// { return v.end(); }
-
-// row_iter StRichPadPlane::end()
-// { return v.end(); }
-
-// size_t StRichPadPlane::row_size()
-// { return v.size(); }
-
-// size_t StRichPadPlane::col_size()
-// { return v.begin()->size(); }
-
 void StRichPadPlane::resize(size_t rows, size_t cols)
-{ 
-    if ( v.size() != rows )
-	v.resize( rows );
-    for ( unsigned int i = 0; i < v.size(); i++ )
-	v[i].resize(cols);
+{
+//      cout << "StRichPadPlane::resize()" << endl;
+//      PR(rows);
+//      PR(cols);
+//      PR(v.size());
+#ifdef NORMAL_ALLOCATION
+//      cout << "NORMAL_ALLOCATION" << endl;
+    StRichPad tmp;
+    if (v.size() != rows)
+	v.resize(rows);
+//     PR(v.size());
+    for ( unsigned int i = 0; i < v.size(); i++ ) {
+	v[i].resize(cols,tmp);
+    }
+#else
+    cout << "NOT NORMAL_ALLOCATION" << endl;
+    if (v.size() != rows)
+	v.resize(rows);
+    PR(v.size());
+    for ( unsigned int i = 0; i < v.size(); i++ ) {
+ 	for(unsigned int j=0; j<cols; j++) {
+ 	    v[i].push_back(StRichPad());
+ 	}
+    }
+#endif
 }
 
 void StRichPadPlane::clear()
 {
-    unsigned int rows = row_size();
-    unsigned int cols = col_size();
-    for(int ii=0; ii<v.size(); ii++) {
-	v[ii].clear();
-    }
+    //cout << "StRichPadPlane::clear()" << endl;
+    unsigned int rows = this->row_size();
+    unsigned int cols = this->col_size();
+//      PR(rows);
+//      PR(cols);
+//      PR(v.size());
+//      for(int ii=0; ii<v.size(); ii++) {
+//  	cout << "ii -> size " << ii << " " << v[ii].size() << endl;
+//  	v[ii].clear();
+//      }
+	
     v.clear();
-    resize(rows,cols);
+    this->resize(rows,cols);
 }
