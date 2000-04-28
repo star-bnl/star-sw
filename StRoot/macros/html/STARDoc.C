981cc59508b2a9623c0ce75f38204c79f03a8689
fine <>
2000-04-28 19:47:00 +0000

    Creates symbolik links for the obsolete names

--- StRoot/macros/html/STARDoc.C
+++ StRoot/macros/html/STARDoc.C
@@ -8,12 +8,40 @@
                            "TPolyLineShape","TTable","TTable3Points",
                            "TTableDescriptor","TTableIter","TTablePoints",
                            "TTableSorter","TVolume","TVolumePosition",
-                           "TVolumeView","TVolumeViewIter","Ttypes","point3",
-                           "tableDescriptor"
+                           "TVolumeView","TVolumeViewIter"
                           };
+
+  const char *former[] = {
+                           "StCL","TChair","St_DataSet","St_DataSetIter",
+                           "St_FileSet","St_ObjectSet","St_Points3D","St_PointsArray3D",
+                           "St_PolyLineShape","St_Table","St_Table3Points",
+                           "St_tableDescriptor","St_TableIter","St_TablePoints",
+                           "St_TableSorter","St_Node","St_NodePosition",
+                           "St_NodeView","St_NodeViewIter"
+                          };
+
   int lClasses = sizeof(cls)/sizeof(char *);
   THtml html;
-  html.SetOutputDir("$STAR/StRoot/html");
+  TString htmlDir = "$STAR/StRoot/html";
+  html.SetOutputDir(htmlDir.Data());
   html.SetSourceDir("$ROOTSYS:$ROOTSYS/include");
-  for (int i=0; i < lClasses; i++)  html.MakeClass(cls[i],kTRUE);
+  for (int i=0; i < lClasses; i++) {
+      html.MakeClass(cls[i],kTRUE);
+      // Create links for the sake of the backward compatibility
+      if (former[i] && *former[i] && strcmp(cls[i],former[i])) {
+        TString thisName = cls[i];   thisName += ".html";
+        TString formName = htmlDir; formName += '/'; formName += former[i]; formName += ".html";
+        gSystem->ExpandPathName(formName);
+        gSystem->Unlink(formName.Data());
+        gSystem->Symlink(thisName.Data(),formName.Data());
+     }
+  }
 }
+//_____________________________________________
+// $id: $
+// $Log: STARDoc.C,v $
+// Revision 1.2  2000/04/28 19:47:00  fine
+// Creates symbolik links for the obsolete names
+//
+//_____________________________________________
+
