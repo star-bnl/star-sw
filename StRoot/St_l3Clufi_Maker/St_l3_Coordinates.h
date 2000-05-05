04e9db0a8e0859eeda436dfdff184b3e1ce924f4
flierl <>
2000-05-05 22:24:38 +0000

    get all necessary parameters out of the db

--- StRoot/St_l3Clufi_Maker/St_l3_Coordinates.h
+++ StRoot/St_l3Clufi_Maker/St_l3_Coordinates.h
@@ -1,83 +1,88 @@
 #ifndef St_l3_Coordinates_hh
 #define St_l3_Coordinates_hh
-#include <Rtypes.h>
+
+#define OFFLINE
+#ifdef OFFLINE
 #include <StObject.h>
+#endif
 
-
-
-class St_l3_xyz_Coordinate: public StObject {
+class St_l3_xyz_Coordinate  {
 
  private:
-    Double_t xyz[3] ;
+    double xyz[3] ;
   
  public:
     St_l3_xyz_Coordinate() { xyz[0] = xyz[1] = xyz[2] = 0 ; } ;  
-    St_l3_xyz_Coordinate(Double_t X, Double_t Y, Double_t Z )   
+    St_l3_xyz_Coordinate(double X, double Y, double Z )   
     {
      	    xyz[0] = X ;  
     	    xyz[1] = Y ;  
      	    xyz[2] = Z ;  
     } 
-    ~St_l3_xyz_Coordinate() { ; }; // empty destructor
+    virtual ~St_l3_xyz_Coordinate() { ; }; // empty destructor
 
     // Setters and Getters
-    Double_t Getx() const { return xyz[0] ; } ;
-    Double_t Gety() const { return xyz[1] ; } ;
-    Double_t Getz() const { return xyz[2] ; } ;
-    Double_t* Getxyz() { return xyz ; } ;
-    void Setx(Double_t X) { xyz[0] = X ; } ; 
-    void Sety(Double_t Y) { xyz[1] = Y ; } ; 
-    void Setz(Double_t Z) { xyz[2] = Z ; } ; 
-    void Setxyz(Double_t X, Double_t Y, Double_t Z ) 
+    double Getx() const { return xyz[0] ; } ;
+    double Gety() const { return xyz[1] ; } ;
+    double Getz() const { return xyz[2] ; } ;
+    double* Getxyz() { return xyz ; } ;
+    void Setx(double X) { xyz[0] = X ; } ; 
+    void Sety(double Y) { xyz[1] = Y ; } ; 
+    void Setz(double Z) { xyz[2] = Z ; } ; 
+    void Setxyz(double X, double Y, double Z ) 
 	{ 
 	    xyz[0] = X ;  
 	    xyz[1] = Y ;  
 	    xyz[2] = Z ;  
 	}
- 
+
+#ifdef OFFLINE
     // Root connection
-    ClassDef(St_l3_xyz_Coordinate, 1)
+    ClassDef(St_l3_xyz_Coordinate,1)
+#endif 
 } ;
 
 
 
-class St_l3_ptrs_Coordinate: public StObject {
+class St_l3_ptrs_Coordinate {
 
  private:
-    Double_t ptrs[4] ; // pad, time, row, sec as double !
+    double ptrs[4] ; // pad, time, row, sec as double !
   
  public:
      St_l3_ptrs_Coordinate() { ptrs[0] = ptrs[1] = ptrs[2] = ptrs[3] = 0 ; }  ;  // empty constructor
-     St_l3_ptrs_Coordinate(Double_t P,Double_t  T, Double_t R, Double_t S ) 
+     St_l3_ptrs_Coordinate(double P,double  T, double R, double S ) 
      {
        ptrs[0] = P ;
        ptrs[1] = T ;
        ptrs[2] = R ;
        ptrs[3] = S ;
      } ;
-    ~St_l3_ptrs_Coordinate() { ;}; // empty destructor
+    virtual ~St_l3_ptrs_Coordinate() { ;}; // empty destructor
 
      // Setters and Getters
-     Double_t Getp() const { return ptrs[0] ; } ;
-     Double_t Gett() const { return ptrs[1] ; } ;
-     Double_t Getr() const { return ptrs[2] ; } ;
-     Double_t Gets() const { return ptrs[3] ; } ;
-     Double_t* Getptrs() { return ptrs ; } ;
+     double Getp() const { return ptrs[0] ; } ;
+     double Gett() const { return ptrs[1] ; } ;
+     double Getr() const { return ptrs[2] ; } ;
+     double Gets() const { return ptrs[3] ; } ;
+     double* Getptrs() { return ptrs ; } ;
 
-     void Setp(Double_t P) { ptrs[0] = P ; } ; 
-     void Sett(Double_t T) { ptrs[1] = T ; } ; 
-     void Setr(Double_t R) { ptrs[2] = R ; } ; 
-     void Sets(Double_t S) { ptrs[2] = S ; } ; 
-     void Setptrs(Double_t P,Double_t  T, Double_t R, Double_t S ) 
+     void Setp(double P) { ptrs[0] = P ; } ; 
+     void Sett(double T) { ptrs[1] = T ; } ; 
+     void Setr(double R) { ptrs[2] = R ; } ; 
+     void Sets(double S) { ptrs[2] = S ; } ; 
+     void Setptrs(double P,double  T, double R, double S ) 
 	 {
 	     ptrs[0] = P ;
 	     ptrs[1] = T ;
 	     ptrs[2] = R ;
 	     ptrs[3] = S ; 
 	 } ;
- 
+
+#ifdef OFFLINE 
      // Root connection
      ClassDef(St_l3_ptrs_Coordinate, 1)
+#endif 
 } ;
 
 #endif //St_l3_Coordinates_hh
