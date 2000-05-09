e6bd96e7cdf6c624475c910f60653e24d53d7f4f
Thomas Ullrich <thomas.ullrich@bnl.gov>
2000-05-09 10:22:28 +0000

    Updated to cope with modified dst_TrgDet.idl

--- StRoot/StEvent/StCtbTriggerDetector.h
+++ StRoot/StEvent/StCtbTriggerDetector.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StCtbTriggerDetector.h,v 2.1 1999/10/13 19:42:56 ullrich Exp $
+ * $Id: StCtbTriggerDetector.h,v 2.2 2000/05/09 10:22:21 ullrich Exp $
  *
  * Author: Thomas Ullrich, Sep 1999
  ***************************************************************************
@@ -10,8 +10,8 @@
  ***************************************************************************
  *
  * $Log: StCtbTriggerDetector.h,v $
- * Revision 2.1  1999/10/13 19:42:56  ullrich
- * Initial Revision
+ * Revision 2.2  2000/05/09 10:22:21  ullrich
+ * Updated to cope with modified dst_TrgDet.idl
  *
  * Revision 2.1  1999/10/13 19:42:56  ullrich
  * Initial Revision
@@ -31,17 +31,31 @@ public:
     // StCtbTriggerDetector(const StCtbTriggerDetector&);            use default
     // StCtbTriggerDetector& operator=(const StCtbTriggerDetector&); use default
     
-    UInt_t   numberOfCtbCounters() const;
-    Float_t  mips(UInt_t) const;
-    Float_t  time(UInt_t) const;
+    UInt_t   numberOfTrays() const;
+    UInt_t   numberOfSlats() const;
+    UInt_t   numberOfPreSamples() const;
+    UInt_t   numberOfPostSamples() const;
+    UInt_t   numberOfAuxWords() const;
+    Float_t  mips(UInt_t tray, UInt_t slot, UInt_t evt = 0) const;
+    Char_t   time(UInt_t tray, UInt_t slot, UInt_t evt = 0) const;
+    Float_t  aux(UInt_t, UInt_t evt = 0) const;
 
-    void setMips(UInt_t, Float_t);
-    void setTime(UInt_t, Float_t);
+    void setMips(UInt_t, UInt_t, UInt_t, Float_t);
+    void setTime(UInt_t, UInt_t, UInt_t, Char_t);
+    void setAux(UInt_t, UInt_t, Float_t);
+    void setNumberOfPreSamples(UInt_t);
+    void setNumberOfPostSamples(UInt_t);
     
 protected:
-    enum {mMaxCtbCounter = 240};
-    Float_t  mMips[mMaxCtbCounter];
-    Float_t  mTime[mMaxCtbCounter];
+    enum {mMaxTrays = 120,
+	  mMaxSlats = 2,
+	  mMaxEventSamples = 11,
+	  mMaxAux = 16};
+    Float_t  mMips[mMaxTrays][mMaxSlats][mMaxEventSamples];
+    Char_t   mTime[mMaxTrays][mMaxSlats][mMaxEventSamples];
+    Float_t  mAux[mMaxAux][mMaxEventSamples];
+    Int_t    mNumberOfPreSamples;
+    Int_t    mNumberOfPostSamples;
     
     ClassDef(StCtbTriggerDetector,1)
 };
--- StRoot/StEvent/StMwcTriggerDetector.h
+++ StRoot/StEvent/StMwcTriggerDetector.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StMwcTriggerDetector.h,v 2.1 1999/10/13 19:43:29 ullrich Exp $
+ * $Id: StMwcTriggerDetector.h,v 2.2 2000/05/09 10:22:28 ullrich Exp $
  *
  * Author: Thomas Ullrich, Sep 1999
  ***************************************************************************
@@ -10,8 +10,8 @@
  ***************************************************************************
  *
  * $Log: StMwcTriggerDetector.h,v $
- * Revision 2.1  1999/10/13 19:43:29  ullrich
- * Initial Revision
+ * Revision 2.2  2000/05/09 10:22:28  ullrich
+ * Updated to cope with modified dst_TrgDet.idl
  *
  * Revision 2.1  1999/10/13 19:43:29  ullrich
  * Initial Revision
@@ -31,14 +31,29 @@ public:
     // StMwcTriggerDetector& operator=(const StMwcTriggerDetector&); use default
     virtual ~StMwcTriggerDetector();
 
-    UInt_t   numberOfMwcSectors() const;
-    Float_t  mips(UInt_t) const;
+    UInt_t   numberOfSectors() const;
+    UInt_t   numberOfSubSectors() const;
+    UInt_t   numberOfPreSamples() const;
+    UInt_t   numberOfPostSamples() const;
+    UInt_t   numberOfAuxWords() const;
 
-    void setMips(UInt_t, Float_t);
+    Float_t  mips(UInt_t sec, UInt_t subsec, UInt_t evt = 0) const;
+    Float_t  aux(UInt_t, UInt_t evt = 0) const;
+
+    void setMips(UInt_t, UInt_t, UInt_t, Float_t);
+    void setAux(UInt_t, UInt_t, Float_t);
+    void setNumberOfPreSamples(UInt_t);
+    void setNumberOfPostSamples(UInt_t);
     
 protected:
-    enum {mMaxMwcSectors = 96};
-    Float_t  mMips[mMaxMwcSectors];
+    enum {mMaxSectors = 24,
+	  mMaxSubSectors = 4,
+	  mMaxEventSamples = 11,
+	  mMaxAux = 32};
+    Float_t  mMips[mMaxSectors][mMaxSubSectors][mMaxEventSamples];
+    Float_t  mAux[mMaxAux][mMaxEventSamples];
+    Int_t    mNumberOfPreSamples;
+    Int_t    mNumberOfPostSamples;
     
     ClassDef(StMwcTriggerDetector,1)
 };
