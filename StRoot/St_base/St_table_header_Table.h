5ebc3cf29da53d90cc9157f2dbea87bfde454b84
Valeri Fine <val.fine@gmail.com>
2000-05-15 18:54:48 +0000

    ClassDefTable macro in use here

--- StRoot/St_base/St_table_header_Table.h
+++ StRoot/St_base/St_table_header_Table.h
@@ -1,5 +1,5 @@
 //*-- Author :    Valery Fine(fine@bnl.gov)   23/03/00
-// $Id: St_table_header_Table.h,v 1.4 2000/03/30 05:20:52 fine Exp $
+// $Id: St_table_header_Table.h,v 1.5 2000/05/15 18:54:48 fine Exp $
 //
 #ifndef STAF_St_table_header_Table
 #define STAF_St_table_header_Table
@@ -13,22 +13,12 @@ extern "C" {
 }
 
 class St_table_header : public TTable
-{
-protected:
-  static TTableDescriptor *fgColDescriptors;
-  virtual TTableDescriptor *GetDescriptorPointer() const { return fgColDescriptors;}
-  virtual void SetDescriptorPointer(TTableDescriptor *list) const { fgColDescriptors = list;}
-  St_table_header() : TTable("table_header",sizeof(table_head_st)) {SetType("table_head");}
-
-public:
-  St_table_header(const TTable *table);
-  table_head_st *GetTable(Int_t i=0) const { return ((table_head_st *)GetArray())+i;}
-  table_head_st &operator[](Int_t i){ assert(i>=0 && i < GetNRows()); return *GetTable(i); }
-  const table_head_st &operator[](Int_t i) const { assert(i>=0 && i < GetNRows()); 
-						   return *((const table_head_st *)(GetTable(i))); }
-  static TTable *Object(TTable *&table,const table_head_st *h);
-
-  ClassDef(St_table_header,0) //C++ wrapper for <table_header> 
+{ 
+ public:
+    St_table_header(const TTable *table);
+    static TTable *Object(TTable *&table,const table_head_st *h);
+    ClassDefTable(St_table_header,table_head_st)
+    ClassDef(St_table_header,0) //C++ wrapper for <table_header> 
 };
 
 //______________________________________________________________________________
@@ -41,6 +31,9 @@ inline TTable *St_table_header::Object(TTable *&table,const table_head_st *heade
 }
 //______________________________________________________________________________
 // $Log: St_table_header_Table.h,v $
+// Revision 1.5  2000/05/15 18:54:48  fine
+// ClassDefTable macro in use here
+//
 // Revision 1.4  2000/03/30 05:20:52  fine
 // bug fixed
 //
