188a02ae020878dbb31397ec9e9f724890b23e4f
Sakrejda Iwona <isakrejda@lbl.gov>
2000-05-25 00:35:36 +0000

    Copying dst_mon_soft_tpc to TpcTag table finished

--- StRoot/StTpcTagMaker/StTpcTagMaker.cxx
+++ StRoot/StTpcTagMaker/StTpcTagMaker.cxx
@@ -1,7 +1,10 @@
 //*-- Author : Iwona Sakrejda
 // 
-// $Id: StTpcTagMaker.cxx,v 1.3 2000/05/24 04:41:00 sakrejda Exp $
+// $Id: StTpcTagMaker.cxx,v 1.4 2000/05/25 00:35:36 sakrejda Exp $
 // $Log: StTpcTagMaker.cxx,v $
+// Revision 1.4  2000/05/25 00:35:36  sakrejda
+// Copying dst_mon_soft_tpc to TpcTag table finished
+//
 // Revision 1.3  2000/05/24 04:41:00  sakrejda
 // Body of the Make() method written
 //
@@ -54,13 +57,45 @@ Int_t StTpcTagMaker::Make(){
   if (!global) return 0;
   St_DataSetIter gime(global);
   St_dst_mon_soft_tpc *tpc_mon_data =(St_dst_mon_soft_tpc *) gime("mon_soft_tpc");
+  dst_mon_soft_tpc_st *tpc_mon_data_st = (dst_mon_soft_tpc_st *)tpc_mon_data->GetTable();
 
   // Create a data set and add the table to it.
   St_TpcTag *tagtab= new St_TpcTag("TpcTag",1); m_DataSet->Add(tagtab);
-  
+  //
+  TpcTag_st *tagtab_st = (TpcTag_st *) tagtab->GetTable();  
  
+  // Fill the Tpc Tags
+
+  tagtab_st->n_clus_tpc_tot = tpc_mon_data_st->n_clus_tpc_tot;
+  for(Int_t ij=0;ij<24;ij++){
+    tagtab_st->n_clus_tpc_in[ij] = tpc_mon_data_st->n_clus_tpc_in[ij];}
+  for(Int_t ij=0;ij<24;ij++){
+    tagtab_st->n_clus_tpc_out[ij] = tpc_mon_data_st->n_clus_tpc_out[ij];}
+  tagtab_st->n_pts_tpc_tot = tpc_mon_data_st->n_pts_tpc_tot;
+  for(Int_t ij=0;ij<24;ij++){
+    tagtab_st->n_pts_tpc_in[ij] = tpc_mon_data_st->n_pts_tpc_in[ij];}
+  for(Int_t ij=0;ij<24;ij++){
+    tagtab_st->n_pts_tpc_out[ij] = tpc_mon_data_st->n_pts_tpc_out[ij];}
+  tagtab_st->n_trk_tpc[0] = tpc_mon_data_st->n_trk_tpc[0];
+  tagtab_st->n_trk_tpc[1] = tpc_mon_data_st->n_trk_tpc[1];
+  for(Int_t ij=0;ij<10;ij++){
+    tagtab_st->chrg_tpc_drift[ij] = tpc_mon_data_st->chrg_tpc_drift[ij];}
+  tagtab_st->chrg_tpc_tot = tpc_mon_data_st->chrg_tpc_tot;
+  for(Int_t ij=0;ij<24;ij++){
+    tagtab_st->chrg_tpc_in[ij] = tpc_mon_data_st->chrg_tpc_in[ij];}
+  for(Int_t ij=0;ij<24;ij++){
+    tagtab_st->chrg_tpc_out[ij] = tpc_mon_data_st->chrg_tpc_out[ij];}
+  tagtab_st->hit_frac_tpc[0] = tpc_mon_data_st->hit_frac_tpc[0];
+  tagtab_st->hit_frac_tpc[1] = tpc_mon_data_st->hit_frac_tpc[1];
+  tagtab_st->avg_trkL_tpc[0] = tpc_mon_data_st->avg_trkL_tpc[0];
+  tagtab_st->avg_trkL_tpc[1] = tpc_mon_data_st->avg_trkL_tpc[1];
+  tagtab_st->res_pad_tpc[0] = tpc_mon_data_st->res_pad_tpc[0];
+  tagtab_st->res_pad_tpc[1] = tpc_mon_data_st->res_pad_tpc[1];
+  tagtab_st->res_drf_tpc[0] = tpc_mon_data_st->res_drf_tpc[0];
+  tagtab_st->res_drf_tpc[1] = tpc_mon_data_st->res_drf_tpc[1];
  return kStOK;
 }
+
 
 
 
