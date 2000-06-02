41fd827d67d4bfa1cc4855578dbbad1acb663931
porter <>
2000-06-02 13:42:39 +0000

    used shared library instead of static in example - plus changed example somewhat

--- StRoot/StDbLib/examples/ReadConfig.cc
+++ StRoot/StDbLib/examples/ReadConfig.cc
@@ -10,22 +10,19 @@ int main(){
 
 
   StDbManager* mgr = StDbManager::Instance();
-  mgr->setVerbose(true);
   StDbConfigNode* node = mgr->initConfig(dbCalibrations,dbTpc,"reconV0");
 
-  node->setFlavor("onl");
   unsigned int rTime = (unsigned int)time(NULL);
-  node->setProdTime(rTime);
   mgr->setRequestTime(rTime);
   mgr->fetchAllTables(node); 
   StDbTable* table = node->findTable("tpcElectronics");
 
-  ofstream of("tpcElectronics.Now.xml");
-  StDbXmlWriter writer(of);  
-  writer.ioTable(table);
 
-  //  mgr->setStoreTime(rTime);
-  //  mgr->storeAllTables(node);
+  if(table){
+    ofstream of("tpcElectronics.Now.xml");
+    StDbXmlWriter writer(of);  
+    writer.ioTable(table);
+  }
 
   delete node;
   delete mgr;
