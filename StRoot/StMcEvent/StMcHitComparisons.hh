621f27eea6d39d9fa5dbbf48ad7ad6aa7c58ec2b
calderon <>
2000-06-09 19:52:07 +0000

    No longer use 2 different functions for SVT and TPC that do the same thing, just use one function for the base class

--- StRoot/StMcEvent/StMcHitComparisons.hh
+++ StRoot/StMcEvent/StMcHitComparisons.hh
@@ -1,6 +1,6 @@
 /**********************************************
  *
- * $Id: StMcHitComparisons.hh,v 2.1 2000/03/07 15:09:54 calderon Exp $
+ * $Id: StMcHitComparisons.hh,v 2.2 2000/06/09 19:52:07 calderon Exp $
  *
  * Author: Manuel Calderon de la Barca Sanchez
  ***********************************************
@@ -10,6 +10,10 @@
  *
  ***********************************************
  * $Log: StMcHitComparisons.hh,v $
+ * Revision 2.2  2000/06/09 19:52:07  calderon
+ * No longer use 2 different functions for SVT and TPC that do the same thing,
+ * just use one function for the base class
+ *
  * Revision 2.1  2000/03/07 15:09:54  calderon
  * Initial Revision.
  * Comparisons used for sorting the hit containers, and
@@ -19,42 +23,52 @@
  **********************************************/
 #ifndef StMcHitComparisons_HH
 #define StMcHitComparisons_HH
-
+class StHit;
 class StTpcHit;
 class StSvtHit;
 class StFtpcHit;
 class StRichHit;
 
+class StMcHit;
 class StMcTpcHit;
 class StMcSvtHit;
 class StMcFtpcHit;
 class StMcRichHit;
-
+class StThreeVectorF;
 #if (defined __SUNPRO_CC && (__SUNPRO_CC < 0x500))
 // bool is defined in utility for SUNPRO_CC 4.2
 #include <utility> 
 #endif
-
-struct compTpcHit{
-    bool operator()(const StTpcHit*,const StTpcHit*) const;
+struct compHit{
+    bool operator()(const StHit*, const StHit*) const;
 };
 
-struct compMcTpcHit{
-    bool operator()(const StMcTpcHit*,const StMcTpcHit*) const;
+struct compMcHit{
+    bool operator()(const StMcHit*, const StMcHit*) const;
 };
 
-struct compSvtHit{
-    bool operator()(const StSvtHit*,const StSvtHit*) const;
-};
+// struct compTpcHit{
+//     bool operator()(const StTpcHit* h1,const StTpcHit* h2) const;
+// };
 
-struct compMcSvtHit{
-    bool operator()(const StMcSvtHit*,const StMcSvtHit*) const;
-};
+// struct compMcTpcHit{
+//     bool operator()(const StMcTpcHit* h1,const StMcTpcHit* h2) const;
+// };
 
+// struct compSvtHit{
+//     bool operator()(const StSvtHit* h1,const StSvtHit* h2) const;
+// };
+
+// struct compMcSvtHit{
+//     bool operator()(const StMcSvtHit* h1,const StMcSvtHit* h2) const;
+// };
+
+struct compRPhi{
+    bool operator()(const StThreeVectorF&, const StThreeVectorF&) const;
+};
 struct compFtpcHit{
     bool operator()(const StFtpcHit*,const StFtpcHit*) const;
 };
-
 struct compMcFtpcHit{
     bool operator()(const StMcFtpcHit*,const StMcFtpcHit*) const;
 };
