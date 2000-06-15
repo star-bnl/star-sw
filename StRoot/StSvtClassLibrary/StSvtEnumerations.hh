6878f1e939f9934efbcd44648057ffa9e09c05bb
Helen L. Caines <helen.caines@yale.edu>
2000-06-15 15:48:54 +0000

    Add Enumeration Class for SVT

---
+++ StRoot/StSvtClassLibrary/StSvtEnumerations.hh
@@ -0,0 +1,24 @@
+/***************************************************************************
+ *
+ * $Id: StSvtEnumerations.hh,v 1.1 2000/06/15 15:48:54 caines Exp $
+ *
+ * Author: Marcelo Munhoz
+ ***************************************************************************
+ *
+ * Description: SVT Enumerations
+ *
+ ***************************************************************************
+ *
+ * $Log: StSvtEnumerations.hh,v $
+ * Revision 1.1  2000/06/15 15:48:54  caines
+ * Add Enumeration Class for SVT
+ *
+ **************************************************************************/
+
+#ifndef STSVTENUMERATIONS_HH
+#define STSVTENUMERATIONS_HH
+
+enum pedestalType {kCapacitor, kTime};
+enum dbSvtType {kCalibration, kGeometry, kConditions};
+
+#endif
