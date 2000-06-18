6b5ead144cc4d92f31bca97ab1a1951de65d29f3
vanyashi <>
2000-06-18 21:12:04 +0000

    code cleaned up

--- StRoot/macros/examples/Example_tags.C
+++ StRoot/macros/examples/Example_tags.C
@@ -1,11 +1,11 @@
-void Example_tags(const char* topDir = "/star/rcf/GC/daq/tags/")
+void Example_tags(TString topDir = "/star/rcf/GC/daq/tags")
 {
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
 // Example_tags.C                                                       //
 //                                                                      //
 // shows how to use the STAR tags files                                 //
-// Input: top level directory (must end with "/")                       //
+// Input: top level directory                                           //
 //                                                                      //
 // what it does:                                                        //
 // 1. creates TChain from all tags files down from the topDir           //
@@ -23,64 +23,58 @@ void Example_tags(const char* topDir = "/star/rcf/GC/daq/tags/")
   // set loop optimization level
   gROOT->ProcessLine(".O4");
   // gather all files from the same top directory into one chain
+  // topDir must end with "/"
+  topDir +='/';
   St_FileSet dirs(topDir);
   St_DataSetIter next(&dirs,0);
   St_DataSet *set = 0; 
   TChain chain("Tag");
   while ( (set = next()) ) {           
-    if (strcmp(set->GetTitle(),"file") || !(strstr(set->GetName(),".tags.root"))) continue;
-    TString p = set->Path();
-    Char_t *tagsfile = gSystem->ConcatFileName(topDir,p.Data());
-    chain.Add(tagsfile);
-  } 
-  cout<<"chained "<<chain->GetEntries()<<" events "<<endl;
+    if (strcmp(set->GetTitle(),"file") || 
+	!(strstr(set->GetName(),".tags.root"))) continue;
+    chain.Add(gSystem->ConcatFileName(topDir,set->Path()));
+  }
+  UInt_t nEvents = chain->GetEntries();
+  cout<<"chained "<<nEvents<<" events "<<endl;
 
   TObjArray *files = chain.GetListOfFiles();
   UInt_t nFiles = files->GetEntriesFast();
   cout << "chained " << nFiles << " files from " << topDir << endl;
 
-  TObjArray *branches = chain.GetListOfBranches();
   TObjArray *leaves = chain.GetListOfLeaves();
   Int_t nleaves = leaves->GetEntriesFast();
 
-  TString file;
-  Int_t l;
-  TLeaf *leaf;
-  TBranch *branch;
-
-  TString *tName = new TString(" ");
-  TNamed *tableName;
+  TString tableName = " ";
   TObjArray *tagTable = new TObjArray;
+
   Int_t tableCount = 0;
   Int_t *tableIndex = new Int_t[nleaves];
   Int_t tagCount = 0;
 
-  //count number of tag tables encoded in the TTree branch names
+  // decode tag table names
   for (Int_t l=0;l<nleaves;l++) {
-    leaf = (TLeaf*)leaves->UncheckedAt(l);
+    TLeaf *leaf = (TLeaf*)leaves->UncheckedAt(l);
     tagCount+=leaf->GetNdata();
-    branch = leaf->GetBranch();
-    //new tag table name
-    if ( strstr(branch->GetName(), tName->Data()) == 0 ) {
-      tName = new TString(branch->GetName());
-      tName->Resize(tName->Index("."));
-      //the tableName is encoded in the branch Name before the "."
-      tableName = new TNamed(tName->Data(),"Tag");
-      tagTable->AddLast(tableName);
+    TBranch *branch = leaf->GetBranch();
+    // new tag table name
+    if ( strstr(branch->GetName(), tableName.Data()) == 0 ) {
+      tableName = branch->GetName();
+      // the tableName is encoded in the branch Name before the "."
+      tableName.Resize(tableName->Last('.'));
+      tagTable->AddLast(new TObjString(tableName.Data()));
       tableCount++;
     }
     tableIndex[l]=tableCount-1;
   }
-
-  cout << " tot num tables,tags = " << tableCount << "   " 
+  cout << " tot num tables, tags = " << tableCount << "   " 
        << tagCount << endl << endl;
 
-  //print out names of all tags and the first value
+  //EXAMPLE 1: how to print out names of all tags and values for first event
   for (l=0;l<nleaves;l++) {
     leaf = (TLeaf*)leaves->UncheckedAt(l);
     branch = leaf->GetBranch();
     branch->GetEntry();
-    //tag comment is in the title
+    // tag comment is in the title
     TString Title = leaf->GetTitle();
     Int_t dim = leaf->GetNdata();
     if (dim==1) {
@@ -89,8 +83,8 @@ void Example_tags(const char* topDir = "/star/rcf/GC/daq/tags/")
     }
     cout << "\n Table: ";
     cout.width(10);
-    cout << ((TNamed*)tagTable->UncheckedAt(tableIndex[l]))->GetName()<< 
-      " -- has tag: " << Title<<endl;
+    cout << ((TObjString*)tagTable->UncheckedAt(tableIndex[l]))->GetString()
+	 <<" -- has tag: " << Title << endl;
     for (Int_t i=0;i<dim;i++) {
       cout <<"                               "<< leaf->GetName();
       if (dim>1) cout << '['<<i<<']';
@@ -98,48 +92,41 @@ void Example_tags(const char* topDir = "/star/rcf/GC/daq/tags/")
     }
   }
 
-  //example of a plot:
+  // EXAMPLE 2: how to make a plot
   c1 = new TCanvas("c1","Beam-Gas Rejection",600,1000);
   gStyle->SetMarkerStyle(8);
   chain->Draw("n_trk_tpc[0]:n_trk_tpc[1]");
 
-  ntrk = chain.GetLeaf("n_trk_tpc");
-  TLeaf *event = chain.GetLeaf("mEventNumber");
-  TLeaf *ivent = chain.GetLeaf("mIventNumber");
-  TBranch *ebranch = event->GetBranch();
-
-  //example of a selection: write event numbers for collisions on the plot
+  // EXAMPLE 3: how to make a selection (write selected event numbers on the plot)
   Int_t ncoll=0;
   char aevent[10];
   TText t(0,0,"a");
   t.SetTextFont(52);
   t.SetTextSize(0.02);
   Float_t cut = 0.35;
-  //loop over all events: READ ONLY REQUIRED BRANCHES!
+  cout <<"\n Events with ntrk>400 and |asim|<"<<cut<<endl;
+  //loop over all events: READ ONLY n_trk_tpc AND mEventNumber BRANCHES!
   gBenchmark->Start("loop");
   for (Int_t i=0;i<nFiles;i++) {
-    file = (files->UncheckedAt(i))->GetTitle();
-    cout <<"Start of file "<< i+1 <<": "<< file.Data() <<endl;
     chain.LoadTree(*(chain.GetTreeOffset()+i));
     TTree *T = chain.GetTree();
     //must renew leaf pointer for each tree
-    ntrk = T->GetLeaf("n_trk_tpc");
-    event = T->GetLeaf("mEventNumber");
-    ivent = T->GetLeaf("mIventNumber");
-    for (Int_t j = 0; j<T->GetEntries();j++){
+    TLeaf *ntrk = T->GetLeaf("n_trk_tpc");
+    TLeaf *run = T->GetLeaf("mRunNumber");
+    TLeaf *event = T->GetLeaf("mEventNumber");
+    for (Int_t j=0; j<T->GetEntries(); j++){
       ntrk->GetBranch()->GetEntry(j);
       event->GetBranch()->GetEntry(j);
-      ivent->GetBranch()->GetEntry(i);
-      
+      run->GetBranch()->GetEntry(j);
       Int_t Nm=ntrk->GetValue(0);
       Int_t Np=ntrk->GetValue(1);
       Int_t Ntrk = Np+Nm;
-      Float_t asim=(Np-Nm)/float(Np+Nm);
+      // avoid division by 0
+      Float_t asim = Np-Nm;
+      if (Ntrk>0) asim /= Ntrk;
       if (-cut < asim&&asim < cut && Ntrk>400) {
-	cout
-	  <<" bfc event number for this file = "<<ivent->GetValue()
-	  <<" DAQ event number = "<<event->GetValue()
-	  <<endl;
+	cout<<"   Run "<<(UInt_t)run->GetValue()
+	    <<", Event "<<event->GetValue() <<endl;
 	ncoll++;
 	sprintf(aevent,"%d",event->GetValue());
 	t.DrawText(Np+10,Nm+10,aevent);
@@ -150,7 +137,8 @@ void Example_tags(const char* topDir = "/star/rcf/GC/daq/tags/")
   t.SetTextSize(0.05);
   t.DrawText(50,2550,"ntrk>400 and |(Np-Nm)/(Np+Nm)| < 0.35 ");
   t.DrawText(500,-300,"Ntrk with tanl<0 ");
-  cout <<" number of events with ntrk>400 and |asim|<"<<cut<<" = "<<ncoll<<endl;
+  cout << " Selected " << ncoll << " collision candidates out of "
+       << nEvents << " events" << endl;
   // stop timer and print benchmarks
   gBenchmark->Print("loop");  
   gBenchmark->Stop("total");
