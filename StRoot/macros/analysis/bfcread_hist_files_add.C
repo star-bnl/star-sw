c50e9e940e21546e3245bd841175445275fd78e6
lansdell <>
2000-06-23 23:16:12 +0000

    now reads .hist.root file list from text file; be sure to use nevents <= the number of .hist.root files

--- StRoot/macros/analysis/bfcread_hist_files_add.C
+++ StRoot/macros/analysis/bfcread_hist_files_add.C
@@ -1,5 +1,8 @@
-// $Id: bfcread_hist_files_add.C,v 2.7 2000/06/23 21:34:21 kathy Exp $
+// $Id: bfcread_hist_files_add.C,v 2.8 2000/06/23 23:16:12 lansdell Exp $
 // $Log: bfcread_hist_files_add.C,v $
+// Revision 2.8  2000/06/23 23:16:12  lansdell
+// now reads .hist.root file list from text file; be sure to use nevents <= the number of .hist.root files
+//
 // Revision 2.7  2000/06/23 21:34:21  kathy
 // remove unneeded macro; make bfcread_hist_files_add work with a chain - cleanup
 //
@@ -25,27 +28,27 @@
 // bfcread_hist_files_add.C 
 //
 // what it does: 
-//  - reads in 2 different *.hist.root file produced from a chain 
+//  - reads in different *.hist.root files produced from a chain 
 //               (such as bfc)
 //  - creates a copy of all histograms in the first file (newHist)
 //  - adds contents of all histograms in subsequent files
 //          with the same NAME into the newHist
 //
-//  **** currently only works with 2 input files!
-//  **** needs to be changed to work with input file list!
 //  **** needs to have new histograms written out to new file
 //
 // inputs: 
-//    MainFile - *.hist.root file from bfc output
-//    MakerHistDir - directory name of Maker that you want histograms 
-//                   from (this will be first input when you did constructor)
-//             -- see standard Maker names note below!
+//   nevents - number of files to process (assuming 1 "event" per file)
+//   fileName - .txt file with list of hist.root files from bfc output
+//   outHistFile - output .hist.root file
+//   MakerHistDir - directory name of Maker that you want histograms 
+//                  from (this will be first input when you did constructor)
+//                - see standard Maker names note below!
 //   TopDirTree - top level directory tree in your input hist file
 //                (this is 3rd argument of constructor for StTreeMaker that
 //                 you probably used to write the *.hist.root file)
 //            NOTE: if you ran bfc, then the TopDirTree = bfcTree !!
-//   mxCopy -- maximum number of histograms in first file 
-//              (this is the max number that will be copied)
+//   mxCopy - maximum number of histograms in first file 
+//            (this is the max number that will be copied)
 //
 //======================================================================
 
@@ -62,92 +65,99 @@ class StHistUtil;
 //------------------------------------------------------------------------
 
 void bfcread_hist_files_add(
-  const Char_t *MainFile1=
-    "/star/rcf/test/dev/tfs_redhat61/Tue/year_1h/hc_standard/hc_standard.40_evts.hist.root",
- const Char_t *MainFile2=
-    "/star/rcf/test/dev/tfs_redhat61/Tue/year_1h/hc_lowdensity/hc_lowdensity.400_evts.hist.root",
+  int nevents=3,
+  const Char_t *fileName="fileList.txt",
   const Char_t *outHistFile="kathy",
   const Char_t *MakerHistDir="QA",
   const Char_t *TopDirTree="bfcTree")
 {
-  cout << "bfcread_hist_files_add.C, input hist file 1= " 
-       << MainFile1 << endl;
-  cout << "bfcread_hist_files_add.C, input hist file 2= " 
-       << MainFile2 << endl;
+  cout << "bfcread_hist_files_add.C, input file list = " 
+       << fileName << endl;
+  cout << "bfcread_hist_files_add.C, output file list = "
+       << outHistFile << endl;
   cout << "bfcread_hist_files_add.C, directory name for hist = " 
        << MakerHistDir << endl;
   cout << "bfcread_hist_files_add.C, top level directory in hist file = " 
        << TopDirTree << endl;
 
-  Int_t fnum=2;
-  cout << "bfcread_hist_files_add.C, hardwired # input files = " << 
-    fnum << endl << endl <<endl;
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+  gSystem->Load("StIOMaker");
+  gSystem->Load("St_QA_Maker");
+  gSystem->Load("StarClassLibrary");
+  gSystem->Load("StUtilities");
+  gSystem->Load("StAnalysisUtilities");
+  gSystem->Load("libglobal_Tables");
 
-//
-    gSystem->Load("St_base");
-    gSystem->Load("StChain");
-    gSystem->Load("StIOMaker");
-    gSystem->Load("St_QA_Maker");
-    gSystem->Load("StarClassLibrary");
-    gSystem->Load("StUtilities");
-    gSystem->Load("StAnalysisUtilities");
-    gSystem->Load("libglobal_Tables");
+// read file list from text file
+  char **fList = new char[512][200];
+  ifstream fin(fileName);
+  if (fin) {
+    int j=0;
+    cout << "Input file " << fileName << " found." << endl;
+    while (!fin.eof()) {
+      char *line = new char[200];
+      fin >> line;
+      fList[j] = line;
+      cout << " Added file : " << fList[j] << endl;
+      j++;
+    }
+  }
+  fin.close();
 
 //  Setup top part of chain
-   chain = new StChain("MyChain");
+  chain = new StChain("MyChain");
 
 // constructor for other maker (not used in chain)
-   StHistUtil   *HU  = new StHistUtil;
+  StHistUtil   *HU  = new StHistUtil;
 
 // loop over files:
- Int_t ifl=1;
- Int_t hCCount=0;
+  Int_t istat=0;
+  Int_t ifl=0;
+  Int_t hCCount=0;
+  Int_t hACount=0;
 
- EventLoop: if (ifl<=fnum) {  
-
- cout << endl << " NOW GOING TO POINT TO FILE " << ifl <<  endl;
-
- Char_t *MainFile=0;
- if (ifl==1) MainFile=MainFile1;
- if (ifl==2) MainFile=MainFile2;
+ EventLoop: if (ifl<nevents && istat==0) {  
   
- StIOMaker *IOMk = new StIOMaker("IO","r",MainFile,TopDirTree);
-    IOMk->SetIOMode("r");
-    IOMk->SetBranch("*",0,"0");                 //deactivate all branches
-    IOMk->SetBranch("histBranch",0,"r"); //activate hist Branch
+   cout << "Processing Event : " << ifl+1 << endl;
+   cout << endl << " NOW GOING TO POINT TO FILE " << ifl+1 <<  endl;
 
-  HU->SetPntrToMaker((StMaker *)IOMk);
+   StIOMaker *IOMk = new StIOMaker("IO","r",fList[ifl],TopDirTree);
+   IOMk->SetIOMode("r");
+   IOMk->SetBranch("*",0,"0");                 //deactivate all branches
+   IOMk->SetBranch("histBranch",0,"r"); //activate hist Branch
+
+   HU->SetPntrToMaker((StMaker *)IOMk);
 
 // --- each file contains only histograms (1 "event" == 1 Make call)
-  chain->Init();
-  chain->Clear();
-  chain->Make();
+   chain->Init();
+   chain->Clear();
+   istat = chain->Make();
 
 // get the TList pointer to the histograms:
-  TList  *dirList = 0;
-  dirList = HU->FindHists(MakerHistDir);
+   TList  *dirList = 0;
+   dirList = HU->FindHists(MakerHistDir);
 
 // clone(copy) histograms from first file  -----------------------------
- if (ifl==1) {
+   if (ifl==0) {
 
 // now make a copy of all histograms into my new histograms!
-  hCCount = HU->CopyHists(dirList);
+     hCCount = HU->CopyHists(dirList);
 
-  cout << "bfcread_hist_files_add.C, # histograms copied = " << 
+     cout << "bfcread_hist_files_add.C, # histograms copied = " << 
        hCCount << endl;
 
- }  // if ifl==1
+   }  // if ifl==0
 
- else{
+   else {
 
 // now make a copy of all histograms into my new histograms!
-  Int_t hACount=0;
-  hACount = HU->AddHists(dirList,hCCount);
+     hACount = HU->AddHists(dirList,hCCount);
 
-  cout << "bfcread_hist_files_add.C, # histograms added = " << 
-    hACount << endl << endl;
+     cout << "bfcread_hist_files_add.C, # histograms added = " << 
+       hACount << endl << endl;
 
- }  //else (ifl not #1)
+   }  //else (ifl not #1)
 
    ifl++;                                
    goto EventLoop;   
@@ -157,14 +167,14 @@ void bfcread_hist_files_add(
 
 // to see an example of histograms being added together:   
   cout << "bfcread_hist_files_add.C, an example! = " <<  endl;
-   Int_t imk = 0;
-   for (imk=0;imk<hCCount;imk++) {
-       TH1** kathyArray = HU->getNewHist();
-       if (strcmp(kathyArray[imk]->GetName(),"TabQaEvsumTrkTot")==0) {       
-	 kathyArray[imk]->Draw();
-          gPad->Update();
-       } // if strcmp -- to draw
-   } // for -- end of example
+  Int_t imk = 0;
+  for (imk=0;imk<hCCount;imk++) {
+    TH1** kathyArray = HU->getNewHist();
+    if (strcmp(kathyArray[imk]->GetName(),"TabQaEvsumTrkTot")==0) {       
+      kathyArray[imk]->Draw();
+      gPad->Update();
+    } // if strcmp -- to draw
+  } // for -- end of example
 
 
 // constructor 
