a9fe60e1ebf5e0bee72ff0e4266504a3d2d590ff
kathy <>
2000-06-23 16:19:25 +0000

    macro that will read list of files and add all histograms together

---
+++ StRoot/macros/analysis/bfcread_hist_filelist_add.C
@@ -0,0 +1,172 @@
+// $Id: bfcread_hist_filelist_add.C,v 2.1 2000/06/23 16:19:25 kathy Exp $
+// $Log: bfcread_hist_filelist_add.C,v $
+// Revision 2.1  2000/06/23 16:19:25  kathy
+// macro that will read list of files and add all histograms together
+//
+//
+//=======================================================================
+// owner:  Kathy Turner
+//
+// bfcread_hist_filelist_add.C 
+//
+// what it does: 
+//  - reads in list of  different *.hist.root file produced from a chain 
+//               (such as bfc)
+//  - creates a copy of all histograms in the first file (newHist)
+//  - adds contents of all histograms in subsequent files
+//          with the same NAME into the newHist
+//
+//  **** needs to be changed to work with input file list!
+//  **** needs to have new histograms written out to new file
+//
+// inputs: 
+//    MainFile - *.hist.root file from bfc output
+//    MakerHistDir - directory name of Maker that you want histograms 
+//                   from (this will be first input when you did constructor)
+//             -- see standard Maker names note below!
+//   TopDirTree - top level directory tree in your input hist file
+//                (this is 3rd argument of constructor for StTreeMaker that
+//                 you probably used to write the *.hist.root file)
+//            NOTE: if you ran bfc, then the TopDirTree = bfcTree !!
+//   mxCopy -- maximum number of histograms in first file 
+//              (this is the max number that will be copied)
+//
+//======================================================================
+
+class StChain;
+StChain *chain;
+
+class StIOMaker;
+StIOMaker *IOMk=0;
+
+class StHistUtil;
+
+//------------------------------------------------------------------------
+
+void bfcread_hist_files_add(
+  const Char_t *MainFile1=
+    "/star/rcf/test/dev/tfs_redhat61/Tue/year_1h/hc_standard/hc_standard.40_evts.hist.root",
+ const Char_t *MainFile2=
+    "/star/rcf/test/dev/tfs_redhat61/Tue/year_1h/hc_lowdensity/hc_lowdensity.400_evts.hist.root",
+  const Char_t *MakerHistDir="QA",
+  const Char_t *TopDirTree="bfcTree")
+{
+  cout << "bfcread_hist_files_add.C, input hist file 1= " 
+       << MainFile1 << endl;
+  cout << "bfcread_hist_files_add.C, input hist file 2= " 
+       << MainFile2 << endl;
+  cout << "bfcread_hist_files_add.C, directory name for hist = " 
+       << MakerHistDir << endl;
+  cout << "bfcread_hist_files_add.C, top level directory in hist file = " 
+       << TopDirTree << endl;
+
+  Int_t fnum=2;
+  cout << "bfcread_hist_files_add.C, hardwired # input files = " << 
+    fnum << endl << endl <<endl;
+
+//
+    gSystem->Load("St_base");
+    gSystem->Load("StChain");
+    gSystem->Load("StIOMaker");
+    gSystem->Load("StarClassLibrary");
+    gSystem->Load("StUtilities");
+    gSystem->Load("StAnalysisUtilities");
+    gSystem->Load("libglobal_Tables");
+
+
+// constructor for other maker (not used in chain)
+   StHistUtil   *HU  = new StHistUtil;
+
+
+// loop over files:
+ Int_t ifl=1;
+
+ EventLoop: if (ifl<=fnum) {  
+
+ cout << endl << " NOW GOING TO POINT TO FILE " << ifl <<  endl;
+
+// clone(copy) histograms from first file  -----------------------------
+ if (ifl==1) {
+
+// read in file 1:
+  StIOMaker *IOMk = new StIOMaker("IO","r",MainFile1,TopDirTree);
+  IOMk->SetIOMode("r");
+  IOMk->SetBranch("*",0,"0");                 //deactivate all branches
+  IOMk->SetBranch("histBranch",0,"r"); //activate hist Branch
+
+// --- each file contains only histograms (1 "event" == 1 Make call)
+  IOMk->Init();
+  IOMk->Clear();
+  IOMk->Make();
+// - end of read
+
+  HU->SetPntrToMaker((StMaker *)IOMk);
+
+// get the TList pointer to the histograms:
+  TList  *dirList = 0;
+  dirList = HU->FindHists(MakerHistDir);
+
+// now make a copy of all histograms into my new histograms!
+  Int_t hCCount=0;
+  hCCount = HU->CopyHists(dirList);
+
+  cout << "bfcread_hist_files_add.C, # histograms copied = " << 
+       hCCount << endl;
+
+ }  // if ifl==1
+
+ else{
+
+  if (ifl==2) {
+// 
+    StIOMaker *IOMk = new StIOMaker("IO","r",MainFile2,TopDirTree);
+    IOMk->SetIOMode("r");
+    IOMk->SetBranch("*",0,"0");                 //deactivate all branches
+    IOMk->SetBranch("histBranch",0,"r"); //activate hist Branch
+
+// --- each file contains only histograms (1 "event" == 1 Make call)
+    IOMk->Init();
+    IOMk->Clear();
+    IOMk->Make();
+
+    HU->SetPntrToMaker((StMaker *)IOMk);
+
+  } // if ifl=2
+
+// get the TList pointer to the histograms:
+  TList  *dirList = 0;
+  dirList = HU->FindHists(MakerHistDir);
+
+// now make a copy of all histograms into my new histograms!
+  Int_t hACount=0;
+  hACount = HU->AddHists(dirList,hCCount);
+
+  cout << "bfcread_hist_files_add.C, # histograms added = " << 
+    hACount << endl << endl;
+
+// to see an example of histograms being added together:   
+  cout << "bfcread_hist_files_add.C, an example! = " <<  endl;
+   Int_t imk = 0;
+   for (imk=0;imk<hCCount;imk++) {
+       TH1** kathyArray = HU->getNewHist();
+       if (strcmp(kathyArray[imk]->GetName(),"TabQaEvsumTrkTot")==0) {       
+	 kathyArray[imk]->Draw();
+          gPad->Update();
+       } // if strcmp -- to draw
+   } // for -- end of example
+
+ }  //else (ifl not #1)
+
+   ifl++;                                
+   goto EventLoop;   
+
+ } // loop over files
+
+} // end of the macro!
+ 
+
+
+
+
+
+
