300e72545b83d6810f90e46fc086fb8a077eee4a
caines <>
2000-07-06 03:47:31 +0000

    Add PedOffset to class so can calc. sum adc

--- StRoot/StSvtClassLibrary/StSvtData.hh
+++ StRoot/StSvtClassLibrary/StSvtData.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StSvtData.hh,v 1.1.1.1 2000/03/10 14:26:21 munhoz Exp $
+ * $Id: StSvtData.hh,v 1.2 2000/07/06 03:47:31 caines Exp $
  *
  * Author: Marcelo Munhoz
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StSvtData.hh,v $
+ * Revision 1.2  2000/07/06 03:47:31  caines
+ * Add PedOffset to class so can calc. sum adc
+ *
  * Revision 1.1.1.1  2000/03/10 14:26:21  munhoz
  * SVT Class Library
  *
@@ -40,12 +43,14 @@ public:
   void setTrigWord(int trigger) { mTriggerWord = trigger;}
   void setSCAZero(int SCAZero){ mSCAZero = SCAZero;}
   void setTimeZero(int t0, int sector){ mTimeZero[sector-1] = t0;}
+  void setPedOffset(int offset){mPedOffset = offset;}
 
   int getRunNumber()   {return mRunNumber;}
   int getEventNumber() {return mEventNumber;}
   int getTrigWord() {return mTriggerWord;}
   int getSCAZero(){return mSCAZero;}
   int getTimeZero(int sector){return mTimeZero[sector-1];}
+  int getPedOffset(){return mPedOffset;}
 
 protected:
   StSvtHybridData* mData; // Hybrid Data Object
@@ -55,6 +60,7 @@ protected:
   int mTriggerWord; // Trigger Type
   int mSCAZero;     // Number of SCA capacitor for time bucket zero
   int mTimeZero[N_SECTORS];    // Time zero given by each read out box (fiber header)
+  int mPedOffset; //Pedestal offset added so can see zero
 
   ClassDef(StSvtData,1)
 };
