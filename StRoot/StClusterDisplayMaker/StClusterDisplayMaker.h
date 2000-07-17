88c8049e84a088dc7569ea4a84b37e6f5cf342f7
flierl <>
2000-07-17 22:08:44 +0000

    major changes in nearly all parts of the maker

--- StRoot/StClusterDisplayMaker/StClusterDisplayMaker.h
+++ StRoot/StClusterDisplayMaker/StClusterDisplayMaker.h
@@ -1,4 +1,4 @@
-// $Id: StClusterDisplayMaker.h,v 1.2 2000/07/09 20:15:55 flierl Exp $
+// $Id: StClusterDisplayMaker.h,v 1.3 2000/07/17 22:08:44 flierl Exp $
 #ifndef STAR_StClusterDisplayMaker
 #define STAR_StClusterDisplayMaker
 
@@ -18,31 +18,32 @@
 class StClusterDisplayMaker : public StMaker {
 
  private:
-    // static Char_t  m_VersionCVS = "$Id: StClusterDisplayMaker.h,v 1.2 2000/07/09 20:15:55 flierl Exp $";
+    // static Char_t  m_VersionCVS = "$Id: StClusterDisplayMaker.h,v 1.3 2000/07/17 22:08:44 flierl Exp $";
     TTree* mytree ;
     TH2S*  myhist ;
         
     Int_t sec ;
     Int_t row ;
+    Int_t pad_max ;
+    Int_t pad_min ;
+    Int_t time_min ;
+    Int_t time_max ;
     
     // l3 off
     TGraphErrors* graph_l3_off_points ;
     Float_t* l3offpoints_pad;
     Float_t* l3offpoints_time;
+    Double_t Max_time_bucket_l3off ;
     
-
     // l3 on
     TGraphErrors* graph_l3_on_points ;
     Float_t* l3onpoints_pad;
     Float_t* l3onpoints_time;
+    Double_t Max_time_bucket_l3on ;
 
     // off
-    TGraphErrors* graph_off_points ;
-    Float_t* offpoints_pad ;
-    Float_t* offpoints_pad_err ;
-    Float_t* offpoints_time ;
-    Float_t* offpoints_time_err;
-
+    Double_t Max_time_bucket_off ;
+    
     // matcher
     TGraphErrors* matched_points ;
     Float_t* matched_points_pad ;
@@ -61,9 +62,14 @@ class StClusterDisplayMaker : public StMaker {
    virtual Int_t  Init();
    virtual Int_t  Make(Int_t sec = 1, Int_t row = 1, Char_t* opt =""  ,Int_t pad_min = 1, Int_t pad_max =184, Int_t time_min = 0, Int_t time_max = 511);
    virtual Int_t Get_l3off_points();
-   virtual Int_t Get_off_points();
+   virtual Int_t getOffPoints(Float_t* padvec, Float_t* padvecerr, 
+				Float_t* timevec, Float_t* timevecerr, 
+				Int_t max_number_of_points, Char_t* off_on_track);
    virtual Int_t Get_l3on_points();
    virtual Int_t Get_matched_points(Int_t&);
+   virtual void displayScale() ;
+   virtual void displayInfo() ;
+   virtual void help() ;
 
 // virtual Int_t InitRun  (int runumber){return 0;}; // Overload empty StMaker::InitRun 
 // virtual Int_t FinishRun(int runumber){return 0;}; // Overload empty StMaker::FinishRun 
@@ -73,13 +79,13 @@ class StClusterDisplayMaker : public StMaker {
    void Fill_Pixel_into_histo_artificial(TH2S* histo, Int_t row, Int_t sec) ;
    TTree* GetTree() {return mytree;};
    TH2S* GetHist() {return myhist;};
-   TGraphErrors* Get_graph_l3_off_points() { return graph_l3_off_points;} ;
-   TGraphErrors* Get_graph_off_points() { return graph_off_points;} ;
-   TGraphErrors* Get_graph_l3_on_points() { return graph_l3_on_points;} ;
-   TGraphErrors* Get_graph_matched_points() { return matched_points;} ;
+   //TGraphErrors* Get_graph_l3_off_points() { return graph_l3_off_points;} ;
+   //TGraphErrors* Get_graph_off_points() { return graph_off_points;} ;
+   //TGraphErrors* Get_graph_l3_on_points() { return graph_l3_on_points;} ;
+   //TGraphErrors* Get_graph_matched_points() { return matched_points;} ;
 
    virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StClusterDisplayMaker.h,v 1.2 2000/07/09 20:15:55 flierl Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StClusterDisplayMaker.h,v 1.3 2000/07/17 22:08:44 flierl Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
    ClassDef(StClusterDisplayMaker, 1)   //StAF chain virtual base class for Makers
 };
