78e60f70576cea6c88e7b3691651b3de1cce0953
Curtis Lansdell <lansdell>
2000-08-09 18:57:44 +0000

    improvements in TPC gains code reduces CPU time per event by factor of 2

--- StRoot/St_QA_Maker/TpcHitUtilities.cxx
+++ StRoot/St_QA_Maker/TpcHitUtilities.cxx
@@ -1,8 +1,22 @@
-// TpcHitUtilities.cxx
-// M.L. Miller
-// Yale Software
-// 7/00
+///////////////////////////////////////////////////////////////////////////
+// $Id: TpcHitUtilities.cxx,v 1.2 2000/08/09 18:57:44 lansdell Exp $
+//
+// Author: M.L. Miller, Yale
+//
+///////////////////////////////////////////////////////////////////////////
+//
+// Description: TPC sector gains hit utilities class
+//
+///////////////////////////////////////////////////////////////////////////
+//
+// $Log: TpcHitUtilities.cxx,v $
+// Revision 1.2  2000/08/09 18:57:44  lansdell
+// improvements in TPC gains code reduces CPU time per event by factor of 2
+//
+//
+///////////////////////////////////////////////////////////////////////////
 
+#include "TpcMapUtilities.h"
 #include "TpcHitUtilities.h"
 #include "StEventTypes.h" // StEvent
 #include "StThreeVectorD.hh"
@@ -10,14 +24,24 @@
 // StDb
 #include "StDbUtilities/StTpcCoordinateTransform.hh"
 #include "StDbUtilities/StTpcLocalSectorCoordinate.hh"
+#include "StDbUtilities/StTpcPadCoordinate.hh"
 #include "StDbUtilities/StGlobalCoordinate.hh"
 #include "StTpcDb/StTpcDb.h"
 #include "StTpcDb/StTpcPadPlaneI.h"
 
-TpcHitUtilities::TpcHitUtilities() {};
+using std::sort;
+using std::pair;
+
+TpcHitUtilities::TpcHitUtilities()
+{
+    buildMaps();
+    clear();    
+}
 
 TpcHitUtilities::TpcHitUtilities(StTrack* tck, double magneticField)
 {
+    buildMaps();
+    clear();
     m_StTrack = tck;
     m_BField = magneticField;
 }
@@ -85,73 +109,76 @@ const StThreeVectorD TpcHitUtilities::sectorNormal(int sector)
 double TpcHitUtilities::dx(StTpcHit* tpcHit)
 {
     double ds=0.;
-    StTpcCoordinateTransform transformer(gStTpcDb);
-
-    double padlength;
-    if (tpcHit->padrow()<14) {
-	padlength = gStTpcDb->PadPlaneGeometry()->innerSectorPadLength();}
-    else {
-	padlength = gStTpcDb->PadPlaneGeometry()->outerSectorPadLength();}
-
-    //Get the position of the hit, transform to local sector coordinates
-    const StGlobalCoordinate& gMidCoord(tpcHit->position());
-    StTpcLocalSectorCoordinate lsMidCoord;
-    transformer(gMidCoord, lsMidCoord);
-
-    //Boost the local y value by +- padlength / 2.
-    const StThreeVector<double>& lsPos = lsMidCoord.position();
-    StTpcLocalSectorCoordinate lsTopCoord(lsPos.x(),
-					  lsPos.y()+padlength/2.,
-					  lsPos.z(),
-					  tpcHit->sector());
-    StTpcLocalSectorCoordinate lsBotCoord(lsPos.x(),
-					  lsPos.y()-padlength/2.,
-					  lsPos.z(),
-					  tpcHit->sector());
-    //Transform back to global coordinates
-    StGlobalCoordinate gBotCoord, gTopCoord;
-    transformer(lsTopCoord, gTopCoord);
-    transformer(lsBotCoord, gBotCoord);
-    const StThreeVector<double>& gTopPos = gTopCoord.position();
-    const StThreeVector<double>& gMidPos = gMidCoord.position();
-    const StThreeVector<double>& gBotPos = gBotCoord.position();
-    const StThreeVectorD gTopPosD(gTopPos.x(),gTopPos.y(),gTopPos.z());
-    const StThreeVectorD gMidPosD(gMidPos.x(),gMidPos.y(),gMidPos.z());
-    const StThreeVectorD gBotPosD(gBotPos.x(),gBotPos.y(),gBotPos.z());
-
-    //Now Calculate pathlength difference between inner and outer points
-    const StThreeVectorD normal = sectorNormal(tpcHit->sector());
-    double s_out = m_StTrack->geometry()->helix().pathLength(gTopPosD, normal);
-    double s_in  = m_StTrack->geometry()->helix().pathLength(gBotPosD, normal);
-    ds = s_out - s_in;
+    HitMapKey mykey;    //Build a key to the map (sector, padrow)
+    mykey.sector = tpcHit->sector();
+    mykey.padrow = tpcHit->padrow();
+    PadrowLocation padLoc = m_PadrowMap[mykey];
+    const StThreeVectorD normal = m_SectorNormalMap[tpcHit->sector()];
+    double s_out = m_StTrack->geometry()->helix().pathLength(padLoc.outsidePoint(), normal);
+    double s_in  = m_StTrack->geometry()->helix().pathLength(padLoc.insidePoint(), normal);
+    ds = s_out-s_in;
     if (ds < 0.) {ds = -1.*ds;}
-
     if (s_out==DBL_MAX || s_in==DBL_MAX) {ds = 0.;}
     return ds;
 }
 
-//Calculate Crossing Angle in degrees
-double TpcHitUtilities::crossingAngle(StTpcHit* hit)
+void TpcHitUtilities::buildMaps()
 {
-    const StThreeVectorD normal = sectorNormal(hit->sector());
-    //Get the pathlength at the middle of the pad, a big mess because of no template support
-    const StGlobalCoordinate& gMidCoord(hit->position());
-    const StThreeVector<double>& gMidPos = gMidCoord.position();
-    const StThreeVectorD gMidPosD(gMidPos.x(),gMidPos.y(),gMidPos.z());
-    const double pathlength = m_StTrack->geometry()->helix().pathLength(gMidPosD);
-    const StThreeVectorD momentum = m_StTrack->geometry()->helix().momentumAt(pathlength, m_BField);
-    const StThreeVectorD xymomentum(momentum.x(), momentum.y(), 0.);
-    const double crossingAngle = normal.angle(xymomentum);
-
-    return 180./3.14159*crossingAngle;
-}
-
-//Utilities----------------------------------
-//Print tpcHitVec to screen (debugging)
-void TpcHitUtilities::printTpcHitVec()
-{
-    for (vector<StTpcHit*>::iterator it1 = m_tpcHitVec.begin(); it1 != m_tpcHitVec.end(); it1++) {
-	cout <<(*it1)->sector()<<"\t"<<(*it1)->padrow()<<"\t"<<(*it1)->charge()<<"\t"<<endl;
+    StTpcCoordinateTransform transformer(gStTpcDb);
+    //Build map with an outward pointing normal keyed by sector number
+    for (int sector=1; sector<=24; sector++) {
+	m_SectorNormalMap[sector] = sectorNormal(sector);
     }
+
+    //Build map with a point on outside, center, and inside of padrow (this plus normal define a plane)
+    //StTpcCoordinateTransform transformer(gStTpcDb);
+    for (int sector=1; sector<=24; sector++) {
+	
+	for (int padrow=1; padrow<=45; padrow++) {
+	    double padlength;
+	    if (padrow<14) {
+		padlength = gStTpcDb->PadPlaneGeometry()->innerSectorPadLength();}
+	    else {
+		padlength = gStTpcDb->PadPlaneGeometry()->outerSectorPadLength();}
+	    
+	    //Get the position of the padrow center, transform to local sector coordinates
+	    StTpcPadCoordinate padCoord(sector, padrow, 1, 1);
+	    StTpcLocalSectorCoordinate lsMidCoord;
+	    transformer(padCoord, lsMidCoord);
+
+	    //Boost the local y value by +- padlength / 2.
+	    const StThreeVector<double>& lsPos = lsMidCoord.position();
+	    StTpcLocalSectorCoordinate lsTopCoord(lsPos.x(),
+						  lsPos.y()+padlength/2.,
+						  lsPos.z(),
+						  sector);
+	    StTpcLocalSectorCoordinate lsBotCoord(lsPos.x(),
+						  lsPos.y()-padlength/2.,
+						  lsPos.z(),
+						  sector);
+	    //Transform back to global coordinates
+	    StGlobalCoordinate gBotCoord, gMidCoord, gTopCoord;
+	    transformer(lsTopCoord, gTopCoord);
+	    transformer(lsBotCoord, gBotCoord);
+	    transformer(lsMidCoord, gMidCoord);
+	    const StThreeVector<double>& gTopPos = gTopCoord.position();
+	    const StThreeVector<double>& gMidPos = gMidCoord.position();
+	    const StThreeVector<double>& gBotPos = gBotCoord.position();
+	    const StThreeVectorD gTopPosD(gTopPos.x(),gTopPos.y(),gTopPos.z());
+	    const StThreeVectorD gMidPosD(gMidPos.x(),gMidPos.y(),gMidPos.z());
+	    const StThreeVectorD gBotPosD(gBotPos.x(),gBotPos.y(),gBotPos.z());
+
+	    //Store in this form
+	    PadrowLocation padLocation(gTopPosD, gMidPosD, gBotPosD);
+	    HitMapKey myKey;
+	    myKey.sector = sector;
+	    myKey.padrow = padrow;
+	    m_PadrowMap.insert(padrowMapValType(myKey,padLocation));
+	    //cout <<sector<<"\t"<<padrow<<"\t"<<endl;
+	    //padLocation.print();
+	}
+    }
+
+    cout<<"Done Building Maps"<<endl;
     return;
 }
