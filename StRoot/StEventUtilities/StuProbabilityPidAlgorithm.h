dc85df21461da829073badcd14bc98248720c55c
Aihong Tang <aihong@bnl.gov>
2000-08-15 23:04:18 +0000

    speed it up by looking up table

--- StRoot/StEventUtilities/StuProbabilityPidAlgorithm.h
+++ StRoot/StEventUtilities/StuProbabilityPidAlgorithm.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StuProbabilityPidAlgorithm.h,v 1.4 2000/07/22 22:45:27 aihong Exp $
+ * $Id: StuProbabilityPidAlgorithm.h,v 1.5 2000/08/15 23:04:18 aihong Exp $
  *
  * Author:Aihong Tang, Richard Witt(FORTRAN version). Kent State University
  *        Send questions to aihong@cnr.physics.kent.edu 
@@ -11,6 +11,9 @@
  ***************************************************************************
  *
  * $Log: StuProbabilityPidAlgorithm.h,v $
+ * Revision 1.5  2000/08/15 23:04:18  aihong
+ * speed it up by looking up table
+ *
  * Revision 1.4  2000/07/22 22:45:27  aihong
  * change include path
  *
@@ -21,20 +24,31 @@
  * add head for Log
  *
  **************************************************************************/
-
-
 #ifndef StuProbabilityPidAlgorithm_hh
 #define StuProbabilityPidAlgorithm_hh
 
+
 #include "TString.h"
 #include "TObjArray.h"
+#include "TMap.h"
+//#include "TGraph.h"
+#include "TVectorD.h"
+
+
 //#include "tables/St_tpcDedxPidAmpDb_Table.h"
 #include "StEventTypes.h"
 #include "StParticleTable.hh"
 #include "StParticleTypes.hh"
 #include "StEventUtilities/StPidAmpNetOut.h"
+#include "StEventUtilities/StuObjPidReport.h"
 
 class StEvent;
+class StPidAmpNetOut;
+class StuObjPidReport;
+class TString;
+class TObject;
+class TObjArray;
+
 
 
 class StuProbabilityPidAlgorithm : public StPidAlgorithm {
@@ -45,8 +59,6 @@ class StuProbabilityPidAlgorithm : public StPidAlgorithm {
       virtual  ~StuProbabilityPidAlgorithm();
 
 
-      
-      void setDedxMethod(StDedxMethod method);//use what dedx to perform the job.
 
       StParticleDefinition* mostLikelihoodParticle();
       StParticleDefinition* secondLikelihoodParticle();     
@@ -58,55 +70,116 @@ class StuProbabilityPidAlgorithm : public StPidAlgorithm {
       double mostLikelihoodProbability();
       double secondLikelihoodProbability();
       double thirdLikelihoodProbability();
-      double lowRigReso(double xa, double xb, double ya, double yb,double theX);
+  
       bool isExtrap();
 
       StParticleDefinition*
       operator() (const StTrack&, const StSPtrVecTrackPidTraits&);
 
-
+      
+      static void setDedxMethod(StDedxMethod method);
+             //use what dedx to perform the job.
+      static void setDynamicallyCalculatePID();//set a tag.
+    
       static void readParametersFromFile(TString fileName);
       //      static void readParametersFromTable(St_Table* tb);
+                  //from data base.
       static void printParameters();
-      //      static void refreshParameters(St_Table* theTable);
-      static void readAType(StParticleDefinition* def, Float_t* mean, Float_t* amp,Float_t* sig,Float_t cal,TObjArray*  theChannel);
+
+
+     static  void   fillReportObjArray(TString fileName);
+
+     /*
+     static  void   debug(double theDedx, double theRig, double theNHits, double thePt, int theCharge, double theDca);
+     static  void   debug2(double theDedx, double theRig, int theNHits, double thePt, int theCharge, double theDca);
+     */
+
+
 
  private:
-
-      double ( *funcBandPt) (double *, double *);
-      double ( *funcAmpPt ) (double *, double *);
-      double ( *funcResoPt) (double *, double *);
-
      
-      double bandCenter(double rig, TArrayD* bandPars);
-      double resolution(double rig, TArrayD* linrPars,TArrayD* bandPars);
-      double peak(double rig, TArrayD* ampPars);
-      double tossTail(double rig);
-      void   lowRigPID(double rig,double dedx);
+     void    fillPIDByLookUpTable(int myCharge, int myDca, int myNhits, double myPt, double myDedx, double myRig);
+     void    fillPIDByCalculation(int myCharge, int myDca, int myNhits, double myPt, double myDedx, double myRig);
+
+
+     static  double bandCenter(double rig, TArrayD* bandPars);
+     static  double bandCenter(double rig, int NChannel, int NType);
+
+     static  double resolution(double rig, TArrayD* linrPars,TArrayD* bandPars);
+     static  double resolution(double rig, int NChannel, int NType);
+
+     static  double peak(double rig, TArrayD* ampPars);
+     static  double peak(double rig, int NChannel, int NType);
+
+     static  double tossTail(double rig);
+
+
+     static  double amplitude(double dedx, double rig, TArrayD* bandPars, TArrayD* linrPars, TArrayD* ampPars);
+     static  double amplitude(double dedx, double rig, int NChannel, int NType);
+
+      static void readAType(StParticleDefinition* def, Float_t* mean, Float_t* amp,Float_t* sig,Float_t cal,TObjArray*  theChannel);
+      //      static void refreshParameters(St_Table* theTable);
+
+     static  bool   tagExtrap(double rig, double dedx,TObjArray* channelLevel);
+
+     static  void   fill(double* probAry, int* pidAry, double prob, StPidAmpNetOut* netOut); 
+     static  double lowRigReso(double xa, double xb, double ya, double yb,double theX);
+     static  void   setBins4Table();
+     static  void   setRanges4Table(TVectorD* theSetting);
+
+
       void   fill(double prob, StPidAmpNetOut* netOut);
       void   fillAsUnknown();
-      void   tagExtrap(double rig, double dedx,TObjArray* channelLevel);
+      void   lowRigPID(double rig,double dedx);
 
-      double amplitude(double dedx, double rig, TArrayD* bandPars, TArrayD* linrPars, TArrayD* ampPars);
 
-      void lowRigPID();
-
-      StDedxMethod mDedxMethod;
       StParticleTable* table;
 
       int      PID[3];
       double   mProb[3];
-      double   mNoise;
+
       bool     mExtrap;
-      bool     mTurnOnNoise;
+
       StEvent* mEvent; //!
+
+     static    double   mNoise;
+     static    double   mDedxStart;
+     static    double   mDedxEnd;
+     static    double   mRigStart;
+     static    double   mRigEnd;
+     static    double   mNHitsStart;
+     static    double   mNHitsEnd;
+     static    double   mPtStart;
+     static    double   mPtEnd;
+     static    double   mDcaCutPoint; //just 2 bins for dca
+
+    static  bool     mTurnOnNoise;
+    static  bool     mDynamicallyCalculatePID;
+    static  double   mDedxBinWidth;
+    static  double   mRigBinWidth; //do not let NBins exceed 999.
+    static  double   mPtBinWidth; //not implemented yet.
+    static  double   mNHitsBinWidth;
       
+    static  int      mNDedxBins;
+    static  int      mNRigBins;
+    static  int      mNNhitsBins;
+    static  int      mNPtBins;
+    static  int      mNDcaBins;
+    static  int      mNChargeBins;
+
+  static  StDedxMethod mDedxMethod;
+  static  TObjArray    mDataSet;
+  //array of Channel datas. be initialize by readParametersFromFile
+  static  TObjArray    mMaxllFuncSet; 
+  static  TObjArray    mLinearFuncSet;
+  static  TObjArray    mBetheBlochFuncSet; 
 
 
-  static TObjArray mDataSet;//array of Channel datas. be initialize by readParametersFromFile
-  //  static St_Table* mDataTable;
+ //parallel sturcture with mDataSet, 
+  //but there functions taken parameters from mFuncSet.
 
-
+  static TObjArray*    mTheReportTable;
+  static TVectorD*     mTheRangeSettingVector;
 
 };
 
