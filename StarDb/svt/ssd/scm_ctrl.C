389043b798c00f008f310347eae20f554ea26068
Boris Hippolyte <hippolyt@in2p3.fr>
2000-08-16 02:56:16 +0000

    Extend maximun size of packages from 12 to 13 (default)

--- StarDb/svt/ssd/scm_ctrl.C
+++ StarDb/svt/ssd/scm_ctrl.C
@@ -9,7 +9,7 @@ scm_ctrl_st row;
 St_scm_ctrl *tableSet = new St_scm_ctrl("scm_ctrl",1);
 //
 memset(&row,0,tableSet->GetRowSize());
-    row.clusterTreat	 =         12; // max number of cluster in a matched ;
+    row.clusterTreat	 =         13; // max number of cluster in a matched ;
     row.AdcTolerance	 =        0.2; // set to 20% ;
     row.matchMean	 =          0; // matching mean ;
     row.matchSigma	 =          8; // matching sigma ;
