168d2f995c56474c21644e214f5181b1c5bc9b95
Helen L. Caines <helen.caines@yale.edu>
2000-08-21 13:04:29 +0000

    First version of hit fitting routines

---
+++ StRoot/StSvtClusterMaker/StSvtAnalysedHybridClusters.cc
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *
+ * $Id: StSvtAnalysedHybridClusters.cc,v 1.1 2000/08/21 13:04:29 caines Exp $
+ *
+ * Author: Selemon Bekele
+ ***************************************************************************
+ *
+ * Description: SVT Analysis Object BASE class
+ *
+ ***************************************************************************
+ *
+ * $Log: StSvtAnalysedHybridClusters.cc,v $
+ * Revision 1.1  2000/08/21 13:04:29  caines
+ * First version of hit fitting routines
+ *
+ * Revision
+ *
+ **************************************************************************/
+#include <iostream.h>
+#include <math.h>
+
+#include "StSvtAnalysis.hh"
+#include "StSvtAnalysedHybridClusters.hh"
+
+
+StSvtAnalysedHybridClusters::StSvtAnalysedHybridClusters(int barrel, int ladder, int wafer, int hybrid):StSvtHybridObject(barrel,ladder,wafer,hybrid), mGlobalPos() 
+{ 
+ mNumOfHits = 0;
+ mSvtHitData = NULL;
+ mSvtHit = NULL;
+ mPos = NULL;
+
+}
+
+StSvtAnalysedHybridClusters::~StSvtAnalysedHybridClusters()
+{ 
+ delete [] mSvtHitData;
+ delete [] mSvtHit;
+ delete [] mPos;
+}
+
+void StSvtAnalysedHybridClusters::setMembers(int numOfClu, int wafer, int ladder)
+{
+
+ mNumOfHits = numOfClu;
+ mHardWarePosition = getLayerID()*1000 + 100*wafer + ladder;
+ mSvtHitData = new StSvtHitData[mNumOfHits];
+ mSvtHit = new StSvtHit[mNumOfHits];
+ mPos = new StThreeVectorD[mNumOfHits];
+}
+
+
+int StSvtAnalysedHybridClusters::setSvtHit(StSvtAnalysis* mSvtAnalysis)
+{
+
+ for(int hit = 0; hit < mNumOfHits; hit++)
+  {
+    mSvtHitData[hit].id = mSvtAnalysis->GetCluID(hit);
+    mSvtHitData[hit].id_cluster = mSvtAnalysis->GetCluDeconvID(hit);
+    mSvtHitData[hit].id_globtrk = 0;
+    mSvtHitData[hit].id_track = 0;
+    mSvtHitData[hit].id_match = 0;
+
+    
+    mSvtHit[hit].setFlag((unsigned char)(mSvtAnalysis->GetCluFlag(hit)));
+    mSvtHit[hit].setHardwarePosition((long)mHardWarePosition<<4);
+    mSvtHit[hit].setCharge(mSvtAnalysis->GetCluCharge(hit));
+    
+   
+    mPos[hit].setX((float)mSvtAnalysis->GetMeanClusterTimeBin(hit));
+    mPos[hit].setY((float)mSvtAnalysis->GetMeanClusterAnode(hit));
+    mPos[hit].setZ(0.0);
+    
+    mGlobalPos.setX(sqrt(mSvtAnalysis->GetCluXCov(hit)));
+    mGlobalPos.setY(sqrt(mSvtAnalysis->GetCluYCov(hit)));
+    mGlobalPos.setZ(0.0042);
+    
+    mSvtHit[hit].setPositionError(mGlobalPos); 
+
+    
+    mGlobalPos.setX(-9999.0);
+    mGlobalPos.setY(-9999.0);
+    mGlobalPos.setZ(-9999.0);
+    mSvtHit[hit].setPosition(mGlobalPos);  //invokes StMeasuredPoint::setPosition(StThreeVectorF&)
+
+
+    mSvtHitData[hit].peakAdc = mSvtAnalysis->GetCluPeakAdc(hit);
+    mSvtHitData[hit].numOfAnodesInClu = mSvtAnalysis->GetCluNumAnodes(hit);
+    mSvtHitData[hit].numOfPixelsInClu = mSvtAnalysis->GetCluNumPixels(hit);
+    
+    mSvtHitData[hit].mom2[0] = mSvtAnalysis->GetSecondMomClusterTimeBin(hit);
+    mSvtHitData[hit].mom2[1] = mSvtAnalysis->GetSecondMomClusterAnode(hit);
+
+   }  
+
+
+return 0;
+}
+
