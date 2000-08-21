e09ef7e7b6dc806c21834ba7c269de24bcee1dd8
Helen L. Caines <helen.caines@yale.edu>
2000-08-21 13:03:40 +0000

    First version of cluster->STAR hit maker

---
+++ StRoot/StSvtClusterMaker/StSvtHitMaker.h
@@ -0,0 +1,77 @@
+// $Id: StSvtHitMaker.h,v 1.1 2000/08/21 13:03:40 caines Exp $
+// $Log: StSvtHitMaker.h,v $
+// Revision 1.1  2000/08/21 13:03:40  caines
+// First version of cluster->STAR hit maker
+//
+//
+#ifndef STAR_StSvtHit
+#define STAR_StSvtHit
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// StSvtHit base class                                                  //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+#ifndef StMaker_H
+#include "StMaker.h"
+#endif
+
+#include "tables/St_scs_spt_Table.h"
+#include "tables/St_srs_srspar_Table.h"
+#include "tables/St_svg_config_Table.h"
+#include "tables/St_svg_shape_Table.h"
+#include "tables/St_svg_geom_Table.h"
+
+
+#include "TH2.h"
+
+class TFIle;
+class TNtuple;
+
+class St_scs_spt;
+class St_svg_config;
+class St_svg_shape;
+class St_svg_geom;
+class St_srs_srspar;
+class StSvtHybridCollection;
+class StSvtAnalysedHybridClusters;
+ 
+class StSvtHitMaker : public StMaker
+{
+ public: 
+  StSvtHitMaker(const char *name = "SvtHit");
+  StSvtHitMaker(StSvtHitMaker& svthit);
+  ~StSvtHitMaker();
+
+  virtual Int_t Init();
+  virtual Int_t Make();
+  virtual Int_t Finish();
+  Int_t FillHistograms();
+  void TransformIntoSpacePoint();
+  void SaveIntoTable(int numOfCluster, int index);
+  void SaveIntoNtuple(int numOfCluster, int index);
+  void SetWriteNtuple(int iwrite=0){iWrite = iwrite;};
+ protected:
+
+  int iMyEvt;
+  int iWrite;
+
+  St_svg_config   *m_config;//!
+  St_svg_shape    *m_shape; //!
+  St_svg_geom     *m_geom;  //!
+  St_srs_srspar   *m_srs_srspar; //!
+
+  StSvtHybridCollection *mSvtCluColl; //!
+  StSvtAnalysedHybridClusters  *mSvtBigHit;  //!
+  TH2F     *m_x_vs_y;  //! x vs y of Si points
+  TH2F     **m_waf_no;  //! ladder no vs z of Si hit
+
+  TNtuple *m_ClusTuple;                       //!
+  TFile   *m_hfile;                           //!
+
+
+  ClassDef(StSvtHitMaker,1)   //virtual base class for Makers
+
+};
+
+
+#endif
