943022b717640bf89684774a9d3d1aec8f9f054a
Frank Laue <laue@bnl.gov>
2000-08-22 14:58:34 +0000

    anti dedx cuts added : SetNSigmaAntiElectron()                        SetNSigmaAntiPion()                        SetNSigmaAntiKaon()                        SetNSigmaAntiProton()

--- StRoot/StHbtMaker/Cut/franksTrackCut.cxx
+++ StRoot/StHbtMaker/Cut/franksTrackCut.cxx
@@ -22,8 +22,18 @@
 
 franksTrackCut::franksTrackCut(){
   mNTracksPassed = mNTracksFailed = 0;
-  mNSigmaElectron[0] = -1e9;
-  mNSigmaElectron[1] = +1e9;
+  mNSigmaElectron[0] = -1e9;  mNSigmaElectron[1] = +1e9;
+  mNSigmaPion[0]     = -1e9;  mNSigmaPion[1]     = +1e9;
+  mNSigmaKaon[0]     = -1e9;  mNSigmaKaon[1]     = +1e9;
+  mNSigmaProton[0]   = -1e9;  mNSigmaProton[1]   = +1e9;
+  mNSigmaAntiElectron[0] = 0.;  mNSigmaAntiElectron[1] = 0.;
+  mNSigmaAntiPion[0]     = 0.;  mNSigmaAntiPion[1]     = 0.;
+  mNSigmaAntiKaon[0]     = 0.;  mNSigmaAntiKaon[1]     = 0.;
+  mNSigmaAntiProton[0]   = 0.;  mNSigmaAntiProton[1]   = 0.;
+  mPt[0] = -1e9;  mPt[1] = +1e9;
+  mPx[0] = -1e9;  mPx[1] = +1e9;
+  mPy[0] = -1e9;  mPy[1] = +1e9;
+  mPz[0] = -1e9;  mPz[1] = +1e9;
 }
 //------------------------------
 franksTrackCut::franksTrackCut(franksTrackCut& c) : StHbtTrackCut(c) {
@@ -37,12 +47,21 @@ franksTrackCut::franksTrackCut(franksTrackCut& c) : StHbtTrackCut(c) {
   mNSigmaKaon[1] = c.mNSigmaKaon[1];
   mNSigmaProton[0] = c.mNSigmaProton[0];
   mNSigmaProton[1] = c.mNSigmaProton[1];
+  mNSigmaAntiElectron[0] = c.mNSigmaAntiElectron[0];
+  mNSigmaAntiElectron[1] = c.mNSigmaAntiElectron[1];
+  mNSigmaAntiPion[0] = c.mNSigmaAntiPion[0];
+  mNSigmaAntiPion[1] = c.mNSigmaAntiPion[1];
+  mNSigmaAntiKaon[0] = c.mNSigmaAntiKaon[0];
+  mNSigmaAntiKaon[1] = c.mNSigmaAntiKaon[1];
+  mNSigmaAntiProton[0] = c.mNSigmaAntiProton[0];
+  mNSigmaAntiProton[1] = c.mNSigmaAntiProton[1];
   mNHits[0] = c.mNHits[0];
   mNHits[1] = c.mNHits[1];
-  mP[0] = c.mP[0];
-  mP[1] = c.mP[1];
-  mPt[0] = c.mPt[0];
-  mPt[1] = c.mPt[1];
+  mP[0] = c.mP[0]; mP[1] = c.mP[1];
+  mPt[0] = c.mPt[0]; mPt[1] = c.mPt[1];
+  mPx[0] = c.mPx[0]; mPx[1] = c.mPx[1];
+  mPy[0] = c.mPy[0]; mPy[1] = c.mPy[1];
+  mPz[0] = c.mPz[0]; mPz[1] = c.mPz[1];
   mRapidity[0] = c.mRapidity[0];
   mRapidity[1] = c.mRapidity[1];
   mDCA[0] = c.mDCA[0];
@@ -60,7 +79,8 @@ franksTrackCut::~franksTrackCut(){
 
 //------------------------------
 bool franksTrackCut::Pass(const StHbtTrack* track){
-  // cout << " *** franksTrackCut::Pass(const StHbtTrack* track) " << endl;
+  //   cout << " *** franksTrackCut::Pass(const StHbtTrack* track) " << endl;
+
   float TEnergy = sqrt(track->P().mag2()+mMass*mMass);
   float TRapidity = 0.5*log((TEnergy+track->P().z())/
 			    (TEnergy-track->P().z()));
@@ -76,36 +96,54 @@ bool franksTrackCut::Pass(const StHbtTrack* track){
     track->P().mag() << " " << 
     track->Pt() << " " <<  
     TRapidity << " " << 
-    track->Charge() << " "; 
+    track->Charge() << " ";
 #endif
 
    bool goodPID = (
+		   (track->NSigmaElectron() >= mNSigmaElectron[0]) &&
+		   (track->NSigmaElectron() <= mNSigmaElectron[1]) &&
 		   (track->NSigmaPion()  >= mNSigmaPion[0]) &&
 		   (track->NSigmaPion()   <= mNSigmaPion[1]) &&
 		   (track->NSigmaKaon()   >= mNSigmaKaon[0]) &&
 		   (track->NSigmaKaon()   <= mNSigmaKaon[1]) &&
 		   (track->NSigmaProton() >= mNSigmaProton[0]) &&
 		   (track->NSigmaProton() <= mNSigmaProton[1]) &&
-		   (track->NSigmaElectron() >= mNSigmaElectron[0]) &&
-		   (track->NSigmaElectron() <= mNSigmaElectron[1]) &&
+		   !( (track->NSigmaElectron() >= mNSigmaAntiElectron[0]) &&
+		      (track->NSigmaElectron() <= mNSigmaAntiElectron[1]) ) &&
+		   !( (track->NSigmaPion()  >= mNSigmaAntiPion[0]) &&
+		      (track->NSigmaPion()   <= mNSigmaAntiPion[1]) ) &&
+		   !( (track->NSigmaKaon()   >= mNSigmaAntiKaon[0]) &&
+		      (track->NSigmaKaon()   <= mNSigmaAntiKaon[1]) ) &&
+		   !( (track->NSigmaProton() >= mNSigmaAntiProton[0]) &&
+		      (track->NSigmaProton() <= mNSigmaAntiProton[1]) ) &&
 		   (track->Charge() == mCharge || mCharge==0 )
 		   );
+
+
+
 #ifdef STHBTDEBUG
    cout  << mNSigmaElectron[0] << " << " << track->NSigmaElectron() << " << " << mNSigmaElectron[1] << " ";
    cout << (track->NSigmaElectron() >= mNSigmaElectron[0]) << (track->NSigmaElectron() <= mNSigmaElectron[1]) << endl;
 #endif
 
-  bool goodTrack=
-    ((track->DCAxy()  >= mDCA[0]) &&
+   bool goodTrack=( true &&
+     (track->DCAxy()  >= mDCA[0]) &&
      (track->DCAxy()  <= mDCA[1]) &&
      (track->NHits() >= mNHits[0]) &&
      (track->NHits() <= mNHits[1]) &&
-     (track->P().mag() >= mP[0]) &&
-     (track->P().mag() <= mP[1]) &&
-     (track->Pt()    >= mPt[0]) &&
-     (track->Pt()    <= mPt[1]) &&
-     (TRapidity      >= mRapidity[0]) &&
-     (TRapidity      <= mRapidity[1]));
+      (track->P().mag() >= mP[0]) &&
+      (track->P().mag() <= mP[1]) &&
+      (track->Pt()    >= mPt[0]) &&
+      (track->Pt()    <= mPt[1]) &&
+      (track->P().x() >= mPx[0]) &&
+      (track->P().x() <= mPx[1]) &&
+      (track->P().y() >= mPy[0]) &&
+      (track->P().y() <= mPy[1]) &&
+      (track->P().z() >= mPz[0]) &&
+      (track->P().z() <= mPz[1]) &&
+      (TRapidity      >= mRapidity[0]) &&
+      (TRapidity      <= mRapidity[1])
+);
   
     
 #ifdef STHBTDEBUG
