2d8b3aa4e24d735e7bcaaaecc3adf26d26c0478d
Yuri Fisyak <fisyak@bnl.gov>
2000-08-27 17:46:08 +0000

    Call Finish for a Chain

--- StRoot/macros/rootlogoff.C
+++ StRoot/macros/rootlogoff.C
@@ -1,5 +1,8 @@
-// $Id: rootlogoff.C,v 1.2 1999/11/02 22:55:43 kathy Exp $
+// $Id: rootlogoff.C,v 1.3 2000/08/27 17:46:08 fisyak Exp $
 // $Log: rootlogoff.C,v $
+// Revision 1.3  2000/08/27 17:46:08  fisyak
+// Call Finish for a Chain
+//
 // Revision 1.2  1999/11/02 22:55:43  kathy
 // fixing documentation in macro
 //
@@ -8,7 +11,7 @@
 // description:
 ///////////////////////////////////////////////////////////////
 {
-if (gClassTable->GetID("StBFChain") >=0) {
+if (gClassTable->GetID("StChain") >=0) {
   delete StMaker::GetChain();
 }
 printf("\nThis is the end of ROOT -- Goodbye\n\n");
