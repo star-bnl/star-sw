632f961d5d98bb279ae1f7a2e5c4d83074aa2ac6
genevb <>
2000-08-31 21:47:08 +0000

    Allow V0s to be trimmed after finding Xis

--- StRoot/St_dst_Maker/StV0Maker.h
+++ StRoot/St_dst_Maker/StV0Maker.h
@@ -5,8 +5,11 @@
 //                                                                      //
 // StV0Maker virtual base class for Maker                               //
 //                                                                      //
-// $Id: StV0Maker.h,v 1.5 1999/07/15 13:57:54 perev Exp $
+// $Id: StV0Maker.h,v 1.6 2000/08/31 21:47:08 genevb Exp $
 // $Log: StV0Maker.h,v $
+// Revision 1.6  2000/08/31 21:47:08  genevb
+// Allow V0s to be trimmed after finding Xis
+//
 // Revision 1.5  1999/07/15 13:57:54  perev
 // cleanup
 //
@@ -23,14 +26,17 @@
 
 class St_ev0_ev0par;
 class St_ev0_ev0par2;
+class St_dst_v0_vertex;
 
 class StV0Maker : public StMaker {
 
  private:
   Bool_t m_ev0EvalOn;   //switch for the evaluation
-  // static Char_t m_VersionCVS = "$Id: StV0Maker.h,v 1.5 1999/07/15 13:57:54 perev Exp $";
-  St_ev0_ev0par  *m_ev0par;      //!
-  St_ev0_ev0par2 *m_ev0par2;     //!
+  // static Char_t m_VersionCVS = "$Id: StV0Maker.h,v 1.6 2000/08/31 21:47:08 genevb Exp $";
+  St_ev0_ev0par  *m_ev0par;        //!
+  St_ev0_ev0par2 *m_ev0par2;       //!
+  St_ev0_ev0par2 *m_ev0parT;       //!
+  St_dst_v0_vertex *dst_v0_vertex; //!
 
  protected:
 
@@ -41,10 +47,11 @@ class StV0Maker : public StMaker {
   virtual Int_t Init();
   virtual Int_t  Make();
   virtual void   ev0Eval(Bool_t flag=kFALSE){m_ev0EvalOn=flag;} // *MENU*
-  virtual void   ev0EvalOn() {ev0Eval(kTRUE);} 
-  virtual void   ev0EvalOff(){ev0Eval();}      
+  virtual void   ev0EvalOn() {ev0Eval(kTRUE);}
+  virtual void   ev0EvalOff(){ev0Eval();}
+  virtual void   Trim();
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StV0Maker.h,v 1.5 1999/07/15 13:57:54 perev Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StV0Maker.h,v 1.6 2000/08/31 21:47:08 genevb Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
   ClassDef(StV0Maker, 1)   //StAF chain virtual base class for Makers
 };
