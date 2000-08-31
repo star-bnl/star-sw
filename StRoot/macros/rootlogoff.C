cbad33e57392341b62f957916a2da72c82588a9c
fisyak <>
2000-08-31 13:15:45 +0000

    Force call to Finish before quit (Victor)

--- StRoot/macros/rootlogoff.C
+++ StRoot/macros/rootlogoff.C
@@ -1,5 +1,8 @@
-// $Id: rootlogoff.C,v 1.3 2000/08/27 17:46:08 fisyak Exp $
+// $Id: rootlogoff.C,v 1.4 2000/08/31 13:15:45 fisyak Exp $
 // $Log: rootlogoff.C,v $
+// Revision 1.4  2000/08/31 13:15:45  fisyak
+// Force call to Finish before quit (Victor)
+//
 // Revision 1.3  2000/08/27 17:46:08  fisyak
 // Call Finish for a Chain
 //
@@ -12,7 +15,8 @@
 ///////////////////////////////////////////////////////////////
 {
 if (gClassTable->GetID("StChain") >=0) {
-  delete StMaker::GetChain();
+  StMaker *mk = StMaker::GetChain();
+  if (mk) {mk->Finish(); delete mk;}
 }
 printf("\nThis is the end of ROOT -- Goodbye\n\n");
 }
