de7f76b878ca089a7c723d46a7ab8f22411e8ede
Valeri Fine <val.fine@gmail.com>
2000-09-25 01:34:11 +0000

    a macro to set the custom filter and to change the default list of the tables to draw

---
+++ StRoot/macros/graphics/SetFtpcFilter.C
@@ -0,0 +1,21 @@
+{
+  // Get a pointer to "displayMAker from chain
+  StEventDisplayMaker *ds = (StEventDisplayMaker *)chain->Maker("EventDisplay");
+
+  // Print the list of the defaults tables to be drawn
+  ds->PrintNames();
+
+  // Create the custom filter and attach it to "EventDisplay"
+  StFtpcTrackFilter *f = new StFtpcTrackFilter();
+  ds->SetFilter(f,StEventDisplayMaker::kTptTrack);
+  ds->SetFilter(f,StEventDisplayMaker::kTable);
+
+  // Edit the list of tables to be drawn
+  ds->RemoveName("dst/point(id_track,position[0]:position[1]:charge)");
+  ds->RemoveName("dst/primtrk");
+  ds->AddName("dst/globtrk");
+  ds->PrintNames();
+
+  // Redraw the last event
+  ds->ReDraw();
+}
