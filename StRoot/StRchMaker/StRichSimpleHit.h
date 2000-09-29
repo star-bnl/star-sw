d835dd5d094a602cc9e2252762cf42045758b7a5
lasiuk <>
2000-09-29 19:01:26 +0000

    enumerated types added for flags c'tor includes cp from persistent hit number of pads added as a member

--- StRoot/StRchMaker/StRichSimpleHit.h
+++ StRoot/StRchMaker/StRichSimpleHit.h
@@ -1,5 +1,5 @@
 /***************************************************************
- * $Id: StRichSimpleHit.h,v 2.0 2000/08/09 16:22:12 gans Exp $
+ * $Id: StRichSimpleHit.h,v 2.1 2000/09/29 19:01:26 lasiuk Exp $
  *
  * Description:
  *   Definition of the Hit object as reconstructed by
@@ -7,8 +7,10 @@
  *
  ***************************************************************
  * $Log: StRichSimpleHit.h,v $
- * Revision 2.0  2000/08/09 16:22:12  gans
- * Cosmetic Changes. Naming convention for TDrawable objects
+ * Revision 2.1  2000/09/29 19:01:26  lasiuk
+ * enumerated types added for flags
+ * c'tor includes cp from persistent hit
+ * number of pads added as a member
  *
  * Revision 2.1  2000/09/29 19:01:26  lasiuk
  * enumerated types added for flags
@@ -45,6 +47,8 @@ using std::vector;
 
 #include "StThreeVector.hh"
 
+// From StEvent
+#ifdef __ROOT__
 #include "StEvent/StRichHit.h"
 #endif
 
@@ -58,10 +62,11 @@ public:
     StRichSimpleHit(const StRichHit*);
 #endif
     virtual ~StRichSimpleHit();
-    const StThreeVector<double>& global()    const;
-    const StThreeVector<double>& local()    const;
-    const StThreeVector<double>& internal() const;
+    virtual StRichSimpleHit* clone();
+    
+    //StRichSimpleHit(const StRichSimpleHit&){}
     //StRichSimpleHit& operator=(const StRichSimpleHit&){}
+    
     const StThreeVector<double>& global()     const;
     const StThreeVector<double>& local()      const;
     const StThreeVector<double>& internal()   const;
@@ -71,10 +76,22 @@ public:
     double charge()        const;
     double maxAmplitude()  const;
     int    clusterNumber() const;
+    
     StThreeVector<double>&  global();
     StThreeVector<double>&  local();
     StThreeVector<double>&  internal();
     StThreeVector<double>&  localError();
+    StThreeVector<double>&  sigma();
+    
+    void setCharge(double q);
+    void setMaxAmplitude(double m);
+    void setClusterNumber(int no);
+
+    unsigned short numberOfPads() const;
+    void setNumberOfPads(unsigned short);
+
+    //
+    // Flag Operation
     bool isSet(StRichSimpleHitFlag f)      const;
     void setBit(StRichSimpleHitFlag f);
     void unSetBit(StRichSimpleHitFlag f);
@@ -85,6 +102,8 @@ protected:
     StThreeVector<double> mGlobal;
     StThreeVector<double> mLocal;  // local coordinates
     StThreeVector<double> mLError;  // local error
+    StThreeVector<double> mInternal;  // fraction pad/row
+    StThreeVector<double> mSigma;
     double                mCharge;
     double                mMaxAmplitude;
     unsigned int          mClusterNumber;
@@ -95,13 +114,23 @@ protected:
 inline const StThreeVector<double>& StRichSimpleHit::global() const {return mGlobal;}
 inline StThreeVector<double>& StRichSimpleHit::global() {return mGlobal;}
 inline const StThreeVector<double>& StRichSimpleHit::local() const {return mLocal;}
+inline StThreeVector<double>& StRichSimpleHit::local() {return mLocal;}
+inline const StThreeVector<double>& StRichSimpleHit::internal() const {return mInternal;}
 inline StThreeVector<double>& StRichSimpleHit::internal() {return mInternal;}
 inline const StThreeVector<double>& StRichSimpleHit::localError() const {return mLError;}
 inline StThreeVector<double>& StRichSimpleHit::localError() {return mLError;}
 inline const StThreeVector<double>& StRichSimpleHit::sigma() const {return mSigma;}
 inline StThreeVector<double>& StRichSimpleHit::sigma() {return mSigma;}
 inline void StRichSimpleHit::setCharge(double q) {mCharge = q;}
+inline double StRichSimpleHit::charge() const {return mCharge;}
+inline void StRichSimpleHit::setMaxAmplitude(double m) {mMaxAmplitude = m;}
+inline double StRichSimpleHit::maxAmplitude() const {return mMaxAmplitude;}
 inline void StRichSimpleHit::setClusterNumber(int no) {mClusterNumber = no;}
+inline int StRichSimpleHit::clusterNumber() const {return mClusterNumber;}
+inline unsigned short StRichSimpleHit::numberOfPads() const {return mNumberOfPads;}
+inline void StRichSimpleHit::setNumberOfPads(unsigned short n) {mNumberOfPads=n;}
+
+inline StRichSimpleHit* StRichSimpleHit::clone() {return new StRichSimpleHit(*this);}
 // Flags
 inline void StRichSimpleHit::setBit(StRichSimpleHitFlag b) { mFlags |= b; }
 inline void StRichSimpleHit::unSetBit(StRichSimpleHitFlag b) { mFlags &= ~(b);}
