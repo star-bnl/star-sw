5bbb95ffc4ed71ab25b44e9163b85ec3b545b573
lasiuk <>
2000-11-01 16:55:28 +0000

    add hilite() members which utilize the flags defined in StEvent.  draw() member also added.  Hits are hilited only by demand.  Not by default

--- StRoot/StRichDisplayMaker/StRichDrawableTRings.cxx
+++ StRoot/StRichDisplayMaker/StRichDrawableTRings.cxx
@@ -1,12 +1,14 @@
 /**********************************************************
- * $Id: StRichDrawableTRings.cxx,v 2.1 2000/09/29 17:36:58 gans Exp $
+ * $Id: StRichDrawableTRings.cxx,v 2.2 2000/11/01 16:55:26 lasiuk Exp $
  *
  * Description:
  *  
  *
  *  $Log: StRichDrawableTRings.cxx,v $
- *  Revision 2.1  2000/09/29 17:36:58  gans
- *  Modified addHit(), StThreeVector<double> -> StThreeVectorF,other minor stuff
+ *  Revision 2.2  2000/11/01 16:55:26  lasiuk
+ *  add hilite() members which utilize the flags defined in
+ *  StEvent.  draw() member also added.  Hits are hilited only
+ *  by demand.  Not by default
  *
  *  Revision 2.1  2000/09/29 17:36:58  gans
  *  Modified addHit(), StThreeVector<double> -> StThreeVectorF,other minor stuff
@@ -28,17 +30,18 @@
  **********************************************************/
 
 #include "StRichDrawableTRings.h"
-#include "StRichPIDMaker/StRichRings.h"
+
+#include <vector>
+#include "TRandom.h"
 #include "TPolyLine.h"
-#include "StRichPIDMaker/StRichTrack.h"
 
 #include "StThreeVectorD.hh"
 #include "StThreeVectorF.hh"
-#include "TRandom.h"
+
 #include "StRichHit.h"
 
-#include <vector>
-
+#include "StRichPIDMaker/StRichRings.h"
+#include "StRichPIDMaker/StRichTrack.h"
 
 #ifndef ST_NO_NAMESPACES
 using namespace units;
@@ -51,8 +54,8 @@ StRichDrawableTRings::StRichDrawableTRings(StRichRings& ring) {
     // no need to set a new seed
     
     time_t t1 = time(0);   // to be used as a seed  
-    rand = new TRandom();
-    rand->SetSeed(t1);
+    mRand = new TRandom();
+    mRand->SetSeed(t1);
     
     mTrack = ring.getTrack();
     mParticle = ring.getParticle();
@@ -65,74 +68,93 @@ StRichDrawableTRings::StRichDrawableTRings(StRichRings& ring) {
   const Int_t maxInnerSize = 3600;    // each ring <= 3600 line segments 
   const Int_t maxOuterSize = 3600;
 
+  //
   // Copy into temp vector (should make a vector<pair<double>>)
+  //
   vector<StThreeVectorF > in  = ring.getInnerPoints(maxInnerSize);
   vector<StThreeVectorF > out = ring.getOuterPoints(maxOuterSize);
 
+  //
   // Set Up Arrays for TPolyLine
-  Float_t ix[maxInnerSize];        // inner X points
-  Float_t iy[maxInnerSize];        // inner Y points
-  
-  Float_t ox[maxOuterSize];        // outer
+  // inner points
+  //
+  Float_t ix[maxInnerSize];
+  Float_t iy[maxInnerSize];
+
+  //
+  //  outer
+  //
+  Float_t ox[maxOuterSize];
   Float_t oy[maxOuterSize];
 
-  Float_t tempX,tempY;        // temp variables
+  Float_t tempX,tempY;
   int innerSize = 0;
-  
-  for (unsigned int hh=0;hh<in.size();hh++) // loop over inner ring points
-      {
-	  tempX = in[hh].x();      // grab X and Y
-	  tempY = in[hh].y();
-	  
-	  if(tempX || tempY)      // if not both at 0,0 (ie, on pad plane)
-	      {
-		  ix[innerSize]   = tempX; // add variables to array
-		  iy[innerSize] = tempY;   // for TPolyLine
-		  innerSize++;             // incriment size counter of arrays
-	      }
+
+  //
+  // loop over inner ring points
+  //
+  for (unsigned int hh=0;hh<in.size();hh++) {
+      // grab X and Y
+      tempX = in[hh].x();
+      tempY = in[hh].y();
+
+      //
+      // if not both at 0,0 (ie, on pad plane)
+      //
+      
+      if(tempX || tempY) {
+	  // add variables to array
+	  // for TPolyLine and
+	  // incriment size counter of arrays
+	  ix[innerSize]   = tempX;
+	  iy[innerSize] = tempY;
+	  innerSize++;       
       }
-  
-  int outerSize = 0;                    // size counter fro Outer Ring
-		    
-  for (unsigned int hh=0;hh<out.size();hh++)        // same as above for Outer Ring
-      {
-	  tempX = out[hh].x();
-	  tempY = out[hh].y();
-	  
-	  if(tempX || tempY)
-	      {
-		  ox[outerSize]   = tempX;
-		  oy[outerSize] = tempY;
-		  outerSize++;
-	      }
-	  
+  }
+
+  // size counter fro Outer Ring
+  int outerSize = 0;
+
+  //
+  // same as above for Outer Ring
+  //
+  for (unsigned int hh=0;hh<out.size();hh++) {
+      tempX = out[hh].x();
+      tempY = out[hh].y();
+      
+      if(tempX || tempY) {
+	  ox[outerSize]   = tempX;
+	  oy[outerSize] = tempY;
+	  outerSize++;
       }
+	  
+  }
   
   mInnerRing = new TPolyLine(innerSize,ix,iy); // Make T drawable Ring
   mOuterRing = new TPolyLine(outerSize,ox,oy); // Only use valid points
   
   mInnerRing->SetLineStyle(1); // solid
   mOuterRing->SetLineStyle(1);
-
+  
   // Set Colors for rings
   if (mParticle->name() == "e-" || mParticle->name() == "e+") {
-    mInnerRing->SetLineColor(5);
-    mOuterRing->SetLineColor(5);
+      mInnerRing->SetLineColor(5);
+      mOuterRing->SetLineColor(5);
   }
   
   if (mParticle->name() == "pi-" || mParticle->name() == "pi+") {
-    mInnerRing->SetLineColor(2);
-    mOuterRing->SetLineColor(2);
+      mInnerRing->SetLineColor(2);
+      mOuterRing->SetLineColor(2);
   }
   
   if (mParticle->name() == "kaon-" || mParticle->name() == "kaon+") {
-    mInnerRing->SetLineColor(1);
-    mOuterRing->SetLineColor(1);
+      mInnerRing->SetLineColor(1);
+      mOuterRing->SetLineColor(1);
   }
   
   if (mParticle->name() == "anti_proton" || mParticle->name() == "proton") {
-    mInnerRing->SetLineColor(3);
-    mOuterRing->SetLineColor(3);
+      mInnerRing->SetLineColor(3);
+      mOuterRing->SetLineColor(3);
   }
 }
 
@@ -143,12 +165,13 @@ StRichDrawableTRings::~StRichDrawableTRings() {
     
     delete mInnerRing;
     delete mOuterRing;
+    delete mRand;
 
    for (unsigned int i=0;i<mHits.size();i++) {
        delete mHits[i];
    }
    mHits.clear();
-   mHits.resize(0);   
+   mHits.resize(0);
 }
 
 
@@ -156,9 +179,12 @@ void StRichDrawableTRings::draw() {
 
     mInnerRing->Draw();
     mOuterRing->Draw();
-   
-    for(unsigned int i=0;i<mHits.size();i++)
-	mHits[i]->Draw();
+
+    //
+    // used to draw the hits here as well
+    //
+//     for(unsigned int i=0;i<mHits.size();i++)
+//      	mHits[i]->Draw();
 }
 
 void StRichDrawableTRings::clear() {
@@ -218,24 +244,78 @@ StParticleDefinition* StRichDrawableTRings::getParticle() {
 }
 
 
-void StRichDrawableTRings::addHit(StRichHit* hit){
-  float x,y;
-  if (hit) {
-    x = hit->local().x();
-    y = hit->local().y();
-  }
-  
-  StRichDrawableTHit * tempHit = new StRichDrawableTHit(x,y,4); // 4 == Circle
-  tempHit->SetMarkerColor(mInnerRing->GetLineColor());  // Make Same Color as Ring
-  tempHit->SetMarkerSize(2+ rand->Rndm());              // Size == 2+ (num between 0 and 1)
-  mHits.push_back(tempHit);                             // add hit to list
+void StRichDrawableTRings::addHit(StRichHit* hit) {
+
+    if (!hit) {
+	cout << "StRichDrawableTRings::addHit()\n";
+	cout << "\t ERROR: null pointer" << endl;
+	return;
+    }
+
+    //
+    // Make a drawable hit and save it
+    //
+
+    //
+    // 4 == Circle
+    //
+    StRichDrawableTHit* tempHit = new StRichDrawableTHit(hit,4);
+
+    // Make Same Color as Ring
+    tempHit->SetMarkerColor(mInnerRing->GetLineColor());
+
+    //
+    // Size == 2+ (num between 0 and 1)
+    //
+    tempHit->SetMarkerSize(2+ mRand->Rndm());
+    mHits.push_back(tempHit);
+}
+
+void StRichDrawableTRings::hilite() {
+
+    cout << "StRichDrawableTRings::hilite() size => " << mHits.size() << endl;
+    // loop over all the Hits
+    for(size_t ii=0; ii<mHits.size(); ii++) {
+	//
+	// Make Same Color as Ring
+	//
+	mHits[ii]->SetMarkerColor(mInnerRing->GetLineColor());
+	mHits[ii]->SetMarkerSize(2+ mRand->Rndm());
+	mHits[ii]->Draw();
+    }
+
+}
+
+void StRichDrawableTRings::hilite(const StRichHitFlag& f) {
+
+    // loop over all the DrawableTHits
+    for(size_t ii=0; ii<mHits.size(); ii++) {
+	//
+	// Make Same Color as Ring
+	//
+	if(!mHits[ii]->getRichHit()) {
+	    cout << "StRichDrawableTRings::hilite(flag)\n";
+	    cout << "\tptr problem" << endl;
+	    continue;
+	}
+
+	if(mHits[ii]->getRichHit()->isSet(f)) {
+	    mHits[ii]->SetMarkerColor(mInnerRing->GetLineColor());
+	    mHits[ii]->SetMarkerSize(2+ mRand->Rndm());
+	    mHits[ii]->Draw();
+	}
+
+    }
 }
 
 Int_t StRichDrawableTRings::numberOfHits(){
-    return mHits.size();}
+    return mHits.size();
+}
 
 StRichDrawableTHit* StRichDrawableTRings::getHit(unsigned int i){
+
     if(i < mHits.size())
 	return mHits[i];
     else
-	return 0;}
+	return 0;
+}
--- StRoot/StRichDisplayMaker/StRichDrawableTRings.h
+++ StRoot/StRichDisplayMaker/StRichDrawableTRings.h
@@ -1,12 +1,14 @@
 /**********************************************************
- * $Id: StRichDrawableTRings.h,v 2.1 2000/09/29 17:36:58 gans Exp $
+ * $Id: StRichDrawableTRings.h,v 2.2 2000/11/01 16:55:28 lasiuk Exp $
  *
  * Description:
  *  
  *
  *  $Log: StRichDrawableTRings.h,v $
- *  Revision 2.1  2000/09/29 17:36:58  gans
- *  Modified addHit(), StThreeVector<double> -> StThreeVectorF,other minor stuff
+ *  Revision 2.2  2000/11/01 16:55:28  lasiuk
+ *  add hilite() members which utilize the flags defined in
+ *  StEvent.  draw() member also added.  Hits are hilited only
+ *  by demand.  Not by default
  *
  *  Revision 2.1  2000/09/29 17:36:58  gans
  *  Modified addHit(), StThreeVector<double> -> StThreeVectorF,other minor stuff
@@ -40,6 +42,8 @@
 using std::vector;
 #endif
 
+#include "StEnumerations.h"
+
 class StRichTrack;
 class StRichRings;
 class StRichHit;
@@ -50,9 +54,19 @@ public:
     StRichDrawableTRings();  
     StRichDrawableTRings(StRichRings& ring);  
     ~StRichDrawableTRings();
+
+    //
+    // Draws the rings and loads the hits
     void draw();
-    void clear(); // Clearss Ring?
+    void clear();
     void addHit(StRichHit*);
+
+    //
+    // Draws (hilites) the hits
+    //
+    void hilite();
+    void hilite(const StRichHitFlag&);
+    
     StRichDrawableTHit* getHit(unsigned int);
     Int_t numberOfHits();
     
@@ -61,12 +75,13 @@ public:
 
   
 private:
-    TRandom* rand;
-    TPolyLine*   mInnerRing;
-    TPolyLine*   mOuterRing;
-    StRichTrack* mTrack;
+    TRandom*       mRand;
+    TPolyLine*     mInnerRing;
+    TPolyLine*     mOuterRing;
+    StRichTrack*   mTrack;
+
     StParticleDefinition* mParticle;
-    vector<StRichDrawableTHit*> mHits;  
+    vector<StRichDrawableTHit*> mHits; //! 
   
 };
 
