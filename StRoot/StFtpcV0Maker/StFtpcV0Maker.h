bddcd040e17ab2da517c585cbb34c675cd644a63
Janet Seyboth <jcs@mppmu.mpg.de>
2000-11-16 12:48:39 +0000

    Save FTPC vzero inforamtion in correct banks Use correct FTPC track class

--- StRoot/StFtpcV0Maker/StFtpcV0Maker.h
+++ StRoot/StFtpcV0Maker/StFtpcV0Maker.h
@@ -1,6 +1,10 @@
-// $Id: StFtpcV0Maker.h,v 1.4 2000/01/03 13:16:17 jcs Exp $
+// $Id: StFtpcV0Maker.h,v 1.5 2000/11/16 12:48:39 jcs Exp $
 //
 // $Log: StFtpcV0Maker.h,v $
+// Revision 1.5  2000/11/16 12:48:39  jcs
+// Save FTPC vzero inforamtion in correct banks
+// Use correct FTPC track class
+//
 // Revision 1.4  2000/01/03 13:16:17  jcs
 // Add CVS Id strings
 //
@@ -16,11 +20,17 @@
 #ifndef StMaker_H
 #include "StMaker.h"
 #endif
-//class St_stk_stkpar;
+
+#include "TH2.h"
+class TH2F;
+
+class dst_vertex_st;
+class St_dst_vertex;
+
 class StFtpcV0Maker : public StMaker {
  private:
    Bool_t drawinit;
-// static Char_t  m_VersionCVS = "$Id: StFtpcV0Maker.h,v 1.4 2000/01/03 13:16:17 jcs Exp $";
+// static Char_t  m_VersionCVS = "$Id: StFtpcV0Maker.h,v 1.5 2000/11/16 12:48:39 jcs Exp $";
 // Int_t          m_mode;        // mode 1 = primaries;
 // St_stk_stkpar *m_stk_stkpar;  //! pointer to stk parameters
  
@@ -30,6 +40,7 @@ class StFtpcV0Maker : public StMaker {
  TH1F  *m_kaonMass;
  TH1F  *m_lambdaMass;
  TH1F  *m_antiLambdaMass;
+ TH1F  *m_z;
  
  public: 
                   StFtpcV0Maker(const char *name="fv0", const char *title="fv0_something");
@@ -38,7 +49,7 @@ class StFtpcV0Maker : public StMaker {
    virtual Int_t  Make();
 // virtual void Set_mode       (Int_t   m =      2){m_mode       = m;} // *MENU*
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StFtpcV0Maker.h,v 1.4 2000/01/03 13:16:17 jcs Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StFtpcV0Maker.h,v 1.5 2000/11/16 12:48:39 jcs Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
    ClassDef(StFtpcV0Maker, 1)   //StAF chain virtual base class for Makers
 };
