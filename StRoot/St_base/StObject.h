12069d8ec9c9e9b5d24491a490e2b96999381be0
perev <>
2000-12-09 02:11:02 +0000

    clone() called Clone()

--- StRoot/St_base/StObject.h
+++ StRoot/St_base/StObject.h
@@ -1,31 +1,8 @@
-// $Id: StObject.h,v 1.10 2000/09/30 17:48:27 perev Exp $
+// $Id: StObject.h,v 1.11 2000/12/09 02:11:02 perev Exp $
 // $Log: StObject.h,v $
-// Revision 1.10  2000/09/30 17:48:27  perev
-// Zombies cons and loop for stru vector
+// Revision 1.11  2000/12/09 02:11:02  perev
+// clone() called Clone()
 //
-// Revision 1.9  2000/09/15 15:11:58  perev
-// Zombie for StEvent
-//
-// Revision 1.8  2000/07/30 01:49:03  perev
-// StObject vers restored
-//
-// Revision 1.6  2000/04/20 14:24:09  perev
-// StArray fixes
-//
-// Revision 1.5  2000/04/18 02:57:26  perev
-// StEvent browse
-//
-// Revision 1.4  1999/11/15 23:09:10  perev
-// Streamer for StrArray and auto remove
-//
-// Revision 1.3  1999/10/30 19:36:02  perev
-// Added clone() to StObject
-//
-// Revision 1.2  1999/06/23 20:31:04  perev
-// StArray I/O + browser
-//
-// Revision 1.1  1999/04/30 13:15:56  fisyak
-// Ad StObject, modification StArray for StRootEvent
 //
 
 #ifndef STAR_StObject
@@ -43,7 +20,7 @@ class StObject : public TObject {
   virtual ~StObject();
   virtual void Browse(TBrowser *b);
   virtual Bool_t IsFolder() const;
-  virtual TObject *clone() const {return 0;}
+  virtual TObject *clone() const {return ((TObject*)this)->Clone();}
   Int_t   isZombie() const {return IsZombie();}
   virtual void makeZombie(){MakeZombie();}
   ClassDef(StObject,2) // Base class for StEvent
