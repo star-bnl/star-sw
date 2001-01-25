85c90a9887075eee4bccaf768cefe0b5630ad251
Lilian Martin <lilian.martin@subatech.in2p3.fr>
2001-01-25 17:39:22 +0000

    Unused variables removed Variables declared and Const rather than by using define

--- StRoot/StEstMaker/StEstConst.h
+++ StRoot/StEstMaker/StEstConst.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StEstConst.h,v 1.1 2000/12/07 11:14:21 lmartin Exp $
+ * $Id: StEstConst.h,v 1.2 2001/01/25 17:39:22 lmartin Exp $
  *
  * Author: PL,AM,LM,CR (Warsaw,Nantes)
  ***************************************************************************
@@ -10,13 +10,15 @@
  ***************************************************************************
  *
  * $Log: StEstConst.h,v $
+ * Revision 1.2  2001/01/25 17:39:22  lmartin
+ * Unused variables removed
+ * Variables declared and Const rather than by using define
+ *
  * Revision 1.1  2000/12/07 11:14:21  lmartin
  * First CVS commit
  *
  **************************************************************************/
-#define RAD_TO_DEG 57.29578
-#define MAXFINDWAF 70         // max number of wafer found for preprojection
-#define MAXHITPROJ 400        // max number of hits in projection
-#define MAXWAFINBIN 8         //??? maximum number of wafers in one bin (for indexing)
-#define B 5                 // magnetic field
-#define KAPPA  0.000299792458
+const int MAXFINDWAF=70;         // max number of wafer found for preprojection
+const int MAXHITPROJ=400;        // max number of hits in projection
+
+
