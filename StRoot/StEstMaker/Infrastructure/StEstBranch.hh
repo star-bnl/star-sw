3fef3ffd3e9252d5b28cab2a06ce00285c6364d8
Lilian Martin <lilian.martin@subatech.in2p3.fr>
2001-01-26 10:16:45 +0000

    Minor changes. Unused mLastLay data member removed. Short description of the data members added.

--- StRoot/StEstMaker/Infrastructure/StEstBranch.hh
+++ StRoot/StEstMaker/Infrastructure/StEstBranch.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StEstBranch.hh,v 1.2 2001/01/25 18:13:27 lmartin Exp $
+ * $Id: StEstBranch.hh,v 1.3 2001/01/26 10:16:44 lmartin Exp $
  *
  * Author: PL,AM,LM,CR (Warsaw,Nantes)
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StEstBranch.hh,v $
+ * Revision 1.3  2001/01/26 10:16:44  lmartin
+ * Minor changes. Unused mLastLay data member removed. Short description of the data members added.
+ *
  * Revision 1.2  2001/01/25 18:13:27  lmartin
  * Minor changes in the GetHit and SetHelix methods
  *
@@ -30,23 +33,22 @@ class StEstBranch {
   
 protected:
   
-  StEstTrack* mTrack;
-  StEstHit**  mHits;
-  StHelix* mHelix;
-  long int mNHits;
-  long int mMaxHits;
-  long int mNFit;
-  long int mLastFitStatus;
-  double mChisq;
-  double mChisqCir;
-  double mChisqLin;
-  double *mDist;        // table of distances between hits and projections
-  int mLastLay;
-  int mDebugLevel;
-  int mStep;
-  int mIsGood;
-  int mIsGoodOld;
-  int mHitPosition;
+  StEstTrack* mTrack; // pointer to the mother track
+  StEstHit**  mHits; // list of pointers to the hits in the branch
+  StHelix* mHelix; // pointer to the branch helix
+  long int mNHits; // number of hits in the branch
+  long int mMaxHits; //maximum number of hits in the branch 
+  long int mNFit; // number of hits used in the fit
+  long int mLastFitStatus; // Status flag return by the egr fitting routine
+  double mChisq; // Total chisq
+  double mChisqCir; // Circular chisq
+  double mChisqLin; // Linear chisq
+  double *mDist;  // table of distances between hits and branch projections
+  int mDebugLevel; // Printing level for the methods
+  int mStep; // Step (10*mSuperPass+mPass) when the branch is formed
+  int mIsGood; // The branch matches the ideal branch pattern at a given step
+  int mIsGoodOld; //The branch matches the ideal branch pattern at the previous step
+  int mHitPosition; //Rank of the selected hit according to the hits-projection distance
   
 public :
   StEstBranch(StEstTrack* tr=NULL, long int maxhits=1, long int nf=0, long int nh=0, StEstHit** hit=NULL, double *dist=0, int isgood=1);
@@ -73,7 +75,6 @@ public :
   double GetDist(long nr);
   double GetDist(StEstHit* hit);
   long int GetNFit();
-  int GetLastLayer();
   void SetDebugLevel(int deb);
   void SetStep(int step);
   int GetStep();
@@ -82,7 +83,6 @@ public :
   void SetIsGoodOld(int isgood);
   int GetIsGoodOld();
   friend class StEstTracker;
-  //  StEstBranch& operator=(const StEstBranch);
 };
  
 inline int StEstBranch::CheckAvailability() {
@@ -99,7 +99,6 @@ inline double   StEstBranch::GetChiSq()             {return mChisq;};
 inline double   StEstBranch::GetChiSqCir()             {return mChisqCir;};
 inline double   StEstBranch::GetChiSqLin()             {return mChisqLin;};
 inline double   StEstBranch::GetDist(long nr)          {return mDist[nr];};
-inline int      StEstBranch::GetLastLayer()         {return mLastLay;}; 
 inline void     StEstBranch::SetDebugLevel(int deb) {mDebugLevel = deb;};
 inline void     StEstBranch::SetStep(int step) {mStep = step;};
 inline int      StEstBranch::GetStep() {return mStep;};
