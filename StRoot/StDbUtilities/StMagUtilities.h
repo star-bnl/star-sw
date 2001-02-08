a8162f508839032fd3040ee324a34ae46b5bf385
Jim Thomas <jhthomas@lbl.gov>
2001-02-08 22:26:20 +0000

    Added corrections for CM electrostatic distortions

--- StRoot/StDbUtilities/StMagUtilities.h
+++ StRoot/StDbUtilities/StMagUtilities.h
@@ -1,16 +1,19 @@
 /***********************************************************************
  *
- * $Id: StMagUtilities.h,v 1.3 2000/12/15 16:10:45 jhthomas Exp $
+ * $Id: StMagUtilities.h,v 1.4 2001/02/08 22:26:20 jhthomas Exp $
  *
  * Author: Jim Thomas   11/1/2000
  *
  ***********************************************************************
  *
- * Description: Utilities for the Magnetid Field
+ * Description: Utilities for the Magnetic Field
  *
  ***********************************************************************
  *
  * $Log: StMagUtilities.h,v $
+ * Revision 1.4  2001/02/08 22:26:20  jhthomas
+ * Added corrections for CM electrostatic distortions
+ *
  * Revision 1.3  2000/12/15 16:10:45  jhthomas
  * Add PadRow13, Clock, and Twist corrections to UndoDistortion
  *
@@ -36,41 +39,21 @@
 #include "TROOT.h"
 #include "TFile.h"
 
-#define  nZ               57            // Standard STAR Map grid # of Z points in table
-#define  nR               28            // Number of R points in table
-#define  StarDriftV     5.45            // STAR Drift Velocity (cm/microSec)
-#define  StarMagE      148.0            // STAR Electric Field (V/cm) Magnitude
-#define  TPC_Z0        208.7            // Z location of STAR TPC Gating Grid
-#define  GAP13         1.595            // Width of the gap between the grids at row 13 and row 14 (cm)
-#define  GAPRADIUS     121.8            // Radius of row 13 at phi = zero degrees (cm)
-#define  GG           -127.5            // Gating Grid voltage (volts)
-#define  XTWIST       -0.075           // X Displacement of West end of TPC (cm) [Survey say -0.08]
-#define  YTWIST        0.02            // Y Displacement of West end of TPC (cm)
-#define  TPCLENGTH     220.0            // Overall Length of half the TPC == Z location of survey points
-#define  EASTCLOCKERROR  0.0            // Phi rotation of East end of TPC in milli-radians
-#define  WESTCLOCKERROR -0.43           // Phi rotation of West end of TPC in milli-radians
-
-enum EBField   { kUndefined=0, kConstant=1, kMapped=2, kChain=3 } ;
+enum   EBField  { kUndefined=0, kConstant=1, kMapped=2, kChain=3 } ;
 
 class StMagUtilities {
-  
+
 
  private:
 
-  Float_t Bz[nZ][nR], Br[nZ][nR] ;         
-  Float_t Radius[nR], ZList[nZ]  ;         
-
   virtual void    ReadField ( ) ;
+  virtual Int_t   Search ( Int_t N, Float_t Xarray[], Float_t x ) ;
   virtual Float_t QuadInterp ( const Float_t Xarray[], const Float_t Yarray[], 
 			       const Float_t x ) ;
   virtual void    InterpolateBfield ( const Float_t r, const Float_t z, 
 				      Float_t &Br_value, Float_t &Bz_value ) ;
-
- protected:
-  
-  EBField   fMap ;      // Field Map identifier
-  Float_t   fFactor ;   // Multiplicative factor (allows scaling and sign reversal)
-  Float_t   fRescale ;  // Multiplicative factor (allows re-scaling according to which map read)
+  virtual void    InterpolateEdistortion ( const Float_t r, const Float_t phi, const Float_t z, 
+                                           Float_t &Er_value, Float_t &Ephi_value ) ;
 
  public:
 
@@ -80,24 +63,21 @@ class StMagUtilities {
 
   virtual void    BField ( const Float_t x[], Float_t B[] ) ;
   virtual void    BrBzField( const Float_t r, const Float_t z, Float_t &Br_value, Float_t &Bz_value ) ;
-  virtual void    EField ( const Float_t x[], Float_t E[] ) ;
   virtual void    DoDistortion ( const Float_t x[], Float_t Xprime[] ) ;
   virtual void    UndoDistortion ( const Float_t x[], Float_t Xprime[] ) ;
   virtual void    UndoBDistortion ( const Float_t x[], Float_t Xprime[] ) ;
-  virtual void    UndoEDistortion ( const Float_t x[], Float_t Xprime[] ) ;
   virtual void    UndoPad13Distortion( const Float_t x[], Float_t Xprime[] ) ;
   virtual void    UndoTwistDistortion( const Float_t x[], Float_t Xprime[] ) ;
   virtual void    UndoClockDistortion( const Float_t x[], Float_t Xprime[] ) ;
-  virtual Float_t GetFactor() { return fFactor ; }
-  virtual void    SetFactor ( Float_t value ) { fFactor = value ; }
-  virtual Int_t   Search( Int_t N, Float_t Xarray[], Float_t x ) ;
-  virtual EBField GetMap() { return fMap ; }
+  virtual void    UndoMembraneDistortion( const Float_t x[3], Float_t Xprime[3] ) ;
 
   ClassDef(StMagUtilities,1)    // Base class for all STAR MagField
 
 };
 
 #endif
+
+
 
 
 
