070f759b9cca7317f35b7df8f7c6bdd2cd0b2371
fine <>
2001-03-03 00:08:44 +0000

    Change to make CINT happy

--- StRoot/macros/test/TestFileIter.C
+++ StRoot/macros/test/TestFileIter.C
@@ -1,4 +1,4 @@
-// $Id: TestFileIter.C,v 1.1 2001/03/02 21:22:23 fine Exp $
+// $Id: TestFileIter.C,v 1.2 2001/03/03 00:08:44 fine Exp $
 // Author: Valery Fine(fine@bnl.gov)   01/03/2001
 // Copyright(c) 2001 [BNL] Brookhaven National Laboratory, Valeri Fine (fine@bnl.gov). All right reserved",
 void TestFileIter(){
@@ -11,16 +11,17 @@ void TestFileIter(){
   TRandom run;
   TRandom event;
   {
-    TFileIter outSet("test.root","RECREATE");
-    UInt_t totalEvent = 10;
+    TFileIter *outSet = new TFileIter("test.root","RECREATE");
+    UInt_t totalEvent = 4;
     UInt_t runNumber  = 20010301;
     Int_t i=0;
     Int_t j=0;
-    for (;j < 10;j++) {
+    for (;j < 3;j++) {
       for (i = 1;i<totalEvent;i++) {
-        outSet.NextEventPut(ds,UInt_t(i),UInt_t(runNumber+j+10*run.Rndm()-5));
+        outSet->NextEventPut(ds,UInt_t(i),UInt_t(runNumber+j+10*run.Rndm()-5));
       }
     }
+    delete outSet;
   }
   printf(" ----------------------> TFile has been created <--------------------\n");
   TFile *f = new TFile("test.root");
@@ -33,7 +34,7 @@ void TestFileIter(){
   // Loop over all objects, read them in to memory one by one
 
   printf(" -- > Loop over all objects, read them in to memory one by one < -- \n");
-  for( readObj = 0; int(readObj) < size; readObj.SkipObjects()){ 
+  for( readObj = 0; int(readObj) < size; readObj.SkipObjects() ){ 
       nextObject = *readObj; 
       printf(" %d bytes of the object \"%s\" of class \"%s\" written with TKey \"%s\"  has been read from file\n"
                ,readObj.GetObjlen()
@@ -73,7 +74,7 @@ void TestFileIter(){
       delete nextObject;
   }
 // Loop over the objects starting from the object with the key name "event.02.01"
-  printf(" -- > Loop over the objects starting from the 86-th object" < -- \n");
+  printf(" -- > Loop over the objects starting from the 86-th object < -- \n");
   for( readObj = (const char *)(readObj = 86); (const char *)readObj != 0; readObj.SkipObjects()){ 
       nextObject = *readObj; 
       printf(" Object \"%s\" of class \"%s\" written with Tkey \"%s\"  has been read from file\n"
