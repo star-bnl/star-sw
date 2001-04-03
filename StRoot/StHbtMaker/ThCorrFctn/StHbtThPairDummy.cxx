c6ca56db327ac3afae8183244bd617ac0c3cdc96
Adam Kisiel <kisiel@if.pw.edu.pl>
2001-04-03 21:07:48 +0000

    The code to perform Theoretical CF calculations.   Includes Fortran code from Richard Lednicky.   The code also allows to study the effects of   momentum resolution and particle purity.

---
+++ StRoot/StHbtMaker/ThCorrFctn/StHbtThPairDummy.cxx
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *
+ *  
+ *
+ * Author: Laurent Conin, Fabrice Retiere, Subatech, France
+ ***************************************************************************
+ *
+ * Description : implementation of StHbtThPairDummy
+ *
+ ***************************************************************************
+ *
+ *  
+ *
+ ***************************************************************************/
+
+#include "StHbtMaker/ThCorrFctn/StHbtThPairDummy.h"
+
+StHbtThPairDummy::StHbtThPairDummy() : StHbtThPair() 
+{mWeightNum=mWeightDen=1.;mWeightOk=true;};
+
+void StHbtThPairDummy::Set(const StHbtPair* aPair){
+  mMeasPair=aPair;
+
+}
+
