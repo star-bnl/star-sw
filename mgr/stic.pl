09c6970bafd43a804bae203ac0470a7201b944ce
Jerome Lauret <jeromel@bnl.gov>
2001-04-10 19:03:58 +0000

    No changes. Just made human readable with comments.

--- mgr/stic.pl
+++ mgr/stic.pl
@@ -2,6 +2,7 @@
 use File::Basename;
 use Cwd;
 use File::Copy;
+
 my $target = shift;
 my @pars = ();
 while (my $par = shift) {
@@ -9,17 +10,30 @@ while (my $par = shift) {
   push @pars, $par;
 }
 my $file = basename($target); 
-my $dir = dirname($target);   
-my $Dir = cwd();
-my $idl = pop @pars;
-if ($idl !~ /^\//) {$idl = $Dir . "/" . $idl;}# print "Full name $idl\n";
-my $tmpdir = "/tmp/" . $$;# print "tmpdir = $tmpdir \n";
-if (! -d $dir    && !mkdir ($dir,    0755)) {die "$0:Can't create directory $dir\n";};
-if (! -d $tmpdir && !mkdir ($tmpdir, 0755)) {die "$0:Can't create directory $tmpdir\n";};
-my $com = "cd $tmpdir && stic @pars $idl";# print "=========================\n$com\n";
+my $dir  = dirname($target);   
+my $Dir  = cwd();
+my $idl  = pop @pars;
+
+# check target path
+if ($idl !~ /^\//) { $idl = $Dir . "/" . $idl;}
+if (! -d $dir    && !mkdir ($dir,    0755)) {
+    die "$0:Can't create directory $dir\n";
+}
+
+# create temp dir
+my $tmpdir = "/tmp/" . $$;
+if (! -d $tmpdir && !mkdir ($tmpdir, 0755)) {
+    die "$0:Can't create directory $tmpdir\n";
+}
+
+# exec command
+my $com = "cd $tmpdir && stic @pars $idl";
 my $exec = `$com`; if ($?) { exit 2; }
-my $sfile = $tmpdir . "/" . $file;# print "sfile $sfile\n";
+
+# copy file and delete temp directory
+my $sfile = $tmpdir . "/" . $file;
 if (-f $sfile) {copy ($sfile,$target);}
 `rm -rf $tmpdir`; 
 if ($?) { exit 2; }
 exit 0;
+
