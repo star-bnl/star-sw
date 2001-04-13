fcb0b940b37e8b374983551625269fb36790b7cb
pruneau <>
2001-04-13 15:14:17 +0000

    adding sti specific constants class

---
+++ StRoot/Sti/StiConstants.cxx
@@ -0,0 +1,8 @@
+#include "StiConstants.h"
+ClassImp(StiConstants)
+
+
+int StiConstants::Ok = 0 ;
+int StiConstants::Warning = 1;
+int StiConstants::Error   = 2;
+int StiConstants::SevereError = 3;
---
+++ StRoot/Sti/StiConstants.h
@@ -0,0 +1,16 @@
+#ifndef StiHit_H
+#define StiHit_H 1
+#include "TObject.h"
+
+
+class StiConstants
+{
+ public:
+
+  static int Ok;
+  static int Warning;
+  static int Error;
+  static int SevereError;
+
+  ClassDef(StiConstants, 1)
+}
