8a0feb131ab19aa55e6b7fbe173a35cbd571d9e1
Michael Lee Miller <mlmiller@mit.edu>
2001-04-18 20:12:38 +0000

    Provided dummy implementation of non-pure virtual function reset(), minor changes to get Sti library to load.

--- StRoot/Sti/StiTrack.cxx
+++ StRoot/Sti/StiTrack.cxx
@@ -1,7 +1,21 @@
 #include <iostream.h>
 #include <stdlib.h>
 
+//StEvent
+#include "StEventTypes.h"
 #include "StiTrack.h"
 
 ClassImp(StiTrack)
+
+StiTrack::StiTrack()
+{
+}
  
+StiTrack::~StiTrack()
+{
+}
+
+void StiTrack::reset()
+{
+    return;
+}
