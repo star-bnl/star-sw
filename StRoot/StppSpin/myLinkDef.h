1e3d496c813777d6f26377394368376433f2ccb8
Jan Balewski <balewski@lbl.gov>
2001-04-19 21:30:37 +0000

    add I/O to ppDst

---
+++ StRoot/StppSpin/myLinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// ... main persistency classes
+#pragma link C++ class ppDst-;
+#pragma link C++ class ppDst_t-;
+#pragma link C++ class xxDst-;
+#pragma link C++ class xxDst_t-;
+#endif
+
