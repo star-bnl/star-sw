54401f507b90b775d489e3ee03a5f106221547e9
Thomas Ullrich <thomas.ullrich@bnl.gov>
2001-04-23 19:28:53 +0000

    Inherit from StObject. Not a singleton anymore.

--- StRoot/StEvent/StEventClusteringHints.h
+++ StRoot/StEvent/StEventClusteringHints.h
@@ -1,12 +1,12 @@
 /***************************************************************************
  *
- * $Id: StEventClusteringHints.h,v 2.3 2001/04/20 00:50:49 ullrich Exp $
+ * $Id: StEventClusteringHints.h,v 2.4 2001/04/23 19:28:53 ullrich Exp $
  *
  * Author: Thomas Ullrich, Apr 2001
  ***************************************************************************
  *
- * Description: Singeleton class to provide clustering hints for
- *              StEvent I/O. The class can distinguish between two modes
+ * Description: Class to provide clustering hints for StEvent I/O.
+ *              The class can distinguish between two modes
  *              miniDST and DST mode in which clustering hints
  *              are different.
  *              With clustering hints we mean the association
@@ -17,6 +17,9 @@
  ***************************************************************************
  *
  * $Log: StEventClusteringHints.h,v $
+ * Revision 2.4  2001/04/23 19:28:53  ullrich
+ * Inherit from StObject. Not a singleton anymore.
+ *
  * Revision 2.3  2001/04/20 00:50:49  ullrich
  * Added new query methods.
  *
@@ -34,17 +37,18 @@
 #include <string>
 #include <vector>
 #include <iostream.h>
-#include "Rtypes.h"
+#include "StObject.h"
 #if !defined(ST_NO_NAMESPACES)
 using std::map;
 using std::string;
 using std::vector;
 #endif
 
-class StEventClusteringHints {
+class StEventClusteringHints : public StObject {
 public:
-    static StEventClusteringHints* instance();  // get one and only instance (singleton)
-    
+    StEventClusteringHints();
+    virtual ~StEventClusteringHints();
+        
     void setDstMode();                          // switch to DST mode
     void setMiniDstMode();                      // switch to miniDST mode
     const char* branchName(const char*) const;  // get branch name for given class name
@@ -58,15 +62,10 @@ public:
     void setBranch(const char*, const char*);   // assign classname with a branch name
     void print(ostream& = cout);                // print current configuration
     
-    friend class nobody;                        // makes g++ warning disappear
-
 private:
-    StEventClusteringHints();
-    virtual ~StEventClusteringHints();
     StEventClusteringHints(const StEventClusteringHints&);
 
 private:
-    static StEventClusteringHints *mSelf;   //!
     map<string,string> *mNameMap;           //!
     map<string,string> mDstMap;             //!
     map<string,string> mMiniDstMap;         //!
