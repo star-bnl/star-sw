c91c77b6a8f386c22784d740048e7a0ca8e878b3
Victor Perevoztchikov <perev@bnl.gov>
2001-04-23 16:02:02 +0000

    small fix

--- StRoot/StEventMaker/StRootEventManager.cc
+++ StRoot/StEventMaker/StRootEventManager.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StRootEventManager.cc,v 2.6 2001/04/20 16:23:22 perev Exp $
+ * $Id: StRootEventManager.cc,v 2.7 2001/04/23 16:02:02 perev Exp $
  *
  * Author: Original version by T. Wenaus, BNL
  *         Revised version for new StEvent by T. Ullrich, Yale
@@ -12,6 +12,9 @@
  ***************************************************************************
  *
  * $Log: StRootEventManager.cc,v $
+ * Revision 2.7  2001/04/23 16:02:02  perev
+ * small fix
+ *
  * Revision 2.6  2001/04/20 16:23:22  perev
  * Remove annoing warning
  *
@@ -34,6 +37,7 @@
  * Revised to build new StEvent version
  *
  **************************************************************************/
+#include "Rtypes.h"
 #include "StRootEventManager.hh"
 #include "StChain.h"
 #include <string>
@@ -68,22 +72,23 @@ void StRootEventManager::setup() { /* noop */ }
 void StRootEventManager::shutdown() { /* noop */ }
 
 #define RETURN_TABLE(TABLE_TYPE,TABLE_NAME) \
-TABLE_TYPE ##_st* StRootEventManager::returnTable_ ## TABLE_TYPE(long& nentries) const\
+_NAME2_(TABLE_TYPE,_st)* StRootEventManager::_NAME2_(returnTable_,TABLE_TYPE)(long& nentries) const\
 {\
-  TABLE_TYPE ## _st* table = NULL;\
-  St_ ## TABLE_TYPE  *tableWrap;\
-  const char *nm =  #TABLE_NAME;\
-  const char *nt =  #TABLE_TYPE;\
+  static int n13=13;\
+  _NAME2_(TABLE_TYPE,_st)* table = NULL;\
+  _NAME2_(St_,TABLE_TYPE)  *tableWrap;\
+  const char *nm =  _QUOTE_(TABLE_NAME);\
+  const char *nt =  _QUOTE_(TABLE_TYPE);\
   St_DataSetIter *Dst = (St_DataSetIter*)&mDst;\
-  tableWrap = (St_ ## TABLE_TYPE *) (*Dst)[nt];\
-  if (!tableWrap && nm[0]!='-') tableWrap = (St_ ## TABLE_TYPE *) (*Dst)[nm];\
+  tableWrap = (_NAME2_(St_,TABLE_TYPE) *) (*Dst)[nt];\
+  if (!tableWrap && nm[0]!='-') tableWrap = (_NAME2_(St_,TABLE_TYPE) *) (*Dst)[nm];\
   if (tableWrap) {\
     table = tableWrap->GetTable();\
     nentries = tableWrap->GetNRows();\
   } \
   else {\
-//VP    cerr << "StRootEventManager: Table type  " << nt << \
-//VP	  " - name " << nm << " not found in DataSet " << Dst->Pwd()->GetName() << endl;\
+  if ((n13--)>0) {    cerr << "StRootEventManager: Table type  " << nt << \
+  " - name " << nm << " not found in DataSet " << Dst->Pwd()->GetName() << endl;}\
     nentries = 0;\
   }\
   return table;\
