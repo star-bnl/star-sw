e14fb00a87e46dd3bd2a3f120e5076ea064184a7
Thomas Ullrich <thomas.ullrich@bnl.gov>
2001-04-24 18:20:13 +0000

    Added hits and slats to collection.

--- StRoot/StEvent/StTofCollection.cxx
+++ StRoot/StEvent/StTofCollection.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTofCollection.cxx,v 2.1 2000/12/08 03:52:42 ullrich Exp $
+ * $Id: StTofCollection.cxx,v 2.2 2001/04/24 18:20:13 ullrich Exp $
  *
  * Author: Thomas Ullrich, Dec 2000
  ***************************************************************************
@@ -14,16 +14,68 @@
  ***************************************************************************
  *
  * $Log: StTofCollection.cxx,v $
+ * Revision 2.2  2001/04/24 18:20:13  ullrich
+ * Added hits and slats to collection.
+ *
  * Revision 2.1  2000/12/08 03:52:42  ullrich
  * Initial Revision
  *
  **************************************************************************/
 #include "StTofCollection.h"
 
-static const char rcsid[] = "$Id: StTofCollection.cxx,v 2.1 2000/12/08 03:52:42 ullrich Exp $";
+static const char rcsid[] = "$Id: StTofCollection.cxx,v 2.2 2001/04/24 18:20:13 ullrich Exp $";
 
 ClassImp(StTofCollection)
     
 StTofCollection::StTofCollection() { /* noop */ }
 
 StTofCollection::~StTofCollection() { /* noop */ }
+
+const StSPtrVecTofSlat&
+StTofCollection::tofSlats() const
+{
+    return mTofSlats;
+}
+
+StSPtrVecTofSlat&
+StTofCollection::tofSlats()
+{
+    return mTofSlats;
+}
+
+const StSPtrVecTofHit&
+StTofCollection::tofHits() const
+{
+    return mTofHits;
+}
+
+StSPtrVecTofHit&
+StTofCollection::tofHits()
+{
+    return mTofHits;
+}
+
+void
+StTofCollection::addSlat(const StTofSlat* aSlat)  
+{
+    if (aSlat) mTofSlats.push_back(aSlat);
+}
+
+void
+StTofCollection::addHit(const StTofHit* aHit)
+{
+    if (aHit) mTofHits.push_back(aHit);
+}
+
+bool
+StTofCollection::slatsPresent() const
+{
+    return mTofSlats.size();
+}
+
+
+bool
+StTofCollection::hitsPresent() const
+{
+    return mTofHits.size();
+}
