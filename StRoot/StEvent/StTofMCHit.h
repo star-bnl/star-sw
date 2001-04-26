f83ddf998a576136f4fdebfaca23b02347fac961
Thomas Ullrich <thomas.ullrich@bnl.gov>
2001-04-26 01:07:42 +0000

    Initial Revision.

---
+++ StRoot/StEvent/StTofMCHit.h
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *
+ * $Id: StTofMCHit.h,v 2.1 2001/04/26 01:07:42 ullrich Exp $
+ *
+ * Author: Wei-Ming Zhang, April 2001 
+ ***************************************************************************
+ *
+ * Description:
+ *
+ ***************************************************************************
+ *
+ * $Log: StTofMCHit.h,v $
+ * Revision 2.1  2001/04/26 01:07:42  ullrich
+ * Initial Revision.
+ *
+ **************************************************************************/
+#ifndef StTofMCHit_hh
+#define StTofMCHit_hh
+
+#include <iostream.h>
+#include "StTofHit.h"
+
+class StTofMCHit : public StTofHit {
+public:
+    StTofMCHit();
+    //StTofMCHit(const StTofMCHit&);
+    //StTofMCHit& operator=(const StTofMCHit&);
+    ~StTofMCHit();
+
+    int       trkId()            const;
+    int       gId()              const;
+
+    void      setTrkId(Int_t);
+    void      setGId(Int_t);
+
+protected:
+    StObject* clone() const;
+    Int_t     mTrkId;
+    Int_t     mGId;
+
+    ClassDef(StTofMCHit,1)
+};
+
+inline void
+StTofMCHit::setTrkId(int trkId)
+{
+    mTrkId = trkId;
+}
+
+inline void
+StTofMCHit::setGId(int gId)
+{
+    mGId = gId;
+}
+
+inline int
+StTofMCHit::trkId() const
+{
+    return mTrkId;
+}
+
+inline int
+StTofMCHit::gId() const
+{
+    return mGId;
+}
+
+//non-members
+ostream& operator<<(ostream&, const StTofMCHit&);
+
+#endif
