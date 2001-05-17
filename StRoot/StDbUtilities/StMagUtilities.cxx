6826240c474bca2e3a544053fd7e34c604508d62
lasiuk <>
2001-05-17 18:35:11 +0000

    make changes to allow full transport (brian II)

--- StRoot/StDbUtilities/StMagUtilities.cxx
+++ StRoot/StDbUtilities/StMagUtilities.cxx
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * $Id: StMagUtilities.cxx,v 1.6 2001/04/23 17:06:04 didenko Exp $
+ * $Id: StMagUtilities.cxx,v 1.7 2001/05/17 18:35:11 lasiuk Exp $
  *
  * Author: Jim Thomas   11/1/2000
  *
@@ -11,8 +11,15 @@
  ***********************************************************************
  *
  * $Log: StMagUtilities.cxx,v $
- * Revision 1.6  2001/04/23 17:06:04  didenko
- * restore right version
+ * Revision 1.7  2001/05/17 18:35:11  lasiuk
+ * make changes to allow full transport (brian II)
+ *
+ * Revision 1.5  2001/04/17 16:22:29  lasiuk
+ * exb transport code for drifting electrons.
+ * The clock rotation and rotation of the TPC into
+ * the global coordinate (magenet) system are takne
+ * into account.  Transport alters x,y,z position of
+ * space point.
  *
  * Revision 1.4  2001/02/08 22:26:09  jhthomas
  * Added corrections for CM electrostatic distortions
@@ -30,6 +37,7 @@
 // StMagUtilities Class                                                 //
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
+
 // 
 // StMagUtilities - Jim Thomas 10 October 2000
 // A package of Bfield routines and distortion corrections for STAR.  
@@ -54,6 +62,13 @@
 //
 
 #include "StMagUtilities.h"
+#include "StGlobals.hh"
+#include "SystemOfUnits.h"
+
+#ifndef ST_NO_NAMESPACES
+using namespace units;
+#endif
+
 #define   gufld gufld_
 extern   "C" { void gufld(Float_t *, Float_t *) ; }
 
@@ -90,6 +105,7 @@ ClassImp(StMagUtilities)
 StMagUtilities::StMagUtilities( )
 
 {                                           // StMagUtilities constructor
+    printf("Default c'tor --- no arguments\n");
 
   float b[3], x[3] = { 0, 0, 0 } ;
   
@@ -97,9 +113,13 @@ StMagUtilities::StMagUtilities( )
     {
       gufld(x,b) ;                          // Read crude values from Chain to get scale
       gFactor = b[2] / 4.980 ;              // Select factor based on Chain values (kGauss) 
+
+      mBNominal = StThreeVectorD(b[0]*kilogauss, b[1]*kilogauss, b[2]*kilogauss);
+      
       gMap = kMapped ;                      // Do once & Select the B field map (mapped field or constant)
       ReadField() ;                         // Read the Magnetic and Electric Field Data Files
     }
+    this->init();
 
 }
 
@@ -108,6 +128,8 @@ StMagUtilities::StMagUtilities( )
 StMagUtilities::StMagUtilities( const EBField map = kMapped, const Float_t factor = 1.0 )
 
 {                                           // StMagUtilities constructor
+    printf("c'tor --- all default arguments\n");
+    this->init();
 
   if ( gMap == kUndefined ) 
     {
@@ -121,7 +143,61 @@ StMagUtilities::StMagUtilities( const EBField map = kMapped, const Float_t facto
       cout << "StMagUtilities Warning: The Maps have already been read and scaled." << endl
 	   << "                        Try instantiating with StMagUtilities( <empty> ) " << endl ;
     }
+}
 
+//________________________________________
+
+void StMagUtilities::init()
+{
+
+    mVdrift = StarDriftV*centimeter/microsecond;
+    mEField = StThreeVectorD(0.,0.,1.*StarMagE*(volt/centimeter));
+    
+    mMobility = (mVdrift/abs(mEField));
+    mOmegaTau = mMobility*.25*tesla;
+
+    mFrischGrid = TPC_Z0*centimeter;
+
+    mTpcCenter = StThreeVectorD(-2.76*millimeter, -.82*millimeter,-1.92*millimeter);
+
+    PR(mVdrift/(centimeter/microsecond));
+
+    PR(mMobility/(meter*meter/(volt*second)));
+    PR(mOmegaTau);
+
+    PR(mEField/(volt/centimeter));    
+    PR(mBNominal/tesla);
+
+    PR(mFrischGrid/centimeter);
+    PR(mTpcCenter/millimeter);
+
+    mWestClockRotation = WESTCLOCKERROR*milliradian;
+
+    mThetaX = -.156*milliradian;
+//     mThetaX = -.090*milliradian;
+    mThetaY = -.381*milliradian;
+//     mThetaY = -.341*milliradian;
+    mThetaZ = -.040*milliradian;
+
+    mCx = cos(mThetaX);
+    mSx = sin(mThetaX);
+    mCy = cos(mThetaY);
+    mSy = sin(mThetaY);
+    mCz = cos(mThetaZ);
+    mSz = sin(mThetaZ);
+
+    PR(mWestClockRotation/milliradian);
+    PR(mThetaX/milliradian);
+    PR(mThetaY/milliradian);
+    PR(mThetaZ/milliradian);
+
+    PR(mDriftField/(volt/centimeter));    
+    PR(mEField/(volt/centimeter));    
+    mDriftField = this->rotateToMagnet(mEField);
+    PR(mDriftField/(volt/centimeter));
+
+
+    
 }
 
 //________________________________________
@@ -133,7 +209,6 @@ StMagUtilities::StMagUtilities( const EBField map = kMapped, const Float_t facto
 void StMagUtilities::BField( const Float_t x[3], Float_t B[3] )
 
 {                          
-
   Float_t r, z, Br_value, Bz_value ;
 
   z  = x[2] ;
@@ -165,7 +240,6 @@ void StMagUtilities::BField( const Float_t x[3], Float_t B[3] )
 void StMagUtilities::BrBzField( const Float_t r, const Float_t z, Float_t &Br_value, Float_t &Bz_value )
 
 {
-  
   InterpolateBfield( r, z, Br_value, Bz_value ) ;
 
 }
@@ -179,14 +253,52 @@ void StMagUtilities::BrBzField( const Float_t r, const Float_t z, Float_t &Br_va
 void StMagUtilities::UndoDistortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    //printf("StMagUtilities::UndoDistortion()\n");
 
-  Float_t Xprime1[3], Xprime2[3] ;
-  
-  UndoBDistortion        ( x, Xprime1 ) ;
-  UndoPad13Distortion    ( Xprime1, Xprime2 ) ;
-  UndoTwistDistortion    ( Xprime2, Xprime1 ) ;
-  UndoClockDistortion    ( Xprime1, Xprime2 ) ;
-  UndoMembraneDistortion ( Xprime2, Xprime ) ;
+    Float_t Xprime1[3], Xprime2[3] ;
+    StThreeVectorD ip(x[0],x[1],x[2]);
+
+//        PR(ip);
+    this->calculateDriftTime(ip);
+    
+    StThreeVectorD rot = this->clockRotation(ip);
+
+    //
+    // account for which side of chamber here
+    //
+    if(ip.z() > 0) {
+	rot.setZ(mFrischGrid);
+    }
+    else {
+	rot.setZ(-1.*mFrischGrid);
+    }
+//      PR(rot);
+
+    // magnet point
+    StThreeVectorD  mp = rotateToMagnet(rot);
+//      PR(mp);
+    
+    StThreeVectorD pt = mp + mTpcCenter;
+//      PR(pt);
+    StThreeVectorD transPt = this->transport(pt);
+//      PR(transPt);
+
+    transPt -= mTpcCenter;
+    StThreeVectorD  newPt = rotateToTpc(transPt);
+    // StThreeVectorD newPt(transPt);
+//     PR(newPt);
+
+    
+    Xprime[0] = newPt.x();
+    Xprime[1] = newPt.y();
+    Xprime[2] = newPt.z();
+    
+    
+//      UndoBDistortion        ( x, Xprime1 ) ;
+//     UndoPad13Distortion    ( Xprime1, Xprime2 ) ;
+//     UndoTwistDistortion    ( Xprime2, Xprime1 ) ;
+//     UndoClockDistortion    ( Xprime1, Xprime2 ) ;
+//     UndoMembraneDistortion ( Xprime2, Xprime ) ;
   
 }
 
@@ -199,6 +311,7 @@ void StMagUtilities::UndoDistortion( const Float_t x[3], Float_t Xprime[3] )
 void StMagUtilities::DoDistortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    printf("StMagUtilities::DoDistortion()\n");
 
   UndoDistortion ( x, Xprime ) ;
 
@@ -214,6 +327,7 @@ void StMagUtilities::DoDistortion( const Float_t x[3], Float_t Xprime[3] )
 void StMagUtilities::UndoBDistortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    printf("StMagUtilities::UndoBDistortion()\n");
 
   double   Const_1, Const_2 ;
   double   OmegaTau ;                                  // OmegaTau carries the sign opposite of B
@@ -261,6 +375,7 @@ void StMagUtilities::UndoBDistortion( const Float_t x[3], Float_t Xprime[3] )
 void StMagUtilities::UndoPad13Distortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    printf("StMagUtilities::UndoPad13Distortion()\n");
 
   const Int_t   TERMS    = 400 ;                 // Number of terms in the sum
   const Float_t SCALE    = 0.192 ;               // Set the scale for the correction
@@ -318,6 +433,7 @@ void StMagUtilities::UndoPad13Distortion( const Float_t x[3], Float_t Xprime[3] 
 void StMagUtilities::UndoTwistDistortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    printf("StMagUtilities::UndoTwistDistortion()\n");
 
   static Int_t    Flag = 0 ;                  // Do once
   static Float_t  Const_3, Const_4, OmegaTau ;
@@ -350,6 +466,7 @@ void StMagUtilities::UndoTwistDistortion( const Float_t x[3], Float_t Xprime[3] 
 void StMagUtilities::UndoClockDistortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    printf("StMagUtilities::UndoClockDistortion()\n");
 
   Double_t r, phi ;
 
@@ -372,6 +489,7 @@ void StMagUtilities::UndoClockDistortion( const Float_t x[3], Float_t Xprime[3] 
 void StMagUtilities::UndoMembraneDistortion( const Float_t x[3], Float_t Xprime[3] )
 
 {
+    printf("StMagUtilities::UndoMembraneDistortion()\n");
 
   Float_t  Er_integral, Ephi_integral ;
   Double_t r, phi, z ;
@@ -414,6 +532,7 @@ void StMagUtilities::UndoMembraneDistortion( const Float_t x[3], Float_t Xprime[
 void StMagUtilities::ReadField( )
 
 {
+    printf("StMagUtilities::ReadField()\n");
 
   FILE    *magfile, *efile ;
   TString comment, filename ;
@@ -458,7 +577,8 @@ void StMagUtilities::ReadField( )
       
   printf("Reading Magnetic Field:  %s,  Scale factor = %f \n",comment.Data(),gFactor);
   printf("Filename is %s, Adjusted Scale factor = %f \n",filename.Data(),gFactor*gRescale);
-  printf("Version: Mag Field Distortions + PadRow13 + Twist + Clock + Membrane \n" ) ;
+  //printf("Version: Mag Field Distortions + PadRow13 + Twist + Clock + Membrane \n" ) ;
+  printf("Version: Brian and Jon\n");
   MapLocation = BaseLocation + filename ;
   gSystem->ExpandPathName(MapLocation) ;
   magfile = fopen(MapLocation.Data(),"r") ;
@@ -529,7 +649,7 @@ void StMagUtilities::ReadField( )
     }
 
   fclose(efile) ;
-  
+  printf("READFIELD complete:\n");
 }
 
 
@@ -613,7 +733,6 @@ void StMagUtilities::InterpolateEdistortion( const Float_t r, const Float_t phi,
 Float_t StMagUtilities::QuadInterp( const Float_t Xarray[], const Float_t Yarray[], const Float_t x )
 
 {
-
   Float_t y ;
 
   y  = (x-Xarray[1]) * (x-Xarray[2]) * Yarray[0] / ( (Xarray[0]-Xarray[1]) * (Xarray[0]-Xarray[2]) ) ; 
@@ -685,14 +804,245 @@ Int_t StMagUtilities::Search( Int_t N, Float_t Xarray[], Float_t x )
        
 }
 
+StThreeVectorD StMagUtilities::transport(StThreeVectorD& pos) {
+
+    
+//     printf("::transport\n");
+//     PR(pos);
+    double step = -6*centimeter;
+    double h = step/mVdrift;
+//     PR(h/microsecond);
+
+    //StThreeVectorD p(pos);
+
+    double time = mDriftTime;
+//     PR(time/microsecond);
+
+    while (time > 0.-h) {
+	pos = rKIntegration(h,&time,pos);
+// 	mPathIntegral.push_back(pos);
+// 	PR(time/microsecond);
+ 	//PR(pos.z()/centimeter);
+    }
+//     cout << "Done time= " << (time/microsecond) << " us" << endl;
+
+    h /=4.;
+
+    StThreeVectorD rv;
+    do {
+	rv = pos;
+	pos = rKIntegration(h,&time,pos);
+    } while (time > 0.);
+
+    double hh = h - time;
+    time -= h;
+
+    rv = rKIntegration(hh,&time,rv);
+//     mPathIntegral.push_back(rv);
+
+//     PR(rv/centimeter);
+
+    return rv;
+}
+
+StThreeVectorD StMagUtilities::calculateDriftVelocity(StThreeVectorD r) {
+
+    //
+    // sign of electron charge (drifting electrons)
+    //
+    double q = -1.;
+
+    //
+    // wt = uB
+    //
+    
+    float bArray[3] = {0};
+    float rArray[] = {r.x(),r.y(),r.z()};
+    
+    this->BField(rArray,bArray);
+    StThreeVectorD B(bArray[0]*kilogauss,bArray[1]*kilogauss,bArray[2]*kilogauss);
+    
+//     PR(B/tesla);
+    
+    double omegaTau = mMobility*abs(B);
+//     PR(omegaTau);
+//     PR(mOmegaTau);
+
+    //
+    // velocity for positron transport
+    //
+    //         u |E|       ^           ^   ^              ^   ^  ^
+    // vd = ------------ ( E  + (wt) ( E x B)  +  (wt)^2 (E . B) B )
+    //       1 + (wt)^2
+    //
+
+    double constant = (mMobility*abs(mDriftField))/(1+sqr(omegaTau));
+//     PR(constant/(centimeter/microsecond));
+
+    //
+    // Get the field values
+    //
+    StThreeVectorD e;
+    StThreeVectorD b;
+
+    if(r.z() > 0) {
+	e = StThreeVectorD(mDriftField.unit());
+    }
+    else {
+	e = StThreeVectorD(-1.*mDriftField.unit());
+    }
+    
+    b = StThreeVectorD(B.unit());
+    //    PR(e);
+//     PR(b);
+    
+    StThreeVectorD
+	vDrift(e.x() - omegaTau*(e.y()*b.z()-e.z()*b.y()) + sqr(omegaTau)*(e*b)*b.x(),
+	       e.y() - omegaTau*(e.z()*b.x()-e.x()*b.z()) + sqr(omegaTau)*(e*b)*b.y(),
+	       e.z() - omegaTau*(e.x()*b.y()-e.y()*b.x()) + sqr(omegaTau)*(e*b)*b.z());
+
+    vDrift*=constant;
+//     PR(vDrift/(centimeter/microsecond));
+
+    return vDrift;
+}
 
 
 
+StThreeVectorD StMagUtilities::rKIntegration(double h, double* time, StThreeVectorD r) {
+
+//     int i;
+//     StThreeVectorD k1,k2,k3,k4;
+    StThreeVectorD p;
+
+    StThreeVectorD vDrift = this->calculateDriftVelocity(r);
+//     PR(vDrift/(centimeter/microsecond));
+
+    StThreeVectorD k1 = h*vDrift;
+//     PR(k1/centimeter);
+
+    p = r + (k1)/2.;
+//     PR(p/centimeter);
+
+    vDrift = this->calculateDriftVelocity(p);
+//     PR(vDrift/(centimeter/microsecond));
+    StThreeVectorD k2 = h*vDrift;
+//     PR(k2/centimeter);
+    
+    p = r - k1/2. + k2;
+//     PR(p/centimeter);
+
+    vDrift = this->calculateDriftVelocity(p);
+//     PR(vDrift/(centimeter/microsecond));
+    StThreeVectorD k3 = h*vDrift;
+//     PR(k3/centimeter);
+    
+    p =  r + k3;
+//     PR(p/centimeter);
+
+    vDrift = this->calculateDriftVelocity(p);
+//     PR(vDrift/(centimeter/microsecond));
+    StThreeVectorD k4 = h*vDrift;
+//     PR(k4/centimeter);
+    
+    p =  r + (k1 + 2.*k2 + 2.*k3 + k4)/6.;
+//     PR(p/centimeter);
+
+    *time = *time + h;
+//     PR(*time/microsecond);
+    return p;
+}
+
+//________________________________________
+
+StThreeVectorD StMagUtilities::clockRotation(StThreeVectorD pos)
+{
+    double phi = pos.phi();
+    if(pos.z() > 0) {
+	phi += mWestClockRotation;
+	pos.setPhi(phi);
+// 	cout << "PHI: " << (phi/degree) << "\tposition= " << pos << "\tnewphi: " << (pos.phi()/degree) << endl;
+    }
+    else {
+// 	cout << "nothing to do" << endl;
+    }
+    return pos;
+}
+
+//________________________________________
+
+// StThreeVectorD StMagUtilities::coordinateShift(StThreeVectorD pos)
+// {
+    //printf("StMagUtilities::coordinateShift\n");
+
+//     cout.precision(5);
+//     cout << "( " << (mCy*mCz) << '\t' << (mSx*mSy*mCz-mCx*mSz) << '\t' << (mCx*mSy*mCz+mSx*mSz) << "\t)" << endl;
+//     cout << "( " << (mCy*mSz) << '\t' << (mSx*mSy*mSz+mCx*mCz) << '\t' << (mCx*mSy*mSz-mSx*mCz) << "\t)" << endl;
+//     cout << "( " << (-1.*mSy) << '\t' << (mCy*mSx) << '\t' << (mCy*mCx) << "\t)" << endl;
 
 
+    
+//     double xPrime = mTpcCenter.x() +
+// 	pos.x()*mCy*mCz + pos.y()*(mSx*mSy*mCz-mCx*mSz) + pos.z()*(mCx*mSy*mCz+mSx*mSz);
+
+//     double yPrime = mTpcCenter.y() +
+// 	pos.x()*(mCy*mSz) + pos.y()*(mSx*mSy*mSz+mCx*mCz) + pos.z()*(mCx*mSy*mSz-mSx*mCz);
+
+//     double zPrime =  mTpcCenter.z() +
+// 	pos.x()*(-1.*mSy) + pos.y()*(mCy*mSx) +pos.z()*mCy*mCx;
+
+//     return StThreeVectorD(xPrime,yPrime,zPrime);
+    
+// }
+
+void StMagUtilities::calculateDriftTime(StThreeVectorD& pos) {
+
+    mDriftTime = (pos.z()>0) ?
+	((mFrischGrid - pos.z())/(mVdrift)) :
+	((pos.z() + mFrischGrid)/(mVdrift));
+
+//     PR(mDriftTime/microsecond);
+}
 
 
+StThreeVectorD StMagUtilities::rotateToMagnet(StThreeVectorD& pos) {
 
+//      cout << "StMagUtilities::ROTATE TO MAGNET" << endl;
 
+//     // used to be in shiftcoordinates
+//      cout.precision(5);
+//      cout << "( " << (mCy*mCz) << '\t' << (mSx*mSy*mCz-mCx*mSz) << '\t' << (mCx*mSy*mCz+mSx*mSz) << "\t)" << endl;
+//      cout << "( " << (mCy*mSz) << '\t' << (mSx*mSy*mSz+mCx*mCz) << '\t' << (mCx*mSy*mSz-mSx*mCz) << "\t)" << endl;
+//      cout << "( " << (-1.*mSy) << '\t' << (mCy*mSx) << '\t' << (mCy*mCx) << "\t)" << endl;
 
+    double xPrime =
+	pos.x()*mCy*mCz + pos.y()*(mSx*mSy*mCz-mCx*mSz) + pos.z()*(mCx*mSy*mCz+mSx*mSz);
 
+    double yPrime =
+	pos.x()*(mCy*mSz) + pos.y()*(mSx*mSy*mSz+mCx*mCz) + pos.z()*(mCx*mSy*mSz-mSx*mCz);
+
+    double zPrime =
+	pos.x()*(-1.*mSy) + pos.y()*(mCy*mSx) +pos.z()*mCy*mCx;
+
+    return StThreeVectorD(xPrime,yPrime,zPrime);
+}
+
+StThreeVectorD StMagUtilities::rotateToTpc(StThreeVectorD& pos) {
+
+//      cout << "StMagUtilities::ROTATE TO TPC" << endl;
+//      cout.precision(5);
+//      cout << "( " << (mCy*mCz) << '\t' << (mCy*mSz) << '\t' << (-1.*mSy) << "\t)" << endl;
+//      cout << "( " << (mSx*mSy*mCz-mCx*mSz) << '\t' << (mSx*mSy*mSz+mCx*mCz) << '\t' << (mCy*mSx) << "\t)" << endl;
+//      cout << "( " << (mCx*mSy*mCz+mSx*mSz) << '\t' << (mCx*mSy*mSz-mSx*mCz) << '\t' << (mCy*mCx) << "\t)" << endl;
+
+    double xPrime =
+	pos.x()*mCy*mCz + pos.y()*(mCy*mSz) + pos.z()*(-1.*mSy);
+
+    double yPrime = 
+	pos.x()*(mSx*mSy*mCz-mCx*mSz) + pos.y()*(mSx*mSy*mSz+mCx*mCz) + pos.z()*(mCy*mSx);
+
+    double zPrime =
+	pos.x()*(mCx*mSy*mCz+mSx*mSz) + pos.y()*(mCx*mSy*mSz-mSx*mCz) +pos.z()*mCy*mCx;
+
+    return StThreeVectorD(xPrime,yPrime,zPrime);
+}
