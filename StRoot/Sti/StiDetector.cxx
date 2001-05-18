cb0a3e30ee82d1686a20da4ca736183af73fbeb4
bnorman <>
2001-05-18 20:47:43 +0000

    *** empty log message ***

--- StRoot/Sti/StiDetector.cxx
+++ StRoot/Sti/StiDetector.cxx
@@ -2,12 +2,13 @@
 
 //STD
 #include <iostream>
+#include <string>
 //SCL
 #include "StGetConfigValue.hh"
 //Sti
 #include "StiDetector.h"
 
-StiDetector::StiDetector() : name(0)
+StiDetector::StiDetector()
 {
 }
 
@@ -24,10 +25,13 @@ void StiDetector::build(const char* buildfile)
     
     string materialName;
     StGetConfigValue(buildfile, "gas", materialName);
-    gas = StiMaterial::findMaterial(materialName.c_str());
+    if(materialName != "(null)"){
+      //TODO: set gas by name
+    }
     StGetConfigValue(buildfile, "material", materialName);
-    material = StiMaterial::findMaterial(materialName.c_str());
-
+    if(materialName != "(null)"){
+      //TODO: set gas by name
+    }
     int code;
     StGetConfigValue(buildfile, "shapeCode", code);
     shapeCode = static_cast<StiShapeCode>(code);
@@ -49,7 +53,38 @@ void StiDetector::build(const char* buildfile)
     StGetConfigValue(buildfile,"name",name);
 
     return;
-}
+}  // build()
+ 
+void StiDetector::write(const char *szFileName){
+  
+  ofstream os(szFileName);
+
+  os << "on:\t\t\t" << static_cast<int>(on) << endl;
+  os << "active:\t\t\t" << static_cast<int>(active) << endl;
+  os << "continuousMedium:\t" << static_cast<int>(continuousMedium) << endl;
+  os << "discreteScatterer:\t" << static_cast<int>(discreteScatterer) << endl;
+  
+  os << "gas:\t\t\t" << (gas ? gas->getName() : "(null)") << endl;
+  os << "material:\t\t" << (material ? material->getName() : "(null)") << endl;
+
+  os << "shapeCode:\t\t" << static_cast<int>(shapeCode) << endl;
+
+  // only store one representation of geometry
+  os << "centerRadius:\t\t" << centerRadius << endl;
+  os << "centerRefAngle:\t\t" << centerRefAngle << endl;
+  os << "orientationAngle:\t" << orientationAngle << endl;
+  os << "halfWidth:\t\t" << halfWidth << endl;
+
+  os << "activePosition:\t\t" << activePosition << endl;
+  os << "zCenter:\t\t" << zCenter << endl;
+  os << "halfDepth:\t\t" << halfDepth << endl;
+  os << "thickness:\t\t" << thickness << endl;
+
+  os << "sector:\t\t\t" << sector << endl;
+  os << "padrow:\t\t\t" << padrow << endl;
+  os << "name:\t\t\t" << name << endl;
+
+} // write()
 
 ostream& operator<<(ostream& os, const StiDetector& d)
 {
@@ -64,4 +99,4 @@ ostream& operator<<(ostream& os, const StiDetector& d)
 	    <<d.getActivePosition()<<" "<<d.getZCenter()<<" "
             <<d.getHalfDepth()<<" "<<d.getThickness()<<" "
             <<d.getSector()<<" "<<d.getPadrow()<<" "<<d.getName();
-}
+} // <<
