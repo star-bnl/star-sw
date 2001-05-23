1dedd218ed29db45807e48f58897eb38a8c8a2e4
lansdell <>
2001-05-23 00:17:58 +0000

    introduction of file

---
+++ StRoot/St_QA_Maker/QAhlist_logx.h
@@ -0,0 +1,16 @@
+// $Id: QAhlist_logx.h,v 2.1 2001/05/23 00:17:58 lansdell Exp $
+// $Log: QAhlist_logx.h,v $
+// Revision 2.1  2001/05/23 00:17:58  lansdell
+// introduction of file
+//
+//
+///////////////////////////////////////////////////////////////////////
+// Names of histograms to be plotted with log x
+///////////////////////////////////////////////////////////////////////
+// Note: Editing this file means that StAnalysisUtilities/StHistUtil
+// must be recompiled
+
+ "QaGtrkRT",
+ "QaGtrkRTS",
+ "QaPtrkRT",
+ "QaPtrkRTS"
