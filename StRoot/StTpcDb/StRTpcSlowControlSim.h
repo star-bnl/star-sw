6e9ed265dbb472f542b8a3b932707f976e3534e3
David Hardtke <hardtke@uclink4.berkeley.edu>
2001-06-20 22:25:26 +0000

    Get TRS gain parameters from tsspar table

--- StRoot/StTpcDb/StRTpcSlowControlSim.h
+++ StRoot/StTpcDb/StRTpcSlowControlSim.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StRTpcSlowControlSim.h,v 1.3 1999/12/16 22:00:53 hardtke Exp $
+ * $Id: StRTpcSlowControlSim.h,v 1.4 2001/06/20 22:25:26 hardtke Exp $
  *
  * Author:  David Hardtke
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StRTpcSlowControlSim.h,v $
+ * Revision 1.4  2001/06/20 22:25:26  hardtke
+ * Get TRS gain parameters from tsspar table
+ *
  * Revision 1.3  1999/12/16 22:00:53  hardtke
  * add CVS tags
  *
@@ -19,19 +22,24 @@
 //#include <TObject.h>
 #include "StTpcSlowControlSimI.h"
 #include "tables/St_tpcSlowControlSim_Table.h"
+#include "tables/St_tss_tsspar_Table.h"
 
 class StRTpcSlowControlSim : public StTpcSlowControlSimI {
 
 private:
 
   St_tpcSlowControlSim *mSC;
+  St_tss_tsspar *mTSS;
 
 public:
 
-  StRTpcSlowControlSim(St_tpcSlowControlSim* SCIn=0){AddData(SCIn);}
+  StRTpcSlowControlSim(St_tpcSlowControlSim* SCIn=0,St_tss_tsspar* TSSIn=0){AddData(SCIn);AddData(TSSIn);}
   ~StRTpcSlowControlSim(){}
   void AddData(St_tpcSlowControlSim* SCIn) {
       mSC = SCIn;
+   }
+  void AddData(St_tss_tsspar* TSSIn) {
+      mTSS = TSSIn;
    }
 
   //Implements Abstract Interface 
@@ -69,13 +77,13 @@ public:
    inline double StRTpcSlowControlSim::outerSectorGatingGridV()   const {
       return (*mSC)[0].outerSectorGatingGridV;}
    inline double StRTpcSlowControlSim::innerSectorGasGain()   const {
-      return (*mSC)[0].innerSectorGasGain;}
+      return (*mTSS)[0].gain_in;}
    inline double StRTpcSlowControlSim::innerSectorGasGainVzero()   const {
       return (*mSC)[0].innerSectorGasGainVzero;}
    inline double StRTpcSlowControlSim::innerSectorGasGainb()   const {
       return (*mSC)[0].innerSectorGasGainb;}
    inline double StRTpcSlowControlSim::outerSectorGasGain()   const {
-      return (*mSC)[0].outerSectorGasGain;}
+      return (*mTSS)[0].gain_out;}
    inline double StRTpcSlowControlSim::outerSectorGasGainVzero()   const {
       return (*mSC)[0].outerSectorGasGainVzero;}
    inline double StRTpcSlowControlSim::outerSectorGasGainb()   const {
