0fb339797c9eaa60e56e28f061f62aa4498ac32c
subhasis <>
2001-07-16 18:31:39 +0000

    EMC_Reader, initial version

--- StRoot/StDaqLib/EMC/EMC_Reader.hh
+++ StRoot/StDaqLib/EMC/EMC_Reader.hh
@@ -1,15 +1,8 @@
 /***************************************************************************
  *  
- * Author: M.J. LeVine
+ * Authors: Herbert Ward, Subhasis Chattopadhya, Mike Levine
  ***************************************************************************
- * Description: common definitions for EMC (dummy placeholder)
- *      
- *
- *   change log
- * 02-Jul-99 MJL add navigation code to get to EMCP bank
- * 08-Jul-99 MJL completely change definition - EMC_Reader is independent 
- *               class which is handed a pointer at the constructor invocation
- *
+ * Description: common definitions for EMC Bank and EMC_Reader
  ***************************************************************************
  *  
  *
@@ -18,50 +11,137 @@
 #define EMC_READER_HH
 #include "StDaqLib/GENERIC/EventReader.hh"
 #include "StDaqLib/GENERIC/RecHeaderFormats.hh"
+#include "StDaqLib/GENERIC/swaps.hh"
 
-// Detector Reader Virtual Class
 
-
-struct  Bank_EMCP: public Bank
+struct Bank_EMCP: public Bank
 {
-  Pointer dummy[6];   //**** PLACE HOLDER for real bank definition ******
-  // look at TPC/TPCV2P0.cxx, TPCV2P0.hh for further details
+  struct Pointer EMCSecPointer[6] ; /* BEMC (tower,prs,smd), EEMC */
+} ;
+
+struct Bank_EMCSECP: public Bank
+{
+  struct Pointer FiberPointer[8] ; /* No of fibere to daq from each subdetector, can be maximum 8 */
+} ;
+
+struct Bank_EMCRBP: public Bank
+{
+  struct Pointer EMCADCR;  /* RAW DATA */
+  struct Pointer EMCADCD ; /* one tower block*/
+  struct Pointer EMCPEDR; 
+  struct Pointer EMCRMSR ; /* one tower block*/
+} ;
+
+struct Bank_TOWERADCR: public Bank{
+  //        int bankHeader[10] ;    // 40 bytes of the DAQ bank header
+        int dummy ;             // 4 bytes of junk - should read as  0x00c0ffee
+        short fiberHeader[64] ; // 128 bytes of standard FEE header
+        short TDCHeader[120] ; // TDC header - error flag etc
+        short fiberData[4800] ; // stuff on the fiber - (Raw data)
 };
 
-class EMC_Reader
-{
-  friend class EventReader;
+
+
+struct BTOWERDATA{
+  char * BankType; // Will be filled with a 9 char array (8 letters + NULL)
+  unsigned int DetFlag; //DAQ Detector flag for BEMC, PRS, SMDE or SMDP
+  unsigned int EventNumber; //Token number
+  unsigned int PedFlag; //Pedestal subtracted or not (??)
+  unsigned int TDCErrorFlag; // Error from TDC
+  unsigned int ReceivedByteCount; //Total number of Bytes 
+  unsigned int NTowerHits; //Total number of channels
+  unsigned short TowerMatrix[120][20][2] ; // Matrix of ADC's in Physical Positions
+  unsigned short TowerADCArray[4800] ; // Matrix of ADC's as obtained from daq
+};
+
+// similar banks for ADCR, ADCD, PED and RMS
+
+struct Bank_BTOWERADCR: public BTOWERDATA{};
+struct Bank_BTOWERADCD: public BTOWERDATA{};
+struct Bank_BTOWERPEDR: public BTOWERDATA{};
+struct Bank_BTOWERRMSR: public BTOWERDATA{};
+
+
+
+struct Bank_SMDADCR: public Bank{  // We don't know yet, unless something
+                                   //is written for SMD
+  //        int bankHeader[10] ;    // 40 bytes of the DAQ bank header
+        int dummy ;             // 4 bytes of junk - should read as  0x00c0ffee
+        short fiberHeader[64] ; // 128 bytes of standard FEE header
+        short TDCHeader[120] ; // TDC header - error flag etc
+        short fiberData[4800] ; // stuff on the fiber - (Raw data)
+};
+
+
+struct BSMDDATA{
+  char * BankType; // Will be filled with a 9 char array (8 letters + NULL)
+  unsigned int DetFlag; //Daq detector flag
+  unsigned int EventNumber; //Token number
+  unsigned int PedFlag; //Pedestal subtracted or not (??)
+  unsigned int SMDErrorFlag; // Error from TDC
+  unsigned int ReceivedByteCount; //Total number of channels
+  unsigned int NSmdHits; //Total number of channels
+  unsigned int TimeBin[8]; // I do not know if 8 fibers will have different time bin
+  unsigned short SMDADCArray[8][4800] ; // Matrix of ADC's as obtained from daq
+  unsigned short SmdE_ADCMatrix[120][150] ; // Matrix of ADC's in Physical Positions for SMD_Eta
+  unsigned short SmdP_ADCMatrix[120][10][15] ; // Matrix of ADC's in Physical Positions for SMD_Phi
+};
+
+
+struct Bank_BSMDADCR: public BSMDDATA{};
+struct Bank_BSMDADCD: public BSMDDATA{};
+struct Bank_BSMDPEDR: public BSMDDATA{};
+struct Bank_BSMDRMSR: public BSMDDATA{};
+
+
+
+class EMC_Reader {
+ void ProcessEvent(const Bank_EMCP *EmcPTR);
 
 public:
   //  move the constructor guts {...} to a .cxx file
-  EMC_Reader(EventReader *er, Bank_EMCP *pEMCP){
-    pBankEMCP = pEMCP; //copy into class data member for use by other methods
-    ercpy = er; // squirrel away pointer eventreader for our friends
-    if (!pBankEMCP->test_CRC())  {
-    printf("CRC error in EMCP: %s %d\n",__FILE__,__LINE__) ;
-  }
-    if (pBankEMCP->swap() < 0) {
-    printf("swap error in EMCP: %s %d\n",__FILE__,__LINE__) ;
-  }
-    pBankEMCP->header.CRC = 0;
-    //do whatever else needs to be done
-  };
+  EMC_Reader(EventReader *er, Bank_EMCP *pEMCP);
+
+  Bank_BTOWERADCR& getBTOWERADCR();
+  int getTowerADC(int,int,int,unsigned short&);
+  int getTowerADC(int,unsigned short&);
+  int NTowerHits();
+  //  int getTowerADCR(unsigned short***);
+
+  Bank_BSMDADCR& getSMD_ADCR();
+
+  int getSMD_ADC(int,int,unsigned short&);
+  int getSMDE_ADC(int,int,unsigned short&);
+  int getSMDP_ADC(int,int,int,unsigned short&);
+  int getSMD_TIMEBIN(int,unsigned int&);
+  int NSmdHits();
+
+  //  int getTowerADCR(unsigned short***);
 
 
   ~EMC_Reader(){}; 
-
-
 protected:
 
   // copy of EventReader pointer
   EventReader *ercpy;
+  Bank_EMCP* pBankEMCP;
 
-  // Bank Pointers
-  Bank_DATAP *pBankDATAP;
-  Bank_EMCP *pBankEMCP;
+  // Data Banks
+  Bank_BTOWERADCR mTheTowerAdcR;
+  Bank_BTOWERADCD mTheTowerAdcD;
+  Bank_BTOWERPEDR mTheTowerPedR;
+  Bank_BTOWERRMSR mTheTowerRMSR;
+
+  Bank_BSMDADCR mTheSmdAdcR;
+  Bank_BSMDADCD mTheSmdAdcD;
+  Bank_BSMDPEDR mTheSmdPedR;
+  Bank_BSMDRMSR mTheSmdRMSR;
+  bool TowerPresent;
+  bool SmdPresent;
 
 };
 
 EMC_Reader *getEMCReader(EventReader *er);
+
 
 #endif
