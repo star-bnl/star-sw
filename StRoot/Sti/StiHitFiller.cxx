a9d3e40a58944a4b42daf131395cefc350bf4d78
mmiller <>
2001-08-27 13:14:15 +0000

    Change flag on used svt hits to flag()<4.

--- StRoot/Sti/StiHitFiller.cxx
+++ StRoot/Sti/StiHitFiller.cxx
@@ -43,16 +43,21 @@ void StiHitFiller::addDetector(StDetectorId det)
 
 void StiHitFiller::fillHits(StiHitContainer* store, StiHitFactory* factory)
 {
+    cout <<"StiHitFiller::fillHits()"<<endl;
     for (det_id_vector::const_iterator it=mvec.begin(); it!=mvec.end(); ++it) {
 	if ( *it == kTpcId) fillTpcHits(store, factory);
 	if ( *it == kSvtId) fillSvtHits(store, factory);
     }
     fillPrimaryVertices(store, factory);
+    cout <<"StiHitFiller::fillHits()\tDone Filling Hits"<<endl;
     return;
 }
 
 void StiHitFiller::fillTpcHits(StiHitContainer* store, StiHitFactory* factory)
 {
+    cout <<"StiHitFiller::fillTpcHits()"<<endl;
+    double nhit=0;
+    double nprint = 1000.;
     const StTpcHitCollection* tpcHits = mevent->tpcHitCollection();    
     //Loop over sectors
     for (int sector=1; sector<=24; sector++) {
@@ -74,6 +79,10 @@ void StiHitFiller::fillTpcHits(StiHitContainer* store, StiHitFactory* factory)
 
 		    //Now Fill the Hit Container!
 		    store->push_back( stihit );
+		    ++nhit;
+		    if (fmod(nhit, nprint)==0.) {
+			cout <<"Filling TpcHit:\t"<<nhit<<endl;
+		    }
 		}
 	    }
 	}	    
@@ -84,6 +93,43 @@ void StiHitFiller::fillTpcHits(StiHitContainer* store, StiHitFactory* factory)
 
 void StiHitFiller::fillSvtHits(StiHitContainer* store, StiHitFactory* factory)
 {
+    cout <<"StiHitFiller::fillSvtHits()"<<endl;
+    double nhit=0;
+    double nprint = 1000.;
+    StSvtHitCollection* svthits = mevent->svtHitCollection();
+    //loop on barrels
+    for (unsigned int barrel=0; barrel<svthits->numberOfBarrels(); ++barrel) {
+	StSvtBarrelHitCollection* barrelhits = svthits->barrel(barrel);
+	//Loop on ladders
+	for (unsigned int ladder=0; ladder<barrelhits->numberOfLadders(); ++ladder) {
+	    StSvtLadderHitCollection* ladderhits = barrelhits->ladder(ladder);
+	    //Loop on wafers
+	    for (unsigned int wafer=0; wafer<ladderhits->numberOfWafers(); ++wafer) {
+		//cout <<"Barrel "<<barrel<<"\tladder "<<ladder<<"\twafer "<<wafer<<endl;
+		StSvtWaferHitCollection* waferhits = ladderhits->wafer(wafer);
+		const StSPtrVecSvtHit& hits = waferhits->hits();  //Finally!
+		for (vector<StSvtHit*>::const_iterator it=hits.begin(); it!=hits.end(); ++it) {
+		    StSvtHit* hit = dynamic_cast<StSvtHit*>(*it);
+		    if (hit) {
+			if (hit->flag()<4) {
+			    //Now we've got the hit, fillerup!
+			    StiHit* stihit = factory->getObject();
+			    stihit->reset();
+			    
+			    mtranslator->operator()(hit, stihit);
+			    
+			    //Now Fill the Hit Container!
+			    store->push_back( stihit );
+			    ++nhit;
+			    if (fmod(nhit, nprint)==0.) {
+				cout <<"Filling TpcHit:\t"<<nhit<<endl;
+			    }
+			}
+		    }
+		}
+	    }
+	}
+    }
     return;
 }
 
