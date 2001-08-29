fbe35a0a435076ee52921fa35e45e7307bde3814
mmiller <>
2001-08-29 22:49:34 +0000

    Updated to reflect changes in Sti.

--- StRoot/StiMaker/StiEvaluator.h
+++ StRoot/StiMaker/StiEvaluator.h
@@ -7,6 +7,53 @@
 
 //forward declarations (must #include these in the source file)
 class StiTrackContainer;
+class TFile;
+class TNtuple;
+class TTree;
+
+
+//Temp class to be stored in TTree, eventually move to it's own .h, .cxx files
+#include "TObject.h"
+
+class TClonesArray;
+
+class ArrayEntry : public TObject
+{
+public:
+    ArrayEntry() {};
+    virtual ~ArrayEntry() {};
+
+    void setVal(double);
+    
+private:
+    double mval;
+    
+    ClassDef(ArrayEntry,1)
+};
+
+class TreeEntry
+{
+public:
+    TreeEntry();
+    virtual ~TreeEntry() {};
+
+    void setA(double val);
+    void setB(double val);
+
+    double a() const;
+    double b() const;
+
+    void clear();
+    void addArrayEntry(const ArrayEntry&);
+
+private:
+    double ma;
+    double mb;
+    TClonesArray* mArray;
+    int mCounter;
+    
+    ClassDef(TreeEntry,1) 
+};
 
 class StiEvaluator
 {
@@ -19,10 +66,20 @@ class StiEvaluator
     void evaluateForEvent(const StiTrackContainer*);
     
  private:
+    //singleton stuff
     StiEvaluator();
     virtual ~StiEvaluator();
-    
     static StiEvaluator* sinstance;
+
+private:
+    void build();
+    void fillTuple();
+    
+    TFile* mFile;
+    TNtuple* mNtuple;
+    TTree* mTree;
+    TreeEntry* mEntry;
+    
 };
 
 #endif
