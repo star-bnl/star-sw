371c83911ebed5e02f0cd46eb30da295e4a1e64f
mmiller <>
2001-08-30 18:26:26 +0000

    Modified to follow new Sti Factory typedef scheme.  Removed StiGuiTypedefs.h

--- StRoot/StiGui/StiGuiTypedefs.h
+++
@@ -1,8 +0,0 @@
-//StiGuiTypedefs.h
-//M.L. Miller (Yale Software)
-//07/01
-
-#include "Sti/StiObjectFactory.h"
-
-#include "StiRootDrawableStiEvaluableTrack.h"
-typedef StiObjectFactory<StiRootDrawableStiEvaluableTrack> StiRDEvaluableTrackFactory;
