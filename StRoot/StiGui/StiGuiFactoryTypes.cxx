0d0203804dd9a00d1d343539fbdd697281908f14
Michael Lee Miller <mlmiller@mit.edu>
2001-09-18 17:29:29 +0000

    Updated to synch with change of factory scheme.

---
+++ StRoot/StiGui/StiGuiFactoryTypes.cxx
@@ -0,0 +1,44 @@
+//StiGuiFactoryTypes.cxx
+//M.L. Miller (Yale Software)
+//09/01
+
+//StiGui
+#include "StiRootDrawableDetector.h"
+#include "StiRootDrawableStiEvaluableTrack.h"
+#include "StiGuiFactoryTypes.h"
+
+//StiRDEvaluableTrack Factory
+
+StiRDEvaluableTrackFactory::StiRDEvaluableTrackFactory(const string& newName,int original, int incremental, int maxInc)
+    : StiObjectFactoryInterface<StiKalmanTrack>(newName, original, incremental, maxInc)
+{
+    initialize();
+}
+
+StiRDEvaluableTrackFactory::~StiRDEvaluableTrackFactory()
+{
+}
+
+void* StiRDEvaluableTrackFactory::makeNewObject() const
+{
+    return new StiRootDrawableStiEvaluableTrack();
+}
+
+
+//StiRDDetector Factory
+
+StiRDDetectorFactory::StiRDDetectorFactory(const string& newName, int original, int incremental, int maxInc)
+    : StiObjectFactoryInterface<StiDetector>(newName, original, incremental, maxInc)
+{
+    initialize();
+}
+
+StiRDDetectorFactory::~StiRDDetectorFactory()
+{
+}
+
+void* StiRDDetectorFactory::makeNewObject() const
+{
+    return new StiRootDrawableDetector();
+}
+
