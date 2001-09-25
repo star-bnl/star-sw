eaaec84edfd20bd464ec28d9612cafb92832e25b
subhasis <>
2001-09-25 07:05:28 +0000

    modified after new data, decoding checked with online decoder

--- StRoot/StDaqLib/EMC/EMC_BarrelReader.cxx
+++ StRoot/StDaqLib/EMC/EMC_BarrelReader.cxx
@@ -9,10 +9,15 @@
 #include "EMC_BarrelReader.hh"
 #include <assert.h>
 #define MAX_ADC 0xFFF
+#include <fstream.h>
+
+//ofstream fout("decode.out");
 
 EMC_BarrelReader::EMC_BarrelReader(EventReader *er,Bank_EMCP *pEMCP):pBankEMCP(pEMCP),ercpy(er)
 {
   cout<<"ctor EMC_Barrelreader**"<<endl;
+  Initialize();
+
 }
 
 
@@ -54,7 +59,8 @@ int EMC_BarrelReader::getTowerBin(const int rid,int &m,int &e,int &s)
   m   = (rid - 1) / 40 + 1; // Module number
   idw = (rid - 1) % 40;     // change from 0 to 39
   s   = idw/20 + 1;
-  e   = 20 - idw%20;
+  e   = idw%20+1;
+//  e   = 20 - idw%20;
   return 1;                   // zero is bad
 }
 
@@ -204,12 +210,9 @@ return pADCR;
 
  //fiber header
  for(int i=0;i<64;i++){
-//  printf("Fiber header**: %x\n",pADCR->fiberHeader[i]);
-  //   cout<<"Fiberheader "<<i<<" "<<pADCR->fiberHeader[i]<<endl;
    //   cout<<"fiberheader "<<i<<" "<<pADCR->fiberHeader[i]<<endl;
  }
  for(int i=0;i<120;i++){
-//  printf("Fiber header**: %x\n",pADCR->TDCHeader[i]);
 //  cout<<"TDC header**:"<<pADCR->TDCHeader[i]<<endl;
  }
  //
@@ -223,25 +226,21 @@ return pADCR;
 
  //fiberdata
  for(int i1=0;i1<160;i1++){
-//   cout<<"***********ADC"<<i1<<"******"<<endl;
- for(int i=0;i<30;i++){
+  for(int i=0;i<30;i++){
    int index=i1*30+i;
     //Fill towerrawdata struct
    mTheTowerAdcR.TowerADCArray[index]=pADCR->fiberData[index];
-//         if(i<1)cout<<"index "<<index<<" data  "<<pADCR->fiberData[index]<<endl;
    if(pADCR->fiberData[index]>0)
     {mTheTowerAdcR.NTowerHits++;} //Total number of channels
-//   int index_jose=i*160+i1;
 //
-  int index_jose=-1;
+   int index_jose=-1;
    int stat_index=get_index_jose(index,index_jose);
-
-// Index-jose runs here from 0 to 4799
+// Index-jose runs here from 1 to 4800
 
    int m=0,e=0,s=0;
-//  int binstat=getTowerBin(index+1,m,e,s);
+ if(index_jose!=-1){
   if(stat_index){
-   int binstat=getTowerBin(index_jose+1,m,e,s);
+   int binstat=getTowerBin(index_jose,m,e,s);
    if(!binstat)
     {
     cout<<" problem in bin conversion "<<index<<endl;
@@ -249,10 +248,11 @@ return pADCR;
    else
    {
    mTheTowerAdcR.TowerMatrix[m-1][e-1][s-1]=pADCR->fiberData[index];
-//         if(i<5 && i1<10)cout<<"Channel no"<<i+1<<"m "<<m<<"e "<<e<<"s "<<s<<" data  "<<pADCR->fiberData[index]<<endl;
+// fout<<"index "<<index<<"m "<<m<<"e "<<e<<"s "<<s<<"ADC  "<<pADCR->fiberData[index]<<endl;
    }
    mTheTowerAdcR.TowerADCArray[index]=pADCR->fiberData[index];
    }//stat_index
+  }
 
   } 
  }
@@ -320,27 +320,32 @@ Bank_BTOWERADCR& EMC_BarrelReader::getBTOWERADCR()
 {
  
 int Init_Crate[15]={2180,2020,1860,1700,1540,1380,1220,1060,900,740,580,420,260,100,2340};
-int TDC2Crate[]= {3,2,1,15,14,0,0,0,0,0,0,0,0,0,0};
+//int TDC2Crate[]= {3,2,1,15,14,0,0,0,0,0,0,0,0,0,0};
+int TDC2Crate[]= {3,2,1,0,0,0,0,0,0,0,0,0,0,0,0};
  
 int crate_seq=0;
 int Crate=-1;
 if(daq_tower<0 || daq_tower>4799)return 0;
  
-int tdc=GetCrate(daq_tower,crate_seq);
-if(tdc<1 || tdc>15)return 0;
-
-if(tdc>0 && tdc<15)Crate=TDC2Crate[tdc-1];
+//int tdc=GetCrate(daq_tower,crate_seq);
+//if(tdc<1 || tdc>15)return 0;
+//if(tdc>0 && tdc<15)Crate=TDC2Crate[tdc-1];
  
-if(Crate<15 && Crate>=0){
-int start=Init_Crate[Crate];
-int index_jose=Getjose_tower(start,crate_seq);
+int tdc=GetCrate(daq_tower,crate_seq);
+if(tdc<0 || tdc>14)return 0;
+if(tdc>=0 && tdc<15)Crate=TDC2Crate[tdc];
+
+//if(Crate<15 && Crate>=0){
+if(Crate<=15 && Crate>0){
+int start=Init_Crate[Crate-1];
+index_jose=Getjose_tower(start,crate_seq);
+//fout<<"index "<<daq_tower<<"index_jose "<<index_jose<<"Crate "<<Crate+15<<"tdc "<<tdc<<endl;
 }
 else
 {
 return 0;
 }
  
-if(Crate==0)cout<<"daq_tower  "<<daq_tower<<"crate "<<Crate<<"jose_tower "<<index_jose<<endl;
 return 1;
 }
  
@@ -349,10 +354,11 @@ int EMC_BarrelReader::GetCrate(int daq_tower,int& crate_seq)
  
 {
  
-int Crate=daq_tower%30;
+int tdc=daq_tower%30;
+//int tdc=daq_tower%30+1;
 crate_seq=daq_tower/30;
 //if(Crate>14)Crate-=15;
-return Crate;
+return tdc;
  
 }
  
@@ -362,10 +368,9 @@ int EMC_BarrelReader::Getjose_tower(int start,int& crate_seq)
  
 int card=crate_seq/32;
 int card_seq=31-(crate_seq%32);
-if(start==2181)cout<<"card  "<<card<<"card_seq  "<<card_seq<<endl;
 int channel_seq=card_seq/4;
 int channel=card_seq-(channel_seq*4)+1;
-int jose_tower=start+channel_seq*20+card*4+channel;
+int jose_tower=start+channel_seq*20+card*4+(5-channel);
 if(jose_tower>2400)jose_tower-=2400;
 return jose_tower;
 }                                                                                              
