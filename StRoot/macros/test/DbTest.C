227b0c5148869dea439abe0ff6ab52d68f9b5093
perev <>
2001-09-27 00:48:33 +0000

    Flavor test added

--- StRoot/macros/test/DbTest.C
+++ StRoot/macros/test/DbTest.C
@@ -4,31 +4,41 @@ void DbTest(){
   gSystem->Load("St_base");
   gSystem->Load("StChain");
   gSystem->Load("StUtilities");
+  gSystem->Load("St_Tables");
 
   // DB-specific libs
+
   gSystem->Load("libStDb_Tables.so");
   gSystem->Load("StDbLib");
   gSystem->Load("StDbBroker"); 
   gSystem->Load("St_db_Maker");
 
-  // create makers connecting to Star databases 
+  // create makers connecting to databases RunParams & Geometry
 
-  St_db_Maker *dbMk = new St_db_Maker("StarDb","MySQL:StarDb");
-
+  St_db_Maker *dbMk = new St_db_Maker("db","MySQL:StarDb","StarDb","$STAR/StarDb");
+  dbMk->SetFlavor("sim","tpcEffectiveGeom");
+ 
   dbMk->Init();
 
   // Make reaquests for data
+
   // choose timestamp 
-  dbMk->SetDateTime(20001001,10000);
+  dbMk->SetDateTime(20010501,10000);
 
   TDataSet *p = dbMk->GetDataBase("Calibrations/tpc");
 //  p->ls(99);
-  TTable *tb = p->Find("tpc/tpcDriftVelocity");
+  TTable *tb = p->Find("tpc/tpcEffectiveGeom");
   printf("NRows=%d\n",tb->GetNRows());
   tb->Print(0,1);  
 
-  TBrowser* b = new TBrowser("TestBrowser",p);
 
+  // choose timestamp 
+  dbMk->SetDateTime(20010501,10000);
+
+
+
+
+ TBrowser* b = new TBrowser("TestBrowser",p);
 }
 
 
