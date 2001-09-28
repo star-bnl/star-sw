f34e0039e2cda4a6de1fe7e1e92c67be1cae362e
llope <>
2001-09-28 18:45:43 +0000

    modified for compatibility w/ updated StTofMaker

--- StRoot/StDaqLib/TOF/TOF_Reader.cxx
+++ StRoot/StDaqLib/TOF/TOF_Reader.cxx
@@ -1,11 +1,15 @@
 /***************************************************************************
-* $Id: TOF_Reader.cxx,v 1.1 2001/07/08 21:41:58 geurts Exp $
+* $Id: TOF_Reader.cxx,v 1.2 2001/09/28 18:45:43 llope Exp $
 * Author: Frank Geurts
 ***************************************************************************
 * Description:  TOF Event Reader
 ***************************************************************************
 * $Log: TOF_Reader.cxx,v $
+* Revision 1.2  2001/09/28 18:45:43  llope
+* modified for compatibility w/ updated StTofMaker
+*
 * Revision 1.1  2001/07/08 21:41:58  geurts
+* Revision 1.2  2001/09/28 13:13:13  llope
 * First release
 *
 **************************************************************************/
@@ -79,7 +83,7 @@ void TOF_Reader::ProcessEvent(const Bank_TOFP * TofPTR) {
     }    
   }
 
-  //Read A2D Bank
+  //Read A2D Bank...
   TOFA2DD * TofA2dD;
   if (TofPTR->A2dPTR.length>0) {
     TofA2dD = (TOFA2DD *) ((unsigned long *)TofPTR + TofPTR->A2dPTR.offset);
@@ -174,21 +178,38 @@ unsigned short TOF_Reader::GetTdcFromSlat(int slatId){
   return mTheTofArray.TdcData[slatId];
 }
 
+short TOF_Reader::GetTc(int chanId){
+  if ((chanId<0) || (chanId>TOFP_NUM_A2D_CHANNELS-1)){
+    cout << "TOF_Reader::GetTc chanId out of range " << chanId << endl;
+    return 0;
+  }
+  return mTheTofArray.A2dData[chanId];
+}
+
+
+unsigned short TOF_Reader::GetSc(int chanId){
+  if ((chanId<0) || (chanId>TOFP_NUM_SCA_CHANNELS-1)){
+    cout << "TOF_Reader::GetSc chanId out of range " << chanId << endl;
+    return 0;
+  }
+  return mTheTofArray.ScaData[chanId];
+}
+
 
 unsigned int TOF_Reader::GetEventNumber(){
   return mTheTofArray.EventNumber;
 }
 
-
 void TOF_Reader::printRawData(){
-  cout << "TOF_Reader::printRawData";
-  cout << "\nADCD: ";
-  for (int i=0;i<TOFP_NUM_ADC_CHANNELS;i++) cout << "  " << mTheTofArray.AdcData[i];
-  cout << "\nTDCD: ";
-  for (int i=0;i<TOFP_NUM_TDC_CHANNELS;i++) cout << "  " << mTheTofArray.TdcData[i];
-  cout << "\nA2DD: ";
-  for (int i=0;i<TOFP_NUM_A2D_CHANNELS;i++) cout << "  " << mTheTofArray.A2dData[i];
-  cout << "\nSCAD: ";
-  for (int i=0;i<TOFP_NUM_SCA_CHANNELS;i++) cout << "  " << mTheTofArray.ScaData[i];
-  cout << "\nTOF_Reader::printAllData done."<<endl;
+  cout << "StDaqLib/TOF/TOF_Reader  Printing Raw Data...";
+  cout << "\n --- ADCD: ";
+  for (int i=0;i<TOFP_NUM_ADC_CHANNELS;i++) cout << " " << mTheTofArray.AdcData[i];
+  cout << "\n --- TDCD: ";
+  for (int i=0;i<TOFP_NUM_TDC_CHANNELS;i++) cout << " " << mTheTofArray.TdcData[i];
+  cout << "\n --- A2DD: ";
+  for (int i=0;i<TOFP_NUM_A2D_CHANNELS;i++) cout << " " << mTheTofArray.A2dData[i];
+  cout << "\n --- SCAD: ";
+  for (int i=0;i<TOFP_NUM_SCA_CHANNELS;i++) cout << " " << mTheTofArray.ScaData[i];
+  cout << "\nStDaqLib/TOF/TOF_Reader  Done Printing Raw Data..." << endl;
 }
+
