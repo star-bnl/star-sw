221104742ffc89eb292cad3dbdac33066e1ab30f
munhoz <>
2001-10-24 16:47:52 +0000

    adding RMS methods

--- StRoot/StSvtCalibMaker/StSvtPedMaker.h
+++ StRoot/StSvtCalibMaker/StSvtPedMaker.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StSvtPedMaker.h,v 1.2 2000/11/30 20:32:03 caines Exp $
+ * $Id: StSvtPedMaker.h,v 1.3 2001/10/24 16:47:52 munhoz Exp $
  *
  * Author: Marcelo Munhoz
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StSvtPedMaker.h,v $
+ * Revision 1.3  2001/10/24 16:47:52  munhoz
+ * adding RMS methods
+ *
  * Revision 1.2  2000/11/30 20:32:03  caines
  * Use MessMgr
  *
@@ -32,6 +35,7 @@ class TObjectSet;
 class StSvtHybridStat;
 class StSvtHybridStat2;
 class StSvtHybridPed;
+class StSvtHybridPixels;
 class StSvtHybridPixels2;
 class StSvtHybridCollection;
 class StSvtHybridData;
@@ -43,15 +47,18 @@ class StSvtPedMaker : public StMaker {
   StSvtHybridStat2       *fStat2;    //!
   StSvtHybridPed         *fPed;      //!
   StSvtHybridPixels2     *fPed2;     //!
+  StSvtHybridPixels      *fPedRms;   //!
   StSvtHybridData        *fData;     //!
   StSvtHybridCollection  *fSvtStat;  //!
   StSvtHybridCollection  *fSvtStat2;  //!
   StSvtHybridCollection  *fSvtPed;   //!
   StSvtHybridCollection  *fSvtPed2;   //!
+  StSvtHybridCollection  *fSvtPedRms;//!
   StSvtData              *fSvtData;  //!
 
   TObjectSet           *fPedSet;   //! 
   TObjectSet           *fPedSet2;   //! 
+  TObjectSet           *fPedRmsSet;     //! 
 
   pedestalType fType;  //!                 
 
@@ -62,16 +69,21 @@ class StSvtPedMaker : public StMaker {
   virtual Int_t  Init();
   Int_t  SetType(pedestalType option);
   pedestalType  GetType(){return fType;}
+
   virtual Int_t  SetSvtData();
   virtual Int_t  SetSvtPed();
   virtual Int_t  SetSvtPed2ndOrd();
+  virtual Int_t  SetSvtRMSPed();
+  
   virtual Int_t  AddStat();
   virtual Int_t  AddStat2ndOrd();
   virtual Int_t  Make();
   virtual Int_t  CalcPed();
   virtual Int_t  CalcPed2ndOrd();
   virtual Int_t  WriteToFile(const char* fileName = "svtPedestal.root", char* option = "NEW");
+  virtual Int_t  WriteRMSToFile(const char* fileName = "svtPedestal.root", char* option = "NEW");
   virtual Int_t  ReadFromFile(const char* fileName = "svtPedestal.root");
+  virtual Int_t  ReadRMSFromFile(const char* fileName = "svtPedestal.root");
   virtual Int_t  ResetStat();
   virtual Int_t  ResetPed();
   virtual Int_t  Finish();
