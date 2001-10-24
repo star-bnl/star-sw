a5f39a636a9083b9bb172d3db37122264a014fbc
Benjamin Norman <bnorman>
2001-10-24 15:49:03 +0000

    Added Messenger::getMessageName

--- StRoot/Sti/Messenger.h
+++ StRoot/Sti/Messenger.h
@@ -9,6 +9,7 @@
 #include <iostream.h>
 #include <map>
 #include <fstream.h>
+#include <string>
 
 #include "MessengerBuf.h"
 
@@ -27,6 +28,9 @@ static const int g_nMessageTypes = 5;
 /// Output streams corresponding to the message types
 static ostream *g_apMessageOstreams[g_nMessageTypes] = 
 { &cout, &cout, &cout, &cout, &cout};
+/// Human readable names for message types
+static const char *g_apMessageNames[g_nMessageTypes] =
+{"Hit", "Track", "Node", "Detector", "Geometry"};
 
 /// Typedefs for containers
 typedef map<unsigned int, Messenger*> messengerMap;
@@ -73,6 +77,16 @@ public:
     }
     /// Returns the number of message types
     static int nMessageTypes(){ return g_nMessageTypes; }
+
+    /// Returns the human-readable name for the message type
+    static string getMessageName(unsigned int message){ 
+      int iMessageType = 0;
+      while( (message /= 2) > 0 ){ iMessageType++; }
+      if(iMessageType<g_nMessageTypes){
+        return string(g_apMessageNames[iMessageType]);
+      }
+      return string();
+    }
 
     /// Initialize the output streams for the message maps.  This must be
     /// called before using a Messenger.  If a routing code is specified,
