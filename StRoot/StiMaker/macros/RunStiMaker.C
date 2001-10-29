3f0abccf2888966b16730d5370c7a7c43fb5b29e
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2001-10-29 21:00:21 +0000

    macro update

--- StRoot/StiMaker/macros/RunStiMaker.C
+++ StRoot/StiMaker/macros/RunStiMaker.C
@@ -6,25 +6,25 @@ class StChain;
 StChain *chain=0;
 
 void RunStiMaker(Int_t nevents=1,
-		 bool simulated=true,
-		 
-		 //bool doFit=true,
-		 bool doFit=false,
-		 
-		 const char* outfile = "Evaluation.root",
-		 
-		 //This file points to 30 events of 10 neg muons w/ pt=.9
-		 const char* MainFile="/star/data17/ITTF/data/simple_geant/DEV_10_8_01/*.event.root")
-    
-    //This file points to 5 muons /event
-    //const char* MainFile="/star/data17/ITTF/EvalData/MCNtuple/muon_100_neg.event.root")
-    
-    //This file points to 110 events from mevsim (homebrew had. cocktail)
-    //const char* MainFile="/star/data17/ITTF/data/mevsim/10_9_01/*.event.root")
-    
-    //This file points to a nightly low density hadronic cocktail reconstruction.
-    //const char* MainFile="/star/rcf/test/dev/trs_redhat61/Tue/year_2001/hc_lowdensity/*.event.root")
-
+								 bool simulated=true, /*!sim or data?*/
+								 //bool doFit=true, /*! true->fit track only */
+								 bool doFit=false, /*! false->find track only */
+								 const char* outfile = "Evaluation.root",
+								 //This file points to 30 events of 10 neg muons w/ pt=.9
+								 const char* MainFile="/star/data17/ITTF/data/simple_geant/DEV_10_8_01/muon_10_neg.event.root")
+	//const char* MainFile="/star/data17/ITTF/data/simple_geant/DEV_10_8_01/*.event.root")
+	//const char* MainFile="/star/data17/ITTF/EvalData/MCNtuple/muon_100_neg.event.root")
+	//This file points to 110 events from mevsim (homebrew had. cocktail)
+	//const char* MainFile="/star/data17/ITTF/data/mevsim/10_9_01/*.event.root")
+	//This file points to a nightly low density hadronic cocktail reconstruction.
+	//const char* MainFile="/star/rcf/test/dev/trs_redhat61/Tue/year_2001/hc_lowdensity/*.event.root")
+	//This file points to 5 muons /event
+	//const char* MainFile="/star/data17/ITTF/EvalData/MCNtuple/muon_100_neg.event.root")
+	//This file points to 110 events from mevsim (homebrew had. cocktail)
+	//const char* MainFile="/star/data17/ITTF/data/mevsim/10_9_01/*.event.root")
+	//This file points to a nightly low density hadronic cocktail reconstruction.
+	//const char* MainFile="/star/rcf/test/dev/trs_redhat61/Tue/year_2001/hc_lowdensity/*.event.root")
+	
 {    
     // Dynamically link needed shared libs
     cout <<"Loading St_base"<<endl;
