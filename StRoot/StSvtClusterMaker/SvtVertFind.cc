84cdac962256203ec3dd25abac8b4fda9ecbcc52
caines <>
2001-11-06 17:01:30 +0000

    Added Juns improvements to vertex finder

--- StRoot/StSvtClusterMaker/SvtVertFind.cc
+++ StRoot/StSvtClusterMaker/SvtVertFind.cc
@@ -17,6 +17,7 @@
 #include "StVertexId.h"
 #include "tables/St_dst_vertex_Table.h"
 #include "tables/St_scs_spt_Table.h"
+#include "TH1.h"
 
 
 
@@ -27,40 +28,44 @@ int             *OccupiedPixels[SPT_PTR_NMAX];
 long             spt_n;
 
 double           r_ref;
-
-double           x_scale, 
-		 z_scale;
-long     	 x_size, 
-                 z_size, 
-                 iptr;
-double   x_min,
-		 x_max,
-		 z_min,
-		 z_max;
+double           x_scale, z_scale;
+long     	 x_size, z_size, iptr;
+double           x_min, x_max, z_min, z_max;
+int              icall = 1;
+TH1F             *mtemp;
 
 void sft_init(void);
-int sft_process_event ( long NSpt, scs_spt_st *scs_spt, dst_vertex_st *sft_vertex, long Nvtx);
+float sft_process_event ( long NSpt, scs_spt_st *scs_spt, dst_vertex_st *sft_vertex, long Nvtx);
 float sft_vf_ntrack(double z0);
 float sft_find_vertex();
 int bracket2 ( float (*)(double), float    fStart, float    fStep,
 	       float   *ax, float   *bx, float   *cx, float   *fa,
 	       float   *fb, float   *fc);
+float x0tpc=0;
+float y0tpc=0;
 
-long sft_main(  St_scs_spt  *scs_spt, St_dst_vertex *svt_vertex)
+float sft_main(  St_scs_spt  *scs_spt, St_dst_vertex *svt_vertex, float x0,float y0, TH1F *hist)
 {
    sft_init();
 
-  scs_spt_st *s_spt = scs_spt->GetTable();  
-  long NSpt = scs_spt->GetNRows();
-  dst_vertex_st *sft_vertex = svt_vertex->GetTable();
-  long Nvtx = svt_vertex->GetNRows();
-  long ok =sft_process_event ( NSpt, s_spt, sft_vertex, Nvtx);
-  if( ok == 0){
-    svt_vertex->SetNRows(++Nvtx);
-    return 1;
-  }
-  else
-    return 0;
+   x0tpc=x0;
+   y0tpc=y0;
+   
+   gMessMgr->Info() << "TPC X at = " << x0tpc << endm;
+   gMessMgr->Info() << "TPC Y at = " << y0tpc << endm;
+   mtemp=hist;
+   scs_spt_st *s_spt = scs_spt->GetTable();  
+   long NSpt = scs_spt->GetNRows();
+   dst_vertex_st *sft_vertex = svt_vertex->GetTable();
+   long Nvtx = svt_vertex->GetNRows();
+   float zsvt = sft_process_event ( NSpt, s_spt, sft_vertex, Nvtx);
+   
+   if( zsvt != -999){
+     svt_vertex->SetNRows(++Nvtx);
+     return zsvt;
+   }
+   else
+     return -999;
 }
 
 
@@ -70,10 +75,12 @@ long sft_main(  St_scs_spt  *scs_spt, St_dst_vertex *svt_vertex)
  *  calculate average difference and rms
  */
 
-int sft_process_event ( long Nspt,  scs_spt_st    *staf_spt,
+float sft_process_event ( long Nspt,  scs_spt_st    *staf_spt,
                    dst_vertex_st *vertex, long n_vtx)
 {    
  
+   float my_ntracks=0;
+   float zsvt=-999;
    long     ispt,
             jspt;
  
@@ -82,17 +89,15 @@ int sft_process_event ( long Nspt,  scs_spt_st    *staf_spt,
       return -1;
  
    /*  Start by grabbing the contents of the scs_spt
-    *  array and copying them into our local array. 
-    */
+    *  array and copying them into our local array.     */
 
    ispt = 0;
    r_ref = 11.; 
  
    for (jspt = 0; jspt < Nspt; jspt++)
    {
-     if (staf_spt[jspt].id_wafer < 7000 && staf_spt[jspt].flag<4 &&
-	 staf_spt[jspt].de[1] > 15) {
-
+     if (staf_spt[jspt].id_wafer < 7000 && staf_spt[jspt].flag<2) 
+       {
 	 spt[ispt].x[0]  = (double)staf_spt[jspt].x[0];
 	 spt[ispt].x[1]  = (double)staf_spt[jspt].x[1];
 	 spt[ispt].x[2]  = (double)staf_spt[jspt].x[2];
@@ -104,15 +109,16 @@ int sft_process_event ( long Nspt,  scs_spt_st    *staf_spt,
 
    if( spt_n < 4) return -1;
 
-   vertex[n_vtx].z = sft_find_vertex ();
-   vertex[n_vtx].x = 0.;
+   zsvt= sft_find_vertex ();
+   vertex[n_vtx].z = zsvt;
+   vertex[n_vtx].x = -sft_vf_ntrack(zsvt); 
    vertex[n_vtx].y = 0.;
    vertex[n_vtx].n_daughters =0;
    vertex[n_vtx].id_aux_ent = 0;
    vertex[n_vtx].chisq[0] = 0.;
    vertex[n_vtx].chisq[1] = 0.;
-   vertex[n_vtx].vtx_id = kEventVtxId ;
-   vertex[n_vtx].det_id = kSvtId;
+   vertex[n_vtx].vtx_id = kEventVtxId;
+   vertex[n_vtx].det_id = kSvtId; 
    vertex[n_vtx].iflag = 201;
    vertex[n_vtx].id = n_vtx+1;
    vertex[n_vtx].covar[0] = 0.;
@@ -125,60 +131,40 @@ int sft_process_event ( long Nspt,  scs_spt_st    *staf_spt,
    gMessMgr->Info() << "SVT vertex found at z= " <<  vertex[n_vtx].z << endm;
    
 
-  return 0;
+  return zsvt;
 
 }
 
 
-int icall = 1;
 
 /* calculate the number of straight lines  */
 /* compatible with the vertex position z0   */
-
-float
-sft_vf_ntrack(double z0)
+float sft_vf_ntrack(double z0)
 {
-   double     xc,
-              zc, 
-              zc0, 
-              x,
-              y,
-              z,
-              r,
-              rx,
-              rz;
-   int        ix,
-              iz,
-              ii,
-              ntrack,
-              ispt;
-
-
+   double     xc, zc, zc0, x, y, z, r, rx, rz;
+   int        ix, iz, ii, ntrack, ispt;
 
    if (icall == 1)
    {
-      icall = 0;
+      icall = 0; 
  
       /* build or update the list in feature space */
       /* loop on all space points                  */
- 
-
       for (ispt = 0; ispt < spt_n; ispt++)
       {
-         x   = spt[ispt].x[0]+0.27;
-         y   = spt[ispt].x[1]-0.5;
+         x   = spt[ispt].x[0]-x0tpc;
+         y   = spt[ispt].x[1]-y0tpc;
          z   = spt[ispt].x[2];
          r   = sqrt (x * x + y * y);          /*xc = fatan2(y,x)   */
-
-         rz  = r_ref / r;
+         rz  = r_ref / r;                     /* r_ref = 11. */
          /*rx  = x_scale / r;
 	   xc  = x * rx;
 	   Changed by Helen */
 	 xc  = atan2(y,x); 
-         zc0 = z * rz;                        /* the constant part */
+         zc0 = z * rz;                               /* the constant part */
          zc  = (zc0 + z0 - z0 * rz);                 /* the variable part */
-         iz  = (int)((zc - z_min)*z_scale); /* Split into mm bins */
-         ix  = (int)((xc - x_min)*x_scale); /* Split into 3.6 degree bins */
+         iz  = (int)((zc - z_min)*z_scale);          /* Split into mm bins Z-scale=140, zmin=-70*/
+         ix  = (int)((xc - x_min)*x_scale);          /* Split into 3.6 degree bins x-scale=6.6*/
          spt[ispt].off = ix*z_size; 
          ii = spt[ispt].off + iz;
 
@@ -188,7 +174,7 @@ sft_vf_ntrack(double z0)
 	      continue;
 	 }
 
-	 spt[ispt].rx  = rx;
+	 spt[ispt].rx  = r;
          spt[ispt].rz  = rz;
          spt[ispt].xc  = xc;
          spt[ispt].zc0 = zc0;
@@ -218,8 +204,8 @@ sft_vf_ntrack(double z0)
          {
              printf("\n error: out of bounds ii=%d",ii); 
          }
-      }
-   }
+      } /* for loop */
+   }    /* if icall ==1 loop */
    else
    {
       for (ispt = 0; ispt < spt_n; ispt++)
@@ -242,32 +228,36 @@ sft_vf_ntrack(double z0)
          /* ii     is a pixel index                               */
          /* ilist  is the position of the pixel index in the list */
 
-         if (ii >= 0 && ii < PIX_NMAX)
-         {
-            PixelOcc[ii]++;
-
-            if (PixelOcc[ii] == 1)
-            {
-               OccupiedPixels[iNumOccupiedPixels] = PixelOcc + ii;
-               iNumOccupiedPixels++;
-	       if( iNumOccupiedPixels >= SPT_PTR_NMAX) {
-		 printf("\n error: iNumOccupiedPixels =%d",iNumOccupiedPixels);
-		 ispt=spt_n;
-	       }
-	    }
+         if (ii >= 0 && ii < PIX_NMAX){
+	   PixelOcc[ii]++;
+	   if (PixelOcc[ii] == 1){
+	     OccupiedPixels[iNumOccupiedPixels] = PixelOcc + ii;
+	     iNumOccupiedPixels++;
+	     if( iNumOccupiedPixels >= SPT_PTR_NMAX) {
+	       printf("\n error: iNumOccupiedPixels =%d",iNumOccupiedPixels);
+	       ispt=spt_n;
+	     }
+	   }
          }
          else
          {
             printf("\n error: out of bounds ii=%d",ii);
          }
       }
-   }
+   } /* if icall != 1 loop */
+
+
 
    /* count the number of groups */
-
+   /* group = tracks             */ 
+   /*                            */
    ntrack = 0;
    for (iptr = 0; iptr < iNumOccupiedPixels; iptr++)
    {
+     /* definition of track */
+     if (*(OccupiedPixels[iptr]) > 1){
+       ntrack++;
+     }
      if (*(OccupiedPixels[iptr]) > 2){
        ntrack++;
      }
@@ -283,8 +273,8 @@ sft_vf_ntrack(double z0)
 void sft_init (void)
 {
 
-   x_size  = SPT_X;
-   z_size  = SPT_Z;
+  x_size  = SPT_X;   /* 60 */
+  z_size  = SPT_Z;  /* 1400 */
    x_min   = -3.3;
    z_min   = -70.;
    x_max   =  3.3;
@@ -307,32 +297,29 @@ void sft_init (void)
 float sft_find_vertex ()
 {
 
-   int           iNumPoints;
-   float ax, bx, cx, fa, fb, fc;
-   float step=1.;
-   double d, p, q, r;
+   int     iNumPoints;
+   float   ax, bx, cx, fa, fb, fc;
+   float   step;
+   double  d, p, q, r;
 
    icall = 1;
 
-   d = 0.0;
-
    /* Home in on primary vertex */
-   // No longer do first scane as noise makes a bias towards z=0 and
-   // were often missing the high z vertices.
-   // So for now just dumbly scan from -50 to 50 , hey it works
-
+   d = 0.0;
+   //   step=1.;
    //iNumPoints = bracket2 (sft_vf_ntrack, (float)d, step, &ax, &bx, &cx, &fa, &fb, &fc);
 
+   //r = (double)((bx - cx) * (fb - fa));
+   //q = (double)((bx - ax) * (fb - fc));
+   //p = (double)((bx - ax)*q - (bx - cx)*r);
+   //q = 2.0 * (q - r);
 
-
-     step=.1;
-     //iNumPoints = bracket2 (sft_vf_ntrack, bx, step, &ax, &bx, &cx, &fa, &fb, &fc);
-   /* printf ("point a:   % 12.5f    % 12.5f\n", ax, fa);
-      printf ("point b:   % 12.5f    % 12.5f\n", bx, fb);
+   step=.1;
+   iNumPoints = bracket2 (sft_vf_ntrack, (float)d, step, &ax, &bx, &cx, &fa, &fb, &fc);
+   /* printf ("point a: z= % 12.5f with % 12.5f tracks \n", ax, fa);
+      printf ("point b: z= % 12.5f with % 12.5f tracks \n", bx, fb);
       printf ("point c:   % 12.5f    % 12.5f\n", cx, fc);  */
 
-     iNumPoints = bracket2 (sft_vf_ntrack, (float)d, step, &ax, &bx, &cx, &fa, &fb, &fc);
-   
    /* Not sure how this works!*/
    r = (double)((bx - cx) * (fb - fa));
    q = (double)((bx - ax) * (fb - fc));
@@ -345,19 +332,20 @@ float sft_find_vertex ()
       q = -q;
 
    if( q !=0) {
-     
      d = (double)bx + p / q; 
    }
    else{
      d = -999;
    }
+
+   if (fb > -15) {d=-888;}
      
    return (float)d;
 }
 
 
-int
-bracket2 ( float (*func)(double),
+
+int bracket2 ( float (*func)(double),
           float    fStart,
           float    fStep,
           float   *ax,
@@ -367,98 +355,84 @@ bracket2 ( float (*func)(double),
           float   *fb,
           float   *fc)
 {
-   int     iNumPoints = 0,
-           iDone,
-           iStep,
-           iMaxPos,
-           iLeftPos,
-           iRightPos;
-   float   fX,
-           fVal,
-           fMaxVal = 0.0,
-           fMaxPos;
-   POINT   pPoints[1000];
-   int     iMaxPoints = 1000;
+  int     iNumPoints = 0, iDone, iStep, iMaxPos, iLeftPos, iRightPos;
+  float   fX, fVal, fMaxVal = 0.0, fMaxPos;
+  POINT   pPoints[1000];
+  int     iMaxPoints = 1000;
 #ifdef DEBUG
-   FILE    *fout;
+  FILE    *fout;
 #endif
-   /* start in the middle, and work our way outwards */
+  /* start in the middle, and work our way outwards */
 
-   iStep = 1;
+  iStep = 1;
 
-   fX = fStart;
+  fX = fStart; /* 0 in the first loop  and bx in the second loop */
 
-   iMaxPos =0;
-   fVal = (*func)(fX);
-   pPoints[iNumPoints].x = fX;
-   pPoints[iNumPoints].y = fVal;
-   iNumPoints++;
+  iMaxPos =0;
+  fVal = (*func)(fX);         /* is the number of tracks found for z=fx */
+  pPoints[iNumPoints].x = fX;
+  pPoints[iNumPoints].y = fVal;
+  iNumPoints++;
 
-  
-   iDone = 0;
-   while (!iDone && iNumPoints < iMaxPoints)
-   {
-      if (fVal < fMaxVal)
-      {
-         fMaxVal = fVal;
-         iMaxPos = iNumPoints - 1;
-         fMaxPos = fX;
-   }
-
+  iDone = 0;
+  while (!iDone && iNumPoints < iMaxPoints)
+    {
+      if (fVal < fMaxVal){           /* fVal is the -ntrack */
+	fMaxVal = fVal;              /* that is why Max is found by "<" */ 
+	iMaxPos = iNumPoints - 1;
+	fMaxPos = fX;
+      }
+      
       /*  If the max wasn't one of the last two points we
        *  looked at, it we must have bracketed it.
        */
-
-      /* if (iNumPoints - iMaxPos > 2 && iNumPoints > 3)
-       {
-         iDone = 1;
-         break;
-	 } */
-
+      
       /*  Didn't find a max?  Keep going! */
-
+      
       fX += iStep * fStep;
       fVal = (*func)(fX);
+      // printf("%f %f \n",fX,-fVal);
+      //if (fStep==.1){
+      if (mtemp)
+	mtemp->Fill(fX,-fVal);
+	//}
+
       pPoints[iNumPoints].x = fX;
       pPoints[iNumPoints].y = fVal;
       iNumPoints++;
       iStep = (iStep > 0 ? -1 * (iStep + 1) : -1 * (iStep - 1));
-   }
-
-   /*  OK; we have the minimum bracketed.  Now let's pick out
-    *  the three bracketing points.
-    */
-
-   
-   
-   iLeftPos  = (iMaxPos-2 > 0 ? iMaxPos - 2 : 0);
-   iRightPos = iMaxPos + 2;
-   if( iMaxPos == 0){
-     iLeftPos=1;
-   }
-
-   *ax = pPoints[iLeftPos].x;
-   *fa = pPoints[iLeftPos].y;
-   *bx = pPoints[iMaxPos].x;
-   *fb = pPoints[iMaxPos].y;
-   *cx = pPoints[iRightPos].x;
-   *fc = pPoints[iRightPos].y;
-
+    } /* end of while */
+  
+  /*  OK; we have the minimum bracketed.  Now let's pick out
+   *  the three bracketing points.
+   */
+  
+  iLeftPos  = (iMaxPos-2 > 0 ? iMaxPos - 2 : 0);
+  iRightPos = iMaxPos + 2;
+  if( iMaxPos == 0){
+    iLeftPos=1;
+  }
+  
+  *ax = pPoints[iLeftPos].x;
+  *fa = pPoints[iLeftPos].y;
+  *bx = pPoints[iMaxPos].x;
+  *fb = pPoints[iMaxPos].y;
+  *cx = pPoints[iRightPos].x;
+  *fc = pPoints[iRightPos].y;
+  
 #ifdef DEBUG
-
-   fout = fopen ("groups.dat", "w");
-
-   for (iLeftPos=-500; iLeftPos<500; iLeftPos++){
-     
-     fX = (float) iLeftPos/10.;
-     fprintf(fout,"%f %f \n",fX,(*func)(fX));
-   }
-   fclose(fout);
+  
+  fout = fopen ("groups.dat", "w");
+  
+  for (iLeftPos=-500; iLeftPos<500; iLeftPos++){
+    
+    fX = (float) iLeftPos/10.;
+    fprintf(fout,"%f %f \n",fX,(*func)(fX));
+  }
+  fclose(fout);
 #endif     
+  
+  return iNumPoints;
 
-   return iNumPoints;
-
-
-   
 }
 
