b063a6cee96fdc63fc8537356f986b4ca8b4165e
Frank Laue <laue@bnl.gov>
2001-11-11 18:34:14 +0000

    StHbtPicoEventCollectionVectorHideAway: updated for 3d grid StHbtVertexMultAnalysis: new

--- StRoot/StHbtMaker/Infrastructure/StHbtPicoEventCollectionVectorHideAway.cc
+++ StRoot/StHbtMaker/Infrastructure/StHbtPicoEventCollectionVectorHideAway.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StHbtPicoEventCollectionVectorHideAway.cc,v 1.1 2000/07/16 21:44:11 laue Exp $
+ * $Id: StHbtPicoEventCollectionVectorHideAway.cc,v 1.2 2001/11/11 18:34:13 laue Exp $
  *
  * Author: Mike Lisa, Ohio State, lisa@mps.ohio-state.edu
  ***************************************************************************
@@ -10,6 +10,10 @@
  ***************************************************************************
  *
  * $Log: StHbtPicoEventCollectionVectorHideAway.cc,v $
+ * Revision 1.2  2001/11/11 18:34:13  laue
+ * StHbtPicoEventCollectionVectorHideAway: updated for 3d grid
+ * StHbtVertexMultAnalysis: new
+ *
  * Revision 1.1  2000/07/16 21:44:11  laue
  * Collection and analysis for vertex dependent event mixing
  *
@@ -18,26 +22,45 @@
 #include "StHbtMaker/Infrastructure/StHbtPicoEventCollectionVectorHideAway.hh"
 
 // -----------------------------------
-StHbtPicoEventCollectionVectorHideAway::StHbtPicoEventCollectionVectorHideAway(int bins, double min, double max) {
-    mBins = bins;
-    mMin  = min;
-    mMax  = max;
-    mStep = (max-min)/bins;
+StHbtPicoEventCollectionVectorHideAway::StHbtPicoEventCollectionVectorHideAway(int bx, double lx, double ux,
+									       int by, double ly, double uy,
+									       int bz, double lz, double uz) {
+    mBinsx = bx;  mMinx = lx; mMaxx = ux;
+    mBinsy = by;  mMiny = ly; mMaxy = uy;
+    mBinsz = bz;  mMinz = lz; mMaxz = uz;
+
+    mBinsTot = mBinsx * mBinsy * mBinsz;
+    mStepx=0;  mStepx = (mMaxx-mMinx)/mBinsx;
+    mStepy=0;  mStepy = (mMaxy-mMiny)/mBinsy;
+    mStepz=0;  mStepz = (mMaxz-mMinz)/mBinsz;
+    
+
     //mCollectionVector = new StHbtPicoEventCollectionVector();
     mCollection = 0;
-    for ( int i=0; i<mBins; i++) {
+    for ( int i=0; i<mBinsTot; i++) {
 	mCollection = new StHbtPicoEventCollection();
 	mCollectionVector.push_back(mCollection);
     }
 }
 // -----------------------------------
-StHbtPicoEventCollection* StHbtPicoEventCollectionVectorHideAway::PicoEventCollection(int bin) { 
-  //cout << " StHbtPicoEventCollectionVectorHideAway::PicoEventCollection(int bin) - bin=" << bin << endl;
-  if ( bin<0 || bin >= mBins) return 0;
+StHbtPicoEventCollection* StHbtPicoEventCollectionVectorHideAway::PicoEventCollection(int ix, int iy, int iz) { 
+  if ( ix<0 || ix >= mBinsx) return 0;
+  if ( iy<0 || iy >= mBinsy) return 0;
+  if ( iz<0 || iz >= mBinsz) return 0;
+  int bin = ix + iy*mBinsx + iz*mBinsy*mBinsz; 
+  cout << " StHbtPicoEventCollectionVectorHideAway::PicoEventCollection(...) - bin(ix,iy,iz): ";
+  cout << bin << "(" << ix <<"," << iy << "," << iz <<")" << endl;
   return mCollectionVector[bin]; 
 }
 // -----------------------------------
-StHbtPicoEventCollection* StHbtPicoEventCollectionVectorHideAway::PicoEventCollection(double x) { 
-  return PicoEventCollection( (int)floor( (x-mMin)/mStep )  );
+StHbtPicoEventCollection* StHbtPicoEventCollectionVectorHideAway::PicoEventCollection(double x, double y, double z) {
+  int ix,iy,iz;
+  ix=0;iy=0;iz=0;
+
+  ix = (int)floor( (x-mMinx)/mStepx );
+  iy = (int)floor( (y-mMiny)/mStepy );
+  iz = (int)floor( (z-mMinz)/mStepz );
+
+  return PicoEventCollection( ix,iy,iz );
 }
 
--- StRoot/StHbtMaker/Infrastructure/StHbtPicoEventCollectionVectorHideAway.hh
+++ StRoot/StHbtMaker/Infrastructure/StHbtPicoEventCollectionVectorHideAway.hh
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StHbtPicoEventCollectionVectorHideAway.hh,v 1.1 2000/07/16 21:44:11 laue Exp $
+ * $Id: StHbtPicoEventCollectionVectorHideAway.hh,v 1.2 2001/11/11 18:34:13 laue Exp $
  *
  * Author: Mike Lisa, Ohio State, lisa@mps.ohio-state.edu
  ***************************************************************************
@@ -10,6 +10,10 @@
  ***************************************************************************
  *
  * $Log: StHbtPicoEventCollectionVectorHideAway.hh,v $
+ * Revision 1.2  2001/11/11 18:34:13  laue
+ * StHbtPicoEventCollectionVectorHideAway: updated for 3d grid
+ * StHbtVertexMultAnalysis: new
+ *
  * Revision 1.1  2000/07/16 21:44:11  laue
  * Collection and analysis for vertex dependent event mixing
  *
@@ -22,6 +26,7 @@
 #include "StHbtMaker/Infrastructure/StHbtPicoEventCollectionVector.hh"
 #include <vector>
 #include <list>
+#include <limits.h>
 
 #if !defined(ST_NO_NAMESPACES)
 using std::vector;
@@ -30,16 +35,19 @@ using std::list;
 
 class StHbtPicoEventCollectionVectorHideAway {
 public:
-    StHbtPicoEventCollectionVectorHideAway(int bins=10, double min=-10., double max=10.);
-    StHbtPicoEventCollection* PicoEventCollection(int);
-    StHbtPicoEventCollection* PicoEventCollection(double);
+  StHbtPicoEventCollectionVectorHideAway(int bx=1, double lx=DBL_MIN, double ux=DBL_MAX,
+					 int by=1, double ly=DBL_MIN, double uy=DBL_MAX,
+					 int bz=1, double lz=DBL_MIN, double uz=DBL_MAX);
+  StHbtPicoEventCollection* PicoEventCollection(int, int, int);
+  StHbtPicoEventCollection* PicoEventCollection(double x, double y=0, double z=0);
 private:
-    int mBins;
-    double mMin;
-    double mMax;
-    double mStep;
-    StHbtPicoEventCollection* mCollection;
-    StHbtPicoEventCollectionVector mCollectionVector;
+  int mBinsTot;
+  int mBinsx,mBinsy,mBinsz;
+  double mMinx,mMiny,mMinz;
+  double mMaxx,mMaxy,mMaxz;
+  double mStepx,mStepy,mStepz;
+  StHbtPicoEventCollection* mCollection;
+  StHbtPicoEventCollectionVector mCollectionVector;
 };
 
 #endif
