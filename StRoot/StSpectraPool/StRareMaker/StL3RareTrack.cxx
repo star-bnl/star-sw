800011bff59b25f1da79f59b70485cc43ed71414
struck <>
2001-12-05 21:02:04 +0000

    make solaris happy (take out default values in .cxx, leave them in .h-files)

--- StRoot/StSpectraPool/StRareMaker/StL3RareTrack.cxx
+++ StRoot/StSpectraPool/StRareMaker/StL3RareTrack.cxx
@@ -77,7 +77,7 @@ StL3RareTrack::StL3RareTrack(StGlobalTrack* track)
 }
 
 
-float StL3RareTrack::dedxExpected(float mass, float charge = 1) const {
+float StL3RareTrack::dedxExpected(float mass, float charge) const {
   float real_mom = p()*charge;
   float dedx = charge*charge*dEdx_formula(real_mom,mass);
   return dedx;
