e370e7c2fe900d14fba047f45457eef4bd41903c
Thomas Ullrich <thomas.ullrich@bnl.gov>
2002-01-09 15:37:55 +0000

    AdcSum functions and scaler infos added.

--- StRoot/StEvent/StFpdCollection.h
+++ StRoot/StEvent/StFpdCollection.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StFpdCollection.h,v 2.1 2002/01/03 20:57:36 ullrich Exp $
+ * $Id: StFpdCollection.h,v 2.2 2002/01/09 15:37:55 ullrich Exp $
  *
  * Author: Akio Ogawa, Jan 2002
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StFpdCollection.h,v $
+ * Revision 2.2  2002/01/09 15:37:55  ullrich
+ * AdcSum functions and scaler infos added.
+ *
  * Revision 2.1  2002/01/03 20:57:36  ullrich
  * Initial Revision.
  *
@@ -29,13 +32,18 @@ public:
     unsigned int    numberOfTDC() const;
     unsigned int    numberOfRegisters() const;
     unsigned int    numberOfPedestal() const;
+    unsigned int    numberOfPMTpEEMC() const;
+    unsigned int    numberOfPMTPbg() const;
+    unsigned int    numberOfPMTSmdX() const;
+    unsigned int    numberOfPMTSmdY() const;
+    unsigned int    numberOfScalers() const;
     
     unsigned short* adc();
     unsigned short* tdc();
     unsigned short  registers(unsigned int) const;
     unsigned short* pedestal();
-    int             token() const;
-    
+    unsigned int    scaler(unsigned int) const;
+
     unsigned short  north(unsigned int);
     unsigned short  south(unsigned int);
     unsigned short  top(unsigned int);
@@ -44,26 +52,46 @@ public:
     unsigned short  smdy(unsigned int);
     unsigned short  pres1(unsigned int);
     unsigned short  pres2(unsigned int);
+
+    unsigned int sumAdcNorth();
+    unsigned int sumAdcSouth();
+    unsigned int sumAdcTop();
+    unsigned int sumAdcBottom();
+    unsigned int sumAdcPreShower1();
+    unsigned int sumAdcPreShower2();
+    unsigned int sumAdcSmdX();
+    unsigned int sumAdcSmdY();
     
     void setAdc(unsigned int, unsigned short);
     void setTdc(unsigned int, unsigned short);
     void setRegister(unsigned int, unsigned short);
     void setPedestal(unsigned int, unsigned short);
-    
+    void setScaler(unsigned int, unsigned int);
+
+    unsigned int token() const;
+    void setToken(unsigned int);
+
     void dump();
     
 protected:
     enum {mMaxAdc = 256,
 	  mMaxTdc = 8,
 	  mMaxRegisters = 3,
-	  mMaxPedestal = 256};
+	  mMaxPedestal = 256,
+          mMaxPMTpEEMC = 12,
+          mMaxPMTPbg   = 16,
+          mMaxPMTSmdX  = 100,
+          mMaxPMTSmdY  = 60,
+          mMaxScalers  = 128};
     
     UShort_t mAdc[mMaxAdc];
     UShort_t mTdc[mMaxTdc];
     UShort_t mReg[mMaxRegisters];
     UShort_t mPed[mMaxPedestal];
-    
-    ClassDef(StFpdCollection,1)
+    UInt_t   mScl[mMaxScalers];
+    UShort_t mToken;
+  
+    ClassDef(StFpdCollection,2)
 };
 
 inline unsigned int
@@ -78,7 +106,22 @@ StFpdCollection::numberOfRegisters() const {return mMaxRegisters;}
 inline unsigned int
 StFpdCollection::numberOfPedestal() const {return mMaxPedestal;}
 
-inline int
-StFpdCollection::token() const {return mReg[1];}
+inline unsigned int
+StFpdCollection::numberOfPMTpEEMC() const {return mMaxPMTpEEMC;}
+
+inline unsigned int
+StFpdCollection::numberOfPMTPbg() const {return mMaxPMTPbg;}
+
+inline unsigned int
+StFpdCollection::numberOfPMTSmdX() const {return mMaxPMTSmdX;}
+
+inline unsigned int
+StFpdCollection::numberOfPMTSmdY() const {return mMaxPMTSmdY;}
+
+inline unsigned int
+StFpdCollection::numberOfScalers() const {return mMaxScalers;}
+
+inline unsigned int
+StFpdCollection::token() const {return mToken;}
 
 #endif
