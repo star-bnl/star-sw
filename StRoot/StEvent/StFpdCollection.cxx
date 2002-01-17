f762ba4450d92939ec3820ab8f19957a74368ffc
ullrich <>
2002-01-17 18:38:11 +0000

    Several new methods added. Bug fixed.

--- StRoot/StEvent/StFpdCollection.cxx
+++ StRoot/StEvent/StFpdCollection.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StFpdCollection.cxx,v 2.2 2002/01/09 15:37:55 ullrich Exp $
+ * $Id: StFpdCollection.cxx,v 2.3 2002/01/17 18:38:10 ullrich Exp $
  *
  * Author: Akio Ogawa, Jan 2002
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StFpdCollection.cxx,v $
+ * Revision 2.3  2002/01/17 18:38:10  ullrich
+ * Several new methods added. Bug fixed.
+ *
  * Revision 2.2  2002/01/09 15:37:55  ullrich
  * AdcSum functions and scaler infos added.
  *
@@ -20,7 +23,7 @@
 #include "StFpdCollection.h"
 #include <iostream>
 
-static const char rcsid[] = "$Id: StFpdCollection.cxx,v 2.2 2002/01/09 15:37:55 ullrich Exp $";
+static const char rcsid[] = "$Id: StFpdCollection.cxx,v 2.3 2002/01/17 18:38:10 ullrich Exp $";
 
 ClassImp(StFpdCollection)
 
@@ -39,8 +42,14 @@ StFpdCollection::~StFpdCollection() {/* noop */}
 unsigned short*
 StFpdCollection::adc() {return mAdc;}
 
+const unsigned short*
+StFpdCollection::adc() const {return mAdc;}
+
 unsigned short*
 StFpdCollection::tdc() {return mTdc;}
+
+const unsigned short*
+StFpdCollection::tdc() const {return mTdc;}
 
 unsigned short
 StFpdCollection::registers(unsigned int n) const
@@ -58,6 +67,12 @@ StFpdCollection::scaler(unsigned int n) const
 
 unsigned short*
 StFpdCollection::pedestal()
+{
+    return mPed;
+}
+
+const unsigned short*
+StFpdCollection::pedestal() const
 {
     return mPed;
 }
@@ -93,63 +108,69 @@ StFpdCollection::setScaler(unsigned int n, unsigned int val)
 }
 
 unsigned short
-StFpdCollection::north(unsigned int n)
+StFpdCollection::north(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTpEEMC) return mAdc[n+48]; 
+    if(n>0 && n<=mMaxPMTpEEMC) return mAdc[n+47]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::south(unsigned int n)
+StFpdCollection::south(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTPbg) return mAdc[n+32]; 
+    if(n>0 && n<=mMaxPMTPbg) return mAdc[n+31]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::top(unsigned int n)
+StFpdCollection::top(unsigned int n) const
 {
     if(n>0 && n<=mMaxPMTPbg) return mAdc[n-1]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::bottom(unsigned int n)
+StFpdCollection::bottom(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTPbg) return mAdc[n+16]; 
+    if(n>0 && n<=mMaxPMTPbg) return mAdc[n+15]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::smdx(unsigned int n)
+StFpdCollection::smdx(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTSmdX) return mAdc[n+120]; 
+    if(n>0 && n<=mMaxPMTSmdX) return mAdc[n+119]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::smdy(unsigned int n)
+StFpdCollection::smdy(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTSmdY) return mAdc[n+60]; 
+    if(n>0 && n<=mMaxPMTSmdY) return mAdc[n+59]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::pres1(unsigned int n)
+StFpdCollection::pres1(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTpEEMC) return mAdc[n+220]; 
+    if(n>0 && n<=mMaxPMTpEEMC) return mAdc[n+219]; 
     return 9999;
 }
 
 unsigned short
-StFpdCollection::pres2(unsigned int n)
+StFpdCollection::pres2(unsigned int n) const
 {
-    if(n>0 && n<=mMaxPMTpEEMC) return mAdc[n+236]; 
+    if(n>0 && n<=mMaxPMTpEEMC) return mAdc[n+235]; 
     return 9999;
+}
+
+unsigned short
+StFpdCollection::southVeto() const
+{
+    return mAdc[253]; 
 }
 
 unsigned int
-StFpdCollection::sumAdcNorth()
+StFpdCollection::sumAdcNorth() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTpEEMC; i++){sum+=(unsigned int)north(i);}
@@ -157,7 +178,7 @@ StFpdCollection::sumAdcNorth()
 }
 
 unsigned int
-StFpdCollection::sumAdcSouth()
+StFpdCollection::sumAdcSouth() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTPbg; i++){sum+=(unsigned int)south(i);}
@@ -165,7 +186,7 @@ StFpdCollection::sumAdcSouth()
 }
 
 unsigned int
-StFpdCollection::sumAdcTop()
+StFpdCollection::sumAdcTop() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTPbg; i++){sum+=(unsigned int)top(i);}
@@ -173,7 +194,7 @@ StFpdCollection::sumAdcTop()
 }
 
 unsigned int
-StFpdCollection::sumAdcBottom()
+StFpdCollection::sumAdcBottom() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTPbg; i++){sum+=(unsigned int)bottom(i);}
@@ -181,7 +202,7 @@ StFpdCollection::sumAdcBottom()
 }
 
 unsigned int
-StFpdCollection::sumAdcPreShower1()
+StFpdCollection::sumAdcPreShower1() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTpEEMC; i++){sum+=(unsigned int)pres1(i);}
@@ -189,7 +210,7 @@ StFpdCollection::sumAdcPreShower1()
 }
 
 unsigned int
-StFpdCollection::sumAdcPreShower2()
+StFpdCollection::sumAdcPreShower2() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTpEEMC; i++){sum+=(unsigned int)pres2(i);}
@@ -197,7 +218,7 @@ StFpdCollection::sumAdcPreShower2()
 }
 
 unsigned int
-StFpdCollection::sumAdcSmdX()
+StFpdCollection::sumAdcSmdX() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTSmdX; i++){sum+=(unsigned int)smdx(i);}
@@ -205,7 +226,7 @@ StFpdCollection::sumAdcSmdX()
 }
 
 unsigned int
-StFpdCollection::sumAdcSmdY()
+StFpdCollection::sumAdcSmdY() const
 {
     unsigned int sum=0;
     for(int i=1; i<=mMaxPMTSmdY; i++){sum+=(unsigned int)smdy(i);}
@@ -231,5 +252,6 @@ StFpdCollection::dump()
     cout << "PreShower2 "; for(i=1; i<13;  i++){cout << pres2(i) << " ";}; cout << endl;
     cout << "SMD X      "; for(i=1; i<101; i++){cout << smdx(i) << " ";}; cout << endl;
     cout << "SMD Y      "; for(i=1; i<61;  i++){cout << smdx(i) << " ";}; cout << endl;
-    cout << "Scalers    "; for(i=1; i<128; i++){cout << mScl[i] << " ";}; cout << endl;
+    cout << "South Veto  " << southVeto() << endl;
+    cout << "Scalers    "; for(i=0; i<128; i++){printf(" %x ",mScl[i]);}; cout << endl;
 }
