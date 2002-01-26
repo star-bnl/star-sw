667058afde25a86efa0cf14d837d7aa1ea9db954
Jerome Lauret <jeromel@bnl.gov>
2002-01-26 20:13:32 +0000

    doxygenized

--- StRoot/St_base/Stypes.h
+++ StRoot/St_base/Stypes.h
@@ -1,40 +1,44 @@
-//*-- Author :    Valeri Fine  08/12/94 begin_html mailto://fine@bnl.gov  end_html
+/*!
+ * \file Stypes.h
+ *
+ * \author Valeri Fine  08/12/94
+ *
+ * Stypes
+ * Basic types used by STAF - ROOT interface.                           
+ *
+ * This header file contains the set of the macro definitions           
+ * to generate a ROOT dictionary for "pure" C-strucutre the way ROOT    
+ * does it for the "normal" C++ classes                                 
+ *                                                                      
+ * This header file should be included into the all STAF table wrapper  
+ * classes (by stic compiler)                                           
+ *                                                                      
+ */
 
 #ifndef STAR_Stypes
 #define STAR_Stypes
  
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// Stypes                                                               //
-// $Id: Stypes.h,v 1.17 2000/09/15 15:10:31 perev Exp $                                                               
-// Basic types used by STAF - ROOT interface.                           //
-//                                                                      //
-// This header file contains the set of the macro definitions           //
-// to generate a ROOT dictionary for "pure" C-strucutre the way ROOT    //
-// does it for the "normal" C++ classes                                 //
-//                                                                      //
-// This header file should be included into the all STAF table wrapper  //
-// classes (by stic compiler)                                           //
-//                                                                      //
-//////////////////////////////////////////////////////////////////////////
 #include <Ttypes.h>
 #include "StTypeDefs.h"
 #ifdef __HP_aCC
 #include "St_HP_aCC.h"
 #endif
-enum EReturnCodes { 
-  kStOK    = 0, 	// OK
-  kStOk    = 0, 	// OK
-  kStWarn  = 1, 	// Warning, something wrong but work can be continued
-  kStEOF   = 2, 	// End Of File 
-  kStErr   = 3, 	// Error, drop this and go to the next event
-  kStERR   = 3, 	// Error, drop this and go to the next event
-  kStFatal = 4,    	// Fatal error, processing impossible
-  kStFATAL = 4,    	// Fatal error, processing impossible
-  kStSKIP  = kStErr   + 10,
-  kStSkip  = kStSKIP,
-  kStSTOP  = kStFATAL + 10,
-  kStStop  = kStSTOP
+/*!
+ * \enum EReturnCodes 
+ */
+enum EReturnCodes {                                                    
+  kStOK    = 0,                           /**< OK     */
+  kStOk    = 0,                           /**< OK     */
+  kStWarn  = 1,                           /**< Warning, something wrong but work can be continued */
+  kStEOF   = 2,                           /**< End Of File   */
+  kStErr   = 3,                           /**< Error, drop this and go to the next event    */
+  kStERR   = 3,                           /**< Error, drop this and go to the next event   */
+  kStFatal = 4,                           /**< Fatal error, processing impossible  */
+  kStFATAL = 4,                           /**< Fatal error, processing impossible  */
+  kStSKIP  = kStErr   + 10,               /**< enum value kStSKIP   */
+  kStSkip  = kStSKIP,                     /**< enum value kStSkip   */
+  kStSTOP  = kStFATAL + 10,               /**< enum value kStSTOP   */
+  kStStop  = kStSTOP                      /**< enum value kStStop   */
 };  
 
 #endif 
