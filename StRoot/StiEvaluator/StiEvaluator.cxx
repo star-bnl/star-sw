a3e6e283c6c77348c01c7e2be839b8027a171471
mmiller <>
2002-02-04 16:08:10 +0000

    Moved classes for TTree into stand alone file.

--- StRoot/StiEvaluator/StiEvaluator.cxx
+++ StRoot/StiEvaluator/StiEvaluator.cxx
@@ -156,6 +156,15 @@ void StiEvaluator::evaluateForEvent(const StiTrackContainer* trackStore)
 		//We already know that we're safe here
 		mEntry->setStiTrack(bestStiPair->partnerTrack());
 		mEntry->setAssociation(*bestStiPair);
+		const StiKalmanTrack* tkt =
+		    dynamic_cast<const StiKalmanTrack*>(bestStiPair->partnerTrack());
+		if (!tkt) {
+		    cout <<"StiEvaluator::evaluateForEvent(). ERROR:\t"
+			 <<"cast to kalman track failed."<<endl;
+		}
+		else {
+		    //fillHitEntry(tkt);
+		}
 		mTree->Fill();
 	    }
 
@@ -173,6 +182,15 @@ void StiEvaluator::evaluateForEvent(const StiTrackContainer* trackStore)
 		    
 		    //We already know that we're safe here
 		    mEntry->setStiTrack(info.partnerTrack());
+		    const StiKalmanTrack* tkt =
+			dynamic_cast<const StiKalmanTrack*>(info.partnerTrack());
+		    if (!tkt) {
+			cout <<"StiEvaluator::evaluateForEvent(). ERROR:\t"
+			     <<"cast to kalman track failed."<<endl;
+		    }
+		    else {
+			//fillHitEntry(tkt);
+		    }
 		    mEntry->setAssociation(info);
 		    mTree->Fill();
 		}
@@ -213,6 +231,8 @@ void StiEvaluator::fillHitEntry(const StiKalmanTrackNode* node)
 
 void StiEvaluator::fillHitEntry(const StiHit* hit)
 {
+    mStiHitEntry.hitTimesUsed = hit->timesUsed();
+    
     mStiHitEntry.hitPosition = hit->position();
     mStiHitEntry.hitRefAngle = hit->refangle();
     mStiHitEntry.hitLocalX = hit->x();
@@ -226,7 +246,6 @@ void StiEvaluator::fillHitEntry(const StiHit* hit)
     mStiHitEntry.hitLocalSxy = hit->sxy();
     mStiHitEntry.hitLocalSxz = hit->sxz();
     mStiHitEntry.hitLocalSyz = hit->syz();
-    
     
     const StThreeVectorF& pos = hit->globalPosition();
     mStiHitEntry.hitGlobalX = pos.x();
@@ -270,142 +289,3 @@ void StiEvaluator::fillHitEntry(const StiKalmanTrack* track)
 
 //Temp, to be moved to own file
 
-ClassImp(StiHitEntry)
-
-    StiHitEntry::StiHitEntry()
-{
-    reset();
-}
-
-StiHitEntry::~StiHitEntry()
-{
-   
-}
-
-
-void StiHitEntry::reset()
-{
-    hitPosition = hitRefAngle = hitLocalX = hitLocalY = hitLocalZ = 0.;
-    hitGlobalX = hitGlobalY = hitGlobalZ;
-    nodeAlpha = nodeLocalX = nodeLocalY = nodeLocalZ = nodeLocalEta = nodeLocalCurvature
-	= nodeLocalTanLambda = nodeXCenter = nodeYCenter = nodeLocalChi2 = 0.;
-    nodeHasHit = 0;
-}
-
-ClassImp(TrackEntry)
-
-    TrackEntry::TrackEntry()
-	: mArray(new TClonesArray("StiHitEntry",100))
-{
-}
-
-void TrackEntry::addStiHitEntry(const StiHitEntry& hit)
-{
-    TClonesArray& cArr = *mArray;
-    new(cArr[mHitCounter++]) StiHitEntry(hit);
-}
-
-void TrackEntry::clear()
-{
-    mArray->Clear();
-    mHitCounter = 0;
-
-    mcNTimesFound = bestMatch = 0;
-    mcTrackId = mcTrackPsi = mcTrackRapidity = mcTrackE = 0.;
-    mcTrackPx = mcTrackPy = mcTrackPz = mcTrackEta = 0.;
-    mcTrackNTpcHits = mcTrackNSvtHits = mcTrackNFtpcHits = 0;
-    
-    globalTrackQ = 0;
-    globalTrackM = globalTrackPsi = globalTrackChi2 = globalTrackNHit = 0.;
-    globalTrackPx = globalTrackPy = globalTrackPz = globalTrackPt = globalTrackEta = 0.;
-    globalTrackFitPoints = 0;
-    globalTrackNAssocHits = globalTrackNAssocTpcHits = globalTrackNAssocSvtHits =
-	globalTrackNAssocFtpcHits = 0;
-    
-    stiTrackM = stiTrackPsi = stiTrackChi2 = stiTrackNHit = 0.;
-    stiTrackY = stiTrackTanL = stiTrackPx = stiTrackPy = stiTrackPz = stiTrackPt = stiTrackEta = 0.;
-    stiTrackNHits = stiTrackNTpcHits = stiTrackNSvtHits = 0;
-    stiTrackNAssocHits = stiTrackNAssocTpcHits = stiTrackNAssocSvtHits  = 0;
-    stiTrackFlag = stiTrackNSeedHits = 0;
-
-}
-
-void TrackEntry::setStiTrack(const StiTrack *newtrack)
-{
-    stiTrackFlag=0;
-    StThreeVector<double> mom;
-    
-    try {
-	stiTrackQ          = newtrack->getCharge();
-	stiTrackPsi        = newtrack->getPhi();
-	stiTrackM          = newtrack->getMass();
-	stiTrackChi2       = newtrack->getChi2();
-	stiTrackNHit       = newtrack->getPointCount();
-	stiTrackNSeedHits = newtrack->getSeedHitCount();
-	stiTrackY          = newtrack->getRapidity();
-	stiTrackTanL       = newtrack->getTanL();
-	mom = newtrack->getMomentumAtOrigin();
-	stiTrackPx = mom.x();
-	stiTrackPy = mom.y();
-	stiTrackPz = mom.z();
-	stiTrackPt = mom.perp();
-	stiTrackEta = mom.pseudoRapidity();
-    }
-    
-    catch (runtime_error & rte)	{
-	//cout << "RunTime Error Exception: " << rte.what()<<endl;
-    }
-    catch (exception & e) {
-	//cout << "Exception: " << e.what()<<endl;
-    }
-    
-    //We made it all the way through, mark this track as legit
-    stiTrackFlag = 100 + newtrack->getFlag();
-}
-
-void TrackEntry::setAssociation(const StiTrackPairInfo& info)
-{
-    stiTrackNAssocHits = info.commonTpcHits()+info.commonSvtHits()+info.commonFtpcHits();
-    stiTrackNAssocTpcHits = info.commonTpcHits();
-    stiTrackNAssocSvtHits = info.commonFtpcHits();
-}
-
-void TrackEntry::setMcTrack(const StMcTrack *newtrack, unsigned int nTimesFound, bool best)
-{
-    //cout << "Setting MC ID " << newtrack->geantId();
-    mcNTimesFound += nTimesFound;
-    bestMatch = (best==true) ? 1 : 0;
-    mcTrackId       = newtrack->geantId();
-    mcTrackE        = newtrack->energy();
-    mcTrackRapidity = newtrack->rapidity();
-    const StThreeVectorF& momentum = newtrack->momentum();
-    mcTrackPt = momentum.perp();
-    mcTrackEta = momentum.pseudoRapidity();
-
-    mcTrackNTpcHits = newtrack->tpcHits().size();
-    mcTrackNSvtHits = newtrack->svtHits().size();
-    mcTrackNFtpcHits = newtrack->ftpcHits().size();
-}
-
-void TrackEntry::setGlobalAssoc(const StTrackPairInfo* pr)
-{
-    globalTrackNAssocTpcHits = pr->commonTpcHits();
-    globalTrackNAssocSvtHits = pr->commonSvtHits();
-    globalTrackNAssocFtpcHits = 0;
-    globalTrackNAssocHits = pr->commonTpcHits()+pr->commonSvtHits();
-}
-
-void TrackEntry::setGlobalTrack(const StTrack *newtrack)
-{
-    const StThreeVectorF& mom = newtrack->geometry()->momentum();
-    globalTrackPx = mom.x();
-    globalTrackPy = mom.y();
-    globalTrackPz = mom.z();
-    globalTrackPt = mom.perp();
-    globalTrackEta  = mom.pseudoRapidity();
-    globalTrackQ  = newtrack->geometry()->charge();
-    globalTrackPsi = newtrack->geometry()->psi();
-    globalTrackFitPoints = newtrack->fitTraits().numberOfFitPoints();
-    globalTrackChi2 = newtrack->fitTraits().chi2();
-  
-}
