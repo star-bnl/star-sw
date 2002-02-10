64bcd59ea6287c63871b5cd256d9db0cbef7914e
Jonathan Gans <gans>
2002-02-10 22:07:12 +0000

    Added classes for TPC Voltages for use in ExB Corrections

--- StRoot/StDetectorDbMaker/StDetectorDbMaker.cxx
+++ StRoot/StDetectorDbMaker/StDetectorDbMaker.cxx
@@ -5,6 +5,7 @@
 #include "StDetectorDbMagnet.h"
 #include "StDetectorDbClock.h"
 #include "StDetectorDbSpaceCharge.h"
+#include "StDetectorDbTpcVoltages.h"
 
 #include "StEvent/StEvent.h"
 #include "StEvent/StDetectorState.h"
@@ -55,13 +56,20 @@ Int_t StDetectorDbMaker::Make(){
     StDetectorDbRichScalers* scalers = StDetectorDbRichScalers::instance();
     scalers->update(this);
 
+
+    StDetectorDbTpcVoltages* tpcVolts = StDetectorDbTpcVoltages::instance();
+    tpcVolts->update(this);
+    
     // Also need to update instances for classes done in InitRun.
     // This is needed because of a feature in TTable
+    // Please ingore unused variables..the call to instane() is needed!
     StDetectorDbTpcRDOMasks* masks = StDetectorDbTpcRDOMasks::instance();
     StDetectorDbMagnet* magnet = StDetectorDbMagnet::instance();
     StDetectorDbSpaceCharge* spaceCharge = StDetectorDbSpaceCharge::instance();
     StDetectorDbClock* clock = StDetectorDbClock::instance();
 
+
+   
     // Jamie Asked for SpaceCharge to be couted every event
     cout << "Space Charge Correction = " << spaceCharge->getSpaceChargeCoulombs()
 	 << " Coulombs" << endl;
