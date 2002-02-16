01dc10cbf2bf378564d2b210bc68d4331d5b8bb3
jeromel <>
2002-02-16 21:50:49 +0000

    Marcello's recent changes.

--- StRoot/StDbUtilities/StSvtLinkDef.h
+++ StRoot/StDbUtilities/StSvtLinkDef.h
@@ -2,6 +2,9 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
+#pragma link C++ class StSvtDbMaker;
+#pragma link C++ global gStSvtDbMaker;
 #pragma link C++ function SvtLtoG_;
 #pragma link C++ function SvtGtoL_;
+
 #endif
