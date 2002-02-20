40027ab4a8e0751cbb872a98bd82f8b2fe11973c
gans <>
2002-02-20 17:47:52 +0000

    Changed default return value from DEAD to 0

--- StRoot/StDetectorDbMaker/StDetectorDbTpcRDOMasks.cxx
+++ StRoot/StDetectorDbMaker/StDetectorDbTpcRDOMasks.cxx
@@ -4,7 +4,7 @@
 /*! 
 In database, TPC Sector 1 and 2 are packed into Sector 1 with higher 6 bits belonding to sector 2, lower order to sector 1. Sector 3 and 4 in sector 2, etc. This class pulls the sector, rdo enable out in human readable Sector (1-24) rdo 1-6
 
-It is a singleton which requires manual updating, usually taken care of in StDetectorDbMaker.cxx::Make(). If no data exists all values return DEAD. To use:
+It is a singleton which requires manual updating, usually taken care of in StDetectorDbMaker.cxx::Make(). If no data exists all values return 0. To use:
 
   #include "StDetectorDbMaker/StDetectorDbTpcRDOMasks.h"
   StDetectorDbTpcRDOMasks * masks = StDetectorDbTpcRDOMasks::intstance();
@@ -92,7 +92,7 @@ bool StDetectorDbTpcRDOMasks::isOn(unsigned int sector,unsigned int rdo){
 /// higher order bit is higher mask
 unsigned int StDetectorDbTpcRDOMasks::getSectorMask(unsigned int sector){
 
-    unsigned int mask = 0xDEAD; // DEAD in Hex (all masks should only be 12 bit anyways)
+    unsigned int mask = 0x0000; //
     
     if(sector < 1 || sector > 24 || mNumEntries == 0)
 	return mask;
