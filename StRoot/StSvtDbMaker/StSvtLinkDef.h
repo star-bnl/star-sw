49c26f34509b5fdf96fa2a8f6462ea4197aab756
Helen L. Caines <helen.caines@yale.edu>
2002-02-20 17:09:34 +0000

    Added for database fortran2C code

---
+++ StRoot/StSvtDbMaker/StSvtLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class StSvtDbMaker;
+#pragma link C++ global gStSvtDbMaker;
+#pragma link C++ function SvtLtoG_;
+#pragma link C++ function SvtGtoL_;
+
+#endif
