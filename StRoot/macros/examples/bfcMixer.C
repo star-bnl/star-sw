1981ef205b890580f3f6e51f652c1853e21998ae
pfachini <>
2002-03-12 22:47:44 +0000

    Adding 	if (geantMk) geantMk->SetMode(1); so that EvtHddr is not changed in St_geant_maker.cxx
    Also, some clean-up...

--- StRoot/macros/examples/bfcMixer.C
+++ StRoot/macros/examples/bfcMixer.C
@@ -1,13 +1,11 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//
 // Macro for running chain with different inputs
 //
-// owner:  Yuri Fisyak
+// Owner:  Yuri Fisyak
 //
+// $Id: bfcMixer.C,v 1.12 2002/03/12 22:47:44 pfachini Exp $
 //
-//
-// $Id: bfcMixer.C,v 1.11 2001/04/03 21:53:09 wdeng Exp $
 //////////////////////////////////////////////////////////////////////////
 
 TBrowser *b = 0;
@@ -34,82 +32,64 @@ void Load(){
   if (chain) delete chain;
 }
 //_____________________________________________________________________
-void bfcMixer(const Int_t Nevents=2,
-             const Char_t *kind1="fz",
-             const Char_t *file1="/star/u2c/pfachini/workdir/SpectraChain/fz/st_physics_1243031_raw_0029.fz",
-             const Char_t *kind2="daq",
-             const Char_t *file2="/star/rcf/data06/reco/embedding/test/st_physics_1243031_raw_0029.daq")
+void bfcMixer(const Int_t Nevents=5,
+             const Char_t *file1="/star/data09/hrm-cache/st_physics_2270008_raw_0030.daq",
+	     const Char_t *file2="/afs/rhic/star/tpc/hjort/gtest.fz",
+             const Char_t *file3="/direct/star+u/hjort/test/st_physics_2270008_raw_0030.vertices.dat",
+             const Float_t zvertex_low=-50.0,
+             const Float_t zvertex_high=50.0)
 {
   // Dynamically link some shared libs
   if (gClassTable->GetID("StBFChain") < 0) Load();
-  chain = new StChain("Embedding");   // Create the main chain object
+
+  // Create the main chain object
+  chain = new StChain("Embedding");
 
   StMaker *saveMk = 0;
-  // File2 -> trs or daq
-  if (!strcmp(kind2,"trs")) {
-    chain2 = new StBFChain("Two");
-    saveMk = chain2->cd();
-    chain2->SetFlags("NoInput tpcDB tpc trs -tcl -tpt -PreVtx -tpc_daq"); //  
-    chain2->Set_IO_Files(0);
-    chain2->Load();
-    chain2->Instantiate();
-    StTrsMaker *trsMk = (StTrsMaker *) chain2->GetMaker("Two");
-    trsMk->readFile(file2);
-    saveMk->cd();
-  }
-  if (!strcmp(kind2,"daq")) {
-    chain2 = new StBFChain("Two");
-    saveMk = chain2->cd();
-    chain2->SetFlags("in calib db debug"); //  
-    chain2->Set_IO_Files(file2);
-    chain2->Load();
-    chain2->Instantiate();
-    saveMk->cd();
-  }
-  if (!strcmp(kind1,"fz")) {
-    kind1="trs";
-    chain1 = new StBFChain("One");
-    saveMk = chain1->cd();
-    chain1->SetFlags("fzin debug gen_T geomT sim_T tpc trs -tcl -tpt -PreVtx -tpc_daq"); // 
-    chain1->Set_IO_Files(file1);
-    chain1->Load();
-    chain1->Instantiate();
-    saveMk->cd();
-  }
 
+  // Create chain1 object
+  chain1 = new StBFChain("One");
+  saveMk = chain1->cd();
+  chain1->SetFlags("in NoDefault");
+  chain1->Set_IO_Files(file1);
+  chain1->Load();
+  chain1->Instantiate();
+  saveMk->cd();
+
+  // Create chain2 object
+  chain2 = new StBFChain("Two");
+  saveMk = chain2->cd();
+  chain2->SetFlags("fzin gen_T geomT sim_T tpc trs -tcl -tpt -PreVtx -tpc_daq");   // 
+  chain2->Set_IO_Files(file2);
+  chain2->Load();
+  chain2->Instantiate();
+  St_geant_Maker *geantMk = chain2->GetMaker("geant");
+  if (geantMk) geantMk->SetMode(1);   // Mixer mode - do not modify EvtHddr
+  saveMk->cd();
 
   // Mixer
   gSystem->Load("StMixerMaker");
-  StMixerMaker  *mixer = new StMixerMaker("Mixer",kind1,kind2);
-
-  chain1->SetInput("Input1","Event");
-
-  if (!strcmp(kind2,"trs")) {
-    chain2->SetInput("Input2","Event");
-  }
-  if (!strcmp(kind2,"daq")) {
-    chain2->SetInput("Input2","StDAQReader");
-  }
+  StMixerMaker  *mixer = new StMixerMaker("Mixer","daq","trs");
+  chain1->SetInput("Input1","StDAQReader");
+  chain2->SetInput("Input2","Event");
   mixer->writeFile("mixer.trs",Nevents);
+
+  // Create chain3 object
   chain3 = new StBFChain("Three");
   saveMk = chain3->cd();
-  //chain3->SetFlags("NoInput tpc tpc_daq cdst allevent tree");
-  //chain3->SetFlags("P00h NoInput -xin GeantOut -QA -EventQA debug");
-  //chain3->SetFlags("P00h NoInput l3 -in -xin -tags AllEvent");
-  //chain3->SetFlags("P00h NoInput DbV0713 -y1h -in -xin -tags AllEvent");
-  chain3->SetFlags("NoInput in tpc_daq tpc global dst Kalman Tree GeantOut");   
-  //ry1h,in,tpc_daq,tpc,rich,trg,Cdst,Kalman,tags,Tree,evout
-  TString OutputFileName(gSystem->BaseName(file2));
+  chain3->SetFlags("Simu NoDefault NoInput DbV20020226 db tpc_daq tpc global dst Kalman event qa Tree GeantOut"); 
+
+  TString OutputFileName(gSystem->BaseName(file1));
   OutputFileName.ReplaceAll("*","");
   OutputFileName.ReplaceAll(".daq","");
   OutputFileName.Append(".root");
   chain3->Set_IO_Files(0,OutputFileName.Data());
   chain3->Load();
   chain3->Instantiate();
-  St_geant_Maker *geantMk = chain->GetMaker("geant");
+  St_geant_Maker *geantMk = (St_geant_Maker *) chain->GetMaker("geant");
   geantMk->SetActive(kTRUE);
   StMaker *tpcdaqMk = chain3->GetMaker("tpc_raw");
-  tpcdaqMk->SetMode(1); // Trs
+  tpcdaqMk->SetMode(1);   // Trs
   tpcdaqMk->SetInput("Event","MixerEvent");
   saveMk->cd();
   {
@@ -117,35 +97,89 @@ void bfcMixer(const Int_t Nevents=2,
     printf ("QAInfo:Run is started at Date/Time%i/%i\n",t.GetDate(),t.GetTime());
   }
   printf ("QAInfo:Run on %s in %s\n",
-          gSystem->HostName(),
+        gSystem->HostName(),
         gSystem->WorkingDirectory());
   printf ("QAInfo: with %s\n", chain->GetCVS());
 
-// Init the chain and all its makers
+  // Init the chain and all its makers
   if (Nevents >= 0) {
     Int_t iInit = chain->Init();
   }
-  //chain->SetDEBUG();
+  // chain->SetDEBUG();
   treeMk = chain->GetMaker("tree");
   TBenchmark evnt;
   Int_t iMake = 0, i = 1, iBad = 0;
+
+  StIOMaker *inpMk = (StIOMaker *)chain1->GetMaker("inputStream");
+  FILE *fp = fopen(file3,"r");
+  Float_t x, y, z;
+  Int_t ncols, eventnumber, mult, skip=0, oldskip = 0, skiptest=0;
+  printf("zvertex_low = %f zvertex_high = %f\n",zvertex_low, zvertex_high);
+
+  // vtxMk = (StVertexMaker*) chain3->GetMaker("vertex");
+
  EventLoop: if (i <= Nevents && iMake != kStEOF && iMake != kStFatal) {
    evnt->Reset();
    evnt->Start("QAInfo:");
    chain->Clear();
+   
+   ncols = fscanf(fp,"%d %d %d %f %f %f",&eventnumber,&skip,&mult,&x,&y,&z);
+   if(ncols<0) break;
+
+   printf("\nEvent number: %d Multiplicity = %d\n",eventnumber,mult);
+   printf("vertex in bfcMixer: %16f %16f %16f\n",x,y,z);
+
+   // skip events in chain1 (daq file) only.
+   // fz file does not have these events.
+   // strange initialization due to details of inpMk->Skip
+   // ...not well understood.  elh
+
+   printf ("bfcMixer: skip = %i\n",skip);
+   printf ("bfcMixer: oldskip = %i\n",oldskip);
+   if (inpMk && skip>0) {
+     if(i == 1) {skip++;}
+     skiptest = inpMk->Skip(skip-oldskip-1);
+     printf("bfcMixer: skiptest = %i\n",skiptest);
+     if(i == 1) {skip--;}
+   }
+   oldskip = skip;
+
+   use this to force the vertex position to match:
+   // vtxMk->FixVertex(x,y,z);
+
    iMake = chain->Make(i);
    if (treeMk && iMake == kStErr) {treeMk->Make(i); iBad++;}
    StEvtHddr *fEvtHddr = (StEvtHddr*)chain->GetDataSet("EvtHddr");
-   StEvtHddr *fEvtHddrDaq = (StEvtHddr*)chain2->GetDataSet("EvtHddr");
+   StEvtHddr *fEvtHddrDaq = (StEvtHddr*)chain1->GetDataSet("EvtHddr");
    *fEvtHddr = *fEvtHddrDaq;
-  //    gSystem->Exec("ps ux");
-  evnt->Stop("QAInfo:");
-  //  evnt->Show("QAInfo:");
-  printf ("QAInfo: Done with Event [no. %d/run %d/evt. %d/Date.Time%d.%d/sta %d] Real Time = %10.2f seconds Cpu Time =  %10.2f seconds \n",
-	  i,chain->GetRunNumber(),chain->GetEventNumber(),chain->GetDate(), chain->GetTime(),
+   // gSystem->Exec("ps ux");
+   evnt->Stop("QAInfo:");
+   // evnt->Show("QAInfo:");
+   printf ("QAInfo: Done with Event [no. %d/run %d/evt. %d/Date.Time%d.%d/sta %d] Real Time = %10.2f seconds Cpu Time =  %10.2f seconds \n", i,chain->GetRunNumber(),chain->GetEventNumber(),chain->GetDate(), chain->GetTime(),
 	  iMake,evnt->GetRealTime("QAInfo:"),evnt->GetCpuTime("QAInfo:"));
-  i++; goto EventLoop;
+
+
+   // This doesn't work...  elh
+   // TString *fnam = OutputFileName.Data();
+   // TFile *fileptr = TFILE::Open(fnam);
+   // TTree *tree = (TTree*)fileptr->Get("Tag");
+   // tree->GetEvent(i);
+   // Float_t zvert = tree->GetLeaf("primaryVertexX")->GetValue();
+   // printf("zvert = %f\n",zvert);
+
+   // Be sure to get the same event from daq and fz files...  elh
+   if(eventnumber == chain->GetEventNumber()){
+     printf("bfcMixer:  Event number %d found in both .fz file and .daq file\n",eventnumber);
+   }else{
+     printf("bfcMixer: Error! Event number from .fz file: %d\n",eventnumber);
+     printf("bfcMixer: Error! Event number from .daq file: %d\n",chain->GetEventNumber());
+     printf("bfcMixer: Error! Events are out-of-sync, exiting!\n");
+     break;
+   }
+   i++;
+   goto EventLoop;
  }
+
   fflush(stdout);
   printf ("QAInfo:Run completed ");
   gSystem->Exec("date");
@@ -155,3 +189,4 @@ void bfcMixer(const Int_t Nevents=2,
     printf ("\nQAInfo:Run is finished at Date/Time%i/%i\n",t.GetDate(),t.GetTime());
   }
 }
+
