8a73b65f6f1b0c0feb78268a7b1d2f71c3ebbbb9
genevb <>
2002-04-23 01:59:16 +0000

    New offset abilities

--- StRoot/StUtilities/StMultiH1F.h
+++ StRoot/StUtilities/StMultiH1F.h
@@ -1,17 +1,10 @@
-// $Id: StMultiH1F.h,v 1.4 2000/09/05 19:54:46 genevb Exp $
-// $Log: StMultiH1F.h,v $
-// Revision 1.4  2000/09/05 19:54:46  genevb
-// Erroneous delete in destructor removed
-//
-// Revision 1.3  2000/08/28 19:21:05  genevb
-// Improved projection code
-//
-// Revision 1.2  2000/08/25 15:46:42  genevb
-// Added stats box, legend names
-//
-// Revision 1.1  2000/07/26 22:00:28  lansdell
-// new multi-hist class for superimposing the x-projections of y-bins (of a TH2F histogram) into one TH1F histogram
-//
+/*!
+  \class StMultiH1F
+  
+  StMultiH1F allows multiple similar TH1F histograms to be
+  easily plotted on one graph
+
+*/
 
 #ifndef ClassStMultiH1F
 #define ClassStMultiH1F
@@ -39,10 +32,34 @@ class StMultiH1F : public TH2F {
   // Overload the Rebin() function to allow naming of y bins with TH2F pointer
   virtual        TH1* Rebin(Int_t ngroup, const char* newname)
                               { SetNames(ngroup, newname); return 0; }
+  // Overload the SetBarOffset() function to allow offsetting of y bins vertically
+  virtual        void SetBarOffset(Float_t offset);
+  virtual    Double_t GetNonZeroMinimum() const;
+  virtual    Double_t GetNonZeroMaximum() const;
  protected:
   TString names[10];
+  Float_t fMOffset;
   virtual       TH1F* XProjection(const char* name, Int_t ybin=-1);
   ClassDef(StMultiH1F,1)
 };
 
 #endif
+
+// $Id: StMultiH1F.h,v 1.5 2002/04/23 01:59:16 genevb Exp $
+// $Log: StMultiH1F.h,v $
+// Revision 1.5  2002/04/23 01:59:16  genevb
+// New offset abilities
+//
+// Revision 1.4  2000/09/05 19:54:46  genevb
+// Erroneous delete in destructor removed
+//
+// Revision 1.3  2000/08/28 19:21:05  genevb
+// Improved projection code
+//
+// Revision 1.2  2000/08/25 15:46:42  genevb
+// Added stats box, legend names
+//
+// Revision 1.1  2000/07/26 22:00:28  lansdell
+// new multi-hist class for superimposing the x-projections of y-bins (of a TH2F histogram) into one TH1F histogram
+//
+
