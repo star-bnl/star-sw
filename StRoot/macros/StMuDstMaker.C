f1adde2cfd677e20d159b079fab60504fbd72061
Jerome Lauret <jeromel@bnl.gov>
2002-05-22 03:01:08 +0000

    First version for production (no Emc yet)

---
+++ StRoot/macros/StMuDstMaker.C
@@ -0,0 +1,130 @@
+//
+// NOTE - chain needs to be declared global so for StHbtEventReader
+// Modified from StRoot/StMuDstmaker/COMMON/macros i.e. initially
+// written by Frank Laue.
+//
+class   StChain;
+StChain *chain=0;    
+void    ProcessQQ(const Int_t nevents, const Char_t **fileList, const Char_t*, const Char_t* );
+
+
+
+//==========================================================================================
+void StMuDstMaker(const Int_t nevents=10,
+		    const Char_t *path="-",
+		    const Char_t *file="/star/data13/reco/dev/2001/10/st_physics_2304060_raw_0303.event.root",
+		    const Char_t* outDir="./",		
+		    const Char_t* outFile="dummy",
+		    const Char_t* appendix="test.microDst")		
+{ 
+
+  const char *fileListQQ[]={0,0};
+
+  if (path[0]=='-') {
+    fileListQQ[0]=file;
+  } else {
+    fileListQQ[0] = gSystem->ConcatFileName(path,file);
+  }
+  ProcessQQ(nevents,fileListQQ,outDir,outFile);
+}
+
+
+//==========================================================================================
+void ProcessQQ(const Int_t nevents, const Char_t **fileList, const Char_t* dirName, const Char_t* fileName)
+{
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+  gSystem->Load("St_Tables");
+  gSystem->Load("StMagF");
+  gSystem->Load("StUtilities"); 
+  gSystem->Load("StTreeMaker");
+  gSystem->Load("StIOMaker");
+  gSystem->Load("StarClassLibrary");
+  gSystem->Load("StTpcDb");
+  gSystem->Load("StDbUtilities");
+  gSystem->Load("StEvent");
+  gSystem->Load("StEventUtilities"); 
+  gSystem->Load("StMcEvent"); 
+  gSystem->Load("StMcEventMaker"); 
+  gSystem->Load("StAssociationMaker");
+
+  //gSystem->Load("StDbLib");
+  //gSystem->Load("StDbBroker");
+  //gSystem->Load("St_db_Maker");
+
+  gSystem->Load("StMcAnalysisMaker");
+  gSystem->Load("StStrangeMuDstMaker");
+  gSystem->Load("StMuDSTMaker");
+
+  //gSystem->Load("StEmcADCtoEMaker"); // analysis maker
+  //gSystem->Load("StPreEclMaker"); // analysis maker
+  //gSystem->Load("StEpcMaker"); // analysis maker
+
+
+
+  cout << " loading done " << endl;
+
+  chain = new StChain("StChain"); 
+  chain->SetDebug();
+
+
+  StFile *setFiles= new StFile();
+  for (int ifil=0; fileList[ifil]; ifil++)
+    setFiles->AddFile(fileList[ifil]);
+
+
+  // ********************************
+  // Now we add Makers to the chain... 
+  // ********************************
+
+  // *************
+  // file handling
+  // *************
+  StIOMaker* ioMaker = new StIOMaker("IOMaker","r",setFiles,"bfcTree");
+  ioMaker->SetDebug();
+
+  ioMaker->SetIOMode("r");
+  ioMaker->SetDebug();
+  ioMaker->SetBranch("*",0,"0");         //deactivate all branches
+  ioMaker->SetBranch("eventBranch",0,"r"); //activate evt.root Branch
+
+  // ***********************
+  // the StStrangeMuDstMaker
+  // ***********************
+
+  StStrangeMuDstMaker* v0dst = new StStrangeMuDstMaker("StrangeMaker");
+  v0dst->DoV0(); //Set v0MiniDstMaker to find only v0s
+  v0dst->DoXi(); //Set v0MiniDstMaker to find only v0s
+  v0dst->DoKink(); //Set v0MiniDstMaker to find only v0s
+  v0dst->SetNoKeep(); 
+  //v0dst->SetWrite("depp.root"); // Set V0muDStMaker output file and Event output file
+  // v0dst->SetWrite("StrangemuEventHBTPeriphdst.root","Strangemuv0HBTPeriphdst.root"); // Set V0muDStMaker output file and Event output file
+
+
+  // ******************
+  // Processing here
+  // ******************
+
+  StMuDstMaker* maker = new StMuDstMaker(1,1,dirName);
+  maker->setProbabilityPidFile("/afs/rhic/star/users/aihong/www/PIDTableP01gl.root");
+
+  StMuL3Filter* l3Filter = new StMuL3Filter(); maker->setL3TrackFilter(l3Filter);
+  StMuFilter* filter = new StMuFilter();       maker->setTrackFilter(filter);
+
+
+  chain->Init(); // This should call the Init() method in ALL makers
+  chain->PrintInfo();
+  for (Int_t iev=0;iev<nevents; iev++) {
+    cout << "--> Working on eventNumber " << iev << endl;
+    chain->Clear();
+    int iret = chain->Make(iev); // This should call the Make() method in ALL makers    
+    if (iret) {
+      cout << "Bad return code!" << endl;
+      break;
+    }
+  } // Event Loop
+  chain->Finish(); // This should call the Finish() method in ALL makers
+
+  cout << " End of Analysis " << endl;
+}
+
