5e9f4f265e93d621b2da85c8c95bcd071a14b665
jones <>
2002-05-30 11:31:08 +0000

    Changed tray and slat to UInt_t

--- StRoot/StEventUtilities/StuJitterBug.hh
+++ StRoot/StEventUtilities/StuJitterBug.hh
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * $Id: StuJitterBug.hh,v 1.1 2000/09/28 20:12:54 jones Exp $
+ * $Id: StuJitterBug.hh,v 1.2 2002/05/30 11:31:08 jones Exp $
  *
  * Author: Peter G. Jones, University of Birmingham, Aug 2000
  *
@@ -12,6 +12,9 @@
  * Otherwise the function returns kFALSE.
  *
  * $Log: StuJitterBug.hh,v $
+ * Revision 1.2  2002/05/30 11:31:08  jones
+ * Changed tray and slat to UInt_t
+ *
  * Revision 1.1  2000/09/28 20:12:54  jones
  * Adding StuJitterBug abort function
  *
@@ -40,8 +43,8 @@ Bool_t t0JitterAbort(StEvent* event) {
 
   // Loop over slats/trays and calc event and pre samples
 
-  for( Int_t slat=0; slat<ctb.numberOfSlats(); slat++ ) {
-    for( Int_t tray=0; tray<ctb.numberOfTrays(); tray++) {
+  for( UInt_t slat=0; slat<ctb.numberOfSlats(); slat++ ) {
+    for( UInt_t tray=0; tray<ctb.numberOfTrays(); tray++) {
       ctb_evt += ctb.mips( tray, slat, 0 );
       ctb_pre += ctb.mips( tray, slat, ctb.numberOfPreSamples() );
     }
