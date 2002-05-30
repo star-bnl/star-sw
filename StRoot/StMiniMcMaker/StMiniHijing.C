352114a1c25cfae9357be035a154c05fa2e04eb4
Manuel Calderon de la Barca Sanchez <calderon@physics.ucdavis.edu>
2002-05-30 01:18:23 +0000

    StMiniMcMaker is a the package that fills the StMiniMcEvent classes from StEvent StMcEvent and the association maker, this is used as a general framework for extracting single particle corrections: acceptance, efficiency, resolution, weak decay backgrounds, secondaries, split and merged tracks.

---
+++ StRoot/StMiniMcMaker/StMiniHijing.C
@@ -0,0 +1,133 @@
+//////////////////////////////////////////////////////////////////////
+// owner: Manuel Calderon de la Barca Sanchez
+//
+// what it does: reads .geant.root file, 
+//               runs a chain of 4 makers: 
+//                 StEventReaderMaker, StMcEventMaker,StAssociationMaker,
+//                 StMcAnalysisMaker
+//
+// note: for more info on StMcEvent and StAssociationMaker, do a 
+//      cvs checkout and say "make" in the doc/tex directory - you'll
+//      get a ps file with user guide and reference manual.
+//////////////////////////////////////////////////////////////////////
+
+class StChain;
+StChain *chain=0;
+
+void StMiniHijing(Int_t nevents=3,
+		  const char* MainFile=
+		  "hijing_b0_3/rcf0147_01*geant.root",
+		  const char* outDir = "./",
+		  int commonHits=3)
+{
+
+  // Dynamically link needed shared libs
+  //gSystem->Load("StarRoot");
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+
+  gSystem->Load("libglobal_Tables");
+  gSystem->Load("libgeometry_Tables");
+  gSystem->Load("libsim_Tables");
+  gSystem->Load("libgen_Tables");
+  gSystem->Load("StUtilities");
+  gSystem->Load("StIOMaker");
+  gSystem->Load("StarClassLibrary");
+    
+  gSystem->Load("StTpcDb");
+  gSystem->Load("StEvent");
+  gSystem->Load("StEventMaker"); 
+  gSystem->Load("StEmcUtil"); 
+
+  gSystem->Load("StMcEvent");
+  gSystem->Load("StMcEventMaker");
+  gSystem->Load("StAssociationMaker");
+  gSystem->Load("StMcAnalysisMaker");
+
+  gSystem->Load("StMiniMcEvent");
+  gSystem->Load("StMiniMcMaker");
+//   gSystem->Load("Common");
+
+  chain = new StChain("StChain"); 
+  chain->SetDebug();
+  
+  // Now we add Makers to the chain...
+  
+  StIOMaker* ioMaker = new StIOMaker("IO","r",MainFile,"bfcTree");
+  ioMaker->SetDebug();
+  ioMaker->SetIOMode("r");
+  ioMaker->SetBranch("*",0,"0");                 //deactivate all branches
+  ioMaker->SetBranch("geantBranch",0,"r"); //activate geant Branch
+  ioMaker->SetBranch("dstBranch",0,"r"); //activate Event Branch
+  // ioMaker->SetBranch("runcoBranch",0,"r"); //activate runco Branch
+  
+  //     const char *mainDB = "MySQL:Geometry_tpc";
+  //     St_db_Maker *dbMk = new St_db_Maker("Geometry",mainDB);
+  //     dbMk->SetDebug();
+  
+  //     const char *calibDB = "MySQL:Calibrations_tpc";
+  //     St_db_Maker *calibMk = new St_db_Maker("Calibrations",calibDB);
+  //     calibMk->SetDebug();
+  
+  //     StTpcDbMaker *tpcDbMk = new StTpcDbMaker("tpcDb");
+  
+  // Note, the title "events" is used in the Association Maker, so don't change it.
+  StEventMaker*       eventReader   = new StEventMaker("events","title");
+  eventReader->doPrintMemoryInfo = kFALSE;
+  StMcEventMaker*     mcEventReader = new StMcEventMaker; // Make an instance...
+  //     mcEventReader->doPrintMemoryInfo = kFALSE;
+  //     mcEventReader->doUseTpc = kTRUE;
+  //     mcEventReader->doUseSvt = kTRUE;
+  //     mcEventReader->doUseFtpc = kTRUE;
+  //     mcEventReader->doUseRich = kTRUE;
+  StAssociationMaker* associator    = new StAssociationMaker;
+  //associator->doPrintMemoryInfo = kTRUE;
+
+  //
+  // mdst of association stuff
+  //
+  StMiniMcMaker *krap = new StMiniMcMaker;
+  krap->setDebug(1);
+  krap->setGhost();
+  krap->setOutDir(outDir);
+  krap->setPtCut(1);
+
+  // Define the cuts for the Associations
+  
+  StMcParameterDB* parameterDB = StMcParameterDB::instance();  
+  // TPC
+  parameterDB->setXCutTpc(.5); // 5 mm
+  parameterDB->setYCutTpc(.5); // 5 mm
+  parameterDB->setZCutTpc(.5); // 5 mm
+  parameterDB->setReqCommonHitsTpc(commonHits); // Require 3 hits in common for tracks to be associated
+  // FTPC
+  parameterDB->setRCutFtpc(.3); // 3 mm
+  parameterDB->setPhiCutFtpc(5*(3.1415927/180.0)); // 5 degrees
+  parameterDB->setReqCommonHitsFtpc(3); // Require 3 hits in common for tracks to be associated
+  // SVT
+  parameterDB->setXCutSvt(.08); // 800 um
+  parameterDB->setYCutSvt(.08); // 800 um
+  parameterDB->setZCutSvt(.08); // 800 um
+  parameterDB->setReqCommonHitsSvt(1); // Require 1 hits in common for tracks to be associated
+  
+  
+  // now execute the chain member functions
+  
+  //chain->PrintInfo();
+  Int_t initStat = chain->Init(); // This should call the Init() method in ALL makers
+  if (initStat) chain->Fatal(initStat, "during Init()");
+  
+  int istat=0,iev=1;
+ EventLoop: if (iev<=nevents && istat!=2) {
+   chain->Clear();
+   cout << "---------------------- Processing Event : " << iev << " ----------------------" << endl;
+   istat = chain->Make(iev); // This should call the Make() method in ALL makers
+   if (istat == 2) { cout << "Last  Event Processed. Status = " << istat << endl; }
+   if (istat == 3) { cout << "Error Event Processed. Status = " << istat << endl; }
+   iev++; goto EventLoop;
+ } // Event Loop
+ 
+ chain->Finish();
+  
+}
+
---
+++ StRoot/StMiniMcMaker/StMiniMcMaker.h
@@ -0,0 +1,193 @@
+/**
+ * $Id $
+ * \file  StMiniMcMaker.h
+ * \brief Filling of StMiniMcEvent classes from StMcEvent, StEvent, StAssociationMaker
+ * 
+ *
+ * \author Bum Choi
+ * \date   March 2001
+ *  
+ * Fills the mDst of the association maker results.
+ * basically an amalgamation of the flow maker and
+ * manuel calderon de la barca's code.
+ *
+ */
+
+#ifndef StMiniMcMaker_H
+#define StMiniMcMaker_H
+
+#include "StMaker.h"
+
+#include <vector>
+#include <utility>
+#include <map>
+
+#include "TString.h"
+
+#include "StAssociationMaker/StAssociationMaker.h"
+#include "StAssociationMaker/StTrackPairInfo.hh"
+
+class StMiniMcEvent;
+class StMiniMcPair;
+class StTinyMcTrack;
+class StTinyRcTrack;
+class StContamPair;
+
+class TFile;
+class TTree;
+class StRun;
+class StEvent;
+class StMcEvent;
+class StMcTrack;
+class StTrack;
+class StPrimaryTrack;
+class StIOMaker;
+class StThreeVectorF;
+class StTpcDedxPidAlgorithm;
+class StuProbabilityPidAlgorithm;
+class StTpcHit;
+
+// typdef's
+#ifndef __CINT__
+
+typedef map<UInt_t,Int_t> RCFOUNDMAP;
+typedef map<long,Int_t> MCFOUNDMAP;
+typedef vector<StTrackPairInfo*> PAIRVEC;
+typedef pair<StTpcHit*,StTpcHit*> PAIRHIT;
+inline bool pairCmp(StTrackPairInfo* p1, StTrackPairInfo* p2){
+  return p1->commonTpcHits() < p2->commonTpcHits();
+}
+inline bool sortCmp(StTrackPairInfo* p1, StTrackPairInfo* p2){
+  return p1->commonTpcHits() > p2->commonTpcHits();
+}
+
+#endif
+bool hitCmp(StTpcHit* p1, StTpcHit* p2);
+
+
+// check StMiniMcEvent for the track categories...
+
+//____________________________________________
+
+class StMiniMcMaker : public StMaker{
+ public:
+  StMiniMcMaker(const Char_t* name="StMiniMcMaker",
+		const Char_t* title="event/StMiniMcMaker");
+  virtual ~StMiniMcMaker();
+
+  void  Clear(Option_t *option="");
+  Int_t Init();
+  Int_t Make();
+  Int_t Finish();
+
+  //---- SETS -------
+
+  void  setGhost(Bool_t doit=kTRUE)      { mGhost = doit; }
+  void  setDebug(Bool_t debug=kTRUE)     { mDebug = debug; }
+  void  setOutDir(const char* dir= "./") { mOutDir = dir; }  
+  void  setPtCut(Float_t minPt=0, Float_t maxPt=9999) 
+    { mMinPt=minPt; mMaxPt=maxPt; }
+  void  setBField(Float_t val=0.25) { mBField=val; }
+  void  setFileName(TString& val)      { mInFileName = val; }
+
+ private:
+  //static const Float_t mSharedHitsCut = .5;
+
+  // methods
+  
+  Bool_t           initAssociation();  // sets all the assoc maps
+  Bool_t           initVertex(); // finds the primary vertex if it exists
+  Bool_t           acceptRaw(StMcTrack*);
+  Bool_t           accept(StMcTrack*);
+  Bool_t           accept(StTrack*);
+  Bool_t           acceptCentrality(StTrack*);
+  Bool_t           acceptUncorrected(StTrack*);
+  Bool_t           ok(StTrack*);
+  Bool_t           isSameSign(StTrack*,StMcTrack*);
+  Bool_t           acceptPt(StTrack*);
+  Bool_t           acceptPt(StMcTrack*);
+  Bool_t           acceptDebug(StMcTrack*);
+  
+  PAIRVEC          findMatchedRc(StMcTrack*);
+  PAIRHIT          findFirstLastHit(const StTrack*);
+  PAIRHIT          findFirstLastFitHit(const StTrack*);
+
+  Float_t          computeXY(const StThreeVectorF*, const StTrack*);
+  Float_t          computeDedx(const StTrack*);
+  //pair<Float_t,Float_t>  computeProj(const StThreeVectorF*,const StTrack*);
+  Float_t          computeZDca(const StThreeVectorF*,const StTrack*);
+
+  StPrimaryTrack*  isPrimaryTrack(StTrack*);
+  Bool_t           isPrimaryTrack(StMcTrack*);
+  Int_t            openFile();
+  Int_t            closeFile();
+  void             trackLoop();
+  void             fillEventInfo(Int_t nGoodTrack);
+  void             fillTrackPairInfo(StMiniMcPair*,
+				     const StMcTrack*,
+				     const StTrack* prTrack, 
+				     const StTrack* glTrack,
+				     Int_t commonHits, Int_t nAssMc,
+				     Int_t nAssGl, Int_t nAssPr,
+				     Bool_t isBestContam=kFALSE);
+  
+  void             fillRcTrackInfo(StTinyRcTrack*,
+				   const StTrack* prTrack,
+				   const StTrack* glTrack,
+				   Int_t nAssMc);
+  
+  void             fillMcTrackInfo(StTinyMcTrack*,
+				   const StMcTrack*,
+				   Int_t nAssGl, Int_t nAssPr);
+
+  void             checkMerged(StMcTrack* merged, Int_t mergedCommonHits,
+			       StTrack* prTrack);
+  void             checkSplit(StMcTrack*,StTrack*,Int_t);
+  void             checkContam(StMcTrack*,StGlobalTrack*,Int_t);
+
+
+  // members
+  StMiniMcEvent*   mMiniMcEvent; //! 
+  StIOMaker*       mIOMaker;      //!
+  TTree*           mMiniMcTree;   //!
+  TFile*           mMiniMcDST;    //!
+  TString          mInFileName;   //!
+  TString          mOutFileName;  //!
+  TString          mOutDir;       //!
+  TString          mParameterFileName; //!
+
+  StEvent*         mRcEvent;      //!
+  StMcEvent*       mMcEvent;      //!
+  StRun*           mRun;          //!
+  rcTpcHitMapType* mRcHitMap;     //!
+  rcTrackMapType*  mRcTrackMap;   //!
+  mcTrackMapType*  mMcTrackMap;   //!
+  const StThreeVectorF*  mRcVertexPos;  //!
+  const StThreeVectorF*  mMcVertexPos;  //!
+  StTpcDedxPidAlgorithm* mTpcDedxAlgo; //!
+  StuProbabilityPidAlgorithm* mPidAlgo; //!
+
+  Bool_t           mGhost;        //!
+  Bool_t           mDebug;        //!
+  
+  Float_t          mMinPt;        //!
+  Float_t          mMaxPt;        //!
+
+  Float_t          mBField;       //! in tesla
+
+  Int_t            mNSplit; //!
+  Int_t            mNRc; //!
+  Int_t            mNGhost; //!
+  Int_t            mNContam; //!
+  Int_t            mNMatched; //!
+
+  ClassDef(StMiniMcMaker,1)
+};
+  
+  
+
+  
+#endif  
+//
+// $Log $
+//
