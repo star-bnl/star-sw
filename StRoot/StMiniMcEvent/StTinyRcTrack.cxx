dce95445ad24e86889c4173295a0848e43166abd
calderon <>
2002-05-30 01:20:58 +0000

    Classes for use in a general framework for extracting efficiencies from both embedding and full simulations (after GSTAR+TRS+StEvent+StMcEvent+StAssociationMaker) so that the information of the track matches gets stored persistently.

---
+++ StRoot/StMiniMcEvent/StTinyRcTrack.cxx
@@ -0,0 +1,26 @@
+/**
+ * $Id $
+ * \file  StTinyRcTrack.cxx
+ * \brief Implementation of StTinyMcTrack (only a basic constructor and destructor are needed, plus ClassImp for CINT).
+ * 
+ *
+ * \author Bum Choi
+ * \date   March 2001
+ *  
+ */
+#include "StTinyRcTrack.h"
+
+ClassImp(StTinyRcTrack)
+
+StTinyRcTrack::StTinyRcTrack() {}
+
+StTinyRcTrack::~StTinyRcTrack() {}
+//
+// $Log: StTinyRcTrack.cxx,v $
+// Revision 1.1  2002/05/30 01:20:58  calderon
+// Classes for use in a general framework for extracting efficiencies
+// from both embedding and full simulations
+// (after GSTAR+TRS+StEvent+StMcEvent+StAssociationMaker)
+// so that the information of the track matches gets stored persistently.
+//
+//
