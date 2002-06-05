87c2bbe9ceb70ed267fd7429c87889fa8836bbbd
Jennifer L. Klay <jlklay@lbl.gov>
2002-06-05 04:03:38 +0000

    More centrality definitions added

--- StRoot/StHighptPool/Common/Centrality.cc
+++ StRoot/StHighptPool/Common/Centrality.cc
@@ -5,7 +5,7 @@
 // definition -- JLK 15.apr.2002
 
 
-NchCentrality centralityNch(int nCh)
+	NchCentrality centralityNch(int nCh)
 {
 
   // 4-June-2002 JLK
@@ -22,5 +22,25 @@ NchCentrality centralityNch(int nCh)
   else if(nCh>=14) return kEighty;
   else return kTotal;
 
+}
+
+int flowCentrality(int N)
+{
+
+  // 4-June-2002 JLK
+  //Updated with Zhangbu's new numbers for P02gc
+
+  int cent[] = {14,30,56,94,146,217,312,431,510};
+      if (N < cent[0])       { return 0; }
+      else if (N < cent[1])  { return 1; }
+      else if (N < cent[2])  { return 2; }
+      else if (N < cent[3])  { return 3; }
+      else if (N < cent[4])  { return 4; }
+      else if (N < cent[5])  { return 5; }
+      else if (N < cent[6])  { return 6; }
+      else if (N < cent[7])  { return 7; }
+      else if (N < cent[8])  { return 8; }
+      else                        { return 9; }  
+      
 }
 
