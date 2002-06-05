6ea2b99102202a5ff80c544114ada615c4d75dbb
Jennifer L. Klay <jlklay@lbl.gov>
2002-06-05 02:29:05 +0000

    Reordered enum to match flow centrality numbering

--- StRoot/StHighptPool/Common/Centrality.h
+++ StRoot/StHighptPool/Common/Centrality.h
@@ -6,9 +6,11 @@
 #ifndef Centrality_hh
 #define Centrality_hh
 
-enum NchCentrality {kFive, kTen, kTwenty,
-		    kThirty, kForty, kFifty,
-		    kSixty, kSeventy, kEighty, kTotal, kNull, kNoClue};
+//4-Jun-2002 JLK
+//Changed the order of these to match the Flow centrality definitions
+enum NchCentrality {kEighty, kSeventy, kSixty,
+		    kFifty, kForty, kThirty,
+		    kTwenty, kTen, kFive, kTotal, kNull, kNoClue};
 
 NchCentrality centralityNch(int);
 
