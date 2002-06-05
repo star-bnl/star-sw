b4e96da314b6115e2b3351e1031acc52b69d6d49
jklay <>
2002-06-05 04:03:38 +0000

    More centrality definitions added

--- StRoot/StHighptPool/Common/Centrality.h
+++ StRoot/StHighptPool/Common/Centrality.h
@@ -6,12 +6,14 @@
 #ifndef Centrality_hh
 #define Centrality_hh
 
-//4-Jun-2002 JLK
-//Changed the order of these to match the Flow centrality definitions
-enum NchCentrality {kEighty, kSeventy, kSixty,
-		    kFifty, kForty, kThirty,
-		    kTwenty, kTen, kFive, kTotal, kNull, kNoClue};
+enum NchCentrality {kFive, kTen, kTwenty,
+		    kThirty, kForty, kFifty,
+		    kSixty, kSeventy, kEighty, kTotal, kNull, kNoClue};
 
 NchCentrality centralityNch(int);
+
+//Added this for offline centrality determination
+//4-Jun-2002
+int flowCentrality(int);
 
 #endif
