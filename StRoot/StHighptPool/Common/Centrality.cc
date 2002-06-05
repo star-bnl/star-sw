d430b7480fcf9176bab8b42c7c396a8020152a8c
Jennifer L. Klay <jlklay@lbl.gov>
2002-06-05 02:28:17 +0000

    Updated Nch limits with Zhangbus new numbers

--- StRoot/StHighptPool/Common/Centrality.cc
+++ StRoot/StHighptPool/Common/Centrality.cc
@@ -4,16 +4,21 @@
 //NOTE:  This is now the same as what is filled in StHiMicroEvent as the Flow Centrality
 // definition -- JLK 15.apr.2002
 
+
 NchCentrality centralityNch(int nCh)
 {
-  if(nCh>=500) return kFive;
-  else if(nCh>=428) return kTen;
-  else if(nCh>=311) return kTwenty;
-  else if(nCh>=221) return kThirty;
-  else if(nCh>=150) return kForty;
-  else if(nCh>=98) return kFifty;
-  else if(nCh>=59) return kSixty;
-  else if(nCh>=33) return kSeventy;
+
+  // 4-June-2002 JLK
+  //Updated with Zhangbu's new numbers for P02gc
+
+  if(nCh>=510) return kFive;
+  else if(nCh>=431) return kTen;
+  else if(nCh>=312) return kTwenty;
+  else if(nCh>=217) return kThirty;
+  else if(nCh>=146) return kForty;
+  else if(nCh>=94) return kFifty;
+  else if(nCh>=56) return kSixty;
+  else if(nCh>=30) return kSeventy;
   else if(nCh>=14) return kEighty;
   else return kTotal;
 
