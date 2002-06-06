e45b52fc36b6e68a3046bb9259de5c2f73e9b91f
calderon <>
2002-06-06 18:58:30 +0000

    Added $Log$ Added mDedxPts data member, get and set methods, and updated ClassDef(StTinyRcTrack,1) to ClassDef(StTinyRcTrack,2) because of this change for schema evolution

--- StRoot/StMiniMcEvent/StTinyRcTrack.cxx
+++ StRoot/StMiniMcEvent/StTinyRcTrack.cxx
@@ -6,7 +6,7 @@
  *
  * \author Bum Choi
  * \date   March 2001
- *  
+ *  $Log $
  */
 #include "StTinyRcTrack.h"
 
@@ -17,6 +17,11 @@ StTinyRcTrack::StTinyRcTrack() {}
 StTinyRcTrack::~StTinyRcTrack() {}
 //
 // $Log: StTinyRcTrack.cxx,v $
+// Revision 1.2  2002/06/06 18:58:29  calderon
+// Added $Log$
+// Added mDedxPts data member, get and set methods, and updated ClassDef(StTinyRcTrack,1)
+// to ClassDef(StTinyRcTrack,2) because of this change for schema evolution
+//
 // Revision 1.1  2002/05/30 01:20:58  calderon
 // Classes for use in a general framework for extracting efficiencies
 // from both embedding and full simulations
