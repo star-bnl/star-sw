b0df8e13049b3b9526d300c2364d8379cdabdd98
calderon <>
2002-06-06 15:17:49 +0000

    The embedding macro is now changed to write  into a directory structure that sits in /auto/pdsfdv41/starprod/QA/MiniDst/ The perl script is modified to work with the directories where the output embedding jobs are located in the /beta/ disk at PDSF

--- StRoot/StMiniMcMaker/submit.pl
+++ StRoot/StMiniMcMaker/submit.pl
@@ -40,7 +40,9 @@ my $queue = $opt{q} || ($ENV{HOST} =~ /pdsf/ ? "short" : "star_cas");
 my $io = $opt{r} || 40;
 my $max  = $opt{n} || 10000;
 my $count = 0;
-my $scriptDir = "StRoot/StMiniMcMaker/";
+#my $scriptDir = "StRoot/StMiniMcMaker";
+my $scriptDir = "minimcscripts";
+my $workDir = "/auto/pdsfdv34/starspec/calderon/minimctrees";
 my $macroName;
 my $ext = ".geant.root";
 my $user = $opt{u} ? "-u $opt{u}" : '-u bum@rcf.rhic.bnl.gov';
@@ -57,62 +59,71 @@ my $add = ($opt{a} =~ /^\./) ? $opt{a} : (defined $opt{a} ? "$opt{a}." : "");
 print "Using...\n",
   "in dir : $inDir\n",
   "out dir: $outDir\n",
+  "log dir: $logDir\n",
   "libary ver : $library\n";
 
 
 if($type eq 'hijing'){
-  $macroName = 'StMiniHijing.C';
+  $macroName = 'StRoot/StMiniMcMaker/StMiniHijing.C';
 }
 elsif($type eq 'embed'){
-  $macroName = 'StMiniEmbed.C';
+  $macroName = 'StRoot/StMiniMcMaker/StMiniEmbed.C';
 }
 else{
   die "$usage\n";
 }
 
 opendir(DH,$inDir) or die "$inDir : $!\n";
-while(my $file = readdir DH){
-  next if $file =~ /^\./;
-  next unless $file =~ /geant.root/;
 
-  if($opt{m}){
-    next unless $file =~ /$opt{m}/;
-  }
-  (my $found = $file ) =~ s/geant.root/minimc.root/;
-  if($opt{z}){
-    $found =~ /st_physics_(\d+)_(\S+)/;
-    $found = "st_physics_$1.$opt{z}_$2";
-  }
-  unless($opt{o}){
-    next if -e "$outDir/$found";
-  }
-  last if ++$count>$max;
-
-  (my $label = $file) =~ s/$ext$//;
-
-  my $mainFile = "$inDir/$label.geant.root";
-  # find the corresponding dst.root file
-  (my $dst = $mainFile) =~ s/\.geant\.root/\.dst\.root/;
-  unless(-e $dst){
-    print "\tCannot find $dst\n"; next;
-  }
-
-  (my $base = $inDir) =~ s/\/?$//;
-  $base = basename $base; $base =~ s/\.dst$//;
-  my $scriptName = "$scriptDir/$base.$add$label.csh";
-  my $logName = "$logDir/$base.$add$label.log";
-  my $jobName = "$base.$add$label";
-
-  my $cwd = cwd();
-
-  unless($opt{t}){
-    local *FH;
-    open(FH,">$scriptName") or die "$scriptName : $!\n";
+my @all = glob("$inDir/*");
+foreach my $dir1 (@all) {
+  #print "$dir1\n";
+  opendir(DG,$dir1) or die "$dir1 : $!\n";
+  while (my $file = readdir DG) {
+    #print "$file\n";
+    next if $file =~ /^\./;
+    next unless $file =~ /geant.root/;
     
-    my $text=<<FINIS;
+    if($opt{m}){
+      next unless $file =~ /$opt{m}/;
+    }
+    (my $found = $file ) =~ s/geant.root/minimc.root/;
+    if($opt{z}){
+      $found =~ /st_physics_(\d+)_(\S+)/;
+      $found = "st_physics_$1.$opt{z}_$2";
+    }
+    unless($opt{o}){
+      next if -e "$outDir/$found";
+    }
+    last if ++$count>$max;
+    
+    (my $label = $file) =~ s/$ext$//;
+    
+    my $mainFile = "$dir1/$label.geant.root";
+    #print "$mainFile\n";
+    # find the corresponding dst.root file
+    (my $dst = $mainFile) =~ s/\.geant\.root/\.dst\.root/;
+    unless(-e $dst){
+      print "\tCannot find $dst\n"; next;
+    }
+    
+    (my $base = $dir1) =~ s/\/?$//;
+    $base = basename $base; $base =~ s/\.dst$//;
+    my $scriptName = "$scriptDir/$base.$add$label.csh";
+    my $logName = "$logDir/$base.$add$label.log";
+    my $jobName = "$base.$add$label";
+    
+    my $cwd = cwd();
+    
+    unless($opt{t}){
+      local *FH;
+      open(FH,">$scriptName") or die "$scriptName : $!\n";
+      
+      my $text=<<FINIS;
 #!/bin/csh
 
 source \$GROUP_DIR/.starver $library
+cd $workDir
 set start = `date`
 root4star -b <<EOF >&! $logName
 char* mainFile = "$mainFile"
@@ -122,18 +133,18 @@ char* outdir   = "$outDir"
 EOF
 
 FINIS
-    print FH $text;
-    close FH;
-    chmod 0754, $scriptName;
+      print FH $text;
+      close FH;
+      chmod 0754, $scriptName;
+    }
+
+    my $extra;
+    if($ENV{HOST} =~ /pdsf/){
+      $extra = qq{ -R "select[defined(dv${disk}io)] rusage[dv${disk}io=$io]"};
+    }
+    
+    my $command = "bsub -q $queue -J $jobName $extra $scriptName";
+    print $command, "\n";
+    system($command) unless $opt{t};
   }
-
-  my $extra;
-  if($ENV{HOST} =~ /pdsf/){
-    $extra = qq{ -R "select[defined(dv${disk}io)] rusage[dv${disk}io=$io]"};
-  }
-
-  my $command = "bsub -q $queue -J $jobName $extra $scriptName";
-  print $command, "\n";
-  system($command) unless $opt{t};
-
 }
