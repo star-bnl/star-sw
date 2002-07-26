c7013892eae618ca3e7d8642158945ffa1afd0b8
Andrew Allen Rose <aarose@lbl.gov>
2002-07-26 17:22:04 +0000

    Implementation of getDedx() function. User is required to have set the fraction used, and the detector desired.

--- StRoot/Sti/StiDedxCalculator.cxx
+++ StRoot/Sti/StiDedxCalculator.cxx
@@ -1,5 +1,11 @@
 #include <math.h>
 #include <float.h>
+#include "StiKalmanTrackNode.h"
+#include "StiKalmanTrack.h"
+#include "StiHit.h"
+#include "StiDetector.h"
+#include "StiShape.h"
+#include "StDetectorId.h"
 
 #include <algorithm>
 using std::transform;
@@ -10,6 +16,7 @@ using std::accumulate;
 #include <functional>
 using std::less;
 
+#include "StDetectorId.h"
 #include "StiDedxCalculator.h"
 
 
@@ -22,14 +29,15 @@ StiDedxCalculator::StiDedxCalculator()
   truncation fraction (see setFractionUsed(double)).  Dedx has units of
   KeV/cm.
  */
-float StiDedxCalculator::getDedx(const StiTrack* track)
+float StiDedxCalculator::getDedx(const StiKalmanTrack* track)
 {
+  cout <<"In Dedx Calculator"<<endl;
     mVector.clear();
     //hopefully this can be a reference to a vec
     //We assume that the vector contains only nodes with hits!!!!!
-    const StiTrackNodeVec nodes; 
-    //nodes = track->getNodes(); //Claude t.b.d.
+    vector<StiKalmanTrackNode*> nodes = track->getNodes(mDetector); //Claude t.b.d.
 
+    cout <<"Dedx: Got the node vector."<<endl;
     //Transform each node to a double=dedx of it's hit
     //I did it in one line because it should be a little faster than
     //a loop, since the call to NodeDedxCalculator() should be
@@ -48,9 +56,42 @@ float StiDedxCalculator::getDedx(const StiTrack* track)
     return (nPoints>=0.) ? (sum/nPoints) : DBL_MAX;
 }
 
-double NodeDedxCalculator::operator()(const StiTrackNode*)
+double NodeDedxCalculator::operator()(const StiKalmanTrackNode *mNode)
 {
     double dedx=0.;
-    // ... add code to calculate the dedx of each track node w/ a hit ...
+    double dx, ds;
+    //double dx;
+    //double radius, dr;                //radius and thickness of the detector
+    //StThreeVectorD dPlane, dNormal;
+
+    //The real dx for each hit is calculated through the helix 
+    //extrapolation between top and bottom of pad. A fair 
+    //approximation is to use the TanL to get the pathlength.
+
+    //To use the helix extrapolation, first we need the coordinates 
+    //of the top and bottom mid point of the pad in question. The 
+    //detector coordinates are specified as z,r,phi of the center of
+    //the detector, and dx,dy,dz the detector spans.
+    //For the helix extraplolation, we need the global coordinates of a 
+    //point on the top and bottom of the detector, and the normal.
+    //radius = mNode->getDetector()->getPlacement()->getCenterRadius();
+    //dr     = mNode->getDetector()->getShape()->getThickness() / 2.;
+
+    //dNormal.setMagnitude(mNode->getDetector()->getPlacement());
+    //dsBot = mNode->getDetecotr()->getPlacement();
+    //dx = abs( dsTop - dsBot );
+    ds   = mNode->getDetector()->getShape()->getThickness();
+    ds   = ds * ds;
+    dx   = sqrt(ds + ds / (mNode->getTanL() * mNode->getTanL()));
+ 
+
+    //dx is checked here for pathologic values; if the pathlength is
+    //greater than the length of the pad, then there is significant
+    //curvature in the pad. If the pathlength is greater than
+    //length+2*width, then the track must oscillate in the
+    //pad itself.
+    
+    dedx = (mNode->getHit()->getEloss())/dx;
+    cout << "Dedx of hit: "<<dedx<<endl;
     return dedx;
 }
