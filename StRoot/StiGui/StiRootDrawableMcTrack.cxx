9698f62d4c7ace6567f7a53cd7d3dd4ebce18109
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2002-09-05 05:48:30 +0000

    various changes

--- StRoot/StiGui/StiRootDrawableMcTrack.cxx
+++ StRoot/StiGui/StiRootDrawableMcTrack.cxx
@@ -15,6 +15,9 @@ using namespace std;
 #include "StiGui/StiTPolyLine3D.h"
 #include "StiGui/StiRootDisplayManager.h"
 #include "StiGui/StiRootDrawableMcTrack.h"
+#include "StMcTrack.hh"
+#include "StMcContainers.hh"
+#include "StMcTpcHit.hh"
 
 StiRootDrawableMcTrack::StiRootDrawableMcTrack()
   : StiRootDrawableTrack()
@@ -34,6 +37,7 @@ void StiRootDrawableMcTrack::getNewState()
 
 void StiRootDrawableMcTrack::fillHitsForDrawing()
 {
+  cout << "StiRootDrawableMcTrack::fillHitsForDrawing() - start " << endl;
   _line->clear();
   _hits->clear();
   _line->clearLine();
@@ -41,16 +45,55 @@ void StiRootDrawableMcTrack::fillHitsForDrawing()
   //Loop over nodes by hand (faster than using StiMcTrack interface)
   //This is essentailly the guts of an interpolation routine that should become
   // a class at some point.
-  double step = 1.; //cm
+  //double step = 1.; //cm
+
+  cout << "StiRootDrawableMcTrack::fillHitsForDrawing() - cleared " << endl;
   
-  _line->fillHitsForDrawing();
+  const StPtrVecMcTpcHit & tpcHits = mcTrack->tpcHits();
+  StMcTpcHitConstIterator hitIter;
+  cout << "StiRootDrawableMcTrack::fillHitsForDrawing() - have tpcHits " << endl;
+
+  for (hitIter= tpcHits.begin();hitIter!=tpcHits.end();hitIter++)
+    {
+      StThreeVectorF position = (*hitIter)->position();
+      _hits->push_back( position.x() );
+      _hits->push_back( position.y() );
+      _hits->push_back( position.z() );
+    }
+  cout << "StiRootDrawableMcTrack::fillHitsForDrawing() - tpcHits filled into _hits" << endl;
+  
+  ///_line->fillHitsForDrawing();
   _hits->fillHitsForDrawing();
   
   //These get automatically removed from display each event
   //The display dynamically shrinks temp objects each event (tracks, hits, etc)
-  if (!_line->isAdded())
-    StiRootDisplayManager::instance()->addDrawable( _line );
+  //if (!_line->isAdded())
+  //  StiRootDisplayManager::instance()->addDrawable( _line );
   if (!_hits->isAdded()) 
     StiRootDisplayManager::instance()->addDrawable( _hits );
+  cout << "StiRootDrawableMcTrack::fillHitsForDrawing() - Done"<<endl;
   return;
+}
+
+void StiRootDrawableMcTrack::reset()
+{
+  this->StiMcTrack::reset();
+  this->StiRootDrawableTrack::reset();
+}
+
+StiRootDrawableMcTrackFactory::StiRootDrawableMcTrackFactory(const string& newName,
+							     int original,
+							     int incremental, 
+							     int maxInc)
+  : StiMcTrackFactory(newName, 
+		      original, 
+		      incremental, 
+		      maxInc)
+{
+  initialize();
+}
+
+StiRootDrawableMcTrackFactory::~StiRootDrawableMcTrackFactory()
+{
+  // cout <<"StiRootDrawableMcTrackFactory::~StiRootDrawableMcTrackFactory()"<<endl;
 }
