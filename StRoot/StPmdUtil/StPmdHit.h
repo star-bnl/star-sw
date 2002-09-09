00b4a7b192b9ce3b40495bd470426d25d54e0919
subhasis <>
2002-09-09 11:28:12 +0000

    ADC added

--- StRoot/StPmdUtil/StPmdHit.h
+++ StRoot/StPmdUtil/StPmdHit.h
@@ -3,9 +3,11 @@
  *\author
 */
 /*************************************************
- * $Id: StPmdHit.h,v 1.1 2002/08/27 12:21:01 subhasis Exp $
+ * $Id: StPmdHit.h,v 1.2 2002/09/09 11:27:59 subhasis Exp $
  * 
  * Author: Subhasis Chattopadhyay
+ * Revision: Prem Ghosh
+ * 5/9/02: ADC functionality added.
  * 
  *************************************************
  *
@@ -13,8 +15,8 @@
  *
  **************************************************
  * $Log: StPmdHit.h,v $
- * Revision 1.1  2002/08/27 12:21:01  subhasis
- * First version
+ * Revision 1.2  2002/09/09 11:27:59  subhasis
+ * ADC added
  *
  **************************************************/
 #ifndef STAR_StPmdHit
@@ -33,6 +35,7 @@ private:
   Int_t             mRow;      //! row no. in the supermodule 
   Int_t             mCol;      //! col no. in the supermodule
   Float_t           mEdep;     //! energy deposition
+  Int_t           mAdc;     //! adc
 public: 
   StPmdHit();                      //! A constructor
   StPmdHit(TArrayI*);              //! A constructor
@@ -45,11 +48,13 @@ public:
   Int_t           Row() const;         //! function for row
   Int_t           Column() const;      //! function for col
   Float_t           Edep() const;      //! function for edep
+  Int_t           Adc() const;      //! function for adc
   void           setGsuper(Int_t);
   void            setSubDetector(Int_t);
   void            setRow(Int_t);
   void            setColumn(Int_t);
   void            setEdep(Float_t);
+  void            setAdc(Int_t);
   virtual void      print(ostream *os);
   virtual void      Browse(TBrowser *b);
   ClassDef(StPmdHit,1)
@@ -59,16 +64,20 @@ public:
 inline              StPmdHit::~StPmdHit(){ /* Nobody */ }
 inline   Int_t    StPmdHit::Gsuper() const {return mGsuper;}
 inline   Int_t    StPmdHit::module()  {if(mGsuper<=12){return mGsuper;}
-                                        else {return -1;}}
+                                     else {return -1;}}
 inline   Int_t    StPmdHit::SubDetector() const     {return mSubdet;} 
 inline   Int_t    StPmdHit::Row() const     {return mRow;} 
 inline   Int_t    StPmdHit::Column() const     {return mCol;} 
 inline   Float_t    StPmdHit::Edep() const  {return mEdep;}
+inline   Int_t    StPmdHit::Adc() const  {return mAdc;}
 inline void  StPmdHit::setGsuper(Int_t var) {mGsuper = var;}
 inline void  StPmdHit::setSubDetector(Int_t var)  {mSubdet = var;}
 inline void  StPmdHit::setRow(Int_t var)  {mRow = var;}
 inline void  StPmdHit::setColumn(Int_t var)  {mCol = var;}
 inline void  StPmdHit::setEdep(Float_t var)  {mEdep = var;}
+inline void  StPmdHit::setAdc(Int_t var)  {mAdc = var;}
 
 #endif
+
+
 
