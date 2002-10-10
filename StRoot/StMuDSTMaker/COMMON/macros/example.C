95fe0804529af84bb39ce508abba115dafcdde3e
laue <>
2002-10-10 22:20:16 +0000

    update

--- StRoot/StMuDSTMaker/COMMON/macros/example.C
+++ StRoot/StMuDSTMaker/COMMON/macros/example.C
@@ -30,32 +30,38 @@ void example() {
   StMuDebug::setLevel(0);  // switch of some debug output
 
   int iret=0;
-  maker = new StMuDstMaker(0,0,"MuDST/central/ReversedFullField/runs/","","MuDst.root",1);   // set up maker in read mode
+  maker = new StMuDstMaker(0,0,"","AuAu200.lis","MuDst.root",2);   // set up maker in read mode
+  StMuDbReader* db = StMuDbReader::instance();
+  db->addDb("/star/u/laue/afsWork/P02gc.db");
+  db->addDb("/star/u/laue/afsWork/P02gd.db");
+
   iret = maker->Make();  // read an event 
   iret = maker->Make();  
 
+
   StMuEvent* e = maker->muDst()->event();
+  StL0Trigger t;
+  StEventInfo info;
   if (e) {
-  StL0Trigger &t=e->l0Trigger();
-  StEventInfo &info=e->eventInfo();
+  t=e->l0Trigger();
+  info=e->eventInfo();
   }
 
   int n = maker->muDst()->primaryTracks()->GetEntries();  // get number of primary tracks
   
   for (int i=0; i<n; i++) {
     StMuTrack* primaryTrack = maker->muDst()->primaryTracks(i);     // get pointer to primary track
-    printf("momentumPrimary=%8f ",primaryTrack->p().mag());
-    if (primaryTrack->globalTrack()) {
-      printf("momentumGlobal=%8f ratio=%8f first-helix=%8f  last-outerHelix=%8f  \n",
-	    primaryTrack->globalTrack()->p().mag(),
-	    primaryTrack->p().mag()/primaryTrack->globalTrack()->p().mag(),
-	    (primaryTrack->globalTrack()->firstPoint() - primaryTrack->globalTrack()->helix().origin()).mag(),
-	    (primaryTrack->globalTrack()->lastPoint() - primaryTrack->globalTrack()->outerHelix().origin()).mag()
-	    );
-    }
+    StMuTrack* global = primaryTrack->globalTrack();
+    cout << endl;
+    StMuTrack*  tt = primaryTrack;
+    printf("momentum primary=%8f first-helix=%8f  last-outerHelix=%8f length=%8f lengthMeasured=%8f \n",
+	   tt->p().mag(), (tt->firstPoint() - tt->helix().origin()).mag(), (tt->lastPoint() - tt->outerHelix().origin()).mag(), tt->length(), tt->lengthMeasured() );
+    StMuTrack*  tt = global;
+    printf("primary momentum=%8f first-helix=%8f  last-outerHelix=%8f length=%8f lengthMeasured=%8f \n",
+	   tt->p().mag(), (tt->firstPoint() - tt->helix().origin()).mag(), (tt->lastPoint() - tt->outerHelix().origin()).mag(), tt->length(), tt->lengthMeasured() );
   }
   printf("EVENT id=%d, runId=%d  unix time=%d trigWord=0x%0x bXing=%d spinBits=%d nPrim=%d\n", info.id(), info.runId(), info.time(), t.triggerWord(),t.bunchCrossingId(), t.spinBits(),n);
-
+  
 
 }
 
