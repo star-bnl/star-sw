ca43147eabe17846e959bab7e5d3957ea36a98b1
fsimon <>
2002-10-16 12:29:18 +0000

    Include ftpcAmpSlope, ftpcAmpOffset and ftpcTimeOffset in Database access permits usage of gain factors and time offset in the simulator

--- StRoot/StFtpcSlowSimMaker/StFtpcSlowSimMaker.h
+++ StRoot/StFtpcSlowSimMaker/StFtpcSlowSimMaker.h
@@ -1,5 +1,9 @@
-// $Id: StFtpcSlowSimMaker.h,v 1.6 2001/10/29 12:56:55 jcs Exp $
+// $Id: StFtpcSlowSimMaker.h,v 1.7 2002/10/16 12:29:18 fsimon Exp $
 // $Log: StFtpcSlowSimMaker.h,v $
+// Revision 1.7  2002/10/16 12:29:18  fsimon
+// Include ftpcAmpSlope, ftpcAmpOffset and ftpcTimeOffset in Database access
+// permits usage of gain factors and time offset in the simulator
+//
 // Revision 1.6  2001/10/29 12:56:55  jcs
 // select FTPC drift maps according to flavor of magnetic field
 //
@@ -30,39 +34,50 @@
 #ifndef StMaker_H
 #include "StMaker.h"
 #endif
+
 class St_ftpcClusterPars;
-class St_ftpcSlowSimGas;
-class St_ftpcSlowSimPars;
+class St_ftpcFastSimGas;
+class St_ftpcFastSimPars;
 class St_ftpcDimensions;
 class St_ftpcEField;
 class St_ftpcVDrift;
 class St_ftpcDeflection;
 class St_ftpcdVDriftdP;
 class St_ftpcdDeflectiondP;
-class St_ftpcGas;
+class St_ftpcAmpSlope;
+class St_ftpcAmpOffset;
+class St_ftpcTimeOffset;
 class St_ftpcDriftField;
+class St_ftpcGas;
 class St_ftpcElectronics;
+class St_ftpcSlowSimGas;
+class St_ftpcSlowSimPars;
 
 class TH1F;
 class TH2F;
 
 class StFtpcSlowSimMaker : public StMaker {
  private:
-  // static Char_t m_VersionCVS = "$Id: StFtpcSlowSimMaker.h,v 1.6 2001/10/29 12:56:55 jcs Exp $";
+  // static Char_t m_VersionCVS = "$Id: StFtpcSlowSimMaker.h,v 1.7 2002/10/16 12:29:18 fsimon Exp $";
   // Int_t         m_mode;        // mode 1 = primaries;
-  St_ftpcClusterPars   *m_clusterpars;    //!
-  St_ftpcSlowSimGas    *m_slowsimgas;     //!
-  St_ftpcSlowSimPars   *m_slowsimpars;    //!
-  St_ftpcDimensions    *m_dimensions;     //!
-  St_ftpcEField        *m_efield;         //!
-  St_ftpcVDrift        *m_vdrift;         //!
-  St_ftpcDeflection    *m_deflection;     //!
-  St_ftpcdVDriftdP     *m_dvdriftdp;      //!
-  St_ftpcdDeflectiondP *m_ddeflectiondp;  //!
-  St_ftpcGas           *m_gas;            //! 
-  St_ftpcDriftField    *m_driftfield;     //!
-  St_ftpcElectronics   *m_electronics;    //!
-
+   St_ftpcClusterPars   *m_clusterpars;           //!
+   //St_ftpcFastSimGas    *m_fastsimgas;            //!
+   //St_ftpcFastSimPars   *m_fastsimpars;           //!
+   St_ftpcSlowSimGas    *m_slowsimgas;     //!
+   St_ftpcSlowSimPars   *m_slowsimpars;    //!   
+   St_ftpcDimensions    *m_dimensions;            //!
+   St_ftpcEField        *m_efield;                //!
+   St_ftpcVDrift        *m_vdrift;                //!
+   St_ftpcDeflection    *m_deflection;            //!
+   St_ftpcdVDriftdP     *m_dvdriftdp;             //!
+   St_ftpcdDeflectiondP *m_ddeflectiondp;         //!
+   St_ftpcAmpSlope      *m_ampslope;              //!
+   St_ftpcAmpOffset     *m_ampoffset;             //!
+   St_ftpcTimeOffset    *m_timeoffset;            //!
+   St_ftpcDriftField    *m_driftfield;            //!
+   St_ftpcGas           *m_gas;                   //!
+   St_ftpcElectronics   *m_electronics;           //! 
+ 
   void                  MakeHistograms();// Histograms for FTPC slow simulator
   
  protected:
@@ -77,7 +92,7 @@ class StFtpcSlowSimMaker : public StMaker {
   virtual Int_t  Make();
   // virtual void Set_mode       (Int_t   m =      2){m_mode       = m;} // *MENU*
   virtual const char *GetCVS() const
-    {static const char cvs[]="Tag $Name:  $ $Id: StFtpcSlowSimMaker.h,v 1.6 2001/10/29 12:56:55 jcs Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+    {static const char cvs[]="Tag $Name:  $ $Id: StFtpcSlowSimMaker.h,v 1.7 2002/10/16 12:29:18 fsimon Exp $ built "__DATE__" "__TIME__ ; return cvs;}
   
   ClassDef(StFtpcSlowSimMaker, 1)   //StAF chain virtual base class for Makers
 };
