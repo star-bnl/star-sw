9b81cc9ec534bbc762476c86a84e24fef8775f73
Jerome Lauret <jeromel@bnl.gov>
2002-11-06 00:37:45 +0000

    Corrected miss-feature. Since the build on non-Linux platform is explicitly made for the debug version only, the include/ etc/ icons/ directory need to point to that version by default. cint/ as well (unfortunatly).

--- mgr/MakeRootDir.pl
+++ mgr/MakeRootDir.pl
@@ -62,11 +62,11 @@ for ( my $iter=0; $iter < 2; $iter++ ) {
    # backward infrastructure compatible layout (AFS only).
    if( ! -l "$lib")   { symlink("$ANFS/$root/lib","$lib" );}
    if( ! -l "$bin")   { symlink("$ANFS/$root/bin","$bin" );}
-   if( $iter == 1){
+   if( $iter == 0){
        if( ! -l "etc")    { symlink("$ANFS/$root/etc","etc"  );}
-       if( ! -l "cint")   { symlink("$ANFS/$root/cint","cint");}
        if( ! -l "icons")  { symlink("$ANFS/$root/icons","icons");}
        if( ! -l "include"){ symlink("$ANFS/$root/include","include");}
+       if( ! -l "cint")   { symlink("$ANFS/$root/cint","cint");}
    }
 
    if( ! $FLINK ){
