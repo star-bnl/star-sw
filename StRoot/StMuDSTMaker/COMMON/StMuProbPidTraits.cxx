6433e6966c04270e9da8c30e56301bcad39029d8
Frank Laue <laue@bnl.gov>
2002-11-18 20:29:01 +0000

    New class. Wrapper for Yuri's new StProbPidTraits.

---
+++ StRoot/StMuDSTMaker/COMMON/StMuProbPidTraits.cxx
@@ -0,0 +1,20 @@
+/***************************************************************************
+ *
+ * $Id: StMuProbPidTraits.cxx,v 1.1 2002/11/18 20:28:52 laue Exp $
+ * Author: Frank Laue, BNL, laue@bnl.gov
+ *
+ ***************************************************************************/
+
+#include "StMuProbPidTraits.h"
+
+ClassImp(StMuProbPidTraits)
+
+
+/***************************************************************************
+ *
+ * $Log: StMuProbPidTraits.cxx,v $
+ * Revision 1.1  2002/11/18 20:28:52  laue
+ * New class. Wrapper for Yuri's new StProbPidTraits.
+ *
+ *
+ **************************************************************************/
