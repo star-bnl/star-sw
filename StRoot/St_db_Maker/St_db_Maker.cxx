d25b8b43902e6196d9d9757288ddc0838a2e3016
Jerome Lauret <jeromel@bnl.gov>
2002-11-27 03:12:26 +0000

    Expand array for a new geometry time stamp. Beware, timestamp not fixed yet (had to revert to the preceeding one as the new timeline does not exists but any stamp ended with garbage).Next commit will make it right ...

--- StRoot/St_db_Maker/St_db_Maker.cxx
+++ StRoot/St_db_Maker/St_db_Maker.cxx
@@ -1,6 +1,11 @@
 //*-- Author :    Valery Fine(fine@bnl.gov)   10/08/98 
-// $Id: St_db_Maker.cxx,v 1.60 2002/06/14 14:48:00 jeromel Exp $
+// $Id: St_db_Maker.cxx,v 1.61 2002/11/27 03:12:26 jeromel Exp $
 // $Log: St_db_Maker.cxx,v $
+// Revision 1.61  2002/11/27 03:12:26  jeromel
+// Expand array for a new geometry time stamp. Beware, timestamp not fixed yet
+// (had to revert to the preceeding one as the new timeline does not exists but
+// any stamp ended with garbage).Next commit will make it right ...
+//
 // Revision 1.60  2002/06/14 14:48:00  jeromel
 // Last change moved after Use()
 //
@@ -155,16 +160,16 @@ static Int_t AliasTime(const char *alias);
 static const char *aliases[]={
 "sd97",   "sd98",   "year_1a","year_1b","year_1c",
 "es99",   "er99",   "dc99"   ,"year_1d","year_1e",
-"year_1h","year_2a", "year_2b", "year2001", 0};   
+"year_1h","year_2a", "year_2b", "year2001", "year2003", 0};   
 
 static const int   dates[]=  {
 19970101, 19980101, 19990101, 19990501, 19991001,
 19990615, 19990616, 19991206, 19991101, 19991201,
-20000614, 20010610, 20010501, 20010615, 0};
+20000614, 20010610, 20010501, 20010615, 20010615, 0}; // should be 20021115, 0};
 static const int   times[]=  {
        0,        0,        0,        0,        0,
        0,   120000,    80000,        0,        0,
-  175430,        0,        0,        0,        0};
+  175430,        0,        0,        0,        0, 0};
 
 enum eDBMAKER {kUNIXOBJ = 0x2000};
 
