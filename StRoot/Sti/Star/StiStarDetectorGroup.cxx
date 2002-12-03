f7d79fe488c7c86a7e27016c844d1df0e100eb75
pruneau <>
2002-12-03 14:54:36 +0000

    Adding Star files for version 2.0

---
+++ StRoot/Sti/Star/StiStarDetectorGroup.cxx
@@ -0,0 +1,17 @@
+#include <stdexcept>
+#include "StEvent.h"
+#include "Sti/Star/StiStarDetectorGroup.h"
+#include "Sti/Star/StiStarDetectorBuilder.h"
+#include "Sti/StiHitLoader.h"
+
+StiStarDetectorGroup::StiStarDetectorGroup(bool active)
+  : StiDetectorGroup<StEvent>("STAR",
+			      0,
+			      new StiStarDetectorBuilder(),
+			      0,
+			      0)
+{}
+
+StiStarDetectorGroup::~StiStarDetectorGroup()
+{}
+
