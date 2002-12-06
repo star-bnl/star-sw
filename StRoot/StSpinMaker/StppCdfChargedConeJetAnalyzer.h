6d932f35dd7f4b3d4245bf52b3d053b5998a0100
mmiller <>
2002-12-06 19:40:52 +0000

    Add first version of StppCdfChargedConeJetaAnalyzer.  Also, add StppEventReader (for viewer) and StppuDstReader (for general use).  Add first version of StppJetAssociator.  Add ana2.F to fix library load problem.

---
+++ StRoot/StSpinMaker/StppCdfChargedConeJetAnalyzer.h
@@ -0,0 +1,60 @@
+//StppCdfChargedConeJetAnalyzer.h
+//M.L. Miller (Yale Software)
+//12/02
+
+#ifndef StppCdfChargedConeJetAnalyzer_HH
+#define StppCdfChargedConeJetAnalyzer_HH
+
+#include "TObject.h"
+#include "StJetFinder/StJetFinder.h"
+#include "StSpinMaker/StppJetAnalyzer.h"
+
+class StKtCluJetFinder;
+class StConeJetFinder;
+class AbstractFourVec;
+class StProtoJet;
+class StppEvent;
+class TH1;
+class TFile;
+class TNtuple;
+
+class StppCdfChargedConeJetAnalyzer : public StppJetAnalyzer
+{
+public:
+    typedef StJetFinder::JetList JetList;
+    typedef list<AbstractFourVec*> FourList;
+    
+    StppCdfChargedConeJetAnalyzer(int, double, double, int, double, double);
+    virtual ~StppCdfChargedConeJetAnalyzer();
+
+    void setConeR(double v) { mFinder->setR(v); };
+    void setConeSeedEtMin(double v) { mFinder->setSeedEtMin(v); };
+    void setConeAssocEtMin(double v) { mFinder->setAssocEtMin(v); };
+
+    ///Let jet wander to minimum?
+    void setPerformMinimization(bool v);
+    bool performMinimization();
+        
+    ///Add seeds at midpoints?
+    void setAddMidpoints(bool v);
+    bool addMidpoints();
+        
+    ///Do Split/Merge step?
+    void setDoSplitMerge(bool v);
+    bool doSplitMerge();
+
+    void setSplitFraction(double v);
+    double splitFraction(void);
+
+    ///Use r/sqrt(2) for iteration search?
+    void setDoMidpointFix(bool v);
+    bool doMidpointFix() const;
+    
+    ///Require stable midpoints?
+    void setRequireStableMidpoints(bool v);
+    bool requiredStableMidpoints() const;
+
+    ClassDef(StppCdfChargedConeJetAnalyzer,1)
+};
+
+#endif
