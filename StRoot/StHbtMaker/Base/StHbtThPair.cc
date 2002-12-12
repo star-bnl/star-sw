1ab7b073c6e5b9e38747ca48a35b9f8eed4c02e8
kisiel <>
2002-12-12 17:04:49 +0000

    Add pair variables calculation to StHbtTHPair

--- StRoot/StHbtMaker/Base/StHbtThPair.cc
+++ StRoot/StHbtMaker/Base/StHbtThPair.cc
@@ -15,6 +15,7 @@
 
 #include <iostream.h>  
 #include <strstream.h>
+#include <stdlib.h>
 
 #include "StHbtMaker/Base/StHbtThPair.hh"
 #include "StHbtMaker/Base/StHbtFsiWeight.hh"
@@ -29,12 +30,69 @@ StHbtThPair::StHbtThPair(){
   mMeasPair=0;
   mWeight=0;
   mWeightNum=mWeightDen=1.;
-  mWeightOk=false;};
+  mWeightOk=false;
+  mPairPurity=1;
+};
+
+void StHbtThPair::setVariables(const StHbtPair*){
+  /* no-op */
+}
+
+void StHbtThPair::setMomRes1(int aPid){
+  if(aPid==7 || aPid==8){// pion
+    mMomRes1=1;
+    mPLoss1[0]=-0.00275; // (PRec-Pmc)P = [0]+[1]*P^[2]
+    mPLoss1[1]=-0.000335;
+    mPLoss1[2]=-2.052;
+    mPtRes1[0]=0.0178; // Dpt/pt = [0] + [1]*Pt^[2]
+    mPtRes1[1]=7.95e-5;      
+    mPtRes1[2]=-2.273;
+    mPhiRes1[0]=0.0642; // DPhi = [0] +[1]*P^[2]
+    mPhiRes1[1]=0.0322;
+    mPhiRes1[2]=-1.511;
+    mThetaRes1[0]=0.0779; // DTheta = [0] + P^[2]
+    mThetaRes1[1]=0.0445;
+    mThetaRes1[2]=-1.528;
+  }
+  if(aPid==11 || aPid==12){//kaon
+    mMomRes1=1;
+  }
+}
+void StHbtThPair::setMomRes2(int aPid){
+  if(aPid==7 || aPid==8){// pion
+    mMomRes2=1;
+    mPLoss2[0]=-0.00275; // (PRec-Pmc)P = [0]+[1]*P^[2]
+    mPLoss2[1]=-0.000335;
+    mPLoss2[2]=-2.052;
+    mPtRes2[0]=0.0178; // Dpt/pt = [0] + [1]*Pt^[2]
+    mPtRes2[1]=7.95e-5;      
+    mPtRes2[2]=-2.273;
+    mPhiRes2[0]=0.0642; // DPhi = [0] +[1]*P^[2]
+    mPhiRes2[1]=0.0322;
+    mPhiRes2[2]=-1.511;
+    mThetaRes2[0]=0.0779; // DTheta = [0] + P^[2]
+    mThetaRes2[1]=0.0445;
+    mThetaRes2[2]=-1.528;
+  }
+  if(aPid==11 || aPid==12){//kaon
+    mMomRes2=1;
+  }
+}
+
 
 void StHbtThPair::UpdateWeight() {
   if (mWeight) {
     mWeightNum=mWeight->GetWeight(this);
+    mWeightNum=(mWeightNum-1.)*mPairPurity+1.;
     mWeightDen=mWeight->GetWeightDen();
+    if(mWeightNum<=0. || mWeightNum>1000.){
+      ostrstream tCom;
+      tCom << "echo ---> " << mWeightNum << " " 
+	   << mEmPoint1 << " " << mEmPoint2 << " " 
+	   << mMomentum1 << " " << mMomentum2 << " >> Err.txt" << ends;
+      system(tCom.str());
+      mWeightNum=1;
+    }
   } else {
     cout << "StHbtThPair Error - No Weight Generator plugged - set to 1 " <<endl;
     mWeightNum=mWeightDen=1.;
@@ -121,3 +179,101 @@ double StHbtThPair::RealqLongPf() const
  return(this->RealqLongCMS());
 }
 
+//___________________________________
+
+void StHbtThPair::calcMomParameters() const{ // fortran like function! faster?
+  mMomParCalculated=1;
+
+  double tPx1(mMomentum1->px());
+  double tPy1(mMomentum1->py());
+  double tPz1(mMomentum1->pz());
+  double tE1(mMomentum1->e());
+  double tPx(tPx1+mMomentum2->px());
+  double tPy(tPy1+mMomentum2->py());
+  double tPz(tPz1+mMomentum2->pz());
+  double tE(tE1 +mMomentum2->e());
+  mPt = tPx*tPx + tPy*tPy;
+  double tP = tPz*tPz;
+  double tMt = tE*tE - tP;
+  tP += mPt;
+  tP = sqrt(tP);
+  double tM =   sqrt(tMt - mPt);
+  tMt = sqrt(tMt);
+  mPt = sqrt(mPt);
+  mBetat = mPt/tMt;
+
+  // Boost to LCMS
+  double tBeta = tPz/tE;
+  double tGamma = tE/tMt;	    
+  mKStarLong = tGamma * (tPz1 - tBeta * tE1);
+  double tE1L = tGamma * (tE1  - tBeta * tPz1);
+
+  // Rotate in transverse plane
+  mKStarOut  = ( tPx1*tPx + tPy1*tPy)/mPt;
+  mKStarSide = (-tPx1*tPy + tPy1*tPx)/mPt;
+
+  // Boost to pair cms
+  mKStarOut = tMt/tM * (mKStarOut - mPt/tMt * tE1L);
+
+  mKStar = sqrt(mKStarOut*mKStarOut+mKStarSide*mKStarSide+
+		mKStarLong*mKStarLong);
+
+  mCVK = (mKStarOut*mPt + mKStarLong*tPz)/mKStar/mCVK;
+  mKStarLong = (tPz>=0)* mKStarLong - (tPz<0)*mKStarLong;
+
+}
+
+
+void StHbtThPair::calcPosParameters() const{ // fortran like function! faster?
+  mPosParCalculated=1;
+
+  double tPx = mMomentum1->px()+mMomentum2->px();
+  double tPy = mMomentum1->py()+mMomentum2->py();
+  double tPz = mMomentum1->pz()+mMomentum2->pz();
+  double tE = mMomentum1->e()+mMomentum2->e();
+  double tPt = tPx*tPx + tPy*tPy;
+  double tMt = tE*tE - tPz*tPz;
+  double tM =   sqrt(tMt - tPt);
+  tMt = sqrt(tMt);
+  tPt = sqrt(tPt);
+   mUt = tPt/tMt;
+   
+  double tDX = mEmPoint1->x()-mEmPoint2->x();
+  double tDY = mEmPoint1->y()-mEmPoint2->y();
+  mRLong = mEmPoint1->z()-mEmPoint2->z();
+  mDTime = mEmPoint1->t()-mEmPoint2->t();
+  
+  mRTrans = tDX>0.? sqrt(tDX*tDX+tDY*tDY) : -1.*sqrt(tDX*tDX+tDY*tDY);
+  mROut = (tDX*tPx + tDY*tPy)/tPt;
+  mRSide = (-tDX*tPy + tDY*tPx)/tPt;
+  mRSidePairCMS = mRSide;
+
+  // Lab -> LCMS -> PRF method
+  double tBeta = tPz/tE;
+  double tGamma = tE/tMt;
+  mRLongPairCMS = tGamma*(mRLong - tBeta* mDTime);
+  mDTimePairLCMS = tGamma*(mDTime - tBeta* mRLong);
+  tBeta = tPt/tMt;
+  tGamma = tMt/tM;
+  mROutPairCMS = tGamma*(mROut - tBeta* mDTimePairLCMS);
+  mDTimePairCMS = tGamma*(mDTimePairLCMS - tBeta* mROut);
+  mRStar = sqrt(mROutPairCMS*mROutPairCMS + mRSidePairCMS*mRSidePairCMS +
+		mRLongPairCMS*mRLongPairCMS);
+
+
+  /* 
+     // Lab -> PRF method
+  double tRS12 = DX*Px+DY*Py+mRLong*Pz;
+  double tH1 = (tRS12/(E+M) - mDTime) / M;
+  double DXPairCMS = DX + Px* tH1;
+  double DYPairCMS = DY + Py* tH1;
+  mRLongPairCMS = mRLong + Pz* tH1;
+  //mRLong = Pz*mRLong>0? mRLong : -mRLong;
+  //mRLongPairCMS = Pz*mRLongPairCMS>0? mRLongPairCMS : -mRLongPairCMS;
+  mDTimePairCMS = (E* mDTime - tRS12) / M;
+  mRStar = sqrt(DXPairCMS*DXPairCMS + DYPairCMS*DYPairCMS +
+		mRLongPairCMS * mRLongPairCMS);
+  mROutPairCMS = (DXPairCMS*Px + DYPairCMS*Py)/Ptrans;
+  mRSidePairCMS = (-DXPairCMS*Py + DYPairCMS*Px)/Ptrans;
+  */
+}
--- StRoot/StHbtMaker/Base/StHbtThPair.hh
+++ StRoot/StHbtMaker/Base/StHbtThPair.hh
@@ -32,11 +32,11 @@ class StHbtFsiWeight;
 
 class StHbtThPair {
 public:
+
   StHbtThPair();
   virtual ~StHbtThPair(){/* no-op */};
 
-  virtual void Set(const StHbtPair*)=0;
-
+  virtual void Set(const StHbtPair*);
   virtual void SetWeight(StHbtFsiWeight*);
 
   virtual const StHbtLorentzVector* GetRealMomentum1() const;
@@ -50,40 +50,111 @@ public:
   virtual const StHbtPair* GetMeasPair() const;
   virtual double GetWeightNum() ;
   virtual double GetWeightDen() ;
-
   virtual StHbtString Report();  
-
   virtual double RealQInv() const;
-
-  // Bertsch-Pratt momentum components in Pair Frame - written by Bekele/Humanic
   double RealqSideCMS() const;
   double RealqOutCMS() const;
   double RealqLongCMS() const;
-  
   double RealqSidePf() const;
   double RealqOutPf() const;
   double RealqLongPf() const;
 
+  // Fabrice private
+  virtual void set(const StHbtPair*);
+  void setMomRes1(int pid);
+  void setMomRes2(int pid);
+  void setPairPurity(double aPairPurity);
+  virtual StHbtLorentzVector const* getMomentum1() const;
+  virtual StHbtLorentzVector const* getMomentum2() const;
+  virtual StHbtLorentzVector const* getEmPoint1() const;
+  virtual StHbtLorentzVector const* getEmPoint2() const;
+  virtual int getPid1() const;
+  virtual int getPid2() const;
+  virtual double QInv() const;
+
+  double KStarSide() const;
+  double KStarOut() const;
+  double KStarLong() const;
+  double KStar() const;
+  double CVK() const;
+  double KStarFlipped() const;
+  double CVKFlipped() const;  
+  double RStar() const;
+  double RTrans() const;
+  double ROut() const;
+  double RLong() const;
+  double RSide() const;
+  double DTime() const;
+  double DTimePairLCMS() const;
+  double ROutPairCMS() const;
+  double RLongPairCMS() const;
+  double RSidePairCMS() const;
+  double DTimePairCMS() const;
+
+  double Betat() const;
+  double Ut() const;
+  double Pt() const;
+  
 protected:
+  virtual void setVariables(const StHbtPair*);
+
   const StHbtLorentzVector* mMomentum1;//!
   const StHbtLorentzVector* mMomentum2;//!
-
-  const StHbtLorentzVector* mEmPoint1;//!
-  const StHbtLorentzVector* mEmPoint2;//!
-  
+  StHbtLorentzVector* mEmPoint1;//!
+  StHbtLorentzVector* mEmPoint2;//!  
   int mPid1;//!
   int mPid2;//!
 
   const StHbtPair* mMeasPair;//!
 
   StHbtFsiWeight* mWeight;//!
-
   double mWeightNum;
   double mWeightDen;
-
   bool mWeightOk;
-
   void UpdateWeight();
+  
+  mutable short mMomParCalculated;
+  void calcMomParameters() const;
+  mutable double mKStarSide;
+  mutable double mKStarOut;
+  mutable double mKStarLong;
+  mutable double mKStar;
+  mutable double mCVK;
+  mutable double mKStarFlipped;
+  mutable double mCVKFlipped;  
+  mutable double mBetat;
+  mutable double mUt;
+  mutable double mPt;
+
+  mutable short mPosParCalculated;
+  void calcPosParameters() const;
+  mutable double mRStar;
+  mutable double mRTrans;
+  mutable double mROut;
+  mutable double mRSide;
+  mutable double mRLong;
+  mutable double mDTime;
+  mutable double mDTimePairLCMS;
+  mutable double mROutPairCMS;
+  mutable double mRSidePairCMS;
+  mutable double mRLongPairCMS; // same as LCMS?
+  mutable double mDTimePairCMS;
+
+  short mMomRes1;
+  short mMomRes2;
+  // param : Y = [0] + [1] * X^[2]
+  double mPLoss1[3]; // Momentum loss parametrization
+  double mPtRes1[3]; // pt resolution
+  double mPhiRes1[3]; // Phi res
+  double mThetaRes1[3]; // Theta res
+  double mPLoss2[3]; // Momentum loss parametrization
+  double mPtRes2[3]; // pt resolution
+  double mPhiRes2[3]; //
+  double mThetaRes2[3]; //
+  //void applyMomRes1();
+  //void applyMomRes2();
+
+  double mPairPurity;
 
 #ifdef __ROOT__
   ClassDef(StHbtThPair,1)
@@ -120,5 +191,134 @@ inline double StHbtThPair::RealQInv() const{
   return abs(*mMomentum1-*mMomentum2);
 }
 
+inline void StHbtThPair::setPairPurity(double aPairPurity){
+  mPairPurity=aPairPurity;
+}
+inline void StHbtThPair::Set(const StHbtPair* aPair){
+  mMomParCalculated=0;
+  mPosParCalculated=0;
+  mWeightOk=false;
+  mMeasPair=aPair;
+  setVariables(aPair);
+
+  //if(mMomRes1) applyMomRes1();
+  //if(mMomRes2) applyMomRes2();
+}
+// Fabrice private
+inline void StHbtThPair::set(const StHbtPair* aPair){
+  static StHbtParticle* tPrevPart1;
+  static StHbtParticle* tPrevPart2;
+  if(tPrevPart1 != aPair->track1() || tPrevPart2 != aPair->track2()){
+    mWeightOk=false;
+    mMeasPair=aPair;
+    setVariables(aPair);
+    calcMomParameters();
+    calcPosParameters();
+  }
+  tPrevPart1 = aPair->track1();
+  tPrevPart2 = aPair->track2();
+  //if(mMomRes1) applyMomRes1();
+  //if(mMomRes2) applyMomRes2();
+}
+inline StHbtLorentzVector const* StHbtThPair::getMomentum1() const 
+{return mMomentum1;};
+inline StHbtLorentzVector const* StHbtThPair::getMomentum2() const 
+{return mMomentum2;};
+inline StHbtLorentzVector const* StHbtThPair::getEmPoint1() const 
+{return mEmPoint1;};
+inline StHbtLorentzVector const* StHbtThPair::getEmPoint2() const 
+{return mEmPoint2;};  
+inline int StHbtThPair::getPid1() const {return mPid1;};
+inline int StHbtThPair::getPid2() const {return mPid2;};
+
+inline double StHbtThPair::QInv() const{
+  return abs((*mMomentum1)-(*mMomentum2));
+}
+
+inline double StHbtThPair::KStarSide() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mKStarSide;
+}
+inline double StHbtThPair::KStarOut() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mKStarOut;
+}
+inline double StHbtThPair::KStarLong() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mKStarLong;
+}
+inline double StHbtThPair::KStar() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mKStar;
+}
+inline double StHbtThPair::CVK() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mCVK;
+}
+inline double StHbtThPair::KStarFlipped() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mKStarFlipped;
+}
+inline double StHbtThPair::CVKFlipped() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mCVKFlipped;
+}
+inline double StHbtThPair::Betat() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mBetat;
+}
+inline double StHbtThPair::Ut() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mUt;
+}
+inline double StHbtThPair::Pt() const{
+  if(!mMomParCalculated) calcMomParameters();
+  return mPt;
+}
+
+inline double StHbtThPair::RStar() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mRStar;
+}
+inline double StHbtThPair::RTrans() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mRTrans;
+}
+inline double StHbtThPair::ROut() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mROut;
+}
+inline double StHbtThPair::RLong() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mRLong;
+}
+inline double StHbtThPair::RSide() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mRSide;
+}
+inline double StHbtThPair::DTime() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mDTime;
+}
+inline double StHbtThPair::DTimePairLCMS() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mDTimePairLCMS;
+}
+inline double StHbtThPair::ROutPairCMS() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mROutPairCMS;
+}
+inline double StHbtThPair::RLongPairCMS() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mRLongPairCMS;
+}
+inline double StHbtThPair::RSidePairCMS() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mRSidePairCMS;
+}
+inline double StHbtThPair::DTimePairCMS() const{
+  if(!mPosParCalculated){ calcPosParameters(); }
+  return mDTimePairCMS;
+}
 
 #endif
