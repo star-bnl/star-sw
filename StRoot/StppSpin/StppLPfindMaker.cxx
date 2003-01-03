008d5ad196ca77681ef5f7ca4456066391d20461
balewski <>
2003-01-03 23:37:18 +0000

    cleanup

--- StRoot/StppSpin/StppLPfindMaker.cxx
+++ StRoot/StppSpin/StppLPfindMaker.cxx
@@ -1,50 +1,9 @@
 //*-- Author : Jan Balewski
 //  
-// JB 3/30/01 - divorce with MC. Only StEvent is used. No evaluation
-//
-// $Id: StppLPfindMaker.cxx,v 1.9 2003/01/02 22:19:45 balewski Exp $
+// $Id: StppLPfindMaker.cxx,v 1.10 2003/01/03 23:37:18 balewski Exp $
 // $Log: StppLPfindMaker.cxx,v $
-// Revision 1.9  2003/01/02 22:19:45  balewski
-// cleanup #1 of unused code, all makers are now empty
-//
-// Revision 1.8  2001/11/28 23:03:41  balewski
-// ppLMV uses only tracks matched to CTB slats, runs with DAQ & MC data
-//
-// Revision 1.7  2001/06/07 17:02:52  balewski
-// *** empty log message ***
-//
-// Revision 1.6  2001/05/03 23:38:10  balewski
-// *** empty log message ***
-//
-// Revision 1.5  2001/04/27 20:50:45  balewski
-// *** empty log message ***
-//
-// Revision 1.4  2001/04/26 20:04:52  balewski
-// *** empty log message ***
-//
-// Revision 1.3  2001/04/23 19:44:26  balewski
-// *** empty log message ***
-//
-// Revision 1.2  2001/04/23 15:02:10  balewski
-// *** empty log message ***
-//
-// Revision 1.6  2001/04/19 21:30:36  balewski
-// add I/O to ppDst
-//
-// Revision 1.5  2001/04/13 20:15:13  balewski
-// *** empty log message ***
-//
-// Revision 1.4  2001/04/12 21:05:46  balewski
-// *** empty log message ***
-//
-// Revision 1.3  2001/04/12 15:19:09  balewski
-// *** empty log message ***
-//
-// Revision 1.2  2001/02/28 19:06:12  balewski
-// some reorganizations
-//
-// Revision 1.1.1.1  2001/01/31 14:00:07  balewski
-// First release
+// Revision 1.10  2003/01/03 23:37:18  balewski
+// cleanup
 //
 //
 //////////////////////////////////////////////////////////////////////////
@@ -74,6 +33,7 @@ StppLPfindMaker::StppLPfindMaker(const char *name):StMaker(name)
 //_____________________________________________________________________________
 StppLPfindMaker::~StppLPfindMaker()
 {
+
 }
 
 //_____________________________________________________________________________
@@ -81,8 +41,13 @@ Int_t StppLPfindMaker::Init()
 {
   return StMaker::Init();
 }
+//___________________________________________________________
+Int_t StppLPfindMaker::InitRun(int runumber){
 
-//_____________________________________________________________________________
+  return StMaker::Init();
+}
+
+//___________________________________________________________
 Int_t StppLPfindMaker::Finish()
 {
   return  kStOK;
