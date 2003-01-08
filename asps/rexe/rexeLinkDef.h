87db0a6a6ce524ec70d0ae42c471254f5c6527e6
perev <>
2003-01-08 19:26:33 +0000

    CleanUp

--- asps/rexe/rexeLinkDef.h
+++ asps/rexe/rexeLinkDef.h
@@ -6,9 +6,4 @@
 #pragma link C++ class StarMC;
 #pragma link C++ class TGeant3;
 #pragma link C++ class TPaveTree;
-#pragma link C++ function hlimit_;
-#pragma link C++ function hsetpr_;
-#pragma link C++ function hparmn_;
-#pragma link C++ function hrval_;
-#pragma link C++ function fint_;
 #endif
