9d55f775362571ee13d25ec0cc10626de083d457
pruneau <>
2003-01-10 20:34:13 +0000

    moving base classes Parameters, Vectorized and EditableParameters into the Base package

---
+++ StRoot/Sti/Base/EditableParameters.cxx
@@ -0,0 +1,42 @@
+#include <stdexcept>
+#include "Sti/Base/Factory.h"
+#include "Sti/Base/EditableParameter.h"
+#include "Sti/Base/EditableParameters.h"
+
+EditableParameters::EditableParameters()
+  : Parameters(),
+    Subject()
+{}
+
+EditableParameters::EditableParameters(const string & name, const string & description)
+  : Parameters(name, description),
+    Subject()
+{}
+
+EditableParameters::EditableParameters(const EditableParameters & parameter)
+  : Parameters(parameter.getName(), parameter.getDescription()),
+    Subject()
+{}
+
+EditableParameters::~EditableParameters()
+{}
+
+const EditableParameters & EditableParameters::operator=(const EditableParameters & parameter)
+{
+  this->Parameters::operator=(parameter);
+  return *this;
+}
+
+///Reset the parameter elements held by this container to their default value.
+///Non editable parameters are simply skipped.
+void EditableParameters::setDefaults()
+{
+  ParameterIterator iter;
+  for (iter=begin();iter!=end();iter++)
+    {
+      EditableParameter * ep = static_cast<EditableParameter *>(*iter);
+      if (ep)
+	ep->reset();
+    }
+}
+
---
+++ StRoot/Sti/Base/EditableParameters.h
@@ -0,0 +1,21 @@
+#if !defined(EDITABLEPARAMETERS_H_INCLUDED_)
+#define EDITABLEPARAMETERS_H_INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+#include "Sti/Base/Parameters.h"
+#include "Sti/Base/SubjectObserver.h"
+
+class EditableParameters : public Parameters, public Subject
+{
+ public:
+  EditableParameters();
+  EditableParameters(const string & name, const string & description);
+  EditableParameters(const EditableParameters & parameter);
+  virtual ~EditableParameters();
+  const EditableParameters & operator=(const EditableParameters & parameter);
+  virtual void setDefaults();
+};
+
+#endif  // !defined(EDITABLEPARAMETERS_H_INCLUDED_)
