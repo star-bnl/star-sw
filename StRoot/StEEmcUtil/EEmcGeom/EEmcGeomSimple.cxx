c592532aa8ab9a8c950b74f0e2903eda2b2ee499
Piotr A Zolnierczuk <zolnie@iucf.indiana.edu>
2003-01-16 23:04:05 +0000

    added more functionality

--- StRoot/StEEmcUtil/EEmcGeom/EEmcGeomSimple.cxx
+++ StRoot/StEEmcUtil/EEmcGeom/EEmcGeomSimple.cxx
@@ -1,5 +1,8 @@
-// $Id: EEmcGeomSimple.cxx,v 1.1 2003/01/16 19:33:50 zolnie Exp $
+// $Id: EEmcGeomSimple.cxx,v 1.2 2003/01/16 23:04:05 zolnie Exp $
 // $Log: EEmcGeomSimple.cxx,v $
+// Revision 1.2  2003/01/16 23:04:05  zolnie
+// added more functionality
+//
 // Revision 1.1  2003/01/16 19:33:50  zolnie
 // added a simple Geom class to conver a track hit -> tower hit
 //
@@ -7,7 +10,12 @@
 
 #include "SystemOfUnits.h"
 #include "StThreeVectorD.hh"
+#include "StPhysicalHelixD.hh"
+
 #include "StEmcRawHit.h"
+#include "StTrackGeometry.h"
+#include "StTrack.h"
+
 #include "EEmcGeomSimple.h"
 
 
@@ -58,16 +66,17 @@ EEmcGeomSimple::InitDefaults()
   mZ1     =  270.190*centimeter;
   mZ2     =  306.158*centimeter;
   mPhi0   =  75.0*degree;  //  
-  mClock  = -1;
+  mClock  = -1;            // +1 clockwise
 
 }
 
 // =========================================================================
-// gets a hit vector r checks if inside the EEMC
+// gets a hit vector r checks if inside the EEmc
 // and returns sector (1..12), subsector (1..5) and eta(1..mNumEta)
+// 
 // =========================================================================
 int 
-EEmcGeomSimple::getHit(const StThreeVectorD& r,  StEmcRawHit &t)
+EEmcGeomSimple::getHit(const StThreeVectorD& r,  StEmcRawHit &hit)
 {
   const double dPhiSec  = 2.0*M_PI/mNumSec;
   
@@ -75,26 +84,38 @@ EEmcGeomSimple::getHit(const StThreeVectorD& r,  StEmcRawHit &t)
   double  eta = r.pseudoRapidity();
   double  phi = r.phi() - mPhi0;
 
-  // check if inside the endcap
-  if(z  <mZ1              || mZ2<z          ) return 0;
-  if(eta<mEtaBin[mNumEta] || mEtaBin[0]<eta ) return 0;
+  // check if inside 
+  if(z  <mZ1              || mZ2<z          ) return kEEmcGeomZRangeErr;
+  if(eta<mEtaBin[mNumEta] || mEtaBin[0]<eta ) return kEEmcGeomEtaRangeErr;
 
+  
   int teta = 0;
   for(teta=1;teta<=mNumEta;teta++) if(mEtaBin[teta]<eta) break;
   --teta; // step back
 
+  // ------------------------------------------------------------------------
+  // this code is a wonder code
+  // nobody know how it works
+  //
   // get the sector number
   int  k  = (mClock>0) ? (int) floor(phi/dPhiSec) : (int) ceil(phi/dPhiSec); 
   // adjust to 0..(mNumSec-1)
   int tsec    = (mNumSec + mClock*k)%mNumSec;
-
   // get the subsector
   int tsubsec = (int) ( ((k*dPhiSec - phi)/dPhiSec*mNumSSec) ) % mNumSSec ;
-  
-  t.setId(kEndcapEmcTowerId,tsec,teta,tsubsec);
+  // ------------------------------------------------------------------------
+
+  hit.setId(kEndcapEmcTowerId,tsec,teta,tsubsec);
 
   return 1;
 
 }
 
 
+int   
+EEmcGeomSimple::getHit(const StTrack& track , double z, StEmcRawHit &hit)
+{
+  StPhysicalHelixD   helix = track.geometry()->helix();
+  double s  = ( z - helix.origin().z() ) / sin( helix.dipAngle())  ;
+  return getHit(helix.at(s),hit);
+}
--- StRoot/StEEmcUtil/EEmcGeom/EEmcGeomSimple.h
+++ StRoot/StEEmcUtil/EEmcGeom/EEmcGeomSimple.h
@@ -5,12 +5,15 @@
 #ifndef EEmcGeomSimple_h
 #define EEmcGeomSimple_h
 /*********************************************************************
- * $Id: EEmcGeomSimple.h,v 1.1 2003/01/16 19:33:51 zolnie Exp $
+ * $Id: EEmcGeomSimple.h,v 1.2 2003/01/16 23:04:05 zolnie Exp $
  *********************************************************************
  * Description:
  * STAR Endcap Electromagnetic Calorimeter Simple Geometry Class
  *********************************************************************
  * $Log: EEmcGeomSimple.h,v $
+ * Revision 1.2  2003/01/16 23:04:05  zolnie
+ * added more functionality
+ *
  * Revision 1.1  2003/01/16 19:33:51  zolnie
  * added a simple Geom class to conver a track hit -> tower hit
  *
@@ -21,33 +24,38 @@
 
 class StThreeVectorD;
 class StEmcRawHit;
+class StTrack;
 
+const int kEEmcGeomZRangeErr   = -1;
+const int kEEmcGeomEtaRangeErr = -2;
 
 class  EEmcGeomSimple : public TObject { 
 public:
- 
   EEmcGeomSimple();
   virtual ~EEmcGeomSimple();
   
-  int   getHit(const StThreeVectorD& r,  StEmcRawHit &t);
-  float getEtaBin(int eta) { 
+  int   getHit(const StThreeVectorD& r,        StEmcRawHit& hit); // given r point return EmcRawHit 
+  int   getHit(const StTrack& track, double z, StEmcRawHit& hit); // a wrapper for the above when 
+                                                                  //         a track & z is given
+
+  float getEtaBin(int eta) {  // return the "mean" value of an eta bin
     if(eta<1 && mNumEta<eta) return (-1.0);
     return 0.5 * ( mEtaBin[eta] + mEtaBin[eta+1] );
   }
-  float getEtaBinWidth(int eta) { 
+  float getEtaBinWidth(int eta) { // return the "half-width" of an eta bin
     if(eta<1 && mNumEta<eta) return (-1.0);
     return 0.5 * ( mEtaBin[eta] - mEtaBin[eta+1] );
   }
 
 protected:  
-  Float_t  mZ1   ;
-  Float_t  mZ2   ;
-  Float_t *mEtaBin;
-  Int_t    mNumEta;
-  Int_t    mNumSec;
-  Int_t    mNumSSec;
-  Float_t  mPhi0;
-  Int_t    mClock;
+  Float_t  mZ1   ;   // z1  
+  Float_t  mZ2   ;   // z2
+  Float_t *mEtaBin;  // eta bins [0..mNumEta]
+  Int_t    mNumEta;  // number of eta bins 
+  Int_t    mNumSec;  // number of sectors    (in phi)
+  Int_t    mNumSSec; // number of subsectors (in phi)
+  Float_t  mPhi0;    // phi0 of the 0th sector 
+  Int_t    mClock;   // +1 == clockwise  -1 == counter-clockwise
 
 
 private:
@@ -56,8 +64,6 @@ private:
   ClassDef(EEmcGeomSimple,1)   // STAR Endcap Electromagnetic Calorimeter Simple Geometry Class
    
 };
-
-
 
 #endif
 
