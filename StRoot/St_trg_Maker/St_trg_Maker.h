7af06cdfec1665ce4d0280f210bee7c8a7e30696
Jerome Lauret <jeromel@bnl.gov>
2003-01-21 01:31:57 +0000

    Minor changes in messaging (small request from Janet) to bring some consistency. doxygenized the .h

--- StRoot/St_trg_Maker/St_trg_Maker.h
+++ StRoot/St_trg_Maker/St_trg_Maker.h
@@ -1,5 +1,110 @@
-// $Id: St_trg_Maker.h,v 1.17 2003/01/16 13:32:32 ward Exp $
+
+#ifndef STAR_St_trg_Maker
+#define STAR_St_trg_Maker
+
+//////////////////////////////////////////////////////////////////////////
+/*!
+  \class St_trg_Maker
+
+  Stt_trg_Maker, also a maker, gets the address of the StDAQReader from the dataset
+  during its Init().
+
+  Please read the documentation in the doc/ directory for a description of the
+  interraction between StDAQMaker, StDAQReader and St_trg_Maker .
+ 
+*/                                                                     
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef StMaker_H
+#include "StMaker.h"
+#endif
+class St_ctu_raw;
+class St_mwc_raw;
+class St_dst_L0_Trigger;
+class St_dst_L1_Trigger;
+class St_dst_L2_Trigger;
+class St_dst_TrgDet;
+class StDAQReader;
+class StTRGReader;
+
+class St_trg_Maker : public StMaker {
+ private:
+   Bool_t drawinit;
+   StDAQReader *fVictorPrelim;           //!
+   StTRGReader *fVictor;                 //!
+
+   void dumpDataToScreenAndExit    ();
+   void dumpDataToScreenAndExit2000();
+   void dumpDataToScreenAndExit2003();
+   void InitMwcArrays();
+   void InitCtbArrays();
+   int St_trg_Maker::YearOfData(St_DataSet *herb);
+   void InitCtbArrays2001();             // For data taken 2001 and (?) after. 
+   Int_t SanityCheck    ();
+   Int_t SanityCheck2000();
+   Int_t SanityCheck2003();
+   int auxctbmap[16],ctbmap[120][2];     // Hardcoded from ctb_dsm.map.
+   int auxmwcmap[32],mwcmap[24][4];      // Hardcoded from mwc_dsm.map.
+ 
+ protected:
+
+ public: 
+   int getTrayCtb ( float phi, float z ) ;
+   int HandleCtu(St_ctu_raw *ctu_raw,St_dst_TrgDet *dst1);
+   int HandleMwc(St_mwc_raw *mwc_raw,St_dst_TrgDet *dst1);
+
+   void Vladimir2Herbert(int,int*,int*);
+   void Emc    (St_dst_TrgDet *dst1);
+   void Emc2000(St_dst_TrgDet *dst1);
+   void Emc2003(St_dst_TrgDet *dst1);
+   void CtbMwcDaq    (St_dst_TrgDet *dst1);
+   void CtbMwcDaq2000(St_dst_TrgDet *dst1);
+   void CtbMwcDaq2003(St_dst_TrgDet *dst1);
+   void SecondDstSim(St_dst_L0_Trigger *dst2);
+   void TakeCareOfL1andL2Daq    (St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   void TakeCareOfL1andL2Daq2000(St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   void TakeCareOfL1andL2Daq2003(St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   void TakeCareOfL1andL2Sim(St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   void SecondDstDaq    (St_dst_L0_Trigger *dst2);
+   void SecondDstDaq2000(St_dst_L0_Trigger *dst2);
+   void SecondDstDaq2003(St_dst_L0_Trigger *dst2);
+   void VpdSim(St_dst_TrgDet *dst);
+   void ZdcSim(St_dst_TrgDet *dst);
+   void VpdDaq    (St_dst_TrgDet *dst);
+   void VpdDaq2000(St_dst_TrgDet *dst);
+   void VpdDaq2003(St_dst_TrgDet *dst);
+   void ZdcDaq    (St_dst_TrgDet *dst);
+   void ZdcDaq2000(St_dst_TrgDet *dst);
+   void ZdcDaq2003(St_dst_TrgDet *dst);
+
+   int Daq    (St_DataSet*,St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   int Daq2000(St_DataSet*,St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   int Daq2003(St_DataSet*,St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+   int Sim    (St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
+
+                  St_trg_Maker(const char *name="trg");
+   virtual       ~St_trg_Maker();
+
+   virtual Int_t Init();
+   virtual Int_t Make();
+
+   virtual const char *GetCVS() const {
+     static const char cvs[]="Tag $Name:  $ $Id: St_trg_Maker.h,v 1.18 2003/01/21 01:31:57 jeromel Exp $ built "__DATE__" "__TIME__ ; 
+     return cvs;
+   }
+
+   ClassDef(St_trg_Maker, 1)   //StAF chain virtual base class for Makers
+};
+
+#endif
+
+
+// $Id: St_trg_Maker.h,v 1.18 2003/01/21 01:31:57 jeromel Exp $
 // $Log: St_trg_Maker.h,v $
+// Revision 1.18  2003/01/21 01:31:57  jeromel
+// Minor changes in messaging (small request from Janet) to bring some consistency.
+// doxygenized the .h
+//
 // Revision 1.17  2003/01/16 13:32:32  ward
 // Accomodation of the new trgStructures.h.
 //
@@ -60,88 +165,3 @@
 // Revision 1.2  1998/07/20 15:08:15  fisyak
 // Add tcl and tpt
 //
-#ifndef STAR_St_trg_Maker
-#define STAR_St_trg_Maker
-
-//////////////////////////////////////////////////////////////////////////
-//                                                                      //
-// St_trg_Maker virtual base class for Maker                            //
-//                                                                      //
-//////////////////////////////////////////////////////////////////////////
-#ifndef StMaker_H
-#include "StMaker.h"
-#endif
-class St_ctu_raw;
-class St_mwc_raw;
-class St_dst_L0_Trigger;
-class St_dst_L1_Trigger;
-class St_dst_L2_Trigger;
-class St_dst_TrgDet;
-class StDAQReader;
-class StTRGReader;
-
-class St_trg_Maker : public StMaker {
- private:
-   Bool_t drawinit;
-   StDAQReader *fVictorPrelim; //!
-   StTRGReader *fVictor;       //!
-// static Char_t  m_VersionCVS = "$Id: St_trg_Maker.h,v 1.17 2003/01/16 13:32:32 ward Exp $";
-// Int_t          m_mode;        // mode 1 = primaries;
-// St_stk_stkpar *m_stk_stkpar;  //! pointer to stk parameters
-   void dumpDataToScreenAndExit    ();
-   void dumpDataToScreenAndExit2000();
-   void dumpDataToScreenAndExit2003();
-   void InitMwcArrays();
-   void InitCtbArrays();
-   int St_trg_Maker::YearOfData(St_DataSet *herb);
-   void InitCtbArrays2001(); // For data taken 2001 and (?) after. 
-   Int_t SanityCheck    ();
-   Int_t SanityCheck2000();
-   Int_t SanityCheck2003();
-   int auxctbmap[16],ctbmap[120][2];  // Hardcoded from ctb_dsm.map.
-   int auxmwcmap[32],mwcmap[24][4];  // Hardcoded from mwc_dsm.map.
- 
- protected:
- public: 
-   int getTrayCtb ( float phi, float z ) ;
-   int HandleCtu(St_ctu_raw *ctu_raw,St_dst_TrgDet *dst1);
-   int HandleMwc(St_mwc_raw *mwc_raw,St_dst_TrgDet *dst1);
-   void Vladimir2Herbert(int,int*,int*);
-   void Emc    (St_dst_TrgDet *dst1);
-   void Emc2000(St_dst_TrgDet *dst1);
-   void Emc2003(St_dst_TrgDet *dst1);
-   void CtbMwcDaq    (St_dst_TrgDet *dst1);
-   void CtbMwcDaq2000(St_dst_TrgDet *dst1);
-   void CtbMwcDaq2003(St_dst_TrgDet *dst1);
-   void SecondDstSim(St_dst_L0_Trigger *dst2);
-   void TakeCareOfL1andL2Daq    (St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   void TakeCareOfL1andL2Daq2000(St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   void TakeCareOfL1andL2Daq2003(St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   void TakeCareOfL1andL2Sim(St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   void SecondDstDaq    (St_dst_L0_Trigger *dst2);
-   void SecondDstDaq2000(St_dst_L0_Trigger *dst2);
-   void SecondDstDaq2003(St_dst_L0_Trigger *dst2);
-   void VpdSim(St_dst_TrgDet *dst);
-   void ZdcSim(St_dst_TrgDet *dst);
-   void VpdDaq    (St_dst_TrgDet *dst);
-   void VpdDaq2000(St_dst_TrgDet *dst);
-   void VpdDaq2003(St_dst_TrgDet *dst);
-   void ZdcDaq    (St_dst_TrgDet *dst);
-   void ZdcDaq2000(St_dst_TrgDet *dst);
-   void ZdcDaq2003(St_dst_TrgDet *dst);
-   int Daq    (St_DataSet*,St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   int Daq2000(St_DataSet*,St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   int Daq2003(St_DataSet*,St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-   int Sim(            St_dst_TrgDet*,St_dst_L0_Trigger*,St_dst_L1_Trigger*,St_dst_L2_Trigger*);
-                  St_trg_Maker(const char *name="trg");
-   virtual       ~St_trg_Maker();
-   virtual Int_t Init();
-   virtual Int_t  Make();
-// virtual void Set_mode       (Int_t   m =      2){m_mode       = m;} // *MENU*
-  virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: St_trg_Maker.h,v 1.17 2003/01/16 13:32:32 ward Exp $ built "__DATE__" "__TIME__ ; return cvs;}
-
-   ClassDef(St_trg_Maker, 1)   //StAF chain virtual base class for Makers
-};
-
-#endif
