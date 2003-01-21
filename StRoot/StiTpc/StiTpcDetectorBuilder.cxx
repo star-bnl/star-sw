f79cc6f26546e4d894a4b0999b09936813e89ad7
andrewar <>
2003-01-21 18:50:38 +0000

    Moved from Sti to StiTpc

---
+++ StRoot/StiTpc/StiTpcDetectorBuilder.cxx
@@ -0,0 +1,224 @@
+#include <stdexcept>
+#include "StDbUtilities/StTpcLocalCoordinate.hh"
+#include "StDbUtilities/StTpcCoordinateTransform.hh"
+#include "StTpcDb/StTpcDb.h"
+#include "StTpcDb/StTpcPadPlaneI.h"
+#include "StTpcDb/StTpcDimensionsI.h"
+#include "StiTpc/StiTpcDetectorBuilder.h" 
+#include "StiTpc/StiTpcIsActiveFunctor.h"
+#include "Sti/StiPlanarShape.h"
+#include "Sti/StiCylindricalShape.h"
+#include "Sti/StiMaterial.h"
+#include "Sti/StiPlacement.h"
+#include "Sti/StiDetector.h"
+#include "Sti/Base/Factory.h"
+#include "Sti/StiToolkit.h"
+#include "Sti/StiIsActiveFunctor.h"
+#include "Sti/StiNeverActiveFunctor.h"
+#include "Sti/Base/Messenger.h"
+#include <stdio.h>
+#include "Sti/StiHitErrorCalculator.h"
+
+StiTpcDetectorBuilder::StiTpcDetectorBuilder()
+  : StiDetectorBuilder("TpcBuilder")
+{
+  _innerCalc = new StiDefaultHitErrorCalculator();
+  _innerCalc->set(3.12735e-03,1.51055e-04,2.43806e-02,1.68243e-03,5.23272e-05,5.75341e-02);
+  _outerCalc = new StiDefaultHitErrorCalculator();
+  _outerCalc->set(8.158e-03,5.69582e-05,4.48440e-02,2.02775e-04,3.55219e-05,6.45610e-02);
+}
+
+StiTpcDetectorBuilder::~StiTpcDetectorBuilder()
+{
+  delete _innerCalc;
+  delete _outerCalc;
+}
+
+/*! Builds the material required for the TPC
+<p>
+The material currently used are P10, and NOMEX. The properties
+of these materials are extracted from the Particle Data Book.
+<table>
+<tr><td>Name</td><td>Z</td><td>A</td><td>Z</td><td>Density(g/cm3)</td><td>RadLength(g/cm2)</td><td>Ionization</td></tr>
+<tr><td>P10 </td><td></td><td>A</td><td>Z</td><td>Density(g/cm3)</td><td>RadLength(g/cm2)</td><td>Ionization</td></tr>
+<tr><td>Name</td><td>Z</td><td>A</td><td>Z</td><td>Density(g/cm3)</td><td>RadLength(g/cm2)</td><td>Ionization</td></tr>
+</table>
+*/  
+void StiTpcDetectorBuilder::buildMaterials()
+{
+  _messenger << "StiTpcDetectorBuilder::buildMaterials() - INFO - Started" << endl;
+  _gas = add(new StiMaterial("P10", 0.5, 1.,12850., 0.00156, 5.));
+  _fcMaterial = add(new StiMaterial("Nomex",6.,12.,625.,0.064,5.));
+  _messenger << "StiTpcDetectorBuilder::buildMaterials() - INFO - Done" << endl;
+}
+
+void StiTpcDetectorBuilder::buildShapes()
+{
+  _messenger << "StiTpcDetectorBuilder::buildShapes() - INFO - Started" << endl;
+  char name[50];
+  unsigned int _nInnerPadrows = _padPlane->numberOfInnerRows();
+  unsigned int row;
+  for(row = 0; row<getNRows(); row++)
+    {
+      StiPlanarShape *pShape = new StiPlanarShape;
+      if(row<_nInnerPadrows){
+	pShape->setThickness(_padPlane->innerSectorPadLength());
+      }else{
+	pShape->setThickness(_padPlane->outerSectorPadLength());
+      }
+      pShape->setHalfDepth(_dimensions->tpcTotalLength()/2.);
+      pShape->setHalfWidth(_padPlane->PadPitchAtRow(row) *
+			   _padPlane->numberOfPadsAtRow(row) / 2.);
+      sprintf(name, "Tpc/Padrow_%d", row+1);
+      pShape->setName(name);
+      add(pShape);
+    }
+  
+  // Inner field cage
+  StiCylindricalShape *ifcShape = new StiCylindricalShape;
+  ifcShape->setName("tpc/ifc");
+  ifcShape->setThickness(1.27); 
+  ifcShape->setHalfDepth( _dimensions->tpcTotalLength()/2. );
+  ifcShape->setOpeningAngle( M_PI/6. );
+  ifcShape->setOuterRadius(_dimensions->ifcRadius() + ifcShape->getThickness()/2.);
+  add(ifcShape);
+  
+  //Outer  field cage
+  StiCylindricalShape *ofcShape = new StiCylindricalShape;
+  ofcShape->setName("tpc/ofc");
+  ofcShape->setThickness(1.27); 
+  ofcShape->setHalfDepth( _dimensions->tpcTotalLength()/2. );
+  ofcShape->setOpeningAngle( M_PI/6. );
+  ofcShape->setOuterRadius(_dimensions->ofcRadius() + ofcShape->getThickness()/2.);
+  add(ofcShape);
+  _messenger << "StiTpcDetectorBuilder::buildShapes() - INFO - Done" << endl;
+}
+
+/*! Build all detector components of the TPC.
+<p>
+The detector components of the TPC include the 24 sectors, 45 padrow gas volumes, and
+the inner and outer field cage of the TPC. The padrows  are polygonal with 12  sides
+whereas  the field cage are cylindrical. However to match the 12 fold symmetry of the 
+TPC, the field cage are artificially segmented into 12 sectors each.
+*/
+void StiTpcDetectorBuilder::buildDetectors()
+{
+  char name[50];
+  _messenger << "StiTpcDetectorBuilder::buildDetectors() - INFO - Started" << endl;
+  unsigned int row;
+
+
+  // IFC and OFC
+  StiShape *ifcShape = findShape("tpc/ifc");  
+  StiShape *ofcShape = findShape("tpc/ofc");  
+  if (!ifcShape || !ofcShape)
+    throw runtime_error("StiTpcDetectorBuilder::buildDetectors() - FATAL - ifcShape==0||ofcShape==0");
+  float fIfcRadius = _dimensions->ifcRadius();   
+  float fOfcRadius = _dimensions->ofcRadius();   
+
+  //for(unsigned int sector = 0; sector<getNSectors(); sector++)
+  for(unsigned int sector = 0; sector<12; sector++)
+    {
+      // inner field cage
+      StiPlacement *p = new StiPlacement;
+      p->setZcenter(0.);
+      p->setLayerRadius(fIfcRadius);
+      p->setNormalRep(phiForTpcSector(sector), fIfcRadius, 0.);
+      
+      StiDetector *ifcVolume = _detectorFactory->getInstance();
+      sprintf(name, "TPC/Ifc/Sector_%d", sector);
+      ifcVolume->setName(name);
+      ifcVolume->setIsOn(true);
+      ifcVolume->setIsActive(new StiNeverActiveFunctor);
+      ifcVolume->setIsContinuousMedium(false);
+      ifcVolume->setIsDiscreteScatterer(true);
+      ifcVolume->setShape(ifcShape);
+      ifcVolume->setPlacement(p);
+      ifcVolume->setGas(_gas);
+      ifcVolume->setMaterial(_fcMaterial);
+      add(ifcVolume);
+      
+      // outer field cage
+      p = new StiPlacement;
+      p->setZcenter(0.);
+      p->setLayerRadius(fOfcRadius);
+      p->setNormalRep(phiForTpcSector(sector), fOfcRadius, 0.);
+      
+      StiDetector *ofcVolume = _detectorFactory->getInstance();
+      sprintf(name, "TPC/Ofc/Sector_%d", sector);
+      ofcVolume->setName(name);
+      ofcVolume->setIsOn(true);
+      ofcVolume->setIsActive(new StiNeverActiveFunctor);
+      ofcVolume->setIsContinuousMedium(false);
+      ofcVolume->setIsDiscreteScatterer(true);
+      ofcVolume->setShape(ofcShape);
+      ofcVolume->setPlacement(p);
+      ofcVolume->setGas(_gas);
+      ofcVolume->setMaterial(_fcMaterial);
+      add(ofcVolume);
+  } 
+
+  //Active TPC padrows
+  for(row = 0; row<getNRows(); row++)
+    {
+      // create properties shared by all sectors in this padrow
+      float fRadius = _padPlane->radialDistanceAtRow(row+1);
+      sprintf(name, "Tpc/Padrow_%d", row+1);
+      StiPlanarShape *pShape = (StiPlanarShape *)findShape(name);
+      if (!pShape)
+	throw runtime_error("StiTpcDetectorBuilder::buildDetectors() - FATAL - pShape==0||ofcShape==0");
+      for(unsigned int sector = 0; sector<getNSectors(); sector++)
+	{
+	  // create unique detector properties (placement & name)
+	  StiPlacement *pPlacement = new StiPlacement;
+	  pPlacement->setZcenter(0.);
+	  pPlacement->setNormalRep(phiForTpcSector(sector), fRadius, 0.); 
+	  pPlacement->setLayerRadius(fRadius);
+	  sprintf(name, "Tpc/Padrow_%d/Sector_%d", row+1, sector+1);
+	  // fill in the detector object and save it in our vector
+	  StiDetector *pDetector = _detectorFactory->getInstance();
+	  pDetector->setName(name);
+	  pDetector->setIsOn(true);
+	  pDetector->setIsActive(new StiTpcIsActiveFunctor(sector, row));
+	  pDetector->setIsContinuousMedium(true);
+	  pDetector->setIsDiscreteScatterer(false);
+	  pDetector->setMaterial(_gas);
+	  pDetector->setGas(_gas);
+	  pDetector->setShape(pShape);
+	  pDetector->setPlacement(pPlacement);
+	  if (row<15)
+	    pDetector->setHitErrorCalculator(_innerCalc);
+	  else
+	    pDetector->setHitErrorCalculator(_outerCalc);
+	  add(row,sector,pDetector);
+	}// for sector
+    }// for row
+
+  _messenger << "StiTpcDetectorBuilder::buildDetectors() - INFO - Done" << endl;
+}
+
+void StiTpcDetectorBuilder::loadDb()
+{
+  if (!gStTpcDb) 
+		throw runtime_error("StiTpcDetectorBuilder::buildShapes() - ERROR - gStTpcDb==0");
+	_padPlane = gStTpcDb->PadPlaneGeometry();
+  if (!_padPlane) 
+		throw runtime_error("StiTpcDetectorBuilder::buildShapes() - ERROR - _padPlane==0");
+	_dimensions = gStTpcDb->Dimensions();
+  if (!_dimensions)
+		throw runtime_error("StiTpcDetectorBuilder::loadDb() - ERROR - _dimensions==0");
+  _transform = new StTpcCoordinateTransform(gStTpcDb);
+  if (!_transform)
+    throw runtime_error("StiTpcDetectorBuilder::loadDb() - ERROR - gStTpcDb==0");
+  StTpcPadPlaneI *_padPlane = gStTpcDb->PadPlaneGeometry();
+  if (!_padPlane)
+    throw runtime_error("StiTpcDetectorBuilder::loadDb() - ERROR - _padPlane==0");
+  unsigned int nRows = _padPlane->numberOfRows();
+  setNRows(nRows);
+  for(unsigned int row = 0; row<nRows;row++)
+    {
+      setNSectors(row,12);
+    }
+}
+
+
---
+++ StRoot/StiTpc/StiTpcHitLoader.cxx
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+#include <stdio.h>
+#include "StEvent.h"
+#include "StEventTypes.h"
+#include "StDbUtilities/StTpcCoordinateTransform.hh"
+#include "StDbUtilities/StTpcLocalSectorCoordinate.hh"
+#include "StDbUtilities/StGlobalCoordinate.hh"
+#include "StTpcDb/StTpcDb.h"
+#include "Sti/Base/Factory.h"
+#include "Sti/StiHit.h"
+#include "Sti/StiHitContainer.h"
+#include "Sti/StiDetector.h"
+#include "Sti/StiDetectorBuilder.h"
+#include "StiTpc/StiTpcHitLoader.h"
+
+StiTpcHitLoader::StiTpcHitLoader()
+  : StiHitLoader<StEvent,StiDetectorBuilder>("TpcHitLoader")
+{}
+    
+StiTpcHitLoader::StiTpcHitLoader(StiHitContainer* hitContainer,
+				 Factory<StiHit>*hitFactory,
+				 StiDetectorBuilder * detector)
+  : StiHitLoader<StEvent,StiDetectorBuilder>("TpcHitLoader",hitContainer,hitFactory,detector)
+{}
+
+StiTpcHitLoader::~StiTpcHitLoader()
+{}
+
+void StiTpcHitLoader::loadHits(StEvent* source)
+{
+  //_messenger << "StiTpcHitLoader::loadHits(StEvent*) - INFO - Started" << endl;
+  if (!_detector)
+    throw runtime_error("StiTpcHitLoader::loadHits(StEvent*) - FATAL - _detector==0");
+  if(!_hitContainer)
+    throw runtime_error("StiTpcHitLoader::loadHits(StEvent*) - FATAL - _hitContainer==0");
+
+  StiDetector * detector;
+  StiHit* stiHit;
+  const StTpcHitCollection* tpcHits = source->tpcHitCollection();
+  unsigned int stiSector;
+  for (unsigned int sector=0; sector<24; sector++) 
+    {
+      if (sector<12)
+	stiSector = sector;
+      else
+	stiSector = 11 - (sector-11)%12;
+      const StTpcSectorHitCollection* secHits = tpcHits->sector(sector);
+      if (!secHits) 
+	{
+	  _messenger << "StiTpcHitLoader::loadHits(StEvent* source) -W- no hits for sector:"<<sector<<endl;
+	  break;
+	}
+      for (unsigned int row=0; row<45; row++) 
+	{
+	  //_messenger << "StiTpcHitLoader:loadHits() -I- Loading row:"<<row<<" sector:"<<sector<<endl;
+	  const StTpcPadrowHitCollection* padrowHits = secHits->padrow(row);
+	  if (!padrowHits)
+	    {
+	      //_messenger << "StiTpcHitLoader:loadHits() - row=="<<row<<" has padrowHits==0"<<endl;
+	      break;
+	    }
+	  const StSPtrVecTpcHit& hitvec = padrowHits->hits();
+	  detector = _detector->getDetector(row,stiSector);
+	  
+	  if (!detector)
+	    {
+	      cout << "StiTpcHitLoader::loadHits(StEvent*) -E- Detector not found for pad row:" << row
+		   << " sector:"<<sector<<endl;
+	      throw runtime_error("StiTpcHitLoader::loadHits(StEvent*) -E- Detector element not found");
+	    }
+	  for (vector<StTpcHit*>::const_iterator iter = hitvec.begin();
+	       iter != hitvec.end(); 
+	       iter++) 
+	    {
+	      StTpcHit*hit=*iter;
+	      if(!_hitFactory)
+		throw runtime_error("StiTpcHitLoader::loadHits(StEvent*) -E- _hitFactory==0");
+	      stiHit = _hitFactory->getInstance();
+	      if(!stiHit)
+		throw runtime_error("StiTpcHitLoader::loadHits(StEvent*) -E- stiHit==0");
+	      stiHit->setGlobal(detector, 
+				hit, 
+				hit->position().x(),
+				hit->position().y(),
+				hit->position().z(),
+				hit->charge());
+	      _hitContainer->push_back( stiHit );
+	    }
+	}
+    }
+}
+
