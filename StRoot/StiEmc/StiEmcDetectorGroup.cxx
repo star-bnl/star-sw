3be570daee0eedd75c1767350108c6ee14b61bcb
Andrew Allen Rose <aarose@lbl.gov>
2003-01-21 18:11:09 +0000

    Moved from Sti to StiEmc

---
+++ StRoot/StiEmc/StiEmcDetectorGroup.cxx
@@ -0,0 +1,19 @@
+#include "StiEmc/StiEmcDetectorGroup.h"
+#include "StiEmc/StiEmcDetectorBuilder.h"
+#include "StiEmc/StiEmcHitLoader.h"
+#include "Sti/StiDetectorBuilder.h"
+#include "StEvent.h"
+#include <stdexcept>
+
+StiEmcDetectorGroup::StiEmcDetectorGroup(bool active)
+  : StiDetectorGroup<StEvent>("EMC",
+			      active?new StiEmcHitLoader():0,
+			      new StiEmcDetectorBuilder(),
+			      0,
+			      0)
+{}
+
+StiEmcDetectorGroup::~StiEmcDetectorGroup()
+{}
+
+
