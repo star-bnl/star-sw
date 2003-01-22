32af66573f813d30af128b048fb5da8c5524748d
suaide <>
2003-01-22 22:49:11 +0000

    fix to create StEvent without L3

--- StRoot/StEmcCalibrationMaker/StEmcPreCalibrationMaker.cxx
+++ StRoot/StEmcCalibrationMaker/StEmcPreCalibrationMaker.cxx
@@ -1,7 +1,10 @@
 //*-- Author : Alexandre Suaide 
 // 
-// $Id: StEmcPreCalibrationMaker.cxx,v 1.8 2002/12/02 21:30:57 suaide Exp $
+// $Id: StEmcPreCalibrationMaker.cxx,v 1.9 2003/01/22 22:49:11 suaide Exp $
 // $Log: StEmcPreCalibrationMaker.cxx,v $
+// Revision 1.9  2003/01/22 22:49:11  suaide
+// fix to create StEvent without L3
+//
 // Revision 1.8  2002/12/02 21:30:57  suaide
 // New EMC calibration maker
 //
@@ -29,16 +32,16 @@
 #include <iostream.h>
 #include <math.h>
 #include "St_DataSetIter.h"
-#include "StEvent/StEvent.h"
-#include "StEvent/StEventTypes.h"
-#include "StEmcUtil/emcDetectorName.h"
+#include "StEvent.h"
+#include "StEventTypes.h"
+#include "emcDetectorName.h"
 #include "StGlobals.hh"
 // DAQ Libraries
 #include "StDaqLib/GENERIC/EventReader.hh"
 #include "StDaqLib/EMC/EMC_Reader.hh"
 #include "StDAQMaker/StDAQReader.h"
 
-#include "StEmcUtil/StEmcDaqUtil.h"
+#include "StEmcDaqUtil.h"
 ClassImp(StEmcPreCalibrationMaker)
 
 //_____________________________________________________________________________
@@ -97,8 +100,7 @@ Int_t StEmcPreCalibrationMaker::Make()
     return kStWarn;
   }
   currevent->setEmcCollection(emccol);
-  
-  
+   
   StDAQReader* mTheDataReader = (StDAQReader*)(mTheEmcData->GetObject());
   //if(!mTheDataReader->L3Present()) 
   //{
@@ -106,79 +108,81 @@ Int_t StEmcPreCalibrationMaker::Make()
   //  return kStWarn;
   //}
   StL3Reader *L3=mTheDataReader->getL3Reader();
-  if(!L3) return kStWarn;
-  
-  GlobalTrackReader *L3Tracks=L3->getGlobalTrackReader();
-  if(!L3Tracks) return kStWarn;
-  
-  Int_t ntracks=L3Tracks->getNumberOfTracks();
-  globalTrack *tracks=L3Tracks->getTrackList();
-  cout <<"StEmcPreCalibrationMaker:: Number of global tracks = "<<ntracks<<endl;
-
-  StL3Trigger* l3t = new StL3Trigger();
-  StSPtrVecTrackNode& nodes = l3t->trackNodes() ;
-
-  Float_t cte=1.; // check if L3 angles are dregee or radians....
-  
-  for(Int_t i=0;i<ntracks;i++)
-  {
-    StGlobalTrack *gTrack = new StGlobalTrack();
-    
-    Float_t psi = tracks[i].psi*cte;
-    Float_t tanl=tracks[i].tanl;
-    Short_t charge = tracks[i].q;
-    Float_t curvature=0;
-    Float_t pt=tracks[i].pt;
-    Float_t r0=tracks[i].r0;
-    Float_t phi0=tracks[i].phi0*cte;
-    Float_t z0=tracks[i].z0;
-    StThreeVectorF momentum( pt * cos(psi), pt * sin(psi), pt * tanl ) ;
-    StThreeVectorF origin( r0*cos(phi0),r0*sin(phi0),z0) ;
-    StHelixModel* helixModel = new StHelixModel(charge,psi,curvature,tanl,origin,momentum,0) ;
-    //cout <<"Track "<<i<<"  p = "<<momentum.mag()<<"  pt = "<<pt<<"  q = "<<charge<<"  psi = "<<psi<<"  phi0 = "<<phi0<<"  tanl = "<<tanl<<endl;
-
-    gTrack->setLength(tracks[i].length);    
-    gTrack->setGeometry(helixModel);
-    
-    /*StTrackDetectorInfo* detecInfo = new StTrackDetectorInfo() ;
-    detecInfo->setNumberOfPoints(tracks[i].nHits) ;
-    gTrack->setDetectorInfo(detecInfo) ;*/
-    
-    Float_t a[2],b[15];
-    StTrackFitTraits fit(0,(Int_t)tracks[i].nHits,a,b);
-    gTrack->setFitTraits(fit);
-        
-    pDistr->Fill(momentum.mag(),(Int_t)tracks[i].nHits);
-    Float_t R=230;
-    Float_t dr=R-r0;
-    StThreeVectorF tmp(dr*cos(psi),dr*sin(psi),dr*tanl);
-    StThreeVectorF tmp1=tmp+origin;
-    Float_t eta=tmp1.pseudoRapidity();      
-    pDistr1->Fill(eta,momentum.mag());
-    if(momentum.mag()>1.2) 
+  if(L3)
+  { 
+    GlobalTrackReader *L3Tracks=L3->getGlobalTrackReader();
+    if(L3Tracks) 
     {
-      etaDistr->Fill(eta,(Int_t)tracks[i].nHits);
-      /*if(eta>=0 && eta<=1) cout <<" track "<<i<<"  p = "<<momentum.mag()
+      Int_t ntracks=L3Tracks->getNumberOfTracks();
+      globalTrack *tracks=L3Tracks->getTrackList();
+      cout <<"StEmcPreCalibrationMaker:: Number of global tracks = "<<ntracks<<endl;
+
+      StL3Trigger* l3t = new StL3Trigger();
+      StSPtrVecTrackNode& nodes = l3t->trackNodes() ;
+
+      Float_t cte=1.; // check if L3 angles are dregee or radians....
+  
+      for(Int_t i=0;i<ntracks;i++)
+      {
+        StGlobalTrack *gTrack = new StGlobalTrack();
+    
+        Float_t psi = tracks[i].psi*cte;
+        Float_t tanl=tracks[i].tanl;
+        Short_t charge = tracks[i].q;
+        Float_t curvature=0;
+        Float_t pt=tracks[i].pt;
+        Float_t r0=tracks[i].r0;
+        Float_t phi0=tracks[i].phi0*cte;
+        Float_t z0=tracks[i].z0;
+        StThreeVectorF momentum( pt * cos(psi), pt * sin(psi), pt * tanl ) ;
+        StThreeVectorF origin( r0*cos(phi0),r0*sin(phi0),z0) ;
+        StHelixModel* helixModel = new StHelixModel(charge,psi,curvature,tanl,origin,momentum,0) ;
+        //cout <<"Track "<<i<<"  p = "<<momentum.mag()<<"  pt = "<<pt<<"  q = "<<charge<<"  psi = "<<psi<<"  phi0 = "<<phi0<<"  tanl = "<<tanl<<endl;
+
+        gTrack->setLength(tracks[i].length);    
+        gTrack->setGeometry(helixModel);
+    
+        /*StTrackDetectorInfo* detecInfo = new StTrackDetectorInfo() ;
+        detecInfo->setNumberOfPoints(tracks[i].nHits) ;
+        gTrack->setDetectorInfo(detecInfo) ;*/
+    
+        Float_t a[2],b[15];
+        StTrackFitTraits fit(0,(Int_t)tracks[i].nHits,a,b);
+        gTrack->setFitTraits(fit);
+        
+        pDistr->Fill(momentum.mag(),(Int_t)tracks[i].nHits);
+        Float_t R=230;
+        Float_t dr=R-r0;
+        StThreeVectorF tmp(dr*cos(psi),dr*sin(psi),dr*tanl);
+        StThreeVectorF tmp1=tmp+origin;
+        Float_t eta=tmp1.pseudoRapidity();      
+        pDistr1->Fill(eta,momentum.mag());
+        if(momentum.mag()>1.2) 
+        {
+          etaDistr->Fill(eta,(Int_t)tracks[i].nHits);
+          /*if(eta>=0 && eta<=1) cout <<" track "<<i<<"  p = "<<momentum.mag()
                                 <<"  nHits = "<<(Int_t)tracks[i].nHits
                                 <<"  z0 = "<<z0
                                 <<"  r0 = "<<r0
                                 <<"  tanl = "<<tanl
                                 <<"  dr = "<<dr
                                 <<"  etaEMc = "<<eta<<endl;*/
-    }
+        }
     
-    StTrackNode* trackNode = new StTrackNode() ;
-    trackNode->addTrack(gTrack) ;
-    nodes.push_back(trackNode) ; 
-  }
-  vertex l3v=L3Tracks->getVertex();
-  StThreeVectorF vv(l3v.x,l3v.y,l3v.z);
-  StPrimaryVertex *v=new StPrimaryVertex();
-  v->setPosition(vv);
+        StTrackNode* trackNode = new StTrackNode() ;
+        trackNode->addTrack(gTrack) ;
+        nodes.push_back(trackNode) ; 
+      }
+      vertex l3v=L3Tracks->getVertex();
+      StThreeVectorF vv(l3v.x,l3v.y,l3v.z);
+      StPrimaryVertex *v=new StPrimaryVertex();
+      v->setPosition(vv);
   
-  l3t->addPrimaryVertex(v);
+      l3t->addPrimaryVertex(v);
 
-  currevent->setL3Trigger(l3t);
+      currevent->setL3Trigger(l3t);
+    }
+  }
 	    
   AddData(currevent);
   return kStOK;
