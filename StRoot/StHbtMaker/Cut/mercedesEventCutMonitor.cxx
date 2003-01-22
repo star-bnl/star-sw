f50153594f779901d40e5cd89e372331f8a6e897
mercedes <>
2003-01-22 16:05:11 +0000

    Minor changes

--- StRoot/StHbtMaker/Cut/mercedesEventCutMonitor.cxx
+++ StRoot/StHbtMaker/Cut/mercedesEventCutMonitor.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: mercedesEventCutMonitor.cxx,v 1.2 2003/01/17 16:47:31 mercedes Exp $
+ * $Id: mercedesEventCutMonitor.cxx,v 1.3 2003/01/22 16:05:11 mercedes Exp $
  *
  * Author: Mercedes Lopez Noriega , Ohio State, mercedes@pacific.mps.ohio-state.edu
  *
@@ -15,6 +15,9 @@
  ***************************************************************************
  *
  * $Log: mercedesEventCutMonitor.cxx,v $
+ * Revision 1.3  2003/01/22 16:05:11  mercedes
+ * Minor changes
+ *
  * Revision 1.2  2003/01/17 16:47:31  mercedes
  * Updated to get reference multiplicity
  *
@@ -34,11 +37,11 @@ ClassImp(mercedesEventCutMonitor)
 #endif
 
 mercedesEventCutMonitor::mercedesEventCutMonitor(){
-  mEventMultHisto = new StHbt1DHisto("Event Multiplicity","EventMultHisto",1000,0.,1000.);
+  mEventMultHisto = new StHbt1DHisto("EventMultHisto","Event Multiplicity",1000,0.,1000.);
   mEventMultHisto->SetDirectory(0);
-  mZVertexPosHisto = new StHbt1DHisto("ZVertex Position","zVertexPos",1000,-50.0,50.0);
+  mZVertexPosHisto = new StHbt1DHisto("ZVertexPos","zVertex Position",1000,-50.0,50.0);
   mZVertexPosHisto->SetDirectory(0);
-  mEventMultvsTracks = new StHbt2DHisto("Event Multiplicity vs Tracks ","EventMultvsTracks",1000, 0., 1000., 1000,0.,10000.0);
+  mEventMultvsTracks = new StHbt2DHisto("EventMultvsTracks ","Event Multiplicity vs Total Tracks",1000, 0., 1000., 1000,0.,10000.0);
   mEventMultvsTracks->SetDirectory(0);
 }
 //------------------------------
