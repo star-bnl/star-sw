0fef9f6b6902265108e5285df8106884dae41655
perev <>
2003-02-06 18:10:40 +0000

    Increase max browsed

--- StRoot/St_base/StArray.cxx
+++ StRoot/St_base/StArray.cxx
@@ -76,7 +76,7 @@ void StObjArray::Streamer(TBuffer &b)
 //______________________________________________________________________________
 void StObjArray::Browse(TBrowser *b)
 {
-  enum { maxBrowsable =  50 };
+  enum { maxBrowsable =  500 };
 
    // Browse this collection (called by TBrowser).
    // If b=0, there is no Browse call TObject::Browse(0) instead.
