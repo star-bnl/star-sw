5f61f3671fb541dd4540e680d1eb4b9146c0d7fa
gans <>
2003-02-10 18:11:32 +0000

    Added trigger prescales table

--- StRoot/StDetectorDbMaker/StDetectorDbTriggerID.h
+++ StRoot/StDetectorDbMaker/StDetectorDbTriggerID.h
@@ -4,6 +4,7 @@
 #include "StMaker.h"
 
 struct triggerID_st;
+struct trigPrescales_st;
 
 #ifdef sun
 #include <iostream.h>
@@ -16,8 +17,8 @@ class ostream;
 class StDetectorDbTriggerID{
 public:
     static StDetectorDbTriggerID*  instance();
-    unsigned int               getRunNumber();
-    unsigned int               getNumRows();
+    unsigned int               getIDRunNumber();
+    unsigned int               getIDNumRows();
     unsigned int               getIdxTrg(unsigned int entry = 0);
     unsigned int               getDaqTrgId(unsigned int entry = 0);
     unsigned int               getOfflineTrgId(unsigned int entry = 0);
@@ -25,6 +26,13 @@ public:
     unsigned int               getTrgVersion(unsigned int entry = 0);
     unsigned int               getThreashVersion(unsigned int entry = 0);
     unsigned int               getPsVersion(unsigned int entry = 0);
+
+    int                        getSRunNumber();
+    unsigned int              getSNumRows();
+    int                        getIdxTrigger(unsigned int entry = 0);
+    int                        getIdxLevel(unsigned int entry = 0);
+    int                        getId(unsigned int entry = 0);
+    float                      getPs(unsigned int entry = 0);
     friend ostream& operator<<(ostream& os, StDetectorDbTriggerID& v);
 
     // These fuction will be public
@@ -33,11 +41,18 @@ public:
     friend class nobody; // for virtual ~
     
 protected:
+// members of triggerID 
     virtual ~StDetectorDbTriggerID();
     StDetectorDbTriggerID();
-    triggerID_st * mTriggerID; // points to trigger struct
-    TTable* mTable; // points to table, need to re-intilize mTriggerID every event
-    unsigned int mNumRows;
+    triggerID_st * mTriggerID; // points to triggerID struct
+    TTable* mIDTable; // points to table, need to re-intilize mTriggerID every event
+    unsigned int mIDNumRows;
+
+// members of trigPrescales
+    trigPrescales_st * mTrigPrescales; // points to prescales struct
+    TTable* mSTable; // points to table, need to re-intilize mTrigPrescales every event
+    unsigned int mSNumRows;
+
     StMaker * mMaker; // Holds pointer to maker
 private:
     static StDetectorDbTriggerID* sInstance;
