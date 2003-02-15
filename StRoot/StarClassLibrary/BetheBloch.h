b1f43c691912727500ba7829cc7a1ff79316a3ef
fisyak <>
2003-02-15 02:24:57 +0000

    Add static function Girrf for GEANT3 prediction for dE/dx

--- StRoot/StarClassLibrary/BetheBloch.h
+++ StRoot/StarClassLibrary/BetheBloch.h
@@ -1,11 +1,14 @@
 //
-// $Id: BetheBloch.h,v 1.4 2001/04/24 15:36:30 fisyak Exp $
+// $Id: BetheBloch.h,v 1.5 2003/02/15 02:24:57 fisyak Exp $
 //
 // Description:
 // Function to return the expected dE/dx as a function of
 // beta*gamma for the TPC
 // 
 // $Log: BetheBloch.h,v $
+// Revision 1.5  2003/02/15 02:24:57  fisyak
+// Add static function Girrf for GEANT3 prediction for dE/dx
+//
 // Revision 1.4  2001/04/24 15:36:30  fisyak
 // Restore Manuel's parameterization, move scale of dE/dx from 2.4 to 1.4 keV/cm for MIP
 //
@@ -35,6 +38,7 @@ public:
     virtual ~BetheBloch();
     double   operator() (double);
     static   Double_t Sirrf(Double_t poverm, Double_t Length=60., Int_t k=0);
+    static   Double_t Girrf(Double_t poverm, Double_t Tmin=1.e-3, Int_t k=0);
     ClassDef(BetheBloch,1)
 };
 #endif
