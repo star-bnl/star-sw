6a0aa9bb3cda634ee9372bac275538309ce30e17
Piotr A Zolnierczuk <zolnie@iucf.indiana.edu>
2003-02-20 21:15:17 +0000

    *** empty log message ***

---
+++ StRoot/StEEmcUtil/EEmcGeom/EEmcDefs.h
@@ -0,0 +1,24 @@
+// Hey Emacs this is really -*-c++-*- ! 
+// \author Piotr A. Zolnierczuk    Aug 26, 2002
+#ifndef EEmcGeom_EEmcDefs_h
+#define EEmcGeom_EEmcDefs_h
+/*********************************************************************
+ * $Id: EEmcDefs.h,v 1.1 2003/02/20 21:15:16 zolnie Exp $
+ *********************************************************************
+ * Descripion:
+ * STAR Endcap Electromagnetic Calorimeter Definitions (temp file)
+ *********************************************************************
+ * $Log: EEmcDefs.h,v $
+ * Revision 1.1  2003/02/20 21:15:16  zolnie
+ * *** empty log message ***
+ *
+ *********************************************************************/
+
+const int kEEmcNumDepths     =  5;
+const int kEEmcNumSectors    = 12;
+const int kEEmcNumSubSectors =  5;
+const int kEEmcNumEtas       = 12;
+const int kEEmcNumStrips     =288;
+
+
+#endif
