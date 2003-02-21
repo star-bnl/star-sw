8936c97208f567fe97308d0b724a051110fc6c87
laue <>
2003-02-21 14:32:47 +0000

    Yuri's updates to the PID probabilities. dE/dx track length in TPC added

--- StRoot/StMuDSTMaker/COMMON/StMuProbPidTraits.h
+++ StRoot/StMuDSTMaker/COMMON/StMuProbPidTraits.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StMuProbPidTraits.h,v 1.1 2002/11/18 20:29:01 laue Exp $
+ * $Id: StMuProbPidTraits.h,v 1.2 2003/02/21 14:32:47 laue Exp $
  * Author: Frank Laue, BNL, laue@bnl.gov
  *
  ***************************************************************************/
@@ -31,6 +31,7 @@ public:
   StMuProbPidTraits(){/* no-op*/};
   double dEdxFit() const;                      ///< returns the fitted dEdx value
   double dEdxTruncated() const;                ///< returns the truncated 70% dEdx value
+  double dEdxTrackLength() const;              ///< returns the track length in TPC used for dE/dx calculations
   double ndf() const;                          ///< returns number of degrees of freedon
   double probability(unsigned int i) const;    ///< returns the probability of the track being of particle type i
   double sum() const;                          ///< returns the sum of all probabilities
@@ -38,6 +39,7 @@ public:
 
   void   setdEdxFit(double dedx );             ///< sets the fitted dEdx value;
   void   setdEdxTruncated(double dedx);        ///< sets the truncated dEdx value;
+  void   setdEdxTrackLength(double dedx );     ///< sets the track length in TPC used for dE/dx calculations
   void   setNdf(unsigned int);                 ///< set number of degrees of freedom
   void   setProbability(unsigned int, double); ///< set the probability for particle i
 
@@ -45,13 +47,15 @@ private:
   unsigned char mNDF;
   float mdEdxFit;
   float mdEdxTruncated;
+  float mdEdxTrackLength;
   float mProbabilities[__NPARTICLES__];      ///< array holding the pid probabilities
 
-  ClassDef(StMuProbPidTraits,1)
+  ClassDef(StMuProbPidTraits,2)
 };
 
 inline double StMuProbPidTraits::dEdxFit() const {return mdEdxFit;}
 inline double StMuProbPidTraits::dEdxTruncated() const { return mdEdxTruncated;}
+inline double StMuProbPidTraits::dEdxTrackLength() const {return mdEdxTrackLength;}
 inline double StMuProbPidTraits::ndf() const  { return mNDF;}
 inline double StMuProbPidTraits::probability(unsigned int i) const { return (i<__NPARTICLES__) ? mProbabilities[i] : 0;}  
 inline double StMuProbPidTraits::sum() const { double s=0; for (int i=0;i<__NPARTICLES__;i++) s += mProbabilities[i]; return s;}
@@ -59,6 +63,7 @@ inline int    StMuProbPidTraits::numberOfParticles() const { return __NPARTICLES
 
 inline void   StMuProbPidTraits::setdEdxFit(double dEdx ) { mdEdxFit = dEdx; }
 inline void   StMuProbPidTraits::setdEdxTruncated(double dEdx) { mdEdxTruncated = dEdx; }
+inline void   StMuProbPidTraits::setdEdxTrackLength(double dEdxTrackLength) { mdEdxTrackLength = dEdxTrackLength; }
 inline void   StMuProbPidTraits::setProbability(unsigned int i, double prob) { if (i<__NPARTICLES__) mProbabilities[i]=prob;}
 inline void   StMuProbPidTraits::setNdf(unsigned int i) { mNDF = i; }
 
@@ -69,6 +74,9 @@ inline void   StMuProbPidTraits::setNdf(unsigned int i) { mNDF = i; }
 /***************************************************************************
  *
  * $Log: StMuProbPidTraits.h,v $
+ * Revision 1.2  2003/02/21 14:32:47  laue
+ * Yuri's updates to the PID probabilities. dE/dx track length in TPC added
+ *
  * Revision 1.1  2002/11/18 20:29:01  laue
  * New class. Wrapper for Yuri's new StProbPidTraits.
  *
