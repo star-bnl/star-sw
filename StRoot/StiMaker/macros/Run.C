143508180ee1af9f2acab476d73143136273e215
pruneau <>
2003-03-12 17:58:13 +0000

    fixing stuff

--- StRoot/StiMaker/macros/Run.C
+++ StRoot/StiMaker/macros/Run.C
@@ -40,27 +40,16 @@ void loadLibrairies(bool doProfile)
 }
 
 void Run(int firstEvent=0,
-	 int nEvents=5,
+	 int nEvents=10,
 	 //const char * filePrefix = "st_physics_",
 	 const char * filePrefix = "rcf",
 	 //const char * path= "/star/data13/reco/ppMinBias/FullField/P02gf/2002/019/",
 	 //const char * path ="/star/data22/ITTF/EvalData/Event/ppMinBias/",
   	 //const char * file="st_physics_3019045_raw_0031.event.root",
 	 //const char * path= "/star/data22/ITTF/EvalData/MCFiles/auau200",
-	 const char * path= "/a1/ittf/auau200/hijing/b0_20/standard/year2001/",
+	 const char * path= "/data/r20b/ittf/auau200/hijing/b0_20/standard/year2001/",
 	 //const char * file="rcf0183_12_300evts.event.root",
 	 const char * file="rcf0183_05_300evts.geant.root",
-  
-//  	 bool useGui=false,
-//  	 bool doSimulation=false,
-//  	 bool doAssociation=false,
-//  	 bool doMiniMcEvent=false,
-//  	 bool doDst=false,
-//  	 bool doStEventOutput=true,
-//  	 bool doStEventInput=true,
-//  	 bool doProfile=false
-
-	 //Example of switch setup for minimctree output
 	 bool useGui=false,
 	 bool doSimulation=true,
 	 bool doAssociation=true,
@@ -87,7 +76,7 @@ void Run(int firstEvent=0,
       doSimulation,
       doAssociation,
       doMiniMcEvent,
-      doDst=false,
+      doDst,
       doStEventOutput,
       doStEventInput,
       doProfile);
@@ -97,13 +86,13 @@ void Run(Int_t firstEvent,
 	 Int_t nEvents, 
 	 const char *  filePrefix,
 	 const char ** fileList,
-	 bool useGui=true,
-	 bool doSimulation=true,
-	 bool doAssociation=true,
-	 bool doMiniMcEvent=true,
-	 bool doDst=false,
-	 bool doStEventOutput=false,
-	 bool doStEventInput=false,
+	 bool useGui,
+	 bool doSimulation,
+	 bool doAssociation,
+	 bool doMiniMcEvent,
+	 bool doDst,
+	 bool doStEventOutput,
+	 bool doStEventInput,
 	 bool doProfile=false )
 {
   loadLibrairies(doProfile);
@@ -118,51 +107,3 @@ void Run(Int_t firstEvent,
   pars->doStEventInput=doStEventInput;  
   miniChain->run(firstEvent,nEvents,filePrefix,fileList);
 }
-
-
-
-// keep this here for convenience for a short while... CP
-
-	/*
-  StiRootIOBroker * stiIoBroker = miniChain->getIOBroker();
-  stiIoBroker->setTPHFMinPadrow(1);
-  stiIoBroker->setTPHFMaxPadrow(45);
-  stiIoBroker->setETSFLowerBound(5);
-  stiIoBroker->setETSFMaxHits(6);
-  stiIoBroker->setDoTrackFit(false);
-  //Set Kalman Track Finder (KTF) run-time values:
-  //stiIoBroker->setKTFMcsCalculated(true);
-  //stiIoBroker->setKTFElossCalculated(true);
-  stiIoBroker->setKTFMcsCalculated(false);
-  stiIoBroker->setKTFElossCalculated(false);
-  stiIoBroker->setKTFMaxChi2ForSelection(20);
-  stiIoBroker->setKTFBField(.5); //Tesla
-  stiIoBroker->setKTFMassHypothesis(.1395); //GeV
-  stiIoBroker->setKTFMinContiguousHitCount(2);
-  stiIoBroker->setKTFMaxNullCount(20);
-  stiIoBroker->setKTFMaxContiguousNullCount(15);
-  stiIoBroker->setKTFMinSearchRadius(.5); //cm
-  stiIoBroker->setKTFMaxSearchRadius(4.); //cm
-  stiIoBroker->setKTFSearchWindowScale(5.); //cm
-  //Set Local Track Seed Finder (LTSF) run-time values
-  //stiIoBroker->setLTSFZWindow(5.);
-  //stiIoBroker->setLTSFYWindow(2.);
-  stiIoBroker->setLTSFZWindow(10.);
-  stiIoBroker->setLTSFYWindow(4.);
-  stiIoBroker->setLTSFSeedLength(2);
-  stiIoBroker->setLTSFDoHelixFit(true);
-  stiIoBroker->setLTSFExtrapYWindow(1.);
-  stiIoBroker->setLTSFExtrapZWindow(2.);
-  stiIoBroker->setLTSFExtrapMaxSkipped(2);
-  stiIoBroker->setLTSFExtrapMinLength(4);
-  stiIoBroker->setLTSFExtrapMaxLength(5);
-  stiIoBroker->setLTSFUseVertex(true);
-  stiIoBroker->setLTMDeltaR(1.); //10% in r
-  //TPC Sectors
-  for (unsigned int sector=1; sector<=12; ++sector) 
-    stiIoBroker->addLTSFSector(sector);
-  //Add padrows;
-  for (unsigned int padrow=6; padrow<=45; padrow+=1)
-    stiIoBroker->addLTSFPadrow(padrow);
-  stiIoBroker->setSimulated(doSimulation);
-	*/
