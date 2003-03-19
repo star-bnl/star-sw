da6eace6edeabca817f1c1d18dc95215e2569eda
Frank Laue <laue@bnl.gov>
2003-03-19 18:58:52 +0000

    printing of trigger id collecton added to the example

--- StRoot/StMuDSTMaker/COMMON/macros/exampleInChain.C
+++ StRoot/StMuDSTMaker/COMMON/macros/exampleInChain.C
@@ -4,37 +4,14 @@ class StMuDstMaker;
 
 
 void exampleInChain() {
-   if (gClassTable->GetID("TTable") < 0) {
-     gSystem->Load("libStar");
-     gSystem->Load("libPhysics");
-   }     
-  gSystem->Load("St_base");
-  gSystem->Load("StChain");
-  gSystem->Load("St_Tables");
-  gSystem->Load("StMagF");
-  gSystem->Load("StUtilities");  // new addition 22jul99
-  gSystem->Load("StTreeMaker");
-  gSystem->Load("StIOMaker");
-  gSystem->Load("StarClassLibrary");
-  gSystem->Load("StTpcDb");
-  gSystem->Load("StDbUtilities");
-  gSystem->Load("StEvent");
-  gSystem->Load("StEventUtilities"); 
-  gSystem->Load("StMcEvent"); 
-  gSystem->Load("StMcEventMaker"); 
-  gSystem->Load("StAssociationMaker");
-  gSystem->Load("StMcAnalysisMaker");
-  gSystem->Load("StStrangeMuDstMaker");
-  gSystem->Load("StEmcUtil");
-  gSystem->Load("StMuDSTMaker");
-
-  cout << " loading done " << endl;
+  gROOT->LoadMacro("StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  loadSharedLibraries();
   
   StChain* chain = new StChain("StChain"); 
   chain->SetDebug(1);
   
   StMuDebug::setLevel(0);  // switch of some debug output
-  StMuDstMaker* muDstMaker = new StMuDstMaker(0,0,"/star/data02/scratch/laue/MuDstTest/","","MuDst.root",2,"MuDst");   // set up maker in read mode
+  StMuDstMaker* muDstMaker = new StMuDstMaker(0,0,"","dAu200.lis","MuDst.root",2,"MuDst");   // set up maker in read mode
   StMuDst2StEventMaker* eventMaker = new StMuDst2StEventMaker("MuDst2StEvent");
 
   chain->Init(); // This should call the Init() method in ALL makers
