02a29dc42d38e6f2ebb2f6f574dd641d59d0022a
Jan Balewski <balewski@lbl.gov>
2003-03-22 19:37:39 +0000

    *** empty log message ***

--- StRoot/StEEmcUtil/macros/fzd2EEevent.C
+++ StRoot/StEEmcUtil/macros/fzd2EEevent.C
@@ -9,7 +9,11 @@ St_geant_Maker *geant= 0;
 // reads .fzd file and prints on the screen
 
 // ______________________________________________
-void fzd2EEevent(const Int_t Nevents=5, TString fzFile ="../sim2003/mc_eve2.fzd") {
+void fzd2EEevent(const Int_t Nevents=10000, TString fzFile ="../sim2003/mc_eve2.fzd") {
+  fzFile ="pid6.fzd";
+  fzFile ="dAu83.fzd";
+  //fzFile ="../sim2003/mc_pi0-1.5.fzd";
+  //fzFile ="../sim2003/mc_eve3.fzd";
   Int_t  i=0;
   gSystem->Load("St_base");
   gSystem->Load("StChain");
--- StRoot/StEEmcUtil/macros/fzd2print.C
+++ StRoot/StEEmcUtil/macros/fzd2print.C
@@ -9,13 +9,14 @@ St_geant_Maker *geant= 0;
 // reads .fzd file and prints on the screen
 
 // ______________________________________________
-void fzd2print(const Int_t Nevents=1, const Char_t *fzfile ="mc_eve.fzd",int socketID=9093) {
+void fzd2print(const Int_t Nevents=1, const Char_t *fzfile ="../sim2003/mc_eve2.fzd") {
   Int_t  i=0;
   gSystem->Load("St_base");
   gSystem->Load("StChain");
+  gSystem->Load("StarClassLibrary");
 
   gROOT->LoadMacro("$STAR/StRoot/macros/bfc.C");
-  gSystem->Load("EEmc.so"); 
+  gSystem->Load("StEEmcUtil.so"); 
   bfc(0,"fzin sim_T gen_T",fzfile);
   EEmcMCData *evIN=new   EEmcMCData;
   
