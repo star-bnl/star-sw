59d2c2baf9f705a88b46a3ea71353dc467705538
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2003-03-31 21:21:03 +0000

    name changed from MiniChainParameter

---
+++ StRoot/StiMaker/StiMakerParameters.cxx
@@ -0,0 +1,46 @@
+#include "StiMakerParameters.h"
+
+ClassImp(StiMakerParameters)
+
+StiMakerParameters::StiMakerParameters()
+  : useEmc(false),
+    useEemc(false),
+    useSvt(false),
+    useSsd(false),
+    useTpc(true),
+    useFtpc(false),
+    useMcAsRec(false),
+    useGui(false),
+    doSimulation(false),
+    doAssociation(false),
+    doMiniMcEvent(false),
+    doDst(false),
+    doStEventOutput(false),
+    doStEventInput(false),
+    doPlots(false)
+{}
+
+StiMakerParameters::~StiMakerParameters()
+{}
+
+ostream& operator<<(ostream& os, const StiMakerParameters&pars)
+{
+  cout << "StiMakerParameters :" <<endl
+       << "                         Use EMC :" << pars.useEmc  <<endl
+       << "                        Use EEMC :" << pars.useEemc <<endl
+       << "                         Use SVT :" << pars.useSvt  <<endl
+       << "                         Use SSD :" << pars.useSsd  <<endl
+       << "                         Use TPC :" << pars.useTpc  <<endl
+       << "                        Use FTPC :" << pars.useFtpc <<endl
+       << "                        Use GUI  :" << pars.useGui  <<endl
+       << "Use MC HITS As Reconstructed Hits:" << pars.useMcAsRec << endl
+       << "                    doSimulation :" << pars.doSimulation <<endl
+       << "                   doAssociation :" << pars.doAssociation <<endl
+       << "                   doMiniMcEvent :" << pars.doMiniMcEvent <<endl
+       << "                           doDst :" << pars.doDst <<endl
+       << "                 doStEventOutput :" << pars.doStEventOutput <<endl
+       << "                  doStEventInput :" << pars.doStEventInput   <<endl
+       << "                         doPlots :" << pars.doPlots << endl;
+  return os;
+}
+
---
+++ StRoot/StiMaker/StiMakerParameters.h
@@ -0,0 +1,37 @@
+#ifndef StiMakerParameters_H_INCLUDED
+#define StiMakerParameters_H_INCLUDED
+
+#include <iostream.h>
+#include "TObject.h"
+
+class StiMakerParameters : public TObject
+{
+ public:
+  StiMakerParameters();
+  virtual ~StiMakerParameters();
+
+  // Detectors
+  bool useEmc;
+  bool useEemc;
+  bool useSvt;
+  bool useSsd;
+  bool useTpc;
+  bool useFtpc;
+
+  bool useMcAsRec;
+  bool useGui;
+  bool doSimulation;
+  bool doAssociation;
+  bool doMiniMcEvent;
+  bool doDst;
+  bool doStEventOutput;
+  bool doStEventInput;  
+  bool doPlots;
+
+  friend ostream& operator<<(ostream& os, const StiMakerParameters&pars);
+  
+  ClassDef(StiMakerParameters, 1)
+
+};
+
+#endif
