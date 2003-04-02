f558505fadd137c890f093d3db375f5efa16ec29
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2003-04-02 16:45:19 +0000

    Fixed error calculation and impose hard coded upper cut on the size of the error.

--- StRoot/Sti/StiHitErrorCalculator.cxx
+++ StRoot/Sti/StiHitErrorCalculator.cxx
@@ -1,5 +1,5 @@
 /*!
- * $Id: StiHitErrorCalculator.cxx,v 2.1 2003/01/08 21:17:33 pruneau Exp $  
+ * $Id: StiHitErrorCalculator.cxx,v 2.2 2003/04/02 16:45:19 pruneau Exp $  
  *
  * Author: A. Rose, WSU, Jan 2002
  *
@@ -11,6 +11,9 @@
  *
  *
  * $Log: StiHitErrorCalculator.cxx,v $
+ * Revision 2.2  2003/04/02 16:45:19  pruneau
+ * Fixed error calculation and impose hard coded upper cut on the size of the error.
+ *
  * Revision 2.1  2003/01/08 21:17:33  pruneau
  * Addind class StiSortedHitIterator to work in the seed finder
  * and StiDummyVertex finder to provide an StEvent based vertex
@@ -52,12 +55,19 @@ void StiDefaultHitErrorCalculator::calculateError(StiKalmanTrackNode * node) con
   double dz = (fabs(node->getZ())-200.)/100.;
   double cosCA = node->_cosCA;
   double sinCA = node->_sinCA;
+  if (cosCA==0.)
+    cosCA=1.e-10;
   double tanCA = sinCA/cosCA;
-  double ecross=coeff[0]-coeff[1]*dz/cosCA +coeff[2]*tanCA*tanCA;
+  double ecross=coeff[0]-coeff[1]*dz/(cosCA*cosCA) +coeff[2]*tanCA*tanCA;
   double tanDip=node->getTanL();
   double cosDipInv=sqrt(1+tanDip*tanDip);
   double edip=coeff[3]-coeff[4]*dz*cosDipInv+coeff[5]*tanDip*tanDip;
-  node->eyy = 10000.*ecross*ecross;
-  node->ezz = 10000.*edip*edip;
+  if (ecross>20.e-2) ecross = 20.e-2; // cp 04/02/2003
+  if (edip>20.e-2) edip = 20.e-2;     // in meters here
+  node->eyy = 10000.*ecross; // in centimeters here...
+  node->ezz = 10000.*edip;
+  if (ecross<0 || edip<0 || node->eyy<=0. || node->eyy>10. || node->ezz<=0. || node->ezz>10.)
+    cout << " Hit Error - ecross:"<<ecross<<" edip:"<<edip
+	 <<" node->eyy:"<<node->eyy<<" node->ezz:"<<node->ezz<<endl;
 }
 
