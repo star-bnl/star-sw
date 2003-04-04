7a31dfb6a9bea730f0f6feecc20efc330d0645af
wzhang <>
2003-04-04 15:35:47 +0000

    added SMD constants

--- StRoot/StEEmcUtil/EEmcGeom/EEmcGeomDefs.h
+++ StRoot/StEEmcUtil/EEmcGeom/EEmcGeomDefs.h
@@ -3,12 +3,15 @@
 #ifndef EEmcGeom_EEmcDefs_h
 #define EEmcGeom_EEmcDefs_h
 /*********************************************************************
- * $Id: EEmcGeomDefs.h,v 1.3 2003/03/22 22:44:57 zolnie Exp $
+ * $Id: EEmcGeomDefs.h,v 1.4 2003/04/04 15:35:47 wzhang Exp $
  *********************************************************************
  * Descripion:
  * STAR Endcap Electromagnetic Calorimeter Definitions (temp file)
  *********************************************************************
  * $Log: EEmcGeomDefs.h,v $
+ * Revision 1.4  2003/04/04 15:35:47  wzhang
+ * added SMD constants
+ *
  * Revision 1.3  2003/03/22 22:44:57  zolnie
  * make it standalone library
  *
@@ -27,7 +30,10 @@ const int kEEmcNumDepths     =  5;
 const int kEEmcNumSectors    = 12;
 const int kEEmcNumSubSectors =  5;
 const int kEEmcNumEtas       = 12;
+const int kEEmcNumSmdPlanes  =  3;
 const int kEEmcNumStrips     =288;
+const int kEEmcNumEdgeStrips =283;
+const int kEEmcNumSmdLayers  =  2;  // 2 SMD layers: U and V
 
 
 const float kEEmcZPRE1=270.190; // [cm] z preshower1
@@ -35,6 +41,29 @@ const float kEEmcZPRE2=271.695; // [cm] z preshower2
 const float kEEmcZSMD =279.542; // [cm] z location of the EEMC SMD layer 
 const float kEEmcZPOST=306.158; // [cm] z postshower  
 
+// z-shift [cm] of innermost (negative) and outermost (positve) EEMC SMD plane 
+const float kEEmcSmdZPlaneShift = 1.215;
+
+// inner-shift offset of radius [cm] of EEMC SMD layers in 3 planes
+const float kEEmcSmdROffset[kEEmcNumSmdPlanes]={1.850,0.925,0.0};
+
+// The value of the map matrix(3x12) indicates if a EEMC SMD layer in a plane 
+// is a U layer(1), V layer(2), or void(0)  
+const int   kEEmcSmdMapUV[kEEmcNumSmdPlanes][kEEmcNumSectors] = 
+                                     {{2,1,0,2,1,0,2,1,0,2,1,0},
+                                      {0,2,1,0,2,1,0,2,1,0,2,1},
+                                      {1,0,2,1,0,2,1,0,2,1,0,2}};
+
+// The value of the map matrix(3x12) indicates if a EEMC SMD module in a plane
+// is an edge module 
+const int   kEEmcSmdMapEdge[kEEmcNumSmdPlanes][kEEmcNumSectors] = 
+                                     {{0,0,0,1,0,0,0,0,0,1,0,0},
+                                      {0,0,1,0,0,0,0,0,1,0,0,0},
+                                      {0,0,0,0,0,0,0,0,0,0,0,0}};
+
+const char kEEmcSmdLayerChar[kEEmcNumSmdLayers] = {'U','V'};
+
+const int kEEmcSmdModuleIdPhiCrossPi = 9;
 
 #endif
 /* ------------------------------------------
