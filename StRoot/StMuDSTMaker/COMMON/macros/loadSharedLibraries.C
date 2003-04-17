b32f3eb514d0c3cf1a2d32fc807e065cd51ddb38
jeromel <>
2003-04-17 20:35:49 +0000

    Library added

--- StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C
+++ StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C
@@ -5,10 +5,11 @@ void loadSharedLibraries() {
   gSystem->Load("St_base");
   gSystem->Load("StChain");
   gSystem->Load("St_Tables");
-  gSystem->Load("StUtilities");  // new addition 22jul99
+  gSystem->Load("StUtilities");        // new addition 22jul99
   gSystem->Load("StTreeMaker");
   gSystem->Load("StIOMaker");
   gSystem->Load("StarClassLibrary");
+  gSystem->Load("StTriggerDataMaker"); // new starting from April 2003
   gSystem->Load("StEvent");
   gSystem->Load("StEventUtilities");
   gSystem->Load("StEmcUtil");
