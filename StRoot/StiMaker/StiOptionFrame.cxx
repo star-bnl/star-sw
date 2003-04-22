12cd88604bc22d9de0a2f8eb906b080573c3e69c
pruneau <>
2003-04-22 21:22:44 +0000

    tweeking

--- StRoot/StiMaker/StiOptionFrame.cxx
+++ StRoot/StiMaker/StiOptionFrame.cxx
@@ -108,54 +108,54 @@ void StiOptionFrame::initialize()
     {
       RootEditableParameter * par = static_cast<RootEditableParameter *>(*it);
       if (!par)
-				{
-					cout << "StiOptionFrame::initialize() - ERROR - static_cast failed."<<endl;
-					break;
-				}
+	{
+	  cout << "StiOptionFrame::initialize() - ERROR - static_cast failed."<<endl;
+	  break;
+	}
       //fNumericEntries.back().second->SetFormat(TGNumberFormat::kNESRealTwo);
       //TGNumberFormat::kNESRealFour,TGNumberFormat::kNESInteger
       
       frame.push_back( new TGHorizontalFrame(frame1, 200, 30) );
       frame1->AddFrame(frame.back(), layout2);
       if (par->getType()==Parameter::Boolean)
-				{
-					TGCheckButton* tempButton = new TGCheckButton(frame.back(),
-																												new TGHotString(par->getDescription().c_str()), -1);
-					par->setCheckButton(tempButton);
-					if (par->getBoolValue()==true)
-						tempButton->SetState(kButtonDown);
-					frame.back()->AddFrame(tempButton, layout2);
-				}
+	{
+	  TGCheckButton* tempButton = new TGCheckButton(frame.back(),
+							new TGHotString(par->getDescription().c_str()), -1);
+	  par->setCheckButton(tempButton);
+	  if (par->getBoolValue()==true)
+	    tempButton->SetState(kButtonDown);
+	  frame.back()->AddFrame(tempButton, layout2);
+	}
       else
-				{
-					TGNumberEntry * numberEntry = new TGNumberEntry( frame.back() );
-					par->setNumberEntry(numberEntry);
-					if (par->getType()==Parameter::Integer)
-						{
-							// Integer
-							numberEntry->SetNumber( par->getIntValue() );
-							numberEntry->SetFormat(TGNumberFormat::kNESInteger, 
-																		 TGNumberFormat::kNEAAnyNumber);
-						}
-					else
-						{
-							// Double 
-							numberEntry->SetNumber( par->getDoubleValue() );
-							if (par->getIncrement()==2)
-								// two significant figures
-								numberEntry->SetFormat(TGNumberFormat::kNESRealTwo, 
-																			 TGNumberFormat::kNEAAnyNumber);
-							else
-								// one only
-								numberEntry->SetFormat(TGNumberFormat::kNESRealOne, 
-																			 TGNumberFormat::kNEAAnyNumber);
-						}
-					numberEntry->SetLimits(TGNumberFormat::kNELLimitMinMax, par->getMinimum(),par->getMaximum() );
-					numberEntry->Associate(this);
-					frame.back()->AddFrame(numberEntry, layout2);
-					fLabel.push_back( new TGLabel(frame.back(), par->getDescription().c_str() ));
-					frame.back()->AddFrame(fLabel.back(), layout2);
-				}
+	{
+	  TGNumberEntry * numberEntry = new TGNumberEntry( frame.back() );
+	  par->setNumberEntry(numberEntry);
+	  if (par->getType()==Parameter::Integer)
+	    {
+	      // Integer
+	      numberEntry->SetNumber( par->getIntValue() );
+	      numberEntry->SetFormat(TGNumberFormat::kNESInteger, 
+				     TGNumberFormat::kNEAAnyNumber);
+	    }
+	  else
+	    {
+	      // Double 
+	      numberEntry->SetNumber( par->getDoubleValue() );
+	      if (par->getIncrement()==2)
+		// two significant figures
+		numberEntry->SetFormat(TGNumberFormat::kNESRealTwo, 
+				       TGNumberFormat::kNEAAnyNumber);
+	      else
+		// one only
+		numberEntry->SetFormat(TGNumberFormat::kNESRealOne, 
+				       TGNumberFormat::kNEAAnyNumber);
+	    }
+	  numberEntry->SetLimits(TGNumberFormat::kNELLimitMinMax, par->getMinimum(),par->getMaximum() );
+	  numberEntry->Associate(this);
+	  frame.back()->AddFrame(numberEntry, layout2);
+	  fLabel.push_back( new TGLabel(frame.back(), par->getDescription().c_str() ));
+	  frame.back()->AddFrame(fLabel.back(), layout2);
+	}
     }
 }
 
