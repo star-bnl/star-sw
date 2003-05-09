8d30332713ef2688402813838a4f1ca078baaf24
pruneau <>
2003-05-09 14:59:41 +0000

    Synch

--- StRoot/StiSvt/StiSvtHitLoader.cxx
+++ StRoot/StiSvt/StiSvtHitLoader.cxx
@@ -31,7 +31,6 @@ void StiSvtHitLoader::loadHits(StEvent* source,
 			       Filter<StiTrack> * trackFilter, 
 			       Filter<StiHit> * hitFilter)
 {
-  return;
   _messenger <<"StiSvtHitLoader::loadHits() - Started"<<endl;
   if (!source)
     throw runtime_error("StiSvtHitLoader::loadHits() - FATAL - source==0 ");
@@ -77,7 +76,91 @@ void StiSvtHitLoader::loadHits(StEvent* source,
 		      _messenger <<"StiSvtHitLoader::loadHits() - WARNING - hit==0"<<endl;
 		      break;
 		    }
-		  detector = _detector->getDetector(hit->layer(),hit->ladder());
+		  int svtLayer = hit->layer();
+		  int svtLadder = hit->ladder();
+		  int layer,ladder;
+		  switch (svtLayer)
+		    {
+		    case 1:
+		      switch (svtLadder)
+			{
+			case 2: layer = 0; ladder = 0;break;
+			case 4: layer = 0; ladder = 1;break;
+			case 6: layer = 0; ladder = 2;break;
+			case 8: layer = 0; ladder = 3;break;
+			default: throw runtime_error("StiSvtHitLoader:loadHits() -E- 1");
+			}
+		      break;
+		    case 2:
+		      switch (svtLadder)
+			{
+			case 1: layer = 1; ladder = 0;break;
+			case 3: layer = 1; ladder = 1;break;
+			case 5: layer = 1; ladder = 2;break;
+			case 7: layer = 1; ladder = 3;break;
+			default: throw runtime_error("StiSvtHitLoader:loadHits() -E- 2");
+			}
+		      break;
+		    case 3:
+		      switch (svtLadder)
+			{
+			case  2: layer = 2; ladder = 0;break;
+			case  4: layer = 2; ladder = 1;break;
+			case  6: layer = 2; ladder = 2;break;
+			case  8: layer = 2; ladder = 3;break;
+			case 10: layer = 2; ladder = 4;break;
+			case 12: layer = 2; ladder = 5;break;
+			default: throw runtime_error("StiSvtHitLoader:loadHits() -E- 3");
+			}
+		      break;
+		    case 4:
+		      switch (svtLadder)
+			{
+			case  1: layer = 3; ladder = 0;break;
+			case  3: layer = 3; ladder = 1;break;
+			case  5: layer = 3; ladder = 2;break;
+			case  7: layer = 3; ladder = 3;break;
+			case  9: layer = 3; ladder = 4;break;
+			case 11: layer = 3; ladder = 5;break;
+			default: throw runtime_error("StiSvtHitLoader:loadHits() -E- 4");
+			}
+		      break;
+		    case 5:
+		      switch (svtLadder)
+			{
+			case  2: layer = 4; ladder = 0;break;
+			case  4: layer = 4; ladder = 1;break;
+			case  6: layer = 4; ladder = 2;break;
+			case  8: layer = 4; ladder = 3;break;
+			case 10: layer = 4; ladder = 4;break;
+			case 12: layer = 4; ladder = 5;break;
+			case 14: layer = 4; ladder = 6;break;
+			case 16: layer = 4; ladder = 7;break;
+			default: throw runtime_error("StiSvtHitLoader:loadHits() -E- 5");
+			}
+		      break;
+		    case 6:
+		      switch (svtLadder)
+			{
+			case  1: layer = 5; ladder = 0;break;
+			case  3: layer = 5; ladder = 1;break;
+			case  5: layer = 5; ladder = 2;break;
+			case  7: layer = 5; ladder = 3;break;
+			case  9: layer = 5; ladder = 4;break;
+			case 11: layer = 5; ladder = 5;break;
+			case 13: layer = 5; ladder = 6;break;
+			case 15: layer = 5; ladder = 7;break;
+			default: throw runtime_error("StiSvtHitLoader:loadHits() -E- 6");
+			}
+		      break;
+		    }
+		  cout << "hit->layer():"<<hit->layer()
+		       << "hit->ladder():"<<hit->ladder()
+		       << " layer:"<<layer
+		       << " ladder:"<<ladder<<endl;
+		  
+		  //detector = _detector->getDetector(hit->layer(),hit->ladder());
+		  detector = _detector->getDetector(layer,ladder);
 		  if (hit && detector && hit->flag()<4) 
 		    {
 		      cout << "StiSvtHitLoader::loadHits() - INFO - set global"<<endl;
