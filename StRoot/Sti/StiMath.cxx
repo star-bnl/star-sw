31839d7e1f51912e0eb8dc253dbadf4ac021791f
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2003-05-11 07:59:12 +0000

    removed faulty ClassImpl

--- StRoot/Sti/StiMath.cxx
+++ StRoot/Sti/StiMath.cxx
@@ -1,9 +1,14 @@
-#include "TObject.h"
 #include <stdexcept>
 #include <values.h>
 #include <math.h>
 #include "Sti/StiMath.h"
-ClassImp(StiMath)
+double StiMath::_logGammaCof[6];
+/*={76.18009172947146,-86.50532032941677,
+				 24.01409824083091,-1.231739572450155,
+				 0.1208650973866179e-2,-0.5395239384953e-5};*/
+int StiMath::_nTop=4;
+double StiMath::_fac[33];/*={1.0,1.,2.0,6.0,24.0};*/
+double StiMath::_logFac[101];
 
 double StiMath::chi2(double x, int n)
 {
@@ -12,8 +17,6 @@ double StiMath::chi2(double x, int n)
   return exp(logChi2);
 }
 
-
-
 ///Calculates the log of the gamma fct 
 ///based on a clever recipee by Lanczos
 ///This code taken from Numerical Recipees by Press et al.
@@ -21,16 +24,13 @@ double StiMath::chi2(double x, int n)
 double StiMath::logGamma(double xx)
 {
  double x,y,tmp,ser;
- static double cof[6]={76.18009172947146,-86.50532032941677,
-  24.01409824083091,-1.231739572450155,
-  0.1208650973866179e-2,-0.5395239384953e-5};
  int j;
    
  y=x=xx;
  tmp=x+5.5;
  tmp -= (x+0.5)*log(tmp);
  ser=1.000000000190015;
- for (j=0;j<=5;j++) ser += cof[j]/++y;
+ for (j=0;j<=5;j++) ser += _logGammaCof[j]/++y;
  return -tmp+log(2.5066282746310005*ser/x);
 }
 
@@ -49,19 +49,17 @@ double StiMath::gamma(double x)
 ///May overflow for large n.
 double StiMath::factorial(int n)
 {
-  static int nTop=4;
-  static double fac[33]={1.0,1.,2.0,6.0,24.0};
   int j;
   if (n<0)
     throw runtime_error("StiMath:factorial(int n) -E- n out of bound");
   if (n>32)
     return gamma(n+1.0);
-  while (n>nTop)
+  while (n>_nTop)
     {
-      j=nTop++;
-      fac[nTop]=fac[j]*nTop;
+      j=_nTop++;
+      _fac[_nTop]=_fac[j]*_nTop;
     }
-  return fac[n];
+  return _fac[n];
 }
 
 ///Calculates and returns the log(factorial(n))
@@ -69,7 +67,6 @@ double StiMath::factorial(int n)
 ///May overflow for large n
 double StiMath::logFactorial(int n)
 {
-  static double _logFac[101];
   if (n<0) throw runtime_error("StiMath:logFactorial(int n) -E- n out of bound");
   if (n<=1) return 0.0;
   if (n<=100) return _logFac[n]?_logFac[n]:(_logFac[n]=logGamma(n+1.0));
