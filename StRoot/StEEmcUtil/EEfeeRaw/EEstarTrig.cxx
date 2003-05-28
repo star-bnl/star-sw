24dfef166f3640e4bf37f676ab1216f54db616d5
Jan Balewski <balewski@lbl.gov>
2003-05-28 21:01:00 +0000

    added spin bits

--- StRoot/StEEmcUtil/EEfeeRaw/EEstarTrig.cxx
+++ StRoot/StEEmcUtil/EEfeeRaw/EEstarTrig.cxx
@@ -19,11 +19,12 @@ EEstarTrig ::  ~EEstarTrig() { }
 
 
 
-#define clearVec(x)  memset( x, 0, sizeof(x))
 //--------------------------------------------------
 //
 //--------------------------------------------------
 void EEstarTrig :: clear() {
+#define clearVec(x)  memset( x, 0, sizeof(x))
+
   bX48hi=bX48lo=bX7bit=0;
   daqbits=0;
   clearVec( offline_id );
@@ -31,8 +32,12 @@ void EEstarTrig :: clear() {
   clearVec( EEMC_l1    );
   clearVec( BEMC       );
   clearVec( BEMC_l1    );
-  clearVec( CTB        );
-  // clearOne( );
+  clearVec( lastDSM    );
+  clearVec( VTX        );
+  clearVec( EMC        );
+  npre=npost=0;
+
+#undef clearVec
 }
 
 
@@ -53,7 +58,7 @@ void EEstarTrig :: print( int k) const{
   if(k<=0) return;
 
   int ii;
-  printf("  daqBits=0x%x\n",daqbits);
+  printf("  daqBits=0x%x  npre=%d npost=%d\n",daqbits,npre,npost);
   
   printf("  trigID[] -->  ");
   for(ii=0;ii<32;ii++)  {
@@ -63,17 +68,32 @@ void EEstarTrig :: print( int k) const{
   } 
   printf("\n");
 
+  printf(" bbcTimeDiff=%d spinBits=x%2.2x lastDSM[7]=x%4.4x\n",bbcTimeDiff(), spinBits(),lastDSM[7]);
+  printf(" Yell fill=%d   Up=%d  Down=%d  UnPol=%d \n",bitYellFill() ,bitYellUp(),bitYellDown(), bitYellUnpol());  
+  printf(" Blue fill=%d   Up=%d  Down=%d  UnPol=%d \n",bitBlueFill() ,bitBlueUp(),bitBlueDown(), bitBlueUnpol());  
   if(k<=1) return;
 
 
   for(ii=0;ii<144;ii++)  {
-	   if(ii%16==8)   printf(" , ");
+    if(ii%16==8)   printf(" , ");
     if(ii%16==0)   printf("\n EEMC board-%d= ",ii/16+1);
-    printf("x%02x  ",  EEMC[ii]);
+    printf("x%2.2x  ",  EEMC[ii]);
 }
   printf("\n");
+
+  int j;
+  
+  printf(" DSM VTX[j]   = ");
+  for (j=0; j<8; j++) printf("[%d]=x%4.4x ",j, VTX[j]);
+  printf("\n");
+  printf(" emcDSM[j]= ");
+  for (j=0; j<8; j++) printf("[%d]=x%4.4x ",j, EMC[j]);
+  printf("\n");
+  printf(" lastDSM[j]= ");
+  for (j=0; j<8; j++) printf("[%d]=x%4.4x ",j, lastDSM[j]);
+  printf("\n");
+
 }
- 
 
 
 
