2829c90a3ddf96c19cd9997e2b9cdfbda1f23b9a
Subhasis Chattopadyay (Chattopadhyay) <sub@vecc.gov.in>
2003-05-29 13:20:12 +0000

    NN Input cluster

---
+++ StRoot/StPmdDiscriminatorMaker/StNNCluster.h
@@ -0,0 +1,71 @@
+/*!
+ * \class StNNCluster
+ * \author
+ */
+/**************************************************************
+ *
+ * $Id: StNNCluster.h,v 1.1 2003/05/29 13:20:07 subhasis Exp $
+ *
+ * Author:
+ **************************************************************
+ *
+ * Description: class for PMD NN cluster
+ *
+ **************************************************************
+*
+* $Log: StNNCluster.h,v $
+* Revision 1.1  2003/05/29 13:20:07  subhasis
+* NN Input cluster
+*
+*
+***************************************************************/
+#ifndef STAR_StNNCluster
+#define STAR_StNNCluster
+
+#include <math.h>
+#include <iostream.h>
+#include "TArrayI.h"
+#include "StObject.h"
+#include "StPmdUtil/StPmdGeom.h"
+#include "StPmdUtil/StPmdHit.h"
+//#include "StEventTypes.h"
+
+class StPhmdCluster;
+
+class StNNCluster : public StObject {
+
+private:
+  //! cluster objects
+ StPhmdCluster * mpmdcluster; 
+ StPhmdCluster * mcpvcluster; 
+public: 
+  StNNCluster();      //! constructor     
+  StNNCluster(StPhmdCluster*,StPhmdCluster*);   //! constructor
+  ~StNNCluster();          //! destructor
+
+  /*! member functions*/
+
+  StPhmdCluster*    PmdCluster();
+  StPhmdCluster*    CpvCluster();
+
+  void            setPmdCluster(StPhmdCluster*);
+  void            setCpvCluster(StPhmdCluster*);
+
+  ClassDef(StNNCluster,1)// Base class for PMD cluster
+};
+
+
+inline              StNNCluster::~StNNCluster(){ /* Nobody */ }
+
+inline void StNNCluster::setPmdCluster(StPhmdCluster* var){mpmdcluster=var;}
+inline void StNNCluster::setCpvCluster(StPhmdCluster* var){mcpvcluster=var;}
+inline StPhmdCluster* StNNCluster::PmdCluster(){return mpmdcluster;}
+inline StPhmdCluster* StNNCluster::CpvCluster(){return mcpvcluster;}
+#endif
+
+
+
+
+
+
+
