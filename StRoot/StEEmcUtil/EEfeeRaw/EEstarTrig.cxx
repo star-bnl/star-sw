113017586586cf6fe1cd912eb65ad0a299cf940b
zolnie <>
2003-05-29 21:42:11 +0000

    add file destriptor to print() method

--- StRoot/StEEmcUtil/EEfeeRaw/EEstarTrig.cxx
+++ StRoot/StEEmcUtil/EEfeeRaw/EEstarTrig.cxx
@@ -45,7 +45,7 @@ void EEstarTrig :: clear() {
 //--------------------------------------------------
 //
 //--------------------------------------------------
-void EEstarTrig :: print( int k) const{
+void EEstarTrig :: print(FILE *fd,  int k) const{
   // printf("EEstarTrig:: print0() %d %d %d \n",bX48hi,bX48lo, bX7bit);
   unsigned long long int hi=bX48hi;
   unsigned long long int lo=bX48lo;
@@ -54,44 +54,44 @@ void EEstarTrig :: print( int k) const{
   int bx=bx48%120;
   int off=bx-bX7bit;
   if(off<0) off+=120;  
-  printf("EEstarTrig:: print() bX=%d bX7=%d off=%d\n",bx,bX7bit,off);
+  fprintf(fd,"EEstarTrig:: print() bX=%d bX7=%d off=%d\n",bx,bX7bit,off);
   if(k<=0) return;
 
   int ii;
-  printf("  daqBits=0x%x  npre=%d npost=%d\n",daqbits,npre,npost);
+  fprintf(fd,"  daqBits=0x%x  npre=%d npost=%d\n",daqbits,npre,npost);
   
-  printf("  trigID[] -->  ");
+  fprintf(fd,"  trigID[] -->  ");
   for(ii=0;ii<32;ii++)  {
     if(offline_id[ii]==0) break;
-    printf("[%2d]=0x%02X  [%u dec], ",ii,offline_id[ii],offline_id[ii]);
+    fprintf(fd,"[%2d]=0x%02X  [%u dec], ",ii,offline_id[ii],offline_id[ii]);
     if(ii%4==3) printf("\n      ");
   } 
-  printf("\n");
+  fprintf(fd,"\n");
 
-  printf(" bbcTimeDiff=%d spinBits=x%2.2x lastDSM[7]=x%4.4x\n",bbcTimeDiff(), spinBits(),lastDSM[7]);
-  printf(" Yell fill=%d   Up=%d  Down=%d  UnPol=%d \n",bitYellFill() ,bitYellUp(),bitYellDown(), bitYellUnpol());  
-  printf(" Blue fill=%d   Up=%d  Down=%d  UnPol=%d \n",bitBlueFill() ,bitBlueUp(),bitBlueDown(), bitBlueUnpol());  
+  fprintf(fd," bbcTimeDiff=%d spinBits=x%2.2x lastDSM[7]=x%4.4x\n",bbcTimeDiff(), spinBits(),lastDSM[7]);
+  fprintf(fd," Yell fill=%d   Up=%d  Down=%d  UnPol=%d \n",bitYellFill() ,bitYellUp(),bitYellDown(), bitYellUnpol());  
+  fprintf(fd," Blue fill=%d   Up=%d  Down=%d  UnPol=%d \n",bitBlueFill() ,bitBlueUp(),bitBlueDown(), bitBlueUnpol());  
   if(k<=1) return;
 
 
   for(ii=0;ii<144;ii++)  {
     if(ii%16==8)   printf(" , ");
     if(ii%16==0)   printf("\n EEMC board-%d= ",ii/16+1);
-    printf("x%2.2x  ",  EEMC[ii]);
-}
-  printf("\n");
+    fprintf(fd,"x%2.2x  ",  EEMC[ii]);
+  }
+  fprintf(fd,"\n");
 
   int j;
   
-  printf(" DSM VTX[j]   = ");
+  fprintf(fd," DSM VTX[j]   = ");
   for (j=0; j<8; j++) printf("[%d]=x%4.4x ",j, VTX[j]);
-  printf("\n");
-  printf(" emcDSM[j]= ");
+  fprintf(fd,"\n");
+  fprintf(fd," emcDSM[j]= ");
   for (j=0; j<8; j++) printf("[%d]=x%4.4x ",j, EMC[j]);
-  printf("\n");
-  printf(" lastDSM[j]= ");
+  fprintf(fd,"\n");
+  fprintf(fd," lastDSM[j]= ");
   for (j=0; j<8; j++) printf("[%d]=x%4.4x ",j, lastDSM[j]);
-  printf("\n");
+  fprintf(fd,"\n");
 
 }
 
--- StRoot/StEEmcUtil/EEfeeRaw/EEstarTrig.h
+++ StRoot/StEEmcUtil/EEfeeRaw/EEstarTrig.h
@@ -1,10 +1,13 @@
 #ifndef EEstarTrig_h
 #define EEstarTrig_h
 /*********************************************************************
- * $Id: EEstarTrig.h,v 1.2 2003/05/28 21:01:00 balewski Exp $
+ * $Id: EEstarTrig.h,v 1.3 2003/05/29 21:42:11 zolnie Exp $
  *********************************************************************
  * container for STAR trigger data
  * $Log: EEstarTrig.h,v $
+ * Revision 1.3  2003/05/29 21:42:11  zolnie
+ * add file destriptor to print() method
+ *
  * Revision 1.2  2003/05/28 21:01:00  balewski
  * added spin bits
  *
@@ -57,7 +60,7 @@ class EEstarTrig : public TObject {
   u_short bbcTimeDiff() const { return VTX[3] & 0x1ff; }
   EEstarTrig();
   virtual ~EEstarTrig();
-  void  print(int k=0) const;
+  void  print(FILE *stdout, int k=0) const;
   void  clear();
   ClassDef(EEstarTrig,2) 
 
