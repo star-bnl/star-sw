bb57776488e2220a908585e8649080ab2af2b93d
Andrew Allen Rose <aarose@lbl.gov>
2003-07-14 21:07:28 +0000

    Reverting to previous version to eliminate bug.

--- StRoot/StiTpc/StiTpcDetectorBuilder.cxx
+++ StRoot/StiTpc/StiTpcDetectorBuilder.cxx
@@ -20,25 +20,20 @@
 #include "StiTpcIsActiveFunctor.h"
 
 StiTpcDetectorBuilder::StiTpcDetectorBuilder(bool active)
-  : StiDetectorBuilder("Tpc",active)
+  : StiDetectorBuilder("TpcBuilder",active)
 {
   _innerCalc = new StiDefaultHitErrorCalculator();
-  _innerCalc->set(.04, 1.2e-04, 0.004,
-		  .066, 4.4e-4, 2.8-02);
+  _innerCalc->set(.066, 1.2e-04, 0.0004,
+		  .03, 2.2e-4, 2.8-02);
   //_innerCalc->set(3.12735e-03,1.51055e-04,2.43806e-02,
   //3.28243e-03,5.23272e-05,5.75341e-02);
   //		  3.68243e-03,5.23272e-05,5.75341e-02); //original C.P.5/1/03
   _outerCalc = new StiDefaultHitErrorCalculator();
-  _outerCalc->set(.02, 4.e-3, 0.04,
-		  .02, 3.2e-3, 9.e-2);
+  _outerCalc->set(.015, 8e-04, 0.0004,
+		  .012, 1.6e-4, 1.3e-01);
   //_outerCalc->set(8.158e-03,5.69582e-05,4.48440e-02,
   //8.02775e-03,3.55219e-05,6.45610e-02);
   //2.02775e-04,3.55219e-05,6.45610e-02);  //original C.P. 5/1/03
-  StiTrackingParameters * trackingPars = getTrackingParameters();
-  trackingPars->setMaxChi2ForSelection(8.);
-  trackingPars->setMinSearchWindow(1.5);
-  trackingPars->setMaxSearchWindow(7.);
-  trackingPars->setSearchWindowScaling(10.);
 }
 
 StiTpcDetectorBuilder::~StiTpcDetectorBuilder()
