b6a59168898581fc4616f5aa90179802c496c8a6
Helen L. Caines <helen.caines@yale.edu>
2003-07-30 15:27:00 +0000

    Set options so you delete TPC and SVT hit if Zert >30. If ZVert<30cm save all good svt hits and TPC hits on tracks

--- StRoot/StHitFilterMaker/StHitFilterMaker.h
+++ StRoot/StHitFilterMaker/StHitFilterMaker.h
@@ -1,4 +1,4 @@
-// $Id: StHitFilterMaker.h,v 1.1 2003/02/07 02:16:15 jeromel Exp $
+// $Id: StHitFilterMaker.h,v 1.2 2003/07/30 15:27:00 caines Exp $
 
 #ifndef STAR_StHitFilterMaker
 #define STAR_StHitFilterMaker
@@ -29,6 +29,7 @@ using std::vector;
 #endif
 class StTrackNode;
 class StTrack;
+class StHit;
 
 class StEvent;
 
@@ -38,33 +39,37 @@ class StHitFilterMaker : public StMaker {
   Double_t mPtLowerCut;
   Double_t mPtUpperCut;
   Double_t mAbsEtaCut;
-    
+  Double_t mAbsZVertCut;
     
  protected:
   // Protected method if any
 
  public: 
-  StHitFilterMaker(const char *name="StHitFilterMaker",Double_t ptLowerCut=1.5,Double_t ptUpperCut=-1, Double_t absEtaCut=1.);
+  StHitFilterMaker(const char *name="StHitFilterMaker",Double_t ptLowerCut=1.5,Double_t ptUpperCut=-1, Double_t absEtaCut=1., Double_t absZVertCut=999.);
   virtual       ~StHitFilterMaker();
   virtual Int_t Init();
   virtual Int_t  Make();
+  bool accept(StEvent *);
   bool accept(StTrack *);
+  bool accept(StHit *);
     
   bool removeTpcHitsNotOnTracks(StEvent *, vector<StTrackNode*>&); //!
+  bool removeBadSvtHits(StEvent *); //!
   void setPtLowerCut(Double_t pt) {mPtLowerCut = pt;}
   void setPtUpperCut(Double_t pt) {mPtUpperCut = pt;}
   void setAbsEtaCut(Double_t eta) {mAbsEtaCut = eta;}
+  void setAbsZVertCut(Double_t zvert) {mAbsZVertCut = zvert;}
   Double_t ptLowerCut() { return mPtLowerCut;}
   Double_t ptUpperCut() { return mPtUpperCut;}
   Double_t absEtaCut() { return mAbsEtaCut;}
-
+  Double_t absZVertCut() { return mAbsZVertCut;}
     
   // virtual Int_t InitRun  (int runumber){return 0;}; // Overload empty StMaker::InitRun 
   // virtual Int_t FinishRun(int runumber){return 0;}; // Overload empty StMaker::FinishRun 
 
   /// Displayed on session exit, leave it as-is please ...
   virtual const char *GetCVS() const {
-    static const char cvs[]="Tag $Name:  $ $Id: StHitFilterMaker.h,v 1.1 2003/02/07 02:16:15 jeromel Exp $ built "__DATE__" "__TIME__ ; 
+    static const char cvs[]="Tag $Name:  $ $Id: StHitFilterMaker.h,v 1.2 2003/07/30 15:27:00 caines Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
     
@@ -76,6 +81,9 @@ class StHitFilterMaker : public StMaker {
 
 
 // $Log: StHitFilterMaker.h,v $
+// Revision 1.2  2003/07/30 15:27:00  caines
+// Set options so you delete TPC and SVT hit if Zert >30. If ZVert<30cm save all good svt hits and TPC hits on tracks
+//
 // Revision 1.1  2003/02/07 02:16:15  jeromel
 // First version of a generlized HitFilter/removal maker. Expeditious review
 // done.
