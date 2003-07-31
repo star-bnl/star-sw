1259505e33b6920bc43801580429625afc4f3d82
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2003-07-31 15:31:08 +0000

    Missing file added. Part of the association tools.

---
+++ StRoot/Sti/StiTrackToObjMap.h
@@ -0,0 +1,24 @@
+#ifndef StiTrackToObjMap_H_INCLUDED
+#define StiTrackToObjMap_H_INCLUDED
+#include "Sti/StiTrackContainer.h"
+#include "Sti/StiTrack.h"
+#include "Sti/StiTrackToIntMap.h"
+#include "Sti/StiHitToHitMap.h"
+#include "Sti/StiHitToTrackMap.h"
+#include <map>
+typedef map<StiTrack*, StiTrackToIntMap*> TrackToObjMap;
+
+class StiTrackToObjMap: public TrackToObjMap
+{
+ public:
+  StiTrackToObjMap();
+  ~StiTrackToObjMap();
+  
+  void build(StiTrackContainer*,
+	     StiHitToHitMap*, 
+	     StiHitToTrackMap*);
+  void clear();
+  void analyze(Filter<StiTrack> * filter);
+};
+
+#endif
