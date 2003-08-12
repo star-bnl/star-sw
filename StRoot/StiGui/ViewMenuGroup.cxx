b412834566b96d5c857b1279ecfb66a3a77db109
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2003-08-12 16:04:05 +0000

    Fixes bug in dispatch function which enabled incorrect calls to setView

--- StRoot/StiGui/ViewMenuGroup.cxx
+++ StRoot/StiGui/ViewMenuGroup.cxx
@@ -16,7 +16,7 @@ ViewMenuGroup::~ViewMenuGroup()
 
 void ViewMenuGroup::create(TGMenuBar *menuBar, TGLayoutHints *itemLayout)
 { 
-  cout << "ViewMenuGroup::create() -I- Started"<<endl;
+  //cout << "ViewMenuGroup::create() -I- Started"<<endl;
   TGPopupMenu * menu = new TGPopupMenu(getClient()->GetRoot());
   menu->AddEntry("SetDefault",  _offset+_cmdSetDefaultView);
   cout << "ViewMenuGroup::create() -I- Get Views"<<endl;
@@ -41,7 +41,7 @@ void ViewMenuGroup::create(TGMenuBar *menuBar, TGLayoutHints *itemLayout)
        groupIter!=groups->end();
        ++groupIter)
     {
-      cout << "ViewMenuGroup::create() -I- Setup for group:"<<(*groupIter)->getName()<<endl;
+      //cout << "ViewMenuGroup::create() -I- Setup for group:"<<(*groupIter)->getName()<<endl;
       int view = 0;
       StiDetectorViews *groupViews = (*groupIter)->getDetectorViews();
       if (!groupViews)
@@ -56,7 +56,7 @@ void ViewMenuGroup::create(TGMenuBar *menuBar, TGLayoutHints *itemLayout)
 	   ++viewIter)
 	{
 	  StiDetectorView * aView = *viewIter;
-	  cout <<"ViewMenuGroup::create() -I- DetectorView:"<<aView->getName()<<endl;
+	  //cout <<"ViewMenuGroup::create() -I- DetectorView:"<<aView->getName()<<endl;
 	  subMenu->AddEntry(aView->getName().c_str(),_offset+10+10*group+view);
 	  ++view;
 	}
@@ -67,23 +67,29 @@ void ViewMenuGroup::create(TGMenuBar *menuBar, TGLayoutHints *itemLayout)
   menuBar->AddPopup("&View", menu, itemLayout);
 
   setDefaultView();
-  cout << "ViewMenuGroup::create() -I- Done"<<endl;
+  //cout << "ViewMenuGroup::create() -I- Done"<<endl;
 }
 
 void ViewMenuGroup::dispatch(int option)
 {
-  cout << "ViewMenuGroup::dispatch(int option) -I- Started with option=="<<option<<endl;
-  switch (option-_offset)
+  //cout << "ViewMenuGroup::dispatch(int option) -I- Started with option=="<<option<<endl;
+  option -= _offset;
+  if (option<0 || option>999) return;
+  switch (option)
     {
     case _cmdSetDefaultView : 
-      cout << "setDefault"<<endl;
-setDefaultView(); break;
+      //cout << "setDefault"<<endl;
+      setDefaultView(); break;
+      getDisplay()->draw();
     default: 
       int viewOption = option-_offset-10;
-      cout << "setSelectedView:"<<viewOption<<endl;
-      if (viewOption<1000 && viewOption>=0) setSelectedView(viewOption);
+      //cout << "setSelectedView:"<<viewOption<<endl;
+      if (viewOption<1000 && viewOption>=0) 
+	{
+	  setSelectedView(viewOption);
+	  getDisplay()->draw();
+	}
     }  
-  getDisplay()->draw();
 }
 
 void ViewMenuGroup::setDefaultView()
@@ -101,13 +107,36 @@ void ViewMenuGroup::setDefaultView()
 
 void ViewMenuGroup::setSelectedView(int option)
 {   
-  cout << "setSelectedView():"<<option<<endl;
+  //cout << "setSelectedView():"<<option<<endl;
   StiDetectorViews * views = getDisplay()->getDetectorViews();
   StiDetectorGroups<StEvent,StMcEvent> * groups = getToolkit()->getDetectorGroups();
   int group = option/10;
   int view  = option - 10*group;
+  if (!groups)
+    {
+      cout << "ViewMenuGroup::setSelectedView() -E- groups==0"<<endl;
+      return;
+    }
+  if (group<0 || group>=(*groups).size())
+    {
+      cout << "ViewMenuGroup::setSelectedView() -E- group:"<<group<<endl;
+      return;
+    }
+  if (!(*groups)[group])
+    {
+      cout << "ViewMenuGroup::setSelectedView() -E- (*groups)[group]==0"<<endl;
+      return;
+    }
   StiDetectorViews * groupViews = (*groups)[group]->getDetectorViews();
-  (*views)[group] = (*groupViews)[view];
-  (*groupViews)[view]->activate(); 
+  if (groupViews)
+    {
+      (*views)[group] = (*groupViews)[view];
+      if ( (*views)[group] )
+	(*groupViews)[view]->activate(); 
+      else
+	cout << "ViewMenuGroup::setSelectedView() -E- (*groupViews)[view]==0"<<endl;
+    }
+  else
+    cout << "ViewMenuGroup::setSelectedView() -E- groupViews==0"<<endl;
 }
  
