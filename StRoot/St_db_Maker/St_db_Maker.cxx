dff96305fdc6d47f7b88db4a9abf51f2c1e19e56
jeromel <>
2003-08-21 16:11:08 +0000

    Labelling consistency correction

--- StRoot/St_db_Maker/St_db_Maker.cxx
+++ StRoot/St_db_Maker/St_db_Maker.cxx
@@ -10,8 +10,11 @@
 
 // Most of the history moved at the bottom
 //
-// $Id: St_db_Maker.cxx,v 1.65 2003/07/16 19:58:34 perev Exp $
+// $Id: St_db_Maker.cxx,v 1.66 2003/08/21 16:11:08 jeromel Exp $
 // $Log: St_db_Maker.cxx,v $
+// Revision 1.66  2003/08/21 16:11:08  jeromel
+// Labelling consistency correction
+//
 // Revision 1.65  2003/07/16 19:58:34  perev
 // Cleanup of StTriggerData2003 at all
 //
@@ -61,16 +64,22 @@ static Int_t AliasTime(const char *alias);
 static const char *aliases[]={
 "sd97",   "sd98",   "year_1a","year_1b","year_1c",
 "es99",   "er99",   "dc99"   ,"year_1d","year_1e",
-"year_1h","year_2a", "year_2b", "year2001", "year2003", "y2003x",0};   
+"year_1h","year_2a", "year_2b", "year2001", "year2003", "year2003x",
+0};   
 
 static const int   dates[]=  {
 19970101, 19980101, 19990101, 19990501, 19991001,
 19990615, 19990616, 19991206, 19991101, 19991201,
-20000614, 20010610, 20010501, 20010615, 20021115, 20021115, 0};
+20000614, 20010610, 20010501, 20010615, 20021115, 20021115, 
+0
+};
+
 static const int   times[]=  {
        0,        0,        0,        0,        0,
        0,   120000,    80000,        0,        0,
-  175430,        0,        0,        0,        0, 0};
+  175430,        0,        0,        0,        0, 
+       0
+};
 
 enum eDBMAKER {kUNIXOBJ = 0x2000};
 
