74431aef7f5959693c617f29ba9423e73df7bf3a
caines <>
2003-08-22 17:47:14 +0000

    Get sign AND magnitude of mag field correctly for Xi and V0 finder

--- StRoot/StSecondaryVertexMaker/StV0FinderMaker.h
+++ StRoot/StSecondaryVertexMaker/StV0FinderMaker.h
@@ -131,6 +131,7 @@ class StV0FinderMaker : public StMaker {
 
   virtual void   GetPars();
   virtual Int_t  Init();
+  virtual Int_t  InitRun(int RunNumber);
   virtual Int_t  Make();
   virtual void   Clear(Option_t *option="") { prepared = kFALSE; }
   virtual void   UseExistingV0s(Bool_t opt=kTRUE) { useExistingV0s = opt; }
@@ -153,7 +154,7 @@ class StV0FinderMaker : public StMaker {
   virtual void   Trim();
 
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StV0FinderMaker.h,v 1.3 2003/05/14 19:14:41 faivre Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StV0FinderMaker.h,v 1.4 2003/08/22 17:47:14 caines Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
  protected:
   virtual Int_t Prepare();
@@ -199,8 +200,11 @@ class StV0FinderMaker : public StMaker {
 #endif
 
 //_____________________________________________________________________________
-// $Id: StV0FinderMaker.h,v 1.3 2003/05/14 19:14:41 faivre Exp $
+// $Id: StV0FinderMaker.h,v 1.4 2003/08/22 17:47:14 caines Exp $
 // $Log: StV0FinderMaker.h,v $
+// Revision 1.4  2003/08/22 17:47:14  caines
+// Get sign AND magnitude of mag field correctly for Xi and V0 finder
+//
 // Revision 1.3  2003/05/14 19:14:41  faivre
 // Setting new enum's. Fancy choices Fortran/C++ V0's and Xi's. Xi rotating and like-sign. SVT tracks.
 //
