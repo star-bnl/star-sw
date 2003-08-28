715261f993d9162a6d0469b8043bb5731b437e6a
Thomas Henry <thenry@physics.tamu.edu>
2003-08-28 16:53:06 +0000

    Created for the purpose of finding and avoiding hot towers and bad runs.

---
+++ StRoot/StJetFinder/TowerSanityMaker.cxx
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *
+ * $Id: TowerSanityMaker.cxx,v 1.1 2003/08/28 16:53:06 thenry Exp $
+ * 
+ * Author: Thomas Henry August 2003
+ ***************************************************************************
+ *
+ * Description:  Maker which creates a database of average tower values per
+ * run, accessible by runnumber or eventid and towerid
+ *
+ ***************************************************************************
+ *
+ * Revision 1.0  2003/02/20 thenry
+ *
+ **************************************************************************/
+#include <string.h>
+#include <iostream.h>
+#include <math.h>
+#include <sys/times.h>
+
+#include "TowerSanityMaker.h"
+
+ClassImp(TowerSanityMaker)
+  
+
+
+
+
+
+
+
+
