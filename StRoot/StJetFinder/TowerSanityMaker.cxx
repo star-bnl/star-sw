69b301f2c5938c1abc654b73dc88fea84a5c4188
thenry <>
2003-09-04 18:54:24 +0000

    This is the real first version of TowerSanityMaker

--- StRoot/StJetFinder/TowerSanityMaker.cxx
+++ StRoot/StJetFinder/TowerSanityMaker.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: TowerSanityMaker.cxx,v 1.1 2003/08/28 16:53:06 thenry Exp $
+ * $Id: TowerSanityMaker.cxx,v 1.2 2003/09/04 18:54:24 thenry Exp $
  * 
  * Author: Thomas Henry August 2003
  ***************************************************************************
@@ -18,14 +18,117 @@
 #include <math.h>
 #include <sys/times.h>
 
+#ifdef __ROOT__
+#include "StChain.h"
+#include "StEventTypes.h"
+#include "StMessMgr.h"
+#include "StIOMaker/StIOMaker.h"
+#include "StEvent.h"
+#include "StMuDSTMaker/COMMON/StMuDst.h"
+#include "StMuDSTMaker/COMMON/StMuEvent.h"
+#include "StMuDSTMaker/COMMON/StMuDstMaker.h"
+#include "StSpinMaker/StMuTrackFourVec.h"
+
+#include "StEmcClusterCollection.h"
+#include "StMuDSTMaker/COMMON/StMuEmcCollection.h"
+#include "StMuDSTMaker/COMMON/StMuDst.h"
+#include "StEmcPoint.h"
+#include "StMuDSTMaker/COMMON/StMuEmcUtil.h"
+#include "StEmcTpcFourPMaker.h"
+#include "SystemOfUnits.h"
+#include "StEmcUtil/geometry/StEmcGeom.h"
+#include "StEmcUtil/others/emcDetectorName.h"
+#include "StEmcADCtoEMaker/StBemcData.h"
+
 #include "TowerSanityMaker.h"
 
 ClassImp(TowerSanityMaker)
   
+TowerSanityMaker::TowerSanityMaker(const char* name,
+				   StEmcADCtoEMaker* adcToEMaker,
+				   StMuDstMaker* uDstMkr,
+				   const char* dataBaseName, 
+				   dataBaseUse dbu) : 
+  SafetyArray(name), adcToE(adcToEMaker), muDstMkr(uDstMkr),
+  dbName(dataBaseName), dbUse(dbu), ofile(NULL) 
+{
+}
 
+Int_t TowerSanityMaker::Make()
+{
+  StMuDst* uDst = muDstMkr->muDst();
+  StMuEvent* uEvent = uDst->event();
+  unsigned int runNumber = uEvent->runNumber();
+  int pointIndex = 0;
+  int& hitId = pointIndex;
+  StBemcData* data = adcToE->getBemcData();
+  int numHits = data->NTowerHits;
+  cout << "Number Hits: " << numHits;
+  int maxHits = 4800;
 
+  tripower ttrip;
+  for(hitId = 1; hitId <= maxHits; hitId++)
+    {
+      if(data->TowerStatus[hitId-1] != 1) continue;
+      double energy = data->TowerEnergy[hitId-1];
+      ttrip.set(hitId-1, energy);
+      TowerSanity.add(ttrip, static_cast<long>(runNumber));
+    }
+  
+  return kStOK;
+}
 
+Int_t TowerSanityMaker::Init()
+{
+  if((dbUse == toRead) || (dbUse == toMerge))
+    {
+      ifstream is(dbName);
+      TowerSanity.load(is);
+    }
+  return kStOK;
+}
 
+Int_t TowerSanityMaker::Finish()
+{
+  if((dbUse == toWrite) || (dbUse == toMerge))
+    {
+      ofstream os(dbName, ofstream::out | ofstream::trunc);
+      TowerSanity.save(os);
+    }
+  /*
+  for(edbType::iterator it = TowerSanity.EMCDB.begin(); 
+      it != TowerSanity.EMCDB.end(); ++it)
+    {
+      long runNumber = (*it).first;
+      cout << "runAverageEnergyPerTower: " << 
+	runAverageEnergyPerTower(runNumber) << endl;
+      cout << "runStandardDeviation: " << 
+	runStandardDeviation(runNumber) << endl;
+      tdbType &TDB = (*it).second.TDB;
+      for(tdbType::iterator it = TDB.begin(); it != TDB.end(); ++it)
+	{
+	  tripower &tr = (*it).second;
+	  cout << "Tower Id: " << tr.towerId << endl;
+	  cout << "Number Events: " << tr.events << endl;
+	  cout << "Tower Max: " << tr.max << endl;
+	  cout << "Tower Average: " << tr.average() << endl;
+	  cout << "Tower StdDev: " << tr.stddev() << endl;
+	}
+	}*/
+  return kStOK;
+}
+
+bool TowerSanityMaker::isGood(unsigned int runNumber, long index)
+{
+  if(runAverageEnergyPerTower(runNumber) > runThreshold)
+    return false;
+  if(maxTowerEnergy(runNumber, index) > maxTowerThreshold)
+    return false;
+  if(averageTowerEnergy(runNumber, index) > avgTowerThreshold)
+    return false;
+  return true;
+}
+#endif //__ROOT__
 
 
 
--- StRoot/StJetFinder/TowerSanityMaker.h
+++ StRoot/StJetFinder/TowerSanityMaker.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: TowerSanityMaker.h,v 1.1 2003/08/28 16:53:06 thenry Exp $
+ * $Id: TowerSanityMaker.h,v 1.2 2003/09/04 18:54:24 thenry Exp $
  * 
  * Author: Thomas Henry August 2003
  ***************************************************************************
@@ -22,35 +22,181 @@
 #include <iostream>
 #include <vector>
 #include <typeinfo>
-using namespace std;
+
+class CumuTowerException
+{
+ public:
+  string Msg;
+ public:
+  CumuTowerException(string str) : Msg(str) {};
+};
+
+class towerIdMismatchException : public CumuTowerException
+{
+ public:
+  towerIdMismatchException() : CumuTowerException("Tower Id mismatch!"){};
+};
+
+class eofTowerException : public CumuTowerException
+{
+ public:
+  eofTowerException() : CumuTowerException("EOF Exception!"){};
+};
 
 class tripower
 {
  public:
+  long towerId;
   long events;
   double sum;
   double sumsqr;
-  tripower() : events(0), sum(0), sumsqr(0) {};
-  tripower(tripower& tr) : 
-    events(tr.events), sum(tr.sum), sumsqr(tr.sumsqr) {};
+  double max;
+  tripower() : towerId(0), events(0), sum(0), sumsqr(0), max(0) {};
+  tripower(const tripower& tr) : 
+    towerId(tr.towerId), events(tr.events), 
+    sum(tr.sum), sumsqr(tr.sumsqr), max(tr.max) {};
+  tripower(long ptowerId, long pevents, double psum, double psumsqr) :
+    towerId(ptowerId), events(pevents), 
+    sum(psum), sumsqr(psumsqr), max(0) {};
+  tripower(long ptowerId, double energy) :
+    towerId(ptowerId), events(1), 
+    sum(energy) { sumsqr = energy*energy; max = energy; };
 
-  void Add(tripower& tr) { 
-    events += tr.events; sum += tr.sum; sumsqr += tr.sumsqr; };
+  void add(const tripower& tr) 
+    {
+      if(towerId == 0)
+	towerId = tr.towerId;
+      if(towerId != tr.towerId) 
+	throw towerIdMismatchException();
+      addAny(tr);
+    };
+
+  void addAny(const tripower& tr) 
+    {
+      events += tr.events; sum += tr.sum; sumsqr += tr.sumsqr;
+      if(tr.max > max) max = tr.max;
+    };
+
+  void set(long ptowerId, double penergy) 
+    {
+      towerId = ptowerId;
+      events = 1;
+      sum = penergy;
+      sumsqr = sum*sum;
+      max = penergy;
+    };
 
   double average(void) { if(events) return sum/events; else return 0; };
   double stddev(void) { 
-    if(events) return sumsqr/events - average();
+    if(events) return sqrt(sumsqr/events - average()*average());
     else return 0;
   };
 };
 
-class TowerDB : map<long, tripower, less<long> >
+typedef map<long, tripower, less<long> > tdbType;
+
+class TowerDB
 {
+ public:
+  tdbType TDB;
+  tripower totals;
+ public:
+  TowerDB() {};
+  TowerDB(const TowerDB& tower) : TDB(tower.TDB) { };
+  ~TowerDB() { clear(); };
+  void clear(void) { 
+    TDB.clear();
+  };
+  void add(const tripower& tr) {
+    TDB[tr.towerId].add(tr);
+    totals.addAny(tr);
+  };
+  void add(const TowerDB& db)
+    {
+      for(tdbType::const_iterator it = db.TDB.begin(); 
+	  it != db.TDB.end(); ++it)
+	add((*it).second);
+      totals.addAny(db.totals);
+    };
+  istream &load(istream &is){
+    if(is.eof()) throw eofTowerException();
+    is.read(reinterpret_cast<char*>(&totals), sizeof(totals));
+    unsigned long lsize = 0;
+    is.read(reinterpret_cast<char*>(&lsize), sizeof(lsize));
+    tripower tempload;
+    for(unsigned long i = 0; i < lsize; i++)
+      {
+	is.read(reinterpret_cast<char*>(&tempload), sizeof(tempload));
+	add(tempload);
+      }
+    return is;
+  };
+  ostream &save(ostream &os){
+    os.write(reinterpret_cast<char*>(&totals), sizeof(totals));
+    unsigned long lsize = TDB.size();
+    os.write(reinterpret_cast<char*>(&lsize), sizeof(lsize));
+    for(tdbType::iterator it = TDB.begin(); it != TDB.end(); ++it)
+      {
+	tripower tr = (*it).second;
+	os.write(reinterpret_cast<char*>(&tr), sizeof(tr));
+      }
+    return os;
+  };
 };
 
-class MultiRunTowerDB : map<long, TowerDB, less<long> >
+typedef map<long, TowerDB, less<long> > edbType;
+
+class EMCRunDB
 {
+ public:
+  edbType EMCDB;
+ public:
+  EMCRunDB() {};
+  EMCRunDB(const EMCRunDB& pEMCDB) : EMCDB(pEMCDB.EMCDB) {};
+  ~EMCRunDB() { clear(); };
+  void clear(void) {
+    EMCDB.clear();
+  };
+  void add(const tripower& tr, long runNumber) {
+    EMCDB[runNumber].add(tr);
+  };
+  void add(const TowerDB &db, long runNumber) {
+    EMCDB[runNumber].add(db);
+  };
+  void add(const EMCRunDB &db)
+    {
+      for(edbType::const_iterator it = db.EMCDB.begin(); 
+	  it != db.EMCDB.end(); ++it)
+	EMCDB[(*it).first].add((*it).second);
+    };
+  istream &load(istream &is){
+    if(is.eof()) throw eofTowerException();
+    unsigned long lsize = 0;
+    is.read(reinterpret_cast<char*>(&lsize), sizeof(lsize));
+    TowerDB tempload;
+    for(unsigned long i = 0; i < lsize; i++)
+      {
+	long runNumber = 0;
+	is.read(reinterpret_cast<char*>(&runNumber), sizeof(runNumber));
+	tempload.clear();
+	tempload.load(is);
+	add(tempload, runNumber);
+      }
+    return is;
+  };
+  ostream &save(ostream &os){
+    unsigned long lsize = EMCDB.size();
+    os.write(reinterpret_cast<char*>(&lsize), sizeof(lsize));
+    for(edbType::iterator it = EMCDB.begin(); it != EMCDB.end(); ++it)
+      {
+	long runNumber = (*it).first;
+	os.write(reinterpret_cast<char*>(&runNumber), sizeof(runNumber));
+	((*it).second).save(os);
+      }
+    return os;
+  };
 };
+
 
 #endif
 
@@ -64,22 +210,54 @@ class MultiRunTowerDB : map<long, TowerDB, less<long> >
 
 class TowerSanityMaker : public SafetyArray {
  public:
-  enum dataBaseUse {toRead=0, toWrite=1};
+  enum dataBaseUse {toRead=0, toWrite=1, toMerge=2};
    
  private:
+  StEmcADCtoEMaker* adcToE;
+  StMuDstMaker* muDstMkr;
   const char* dbName;
   dataBaseUse dbUse;
+  ofstream* ofile;
+  EMCRunDB TowerSanity;
+
+  double runThreshold;
+  double maxTowerThreshold;
+  double avgTowerThreshold;
 
  public:
-  TowerSanityMaker(const char* name, const char* dataBaseName, 
-		   dataBaseUse dbu) : 
-    SafetyArray(name), dbName(dataBaseName), dbUse(dbu) {};
+  TowerSanityMaker(const char* name,
+		   StEmcADCtoEMaker* adcToEMaker,
+		   StMuDstMaker* uDstMkr,
+		   const char* dataBaseName, 
+		   dataBaseUse dbu);
 
   virtual Int_t Make();
   virtual Int_t Init();
   virtual Int_t Finish();
 
-  virtual bool isGood(unsigned int runNumber, long index) { return true; };
+  virtual bool isGood(unsigned int runNumber, long index);
+
+  double runAverageEnergyPerTower(long runNumber) { 
+    return TowerSanity.EMCDB[runNumber].totals.average(); 
+  };
+  double runStandardDeviation(long runNumber) { 
+    return TowerSanity.EMCDB[runNumber].totals.stddev(); 
+  };
+  double averageTowerEnergy(long runNumber, long index){
+    return TowerSanity.EMCDB[runNumber].TDB[index].average();
+  };
+  double maxTowerEnergy(long runNumber, long index){
+    return TowerSanity.EMCDB[runNumber].TDB[index].max;
+  };
+  double towerStdDev(long runNumber, long index){
+    return TowerSanity.EMCDB[runNumber].TDB[index].stddev();
+  };
+
+  void setRunThreshold(double runThresh) { runThreshold = runThresh; };
+  void setMaxTowerThreshold(double maxTowerThresh) { 
+    maxTowerThreshold = maxTowerThresh; };
+  void setAvgTowerThreshold(double avgTowerThresh) {
+    avgTowerThreshold = avgTowerThresh; };
     
   ClassDef(TowerSanityMaker,1)
 };
