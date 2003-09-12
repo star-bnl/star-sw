c1d25ca9e7ffece0933d80dacf5afb474f5b12ff
jeromel <>
2003-09-12 21:30:59 +0000

    Use loadSharedLibraries.C instead

--- StRoot/StMuDSTMaker/COMMON/macros/exampleStEvent.C
+++ StRoot/StMuDSTMaker/COMMON/macros/exampleStEvent.C
@@ -8,25 +8,8 @@ StEvent* ev;
 
 
 void exampleStEvent() {
-   if (gClassTable->GetID("TTable") < 0) gSystem->Load("libTable");
-  gSystem->Load("St_base");
-  gSystem->Load("StChain");
-  gSystem->Load("St_Tables");
-  gSystem->Load("StMagF");
-  gSystem->Load("StUtilities");  // new addition 22jul99
-  gSystem->Load("StTreeMaker");
-  gSystem->Load("StIOMaker");
-  gSystem->Load("StarClassLibrary");
-  gSystem->Load("StTpcDb");
-  gSystem->Load("StDbUtilities");
-  gSystem->Load("StEvent");
-  gSystem->Load("StEventUtilities"); 
-  gSystem->Load("StMcEvent"); 
-  gSystem->Load("StMcEventMaker"); 
-  gSystem->Load("StAssociationMaker");
-  gSystem->Load("StMcAnalysisMaker");
-  gSystem->Load("StStrangeMuDstMaker");
-  gSystem->Load("StMuDSTMaker");
+  gROOT->LoadMacro("StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  loadSharedLibraries();
 
   cout << " loading done " << endl;
   
