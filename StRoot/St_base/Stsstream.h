7bab7f111d095efd94bc70ef389722440564cd31
perev <>
2003-09-22 01:32:47 +0000

    ostrstream bug fix. It was according to VC++ but linux is different

--- StRoot/St_base/Stsstream.h
+++ StRoot/St_base/Stsstream.h
@@ -8,16 +8,25 @@
  */
 #ifndef STSSTREAM_H
 #define STSSTREAM_H
+#include <ctype.h>
 #include "Rstrstream.h"
 #ifdef R__SSTREAM
 using std::streamsize;
 class ostrstream : public std::ostringstream {
+std::string myString;
 public:
-const char *str()         {return std::ostringstream::str().c_str();}
+const char *str()         
+{
+  std::string tmp = std::ostringstream::str();
+  if (myString != tmp) myString=tmp;
+  return myString.c_str();
+}	
 int        pcount()       {return int(tellp()) ;}
 void       seekp(int pos) {if (int(tellp())>=0) std::ostringstream::seekp(pos);}
-void freeze(bool) const{;};
-};
+void freeze(bool) const{;}
+};	
+
+
 class istrstream : public std::istringstream {
 public:
 istrstream(): std::istringstream(){}
