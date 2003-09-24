1e9b4e58295ec630975cfdb1da4975705c12cce6
Yuri Fisyak <fisyak@bnl.gov>
2003-09-24 23:28:27 +0000

    Add parameterization for P10; fix ToF correction

--- StRoot/StBichsel/Bichsel.h
+++ StRoot/StBichsel/Bichsel.h
@@ -9,12 +9,14 @@
 #ifndef Bichsel_h
 #define Bichsel_h
 //#define P03ia
+#include "TString.h"
 #include "dEdxParameterization.h"
 class Bichsel {
  private: 
-  static dEdxParameterization *m_dEdxParameterization; //!
+  TString               m_Tag;
+  dEdxParameterization *m_dEdxParameterization; //!
  public :
-  Bichsel();
+  Bichsel(const Char_t *tag="bich");
  virtual ~Bichsel() {}
   Double_t    GetMostProbableZ(Double_t log10bg, Double_t log2dx = 1., Int_t kase=0) {
     return m_dEdxParameterization->GetMostProbableZ(log10bg,log2dx,kase);
@@ -38,6 +40,7 @@ class Bichsel {
   Double_t    GetProbability(Double_t log10bg, Double_t log2dx, Double_t z, Int_t kase=0) {
     return m_dEdxParameterization->GetProbability(log10bg,log2dx,z,kase);}
   Double_t    TofCorrection(Double_t log10bg);
+  virtual void Print();
   ClassDef(Bichsel,0)
 };
 #endif
