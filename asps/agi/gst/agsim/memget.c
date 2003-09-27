a4fc6a7feac4937bd2eae3c5754ba4aa9bb805a5
Star Software Library <starlib>
2003-09-27 18:45:14 +0000

    C code (not C++) adjustment ...

--- asps/agi/gst/agsim/memget.c
+++ asps/agi/gst/agsim/memget.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <errno.h>
 /*
- * $Id: memget.c,v 1.6 2003/09/25 21:28:19 potekhin Exp $
+ * $Id: memget.c,v 1.7 2003/09/27 18:45:14 starlib Exp $
  *
  * $Log: memget.c,v $
+ * Revision 1.7  2003/09/27 18:45:14  starlib
+ * C code (not C++) adjustment ...
+ *
  * Revision 1.6  2003/09/25 21:28:19  potekhin
  * 1) Made the function memgetf much less cryptic in terms
  * of variable names and comments
@@ -51,7 +54,10 @@ unsigned long type_of_call memget_  (unsigned int *n)
 
 unsigned long type_of_call memgetf_ (unsigned int *n)
 {
-  /* see comment above about the Fortran vs C memory count -- 4 byte words vs bytes */
+  /* see comment above about the Fortran vs C memory count -- 
+     4 byte words vs bytes 
+  */
+  unsigned long mem;
   unsigned int n_bytes= *n<<2;
   void* mem_pointer = malloc(n_bytes);
 
@@ -60,7 +66,7 @@ unsigned long type_of_call memgetf_ (unsigned int *n)
     /* to signal the caller that we failed here; the caller MUST check for zero */
   }
 
-  unsigned long mem = (unsigned long) mem_pointer;
+  mem = (unsigned long) mem_pointer;
   mem=mem>>2;
   return mem;
 }
