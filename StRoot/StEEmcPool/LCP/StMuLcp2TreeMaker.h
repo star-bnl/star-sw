ddfbd112401bd0479354d30c78e04db67136c0f4
Jan Balewski <balewski@lbl.gov>
2003-10-20 17:04:39 +0000

    LCP analysis code

--- StRoot/StEEmcPool/LCP/StMuLcp2TreeMaker.h
+++ StRoot/StEEmcPool/LCP/StMuLcp2TreeMaker.h
@@ -1,4 +1,4 @@
-// $Id: StMuLcp2TreeMaker.h,v 1.1 2003/09/16 19:18:36 balewski Exp $
+// $Id: StMuLcp2TreeMaker.h,v 1.2 2003/10/20 17:04:39 balewski Exp $
 
 #ifndef STAR_StMuLcp2TreeMaker
 #define STAR_StMuLcp2TreeMaker
@@ -24,13 +24,15 @@ class TH2F;
 class TClonesArray;
 class TTree;
 class StMuTrack;
-class ExampleUsage;
+//class ExampleUsage;
+class StMuEvent;
+class CtbMatching;
 
 class StMuLcp2TreeMaker : public StMaker {
  private:
   StMuDstMaker* mMuDstMaker;
   TClonesArray* primTrA;
-  // ExampleUsage *rejector;
+  //ExampleUsage *rejector;
   TFile *hfile;
   TH1F *h[16]; 
   TH1F *hc[8]; 
@@ -45,7 +47,7 @@ class StMuLcp2TreeMaker : public StMaker {
   Int_t eve_id; // event ID
   Int_t eve_sb; // spin bits
   Int_t eve_nPrim; // # of primary tracks
-  Int_t eve_cosm; //Mike's Cosmics Rejector return value
+  Int_t eve_cosm; //Mike's Cosmics Rejector & other tests value
   Float_t lcp_eta,lcp_phi,lcp_pt; // momentum
   Int_t lcp_q,lcp_nFit;// charge, # of hits on track
  
@@ -58,7 +60,10 @@ class StMuLcp2TreeMaker : public StMaker {
   float C_maxDCAxy; // (cm)
   float C_maxEta; 
   float C_minPt; // (GeV/c) 
+  float C_maxZvertex; // (cm)
 
+  CtbMatching *ctb;
+  
  public: 
   StMuLcp2TreeMaker(const char *self="jasLcp2Tree", const char* muDstMakerName="muDstMaker");
   virtual       ~StMuLcp2TreeMaker();
@@ -71,12 +76,13 @@ class StMuLcp2TreeMaker : public StMaker {
   void SetMaxDCAxy(float x){C_maxDCAxy=x;}
   void SetMaxEta(float x){C_maxEta=x;}
   void SetMinPt(float x){C_minPt=x;}
+  void SetMaxZvert(float x){C_maxZvertex=x;}
   void SetMinFitPfrac(float x){C_minFitPfrac=x;}
 
 
   /// Displayed on session exit, leave it as-is please ...
   virtual const char *GetCVS() const {
-    static const char cvs[]="Tag $Name:  $ $Id: StMuLcp2TreeMaker.h,v 1.1 2003/09/16 19:18:36 balewski Exp $ built "__DATE__" "__TIME__ ; 
+    static const char cvs[]="Tag $Name:  $ $Id: StMuLcp2TreeMaker.h,v 1.2 2003/10/20 17:04:39 balewski Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
 
@@ -87,6 +93,9 @@ class StMuLcp2TreeMaker : public StMaker {
 
 
 // $Log: StMuLcp2TreeMaker.h,v $
+// Revision 1.2  2003/10/20 17:04:39  balewski
+// LCP analysis code
+//
 // Revision 1.1  2003/09/16 19:18:36  balewski
 // extraction of LCP from muDst
 //
