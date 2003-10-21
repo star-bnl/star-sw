b851f10f24b00fcb5aa6d2728aba41ae5f737b39
Yuri Fisyak <fisyak@bnl.gov>
2003-10-21 15:18:48 +0000

    Add Y2003 calibration

--- StRoot/StdEdxY2Maker/dEdxPoint.h
+++ StRoot/StdEdxY2Maker/dEdxPoint.h
@@ -7,31 +7,42 @@ class dEdx_t : public TObject {
  public:
   dEdx_t() {}
   virtual ~dEdx_t() {}
-  /* U(uncorrected) -> R(ADC nonlinearity) ->  Z(Drift distance) -> P(ressure) -> T(ime ?) -> 
-     S(ecRow) ->  M(ultiplicity charge) */
+  /* U->R->S->P->O->Z->X
+     U->R (TpcAdcCorrection) -> P (tpcPressure) ->
+     S (TpcSecRowB) ->  O (TpcDriftDistOxygen) ->  
+     Z (TpcZCorrection) -> X(TpcdXCorrection) */
   Int_t    sector;
   Int_t    row;
   Int_t    pad;
   Int_t    Fee;
+  Double_t ZdriftDistance;     // drift distance
+  Double_t ZdriftDistanceO2;     // ZdriftDistance*ppmOxygenIn
   Double_t dE;
   Double_t dx;
+  Double_t dx0;    // stright line approximation
   Double_t dEdx;   // after all corrections
   Double_t dEdxL;  // log of dEdx
   Double_t dEdxN;  // normolized to BB
   Double_t dEU;    // before correction (only Scale2keV scale)
-  Double_t dER;    // after row correction
-  Double_t dERdx;    // after row correction
-  Double_t dERdxL;    // after row correction
   Double_t dEUdx; 
   Double_t dEUdxL; // log of dEdx
   Double_t dEUdxN;  // normolized to BB
+  Double_t dER;    // after row correction
+  Double_t dERdx;    // after row correction
+  Double_t dERdxL;    // after row correction
+  Double_t dERdxN; 
   Double_t dEP;    // after Pressure correction
   Double_t dEPdx; 
+  Double_t dEPdxN; 
   Double_t dEPdxL; 
   Double_t dET;    // after TimeScale
   Double_t dETdx; 
   Double_t dEdxLT; 
   Double_t dETdxN;  // normolized to BB
+  Double_t dEO;    // after Blair correction
+  Double_t dEOdx; 
+  Double_t dEdxLO; 
+  Double_t dEOdxN;  // normolized to BB
   Double_t dES;    // after TimeScale + SecRow corrections
   Double_t dESdx; 
   Double_t dEdxLS; 
@@ -40,11 +51,14 @@ class dEdx_t : public TObject {
   Double_t dEZdx; 
   Double_t dEZdxL; 
   Double_t dEZdxN;  // normolized to BB
+  Double_t dEX;    //  dX corrections
+  Double_t dEXdx; 
+  Double_t dEXdxL; 
+  Double_t dEXdxN;  // normolized to BB
   Double_t dEM;     // after TimeScale + SecRow + Sec Z + Multipicity 
   Double_t dEMdx; 
   Double_t dEMdxL; 
   Double_t dEMdxN;  // normolized to BB
-  Double_t dEdxNB;  // normolized to Bichsel
   Double_t dETot; 
   Double_t xyz[3];
   Double_t Prob; 
@@ -61,6 +75,6 @@ class dEdx_t : public TObject {
   Double_t RMS;     // rms from volume charge
   Double_t zP;      // the most probable value from Bichsel
   Double_t sigmaP;  // sigma from Bichsel
-  ClassDef(dEdx_t,0); 
+  ClassDef(dEdx_t,1); 
 }; 
 #endif 
