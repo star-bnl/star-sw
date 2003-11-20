926a119b80faeb502a0a7f1e12cf203c1a26df1c
Jan Balewski <balewski@lbl.gov>
2003-11-20 22:59:40 +0000

    *** empty log message ***

--- StRoot/StEEmcUtil/EEfeeRaw/EEfeeRawEvent.h
+++ StRoot/StEEmcUtil/EEfeeRaw/EEfeeRawEvent.h
@@ -1,22 +1,10 @@
 #ifndef EEfeeRawEvent_h
 #define EEfeeRawEvent_h
 /*********************************************************************
- * $Id: EEfeeRawEvent.h,v 1.2 2003/11/20 16:01:46 balewski Exp $
+ * $Id: EEfeeRawEvent.h,v 1.3 2003/11/20 22:59:40 balewski Exp $
  *********************************************************************
  * Descripion:
  * STAR Endcap Electromagnetic Calorimeter Raw FEE Events
- *********************************************************************
- * $Log: EEfeeRawEvent.h,v $
- * Revision 1.2  2003/11/20 16:01:46  balewski
- * towars run 4
- *
- * Revision 1.1  2003/01/28 23:17:14  balewski
- * start
- *
- * Revision 1.1  2002/11/30 20:04:37  balewski
- * start
- *
- *
  *********************************************************************/
 #include "TObject.h"
 #include "TClonesArray.h"
@@ -36,9 +24,25 @@ public:
   int  getID() const{return ID;};
   void addFeeDataBlock(EEfeeDataBlock*);
   void maskWrongCrates( long timeStamp);
-
+  UShort_t  getValue(int crateID, int channel) const;
   ClassDef(EEfeeRawEvent,1) 
 };
 #endif
+
+/* $Log: EEfeeRawEvent.h,v $
+ * Revision 1.3  2003/11/20 22:59:40  balewski
+ * *** empty log message ***
+ *
+ * Revision 1.2  2003/11/20 16:01:46  balewski
+ * towars run 4
+ *
+ * Revision 1.1  2003/01/28 23:17:14  balewski
+ * start
+ *
+ * Revision 1.1  2002/11/30 20:04:37  balewski
+ * start
+ *
+ *
+ *********************************************************************/
 
 
