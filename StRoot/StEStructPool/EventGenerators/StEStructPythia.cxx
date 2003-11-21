f0a88d81af91da051c91508713d72e86d85d6690
R. Jefferson (Jeff) Porter <rjporter@lbl.gov>
2003-11-21 06:25:00 +0000

    Pythia event generater as an StEStructEventReader

---
+++ StRoot/StEStructPool/EventGenerators/StEStructPythia.cxx
@@ -0,0 +1,174 @@
+/**********************************************************************
+ *
+ * $Id: StEStructPythia.cxx,v 1.1 2003/11/21 06:24:56 porter Exp $
+ *
+ * Author: Jeff Porter 
+ *
+ **********************************************************************
+ *
+ * Description:  EStructEventReader wrapper for (T)Pythia event generator
+ *
+ **********************************************************************/
+#include "StEStructPythia.h"
+
+#include "StEStructPool/AnalysisMaker/StEStructEventCuts.h"
+#include "StEStructPool/AnalysisMaker/StEStructTrackCuts.h"
+#include "StEStructPool/EventMaker/StEStructEvent.h"
+#include "StEStructPool/EventMaker/StEStructTrack.h"
+
+StEStructPythia::StEStructPythia(): mpythia(0), meventCount(0), meventsToDo(0), mAmDone(false), mECuts(0), mTCuts(0){};
+
+StEStructPythia::StEStructPythia(int nevents, TPythia6* pythia, StEStructEventCuts* ecuts, StEStructTrackCuts* tcuts): meventCount(0), mAmDone(false){
+
+  
+  meventsToDo=nevents;
+  mpythia=pythia;
+  mECuts=ecuts;
+  mTCuts=tcuts;
+
+};
+
+bool StEStructPythia::hasGenerator() { return (mpythia) ? true : false ; };
+bool StEStructPythia::hasEventCuts() { return (mECuts) ? true : false ; }
+bool StEStructPythia::hasTrackCuts() { return (mTCuts) ? true : false ; }
+
+
+//-------------------------------------------------------------------------
+StEStructEvent* StEStructPythia::next() {
+
+  if(!mpythia || meventCount==meventsToDo){
+    mAmDone=true;
+    return (StEStructEvent*)NULL;
+  }
+  return generateEvent();
+}
+
+//--------------------------------------------------------------------------
+StEStructEvent* StEStructPythia::generateEvent(){
+
+  StEStructEvent* retVal=NULL;
+
+  if(mpythia) mpythia->GenerateEvent();
+
+  retVal = new StEStructEvent();
+
+  fillTracks(retVal);
+  bool useEvent=mECuts->goodNumberOfTracks(mrefMult);
+  if(!useEvent){
+    delete retVal;
+    retVal=NULL;
+  } else {
+    retVal->FillChargeCollections();
+  }
+  mECuts->fillHistogram(mECuts->numTracksName(),(float)mrefMult,useEvent);
+
+  return retVal;
+}   
+
+//--------------------------------------------------------------------------
+void StEStructPythia::fillTracks(StEStructEvent* estructEvent){
+
+  mrefMult=0;
+  Pyjets_t* pstr= mpythia->GetPyjets();
+  int numParticles=mpythia->GetN();
+
+  StEStructTrack* eTrack= new StEStructTrack();
+  int pid;
+
+  for(int i=2;i<numParticles;i++){ // 0 & 1 for incoming protons 
+
+    eTrack->SetInComplete();
+    pid = pstr->K[1][i];
+    if(!measureable(pid))continue;  // checks if pi,k,p or e
+    float p[3];
+    float v[3];
+    for(int k=0;k<3;k++){
+      p[k]=pstr->P[k][i];
+      v[k]=pstr->V[k][i];
+    }
+
+    float pt=sqrt(p[0]*p[0]+p[1]*p[1]);
+    if(pt<0.15)continue;
+
+    float* gdca = globalDCA(p,v);
+    
+    bool useTrack=true;
+    useTrack = (mTCuts->goodGlobalDCA(gdca[3]) && useTrack);
+
+    float energy=pstr->P[3][i];
+    float num=energy+p[2];
+    float den=energy-p[2];
+    float eta=-999.;
+    if(den!=0.) { 
+       float arg=num/den;
+       if(arg>0.) eta=0.5*log(arg);
+    }
+    useTrack = (mTCuts->goodEta(eta) && useTrack);
+    float phi=atan2((double)p[1], (double)p[0]);
+    useTrack=(mTCuts->goodPhi(phi) && useTrack);
+
+    if(useTrack)mrefMult++;
+
+    useTrack=(mTCuts->goodPt(pt) && useTrack);
+ 
+    float _r=pt/0.139;
+    float yt=log(sqrt(1+_r*_r)+_r);
+    useTrack = (mTCuts->goodYt(yt) && useTrack);
+
+    mTCuts->fillHistograms(useTrack);
+
+    eTrack->SetBx(gdca[0]);
+    eTrack->SetBy(gdca[1]);
+    eTrack->SetBz(gdca[2]);
+    eTrack->SetBxGlobal(gdca[0]);
+    eTrack->SetByGlobal(gdca[1]);
+    eTrack->SetBzGlobal(gdca[2]);
+
+    delete [] gdca;
+    if(!useTrack) continue;
+  
+    eTrack->SetPx(p[0]);
+    eTrack->SetPy(p[1]);
+    eTrack->SetPz(p[2]);
+    eTrack->SetEta(eta);
+    eTrack->SetPhi(phi);
+
+    if(pid<0){
+      eTrack->SetCharge(-1);
+    } else {
+      eTrack->SetCharge(1);
+    }    
+    estructEvent->AddTrack(eTrack);
+  }
+
+  delete eTrack;
+  return;
+
+}    
+
+
+//--------------------------------------------------------------------------
+void StEStructPythia::setEventCuts(StEStructEventCuts* cuts){
+
+  if(mECuts) delete mECuts;
+  mECuts=cuts;
+
+};
+
+//---------------------------------------------------------------
+void StEStructPythia::setTrackCuts(StEStructTrackCuts* cuts){
+  if(mTCuts) delete mTCuts;
+  mTCuts=cuts;
+}
+
+
+
+
+/**********************************************************************
+ *
+ * $Log: StEStructPythia.cxx,v $
+ * Revision 1.1  2003/11/21 06:24:56  porter
+ * Pythia event generater as an StEStructEventReader
+ *
+ *
+ *********************************************************************/
---
+++ StRoot/StEStructPool/EventGenerators/StEStructPythia.h
@@ -0,0 +1,133 @@
+/**********************************************************************
+ *
+ * $Id: StEStructPythia.h,v 1.1 2003/11/21 06:25:00 porter Exp $
+ *
+ * Author: Jeff Porter 
+ *
+ **********************************************************************
+ *
+ * Description:  EStructEventReader wrapper for (T)Pythia event generator
+ *
+ **********************************************************************/
+#ifndef __STESTRUCTPYTHIA__H
+#define __STESTRUCTPYTHIA__H
+
+#include "StEStructPool/AnalysisMaker/StEStructEventReader.h"
+
+#include "TROOT.h"
+#include "TPythia6.h"
+
+class StEStructEventCuts;
+class StEStructTrackCuts;
+
+
+class StEStructPythia : public StEStructEventReader {
+
+  TPythia6* mpythia;
+  int meventCount;
+  int meventsToDo;
+  bool mAmDone;
+  int mrefMult;
+
+  StEStructEventCuts* mECuts;
+  StEStructTrackCuts* mTCuts;
+
+  void fillTracks(StEStructEvent* estructEvent);
+
+ public:
+
+  StEStructPythia();
+  StEStructPythia(int nevents, TPythia6* pythia, StEStructEventCuts* ecuts, StEStructTrackCuts* tcuts);
+
+  virtual ~StEStructPythia(){};
+  void setEventCuts(StEStructEventCuts* cuts);
+  void setTrackCuts(StEStructTrackCuts* cuts);
+  bool hasGenerator();
+  bool hasEventCuts();
+  bool hasTrackCuts();
+  bool measureable(int pid);
+  float* globalDCA(float* p, float* v);
+
+  virtual StEStructEvent* next();
+  virtual bool         done();
+  virtual StEStructEvent* generateEvent();
+
+  ClassDef(StEStructPythia,1)
+};
+
+
+inline bool StEStructPythia::done(){ return mAmDone; };
+
+inline bool StEStructPythia::measureable(int pid){
+  bool retVal=false;
+  if(pid<0)pid*=-1;
+
+  switch(pid){
+
+  case 211:
+    {   // charged pion
+       retVal=true;
+       break;
+    }
+  case 321:
+    {   // charged kaon
+      retVal=true;
+      break;
+    }
+  case 2212:
+    {   // proton
+      retVal=true;
+      break;
+    }
+  case 11:
+    {   // electron
+      retVal=true;
+      break;
+    }
+  default:
+    {
+      break;
+    }
+  }
+  return retVal;
+}
+
+inline float* StEStructPythia::globalDCA(float* p, float* v){
+
+  // assumes primaryVertex at origin 
+  float* r=new float[4];
+  r[0]=r[1]=r[2]=r[3]=0;
+
+  if(fabs(p[2])<0.01){ // then 2D
+    if(fabs(p[0])<0.01){ 
+      r[3]=r[1]=v[1]; // p=py only
+      return r;
+    }
+    float a = p[1]/p[0];
+    float x = -1.0*(a*v[1])/(a*a+1);
+    float y=a*x+v[1];
+    r[0]=x; r[1]=y;
+    r[3]=sqrt(x*x+y*y);
+    return r;
+  }
+  float ax = p[0]/p[2];
+  float ay = p[1]/p[2];
+  float z  = -1.0*(ax*v[0]+ay*v[1])/(ax*ax+ay*ay+1);
+  float x  = ax*z+v[0];
+  float y  = ay*z+v[1];
+  r[0]=x; r[1]=y; r[2]=z;
+  r[3]=sqrt(x*x+y*y+z*z);
+  return r;
+}
+
+
+#endif
+
+/**********************************************************************
+ *
+ * $Log: StEStructPythia.h,v $
+ * Revision 1.1  2003/11/21 06:25:00  porter
+ * Pythia event generater as an StEStructEventReader
+ *
+ *
+ *********************************************************************/
