c38a94cfb275dd89f16ad29606ed75a46f3bf512
Subhasis Chattopadyay (Chattopadhyay) <sub@vecc.gov.in>
2003-12-10 10:43:24 +0000

    loop for No of channels read is changed to PMD_CRAMS_CH_MAX

--- StRoot/StDAQMaker/StPMDReader.h
+++ StRoot/StDAQMaker/StPMDReader.h
@@ -1,11 +1,16 @@
 /***************************************************************************
+ *$Id: StPMDReader.h,v 1.2 2003/12/10 10:43:24 subhasis Exp $
  *
+ * StPMDReader.h
  * Author: Susanta and Subhasis
  ***************************************************************************
  *
  * Description: Offline Wrapper for DAQ PMD reader functions
  *
  ***************************************************************************
+ *$Log: StPMDReader.h,v $
+ *Revision 1.2  2003/12/10 10:43:24  subhasis
+ *loop for No of channels read is changed to PMD_CRAMS_CH_MAX
  *
  **************************************************************************/
 #include "StDaqLib/PMD/PMD_Reader.hh"
@@ -28,6 +33,8 @@ class StPMDReader
   int NPMDHits();
   int getNoOfChannelsInCramBlock(int, int, int );   // sec/Crate_No, Cram, Blk , Return No. Of channels in that Cram Block 
   int getAllPmdCpvData(int *); // get ADC values, Return total No. of Channel
+  int getAllPmdCpvPed(int *); // get ADC values, Return total No. of Channel
+  int getAllPmdCpvRms(int *); // get ADC values, Return total No. of Channel
   int getAllPmdCpvDataChannelByChannel(int, int, int, int); // Sector/Crate No.,Crams,Block,Channel, Return ADC Value 
   int getNoOfChannelsInPmdChain(int );   // Chain No.  , Return No. Of channels in that chain
   int getPmdChainData(int, int *); // Chain No., ADC Value, Return No. Of channels in that chain
