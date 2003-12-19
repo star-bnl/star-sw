64b2181dabfb84553c18a28347dce0e708eb9bd5
zolnie <>
2003-12-19 17:32:44 +0000

    ver pre-alpha

--- StRoot/StEEmcPool/macros/TTM/compatTTM.C
+++ StRoot/StEEmcPool/macros/TTM/compatTTM.C
@@ -7,40 +7,37 @@ StChain *chain=0;
 
 
 int 
-compatTTM(
-		char* inDir   ="/star/fy2003/mudst/",
-		char* file    ="",//"st_physics_4145010_raw_0010001.MuDst.root",
-		Int_t nFiles  =100 
-		)
+compatTTM
+(
+ char* inpDir  = "/star/fy2003/mudst/",
+ char* inpFile = "", //"st_physics_4145010_raw_0010003.MuDst.root",
+ Int_t nFiles  = 50
+ )
 { 
-
-
-  gROOT->LoadMacro("/afs/rhic/star/packages/DEV/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  
+  gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
   loadSharedLibraries();
 
   StMuTimer timer;
   timer.start();
   // switch of debug messages 
-  StMuDebug::setLevel(0);
+
 
   gSystem->Load("StDbLib");
   gSystem->Load("StDbBroker");
   gSystem->Load("St_db_Maker");
-
 
   // Load my maker
   gSystem->Load("StEEmcUtil");
   gSystem->Load("StEEmcDbMaker");
   gSystem->Load("StEEmcPoolTTM");
 
-
-// create chain    
+  // create chain    
   chain = new StChain("StChain"); 
   
-// Now we add Makers to the chain...   
-  muDstMaker = new StMuDstMaker(0,0,inDir,file,"MuDst.root",nFiles);
+  // Now we add Makers to the chain...   
+  muDstMaker = new StMuDstMaker(0,0,inpDir,inpFile,"MuDst.root",nFiles);
   StMuDbReader* db = StMuDbReader::instance();
-
   
   // instantiate your maker here 
   StEEmcDbMaker  *eemcDbMaker=new StEEmcDbMaker("eemcDb");  
@@ -49,6 +46,7 @@ compatTTM(
   
   // request DB for sectors you need (dafault:1-12)
   // myMk->setSectors(5,8);
+
 
   // to overwritte the time stamp 
   // reverse order of the above makers: first #2, then #1
@@ -62,11 +60,12 @@ compatTTM(
   // myMk->setPreferedFlavor("set430","eemcPMTcal");
 
   EETowCompatMatchMaker *m = new  EETowCompatMatchMaker ("CompatTM",muDstMaker,eemcDbMaker);
-  m->SetDebugLevel(kInfo);
 
+
+  m->SetDebugLevel(kWarning);
+  StMuDebug::setLevel(0);
 
   chain->Init();
-
   chain->ls(3);
 
   int counter=0;
@@ -80,22 +79,21 @@ compatTTM(
   float curUsed=0.0;
   //---------------------------------------------------
   while ( stat==0 )  {// loop over events
-    chain->Clear();
+    //chain->Clear();
+    //cerr << "file: " << chain->GetCurrentFile()->GetName() << endl;
+    cout.precision(2);
     cout << " #" << counter;
-    curUser=memStat.Used();
+    curUsed=memStat.Used();
     curSize=memStat.ProgSize();
     if(curSize>maxSize) maxSize=curSize;
     if(curSize<minSize) minSize=curSize;
-    cout << " heap= "<< curUser;
+    cout << " heap= "<< curUsed;
     cout << " prog= "<< curSize;
-    cout << "\t(" << minSize << "/" << maxSize << ")";
-    cout << "                                          \r";
-    cout.flush();
+    cout << " (" << minSize << "/" << maxSize << ")" << endl;
     counter++;
     stat = chain->Make();
   }
-  
-  chain->Finish();
+  //chain->Finish();
 
   cout << endl;
   if (counter) cout << "time/event " << timer.elapsedTime()/counter <<endl;
