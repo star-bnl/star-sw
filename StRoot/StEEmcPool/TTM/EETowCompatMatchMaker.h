7f6352e4860dd72e9fd8bf5a103cb6e94e557462
zolnie <>
2003-12-30 15:11:10 +0000

    working version

--- StRoot/StEEmcPool/TTM/EETowCompatMatchMaker.h
+++ StRoot/StEEmcPool/TTM/EETowCompatMatchMaker.h
@@ -1,4 +1,4 @@
-// $Id: EETowCompatMatchMaker.h,v 1.1.1.1 2003/12/18 18:00:54 zolnie Exp $
+// $Id: EETowCompatMatchMaker.h,v 1.2 2003/12/30 15:10:56 zolnie Exp $
 
 #ifndef STAR_EETowCompatMatchMaker
 #define STAR_EETowCompatMatchMaker
@@ -21,6 +21,7 @@
 #endif
 
 #include "TString.h"
+#include "TVector3.h"
 
 
 class TH1F;
@@ -82,6 +83,8 @@ struct CompatTTM {
 
 class EETowCompatMatchMaker : public StMaker {
  public: 
+  enum ADCMode_t { kRawAdc, kPedSub, kPedAndGain };
+
   EETowCompatMatchMaker(const char          *self      ="MuEmcTowerCalib", 
 		       StMuDstMaker  *mumaker = NULL,
 		       StEEmcDbMaker *dbmaker = NULL);
@@ -96,11 +99,24 @@ class EETowCompatMatchMaker : public StMaker {
   Int_t   GetDebugLevel(           ) { return mDebugLevel;      } //
 
   void    SetFileName( const char *string) { mFileName=TString(string); }
-   
+
+  ULong_t GetNMatched() { return mNMatch; };
 
  protected:
-  Int_t            mDebugLevel           ;  // debug level:  use kInfo,kWarning,etc. from TError.h
-  Bool_t           accept(StMuTrack*    ) ; 
+  Int_t   mDebugLevel           ;  // debug level:  use kInfo,kWarning,etc. from TError.h
+  Bool_t  accept(StMuTrack*    ) ; 
+  Bool_t  matchTrack( const StMuTrack *track, 
+		      const double   z,
+		      const int      sec, 
+		      const int      ssec, 
+		      const int      etabin,
+		      TVector3&      dr,
+		      float          deta=0.0,
+		      float          dphi=0.0,
+		      float          dz  =0.0);      
+
+
+  Bool_t extrapolateToZ(const StMuTrack *track, const double   z, TVector3&      r);
 
   // control histograms for tracks
   TH1F *hTrackNHits;
@@ -121,10 +137,13 @@ class EETowCompatMatchMaker : public StMaker {
   StMuDstMaker   *mMuDstMaker; // toplevel muDST maker
   StEEmcDbMaker  *mEEmcDb;     // EEMC database maker
   EEmcGeomSimple *mGeom;       // tower geometry
+  ADCMode_t       mAdcMode;
 
   TString         mFileName;  // output file name
   TFile          *mFile;      // output file
   TTree          *mTree;      // output tree
+
+  ULong_t         mNMatch; 
 
   struct CompatTTM *mMatch;   // data in "compatible" format 
 
@@ -132,7 +151,7 @@ class EETowCompatMatchMaker : public StMaker {
   //  StMaker jumbo mumbo
   /// Displayed on session exit, leave it as-is please ...
   virtual const char *GetCVS() const {
-    static const char cvs[]="Tag $Name:  $ $Id: EETowCompatMatchMaker.h,v 1.1.1.1 2003/12/18 18:00:54 zolnie Exp $ built "__DATE__" "__TIME__ ; 
+    static const char cvs[]="Tag $Name:  $ $Id: EETowCompatMatchMaker.h,v 1.2 2003/12/30 15:10:56 zolnie Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
 
@@ -144,6 +163,9 @@ class EETowCompatMatchMaker : public StMaker {
 
 
 // $Log: EETowCompatMatchMaker.h,v $
+// Revision 1.2  2003/12/30 15:10:56  zolnie
+// working version
+//
 // Revision 1.1.1.1  2003/12/18 18:00:54  zolnie
 // Imported sources
 //
