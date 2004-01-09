96ec8ecb9b5c665d842132b34f00983c8f3b3bd7
suaide <>
2004-01-09 18:12:34 +0000

    map changed for 2004 run (towers and SMD)

--- StRoot/StDaqLib/EMC/StEmcDecoder.cxx
+++ StRoot/StDaqLib/EMC/StEmcDecoder.cxx
@@ -111,9 +111,15 @@ void StEmcDecoder::Init(unsigned int date,unsigned int time)
     for(int i=0;i<30;i++) TDC_Crate[i]=TDC_Crate_tmp[i];      
     goto SMD;
   }
-  if(date >= 20011226)
+  if(date >= 20011226 && date < 20030701) // year 2002/2003 pp and dAu runs
   {
     int TDC_Crate_tmp[]= {18,17,16,30,29,28,27,26,25,24,23,22,21,20,19,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    for(int i=0;i<30;i++) TDC_Crate[i]=TDC_Crate_tmp[i];      
+    goto SMD;
+  }
+  if(date >= 20030701) // year 2004 AuAu and pp runs
+  {
+    int TDC_Crate_tmp[]= {18,17,16,30,29,28,27,26,25,24,23,22,21,20,19,4,3,2,1,15,14,13,12,11,10,9,8,7,6,5};
     for(int i=0;i<30;i++) TDC_Crate[i]=TDC_Crate_tmp[i];      
     goto SMD;
   }
@@ -161,17 +167,33 @@ void StEmcDecoder::Init(unsigned int date,unsigned int time)
     goto FEE;
   }
   
-  if(date >= 20021001) // SMD modules connection for Y3
+  if(date >= 20021001 && date < 20030701) // year 2002/2003 pp and dAu runs
   {
     int SmdModules_tmp[8][15]={
-                              {46,47,48,49,50,51,52,53,54,55,56,57,58,59,60},                //RDO 2
+                              {46,47,48,49,50,51,52,53,54,55,56,57,58,59,60},                //RDO 0
                               {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},                         //RDO 1
-                              {31,32,33,34,35,36,37,38,39,40,41,42,43,44,45},                //RDO 0
+                              {31,32,33,34,35,36,37,38,39,40,41,42,43,44,45},                //RDO 2
                               {16,17,18,19,20,21,22,23,24,25,26,27,28,29,30},                //RDO 3
                               {61,62,63,64,65,66,67,68,69,70,71,72,73,74,75},                //RDO 4
                               {76,77,78,79,80,81,82,83,84,85,86,87,88,89,90},                //RDO 5
                               {91,92,93,94,95,96,97,98,99,100,101,102,103,104,105},          //RDO 6
                               {106,107,108,109,110,111,112,113,114,115,116,117,118,119,120}  //RDO 7
+                              }; 
+    for(int i=0;i<8;i++) for(int j=0;j<15;j++) SmdModules[i][j]=SmdModules_tmp[i][j];
+    goto FEE;
+  }
+  
+  if(date >=  20030701) // year 2004 AuAu and pp runs
+  {
+    int SmdModules_tmp[8][15]={
+                              {31,32,33,34,35,36,37,38,39,40,41,42,43,44,45},                //RDO 0
+                              {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},                         //RDO 1
+                              {46,47,48,49,50,51,52,53,54,55,56,57,58,59,60},                //RDO 2
+                              {16,17,18,19,20,21,22,23,24,25,26,27,28,29,30},                //RDO 3
+                              {106,107,108,109,110,111,112,113,114,115,116,117,118,119,120}, //RDO 4
+                              {76,77,78,79,80,81,82,83,84,85,86,87,88,89,90},                //RDO 5
+                              {91,92,93,94,95,96,97,98,99,100,101,102,103,104,105},          //RDO 6
+                              {61,62,63,64,65,66,67,68,69,70,71,72,73,74,75}                 //RDO 7
                               }; 
     for(int i=0;i<8;i++) for(int j=0;j<15;j++) SmdModules[i][j]=SmdModules_tmp[i][j];
     goto FEE;
@@ -628,13 +650,14 @@ void StEmcDecoder::PrintTowerMap(ofstream *out)
   *out <<"-----------------------------------------------------------\n";
   for(int daq =0;daq<4800;daq++)
   {
-    int towerId,tdc,crate,position;
+    int towerId,tdc,crate,position,m,e,s;
     GetTowerIdFromDaqId(daq,towerId);
     GetTowerTDCFromDaqId(daq,tdc);
     GetTowerCrateFromDaqId(daq,crate,position);
+    GetTowerBin(towerId,m,e,s);
     *out <<"  daq id = "<<daq<<"  TDC channel = "<<tdc
          <<"  Crate = "<<crate<<"  position in crate = "<<position
-         <<"  software id = "<<towerId<<endl;
+         <<"  software id = "<<towerId<<"  m = "<<m<<"  e = "<<e<<"  s = "<<s<<endl;
   }
   *out <<endl;
 }
