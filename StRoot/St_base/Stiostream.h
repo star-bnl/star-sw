b1dbcdcb3b531254a8471d7eb9499de69fdfc7ec
fisyak <>
2004-01-14 23:08:44 +0000

    addition ifdef block for GNU (for alpha)

--- StRoot/St_base/Stiostream.h
+++ StRoot/St_base/Stiostream.h
@@ -35,12 +35,13 @@
 #define STIOSTREAM_H
 #include "Riostream.h"
 #ifndef __CINT__
+#ifdef __GNUC__
 #if __GNUC__>=3
 #   include <istream>
 #else
 #   include <istream.h>
 #endif
 #endif
-
+#endif
 
 #endif 
