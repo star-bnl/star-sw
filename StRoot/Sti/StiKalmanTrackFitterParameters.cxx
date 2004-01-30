178678c287355489a7ee4f0ab83f89aba12222a9
pruneau <>
2004-01-30 21:38:07 +0000

    Added load method to fetch db values

--- StRoot/Sti/StiKalmanTrackFitterParameters.cxx
+++ StRoot/Sti/StiKalmanTrackFitterParameters.cxx
@@ -2,7 +2,7 @@
 #include "Sti/StiToolkit.h" 
 #include "Sti/Base/Factory.h" 
 #include "Sti/Base/EditableParameter.h" 
- 
+#include "tables/St_KalmanTrackFitterParameters_Table.h"
 StiKalmanTrackFitterParameters::StiKalmanTrackFitterParameters() 
   : EditableParameters("KalmanTrackFitterParameters","KalmanTrackFitterParameters") 
 { 
@@ -29,6 +29,14 @@ const StiKalmanTrackFitterParameters & StiKalmanTrackFitterParameters::operator=
   _editable = p._editable; 
   return *this; 
 } 
+
+const StiKalmanTrackFitterParameters & StiKalmanTrackFitterParameters::operator=(const KalmanTrackFitterParameters_st & p) 
+{ 
+	_maxChi2 = p.maxChi2;
+  return *this; 
+} 
+
+
  
 void StiKalmanTrackFitterParameters::initialize() 
 { 
@@ -36,3 +44,16 @@ void StiKalmanTrackFitterParameters::initialize()
   _editable = true; 
   add( new EditableParameter("maxChi2", "Maximum Chi2", &_maxChi2, 3., 0., 1000., 0.1, 0) );
 } 
+
+
+void StiKalmanTrackFitterParameters::load(TDataSet * ds)
+{
+	// validate source
+	if (!ds) throw runtime_error("StiKalmanTrackFitterParameters::load(TDataSet * ds)");
+	// 	
+	St_KalmanTrackFitterParameters * a = dynamic_cast<St_KalmanTrackFitterParameters*>(ds->Find(getName().c_str() ));
+  if (!a) throw runtime_error("StiKalmanTrackFitterParameters::load(TDataSet * ds) -E- a==0");
+	KalmanTrackFitterParameters_st * b = a->GetTable();
+	if (!b) throw runtime_error("StiKalmanTrackFitterParameters::load(TDataSet * ds) -E- b==0");
+	*this = *b;
+}
