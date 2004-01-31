b523817268687ce9feb92515783b95a9da8802b7
porter <>
2004-01-31 02:17:22 +0000

    got rid of last(?) tabname limit

--- StRoot/StDbBroker/dbConfig.h
+++ StRoot/StDbBroker/dbConfig.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: dbConfig.h,v 1.1 2000/01/10 20:31:16 porter Exp $
+ * $Id: dbConfig.h,v 1.2 2004/01/31 02:17:22 porter Exp $
  *
  * Author: R. Jeff Porter & V. Perevoztchikov
  ***************************************************************************
@@ -11,6 +11,9 @@
  ***************************************************************************
  *
  * $Log: dbConfig.h,v $
+ * Revision 1.2  2004/01/31 02:17:22  porter
+ * got rid of last(?) tabname limit
+ *
  * Revision 1.1  2000/01/10 20:31:16  porter
  * modified StDbBroker to be an interface to the DB-interface, StDbLib.
  *  - old functionality is retained for the short-term & modifications
@@ -26,10 +29,10 @@
 
 struct dbConfig_st {
 
-  char tabname[20]; // named reference to table or node
+  char tabname[64]; // named reference to table or node
   int  tabID;       // StDbBroker's table ID
-  char tabtype[20]; // type of table or node referenced
-  char parname[20]; // named reference of parent to table or node referenced
+  char tabtype[64]; // type of table or node referenced
+  char parname[64]; // named reference of parent to table or node referenced
   int  parID;       // StDbBroker's parent ID
 
 };
