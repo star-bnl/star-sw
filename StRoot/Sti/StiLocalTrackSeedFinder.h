0f9836bbdb436eed3de45615a12ad51b9f3d3af1
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2004-02-21 18:11:57 +0000

    Adding parameter class StiLocalTrackSeedFinderParameters All control variables of the finder transfered to the Parameters class. Changes reflected in the finder. Also removing the abstract class StiTrackSeedFinder and using StiTrackFinder as a base class instead. All changes reflected here.

--- StRoot/Sti/StiLocalTrackSeedFinder.h
+++ StRoot/Sti/StiLocalTrackSeedFinder.h
@@ -6,46 +6,58 @@
 using std::ostream;
 #include <vector>
 using std::vector;
-#include "StiHelixCalculator.h"
-#include "StiHelixFitter.h"
-#include "StiTrackSeedFinder.h"
-#include "StiSortedHitIterator.h"
-class StiHitContainer;
-class Sti2HitComboFilter;
-class StiDetector;
 #include "Sti/Base/EditableParameters.h"
+#include "Sti/StiHelixCalculator.h"
+#include "Sti/StiHelixFitter.h"
+#include "Sti/StiTrackFinder.h"
+#include "Sti/StiSortedHitIterator.h"
+#include "Sti/StiMasterDetectorBuilder.h"
+#include "Sti/StiLocalTrackSeedFinderParameters.h"
+#include "Sti/StiDefaultTrackFilter.h"
+#include "Sti/StiLocalTrackSeedFinderParameters.h"
+
+class StiDetector;
 
 /// \class StiLocalTrackSeedFinder
-/// StiLocalTrackSEedFinder is a concrete implementation of StiTrackSeedFinder.
+/// StiLocalTrackSeedFinder is a concrete implementation of StiTrackFinder.
 /// It is built from a collection of StiDetectorObjects, which it
 /// stores in a vector and orders properly, then uses each detector
 /// as a layer from which a one-point seed can be generated.  It then proceeds
 /// to step inwards, iteratively making a local decision at each step.
 /// \author M.L. Miller (Yale Software) 10/01
 /// \author Claude A Pruneau (Wayne State) Jan 2003
-class StiLocalTrackSeedFinder : public StiTrackSeedFinder
+class StiLocalTrackSeedFinder : public StiTrackFinder
 {
 public:
   StiLocalTrackSeedFinder(const string& name,
+			  const string& description, 
 			  Factory<StiKalmanTrack> * trackFactory,
 			  StiHitContainer         * hitContainer,
-			  StiDetectorContainer    * detectorContainer);
+			  StiDetectorContainer * detectorContainer);
   virtual ~StiLocalTrackSeedFinder();
-  virtual bool hasMore();
-  virtual StiKalmanTrack* next();
-  virtual void reset();
-  virtual void initialize();
-  virtual void addLayer(StiDetector*);
+  StiTrack* findTrack();
+  virtual void clear(){reset();};
+  virtual void initialize(){};
   virtual void print() const;
-	void load(TDataSet*);
-	void load(ifstream& inFile); 
-	friend ostream& operator<<(ostream& os, const StiLocalTrackSeedFinder &f);
-  
+  Filter<StiTrack>    * getTrackFilter();
+  EditableParameters  & getParameters();
+  virtual void reset();
+  bool isReset();
+
+  void findTracks(){}; 
+  bool find(StiTrack *track, int direction){return false;};
+  void extendTracksToVertex(StiHit* vertex){};
+  StiVertexFinder * getVertexFinder(){return 0;};
+  void setVertexFinder(StiVertexFinder *){};
+
+  friend ostream& operator<<(ostream& os, const StiLocalTrackSeedFinder & f);
+
 protected:
-  StiSortedHitIterator begin();
-  StiSortedHitIterator end();
+  void loadDS(TDataSet&);
+  void loadFS(ifstream&);
+  StiKalmanTrack*makeTrack(StiHit* hit);
   ///Extend hit looking for closest neighbor in z
-  bool extendHit(StiHit * hit);
+  bool extendHit(StiHit & hit);
   ///Extrapolate to next layer using straight line, add hit closest in z
   bool extrapolate();
   StiKalmanTrack* initializeTrack(StiKalmanTrack*);
@@ -53,33 +65,18 @@ protected:
   void calculateWithOrigin(StiKalmanTrack*);
   //Perform helix fit, Perform helix calculation (doesn't assume any vertex)
   bool fit(StiKalmanTrack*);
-  typedef vector<StiHit*> HitVec; 
-  typedef vector<StiDetector*> DetVec;
- StiSortedHitIterator _hitIter;
-  virtual StiKalmanTrack* makeTrack(StiHit*);
-  DetVec mDetVec;
 
-  //define search window in the next layer when connecting two points
-  double mDeltaY;
-  double mDeltaZ;
-  //define the number of points to connect
-  int mSeedLength;
-  //define search window in the next layer when extending a coonection of points
-  double mExtrapDeltaY;
-  double mExtrapDeltaZ;
-  //Count how many hits we've skipped in extrapolation
-  int mSkipped;
-  //Define the max number we can skip
-  int mMaxSkipped;
-  //define the Min/Max number of points to extrapolate
-  int mExtrapMinLength;
-  int mExtrapMaxLength;
-  //Use the origin to calculate helix?
-  bool mUseOrigin;
-  vector<StiHit*> mSeedHitVec;
-  bool mDoHelixFit; //true-> fit, false-> calculate
-  StiHelixCalculator mHelixCalculator;
-  StiHelixFitter mHelixFitter;
+  bool   _reset;
+  Factory<StiKalmanTrack> * _trackFactory;
+  StiHitContainer         * _hitContainer;
+  StiDetectorContainer    * _detectorContainer;
+
+  StiSortedHitIterator _hitIter;
+  vector<StiHit*>        _seedHits;
+  StiHelixCalculator     _helixCalculator;
+  StiHelixFitter         _helixFitter;
+  StiDefaultTrackFilter  _trackFilter;
+  StiLocalTrackSeedFinderParameters _pars;
   
  private:
   //The following are not implemented, as they are non-trivial
@@ -87,38 +84,35 @@ protected:
   StiLocalTrackSeedFinder();
   StiLocalTrackSeedFinder(const StiLocalTrackSeedFinder&);
   StiLocalTrackSeedFinder operator=(const StiLocalTrackSeedFinder&);
+  //Count how many hits we've skipped in extrapolation
+  int    _skipped;
 };
 
-inline bool StiLocalTrackSeedFinder::hasMore()
-{
-  return _hitIter!=end();
-}
-
-///Return an iterator pointing to the first hit of the current event
-inline StiSortedHitIterator  StiLocalTrackSeedFinder::begin()
-{
-  return StiSortedHitIterator(getHitContainer(),mDetVec.begin(), mDetVec.end());
-}
-
-/// return an iterator pointing to no hit
-inline StiSortedHitIterator  StiLocalTrackSeedFinder::end()
-{
-  return StiSortedHitIterator();
-}
 
 inline void StiLocalTrackSeedFinder::reset()
 {
-  _messenger <<"StiLocalTrackSeedFinder::reset() -I- Started"<<endl;
-  _hitIter = begin();
-  //Cleanup the base-class
-  StiTrackSeedFinder::reset();
-  _messenger <<"StiLocalTrackSeedFinder::reset() -I- Done"<<endl;
+  cout <<"StiLocalTrackSeedFinder::reset() -I- Started"<<endl;
+  _reset = true;
+  cout <<"StiLocalTrackSeedFinder::reset() -I- Done"<<endl;
 }
 
-struct RPhiLessThan
+inline bool StiLocalTrackSeedFinder::isReset()
 {
-    bool operator()(const StiDetector*, const StiDetector*);
-};
+  bool state = _reset;
+  if (_reset) _reset = false;
+  return state;
+}
+
+inline Filter<StiTrack> * StiLocalTrackSeedFinder::getTrackFilter() 
+{
+  return &_trackFilter;
+}
+
+inline EditableParameters       & StiLocalTrackSeedFinder::getParameters()
+{
+  return _pars;
+}
+
 
 struct ScaleHitError
 {
