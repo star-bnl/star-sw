d92866fce3e685679c243339eeaaa67fd09fe591
cmironov <>
2004-03-03 01:46:13 +0000

    (J.L. commit) Modified doxygen documentation (was not taken before). Indentation for numerous enum{} which would need some //< comments for usage summary ...

--- StRoot/StSecondaryVertexMaker/StV0FinderMaker.h
+++ StRoot/StSecondaryVertexMaker/StV0FinderMaker.h
@@ -1,9 +1,9 @@
-/*
-  \class StV0FinderMaker
-  
-  StV0FinderMaker finds V0 secondary vertices
-
-*/
+/*!
+ * \class StV0FinderMaker
+ *
+ * StV0FinderMaker finds V0 secondary vertices
+ *
+ */
 
 #ifndef StV0FinderMaker_hh
 #define StV0FinderMaker_hh
@@ -24,78 +24,86 @@ class StMuDstMaker;
 //end Betty
 
 enum TrackerUsage
-{kTrackerUseTPT = 0,
- kTrackerUseITTF = 1,
- kTrackerUseBOTH = 2
- };
+{
+  kTrackerUseTPT  = 0,
+  kTrackerUseITTF = 1,
+  kTrackerUseBOTH = 2
+};
 
 
 
 ///Begin Betty
 enum SVTUsage
-  {kNoSVT = 0,
-   kUseSVT = 1
-  };
+{
+  kNoSVT = 0,
+  kUseSVT = 1
+};
 
 enum EventModelUsage
-  {kUseStEvent = 0,
-   kUseMuDst   =1
-  };
+{
+  kUseStEvent = 0,
+  kUseMuDst   =1
+};
 ///End Betty
 
 
 enum V0LanguageUsage
-{kV0LanguageUseFortran = 1,
- kV0LanguageUseCpp = 2,
- kV0LanguageUseBoth = 3
- };
+{
+  kV0LanguageUseFortran = 1,
+  kV0LanguageUseCpp = 2,
+  kV0LanguageUseBoth = 3
+};
 
 
 
 
 enum XiLanguageUsage
-{kXiLanguageUseFortran = 1,
- kXiLanguageUseCppOnFortranV0 = 2,
- kXiLanguageUseCppOnCppV0 = 4,
- kXiLanguageUseFortranAndCppOnFortranV0 = 3,
- kXiLanguageUseFortranAndCppOnCppV0 = 5,
- kXiLanguageUseBothCpp = 6,
- kXiLanguageUseAll = 7
- };
+{
+  kXiLanguageUseFortran = 1,
+  kXiLanguageUseCppOnFortranV0 = 2,
+  kXiLanguageUseCppOnCppV0 = 4,
+  kXiLanguageUseFortranAndCppOnFortranV0 = 3,
+  kXiLanguageUseFortranAndCppOnCppV0 = 5,
+  kXiLanguageUseBothCpp = 6,
+  kXiLanguageUseAll = 7
+};
 
 
 
 
 enum LanguageUsage
-{kLanguageUseSpecial = 0,
- kLanguageUseOldRun = 1,
- kLanguageUseRun = 2,
- kLanguageUseTestV0Finder = 5,
- kLanguageUseTestXiFinder = 6,
- kLanguageUseTestBothFinders = 7
- };
+{
+  kLanguageUseSpecial = 0,
+  kLanguageUseOldRun = 1,
+  kLanguageUseRun = 2,
+  kLanguageUseTestV0Finder = 5,
+  kLanguageUseTestXiFinder = 6,
+  kLanguageUseTestBothFinders = 7
+};
 
 
 
 
 enum LikesignUsage
-{kLikesignUseStandard = 0,
- kLikesignUseLikesign = 2
- };
+{
+  kLikesignUseStandard = 0,
+  kLikesignUseLikesign = 2
+};
 
 
 
 
 enum RotatingUsage
-{kRotatingUseStandard = 0,
- kRotatingUseRotating = 1,
- kRotatingUseSymmetry = 2,
- kRotatingUseRotatingAndSymmetry = 3
- };
+{
+  kRotatingUseStandard = 0,
+  kRotatingUseRotating = 1,
+  kRotatingUseSymmetry = 2,
+  kRotatingUseRotatingAndSymmetry = 3
+};
 
 
 
-/*
+/*!
 For the V0Finder : all storages allowed :
    - Fortran V0s
    - C++ V0s
@@ -115,8 +123,7 @@ For the XiFinder : list of the allowed storages :
        - Fortran Xis and C++ Xis made of C++ V0s (kLanguageUseTestBothFinders)
        - C++ Xis made of Fortran V0s and C++ Xis made of C++ V0s
        - Fortran Xis, C++ Xis made of Fortran V0s and C++ Xis made of C++ V0s
-*/
-/*
+
 For the V0Finder :
 For the XiFinder :
  - kLikesignAnalysisUseLikesign : associates Lambda with pi+ and antiLambda with pi- instead of the contrary.
@@ -167,7 +174,7 @@ class StV0FinderMaker : public StMaker {
   //end Betty
   
   virtual const char *GetCVS() const
-    {static const char cvs[]="Tag $Name:  $ $Id: StV0FinderMaker.h,v 1.6 2004/01/27 17:56:06 betya Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+    {static const char cvs[]="Tag $Name:  $ $Id: StV0FinderMaker.h,v 1.7 2004/03/03 01:46:13 cmironov Exp $ built "__DATE__" "__TIME__ ; return cvs;}
   
  protected:
   virtual Int_t Prepare();         //!
@@ -219,9 +226,16 @@ class StV0FinderMaker : public StMaker {
 #endif
 
 //_____________________________________________________________________________
-// $Id: StV0FinderMaker.h,v 1.6 2004/01/27 17:56:06 betya Exp $
+// $Id: StV0FinderMaker.h,v 1.7 2004/03/03 01:46:13 cmironov Exp $
 // $Log: StV0FinderMaker.h,v $
+// Revision 1.7  2004/03/03 01:46:13  cmironov
+// (J.L. commit)
+// Modified doxygen documentation (was not taken before). Indentation
+// for numerous enum{} which would need some //< comments for usage
+// summary ...
+//
 // Revision 1.6  2004/01/27 17:56:06  betya
+//
 // added EventModelUsage so that the V0Finder and XiFinder can no run on
 // MuDst as well as on StEvent.  Note that the output is still in the StEvent
 // format.  Added Clear() in StV0FinderMaker.cxx to accomodate this addition.
