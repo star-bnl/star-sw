1f9628db349d06484d4aa1f2e5be6e38b0b1dd8f
Manuel Calderon de la Barca Sanchez <calderon@physics.ucdavis.edu>
2004-03-18 17:07:22 +0000

    Adding of StiStarVertexFinder as [er Lee.  Changes from previous version are the call to StGenericVertexMaker and obtaining the pointer to the StGenericVertexFinder base class (which inside StGenericVertexFinder is created as an object of type StMinuitVertexFinder).

--- StRoot/Sti/StiStarVertexFinder.h
+++ StRoot/Sti/StiStarVertexFinder.h
@@ -1,9 +1,10 @@
 #ifndef StiStarVertexFinder_H
 #define StiStarVertexFinder_H 1
 #include "Sti/StiVertexFinder.h"
+#include "StMaker.h"
 //#include "StGenericVertexFinderMaker
 
-class StiStarVertexFinder : public StiVertexFinder//, public StGenericVertexFinderMaker
+class StiStarVertexFinder : public StMaker, public StiVertexFinder
 {
  public:
   StiStarVertexFinder(const string & name);
