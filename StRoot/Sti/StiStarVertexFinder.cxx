fe1255056c85a2f25377daadc5bd82395de1cd55
Andrew Allen Rose <aarose@lbl.gov>
2004-03-23 21:59:42 +0000

    Modified logic for case where vertex fit fails. If the StMinuitVertexFinder::fit() returns false, the vertex (a vector of zeros) is no longer added to the event.
    The result is that there is no longer a guarantee that a vertex exists. Traps
    were added to protect against this.

--- StRoot/Sti/StiStarVertexFinder.cxx
+++ StRoot/Sti/StiStarVertexFinder.cxx
@@ -42,19 +42,30 @@ StiHit * StiStarVertexFinder::findVertex(StEvent * event)
   
   StGenericVertexMaker* gvm = (StGenericVertexMaker*)GetMaker("GenericVertex");
   StGenericVertexFinder* gvf = gvm->GetGenericFinder();
-  gvf->fit(event);
-  gvf->FillStEvent(event);
 
+  //AAR - modified
+  // changed to fill primary vert only if fit returns true (okay)
+  if( gvf->fit(event) )
+    {
+      //vertex fit returns okay, so save
+      gvf->FillStEvent(event);
+    }
 
-  const StThreeVectorF& vp = event->primaryVertex()->position();
-  const StThreeVectorF& ve = event->primaryVertex()->positionError();
-  cout <<"StiStarVertexFinder::findVertex(StEvent * event) -I- set hit parameters"<<endl;
-  cout << "x:"<< vp.x() << "+-" << ve.x()<<endl;
-  cout << "y:"<< vp.y() << "+-" << ve.y()<<endl;
-  cout << "z:"<< vp.z() << "+-" << ve.z()<<endl;
-  vertex->set(0, event->primaryVertex(), 
+  if(event->primaryVertex())
+    {
+     const StThreeVectorF& vp = event->primaryVertex()->position();
+     const StThreeVectorF& ve = event->primaryVertex()->positionError();
+     cout <<"StiStarVertexFinder::findVertex(StEvent * event) -I- set hit parameters"<<endl;
+     cout << "x:"<< vp.x() << "+-" << ve.x()<<endl;
+     cout << "y:"<< vp.y() << "+-" << ve.y()<<endl;
+     cout << "z:"<< vp.z() << "+-" << ve.z()<<endl;
+     vertex->set(0, event->primaryVertex(), 
 	      0., 
 	      vp.x(),vp.y(),vp.z(),
 	      ve.x()*ve.x(),0.,0.,ve.y()*ve.y(),0.,ve.z()*ve.z());
-  return vertex;
+     return vertex;
+    }
+  else return 0;
+  
+
 }
