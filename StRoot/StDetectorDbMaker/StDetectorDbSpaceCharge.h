dde91b8aed63135d2d614ed1360f72fa2de96978
Gene Van Buren <gene@bnl.gov>
2004-04-02 17:42:51 +0000

    Use separate tables for uniform and R2

--- StRoot/StDetectorDbMaker/StDetectorDbSpaceCharge.h
+++ StRoot/StDetectorDbMaker/StDetectorDbSpaceCharge.h
@@ -11,11 +11,14 @@ struct spaceChargeCor_st;
 class StDetectorDbSpaceCharge{
 public:
     static StDetectorDbSpaceCharge*  instance();
+    static StDetectorDbSpaceCharge*  instanceR2();
     double                     getSpaceChargeCorrection();
     double                     getSpaceChargeCorrection(double scaleFactor);
     double                     getSpaceChargeSatRate();
     double                     getSpaceChargeCoulombs();
     double                     getSpaceChargeCoulombs(double scaleFactor);
+    float                      getSpaceChargeFactor();
+    float                      getSpaceChargeDetector();
     friend ostream& operator<<(ostream& os, StDetectorDbSpaceCharge& v);
 
     // These fuction will be public
@@ -29,8 +32,20 @@ protected:
     spaceChargeCor_st * mSpaceCharge; // points to clock struct
     TTable* mTable; // points to table, need to re-intilize mStarClockOnl every event
     StMaker * mMaker; // Holds pointer to maker
+    char tableName[32];
 private:
     static StDetectorDbSpaceCharge* sInstance;
+};
+
+
+class StDetectorDbSpaceChargeR2 : public StDetectorDbSpaceCharge{
+public:
+    static StDetectorDbSpaceCharge*  instance();
+protected:
+    virtual ~StDetectorDbSpaceChargeR2();
+    StDetectorDbSpaceChargeR2();
+private:
+    static StDetectorDbSpaceChargeR2* sInstanceR2;
 };
 
 #endif
