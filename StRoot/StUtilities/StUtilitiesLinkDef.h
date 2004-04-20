3d7f36cb60a95133f9dad2bb02d4669a8d8bfee9
perev <>
2004-04-20 20:55:11 +0000

    remove redundant classes from dictionary

--- StRoot/StUtilities/StUtilitiesLinkDef.h
+++ StRoot/StUtilities/StUtilitiesLinkDef.h
@@ -2,8 +2,4 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class StMessTypePair;
-#pragma link C++ class StMessTypeList;
-#pragma link C++ class StMessage;
-#pragma link C++ class StMessageCounter;
 #endif
