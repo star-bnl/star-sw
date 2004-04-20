218c3b76a92745be7ca58bda9afa434d3ebbb4ba
perev <>
2004-04-20 18:49:16 +0000

    Big reorganization, now StMuIOMkaer inherits from StMuDstMaker

--- StRoot/StMuDSTMaker/COMMON/StMuIOMaker.h
+++ StRoot/StMuDSTMaker/COMMON/StMuIOMaker.h
@@ -1,7 +1,10 @@
 /***************************************************************************
  *
- * $Id: StMuIOMaker.h,v 1.8 2004/04/14 17:15:57 subhasis Exp $
+ * $Id: StMuIOMaker.h,v 1.9 2004/04/20 18:49:10 perev Exp $
  * Author: Frank Laue, BNL, laue@bnl.gov
+ *
+ * Made it integrated to StIOMaker for applying Grid Collector 
+ *                                            Wei-Ming Zhang KSU 3/8/2004
  ***************************************************************************/
 #ifndef StMuIOMaker_hh
 #define StMuIOMaker_hh
@@ -12,50 +15,17 @@
 using namespace std;
 #endif
 
-#include "StIOInterFace.h"
-#include "St_DataSetIter.h"
+#include "StMuDstMaker.h"
 
-#include "StMuArrays.h"
-
-class StMuEvent;
 class StMuDst;
-class StMuCut;
-
-class StEvent;
-class StTrackNode;
-class StTrack;
-class StRichSpectra;
-class StDetectorState;
-class StL3AlgorithmInfo;
-
-class StuProbabilityPidAlgorithm;
-
-class StIOMaker;
-class StTreeMaker;
-
-/// strangeness group stuff
-class StStrangeEvMuDst;
-class StStrangeMuDstMaker;
-class StV0MuDst;
-class StV0Mc; 
-class StXiMuDst;
-class StXiMc;
-class StKinkMuDst;
-class StKinkMc;
-class StStrangeAssoc;
-class StStrangeCuts;
-
-/// emc stuff
-#include "StMuEmcCollection.h"
-class StMuEmcUtil;
 
 /// pmd stuff
 //#include "StMuPmdCollection.h"
+class StMuEmcUtil;
 class StMuPmdUtil;
 class StMuTofUtil;
 
 class TFile;
-class TTree;
 class TChain;
 class TClonesArray;
 
@@ -70,103 +40,57 @@ class TClonesArray;
    know what "StMaker" and "StChain" mean.
    
 */
-class StMuIOMaker : public StIOInterFace {
+class StMuIOMaker : public StMuDstMaker {
  public:
     /// Default constructor
-    StMuIOMaker(const char* name="MuDst");
+    StMuIOMaker(const char* name="",const char *ioFile="");
     ~StMuIOMaker();
     
   int Init();
-  void Clear(const char *opt="");
   int Make();
   int Make(int index);
   int Make(int major, int minor);
   int Make(const StUKey &RunEvent);
-
-  int MakeRead() {return Make(); }
-  int MakeRead(const StUKey &RunEvent) { return Make(RunEvent); }
   int Skip(int nskip) { return Make(mCurrentIndex+nskip); }
-
   int Finish();
-  void  SetFileName(const char *fileName);
-  void  SetFile(const char *fileName);
 
-  StMuDst* muDst();                             ///< return pointer the  current (last read) StMuDst
-  StMuEmcUtil* muEmcUtil() { return mEmcUtil; } ///< return pointer to StMuEmcUtil;
-  StMuPmdUtil* muPmdUtil() { return mPmdUtil; } ///< return pointer to StMuPmdUtil;
-  StMuTofUtil* muTofUtil() { return mTofUtil; } ///< return pointer to StMuPmdUtil;
+// virtual methods of inherited StIOInterFace
+  virtual Int_t  Open(const Char_t *ioFile=0);
+  virtual void  Close(Option_t *opt=0);
+
   int currentIndex() { return mCurrentIndex; }
   int eventCounter() { return mEventCounter; }
   int numberOfEvents() { return mNumberOfEvents; }
-  virtual const char *GetCVS() const {  ///< Returns version tag.
 
-    static const char cvs[]="Tag $Name:  $ $Id: StMuIOMaker.h,v 1.8 2004/04/14 17:15:57 subhasis Exp $ built "__DATE__" "__TIME__ ; 
+
+  virtual const char *GetCVS() const {  ///< Returns version tag.
+    static const char cvs[]="Tag $Name:  $ $Id: StMuIOMaker.h,v 1.9 2004/04/20 18:49:10 perev Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
 
-
-private:
-  StMuDst* mStMuDst;
-  StMuEmcUtil* mEmcUtil;
-  StMuPmdUtil* mPmdUtil;
-  StMuTofUtil* mTofUtil;
-
-  TChain* mChain;
-  int mNumberOfEvents; ///< holds the number of events in the current chain (file)
-  int mCurrentIndex;   ///< holds the index of the last event read
-  int mEventCounter;   ///< a running counter holding the total number of events read
-  
-  template<class T> 
-  void saveDelete(T* t) { if (t!=0) delete t; t=0;}    
-
-  void openRead();
-  void setBranchAddresses();
-  void closeRead();
-
-  void setBranchAddresses(TChain*);
-
-  void clear(TClonesArray* t, int& counter);
-  void del(TClonesArray* t, int& counter);
-  void clear();
-
-  void createArrays();
-  TClonesArray* clonesArray(TClonesArray*& p, const char* type, int size, int& counter);
-
-  string basename(string);
-  string dirname(string);
-  string buildFileName(string dir, string fileName, string extention);
- 
-  friend class StMuDst;
-  friend class StMuDstFilterMaker;
-    
-  TClonesArray* arrays[__NARRAYS__];//->
-  TClonesArray* mArrays[__NARRAYS__];//->
-
-  TClonesArray* strangeArrays[__NSTRANGEARRAYS__];//->
-  TClonesArray* mStrangeArrays[__NSTRANGEARRAYS__];//->
-
-  TClonesArray* emcArrays[__NEMCARRAYS__];//->
-  TClonesArray* mEmcArrays[__NEMCARRAYS__];//->
-
-  TClonesArray* pmdArrays[__NPMDARRAYS__];//->
-  TClonesArray* mPmdArrays[__NPMDARRAYS__];//->
-
-  TClonesArray* tofArrays[__NTOFARRAYS__];//->
-  TClonesArray* mTofArrays[__NTOFARRAYS__];//->
+protected:
+  string mOutFileName; //!
+  TFile* mOutFile;     //!
 
 
-  ClassDef(StMuIOMaker, 2)
+  int mNumberOfEvents; //! holds the # of events in the current chain (file)
+  int mCurrentIndex;   //! holds the index of the last event read
+  bool mMuSave;        //!
+  bool mBadInFile;     //!
+
+  int  openRead();
+//@
+
+  ClassDef(StMuIOMaker, 0)
 }; 
-
-inline StMuDst* StMuIOMaker::muDst() { return mStMuDst;}
 
 #endif
 
 /***************************************************************************
  *
  * $Log: StMuIOMaker.h,v $
- * Revision 1.8  2004/04/14 17:15:57  subhasis
- * Xin's TOF reinclusion
+ * Revision 1.9  2004/04/20 18:49:10  perev
+ * Big reorganization, now StMuIOMkaer inherits from StMuDstMaker
  *
  * Revision 1.7  2004/04/09 22:02:50  subhasis
  * after tof createevent fix by Xin
