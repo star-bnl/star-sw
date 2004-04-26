a821e8409c1d97154c4395ee34113a4304ac3748
fisyak <>
2004-04-26 16:33:35 +0000

    Make use of StTpcPixel

--- StRoot/StEvent/StTpcPixel.cxx
+++ StRoot/StEvent/StTpcPixel.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTpcPixel.cxx,v 2.3 2001/04/05 04:00:57 ullrich Exp $
+ * $Id: StTpcPixel.cxx,v 2.4 2004/04/26 16:33:35 fisyak Exp $
  *
  * Author: Thomas Ullrich, July 1999
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StTpcPixel.cxx,v $
+ * Revision 2.4  2004/04/26 16:33:35  fisyak
+ * Make use of StTpcPixel
+ *
  * Revision 2.3  2001/04/05 04:00:57  ullrich
  * Replaced all (U)Long_t by (U)Int_t and all redundant ROOT typedefs.
  *
@@ -22,67 +25,23 @@
  **************************************************************************/
 #include "StTpcPixel.h"
 
-static const char rcsid[] = "$Id: StTpcPixel.cxx,v 2.3 2001/04/05 04:00:57 ullrich Exp $";
+void StTpcPixel::Print(Option_t *option) const {
+      cout << option << "\t" << *this << endl;
+    }
+
+static const char rcsid[] = "$Id: StTpcPixel.cxx,v 2.4 2004/04/26 16:33:35 fisyak Exp $";
+ostream& operator<< (ostream& os, const StTpcPixel& m)
+{
+  os << "d:" << (int) m.detector() 
+     << "/s:" << (int) m.sector() 
+     << "/r:" << (int) m.padrow() 
+     << "/p:" << (int) m.pad() 
+     << "/t:" << (int) m.timebin() 
+     << "/a:" << m.adc() 
+     << "/i:" << m.idTruth() 
+     << endl;
+    return os;
+}
+
 
 ClassImp(StTpcPixel)
-
-StTpcPixel::StTpcPixel()
-{
-    mDetectorSectorRow = 0;
-    mPadTimeAdc = 0;
-}
-
-StTpcPixel::StTpcPixel(unsigned short sr, unsigned int pta)
-   : mDetectorSectorRow(sr), mPadTimeAdc(pta)
-{ /* noop */ }
-
-StTpcPixel::~StTpcPixel() { /* noop */ }
-    
-int
-StTpcPixel::operator==(const StTpcPixel& p) const
-{
-    return p.mDetectorSectorRow  == mDetectorSectorRow &&
-           p.mPadTimeAdc == mPadTimeAdc;
-}
-
-int
-StTpcPixel::operator!=(const StTpcPixel& p) const
-{
-    return !(*this == p);  // use operator==()
-}
-
-unsigned short
-StTpcPixel::detector() const
-{
-    return (mDetectorSectorRow & ~(~0U<<4));      // bits 0-3
-}
-
-unsigned short
-StTpcPixel::sector() const
-{
-    return (mDetectorSectorRow>>4) & ~(~0U<<5);   // bits 4-8
-}
-
-unsigned short
-StTpcPixel::padrow() const
-{
-    return (mDetectorSectorRow>>9) & ~(~0U<<6);   // bits 9-14
-}
-
-unsigned int
-StTpcPixel::pad()  const
-{
-    return (mPadTimeAdc & ~(~0UL<<8));             // bits 0-7
-}
-
-unsigned int
-StTpcPixel::timebin() const
-{
-    return (mPadTimeAdc>>8) & ~(~0UL<<9);          // bits 8-16
-}
-
-unsigned int
-StTpcPixel::adc()  const
-{
-    return (mPadTimeAdc>>17) & ~(~0UL<<10);        // bits 17-26
-}
