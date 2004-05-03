3f58391e48c9758118260a7f63c4761e9f6f2393
fisyak <>
2004-05-03 14:28:56 +0000

    Clean up difition of StTrack::mEncodedMethod / dst_track::method : Fitting method is byte [0:3] + Finding schema is bit pattern [4-15]

--- StRoot/StEvent/StTrackDefinitions.h
+++ StRoot/StEvent/StTrackDefinitions.h
@@ -1,26 +1,15 @@
 #ifndef StTrackDefinitions_hh
 #define StTrackDefinitions_hh
-/* Numbering scheme for dst_track::methods, finder scheme
+/* Numbering scheme for StTrack::mEncodedMethod and dst_track::method
+** includes finder scheme + fitting method
+** 
+** fitting method byte 0-3 (only fit method per track)
+** finder scheme bits (4-14) 
 ** This is a bitmap scheme. Places are reserved for detector trackers.
 ** Total number of bits reserved (including sign bit) is 12-bits for
-** finders (trackers) and 4-bits for fitting (see below). Note that
-** fitting occupies the first 4-LSB in the 'short'. 
-**                    Bit position   
-**   Unknown Method                all bits off      
-**   SVT Grouper           5 (LSB) on/off
-**   SVT Stk               6
-**   SVT Other             7
-**   TPC Standard          8
-**   TPC Other             9
-**   FTPC Conformal        10
-**   FTPC Current          11
-**   SVT-TPC svm           12      vector-vector matcher
-**   SVT-TPC est           13      TPCvector-SVTpoint matcher
-**   SVT-TPC pattern       14      under development
-**   Spare               15-16 (MSB)
-*/
-
-/* Numbering scheme for dst_track::methods, fitter scheme
+** finders (trackers) and first 4-bits for fitting (see below).
+**________________________________________________________
+**   StTrack::fittingMethod    byte (0:3)
 **   Unknown/undefined fitter     0
 **   Helix 2 Step                 1   !bending/non-bending plane fits
 **   Helix 3D                     2
@@ -30,10 +19,25 @@
 **   L3 Fitter                    6
 **   ITTF Kalman Fit              7
 **   Spare                    upto 15 (total of 4 bits)
+**_______________________________________________________
+**   StTrack::finderMethod     bits                                    
+**   SVT Grouper                  4 (LSB) on/off                       
+**   SVT Stk                      5                                    
+**   SVT Other                    6                                    
+**   TPC Standard                 7                                    
+**   TPC Other                    8                                    
+**   FTPC Conformal               9                                    
+**   FTPC Current                10                                    
+**   SVT-TPC svm                 11       vector-vector matcher        
+**   SVT-TPC est                 12       TPCvector-SVTpoint matcher   
+**   SVT-TPC pattern             13      under development             
+**   Spare                    14-15 (MSB)                            
+**_______________________________________________________
+**   StTrack::mEncodedMethod =
+**   dst_track.method = fittig_method (4 bits word, 0:3) + 1<< finding_method [+  1<< finding_method2 ..]
+**  
 */
-
-/* numbering scheme for track fitting types in dst_track.idl*/
-
+/*    Fitting method                 byte[0:3] */
 #define kUndefinedFitterIdentifier      0
 #define kHelix2StepIdentifier           1
 #define kHelix3DIdentifier              2
@@ -42,19 +46,19 @@
 #define kLine3DIdentifier               5
 #define kL3FitIdentifier                6
 #define kITKalmanFitIdentifier          7
-
-/* numbering schem for track finding method has to be in synch. with StEn*/
-#define ksvtGrouperIdentifier      4                           /**< enum value svtGrouper  */
-#define ksvtStkIdentifier          5                           /**< enum value svtStk */
-#define ksvtOtherIdentifier        6                           /**< enum value svtOther */
-#define ktpcStandardIdentifier     7                           /**< enum value tpcStandard */
-#define ktpcOtherIdentifier        8                           /**< enum value tpcOther */
-#define kftpcConformalIdentifier   9                           /**< enum value ftpcConformal */
-#define kftpcCurrentIdentifier    10                           /**< enum value ftpcCurrent */
-#define ksvtTpcSvmIdentifier      11                           /**< enum value svtTpcSvm */
-#define ksvtTpcEstIdentifier      12                           /**< enum value svtTpcEst */
-#define ksvtTpcPatternIdentifier  13
-#define kl3StandardIdentifier     14
+/* numbering for track finder scheme has to be in synch. with StEnumerations.h */
+/* Finder schema                      bit                                      */
+#define ksvtGrouperIdentifier           4 
+#define ksvtStkIdentifier               5
+#define ksvtOtherIdentifier             6
+#define ktpcStandardIdentifier          7
+#define ktpcOtherIdentifier             8
+#define kftpcConformalIdentifier        9
+#define kftpcCurrentIdentifier         10
+#define ksvtTpcSvmIdentifier           11
+#define ksvtTpcEstIdentifier           12
+#define ksvtTpcPatternIdentifier       13
+#define kl3StandardIdentifier          14
 
 
 #endif
