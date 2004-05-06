cef5c5d9f5ece04d6bbc6f91fd29e8cc66b22a05
Piotr A Zolnierczuk <zolnie@iucf.indiana.edu>
2004-05-06 15:42:47 +0000

    *** empty log message ***

--- StRoot/StEEmcPool/TTM/EEmcTower.cxx
+++ StRoot/StEEmcPool/TTM/EEmcTower.cxx
@@ -1,26 +1,29 @@
 // Hey Emacs this is -*-c++-*-
-// $Id: EEmcTower.cxx,v 1.2 2004/05/05 23:00:57 zolnie Exp $
+// $Id: EEmcTower.cxx,v 1.3 2004/05/06 15:42:47 zolnie Exp $
 
 /**
  * \class  EEmcTower
+ * \author Piotr A. Zolnierczuk
+ * \date    created 2003/04/30, last modified $Date: 2004/05/06 15:42:47 $
+ * \version $Revision: 1.3 $
  *
  * \brief EEmcTower holds information about an EEMC tower 'hit'
  *
  *
- * It holds tower description (sector, subsector and eta) plus pedestal adjusted ADC value
- * and optionally calibrated energy loss
+ * The information includes tower description (sector, subsector and eta), 
+ * pedestal adjusted ADC value and (optionally) calibrated energy loss
  *
- * \author Piotr A. Zolnierczuk
- * \date   2004/04/30
-
- * \bug  implicitly assumed that labels are no longer than kMaxLabelLen (16 characters)
-
- * \todo implement EEmcTower::ParseLabel
+ *
+ * \bug  implicitly assumed that labels are no longer than 16 characters
+ * 
+ * \todo 
  */
 
 
 
+#include <iostream>
 #include <ostream>
+#include <cctype>
 
 #include "TList.h"
 #include "EEmcTower.h"
@@ -34,25 +37,51 @@ ClassImp(EEmcTower);
 
 static const int kMaxLabelLen = 16;  /// FIXME - assumed max tower label length
 
-
+//
+EEmcTower::EEmcTower(const char *label , float adc, float ene) 
+{
+ mSec  = -1;
+ mSub  = -1;
+ mEta  = -1;
+ mADC  = 0.0;
+ mEdep = 0.0;
+ mLabel= "";
+ if( ! ParseLabel(label) ) return;
+ mADC  = adc;
+ mEdep = ene;
+ WriteLabel();
+};
+//                                
 
 //=============================================================================
-void 
+bool
 EEmcTower::ParseLabel(const char *label)
 {
-  if(label==NULL) return;
-  //int len = strnlen(label,kMaxLabelLen);
-  // TODO implement ParseLabel 
-  assert(0); // not yet implemented
+  if(label==NULL) return false;
+  char const *p = label;
+  int  s=0;
+  char ss=0;
+  int  e=0;
+  while( *p != 0x00 && !islower(*p) && !isupper(*p) && !isdigit(*p) ) p++;
+  int  r=sscanf(p,"%02dT%1c%02d",&s,&ss,&e);
+  if(r!=3) return false;
+  // adjust indices
+  mSec = s - 1;
+  mSub = ss -1;
+  mEta = e-1;  
+  // format label 
+  return true; 
+  
 }
 
 //=============================================================================
-void
+bool 
 EEmcTower::WriteLabel()
 {
   if(mLabel) delete mLabel;
   mLabel = new char[kMaxLabelLen];
-  sprintf(mLabel,"%02dT%1c%02d",SecLabel(),SubSecLabel(),EtaLabel());
+  int r = sprintf(mLabel,"%02dT%1c%02d",SecLabel(),SubSecLabel(),EtaLabel());
+  return (r==3);
 }
 
 //=============================================================================
@@ -60,9 +89,9 @@ ostream&
 EEmcTower::Out(ostream &out ) const
 {
   out << "<EEmcTower";
-  out << " TOWER=" << mLabel ;
-  out << " ADC="   << mADC ;
-  out << " EDEP="  << mEdep;
+  out << " TOWER=\"" << mLabel << "\"";
+  out << " ADC=\""   << mADC   << "\"";
+  out << " EDEP=\""  << mEdep  << "\"";
   out << " />\n";
   return out;
 }
