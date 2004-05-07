a2688bb1145fc4be6fe907b67fc27700d6363884
balewski <>
2004-05-07 21:38:38 +0000

    gamma finder with SMD

--- StRoot/StEEmcPool/StEEsoloPi0/EEsoloPi0.h
+++ StRoot/StEEmcPool/StEEsoloPi0/EEsoloPi0.h
@@ -3,7 +3,7 @@
 #ifndef EEsoloPi0_h
 #define EEsoloPi0_h
 /*********************************************************************
- * $Id: EEsoloPi0.h,v 1.3 2004/05/05 04:39:45 balewski Exp $
+ * $Id: EEsoloPi0.h,v 1.4 2004/05/07 21:38:38 balewski Exp $
  *********************************************************************
  * Descripion:
  *  finds pi0 based on EEMC tower response
@@ -18,6 +18,8 @@ class TH1F ;
 class EEfeeRawEvent;
 class EEstarTrig;
 class EEmcEventHeader;
+class EEmcDbItem;
+
 #include "StEEmcUtil/EEfeeRaw/EEdims.h"
 
 #ifdef NO_ROOT4STAR
@@ -29,37 +31,38 @@ class EEmcEventHeader;
 #endif
 
 
-
 class EEsoloPi0 :public TObject{
  protected:
   enum {MxTwEta=12, MxTwPhi=60, MxTw=12*60};
 
  private:
   EEmcGeomSimple *geom;
+ protected:
   float seedEnergy; // lower limit in search for seeds
   float shapeLimit; // cut on eHTower/eCluster 
   float mLo, mHi; // define mass of meson
 
  
-  struct EEsoloMipA{int key,id; float e;};
-  struct Cluster {int k1; float eH,eC,fphi,feta; } clust[MxTw] ,oldClust;
-  int nClust; // counts clusters
   int timeSec;// time interval from the first processed event
   int TotN2g; // total # of gam-gam pairs 
   int totPi0; // total # of real pi0
   int totXPi0; // total # of  pi0 from mixed events
   
- protected:
+  int nClust; // counts clusters
+  struct EEsoloMipA{int key,id; float e;};
+  struct Cluster {int k1; float eH,eC,fphi,feta; } clust[MxTw] ,oldClust;
 
   float scaleFactor; // converts energy from eeTree --> GeV, old
 
   TH1F *hA[32], *hR[64], *hM[64]; // all, real , mixed eve
   EEsoloMipA soloMip[MxTw]; // stores all towers
 
-  void doEEsoloPi0();
+  const  EEmcDbItem  *soloMipDb[MxTw]; // stores DB pointers for all towers
+
   void clear();
   void tagCluster(int k0,int d=1);
-  void sumCluster(int ic,int d=1);
+  void sumTwClusterEnergy(int ic,int d=1);
+  float sumPatchEnergy(int k0,int d,EEsoloMipA *soloMipX, float *maxVal=0);
   int findInvM(Cluster *, Cluster *, TH1F **);
   MYDB *db;
  public:
@@ -69,8 +72,10 @@ class EEsoloPi0 :public TObject{
   void print();
   void finish();
 #ifdef NO_ROOT4STAR
-  int sort(EEfeeRawEvent  *feeEve, EEstarTrig *eTrig=0,EEmcEventHeader *eHead=0, int n1=0, int n2=240);
+  int getTowerAdc(EEfeeRawEvent  *feeEve, EEstarTrig *eTrig=0,EEmcEventHeader *eHead=0, int n1=0, int n2=240);
 #endif
+  int findTowerClust();
+  void findTowerPi0();
   void init( MYDB *, TObjArray * L=0);
   void set(float a, float b,  float d, float m1=0.11, float m2=0.16 )
     {scaleFactor =a; seedEnergy=b;  shapeLimit=d;  mLo=m1; mHi=m2; }
@@ -80,8 +85,18 @@ class EEsoloPi0 :public TObject{
 };
 #endif
 
+/* fix in St-code
+1) clear soloMipDb[MxTw] in InitRun
+
+*/
+ 
+
+
 /*****************************************************************
  * $Log: EEsoloPi0.h,v $
+ * Revision 1.4  2004/05/07 21:38:38  balewski
+ * gamma finder with SMD
+ *
  * Revision 1.3  2004/05/05 04:39:45  balewski
  * works with muDst & ezTree
  *
