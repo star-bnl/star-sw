c82199c4ad02ff81c93ac38d7481ee7e4d892a3e
Jan Balewski <balewski@lbl.gov>
2004-06-12 04:09:26 +0000

    start

---
+++ StRoot/StEEmcPool/StEzSmdCal/EzEEsmdCal.cxx
@@ -0,0 +1,179 @@
+// $Id: EzEEsmdCal.cxx,v 1.1 2004/06/12 04:09:23 balewski Exp $
+ 
+#include <assert.h>
+#include <stdlib.h>
+
+
+#include <TClonesArray.h>
+
+#include <TObjArray.h> 
+
+#include "EzEEsmdCal.h"
+
+#include "StEEmcUtil/EEfeeRaw/EEfeeRawEvent.h"
+#include "StEEmcUtil/EEfeeRaw/EEstarTrig.h"
+#include "StEEmcUtil/EEfeeRaw/EEmcEventHeader.h"
+
+#include "StEEmcUtil/EEfeeRaw/EEfeeDataBlock.h"
+#include "StEEmcUtil/EEfeeRaw/EEname2Index.h"
+
+#include "StEEmcDbMaker/EEmcDbItem.h"
+
+
+#ifdef StRootFREE
+  #include "EEmcDb/EEmcDb.h"
+#else
+  #include "StEEmcDbMaker/StEEmcDbMaker.h"
+#endif
+
+
+ClassImp(EzEEsmdCal)
+
+//--------------------------------------------------
+//--------------------------------------------------
+EzEEsmdCal::EzEEsmdCal(int sect){
+  printf("EzEEsmdCal() constructed\n");
+  eHead=0;
+  eEve=0;
+  eTrig=0;
+  setSector(sect);
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EzEEsmdCal::init (){
+  EEsmdCal::init();
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+EzEEsmdCal::~EzEEsmdCal() {/* noop */}
+
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EzEEsmdCal::make(){
+
+  EEsmdCal::clear();
+  unpackEzTree();
+  findSectorMip();// do real analysis
+ 
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EzEEsmdCal::unpackEzTree(){
+  nInpEve++;
+  // printf(" EzEEsmdCal::getData()is called ......\n");
+
+  // check CTB multiplicity 
+  int ctbSum=0;
+  int isl;
+  for ( isl = 0; isl < 240; isl++ ) {
+    ctbSum+= eTrig -> CTB[isl];
+  }
+  //printf("ctbSum=%d \n",ctbSum);
+
+  unpackEzTail();
+  unpackEzSmd();
+  
+  return ;
+
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EzEEsmdCal:: unpackEzTail(){
+  // tower/pre/post
+
+  int nCr=0;
+  int ic;
+  for(ic=0;ic<eEve->block->GetEntries();ic++) {// over dat ablocks
+    EEfeeDataBlock *b=(EEfeeDataBlock *)eEve->block->At(ic);
+    if( !b->isValid() ) continue; // reject corrupted crates corruption
+    
+    int crateID=b->getCrateID();    
+    //    if(crateID>MaxTwCrateID) continue; // just tower crates
+    nCr++; // count number of valid crates
+
+    int chan;
+    UShort_t* data=b->getData();
+    int nd=b->getValidDataLen();
+    
+    for(chan=0;chan<nd;chan++) {
+      const  EEmcDbItem  *x=eeDb->getByCrate(crateID,chan);
+      if(x==0) continue; 
+      if(x->isSMD()) break; // abort pre/post crates
+      // (assuming pre/post/tw is not mixed with SMD pixels) 
+     
+      if(x->fail ) continue; // drop broken channels
+      
+      float rawAdc=data[chan];  
+
+      // accept this hit
+      int iphi=(x->sec-1)*MaxSubSec+(x->sub-'A');
+      int ieta=x->eta-1;
+      assert(iphi>=0 && iphi<MaxPhiBins);
+      assert(ieta>=0 && ieta<MaxEtaBins);
+
+      float adc=rawAdc-x->ped;
+      int iT=-1;
+      if(x->name[2]=='T')
+	iT=0;
+      else
+	iT=x->name[2]-'P'+1;
+      assert(iT>=0 && iT<kTile);
+    
+      tileAdc[iT][ieta][iphi]=adc; // store T,P,Q,R depending on 'iT'
+      tileThr[iT][ieta][iphi]=rawAdc>x->thr;
+      //printf("%s adc=%f out=%d thr=%f\n",x->name,adc,tileThr[iT][ieta][iphi],x->thr);
+    }
+  }
+  
+  return ;
+
+}
+//--------------------------------------------------
+//--------------------------------------------------
+void EzEEsmdCal:: unpackEzSmd(){
+  //  printf("EzEEsmdCal:: unpackEzSmd()\n");
+  int nCr=0;
+  int ic;
+  for(ic=0;ic<eEve->block->GetEntries();ic++) {// over dat ablocks
+    EEfeeDataBlock *b=(EEfeeDataBlock *)eEve->block->At(ic);
+    if( !b->isValid() ) continue; // reject corrupted crates corruption
+    
+    int crateID=b->getCrateID();    
+    if(crateID<=MaxTwCrateID) continue; // drop tower crates
+    nCr++; // count number of valid MAPMT crates
+
+    int chan;
+    UShort_t* data=b->getData();
+    int nd=b->getValidDataLen();
+    
+    for(chan=0;chan<nd;chan++) {
+      const  EEmcDbItem  *x=eeDb->getByCrate(crateID,chan);
+      if(x==0) continue; 
+
+      if(!x->isSMD()) break; // abort pre/post crates
+      // (assuming pre/post is not mixed with SMD pixels) 
+
+      if(x->fail ) continue; // drop broken channels
+      if(x->gain<=0)continue; // drop channels w/o gains
+
+      // tmp mask
+      if(strstr(x->name,"05U035")) continue;
+      if(strstr(x->name,"05V057")) continue;
+      
+      float rawAdc=data[chan];  
+      float adc=rawAdc-x->ped;
+      //     printf(" e=%f ",adc/x->gain); x->print();
+
+      // accept this hit
+      smdE[x->sec-1][x->plane-'U'][x->strip-1]=adc/x->gain;
+    }
+  }
+  
+  return ;
+  
+}
---
+++ StRoot/StEEmcPool/StEzSmdCal/StEEsmdCalMaker.cxx
@@ -0,0 +1,202 @@
+// *-- Author : Jan Balewski
+// 
+// $Id: StEEsmdCalMaker.cxx,v 1.1 2004/06/12 04:09:25 balewski Exp $
+
+#include <TFile.h>
+#include <TH2.h>
+
+#include "StEEsmdCalMaker.h"
+
+#include "StMuDSTMaker/COMMON/StMuEvent.h"
+#include "StMuDSTMaker/COMMON/StMuDst.h"
+#include "StMuDSTMaker/COMMON/StMuDstMaker.h"
+
+#include <StMessMgr.h>
+
+#include "StEEmcDbMaker/EEmcDbItem.h"
+#include "StEEmcDbMaker/StEEmcDbMaker.h"
+
+
+ClassImp(StEEsmdCalMaker)
+
+//________________________________________________
+//________________________________________________
+StEEsmdCalMaker::StEEsmdCalMaker(const char* self ,const char* muDstMakerName) : StMaker(self){
+  mMuDstMaker = (StMuDstMaker*)GetMaker(muDstMakerName);
+  assert(mMuDstMaker);
+  MCflag=0;// default off
+ }
+
+
+//___________________ _____________________________
+//________________________________________________
+StEEsmdCalMaker::~StEEsmdCalMaker(){
+}
+ 
+//________________________________________________
+//________________________________________________
+Int_t StEEsmdCalMaker::Init(){
+  eeDb=(EEDB*)GetMaker("eemcDb");
+  init();
+  printf("%s has MCflag=%d\n",GetName(),MCflag);
+  return StMaker::Init();
+}
+
+//________________________________________________
+//________________________________________________
+Int_t StEEsmdCalMaker::Finish(){
+  finish();
+  return kStOK;
+}
+
+
+//________________________________________________
+//________________________________________________
+Int_t StEEsmdCalMaker::Make(){
+  
+  clear();
+  // ............. acuire EEMC data 
+  if(unpackMuDst()<0)   return kStOK;
+
+  findSectorMip();// do real analysis
+ 
+  return kStOK;
+}
+
+
+//________________________________________________
+//________________________________________________
+Int_t StEEsmdCalMaker::unpackMuDst(){
+  nInpEve++;
+  gMessMgr->Message("","D") <<GetName()<<"::::getAdc() is called "<<endm;
+
+  // Access to muDst .......................
+  StMuEmcCollection* emc = mMuDstMaker->muDst()->emcCollection();
+  if (!emc) {
+    gMessMgr->Message("","W") <<"No EMC data for this event"<<endm;    return kStOK;
+  }
+    
+
+  int i, n1=0,n2=0,n3=0;
+
+  //.........................  T O W E R S .....................
+  for (i=0; i< emc->getNEndcapTowerADC(); i++) {
+    int sec,eta,sub,adc;
+    //muDst  ranges:sec:1-12, sub:1-5, eta:1-12
+    emc->getEndcapTowerADC(i,adc,sec,sub,eta);
+    assert(sec>0 && sec<=MaxSectors);// total corruption of muDst
+
+    //tmp, for fasted analysis use only hits from sectors init in DB
+    if(sec<eeDb->mfirstSecID || sec>eeDb->mlastSecID) continue;
+ 
+    const EEmcDbItem *x=eeDb->getTile(sec,'A'+sub-1,eta,'T');
+    assert(x); // it should never happened for muDst
+ 
+    float value=adc;
+    
+    // M-C & real data needs different handling
+    if(MCflag) {
+      ; // no action
+    } else {
+      if(x->fail ) continue; // drop broken channels
+      value-=x->ped;
+    }
+    
+    int iphi=(x->sec-1)*MaxSubSec+(x->sub-'A');
+    int ieta=x->eta-1;
+    assert(iphi>=0 && iphi<MaxPhiBins);
+    assert(ieta>=0 && ieta<MaxEtaBins);
+    int iT=0;// for towers
+
+    tileAdc[iT][ieta][iphi]=value;// store towers
+    n1++;
+
+    //if(value>0)  printf(" %d %d %f\n",ieta,iphi,value);    
+  }
+
+
+  //.........................  P R E - P O S T .....................  
+  int pNh= emc->getNEndcapPrsHits();
+  for (i=0; i<pNh; i++) {
+    int pre;
+    int sec,eta,sub;
+    //muDst  ranges: sec:1-12, sub:1-5, eta:1-12 ,pre:1-3==>pre1/pre2/post
+    StMuEmcHit *hit=emc->getEndcapPrsHit(i,sec,sub,eta,pre);
+
+    //tmp, for fasted analysis use only hits from sectors init in DB
+    if(sec<eeDb->mfirstSecID || sec>eeDb->mlastSecID) continue;
+ 
+    
+    //Db ranges: sec=1-12,sub=A-E,eta=1-12,type=T,P-R ; slow method
+    const EEmcDbItem *x=eeDb-> getTile(sec,sub-1+'A', eta, pre-1+'P'); 
+    if(x==0) continue;
+    
+    // accept this hit
+    int iphi=(x->sec-1)*MaxSubSec+(x->sub-'A');
+    int ieta=x->eta-1;
+    assert(iphi>=0 && iphi<MaxPhiBins);
+    assert(ieta>=0 && ieta<MaxEtaBins);
+    
+    float value=hit->getAdc();
+    
+    if(MCflag) {
+      value/=1.;// to ~match M-C with with slopeGains1 for data 
+    }else {
+      if(x->fail ) continue; // drop broken channels
+      value-=x->ped;
+    }
+    
+    int iT=pre; // P,Q,R fall in to iT=1,2,3 <== there is no '+1' error, JB
+    assert(iT>0 && iT<kTile);
+    tileAdc[iT][ieta][iphi]=value; // store P,Q,R depending on 'iT'
+    n2++;
+  }
+  
+  
+  //.......................  S M D ................................
+  
+  char uv='U';
+  for(uv='U'; uv<='V'; uv++) {
+    int sec,strip;
+    int nh= emc->getNEndcapSmdHits(uv);
+    //printf("pl=%c nH=%d  secLim=%d %d\n",uv,nh,eeDb->mfirstSecID,eeDb->mlastSecID);
+    for (i=0; i<nh; i++) {
+      StMuEmcHit *hit=emc->getEndcapSmdHit(uv,i,sec,strip);
+      assert(sec>0 && sec<=MaxSectors);// total corruption of muDst
+      
+      //tmp, for fasted analysis use only hits from sectors init in DB
+      if(sec<eeDb->mfirstSecID || sec>eeDb->mlastSecID) continue;
+      
+      const EEmcDbItem *x=eeDb->getByStrip(sec,uv,strip);
+      assert(x); // it should never happened for muDst
+      // x->print();
+      float value=hit->getAdc();
+      
+      // M-C & real data needs different handling
+      if(MCflag) {
+	value/=70;// to ~match M-C with with slopeGains1 for data 
+      }else {
+	if(x->fail ) continue; // drop broken channels
+	if(x->gain<=0) continue;
+	value-=x->ped;
+	value/=x->gain;
+      }
+      //  printf("SMD hit sec=%d plane=%c strip=%d value=%f\n",x->sec,x->plane,x->strip,value);
+      smdE[x->sec-1][x->plane-'U'][x->strip-1]=value;
+      n3++;
+    }
+  }
+  
+  //  printf("nTw=%d nPQR=%d,nSmd=%d\n",n1,n2,n3);
+  
+  return n1;
+}
+
+
+// $Log: StEEsmdCalMaker.cxx,v $
+// Revision 1.1  2004/06/12 04:09:25  balewski
+// start
+//
+// Revision 1.1  2004/06/06 04:54:08  balewski
+// dual analyzis
+//
