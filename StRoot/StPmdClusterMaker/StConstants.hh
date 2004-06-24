ecff30441448728d1e8ed291d080822faa4f025b
subhasis <>
2004-06-24 13:50:14 +0000

    Global constants used in StPmdClustering is put in this .h file

---
+++ StRoot/StPmdClusterMaker/StConstants.hh
@@ -0,0 +1,9 @@
+/*For defining constants used in StPmdClustering.cxx*/
+#ifndef StConstants_hh
+#define StConstants_hh
+
+#define nmx      6912
+#define pi       3.141593
+#define sqrth    1.7320508/2.
+
+#endif
