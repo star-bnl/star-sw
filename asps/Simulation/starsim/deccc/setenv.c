a0f5d816df861eaf2c2b5ff1c7a48f8c49798147
Maxim Potekhin <potekhin@bnl.gov>
2004-06-26 00:19:00 +0000

    Removed an unused variable

--- asps/Simulation/starsim/deccc/setenv.c
+++ asps/Simulation/starsim/deccc/setenv.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 
 void      setenv_(const char *name, const char *value, int ln, int lv)
-{ int i = setenv (name,value, 1); }
+{         setenv (name,value, 1); }
 
 void      unsetenv_(const char *name, int ln)
 {         unsetenv (name);        }
