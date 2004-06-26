0a24b909e44fdb93650a80999777ef0e03817d45
Maxim Potekhin <potekhin@bnl.gov>
2004-06-26 00:14:39 +0000

    Second attempt to reformat the CVS comments on top and prevent it from flipping over

--- asps/Simulation/starsim/deccc/avost.c
+++ asps/Simulation/starsim/deccc/avost.c
@@ -1,9 +1,12 @@
-/* $Id: avost.c,v 1.2 2004/06/26 00:12:16 potekhin Exp $ */
-/* $Log: avost.c,v $
- * Revision 1.2  2004/06/26 00:12:16  potekhin
- * Removed extraneous comment brackets which were confusing the compiler a little
- * */
-/* Revision 1.1.1.1  2004/01/12 23:49:39  potekhin */
+/*
+ * $Id: avost.c,v 1.3 2004/06/26 00:14:39 potekhin Exp $ 
+ * $Log: avost.c,v $
+ * Revision 1.3  2004/06/26 00:14:39  potekhin
+ * Second attempt to reformat the CVS comments on top and
+ * prevent it from flipping over
+ *
+ */
+
 
 void avost_() { int *p=0; int q=*p; }
 
