380d3b432dd3b74ac91bec0190e87799d9c7669b
Yuri Fisyak <fisyak@bnl.gov>
2004-07-01 13:38:23 +0000

    Restore current (SL04g) version

--- StRoot/StdEdxY2Maker/StdEdxY2Maker.h
+++ StRoot/StdEdxY2Maker/StdEdxY2Maker.h
@@ -1,4 +1,4 @@
-// $Id: StdEdxY2Maker.h,v 1.11 2004/07/01 13:36:15 fisyak Exp $
+// $Id: StdEdxY2Maker.h,v 1.12 2004/07/01 13:38:23 fisyak Exp $
 #ifndef STAR_StdEdxY2Maker
 #define STAR_StdEdxY2Maker
 
@@ -12,71 +12,72 @@
 #ifndef StMaker_H
 #include "StMaker.h"
 #endif
-#include "dEdxPoint.h"
+#include "StTpcDb/StTpcdEdxCorrection.h" 
 #include "StThreeVectorF.hh"
-class St_fee_vs_pad_row;
-class St_tpcCorrection;
-class tpcCorrection_st;
-class StThreeVectorD;
-class St_TpcSecRowCor;
-class St_tpcGas;
-class St_tpcPressure;
-class St_TpcSecRowA;
+#include "StThreeVectorD.hh" 
 class Bichsel;
 class StGlobalTrack;
-class St_trigDetSums;
-class trigDetSums_st;
-class St_tpcGainMonitor;
 class TMinuit; 
+class StEvent;
+class StGlobalCoordinate;
+class TH2D;
+class St_trigDetSums;
+class trigDetSums;
 class StdEdxY2Maker : public StMaker {
- private:
-  Bichsel             *m_Bichsel;       //!
-  St_TpcSecRowCor     *m_TpcSecRow;     //!
-  St_tpcCorrection    *m_drift;         //!
-  St_tpcCorrection    *m_Multiplicity;  //!
-  St_tpcCorrection    *m_AdcCorrection; //!
-  St_tpcCorrection    *m_zCorrection;   //!
-  St_tpcCorrection    *m_dXCorrection;  //!
-  St_tpcCorrection    *m_TpcdEdxCor;    //!
-  St_tpcCorrection    *m_TpcLengthCorrection;  //!
-  St_tpcGas           *m_tpcGas;        //!
-  St_tpcCorrection    *m_tpcPressure;   //!
-  St_TpcSecRowA       *m_TpcSecRowA;    //!
-  St_trigDetSums      *m_trigDetSums;   //!
-  trigDetSums_st      *m_trig;          //!
-  Bool_t               m_Simulation;    //!
-  Bool_t               m_InitDone;      //!
-  StThreeVectorD      *mNormal[24];     //!
-  StThreeVectorD      *mRowPosition[24][45][3]; //!
-  St_tpcGainMonitor   *m_tpcGainMonitor;//!
-  Int_t                m_OldClusterFinder; //! old == 1; new == 0
-  Int_t                m_Calibration;   //!
-  Int_t                m_DoNotCorrectdEdx; //!
-  TMinuit             *m_Minuit;        //!
-
  public: 
+  enum  EMode {kOldClusterFinder       ,
+	       kCalibration            ,
+	       kDoNotCorrectdEdx       ,
+	       kPadSelection           ,
+	       kMip                    ,
+	       kAdcHistos              ,
+	       kXYZcheck               ,
+	       kSpaceChargeStudy       ,
+	       kGASHISTOGRAMS          ,
+	       kProbabilityPlot        ,
+	       kMakeTree               ,
+	       kCORRELATION            ,
+	       kAlignment              , 
+	       kZBGX };
   StdEdxY2Maker(const char *name="dEdxY2");
-  virtual       ~StdEdxY2Maker();
+  virtual       ~StdEdxY2Maker() {}
   virtual Int_t Init();
   virtual Int_t InitRun(Int_t RunNumber);
   virtual Int_t Finish();
   virtual Int_t FinishRun(Int_t OldRunNumber);
   virtual Int_t Make();
-  void    SortdEdx(Int_t N, dEdx_t *dE, dEdx_t *dES);
+  virtual void  SetMask(Int_t mask) {m_Mask = mask;}
+  static  Int_t SortdEdx(Int_t N, dEdx_t *dE, dEdx_t *dES);
   Double_t LikeliHood(Double_t Xlog10bg, Int_t NdEdx, dEdx_t *dEdx);
-  Double_t CalcCorrection(const tpcCorrection_st *cor,const Double_t x);
-  void    Histogramming(StGlobalTrack* gTrack);
+  void    Histogramming(StGlobalTrack* gTrack=0);
+  void    TrigHistos(Int_t iok = 0);
+  void    SpaceCharge(Int_t iok = 0, StEvent * pEvent=0, StGlobalCoordinate *global=0, dEdx_t *CdEdx=0);
+  void    XyzCheck(StGlobalCoordinate *global=0, Int_t iokCheck=0);
   void    QAPlots(StGlobalTrack* gTrack = 0);
-  void    BadHit(Int_t iFlag, const StThreeVectorF &xyz);
+  void    BadHit(const StThreeVectorF &xyz);
   void    DoFitZ(Double_t &chisq, Double_t &fitZ, Double_t &fitdZ);
   void    PrintdEdx(Int_t iop = 0);
   static  void Landau(Double_t x, Double_t *val);
   static  void fcn(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag);
   virtual const char *GetCVS() const {
     static const char cvs[]=
-      "Tag $Name:  $ $Id: StdEdxY2Maker.h,v 1.11 2004/07/01 13:36:15 fisyak Exp $ built "__DATE__" "__TIME__ ; 
+      "Tag $Name:  $ $Id: StdEdxY2Maker.h,v 1.12 2004/07/01 13:38:23 fisyak Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
+ private:
+  Bichsel             *m_Bichsel;       //!
+  TMinuit             *m_Minuit;        //!
+  StTpcdEdxCorrection *m_TpcdEdxCorrection; // !
+  Int_t                m_Mask; //!
+  static Int_t  NdEdx;
+  static dEdx_t *CdEdx; // corrected
+  static dEdx_t *FdEdx; // fit
+  static dEdx_t *dEdxS; // dEdx sorted
+  StThreeVectorD      *mNormal[24][45];     //!
+  StThreeVectorD      *mRowPosition[24][45][3]; //!
+  St_trigDetSums      *m_trigDetSums;//!
+  trigDetSums_st      *m_trig;//!
+  TH2D                *mHitsUsage;//!
   
   ClassDef(StdEdxY2Maker,0)   //StAF chain virtual base class for Makers
 };
