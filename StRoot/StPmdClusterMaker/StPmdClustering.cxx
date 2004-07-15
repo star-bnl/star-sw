b86feec51d6d8497e46c224140a160f49c47cc18
subhasis <>
2004-07-15 13:32:41 +0000

    clust dimension changed

--- StRoot/StPmdClusterMaker/StPmdClustering.cxx
+++ StRoot/StPmdClusterMaker/StPmdClustering.cxx
@@ -1,6 +1,6 @@
 /***********************************************************
  *
- * $Id: StPmdClustering.cxx,v 1.13 2004/06/29 07:35:25 subhasis Exp $
+ * $Id: StPmdClustering.cxx,v 1.14 2004/07/15 13:32:41 subhasis Exp $
  *
  * Author: based on original routine written by S. C. Phatak.
  *
@@ -17,6 +17,9 @@
  * 'CentroidCal()' has been put in place of 'gaussfit()'.
  **
  * $Log: StPmdClustering.cxx,v $
+ * Revision 1.14  2004/07/15 13:32:41  subhasis
+ * clust dimension changed
+ *
  * Revision 1.13  2004/06/29 07:35:25  subhasis
  * const.h dropped
  *
@@ -548,7 +551,7 @@ void StPmdClustering::CentroidCal(Int_t ncell,Int_t nclust,Double_t &x,
   Double_t xxc[2000],yyc[2000];
   Double_t zzct[2000],cellsc[2000];
   Double_t str[2000],str1[2000], cln[2000];
-  Double_t xcl[2000], ycl[2000],clust_cell[500][2000];
+  Double_t xcl[2000], ycl[2000],clust_cell[200][2000];
   //Initialisation part starts
   for(i=0;i<=nclust;i++)
     {
@@ -563,7 +566,7 @@ void StPmdClustering::CentroidCal(Int_t ncell,Int_t nclust,Double_t &x,
       yy[i] = *(&y+i);
       zz[i] = *(&z+i);
     }
-  for(i=0;i<500;i++)
+  for(i=0;i<200;i++)
     {
     for(j=0;j<2000;j++)
       {
