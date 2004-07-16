b54d00f55353eb7b266acca1f5347f80c71dd825
Maxim Potekhin <potekhin@bnl.gov>
2004-07-16 22:55:36 +0000

    Geo data

---
+++ StarVMC/ecal/src/CALG.cxx
@@ -0,0 +1,5 @@
+#include "CALG.h"
+
+const Float_t CALG::rmin       = 223.50;   // inner radius
+const Float_t CALG::rmax       = 243.50;   // outer radius
+const Float_t CALG::length     = 459.37;   // ECAL full length
