4c80f1db62588688932c45993210eae4a556adce
Maxim Potekhin <potekhin@bnl.gov>
2004-07-16 22:56:49 +0000

    Initial

---
+++ StarVMC/ecal/include/CALG.h
@@ -0,0 +1,9 @@
+#include <Rtypes.h>
+
+class  CALG {             //  TPC basic dimensions
+ public:
+
+  static const Float_t rmin      ;    // inner radius 
+  static const Float_t rmax      ;    // outer radius 
+  static const Float_t length    ;    // ECAL full length
+};
