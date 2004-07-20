1bc92c71db6d3ef0fb0676858b24c57dfc6f14c1
Yuri Fisyak <fisyak@bnl.gov>
2004-07-20 20:56:52 +0000

    no dX correction

---
+++ StarDb/Calibrations/tpc/TpcdXCorrection.20031120.000000.C
@@ -0,0 +1,36 @@
+TDataSet *CreateTable() { 
+  if (!gROOT->GetClass("St_tpcCorrection")) return 0;
+  Int_t nrows   = 2;
+  tpcCorrection_st row;
+  St_tpcCorrection *tableSet = new St_tpcCorrection("TpcdXCorrection",nrows);
+  memset(&row,0,tableSet->GetRowSize()); 
+#if 0
+  row.nrows      = nrows;       // Outer dXdECGFHist842P04ifAuAu200
+  row.idx      = 1;
+  row.npar       = 4;
+  row.min        = 0.95;
+  row.max        = 2.20;
+  row.a[0]	 = -4.61654e-01;//-4.61302e-01;// parameterization vs dX log2(dX)
+  row.a[1]	 =  8.97964e-01;// 
+  row.a[2]	 = -5.62668e-01;// 
+  row.a[3]	 =  1.12212e-01;// 
+#endif
+  tableSet->AddAt(&row);
+  memset(&row,0,tableSet->GetRowSize()); 
+#if 0
+  row.nrows      = nrows;       // Outer dXdECGFHist842P04ifAuAu200
+  row.idx      = 2;
+  row.npar       = 6;
+  row.min        = 0.2;
+  row.max        = 1.4;
+  row.a[0]	 = -1.55533e-01;// -1.65436e-01;//// parameterization vs dX log2(dX)
+  row.a[1]	 =  1.29436e+00;// 
+  row.a[2]	 = -3.62222e+00;// 
+  row.a[3]	 =  3.98336e+00;// 
+  row.a[4]	 = -1.44925e+00;// 
+  row.a[5]	 =  9.42268e-03;// 
+#endif
+  tableSet->AddAt(&row);
+  // ----------------- end of code ---------------
+  return (TDataSet *)tableSet;
+}
