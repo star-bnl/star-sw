d998d20db40225a30d7026a7a455ae1da81ef4be
fine <>
2004-07-21 00:56:17 +0000

    Chnage the abstract custom filter interface to allow use the custom visual attributes

--- StRoot/StEventUtilities/examples/StCustomFilter.cxx
+++ StRoot/StEventUtilities/examples/StCustomFilter.cxx
@@ -68,8 +68,9 @@ const float  *StCustomFilter::GetDefs() const
   };
   return defs;
 }
+
 //______________________________________________________________________________
-Int_t StCustomFilter::Accept(StPoints3DABC *pnt) 
+Int_t StCustomFilter::Accept(StPoints3DABC *pnt, Color_t&color, Size_t&size, Style_t&style)
 {
    // ---
    // "Accept" method is called by the  StEventDisplayMaker event loop 
@@ -79,8 +80,22 @@ Int_t StCustomFilter::Accept(StPoints3DABC *pnt)
    //   StRoot/StEventUtilities/StEventHelper.cxx:StFilterDef::Accept()
    // method
    //
+   // Input:   pnt - the inetrface to the object 3D coordinate
+   //         color - the default color
+   //         size  - the default size  (it if the line width for trach and the dot diameter for the hits)
+   //         style - the default style (it is the line style or ROOT market style)
+   //
+   // Output  color - the custom color if any
+   //         size  - the custom size  if any
+   //         style - the custom style if any
+   //
    // return :    0 the object is rejected and it will not be drawn
    //            +n the object is accepted and will be passed to next filter if any
+   //                optionally you may change the object visual attribute 
+   //                like color, style and size
+   //             If you have no intention to change the default values 
+   //             you may simply ignore the parameters. 
+   //             The attributes set by the filter do overwrite what other filter may have set
    //
    //             If the object has passed the last filter in row it will be drawn.
    // ---
@@ -93,20 +108,21 @@ Int_t StCustomFilter::Accept(StPoints3DABC *pnt)
 
    // The track component of StEvent was found let's apply our custom cut.
    trk = (StTrack*)to;
-   return Accept(trk);
+   return Accept(trk,color,size,style);
 }
 //______________________________________________________________________________
-Int_t StCustomFilter::Accept(const StTrack* track) 
+Int_t StCustomFilter::Accept(const StTrack* track,Color_t&color, Size_t&size, Style_t&style) 
 {
    //
-   // User provided the selection.
+   // User provided the selection and cutom visual attribute fot the track
    // It is assumed this function needs the value assigned through interactive dialog
    //
-   // This conrete cut implementation was borrowed from StRoot/StMuDSTMaker/COMMON/StMStMuL3Filter
+   // If you don't want to change the default visual attribute you may simply ignore the input parameters
+   // This concrete cut implementation was borrowed from StRoot/StMuDSTMaker/COMMON/StMStMuL3Filter
    //-----
-   
-   
-   
+
+
+
    //-----
    // Note: The statements below make a copy of the object data-mambers to the local variables
    //-----
@@ -114,61 +130,61 @@ Int_t StCustomFilter::Accept(const StTrack* track)
    //    We do that to highlight the difference between the original "StMStMuL3Filter"
    //-----
 
-     float pCutHigh        = fpCutHigh;               // high momentum cut for RICH/Upsilon candidates 
-     int   nHitsCutHighP   = int(fnHitsCutHighP);     // nHits cut for all tracks
+   float pCutHigh        = fpCutHigh;               // high momentum cut for RICH/Upsilon candidates 
+   int   nHitsCutHighP   = int(fnHitsCutHighP);     // nHits cut for all tracks
 
-     // following cuts apply only for tracks with pCutLow < p <pHigh
-     float pCutLow             = fpCutLow;            // low momentum cut
-     int   nHitsCutLowP        = int(fnHitsCutLowP);    
-     int   chargeForLowP       = int(fchargeForLowP); // charge for tracks with pCutLow < p < fpCutHigh, set to 0 for all tracks
-     float dEdxMassCutHigh     = fdEdxMassCutHigh;    // cut below BetheBloch(p/dEdxMassCutHigh), e.g. proton-band
-     float dEdxFractionCutHigh = fdEdxFractionCutHigh;// cut fraction of dEdx-band, i.e. dEdxFractionCut * BetheBloch(p/dEdxMassCut)
-     float dEdxMassCutLow      = fdEdxMassCutLow;     // cut above BetheBloch(p/dEdxMassCutLow), e.g. kaon-band
-     float dEdxFractionCutLow  = fdEdxFractionCutLow;
+   // following cuts apply only for tracks with pCutLow < p <pHigh
+   float pCutLow             = fpCutLow;            // low momentum cut
+   int   nHitsCutLowP        = int(fnHitsCutLowP);    
+   int   chargeForLowP       = int(fchargeForLowP); // charge for tracks with pCutLow < p < fpCutHigh, set to 0 for all tracks
+   float dEdxMassCutHigh     = fdEdxMassCutHigh;    // cut below BetheBloch(p/dEdxMassCutHigh), e.g. proton-band
+   float dEdxFractionCutHigh = fdEdxFractionCutHigh;// cut fraction of dEdx-band, i.e. dEdxFractionCut * BetheBloch(p/dEdxMassCut)
+   float dEdxMassCutLow      = fdEdxMassCutLow;     // cut above BetheBloch(p/dEdxMassCutLow), e.g. kaon-band
+   float dEdxFractionCutLow  = fdEdxFractionCutLow;
    // --  The copy of the all data-members have been made
 
-  int iret = 0;
-  int chargeOK = 0;
-  int dedxOK = 0;
+   int iret = 0;
+   int chargeOK = 0;
+   int dedxOK = 0;
 
-  float magnitude = track->geometry()->momentum().magnitude();
-  int   nPoints   = track->detectorInfo()->numberOfPoints();
+   float magnitude = track->geometry()->momentum().magnitude();
+   int   nPoints   = track->detectorInfo()->numberOfPoints();
 
-  if  (   magnitude > pCutHigh && nPoints >= nHitsCutHighP)   iret = 1;
-  else {
-     if ( magnitude > pCutLow  && nPoints >= nHitsCutLowP ) 
-     {
-        // check charge
-        if (chargeForLowP==0) 
-           chargeOK = 1;
-        else if (track->geometry()->charge() == chargeForLowP) 
-           chargeOK = 1;
+   if  (   magnitude > pCutHigh && nPoints >= nHitsCutHighP)   iret = 1;
+   else {
+      if ( magnitude > pCutLow  && nPoints >= nHitsCutLowP ) 
+      {
+         // check charge
+         if (chargeForLowP==0) 
+            chargeOK = 1;
+         else if (track->geometry()->charge() == chargeForLowP) 
+            chargeOK = 1;
 
-        // check dEdx
-        //	      if (mBB==0) mBB = new BetheBloch();
-        float dedxHigh = dEdxFractionCutHigh * mBB->Sirrf(magnitude/dEdxMassCutHigh);
-        float dedxLow  = dEdxFractionCutLow  * mBB->Sirrf(magnitude/dEdxMassCutLow);
-        float dedx     = 0;
+         // check dEdx
+         //	      if (mBB==0) mBB = new BetheBloch();
+         float dedxHigh = dEdxFractionCutHigh * mBB->Sirrf(magnitude/dEdxMassCutHigh);
+         float dedxLow  = dEdxFractionCutLow  * mBB->Sirrf(magnitude/dEdxMassCutLow);
+         float dedx     = 0;
 
-        // get track dEdx
-        const StSPtrVecTrackPidTraits& traits = track->pidTraits();
-        StDedxPidTraits* dedxPidTr;
-        for (unsigned int itrait = 0; itrait < traits.size(); itrait++){
-           dedxPidTr = 0;
-           if (traits[itrait]->detector() == kTpcId) {
-              StTrackPidTraits* thisTrait = traits[itrait];
-              dedxPidTr = dynamic_cast<StDedxPidTraits*>(thisTrait);
-              if (dedxPidTr && dedxPidTr->method() == kTruncatedMeanId) {
-                 // adjust L3 dE/dx by a factor of 2 to match offline
-                 dedx = 2 * dedxPidTr->mean();
-              }
-           }
-        }
-        if (dedx > dedxHigh && dedx > dedxLow) 
-           dedxOK = 1;
-        // final answer
-        iret = chargeOK * dedxOK;
-     } // if (pCutLow && nHitsCutLowP)
-  }
-  return iret;
+         // get track dEdx
+         const StSPtrVecTrackPidTraits& traits = track->pidTraits();
+         StDedxPidTraits* dedxPidTr;
+         for (unsigned int itrait = 0; itrait < traits.size(); itrait++){
+            dedxPidTr = 0;
+            if (traits[itrait]->detector() == kTpcId) {
+               StTrackPidTraits* thisTrait = traits[itrait];
+               dedxPidTr = dynamic_cast<StDedxPidTraits*>(thisTrait);
+               if (dedxPidTr && dedxPidTr->method() == kTruncatedMeanId) {
+                  // adjust L3 dE/dx by a factor of 2 to match offline
+                  dedx = 2 * dedxPidTr->mean();
+               }
+            }
+         }
+         if (dedx > dedxHigh && dedx > dedxLow) 
+            dedxOK = 1;
+         // final answer
+         iret = chargeOK * dedxOK;
+      } // if (pCutLow && nHitsCutLowP)
+   }
+   return iret;
 }
