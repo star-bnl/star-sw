7ef23f547c5f79bf424d73b74550f90d32a7ffb8
Yuri Fisyak <fisyak@bnl.gov>
2004-07-23 17:41:45 +0000

    Rename tables

--- StarDb/Calibrations/tpc/TpcLengthCorrectionB.C
+++ StarDb/Calibrations/tpc/TpcLengthCorrectionB.C
@@ -1,8 +1,11 @@
 TDataSet *CreateTable() { 
   if (!gROOT->GetClass("St_tpcCorrection")) return 0;
   tpcCorrection_st row;
-  St_tpcCorrection *tableSet = new St_tpcCorrection("TpcdXCorrection",7);
-  memset(&row,0,tableSet->GetRowSize()); 
+  St_tpcCorrection *tableSet = new St_tpcCorrection("TpcLengthCorrectionB",10);
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);
+  tableSet->AddAt(&row);
+  tableSet->AddAt(&row);
   tableSet->AddAt(&row);
   tableSet->AddAt(&row);
   tableSet->AddAt(&row);
