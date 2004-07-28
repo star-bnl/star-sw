7842b49a49d3c1d161b07fb3561f77cb36cd289a
Lee Barnby <l.s.barnby@bnl.gov>
2004-07-28 07:47:45 +0000

    Corrected typo which prevented parsing of comments: '/ *' -> '/*'

--- StRoot/StStrangeTagsMaker/StStrangeTagsMaker.h
+++ StRoot/StStrangeTagsMaker/StStrangeTagsMaker.h
@@ -60,7 +60,7 @@
  *
  *    float range;           // range used in defining mass windows
  *
- * $Id: StStrangeTagsMaker.h,v 1.9 2004/07/27 14:29:09 lbarnby Exp $
+ * $Id: StStrangeTagsMaker.h,v 1.10 2004/07/28 07:47:45 lbarnby Exp $
  *
  */
 
@@ -100,16 +100,19 @@ private:
     Float_t         mMasska2;
     
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StStrangeTagsMaker.h,v 1.9 2004/07/27 14:29:09 lbarnby Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StStrangeTagsMaker.h,v 1.10 2004/07/28 07:47:45 lbarnby Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
     ClassDef(StStrangeTagsMaker,0)   // macro for rootcint
 };
 
 #endif
 
-/ ***************************************************************************
+/***************************************************************************
  *
  * $Log: StStrangeTagsMaker.h,v $
+ * Revision 1.10  2004/07/28 07:47:45  lbarnby
+ * Corrected typo which prevented parsing of comments: '/ *' -> '/*'
+ *
  * Revision 1.9  2004/07/27 14:29:09  lbarnby
  * Updated documentation for doxygen
  *
