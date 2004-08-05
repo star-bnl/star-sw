5d02160ed7e7ce3826d1363c14f839557b767861
Claude Andre Pruneau <pruneau@physics.wayne.edu>
2004-08-05 14:46:31 +0000

    Added "clear" virtual method to the class. Added documentation for the class and for the various methods.

--- StRoot/Sti/Base/Factory.h
+++ StRoot/Sti/Base/Factory.h
@@ -8,30 +8,33 @@
   <p>
   This class defines the concept of factory, an agent responsible for the
   creation or instantiation of a given type of class. The class is templated.
-  The template represents the class to be intanstiated and served by the
-  factory.
+  The template represents the base class to be intanstiated and served by the
+  factory. Implementation (derived class) may serve objects from class derived
+  based on the "Factorized" template class.
 */
 template<class Factorized>
 class Factory : public Named
 {
  public:
   
-  Factory(const string& name);
-  virtual ~Factory();
-  
+  Factory(const string& name)
+  : Named(name)
+    {}
+
+  virtual ~Factory()
+    {}
+
+  ///Initialize this factory
   virtual void initialize()=0;
+
+  ///Reset this factory
   virtual void reset()=0;
+
+  ///Clear/delete all objects owned by this factory
   virtual void clear()=0;
+
+  ///Get a pointer to instance of objects served by this factory.
   virtual Factorized * getInstance()=0;
 };
 
-template<class Factorized>
-Factory<Factorized>::Factory(const string& name)
-  : Named(name)
-{}
-
-template<class Factorized>
-Factory<Factorized>::~Factory()
-{}
-  
 #endif
