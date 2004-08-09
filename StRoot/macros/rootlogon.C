76d7ff8f90ed7ea1e064de2b01f1c92b9723493f
Yuri Fisyak <fisyak@bnl.gov>
2004-08-09 00:45:27 +0000

    Load libTable and legacy St_* class dictionary only for root4star

--- StRoot/macros/rootlogon.C
+++ StRoot/macros/rootlogon.C
@@ -1,4 +1,4 @@
-// $Id: rootlogon.C,v 1.42 2004/08/08 21:41:54 fisyak Exp $
+// $Id: rootlogon.C,v 1.43 2004/08/09 00:45:27 fisyak Exp $
 //
 //=======================================================================
 // owner:  Yuri Fisyak
@@ -33,29 +33,26 @@ namespace rootlogon {
   gSystem->Load("StarRoot");
   if (gPrompt.Index("root4star")>=0 && !strstr(gSystem->GetLibraries(),"libTable")) {
     gSystem->Load("libGeom"); gSystem->Load("libTable");
+    gInterpreter->ProcessLine("typedef TCL              StCL;");              
+    gInterpreter->ProcessLine("typedef TDataSet         St_DataSet ;");       
+    gInterpreter->ProcessLine("typedef TDataSetIter     St_DataSetIter;");    
+    gInterpreter->ProcessLine("typedef TFileSet         St_FileSet;");        
+    gInterpreter->ProcessLine("typedef TVolume          St_Node;");           
+    gInterpreter->ProcessLine("typedef TVolumePosition  St_NodePosition;");   
+    gInterpreter->ProcessLine("typedef TVolumeView      St_NodeView;");       
+    gInterpreter->ProcessLine("typedef TVolumeViewIter  St_NodeViewIter;");   
+    gInterpreter->ProcessLine("typedef TObjectSet       St_ObjectSet;");      
+    //  gInterpreter->ProcessLine("typedef TPointPosition   St_PointPosition;");  
+    gInterpreter->ProcessLine("typedef TPoints3D        St_Points3D;");       
+    gInterpreter->ProcessLine("typedef TPointsArray3D   St_PointsArray3D;");  
+    gInterpreter->ProcessLine("typedef TPolyLineShape   St_PolyLineShape;");  
+    gInterpreter->ProcessLine("typedef TTable           St_Table;");          
+    gInterpreter->ProcessLine("typedef TTable3Points    St_Table3Points;");   
+    gInterpreter->ProcessLine("typedef TTableIter       St_TableIter;");      
+    gInterpreter->ProcessLine("typedef TTablePoints     St_TablePoints;");    
+    gInterpreter->ProcessLine("typedef TTableSorter     St_TableSorter;");    
+    gInterpreter->ProcessLine("typedef TTableDescriptor St_tableDescriptor;");
   }
-  //  gInterpreter->ProcessLine(".x Star2Root.C");
-{
-  gInterpreter->ProcessLine("typedef TCL              StCL;");              
-  gInterpreter->ProcessLine("typedef TDataSet         St_DataSet ;");       
-  gInterpreter->ProcessLine("typedef TDataSetIter     St_DataSetIter;");    
-  gInterpreter->ProcessLine("typedef TFileSet         St_FileSet;");        
-  gInterpreter->ProcessLine("typedef TVolume          St_Node;");           
-  gInterpreter->ProcessLine("typedef TVolumePosition  St_NodePosition;");   
-  gInterpreter->ProcessLine("typedef TVolumeView      St_NodeView;");       
-  gInterpreter->ProcessLine("typedef TVolumeViewIter  St_NodeViewIter;");   
-  gInterpreter->ProcessLine("typedef TObjectSet       St_ObjectSet;");      
-//  gInterpreter->ProcessLine("typedef TPointPosition   St_PointPosition;");  
-  gInterpreter->ProcessLine("typedef TPoints3D        St_Points3D;");       
-  gInterpreter->ProcessLine("typedef TPointsArray3D   St_PointsArray3D;");  
-  gInterpreter->ProcessLine("typedef TPolyLineShape   St_PolyLineShape;");  
-  gInterpreter->ProcessLine("typedef TTable           St_Table;");          
-  gInterpreter->ProcessLine("typedef TTable3Points    St_Table3Points;");   
-  gInterpreter->ProcessLine("typedef TTableIter       St_TableIter;");      
-  gInterpreter->ProcessLine("typedef TTablePoints     St_TablePoints;");    
-  gInterpreter->ProcessLine("typedef TTableSorter     St_TableSorter;");    
-  gInterpreter->ProcessLine("typedef TTableDescriptor St_tableDescriptor;");
-}
 //  printf("\nWelcome to the ROOT tutorials\n\n");
 //  printf("\nType \".x demos.C\" to get a toolbar from which to execute the demos\n");
 //  printf("\nType \".x demoshelp.C\" to see the help window\n\n");
