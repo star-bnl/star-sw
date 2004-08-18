2eab7fe23ed4e645ee8bda173f51dd02f5fdadbb
balewski <>
2004-08-18 19:53:00 +0000

    *** empty log message ***

--- StRoot/StEEmcPool/StRFEmcTrigger/macros/StEventChain.C
+++ StRoot/StEEmcPool/StRFEmcTrigger/macros/StEventChain.C
@@ -1,43 +1,21 @@
-
 class  StChain;
 StChain *chain;
 
-
 void StEventChain( char *fname ="/star/data35/reco/productionPP/ReversedFullField/DEV/2004/117/st_physics_5117072_raw_2010005.event.root"){
 
-  fname ="/star/data05/scratch/balewski/mcMinA/minB-P00-1001+.event.root";
-  int nevents =50;
+  fname ="/star/data05/scratch/balewski/mcMinB/minB-P00-1001+.event.root";
+  int nevents =2000;
   int total=0;
 
   if (gClassTable->GetID("TTable") < 0) {
     gSystem->Load("libStar");
     gSystem->Load("libPhysics");
   } 
-gSystem->Load("St_base");
-  gSystem->Load("StChain");
-  gSystem->Load("libglobal_Tables");
-  gSystem->Load("libsim_Tables");
-  gSystem->Load("libgen_Tables");
-  gSystem->Load("St_Tables");
-  gSystem->Load("StDbLib");
-  gSystem->Load("StDbBroker");
-  gSystem->Load("St_db_Maker"); 
-  gSystem->Load("StUtilities");
-  gSystem->Load("StIOMaker");
-  gSystem->Load("StMagF");
-  gSystem->Load("StarClassLibrary");
-  gSystem->Load("StAnalysisUtilities");
-  gSystem->Load("StEvent");
-  gSystem->Load("StTpcDb"); 
-  gSystem->Load("StEventMaker"); 
-  gSystem->Load("libgeometry_Tables");
-  gSystem->Load("StEmcUtil");    
-  gSystem->Load("StEEmcUtil"); 
+
   gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
   loadSharedLibraries();
   assert(gSystem->Load("StRFEmcTrigger")==0);
-  assert(gSystem->Load("St_TLA_Maker")==0);
-
+  
   chain= new StChain("StChain"); 
   // chain->SetDebug(1);   
 
@@ -52,51 +30,31 @@ gSystem->Load("St_base");
   ioMaker->SetIOMode("r"); 
  
   
-  StRFEmcTrigMaker *trig = new StRFEmcTrigMaker("RFTrig"); 
+  StRFEmcTrigMaker *trig = new StRFEmcTrigMaker("RFTrigA"); 
   //  trig->useMuDst();// or ..
   trig->useStEvent();
 
   // use filtering capabilities
+  //trig->requireBBC();
+
+  trig = new StRFEmcTrigMaker("RFTrigB"); 
+  trig->useStEvent();
   trig->requireBBC();
 
-  St_TLA_Maker *jan=new St_TLA_Maker;
 
-  Int_t EHT;
-  Int_t EJP;
-  Int_t ETOT;
-  Int_t BHT;
-  Int_t BJP;
-  Int_t BTOT;
-  Int_t BBCtrig;
-  int EHTtrig;
-  int thres=5;
+  char *outHistFile="aa2";
+  StTreeMaker* treeMk = new StTreeMaker("tree",outHistFile);
+  treeMk->SetIOMode("w");
+  treeMk->SetBranch("histBranch");
+  chain->ls(3);
   //chain->PrintInfo();
   chain->Init();
   for (Int_t iev=0;iev<nevents; iev++) {
-    cout << "****************************************** " << endl;
-    cout << "Working on eventNumber " << iev << endl;
-    cout << "*************************1***************** " << endl;
+    if(iev%50==0)cout << "Working on eventNumber " << iev << endl;
     chain->Clear();
     int iret = chain->Make(iev); 
     total++;
-    /*
-    EHT= trig->getEEMC_HT_ADC();
-    EJP= trig->getEEMC_JP_ADC();
-    ETOT= trig->getEEMC_TOT_ADC();
-    BHT= trig->getBEMC_HT_ADC();
-    BJP= trig->getBEMC_JP_ADC();
-    BTOT= trig->getBEMC_TOT_ADC();
-    cout <<"In the script now!"<<endl;
-    printf("ETOT=%d,EHT =%d,EJP=%d\n",ETOT,EHT,EJP);
-    printf("BTOT=%d,BHT=%d,BJP=%d\n",BTOT,BHT,BJP);
- 
-    BBCtrig= trig->getBBCtrig();   
-    EHTtrig= trig->getEEMCtrigHT(thres);
-    cout <<"Thres = "<< thres << "  EHTtrig = " << EHTtrig<<endl;
-    cout << "BBCtrig = " << BBCtrig<<endl;
-    */
-
-    if (iret && iert!=kStWarn) {
+    if (iret && iret!=kStErr) {
       cout << "Bad return code!" << endl;
       break;
     }
