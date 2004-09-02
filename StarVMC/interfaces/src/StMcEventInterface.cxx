6c0ce06997183488c8a3c9831485528690b8a9c1
potekhin <>
2004-09-02 23:36:42 +0000

    more code put in

--- StarVMC/interfaces/src/StMcEventInterface.cxx
+++ StarVMC/interfaces/src/StMcEventInterface.cxx
@@ -1,5 +1,8 @@
-// $Id: StMcEventInterface.cxx,v 1.2 2004/07/16 22:57:53 potekhin Exp $
+// $Id: StMcEventInterface.cxx,v 1.3 2004/09/02 23:36:42 potekhin Exp $
 // $Log: StMcEventInterface.cxx,v $
+// Revision 1.3  2004/09/02 23:36:42  potekhin
+// more code put in
+//
 // Revision 1.2  2004/07/16 22:57:53  potekhin
 // Comments
 //
@@ -24,32 +27,38 @@ ClassImp(StMcEventInterface)
 void StMcEventInterface::FinishEventCB(TObjArray* hits_) {
   cout<<"Creating a StMcEvent"<<endl;
 
-  //  StMcEvent* e = new StMcEvent();
-  //   e->initToZero();
+  StMcEvent* e = new StMcEvent();
+  e->initToZero();
+  cout<<"Init passed "<<e<<endl;
 
 
   // Now do individual hits
-  //  TIterator*   it = hits_->MakeIterator();
-  //  StarHit*      h = (StarHit*) it->Next();
+  TIterator*   it = hits_->MakeIterator();
+  StarHit*      h = (StarHit*) it->Next();
 
-  //  while(h) {
-  //    TVector3 pos = h->GetPos();
+  while(h) {
+    TVector3 pos  = h->GetPos();
+    Int_t trackID = h->GetTrackID();
 
-    //    StThreeVectorF stPosition, stMomentum;
+    StThreeVectorF stPosition, stMomentum;
 
-    //    stPosition.setX(pos.X());
-    //    stPosition.setY(pos.Y());
-    //    stPosition.setZ(pos.Z());
 
-  //    float energy = h->GetEdep();
-  //    float dS(0);
+    //    cout << "trackID "<< trackID << "x " << pos.X() << " y " << pos.Y() << " z " << pos.Z() << endl;
 
-    //    StMcTpcHit StH(stPosition, stMomentum, energy, dS, 0, 0, 0);
+    stPosition.setX(pos.X());
+    stPosition.setY(pos.Y());
+    stPosition.setZ(pos.Z());
+
+    float energy = h->GetEdep();
+    float dS(0);
+
+    StMcTpcHit StH(stPosition, stMomentum, energy, dS, 0, 0, 0);
 
     //    pm->SetNextPoint(pos.X(),pos.Y(),pos.Z());
-    //    h=(StarHit*) it->Next();
-  //  }
+
+    h=(StarHit*) it->Next();
+  }
 
 
-  cout<<"finished"<<endl;
+  cout<<"finished StMcEvent"<<endl;
 }
