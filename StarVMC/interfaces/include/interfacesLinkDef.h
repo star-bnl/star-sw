05cd29f88a868464895aadadf07b59d13e86ba95
Maxim Potekhin <potekhin@bnl.gov>
2004-09-03 00:21:15 +0000

    need this header

---
+++ StarVMC/interfaces/include/interfacesLinkDef.h
@@ -0,0 +1,18 @@
+// $Id: interfacesLinkDef.h,v 1.1 2004/09/03 00:21:15 potekhin Exp $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class StMcEventInterface+;
+
+#pragma link C++ enum   FileMode;
+
+#endif
+
+
+
+
+
