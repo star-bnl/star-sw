04ffba0d3cd92ba07e71b3cea81c53533cf571a1
Maxim Potekhin <potekhin@bnl.gov>
2004-09-03 00:11:04 +0000

    *** empty log message ***

---
+++ StarVMC/vmcApp/src/StarMCEvent.cxx
@@ -0,0 +1,36 @@
+// $Id: StarMCEvent.cxx,v 1.1 2004/09/03 00:11:04 potekhin Exp $
+// $Log: StarMCEvent.cxx,v $
+// Revision 1.1  2004/09/03 00:11:04  potekhin
+// *** empty log message ***
+//
+
+
+#include "StarMCEvent.h"
+
+#include <TParticle.h>
+#include <TObjArray.h>
+
+#include <iostream>
+
+using namespace std;
+
+ClassImp(StarMCEvent)
+
+
+//_____________________________________________________________________________
+StarMCEvent::StarMCEvent()
+{
+//   
+}
+
+//_____________________________________________________________________________
+StarMCEvent::~StarMCEvent() 
+{
+//
+}
+
+//
+// public methods
+//
+
+//_____________________________________________________________________________
