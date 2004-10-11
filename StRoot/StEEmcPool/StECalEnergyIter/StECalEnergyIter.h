0ac721c2aa26ef33f3caa504d7b249b035d0d777
Robert Cadman <cadman@hep.anl.gov>
2004-10-11 18:05:02 +0000

    Fixed error in application of gain to the data.  In addition the iterator now skips elements which have no database entry, or the "fail" flag set, or a gain which is out-of-range.

--- StRoot/StEEmcPool/StECalEnergyIter/StECalEnergyIter.h
+++ StRoot/StEEmcPool/StECalEnergyIter/StECalEnergyIter.h
@@ -1,4 +1,4 @@
-// $Id: StECalEnergyIter.h,v 1.1 2004/06/02 22:22:58 cadman Exp $
+// $Id: StECalEnergyIter.h,v 1.2 2004/10/11 18:05:02 cadman Exp $
 
 #ifndef STAR_StECalEnergyIter
 #define STAR_StECalEnergyIter
@@ -25,13 +25,14 @@ class StECalEnergyIter {
   int mdetector;
   int mNhits;
   int mIhits;
+  int mSuppBad;
   inline StECalEnergyIter() { /* no-op */ }
  
  protected:
   // Protected method if any
 
  public: 
-  StECalEnergyIter(StMuEmcCollection *emCol, int det, StEEmcDbMaker *eedb);
+  StECalEnergyIter(StMuEmcCollection *, int, StEEmcDbMaker *, bool=true);
   bool next(float &e, int &adc, int &adclessped, int &sec, int &eta, 
 	    int &phi, char &cdet);
   inline bool operator()(float &e, int &adc, int &adclessped, int &sec, 
@@ -40,7 +41,7 @@ class StECalEnergyIter {
 
   /// Displayed on session exit, leave it as-is please ...
   virtual const char *GetCVS() const {
-    static const char cvs[]="Tag $Name:  $ $Id: StECalEnergyIter.h,v 1.1 2004/06/02 22:22:58 cadman Exp $ built "__DATE__" "__TIME__ ; 
+    static const char cvs[]="Tag $Name:  $ $Id: StECalEnergyIter.h,v 1.2 2004/10/11 18:05:02 cadman Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
 
