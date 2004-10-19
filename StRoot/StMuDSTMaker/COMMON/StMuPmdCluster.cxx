db71737e607ff3978c4c4e5673f20b4e29b50dd9
mvl <>
2004-10-19 01:44:29 +0000

    Changed names of getters and setters (Star coding conventions)

--- StRoot/StMuDSTMaker/COMMON/StMuPmdCluster.cxx
+++ StRoot/StMuDSTMaker/COMMON/StMuPmdCluster.cxx
@@ -1,5 +1,5 @@
 /*****************************************************************
- * $Id: StMuPmdCluster.cxx,v 1.1 2004/04/02 03:36:20 jeromel Exp $
+ * $Id: StMuPmdCluster.cxx,v 1.2 2004/10/19 01:44:29 mvl Exp $
  *
  * Class : StMuPmdCluster
  * Author: Supriya Das
@@ -8,6 +8,9 @@
  * Description: This is the Cluster class for PMD in MuDst
  * ****************************************************************
  * $Log: StMuPmdCluster.cxx,v $
+ * Revision 1.2  2004/10/19 01:44:29  mvl
+ * Changed names of getters and setters (Star coding conventions)
+ *
  * Revision 1.1  2004/04/02 03:36:20  jeromel
  * New files for PMD
  *
@@ -27,13 +30,13 @@ StMuPmdCluster::~StMuPmdCluster()
 
 StMuPmdCluster::StMuPmdCluster(StMuPmdCluster* cl)
 {
-  mSuperModule = cl->GetSuperModule();
-  mNcell = cl->GetNcell();
-  mEta = cl->GetEta();
-  mPhi = cl->GetPhi();
-  mSigma = cl->GetSigma();
-  mEnergy = cl->GetEnergy();
-  mEnergyPID = cl->GetEnergyPID();
-  mPID = cl->GetPID();
-  mMcPID = cl->GetMcPID();
+  mSuperModule = cl->superModule();
+  mNcell = cl->ncell();
+  mEta = cl->eta();
+  mPhi = cl->phi();
+  mSigma = cl->sigma();
+  mEnergy = cl->energy();
+  mEnergyPID = cl->energyPID();
+  mPID = cl->PID();
+  mMcPID = cl->mcPID();
 }
