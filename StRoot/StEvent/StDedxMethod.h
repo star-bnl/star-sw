280efd6395e624e050be4c9963914297cca552e3
ullrich <>
2004-10-20 18:52:52 +0000

    File obsolete. Content moved to StEnumerations.h.

--- StRoot/StEvent/StDedxMethod.h
+++ StRoot/StEvent/StDedxMethod.h
@@ -1,12 +1,9 @@
 #ifndef StDedxMethod_hh
 #define StDedxMethod_hh
-#include "StDedxDefinitions.h"
-enum StDedxMethod {
-  kUndefinedMethodId         = kUndefinedMethodIdentifier,
-  kTruncatedMeanId           = kTruncatedMeanIdentifier,
-  kEnsembleTruncatedMeanId   = kEnsembleTruncatedMeanIdentifier,
-  kLikelihoodFitId           = kLikelihoodFitIdentifier,
-  kWeightedTruncatedMeanId   = kWeightedTruncatedMeanIdentifier,
-  kOtherMethodId             = kOtherMethodIdentifier
-};
+#include "StEnumerations.h"
+// This file (StDedxMethod.h) is obsolete!
+// All content from StDedxMethod.h is now in StEnumerations.h
+// together with all other enumerations used in StEvent.
+// Please add all enumerations to StEnumerations.h and
+// change your code to NOT include StDedxMethod.h.
 #endif
--- StRoot/StEvent/StDetectorId.h
+++ StRoot/StEvent/StDetectorId.h
@@ -1,53 +1,12 @@
 #ifndef StDetectorId_hh
 #define StDetectorId_hh
-#include "StDetectorDefinitions.h"
-enum StDetectorId {
-  kUnknownId = kUnknownIdentifier,
-  kTpcId = kTpcIdentifier,
-  kSvtId = kSvtIdentifier,
-  kRichId = kRichIdentifier,
-  kFtpcWestId = kFtpcWestIdentifier,
-  kFtpcEastId = kFtpcEastIdentifier,
-  kTofId = kTofIdentifier,
-  kCtbId = kCtbIdentifier,
-  kSsdId = kSsdIdentifier,
-  kBarrelEmcTowerId = kBarrelEmcTowerIdentifier,
-  kBarrelEmcPreShowerId = kBarrelEmcPreShowerIdentifier,
-  kBarrelSmdEtaStripId = kBarrelSmdEtaStripIdentifier,
-  kBarrelSmdPhiStripId = kBarrelSmdPhiStripIdentifier,
-  kEndcapEmcTowerId = kEndcapEmcTowerIdentifier,
-  kEndcapEmcPreShowerId = kEndcapEmcPreShowerIdentifier,
-  kEndcapSmdUStripId = kEndcapSmdUStripIdentifier,
-  kEndcapSmdVStripId = kEndcapSmdVStripIdentifier,
-  kZdcWestId   = kZdcWestIdentifier,
-  kZdcEastId   = kZdcEastIdentifier,
-  kMwpcWestId  = kMwpcWestIdentifier,
-  kMwpcEastId  = kMwpcEastIdentifier,
-  kTpcSsdId    = kTpcSsdIdentifier,
-  kTpcSvtId    = kTpcSvtIdentifier,        
-  kTpcSsdSvtId = kTpcSsdSvtIdentifier,        
-  kSsdSvtId    = kSsdSvtIdentifier,
-  kPhmdCpvId   = kPhmdCpvIdentifier,
-  kPhmdId      = kPhmdIdentifier
-};
-// maximal sizes of tracking part of STAR in cm
-enum StarMaxSize { kStarMaxR=200, kStarMaxZ=269 };
+#include "StEnumerations.h"
+// This file (StDetectorId.h) is obsolete!
+// All content from StDetectorId.h is now in StEnumerations.h
+// together with all other enumerations used in StEvent.
+// Please add all enumerations to StEnumerations.h and
+// change your code to NOT include StDetectorId.h.
 #endif
-
-
-// $Id: StDetectorId.h,v 2.2 2004/10/17 03:29:33 perev Exp $
-//
-// $Log: StDetectorId.h,v $
-// Revision 2.2  2004/10/17 03:29:33  perev
-// enum StarMaxSize added
-//
-// Revision 2.1  2004/04/26 16:35:19  fisyak
-// Move enumerations from pams/global/inc => StEvent
-//
-// Revision 1.9  2002/12/18 02:28:48  lbarnby
-// PMD identifiers added
-//
-
 
 
 
