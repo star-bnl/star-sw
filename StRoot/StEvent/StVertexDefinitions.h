b85286b28eb70d61ea4bff8cfe0b5e50709904bd
ullrich <>
2004-11-03 18:30:16 +0000

    Added definitions for FTPC calibration vertices.

--- StRoot/StEvent/StVertexDefinitions.h
+++ StRoot/StEvent/StVertexDefinitions.h
@@ -1,20 +1,36 @@
-#ifndef StVerexDefinitions_hh
-#define StVerexDefinitions_hh
+/***************************************************************************
+ *
+ * $Id: StVertexDefinitions.h,v 2.2 2004/11/03 18:30:16 ullrich Exp $
+ *
+ * Author: unknown
+ ***************************************************************************
+ *
+ * Description: Numbering scheme for vertex types.
+ *              The definitions listed here are used in StEnumeration
+ *              to define the actual enumerations to be used in C/C++
+ *              code. The definitions here are only to allow fortran
+ *              code to use the same identifiers and thus maintain
+ *              backwards compatibility.
+ *
+ ***************************************************************************
+ *
+ * $Log: StVertexDefinitions.h,v $
+ * Revision 2.2  2004/11/03 18:30:16  ullrich
+ * Added definitions for FTPC calibration vertices.
+ *
+ *
+ **************************************************************************/
+#ifndef StVertexDefinitions_hh
+#define StVertexDefinitions_hh
 
-/* Numbering scheme for detectors
-**   Unknown Vertex  0
-**   Event Vertex    1
-**   V0              2
-**   Cascade (Xi)    3
-**   Kink            4
-**   Other           5        */
-/* numbering scheme for vertex types */
+#define kUndefinedVertexIdentifier           0
+#define kEventVertexIdentifier               1
+#define kV0DecayIdentifier                   2
+#define kXiDecayIdentifier                   3
+#define kKinkDecayIdentifier                 4
+#define kOtherTypeIdentifier                 5
+#define kFtpcEastCalibrationVertexIdentifier 6
+#define kFtpcWestCalibrationVertexIdentifier 7
 
-#define kUndefinedVertexIdentifier     0
-#define kEventVertexIdentifier         1
-#define kV0DecayIdentifier             2
-#define kXiDecayIdentifier             3
-#define kKinkDecayIdentifier           4
-#define kOtherTypeIdentifier           5
 #endif
 
