9150db4478a0bb86d5f60bb16205aaf287b7ce36
Jerome Lauret <jeromel@bnl.gov>
2004-11-05 04:06:15 +0000

    doxygenized doc so defines will become cross refs in auto-documentation

--- StRoot/StEvent/StVertexDefinitions.h
+++ StRoot/StEvent/StVertexDefinitions.h
@@ -1,20 +1,30 @@
+/*!
+ * \file StVertexDefinitions.h
+ * 
+ * \author unknown
+ * 
+ * Numbering scheme for vertex types.
+ * The definitions listed here are used in StEnumeration
+ * to define the actual enumerations to be used in C/C++
+ * code. The definitions here are only to allow fortran
+ * code to use the same identifiers and thus maintain
+ * backwards compatibility.
+ * 
+ */
 /***************************************************************************
  *
- * $Id: StVertexDefinitions.h,v 2.2 2004/11/03 18:30:16 ullrich Exp $
+ * $Id: StVertexDefinitions.h,v 2.3 2004/11/05 04:06:15 jeromel Exp $
  *
  * Author: unknown
  ***************************************************************************
  *
- * Description: Numbering scheme for vertex types.
- *              The definitions listed here are used in StEnumeration
- *              to define the actual enumerations to be used in C/C++
- *              code. The definitions here are only to allow fortran
- *              code to use the same identifiers and thus maintain
- *              backwards compatibility.
  *
  ***************************************************************************
  *
  * $Log: StVertexDefinitions.h,v $
+ * Revision 2.3  2004/11/05 04:06:15  jeromel
+ * doxygenized doc so defines will become cross refs in auto-documentation
+ *
  * Revision 2.2  2004/11/03 18:30:16  ullrich
  * Added definitions for FTPC calibration vertices.
  *
@@ -23,14 +33,14 @@
 #ifndef StVertexDefinitions_hh
 #define StVertexDefinitions_hh
 
-#define kUndefinedVertexIdentifier           0
-#define kEventVertexIdentifier               1
-#define kV0DecayIdentifier                   2
-#define kXiDecayIdentifier                   3
-#define kKinkDecayIdentifier                 4
-#define kOtherTypeIdentifier                 5
-#define kFtpcEastCalibrationVertexIdentifier 6
-#define kFtpcWestCalibrationVertexIdentifier 7
+#define kUndefinedVertexIdentifier           0                         /*! \def kUndefinedVertexIdentifier */
+#define kEventVertexIdentifier               1                         /*! \def kEventVertexIdentifier */
+#define kV0DecayIdentifier                   2                         /*! \def kV0DecayIdentifier */
+#define kXiDecayIdentifier                   3                         /*! \def kXiDecayIdentifier */
+#define kKinkDecayIdentifier                 4                         /*! \def kKinkDecayIdentifier */
+#define kOtherTypeIdentifier                 5                         /*! \def kOtherTypeIdentifier */
+#define kFtpcEastCalibrationVertexIdentifier 6                         /*! \def kFtpcEastCalibrationVertexIdentifier */
+#define kFtpcWestCalibrationVertexIdentifier 7                         /*! \def kFtpcWestCalibrationVertexIdentifier */
 
 #endif
 
