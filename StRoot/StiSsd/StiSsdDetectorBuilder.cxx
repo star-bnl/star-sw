6119d392ea1c8ad55fea646ce212302f5121bb9d
pruneau <>
2004-11-08 15:37:39 +0000

    Added/Fixed placement keys, Provisionally put a return in the 1st line of the builder - this code segfaults otherwise

--- StRoot/StiSsd/StiSsdDetectorBuilder.cxx
+++ StRoot/StiSsd/StiSsdDetectorBuilder.cxx
@@ -45,71 +45,84 @@ StiSsdDetectorBuilder::~StiSsdDetectorBuilder()
 
 void StiSsdDetectorBuilder::buildDetectors(StMaker & source)
 {
+  return;
+    cout <<" -1-"<<endl;
     char name[50];  
     int nRows = 1 ;
-    gMessMgr->Info() << "StiSsdDetectorBuilder::buildDetectors() - I - Started "<<endm;
-    load(_inputFile, source);
+    //gMessMgr->Info() << "StiSsdDetectorBuilder::buildDetectors() - I - Started "<<endm;
+    //load(_inputFile, source);
 
     setNRows(nRows);
+    cout <<" -2-"<<endl;
 
-    assert(gStSsdDbMaker);
+    //assert(gStSsdDbMaker);
     if (!gStSsdDbMaker) {
 	throw runtime_error("StiSsdDetectorBuilder::loadDb() - ERROR - gStSsdDbMaker==0");
     }
-  
+      cout <<" -3-"<<endl;
+
     St_SsdDb_Reader *pSsdDb_Reader = gStSsdDbMaker->get_SsdDb_Reader();
-    assert(pSsdDb_Reader);
+    //assert(pSsdDb_Reader);
     if (!pSsdDb_Reader)
 	throw runtime_error("StiSsdDetectorBuilder::loadDb() - ERROR - pSsdDb_Reader==0");
+    cout <<" -4-"<<endl;
 
     //St_ssdWafersPosition*  wafersPosition = pSsdDb_Reader->getWafersPosition() ;
     ssdWafersPosition_st*  wafersPosition = pSsdDb_Reader->getWafersPosition() ;
     _geometry = pSsdDb_Reader->getGeometry(wafersPosition);
-    assert(pSsdDb_Reader);
+    //assert(pSsdDb_Reader);
     if (_geometry) 
-	gMessMgr->Info()<< "StiSsdDetectorBuilder : SSD Geometry loaded..."<<endm;
+	//gMessMgr->Info()<< "StiSsdDetectorBuilder : SSD Geometry loaded..."<<endm;
+      cout<< "StiSsdDetectorBuilder : SSD Geometry loaded..."<<endl;
     else
 	throw runtime_error("StiSsdDetectorBuilder::loadDb() - ERROR - _geometry==0");
-  
+      cout <<" -5-"<<endl;
+
     //St_ssdConfiguration*  ssdConfiguration = pSsdDb_Reader->getSsdConfiguration() ;
     ssdConfiguration_st*  ssdConfiguration = pSsdDb_Reader->getSsdConfiguration() ;
     _config = pSsdDb_Reader->getConfiguration(ssdConfiguration);
-    assert(_config);
+    //assert(_config);
     if (_config) 
-	gMessMgr->Info() <<"StiSsdDetectorBuilder : SSD Configuration loaded..." << endm;
+	//gMessMgr->Info() <<"StiSsdDetectorBuilder : SSD Configuration loaded..." << endm;
+      cout <<"StiSsdDetectorBuilder : SSD Configuration loaded..." << endl;
     else
 	throw runtime_error("StiSsdDetectorBuilder::loadDb() - ERROR - _config==0");
+    cout <<" -6-"<<endl;
 
   
     //St_ssdDimensions*  ssdDimensions = pSsdDb_Reader->getSsdDimensions() ;
     ssdDimensions_st*  ssdDimensions = pSsdDb_Reader->getSsdDimensions() ;
     _dimensions = pSsdDb_Reader->getDimensions(ssdDimensions);
-    assert(_dimensions);
+    //assert(_dimensions);
     if (_dimensions) 
-	gMessMgr->Info() <<"StiSsdDetectorBuilder : SSD Dimensions set..." << endm;
+	//gMessMgr->Info() <<"StiSsdDetectorBuilder : SSD Dimensions set..." << endm;
+      gMessMgr->Info() <<"StiSsdDetectorBuilder : SSD Dimensions set..." << endl;
     else
 	throw runtime_error("StiSsdDetectorBuilder::loadDb() - ERROR - _dimensions==0");
 
-  
+      cout <<" -7-"<<endl;
+
   
     /*! buildMaterials : _gasMat is the gas the SSD lives in 
       _siMat corresponds to Silicon Wafers. 
       _hybridMat corresponds to Hybrids.
       all SSD materials (average).
     */
-    gMessMgr->Info() << "StiSsdDetectorBuilder::buildMaterials() - I - Started "<<endm;
+    //gMessMgr->Info() << "StiSsdDetectorBuilder::buildMaterials() - I - Started "<<endm;
     _gasMat     = add(new StiMaterial("SsdAir", 0.49919,  1., 0.001205, 30420.*0.001205, 5.));
     _siMat      = add(new StiMaterial("SsdSi",14., 28.0855, 2.33, 21.82, 5.));
     _hybridMat  = add(new StiMaterial("SsdHyb",14., 28.0855, 2.33, 21.82, 5.));
-    gMessMgr->Info() << "StiSsdDetectorBuilder::buildMaterials() - I - Done "<<endm;  
-
+    //gMessMgr->Info() << "StiSsdDetectorBuilder::buildMaterials() - I - Done "<<endm;  
+    cout << "StiSsdDetectorBuilder::buildMaterials() - I - Done "<<endl;  
+    cout <<" -8-"<<endl;
     /*! buildShape : SSD has been defined as a PlanarShape 
       Hybrids are defined as a PlanarShape, positionned on the Silicon Wafers; the thickness of 
       the hybrids corresponds to 0.75 % of X0. 
     */
     unsigned int layer = 0;
     int nWafers;
-    gMessMgr->Info() << "StiSsdDetectorBuilder::buildShapes() - I - Started" << endm;
+    //gMessMgr->Info() << "StiSsdDetectorBuilder::buildShapes() - I - Started" << endm;
+    cout  << "StiSsdDetectorBuilder::buildShapes() - I - Started" << endl;
 
     nWafers = _config->getNumberOfWafers();
     _waferShape[layer] = new StiPlanarShape(name,
@@ -119,18 +132,22 @@ void StiSsdDetectorBuilder::buildDetectors(StMaker & source)
 					    _dimensions->getWaferThickness(),
 					    //_dimensions->getWaferLength() );
 					    _dimensions->getWaferLength()/2. ); //too big? MLM 9/1/04
-    gMessMgr->Info() <<"SSD : Wafer dimensions = "<< _dimensions->getWaferLength() 
-		     <<"/"<<_dimensions->getWaferThickness()<<"/"<<_dimensions->getWaferWidth()<<endm;
+    cout <<" -9-"<<endl;
+    //gMessMgr->Info() <<"SSD : Wafer dimensions = "<< _dimensions->getWaferLength() 
+    //	     <<"/"<<_dimensions->getWaferThickness()<<"/"<<_dimensions->getWaferWidth()<<endm;
     add(_waferShape[layer]);
+    cout <<" -10-"<<endl;
 
     _hybridShape[layer] = new StiPlanarShape(name,nWafers*_dimensions->getWaferWidth(),
 					     0.07,_dimensions->getWaferLength() );
     add(_hybridShape[layer]);  
+    cout <<" -11-"<<endl;
 
-    gMessMgr->Info() << "StiSsdDetectorBuilder::buildShapes() - I - Done" << endm;
+    //gMessMgr->Info() << "StiSsdDetectorBuilder::buildShapes() - I - Done" << endm;
 
 
     setNSectors(layer,_config->getTotalNumberOfLadders()); 
+    cout <<" -12-"<<endl;
 
     /*! Placement of Ssd Modules is currently done by reading the geom.C table. 
       Ladders are placed according to the coordinates of its first module number 	  
@@ -144,22 +161,24 @@ void StiSsdDetectorBuilder::buildDetectors(StMaker & source)
 
 
     //ok, let's see what in the container, once and for all... (Brute force...)
-    typedef map<int,int> IntMap;
-    IntMap mymap;
+    //typedef map<int,int> IntMap;
+    //IntMap mymap;
 
     int ind=0;
     for (VecTObjIter it=_geometry->begin(); it!=_geometry->end(); ++it) {
-	//cout <<"look at index:\t"<<ind<<endl;
+	cout <<"look at index:\t"<<ind<<endl;
 	
 	StSsdWaferGeometry* waferGeom = dynamic_cast<StSsdWaferGeometry*>( *it );
-	assert(waferGeom);
-	mymap[waferGeom->getID()] = ind;
+	//assert(waferGeom);
+	//mymap[waferGeom->getID()] = ind;
 	++ind;
+	cout <<" -13 recur-"<<endl;
 
 	for (int ladder=1; ladder<=20; ++ladder) {	    
 	    if(waferGeom->getID() == (7100 + (ladder))) { //this gives us the first wafer on the ladder
 		//int wafer = waferGeom->getID();
-		
+		    cout <<" -14 rec-"<<endl;
+
 		StThreeVectorD centerVector(waferGeom->x(0), waferGeom->x(1), waferGeom->x(2) );
 		StThreeVectorD normalVector(waferGeom->n(0), waferGeom->n(1), waferGeom->n(2) );
 	    
@@ -176,14 +195,17 @@ void StiSsdDetectorBuilder::buildDetectors(StMaker & source)
 		cout <<"Det Nber = "<<waferGeom->getID()<<"\tcv\t:"<<centerVector<<"\tphi:\t"<<phi<<"\tr:\t"<<r<<endl;
 	
 		StiPlacement *pPlacement = new StiPlacement;
+    cout <<" -15-"<<endl;
 		pPlacement->setZcenter(0.);
 		//pPlacement->setLayerRadius(centerVector.perp()); //this is only used for ordering in detector container...
 		pPlacement->setLayerRadius(r); //this is only used for ordering in detector container...
+		pPlacement->setLayerAngle(phi); //this is only used for ordering in detector container...
 		pPlacement->setRegion(StiPlacement::kMidRapidity);
 		//pPlacement->setNormalRep(psi, rprime, dy); //this should work
 		pPlacement->setNormalRep(phi, r, 0.);  //but we have to use this to fix ladders 20 and 12
 		sprintf(name, "Ssd/Layer_%d/Ladder_%d/Wafers", layer, ladder);
-	
+	    cout <<" -16-"<<endl;
+
 		StiDetector *pLadder = _detectorFactory->getInstance();
 		pLadder->setName(name);
 		pLadder->setIsOn(true);
@@ -195,9 +217,11 @@ void StiSsdDetectorBuilder::buildDetectors(StMaker & source)
 		pLadder->setShape(_waferShape[layer]);
 		pLadder->setPlacement(pPlacement); 
 		pLadder->setHitErrorCalculator(&_hitCalculator);
-		add(layer,ladder-1,pLadder); //this fucking ladder-1 cost me two days! (MLM 9/1)
-	
-		StiDetector *pHybrid = _detectorFactory->getInstance();
+		cout <<" -17-"<<endl;
+		add(layer,ladder-1,pLadder); 
+	    cout <<" -18-"<<endl;
+
+		/*StiDetector *pHybrid = _detectorFactory->getInstance();
 		pHybrid->setName(name);
 		pHybrid->setIsOn(true);
 		pHybrid->setIsActive(new StiNeverActiveFunctor);
@@ -207,7 +231,7 @@ void StiSsdDetectorBuilder::buildDetectors(StMaker & source)
 		pHybrid->setMaterial(_hybridMat);
 		pHybrid->setShape(_hybridShape[layer]);
 		pHybrid->setPlacement(pPlacement); 
-		add(pHybrid);
+		add(pHybrid);*/
 	    }
 	}
     }
