a58b016fce027c5935212aba4c7645cb9ffbf315
fisyak <>
2004-11-14 16:36:54 +0000

    Add interface to TCL::TR package

--- StRoot/StarRoot/StarRootLinkDef.h
+++ StRoot/StarRoot/StarRootLinkDef.h
@@ -6,3 +6,25 @@
 #pragma link C++ class THelixTrack-;
 #pragma link C++ class TUnixTime;
 #pragma link C++ class TDirIter;
+#pragma link C++ class TRArray+;
+#pragma link C++ class TRMatrix+;
+#pragma link C++ class TRSymMatrix+;
+#pragma link C++ class TRVector+;
+
+#pragma link C++ enum TRArray::ETRMatrixType;
+#pragma link C++ enum TRArray::ETRMatrixCreatorsOp;
+
+#pragma link C++ function operator-=(TRArray &, const Double_t &);
+#pragma link C++ function operator+=(TRArray &, const Double_t &);
+#pragma link C++ function operator*=(TRArray &, const Double_t &);
+#pragma link C++ function operator*(const TRArray &, const TRArray &);
+#pragma link C++ function operator-=(TRArray &, const TRArray &);
+#pragma link C++ function operator+=(TRArray &, const TRArray &);
+#pragma link C++ function operator==(TRArray &, const Double_t &);
+#pragma link C++ function operator==(TRArray &, const TRArray &);
+#pragma link C++ function operator<<(ostream &, const TRArray &);
+#pragma link C++ function operator>>(istream &, TRArray &);
+
+#pragma link C++ function operator<<(ostream &, const TRMatrix &);
+#pragma link C++ function operator<<(ostream &, const TRSymMatrix &);
+#pragma link C++ function operator<<(ostream &, const TRVector &);
