8e0e01b83acb2634fd40e3d7ba0ec77c7abeb0cf
cvs2git <cvs2git>
2004-11-24 00:08:36 +0000

    This commit was manufactured by cvs2git to create tag 'SL04k_a'.

--- StRoot/Sti/StiTrackNode.cxx
+++
@@ -1,6 +0,0 @@
-#include "Stiostream.h"
-#include "StiHit.h"
-#include "Sti/Base/Messenger.h"
-#include "StiDetector.h"
-
-
--- StRoot/macros/test/RootIORead.C
+++
@@ -1,9 +0,0 @@
-{ 
- gSystem.Load("St_base.so");
- gSystem.Load("St_Tables.so");
- TFile f("/star/tof/fine/auau_ce_b0-2_4041_4060.root");
- gBenchmark->Start("read"); 
- f.ReadAll();
- gBenchmark->Show("read");
-}
-
