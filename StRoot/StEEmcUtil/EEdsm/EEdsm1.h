7150aa7123b233395db464532c16013adeda74dc
balewski <>
2004-11-29 20:12:59 +0000

    first

---
+++ StRoot/StEEmcUtil/EEdsm/EEdsm1.h
@@ -0,0 +1,47 @@
+#ifndef EEdsm1_h
+#define EEdsm1_h
+/**************************************************************
+ * $Id: EEdsm1.h,v 1.1 2004/11/29 20:12:59 balewski Exp $
+ **************************************************************/
+
+class EEdsm1  {
+  static const int nc=6;
+  ushort data[nc];
+  int type;
+  
+ private:
+ public:
+  
+  EEdsm1();
+  virtual ~EEdsm1();
+  void  print(int k=0);
+  void  clear();
+  void setWord(int ch, ushort val);
+  int getNc(){return nc;}
+  ushort getTPsum(int ch);
+  ushort getHTthr(int ch);
+  void setType(int t) {type=t;}
+  
+};
+#endif
+
+
+/* container for STAR trigger data
+ * $Log: EEdsm1.h,v $
+ * Revision 1.1  2004/11/29 20:12:59  balewski
+ * first
+ *
+ * Revision 1.2  2004/04/23 20:16:56  balewski
+ * trig patch fix
+ *
+ * Revision 1.1  2004/02/17 03:09:17  balewski
+ * *** empty log message ***
+ *
+ * Revision 1.1  2003/05/22 19:39:00  balewski
+ * analysis of DSM data
+ *
+ * 
+ * decode INPUT data for one board of EE-DSM Layer-1
+ *
+ **************************************************************/
+
---
+++ StRoot/StEEmcUtil/EEdsm/EEdsm2.cxx
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <assert.h>
+
+
+#include "EEdsm2.h"
+
+//--------------------------------------------------
+//
+//--------------------------------------------------
+EEdsm2 ::  EEdsm2() {
+  clear();
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+EEdsm2 ::  ~EEdsm2() { }
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EEdsm2 :: clear() {
+  memset(data,0,sizeof(data));
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EEdsm2::setWord(int ch, ushort val){
+  assert(ch>=0 && ch<nc);
+  data[ch]=val;
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+ushort EEdsm2::getJPthr(int jp) { 
+  assert(jp>=0 && jp<6);
+  int ch= jp/3;
+  int i=jp%3;
+  assert(ch>=0 && ch<nc);
+  ushort val=data[ch]>>8;
+  val=val>>(2*i);
+  val=val & 3;
+  return val;
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+ushort EEdsm2::get3JPHTthr(int i3p) { 
+  int ch=i3p;
+  assert(ch>=0 && ch<nc);
+  ushort val=data[ch]>>14;
+  val=val & 3;
+  return val;
+}
+
+//--------------------------------------------------
+//--------------------------------------------------
+ushort EEdsm2::get3JPsum(int i3p) { 
+
+  int ch= i3p;
+  assert(ch>=0 && ch<nc);
+  ushort val=data[ch]&0xff;
+  return val;
+}
+
+
+//--------------------------------------------------
+//--------------------------------------------------
+void EEdsm2 :: print( int k) {
+  printf("EEdsm2:: print()  \n");
+  int i;
+
+  printf("\nch    = ");
+  for(i=nc-1;i>=0;i--) printf("  %4d ",i); 
+  printf("\nword  = ");
+  for(i=nc-1;i>=0;i--) printf(" x%4.4x ", data[i] );
+
+  printf("\nJP_Falk =");
+  for(i=njp-1;i>=0;i--) printf("  %4d ",i); 
+  printf("\nJP_Steve=");
+  for(i=njp-1;i>=0; i--) printf("  %4d ",(i+2)%6+1);
+
+  printf("\nJPthr   =");
+  for(i=njp-1;i>=0;i--) printf("  %4d ", getJPthr(i));
+  printf("\n");
+
+  for(i=0;i<2;i++){
+    printf("3x.9 JP_Falk(%d+%d+%d) energy/dec:  8bit=%d  HTthr=%d\n",3*i,3*i+1,3*i+2,get3JPsum(i), get3JPHTthr(i));
+  }
+}
+ 
+// $Log: EEdsm2.cxx,v $
+// Revision 1.1  2004/11/29 20:12:59  balewski
+// first
+//
+// Revision 1.1  2004/02/17 03:09:17  balewski
+// *** empty log message ***
+//
+
