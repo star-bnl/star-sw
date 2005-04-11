3c8763689ed0cd6b32cd2698729f7996a005d361
perev <>
2005-04-11 17:40:16 +0000

    Debug stuff added

---
+++ StRoot/Sti/StiDebug.h
@@ -0,0 +1,18 @@
+/** 
+ * @file  StiDebug.h
+ */
+#ifndef StiDebug_H
+#define StiDebug_H 1
+
+/** 
+ * @class StiDebug
+ */
+class StiDebug 
+{
+public:
+static void Break(int kase);
+
+};
+
+#endif
+
