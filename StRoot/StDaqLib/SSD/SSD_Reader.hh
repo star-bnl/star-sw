be4dd0f5e764976396a91a5551439b8b9127e6ac
Jerome Lauret <jeromel@bnl.gov>
2005-04-23 12:24:44 +0000

    Year4/Year5 mapping fixes

--- StRoot/StDaqLib/SSD/SSD_Reader.hh
+++ StRoot/StDaqLib/SSD/SSD_Reader.hh
@@ -9,13 +9,18 @@ class SSD_Reader {
 
 public:
   SSD_Reader(EventReader *er);
-  int ssdData( int ladder, 
-             char eastWest,
-             int channel,
-             int& data  ,  // for ssd_data.mode=0 ie data 
-             int& ped   ,  // for ssd_data.mode=1 ie pedestal run
-             int& noise   // "      "        "     "       "
+  int ssdData( int  ladder, 
+	       char eastWest,
+	       int  channel,
+	       int &data  ,  // for ssd_data.mode=0 ie data 
+	       int &ped   ,  // for ssd_data.mode=1 ie pedestal run
+	       int &noise   // "      "        "     "       "
   );
+
+private:
+  int      SSDDaqLadder(char,int);
+  unsigned int LDate;
+  unsigned int LTime;
 
 };
 
