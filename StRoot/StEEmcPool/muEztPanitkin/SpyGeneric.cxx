5c115a37b7f59fff724db07b11affd0dbd80357f
Jan Balewski <balewski@lbl.gov>
2005-04-28 20:54:51 +0000

    start

---
+++ StRoot/StEEmcPool/muEztPanitkin/SpyGeneric.cxx
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <TH1.h>
+
+#include "SpyGeneric.h"
+
+//------------------------------
+//------------------------------
+SpyGeneric* 
+SpyGeneric::set(TH1 * h0, TString tx ){
+  h=h0;
+  text=tx;
+  text=tx;
+  return this;
+}
+
+
+ 
