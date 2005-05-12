15a867a0b3bb2d6f7adac33b6ff4904979d220a9
potekhin <>
2005-05-12 22:48:09 +0000

    Layer, module and plane accessors from Mirko

--- StRoot/StMcEvent/StMcFstHit.hh
+++ StRoot/StMcEvent/StMcFstHit.hh
@@ -1,7 +1,10 @@
 /***************************************************************************
  *
- * $Id: StMcFstHit.hh,v 2.1 2005/04/18 20:11:33 calderon Exp $
+ * $Id: StMcFstHit.hh,v 2.2 2005/05/12 22:48:09 potekhin Exp $
  * $Log: StMcFstHit.hh,v $
+ * Revision 2.2  2005/05/12 22:48:09  potekhin
+ * Layer, module and plane accessors from Mirko
+ *
  * Revision 2.1  2005/04/18 20:11:33  calderon
  * Addition of Fgt and Fst files.  Modified other files to accomodate changes.
  *
@@ -22,18 +25,21 @@ class g2t_fst_hit_st;
 
 class StMcFstHit : public StMcHit {
 public:
-    StMcFstHit();
-    StMcFstHit(const StThreeVectorF&,const StThreeVectorF&,
-	       const float, const float, const long, const long, StMcTrack*);
-    StMcFstHit(g2t_fst_hit_st*);
-    ~StMcFstHit();
+  StMcFstHit();
+  StMcFstHit(const StThreeVectorF&,const StThreeVectorF&,
+	     const float, const float, const long, const long, StMcTrack*);
+  StMcFstHit(g2t_fst_hit_st*);
+  ~StMcFstHit();
 
-    void* operator new(size_t)     { return mPool.alloc(); }
-    void  operator delete(void* p) { mPool.free(p); }
+  void* operator new(size_t)     { return mPool.alloc(); }
+  void  operator delete(void* p) { mPool.free(p); }
 
-    unsigned long layer() const; // 
-    unsigned long ladder() const; // 
-    
+  unsigned long layer() const; // 
+  unsigned long ladder() const; // 
+
+  unsigned long plane() const; //
+  unsigned long module() const;  //  
+   
 private:
 
     static StMemoryPool mPool; //!
