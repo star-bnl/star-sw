7ce4a89f23dee91bb179c3f4d4a04b8f84c98187
Maxim Potekhin <potekhin@bnl.gov>
2005-05-12 22:48:38 +0000

    Layer, plane and module accessors from Mirko

--- StRoot/StMcEvent/StMcFstHit.cc
+++ StRoot/StMcEvent/StMcFstHit.cc
@@ -1,7 +1,10 @@
 /***************************************************************************
  *
- * $Id: StMcFstHit.cc,v 2.1 2005/04/18 20:11:33 calderon Exp $
+ * $Id: StMcFstHit.cc,v 2.2 2005/05/12 22:48:38 potekhin Exp $
  * $Log: StMcFstHit.cc,v $
+ * Revision 2.2  2005/05/12 22:48:38  potekhin
+ * Layer, plane and module accessors from Mirko
+ *
  * Revision 2.1  2005/04/18 20:11:33  calderon
  * Addition of Fgt and Fst files.  Modified other files to accomodate changes.
  *
@@ -12,7 +15,7 @@
 #include "StMcFstHit.hh"
 #include "tables/St_g2t_fst_hit_Table.h" 
 
-static const char rcsid[] = "$Id: StMcFstHit.cc,v 2.1 2005/04/18 20:11:33 calderon Exp $";
+static const char rcsid[] = "$Id: StMcFstHit.cc,v 2.2 2005/05/12 22:48:38 potekhin Exp $";
 
 ClassImp(StMcFstHit)
     
@@ -47,9 +50,32 @@ ostream&  operator<<(ostream& os, const StMcFstHit& h)
 unsigned long
 StMcFstHit::layer() const
 {    
-  unsigned long iModule = mVolumeId/1000000;
+  //  unsigned long iModule = mVolumeId/1000000;
   unsigned long iLayer = 0;
-  if (0 < iModule && iModule <=11)
+  iLayer=(mVolumeId/100)%10;
+ cout << "VolId=" << mVolumeId  << endl;
+ cout << "iLayer=" << iLayer  << endl;
+  return iLayer;
+}
+unsigned long
+StMcFstHit::plane() const
+{    
+  //  unsigned long iModule = mVolumeId/1000000;
+  unsigned long iPlane = 0;
+  iPlane=(mVolumeId/100)%10;
+ cout << "VolId=" << mVolumeId  << endl;
+ cout << "iPlane=" << iPlane  << endl;
+  return iPlane;
+}
+
+unsigned long
+StMcFstHit::module() const
+{    
+  //  unsigned long iModule = mVolumeId/1000000;
+  unsigned long imodule = 0;
+  imodule=(mVolumeId/100)%10;
+#if 0 
+ if (0 < iModule && iModule <=11)
     {
       iLayer = 1;
     }
@@ -66,15 +92,20 @@ StMcFstHit::layer() const
       cout << "StMcFstHit::layer() -E- volumeId not known!" << endl;
       iLayer = 10000; 
     }
-  
-  return iLayer;
+ 
+#endif 
+ cout << "VolId=" << mVolumeId  << endl;
+ cout << "imodule=" << imodule  << endl;
+  return imodule;
 }
 
 unsigned long
 StMcFstHit::ladder() const
 {
-  unsigned long iModule = mVolumeId/1000000;
+  //  unsigned long iModule = mVolumeId/1000000;
   unsigned long iLadder = 0;
+  iLadder=(mVolumeId/1000000)%10;
+#if 0
   if (0 < iModule && iModule <=11)
     {
       iLadder = iModule;
@@ -92,6 +123,8 @@ StMcFstHit::ladder() const
       cout << "StMcFstHit::ladder() -E- volumeId not known!" << endl;
       iLadder = 10000; 
     }
-  
+#endif  
+  cout << "VolId=" << mVolumeId  << endl;
+ cout << "iLadder=" << iLadder  << endl;
   return iLadder;
 }
