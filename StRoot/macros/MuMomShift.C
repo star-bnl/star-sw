e15093344ab065062423f0d2f930fce7041768cb
jeromel <>
2005-05-13 19:25:53 +0000

    Quick and dirty trick for CRS system caveat

--- StRoot/macros/MuMomShift.C
+++ StRoot/macros/MuMomShift.C
@@ -3,9 +3,17 @@ MuMomShift(const Int_t   mode=0,
            const Char_t  *path="/star/data13/reco/dev/2001/10/",
            const Char_t  *file="st_physics_2304060_raw_0303.event.root",
            const Char_t* outDir="./"){
+
+  Char_t *tpath = "";
+
   gROOT->Macro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
+  // there is something stupid about the CRS system. It is not
+  // capable of passing empty string argument. So, we use "-"
+  // as a replacement for "".
+  if ( path != "-") tpath = path;
+
   StChain *chain=new StChain();  // Need chain for communication between makers
-  StMuDstMaker *mudst_mk=new StMuDstMaker(mode,0,path,file);
+  StMuDstMaker *mudst_mk=new StMuDstMaker(mode,0,tpath,file);
 
   StMuMomentumShiftMaker *mom_mk=new StMuMomentumShiftMaker(outDir);
   
