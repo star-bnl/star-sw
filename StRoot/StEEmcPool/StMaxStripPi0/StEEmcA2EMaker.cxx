a2c8789f1da9a8bc62f172945275e68099bb9743
jwebb <>
2005-06-10 17:42:32 +0000

    Traps and avoids missing StEmcDetectors when data source is StEvent.

--- StRoot/StEEmcPool/StMaxStripPi0/StEEmcA2EMaker.cxx
+++ StRoot/StEEmcPool/StMaxStripPi0/StEEmcA2EMaker.cxx
@@ -333,6 +333,10 @@ void StEEmcA2EMaker::addTowerHit( Int_t sec, Int_t sub, Int_t eta, Float_t adc, 
   mHitTowers[layer].push_back( mTowers[index][layer] );
   mEnergy[sec][layer] += energy;
 
+#if 0
+  mTowers[index][layer].print();
+#endif  
+
 }
 //---
 void StEEmcA2EMaker::addSmdHit( Int_t sec, Int_t plane, Int_t strip, Float_t adc )
@@ -440,7 +444,11 @@ Bool_t StEEmcA2EMaker::fillFromSt( StEmcCollection *emc )
   /// Set StEmcRawHit pointers in StEEmcTower, towers
   ///
   StEmcDetector *detector=emc->detector(kEndcapEmcTowerId);
-  assert(detector);
+  if ( !detector )
+    {
+      Warning("fillFromSt","\n**********\nemc->detector() NULL for eemc towers.  MAJOR PROBLEM, trying to procede.\n**********\n\n");
+      return false;
+    }
 
   for ( UInt_t sec = 0; sec < detector -> numberOfModules(); sec++ )
     {
@@ -467,9 +475,9 @@ Bool_t StEEmcA2EMaker::fillFromSt( StEmcCollection *emc )
 	  assert(isubsector>=0 && isubsector<5);
 	  assert(ietabin>=0 && ietabin<12);
 
-	  Int_t iphibin = 5*isector+isubsector;
+	  Int_t iphibin = 5 * isector + isubsector;
 	  assert(iphibin>=0 && iphibin<60);
-	  Int_t index = 12*iphibin + ietabin;
+	  Int_t index = 12 * iphibin + ietabin;
 	  assert(index>=0 && index<720);
 
 #if 0
@@ -522,9 +530,13 @@ Bool_t StEEmcA2EMaker::fillFromSt( StEmcCollection *emc )
   ///
 
   detector=emc->detector(kEndcapEmcPreShowerId);
-  assert(detector);
-  
-  for ( UInt_t sec = 0; sec < detector -> numberOfModules(); sec++ )
+
+  if ( !detector )
+    {
+      Warning("fillFromSt","\n**********\nemc->detector() NULL for eemc pre/post.  MAJOR PROBLEM, trying to procede.\n**********\n\n");
+    }
+  else
+    for ( UInt_t sec = 0; sec < detector -> numberOfModules(); sec++ )
     {
 
       /// Remember to watch out for fortran holdovers in
@@ -573,8 +585,12 @@ Bool_t StEEmcA2EMaker::fillFromSt( StEmcCollection *emc )
 
       /// Get the eemc smd collections
       detector=emc->detector( ids[iplane] );
-
-      for ( UInt_t sec = 0; sec < detector -> numberOfModules(); sec++ )
+      if ( !detector )
+	{
+	  Warning("fillFromSt","\n**********\nemc->detector() NULL for esmd plane.  MAJOR PROBLEM, trying to procede.\n**********\n\n");
+	}
+      else
+	for ( UInt_t sec = 0; sec < detector -> numberOfModules(); sec++ )
 	{
 	  
 	  /// Remember to watch out for fortran holdovers in
