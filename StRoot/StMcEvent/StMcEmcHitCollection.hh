2a0f6bb313dedf3a768166edbf37fad78f22f8cd
fine <>
2005-06-28 18:06:41 +0000

    Remove the redundant data-member StMcEmcModuleHitCollection mModules[mNumberOfModules] causing the crash duw double destruction of one and the same object

--- StRoot/StMcEvent/StMcEmcHitCollection.hh
+++ StRoot/StMcEvent/StMcEmcHitCollection.hh
@@ -1,7 +1,10 @@
 /***************************************************************************
  *
- * $Id: StMcEmcHitCollection.hh,v 2.5 2005/01/27 23:40:47 calderon Exp $
+ * $Id: StMcEmcHitCollection.hh,v 2.6 2005/06/28 18:06:41 fine Exp $
  * $Log: StMcEmcHitCollection.hh,v $
+ * Revision 2.6  2005/06/28 18:06:41  fine
+ * Remove the redundant data-member StMcEmcModuleHitCollection mModules[mNumberOfModules] causing the crash duw double destruction of one and the same object
+ *
  * Revision 2.5  2005/01/27 23:40:47  calderon
  * Adding persistency to StMcEvent as a step for Virtual MonteCarlo.
  *
@@ -41,14 +44,29 @@ public:
     StMcEmcModuleHitCollection*       module(unsigned int m);
     const StMcEmcModuleHitCollection* module(unsigned int m) const;
 
+    StMcEmcModuleHitCollection&       Module(unsigned int m);
+    const StMcEmcModuleHitCollection& Module(unsigned int m) const;
+    
     virtual Bool_t IsFolder() const {return kTRUE;} // It is a directory for modules 
     virtual void Browse(TBrowser *b);
     void    print();
-
+protected:
+    void MakeHitCollection();
+    StMcEmcModuleHitCollection&       thisModule(unsigned int m);
+    const StMcEmcModuleHitCollection& thisModule(unsigned int m) const;
 private:
     enum {mNumberOfModules=120};
-    StMcEmcModuleHitCollection mModules[mNumberOfModules];
     ClassDef(StMcEmcHitCollection,1)
 };
 
+inline       StMcEmcModuleHitCollection& StMcEmcHitCollection::thisModule(unsigned int m)
+{
+   TObjArray &modules = *GetObjArray();
+   return *(StMcEmcModuleHitCollection *)modules[m];
+}
+inline const StMcEmcModuleHitCollection& StMcEmcHitCollection::thisModule(unsigned int m) const
+{    
+   TObjArray &modules = *GetObjArray();
+   return *(StMcEmcModuleHitCollection *)modules[m];
+}
 #endif
