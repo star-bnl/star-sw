0acafd3345f44bc132d748d17d0239a8ab3c249e
fisyak <>
2005-07-06 22:25:17 +0000

    use templated StHelixD

--- StRoot/StTableUtilities/St_dst_trackC.h
+++ StRoot/StTableUtilities/St_dst_trackC.h
@@ -1,4 +1,5 @@
 #include "TChair.h"
+#include "StDetectorId.h"
 #include "tables/St_dst_track_Table.h"
 
 class St_dst_trackC : public TChair
@@ -9,12 +10,15 @@ class St_dst_trackC : public TChair
     St_dst_trackC ();
     St_dst_trackC (St_dst_track *table);
     Double_t AbsMoment(Int_t i=-1);
+    StDetectorId detector(Int_t i=-1) const;
     const Double_t *momentum(Int_t i=-1);
     Int_t GetCurrentIndex(){return fLastIndx;}
     ClassDefChair(St_dst_track, dst_track_st )
     ClassDef(St_dst_trackC,1) //C++ TChair for dst_track table class
 };
 
+//_____________________________________________________________________________
+inline StDetectorId St_dst_trackC::detector(Int_t i) const { return (StDetectorId) GetTable(i)->det_id; }
 //_____________________________________________________________________________
 inline Double_t St_dst_trackC::AbsMoment(Int_t i){
    momentum(i); 
