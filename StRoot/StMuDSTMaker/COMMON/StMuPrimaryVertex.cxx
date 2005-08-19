2b99cb86c3a1bd2bafe6b352c1de29090634f202
Marco van Leeuwen <m.vanleeuwen1@uu.nl>
2005-08-19 19:46:06 +0000

    Further updates for multiple vertices. The main changes are: 1) StMudst::primaryTracks() now returns a list (TObjArray*) of tracks    belonging to the 'current' primary vertex. The index number of the    'current' vertex can be set using StMuDst::setCurrentVertex().    This also affects StMuDst::primaryTracks(int i) and    StMuDst::numberOfprimaryTracks(). 2) refMult is now stored for all vertices, in StMuPrimaryVertex. The    obvious way to access these numbers is from the StMuprimaryVertex structures,    but for ebakcward compatibility a function is provided in StMuEvent as well    (this is the only function taht works for existing MuDst)
    As an aside, I've also changes the internals of StMuDst::createStEvent and
    StMuDst::fixTrackIndices() to be able to deal with a larger range of index numbers for tracks as generated by Ittf.
    
    BIG FAT WARNING: StMudst2StEventMaker and StMuDstFilterMaker
    do not fully support the multiple vertex functionality yet.

--- StRoot/StMuDSTMaker/COMMON/StMuPrimaryVertex.cxx
+++ StRoot/StMuDSTMaker/COMMON/StMuPrimaryVertex.cxx
@@ -3,15 +3,17 @@
  * A simple class to store porimary vertex information
  * All functions are inline
  *
- * $Id: StMuPrimaryVertex.cxx,v 1.1 2005/07/15 21:45:09 mvl Exp $ 
+ * $Id: StMuPrimaryVertex.cxx,v 1.2 2005/08/19 19:46:05 mvl Exp $ 
  */
 
 #include "StMuPrimaryVertex.h"
 #include "StEvent/StPrimaryVertex.h"
+#include "StEventUtilities/StuRefMult.hh"
+#include "StEventUtilities/StuFtpcRefMult.hh"
 
 ClassImp(StMuPrimaryVertex)
   
-StMuPrimaryVertex::StMuPrimaryVertex(StPrimaryVertex *vertex) {
+StMuPrimaryVertex::StMuPrimaryVertex(const StPrimaryVertex*& vertex) {
   mPosition       = vertex->position();
   mPosError       = vertex->positionError();
   mVertexFinderId = vertex->vertexFinderId();
@@ -22,4 +24,9 @@ StMuPrimaryVertex::StMuPrimaryVertex(StPrimaryVertex *vertex) {
   mNEEMCMatch     = vertex->numMatchesWithEEMC();
   mNCrossCentralMembrane  = vertex->numTracksCrossingCentralMembrane();
   mSumTrackPt     = vertex->sumOfTrackPt();
+
+  mRefMultPos = uncorrectedNumberOfPositivePrimaries(vertex);
+  mRefMultNeg = uncorrectedNumberOfNegativePrimaries(vertex); 
+  mRefMultFtpcEast = uncorrectedNumberOfFtpcEastPrimaries(vertex);
+  mRefMultFtpcWest = uncorrectedNumberOfFtpcWestPrimaries(vertex); 
 }
