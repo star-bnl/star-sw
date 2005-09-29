b88e8e0e2a43e346811c13f58ab8222caa4c3585
msd <>
2005-09-29 17:40:31 +0000

    Changed empty analysis to create plots for determining centrality bins

--- StRoot/StEStructPool/AnalysisMaker/StEStructEmptyAnalysis.cxx
+++ StRoot/StEStructPool/AnalysisMaker/StEStructEmptyAnalysis.cxx
@@ -1,12 +1,15 @@
 /**********************************************************************
  *
- * $Id: StEStructEmptyAnalysis.cxx,v 1.2 2004/06/25 03:10:28 porter Exp $
+ * $Id: StEStructEmptyAnalysis.cxx,v 1.3 2005/09/29 17:40:30 msd Exp $
  *
  * Author: Jeff Porter 
  *
  **********************************************************************
  *
  * Description:  Empty analysis code for testing
+ *               Can replace StEStruct2ptCorrelations for an analysis that
+ *               reads events, applies all event and track (NOT pair) cuts,
+ *               and outputs a few histograms.  
  *
  **********************************************************************/
 #include "StEStructEmptyAnalysis.h"
@@ -54,16 +57,23 @@ StEStructEmptyAnalysis::StEStructEmptyAnalysis(): moutFileName(0) {
 
   }
 
-
+  hNEvent = new TH1F("hNEvent","dNevent/dNch",1200,0,1200);
+  hNEvent2 = new TH1F("hNEvent2","dNevent / dNch^{1/4} vs Nch^{1/4}",500,0,7);
+  
 
 };
 
 bool StEStructEmptyAnalysis::doEvent(StEStructEvent* event){
 
-
   if(!event) return false;
-  //  cout<<" in doEvent "<<endl;
 
+  int mult = event->Ntrack();
+  //cout << " doing event " << event->EventID() << " with mult " << mult << endl; //***
+  hNEvent->Fill(mult);
+  hNEvent2->Fill(pow(mult,0.25));  // hopefully this will do it...
+  
+  // just comment all this stuff out...
+  /*
   int id=event->Ntrack();
   if(id>=1){
 
@@ -104,6 +114,7 @@ bool StEStructEmptyAnalysis::doEvent(StEStructEvent* event){
     }
 
   }
+  */
 
   delete event;
   return true;
@@ -117,9 +128,33 @@ void StEStructEmptyAnalysis::finish(){
     return;
   }
 
+  // finish transform of hNevent2
+  /*
+  TH1F* htemp1 = (TH1F*)hNEvent->Clone("htemp1");
+  TH1F* htemp2 = (TH1F*)hNEvent2->Clone("htemp2");
+
+  htemp1->SetTitle("dNev/dNch^{1/4}");
+  htemp2->SetTitle("dNev/dNch^{1/4} vs Nch^{1/4}");
+
+  for(int i=1; i<=hNEvent2->GetNbinsX(); i++) {
+    float x1 = hNEvent2->GetBinCenter(i);  // x1 = n^1/4
+    float y1 = hNEvent2->GetBinContent(i); // y1 = dNev/dNch (or dNch^1/4 ?)
+    float y2 = 4*pow(x1, 3)*y1; // y2 = 4n^3/4 * y1 
+    htemp2->SetBinContent(i, y2);
+  }
+
+  for(int i=1; i<=hNEvent->GetNbinsX(); i++) {
+    float x1 = hNEvent->GetBinCenter(i);  // x1 = n;
+    float y1 = hNEvent->GetBinContent(i); // y1 = dNev/dNch
+    float y2 = 4*pow(x1, (float)0.75)*y1; // y2 = 4n^3/4 * y1
+    htemp1->SetBinContent(i, y2);
+  }
+  */
+
   TFile * tf=new TFile(moutFileName,"RECREATE");
   tf->cd();
 
+  /*
   for(int i=0;i<3;i++){
     for(int j=0;j<6;j++){
       etaMean[i][j]->Write();
@@ -127,12 +162,22 @@ void StEStructEmptyAnalysis::finish(){
       ytMean[i][j]->Write();
     }
   }
+  */
+
+  hNEvent->Write();
+  hNEvent2->Write();
+  //htemp1->Write();
+  //htemp2->Write();
+
   tf->Close();
 }
 
 /**********************************************************************
  *
  * $Log: StEStructEmptyAnalysis.cxx,v $
+ * Revision 1.3  2005/09/29 17:40:30  msd
+ * Changed empty analysis to create plots for determining centrality bins
+ *
  * Revision 1.2  2004/06/25 03:10:28  porter
  * added a new common statistics output and added electron cut with momentum slices
  *
