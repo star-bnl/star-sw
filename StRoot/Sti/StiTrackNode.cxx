108af5c96922ec2ba88bf8c1ad0b8689c3ad98f4
Victor Perevoztchikov <perev@bnl.gov>
2005-12-07 22:54:11 +0000

    classes StiNodePars,StiNode2Pars,StiNodeErrs,StiNodeMtx added

--- StRoot/Sti/StiTrackNode.cxx
+++ StRoot/Sti/StiTrackNode.cxx
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include "StiTrackNode.h"
 int StiTrackNode::mgFlag=0;
+  static const int idx66[6][6] =
+  {{ 0, 1, 3, 6,10,15},{ 1, 2, 4, 7,11,16},{ 3, 4, 5, 8,12,17}
+  ,{ 6, 7, 8, 9,13,18},{10,11,12,13,14,19},{15,16,17,18,19,20}};
 
 //______________________________________________________________________________
 void StiTrackNode::errPropag6( double G[21],const double F[6][6],int nF )
 {
   enum {NP=6,NE=21};
-  static const int idx66[6][6] =
-  {{ 0, 1, 3, 6,10,15},{ 1, 2, 4, 7,11,16},{ 3, 4, 5, 8,12,17}
-  ,{ 6, 7, 8, 9,13,18},{10,11,12,13,14,19},{15,16,17,18,19,20}};
 
   double g[NE];      memcpy(g,    G,sizeof( g));
   double fg[NP][NP]; memset(fg[0],0,sizeof(fg));
@@ -44,18 +44,18 @@ void StiHitContino::reset()
    mChi2[0]=1e61;
 }
 //______________________________________________________________________________
-void StiHitContino::add(StiHit *hit,double chi2)
+void StiHitContino::add(StiHit *hit,double chi2,double detr)
 {
    int i=0;
    for (;i<kMaxSize;i++) {
      if (!mHits[i]) 		break;
      if (chi2 > mChi2[i]) 	continue;
      for (int jr = kMaxSize-2;jr>=i;jr--) 
-       {mHits[jr+1]=mHits[jr]; mChi2[jr+1]=mChi2[jr];}
+       {mHits[jr+1]=mHits[jr]; mChi2[jr+1]=mChi2[jr];mDetr[jr+1]=mDetr[jr];}
      				break;
    }
    if (i>=kMaxSize) 		return;
-   mHits[i]=hit; mChi2[i]=chi2; 
+   mHits[i]=hit; mChi2[i]=chi2;mDetr[i]=detr;
 }
 //______________________________________________________________________________
 void StiHitContino::print(const char* tit) const
@@ -170,3 +170,119 @@ double StiTrackNode::getRefPosition() const
   assert(place);
   return place->getLayerAngle();
 }
+
+//______________________________________________________________________________
+double StiNodeErrs::operator()(int i,int j) const
+{
+  return A[idx66[i][j]];
+}
+//______________________________________________________________________________
+StiNodeErrs &StiNodeErrs::merge(double wt,StiNodeErrs &other)
+{
+   double wt0 = 1.-wt;
+   for (int i=0;i<kNErrs;i++) {A[i] = wt0*A[i] + wt*other.A[i];}
+   return *this;
+}
+//______________________________________________________________________________
+void StiNodeErrs::recov(const double *maxerr) 
+{
+static const double CORRMAX  = 0.999;
+static const double CORRMAX2 = CORRMAX*CORRMAX;
+
+  if (maxerr) {
+    for (int i=0;i<kNPars;i++) {
+      double maxDia = maxerr[i]*maxerr[i];
+      int ld = idx66[i][i];
+      if (A[ld]<0) {
+	 printf("StiNodeErrs::cut. Negative igonal %g(%d)\n",A[ld],i);
+	 print();
+	 A[ld] = maxDia;
+      }
+      if (A[ld]<maxDia) continue;
+      double fak = sqrt(maxDia/A[ld]);
+      for (int j=0;j<kNPars;j++) {A[idx66[i][j]]*=fak;}
+    }
+  }
+    for (int i=0;i<kNPars;i++) {
+      double aii = A[idx66[i][i]];
+      for (int j=0;j<kNPars;j++) {
+        if (i==j) continue;
+        double ajj = A[idx66[j][j]];
+        double &aij = A[idx66[i][j]];
+        if (aij*aij <= aii*ajj*CORRMAX2) continue;
+        if (aij*aij > aii*ajj){
+          printf("StiNodeErrs::recov : Correlation too big %g[%d][%d]>%g\n"
+                ,aij,i,j,sqrt(aii*ajj));}	  
+        double tmp = sqrt(aii*ajj)*CORRMAX;
+        aij = (aij>0)? tmp:-tmp;
+      }//end j
+    }//end i
+
+}
+//______________________________________________________________________________
+void StiNodeErrs::print() const
+{
+   const double *d = A;
+   for (int n=1;n<=6;n++) {
+     printf("%d - ",n);
+     for (int i=0;i<n;i++){printf("%g\t",*(d++));}; printf("\n");
+   }  
+}     
+
+//______________________________________________________________________________
+int StiNodeErrs::check(const char *pri) const
+{
+  int i,j,kase=0;
+  double aii,ajj,aij;
+  for (i=0;i<kNPars;i++) {
+    aii = A[idx66[i][i]];
+    if (aii<0) {kase = 1; break;}
+    if (aii<=0) continue;
+    for (j=0;j<kNPars ;j++) {
+      if (i==j) continue;
+      ajj = A[idx66[j][j]];
+      if (ajj<=0) continue;
+      aij = A[idx66[i][j]];
+      if ((aij*aij)> 0.9999*aii*ajj) {kase = 2; break;}
+    }
+    if (kase) break;
+  }  
+  if (!kase || !pri) return kase;
+  switch(kase) {
+  
+    case 1: printf("StiNodeErrs::check(%s) FAILED: Negative diagonal %g[%d][%d]\n",pri,aii,i,i);  
+          break;
+    case 2: printf("StiNodeErrs::check(%s) FAILED: Correlation too big %g[%d][%d]>%g\n"
+                ,pri,aij,i,j,sqrt(aii*ajj)); 	  
+  }    
+  return kase;
+}  
+//______________________________________________________________________________
+int StiNodePars::check(const char *pri) const
+{
+static double MAXPARS[]={200,200,210,1.5,0.2,100};
+  int ierr=0;
+  for (int i=0;i<kNPars;i++) {if (fabs(P[i]) > MAXPARS[i]) {ierr = i+1 ; break;}} 
+  if(ierr) goto FAILED;
+  for (int i=-2;i<0;i++)     {if (fabs(P[i]) > 1.)         {ierr = i+12; break;}} 
+FAILED: 
+  if (!ierr || !pri) return ierr;
+  printf("StiNodePars::check(%s) == FAILED(%d)\n",pri,ierr);
+  print();
+  return ierr;
+} 
+//______________________________________________________________________________
+StiNodePars &StiNodePars::merge(double wt,StiNodePars &other)
+{
+   double wt0 = 1.-wt;
+   for (int i=0;i<kNPars;i++) {P[i] = wt0*P[i] + wt*other.P[i];}
+   ready();
+   return *this;
+}
+//______________________________________________________________________________
+void StiNodePars::print() const
+{
+static const char* tit[]={"cosCA","sinCA","X","Y","Z","Eta","Curv","TanL",0};
+  for (int i=-2;i<kNPars;i++) {printf("%s = %g, ",tit[i+2],P[i]);}
+  printf("\n");
+}   
