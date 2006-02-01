f9e454d94914ec4450ddef8d4505191eecc44514
fine <>
2006-02-01 02:08:36 +0000

    update the job traking table  definition

--- StRoot/StStarLogger/mysql/CreateJobTable.sql
+++ StRoot/StStarLogger/mysql/CreateJobTable.sql
@@ -1,21 +1,21 @@
-# $Id: CreateJobTable.sql,v 1.3 2006/01/27 23:54:10 fine Exp $
+# $Id: CreateJobTable.sql,v 1.4 2006/02/01 02:08:36 fine Exp $
 # Author: Valeri Fine (fine@bnl.gov) 26.01.2006
 # Create the job description table
 
 use logger;
 CREATE TABLE JobDescription (
-                 id              MEDIUMINT NOT NULL AUTO_INCREMENT  KEY COMMENT 'Unique record id',
-                 time            TIMESTAMP                          COMMENT 'current time'   ,
-                 hostid          CHAR(20)                           COMMENT 'computer name'  ,  
-                 JobUser         CHAR(20)                           COMMENT 'user name $USER',
+                 id              INT NOT NULL AUTO_INCREMENT        KEY COMMENT 'Unique record id',
+                 time            TIMESTAMP                          COMMENT 'Current time'   ,
+                 hostid          CHAR(20)                           COMMENT 'Computer name'  ,  
+                 JobUser         CHAR(20)                           COMMENT 'User name $USER',
                  SUMSJobId       CHAR(35)                           COMMENT 'SUMS $REQUESTID', 
                  SUMSProcessID   SMALLINT                           COMMENT 'SUMS $PROCESSID',
                  JobName         CHAR(20)                           COMMENT 'Job name'       ,
                  JobDescription  CHAR(20)                           COMMENT 'Job descriptor' ,
-                 StepName        CHAR(20)                           COMMENT 'Step name'      ,  
+                 StepName        CHAR(20)                           COMMENT 'Step name'      ,  #STAR maker name
                  StepEventId     ENUM('Start','Finish') NULL        COMMENT 'Event Id'       ,
-                 StepEventValue  ENUM('Ok','Failed')    NULL        COMMENT 'Event outcome'  ,
-                 StepConetxt     CHAR(10)                           COMMENT 'Event context'  ,
+                 StepEventValue  ENUM('Ok','Failed')    NULL        COMMENT 'Event outcome'  ,  #STAR Event return code
+                 StepContext     CHAR(10)                           COMMENT 'Event context'  ,  #Field name 
                  MessageId       ENUM('=')              NULL        COMMENT 'Extra message flag',
                  ProgrammMessage VARCHAR(120)                       COMMENT 'Extra Message'
               );
