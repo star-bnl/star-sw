7635c05e7462bc40338cf10bfac8c6464f83cb37
fisyak <>
2006-03-08 16:02:15 +0000

    Lilian's alignment from 20 Feb 2006, used for Pass2

--- StarDb/Geometry/ssd/SsdBarrelOnGlobal.C
+++ StarDb/Geometry/ssd/SsdBarrelOnGlobal.C
@@ -4,7 +4,7 @@ TDataSet *CreateTable() {
 // Positioning of the SSD barrel in the STAR global coordinate system
   if (!gROOT->GetClass("St_Survey")) return 0;
   Survey_st row[1] = {
-    { 0,  0.999838, 0.018000, 0.000000, -0.018000, 0.999838, 0.000000, 0.000000, 0.000000, 1.000000, -0.399300, 0.021700, -0.090000, 0.100000, 0.100000, 0.100000, 0.100000, 0.100000, 0.100000,"     Deduced from the tracking"},
+    { 0,  0.999838, 0.018000, 0.000888, -0.018000, 0.999838, -0.000461, -0.000896, 0.000445, 1.000000, -0.399300, 0.021700, -0.090000, 0.100000, 0.100000, 0.100000, 0.100000, 0.100000, 0.100000,"     Deduced from the tracking"},
   };
   Int_t n = 1;
   St_Survey *tableSet = new St_Survey("SsdBarrelOnGlobal",n);
