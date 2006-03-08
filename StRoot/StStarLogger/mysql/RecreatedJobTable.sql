ede16eb8b650350d755247db2aba201e5acbd7a4
cvs2git <cvs2git>
2006-03-08 16:51:12 +0000

    This commit was manufactured by cvs2git to create tag 'SLMar09'.

--- StRoot/StStarLogger/mysql/RecreatedJobTable.sql
+++
@@ -1,6 +0,0 @@
-# $Id: RecreatedJobTable.sql,v 1.4 2006/02/03 02:36:12 fine Exp $
-# Author: Valeri Fine (fine@bnl.gov) 26.01.2006
-# Recreate the job description table
-use logger; SHOW tables;
-DROP TABLE JobDescription, JobTracking;
-SHOW tables; SOURCE CreateJobTable.sql;
--- StRoot/StStarLogger/mysql/ShowJobTable.sql
+++
@@ -1,8 +0,0 @@
-# $Id: ShowJobTable.sql,v 1.2 2006/02/03 19:51:53 fine Exp $
-# Author: Valeri Fine (fine@bnl.gov) 26.01.2006
-# Show the job description table
-use logger;
-SHOW tables;
-SELECT * FROM JobDescription,JobTracking WHERE JobDescription.dataId=JobTracking.dataId;
-DESCRIBE  JobDescription;
-DESCRIBE  JobTracking;
