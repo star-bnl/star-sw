a1d84265df4d50b1a597e1fcfcf9af870319aa52
Janet Seyboth <jcs@mppmu.mpg.de>
2006-03-13 20:00:37 +0000

    commit initial version of the FTPC calibration maker

---
+++ StRoot/StFtpcCalibMaker/macros/lasertest_single.C
@@ -0,0 +1,75 @@
+void lasertest_single(TString filename,int ftpc, int lsec, int straight, int gfit, int minz, int maxz, int minrad, int maxrad,char* t0, char* gas, float mbfield)
+{
+
+  cout<<"Starting lasertest_single.C:"<<endl;
+  cout<<"                            filename = "<<filename<<".root"<<endl;
+  cout<<"                            ftpc     = "<<ftpc<<endl;
+  cout<<"                            lsec     = "<<lsec<<endl;
+  cout<<"                            straight = "<<straight<<endl;
+  cout<<"                            gfit     = "<<gfit<<endl;
+  cout<<"                            minz     = "<<minz<<endl;
+  cout<<"                            maxz     = "<<maxz<<endl;
+  cout<<"                            minrad   = "<<minrad<<endl;
+  cout<<"                            maxrad   = "<<maxrad<<endl;
+  cout<<"                            t0       = "<<t0<<endl;
+  cout<<"                            gas      = "<<gas<<endl;
+  cout<<"                            mbfield  = "<<mbfield<<endl;
+  cout<<endl;
+
+  if (gClassTable->GetID("TTable") < 0) gSystem->Load("libStar");
+
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+
+  gSystem->Load("libtpc_Tables");
+
+  gSystem->Load("StUtilities");
+  gSystem->Load("StarClassLibrary");
+  gSystem->Load("StEvent");
+  gSystem->Load("StarMagField");
+  gSystem->Load("StMagF");
+
+  gSystem->Load("StDbUtilities");
+  gSystem->Load("StDbLib.so");
+  gSystem->Load("StDbBroker.so");
+  gSystem->Load("libStDb_Tables.so");
+  gSystem->Load("St_db_Maker.so");
+  gSystem->Load("StTpcDb");
+
+  gSystem->Load("StFtpcCalibMaker");
+  gSystem->Load("libftpc_Tables");
+  gSystem->Load("StFtpcClusterMaker");
+  gSystem->Load("StFtpcTrackMaker");
+
+  if (atof(t0)!=0 || atof(gas)!=0)
+    {
+      //  Create the makers to be called by the current chain
+      const char *mysqlDB =  "MySQL:StarDb";
+      const char *paramsDB = "$STAR/StarDb";
+      //const char *paramsDB = "$PWD/StarDb";
+      
+      StChain *chain =  new StChain();
+
+     StFtpcCalibMaker *laser=new StFtpcCalibMaker();
+
+     laser->GetRunInfo(filename);
+     cout<<" date = "<<laser->Date()<<" time = "<<laser->Time()<<endl;
+      
+      St_db_Maker *dbMk = new St_db_Maker("db",mysqlDB,paramsDB);
+      dbMk->SetDateTime(laser->Date(),laser->Time());
+
+      dbMk->Init();
+      dbMk->Make();
+      
+      cout<<"dbDate = "<<dbMk->GetDateTime().GetDate()<<endl;
+      cout<<"After Database init !!!"<<endl;
+      cout<<endl;
+    }
+
+  if (atof(t0)!=0 || atof(gas)!=0)
+    laser->DbInit(mbfield);
+  
+  laser->DoLaserCalib(filename,ftpc,lsec,straight,gfit,minz,maxz,minrad,maxrad,t0,gas,mbfield);
+
+  delete laser;
+}
