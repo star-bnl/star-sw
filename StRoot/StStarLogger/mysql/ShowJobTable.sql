ccfb496745265072240d5f563948035cdcc0e2a9
fine <>
2006-03-30 03:19:58 +0000

    Adjust the ShowTable example

--- StRoot/StStarLogger/mysql/ShowJobTable.sql
+++ StRoot/StStarLogger/mysql/ShowJobTable.sql
@@ -1,8 +1,9 @@
-# $Id: ShowJobTable.sql,v 1.2 2006/02/03 19:51:53 fine Exp $
+# $Id: ShowJobTable.sql,v 1.3 2006/03/30 03:19:58 fine Exp $
 # Author: Valeri Fine (fine@bnl.gov) 26.01.2006
 # Show the job description table
 use logger;
 SHOW tables;
-SELECT * FROM JobDescription,JobTracking WHERE JobDescription.dataId=JobTracking.dataId;
+SELECT * FROM TaskDescription,JobDescription WHERE TaskDescription.taskId=JobDescription.taskId;
 DESCRIBE  JobDescription;
 DESCRIBE  JobTracking;
+DESCRIBE  TaskDescription;
