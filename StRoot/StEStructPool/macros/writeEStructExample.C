a9e2a3924a2e2e24b2ef217c7503b43de2f038d4
porter <>
2006-04-10 23:44:06 +0000

    Updated the writer example - tested it on pp and CuCu at 200

--- StRoot/StEStructPool/macros/writeEStructExample.C
+++ StRoot/StEStructPool/macros/writeEStructExample.C
@@ -1,134 +1,130 @@
 /************************************************************************
- * $Id: writeEStructExample.C,v 1.2 2004/04/15 18:46:34 msd Exp $
+ * $Id: writeEStructExample.C,v 1.3 2006/04/10 23:44:06 porter Exp $
  *
  * Author: Jeff Porter 
  *
  *  example code for reading in MuDst files contained int "fileListFile"
- *  and writing *.estruct.root files to 
+ *  and writng out estruct pico dsts using new format
  *
- *  outputDir/jobName/event/XX/
- *  
- *    where XX = event level selection imposed by the cut files.
- *               This example (from PP) contains 3 selections on event mult
- *
- *************************************************************************/
+ *****************************************************************/
 
-void writeEStructExample(const char* fileListFile, const char* outputDir, const char* jobName=0, int nset=3, int maxNumEvents=0){
+void writeEStructExample( const char* filelist,
+                   const char* outputDir,
+                   const char* scriptDir,
+                   int maxNumEvents = 0 ) {
 
-  // input cut files presumed to be in cwd or use full path 
-  char* evtCutFile[]={"PPCuts01.txt","PPCuts02.txt","PPCuts03.txt"};
 
-  // data and cut directories sub to outputDir/jobName 
-  char* datadirs[]  ={"event/01","event/02","event/03"};
-  char* cutdirs[]   ={"cuts/01","cuts/02","cuts/03"}; 
-
-  // in this example, all cuts (event, track ) are in same file
-  char* trackCutFile=evtCutFile[0];  
-
-  // libraries required and helper macros in $STAR/StRoot/StEStructPool/macros
   gROOT->LoadMacro("load2ptLibs.C");
   load2ptLibs();
-  gROOT->LoadMacro("getOutFileName.C");
 
+  char cutFile[1024];
+  sprintf(cutFile,"%s/CutsFile.txt",scriptDir);
 
-  // simple (global) centrality definition ...not persistant to event file.. 
-  // and not used in this particular example
+  gROOT->LoadMacro("getOutFileName.C"); // for laying out file names
+  const char* scratchDir = "CuCu200data";
+
+  // define centrality (just making stuff up below!!!)
   StEStructCentrality* cent=StEStructCentrality::Instance();
-  const double temp[4]={0,4,7,50};
-  cent->setCentralities(temp,4);
-
+  const double mbBins[]={2,30,100,180,240,320,400,999999}; 
+  int mbNBins=1+1+1+1+1+1+1+1;
+  cent->setCentralities(mbBins,mbNBins);
+  int numberOfAnalyses=mbNBins-1;
 
   // create the low-level reader (here for MuDst)
-  StMuDstMaker* mk = new StMuDstMaker(0,0,"",fileListFile,".",500); 
+  //  reader = reader interface + pointer to Data Maker + cut classes
+
+  //***** depends on PDSF vs RCF .... below is RCF version of scheduler
+  //  char fileListFile[1024];
+  //  sprintf(fileListFile,"%s/%s",scriptDir,filelist);
+  StMuDstMaker* mk = new StMuDstMaker(0,0,"",filelist,".",5000);
 
   // create the generic EStructAnalysisMaker
   StEStructAnalysisMaker *estructMaker = new StEStructAnalysisMaker("EStruct2pt");
 
-  // Set up the EStruct data Readers and Writer codes
+  // Set up the reader with event/track cuts
+  StEStructEventCuts*    ecuts = new StEStructEventCuts(cutFile);
+  StEStructTrackCuts*    tcuts = new StEStructTrackCuts(cutFile);
+  StEStructMuDstReader* reader = new StEStructMuDstReader(mk,ecuts,tcuts,false);
 
-  char* outputFile[3];
-  StEStructEventCuts* ecuts[3];
-  StEStructTrackCuts* tcuts[3];
-  StEStructEventWriter*  analysis[3];
-  StEStructEventMaker*   writeMakers[3];
-  StEStructMuDstReader* readers[3];
+  estructMaker->SetEventReader(reader);
 
-  // only 1 reader actually reads the event, the others just pull from mem.
-  // this 'skipMake' ensures this to be the case
-  bool skipMake[3]; 
-  skipMake[0]=false;
-  skipMake[1]=true;
-  skipMake[2]=true;
+  // create the QAHist object (must come after centrality and cutbin objects)
+  int EventType = 0; // real data
+  StEStructQAHists* qaHists = new StEStructQAHists(EventType);
+  qaHists->initHistograms();
+  estructMaker->SetQAHists(qaHists);
 
-  // just to keep root happy, we'll give each writer maker a diff. name
-  char* makerNames[]={"EStructEventMaker01","EStructEventMaker02","EStructEventMaker03"};
+  StEStructEventWriter** analysis    = new StEStructEventWriter*[mbNBins];
+  StEStructEventMaker**  writeMakers = new StEStructEventMaker*[mbNBins];
 
-  // build the 3 readers & 3 writers.
-  //  writer = analysis interface with a pointer to a StEStructEventMaker
-  //  reader = reader interface + pointer to StMuDstMaker + cut classes
-  for(int i=0;i<nset;i++){
+  int analysisMode = 0; // 2pt correlations mode selection
 
-     writeMakers[i]=new StEStructEventMaker(makerNames[i]);
-     outputFile[i]=getOutFileName(outputDir,jobName,datadirs[i]);
-     analysis[i]=new StEStructEventWriter(writeMakers[i]);
-     analysis[i]->setOutputFileName(outputFile[i]);
+  for(int i=0;i<numberOfAnalyses;i++){
+   int ic=i;
+   if(numberOfAnalyses==1)ic=-1;
 
-     ecuts[i]=new StEStructEventCuts(evtCutFile[i]);
-     tcuts[i]=new StEStructTrackCuts(trackCutFile);
-     readers[i]=new StEStructMuDstReader(mk,ecuts[i],tcuts[i],skipMake[i]);
-     estructMaker->SetReaderAnalysisPair(readers[i],analysis[i]);
+   TString writerName("EStructWriterMaker");  writerName+=i;
+   writeMakers[i]=new StEStructEventMaker(writerName.Data());
+   
+   analysis[i]=new StEStructEventWriter(writeMakers[i]);
+   analysis[i]->setOutputFileName(getOutFileName(outputDir,scratchDir,"event",ic));
+   /*
+   analysis[i]->setQAHists(qaHists); // only 1 QA Object in this case
+   analysis[i]->setZBuffLimits(ecuts); // common to all
+   */
+   analysis[i]->setAnalysisIndex(i);
   }
- 
-  //
+  estructMaker->SetAnalyses(analysis,numberOfAnalyses);
+
   // --- now do the work ---
-  //
 
-  int istat=0,i=1;
-  estructMaker->Init();
-  estructMaker->startTimer();
-  int counter=0;
-  while(istat!=2){
-      istat=estructMaker->Make();
+    estructMaker->Init();
+    estructMaker->startTimer();
+
+    int counter=0, istat=0, i=0;
+
+    while (istat!=2) {
+
+      istat=estructMaker->Make(); // now includes filling qa histograms
+
       i++; counter++;
-      if(counter==200){ 
-        cout<<"doing event ="<<i<<endl;
-        counter=0;
+      if (counter==200) {
+          cout<<"doing event ="<<i<<endl;
+          counter=0;
+          estructMaker->writeDiagnostics(0);
       }
-      if( maxNumEvents!=0 && i>=maxNumEvents )istat=2;
-  }
-  estructMaker->stopTimer();
+      if ( maxNumEvents!=0 && i>=maxNumEvents ) {
+          istat=2;
+      }
+    }
+    estructMaker->stopTimer();
 
-  // write stats to log file 
-  cout<<endl;
-  estructMaker->logAnalysisTime(cout);
-  estructMaker->logInputEvents(cout);
-  estructMaker->logOutputEvents(cout);
-  estructMaker->logOutputRate(cout);
+  //--- now write out stats and cuts ---
+   ofstream ofs(getOutFileName(outputDir,scratchDir,"stats"));
+    estructMaker->logAllStats(ofs);
+    ecuts->printCuts(ofs);
+    tcuts->printCuts(ofs);
+   ofs<<endl;
+   ofs.close();
+   
+   // --> root cut file 
+   TFile* tf=new TFile(getOutFileName(outputDir,scratchDir,"cuts"),"RECREATE");
+   ecuts->writeCutHists(tf);
+   tcuts->writeCutHists(tf);
+   tf->Close();
 
-  //
-  // --> cuts stats streamed to stdout (logfile)
-  //
-  for(int i=0;i<nset;i++){
-     ecuts[i]->printCuts(cout);
-     tcuts[i]->printCuts(cout);
 
-     //
-     // --> root cut file 
-     // 
-     char* rootCutFile=getOutFileName(outputDir,jobName,cutdirs[i]);
-     TFile* tf=new TFile(rootCutFile,"RECREATE");
-     ecuts[i]->writeCutHists(tf);
-     tcuts[i]->writeCutHists(tf);
-     tf->Close();
-  }
-
-  estructMaker->Finish();
+   // --- write out the data 
+   estructMaker->Finish();
 }
 
 
 /**********************************************************************
  *
  * $Log: writeEStructExample.C,v $
+ * Revision 1.3  2006/04/10 23:44:06  porter
+ * Updated the writer example - tested it on pp and CuCu at 200
+ *
  * Revision 1.2  2004/04/15 18:46:34  msd
  * Updated centrality variable types
  *
