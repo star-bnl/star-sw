bdf43ec9be7b0a25e338781cbbf52f46752b78ea
mvl <>
2006-05-04 21:04:35 +0000

    Additions for extra L3 information (from Jamie)

--- StRoot/StMuDSTMaker/COMMON/StMuTriggerIdCollection.h
+++ StRoot/StMuDSTMaker/COMMON/StMuTriggerIdCollection.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StMuTriggerIdCollection.h,v 1.3 2004/05/02 04:10:14 perev Exp $
+ * $Id: StMuTriggerIdCollection.h,v 1.4 2006/05/04 21:04:35 mvl Exp $
  *
  * Author: Frank Laue
  ***************************************************************************
@@ -10,6 +10,19 @@
  * in the muDst without loading of the libraries
  *
  ***************************************************************************/
+
+/*!
+ *                                                                     
+ * \class  StMuTriggerIdCollection
+ * \author Frank Laue
+ * \date   2003/02/20
+ * \brief  Collection of trigger ids as stored in MuDst
+ *
+ * This class stores the trigger ids for every event in the MuDst.
+ * Most users should only access the nominal(); everything else
+ * is for detailed understanding of the triggers.
+ *
+ */    
 #ifndef StMuTriggerIdCollection_hh
 #define StMuTriggerIdCollection_hh
 #include "StEvent/StTriggerId.h"
@@ -26,11 +39,13 @@ public:
     const StTriggerId& l1() const;
     const StTriggerId& l2() const;
     const StTriggerId& l3() const;
+    const StTriggerId& l3Expanded() const;
 
     void fill(const StTriggerIdCollection*);
     void setL1(const StTriggerId);
     void setL2(const StTriggerId);
     void setL3(const StTriggerId);
+    void setL3Expanded(const StTriggerId);
     void setNominal(const StTriggerId);
     
     static bool isEmpty(const StTriggerId&);
@@ -39,8 +54,9 @@ protected:
     StTriggerId  mL2TriggerId;
     StTriggerId  mL3TriggerId;
     StTriggerId  mNTriggerId;  // When unrolling the TBranch, it's name exceeds the maximum. So we changed mNominalTriggerId to mNTriggerId 
+    StTriggerId  mLETriggerId; // When unrolling the TBranch, it's name exceeds the maximum. So we changed mL3ExpandedTriggerId to mLETriggerId 
     
-    ClassDef(StMuTriggerIdCollection,1)
+    ClassDef(StMuTriggerIdCollection,2)
 };
 
 #endif 
@@ -48,6 +64,9 @@ protected:
 /**************************************************************************
  *
  * $Log: StMuTriggerIdCollection.h,v $
+ * Revision 1.4  2006/05/04 21:04:35  mvl
+ * Additions for extra L3 information (from Jamie)
+ *
  * Revision 1.3  2004/05/02 04:10:14  perev
  * private => protected
  *
