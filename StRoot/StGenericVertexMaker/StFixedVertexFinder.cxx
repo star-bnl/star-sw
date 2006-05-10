d8414f0f00bf0e295bf0780c86034fc495ccd3fd
jeromel <>
2006-05-10 14:35:00 +0000

    Changed VertexId to new enum, added doxygen doc

--- StRoot/StGenericVertexMaker/StFixedVertexFinder.cxx
+++ StRoot/StGenericVertexMaker/StFixedVertexFinder.cxx
@@ -23,18 +23,21 @@ StFixedVertexFinder::StFixedVertexFinder(){
     mMode=0;
 }
 
-int StFixedVertexFinder::fit(StEvent* event){
-
+int StFixedVertexFinder::fit(StEvent* event)
+{
+  StPrimaryVertex  prim;
+  StVertexFinderId VFId;
   Float_t cov[6] = {0.0,0.0,0.0,0.0,0.0,0.0}; // All errors are zero
-  StPrimaryVertex prim;
 
   if (mMode == 0){
     // Really the default whihc takes the SetPos() TBI
     LOG_DEBUG << "StFixedVertexFinder::fit() fixing a vertex" << endm;
     StThreeVectorD pos(mFixedX,mFixedY,mFixedZ);
     prim.setPosition(pos);
+    VFId = undefinedVertexFinder;
 
   } else {
+    
     LOG_DEBUG << "StFixedVertexFinder::fit() reading a vertex from StMcEvent" << endm;
     StMcEvent* mcEvent= (StMcEvent*) StMaker::GetChain()->GetDataSet("StMcEvent");
     
@@ -42,6 +45,8 @@ int StFixedVertexFinder::fit(StEvent* event){
       // Not sure what to do here, will leave and do nothing (so no vertex)
       // and it will be super obvious that something went terribly wrong
       LOG_ERROR << "StFixedVertexFinder::fit() no StMcEvent" << endm;
+      return 0;
+
     } else {
       StMcVertex *pv = mcEvent->primaryVertex();
       StThreeVectorD pos(pv->position().x(),
@@ -50,12 +55,13 @@ int StFixedVertexFinder::fit(StEvent* event){
       //cout << "DEBUG :: saving McVertex " << pos << endl;
       prim.setPosition(pos);
     }
+    VFId = mcEventVertexFFinder;
   }
 
   prim.setCovariantMatrix(cov);
-  prim.setFlag(1);              // So that we know its the primary vertex
-  prim.setRanking(-5);          // Have to have something
-  prim.setVertexFinderId(undefinedVertexFinder); // Unless/until this 'finder' gets its own id
+  prim.setFlag(1);                    // So that we know its the primary vertex
+  prim.setRanking(-5);                // Have to have something
+  prim.setVertexFinderId(VFId);       // Unless/until this 'finder' gets its own id
   addVertex(&prim);
 
   return size();
