671b4399725e6a015426377c93f561da3bef0e69
Jerome Lauret <jeromel@bnl.gov>
2006-05-10 14:35:00 +0000

    Changed VertexId to new enum, added doxygen doc

--- StRoot/StGenericVertexMaker/StFixedVertexFinder.h
+++ StRoot/StGenericVertexMaker/StFixedVertexFinder.h
@@ -3,8 +3,21 @@
  *  \author Lee Barnby (University of Birmingham) May 2006.
  *  \brief StGenericVertexFinder implementation for fixing vertex.
  *
- *  $Id: StFixedVertexFinder.h,v 1.1 2006/05/03 22:11:10 lbarnby Exp $
+ *  $Id: StFixedVertexFinder.h,v 1.2 2006/05/10 14:35:00 jeromel Exp $
  *
+ *  Modified J.Lauret for MC vertex
+ *
+ *  This vertex finder has two purposes
+ *  - The first mode of operation, turned on by the VFFV chain option,
+ *    allows to always set a vertex at a fixed position
+ *  - The second mode of operation is turned on by the VFMCE chain option
+ *    and allows instead to get the vertex from StMcEvent and not use a
+ *    a fixed vertex
+ *
+ *  Within this "finder" (or faker really), the rank is set to -5 and
+ *  the vertex finder Id is either respectively undefinedVertexFinder
+ *  and mcEventVertexFFinder. Covariant matrix parameters are all set
+ *  to zero for the lack of a better choice.
  */
 
 #ifndef STAR_StFixedVertexFinder
@@ -35,6 +48,9 @@ private:
 /***************************************************************************
 *
 * $Log: StFixedVertexFinder.h,v $
+* Revision 1.2  2006/05/10 14:35:00  jeromel
+* Changed VertexId to new enum, added doxygen doc
+*
 * Revision 1.1  2006/05/03 22:11:10  lbarnby
 * Initial version of fixed position vertex finder and option in maker to switch it on
 *
