5459b8afcbeb8b04b248a78c617a081cf8dd3797
Valeri Fine <val.fine@gmail.com>
2006-07-05 21:29:15 +0000

    update the default  STAR job tracking MySQL tables

--- StRoot/StStarLogger/mysql/RecreatedJobTable.sql
+++ StRoot/StStarLogger/mysql/RecreatedJobTable.sql
@@ -1,7 +1,13 @@
-# $Id: RecreatedJobTable.sql,v 1.6 2006/05/09 23:31:21 fine Exp $
+# $Id: RecreatedJobTable.sql,v 1.7 2006/07/05 21:29:15 fine Exp $
 # Author: Valeri Fine (fine@bnl.gov) 26.01.2006
 # Recreate the job description table
 use logger; SHOW tables;
-DROP TABLE TaskDescription, JobDescription, JobTracking;
+DROP TABLE BrokerDescriptionDictionary, 
+           TaskDescription            ,
+           JobDescription             ,
+           JobDescriptionFinish       ,
+           SequenceDictionary         ,
+           JobTracking           
+           ;
 SHOW tables; SOURCE CreateJobTable.sql;
 SHOW tables;
