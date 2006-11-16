6b4032cca3ab7f59362a3b22a892a1b3fae08c61
Valeri Fine <val.fine@gmail.com>
2006-11-16 21:44:15 +0000

    clean up

--- StRoot/macros/graphics/GeomBrowse.C
+++ StRoot/macros/graphics/GeomBrowse.C
@@ -3,21 +3,27 @@ StGeomBrowser *geoBrowser=0;
 //_________________________________________________________________________________________________________________
 void GeomDrawUsage() {
           printf("\n");
-          printf("Usage: root4star \'GeomBrowse.C(const char *geomDescriptor,Float_t bombFactor=2.0, const char *outRootFile="")\' \n");
+          printf("Usage: root4star \'GeomBrowse.C(const char *geomDescriptor\"complete\")\' \n");
           printf("-----  where \"geomDescriptor\" can be either \n");
           printf("        1. STAR geometry version like \"year2003\" (see: http://www.star.bnl.gov/STAR/comp/prod/MCGeometry.html) \n");
           printf("            example: root4star \'GeomDraw.C(\"year2003\")\'\n");                
-          printf("        2. the proper ZEBRA fz file, for example \n");
+          printf("\n");                
+          printf("        2. ROOT macros that instantiates the geometry object\n");
+          printf("            example: root4star \'GeomDraw.C(\"Geometry.root\")\'\n");                
+          printf("\n");                
+          printf("        3. ROOT file  that contains the geometry object\n");
+          printf("            example: root4star \'GeomDraw.C(\"$STAR/StarDb/VmcGeometry/Geometry.year2000.C\")\'\n");                
+          printf("\n");                
+          printf("        4. the proper ZEBRA fz file, for example \n");
           printf("            example: root4star \'GeomDraw.C(\"/star/u/potekhin/gstardata/y2003x_complete.fz \")\'\n\n"); 
-          printf("         \"Float_t bombFactor\" - the so-called \"bombFactor\" to get the exploiding view of the detector.\n");
-          printf("                                    To get the normal view the bombFactor has to be set to 1.0\n");
-          printf("                                    It is usually useless to apply bombFactor < 1.0 \n");
           printf("---------------\n"); 
-          
-          printf("\n$Id: GeomBrowse.C,v 1.3 2006/11/16 21:36:11 fine Exp $\n");
+          printf("One always can select the geometry from the \"file menu\" of the GUI interface later on too\n");          
+          printf("\n");                
+          printf("---------------\n"); 
+          printf("\n$Id: GeomBrowse.C,v 1.4 2006/11/16 21:44:15 fine Exp $\n");
 }                 
 //_____________________________________________________________________________________________________________
-void GeomBrowse(const char *fzFile="complete",Float_t bombFactor=1.4, const char *out = "")
+void GeomBrowse(const char *fzFile="complete")
 {
    GeomDrawUsage();
   // gSystem->Load("libGeomBrowser");  
