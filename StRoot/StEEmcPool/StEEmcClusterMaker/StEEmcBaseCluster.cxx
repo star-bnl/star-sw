b8838f861b9a68f24fa38ed2802467c18489ef50
Jason C. Webb <jwebb@bnl.gov>
2006-11-27 22:29:15 +0000

    StEEmcBaseCluster moves some of the functions common to StEEmcCluster and StEEmcSmdCluster into a common object, from which they both are derived. In addition we add association between clusters in this object.

---
+++ StRoot/StEEmcPool/StEEmcClusterMaker/StEEmcBaseCluster.cxx
@@ -0,0 +1,10 @@
+#include "StEEmcBaseCluster.h"
+
+ClassImp(StEEmcBaseCluster);
+
+StEEmcBaseCluster::StEEmcBaseCluster()
+{
+  // initialize storage for associated cluster keys
+  std::vector<Int_t> temp;
+  for ( Int_t i=0;i<6;i++ ) mMatched.push_back(temp);
+}
