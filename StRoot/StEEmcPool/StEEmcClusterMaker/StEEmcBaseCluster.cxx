fde7ead60b4c5e75864b0b1ff6f20f587fc46889
jwebb <>
2006-12-01 18:54:28 +0000

    Cluster key initialized to -1.

--- StRoot/StEEmcPool/StEEmcClusterMaker/StEEmcBaseCluster.cxx
+++ StRoot/StEEmcPool/StEEmcClusterMaker/StEEmcBaseCluster.cxx
@@ -7,4 +7,5 @@ StEEmcBaseCluster::StEEmcBaseCluster()
   // initialize storage for associated cluster keys
   std::vector<Int_t> temp;
   for ( Int_t i=0;i<6;i++ ) mMatched.push_back(temp);
+  mKey=-1;
 }
