336d4d46dca5d9c8597dc3e1dc228498d7d00bcd
Valeri Fine <val.fine@gmail.com>
2006-12-04 01:03:31 +0000

    Introduce the generic  detector group to enjoy OO polymorphism

--- StRoot/Sti/StiDetectorGroup.h
+++ StRoot/Sti/StiDetectorGroup.h
@@ -1,36 +1,19 @@
 #ifndef StiDetectorGroup_H_INCLUDED
 #define StiDetectorGroup_H_INCLUDED
 
-#include <stdexcept>
-#include "Sti/Base/Named.h"
+#include "Sti/StiGenericlDetectorGroup.h"
 #include "Sti/Base/Factory.h"
 #include "Sti/StiToolkit.h"
-#include "Sti/StiDetectorBuilder.h"
 template<class Event,class Detector> class StiHitLoader;
 class StiElossCalculator;
 
 template<class Event>
-class StiDetectorGroup : public Named
+class StiDetectorGroup : public StiGenericDetectorGroup
 {
   public:
 
   /// Get a hit loader appropriate for this detector group
   virtual StiHitLoader<Event,StiDetectorBuilder> * getHitLoader();
-  
-  /// Get a detector builder appropriate for this detector group
-  virtual StiDetectorBuilder * getDetectorBuilder();
-
-  virtual void initialize() {}
-
-  /// Get a pid calculator appropriate for this detector group
-  /// A dedx calculator is used after the track are fitted
-  /// to determine the average (or appropriate measure) dedx.
-  /// Get an energy loss calculator appropriate for this detector group
-  /// An eloss calculator is used in the kalman propagation to determine
-  /// the track energy loss.
-  StiElossCalculator * getElossCalculator();
-  void setGroupId(int id);
-  int  getGroupId() const;
   StiHitLoader<Event,StiDetectorBuilder> *hitLoader() {return _hitLoader;}
 
  protected:
@@ -42,10 +25,6 @@ class StiDetectorGroup : public Named
 		   StiElossCalculator * elossCalculator);
   ~StiDetectorGroup();
   StiHitLoader<Event,StiDetectorBuilder> * _hitLoader;
-  StiDetectorBuilder * _detectorBuilder;
-  StiElossCalculator * _elossCalculator; 
-  /// Detector group identifier.
-  int _groupId;
 };
 
 
@@ -54,11 +33,8 @@ StiDetectorGroup<Event>::StiDetectorGroup(const string & name,
 					  StiHitLoader<Event,StiDetectorBuilder> * hitLoader,
 					  StiDetectorBuilder * detectorBuilder,
 					  StiElossCalculator * elossCalculator)
-  :  Named(name),
-     _hitLoader(hitLoader),
-     _detectorBuilder(detectorBuilder),
-     _elossCalculator(elossCalculator),
-     _groupId(-1)
+  :  StiGenericDetectorGroup(name,detectorBuilder,elossCalculator),
+    _hitLoader(hitLoader)
 {
   // If a loader was specified, make sure it uses the selected detector builder.
   if (_hitLoader)
@@ -70,11 +46,8 @@ StiDetectorGroup<Event>::StiDetectorGroup(const string & name,
 
 template<class Event>
 StiDetectorGroup<Event>::StiDetectorGroup(const string & name)
-  : Named(name),
-     _hitLoader(0),
-     _detectorBuilder(0),
-     _elossCalculator(0),
-     _groupId(-1)
+  :  StiGenericDetectorGroup(name),
+     _hitLoader(0)
 {
   initialize();
 }
@@ -83,7 +56,6 @@ template<class Event>
 StiDetectorGroup<Event>::~StiDetectorGroup()
 {
   delete _hitLoader;
-  delete _detectorBuilder;
 }
 
 /// Get a hit loader appropraite for this detector group
@@ -104,48 +76,5 @@ StiHitLoader<Event,StiDetectorBuilder> * StiDetectorGroup<Event>::getHitLoader()
       }*/
   return _hitLoader;
 }
-
-
-/// Get a detector builder appropriate for this detector group
-template<class Event>
-StiDetectorBuilder * StiDetectorGroup<Event>::getDetectorBuilder()
-{
-  if (_detectorBuilder==0)
-    {
-      string message = "StiDetectorGroup::getDetectorBuilder() - ERROR - builder == 0 for detector:";
-      message += getName();
-      throw logic_error(message.c_str());
-    }
-  return _detectorBuilder; 
-}
-
-/// Get an energy loss calculator appropriate for this detector group
-/// An eloss calculator is used in the kalman propagation to determine
-/// the track energy loss.
-template<class Event>
-StiElossCalculator * StiDetectorGroup<Event>::getElossCalculator()
-{
-  if (_elossCalculator==0)
-    {
-      string message = "StiDetectorGroup::getElossCalculator() - ERROR - elossCalculator == 0 for detector:";
-      message += getName();
-      throw logic_error(message.c_str());
-    }
-  return _elossCalculator; 
-}
-
-template<class Event>
-inline void StiDetectorGroup<Event>::setGroupId(int id)
-{
-  if (_detectorBuilder) _detectorBuilder->setGroupId(id);
-  _groupId = id;
-}
-
-template<class Event>
-inline int  StiDetectorGroup<Event>::getGroupId() const
-{
-  return _groupId;
-}
-
 #endif
 
