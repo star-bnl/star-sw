40340434c5e49a9ea71c1b85b5e70b5da59c1b03
potekhin <>
2007-01-05 21:20:03 +0000

    Introduce a "legacy mode" for backward compatibility, as well as a hit collection

--- StarVMC/StarVMCApplication/StarMCHits.h
+++ StarVMC/StarVMCApplication/StarMCHits.h
@@ -3,6 +3,9 @@
 #include "St_g2t_Chair.h"
 #include "StHitDescriptor.h"
 #include "StarHitVector.h"
+#include "StarMCHitCollection.h"
+
+
 class TGeant3;
 
 
@@ -14,37 +17,50 @@ class StarMCHits : public TDataSet {
     if (fgInstance) return fgInstance;
     return new StarMCHits(name, title); 
   }
-  virtual TDataSet  	  *Instance() const {return (TDataSet *) fgInstance;}   		       
-  virtual Int_t     	   Init();					      		       
+  virtual TDataSet  	  *Instance() const {return (TDataSet *) fgInstance;}
+  virtual Int_t     	   Init();
   virtual void      	   Clear(const Option_t* opt = "");
-  virtual void      	   Step();					      		       
+  virtual void      	   Step();
+  virtual void      	   StepLegacy();
 #if 0
   virtual Float_t   	   GetHitK(Int_t k);				      		       
 #endif
   virtual void      	   SetHitHolder(TDataSet  *m) {fHitHolder = m;}	      	       
+
   virtual void             SetDebug(Int_t m=0)        {fDebug = m;}
-  virtual TDataSet  	  *GetDetectors() {return fDetectors;}		      	       
+  virtual void             SetLegacy(Int_t m=0)       {fLegacy = m;}
+
+  virtual TDataSet  	  *GetDetectors()   {return fDetectors;}		      	       
   virtual TObjArray 	  *GetVolUserInfo() {return fVolUserInfo;}	      		       
-  virtual TDataSet  	  *GetHitHolder() {return fHitHolder;}                  				       
+  virtual TDataSet  	  *GetHitHolder()   {return fHitHolder;}                  				       
+
   virtual StHitDescriptor *GetCurrentDetector() {return fCurrentDetector;}
+
   virtual void      	   FillG2Table();							       	    
   virtual void             FinishEvent();
   static  TTable    	  *NewTable(const Char_t *classname, const Char_t *name="", Int_t nrows=100); 
   static  St_g2t_Chair    *NewChair(const Char_t *type, const Char_t *name="");                       
   virtual Int_t            Debug()        { return fDebug;}
+
+  virtual Int_t            Legacy()       { return fLegacy;}
+
+  virtual StarMCHitCollection *HitCollection(void) { return fHitCollection;}
+
  private:
   StarMCHits(const Char_t *name="StarMCHits",const Char_t *title="");
-  static StarMCHits *fgInstance;
-  GHit_t             fHit;
-  TDataSet          *fDetectors;
-  TDataSet          *fGeoData;
-  THashList         *fDetList;
-  TObjArray         *fVolUserInfo;
-  TDataSet          *fHitHolder;
-  StHitDescriptor   *fCurrentDetector;
-  Int_t              fDebug;
-  UInt_t             fSeed;
-  Int_t              fEventNumber;
+  StarMCHitCollection *fHitCollection;
+  static StarMCHits   *fgInstance;
+  GHit_t               fHit;
+  TDataSet            *fDetectors;
+  TDataSet            *fGeoData;
+  THashList           *fDetList;
+  TObjArray           *fVolUserInfo;
+  TDataSet            *fHitHolder;
+  StHitDescriptor     *fCurrentDetector;
+  Int_t                fDebug;
+  Int_t                fLegacy;
+  UInt_t               fSeed;
+  Int_t                fEventNumber;
   ClassDef(StarMCHits,1)
 };
 #endif
