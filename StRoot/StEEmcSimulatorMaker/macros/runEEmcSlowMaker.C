0b0192fabaf11660f4a1b348e3e997a5f2ed46e0
Jason C. Webb <jwebb@bnl.gov>
2007-01-13 00:07:53 +0000

    Example macro for running the EEmc slow simulator.

--- StRoot/StEEmcSimulatorMaker/macros/runEEmcSlowMaker.C
+++ StRoot/StEEmcSimulatorMaker/macros/runEEmcSlowMaker.C
@@ -6,16 +6,21 @@ class StMuDstMaker;
 class TChain;
 class TObjArray;
 
+class StEEmcA2EMaker;
+
 
 StEEmcDbMaker *myDb;
 StMuDstMaker* muMk;
 StChain *chain=0;
 TObjArray *HList;
 
-// Comment out the line below to read EEMC data from StEvent (default is MuDst)
-#define useMuDst
+StEEmcA2EMaker *mEEbefore;
+StEEmcA2EMaker *mEEafter;
 
-int runEEmcSlowMaker( int nEve=5000 ){
+// Comment out the line below to read EEMC data from StEvent (default is MuDst)
+//#define useMuDst
+
+int runEEmcSlowMaker( int nEve=2800 ){
 
   int firstSec=1;
   int lastSec=12;
@@ -23,27 +28,30 @@ int runEEmcSlowMaker( int nEve=5000 ){
   Int_t nFiles=10; 
 
 #ifdef useMuDst
-  //char* file="rcf1210_164_4219evts.MuDst.root";
-  char* file="mcgamma_svtin_nominalvtx_5000evts.MuDst.root";
+  char* file="mcgamma_svtout_nominalvtx_4400evts.MuDst.root";
 #else
-  //char* file="rcf1210_164_4219evts.event.root";
-  char* file="mcgamma_svtin_nominalvtx_5000evts.event.root";
+  char* file="mcgamma_svtout_nominalvtx_4400evts.event.root";
 #endif
+  char* inDir="/star/data04/sim/jwebb/2006/svt_review/";
 
-  //char* inDir="/star/data19/reco/pp200/pythia6_203/default/minbias/y2004a/gheisha_on/trs_ij/";
-  char* inDir="/star/data04/sim/jwebb/2006/";
-  char *outname="jan";
+#ifdef useMuDst
+  char *outname="mudst";
+#else
+  char *outname="stevent";
+#endif
   TString fullName=file;
 
   gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
   loadSharedLibraries();
+  gROOT->Macro("LoadLogger.C");
   cout << " loading done " << endl;
-  gSystem->Load("StDbLib");
+  //gSystem->Load("StDbLib");
   gSystem->Load("StDbBroker");
   gSystem->Load("St_db_Maker");
   gSystem->Load("StEEmcDbMaker");
   gSystem->Load("StEEmcUtil");
   gSystem->Load("StEEmcSimulatorMaker");
+  gSystem->Load("StEEmcA2EMaker");
 
   // create chain    
   chain = new StChain("StChain"); 
@@ -62,6 +70,7 @@ int runEEmcSlowMaker( int nEve=5000 ){
   ioMaker->SetBranch("*", 0, "0");
   ioMaker->SetBranch("eventBranch", 0, "r");
   ioMaker->SetBranch("geantBranch", 0, "r");
+  Int_t nEntries=nEve;
 #endif
 
   St_db_Maker *stDb = new St_db_Maker("StarDb", "MySQL:StarDb");
@@ -77,11 +86,22 @@ int runEEmcSlowMaker( int nEve=5000 ){
   myDb->setSectors(firstSec, lastSec);
 
 
-  gMessMgr->SwitchOn("D");
-  gMessMgr->SwitchOn("I");
-  gMessMgr->SwitchOn("W");
 
   HList=new TObjArray;
+
+
+
+
+  // ADC 2 energy before slow simulator
+  mEEbefore = new StEEmcA2EMaker("before");
+  mEEbefore->database("eemcDb");
+#ifdef useMuDst
+  mEEbefore->source("MuDst",1);
+#else
+  mEEbefore->source("StEvent",2);
+#endif
+
+
 
   /*  
    * The slow simulator is setup by default with the best
@@ -96,7 +116,8 @@ int runEEmcSlowMaker( int nEve=5000 ){
   slowSim->setDropBad(0);   // 0=no action, 1=drop chn marked bad in db
   slowSim->setAddPed(0);    // 0=no action, 1=ped offset from db
   slowSim->setSmearPed(0);  // 0=no action, 1=gaussian ped, width from db
-  slowSim->setOverwrite(1); // 0=no action, 1=overwrite muDst values
+  slowSim->setOverwrite(1); // 0=no action, 1=overwrite muDst/StEvent
+
 
 #ifdef useMuDst
   slowSim->setSource("MuDst");
@@ -105,7 +126,32 @@ int runEEmcSlowMaker( int nEve=5000 ){
 #endif
 
   /// Uncomment the following line to save debuging histograms
-  //$$$  slowSim->SetHList(HList);
+  slowSim->SetHList(HList);
+
+
+  // ADC 2 energy after slow simulator
+  mEEafter = new StEEmcA2EMaker("after");
+  mEEafter->database("eemcDb");
+#ifdef useMuDst
+  mEEafter->source("MuDst",1);
+#else
+  mEEafter->source("StEvent",2);
+#endif
+
+
+
+  // some test histograms
+  TH2F *h_compare=new TH2F("h_compare","ADC after slow maker vs before; uncorrected ADC; corrected ADC",512,0.,1024.,512,0.,1024. );
+  TH2F *h_compare_pre1=new TH2F("h_compare_pre1","ADC after slow maker vs before",256,0.,1024.,256,0.,256. );
+  TH2F *h_compare_pre2=new TH2F("h_compare_pre2","ADC after slow maker vs before",256,0.,1024.,256,0.,256. );
+  TH2F *h_testit=new TH2F("h_testit","ADC after-before vs ADC pre1+pre2; pre1+pre2 AD; corrected-uncorrected",256,0.,512.,100,-50.,50.);
+  TH2F *h_perinc=new TH2F("h_perinc","percent increase vs before; ADC [before simu]; % increase",128,0.,1024.,120,-10.,50.);
+  TH2F *h_perincE=new TH2F("h_perincE","percent increase vs original E; Energy [before simu]; % increase",150,0.,30.,120,-10.,50.);
+  TH2F *h_perincEZ=new TH2F("h_perincEZ","percent increase vs original E (zeros supressed); Energy [before simu]; % increase",150,0.,30.,120,-10.,50.);
+
+
+
+
   
   // Put your analysis maker here
   // StMyAnaMaker *bhla =new  StMyAnaMaker;
@@ -124,6 +170,24 @@ int runEEmcSlowMaker( int nEve=5000 ){
     stat = chain->Make();
     if(eventCounter%100==0)
       printf("\n====================%d  processing  ============\n", eventCounter);
+
+    for ( Int_t ii=0;ii<mEEafter->numberOfHitTowers(0);ii++ )
+      {
+	StEEmcTower tafter=mEEafter->hittower(ii,0);
+	StEEmcTower tbefore=mEEbefore->tower(tafter.index(),0);
+	StEEmcTower pbefore=mEEbefore->tower(tafter.index(),1);
+	StEEmcTower qbefore=mEEbefore->tower(tafter.index(),1);
+	h_compare->Fill( tbefore.adc(), tafter.adc());
+
+	h_testit->Fill( pbefore.adc()+pbefore.adc(), tafter.adc()-tbefore.adc() );
+
+	h_perinc->Fill( tbefore.adc(), 100.0* (tafter.adc()-tbefore.adc() )/tafter.adc() );
+	h_perincE->Fill( tbefore.energy(), 100.0* (tafter.adc()-tbefore.adc() )/tafter.adc() );
+	if ( TMath::Abs(tbefore.adc() - tafter.adc()) >1. )
+	  h_perincEZ->Fill( tbefore.energy(), 100.0* (tafter.adc()-tbefore.adc() )/tafter.adc() );
+
+      }
+
     
   }
   printf("sorting done, nEve=%d of %d\n",nEve, nEntries);
@@ -140,8 +204,10 @@ int runEEmcSlowMaker( int nEve=5000 ){
   // save output histograms 
   // HList.ls();
 
+
+
   /// Change to 1 to save debugging histograms
-#if 0  
+#if 1  
   TString out="./";
   out+=outname;
   out+=".hist.root";
@@ -149,6 +215,10 @@ int runEEmcSlowMaker( int nEve=5000 ){
   assert(f.IsOpen());
   printf("%d histos are written  to '%s' ...\n",HList->GetEntries(),out.Data());
   HList->Write();
+  h_compare->Write();
+  h_testit->Write();
+  h_perinc->Write();h_perincE->Write();h_perincEZ->Write();
   f.Close();
 #endif  
+
 }
