010eec2aa407fdcd0db4159f34538c586ba62fc9
Gene Van Buren <gene@bnl.gov>
2007-03-13 18:37:54 +0000

    simplified names

--- StRoot/St_QA_Maker/QAhlist_tpcSectors.h
+++ StRoot/St_QA_Maker/QAhlist_tpcSectors.h
@@ -1,5 +1,8 @@
-// $Id: QAhlist_tpcSectors.h,v 2.1 2003/02/15 22:00:52 genevb Exp $
+// $Id: QAhlist_tpcSectors.h,v 2.2 2007/03/13 18:37:54 genevb Exp $
 // $Log: QAhlist_tpcSectors.h,v $
+// Revision 2.2  2007/03/13 18:37:54  genevb
+// simplified names
+//
 // Revision 2.1  2003/02/15 22:00:52  genevb
 // Add tpcSectors, fix ftpc east/west charge
 //
@@ -11,27 +14,27 @@
 // Note: Editing this file means that StAnalysisUtilities/StHistUtil
 // must be recompiled
 
-  "StEQaTpcSector1",
-  "StEQaTpcSector2",
-  "StEQaTpcSector3",
-  "StEQaTpcSector4",
-  "StEQaTpcSector5",
-  "StEQaTpcSector6",
-  "StEQaTpcSector7",
-  "StEQaTpcSector8",
-  "StEQaTpcSector9",
-  "StEQaTpcSector10",
-  "StEQaTpcSector11",
-  "StEQaTpcSector12",
-  "StEQaTpcSector13",
-  "StEQaTpcSector14",
-  "StEQaTpcSector15",
-  "StEQaTpcSector16",
-  "StEQaTpcSector17",
-  "StEQaTpcSector18",
-  "StEQaTpcSector19",
-  "StEQaTpcSector20",
-  "StEQaTpcSector21",
-  "StEQaTpcSector22",
-  "StEQaTpcSector23",
-  "StEQaTpcSector24"
+  "QaTpcSector1",
+  "QaTpcSector2",
+  "QaTpcSector3",
+  "QaTpcSector4",
+  "QaTpcSector5",
+  "QaTpcSector6",
+  "QaTpcSector7",
+  "QaTpcSector8",
+  "QaTpcSector9",
+  "QaTpcSector10",
+  "QaTpcSector11",
+  "QaTpcSector12",
+  "QaTpcSector13",
+  "QaTpcSector14",
+  "QaTpcSector15",
+  "QaTpcSector16",
+  "QaTpcSector17",
+  "QaTpcSector18",
+  "QaTpcSector19",
+  "QaTpcSector20",
+  "QaTpcSector21",
+  "QaTpcSector22",
+  "QaTpcSector23",
+  "QaTpcSector24"
