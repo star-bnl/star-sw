f9af90ed3a15b4d2ce680ae0b1027d23e7c780ec
fisyak <>
2007-03-21 16:37:34 +0000

    More strict wafer definition

--- StRoot/StDbUtilities/St_svtRDOstrippedC.h
+++ StRoot/StDbUtilities/St_svtRDOstrippedC.h
@@ -6,21 +6,21 @@
 
 class St_svtRDOstrippedC : public TChair {
  public:
-  St_svtRDOstrippedC (St_svtRDOstripped *table=0) : TChair(table), fDate(0), fDelay(0), fSwitchedOff(0) {
-			       if (fgsvtRDOstrippedC) delete fgsvtRDOstrippedC; fgsvtRDOstrippedC = this;}
+  St_svtRDOstrippedC (St_svtRDOstripped *table=0);
   virtual           ~St_svtRDOstrippedC() {fgsvtRDOstrippedC = 0;}
-
+  
   static St_svtRDOstrippedC *instance()  {return fgsvtRDOstrippedC;}
   Int_t                      svtRDOstrippedStatus(Int_t barrel, Int_t ladder, Int_t wafer);
   svtRDOstripped_st         *pRDO(Int_t barrel, Int_t ladder, Int_t wafer);
   //                                                           1 hour                   3 hours
-  void                       SetDate(UInt_t date, UInt_t delay=3600, UInt_t switchOff = 10800) 
+  void                       SetDate(Int_t date, Int_t delay=3600, Int_t switchOff = 10800) 
   {fDate = date; fDelay = delay; fSwitchedOff = switchOff;}
   static void                PrintRDOmap();
+  void                       Init();
  private:
-  UInt_t                     fDate;
-  UInt_t                     fDelay;
-  UInt_t                     fSwitchedOff;
+  Int_t                     fDate;
+  Int_t                     fDelay;
+  Int_t                     fSwitchedOff;
   static St_svtRDOstrippedC *fgsvtRDOstrippedC;
 
   ClassDefChair(St_svtRDOstripped, svtRDOstripped_st )
