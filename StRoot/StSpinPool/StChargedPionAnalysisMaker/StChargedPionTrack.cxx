364558b1bfc7f474c71b784633dc482c1d863324
kocolosk <>
2007-05-03 19:40:21 +0000

    Rewrite of StChargedPionTrack to remove redundant / useless info. Single particles takes up 75% less space now.

--- StRoot/StSpinPool/StChargedPionAnalysisMaker/StChargedPionTrack.cxx
+++ StRoot/StSpinPool/StChargedPionAnalysisMaker/StChargedPionTrack.cxx
@@ -1,70 +1,124 @@
 #include "StChargedPionTrack.h"
+#include "StPionPlus.hh"
 
 #include "TClass.h"
 
 ClassImp(StChargedPionTrack)
 
-StChargedPionTrack::StChargedPionTrack() : TObject(),
-	mId(0),mFlag(0),mVertexIndex(0),mNHits(0),mNHitsPoss(0),mNHitsDedx(0),mNHitsFit(0),
-	mPidProbElectron(0),mPidProbPion(0),mPidProbKaon(0),mPidProbProton(0),mNSigmaElectron(0),
-	mNSigmaPion(0),mNSigmaKaon(0),mNSigmaProton(0),mdEdx(0),mChi2(0),mChi2Prob(0),mPt(0),mPhi(0),
-	mEta(0),mCharge(0),mP(0.,0.,0.),mFirstPoint(0.,0.,0.),mLastPoint(0.,0.,0.),
-	mHelix(StThreeVectorF(0.,0.,0.),StThreeVectorF(0.,0.,0.),0.,0.),
-	mOuterHelix(StThreeVectorF(0.,0.,0.),StThreeVectorF(0.,0.,0.),0.,0.),
-	mDCA(0.,0.,0.),mSigmaDcaD(0),mSigmaDcaZ(0)
+StChargedPionTrack::StChargedPionTrack() : TLorentzVector(),
+    mId(0),mFlag(0),mVertexIndex(0),mVertex(0.,0.,0.),mNHits(0),mNHitsPoss(0),mNHitsDedx(0),mNHitsFit(0),
+    mNSigmaElectron(0),mNSigmaPion(0),mNSigmaKaon(0),mNSigmaProton(0),mdEdx(0),mChi2(0),mChi2Prob(0),
+    mCharge(0),mB(0.),mGlobalLastPoint(0.,0.,0.),
+    mGlobalHelix(StThreeVectorF(0.,0.,0.),StThreeVectorF(0.,0.,0.),0.,0.)
 { }
 
-StChargedPionTrack::StChargedPionTrack(const StChargedPionTrack& t) : TObject()
-{
-	this->mId				= t.id();
-	this->mFlag				= t.flag();
-	this->mVertexIndex		= t.vertexIndex();
-	this->mNHits			= t.nHits();
-	this->mNHitsPoss		= t.nHitsPoss();
-	this->mNHitsDedx		= t.nHitsDedx();
-	this->mNHitsFit			= t.nHitsFit();
-	this->mPidProbElectron	= pack2UnsignedShort(t.pidProbElectron(),__PROB_SCALE__);
-	this->mPidProbPion		= pack2UnsignedShort(t.pidProbPion(),__PROB_SCALE__);
-	this->mPidProbKaon		= pack2UnsignedShort(t.pidProbKaon(),__PROB_SCALE__);
-	this->mPidProbProton	= pack2UnsignedShort(t.pidProbProton(),__PROB_SCALE__);
-	this->mNSigmaElectron	= pack2Int( fabsMin(t.nSigmaElectron(),__SIGMA_SCALE__), __SIGMA_SCALE__ );
-	this->mNSigmaPion		= pack2Int( fabsMin(t.nSigmaPion(),__SIGMA_SCALE__), __SIGMA_SCALE__ );
-	this->mNSigmaKaon		= pack2Int( fabsMin(t.nSigmaKaon(),__SIGMA_SCALE__), __SIGMA_SCALE__ );
-	this->mNSigmaProton		= pack2Int( fabsMin(t.nSigmaProton(),__SIGMA_SCALE__), __SIGMA_SCALE__ );
-	this->mdEdx				= t.dEdx();
-	this->mChi2				= t.chi2();
-	this->mChi2Prob			= t.chi2prob();
-	this->mPt				= t.pt();
-	this->mPhi				= t.phi();
-	this->mEta				= t.eta();
-	this->mCharge			= t.charge();
-	this->mP				= t.p();
-	this->mFirstPoint		= t.firstPoint();
-	this->mLastPoint		= t.lastPoint();
-	this->mHelix			= t.helix();
-	this->mOuterHelix		= t.outerHelix();
-	this->mProbPidTraits	= t.probPidTraits();
-	this->mDCA				= t.dca();
-	this->mSigmaDcaD		= t.sigmaDcaD();
-	this->mSigmaDcaZ		= t.sigmaDcaZ();
+StChargedPionTrack::~StChargedPionTrack() { /* no-op */ }
+
+StChargedPionTrack::StChargedPionTrack(const StChargedPionTrack& t) : TLorentzVector(t)
+{    
+    this->mId               = t.mId;
+    this->mFlag             = t.mFlag;
+    
+    this->mVertexIndex      = t.mVertexIndex;
+    this->mVertex           = t.mVertex;
+    
+    this->mNHits            = t.mNHits;
+    this->mNHitsPoss        = t.mNHitsPoss;
+    this->mNHitsDedx        = t.mNHitsDedx;
+    this->mNHitsFit         = t.mNHitsFit;
+    
+    this->mNSigmaElectron   = t.mNSigmaElectron;
+    this->mNSigmaPion       = t.mNSigmaPion;
+    this->mNSigmaKaon       = t.mNSigmaKaon;
+    this->mNSigmaProton     = t.mNSigmaProton;
+    this->mdEdx             = t.mdEdx;
+    
+    this->mChi2             = t.mChi2;
+    this->mChi2Prob         = t.mChi2Prob;
+    
+    this->mCharge           = t.mCharge;
+    this->mB                = t.mB;
+    
+    this->mGlobalLastPoint  = t.mGlobalLastPoint;
+    this->mGlobalHelix      = t.mGlobalHelix;
+}
+
+void StChargedPionTrack::setPtEtaPhi(float aPt, float aEta, float aPhi) {
+    this->SetPtEtaPhiM(aPt,aEta,aPhi,StPionPlus::instance()->mass());
+}
+
+void StChargedPionTrack::setPt(float aPt) {
+    this->SetPerp(aPt);
+}
+
+void StChargedPionTrack::setPhi(float aPhi) {
+    this->SetPhi(aPhi);
+}
+
+void StChargedPionTrack::setEta(float aEta) {
+    this->SetTheta(2*TMath::ATan(TMath::Exp(-1*aEta)));
+}
+
+double StChargedPionTrack::globalPt() const {
+    return this->globalPt(mGlobalHelix.origin());
+}
+
+double StChargedPionTrack::globalPt(StThreeVectorF position) const {
+    double pathLength = mGlobalHelix.pathLength(position,false);
+    StThreeVector<double> gP = mGlobalHelix.momentumAt(pathLength,mB);
+    return gP.perp();
+}
+
+double StChargedPionTrack::globalPhi() const {
+    return this->globalPhi(mGlobalHelix.origin());
+}
+
+double StChargedPionTrack::globalPhi(StThreeVectorF position) const {
+    double pathLength = mGlobalHelix.pathLength(position,false);
+    StThreeVector<double> gP = mGlobalHelix.momentumAt(pathLength,mB);
+    return gP.phi();
+}
+
+double StChargedPionTrack::globalEta() const {
+    return this->globalEta(mGlobalHelix.origin());
+}
+
+double StChargedPionTrack::globalEta(StThreeVectorF position) const {
+    double pathLength = mGlobalHelix.pathLength(position,false);
+    StThreeVector<double> gP = mGlobalHelix.momentumAt(pathLength,mB);
+    return gP.pseudoRapidity();
+}
+
+TLorentzVector StChargedPionTrack::globalP() const {
+    return this->globalP(mGlobalHelix.origin());
+}
+
+TLorentzVector StChargedPionTrack::globalP(StThreeVectorF position) const {
+    double pathLength = mGlobalHelix.pathLength(position,false);
+    StThreeVector<double> gP = mGlobalHelix.momentumAt(pathLength,mB);
+    TLorentzVector vec;
+    vec.SetPtEtaPhiM(gP.perp(),gP.pseudoRapidity(),gP.phi(),StPionPlus::instance()->mass());
+    return vec;
+}
+
+StThreeVectorF StChargedPionTrack::globalDca() const {
+    return this->globalDca(mVertex);
+}
+
+StThreeVectorF StChargedPionTrack::globalDca(StThreeVectorF position) const {
+    double pathlength = mGlobalHelix.pathLength(position,false);
+    return (mGlobalHelix.at(pathlength) - position);
 }
 
 double StChargedPionTrack::length() const 
 { 
-	return fabs( helix().pathLength(StThreeVectorD(mLastPoint)) ); 
+    double end      = mGlobalHelix.pathLength(StThreeVectorD(mGlobalLastPoint));
+    double begin    = mGlobalHelix.pathLength(StThreeVectorD(mVertex));
+    return fabs(end - begin); 
 }
 
 double StChargedPionTrack::lengthMeasured() const 
 { 
-	return fabs( helix().pathLength(StThreeVectorD(mLastPoint)) - helix().pathLength(StThreeVectorD(mFirstPoint)) ); 
-}
-
-float StChargedPionTrack::dcaD() const
-{
-	return 0.;
-}
-
-float StChargedPionTrack::dcaZ() const
-{
-	return 0.;
+    double end      = mGlobalHelix.pathLength(StThreeVectorD(mGlobalLastPoint));
+    return fabs(end); 
 }
