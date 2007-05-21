6d307cbfa2fdccebd974bd239775d7aff93c83b6
rashmi <>
2007-05-21 04:38:11 +0000

    functions for SMChain Combination information 21/05/07

--- StRoot/StPmdUtil/StPmdMapUtil.cxx
+++ StRoot/StPmdUtil/StPmdMapUtil.cxx
@@ -1,6 +1,6 @@
 /********************************************************
  *
- * $Id: StPmdMapUtil.cxx,v 1.4 2007/05/05 13:41:08 rashmi Exp $
+ * $Id: StPmdMapUtil.cxx,v 1.5 2007/05/21 04:38:11 rashmi Exp $
  *
  * Author: Subhasis Chattopadhyay
  *
@@ -11,8 +11,8 @@
  *
  *********************************************************
  * $Log: StPmdMapUtil.cxx,v $
- * Revision 1.4  2007/05/05 13:41:08  rashmi
- * bug fix for unphysical year in StoreMapInfo()
+ * Revision 1.5  2007/05/21 04:38:11  rashmi
+ * functions for SMChain Combination information 21/05/07
  *
  * Revision 1.3  2005/12/07 20:35:45  perev
  * iostream.h ==> iostream. WarnOff
@@ -34,7 +34,7 @@
 #include <math.h>
 #include "StPmdGeom.h"
 #include "StPmdMapUtil.h"
-#include "StMessMgr.h"
+#include "StChain.h"
 ClassImp(StPmdMapUtil)
 
   StPmdMapUtil::StPmdMapUtil()         //! A constructor
@@ -63,12 +63,8 @@ void StPmdMapUtil::StoreMapInfo(Int_t runno1)
   rn=atoi(iRun);
   year=atoi(iyear);
   cout<<"runno, rn1 "<<runno1<<" "<<rn<<" "<<year<<endl;
-  if(year<4){
-    gMessMgr->Warning("Unphysical year");
-    return;
-  }
 //////////////////////////////////////////////
-  Int_t sm=0,row=0,col=0;
+  Int_t sm,row,col;
   //Initialize
   for(Int_t ism=0;ism<PMD_CRAMS_MAX*2;ism++){
     for(Int_t irow=0;irow<PMD_ROW_MAX;irow++){
@@ -90,7 +86,7 @@ void StPmdMapUtil::StoreMapInfo(Int_t runno1)
 	   mPmdGeom->readBoardDetail(runno1);
 	   mPmdGeom->ChainMapping(Chain_No,channel,sm,col,row,chtemp,year);
 	}
-	if(year>=4 && sm>0 && row>0 && col>0){
+	if(sm>0 && row>0 && col>0){
  //  if(sm>12)cout<<"chain,ch,sm,col,row,chtemp,year "<<Chain_No<<" "<<channel<<" "<<sm-1<<" "<<col-1<<" "<<row-1<<" "<<chtemp<<" "<<year<<endl;
 	 m_ChannelInBoard[sm-1][row-1][col-1]=channel;
 	 m_TempChannelInBoard[sm-1][row-1][col-1]=chtemp;
@@ -126,4 +122,48 @@ void StPmdMapUtil::StoreMapInfo()
 
     }
   }
+}
+
+void StPmdMapUtil::SetSMChainCombos(Int_t runno1){
+  
+  nSMChain = 0;
+  mPmdGeom->readBoardDetail(runno1);
+  Int_t rn,year;
+  mPmdGeom->GetRunYear(runno1,rn,year);
+  for(Int_t ichain = 0;ichain<48;ichain++){
+    for(Int_t ism = 0;ism < 24;ism++){
+      nBoardSMChain[ichain][ism]  = 0;
+    }
+  }
+  
+  Int_t mapcheck;
+  for(Int_t ichain = 0;ichain<48;ichain++){
+    for(Int_t ibrd = 0;ibrd < 27;ibrd++){    
+      Int_t channel = 10 + ibrd*64;
+      Int_t sm=0,col=0,row=0,chmod=0;
+      Int_t chain = ichain+1;
+      if (year==5){
+	mapcheck = mPmdGeom->ChainMapping(chain,channel,sm,col,row,chmod);
+      }else{
+        mapcheck = mPmdGeom->ChainMapping(chain,channel,sm,col,row,chmod,year);
+      }
+      if(mapcheck==kStOk && sm > 0) {
+	nBoardSMChain[sm-1][ichain]++;
+	SMChainExists[sm-1][ichain]=nSMChain+1;
+	if(nBoardSMChain[sm-1][ichain]==1) {
+	  nSMChain++;
+	  //	  cout<<nSMChain-1<<"  "<<ichain+1<<"  "<<ibrd+1<<"  "<<sm<<endl;
+	  FirstBoard[nSMChain-1] = ibrd+1;
+	  SM_Combo[nSMChain-1] = sm;
+	  Chain_Combo[nSMChain-1] = chain;
+	}
+      }
+    }
+  }
+  
+    
+  for (Int_t ismchain = 0; ismchain<nSMChain;ismchain++){
+    cout<<"ismchain="<<ismchain<<" chain="<<Chain_Combo[ismchain]<<" sm="<<SM_Combo[ismchain]<<" has number of boards = "<<nBoardSMChain[SM_Combo[ismchain]-1][Chain_Combo[ismchain]-1]<<" first board="<<FirstBoard[ismchain]<<endl;
+  }
+  
 }
