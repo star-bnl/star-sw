d3093a03f8aa56f23acec60dbbb10f3d70646e3f
Pibero Djawotho <pibero@tamu.edu>
2007-06-15 21:37:53 +0000

    Added "missing" decay photons. Removed cout statements.

--- StRoot/StGammaMaker/macros/RunGammaPythiaMaker.C
+++ StRoot/StGammaMaker/macros/RunGammaPythiaMaker.C
@@ -1,21 +1,17 @@
-class  StChain *chain;
-int total=0;
-#include <string>
+#include <iomanip>
 
 void RunGammaPythiaMaker(	      
+	       int nevents = 10,
 	       const char *dir ="",
-	       const char* file="/star/data13/reco/pp200/pythia6_205/55_65gev/cdf_a/y2004y/gheisha_on/p05ih/rcf1271_10_4000evts.MuDst.root",
-	       const char *fname="/star/data13/reco/pp200/pythia6_205/55_65gev/cdf_a/y2004y/gheisha_on/p05ih/rcf1271_10_4000evts.geant.root",
+	       const char* file="/star/institutions/mit/betan/Simulation/photon_9_11.MuDst.root",
+	       const char *fname="/star/institutions/mit/betan/Simulation/photon_9_11.geant.root",
 	       const char *filter = "",
 	       )
 {
-  int nevents =10;
-  
- 
   gROOT->LoadMacro("StRoot/StGammaMaker/macros/loadGammaLibs.C");
   loadGammaLibs();
 
-  chain= new StChain("StChain"); 
+  StChain* chain = new StChain("StChain"); 
   chain->SetDebug(1);
   gMessMgr->SwitchOff("D");
   gMessMgr->SwitchOff("I");
@@ -29,9 +25,12 @@ void RunGammaPythiaMaker(
   StMuDebug::setLevel(1); 
   StMuDstMaker* muDstMaker = new StMuDstMaker(0,0,dir,file,filter,1e6,"MuDst"); 
 
+  StMcEventMaker* mcEventReader = new StMcEventMaker;
+
   StGammaPythiaMaker *weight = new StGammaPythiaMaker("GammaPythia");
   
   TChain* fileChain = muDstMaker->chain();
+  int total = 0;
   int ntotal = fileChain->GetEntries();
   
   for (Int_t iev=0;iev<nevents; iev++) {
@@ -45,6 +44,10 @@ void RunGammaPythiaMaker(
       cout << "Bad return code!" << endl;
       break;
     }
+    cout << "Number of pi0: " << weight->numberOfPion0() << '\n';
+    cout << "Number of prompt photons: " << weight->numberOfPrompt() <<'\n';
+    cout << "Number of decay photons: " << weight->numberOfDecay() << '\n';
+    cout << endl;
   }
   
   chain->Finish();
@@ -52,6 +55,3 @@ void RunGammaPythiaMaker(
   cout << "total number of events  " << total << endl;
   cout << "****************************************** " << endl;
 }
-
-
-
