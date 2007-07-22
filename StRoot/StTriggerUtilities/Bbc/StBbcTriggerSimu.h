f2b606dd757f7d0cc3feb49b414676141da94441
Renee Fatemi <renee.fatemi@uky.edu>
2007-07-22 23:05:55 +0000

    Add StBbcTriggerSimu under Bbc director instead of TrgEmulatorBBC

---
+++ StRoot/StTriggerUtilities/Bbc/StBbcTriggerSimu.h
@@ -0,0 +1,47 @@
+
+#ifndef STAR_StBbcTriggerMaker
+#define STAR_StBbcTriggerMaker
+
+/*!
+ *                                                                     
+ * \class  StBbcTriggerSimu
+ * \author rfatemi
+ * \date   05/23/07
+ * 
+ * Emulates BBC trigger using only ADC and not TDC
+ */                                                                      
+
+#ifndef StMaker_H
+#include "StMaker.h"
+#endif
+
+class StChain;
+class StMuDst;
+class StMuDstMaker;
+class StEvent;
+class StMuEvent;
+class StBbcTriggerDetector;
+class StBbcTriggerSimu {
+
+ private:
+ StMuDstMaker *muDstMaker;
+ StMuDst *muDst;
+ StMuEvent *muEvent;
+ int BBCadcNum;
+ 
+ protected:
+
+ public: 
+  StBbcTriggerSimu();
+  virtual ~StBbcTriggerSimu();
+  void Clear();
+  void Make();
+
+  int Wbbc, Ebbc, bbcTrig, BBCadc[48];
+
+  ClassDef(StBbcTriggerSimu, 1)
+};
+
+#endif
+
+
