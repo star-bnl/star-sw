08468ea9bbf877ed1dd8fabc04f6e0d545132bd5
Jan Balewski <balewski@lbl.gov>
2007-08-17 01:17:32 +0000

    temp blocking of trig code, untill I have time to match it to new EEdsm.so prepared by Xin

--- StRoot/StEEmcPool/muEztPanitkin/macros/rdEztPanitkin.C
+++ StRoot/StEEmcPool/muEztPanitkin/macros/rdEztPanitkin.C
@@ -6,12 +6,14 @@ MuEzPanitkinMaker  *myMk3;
 int rdEztPanitkin( int trigID=0,//96294,
  int nEve=100, 
  Int_t nFiles  =20,
- char* file="lis/R6049126.lis", //R6121034.lis,
+ char* file="runList/R7101015.lis", //R6121034.lis,
  char* inDir   = "./"   
  ){ 
 
   //inDir="/star/data05/scratch/eemcdb/muDst/2005/048/";
   // file="st_physics_6048024_raw_2010006.MuDst.root";
+  inDir=" myMuDst/";
+  file="st_physics_7166007_raw_1040002.MuDst.root";
 
   gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
   loadSharedLibraries();
@@ -53,6 +55,23 @@ int rdEztPanitkin( int trigID=0,//96294,
   // myMk3->SetHistoPixels();
   //  myMk3->SetSpy();
  
+
+
+  {
+   // Endcap specific DSM params
+    int eemcDsmSetup[20]; // see StEemcTriggerSimu::initRun() for definition
+    memset(eemcDsmSetup, 0,sizeof(eemcDsmSetup));// clear all, may be a bad default
+    eemcDsmSetup[0]=3;  // HTthr0
+    eemcDsmSetup[1]=12; // HTthr1
+    eemcDsmSetup[2]=22; // HTthr2
+    eemcDsmSetup[3]=1;  // TPthr0
+    eemcDsmSetup[4]=17; // TPthr1
+    eemcDsmSetup[5]=31; // TPthr2
+    eemcDsmSetup[10]=2; //HTTPthrSelc, 2=use_thres_#1
+    // myMk3->SetDsmSetup(eemcDsmSetup);
+}
+
+
   gMessMgr->SwitchOff("D");
   gMessMgr->SwitchOn("I");
  
