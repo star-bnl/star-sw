98621d80fca4f2f0ea60ee019c54ce23d1cc3b30
Pibero Djawotho <pibero@tamu.edu>
2007-08-20 18:46:38 +0000

    The EEMC gamma fitter now forces a common yield in both ESMD U and V planes. A likelihood variable (Scott's variable) has been added to the result of the fit.

--- StRoot/StGammaMaker/StGammaFitterResult.h
+++ StRoot/StGammaMaker/StGammaFitterResult.h
@@ -1,14 +1,12 @@
-/// -*- mode: C++ -*-
+// -*- mode: C++ -*-
 
-///
-/// \author Pibero Djawotho <pibero@indiana.edu>
-/// \author Indiana University
-/// \date 5 July 2007
-///
-/// \class StGammaFitterResult
-/// \brief This class holds the result of the gamma fitter.
-/// \see StGammaFitter
-///
+//
+// Pibero Djawotho <pibero@indiana.edu>
+// Indiana University
+// 28 July 2007
+//
+// StGammaFitterResult: container for result of gamma fitter.
+//
 
 #ifndef StGammaFitterResult_h
 #define StGammaFitterResult_h
@@ -21,42 +19,82 @@ public:
   ~StGammaFitterResult() {}
 
   float yield() const;
-  float mean() const;
-  float residual() const;
-  float chi2() const;
+  float yieldError() const;
+  float uyield() const;
+  float uyieldError() const;
+  float vyield() const;
+  float vyieldError() const;
+  float umean() const;
+  float umeanError() const;
+  float vmean() const;
+  float vmeanError() const;
+  float uresidual() const;
+  float vresidual() const;
+  float usigma() const;
+  float vsigma() const;
+  float chiSquare() const;
   int ndf() const;
-  float sigma() const;
+  float likelihood() const;	// Scott's variable
 
-  void setYield(float yield);
-  void setMean(float mean);
-  void setResidual(float residual);
-  void setChi2(float chi2);
+  void setYield(float yield, float error = 0);
+  void setUmean(float mean, float error = 0);
+  void setVmean(float mean, float error = 0);
+  void setUresidual(float residual);
+  void setVresidual(float residual);
+  void setUsigma(float sigma);
+  void setVsigma(float sigma);
+  void setChiSquare(float chiSquare);
   void setNdf(int ndf);
-  void setSigma(float sigma);
+  void setLikelihood(float likelihood);
+  void print();
 
 private:
   float mYield;
-  float mMean;
-  float mResidual;
-  float mChi2;
+  float mYieldError;
+  float mUmean;
+  float mUmeanError;
+  float mVmean;
+  float mVmeanError;
+  float mUresidual;
+  float mVresidual;
+  float mUsigma;
+  float mVsigma;
+  float mChiSquare;
   int mNdf;
-  float mSigma;
+  float mLikelihood;
 
   ClassDef(StGammaFitterResult, 1);
 };
 
-inline float StGammaFitterResult::yield() const { return mYield; }
-inline float StGammaFitterResult::mean() const { return mMean; }
-inline float StGammaFitterResult::residual() const { return mResidual; }
-inline float StGammaFitterResult::chi2() const { return mChi2; }
-inline int StGammaFitterResult::ndf() const { return mNdf; }
-inline float StGammaFitterResult::sigma() const { return mSigma; }
+ostream& operator<<(ostream& out, const StGammaFitterResult& fit);
 
-inline void StGammaFitterResult::setYield(float yield) { mYield = yield; }
-inline void StGammaFitterResult::setMean(float mean) { mMean = mean; }
-inline void StGammaFitterResult::setResidual(float residual) { mResidual = residual; }
-inline void StGammaFitterResult::setChi2(float chi2) { mChi2 = chi2; }
+inline float StGammaFitterResult::yield() const { return mYield; }
+inline float StGammaFitterResult::yieldError() const { return mYieldError; }
+inline float StGammaFitterResult::uyield() const { return mYield; }
+inline float StGammaFitterResult::uyieldError() const { return mYieldError; }
+inline float StGammaFitterResult::vyield() const { return mYield; }
+inline float StGammaFitterResult::vyieldError() const { return mYieldError; }
+inline float StGammaFitterResult::umean() const { return mUmean; }
+inline float StGammaFitterResult::umeanError() const { return mUmeanError; }
+inline float StGammaFitterResult::vmean() const { return mVmean; }
+inline float StGammaFitterResult::vmeanError() const { return mVmeanError; }
+inline float StGammaFitterResult::uresidual() const { return mUresidual; }
+inline float StGammaFitterResult::vresidual() const { return mVresidual; }
+inline float StGammaFitterResult::usigma() const { return mUsigma; }
+inline float StGammaFitterResult::vsigma() const { return mVsigma; }
+inline float StGammaFitterResult::chiSquare() const { return mChiSquare; }
+inline int StGammaFitterResult::ndf() const { return mNdf; }
+inline float StGammaFitterResult::likelihood() const { return mLikelihood; }
+
+inline void StGammaFitterResult::setYield(float yield, float error) { mYield = yield; mYieldError = error; }
+inline void StGammaFitterResult::setUmean(float mean, float error) { mUmean = mean; mUmeanError = error; }
+inline void StGammaFitterResult::setVmean(float mean, float error) { mVmean = mean; mVmeanError = error; }
+inline void StGammaFitterResult::setUresidual(float residual) { mUresidual = residual; }
+inline void StGammaFitterResult::setVresidual(float residual) { mVresidual = residual; }
+inline void StGammaFitterResult::setUsigma(float sigma) { mUsigma = sigma; }
+inline void StGammaFitterResult::setVsigma(float sigma) { mVsigma = sigma; }
+inline void StGammaFitterResult::setChiSquare(float chiSquare) { mChiSquare = chiSquare; }
 inline void StGammaFitterResult::setNdf(int ndf) { mNdf = ndf; }
-inline void StGammaFitterResult::setSigma(float sigma) { mSigma = sigma; }
+inline void StGammaFitterResult::setLikelihood(float likelihood) { mLikelihood = likelihood; }
 
 #endif
