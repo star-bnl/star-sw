e93ede1e2cd93cf8562ac32aec9440c9ba428f78
Thomas Ullrich <thomas.ullrich@bnl.gov>
2007-09-19 17:32:18 +0000

    New member (mLeadingEdgeTime,  mTrailingEdgeTime) and related functions and updates added.

--- StRoot/StEvent/StTofCell.cxx
+++ StRoot/StEvent/StTofCell.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTofCell.cxx,v 2.4 2004/02/05 17:59:30 ullrich Exp $
+ * $Id: StTofCell.cxx,v 2.5 2007/09/19 17:32:02 ullrich Exp $
  *
  * Author: F. Geurts, May 2003
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StTofCell.cxx,v $
+ * Revision 2.5  2007/09/19 17:32:02  ullrich
+ * New member (mLeadingEdgeTime,  mTrailingEdgeTime) and related functions and updates added.
+ *
  * Revision 2.4  2004/02/05 17:59:30  ullrich
  * Changed $LINK to StLink mechanism and add new member.
  *
@@ -27,20 +30,32 @@
 #include "StTrack.h"
 #include "StThreeVectorD.hh"
 
-static const char rcsid[] = "$Id: StTofCell.cxx,v 2.4 2004/02/05 17:59:30 ullrich Exp $";
+static const char rcsid[] = "$Id: StTofCell.cxx,v 2.5 2007/09/19 17:32:02 ullrich Exp $";
 
 ClassImp(StTofCell)
 
 StTofCell::StTofCell()
-  : mTrayIndex(0), mModuleIndex(0), mCellIndex(0), mAdc(0), mTdc(0), mAssociatedTrack(0)
+    : mTrayIndex(0), mModuleIndex(0), mCellIndex(0), mDaqIndex(0), mAdc(0), mTdc(0),
+      mLeadingEdgeTime(0), mTrailingEdgeTime(0), mAssociatedTrack(0), mZhit(0),
+      mMatchFlag(0)
 { /* noop */ }
 
-StTofCell::StTofCell(int trayId, int moduleId, int cellId, int daqId, int rawAdc,
-		     int rawTdc, StTrack *track, float zhit,
+StTofCell::StTofCell(int trayId, int moduleId, int cellId, int daqId, 
+		     int rawAdc, int rawTdc, StTrack *track, float zhit,
 		     int flag, const StThreeVectorD& pos)
   : mTrayIndex(trayId), mModuleIndex(moduleId), mCellIndex(cellId),
-    mDaqIndex(daqId), mAdc(rawAdc), mTdc(rawTdc), mAssociatedTrack(track),
-    mZhit(zhit), mMatchFlag(flag), mPosition(pos)
+    mDaqIndex(daqId), mAdc(rawAdc), mTdc(rawTdc),
+    mLeadingEdgeTime(0.), mTrailingEdgeTime(0.),
+    mAssociatedTrack(track), mZhit(zhit), mMatchFlag(flag), mPosition(pos)
+{ /* noop */ }
+
+StTofCell::StTofCell(int trayId, int moduleId, int cellId, int daqId, 
+		     StTrack *track, float zhit,
+		     int flag, const StThreeVectorD& pos)
+  : mTrayIndex(trayId), mModuleIndex(moduleId), mCellIndex(cellId),
+    mDaqIndex(daqId), mAdc(0), mTdc(0),
+    mLeadingEdgeTime(0.), mTrailingEdgeTime(0.),
+    mAssociatedTrack(track), mZhit(zhit), mMatchFlag(flag), mPosition(pos)
 { /* noop */ }
 
 StTofCell::~StTofCell() { /* noop */ }
@@ -66,12 +81,19 @@ StTofCell::position() const {return mPosition;}
 int
 StTofCell::operator==(const StTofCell& p) const
 {
-    return (p.mTrayIndex == mTrayIndex && p.mModuleIndex == mModuleIndex &&
+  return ( (p.mTrayIndex == mTrayIndex && p.mModuleIndex == mModuleIndex &&
 	    p.mCellIndex == mCellIndex && p.mDaqIndex == mDaqIndex &&
 	    p.mAdc  == mAdc && p.mTdc  == mTdc &&
 	    p.mAssociatedTrack == mAssociatedTrack &&
 	    p.mZhit == mZhit &&
-            p.mMatchFlag == mMatchFlag);
+            p.mMatchFlag == mMatchFlag) ||
+	   (p.mTrayIndex == mTrayIndex && p.mModuleIndex == mModuleIndex &&
+	    p.mCellIndex == mCellIndex && p.mDaqIndex == mDaqIndex &&
+	    p.mLeadingEdgeTime  == mLeadingEdgeTime &&
+	    p.mTrailingEdgeTime  == mTrailingEdgeTime &&
+	    p.mAssociatedTrack == mAssociatedTrack &&
+	    p.mZhit == mZhit &&
+            p.mMatchFlag == mMatchFlag) );
 }
 
 int
