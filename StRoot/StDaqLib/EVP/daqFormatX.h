8a9ae497afc0dcbe8fd63bf9b534533902e436f4
perev <>
2007-09-27 18:08:32 +0000

    Remove redundant SC defs

--- StRoot/StDaqLib/EVP/daqFormatX.h
+++ StRoot/StDaqLib/EVP/daqFormatX.h
@@ -3,17 +3,5 @@
 // eXtended definitions of DAQ structures for offline
 #include "daqFormats.h"
 
-#define CHAR_SCD        "SCD     "
-#define SCD_FORMAT_VERSION        0x000500001    // Run FY05, version 1
-struct SCD {
-  struct bankHeader bh ;
-  unsigned int time ;    //unix time
-  int mag_field ;
-  unsigned int rich_scalers[17] ;
-} ;
-
-
-
-
 #endif
 
