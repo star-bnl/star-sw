63d9d1dcdfd5aebd0b29fd4e7939ad33182e3d42
Adam Kocoloski <kocolosk@mit.edu>
2007-10-12 17:12:48 +0000

    rename ABC class for subdetector trigger simulators StTriggerSimu => StVirtualTriggerSimu

--- StRoot/StTriggerUtilities/StVirtualTriggerSimu.h
+++ StRoot/StTriggerUtilities/StVirtualTriggerSimu.h
@@ -1,15 +1,15 @@
-#ifndef STAR_StTriggerSimu
-#define STAR_StTriggerSimu
+#ifndef STAR_StVirtualTriggerSimu
+#define STAR_StVirtualTriggerSimu
 
-// $Id: StTriggerSimu.h,v 1.1 2007/09/24 18:32:06 kocolosk Exp $
+// $Id: StVirtualTriggerSimu.h,v 1.1 2007/10/12 17:12:30 kocolosk Exp $
 
 /*****************************************************************************
- * @class StTriggerSimu
+ * @class StVirtualTriggerSimu
  * @author A.Kocoloski
  *
  * Abstract base class defining an interface for subdetector trigger simulators.
  *****************************************************************************/
-class StTriggerSimu
+class StVirtualTriggerSimu
 {
 public:    
     /// returns 1 if passed, 0 if failed, -1 if don't care
@@ -29,7 +29,11 @@ protected:
 #endif
 
 /*****************************************************************************
- * $Log: StTriggerSimu.h,v $
+ * $Log: StVirtualTriggerSimu.h,v $
+ * Revision 1.1  2007/10/12 17:12:30  kocolosk
+ * rename ABC class for subdetector trigger simulators
+ * StTriggerSimu => StVirtualTriggerSimu
+ *
  * Revision 1.1  2007/09/24 18:32:06  kocolosk
  * added ABC class defining an interface for subdetector simulators
  *
