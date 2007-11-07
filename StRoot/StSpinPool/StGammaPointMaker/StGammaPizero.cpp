dded70f55bccba01ba7b224a630d632a93ea085d
Alan Hoffman <ahoff@mit.edu>
2007-11-07 14:12:59 +0000

    inital check in of StGammaPizer.cpp

---
+++ StRoot/StSpinPool/StGammaPointMaker/StGammaPizero.cpp
@@ -0,0 +1,26 @@
+/*
+ *  StGammaPizero.cpp
+ *  EMCGammaCalib
+ *
+ *  Created by Alan Hoffman on 10/19/07.
+ *
+ */
+
+#include "StGammaPizero.h"
+
+//------------------------------------------------------
+StGammaPizero::StGammaPizero()
+{
+	 
+}
+
+//------------------------------------------------------
+StGammaPizero::StGammaPizero(const StGammaPizero & origin)
+{
+	
+}
+
+//------------------------------------------------------
+void StGammaPizero::Reset() {
+
+}
