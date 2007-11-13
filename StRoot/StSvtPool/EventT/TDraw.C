7ea391dbbaf3c9ce38975c7bf05444dcfb52c072
fisyak <>
2007-11-13 17:06:31 +0000

    Be sure that drift + anode corrections < 10

--- StRoot/StSvtPool/EventT/TDraw.C
+++ StRoot/StSvtPool/EventT/TDraw.C
@@ -1134,6 +1134,7 @@ void TDrawD(const Char_t *tag="duuH", Int_t barrel = 1, Int_t ladder = 0, Int_t 
   TH2 *h = 0;
   Int_t head = 0;
   const Int_t NPol1 = NPOL;// first parameter reserved for degree of polynomial
+  if (TString(tag) == "duvH") {NPol1 = 12 - NPOL;} // only 3 power for anodes
   const Int_t NPMax = NPol1;
   Double_t params[NPol1];
   Double_t dparams[NPol1];
