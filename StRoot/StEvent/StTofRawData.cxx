e4911b12f108a1eaa871a569f6c98eebd5fe9146
Thomas Ullrich <thomas.ullrich@bnl.gov>
2007-11-21 00:28:32 +0000

    Added new data member mTray plus access functions. New overloaded constructor.

--- StRoot/StEvent/StTofRawData.cxx
+++ StRoot/StEvent/StTofRawData.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTofRawData.cxx,v 2.1 2005/04/11 22:35:25 calderon Exp $
+ * $Id: StTofRawData.cxx,v 2.2 2007/11/21 00:28:31 ullrich Exp $
  *
  * Author: Xin Dong, Feb 2005
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StTofRawData.cxx,v $
+ * Revision 2.2  2007/11/21 00:28:31  ullrich
+ * Added new data member mTray plus access functions. New overloaded constructor.
+ *
  * Revision 2.1  2005/04/11 22:35:25  calderon
  * Tof Classes for Run 5.  Modifications and additions from Xin to
  * take care of new TOF daq and electronics.  Added StTofRawData and
@@ -20,32 +23,39 @@
 
 #include "StTofRawData.h"
 
-static const char rcsid[] = "$Id: StTofRawData.cxx,v 2.1 2005/04/11 22:35:25 calderon Exp $";
+static const char rcsid[] = "$Id: StTofRawData.cxx,v 2.2 2007/11/21 00:28:31 ullrich Exp $";
 
 ClassImp(StTofRawData)
 
 StTofRawData::StTofRawData()
-  : mLeTeFlag(0), mChannel(0), mTdc(0), mQuality(0)
+: mLeTeFlag(0), mTray(0), mChannel(0), mTdc(0), mQuality(0)
 { /* noop */ }
 
 StTofRawData::StTofRawData(unsigned short iflag, unsigned short channel,
 			   unsigned int rawTdc, unsigned short quality)
-  : mLeTeFlag(iflag), mChannel(channel), mTdc(rawTdc), mQuality(quality)
+  : mLeTeFlag(iflag), mTray(0), mChannel(channel), mTdc(rawTdc), mQuality(quality)
+{ /* noop */ }
+
+StTofRawData::StTofRawData(unsigned short iflag, unsigned short tray,
+			   unsigned short channel,
+			   unsigned int rawTdc, unsigned short quality)
+  : mLeTeFlag(iflag), mTray(tray), mChannel(channel), mTdc(rawTdc), mQuality(quality)
 { /* noop */ }
 
 StTofRawData::~StTofRawData() { /* noop */ }
     
 int StTofRawData::operator==(const StTofRawData& p) const
 {
-    return (p.mLeTeFlag == mLeTeFlag &&
+    return (p.mLeTeFlag == mLeTeFlag && p.mTray == mTray &&
             p.mChannel == mChannel && p.mTdc == mTdc &&
-	    p.mQuality == mQuality);
+	  p.mQuality == mQuality);
 }
 
 int StTofRawData::operator!=(const StTofRawData& p) const
 {
     return !(*this == p);  // use operator==()
 }
+
 /*
 ostream& operator<<(ostream& os, const StTofRawData& d)
 {
