cf43b326f72090fa1d2cfcf47aac71566557ef22
Jason C. Webb <jwebb@bnl.gov>
2007-12-05 23:44:59 +0000

    Time-dependent DSM thresholds added.

--- StRoot/StEEmcUtil/EEdsm/EemcTrigUtil.cxx
+++ StRoot/StEEmcUtil/EEdsm/EemcTrigUtil.cxx
@@ -1,20 +1,59 @@
 #include <assert.h>
 #include <stdio.h>
 #include <StMessMgr.h>
+#include <TString.h>
 
 #include "EemcTrigUtil.h"
+
+
+
+struct DsmThreshold
+{
+  Int_t date_start; 
+  Int_t time_start;// timezone = EST/EDT
+  Int_t date_finish; 
+  Int_t time_finish;
+  Int_t HT[3];
+  Int_t TP[3];
+  Int_t JP[3];
+  Int_t HTTPselect;
+};
+
+// -----------------------------------------------------------------------------------------------------
+//
+// pp2006 run period
+//
+//                             date0    time0  dateF    timeF  HT[]      TP[]      JP[]       HTTP-select   // run number
+DsmThreshold _pp2006Long1a  = {20060323,141204,20060406,170325,{6,12,18},{1,17,31},{32,49,69},2};           // 7082079
+DsmThreshold _pp2006Trans1a = {20060406,170325,20060508,210815,{6,12,22},{1,17,31},{32,49,69},2};           // 7096033
+DsmThreshold _pp2006Trans1b = {20060508,210815,20060508,224937,{6,13,22},{1,21,31},{32,49,69},2};           // 7128050
+DsmThreshold _pp2006Trans1c = {20060508,224937,20060508,230315,{6,12,22},{1,17,31},{32,49,69},2};           // 7128060
+DsmThreshold _pp2006Trans1d = {20060508,230315,20060510,164714,{6,13,22},{1,21,31},{32,49,69},2};           // 7128061
+DsmThreshold _pp2006Long2a  = {20060510,164714,20060512,152554,{6,12,22},{1,20,31},{38,49,69},2};           // 7130037
+DsmThreshold _pp2006Long2b  = {20060512,152554,20060513,175249,{6,17,22},{1,20,31},{39,50,69},2};           // 7132045
+DsmThreshold _pp2006Long2c  = {20060513,175249,20060607,194011,{6,16,22},{1,20,31},{39,52,69},2};           // 7133052
+
+DsmThreshold _thresholds[] = {
+  _pp2006Long1a, _pp2006Trans1a, _pp2006Trans1b, _pp2006Trans1c, _pp2006Trans1d, _pp2006Long2a, _pp2006Long2b, _pp2006Long2c 
+};
+//
+// end pp2006
+
+
+
+
+
 
 //==================================================
 //==================================================
 
 void 
 EemcTrigUtil::getDsmThresholds(int yymmdd, int hhmmss, int *HTthr, int *TPthr, int *JPthr, int &TPthrSelc, int &HTTPthrSelc, int &BEsumthr, int &EEsumthr, int &JPSIthrSelc, int &BarreSide, int &EtotThr){
+
   double wallTime= yymmdd+ hhmmss/1e6;
   int HTthrA[3]={6,12,22}; 
   int TPthrA[3]={1,17,31}; 
-
   int JPthrA[3]={32,49,69};
-
   int TPthrSelcA=2;
   int HTTPthrSelcA=2;
 
@@ -22,39 +61,58 @@ EemcTrigUtil::getDsmThresholds(int yymmdd, int hhmmss, int *HTthr, int *TPthr, i
   int EEsumthrA=21;//need to find out the real value
 
   int JPSIthrSelcA=0;
-
   int BarreSideA=3;
-
   int EtotThrA=109;
 
-  int i;
-  for(i=0;i<3;i++){
+  for(Int_t i=0;i<3;i++){
     HTthr[i]= HTthrA[i];
     TPthr[i]= TPthrA[i];
-
     JPthr[i]= JPthrA[i];
   }
   
-  if(wallTime>=20060409.120000 ){ // R7099070
-    HTthr[1]=13;
-    TPthr[1]=21;
-  }
-
   TPthrSelc=TPthrSelcA;
   HTTPthrSelc=HTTPthrSelcA;
 
   BEsumthr=BEsumthrA;
   EEsumthr=EEsumthrA;
-
   JPSIthrSelc=JPSIthrSelcA;
-  
   BarreSide=BarreSideA;
-
   EtotThr=EtotThrA;
 
-  // temp limits on the avaliable time stamp to ppTrans
-  assert(yymmdd>=20060406  ); // R7096001
-  assert(yymmdd<=20060509  ); // R7129999
+  
+  // Take the above as defaults, and overide with values
+  // defined below
+
+  // pp2006 time dependent thresholds
+  Bool_t go = false;
+  for ( UInt_t i=0;i<sizeof(_thresholds)/sizeof(DsmThreshold);i++ )
+    {
+      Double_t wallStart = _thresholds[i].date_start + _thresholds[i].time_start / 1.0E6;
+      Double_t wallEnd   = _thresholds[i].date_finish + _thresholds[i].time_finish / 1.0E6;
+
+      // shift from GMT to EDT
+      Double_t myWallTime = wallTime - 040000/1.0E6;
+
+      if ( myWallTime >= wallStart && myWallTime < wallEnd ) {
+	go = true;
+	for(Int_t j=0;j<3;j++)
+	  {
+	    HTthr[j]= _thresholds[i].HT[j];
+	    TPthr[j]= _thresholds[i].TP[j];
+	    JPthr[j]= _thresholds[i].JP[j];
+	  }
+	HTTPthrSelc=_thresholds[i].HTTPselect;
+	break;
+      }
+    }
+  assert(go); // or timestamp out of range of lookup table in EemcTrigUtil
+  
+  LOG_INFO<<Form( "Load DSM thresholds for %8i %06i: HT %02i %02i %02i TP %02i %02i %02i JP %02i %02i %02i",
+		  yymmdd,hhmmss,
+		  HTthr[0], HTthr[1], HTthr[2],
+		  TPthr[0], TPthr[1], TPthr[2], 
+		  JPthr[0], JPthr[1], JPthr[2]
+		  ) << endm;
 
 }
 
