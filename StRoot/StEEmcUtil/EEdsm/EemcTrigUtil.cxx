109f8ddf312fa904242880ec2bb4d3cb34e52837
jwebb <>
2007-12-14 22:46:14 +0000

    Shifted timestamps to GMT (rather than have a 4h offset in code).

--- StRoot/StEEmcUtil/EEdsm/EemcTrigUtil.cxx
+++ StRoot/StEEmcUtil/EEdsm/EemcTrigUtil.cxx
@@ -10,7 +10,7 @@
 struct DsmThreshold
 {
   Int_t date_start; 
-  Int_t time_start;// timezone = EST/EDT
+  Int_t time_start;// timezone = GMT  
   Int_t date_finish; 
   Int_t time_finish;
   Int_t HT[3];
@@ -24,14 +24,14 @@ struct DsmThreshold
 // pp2006 run period http://drupal.star.bnl.gov/STAR/?q=node/8349
 //
 //                             date0    time0  dateF    timeF  HT[]      TP[]      JP[]       HTTP-select   // run number
-DsmThreshold _pp2006Long1a  = {20060323,141204,20060406,170325,{6,12,18},{1,17,31},{32,49,69},2};           // 7082079
-DsmThreshold _pp2006Trans1a = {20060406,170325,20060508,210815,{6,12,22},{1,17,31},{32,49,69},2};           // 7096033
-DsmThreshold _pp2006Trans1b = {20060508,210815,20060508,224937,{6,13,22},{1,21,31},{32,49,69},2};           // 7128050
-DsmThreshold _pp2006Trans1c = {20060508,224937,20060508,230315,{6,12,22},{1,17,31},{32,49,69},2};           // 7128060
-DsmThreshold _pp2006Trans1d = {20060508,230315,20060510,164714,{6,13,22},{1,21,31},{32,49,69},2};           // 7128061
-DsmThreshold _pp2006Long2a  = {20060510,164714,20060512,152554,{6,12,22},{1,20,31},{38,49,69},2};           // 7130037
-DsmThreshold _pp2006Long2b  = {20060512,152554,20060513,175249,{6,17,22},{1,20,31},{39,50,69},2};           // 7132045
-DsmThreshold _pp2006Long2c  = {20060513,175249,20060607,194011,{6,16,22},{1,20,31},{39,52,69},2};           // 7133052
+DsmThreshold _pp2006Long1a  = {20060323,181204,20060406,210325,{6,12,18},{1,17,31},{32,49,69},2}; // 7082079
+DsmThreshold _pp2006Trans1a = {20060406,210325,20060509, 10815,{6,12,22},{1,17,31},{32,49,69},2}; // 7096033
+DsmThreshold _pp2006Trans1b = {20060509, 10815,20060509, 24937,{6,13,22},{1,21,31},{32,49,69},2}; // 7128050
+DsmThreshold _pp2006Trans1c = {20060509, 24937,20060509, 30315,{6,12,22},{1,17,31},{36,49,69},2}; // 7128060
+DsmThreshold _pp2006Trans1d = {20060509, 30315,20060510,204714,{6,13,22},{1,21,31},{32,49,69},2}; // 7128061
+DsmThreshold _pp2006Long2a  = {20060510,204714,20060512,192554,{6,12,22},{1,20,31},{38,49,69},2}; // 7130037
+DsmThreshold _pp2006Long2b  = {20060512,192554,20060513,215249,{6,17,22},{1,20,31},{39,50,69},2}; // 7132045
+DsmThreshold _pp2006Long2c  = {20060513,215249,20060607,234011,{6,16,22},{1,20,31},{39,52,69},2}; // 7133052
 
 DsmThreshold _thresholds[] = {
   _pp2006Long1a, _pp2006Trans1a, _pp2006Trans1b, _pp2006Trans1c, _pp2006Trans1d, _pp2006Long2a, _pp2006Long2b, _pp2006Long2c 
@@ -91,7 +91,7 @@ EemcTrigUtil::getDsmThresholds(int yymmdd, int hhmmss, int *HTthr, int *TPthr, i
       Double_t wallEnd   = _thresholds[i].date_finish + _thresholds[i].time_finish / 1.0E6;
 
       // shift from GMT to EDT
-      Double_t myWallTime = wallTime - 040000/1.0E6;
+      Double_t myWallTime = wallTime;// (in case we need to shift)
 
       if ( myWallTime >= wallStart && myWallTime < wallEnd ) {
 	go = true;
@@ -105,7 +105,7 @@ EemcTrigUtil::getDsmThresholds(int yymmdd, int hhmmss, int *HTthr, int *TPthr, i
 	break;
       }
     }
-  assert(go); // or timestamp out of range of lookup table in EemcTrigUtil
+  assert(go); // timestamp out of range of lookup table in EemcTrigUtil
   
   LOG_INFO<<Form( "Load DSM thresholds for %8i %06i: HT %02i %02i %02i TP %02i %02i %02i JP %02i %02i %02i",
 		  yymmdd,hhmmss,
