62b4e50eda4823195d2c99cf695c120d6b9a2826
jeromel <>
2008-01-08 18:25:32 +0000

    There are no differences (ignore)

--- StRoot/StiTpc/StiTpcDetectorBuilder.cxx
+++ StRoot/StiTpc/StiTpcDetectorBuilder.cxx
@@ -19,11 +19,11 @@
 #include "Stiostream.h"
 #include "Sti/StiNeverActiveFunctor.h"
 #include "Sti/StiHitErrorCalculator.h"
-#include "StiTpcDetectorBuilder.h" 
+#include "StiTpcDetectorBuilder.h"
 #include "StiTpcIsActiveFunctor.h"
 #include "Sti/StiElossCalculator.h"
 #include "StDetectorDbMaker/StDetectorDbTpcRDOMasks.h"
-#include "StDbUtilities/StCoordinates.hh" 
+#include "StDbUtilities/StCoordinates.hh"
 #include "StTpcDb/StTpcDb.h"
 #include "tables/St_HitError_Table.h"
 #include "StMatrixD.hh"
@@ -49,16 +49,16 @@ The material currently used are P10, and NOMEX. The properties
 of these materials are extracted from the Particle Data Book.
 The detector components of the TPC include the 24 sectors, 45 padrow gas volumes, and
 the inner and outer field cage of the TPC. The padrows  are polygonal with 12  sides
-whereas  the field cage are cylindrical. However to match the 12 fold symmetry of the 
+whereas  the field cage are cylindrical. However to match the 12 fold symmetry of the
 TPC, the field cage are artificially segmented into 12 sectors each.
 */
 void StiTpcDetectorBuilder::buildDetectors(StMaker&source)
 {
   cout << "StiTpcDetectorBuilder::buildDetectors() -I- Started" << endl;
-  if (!gStTpcDb) 
+  if (!gStTpcDb)
     throw runtime_error("StiTpcDetectorBuilder::buildDetectors() -E- gStTpcDb==0");
   load(_inputFile,source);
-  useVMCGeometry(); 
+  useVMCGeometry();
   cout << "StiTpcDetectorBuilder::buildDetectors() -I- Done" << endl;
 }
 
@@ -103,11 +103,11 @@ _outerCalc.set(oTpt[0], oTpt[1],oTpt[2], oTpt[3],oTpt[4], oTpt[5]);
 //________________________________________________________________________________
 void StiTpcDetectorBuilder::useVMCGeometry() {
   int debug = 0;
-  
+
   if (debug>1) StiVMCToolKit::SetDebug(1);
   cout << "StiTpcDetectorBuilder::buildDetectors() -I- Use VMC geometry" << endl;
   SetCurrentDetectorBuilder(this);
-  const VolumeMap_t TpcVolumes[] = { 
+  const VolumeMap_t TpcVolumes[] = {
     //  {"TPCE","the TPC system in STAR","HALL_1/CAVE_1/TPCE_1","",""},
     //  {"TPCW","the TPC supporting endcap Wheel","HALL_1/CAVE_1/TPCE_1/TPCW_1-2/*","",""},
     //  {"TPEA","one endcap placed in TPC","HALL_1/CAVE_1/TPCE_1/TPEA_1-2/*","",""},
@@ -120,13 +120,13 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
     {"TPA1","outer pad row","HALL_1/CAVE_1/TPCE_1/TPGV_%d/TPSS_%d/TPA1_%d","tpc",""}
   };
   _padPlane = gStTpcDb->PadPlaneGeometry();
-  if (!_padPlane) 
+  if (!_padPlane)
     throw runtime_error("StiTpcDetectorBuilder::buildDetectors() -E- _padPlane==0");
-  
+
   _dimensions = gStTpcDb->Dimensions();
   if (!_dimensions)
     throw runtime_error("StiTpcDetectorBuilder::buildDetectors() -E- _dimensions==0");
-  
+
   // change to +1 instead of +2 to remove the ofc.
   unsigned int nRows = _padPlane->numberOfRows();// Only sensitive detectors
   setNRows(nRows);
@@ -148,14 +148,14 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
 				mat->GetDensity()*mat->GetRadLen(),
 				PotI));
   Double_t ionization = _gasMat->getIonization();
-  StiElossCalculator *gasElossCalculator =  new StiElossCalculator(_gasMat->getZOverA(), ionization*ionization, 
+  StiElossCalculator *gasElossCalculator =  new StiElossCalculator(_gasMat->getZOverA(), ionization*ionization,
 								   _gasMat->getA(), _gasMat->getZ(), _gasMat->getDensity());
   StDetectorDbTpcRDOMasks *s_pRdoMasks = StDetectorDbTpcRDOMasks::instance();
   StiPlanarShape *pShape;
-  //Active TPC padrows 
+  //Active TPC padrows
   //  double radToDeg = 180./3.1415927;
   StTpcCoordinateTransform transform(gStTpcDb);
-  StMatrixD  local2GlobalRotation; 
+  StMatrixD  local2GlobalRotation;
   StMatrixD  unit(3,3,1);
   StThreeVectorD RowPosition;
   unsigned int _nInnerPadrows = _padPlane->numberOfInnerRows();
@@ -171,7 +171,7 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
     if(row<_nInnerPadrows) {
       pShape->setThickness(_padPlane->innerSectorPadLength());
       dZ = _dimensions->innerEffectiveDriftDistance()/2;
-    }   
+    }
     else {
       pShape->setThickness(_padPlane->outerSectorPadLength());
       dZ = _dimensions->outerEffectiveDriftDistance()/2;
@@ -188,8 +188,8 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
       local2GlobalRotation = unit;
       for (int i = 0; i < 3; i++) {
 	//	if (debug>1) cout << "dirLS\t" << dirLS[i] << endl;
-#ifndef TPC_IDEAL_GEOM 
-	StTpcLocalDirection        dirL;      
+#ifndef TPC_IDEAL_GEOM
+	StTpcLocalDirection        dirL;
 	StTpcLocalSectorAlignedDirection  dirLSA;
 	transform(dirLS[i],dirLSA);//   if (debug>1) cout << "dirLSA\t" << dirLSA << endl;
 	transform(dirLSA,dirL);    //   if (debug>1) cout << "dirL\t" << dirL << endl;
@@ -206,15 +206,15 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
       //      if (debug>1) cout << "Local2GlobalRotation = " << local2GlobalRotation << endl;
       double y  = transform.yFromRow(row+1);
        StTpcLocalSectorCoordinate  lsCoord(0., y, dZ, sector+1, row+1);// if (debug>1) cout << lsCoord << endl;
-#ifndef TPC_IDEAL_GEOM 
-      StTpcLocalSectorAlignedCoordinate lsCoordA;  
-      transform(lsCoord,lsCoordA);//                       if (debug>1) cout << lsCoordA << endl;                   
-      StGlobalCoordinate  gCoord; 
-      transform(lsCoordA, gCoord);//                       if (debug>1) cout << gCoord << endl;                   
+#ifndef TPC_IDEAL_GEOM
+      StTpcLocalSectorAlignedCoordinate lsCoordA;
+      transform(lsCoord,lsCoordA);//                       if (debug>1) cout << lsCoordA << endl;
+      StGlobalCoordinate  gCoord;
+      transform(lsCoordA, gCoord);//                       if (debug>1) cout << gCoord << endl;
 #else  // Ideal geom
       StTpcLocalCoordinate gCoord;
       transform(lsCoord, gCoord);
-#endif      
+#endif
       //unit vector normal to the pad plane
       StThreeVectorD centerVector(gCoord.position().x(),gCoord.position().y(),gCoord.position().z());
       StThreeVectorD normalVector(local2GlobalRotation(2,1),
@@ -232,13 +232,13 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
       pPlacement->setLayerRadius(fRadius);
       pPlacement->setLayerAngle(phi);
       pPlacement->setRegion(StiPlacement::kMidRapidity);
-      pPlacement->setNormalRep(phiD, r*TMath::Cos(phi-phiD), r*TMath::Sin(phi-phiD)); 
+      pPlacement->setNormalRep(phiD, r*TMath::Cos(phi-phiD), r*TMath::Sin(phi-phiD));
       name = Form("Tpc/Padrow_%d/Sector_%d", row, sector);
       // fill in the detector object and save it in our vector
       StiDetector *pDetector = _detectorFactory->getInstance();
       pDetector->setName(name.Data());
       pDetector->setIsOn(true);
-      
+
       int iRdo = rdoForPadrow(row+1);
       bool west = s_pRdoMasks->isOn(sector+1, iRdo);
       bool east = s_pRdoMasks->isOn( 24-(sector+1)%12, iRdo);
@@ -266,7 +266,7 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
   TString pathT("HALL_1/CAVE_1/TPCE_1/TIFC_1");
   TString path("");
   for (Int_t i = 0; i < 1; i++) {
-    gGeoManager->RestoreMasterVolume(); 
+    gGeoManager->RestoreMasterVolume();
     gGeoManager->CdTop();
     pathT = TpcVolumes[i].path;
     gGeoManager->cd(pathT); path = pathT;
@@ -274,5 +274,5 @@ void StiTpcDetectorBuilder::useVMCGeometry() {
     if (! nodeT) continue;
     StiVMCToolKit::LoopOverNodes(nodeT, path, TpcVolumes[i].name, MakeAverageVolume);
   }
-  cout << "StiTpcDetectorBuilder::buildDetectors() -I- Done" << endl; 
+  cout << "StiTpcDetectorBuilder::buildDetectors() -I- Done" << endl;
 }
