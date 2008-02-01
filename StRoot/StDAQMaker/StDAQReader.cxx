cc2ab0110ebeb09391b6e3c938c42870c4968139
Valeri Fine <val.fine@gmail.com>
2008-02-01 01:23:24 +0000

    Add the message

--- StRoot/StDAQMaker/StDAQReader.cxx
+++ StRoot/StDAQMaker/StDAQReader.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StDAQReader.cxx,v 1.58 2008/02/01 01:20:57 fine Exp $
+ * $Id: StDAQReader.cxx,v 1.59 2008/02/01 01:23:24 fine Exp $
  *
  * Author: Victor Perev
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StDAQReader.cxx,v $
+ * Revision 1.59  2008/02/01 01:23:24  fine
+ * Add the message
+ *
  * Revision 1.58  2008/02/01 01:20:57  fine
  * :fix the buffer size for DATAP
  *
@@ -276,6 +279,7 @@ int StDAQReader::open(const char *file)
 #else
   if (fDaqFileReader) close();
   fDaqFileReader = new evpReader((char *)file);
+  LOG_INFO << "StDAQReader::open the DAQ " <<  file << " via evpReader << endm;
 #endif
   return 0;  
 }
