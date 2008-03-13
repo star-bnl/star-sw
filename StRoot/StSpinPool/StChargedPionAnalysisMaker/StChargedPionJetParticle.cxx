855599e88586a8e3d46afb1d8f1a050115e6af69
cvs2git <cvs2git>
2008-03-13 20:08:16 +0000

    This commit was manufactured by cvs2git to create tag 'tai_before_refactroing'.

--- StRoot/StSpinPool/StChargedPionAnalysisMaker/StChargedPionJetParticle.cxx
+++
@@ -1,8 +0,0 @@
-#include "StChargedPionJetParticle.h"
-
-ClassImp(StChargedPionJetParticle)
-
-StChargedPionJetParticle::StChargedPionJetParticle() : LorentzVector< PtEtaPhiE4D<Double32_t> >() { /* no-op */ }
-
-StChargedPionJetParticle::~StChargedPionJetParticle() { /* no-op */ }
-
