88deaef00eb3a30a1d3c896b800ccdca737c93e5
Duncan J. Prindle <prindle@npl.washington.edu>
2008-03-19 22:15:34 +0000

    Thought I had added these already.

---
+++ StRoot/StEStructPool/macros/combineHistograms0.C
@@ -0,0 +1,145 @@
+void combineHistograms0(const char *dirName, const char **inNames, const char *outName, int nCent) {
+
+    // -- Calculate \Delta\rho/\sqrt{\rho_{ref}} for all quantities, i.e. Phi-Phi, Eta-Eta, DPhi-DEta etc.
+    //
+    // root.exe -q -b combineHistograms0.C("dir","inFileList","outFileName",numFiles)
+    // 
+    //    inFileList contains centrality information, so I can have a list
+    //    like {"Data1", "Data2",...}  or {"Sum0_1", "Sum2_3",...}
+    //    Before getting here we use hadd to add histograms from different jobs
+    //    creating files we usually call Data{n}.root (where n is a centrality bin)
+    //    then we may combine centralities be merging the Data{n} files tagging
+    //    histograms with z-vertex index and creating files Sum{n1}_{n2}.root where
+    //    n1 and n2 are the limits of the centrality bin range.
+
+    gROOT->LoadMacro("load2ptLibs.C");
+    load2ptLibs();
+    gSystem->Load("StEStructPoolSupport.so");
+
+    TFile *tf;
+    StEStructSupport *ehelp;
+    TH2F **ptdedp;
+    TH2F **ptdedpC;
+    TH2F **dedp;
+    TH2F **dedpC;
+    TH2F **ytyt;
+    TH2F **ytytC;
+    TH2F **ptetaeta;
+    TH2F **ptetaetaC;
+    TH2F **etaeta;
+    TH2F **etaetaC;
+    TH2F **ptphiphi;
+    TH2F **ptphiphiC;
+    TH2F **phiphi;
+    TH2F **phiphiC;
+
+    const char* binName[]={"Symm"};
+    const char* chargeName[] = {"_LS_", "_US_", "_CD_", "_CI_"};
+    const char* chargeType[] = {"_PP_", "_PM_", "_MP_", "_MM_"};
+    char outFileName[1024];
+    sprintf(outFileName,"%s/%s.root",dirName,outName);
+    TFile *out = new TFile(outFileName,"RECREATE");
+
+    char inFileName[1024];
+
+    for (int ic=0;ic<nCent;ic++) {
+        for (int ibin=0;ibin<1;ibin++) {
+            sprintf(inFileName,"%s/%s%s.root",dirName,inNames[ic],binName[ibin]);
+            tf        = new TFile(inFileName);
+            tf->cd();
+            ehelp     = new StEStructSupport(tf,0);
+            ehelp->msilent            = true;
+            ehelp->mapplyDEtaFix      = false;
+            ehelp->mPairNormalization = true;
+            ehelp->mIdenticalPair     = true;
+            int subtract = 1;
+            ptdedpC   = (TH2F**) ehelp->buildPtCommon("DEtaDPhi",2,subtract);
+            ptetaetaC = (TH2F**) ehelp->buildPtCommon("EtaEta",2,subtract);
+            ptphiphiC = (TH2F**) ehelp->buildPtCommon("PhiPhi",2,subtract);
+
+            ptdedp   = (TH2F**) ehelp->buildPtChargeTypes("DEtaDPhi",2,subtract);
+            ptetaeta = (TH2F**) ehelp->buildPtChargeTypes("EtaEta",2,subtract);
+            ptphiphi = (TH2F**) ehelp->buildPtChargeTypes("PhiPhi",2,subtract);
+
+            dedpC     = (TH2F**) ehelp->buildCommon("DEtaDPhi",2);
+            ytytC     = (TH2F**) ehelp->buildCommon("YtYt",3);
+            etaetaC   = (TH2F**) ehelp->buildCommon("EtaEta",2);
+            phiphiC   = (TH2F**) ehelp->buildCommon("PhiPhi",2);
+
+            dedp     = (TH2F**) ehelp->buildChargeTypes("DEtaDPhi",2);
+            ytyt     = (TH2F**) ehelp->buildChargeTypes("YtYt",3);
+            etaeta   = (TH2F**) ehelp->buildChargeTypes("EtaEta",2);
+            phiphi   = (TH2F**) ehelp->buildChargeTypes("PhiPhi",2);
+
+            out->cd();
+            for (int icharge=0;icharge<4;icharge++) {
+                TString name("NDEtaDPhi"); name += chargeName[icharge];  name += ic;
+                dedp[icharge]->SetName(name.Data());
+                dedp[icharge]->SetTitle(name.Data());
+                dedp[icharge]->Write();
+                TString name("PtDEtaDPhi"); name += chargeName[icharge];  name += ic;
+                ptdedp[icharge]->SetName(name.Data());
+                ptdedp[icharge]->SetTitle(name.Data());
+                ptdedp[icharge]->Write();
+                TString name("YtYt"); name += chargeName[icharge];  name += ic;
+                ytyt[icharge]->SetName(name.Data());
+                ytyt[icharge]->SetTitle(name.Data());
+                ytyt[icharge]->Write();
+
+                TString name("NDEtaDPhi"); name += chargeType[icharge];  name += ic;
+                dedpC[icharge]->SetName(name.Data());
+                dedpC[icharge]->SetTitle(name.Data());
+                dedpC[icharge]->Write();
+                TString name("PtDEtaDPhi"); name += chargeType[icharge];  name += ic;
+                ptdedpC[icharge]->SetName(name.Data());
+                ptdedpC[icharge]->SetTitle(name.Data());
+                ptdedpC[icharge]->Write();
+                TString name("YtYt"); name += chargeType[icharge];  name += ic;
+                ytytC[icharge]->SetName(name.Data());
+                ytytC[icharge]->SetTitle(name.Data());
+                ytytC[icharge]->Write();
+
+
+                TString name("NEtaEta"); name += chargeName[icharge];  name += ic;
+                etaeta[icharge]->SetName(name.Data());
+                etaeta[icharge]->SetTitle(name.Data());
+                etaeta[icharge]->Write();
+                TString name("PtEtaEta"); name += chargeName[icharge];  name += ic;
+                ptetaeta[icharge]->SetName(name.Data());
+                ptetaeta[icharge]->SetTitle(name.Data());
+                ptetaeta[icharge]->Write();
+
+                TString name("NEtaEta"); name += chargeType[icharge];  name += ic;
+                etaetaC[icharge]->SetName(name.Data());
+                etaetaC[icharge]->SetTitle(name.Data());
+                etaetaC[icharge]->Write();
+                TString name("PtEtaEta"); name += chargeType[icharge];  name += ic;
+                ptetaetaC[icharge]->SetName(name.Data());
+                ptetaetaC[icharge]->SetTitle(name.Data());
+                ptetaetaC[icharge]->Write();
+
+
+                TString name("NPhiPhi"); name += chargeName[icharge];  name += ic;
+                phiphi[icharge]->SetName(name.Data());
+                phiphi[icharge]->SetTitle(name.Data());
+                phiphi[icharge]->Write();
+                TString name("PtPhiPhi"); name += chargeName[icharge];  name += ic;
+                ptphiphi[icharge]->SetName(name.Data());
+                ptphiphi[icharge]->SetTitle(name.Data());
+                ptphiphi[icharge]->Write();
+
+                TString name("NPhiPhi"); name += chargeType[icharge];  name += ic;
+                phiphiC[icharge]->SetName(name.Data());
+                phiphiC[icharge]->SetTitle(name.Data());
+                phiphiC[icharge]->Write();
+                TString name("PtPhiPhi"); name += chargeType[icharge];  name += ic;
+                ptphiphiC[icharge]->SetName(name.Data());
+                ptphiphiC[icharge]->SetTitle(name.Data());
+                ptphiphiC[icharge]->Write();
+            }
+            delete tf;
+        }
+    }
+    out->Close();
+    delete out;
+}
