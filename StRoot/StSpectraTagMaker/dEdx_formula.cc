e448f3164057c0b638793f107555ea865dba374c
Yuri Fisyak <fisyak@bnl.gov>
2008-04-02 23:44:23 +0000

    use Bichsel::Instance

--- StRoot/StSpectraTagMaker/dEdx_formula.cc
+++ StRoot/StSpectraTagMaker/dEdx_formula.cc
@@ -2,7 +2,7 @@
 #include "StBichsel/Bichsel.h"
 static Bichsel *fBichsel = 0;
 double dEdx_formula(double x,double massin){
-  if (fBichsel==0) fBichsel = new Bichsel();
+  if (fBichsel==0) fBichsel = Bichsel::Instance();
   double real_momentum = x;
   double mass = massin;
   double bg2 = real_momentum*real_momentum/(mass*mass);
