2d14e4c8cb94a118077c1edba259533add7754e4
Yuri Fisyak <fisyak@bnl.gov>
2008-04-07 13:34:03 +0000

    Add protection against g2t_ssd_hit == 0

--- StRoot/StSsdFastSimMaker/StSsdFastSimMaker.cxx
+++ StRoot/StSsdFastSimMaker/StSsdFastSimMaker.cxx
@@ -164,7 +164,7 @@ Int_t StSsdFastSimMaker::Make()
   LOG_DEBUG << "Geometry Loaded" << endm;
   St_g2t_ssd_hit *g2t_ssd_hit = (St_g2t_ssd_hit *) geant("g2t_ssd_hit");
   if (! g2t_ssd_hit) {
-    LOG_WARN << "g2t_ssd_hit on input, bye bye" << endm; return kStWarn;
+    LOG_WARN << "No g2t_ssd_hit on input, bye bye" << endm; return kStWarn;
     return kStWarn;
   }
   g2t_ssd_hit_st *g2t         = g2t_ssd_hit->GetTable();
