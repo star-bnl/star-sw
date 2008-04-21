49c612d0e975d6b3e8e73d117895fde8821b01f4
tai <>
2008-04-21 16:41:59 +0000

    moved the class StCdfChargedConePars from StCdfChargedConeJetFinder.{cxx,h} to tCdfChargedConePars.{cxx,h}

---
+++ StRoot/StJetFinder/StCdfChargedConePars.cxx
@@ -0,0 +1,5 @@
+// $Id: StCdfChargedConePars.cxx,v 1.1 2008/04/21 16:41:59 tai Exp $
+#include "StCdfChargedConePars.h"
+
+ClassImp(StCdfChargedConePars)
+    
---
+++ StRoot/StJetFinder/StCdfChargedConePars.h
@@ -0,0 +1,14 @@
+// -*- mode: c++;-*-
+// $Id: StCdfChargedConePars.h,v 1.1 2008/04/21 16:41:59 tai Exp $
+#ifndef STCDFCHARGEDCONEPARS_H
+#define STCDFCHARGEDCONEPARS_H
+
+#include "StConePars.h"
+
+class StCdfChargedConePars : public StConePars {
+
+private:    
+  ClassDef(StCdfChargedConePars,1)
+};
+
+#endif // STCDFCHARGEDCONEPARS_H
