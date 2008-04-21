5a92c54ac7a48e876143a1ca23c4fb48a7462f3c
Tai Sakuma <sakuma@bnl.gov>
2008-04-21 21:37:05 +0000

    cleaned up the code

--- StRoot/StJetMaker/StppJetAnalyzer2.h
+++ StRoot/StJetMaker/StppJetAnalyzer2.h
@@ -1,5 +1,5 @@
 // -*- mode: c++;-*-
-// $Id: StppJetAnalyzer2.h,v 1.4 2008/04/21 21:20:26 tai Exp $
+// $Id: StppJetAnalyzer2.h,v 1.5 2008/04/21 21:37:05 tai Exp $
 #ifndef STPPJETANALYZER2_HH
 #define STPPJETANALYZER2_HH
 
@@ -23,43 +23,31 @@ class StppJetAnalyzer2 : public TObject {
 
 public:
 
-  typedef std::list<StProtoJet> JetList;
+  typedef std::list<StProtoJet> ProtoJetList;
 
-  ///Construct a new Analyzer
-  StppJetAnalyzer2(const StppAnaPars* ap, StJetPars* jp, StFourPMaker* fp, JetList& protoJets);
+  StppJetAnalyzer2(const StppAnaPars* ap, StJetPars* jp, StFourPMaker* fp, ProtoJetList& protoJets);
+
   virtual ~StppJetAnalyzer2();
 
-
-  void print();
-
-  ///filter the list, call StJetFinder::findJets(), and filter the jets
   void findJets();
-
-  ///Access to the stl container of protojets
-  JetList& getJets(void) {return mProtoJets;} //!
-    
-  ///Access to the FourPMaker associated with this analyzer.  This 4-p may be shared with other analyzers
-  StFourPMaker* fourPMaker() {return mFourPMaker;}
 
 private:
 
-  StppJetAnalyzer2();
-
+  void collectFourMomentum();
+  void applyCuts();
 
   bool accept4p(StMuTrackFourVec* p);
 
   bool isChargedTrack(StMuTrackFourVec* p);
 
-  void acceptJets();
   bool acceptJet(StProtoJet &pj);
-  void fillLists();
     
-  StJetFinder* mFinder;
+  StJetFinder* _jetFinder;
 
-  JetList& mProtoJets;
-  StFourPMaker* mFourPMaker;
+  ProtoJetList& _protoJetList;
+  StFourPMaker* _fourPMaker;
 
-  StppAnaPars mPars;
+  StppAnaPars _anaPar;
 
 
   ClassDef(StppJetAnalyzer2,1)
