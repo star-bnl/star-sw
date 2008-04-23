e8fc6761b9ccda26e14d8d620d84c979b8c010a3
cvs2git <cvs2git>
2008-04-23 01:13:09 +0000

    This commit was manufactured by cvs2git to create tag 'before_collapsing_stprotojet_stfourvec'.

--- StarDb/Geometry/svt/SvtOnGlobal.20050101.000000.C
+++
@@ -1,7 +0,0 @@
-TDataSet *CreateTable() {
-  if (!gROOT->GetClass("St_Survey")) return 0;
-  Survey_st row = {0, 1,0,-0.00044, 0,1,0.000132, 0.000441,-0.000132,1, 0.221908,0.143028,0.209079,.1,.1,.1,.1,.1,.1,"Inverse Tpc"};
-  St_Survey *tableSet = new St_Survey("SvtOnGlobal",1);
-  tableSet->AddAt(&row.Id, 0);
-  return (TDataSet *)tableSet;
-}
