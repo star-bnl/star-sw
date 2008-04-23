9592e1c0a489f4bcbed170d86c0af9199ec189b8
Tai Sakuma <sakuma@bnl.gov>
2008-04-23 20:00:01 +0000

    cleaned up the code

--- StRoot/StJetFinder/StProtoJet.cxx
+++ StRoot/StJetFinder/StProtoJet.cxx
@@ -1,23 +1,21 @@
-// $Id: StProtoJet.cxx,v 1.10 2008/04/23 19:24:09 tai Exp $
+// $Id: StProtoJet.cxx,v 1.11 2008/04/23 20:00:01 tai Exp $
 
 #include "StProtoJet.h"
 
 using namespace std;
 
 StProtoJet::StProtoJet()
-  : mPx(0), mPy(0), mPz(0), mE(0), mCharge(0) 
+  : mPx(0), mPy(0), mPz(0), mE(0)
+  , mCharge(0) 
 {
 
 }
 
 StProtoJet::StProtoJet(AbstractFourVec* vec)
+  : mPx(vec->px()), mPy(vec->py()), mPz(vec->pz()), mE(vec->e())
+  , mCharge(vec->charge())
 {
     mList.push_back(vec);
-    mPx = vec->px();
-    mPy = vec->py();
-    mPz = vec->pz();
-    mE  = vec->e();
-    mCharge = vec->charge();
 }
 
 StProtoJet::~StProtoJet()
@@ -35,24 +33,6 @@ void StProtoJet::update()
 	mPz += vec->pz();
 	mE  += vec->e();
 	mCharge += vec->charge();
-    }
-}
-
-void StProtoJet::remove(StProtoJet& rhs)
-{
-    FourVecList& l = rhs.list();
-
-    while (l.empty()==false) {
-	FourVecList::iterator where = find(mList.begin(), mList.end(), l.front() );
-
-	if (where==l.end()) {
-	    cout <<"StProtoJet::remove(StProtoJet&). ERROR:\t"
-		 <<"4-vec not found.  return w/o action"<<endl;
-	    return;
-	}
-	else {
-	    l.erase(where);
-	}
     }
 }
 
@@ -87,4 +67,10 @@ void StProtoJet::merge(const StProtoJet& rhs)
     mE  += (*it)->e();
     mCharge += (*it)->charge();
   }
+}
+
+void StProtoJet::clear()
+{
+  mList.clear();
+  mPx = mPy = mPz = mE = mCharge = 0;
 }
