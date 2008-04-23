02dd9fd5a61ede9b55b4e64fa90b1e3a06311cdd
fine <>
2008-04-23 17:48:04 +0000

    the upgraded version of Draw3D.C

--- StRoot/macros/graphics/Draw3D.C
+++ StRoot/macros/graphics/Draw3D.C
@@ -1,13 +1,35 @@
-void Draw3D(int n,  float *xyz)
+#ifndef __CINT__
+#  include "StEventHelper.h"
+#  include "StTrack.h"
+#  include "TPolyMarker3D.h"
+#  include "TPolyLine3D.h"
+#  include "TSystem.h"
+#  include "TROOT.h"
+#  include "TEnv.h"
+#  include "StCheckQtEnv.h"
+#  include "TStyle.h"
+#else
+  class StTrack;
+#endif  
+static int ___draw_3d_init = 0;
+static const Color_t colorDefault = Color_t(-1);
+static const Style_t styDefault   = Style_t(-1);
+static const Size_t  sizDefault   = Size_t (-1);
+//___________________________________________________
+void Draw3DDraw(TObject *o)
 {
-     TPolyMarker3D *markers = new TPolyMarker3D(n,xyz);
-     markers->SetMarkerStyle(5);
-     markers->SetMarkerColor(kBlue);
-     markers->Draw();
+   if (o) {
+      o->Draw();
+      gPad->SetFillColor(kBlack);
+   }
 }
 
-void Draw3D(){
-   gSystem->Load("St_base");
+//___________________________________________________
+int Draw3DInit(){
+   if (___draw_3d_init) return 1;
+   ___draw_3d_init = 1;
+   gROOT->Macro("Load.C");
+
    // check Coin env and load if present
    TString ivrootDir = "$ROOT/5.99.99/Coin2/.$STAR_HOST_SYS/lib/";
    gSystem->ExpandPathName(ivrootDir);
@@ -18,12 +40,49 @@ void Draw3D(){
       printf(" Loading ... libSmallChange %d \n",gSystem->Load(ivrootDir+"libSmallChange"));
       CheckCoin = false;
    }
-   if (!StCheckQtEnv::SetQtEnv(CheckCoin))   {
+   if (!StCheckQtEnv::SetQtEnv(CheckCoin)) {
       // define the background image
-      const char *backShape = "$STAR/StRoot/macros/graphics/StarTPC.iv";   
+      const char *backShape = "$STAR/StRoot/macros/graphics/StarTPC.iv";
       printf(" Setting the background shape to be 	%s\n", backShape);
       gEnv->SetValue("Gui.InventorBackgroundShape",backShape);
    }
+   return 1;
+}
+//___________________________________________________
+TObject *Draw3DPoints(int n, const float *xyz, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
+{
+   if (!___draw_3d_init) Draw3DInit();
+   TPolyMarker3D *plMk  = new TPolyMarker3D(n,(Float_t*)xyz);
+   if (col != colorDefault) plMk->SetMarkerColor(col);
+   if (sty != styDefault)   plMk->SetMarkerStyle(sty);
+   if (siz != sizDefault)   plMk->SetMarkerSize(siz);
+   Draw3DDraw(plMk);
+   return plMk;
+}
+
+//___________________________________________________
+TObject *Draw3D(int n,  const float *xyz)
+{
+    if (!___draw_3d_init) Draw3DInit();
+    return Draw3DPoints(n,xyz,kBlue,5);
+}
+
+
+//___________________________________________________
+TObject *Draw3DLine(int n,  const float *xyz, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
+{
+   if (!___draw_3d_init) Draw3DInit();
+   TPolyLine3D *plLine  = new TPolyLine3D(n,(Float_t*)xyz);
+   if (col != colorDefault) plLine->SetLineColor(col);
+   if (sty != styDefault)   plLine->SetLineStyle(sty);
+   if (siz != sizDefault)   plLine->SetLineWidth(siz);
+   Draw3DDraw(plLine);
+   return plLine;
+}
+
+//___________________________________________________
+void Draw3D(){
+   if (!___draw_3d_init) Draw3DInit();
    float xyz[] = { 189.195,       27.951,       123.966
                  ,187.195,       28.6187,      122.89
                  ,181.195       ,30.6788       ,119.556
@@ -36,5 +95,22 @@ void Draw3D(){
          };
 
    int sizeXYZ = sizeof(xyz)/sizeof(float)/3;
-   draw3D(sizeXYZ,xyz);
+   const float *fff = (const float *)&xyz[0];
+   fprintf(stderr," %d %p\n", sizeXYZ,fff);
+   Draw3D(sizeXYZ,fff);
+   Draw3DLine(sizeXYZ,fff);
+}
+
+//___________________________________________________
+TObject *Draw3D(const StTrack &track, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
+{
+   StTrackPoints trPnt(&track);
+   return Draw3DLine(trPnt.GetN(),trPnt.GetP(),col,sty,siz);
+}
+
+//___________________________________________________
+TObject *Draw3DInnOut(const StTrack &track, Bool_t in, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
+{
+   StInnOutPoints trInOut(&track,in);
+   return Draw3DPoints(trInOut.GetN(),trInOut.GetP(),col,sty,siz);
 }
