2591b13fc4a3fb54cdd18a3be20602e7b9660a08
Tai Sakuma <sakuma@bnl.gov>
2008-04-23 19:24:09 +0000

    collapsed StProtJet and StFourVec.

--- StRoot/StJetFinder/StProtoJet.h
+++ StRoot/StJetFinder/StProtoJet.h
@@ -1,10 +1,9 @@
 // -*- mode: c++;-*-
-// $Id: StProtoJet.h,v 1.9 2008/04/23 01:28:53 tai Exp $
+// $Id: StProtoJet.h,v 1.10 2008/04/23 19:24:09 tai Exp $
 #ifndef StProtoJet_HH
 #define StProtoJet_HH
 
 #include "AbstractFourVec.h"
-#include "StFourVec.h"
 
 #include <iostream>
 #include <vector>
@@ -18,8 +17,7 @@
   algorithms.  It implements the interface of a four vector and contains a list of
   AbstractFourVec objects.  That way, protojets can be "clustered" to form jets.
  */
-class StProtoJet : public StFourVec
-{
+class StProtoJet {
 
 public:
   typedef std::vector<AbstractFourVec*> FourVecList;
@@ -43,7 +41,7 @@ public:
 	
   ///Add a protojet to this one w/o calculating the new parameters
   void add(const StProtoJet&);
-  void add(const AbstractFourVec& rhs) { StFourVec::add(rhs); }
+  void add(const AbstractFourVec& rhs);
   void remove(StProtoJet&);
 
   ///update the parameters of this protojet (in case some have been added via add())
@@ -53,14 +51,50 @@ public:
   void clear()
   {
     mList.clear();
-    StFourVec::clear(); 
+    mPx = mPy = mPz = mE = mCharge = 0;
+    //    StFourVec::clear(); 
   }
 	
   friend std::ostream& operator<<(std::ostream& os, const StProtoJet& j);
 	
+  //momenta
+  double pt() const { return ::sqrt(mPx*mPx + mPy*mPy); }
+  double px() const { return mPx; }
+  double py() const { return mPy; }
+  double pz() const { return mPz; }
+  
+  //angles
+  double phi() const { return atan2(mPy, mPx); }
+  double eta() const
+  {
+    double arg = tan(theta()/2.);
+    return (arg > 0) ? -::log(arg) : -999;
+  }
+
+  //4-th component
+  double e() const { return fabs(mE); }
+  double eT() const
+  {
+    if(p() == 0.0) return 0.0;
+    return ::sqrt(e()*e()*pt()*pt()/(p()*p()));
+  }
+  double mass() const { return ::sqrt(fabs(mE*mE - p()*p() ) ); }
+	
+  //charge
+  double charge() const { return mCharge; }
+	
 private:
 
+  double p() const { return ::sqrt(mPx*mPx + mPy*mPy + mPz*mPz); }
+  double theta() const { return acos( mPz/p()); }
+
   FourVecList mList;
+
+  double mPx;
+  double mPy;
+  double mPz;
+  double mE;
+  double mCharge;
 
 };
 
