a907e08b78dfe278cbea42ae606c91f3652496da
fine <>
2008-04-25 23:39:23 +0000

    add the StMeasuredPoint class

--- StRoot/macros/graphics/Draw3D.C
+++ StRoot/macros/graphics/Draw3D.C
@@ -1,6 +1,7 @@
 #ifndef __CINT__
 #  include "StEventHelper.h"
 #  include "StTrack.h"
+#  include "StMeasuredPoint.h"
 #  include "TPolyMarker3D.h"
 #  include "TPolyLine3D.h"
 #  include "TSystem.h"
@@ -9,19 +10,32 @@
 #  include "StCheckQtEnv.h"
 #  include "TStyle.h"
 #else
-  class StTrack;
+  class StTrack; 
+  class StMeasuredPoint;
 #endif  
-static int ___draw_3d_init = 0;
-static const Color_t colorDefault = Color_t(-1);
-static const Style_t styDefault   = Style_t(-1);
-static const Size_t  sizDefault   = Size_t (-1);
+//static 
+      int ___draw_3d_init = 0;
+// static 
+      const Color_t colorDefault = Color_t(-1);
+// static 
+      const Style_t styDefault   = Style_t(-1);
+// static 
+      const Size_t  sizDefault   = Size_t (-1);
+// static 
+      Color_t bkColor = kBlack;
 //___________________________________________________
 void Draw3DDraw(TObject *o)
 {
    if (o) {
       o->Draw();
-      gPad->SetFillColor(kBlack);
+      if (gPad->GetFillColor() != bkColor)
+      gPad->SetFillColor(bkColor);
    }
+}
+//___________________________________________________
+void Draw3DBkColor(Color_t newBkColor)
+{
+    bkColor = newBkColor;
 }
 
 //___________________________________________________
@@ -50,6 +64,19 @@ TObject *Draw3DPoints(int n, const float *xyz, Color_t col=colorDefault,Style_t 
 }
 
 //___________________________________________________
+TObject *Draw3DPoint(float x, float y, float z, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
+{
+   if (!___draw_3d_init) Draw3DInit();
+   TPolyMarker3D *plMk  = new TPolyMarker3D();
+   plMk->SetNextPoint(x,y,z);
+   if (col != colorDefault) plMk->SetMarkerColor(col);
+   if (sty != styDefault)   plMk->SetMarkerStyle(sty);
+   if (siz != sizDefault)   plMk->SetMarkerSize(siz);
+   Draw3DDraw(plMk);
+   return plMk;
+}
+
+//___________________________________________________
 TObject *Draw3D(int n,  const float *xyz)
 {
     if (!___draw_3d_init) Draw3DInit();
@@ -64,7 +91,7 @@ TObject *Draw3DLine(int n,  const float *xyz, Color_t col=colorDefault,Style_t s
    TPolyLine3D *plLine  = new TPolyLine3D(n,(Float_t*)xyz);
    if (col != colorDefault) plLine->SetLineColor(col);
    if (sty != styDefault)   plLine->SetLineStyle(sty);
-   if (siz != sizDefault)   plLine->SetLineWidth(siz);
+   if (siz != sizDefault)   plLine->SetLineWidth(Width_t(siz));
    Draw3DDraw(plLine);
    return plLine;
 }
@@ -73,7 +100,7 @@ TObject *Draw3DLine(int n,  const float *xyz, Color_t col=colorDefault,Style_t s
 void Draw3D(){
    if (!___draw_3d_init) Draw3DInit();
    float xyz[] = { 189.195,       27.951,       123.966
-                 ,187.195,       28.6187,      122.89
+                 ,187.195,       28.6187,       122.89
                  ,181.195       ,30.6788       ,119.556
                  ,179.195       ,31.3387       ,118.454
                  ,177.195       ,32.0065       ,117.328
@@ -97,6 +124,13 @@ TObject *Draw3D(const StTrack &track, Color_t col=colorDefault,Style_t sty=styDe
    return Draw3DLine(trPnt.GetN(),trPnt.GetP(),col,sty,siz);
 }
 
+//___________________________________________________
+TObject *Draw3D(const StMeasuredPoint &hit, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
+{
+   // Draw the StMeasuredPoint, StHit, StVertex with the graphical attribute provided
+   const StThreeVectorF& position = hit.position();
+   return Draw3DPoint(position.x(),position.y(),position.z(),col,sty,siz);
+}
 //___________________________________________________
 TObject *Draw3DInnOut(const StTrack &track, Bool_t in, Color_t col=colorDefault,Style_t sty=styDefault,Size_t siz=sizDefault)
 {
