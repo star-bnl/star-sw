f1738e96817275fbfc2b756b4ab9e1a2a8add772
perev <>
2008-04-29 03:18:59 +0000

    More complicated rotations accounted

--- StRoot/Sti/StiVMCToolKit.cxx
+++ StRoot/Sti/StiVMCToolKit.cxx
@@ -774,7 +774,7 @@ TGeoPhysicalNode *StiVMCToolKit::Alignment(const TGeoNode *nodeT, const Char_t *
   // Alignment
   TObjArray *listP = gGeoManager->GetListOfPhysicalNodes();
   TGeoPhysicalNode *nodeP = 0;
-  TGeoTranslation *trP = 0;
+  TGeoCombiTrans *trP = 0;
   if (listP) {
     Int_t N = listP->GetEntries();
     for (Int_t i = 0; i < N; i++) {
@@ -803,12 +803,14 @@ TGeoPhysicalNode *StiVMCToolKit::Alignment(const TGeoNode *nodeT, const Char_t *
   if (Debug()) 
     cout << "\tmaster  x\t" << master[0] << "\ty\t" << master[1] << "\tz\t" << master[2] << endl;
   if (!nodeP) nodeP = gGeoManager->MakePhysicalNode(pathT);
+#if 0
   if (nodeP->IsAligned()) {
-    trP = (TGeoTranslation*)nodeP->GetNode()->GetMatrix();
+    trP = nodeP->GetNode()->GetMatrix();
     trP->SetTranslation(master[0], master[1], master[2]);
   } else {  
-    trP = new TGeoTranslation(master[0], master[1], master[2]);
-  }   
+    trP = new TGeoCombiTrans(master[0], master[1], master[2], nodeP->GetNode()->GetMatrix());
+  }  
+#endif 
   nodeP->Align(trP,newshape);//,kTRUE);
   TGeoVolume *newvol = nodeP->GetNode(-1)->GetVolume();
   if (Debug()) 
