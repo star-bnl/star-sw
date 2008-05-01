c11bd7b1cf3e9e18b8e50a6bd47a83d7eb645747
Tai Sakuma <sakuma@bnl.gov>
2008-05-01 17:56:09 +0000

    first commitment

---
+++ StRoot/StJetMaker/StParticleCollector.h
@@ -0,0 +1,17 @@
+// -*- mode: c++;-*-
+// $Id: StParticleCollector.h,v 1.1 2008/05/01 17:56:08 tai Exp $
+// Copyright (C) 2008 Tai Sakuma <sakuma@mit.edu>
+#ifndef STPARTICLECOLLECTOR_HH
+#define STPARTICLECOLLECTOR_HH
+
+namespace StSpinJet {
+
+class StParticleCollector {
+
+public:
+
+};
+
+}
+
+#endif // STPARTICLECOLLECTOR_HH
