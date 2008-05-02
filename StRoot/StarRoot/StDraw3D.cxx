9902a430777079be57534281bc5ebeeb3144209f
Valeri Fine <val.fine@gmail.com>
2008-05-02 17:48:12 +0000

    Add the custom comment and labels

--- StRoot/StarRoot/StDraw3D.cxx
+++ StRoot/StarRoot/StDraw3D.cxx
@@ -1,4 +1,4 @@
-// $Id: StDraw3D.cxx,v 1.7 2008/05/01 17:01:32 fine Exp $
+// $Id: StDraw3D.cxx,v 1.8 2008/05/02 17:48:12 fine Exp $
 //*-- Author :    Valery Fine(fine@bnl.gov)   27/04/2008
 #include "StDraw3D.h"
 #include "TCanvas.h"
@@ -21,25 +21,7 @@ Color_t StDraw3D::fgColorDefault = Color_t(-1);
 Style_t StDraw3D::fgStyDefault   = Style_t(-1);
 Size_t  StDraw3D::fgSizDefault   = Size_t (-1);
 Color_t StDraw3D::fgBkColor      = kBlack;
-//___________________________________________________
-StDraw3D::StDraw3D(TVirtualPad *pad): fPad(pad),fViewer(0)
-{
-   static const Style_t UHitSty = 4; static const Size_t UHitSiz = 0.35; static const Color_t UHitCol=kBlue;
-   static const Style_t NHitSty = 1; static const Size_t NHitSiz = 1.00; static const Color_t NHitCol=kGreen;
-   static const Style_t TrakSty = 1; static const Size_t TrakSiz = 1.00; static const Color_t TrakCol=kRed;
-   static const Style_t VertSty = 5; static const Size_t VertSiz = 3.50; static const Color_t VertCol=kYellow;
-   AddStyle(kVtx,         VertCol,VertSty,VertSiz);
-   AddStyle(kPrimaryTrack,TrakCol,TrakSty,TrakSiz);
-   AddStyle(kGlobalTrack, TrakCol,TrakSty,TrakSiz);
-   AddStyle(kUsedHit,     UHitCol,UHitSty,UHitSiz);
-   AddStyle(kUnusedHit,   NHitCol,NHitSty,NHitSiz);
-   if (!fPad) {
-      fPad = new TCanvas("STAR","Event Viewer", 400,400);
-      fPad->SetFillColor(fBkColor);
-      fPad->Modified();
-      fPad->Update();
-   }
-}
+
 //___________________________________________________
 static inline TVirtualViewer3D *InitCoin(TVirtualPad *pad) 
 {
@@ -65,6 +47,110 @@ static inline TVirtualViewer3D *InitCoin(TVirtualPad *pad)
       viewer->EndScene();
     }
     return viewer;
+}
+
+//___________________________________________________
+//
+//   view_3D
+//___________________________________________________
+class view_3D  {
+   private:
+      TObject *fModel;
+      TString  fComment;
+      TString  fObjectInfo;
+      void MakeInfo()
+      {
+         fObjectInfo="";
+         if (fModel) {
+            fObjectInfo = Form("( %s *)%p ",fModel->ClassName(),fModel);
+         }
+         if (!fComment.IsNull()) fObjectInfo += fComment;
+      }
+   public:
+     view_3D(TObject *model = 0, const char *comment="") : fModel(model),fComment(comment)
+     { MakeInfo(); }
+     ~view_3D(){;}
+     TObject *Model() const { return fModel;}
+     void SetModel(TObject *model) {fModel = model;             MakeInfo();}
+     void SetComment(const char *comment) {fComment  = comment; MakeInfo();}
+     void AddComment(const char *comment) {fComment += comment; MakeInfo();}
+     const TString &GetObjectInfo() const {
+        return fObjectInfo;
+     } 
+     const char  *GetModelInfo(Int_t x, Int_t y) const {
+        const TString &customInfo = GetObjectInfo();
+        const char *info = 0;
+        if (customInfo.IsNull()) info = fModel->GetObjectInfo(x,y);
+        else info = customInfo.Data();
+        return info;
+     }
+};
+
+//___________________________________________________
+//
+//   poly_line_3D
+//___________________________________________________
+class poly_line_3D : public TPolyLine3D, public view_3D {
+   public:
+     poly_line_3D(Int_t n, Float_t *p, Option_t *option="") : TPolyLine3D(n,p){;}
+     virtual ~poly_line_3D(){;}
+     virtual char  *GetObjectInfo(Int_t x, Int_t y) const
+     {
+        return (char *)GetModelInfo(x,y);
+     }
+};
+
+//___________________________________________________
+//
+//   poly_marker_3D
+//___________________________________________________
+class poly_marker_3D : public TPolyMarker3D, public view_3D {
+   public:
+     poly_marker_3D(Int_t n, Float_t *p, Option_t *option="") : TPolyMarker3D(n,p){;}
+     virtual ~poly_marker_3D(){;}
+     virtual char  *GetObjectInfo(Int_t x, Int_t y) const
+     {
+        return (char*)GetModelInfo(x,y);
+     }
+};
+//___________________________________________________
+StDraw3D::StDraw3D(TVirtualPad *pad): fPad(pad),fViewer(0),fView(0)
+{
+   static const Style_t UHitSty = 4; static const Size_t UHitSiz = 0.35; static const Color_t UHitCol=kBlue;
+   static const Style_t NHitSty = 1; static const Size_t NHitSiz = 1.00; static const Color_t NHitCol=kGreen;
+   static const Style_t TrakSty = 1; static const Size_t TrakSiz = 1.00; static const Color_t TrakCol=kRed;
+   static const Style_t VertSty = 5; static const Size_t VertSiz = 3.50; static const Color_t VertCol=kYellow;
+   AddStyle(kVtx,         VertCol,VertSty,VertSiz);
+   AddStyle(kPrimaryTrack,TrakCol,TrakSty,TrakSiz);
+   AddStyle(kGlobalTrack, TrakCol,TrakSty,TrakSiz);
+   AddStyle(kUsedHit,     UHitCol,UHitSty,UHitSiz);
+   AddStyle(kUnusedHit,   NHitCol,NHitSty,NHitSiz);
+
+   if (!fPad) {
+      fPad = new TCanvas("STAR","Event Viewer", 400,400);
+      fPad->SetFillColor(fBkColor);
+      fPad->Modified();
+      fPad->Update();
+   }
+}
+  
+//___________________________________________________
+StDraw3D::~StDraw3D()
+{
+    Clear();
+    delete fPad; 
+    fPad = 0;
+}
+
+//___________________________________________________
+void  StDraw3D::Clear(Option_t *opt)
+{
+   // Clear the view
+   if (fPad) {
+      fPad->Clear(opt);
+      fPad->Modified();
+      fPad->Update();
+   }
 }
 
 //___________________________________________________
@@ -122,10 +208,11 @@ int StDraw3D::Draw3DInit(){
 TObject *StDraw3D::Points(int n, const float *xyz, Color_t col,Style_t sty,Size_t siz)
 {
    if (!fgDraw_3d_init) Draw3DInit();
-   TPolyMarker3D *plMk  = new TPolyMarker3D(n,(Float_t*)xyz);
+   poly_marker_3D *plMk  = new poly_marker_3D(n,(Float_t*)xyz);
    if (col != colorDefault) plMk->SetMarkerColor(col);
    if (sty != styDefault)   plMk->SetMarkerStyle(sty);
    if (siz != sizDefault)   plMk->SetMarkerSize(siz);
+   fView = plMk;
    return Draw(plMk);
 }
 //___________________________________________________
@@ -138,12 +225,8 @@ TObject *StDraw3D::Points(int n, const float *xyz, EDraw3DStyle sty)
 TObject *StDraw3D::Point(float x, float y, float z, Color_t col,Style_t sty,Size_t siz)
 {
    if (!fgDraw_3d_init) Draw3DInit();
-   TPolyMarker3D *plMk  = new TPolyMarker3D();
-   plMk->SetNextPoint(x,y,z);
-   if (col != colorDefault) plMk->SetMarkerColor(col);
-   if (sty != styDefault)   plMk->SetMarkerStyle(sty);
-   if (siz != sizDefault)   plMk->SetMarkerSize(siz);
-   return Draw(plMk);
+   float xyz[]={x,y,z};
+   return Points(1,xyz,col,sty,siz);
 }
 
 //___________________________________________________
@@ -164,10 +247,11 @@ TObject *StDraw3D::Draw3D(int n,  const float *xyz)
 TObject *StDraw3D::Line(int n,  const float *xyz, Color_t col,Style_t sty,Size_t siz)
 {
    if (!fgDraw_3d_init) Draw3DInit();
-   TPolyLine3D *plLine  = new TPolyLine3D(n,(Float_t*)xyz);
+   poly_line_3D *plLine  = new poly_line_3D(n,(Float_t*)xyz);
    if (col != colorDefault) plLine->SetLineColor(col);
    if (sty != styDefault)   plLine->SetLineStyle(sty);
    if (siz != sizDefault)   plLine->SetLineWidth(Width_t(siz));
+   fView = plLine;
    return Draw(plLine);
 }
 
@@ -176,6 +260,27 @@ TObject *StDraw3D::Line(int n,  const float *xyz,EDraw3DStyle sty)
 {
    const StDraw3DStyle &style =  Style(sty);
    return Line(n,xyz,  style.Col(),style.Sty(),style.Siz() );
+}
+
+//___________________________________________________
+void StDraw3D::SetModel(TObject *model)
+{
+   // add the "model" reference to the current view
+   if (fView) fView->SetModel(model);
+}
+
+//___________________________________________________
+void StDraw3D::SetComment(const char *cmnt)
+{
+   // set the "model" comment for the current view
+   if (fView) fView->SetComment(cmnt);
+}
+
+//___________________________________________________
+void StDraw3D::AddComment(const char *cmnt)
+{
+   // add the "model" comment for the current view
+   if (fView) fView->AddComment(cmnt);
 }
 
 //___________________________________________________
@@ -196,27 +301,7 @@ void StDraw3D::Draw3DTest(){
    const float *fff = (const float *)&xyz[0];
    fprintf(stderr," %d %p\n", sizeXYZ,fff);
    Draw3D(sizeXYZ,fff);
+   SetComment("The hits from the TPC sector");
    Line(sizeXYZ,fff,kPrimaryTrack);
+   SetComment("The recontstructed track");
 }
-#if 0
-//___________________________________________________
-TObject *Draw3D(const StTrack &track, Color_t col,Style_t sty,Size_t siz)
-{
-   StTrackPoints trPnt(&track);
-   return Draw3DLine(trPnt.GetN(),trPnt.GetP(),col,sty,siz);
-}
-
-//___________________________________________________
-TObject *Draw3D(const StMeasuredPoint &hit, Color_t col,Style_t sty,Size_t siz)
-{
-   // Draw the StMeasuredPoint, StHit, StVertex with the graphical attribute provided
-   const StThreeVectorF& position = hit.position();
-   return Draw3DPoint(position.x(),position.y(),position.z(),col,sty,siz);
-}
-//___________________________________________________
-TObject *Draw3DInnOut(const StTrack &track, Bool_t in, Color_t col,Style_t styt,Size_t siz)
-{
-   StInnOutPoints trInOut(&track,in);
-   return Draw3DPoints(trInOut.GetN(),trInOut.GetP(),col,sty,siz);
-}
-#endif
