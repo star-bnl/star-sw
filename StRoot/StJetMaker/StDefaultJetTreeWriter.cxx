8e6bcb68ee62ad3c22b87319f1de35c233bca720
Tai Sakuma <sakuma@bnl.gov>
2008-05-02 21:27:30 +0000

    first commitment of StDefaultJetTreeWriter

---
+++ StRoot/StJetMaker/StDefaultJetTreeWriter.cxx
@@ -0,0 +1,166 @@
+// $Id: StDefaultJetTreeWriter.cxx,v 1.1 2008/05/02 21:27:28 tai Exp $
+// Copyright (C) 2008 Tai Sakuma <sakuma@mit.edu>
+# include "StDefaultJetTreeWriter.h"
+
+#include "StJets.h"
+#include "StJet.h"
+#include "StMuTrackFourVec.h"
+
+#include <StJetFinder/StProtoJet.h>
+
+#include <StMuDSTMaker/COMMON/StMuDst.h>
+#include <StMuDSTMaker/COMMON/StMuEvent.h>
+#include <StMuDSTMaker/COMMON/StMuDstMaker.h>
+
+#include <StFourPMakers/StMuEmcPosition.h>
+#include <StFourPMakers/StFourPMaker.h>
+#include "StFourPMakers/StBET4pMaker.h"
+
+#include <TTree.h>
+
+using namespace std;
+
+namespace StSpinJet {
+
+StDefaultJetTreeWriter::StDefaultJetTreeWriter(StMuDstMaker& uDstMaker, std::string outFileName)
+  : _uDstMaker(uDstMaker)
+  , _OutFileName(outFileName)
+  , _jetTree(0)
+  , _outFile(0)
+{
+
+}
+
+StDefaultJetTreeWriter::~StDefaultJetTreeWriter()
+{
+
+}
+
+void StDefaultJetTreeWriter::addJetFinder(StFourPMaker* fourPMaker, list<StProtoJet>* protoJetList, const char* name)
+{
+  AnalyzerCtl anaCtl;
+  anaCtl._branchName = name;
+  anaCtl._fourPMaker = fourPMaker;
+  anaCtl._protoJetList = protoJetList;
+  anaCtl._jets = new StJets();
+
+  _analyzerCtlList.push_back(anaCtl);
+}
+
+void StDefaultJetTreeWriter::Init()
+{
+  _outFile = new TFile(_OutFileName.c_str(), "recreate");
+  _jetTree  = new TTree("jet", "jetTree");
+
+  for(vector<AnalyzerCtl>::iterator it = _analyzerCtlList.begin(); it != _analyzerCtlList.end(); ++it) {
+    _jetTree->Branch((*it)._branchName.c_str(), "StJets", &((*it)._jets));
+  }
+}
+
+void StDefaultJetTreeWriter::Finish()
+{
+  _outFile->Write();
+  _outFile->Close();
+  delete _outFile;
+  _outFile = 0;
+}
+
+void StDefaultJetTreeWriter::fillJetTree()
+{
+  for(vector<AnalyzerCtl>::iterator it = _analyzerCtlList.begin(); it != _analyzerCtlList.end(); ++it) {
+    StFourPMaker* fourPMaker = (*it)._fourPMaker;
+    std::list<StProtoJet>* protoJetList = (*it)._protoJetList;
+    fillJetTreeForOneJetFindingAlgorithm(*(*it)._jets, protoJetList, fourPMaker);
+  }
+  _jetTree->Fill();
+}
+
+void StDefaultJetTreeWriter::fillJetTreeForOneJetFindingAlgorithm(StJets& jets, std::list<StProtoJet>* protoJetList, StFourPMaker* fourPMaker)
+{
+  jets.Clear();
+  jets.setBemcCorrupt(fourPMaker->bemcCorrupt() );
+
+  StMuEvent* event = _uDstMaker.muDst()->event();
+  jets.seteventId(event->eventId());
+  jets.seteventNumber(event->eventNumber());
+  jets.setrunId(event->runId());
+  jets.setrunNumber(event->runNumber());
+
+  StBET4pMaker* bet4p = dynamic_cast<StBET4pMaker*>(fourPMaker);
+  if (bet4p) {
+    jets.setDylanPoints( bet4p->nDylanPoints() );
+    jets.setSumEmcE( bet4p->sumEmcEt() );
+  }
+	
+  for(list<StProtoJet>::iterator it = protoJetList->begin(); it != protoJetList->end(); ++it) {
+    fillJet(jets, *it);
+  }
+}
+
+void StDefaultJetTreeWriter::fillJet(StJets &jets, StProtoJet& pj)
+{
+  StJet aJet(pj.e(), pj.px(), pj.py(), pj.pz(), 0, 0);
+  aJet.zVertex = _uDstMaker.muDst()->event()->primaryVertexPosition().z();
+
+  StProtoJet::FourVecList &trackList = pj.list();
+  for(StProtoJet::FourVecList::iterator it2 = trackList.begin(); it2 != trackList.end(); ++it2)  {
+    const StMuTrackFourVec *track = dynamic_cast<const StMuTrackFourVec*>(*it2);
+    if (!track) {
+      cout <<"StJets::addProtoJet(). ERROR:\tcast to StMuTrackFourVecFailed.  no action"<<endl;
+      return;
+    }
+    int muTrackIndex = track->getIndex();
+    if (muTrackIndex <0) {
+      cout <<"Error, muTrackIndex<0. abort()"<<endl;
+      abort();
+    }
+      
+    TrackToJetIndex t2j( jets.nJets(), muTrackIndex, track->detectorId() );
+    t2j.SetPxPyPzE(track->px(), track->py(), track->pz(), track->e() );
+      
+    //and cache some properties if it really came from a StMuTrack:
+    StMuTrack* muTrack = track->particle();
+    if (muTrack) {  //this will fail for calorimeter towers
+
+      double bField(0.5); // to put it in Tesla
+      double rad(238.6);// geom->Radius()+5.;
+
+      StThreeVectorD momentumAt, positionAt;
+      StMuEmcPosition mMuPosition;
+      mMuPosition.trackOnEmc(&positionAt, &momentumAt, muTrack, bField, rad );
+
+      t2j.setCharge( muTrack->charge() );
+      t2j.setNhits( muTrack->nHits() );
+      t2j.setNhitsPoss( muTrack->nHitsPoss() );
+      t2j.setNhitsDedx( muTrack->nHitsDedx() );
+      t2j.setNhitsFit( muTrack->nHitsFit() );
+      t2j.setNsigmaPion( muTrack->nSigmaPion() );
+      t2j.setTdca ( muTrack->dcaGlobal().mag() );
+      t2j.setTdcaz ( muTrack->dcaZ() );
+      t2j.setTdcaxy ( muTrack->dcaD() );
+      t2j.setetaext ( positionAt.pseudoRapidity() );
+      t2j.setphiext ( positionAt.phi() );
+    }
+     
+    jets.addTrackToIndex(t2j);
+
+    StDetectorId mDetId = track->detectorId();
+    if (mDetId==kTpcId) {
+      aJet.nTracks++;
+      aJet.tpcEtSum += track->eT();
+    }
+    else if (mDetId==kBarrelEmcTowerId) {
+      aJet.nBtowers++;
+      aJet.btowEtSum += track->eT();
+    }
+    else if (mDetId==kEndcapEmcTowerId) {
+      aJet.nEtowers++;
+      aJet.etowEtSum += track->eT();
+    }
+  }
+
+  jets.addJet(aJet);
+
+}
+
+}
