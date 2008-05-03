c58fe4ef3b1223c21e6cfa67e23491c7b7486e21
Tai Sakuma <sakuma@bnl.gov>
2008-05-03 03:13:34 +0000

    *** empty log message ***

---
+++ StRoot/StJetMaker/StTestData.h
@@ -0,0 +1,21 @@
+// $Id: StTestData.h,v 1.1 2008/05/03 03:13:33 tai Exp $
+// Copyright (C) 2008 Tai Sakuma <sakuma@mit.edu>
+#ifndef STTESTDATA_H
+#define STTESTDATA_H
+
+struct TestParticle_t {
+  Int_t     eventID;
+  Char_t    name[128];
+  Double_t  px;
+  Double_t  py;
+  Double_t  pz;
+  Double_t  phi;
+  Double_t  eta;
+  Double_t  eT;
+  Double_t  e;
+  Double_t  mass;
+  Double_t  charge;
+};
+
+
+#endif // STTESTDATA_H
