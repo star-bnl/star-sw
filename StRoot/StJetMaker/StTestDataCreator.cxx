56363319fbc0dfce5fcf4333a952f251901db57c
Tai Sakuma <sakuma@bnl.gov>
2008-05-03 01:23:04 +0000

    *** empty log message ***

--- StRoot/StJetMaker/StTestDataCreator.cxx
+++ StRoot/StJetMaker/StTestDataCreator.cxx
@@ -1,8 +1,10 @@
-// $Id: StTestDataCreator.cxx,v 1.2 2008/05/03 01:06:32 tai Exp $
+// $Id: StTestDataCreator.cxx,v 1.3 2008/05/03 01:23:03 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@mit.edu>
 #include "StTestDataCreator.h"
 
 #include <StJetFinder/AbstractFourVec.h>
+
+#include <iostream>
 
 using namespace std;
 
@@ -33,10 +35,21 @@ void StTestDataCreator::Finish()
 
 void StTestDataCreator::addJetFinder(StFourPMaker* fourPMaker, const std::vector<const AbstractFourVec*>* particleList, std::list<StProtoJet>* protoJetList, const char* name)
 {
+  DataCtl dataCtl;
+  dataCtl._name = name;
+  dataCtl._fourPMaker = fourPMaker;
+  dataCtl._particleList = particleList;
+  dataCtl._protoJetList = protoJetList;
 
+  _dataCtlList.push_back(dataCtl);
 }
 
 void StTestDataCreator::fillJetTree()
 {
-
+  for(vector<DataCtl>::iterator dataCtl = _dataCtlList.begin(); dataCtl != _dataCtlList.end(); ++dataCtl) {
+    const vector<const AbstractFourVec*>& particleList = *((*dataCtl)._particleList) ;
+      for(vector<const AbstractFourVec*>::const_iterator particle = particleList.begin(); particle != particleList.end(); ++particle) {
+	cout << (*dataCtl)._name << " " << (**particle) << endl;
+      }
+  }
 }
