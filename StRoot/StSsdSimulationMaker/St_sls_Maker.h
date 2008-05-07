b4d0de6b89e7cac98743471dfba428618b762509
bouchet <>
2008-05-07 22:59:11 +0000

    EmbeddingMaker:initial version ; modified reading of GEANT hits

--- StRoot/StSsdSimulationMaker/St_sls_Maker.h
+++ StRoot/StSsdSimulationMaker/St_sls_Maker.h
@@ -18,35 +18,75 @@
 
 #ifndef StMaker_H
 #include "StMaker.h"
+#include "StThreeVectorF.hh"
+#include "StThreeVectorD.hh"
+#include "StSsdUtil/StSsdBarrel.hh"
+#include "StSsdUtil/StSsdWafer.hh"
 #endif
 class St_g2t_ssd_hit;
 class St_g2t_svt_hit;
 class St_sls_strip;
 class slsCtrl_st;
 class St_slsCtrl;
+class St_g2t_track;
+class St_ssdDimensions;
+class St_ssdWafersPosition;
+class ssdConfiguration_st;
+class StMcEvent;
+class StDAQReader;
 
 class St_sls_Maker : public StMaker {
- private:
- 
-  Int_t readPointFromTable(St_g2t_ssd_hit *g2t_ssd_hit);
-  Int_t readPointFromTable(St_g2t_svt_hit *g2t_svt_hit) {
-    return readPointFromTable((St_g2t_ssd_hit *) g2t_svt_hit);}
-  Int_t   removeInactiveHitInTable(St_g2t_ssd_hit *g2t_ssd_hit);
-  Int_t   removeInactiveHitInTable(St_g2t_svt_hit *g2t_svt_hit) {
-    return removeInactiveHitInTable((St_g2t_ssd_hit *) g2t_svt_hit);}
-  void  chargeSharingOverStrip(slsCtrl_st  *ctrl);
-  Int_t   writeStripToTable(St_sls_strip *sls_strip);
-  St_slsCtrl           *m_ctrl;
+  private :
+  St_slsCtrl           *m_ctrl;//!
+  St_ssdDimensions     *m_dimensions;//!
+  St_ssdWafersPosition *m_positions; //!
+  ssdConfiguration_st  *m_config;    //!
+  double mBField; // z component of BField; 
+
  public: 
   St_sls_Maker(const char *name="sls_strip") : StMaker(name), m_ctrl(0) {}
   virtual       ~St_sls_Maker() {}
+  virtual Int_t  Init();
   virtual Int_t  InitRun(Int_t runNumber);
   virtual Int_t  Make();
   virtual Int_t  Finish();
   virtual void   PrintInfo();
+  virtual void   Clear(const char *opt);
+  Int_t          readPointFromTable(St_g2t_ssd_hit *g2t_ssd_hit);
+  Int_t          readPointFromTable(St_g2t_svt_hit *g2t_svt_hit) {return readPointFromTable((St_g2t_ssd_hit *) g2t_svt_hit);}
+  Int_t          removeInactiveHitInTable(St_g2t_ssd_hit *g2t_ssd_hit);
+  Int_t          removeInactiveHitInTable(St_g2t_svt_hit *g2t_svt_hit) {return removeInactiveHitInTable((St_g2t_ssd_hit *) g2t_svt_hit);}
+  void           chargeSharingOverStrip(slsCtrl_st  *ctrl);
+  Int_t          writeStripToTable(St_sls_strip *sls_strip);
+  Int_t          readPointFromTableWithEmbedding(St_g2t_ssd_hit *g2t_ssd_hit,St_g2t_track *g2t_track,St_ssdWafersPosition *m_positions);
+  void           setSsdParameters(ssdDimensions_st *geom_par);  
+  void           printSsdParameters();  
+  Int_t          idWaferToWaferNumb(Int_t idWafer); //  idwafer = layer*1000+waf*100+ladder => waferNumb = mNWaferPerLadder*(ladder-1) + waf - 1
+  Int_t          idWaferToLadderNumb(Int_t idWafer);//  idwafer => ladder-1
+  Int_t          waferNumbToIdWafer(Int_t waferNumb);// waferNumb = mNWaferPerLadder*(ladder-1) + waf - 1 => idwafer
+  Int_t          idWaferToWafer(Int_t idWafer) {return (idWafer-7000)/100-1;}
+  Int_t          ideal2RealTranslation(StThreeVector<double> *pos, StThreeVector<double> *mtm, double charge, int wafId, int index, St_ssdWafersPosition  *m_positions,Int_t *IL, Int_t *IW);
+  int            IsOnWafer(const StThreeVector<double>& LocalPosition);
+  void           debugUnPeu(); 
+  
+  private :
+  Int_t    mSsdLayer;
+  Int_t    mNLadder;
+  Int_t    mNWaferPerLadder;
+  Int_t    mNStripPerSide;
+  Int_t    mActiveLadders[20];
+  Float_t  mDetectorLargeEdge;
+  Float_t  mDetectorSmallEdge;
+  Float_t  mStripPitch;
+  Float_t  mTheta;
+  Int_t    *counter;
+  TH1F     *hRejected;
+  ssdDimensions_st *mDimensions;
+   protected:
+  StMcEvent            *mcEvent;
   
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: St_sls_Maker.h,v 1.10 2007/03/21 17:19:56 fisyak Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: St_sls_Maker.h,v 1.11 2008/05/07 22:59:11 bouchet Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
 
 
@@ -55,9 +95,12 @@ class St_sls_Maker : public StMaker {
 #endif
 
  /**************************************************************************
- * $Id: St_sls_Maker.h,v 1.10 2007/03/21 17:19:56 fisyak Exp $
+ * $Id: St_sls_Maker.h,v 1.11 2008/05/07 22:59:11 bouchet Exp $
  *
  * $Log: St_sls_Maker.h,v $
+ * Revision 1.11  2008/05/07 22:59:11  bouchet
+ * EmbeddingMaker:initial version ; modified reading of GEANT hits
+ *
  * Revision 1.10  2007/03/21 17:19:56  fisyak
  * use new StSsdBarrel
  *
