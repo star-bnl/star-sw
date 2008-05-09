65ccfe6833277d57a237c66fdec28075757d21e2
fine <>
2008-05-09 00:50:23 +0000

    Add absolute path to the list

--- StRoot/StarRoot/StDraw3D.cxx
+++ StRoot/StarRoot/StDraw3D.cxx
@@ -1,4 +1,4 @@
-// $Id: StDraw3D.cxx,v 1.20 2008/05/08 23:48:46 fine Exp $
+// $Id: StDraw3D.cxx,v 1.21 2008/05/09 00:50:23 fine Exp $
 //*-- Author :    Valery Fine(fine@bnl.gov)   27/04/2008
 #include "StDraw3D.h"
 #include "TCanvas.h"
@@ -60,7 +60,7 @@ static inline TVirtualViewer3D *InitCoin(TVirtualPad *pad)
       const char *backShape = "StarTPC.iv";
 //      const char *backShape = "$STAR/StRoot/macros/graphics/StarTPC.iv";
       printf(" Setting the background shape to be 	%s\n", backShape);
-      gEnv->SetValue("Gui.InventorShapeDir","$STAR/StRoot/macros/graphics");
+      gEnv->SetValue("Gui.InventorShapeDir",":.:$STAR/StRoot/macros/graphics");
       gEnv->SetValue("Gui.InventorBackgroundShape",backShape);
       if  (viewer = TVirtualViewer3D::Viewer3D(pad,"oiv")) {
          // Create Open GL viewer
