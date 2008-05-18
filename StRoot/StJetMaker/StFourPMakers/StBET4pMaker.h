d70c6b71a6f88ce0022d7fa5cbeb9d6a00edc0d8
tai <>
2008-05-18 23:44:20 +0000

    cleaned up the code

--- StRoot/StJetMaker/StFourPMakers/StBET4pMaker.h
+++ StRoot/StJetMaker/StFourPMakers/StBET4pMaker.h
@@ -1,26 +1,12 @@
-/*!
-  \class StBET4pMaker
-  \author M.L. Miller (MIT Software)
-
-  StBET4pMaker is used to fill the list of 4-momenta that is then passed to a
-  StJetFinder instance.  StBET4pMaker simply instantiates an object of type
-  StBETMuTrackFourVec for every final state particle in the event.
-*/
-
+// -*- mode: c++;-*-
+// $Id: StBET4pMaker.h,v 1.20 2008/05/18 23:44:20 tai Exp $
 #ifndef STBET4PMAKER_HH
 #define STBET4PMAKER_HH
 
 #include "StFourPMaker.h"
 
-class StMuTrack;
-class StEmcCollection;
-class StMuTrackFourVec;
 class StMuDstMaker;
-class StEmcRawHit;
-class EEmcGeomSimple;
 class StBemcTables;
-class StEEmcDbMaker;
-
 class StBET4pMakerImp;
 
 class StBET4pMaker : public StFourPMaker {
@@ -52,45 +38,13 @@ public:
     
 private:
 
-  void collectChargedTracksFromTPC();
-  bool isUsableTrack(const StMuTrack& track) const;
-  void countTracksOnBemcTower(const StMuTrack& track);
-
-  void collectEnergyFromBEMC();
-  double correctBemcTowerEnergyForTracks(double energy, int bemcTowerId);
-
-  void collectEnergyFromEEMC();
-
-  StEmcCollection *find_StEmCCollection();
-  bool isCorrupted();
-
-  void fillBemcTowerHits();
-  double sumEnergyOverBemcTowers(double minE);
-  int numberOfBemcTowersWithEnergyAbove(double minE);
-
-  bool shouldKeepThisBemcHit(StEmcRawHit* theRawHit, int bemcTowerID);
-
   bool mCorrupt;
-  bool mUseEndcap;
 
-  //these arrays are used to correlate tracks w/ towers
-  static const int mNOfBemcTowers = 4800;
-
-  StEmcRawHit* mBTowHits[mNOfBemcTowers + 1]; // indexed from [1,4800]
-  int mNtracksOnTower[mNOfBemcTowers + 1]; // indexed form [1,4800] (number of tracks incident on this tower)
-
-  StMuDstMaker* mMuDstMaker;
   StBemcTables* mTables;
 
-  bool mUse2003Cuts;
-  bool mUse2005Cuts;
-  bool mUse2006Cuts;
   int mDylanPoints;
   double mSumEmcEt;
         
-  EEmcGeomSimple* mEeGeom;
-  StEEmcDbMaker* mEeDb;
-
   StBET4pMakerImp* _imp;
   
   ClassDef(StBET4pMaker,1)
