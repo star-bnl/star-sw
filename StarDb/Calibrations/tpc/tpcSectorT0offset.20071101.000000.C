4133a504b4babf1d764e886d82f74d0461abf8ff
Yuri Fisyak <fisyak@bnl.gov>
2008-05-27 16:29:07 +0000

    Shift from laser run 9027013

--- StarDb/Calibrations/tpc/tpcSectorT0offset.20071101.000000.C
+++ StarDb/Calibrations/tpc/tpcSectorT0offset.20071101.000000.C
@@ -9,7 +9,13 @@ TDataSet *CreateTable() {
   St_tpcSectorT0offset *tableSet = new St_tpcSectorT0offset("tpcSectorT0offset",1);
   //
   memset(&row,0,tableSet->GetRowSize());
-  row.t0[15] =  -21.4 - 0.176; // timebins 21.449   21.947 // after 2to10 : s15/16 r1 22.034 line fit : 22.037 gaus
+  //  row.t0[15] =  -21.4 - 0.176 + 1.74445; // Tpx4 Membrane Shift = 5.22549 +/- 0.000659445
+  //  row.t0[15] =  -21.4 - 0.176 + 1.74445 - 5.23; //Tpx5 Membrane Shift = -5.36681 +/- 42.0076 
+  //  row.t0[15] =  -21.4 - 0.176 + 1.74445 + 5.23; // 04/30/08 Tpx61 Tpx61 Averaged Shift = 7.21025 +/- 0.000653672
+  //  row.t0[15] =  -21.4 - 0.176 + 1.74445 + 5.23 -0.5*7.21025; // 05/01/08 Tpx7 Averaged Shift = 8.70517 +/- 0.00066056
+  //  row.t0[15] =  -21.4 - 0.176 + 1.74445 + 5.23 -0.5*7.21025 + 0.5*8.70517 ; // 05/01/08 Tpx8 Averaged Shift = 5.7161 +/- 0.000657116
+  //  row.t0[15] =  -21.4 - 0.176 + 1.74445 + 5.23 -0.5*7.21025 + 0.5*8.70517 + 5.7161; // 05/01/08 Tpx9  
+  row.t0[15] =  -21.4 - 0.176 + 1.74445 + 5.23 -0.5*7.21025 + 0.5*8.70517 + 0.5*5.7161; // 05/03/08 Tpx10
   tableSet->AddAt(&row);
   // ----------------- end of code ---------------
   return (TDataSet *)tableSet;
