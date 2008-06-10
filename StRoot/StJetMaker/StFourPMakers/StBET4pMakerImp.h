427ed9f6115cb02a257a444027cab038e6e9dc91
tai <>
2008-06-10 09:17:58 +0000

    cleaned up the code

--- StRoot/StJetMaker/StFourPMakers/StBET4pMakerImp.h
+++ StRoot/StJetMaker/StFourPMakers/StBET4pMakerImp.h
@@ -1,5 +1,5 @@
 // -*- mode: c++;-*-
-// $Id: StBET4pMakerImp.h,v 1.37 2008/06/10 08:31:08 tai Exp $
+// $Id: StBET4pMakerImp.h,v 1.38 2008/06/10 09:17:58 tai Exp $
 #ifndef STBET4PMAKERIMP_HH
 #define STBET4PMAKERIMP_HH
 
@@ -22,6 +22,7 @@ class StMuDstMaker;
 
 #include "CollectEnergyDepositsFromBEMC.h"
 #include "CollectEnergyDepositsFromEEMC.h"
+#include "CorrectTowerEnergyForTracks.h"
 
 typedef std::vector<AbstractFourVec*> FourList;
 
@@ -31,8 +32,9 @@ public:
     
   StBET4pMakerImp(StMuDstMaker* uDstMaker,
 		  StSpinJet::CollectChargedTracksFromTPC* collectChargedTracksFromTPC,
-		  StSpinJet::CollectEnergyDepositsFromBEMC *collectEnergyDepositsFromBEMC,
-		  StSpinJet::CollectEnergyDepositsFromEEMC *collectEnergyDepositsFromEEMC
+		  StSpinJet::CollectEnergyDepositsFromBEMC* collectEnergyDepositsFromBEMC,
+		  StSpinJet::CollectEnergyDepositsFromEEMC* collectEnergyDepositsFromEEMC,
+		  StSpinJet::CorrectTowerEnergyForTracks* correctTowerEnergyForTracks
 		  );
     
   virtual ~StBET4pMakerImp() {};
@@ -53,11 +55,6 @@ private:
 
   FourList constructFourMomentumListFrom(const TrackList& trackList);
 
-  void countTracksOnBemcTower(const StMuTrack& track);
-
-  StSpinJet::TowerEnergyDepositList correctBemcTowerEnergyForTracks(const StSpinJet::TowerEnergyDepositList &energyDepositList, const TrackList& trackList);
-  double correctBemcTowerEnergyForTracks_(double energy, int bemcTowerId);
-
   FourList constructFourMomentumListFrom(const StSpinJet::TowerEnergyDepositList& energyDepositList);
 
 
@@ -69,16 +66,13 @@ private:
 
   bool mUseEndcap;
 
-  //these arrays are used to correlate tracks w/ towers
-  static const int mNOfBemcTowers = 4800;
-
-  int mNtracksOnTower[mNOfBemcTowers + 1]; // indexed form [1,4800] (number of tracks incident on this tower)
-
   StMuDstMaker* mMuDstMaker;
 
   StSpinJet::CollectChargedTracksFromTPC *_collectChargedTracksFromTPC;
   StSpinJet::CollectEnergyDepositsFromBEMC *_collectEnergyDepositsFromBEMC;
   StSpinJet::CollectEnergyDepositsFromEEMC *_collectEnergyDepositsFromEEMC;
+
+  StSpinJet::CorrectTowerEnergyForTracks* _correctTowerEnergyForTracks;
 
 };
 
