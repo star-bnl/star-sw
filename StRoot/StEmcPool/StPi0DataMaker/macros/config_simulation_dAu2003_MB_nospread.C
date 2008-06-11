87f49d6616b0bde5584d1297c4c4c70b471fbc4c
cvs2git <cvs2git>
2008-06-11 02:44:52 +0000

    This commit was manufactured by cvs2git to create tag 'JL_080612_testProduction'.

--- StRoot/StEmcPool/StPi0DataMaker/macros/config_simulation_dAu2003_MB_nospread.C
+++
@@ -1,7 +0,0 @@
-// Configuration to run pi0 reconstruction chain on the dAu2003 MB simulated data w/o calibration spread.
-{
-    gROOT->Macro("config_simulation_dAu2003_nospread.C"); // load dAu2003 simulation configuration w/o calibration spread
-
-    dataAnalysisSettings.timestampsFilename = "dAu2003Times_MB.txt";
-}
-
--- StRoot/StStarLogger/mysql/downlaodDbSchema.csh
+++
@@ -1,4 +0,0 @@
-svn co https://fusion.txcorp.com/pubsvn/ucm/trunk/docs/dev
-ln -s dev/CreateUCMDB.sql
-ls -lL CreateUCMDB.sql
-
