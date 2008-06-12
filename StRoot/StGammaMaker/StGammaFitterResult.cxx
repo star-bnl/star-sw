f6c5c0bf885bdfeddf9cb03ad68b03ff90a62ae9
pibero <>
2008-06-12 22:58:44 +0000

    Added constructor.

--- StRoot/StGammaMaker/StGammaFitterResult.cxx
+++ StRoot/StGammaMaker/StGammaFitterResult.cxx
@@ -7,3 +7,22 @@
 #include "StGammaFitterResult.h"
 
 ClassImp(StGammaFitterResult);
+
+StGammaFitterResult::StGammaFitterResult()
+{
+  Clear();
+}
+
+void StGammaFitterResult::Clear(Option_t* option)
+{
+  yield = 0;
+  yieldError = 0;
+  centroid = 0;
+  centroidError = 0;
+  residual = 0;
+  mean = 0;
+  rms = 0;
+  chiSquare = 0;
+  ndf = 0;
+  prob = 0;
+}
