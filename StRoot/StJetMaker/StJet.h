f1983ecd0934d3357ae6ac7eee4ae82bda760ba1
cvs2git <cvs2git>
2008-07-06 01:13:44 +0000

    This commit was manufactured by cvs2git to create tag 'P080822_jet_2005'.

--- StRoot/StJetMaker/StJet.h
+++
@@ -1,1 +0,0 @@
-#include <StSpinPool/StJets/StJet.h>
--- StRoot/StTpcDb/St_tpcGainC.cxx
+++
@@ -1,8 +0,0 @@
-#include "St_tpcGainC.h"
-ClassImp(St_tpcGainC);
-Float_t St_tpcGainC::Gain(Int_t sector, Int_t row, Int_t pad) {
-  tpcGain_st *t = ((St_tpcGain *) Table())->GetTable() + sector-1;
-  Float_t gain =  t ? t->Gain[row-1][pad-1] : 0;
-  return gain;
-}
-St_tpcGainC::~St_tpcGainC() {assert(0);}
