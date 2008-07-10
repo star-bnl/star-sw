b0e65e532b188b5b18b5dda28571c1aeb2acb267
tai <>
2008-07-10 23:02:16 +0000

    updated load path

--- StRoot/StJetMaker/StJetSimuUtil/macros/RunJetSimuSkimReader.C
+++ StRoot/StJetMaker/StJetSimuUtil/macros/RunJetSimuSkimReader.C
@@ -1,70 +1,82 @@
 class  StChain;
 StChain *chain;
 int total=0;
+TObjArray  *HList=0;
+TFile *fd=0;
 
 void RunJetSimuSkimReader(int nevents=100,
-		      const char* jetInFile = "Jets_pt35_11.root",
-		      const char* skimInFile = "Skim_pt35_11.root")
+			  const char* jetInFile = "/star/data04/sim/staszak/2006jets/2006jets_30grids/jets_pt4_01.root",
+			  const char* skimInFile = "/star/data04/sim/staszak/2006jets/2006jets_30grids/skim_pt4_01.root",
+			  const char* histfile = "pt4.hist.root")
 {
-
-
-    gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
-    loadSharedLibraries();
-    gSystem->Load("St_db_Maker");
-    gSystem->Load("StDetectorDbMaker");
-    gSystem->Load("StSpinDbMaker");
-    gSystem->Load("StEmcTriggerMaker");
-    gSystem->Load("StTriggerUtilities");
-    gSystem->Load("StMCAsymMaker");
-    gSystem->Load("StJetFinder");
-    gSystem->Load("StJetMaker");
-
-    double pi = atan(1.0)*4.0;
-    cout << " loading done " << endl;
+  
+ 
+  setMacroPath();
+  gROOT->Macro("LoadJetLibraries.C");
+  double pi = atan(1.0)*4.0;
+  cout << " loading done " << endl;
    
-    chain= new StChain("StChain"); 
-    chain->SetDebug(1);
-    gMessMgr->SwitchOn("D");
-    gMessMgr->SwitchOff("I");
-
-    //Instantiate the JetReader
-    StJetSimuReader* jetReader = new StJetSimuReader("JetReader",0);
-   
-    chain->Init();
-
-    //these 3 lines are critical and must be called after chain->Init()
-    //Call in exactly this order
-    jetReader->InitFile(jetInFile);
-    jetReader->InitJetSkimFile(skimInFile);
-    int ready = jetReader->preparedForDualRead();
-
-
-        
-    chain->PrintInfo();
-    
-    for (Int_t iev=0;iev<nevents; iev++) {
-	cout << "****************************************** " << endl;
-	cout << "Working on eventNumber " << iev << endl;
-	cout << "*************************1***************** " << endl;
-	chain->Clear();
-	int iret = chain->Make(iev); 
-	total++;
-	if (iret) {
-	    cout << "Bad return code!" << endl;
-	    break;
-	}
-	//Here's where you can do your analysis, for an example look in this method
-	jetReader->exampleSimuAna();
-    } 
-    chain->Finish(); 
+  chain= new StChain("StChain"); 
+  chain->SetDebug(1);
+  gMessMgr->SwitchOn("D");
+  gMessMgr->SwitchOff("I");
+  
+  //Instantiate the JetReader
+  StJetSimuReader* jetReader = new StJetSimuReader("JetReader",0);
+  
+  chain->Init();
+  
+  //these 3 lines are critical and must be called after chain->Init()
+  //Call in exactly this order
+  jetReader->InitFile(jetInFile);
+  jetReader->InitJetSkimFile(skimInFile);
+  int ready = jetReader->preparedForDualRead();
+  HList=new TObjArray ;
+  jetReader->SetHList(HList);
+  jetReader->PythiaAnaHisto();
+  
+  chain->PrintInfo();
+  
+  for (Int_t iev=0;iev<nevents; iev++) {
     cout << "****************************************** " << endl;
-    cout << "total number of events  " << total << endl;
-    cout << "****************************************** " << endl;      
+    cout << "Working on eventNumber " << iev << endl;
+    cout << "*************************1***************** " << endl;
+    chain->Clear();
+    int iret = chain->Make(iev); 
+    total++;
+    if (iret)  {
+      cout << "Bad return code!" << endl;
+      break;
+    }
+    //Here's where you can do your analysis, for an example look in this method
+    jetReader->PythiaAna();
+  } 
+  chain->Finish(); 
+  cout << "****************************************** " << endl;
+  cout << "total number of events  " << total << endl;
+  cout << "****************************************** " << endl;      
+  
+
+  fd=new TFile(histfile,"recreate");
+  cout<<"Opening "<<histfile<<endl;
+  assert(fd->IsOpen());
+  cout<<"Open Baby!"<<endl;
+  HList->Write();
+  printf("%d histos are written  to '%s' ...\n",HList->GetEntries(),fd->GetName());
+  fd->Close();
+  
 }
 
-
-
-
-
+void setMacroPath() 
+ {
+   TString path(gROOT->GetMacroPath());
+   path = TString(gSystem->Getenv("STAR")) + "/StRoot/StJetMaker/macros:" + path; 
+   path = "./StRoot/StJetMaker/macros:" + path;
+   path = "./StJetMaker/macros:" + path;
+   path = "./macros:" + path;
+   path = "../macros:" + path;
+   path = ".:" + path;
+   gROOT->SetMacroPath(path);
+ }
 
 
