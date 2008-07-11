e662ee5828795ac07173c570479234223baf4b05
Tai Sakuma <sakuma@bnl.gov>
2008-07-11 23:24:47 +0000

    added runNumber and eventId to TowerEnergy

--- StRoot/StJetMaker/StFourPMakers/TowerEnergyList.h
+++ StRoot/StJetMaker/StFourPMakers/TowerEnergyList.h
@@ -1,5 +1,5 @@
 // -*- mode: c++;-*-
-// $Id: TowerEnergyList.h,v 1.2 2008/07/10 20:49:00 tai Exp $
+// $Id: TowerEnergyList.h,v 1.3 2008/07/11 23:24:47 tai Exp $
 #ifndef TOWERENERGYLIST_H
 #define TOWERENERGYLIST_H
 
@@ -10,6 +10,8 @@
 namespace StSpinJet {
 
 struct TowerEnergy {
+  int            runNumber;
+  int            eventId;
   int            detectorId; // 9: BEMC, 13: EEMC
   int            towerId;
   double         towerX;
