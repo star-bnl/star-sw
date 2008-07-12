43f70a86b7281c3a18e793279ccef58f35a2c27d
fine <>
2008-07-12 20:19:02 +0000

    Better way to fix the issue

--- StRoot/StDaqLib/SSD/SSD_Reader.cxx
+++ StRoot/StDaqLib/SSD/SSD_Reader.cxx
@@ -68,8 +68,9 @@ int SSD_Reader::ssdData(int  ladder,
       pos=0;
       for(time=0;time<192;time++) { // Though "time" is not used explicitly, it's involved in incrementeing "pos".
         for(pad=0;pad<64;pad++) {
+          if (!ssd.counts[daqLadder][pad]) continue;
           for(mtime=0;mtime<ssd.counts[daqLadder][pad];mtime++) {
-            assert(time==ssd.strip[daqLadder][pad][mtime]);
+            if (time==ssd.strip[daqLadder][pad][mtime]);
             {
               det=pos/768; strip=pos%768;
               assert(pos>=0&&pos<12288);
