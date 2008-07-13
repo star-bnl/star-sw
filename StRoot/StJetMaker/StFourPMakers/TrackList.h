9a42d9e7c6402a3eeed3e3bb2002d173119a4472
tai <>
2008-07-13 06:35:59 +0000

    *** empty log message ***

--- StRoot/StJetMaker/StFourPMakers/TrackList.h
+++ StRoot/StJetMaker/StFourPMakers/TrackList.h
@@ -1,8 +1,9 @@
 // -*- mode: c++;-*-
-// $Id: TrackList.h,v 1.3 2008/07/13 06:04:41 tai Exp $
+// $Id: TrackList.h,v 1.4 2008/07/13 06:35:59 tai Exp $
 #ifndef TRACKLIST_H
 #define TRACKLIST_H
 
+#include <ostream>
 #include <vector>
 
 namespace StSpinJet {
@@ -33,6 +34,57 @@ struct Track {
 };
 
 typedef std::vector<Track> TrackList;
+
+inline bool operator==(const Track& v1, const Track& v2)
+{
+  if(v1.runNumber  != v2.runNumber)  return false;
+  if(v1.eventId    != v2.eventId)    return false;   
+  if(v1.pt         != v2.pt)         return false;  
+  if(v1.eta        != v2.eta)        return false;   
+  if(v1.phi        != v2.phi)        return false;   
+  if(v1.flag       != v2.flag)       return false;   
+  if(v1.nHits      != v2.nHits)      return false;   
+  if(v1.charge     != v2.charge)     return false;   
+  if(v1.nHitsPoss  != v2.nHitsPoss)  return false;   
+  if(v1.nHitsDedx  != v2.nHitsDedx)  return false;   
+  if(v1.nHitsFit   != v2.nHitsFit)   return false;   
+  if(v1.nSigmaPion != v2.nSigmaPion) return false;   
+  if(v1.Tdca       != v2.Tdca)       return false;   
+  if(v1.dcaZ       != v2.dcaZ)       return false;   
+  if(v1.dcaD       != v2.dcaD)       return false;   
+  if(v1.BField     != v2.BField)     return false;   
+  if(v1.bemcRadius != v2.bemcRadius) return false;   
+  if(v1.etaext     != v2.etaext)     return false;   
+  if(v1.phiext     != v2.phiext)     return false;   
+  if(v1.dEdx       != v2.dEdx)       return false;   
+  if(v1.trackIndex != v2.trackIndex) return false;   
+  if(v1.id         != v2.id)         return false;   
+  return true;
+  }
+
+inline bool operator!=(const Track& v1, const Track& v2)
+{
+  return(!(v1 == v2));
+}
+
+inline bool operator==(const TrackList& v1, const TrackList& v2){
+  if(v1.size() != v2.size()) return false;
+  for(size_t i = 0; i < v1.size(); ++i) if(v1[i] != v2[i]) return false;
+  return true;
+}
+
+
+inline std::ostream& operator<<(std::ostream& out, const Track& v)
+{
+  out << "trackId: " << v.id << ", pt: " << v.pt << ", .... ";
+  return out;
+}
+
+inline std::ostream& operator<<(std::ostream& out, const TrackList& v)
+{
+  out << "TrackList size: " << v.size();
+  return out;
+}
 
 }
 
