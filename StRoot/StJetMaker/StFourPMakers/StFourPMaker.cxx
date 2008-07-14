d60d4f4f457f9d4e1e25578a934271a181f6a7c4
tai <>
2008-07-14 21:18:48 +0000

    cleaned up the code

--- StRoot/StJetMaker/StFourPMakers/StFourPMaker.cxx
+++ StRoot/StJetMaker/StFourPMakers/StFourPMaker.cxx
@@ -1,10 +1,3 @@
-// $Id: StFourPMaker.cxx,v 1.7 2008/07/14 19:59:54 tai Exp $
+// $Id: StFourPMaker.cxx,v 1.8 2008/07/14 21:18:40 tai Exp $
 #include "StFourPMaker.h"
 ClassImp(StFourPMaker)
-
-
-void StFourPMaker::Clear(const Option_t* o)
-{
-    tracks.clear();
-    return StMaker::Clear();
-}
