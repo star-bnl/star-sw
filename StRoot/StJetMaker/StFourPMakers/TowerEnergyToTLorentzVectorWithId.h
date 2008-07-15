2c9294a50de753f7dcbaef2de7d3afde2a7f0e10
tai <>
2008-07-15 08:39:07 +0000

    *** empty log message ***

--- StRoot/StJetMaker/StFourPMakers/TowerEnergyToTLorentzVectorWithId.h
+++ StRoot/StJetMaker/StFourPMakers/TowerEnergyToTLorentzVectorWithId.h
@@ -1,5 +1,5 @@
 // -*- mode: c++;-*-
-// $Id: TowerEnergyToTLorentzVectorWithId.h,v 1.1 2008/07/15 06:23:43 tai Exp $
+// $Id: TowerEnergyToTLorentzVectorWithId.h,v 1.2 2008/07/15 08:39:06 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
 #ifndef TOWERENERGYTOTLORENTZVECTORWITHID_H
 #define TOWERENERGYTOTLORENTZVECTORWITHID_H
@@ -16,6 +16,7 @@ class TowerEnergyToTLorentzVectorWithId {
 public:
   TowerEnergyToTLorentzVectorWithId(double mass = 0 /* photon mass as default */)
     : _towerenergy2tlorentzvector(*(new TowerEnergyToTLorentzVector(mass))) { }
+  virtual ~TowerEnergyToTLorentzVectorWithId() { delete &_towerenergy2tlorentzvector; }
   TLorentzVectorWithId operator()(const TowerEnergy& deposit);
 
 private:
