025a79f8afa25f23ed1883bbad1894270cc888d0
Tai Sakuma <sakuma@bnl.gov>
2008-07-15 03:42:25 +0000

    *** empty log message ***

---
+++ StRoot/StJetMaker/StFourPMakers/TLorentzVectorWithId.cxx
@@ -0,0 +1,4 @@
+// $Id: TLorentzVectorWithId.cxx,v 1.1 2008/07/15 03:42:24 tai Exp $
+// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
+#include "TLorentzVectorWithId.h"
+ClassImp(TLorentzVectorWithId)
---
+++ StRoot/StJetMaker/StFourPMakers/TowerEnergyToTLorentzVector.cxx
@@ -0,0 +1,22 @@
+// $Id: TowerEnergyToTLorentzVector.cxx,v 1.1 2008/07/15 03:42:24 tai Exp $
+// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
+#include "TowerEnergyToTLorentzVector.h"
+
+namespace StSpinJet {
+
+TLorentzVector TowerEnergyToTLorentzVector::operator()(const TowerEnergy& deposit)
+{
+  TVector3 towerLocation;
+  towerLocation.SetPtEtaPhi(deposit.towerR, deposit.towerEta, deposit.towerPhi); 
+  TVector3 vertex(deposit.vertexX, deposit.vertexY, deposit.vertexZ);
+
+  TVector3 momentum = towerLocation - vertex;
+
+  double pMag = (deposit.energy > _mass) ? sqrt(deposit.energy*deposit.energy - _mass*_mass) : deposit.energy;
+
+  momentum.SetMag(pMag);
+
+  return TLorentzVector(momentum.x(), momentum.y(), momentum.z(), deposit.energy);
+}
+
+}
