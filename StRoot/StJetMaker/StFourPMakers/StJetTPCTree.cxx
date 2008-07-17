8ffbc312a56507780473488827a4206350c2e522
Tai Sakuma <sakuma@bnl.gov>
2008-07-17 07:51:44 +0000

    *** empty log message ***

--- StRoot/StJetMaker/StFourPMakers/StJetTPCTree.cxx
+++ StRoot/StJetMaker/StFourPMakers/StJetTPCTree.cxx
@@ -1,4 +1,4 @@
-// $Id: StJetTPCTree.cxx,v 1.3 2008/07/17 07:34:14 tai Exp $
+// $Id: StJetTPCTree.cxx,v 1.4 2008/07/17 07:51:44 tai Exp $
 #include "StJetTPCTree.h"
 
 #include <TTree.h>
@@ -15,29 +15,32 @@ StJetTPCTree::StJetTPCTree(TTree *tree,
 {
   _tree->BuildIndex(indexMajorName, indexMinorName);
 
-  _tree->SetBranchAddress("eventId"    , &_eventId     );
-  _tree->SetBranchAddress("nTracks"    , &_nTracks     );
-  _tree->SetBranchAddress("pt"         ,  _pt          );
-  _tree->SetBranchAddress("eta"        ,  _eta         );
-  _tree->SetBranchAddress("phi"        ,  _phi         );
-  _tree->SetBranchAddress("etaext"     ,  _etaext      );
-  _tree->SetBranchAddress("phiext"     ,  _phiext      );
-  _tree->SetBranchAddress("trackId"    ,  _trackId     );
-  _tree->SetBranchAddress("flag"       ,  _flag        );
-  _tree->SetBranchAddress("nHits"      ,  _nHits       );
-  _tree->SetBranchAddress("charge"     ,  _charge      );
-  _tree->SetBranchAddress("nHitsPoss"  ,  _nHitsPoss   );
-  _tree->SetBranchAddress("nHitsDedx"  ,  _nHitsDedx   );
-  _tree->SetBranchAddress("nHitsFit"   ,  _nHitsFit    );
-  _tree->SetBranchAddress("nSigmaPion" ,  _nSigmaPion  );
-  _tree->SetBranchAddress("Tdca"       ,  _Tdca        );
-  _tree->SetBranchAddress("dcaZ"       ,  _dcaZ        );
-  _tree->SetBranchAddress("dcaD"       ,  _dcaD        );
-  _tree->SetBranchAddress("BField"     ,  _BField      );
-  _tree->SetBranchAddress("bemcRadius" ,  _bemcRadius  );
-  _tree->SetBranchAddress("dEdx"       ,  _dEdx        );
-  _tree->SetBranchAddress("trackIndex" ,  _trackIndex  );
-  _tree->SetBranchAddress("runNumber"  , &_runNumber   );
+  _tree->SetBranchAddress("eventId"        , &_eventId         );
+  _tree->SetBranchAddress("nTracks"        , &_nTracks         );
+  _tree->SetBranchAddress("pt"             ,  _pt              );
+  _tree->SetBranchAddress("eta"            ,  _eta             );
+  _tree->SetBranchAddress("phi"            ,  _phi             );
+  _tree->SetBranchAddress("exitEta"        ,  _exitEta         );
+  _tree->SetBranchAddress("exitPhi"        ,  _exitPhi         );
+  _tree->SetBranchAddress("trackId"        ,  _trackId         );
+  _tree->SetBranchAddress("flag"           ,  _flag            );
+  _tree->SetBranchAddress("nHits"          ,  _nHits           );
+  _tree->SetBranchAddress("charge"         ,  _charge          );
+  _tree->SetBranchAddress("nHitsPoss"      ,  _nHitsPoss       );
+  _tree->SetBranchAddress("nHitsDedx"      ,  _nHitsDedx       );
+  _tree->SetBranchAddress("nHitsFit"       ,  _nHitsFit        );
+  _tree->SetBranchAddress("nSigmaPion"     ,  _nSigmaPion      );
+  _tree->SetBranchAddress("Tdca"           ,  _Tdca            );
+  _tree->SetBranchAddress("dcaZ"           ,  _dcaZ            );
+  _tree->SetBranchAddress("dcaD"           ,  _dcaD            );
+  _tree->SetBranchAddress("BField"         ,  _BField          );
+  _tree->SetBranchAddress("bemcRadius"     ,  _bemcRadius      );
+  _tree->SetBranchAddress("dEdx"           ,  _dEdx            );
+  _tree->SetBranchAddress("trackIndex"     ,  _trackIndex      );
+  _tree->SetBranchAddress("exitDetectorId" ,  _exitDetectorId  );
+  _tree->SetBranchAddress("exitTowerId"    ,  _exitTowerId     );
+  _tree->SetBranchAddress("detectorId"     , &_detectorId      );
+  _tree->SetBranchAddress("runNumber"      , &_runNumber       );
 }
 
 TrackList StJetTPCTree::getTrackList()
@@ -50,28 +53,31 @@ TrackList StJetTPCTree::getTrackList()
 
     Track track;
 
-    track.runNumber  = _runNumber;
-    track.eventId    = _eventId;
-    track.pt         = _pt[i];
-    track.eta        = _eta[i];
-    track.phi        = _phi[i];
-    track.flag       = _flag[i];
-    track.nHits      = _nHits[i];
-    track.charge     = _charge[i];
-    track.nHitsPoss  = _nHitsPoss[i];
-    track.nHitsDedx  = _nHitsDedx[i];
-    track.nHitsFit   = _nHitsFit[i];
-    track.nSigmaPion = _nSigmaPion[i];
-    track.Tdca       = _Tdca[i];
-    track.dcaZ       = _dcaZ[i];
-    track.dcaD       = _dcaD[i];
-    track.BField     = _BField[i];
-    track.bemcRadius = _bemcRadius[i];
-    track.exitEta    = _etaext[i];
-    track.exitPhi    = _phiext[i];
-    track.dEdx       = _dEdx[i];
-    track.trackIndex = _trackIndex[i];
-    track.id         = _trackId[i];
+    track.runNumber      = _runNumber;
+    track.eventId        = _eventId;
+    track.detectorId     = _detectorId;
+    track.pt             = _pt[i];
+    track.eta            = _eta[i];
+    track.phi            = _phi[i];
+    track.flag           = _flag[i];
+    track.nHits          = _nHits[i];
+    track.charge         = _charge[i];
+    track.nHitsPoss      = _nHitsPoss[i];
+    track.nHitsDedx      = _nHitsDedx[i];
+    track.nHitsFit       = _nHitsFit[i];
+    track.nSigmaPion     = _nSigmaPion[i];
+    track.Tdca           = _Tdca[i];
+    track.dcaZ           = _dcaZ[i];
+    track.dcaD           = _dcaD[i];
+    track.BField         = _BField[i];
+    track.bemcRadius     = _bemcRadius[i];
+    track.exitDetectorId = _exitDetectorId[i];
+    track.exitTowerId    = _exitTowerId[i];
+    track.exitEta        = _exitEta[i];
+    track.exitPhi        = _exitPhi[i];
+    track.dEdx           = _dEdx[i];
+    track.trackIndex     = _trackIndex[i];
+    track.id             = _trackId[i];
 
     ret.push_back(track);
   }
