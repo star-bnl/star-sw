845ac9617f0626e8b7d72bb3759b43b537e3d542
Valeri Fine <val.fine@gmail.com>
2008-07-22 14:19:02 +0000

    Load the UCM interface if present

--- StRoot/macros/LoadLogger.C
+++ StRoot/macros/LoadLogger.C
@@ -4,6 +4,11 @@ void LoadLogger() {
 //  gSystem->Load("logger/lib/liblog4cxx.so");
   gSystem->Load("liblog4cxx.so");
   gSystem->Load("St_base.so");
+  if (gSystem->DynamicPathName("StUCMApai.so"),kTRUE) {
+      gSystem->Load("StUCMApi.so");
+  }
+//  gSystem->Load("data");
+//  gSystem->Load("api");
   gSystem->Load("StStarLogger.so");
   StLoggerManager::StarLoggerInit();
 }
