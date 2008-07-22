16c75919d083a186be9b862edacfe23443175901
tai <>
2008-07-22 05:48:38 +0000

    *** empty log message ***

--- StRoot/StJetMaker/StPythiaFourPMaker.h
+++ StRoot/StJetMaker/StPythiaFourPMaker.h
@@ -1,16 +1,7 @@
 // -*- mode: c++;-*-
-// $Id: StPythiaFourPMaker.h,v 1.3 2008/07/22 05:06:23 tai Exp $
-/*!
-  \class StPythiaFourPMaker
-  \author M.L. Miller (MIT Software)
-
-  StPythiaFourPMaker is used to fill the list of 4-momenta that is then passed to a
-  StJetFinder instance.  StPythiaFourPMaker simply instantiates an object of type
-  StPythiaMuTrackFourVec for every final state particle in the event.
-*/
-#ifndef StPythiaFourPMaker_HH
-#define StPYthiaFourPMaker_HH
-
+// $Id: StPythiaFourPMaker.h,v 1.4 2008/07/22 05:48:29 tai Exp $
+#ifndef STPYTHIAFOURPMAKER_HH
+#define STPYTHIAFOURPMAKER_HH
 
 #include "StFourPMaker.h"
 
@@ -30,7 +21,8 @@ class StPythiaFourPMaker : public StFourPMaker {
 
 public:
     
-  StPythiaFourPMaker(const char *name, StMCAsymMaker*, StMcEventMaker*);
+  StPythiaFourPMaker(const char *name, StMCAsymMaker* sim = 0, StMcEventMaker* mc = 0)
+    : StFourPMaker(name) { }
   virtual ~StPythiaFourPMaker() { };
     
   FourList &getTracks() { return tracks; };
@@ -41,14 +33,9 @@ public:
     
   void Clear(Option_t* opt);
     
-protected:
+private:
 
   FourList tracks;
-
-  typedef std::vector<StMuTrackFourVec*> Pythia4Vec;
-  Pythia4Vec mVec;
-    
-private:
 
   StSpinJet::StJetMC* _mc;
 
@@ -56,4 +43,4 @@ private:
 };
 
 
-#endif
+#endif // STPYTHIAFOURPMAKER_HH
