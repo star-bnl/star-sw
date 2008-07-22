9d7a911bead3d5ba356dff1982542eec16d1da56
tai <>
2008-07-22 00:24:56 +0000

    *** empty log message ***

--- StRoot/StJetMaker/base/TLorentzVectorWithId.cxx
+++
@@ -1,6 +0,0 @@
-// $Id: TLorentzVectorWithId.cxx,v 1.1 2008/07/21 17:24:54 tai Exp $
-// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-#include "TLorentzVectorWithId.h"
-ClassImp(TLorentzVectorWithId)
-
-int TLorentzVectorWithId::n = 0;
