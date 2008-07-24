de6aba529255c4a1d53347b270bc23fa08caae4e
fine <>
2008-07-24 14:57:04 +0000

    Fix the typo

--- StRoot/macros/LoadLogger.C
+++ StRoot/macros/LoadLogger.C
@@ -4,7 +4,7 @@ void LoadLogger() {
    //  gSystem->Load("logger/lib/liblog4cxx.so");
   gSystem->Load("liblog4cxx.so");
   gSystem->Load("St_base.so");
-  if (gSystem->DynamicPathName("StUCMApai.so"),kTRUE) {
+  if (gSystem->DynamicPathName("StUCMApai.so",kTRUE) {
       gSystem->Load("StUCMApi.so");
   }
 //  gSystem->Load("data");
