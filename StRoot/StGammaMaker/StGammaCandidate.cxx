ea35914b340c2707dbf8e0902dea4ee2fcab60df
Jason C. Webb <jwebb@bnl.gov>
2008-07-30 14:56:12 +0000

    Implemented three new ways to define the momentum of the gamma candidate:
    1) ::momentum1x1()  -- returns momentum rescaled to single tower energy
    2) ::momentum1x1c() -- as (1) but corrects for shower leakage into neighboring towers
    3) ::momentum2x1()  -- as (1) but adds closest (in eta,phi) tower to energy sum

--- StRoot/StGammaMaker/StGammaCandidate.cxx
+++ StRoot/StGammaMaker/StGammaCandidate.cxx
@@ -1,6 +1,24 @@
 #include "StGammaCandidate.h"
 
+#include "StEEmcUtil/EEmcGeom/EEmcGeomSimple.h"
+#include "StEEmcUtil/StEEmcSmd/EEmcSmdGeom.h"
+#include "StGammaEEmcLeakage.h"
+
 ClassImp(StGammaCandidate);
+
+// ---------------------------------------------------------------- utilities -
+//
+// Define some useful functions and structures for sorting towers by distance
+//
+struct Tower {
+  StGammaTower *tower;
+  Float_t dR;
+};
+Bool_t SortDistance ( const Tower& t1, const Tower& t2 )
+{
+  return (t1.dR < t2.dR);
+}
+
 
 
 // ------------------------------------------------------- class constructor --
@@ -219,6 +237,86 @@ Int_t StGammaCandidate::numberOfPostshower( Float_t radius, Float_t thresh )
 	count++;
     }
   return count;
+}
+
+
+TVector3 StGammaCandidate::momentum1x1()
+{
+
+  return mMomentum.Unit() * mSeedEnergy;
+
+}
+
+TVector3 StGammaCandidate::momentum1x1c()
+{
+
+  //
+  // Perform light leakage correction w/in the EEmc
+  //
+  if ( detectorId() == kEEmc ) 
+    {
+      static StGammaEEmcLeakage *shape = StGammaEEmcLeakage::instance();
+      static EEmcGeomSimple    &geom  = EEmcGeomSimple::Instance();
+      
+      Int_t sec,sub,eta;
+      if ( !geom.getTower( position(), sec, sub, eta ) )
+	{
+	  return TVector3(0.,0.,-999.0);
+	}
+      
+      Int_t mysec,mysub,myeta;
+      myeta=mTowerId%12;
+      mysec=( mTowerId/12 )/5;
+      mysub=( mTowerId/12 )%5;
+      
+      TVector3 tower = geom.getTowerCenter( (UInt_t)sec, (UInt_t)sub, (UInt_t)eta );
+      
+      Float_t frac = shape->expectation( position() );
+      TVector3 p=momentum1x1();
+      p*=1.0/frac;
+      
+      return p;
+
+    }
+  else
+    {
+      // to be implemented
+    }
+  
+  return momentum1x1();
+      
+}
+
+
+
+TVector3 StGammaCandidate::momentum2x1()
+{
+  Float_t eta = mMomentum.Eta();
+  Float_t phi = mMomentum.Phi();
+  std::vector<Tower> listOfTowers;
+  for ( Int_t i=0;i<numberOfMyTowers(); i++ ) {
+    StGammaTower *t = mytower(i);
+    Float_t deta = eta - t->eta;
+    Float_t dphi = TVector2::Phi_mpi_pi(phi - t->phi);
+    Float_t dR   = TMath::Sqrt(deta*deta+dphi*dphi);
+    Tower T      = { t, dR };
+    listOfTowers.push_back(T);
+  }
+  std::sort( listOfTowers.begin(), listOfTowers.end(), SortDistance );
+  std::vector<Tower>::iterator iter;
+  Float_t energy_sum=0.;
+  Int_t count=0;
+  //std::cout << "==========================" << std::endl;                     
+  for ( iter=listOfTowers.begin(); iter!=listOfTowers.end();iter++ )
+    {
+      if ( count<2 ) {
+        energy_sum += (*iter).tower->energy;
+      }
+      count++;
+    }
+  TVector3 p=mMomentum;
+  p.SetMag(energy_sum);
+  return p;
 }
 
 
