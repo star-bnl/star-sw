46039b14ecc8dbe5263456b403fbc014cea4e889
Gene Van Buren <gene@bnl.gov>
2008-08-05 20:44:39 +0000

    Allow for multiple rows (e.g. detectors) in determining charge

--- StRoot/StDetectorDbMaker/St_spaceChargeCorC.h
+++ StRoot/StDetectorDbMaker/St_spaceChargeCorC.h
@@ -18,41 +18,51 @@ class St_spaceChargeCorC : public TChair {
   Float_t 	factor(Int_t i = 0) 	        {return Struct(i)->factor;}
   Float_t 	detector(Int_t i = 0) 	        {return Struct(i)->detector;}
   Float_t 	offset(Int_t i = 0) 	        {return Struct(i)->offset;}
-  Float_t 	getEWRatio(Int_t i = 0) 	        {return Struct(i)->ewratio;}
-  Double_t      getSpaceChargeCorrection(Double_t scaleFactor){
+  Float_t 	getEWRatio(Int_t i = 0)	        {return Struct(i)->ewratio;}
+  Double_t      getSpaceChargeCorrection(Double_t scaleFactor, Int_t i = 0){
     Double_t value = 0;
-    if(scaleFactor < -.75 && scaleFactor > -1.25) value = fullFieldB();
-    else if(scaleFactor < -0.25)	          value = halfFieldB();
-    else if(scaleFactor < .25)	                  value = zeroField();
-    else if(scaleFactor < 0.75)	                  value = halfFieldA();
-    else if(scaleFactor < 1.25)	                  value = fullFieldA();
+    if(scaleFactor < -.75 && scaleFactor > -1.25) value = fullFieldB(i);
+    else if(scaleFactor < -0.25)	          value = halfFieldB(i);
+    else if(scaleFactor < .25)	                  value = zeroField(i);
+    else if(scaleFactor < 0.75)	                  value = halfFieldA(i);
+    else if(scaleFactor < 1.25)	                  value = fullFieldA(i);
     return value;
   }
   Double_t getSpaceChargeCorrection(){return  getSpaceChargeCorrection(StDetectorDbMagnet::instance()->getScaleFactor());}
   Double_t getSpaceChargeCoulombs(Double_t scaleFactor){
     StDetectorDbRichScalers* scalers = StDetectorDbRichScalers::instance();
-    Double_t mult = 0;
-    if (! scalers ) return mult;
-    switch ((int) this->getSpaceChargeDetector()) {
-    case (0) : mult = scalers->getMult(); break;
-    case (1) : mult = scalers->getBBCX(); break;
-    case (2) : mult = scalers->getZDCX(); break;
-    case (3) : mult = scalers->getZDCEast()+scalers->getZDCWest(); break;
-    case (4) : mult = scalers->getBBCEast()+scalers->getBBCWest(); break;
-    default  : mult = 0.;
+    if (! scalers ) return 0;
+    Double_t coulombs = 0;
+    for (int row=0;row< (int) getNumRows();row++) {
+      Double_t mult = 0;
+      switch ((int) getSpaceChargeDetector(row)) {
+        case (0) : mult = scalers->getMult(); break;
+        case (1) : mult = scalers->getBBCX(); break;
+        case (2) : mult = scalers->getZDCX(); break;
+        case (3) : mult = scalers->getZDCEast()+scalers->getZDCWest(); break;
+        case (4) : mult = scalers->getBBCEast()+scalers->getBBCWest(); break;
+        case (5) : mult = scalers->getZDCEast(); break;
+        case (6) : mult = scalers->getZDCWest(); break;
+        case (7) : mult = scalers->getBBCEast(); break;
+        case (8) : mult = scalers->getBBCWest(); break;
+        case (9) : mult = scalers->getBBCYellowBkg(); break;
+        case (10): mult = scalers->getBBCBlueBkg(); break;
+        default  : mult = 0.;
+      }
+      Double_t saturation = getSpaceChargeSatRate(row);
+      Double_t correction = getSpaceChargeCorrection(scaleFactor,row);
+      Double_t factor     = getSpaceChargeFactor(row);
+      Double_t offset     = getSpaceChargeOffset(row);
+      Double_t intens = (mult < saturation) ? mult : saturation;
+      coulombs += factor * (intens-offset) * correction ;
     }
-    Double_t saturation = getSpaceChargeSatRate();
-    Double_t correction = getSpaceChargeCorrection(scaleFactor);
-    Double_t factor     = getSpaceChargeFactor();
-    Double_t offset     = getSpaceChargeOffset();
-    Double_t intens = (mult < saturation) ? mult : saturation;
-    return factor * (intens-offset) * correction ;
+    return coulombs;
   };
   Double_t getSpaceChargeCoulombs(){return getSpaceChargeCoulombs(StDetectorDbMagnet::instance()->getScaleFactor());}
-  Double_t getSpaceChargeSatRate() {return satRate();}
-  Float_t  getSpaceChargeFactor()  {return factor();}
-  Float_t  getSpaceChargeDetector(){return detector();}
-  Float_t  getSpaceChargeOffset()  {return offset();}
+  Double_t getSpaceChargeSatRate(Int_t i = 0) {return satRate(i);}
+  Float_t  getSpaceChargeFactor(Int_t i = 0)  {return factor(i);}
+  Float_t  getSpaceChargeDetector(Int_t i = 0){return detector(i);}
+  Float_t  getSpaceChargeOffset(Int_t i = 0)  {return offset(i);}
 
  protected:
   St_spaceChargeCorC(St_spaceChargeCor *table=0) : TChair(table) {}
