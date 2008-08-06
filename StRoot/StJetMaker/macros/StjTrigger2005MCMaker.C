37a62ab469a32c670668531de549651231297f02
Tai Sakuma <sakuma@bnl.gov>
2008-08-06 06:36:27 +0000

    *** empty log message ***

---
+++ StRoot/StJetMaker/macros/StjTrigger2005MCMaker.C
@@ -0,0 +1,110 @@
+#include "StMaker.h"
+
+#include "StjTrgPassCondition.h"
+
+#include "StjTrg.h"
+
+#include "StjTrgJPWriter.h"
+#include "StjTrgHTWriter.h"
+#include "StjTrgMBWriter.h"
+
+#include "StjTrg.h"
+
+#include "StjTrgSoftwareEmcTriggerMakerFactory.h"
+#include "StjTrgSoftwareTriggerSimuMakerFactory.h"
+
+class StjTrigger2005MCMaker : public StMaker {
+
+public:
+
+  StjTrigger2005MCMaker(const Char_t *name, TDirectory* file, StMuDstMaker* uDstMaker, StEmcTriggerMaker* emcTrigMaker)
+  : StMaker(name), _file(file), _uDstMaker(uDstMaker)
+  , _softTrgFactory(new StjTrgSoftwareEmcTriggerMakerFactory(emcTrigMaker))
+  { }
+
+  StjTrigger2005MCMaker(const Char_t *name, TDirectory* file, StMuDstMaker* uDstMaker, StTriggerSimuMaker* simuTrig)
+  : StMaker(name), _file(file), _uDstMaker(uDstMaker)
+  , _softTrgFactory(new StjTrgSoftwareTriggerSimuMakerFactory(simuTrig))
+  { }
+
+  virtual ~StjTrigger2005MCMaker() { }
+
+  const char* GetCVS() const
+  {static const char cvs[]="Tag $Name:  $ $Id: StjTrigger2005MCMaker.C,v 1.1 2008/08/06 06:36:27 tai Exp $ built "__DATE__" "__TIME__; return cvs;}
+
+private:
+
+  TDirectory* _file;
+
+  StMuDstMaker* _uDstMaker;
+  StjTrgSoftwareFactory* _softTrgFactory;
+
+  StjTrgWriter* _minbWriter;
+  StjTrgWriter* _bht1Writer;
+  StjTrgWriter* _bht2Writer;
+  StjTrgWriter* _bjp1Writer;
+  StjTrgWriter* _bjp2Writer;
+
+public:
+
+  Int_t Init()
+  {
+    StjTrg* minbTrg = new StjTrg(96011, _uDstMaker, _softTrgFactory->create());
+    StjTrgPassCondition* minbFillCondition = new StjTrgPassConditionSoftOnly(minbTrg);
+    StjTrgPassCondition* minbPassCondition = new StjTrgPassConditionSoftOnly(minbTrg);
+    _minbWriter = new StjTrgMBWriter("trgMINB", "trgMINB", _file, minbTrg, minbFillCondition, minbPassCondition);
+
+    StjTrg* bht1Trg = new StjTrg(96201, _uDstMaker, _softTrgFactory->create());
+    StjTrgPassCondition* bht1FillCondition = new StjTrgPassConditionSoftOnly(bht1Trg);
+    StjTrgPassCondition* bht1PassCondition = new StjTrgPassConditionSoftOnly(bht1Trg);
+    _bht1Writer = new StjTrgHTWriter("trgBHT1", "trgBHT1", _file, bht1Trg, bht1FillCondition, bht1PassCondition);
+
+    StjTrg* bht2Trg = new StjTrg(96211, _uDstMaker, _softTrgFactory->create());
+    StjTrgPassCondition* bht2FillCondition = new StjTrgPassConditionSoftOnly(bht2Trg);
+    StjTrgPassCondition* bht2PassCondition = new StjTrgPassConditionSoftOnly(bht2Trg);
+    _bht2Writer = new StjTrgHTWriter("trgBHT2", "trgBHT2", _file, bht2Trg, bht2FillCondition, bht2PassCondition);
+
+    StjTrg* bjp1Trg = new StjTrg(96221, _uDstMaker, _softTrgFactory->create());
+    StjTrgPassCondition* bjp1FillCondition = new StjTrgPassConditionSoftOnly(bjp1Trg);
+    StjTrgPassCondition* bjp1PassCondition = new StjTrgPassConditionSoftOnly(bjp1Trg);
+    _bjp1Writer = new StjTrgJPWriter("trgBJP1", "trgBJP1", _file, bjp1Trg, bjp1FillCondition, bjp1PassCondition);
+
+    StjTrg* bjp2Trg = new StjTrg(96233, _uDstMaker, _softTrgFactory->create());
+    StjTrgPassCondition* bjp2FillCondition = new StjTrgPassConditionSoftOnly(bjp2Trg);
+    StjTrgPassCondition* bjp2PassCondition = new StjTrgPassConditionSoftOnly(bjp2Trg);
+    _bjp2Writer = new StjTrgJPWriter("trgBJP2", "trgBJP2", _file, bjp2Trg, bjp2FillCondition, bjp2PassCondition);
+
+    _minbWriter->Init();
+    _bht1Writer->Init();
+    _bht2Writer->Init();
+    _bjp1Writer->Init();
+    _bjp2Writer->Init();
+
+    return kStOk;
+  }
+
+  Int_t Make()
+  {
+    _minbWriter->Make();
+    _bht1Writer->Make();
+    _bht2Writer->Make();
+    _bjp1Writer->Make();
+    _bjp2Writer->Make();
+
+    return kStOk;
+  }
+
+  Int_t Finish()
+  {
+    _minbWriter->Finish();
+    _bht1Writer->Finish();
+    _bht2Writer->Finish();
+    _bjp1Writer->Finish();
+    _bjp2Writer->Finish();
+
+    return kStOk;
+  }
+
+  ClassDef(StjTrigger2005MCMaker, 0)
+
+};
