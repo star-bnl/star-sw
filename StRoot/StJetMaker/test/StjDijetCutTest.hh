aacce11494fae0b3da24144b657be9c7f6247a07
tai <>
2008-08-13 06:08:44 +0000

    *** empty log message ***

--- StRoot/StJetMaker/test/StjDijetCutTest.hh
+++ StRoot/StJetMaker/test/StjDijetCutTest.hh
@@ -11,6 +11,16 @@ class StjDijetCutTest : public CPPUNIT_NS::TestFixture
   CPPUNIT_TEST( testEta );
   CPPUNIT_TEST( testEta3MinusEta4 );
   CPPUNIT_TEST( testDPhi );
+  CPPUNIT_TEST( testTrgBHT_notPass );
+  CPPUNIT_TEST( testTrgBHT_pass );
+  CPPUNIT_TEST( testTrgBHT_type );
+  CPPUNIT_TEST( testTrgBHT_detectorId );
+  CPPUNIT_TEST( testTrgBHT_towerId );
+  CPPUNIT_TEST( testTrgBJP_notPass );
+  CPPUNIT_TEST( testTrgBJP_pass );
+  CPPUNIT_TEST( testTrgBJP_type );
+  CPPUNIT_TEST( testTrgBJP_detectorId );
+  CPPUNIT_TEST( testTrgBJP_jetPatchId );
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -21,6 +31,16 @@ public:
   void testEta();
   void testEta3MinusEta4();
   void testDPhi();
+  void testTrgBHT_notPass();
+  void testTrgBHT_pass();
+  void testTrgBHT_type();
+  void testTrgBHT_detectorId();
+  void testTrgBHT_towerId();
+  void testTrgBJP_notPass();
+  void testTrgBJP_pass();
+  void testTrgBJP_type();
+  void testTrgBJP_detectorId();
+  void testTrgBJP_jetPatchId();
 
 private:
 
