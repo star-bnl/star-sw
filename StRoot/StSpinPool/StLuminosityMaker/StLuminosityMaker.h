3b515876d1811efb9b2046d4088ef74b804c07d7
Matthew Walker <mwalker@mit.edu>
2008-08-20 16:11:50 +0000

    Added some additional storage objects

--- StRoot/StSpinPool/StLuminosityMaker/StLuminosityMaker.h
+++ StRoot/StSpinPool/StLuminosityMaker/StLuminosityMaker.h
@@ -16,9 +16,13 @@ class StLuminosityMaker : public StMaker {
  private:
   vector<unsigned int> mTriggers;
   vector<unsigned int> mNTotal;
+  vector<unsigned int> mNVertex;
   vector<unsigned int> mNCuts;
+  vector<unsigned int> mNSoftTrig;
   vector<float> mLumTotal;
   vector<float> mLumCuts;
+  vector<float> mLumVertex;
+  vector<float> mLumSoftTrig;
   vector<float> mPrescales;
   StMuDstMaker* muDstMaker;
   StTriggerSimuMaker* mTriggerSimuMaker;
@@ -54,14 +58,17 @@ class StLuminosityMaker : public StMaker {
   const TClonesArray* getHolder() const {return mLumHolder;}
   const vector<float> getLumTotal() const {return mLumTotal;}
   const vector<float> getLumCuts() const {return mLumCuts;}
+  const vector<float> getLumVertex() const {return mLumVertex;}
+  const vector<float> getLumSoftTrig() const {return mLumSoftTrig;}
   const vector<float> getPrescales() const {return mPrescales;}
   const vector<unsigned int> getTriggers() const {return mTriggers;}
   const vector<unsigned int> getNTotal() const {return mNTotal;}
   const vector<unsigned int> getNCuts() const {return mNCuts;}
-
+  const vector<unsigned int> getNVertex() const {return mNVertex;}
+  const vector<unsigned int> getNSoftTrig() const {return mNSoftTrig;}
 
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StLuminosityMaker.h,v 1.1 2008/08/05 20:09:00 mattheww Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StLuminosityMaker.h,v 1.2 2008/08/20 16:11:50 mattheww Exp $ built "__DATE__" "__TIME__ ; return cvs;}
   
   ClassDef(StLuminosityMaker,0)   //StAF chain virtual base class for Makers
 };
