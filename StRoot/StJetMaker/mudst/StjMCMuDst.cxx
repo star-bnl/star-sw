a9999dbf144d88567ae451c3d01380d9a54a92ac
tai <>
2008-08-22 17:32:54 +0000

    *** empty log message ***

--- StRoot/StJetMaker/mudst/StjMCMuDst.cxx
+++ StRoot/StJetMaker/mudst/StjMCMuDst.cxx
@@ -1,4 +1,4 @@
-// $Id: StjMCMuDst.cxx,v 1.3 2008/08/03 00:29:03 tai Exp $
+// $Id: StjMCMuDst.cxx,v 1.4 2008/08/22 17:32:54 tai Exp $
 
 #include <StjMCMuDst.h>
 
@@ -15,7 +15,7 @@
 
 #include <TLorentzVector.h>
 
-StjMCParticleList StjMCMuDst::getMCPartilceList()
+StjMCParticleList StjMCMuDst::getMCParticleList()
 {
 
   TDataSet *Event = _maker->GetDataSet("geant");
