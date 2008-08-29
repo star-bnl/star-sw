8196ec9b3879f4f769b0900eb909de475f43ba59
Tai Sakuma <sakuma@bnl.gov>
2008-08-29 22:40:30 +0000

    *** empty log message ***

--- StRoot/StJetMaker/base/StjTowerEnergyVariationEnergy.cxx
+++ StRoot/StJetMaker/base/StjTowerEnergyVariationEnergy.cxx
@@ -1,6 +1,6 @@
-// $Id: StjTowerEnergyVariationEnergy.cxx,v 1.1 2008/08/04 21:27:18 tai Exp $
+// $Id: StjTowerEnergyVariationEnergy.cxx,v 1.2 2008/08/29 22:40:30 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-#include "StjTowerEnergyCutEnergy.h"
+#include "StjTowerEnergyVariationEnergy.h"
 
-ClassImp(StjTowerEnergyCutEnergy)
+ClassImp(StjTowerEnergyVariationEnergy)
  
