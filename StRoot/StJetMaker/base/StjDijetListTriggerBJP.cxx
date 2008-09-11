92534ef3e01a99354c17e39174737207c44eebdc
Tai Sakuma <sakuma@bnl.gov>
2008-09-11 22:36:31 +0000

    *** empty log message ***

--- StRoot/StJetMaker/base/StjDijetListTriggerBJP.cxx
+++ StRoot/StJetMaker/base/StjDijetListTriggerBJP.cxx
@@ -1,5 +1,12 @@
-// $Id: StjDijetListTriggerBJP.cxx,v 1.1 2008/09/09 00:12:45 tai Exp $
+// $Id: StjDijetListTriggerBJP.cxx,v 1.2 2008/09/11 22:36:31 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
 #include "StjDijetListTriggerBJP.h"
 
 ClassImp(StjDijetListTriggerBJP)
+
+StjDijetList StjDijetListTriggerBJP::processOneItem(const StjDijetList::value_type& item)
+{
+  StjDijetList retList;
+
+  return retList;
+}
