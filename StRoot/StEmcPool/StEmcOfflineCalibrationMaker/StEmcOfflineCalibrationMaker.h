b4ac7834a72059f39f485b3ead0bdbdd0dc36afa
Matthew Walker <mwalker@mit.edu>
2008-10-16 14:55:57 +0000

    Lots of updates to this code

--- StRoot/StEmcPool/StEmcOfflineCalibrationMaker/StEmcOfflineCalibrationMaker.h
+++ StRoot/StEmcPool/StEmcOfflineCalibrationMaker/StEmcOfflineCalibrationMaker.h
@@ -28,7 +28,8 @@ private:
 	TFile* myFile;
 	TTree* calibTree;
 	TH2* towerSlopes[2]; //[MB][HT]
-	TH3* preshowerSlopes; 
+	TH2* preshowerSlopes; 
+	TH2* smdSlopes[2]; //[eta][phi] no caps
 	StEmcOfflineCalibrationEvent* myEvent;
 	StEmcOfflineCalibrationTrack* myTrack;
 	
@@ -42,23 +43,29 @@ private:
 	
 	//all these random BEMC objects
 	StBemcTables*		mTables;
-	StEmcGeom*			mEmcGeom;
+	StEmcGeom*		mEmcGeom;
 	StEmcPosition*		mEmcPosition;
 	StEmcCollection*	mEmcCollection;
+	StEmcGeom*              mSmdEGeom;
+	StEmcGeom*              mSmdPGeom;
 	
-	//tower info (0==BTOW, 1==BPRS)
+	//tower info (0==BTOW, 1==BPRS, 2=BSMDE, 3=BSMDP)
 	unsigned short	mADC[2][4800];
+	unsigned short mADCSmd[2][18000];
 	float			mPedestal[2][4800];
 	float			mPedRMS[2][4800];
+	float mPedestalSmd[2][18000];
+	float mPedRMSSmd[2][18000];
 	int				mStatus[2][4800];
+	int mStatusSmd[2][18000];
 	unsigned char	mCapacitor[4800]; //only for BPRS
+	unsigned char mCapacitorSmd[2][18000];
 	
-	void getADCs(int det); //1==BTOW, 2==BPRS
-	pair<unsigned short, pair<float,float> > getTrackTower(StMuTrack* track, bool useExitRadius=false);
+	void getADCs(int det); //1==BTOW, 2==BPRS, 3=BSMDE, 4=BSMDP
+	pair<unsigned short, pair<float,float> > getTrackTower(StMuTrack* track, bool useExitRadius=false, int det=1);
 	float getTrackDeltaR(float track_eta, float track_phi, int id);
-	pair<float, float> getTrackDetaDphi(float track_eta, float track_phi, int id);
+	pair<float, float> getTrackDetaDphi(float track_eta, float track_phi, int id, int det);
 	double highestNeighbor(int id);
-	int getCorrectSignalForPRS(int softId);
 	
 	
 public:
