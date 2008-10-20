11c69696f232b503d2afd7bd95ad8c6e0aef4765
Jonathan Bouchet <bouchet@rcf.rhic.bnl.gov>
2008-10-20 19:25:48 +0000

    propagate idMcTrack used for IdTruth

--- StRoot/StSsdUtil/StSsdStrip.hh
+++ StRoot/StSsdUtil/StSsdStrip.hh
@@ -1,6 +1,9 @@
-// $Id: StSsdStrip.hh,v 1.2 2008/05/07 22:48:36 bouchet Exp $
+// $Id: StSsdStrip.hh,v 1.3 2008/10/20 19:25:48 bouchet Exp $
 //
 // $Log: StSsdStrip.hh,v $
+// Revision 1.3  2008/10/20 19:25:48  bouchet
+// propagate idMcTrack used for IdTruth
+//
 // Revision 1.2  2008/05/07 22:48:36  bouchet
 // calculation of quality of hits used embedding
 //
@@ -21,10 +24,11 @@ class StSsdStrip
  public:
 
   StSsdStrip(Int_t rNStrip, Int_t rDigitSig, Float_t rSigma, Int_t rPedestal);
-  StSsdStrip(Int_t rNStrip, Int_t rDigitSig, Float_t rSigma, Int_t rPedestal, Int_t *rMcHit);
+  StSsdStrip(Int_t rNStrip, Int_t rDigitSig, Float_t rSigma, Int_t rPedestal, Int_t *rMcHit,Int_t *rMcTrack);
   // Sls
   StSsdStrip(Int_t rNStrip, Int_t rIdHit,    Int_t rMcHit,   Int_t rMcTrack,  Float_t rAnalogSig);
   // Spa
+  StSsdStrip(Int_t rNStrip, Int_t rMcStrip, Int_t rDigitSig, Float_t rAnalogSig, Int_t *rIdMcHit,Int_t *rMcTrack);
   StSsdStrip(Int_t rNStrip, Int_t rMcStrip, Int_t rDigitSig, Float_t rAnalogSig, Int_t *rIdMcHit);
   StSsdStrip(Int_t rNStrip, Int_t rMcStrip, Float_t rAnalogSig);
   StSsdStrip(Int_t rNStrip, Int_t rDigitSig);
