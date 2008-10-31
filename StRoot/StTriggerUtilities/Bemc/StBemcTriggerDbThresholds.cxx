fb9ff310b499ed101a825488e0426817925b2716
rfatemi <>
2008-10-31 18:39:46 +0000

    update 2007 L0 BEMC thresholds

--- StRoot/StTriggerUtilities/Bemc/StBemcTriggerDbThresholds.cxx
+++ StRoot/StTriggerUtilities/Bemc/StBemcTriggerDbThresholds.cxx
@@ -31,6 +31,7 @@ void StBemcTriggerDbThresholds::LoadTimeStamps(){
   //settings for year 2006
   // subtract 10 seconds from each beginTime and add 10 to each endTime
   // just in case DB record is off be a second (it happens) -- APK
+ 
   //#0   Runs#7097009 - 7101054   2006-04-07 04:43:31 to 2006-04-11 17:21:09
   start_2006[0].Set(2006,04,07,04,43,21);
   end_2006[0].Set(2006,04,11,17,21,19);
@@ -50,6 +51,36 @@ void StBemcTriggerDbThresholds::LoadTimeStamps(){
   start_2006[5].Set(2006,5,16,2,38,45);
   end_2006[5].Set(2006,6,5,15,49,27);
   
+
+  //settings for year 2007
+
+  //#0 Runs#8094008 - 8102029 2007-04-04 05:38:54 to 2007-04-12 09:13:17
+  //200211 - bht2-mb (somewhere in here bit0 changed from 6 to 3!)
+  start_2007[0].Set(2007,4,4,5,38,44);
+  end_2007[0].Set(2007,4,12,9,13,27);
+
+  //#1 Runs#8097121 - 8108014 2007-04-08 03:26:09 to 2007-04-18 08:23:00
+  //200585 - bht2
+  start_2007[1].Set(2007,4,8,3,25,59);
+  end_2007[1].Set(2007,4,15,8,23,10);
+
+  //#2 Runs#8103029 - 8113068 2007-04-13 20:09:39 to 2007-04-23 17:24:13 
+  //200212 - bht2-mb 
+  //200213 - btag, 200601 - L2-upsilon
+  //200220 - bht2-mb
+  //200601 - L2-upsilon bht1
+  start_2007[2].Set(2007,4,13,20,9,29);
+  end_2007[2].Set(2007,4,23,17,24,23);
+  
+  //#3 Runs#8109015 - 8177048 2007-04-19 05:01:58 to 2007-06-26 12:57:31 	
+  //200221 - bht2-mb, 200620 - L2-gamma 
+  //200586 - bht2, 200222 - bht2-mb, 200214 - btag, 
+  //200602 - L2-upsilon, 200621 - L2-gamma (HTbits = 3,18,23)
+  start_2007[3].Set(2007,4,19,5,1,48);
+  end_2007[3].Set(2007,6,26,12,57,41);
+
+
+
 }
 
 //==================================================
@@ -101,6 +132,9 @@ Int_t  StBemcTriggerDbThresholds::GetTP_DSM0_threshold(Int_t DSMmodule, UInt_t t
 	    }
 	}
     }
+
+
+  //There were no TP thresholds in 2007 
   
   return threshold;
 }
@@ -139,7 +173,8 @@ Int_t  StBemcTriggerDbThresholds::GetJP_DSM1_threshold(Int_t DSMmodule,UInt_t ti
 	}
     }
   
-  
+  //There were no JP thresholds in 2007
+
   return threshold;
 }
 
@@ -181,6 +216,41 @@ Int_t  StBemcTriggerDbThresholds::GetHT_DSM0_threshold(Int_t DSMmodule, UInt_t t
 	  }
       }
   }
+
+
+  //2007 HT East and West Thresholds (no separation of east or west in 2007)
+  const Int_t HTW0_TH_2007[4]   = {  6,  4,  3,  3};
+  const Int_t HTW1_TH_2007[4]   = { 18, 18, 18, 18};
+  const Int_t HTW2_TH_2007[4]   = { 24, 24, 24, 23};
+  const Int_t HTE0_TH_2007[4]   = {  6,  4,  3,  3};
+  const Int_t HTE1_TH_2007[4]   = { 18, 18, 18, 18};
+  const Int_t HTE2_TH_2007[4]   = { 24, 24, 24, 23};
+  for (int i=0;i<4;i++){
+    
+    if (DSMmodule<15)
+      {//WEST
+	if ((timestamp>=start_2007[i].Get())&&(timestamp<=end_2007[i].Get()))
+	  {
+	    if (layer==0) threshold=HTW0_TH_2007[i];
+	    if (layer==1) threshold=HTW1_TH_2007[i];
+	    if (layer==2) threshold=HTW2_TH_2007[i];
+	  }
+	
+      }
+    
+    
+    if (DSMmodule>=15)
+      {//EAST 
+	if ((timestamp>=start_2007[i].Get())&&(timestamp<=end_2007[i].Get()))
+	  {
+	    
+	    if (layer==0) threshold=HTE0_TH_2007[i];
+	    if (layer==1) threshold=HTE1_TH_2007[i];
+	    if (layer==2) threshold=HTE2_TH_2007[i];
+	  }
+      }
+  }
+
   
   return threshold;
   
