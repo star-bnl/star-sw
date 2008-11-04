dd20d3eb0f7a2fd57a81586ec58094902c61b713
Tai Sakuma <sakuma@bnl.gov>
2008-11-04 08:08:11 +0000

    *** empty log message ***

--- StRoot/StJetMaker/base/StjTowerEnergyListCut.cxx
+++ StRoot/StJetMaker/base/StjTowerEnergyListCut.cxx
@@ -1,36 +1,4 @@
-// $Id: StjTowerEnergyListCut.cxx,v 1.6 2008/11/04 07:52:50 tai Exp $
-// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-//#include "StjTowerEnergyListCut.h"
-//
-// ClassImp(StjTowerEnergyListCut)
-// 
-// using namespace std;
-// 
-// StjTowerEnergyList StjTowerEnergyListCut::operator()(const StjTowerEnergyList &energyList)
-// {
-//   StjTowerEnergyList ret;
-// 
-//   for(StjTowerEnergyList::const_iterator it = energyList.begin(); it != energyList.end(); ++it) {
-// 
-//     if(shouldNotKeep(*it)) continue;
-// 
-//     ret.push_back(*it);
-// 
-//   }
-// 
-//   return ret;
-// }
-// 
-// 
-// bool StjTowerEnergyListCut::shouldNotKeep(const StjTowerEnergy& energyDeposit)
-// {
-//   for(CutList::iterator cut = _cutList.begin(); cut != _cutList.end(); ++cut){
-//     if((**cut)(energyDeposit)) return true;
-//   }
-// 
-//   return false;
-// }
-// $Id: StjTowerEnergyListCut.cxx,v 1.6 2008/11/04 07:52:50 tai Exp $
+// $Id: StjTowerEnergyListCut.cxx,v 1.7 2008/11/04 08:07:01 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
 #include "StjTowerEnergyListCut.h"
 
