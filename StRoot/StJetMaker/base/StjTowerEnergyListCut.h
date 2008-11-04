ed131fd5d4bb427e8114f60d7f8376d51630a5f4
tai <>
2008-11-04 07:56:30 +0000

    *** empty log message ***

--- StRoot/StJetMaker/base/StjTowerEnergyListCut.h
+++ StRoot/StJetMaker/base/StjTowerEnergyListCut.h
@@ -1,5 +1,5 @@
 // -*- mode: c++;-*-
-// $Id: StjTowerEnergyListCut.h,v 1.8 2008/11/04 07:52:51 tai Exp $
+// $Id: StjTowerEnergyListCut.h,v 1.9 2008/11/04 07:56:30 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
 #ifndef STJTOWERENERGYLISTCUT_H
 #define STJTOWERENERGYLISTCUT_H
@@ -44,7 +44,46 @@ typedef StkListCut<StjTowerEnergy> StjTowerEnergyListCut;
 
 #endif // STJTOWERENERGYLISTCUT_H
 // -*- mode: c++;-*-
-// $Id: StjTowerEnergyListCut.h,v 1.8 2008/11/04 07:52:51 tai Exp $
+// $Id: StjTowerEnergyListCut.h,v 1.9 2008/11/04 07:56:30 tai Exp $
+// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
+#ifndef STJTOWERENERGYLISTCUT_H
+#define STJTOWERENERGYLISTCUT_H
+
+#include <TObject.h>
+
+#include "StjTowerEnergyList.h"
+#include "StjTowerEnergyCut.h"
+
+#include <vector>
+
+class StjTowerEnergyListCut : public TObject {
+
+public:
+  StjTowerEnergyListCut() { }
+  virtual ~StjTowerEnergyListCut() { }
+  
+  StjTowerEnergyList operator()(const StjTowerEnergyList& energyList);
+
+  void addCut(StjTowerEnergyCut* cut) {
+    _cutList.push_back(cut);
+  }
+
+  typedef std::vector<StjTowerEnergyCut*> CutList;
+  CutList getCutList() { return _cutList; }
+
+private:
+
+  bool shouldNotKeep(const StjTowerEnergy& deposit);
+
+  CutList _cutList;
+
+  ClassDef(StjTowerEnergyListCut, 1)
+
+};
+
+#endif // STJTOWERENERGYLISTCUT_H
+// -*- mode: c++;-*-
+// $Id: StjTowerEnergyListCut.h,v 1.9 2008/11/04 07:56:30 tai Exp $
 // Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
 #ifndef STJTOWERENERGYLISTCUT_H
 #define STJTOWERENERGYLISTCUT_H
