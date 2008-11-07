bab1930867e663d1732a468e127412e4907ccaea
jml <>
2008-11-07 17:01:49 +0000

    adding daqReader

---
+++ StRoot/RTS/src/DAQ_READER/daqReader.hh
@@ -0,0 +1,22 @@
+#ifndef _EVP_READER_HH_
+#define _EVP_READER_HH_
+
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <evp.h>		// for some constants
+
+// #include <evpSupport.h>
+
+typedef unsigned int UINT32;
+#include "cfgutil.h"
+
+#include "SFS/sfs_index.h"
+#include "RTS_READER/rts_reader.h"
+
+
+#include "daqReaderClass.h"
+
+#endif
