b75e5333386e0ab6fdabff52f5c91ef2cd64ad19
Tai Sakuma <sakuma@bnl.gov>
2008-11-11 23:24:17 +0000

    *** empty log message ***

---
+++ StRoot/StJetMaker/macros/StjSimuBBCMaker.C
@@ -0,0 +1,81 @@
+#include "StMaker.h"
+
+
+#include <StMuDSTMaker/COMMON/StMuDstMaker.h>
+#include <StMuDSTMaker/COMMON/StMuDst.h>
+#include <StMuDSTMaker/COMMON/StMuEvent.h>
+
+#include <StTriggerUtilities/StTriggerSimuMaker.h>
+#include <StTriggerUtilities/StTriggerSimuResult.h>
+#include <StTriggerUtilities/Bemc/StBemcTriggerSimu.h>
+
+#include <TChain.h>
+#include <TDirectory.h>
+
+#include <libgen.h>
+
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class StjSimuBBCMaker : public StMaker {
+
+public:
+
+  StjSimuBBCMaker(const Char_t *name, TDirectory* file, StMuDstMaker* uDstMaker)
+  : StMaker(name), _file(file), _uDstMaker(uDstMaker)
+  { }
+
+  virtual ~StjSimuBBCMaker() { }
+
+  const char* GetCVS() const
+  {static const char cvs[]="Tag $Name:  $ $Id: StjSimuBBCMaker.C,v 1.1 2008/11/11 23:24:17 tai Exp $ built "__DATE__" "__TIME__; return cvs;}
+
+private:
+
+  TDirectory* _file;
+  TTree* _tree;
+
+  StMuDstMaker* _uDstMaker;
+
+  Int_t _runNumber;
+  Int_t _eventId;
+  Int_t _bbc;
+  Double_t _vertexZ;
+
+public:
+
+  Int_t Init()
+  {
+    _file->cd();
+    _tree = new TTree("simuBBC", "simuBBC");
+    _tree->Branch("runNumber"  , &_runNumber  , "runNumber/I"  );
+    _tree->Branch("eventId"    , &_eventId    , "eventId/I"    );
+    _tree->Branch("bbc"        , &_bbc        , "bbc/I"        );
+    _tree->Branch("vertexZ"    , &_vertexZ    , "vertexZ/D"    );    
+    return kStOk;
+  }
+
+  Int_t Make()
+  {
+    _runNumber = _uDstMaker->muDst()->event()->runId();
+    _eventId = _uDstMaker->muDst()->event()->eventId();
+    _vertexZ = _uDstMaker->muDst()->event()->primaryVertexPosition().z();
+
+    StTriggerSimuMaker* trigSimu = dynamic_cast<StTriggerSimuMaker*>(GetMaker("StarTrigSimu"));
+    StTriggerSimuResult trigResult = trigSimu->detailedResult(117001);
+
+    _bbc = trigResult.bbcDecision();
+
+    _tree->Fill();
+    return kStOk;
+  }
+
+  Int_t Finish()
+  {
+    _tree->BuildIndex("runNumber", "eventId");
+    return kStOk;
+  }
+  ClassDef(StjSimuBBCMaker, 0)
+};
