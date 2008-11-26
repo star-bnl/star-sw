c12fc9a28f8edbb81c4f4c653a52d75c2de84ae2
Valeri Fine <val.fine@gmail.com>
2008-11-26 15:53:12 +0000

    fix the CPP flags for StDAQReader.h

--- StRoot/StDAQMaker/StDAQReader.h
+++ StRoot/StDAQMaker/StDAQReader.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StDAQReader.h,v 1.35 2008/11/25 21:42:53 fine Exp $
+ * $Id: StDAQReader.h,v 1.36 2008/11/26 15:53:12 fine Exp $
  *
  * Author: Victor Perev
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StDAQReader.h,v $
+ * Revision 1.36  2008/11/26 15:53:12  fine
+ * fix the CPP flags for StDAQReader.h
+ *
  * Revision 1.35  2008/11/25 21:42:53  fine
  * preparing  DAQ maker for DAQ_READER
  *
@@ -132,11 +135,12 @@ class StTRGReader ;
 class StSVTReader ;
 class StSCReader  ;
 class TDataSet    ;
-#if !defined(OLD_EVP_READER) && !defined(NEW_DAQ_READER)
-class evpReader   ; // new  2007 DAQ file reader
-#elif defined(NEW_DAQ_READER)
-class daqReader;
-typedef daqReader evpReader;
+
+#ifdef NEW_DAQ_READER
+  class daqReader;
+  typedef daqReader evpReader;
+#else
+  class evpReader   ; // new  2007 DAQ file reader
 #endif
 
 #ifndef __CINT__
