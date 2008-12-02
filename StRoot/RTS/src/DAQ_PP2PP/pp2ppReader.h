41f460b2306cfcd120b842b7f87d8920be039a1b
Valeri Fine <val.fine@gmail.com>
2008-12-02 17:49:50 +0000

    the bunch of RTS leagcy reader to smooth the trnasitionn

---
+++ StRoot/RTS/src/DAQ_PP2PP/pp2ppReader.h
@@ -0,0 +1,7 @@
+#ifndef _PP2PP_READER_H_
+#define _PP2PP_READER_H_
+
+#include "daq_pp2pp.h"
+DAQ_LEGACY_DECL(pp2pp);
+
+#endif
---
+++ StRoot/RTS/src/DAQ_SSD/ssdReader.h
@@ -0,0 +1,7 @@
+#ifndef _SSD_READER_H_
+#define _SSD_READER_H_
+
+#include "daq_ssd.h"
+DAQ_LEGACY_DECL(ssd);
+
+#endif
