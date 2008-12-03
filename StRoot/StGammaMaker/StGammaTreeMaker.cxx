c31335d6db5cb7e5c9ac2e1b4e1d1ac3b9eda557
betan <>
2008-12-03 15:39:18 +0000

    Added switch for storing empty events, cosmetic updates, and increased class def

--- StRoot/StGammaMaker/StGammaTreeMaker.cxx
+++ StRoot/StGammaMaker/StGammaTreeMaker.cxx
@@ -1,121 +1,189 @@
-#include "StGammaTreeMaker.h"
-
 #include <TFile.h>
 #include <TTree.h>
-
-#include "StEventTypes.h"
-#include "StMuDSTMaker/COMMON/StMuTypes.hh"
-
-#include "StGammaEventMaker.h"
-#include  "StGammaEvent.h"
-
-#include "StGammaRawMaker.h"
+#include <TString.h>
 #include "TObjString.h"
 
-ClassImp(StGammaTreeMaker);
+#include "StMuDSTMaker/COMMON/StMuDst.h"
+#include "StMuDSTMaker/COMMON/StMuDstMaker.h"
+#include "StMuDSTMaker/COMMON/StMuEvent.h"
+#include "StMuDSTMaker/COMMON/StMuPrimaryVertex.h"
+
+#include "StGammaEventMaker.h"
+#include "StGammaEvent.h"
+#include "StGammaRawMaker.h"
+
+#include "StGammaTreeMaker.h"
+
 ClassImp(StGammaTreeVersion);
+ClassImp(StGammaTreeMaker);
 
-// ---------------------------------------------------------------------
-StGammaTreeMaker::StGammaTreeMaker(const Char_t *name):StMaker(name)
-{
-  mFilename   = "gammas.gtree.root";
-  mGammaFile  = 0;
-  mGammaTree  = 0;
-  mGammaEvent = 0;
-}
+using namespace std;
 
-// ---------------------------------------------------------------------
-Int_t StGammaTreeMaker::Init()
-{
-  if ( !mGammaFile )
-    mGammaFile=new TFile(mFilename,"RECREATE");
-  if ( !mGammaTree ) {
-    mGammaTree=new TTree("gammas","Gamma TTree $Id: StGammaTreeMaker.cxx,v 1.9 2008/06/30 16:34:13 jwebb Exp $ built "__DATE__" "__TIME__);    
-    mGammaTree->SetDirectory(mGammaFile);
-  }
+//////////////////////////////////////////////////
+//                 Constructor                  //
+//////////////////////////////////////////////////
+StGammaTreeVersion::StGammaTreeVersion(const char *name, const char *title): TNamed(name, title)
+{}
 
-  StGammaEventMaker *gemaker = (StGammaEventMaker*)GetMakerInheritsFrom("StGammaEventMaker");
-  assert(gemaker);
+//////////////////////////////////////////////////
+//                  Destructor                  //
+//////////////////////////////////////////////////
+StGammaTreeVersion::~StGammaTreeVersion()
+{}
 
-  mGammaEvent = gemaker->event();
-  mGammaTree->Branch("GammaEvent",&mGammaEvent,32000,99);
-  mGammaTree->BranchRef();  // to ensure reference table is saved
-
-  // Generate versioning information for each type of storage container
-  mVersion.mStorageTags.push_back( TString(mGammaEvent->GetCVS()) );
-  mVersion.mStorageTags.push_back( TString(mGammaEvent->newCandidate()->GetCVS()) );
-  mVersion.mStorageTags.push_back( TString(mGammaEvent->newTower()->GetCVS()) );
-  mVersion.mStorageTags.push_back( TString(mGammaEvent->newTrack()->GetCVS()) );
-  mVersion.mStorageTags.push_back( TString(mGammaEvent->newStrip()->GetCVS()) );
-  mVersion.mStorageTags.push_back( TString(mGammaEvent->candidate(0)->smdFit().GetCVS() ) ); 
-  // Clear the current event
-  mGammaEvent->Clear();
-
-  assert(mGammaFile);
-  assert(mGammaTree);
-
-  return StMaker::Init();
-
-}
-
-Int_t StGammaTreeMaker::Finish()
-{
-
-  // Write QA information to log files AND append information
-  // to an object which we will save w/in the tree file
-
-  TString cvstag=GetCVS();
-  //  LOG_QA << "[StGamma] " << cvstag.Data() << endm;
-  TIter next( GetParentChain()->GetMakeList());
-  mVersion.mMakerTags.push_back( cvstag );
-  StMaker *maker;
-  while ((maker = (StMaker* )next())) {
-    cvstag=maker->GetCVS();
-    //    LOG_QA << "[StGamma] " << cvstag.Data() << endm;
-    mVersion.mMakerTags.push_back( cvstag );
-  }
-  LOG_QA<<" -- StGammaMaker versioning information follows --" << endm;
-  mVersion.print();
-  mGammaFile->cd();
-  mVersion.Write();
-  mGammaTree->Write();
-
-  return kStOK;
-
-}
-
-// ---------------------------------------------------------------------
-
-Int_t StGammaTreeMaker::Make()
-{
-  // write out the tree
-  mGammaTree->Fill();
-  return kStOK;
-
-}
-
-// ---------------------------------------------------------------------
-void StGammaTreeMaker::Clear(Option_t *opts)
-{
-  mGammaEvent->Clear();
-  StMaker::Clear(opts);
-}
-
-// ---------------------------------------------------------------------
+//////////////////////////////////////////////////
+//           Print version information          //
+//////////////////////////////////////////////////
 void StGammaTreeVersion::print()
 {
 
-  std::cout << "-- List of makers --" << std::endl << std ::endl;
-  for ( UInt_t i=0;i<mMakerTags.size();i++ )
+    cout << "-- List of makers --" << endl << endl;
+    for(UInt_t i = 0; i < mMakerTags.size(); i++)
     {
-      std::cout << Form("[%i]: %s",(int)i,mMakerTags[i].Data()) << std::endl;
+        cout << Form("[%i]: %s", (int)i, mMakerTags[i].Data()) << endl;
+    }
+    
+    cout << endl;
+    cout << "-- List of containers --" << endl <<  endl;
+    for (UInt_t i = 0; i < mStorageTags.size(); i++)
+    {
+        cout << Form("[%i]: %s", (int)i, mStorageTags[i].Data()) << endl;
     }
 
-  std::cout << std::endl;
-  std::cout << "-- List of containers --" << std::endl << std ::endl;
-  for ( UInt_t i=0;i<mStorageTags.size();i++ )
+}
+
+//////////////////////////////////////////////////
+//                 Constructor                  //
+//////////////////////////////////////////////////
+StGammaTreeMaker::StGammaTreeMaker(const char *name): StMaker(name)
+{
+    mFilename="gamma_tree.root";
+    mGammaFile  = 0;
+    mGammaTree  = 0;
+    mGammaEvent = 0;
+    mSkipEmpty = true;
+}
+
+//////////////////////////////////////////////////
+//                  Destructor                  //
+//////////////////////////////////////////////////
+StGammaTreeMaker::~StGammaTreeMaker()
+{}
+
+//////////////////////////////////////////////////
+//                 Maker Init                   //
+//////////////////////////////////////////////////
+Int_t StGammaTreeMaker::Init()
+{
+
+    // Create a file if necessary
+    if(!mGammaFile) mGammaFile = new TFile(mFilename, "RECREATE");
+    
+    // Create a tree if necessary
+    if(!mGammaTree) 
     {
-      std::cout << Form("[%i]: %s",(int)i,mStorageTags[i].Data()) << std::endl;
+        TString title = "Gamma TTree $Id: StGammaTreeMaker.cxx,v 1.10 2008/12/03 15:39:18 betan Exp $ built "__DATE__" "__TIME__;
+        mGammaTree = new TTree("gammas", title);    
+        mGammaTree->SetDirectory(mGammaFile);
+    }
+    
+    // Retrieve StGammaEventMaker from the chain
+    StGammaEventMaker *mGammaEventMaker = dynamic_cast<StGammaEventMaker*>(GetMakerInheritsFrom("StGammaEventMaker"));
+    if(!mGammaEventMaker) 
+    {
+        LOG_DEBUG << "Make() - No StGammaEventMaker found!" << endm;
+        return kStFatal;
+    }
+    
+    mGammaEvent = mGammaEventMaker->event();
+    mGammaTree->Branch("GammaEvent", &mGammaEvent, 32000, 99);
+    mGammaTree->BranchRef(); // Ensure that the reference table is saved
+    
+    // Generate versioning information for each type of storage container
+    mVersion.mStorageTags.push_back( TString(mGammaEvent->GetCVS()) );
+    mVersion.mStorageTags.push_back( TString(mGammaEvent->newCandidate()->GetCVS()) );
+    mVersion.mStorageTags.push_back( TString(mGammaEvent->newTower()->GetCVS()) );
+    mVersion.mStorageTags.push_back( TString(mGammaEvent->newTrack()->GetCVS()) );
+    mVersion.mStorageTags.push_back( TString(mGammaEvent->newStrip()->GetCVS()) );
+    mVersion.mStorageTags.push_back( TString(mGammaEvent->candidate(0)->smdFit().GetCVS() ) ); 
+    
+    // Clear the current event
+    mGammaEvent->Clear();
+    
+    assert(mGammaFile);
+    assert(mGammaTree);
+    
+    return StMaker::Init();
+
+}
+
+//////////////////////////////////////////////////
+//                 Maker Clear                  //
+//////////////////////////////////////////////////
+void StGammaTreeMaker::Clear(Option_t *opts)
+{
+    mGammaEvent->Clear();
+    StMaker::Clear(opts);
+}
+
+//////////////////////////////////////////////////
+//                  Maker Make                  //
+//////////////////////////////////////////////////
+Int_t StGammaTreeMaker::Make()
+{
+
+    if( !GetDataSet("MuDst") ) 
+    {
+        LOG_DEBUG << "Make() - MuDst not found!" << endm;
+        return kStFatal;
     }
 
+    // Grabbing StGammaRawMaker was originally here,
+    // is it still necessary?
+
+    // Store event information in the tree
+    if(mSkipEmpty)
+    {
+        if(mGammaEvent->numberOfCandidates() > 0) mGammaTree->Fill();
+    }
+    else
+    {
+        mGammaTree->Fill();
+    }
+    
+    return kStOK;
+
+}
+
+//////////////////////////////////////////////////
+//                 Maker Finish                 //
+//////////////////////////////////////////////////
+Int_t StGammaTreeMaker::Finish()
+{
+
+    // Write QA information to log files AND append information
+    // to an object which we will save w/in the tree file
+    
+    TString cvstag = GetCVS();
+    
+    TIter next( GetParentChain()->GetMakeList());
+    mVersion.mMakerTags.push_back( cvstag );
+    
+    StMaker *maker;
+    while(maker = (StMaker*)next()) 
+    {
+        cvstag = maker->GetCVS();
+        mVersion.mMakerTags.push_back(cvstag);
+    }
+    
+    LOG_QA <<" -- StGammaMaker versioning information follows --" << endm;
+    mVersion.print();
+    
+    // Save tree
+    mGammaFile->cd();
+    mVersion.Write();
+    mGammaTree->Write();
+  
+    return kStOK;
+    
 }
