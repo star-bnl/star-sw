146a9d2843bee594893027740be361c5cc2f5855
Michael Betancourt <betan@mit.edu>
2008-12-03 15:38:35 +0000

    Cosmetic update

--- StRoot/StGammaMaker/StGammaTrack.h
+++ StRoot/StGammaMaker/StGammaTrack.h
@@ -1,47 +1,62 @@
-// -*- C++ -*-
+////////////////////////////////////////////////////////////
+//                                                        //
+//    StGammaTrack                                        //
+//                                                        //
+//    Lightweight class to hold TPC track information     //
+//                                                        //
+//    Original concept and implementation by              //
+//    Jason Webb (Valpo)                                  //
+//                                                        //
+////////////////////////////////////////////////////////////
 
-#ifndef __StGammaTrack_h__
-#define __StGammaTrack_h__
+#ifndef STAR_StGammaTrack
+#define STAR_StGammaTrack
 
 class StMuTrack;
 class StGammaCandidate;
 
+#include "TObject.h"
 #include "TVector3.h"
 #include "TRefArray.h"
 #include "StPhysicalHelix.hh"
 
-class StGammaTrack : public TObject {
-public:
-  StGammaTrack();
-  StGammaTrack(StMuTrack* track);
-  ~StGammaTrack() {}
+class StGammaTrack: public TObject 
+{
 
-  virtual const char* GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StGammaTrack.h,v 1.7 2008/06/30 14:58:45 jwebb Exp $ built "__DATE__" "__TIME__; return cvs;}
+    public:
+        
+        class Exception {};
+        
+        StGammaTrack();
+        StGammaTrack(StMuTrack* track);
+        ~StGammaTrack();
+        
+        virtual const char* GetCVS() const
+        {static const char cvs[] = "Tag $Name:  $ $Id: StGammaTrack.h,v 1.8 2008/12/03 15:38:35 betan Exp $ built "__DATE__" "__TIME__; return cvs; }
+        
+        Int_t    id;     /// id of the track
+        Int_t    flag;   /// track flag
+        Int_t    type;   /// track type 0=global 1=primary ...
+        Int_t    charge; /// track charge
+        Int_t    nhits;  /// nhits along track
+        Float_t  dEdx;   /// energy loss in keV
+        TVector3 momentum;
+        TVector3 dca;	/// Distance of closest approach
+        StPhysicalHelix helix; /// Returns inner helix (first measured point)
+        StPhysicalHelix outerHelix; /// Returns outer helix (last measured point)
+        TVector3 positionAtRadius(Double_t radius) const; /// Returns (0,0,0) if failed
+        TVector3 positionAtZ(Double_t z) const;	/// Returns (0,0,0) if failed
+        TRefArray candidates; /// Referencing candidates
+        
+        Float_t pt() const;   /// pt at vertex
+        Float_t pz() const;   /// pz at vertex
+        Float_t eta() const;  /// eta at vertex
+        Float_t phi() const;  /// phi angle at vertex
 
-
-  Int_t    id;     /// id of the track
-  Int_t    flag;   /// track flag
-  Int_t    type;   /// track type 0=global 1=primary ...
-  Int_t    charge; /// track charge
-  Int_t    nhits;  /// nhits along track
-  Float_t  dEdx;   /// energy loss in keV
-  TVector3 momentum;
-  TVector3 dca;	/// Distance of closest approach
-  StPhysicalHelix helix; /// Returns inner helix (first measured point)
-  StPhysicalHelix outerHelix; /// Returns outer helix (last measured point)
-  TVector3 positionAtRadius(Double_t radius) const; /// Returns (0,0,0) if failed
-  TVector3 positionAtZ(Double_t z) const;	/// Returns (0,0,0) if failed
-  TRefArray candidates; /// Referencing candidates
-
-  Float_t pt() const;   /// pt at vertex
-  Float_t pz() const;   /// pz at vertex
-  Float_t eta() const;  /// eta at vertex
-  Float_t phi() const;  /// phi angle at vertex
-
-private:
-
-  ClassDef(StGammaTrack,2);
+    private:
+        
+        ClassDef(StGammaTrack, 2);
+        
 };
 
 inline Float_t StGammaTrack::pt() const { return momentum.Pt(); }
