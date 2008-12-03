dda68cb5a33d5ced64ec1039db6a57d105073eb0
Michael Betancourt <betan@mit.edu>
2008-12-03 15:37:52 +0000

    Added reference array to projecting tracks, increased class def

--- StRoot/StGammaMaker/StGammaTower.cxx
+++ StRoot/StGammaMaker/StGammaTower.cxx
@@ -2,57 +2,42 @@
 
 ClassImp(StGammaTower);
 
-StGammaTower::StGammaTower(){ /* nada */ };
-
 
 // Mapping between endcap index and sector, subsector, etabin is defined
 // in StEEmcA2EMaker::index(...)
 
+//////////////////////////////////////////////////
+//   Calculate sector, subsector, etabin, and   //
+//   phibin for EEMC towers.  The mapping is    //
+//      defined in StEEmcA2EMaker::index()      //
+//////////////////////////////////////////////////
+
 Int_t StGammaTower::sector()
 {
-  // endcap tower
-  if ( ((Int_t)layer) < 4 )
-    {
-      return phibin() / 5;
-    }
-  else
-    return -1;
-
+    // Require an endcap tower
+    if( ((Int_t)layer) < 4) return phibin() / 5;
+    else return -1;
 }
 
 Int_t StGammaTower::subsector()
 {
-
-  // endcap tower
-  if ( ((Int_t)layer) < 4 )
-    {
-      return phibin() % 5;
-    }
-  else
-    return -1;
-
+  /// Require an endcap tower
+  if( ((Int_t)layer) < 4) return phibin() % 5;
+  else return -1;
 }
 
 Int_t StGammaTower::etabin()
 {
-  // endcap tower
-  if ( ((Int_t)layer) < 4 ) 
-    {
-      return id % 12;
-    }
-  else
-    return -1;
+    // Require an endcap tower
+    if( ((Int_t)layer) < 4) return id % 12;
+    else return -1;
 }
 
 Int_t StGammaTower::phibin()
 {
-  // endcap tower
-  if ( ((Int_t)layer) < 4 ) 
-    {
-      return id / 12;
-    }
-  else 
-    return -1;
+    // Require an endcap tower
+    if( ((Int_t)layer) < 4) return id / 12;
+    else return -1;
 }
 
 
