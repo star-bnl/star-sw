c2df4bdd1a3be1825f293cb69287931966565217
Michael Betancourt <betan@mit.edu>
2008-12-03 15:25:41 +0000

    Updated tree maker to utilize updates to the StGammaMakerCode

--- StRoot/StGammaMaker/macros/runGammaTreeMaker.C
+++ StRoot/StGammaMaker/macros/runGammaTreeMaker.C
@@ -1,172 +1,152 @@
-//-- switch should be commented out when analysing real data
-//#define MONTE_CARLO
-
-class StChain;
-class St_db_Maker;
-class StEEmcDbMaker;
-class StMuDstMaker;
-class StEEmcA2EMaker;
-
-//--
-//-- globals
-//--
-StChain            *mChain        = 0;
-St_db_Maker        *mStarDatabase = 0;
-StEEmcDbMaker      *mEEmcDatabase = 0;
-StMuDstMaker       *mMuDstMaker   = 0;
-StEEmcA2EMaker     *mEEanalysis   = 0;
-
-Int_t               count         = 0;
-Int_t               stat          = 0; 
-
-Int_t prescale = 1; 
-
-void runGammaTreeMaker( Int_t nevents = -1, 
-			Char_t *name = "/star/institutions/iucf/hew/2006ppLongRuns/7136022/st_physics_7136022_raw_1010001.MuDst.root",
-			Char_t *ofile = "st_physics_7136022_raw_1010001.gtree.root",
-			Char_t *path = "", 
-			Int_t nfiles = 100
-			)
+void runGammaTreeMaker
+(
+    char *inputFile = "root://rcas6132.rcf.bnl.gov:1095//home/starreco/reco/ppProductionLong/FullField/P06ie/2006/131/7131043/st_physics_7131043_raw_1020001.MuDst.root",
+    char *outputFile = "test.root"
+)
 {
- 
-  gROOT->Macro("StRoot/StGammaMaker/macros/loadGammaLibs.C"); 
-  TString pathname = path; 
-  pathname += name;
-  mChain = new StChain("chain");
+    
+    // Display file paths
+    cout << "****************************************" << endl;
+    cout << "Reading MuDst File     : " << inputFile << endl;
+    cout << "Writing Output File    : " << outputFile << endl;
+    
+    // Load libraries
+    cout << "Loading common libraries..." << endl;
 
-#ifdef MONTE_CARLO
-  TString gname=name;
-  if ( !gname.Contains("MuDst.root") )
-    {
-      std::cout << "We need to be running over a single MuDst for MC" << std::endl;
-      return;
-    }
-  gname.ReplaceAll("MuDst.root","geant.root");
-  StIOMaker* ioMaker = new StIOMaker;
-  ioMaker->SetFile(gname);
-  ioMaker->SetIOMode("r");
-  ioMaker->SetBranch("*",0,"0");             //deactivate all branches
-  ioMaker->SetBranch("geantBranch",0,"r");   //activate geant Branch
+    gROOT->Macro("loadMuDst.C");
+    gROOT->Macro("LoadLogger.C");
   
-  StMcEventMaker* mcEventMaker = new StMcEventMaker;
-  mcEventMaker->doPrintEventInfo = false;
-  mcEventMaker->doPrintMemoryInfo = false;
-#endif
+    gSystem->Load("StDetectorDbMaker");
+    gSystem->Load("StDbUtilities");
+    gSystem->Load("StDbBroker");
+    gSystem->Load("St_db_Maker");
+    gSystem->Load("StSpinDbMaker");
+    
+    gSystem->Load("libgeometry_Tables"); // BEMC Only
+    gSystem->Load("StDaqLib");           // BEMC Only
+    gSystem->Load("StEmcRawMaker");      // BEMC Only
+    gSystem->Load("StEmcADCtoEMaker");   // BEMC Only
+    
+    gSystem->Load("StEEmcDbMaker");      // EEMC Only
+    gSystem->Load("StEEmcUtil");         // EEMC Only
+    gSystem->Load("StEEmcA2EMaker");     // EEMC Only
+    gSystem->Load("StEEmcClusterMaker"); // EEMC Only
 
+    gSystem->Load("StMCAsymMaker");
+    gSystem->Load("StJetSkimEvent");
+    gSystem->Load("StGammaMaker");
+    
+    /////////////////////////////////////////////
+    //             Start up the chain          //
+    /////////////////////////////////////////////
+    
+    StChain *chain = new StChain("chain");
+    
+    StMuDstMaker *muDstMaker = new StMuDstMaker(0, 0, "", inputFile, "", 1e6, "MuDst");
 
+    St_db_Maker *StarDatabase = new St_db_Maker("StarDb", "MySQL:StarDb", "$STAR/StarDb");
 
-  mMuDstMaker = new StMuDstMaker(0,0,path,name,"MuDst",nfiles);
+    /*
+    /////////////////////////////////////////////
+    //        Filter Undesired Triggers        //
+    /////////////////////////////////////////////
 
-#if 0
-  mMuDstMaker->SetStatus("*",0);
-  mMuDstMaker->SetStatus("MuEvent",1);
-  mMuDstMaker->SetStatus("EmcAll",1);
-  mMuDstMaker->SetStatus("PrimaryTracks",1);
-#endif
+    // Choose your favorite triggers to keep here
 
-  //StMuDbReader *db = StMuDbReader::instance();
-  //StDetectorDbMaker *detdb = new StDetectorDbMaker();  
+    StTriggerFilterMaker* triggerFilter = new StTriggerFilterMaker();
+    triggerFilter->addTrigger(117001); // minbias
+    triggerFilter->addTrigger(137611); // bemcl2gamma
+    triggerFilter->addTrigger(137641); // eemcl2gamma
+    */
+    
+    /////////////////////////////////////////////
+    //                 Barrel                  //
+    /////////////////////////////////////////////
+    
+    // Only necessary if using the BEMC
+    
+    StEmcADCtoEMaker *bemcAdc2E = new StEmcADCtoEMaker(); // this will just convert what's in MuDst to ADC, use for data only!
+    bemcAdc2E->setPrint(false);
 
-  StTriggerFilterMaker *filterMaker = new StTriggerFilterMaker;
-  filterMaker->addTrigger(137641);
+    /////////////////////////////////////////////
+    //                 Endcap                  //
+    /////////////////////////////////////////////
+    
+    /// Only necessary if using the EEMC
+    
+    // Initialize EEMC database
+    StEEmcDbMaker *EEmcDatabase = new StEEmcDbMaker("eemcDb");
 
-  //  mStarDatabase = new St_db_Maker("StarDb", "MySQL:StarDb");
-  mStarDatabase = new St_db_Maker("StarDb", "MySQL:StarDb", "$STAR/StarDb");
+    // ADC to energy
+    StEEmcA2EMaker *EEanalysis = new StEEmcA2EMaker("mEEanalysis");
+    EEanalysis->database("eemcDb");    // sets db connection
+    EEanalysis->source("MuDst", 1);     // sets mudst as input
+    EEanalysis->threshold(3.0, 0);      // tower threshold (ped+N sigma)
+    EEanalysis->threshold(3.0, 1);      // pre1 threshold 
+    EEanalysis->threshold(3.0, 2);      // pre2 threshold
+    EEanalysis->threshold(3.0, 3);      // post threshold
+    EEanalysis->threshold(3.0, 4);      // smdu threshold
+    EEanalysis->threshold(3.0, 5);      // smdv threshold
 
-#ifdef MONTE_CARLO  
-  // Setup ideal gains for processing MC data
-  mStarDatabase->SetFlavor("sim","eemcPMTcal");
-  mStarDatabase->SetFlavor("sim","eemcPIXcal");
-  mStarDatabase->SetFlavor("sim","eemcPMTped");
-  mStarDatabase->SetFlavor("sim","eemcPMTstat");
-  mStarDatabase->SetFlavor("sim","eemcPMTname");
-  mStarDatabase->SetFlavor("sim","eemcADCconf");
-  //  mStarDatabase->SetDateTime(20050101,0);
-  mStarDatabase->SetDateTime(20060616, 173801);
-#endif
+    /////////////////////////////////////////////
+    //               GammaMaker                //
+    /////////////////////////////////////////////
 
-  // Initialize EEMC database
-  mEEmcDatabase = new StEEmcDbMaker("eemcDb");
+    // Possible values for the second argument,
+    //     kBemc (Use only the BEMC)
+    //     kEemc (Use only the EEMC)
+    //     kBoth (Use both detectors)
 
+    StGammaMaker *gammaMaker = new StGammaMaker("gammaMaker", StGammaMaker::kBoth, StGammaMaker::kData);
+    gammaMaker->setOutputFile(outputFile);
+    gammaMaker->storeEmptyEvents();
+    
+    gammaMaker->setClusterEtThreshold(5.1);
+    gammaMaker->setConeRadius(0.4);
+    gammaMaker->setSmdRange(20.0);
+    
+    gammaMaker->setSeedEnergyThreshold(3.6);
+    gammaMaker->setClusterEnergyThreshold(5.1);
+    
+    gammaMaker->setTrackEtRawThreshold(0.0);
+    gammaMaker->setTowerEtRawThreshold(0.0);
+    
+    gammaMaker->setEemcTowerClusterThreshold(0.1);
 
+    /////////////////////////////////////////////
+    //            Start up the chain           //
+    /////////////////////////////////////////////  
 
-#ifdef MONTE_CARLO
-  // endcap slow simulator
-  StEEmcSlowMaker *slowSim = new StEEmcSlowMaker("slowSim");
-#endif
- 
+    chain->Init();
+    chain->ls(3);
 
-  // ADC to energy
-  mEEanalysis=new StEEmcA2EMaker("mEEanalysis");
-  mEEanalysis->database("eemcDb");    // sets db connection
-  mEEanalysis->source("MuDst",1);     // sets mudst as input
-  mEEanalysis->threshold(3.0,0);      // tower threshold (ped+N sigma)
-  mEEanalysis->threshold(3.0,1);      // pre1 threshold 
-  mEEanalysis->threshold(3.0,2);      // pre2 threshold
-  mEEanalysis->threshold(3.0,3);      // post threshold
-  mEEanalysis->threshold(3.0,4);      // smdu threshold
-  mEEanalysis->threshold(3.0,5);      // smdv threshold
-#ifdef MONTE_CARLO
-  mEEanalysis->scale(1.2);                  // scale tower energies by x1.2
-#endif
+    int ntotal = 1e6;
 
-  // endcap clusters
-  StMyClusterMaker * EEclusters=new StMyClusterMaker("mEEclusters", mEEanalysis, mMuDstMaker );
-  EEclusters->setSeedEnergy(1.5);
-  EEclusters->setMinimumEnergy(0.5);
+    Int_t stat  = 0;
+    Int_t total = 0;
+    for(int iev = 0; iev < ntotal; ++iev)
+    {
 
+        cout << "********** Processing Event: " << iev << " **********" << endl;
 
+        chain->Clear();
+        stat = chain->Make();
+        
+        if(stat && stat != kStSKIP) 
+        {
+            cout << "Bad return code!" << endl;
+            break;
+        }
+        
+        ++total;
 
-  // Get BEMC calibration 
-#ifdef MONTE_CARLO
-  // Use this instead to "redo" conversion from GEANT to ADC
-  StEmcSimulatorMaker* emcSim = new StEmcSimulatorMaker;
-  emcSim->setCheckStatus(kBarrelEmcTowerId,false); //this returns hits regardless of offline tower status
-  emcSim->setCalibSpread(kBarrelEmcTowerId,0.15);//spread gains by 15%
-#else
-  // This will just convert what's in MuDst to ADC, use for data only!
-  StEmcADCtoEMaker* bemcAdc2E = new StEmcADCtoEMaker;
-  bemcAdc2E->setPrint(true);
-#endif
+    }
+    
+    chain->Finish(); 
+    
+    cout << "****************************************** " << endl;
+    cout << total << " Total Events" << endl;
+    cout << "****************************************** " << endl;
 
-
-
-#ifdef MONTE_CARLO
-  StMCAsymMaker* asym = new StMCAsymMaker;
-  StGammaPythiaEventMaker* pythia = new StGammaPythiaEventMaker;
-#endif
-
-  StGammaEventMaker* gemaker = new StGammaEventMaker;
-  StGammaRawMaker* raw = new StGammaRawMaker; 
-  StBarrelEmcClusterMaker* ecl = new StBarrelEmcClusterMaker;
-  StGammaCandidateMaker* gcm = new StGammaCandidateMaker;
-  gcm->SetCompressLevel(0); // 0=No compression, 1=Compress SMD, 2=Compress All
-
-#ifndef MONTE_CARLO
-  StSpinDbMaker* spinDb = new StSpinDbMaker;
-  StGammaSpinMaker* gspmaker = new StGammaSpinMaker;
-#endif
-
-  StGammaTreeMaker* gtm = new StGammaTreeMaker;
-  gtm->SetFilename(ofile);
-
-  mChain->ls(3);
-  mChain->Init();
-
-  //-----------------------------------------------------------------
-  //--
-  Int_t count = 0;    // event count
-  for (Int_t ev = 1; ev <= nevents || nevents == -1; ++ev) {
-
-    cout << "------------------------------------------------";
-    cout << "event=" << count++ << endl;
-
-    mChain -> Clear();
-    Int_t stat = mChain -> Make(ev);
-
-    if (stat % 10 == kStEOF || stat % 10 == kStFatal) break;
-
-  }
-  //--
-  //-----------------------------------------------------------------
 }
+
