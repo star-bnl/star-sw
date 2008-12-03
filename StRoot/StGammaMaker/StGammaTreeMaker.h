46aec5538e04cf05cb966714f97ec1b1e67c92de
Michael Betancourt <betan@mit.edu>
2008-12-03 15:39:18 +0000

    Added switch for storing empty events, cosmetic updates, and increased class def

--- StRoot/StGammaMaker/StGammaTreeMaker.h
+++ StRoot/StGammaMaker/StGammaTreeMaker.h
@@ -1,6 +1,18 @@
-#ifndef __StGammaTreeMaker_h__
-#define __StGammaTreeMaker_h__
+////////////////////////////////////////////////////////////
+//                                                        //
+//    StGammaTreeMaker                                    //
+//                                                        //
+//    Store StGammaEvents in a tree                       //
+//                                                        //
+//    Original concept and implementation by              //
+//    Jason  Webb (Valpo)                                 //
+//                                                        //
+////////////////////////////////////////////////////////////
 
+#ifndef STAR_StGammaTreeMaker
+#define STAR_StGammaTreeMaker
+
+#include <vector>
 #include "StMaker.h"
 #include "TString.h"
 
@@ -9,69 +21,74 @@ class TFile;
 class StGammaEvent;
 class StGammaTreeMaker;
 
-#include <vector>
-
-class StGammaTreeVersion : public TNamed
+class StGammaTreeVersion: public TNamed
 {
 
- public:
-  StGammaTreeVersion( const Char_t *name="version", const Char_t *title="versioning information for StGammaMaker chain"):TNamed(name,title){ /* nada */ };
-  ~StGammaTreeVersion(){ /* nada */ };
-
-  void print();
-
- private:
- protected:
-
-  std::vector<TString> mMakerTags;   
-  std::vector<TString> mStorageTags;
-
-  friend class StGammaTreeMaker;
-  
-  ClassDef(StGammaTreeVersion,1);
+    public:
+        StGammaTreeVersion(const char *name = "version", const char *title = "Versioning Info");
+        ~StGammaTreeVersion();
+        
+        void print();
+        
+    private:
+    protected:
+        
+        std::vector<TString> mMakerTags;   
+        std::vector<TString> mStorageTags;
+        
+        friend class StGammaTreeMaker;
+        
+        ClassDef(StGammaTreeVersion, 2);
 
 };
- 
-class StGammaTreeMaker : public StMaker
+
+class StGammaTreeMaker: public StMaker
 {
 
- public:
-  StGammaTreeMaker( const Char_t *name="gtmaker" );
-  ~StGammaTreeMaker(){ /* nada */ };
+    public:
+        
+        StGammaTreeMaker(const char *name = "mGammaTreeMaker" );
+        ~StGammaTreeMaker();
+        
+        virtual const char* GetCVS() const
+        {static const char cvs[] = "Tag $Name:  $ $Id: StGammaTreeMaker.h,v 1.5 2008/12/03 15:39:12 betan Exp $ built "__DATE__" "__TIME__; return cvs; }
+        
+        // Required Maker Methods
+        Int_t Init();
+        Int_t Make();
+        void  Clear(Option_t *opts="");
+        Int_t Finish();
+        
+        // Accessors
+        TFile *file() { return mGammaFile; }
+        TTree *tree() { return mGammaTree; }
+        StGammaEvent *event() { return mGammaEvent; }
+        
+        // Mutators
+        
+        /// Sets the output file for the TTree.  Defaults into .hist branch
+        void SetFile( TFile *file ) { mGammaFile = file; }
+        /// Sets a pointer to an existing TTree.  Default creates at Init().
+        void SetTree( TTree *tree ) { mGammaTree = tree; }
+        /// Sets the filename (unless a file has been passed above)
+        void SetFilename( const Char_t *fname ) { mFilename = fname; }
+        
+        void skipEmptyEvents() { mSkipEmpty = true; }
+        void storeEmptyEvents() { mSkipEmpty = false; }
 
-  Int_t Init();
-  Int_t Make();
-  void  Clear(Option_t *opts="");
-  Int_t Finish();
+    private:
 
-  /// Sets the output file for the TTree.  Defaults into .hist branch
-  void SetFile( TFile *file ){ mGammaFile = file; }
-  /// Sets a pointer to an existing TTree.  Default creates at Init().
-  void SetTree( TTree *tree ){ mGammaTree = tree; }
-  /// Sets the filename (unless a file has been passeed above)
-  void SetFilename( const Char_t *fname ){ mFilename=fname; }
+        bool mSkipEmpty;
 
-  TFile *file(){ return mGammaFile; }
-  TTree *tree(){ return mGammaTree; }
-
-  /// create a gamma event
-  StGammaEvent *event(){ return mGammaEvent; }
-
-  virtual const char* GetCVS() const
-    {static const char cvs[]="Tag $Name:  $ $Id: StGammaTreeMaker.h,v 1.4 2008/06/30 16:34:15 jwebb Exp $ built "__DATE__" "__TIME__; return cvs;}
-
- private:
- protected:
-
-  TTree *mGammaTree;
-  TFile *mGammaFile;
-
-  StGammaEvent *mGammaEvent;
-  TString mFilename;
-
-  StGammaTreeVersion mVersion; /// code versioning information
-
-  ClassDef(StGammaTreeMaker,1);
+        TTree *mGammaTree;
+        TFile *mGammaFile;
+        
+        StGammaEvent *mGammaEvent;
+        TString mFilename;
+        
+        StGammaTreeVersion mVersion;
+        
+        ClassDef(StGammaTreeMaker, 2);
 
 };
 
