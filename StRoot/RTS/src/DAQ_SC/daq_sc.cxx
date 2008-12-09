229c012f1cee9e4c279e611023bd045b828716d4
tonko <>
2008-12-09 10:00:03 +0000

    Added handle_raw and SFS based read

--- StRoot/RTS/src/DAQ_SC/daq_sc.cxx
+++ StRoot/RTS/src/DAQ_SC/daq_sc.cxx
@@ -3,7 +3,9 @@
 
 #include <rtsLog.h>
 #include <rtsSystems.h>
+#include <daqFormats.h>
 
+#include <SFS/sfs_index.h>
 #include <DAQ_READER/daqReader.h>
 #include <DAQ_READER/daq_dta.h>
 
@@ -41,13 +43,14 @@ daq_sc::daq_sc(daqReader *rts_caller) : daq_det(rts_caller)
 	// dname is ignored 
 	rts_id  = SC_ID ;
 	name = rts2name(rts_id) ;
-	sfs_name = "sc" ;	
+	sfs_name = "scd" ;	
 	
 	caller = rts_caller ;
 
 	if(caller) caller->insert(this, rts_id) ;
 
 	legacy = new daq_dta ;
+	raw = new daq_dta ;
 
 	LOG(DBG,"%s: constructor: caller %p",name,caller) ;
 }
@@ -57,6 +60,7 @@ daq_sc::~daq_sc()
 	LOG(DBG,"%s: destructor",name) ;
 
 	delete legacy ;
+	delete raw ;
 
 	return ;
 }
@@ -65,34 +69,128 @@ daq_sc::~daq_sc()
 daq_dta *daq_sc::get(const char *bank, int c1, int c2, int c3, void *p1, void *p2)
 {
 	Make() ;
-	LOG(NOTE,"SC get") ;
 	if(!present) return 0 ;
+
 
 	if(!bank || strcmp(bank,"*")==0) bank = "legacy" ;	// set default, if called with *
 
-	if(strcasecmp(bank,"legacy") != 0) {
-		LOG(ERR,"%s: unknown bank %s",name,bank) ;
-		return 0 ;
+	if(strcasecmp(bank,"legacy") == 0) {
+		return handle_legacy() ;
+	}
+	else if(strcasecmp(bank,"raw") == 0) {
+		return handle_raw() ;
 	}
 
-	return handle_legacy() ;
+	LOG(ERR,"%s: unknown bank %s",name,bank) ;
+	return 0 ;
+
 
 }
 
 
+daq_dta *daq_sc::handle_raw()
+{
+	int err = 0 ;
+	char *local_store = 0 ;
+	int ret ;
+	char str[256] ;
+	char *full_name ;
+	int bytes ;
+
+	if(!(present & DET_PRESENT_SFS)) return 0 ;	// no SFS -> no raw
+
+
+
+	sprintf(str,"%s",sfs_name) ;
+	full_name = caller->get_sfs_name(str) ;
+
+	if(!full_name) {
+		err = 1 ;
+		goto ret_error ;
+	}
+
+	bytes = caller->sfs->fileSize(full_name) ;
+	if(bytes <= 0) {
+		err = 2 ;
+		goto ret_error ;
+	}
+
+	raw->create(bytes,"sc_raw",rts_id,DAQ_DTA_STRUCT(char)) ;
+
+	local_store = (char *) raw->request(bytes) ;
+
+	ret = caller->sfs->read(str, local_store, bytes) ;
+	if(ret != bytes) {
+		err = 3 ;
+		goto ret_error ;
+	}
+
+
+	raw->finalize(bytes,0,0,0) ;
+	raw->rewind() ;
+
+	return raw ;
+
+	ret_error:;
+
+	LOG(ERR,"%s: handle_raw: error %d",name,err) ;
+	return 0 ;
+	
+}
+
 daq_dta *daq_sc::handle_legacy()
 {
+
+	int err = 0 ;
+
 
 	// I need one object of sc_t type but let the create decide on the necessary size
 	legacy->create(1,"sc_t",rts_id,DAQ_DTA_STRUCT(sc_t)) ;
 	
 
 	sc_t *sc_p = (sc_t *) legacy->request(1) ;	// need ONE sc_t object
- 
-	sc_reader(caller->mem, sc_p, m_Debug) ;
+
+
+	if(present & DET_PRESENT_DATAP) { 	
+		sc_reader(caller->mem, sc_p, m_Debug) ;
+	}
+	else {	// SFS
+		daq_dta *dd = handle_raw() ;
+
+		if(dd==0) {
+			err = 1 ;
+			goto ret_error ;
+		}
+
+		dd->iterate() ;	// do it once...
+
+
+		struct SCD *scd = (struct SCD *) dd->Void ;
+		
+		int swapit = 0 ;
+		if(scd->bh.byte_order != DAQ_RAW_FORMAT_ORDER) {	// swap
+			swapit = 1 ;
+		}
+
+
+		sc_p->time = qswap32(swapit,scd->time) ;
+
+		sc_p->mag_field = qswap32(swapit,scd->mag_field) ;
+
+		for(u_int i=0;i<(sizeof(sc_p->rich_scalers)/4);i++) {
+			sc_p->rich_scalers[i] = qswap32(swapit, scd->rich_scalers[i]) ;
+		}
+
+	}
 
 	legacy->finalize(1,0,0,0) ;	// 1 entry; sector 0, row 0, pad 0
 	legacy->rewind() ;
 
 	return legacy ;
+
+	ret_error :;
+
+	LOG(ERR,"%s: handle_legacy: error %d",name,err) ;
+
+	return 0 ;
 }
