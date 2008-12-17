79f22a77ef8ec3b9924864a9002f17f4e9a39d0a
fine <>
2008-12-17 23:28:58 +0000

    fix the StRtsTable descriptor

--- StRoot/StChain/StRtsTable.cxx
+++ StRoot/StChain/StRtsTable.cxx
@@ -1,19 +1,25 @@
 #include "StRtsTable.h"
-
-//________________________________________________________
-StRtsTable::StRtsTable(size_t structLength, Int_t n) : 
- TGenericTable(TTableDescriptor(1),n)
- {
-   TTableDescriptor *dsc = GetDescriptorPointer();
-   if (dsc) {
-      tableDescriptor_st &row = (*dsc)[0];
-//         row.fColumnName;
+class StRtsTableDescriptor : public TTableDescriptor {
+public: 
+     StRtsTableDescriptor(size_t structLength) : 
+     TTableDescriptor(1)
+     {
+         tableDescriptor_st &row = *GetTable();
          row.fSize = structLength; 
          row.fTypeSize= sizeof(unsigned char);
          row.fType=kUChar;
-      fSize = dsc->Sizeof();
+     }
+}; 
+//________________________________________________________
+StRtsTable::StRtsTable(size_t structLength, Int_t n) : 
+ TGenericTable(*(TTableDescriptor *)&StRtsTableDescriptor(structLength),1)
+ {
+   TTableDescriptor *dsc = GetDescriptorPointer();
+   if (dsc)  fSize = dsc->Sizeof();
+   if ( !dsc || (fSize != structLength) )  {
+      assert(0);
+      Warning("TGenericTable","Wrong table format");
    }
-   if ( !dsc || !fSize ) Warning("TGenericTable","Wrong table format");
    if (n > 0) Set(n);
    SetGenericType();
  }
