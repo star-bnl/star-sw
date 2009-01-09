a799d128ea0def8408e5842ccfa88e5c40680e14
cvs2git <cvs2git>
2009-01-09 19:43:48 +0000

    This commit was manufactured by cvs2git to create branch 'SL09c_SL5_2'.

--- StRoot/RTS/src/DAQ_SVT/svtReader.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SVT_READER_H_
-#define _SVT_READER_H_
-
-#include "daq_svt.h"
-DAQ_LEGACY_DECL(svt);
-
-#endif
--- StRoot/StEmcPool/StPi0Results/StPi0ResultsVersion.h
+++
@@ -1,6 +0,0 @@
-#ifndef StPi0Results_StPi0ResultsVersion_H
-#define StPi0Results_StPi0ResultsVersion_H
-
-#define STPI0RESULTS_VERSION 11
-
-#endif
