cc38f106e73672b8f4683d4d8a1a40b63dcb8a78
Thomas Ullrich <thomas.ullrich@bnl.gov>
2009-01-15 00:48:10 +0000

    mLeTeFlag changed to mFlag, tray(), module(), cell() now return int.

--- StRoot/StEvent/StBTofRawHit.cxx
+++ StRoot/StEvent/StBTofRawHit.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StBTofRawHit.cxx,v 2.1 2008/12/22 20:31:00 ullrich Exp $
+ * $Id: StBTofRawHit.cxx,v 2.2 2009/01/15 00:48:10 ullrich Exp $
  *
  * Author: Xin Dong, Nov 2008
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StBTofRawHit.cxx,v $
+ * Revision 2.2  2009/01/15 00:48:10  ullrich
+ * mLeTeFlag changed to mFlag, tray(), module(), cell() now return int.
+ *
  * Revision 2.1  2008/12/22 20:31:00  ullrich
  * Initial Revision.
  *
@@ -18,22 +21,22 @@
 
 #include "StBTofRawHit.h"
 
-static const char rcsid[] = "$Id: StBTofRawHit.cxx,v 2.1 2008/12/22 20:31:00 ullrich Exp $";
+static const char rcsid[] = "$Id: StBTofRawHit.cxx,v 2.2 2009/01/15 00:48:10 ullrich Exp $";
 
 ClassImp(StBTofRawHit)
 
 StBTofRawHit::StBTofRawHit()
 {
-    mLeTeFlag = 0;
+    mFlag = 0;
     mTray = 0;
     mChannel = 0;
     mTdc = 0;
  }
 
-StBTofRawHit::StBTofRawHit(unsigned char iflag, unsigned char tray,
+StBTofRawHit::StBTofRawHit(char iflag, unsigned char tray,
           	       unsigned char channel, unsigned int rawTdc)
 {
-    mLeTeFlag = iflag;
+    mFlag = iflag;
     mTray = tray;
     mChannel = channel;
     mTdc = rawTdc;
@@ -43,7 +46,7 @@ StBTofRawHit::~StBTofRawHit() { /* noop */ }
     
 int StBTofRawHit::operator==(const StBTofRawHit& p) const
 {
-    return (p.mLeTeFlag == mLeTeFlag &&
+    return (p.mFlag == mFlag &&
 	    p.mTray == mTray &&
             p.mChannel == mChannel &&
 	    p.mTdc == mTdc );
@@ -57,7 +60,7 @@ int StBTofRawHit::operator!=(const StBTofRawHit& p) const
 ostream&
 operator<<(ostream &os, const StBTofRawHit& hit)
 {
-    os << " Le/Te " << hit.leteFlag() << endl
+    os << " Flag " << hit.flag() << endl
        << " Tray " << hit.tray() << endl
        << " Channel " << hit.channel() << endl
        << " Tdc " << hit.tdc() << endl ;
