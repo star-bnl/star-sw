b6d7e516f4fbccfced7ae53d92bf8f2ddd74913a
Yuri Fisyak <fisyak@bnl.gov>
2009-01-26 15:17:06 +0000

    TMemStat => StMemStat

--- StRoot/StarRoot/StarRootLinkDef.h
+++ StRoot/StarRoot/StarRootLinkDef.h
@@ -1,7 +1,7 @@
 <<<#pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
-#pragma link C++ class TMemStat-;
+#pragma link C++ class StMemStat-;
 #pragma link C++ class TTreeHelper-;
 #pragma link C++ class THelixTrack-;
 #pragma link C++ class THack-;
