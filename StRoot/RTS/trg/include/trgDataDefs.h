48e7414e79bdb761466bb3332816186349508922
nelson <>
2009-01-30 18:03:13 +0000

    Updated version of trgDataDefs for 2009 run

--- StRoot/RTS/trg/include/trgDataDefs.h
+++ StRoot/RTS/trg/include/trgDataDefs.h
@@ -4,13 +4,12 @@
 *
 *     Layout of new Trigger Data Block
 *
-*     J.M. Nelson        November 2008
+*     J.M. Nelson        30 January 2009
 *
-*     Notes:  This is illustrative only since the definition of the
-*     Event Descriptor will change once the new TCU has been commissioned.
-*     Similarly, the data from Level 1 (L1_DSM_Data) will be radically
-*     different.   The amount of trigger summary data is also likely to
-*     change.   
+*     Notes:  The event descriptor will describe data from either
+*     the Mk1 or Mk2 TCUs.   The variable TCU_Mark will be 1 for Mk1
+*     and 2 for the Mk2 TCU.  Variables not used by one or other of the
+*     TCUs will be zero.   
 *
 *     The data block structure will always begin with a 4 character
 *     name, followed by the byte-count of data following.  The structure of
@@ -19,11 +18,10 @@
 *     Note:  PrePost data will only be available on local trigger disks and
 *     will not be present in event files.
 ******************************************************************************/
-#define FORMAT_VERSION        0x08121140      /* 11 Dec 2008; Version 4.0 Format: yymmddvv */
-#define MAX_TRG_BLK_SIZE          100000      /* Estimated at 100k bytes including pre/post */
+#define FORMAT_VERSION        0x09013040      /* 30 Jan 2009; Version 4.0 Format: yymmddvv */
+#define MAX_TRG_BLK_SIZE          122880      /* Current total: 113.25k bytes for pre/post non-zero suppressed data.  Allow 120k */
 #define MAX_OFFLEN                    20      /* Depends on the number of crates in the system */
 
-#define ADD_BIT_PILEUP         0              /* Contamination/Pileup bit in event descriptor add-bits */
 #define ADD_BIT_FORCE          5              /* Force store of this event */
 #define ADD_BIT_L2_5           6              /* Level 2.5 abort */
 #define ADD_BIT_SIM            7              /* Simulated event - used by DAQ */
@@ -32,7 +30,7 @@
     
 typedef struct {
   char           name[3];                     /* Contains  EVD */
-  char           TrgDataFmtVer;               /* Exception for use by DAQ */
+  char           TrgDataFmtVer;               /* Exception for use by DAQ (LS byte of FORMAT_VERSION) */
   int            length;                      /* Byte count of data that follows */
   unsigned int   bunchXing_hi;
   unsigned int   bunchXing_lo;                /* Two parts of RHIC bunch crossing number */
@@ -40,17 +38,17 @@ typedef struct {
   unsigned char  actionWdTrgCommand;          /* from Fifo 1 */
   unsigned char  actionWdDaqCommand;          /* from Fifo 1 */  
   unsigned short TrgToken;                    /* from Fifo 2 */
-  unsigned short addBits;                     /* from Fifo 2 - bit 0=Contamination; bit 6=L2.5 abort; bit 7=1 is fake data */
-  unsigned short DSMInput;                    /* from Fifo 3 */
-  unsigned short externalBusy;                /* from Fifo 3 */
-  unsigned short modifiedBusyStatus;          /* from Fifo 4 */
-  unsigned short physicsWord;                 /* from Fifo 4 */
-  unsigned short TriggerWord;                 /* from Fifo 5 */
-  unsigned short DSMAddress;                  /* from Fifo 6 */
-  unsigned short contaminationBusyStatus;     /* from Fifo 6 */
+  unsigned short addBits;                     /* used by trigger/daq: bit 5=Force store; bit 6=L2.5 abort; bit 7=1 is fake data */
+  unsigned short DSMInput;                    /* only for use with Mk1 TCU.  0 if Mk2 TCU is used */
+  unsigned short externalBusy;                /* from Fifo 9 (Fifo 3 Mk1 TCU) */
+  unsigned short internalBusy;                /* from Fifo 9 (Mk2 TCU) */
+  unsigned short physicsWord;                 /* Fifo 4 Mk1 TCU. 0 if Mk2 TCU is used */
+  unsigned short TriggerWord;                 /* Fifo 5 Mk1 TCU. 0 if Mk2 TCU is used */
+  unsigned short DSMAddress;                  /* from Fifo 10 (Fifo 6 Mk1 TCU) */
+  unsigned short TCU_Mark;                    /* TCU_Mark Mk1=1 Mk2=2 */
   unsigned short npre;                        /* pre value for detector raw data */
   unsigned short npost;                       /* post value for detector raw data */
-  unsigned short dummy;                       /* dummy - filler since this structures must be zero (mod 4) */
+  unsigned short res1;                        /* Reserved for future use */
 } EvtDescData;
 
       /* L1 DSM data structures */
@@ -76,7 +74,7 @@ typedef struct {
   unsigned int   L2Sum[2];                    /* L2 Summary */
   unsigned int   L1Result[32];                /* Result from L1 CPU */
   unsigned int   L2Result[64];                /* Result from L2 CPU */
-  unsigned int   CResult[64];                 /* Result from last algorithm */
+  unsigned int   C2Result[64];                /* Result from last algorithm */
 } TrgSumData;
 
 typedef struct {
@@ -90,16 +88,16 @@ typedef struct {
   int length;                                 /* Byte count of data that follows */
   unsigned short BBClayer1[16];               /* This is the layer1 DSM that feeds the VTX DSM */
   unsigned short ZDClayer1[8];                /* This is the new layer1 ZDC DSM that also feeds the VTX DSM */
-  unsigned short VPDlayer1[8];                /* ADC & TAC values for VPD detectors from QT boards */
+  unsigned short VPD[8];                      /* ADC & TAC values for VPD detectors*/
 } BBCBlock;
 
 typedef struct {
   char name[4];                               /* Contains MIX */
   int length;                                 /* Byte count of data that follows */
-  unsigned short FPDEastNSlayer1[8];          /* FPD east north/south layer 1  */  
-  char           MTD_P2Player1[16];           /* Data from MTD and PP2PP QT Boards */
-  unsigned short TOFlayer1[8];                /* This is TOF layer 1 formerly the course pixel array CPA */
-  unsigned short TOF[48];                     /* TOF data hit multiplicity */
+  unsigned short FPDEastNSLayer1[8];          /* FPD east north/south layer 1  */  
+  unsigned char  MTD_P2PLayer1[16];           /* Data from MTD and PP2PP */
+  unsigned short TOFLayer1[8];                /* This is TOF Layer 1 */
+  unsigned short TOF[48];                     /* TOF data */
 } MIXBlock;
 
 typedef struct  {
@@ -148,7 +146,7 @@ typedef struct {
   TrgOfflen L1_DSM_ofl;                       /* Offset/length pair to L1 DSM Data */
   TrgOfflen Summary_ofl;                      /* Offset/length pair to Summary Data */
   TrgOfflen MainX[MAX_OFFLEN];                /* Offset/length pairs for main crossing */
-  int PrePostList[10];                        /* Offsets to offset/length pairs to (5) Pre and (5) Post crossing */
+  int PrePostList[10];                        /* Offsets to offset/length pairs to Pre and Post crossing */
   int raw_data[MAX_TRG_BLK_SIZE/4];           /* Storage for raw data */
 } TriggerDataBlk;
   
