3835d39156391c4595fe2a61d91a1dc267938b2d
cvs2git <cvs2git>
2009-02-05 15:29:47 +0000

    This commit was manufactured by cvs2git to create tag 'LastQt3'.

--- StRoot/StiSvt/StiSvtLayerLadder.h
+++
@@ -1,7 +0,0 @@
-#ifndef StiSvtLayerLadder_h
-// Define conversion Svt <=> Sti numerations for Svt Layer (1-6) & Ladder (1-??)
-#define StiSvtLayerLadder_h
-#define getSvtBarrel(svtLayer)         (((svtLayer)-1)/2+1)
-#define getLayer(svtLayer)             ((svtLayer)-1)
-#define getLadder(svtLayer,svtLadder)  (((svtLadder)-1)/2)
-#endif
