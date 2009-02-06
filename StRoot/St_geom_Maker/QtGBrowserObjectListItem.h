67d7a50a96a38ba149ad20988b4d8c19ee152341
Valeri Fine <val.fine@gmail.com>
2009-02-06 00:06:15 +0000

    Qt4 version of the geombrowser

--- StRoot/St_geom_Maker/QtGBrowserObjectListItem.h
+++
@@ -1,8 +0,0 @@
-#ifndef EXAMPLE_QTGBROWSEROBJECTLISTITEM
-#define EXAMPLE_QTGBROWSEROBJECTLISTITEM
-#ifdef QT_EXTENSION
-#  include "TQtObjectListItem.h"
-#else
-#  include "QExObjectListItem.h"
-#endif
-#endif
