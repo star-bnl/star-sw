66393e80339d3d8f9f133000af209a45aa643856
fine <>
2009-02-19 01:47:09 +0000

    remove the second definition of the tof_t C-structure

--- StRoot/RTS/src/DAQ_TOF/tofReader.h
+++ StRoot/RTS/src/DAQ_TOF/tofReader.h
@@ -1,26 +1,8 @@
 #ifndef _TOF_READER_H_
 #define _TOF_READER_H_
 
-#include <sys/types.h>
+#include "daq_tof.h"
 
-
-struct tof_t {
-	int mode ;
-	int channels ;
-	int max_channels ;
-
-	u_short adc[180] ;	// was 48 in FY02
-	u_short tdc[184];	// was 48 in FY02
-	float   a2d[32];
-	u_int   sca[12];
-
-	// new in FY05
-	u_int ddl[4][10000] ;	// content of up to 4 fibers
-	u_int ddl_words[4] ;	// the count of words (32bit) for above
-} ;
-
-extern struct tof_t tof ;
-
-extern int tofReader(char *mem) ;
+DAQ_LEGACY_DECL(tof);
 
 #endif
