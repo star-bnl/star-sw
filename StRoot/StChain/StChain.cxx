534106d20cb40f49429c3a86f14e27ca5c79a217
perev <>
2009-03-16 21:52:24 +0000

    TMemStat & StMemStat handling improved

--- StRoot/StChain/StChain.cxx
+++ StRoot/StChain/StChain.cxx
@@ -41,7 +41,12 @@
 #include "StChain.h"
 #include "StEvtHddr.h"
 #include "StMessMgr.h"
+
+#ifdef TMEMSTATinSTAR
+#include "TMemStat.h"
+#else
 #include "StMemStat.h"
+#endif
 
 ClassImp(StChain)
 
@@ -259,8 +264,11 @@ Int_t StChain::EventLoop(Int_t jBeg,Int_t jEnd, StMaker *outMk)
 }
 
 
-// $Id: StChain.cxx,v 1.70 2009/01/26 14:32:33 fisyak Exp $
+// $Id: StChain.cxx,v 1.71 2009/03/16 21:52:24 perev Exp $
 // $Log: StChain.cxx,v $
+// Revision 1.71  2009/03/16 21:52:24  perev
+// TMemStat & StMemStat handling improved
+//
 // Revision 1.70  2009/01/26 14:32:33  fisyak
 // rename TMemStat => StMemStat due clash with ROOT class
 //
