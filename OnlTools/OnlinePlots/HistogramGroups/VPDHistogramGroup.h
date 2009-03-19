94b13539a3bbb2890fa2c3769cf12e798c69055c
dkettler <>
2009-03-19 22:50:30 +0000

    VPD Code Updates

--- OnlTools/OnlinePlots/HistogramGroups/VPDHistogramGroup.h
+++ OnlTools/OnlinePlots/HistogramGroups/VPDHistogramGroup.h
@@ -5,12 +5,12 @@
 #include "Rtypes.h"
 #include "HistogramGroup.h"
 
-
 class VPDHistogramGroup : public HistogramGroup {
 
 public:
   VPDHistogramGroup();
   VPDHistogramGroup(const char* group, const char* subGroup="vpd", const char* trigger="any", const char* detector="vpd");
+
   ~VPDHistogramGroup();
 
   virtual void reset();
@@ -20,6 +20,7 @@ public:
 private:
 
   TH2* h_vpd_cdb[4];                // ADC spectra of the 4 CDBs
+
   TH2* h_vpd_tac_east_vs_tac_west;  // east vs. west TAC value
   TH2* h_vpd_vertex_vs_l3_vertex;   // TAC difference vs. L3 vertex z-position
 
