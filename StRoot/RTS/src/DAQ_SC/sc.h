7df687f284c4085e7bcda996247672b2d0130345
fine <>
2009-03-27 22:59:53 +0000

    Add the dedicated header file to define the C-structure alone

---
+++ StRoot/RTS/src/DAQ_SC/sc.h
@@ -0,0 +1,9 @@
+#ifndef STAR_DAQ_SC_H_
+#define STAR_DAQ_SC_H_
+
+#ifndef DAQ_SC_DATA_STRUCTURE
+#  define DAQ_SC_DATA_STRUCTURE
+#  include "daq_sc.h"
+#  undef  DAQ_SC_DATA_STRUCTURE
+#endif   //DAQ_SC_DATA_STRUCTURE 
+#endif	// STAR_DAQ_SC_H_
