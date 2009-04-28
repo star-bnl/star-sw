d24933e2d0844220310722af78497dc0e2493b72
cvs2git <cvs2git>
2009-04-28 02:38:08 +0000

    This commit was manufactured by cvs2git to create tag 'tai_20100217'.

--- StRoot/StEmcPool/StPi0DataMaker/macros/config_pythia_dAu2003_HT2.C
+++
@@ -1,6 +0,0 @@
-// Configuration to run pi0 reconstruction chain on the pp2005 PYTHIA simulated data - dAu2003 HighTower-2 timestamps.
-{
-    gROOT->Macro("config_pythia_dAu2003.C"); // load dAu2003 simulation configuration
-
-    dataAnalysisSettings.timestampsFilename = "dAu2003Times_HT2.txt";
-}
