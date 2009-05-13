8be1359b2251a09a3c0a666ef17ffdb66d9dccd2
fine <>
2009-05-13 23:18:12 +0000

    Adjust TxEventLogWeb module

--- StRoot/StUCMApi/logging/TxEventLogWeb.cpp
+++ StRoot/StUCMApi/logging/TxEventLogWeb.cpp
@@ -2,7 +2,7 @@
  * @file TxEventLogFile.cpp
  * @author Valeri Fine
  *
- * @(#)cpp/api:$Id: TxEventLogWeb.cpp,v 1.1 2009/05/08 14:48:33 fine Exp $
+ * @(#)cpp/api:$Id: TxEventLogWeb.cpp,v 1.2 2009/05/13 23:18:12 fine Exp $
  *
  * Please see TxEventLogFile.h for more documentation.
  *****************************************************************/
@@ -15,7 +15,17 @@ void TxLogging::TxEventLogWeb::writeDown(const std::string& message)
 {
   std::string httpstring="wget -b  -q -o /dev/null ";
   httpstring+= "-O /dev/null \'http://connery.star.bnl.gov/ucm/?m=";
-  httpstring+=message;
+
+  std::string qmessage = message;
+  std::string searchString( "'" ); 
+  std::string replaceString( "%27" );
+
+  std::string::size_type pos = 0;
+  while ( (pos = qmessage.find(searchString, pos)) != std::string::npos ) {
+        qmessage.replace( pos, searchString.size(), replaceString );
+        pos++;
+  }
+  httpstring+=qmessage;
   httpstring+="\'";
   system( httpstring.c_str());
 }
