9f91d0d9e1e06e9473fe302f8a3199d7151d867b
Valeri Fine <val.fine@gmail.com>
2009-06-01 16:05:43 +0000

    Activate MuDST I/O output error is fatal flag

--- StRoot/StarRoot/THack.h
+++ StRoot/StarRoot/THack.h
@@ -23,6 +23,6 @@ static void PadRefresh(TPad *pad,int flag=0);
 static void HistRelease(TDirectory *dir);
 static int  LineToD(const char *line, const char **lend,
 		    int nItems, double *Items, TString *Names=0);
-static bool IsTreeWritable(const TTree *tree, bool fatal=false);
+static bool IsTreeWritable(const TTree *tree, bool fatal=true);
 };
 #endif //ROOT_THack
