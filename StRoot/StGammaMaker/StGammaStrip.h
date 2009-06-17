0f3482f8d822415e4615447e9d4513389a082e92
betan <>
2009-06-17 06:19:14 +0000

    Added left/right data members

--- StRoot/StGammaMaker/StGammaStrip.h
+++ StRoot/StGammaMaker/StGammaStrip.h
@@ -26,24 +26,31 @@ class StGammaStrip: public TObject
         ~StGammaStrip() {};
         
         virtual const char* GetCVS() const
-        {static const char cvs[] = "Tag $Name:  $ $Id: StGammaStrip.h,v 1.6 2008/12/03 15:38:15 betan Exp $ built "__DATE__" "__TIME__; return cvs; }
+        {static const char cvs[] = "Tag $Name:  $ $Id: StGammaStrip.h,v 1.7 2009/06/17 06:19:14 betan Exp $ built "__DATE__" "__TIME__; return cvs; }
         
         Int_t index;      // index of strip in plane
         Int_t sector;     // or bemc module
         Int_t plane;      // 0=esmd-u 1=esmdv 10=bsmd-eta 11=bsmd-phi
         Float_t energy;   // energy deposited
-        Float_t position; // Reference position for the calculation of moments
-                          // bsmd-eta -> theta
-                          // bsmd-phi -> phi
-                          // esmd-u -> u index
-                          // esmd-v -> v index
         Int_t stat;       // status bits (non fatal HW problems)
         Int_t fail;       // fail bits (fatal HW problems)
-        
+
+        // Strip geometry for shower shape analyses
+        // In the BEMC:
+        //   Eta Strips - Location in z (cm)
+        //   Phi Strips - Location in phi (radians)
+        // In the EEMC:
+        //   left/right not used
+        //   U Strips - position -> U Index
+        //   V Strips - position -> V Index
+        Float_t position;
+        Float_t left;
+        Float_t right;
+
         TRefArray candidates; // referencing candidates
         void print();
 
-  ClassDef(StGammaStrip, 2);
+  ClassDef(StGammaStrip, 3);
   
 };
 
