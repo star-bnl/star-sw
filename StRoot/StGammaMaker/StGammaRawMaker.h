50eb3d46baae5ee3c3597e9a952a12145fc8e183
betan <>
2009-06-18 05:53:05 +0000

    Allow BSMD strips to be added externally

--- StRoot/StGammaMaker/StGammaRawMaker.h
+++ StRoot/StGammaMaker/StGammaRawMaker.h
@@ -36,7 +36,7 @@ class StGammaRawMaker: public StMaker
         ~StGammaRawMaker();
         
         virtual const char* GetCVS() const
-        {static const char cvs[] = "Tag $Name:  $ $Id: StGammaRawMaker.h,v 1.5 2009/06/17 06:20:17 betan Exp $ built "__DATE__" "__TIME__; return cvs; }
+        {static const char cvs[] = "Tag $Name:  $ $Id: StGammaRawMaker.h,v 1.6 2009/06/18 05:52:56 betan Exp $ built "__DATE__" "__TIME__; return cvs; }
         
         // Required Maker Methods
         Int_t Init();
@@ -54,13 +54,16 @@ class StGammaRawMaker: public StMaker
 
         StGammaTower *tower(Int_t id, Int_t layer);
         StGammaStrip *strip(Int_t sector, Int_t plane, Int_t index);
-        
+
         // Mutators
         void SetTowerCutoff( Float_t t );        
         void SetTrackCutoff( Float_t t );
         
         void useBemc() { mUseBemc = true; }
         void useEemc() { mUseEemc = true; }
+
+        void AddEtaStrip(StGammaStrip *strip);
+        void AddPhiStrip(StGammaStrip *strip);
 
     protected:
 
