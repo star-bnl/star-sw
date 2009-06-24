89198bed78979cec4c8f5f57361cd7abc11c33b4
mattheww <>
2009-06-24 12:36:01 +0000

    updated macro

--- StRoot/StSpinPool/StLuminosityMaker/macros/RunLuminosityMaker.C
+++ StRoot/StSpinPool/StLuminosityMaker/macros/RunLuminosityMaker.C
@@ -40,23 +40,33 @@ void RunLuminosityMaker(int nevents=10,
     //Database
     St_db_Maker *dbMk = new St_db_Maker("StarDb", "MySQL:StarDb");
 
-
+    /*
     StTriggerFilterMaker* trigfilt = new StTriggerFilterMaker();
     trigfilt->addTrigger(96011);
     trigfilt->addTrigger(96201);
     trigfilt->addTrigger(96211);
     trigfilt->addTrigger(96221);
     trigfilt->addTrigger(96233);
+    */
 
     //Block for TriggerSimuMaker
-    /*
+    
     StEmcADCtoEMaker *adc = new StEmcADCtoEMaker();
 
     StTriggerSimuMaker* trigsim = new StTriggerSimuMaker();
     trigsim->useBbc();
     trigsim->useBemc();
     trigsim->bemc->setConfig(StBemcTriggerSimu::kOffline);
+    /*
+    StGenericL2Emulator* simL2Mk = new StL2_2006EmulatorMaker;
+    assert(simL2Mk);
+    simL2Mk->setSetupPath("./StRoot/StJetMaker/StarTrigSimuSetup/");
+    char outPath[200];
+    sprintf(outPath,"./out/");   
+    simL2Mk->setOutPath(outPath);
+    trigsim->useL2(simL2Mk);
     */
+    
     //end TriggerSimuMaker block
  
     StLuminosityMaker* lum = new StLuminosityMaker();
@@ -69,18 +79,24 @@ void RunLuminosityMaker(int nevents=10,
     lum->addTrigger(96211);
     lum->addTrigger(96233);*/
     lum->setCrossSectionNB(26.1e6);
-    lum->getTriggersFromFilterMaker();
+    //lum->getTriggersFromFilterMaker();
     /*
     lum->setMode("pp2006");//see StLuminosityMaker.cxx for accepted modes
-    lum->setVertexCutcm(60);//set a vertex cut in cm
+    */
     lum->addTrigger(117001);//mb..this trigger must be added first
+    lum->addTrigger(137221);//bemc-jp1-mb
     lum->addTrigger(137222);//bemc-jp1-mb
     lum->addTrigger(137822);//bemc-http-mb-fast
-    lum->addTrigger(117200);//zb
+    lum->addTrigger(117300);//zb
+    lum->addTrigger(137571);//bemc-jp1
+    lum->addTrigger(137575);//bemc-jp0-etot
+    lum->addTrigger(137585);//bemc-jp2
     lum->addTrigger(137586);//bemc-http
+    lum->addTrigger(137611);//bemc-http-mb-l2gamma
     lum->addTrigger(137501);//bemc-jp0-mb
-    lum->addTrigger(137461);//fpd1-tpcdead-fast
-    */
+    lum->addTrigger(137622);//bemc-jp0-etot-mb-L2jet
+    //lum->addTrigger(137461);//fpd1-tpcdead-fast
+
     chain->Init();
     
     chain->PrintInfo();
@@ -109,18 +125,28 @@ void RunLuminosityMaker(int nevents=10,
     
     //vector<StLuminosityHolder> holder;
     TClonesArray* holder = lum->getHolder();
-    cout<<"Got holder"<<endl;
     StLuminosityHolder* dumlum;
-    TFile outfile("lumtest.root","RECREATE");
-    cout<<"dumlum and file created"<<endl;
-    cout<<holder->GetEntries()<<endl;
+
     for(int i = 0; i < holder->GetEntries(); i++){
+      char name[100];
       dumlum = (StLuminosityHolder*)holder->At(i);
-      cout<<"Got dumlum"<<endl;
-      cout<<dumlum->getRunNumber()<<endl;
-      //dumlum.Write();
+      int run1 = dumlum->getRunNumber();
+      sprintf(name,"lum%i.data",run1);
+      ofstream lumout(name);
+      vector<unsigned int> triggers = dumlum->getTriggers();
+      vector<unsigned int> NTotal = dumlum->getNTotal();
+      vector<unsigned int> NCuts = dumlum->getNCuts();
+      vector<unsigned int> NVert = dumlum->getNVertex();
+      vector<unsigned int> NTrig = dumlum->getNSoftTrig();
+      vector<float> LumTotal = dumlum->getLumTotal();
+      vector<float> LumCuts = dumlum->getLumCuts();
+      vector<float> LumVert = dumlum->getLumVertex();
+      vector<float> LumTrig = dumlum->getLumSoftTrig();
+      vector<float> Prescales = dumlum->getPrescales();
+      for(unsigned int j = 0; j < dumlum->getTriggers().size(); j++){
+	lumout<<run1<<"\t"<<triggers[j]<<"\t"<<Prescales[j]<<"\t"<<NTotal[j]<<"\t"<<NCuts[j]<<"\t"<<NVert[j]<<"\t"<<NTrig[j]<<"\t"<<LumTotal[j]<<"\t"<<LumCuts[j]<<"\t"<<LumVert[j]<<"\t"<<LumTrig[j]<<endl;
+      }
+      lumout.close();
     }
-    holder->Write();
-    outfile.Close();
     
 }
