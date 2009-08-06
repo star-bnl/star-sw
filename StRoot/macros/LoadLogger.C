a97ab12f26e732828d8fe7bf79b8a64673754124
Valeri Fine <val.fine@gmail.com>
2009-08-06 19:17:49 +0000

    remove the deprected StUCMAPi loading

--- StRoot/macros/LoadLogger.C
+++ StRoot/macros/LoadLogger.C
@@ -4,11 +4,6 @@ void LoadLogger() {
    //  gSystem->Load("logger/lib/liblog4cxx.so");
   gSystem->Load("liblog4cxx.so");
   gSystem->Load("St_base.so");
-  if (gSystem->DynamicPathName("StUCMApi.so",kTRUE)) {
-      gSystem->Load("StUCMApi.so");
-  }
-//  gSystem->Load("data");
-//  gSystem->Load("api");
   gSystem->Load("StStarLogger.so");
   StLoggerManager::StarLoggerInit();
 }
