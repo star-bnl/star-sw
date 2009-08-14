1115202feb88b9006a81d36dc168149feca5986d
Pibero Djawotho <pibero@tamu.edu>
2009-08-14 14:58:50 +0000

    Removed unused arguments to StPythiaFourPMaker constructor.

--- StRoot/StJetMaker/StJetSimuUtil/macros/RunJetSimuSkimFinder.C
+++ StRoot/StJetMaker/StJetSimuUtil/macros/RunJetSimuSkimFinder.C
@@ -1,15 +1,12 @@
 // NOTE - This macro is ONLY for running MC simulation data!!
 //=========================================================================================
-class  StChain *chain;
-int total=0;
-#include <string>
 
-void RunJetSimuSkimFinder(const int nevents = 100,
-			  const char *file="/star/data32/reco/pp200/pythia6_205/above_35gev/cdf_a/y2004y/gheisha_on/p05ih/rcf1230_11_4000evts.MuDst.root",
-			  const char *fname="/star/data32/reco/pp200/pythia6_205/above_35gev/cdf_a/y2004y/gheisha_on/p05ih/rcf1230_11_4000evts.geant.root",
-			  const char *outfile="Jets_pt35_11.root",
-			  const char *skimFile="Skim_pt35_11.root"){
-
+void RunJetSimuSkimFinder(const int nevents = 2000,
+                          const char* file = "/star/data13/simu/pp200/pythia6_410/minbias/cdf_a/y2006c/gheisha_on/p07ic/rcf1319_01_1688evts.MuDst.root",
+                          const char* fname = "/star/data13/simu/pp200/pythia6_410/minbias/cdf_a/y2006c/gheisha_on/p07ic/rcf1319_01_1688evts.geant.root",
+			  const char* outfile = "Jets_minbias_01.root",
+			  const char* skimFile = "Skim_minbias_01.root")
+{
   gROOT->LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
   loadSharedLibraries();
   gSystem->Load("StTpcDb");
@@ -32,24 +29,25 @@ void RunJetSimuSkimFinder(const int nevents = 100,
   gSystem->Load("StEmcTriggerMaker");
   gSystem->Load("StTriggerUtilities");
   gSystem->Load("StMCAsymMaker");
-  gSystem->Load("StJetFinder");    
+  gSystem->Load("StJetFinder");
+  gSystem->Load("StJetSkimEvent");
+  gSystem->Load("StJets");
   gSystem->Load("StJetMaker");
 
-  double pi = atan(1.0)*4.0;
   cout << " loading done " << endl;
   
-  chain= new StChain("StChain"); 
+  StChain* chain = new StChain; 
   chain->SetDebug(1);
   gMessMgr->SwitchOff("D");
   gMessMgr->SwitchOff("I");
   
-  StIOMaker* ioMaker = new StIOMaker();
+  StIOMaker* ioMaker = new StIOMaker;
   ioMaker->SetFile(fname);
   ioMaker->SetIOMode("r");
   ioMaker->SetBranch("*",0,"0");             //deactivate all branches
   ioMaker->SetBranch("geantBranch",0,"r");   //activate geant Branch
   
-  class StMcEventMaker *mcEventMaker = new StMcEventMaker();
+  StMcEventMaker *mcEventMaker = new StMcEventMaker;
   mcEventMaker->doPrintEventInfo = false;
   mcEventMaker->doPrintMemoryInfo = false;
   
@@ -59,41 +57,43 @@ void RunJetSimuSkimFinder(const int nevents = 100,
   
   //Database -- get a real calibration (this is ok, MLM)
   St_db_Maker* dbMk = new St_db_Maker("StarDb","MySQL:StarDb","MySQL:StarDb","$STAR/StarDb");
-  dbMk->SetDateTime(20050506,214129 ); // for simulation
+  //dbMk->SetDateTime(20050506,214129); // for simulation
+  dbMk->SetDateTime(20060516,110349); // Run 7136022
    
   //Database interface
-  StDetectorDbMaker* detDbMk = new StDetectorDbMaker();
+  StDetectorDbMaker* detDbMk = new StDetectorDbMaker;
   
   //Endcap DB
   StEEmcDbMaker* eemcb = new StEEmcDbMaker("eemcDb");
   
   //get BEMC calibration
-  StEmcSimulatorMaker* emcSim = new StEmcSimulatorMaker(); //use this instead to "redo" converstion from geant->adc
-  emcSim->setCalibSpread(kBarrelEmcTowerId, 0.15);
-  StPreEclMaker* preEcl = new StPreEclMaker(); //need this to fill new StEvent information
+  StEmcSimulatorMaker* emcSim = new StEmcSimulatorMaker; //use this instead to "redo" converstion from geant->adc
+  emcSim->setCalibSpread(kBarrelEmcTowerId,0.15);
+  StPreEclMaker* preEcl = new StPreEclMaker; //need this to fill new StEvent information
   
   //get trigger
-  StEmcTriggerMaker *emcTrig = new StEmcTriggerMaker("bemctrigger");
+  StEmcTriggerMaker* emcTrig = new StEmcTriggerMaker("bemctrigger");
 
   //get pythia record
-  StMCAsymMaker *asym=new StMCAsymMaker("MCAsym");
+  StMCAsymMaker* asym = new StMCAsymMaker("MCAsym");
 
   //get skimMaker
-  StJetSkimEventMaker* skimEventMaker = new StJetSkimEventMaker("StJetSkimEventMaker",muDstMaker, skimFile);
-  skimEventMaker->addSimuTrigger(127501);
-  skimEventMaker->addSimuTrigger(137501);
-  skimEventMaker->addSimuTrigger(137213);  
-  skimEventMaker->addSimuTrigger(127221);
+  StJetSkimEventMaker* skimEventMaker = new StJetSkimEventMaker("StJetSkimEventMaker",muDstMaker,skimFile);
+  //skimEventMaker->addSimuTrigger(127501);
+  //skimEventMaker->addSimuTrigger(137501);
+  //skimEventMaker->addSimuTrigger(137213);  
+  //skimEventMaker->addSimuTrigger(127221);
+  skimEventMaker->addSimuTrigger(137222); // bemc-jp1-mb, th1=60(8.3 GeV)
 
   //test Mike's new 4p maker:
   bool doTowerSwapFix = true;
   StBET4pMaker* bet4pMaker = new StBET4pMaker("BET4pMaker",muDstMaker, doTowerSwapFix);
   
   //Pythia4pMaker
-  StPythiaFourPMaker* pythiaFourPMaker = new StPythiaFourPMaker("StPythiaFourPMaker",asym, mcEventMaker);
+  StPythiaFourPMaker* pythiaFourPMaker = new StPythiaFourPMaker;
   
   //Instantiate the JetMaker
-  StJetMaker* emcJetMaker = new StJetMaker("emcJetMaker", muDstMaker, outfile);
+  StJetMaker* emcJetMaker = new StJetMaker("emcJetMaker",muDstMaker,outfile);
   
   StppAnaPars* anapars = new StppAnaPars();
   anapars->setFlagMin(0); //track->flag() > 0
@@ -105,8 +105,8 @@ void RunJetSimuSkimFinder(const int nevents = 100,
   anapars->setJetEtaMin(0);
   anapars->setJetNmin(0);
 
-  StConePars* cpars4 = new StConePars();
-  cpars4->setGridSpacing(56, -1.6, 1.6, 120, -pi, pi);
+  StConePars* cpars4 = new StConePars;
+  cpars4->setGridSpacing(56, -1.6, 1.6, 120, -TMath::Pi(), TMath::Pi());
   cpars4->setConeRadius(0.4);
   cpars4->setSeedEtMin(0.5);
   cpars4->setAssocEtMin(0.1);
@@ -116,9 +116,9 @@ void RunJetSimuSkimFinder(const int nevents = 100,
   cpars4->setRequireStableMidpoints(true);
   cpars4->setDoSplitMerge(true);
   cpars4->setDebug(false);
-  emcJetMaker->addAnalyzer(anapars, cpars4, bet4pMaker, "MkConeR04");  
+  emcJetMaker->addAnalyzer(anapars,cpars4,bet4pMaker,"MkConeR04");  
 
-  StppAnaPars* pythiapars = new StppAnaPars();
+  StppAnaPars* pythiapars = new StppAnaPars;
   pythiapars->setFlagMin(0);
   pythiapars->setNhits(0);
   pythiapars->setCutPtMin(0.0001);
@@ -128,8 +128,8 @@ void RunJetSimuSkimFinder(const int nevents = 100,
   pythiapars->setJetEtaMin(0);
   pythiapars->setJetNmin(0);
 
-  StConePars* pythia_cpars4 = new StConePars();
-  pythia_cpars4->setGridSpacing(200, -5.0, 5.0, 120, -pi, pi);
+  StConePars* pythia_cpars4 = new StConePars;
+  pythia_cpars4->setGridSpacing(200, -5.0, 5.0, 120, -TMath::Pi(), TMath::Pi());
   pythia_cpars4->setConeRadius(0.4);
   pythia_cpars4->setSeedEtMin(0.5);
   pythia_cpars4->setAssocEtMin(0.1);
@@ -143,31 +143,6 @@ void RunJetSimuSkimFinder(const int nevents = 100,
  
   chain->Init();
   chain->PrintInfo();
-    
   chain->ls(3);
-  
-  TChain* fileChain = muDstMaker->chain();
-  assert(fileChain);
-  int ntotal = fileChain->GetEntries();
-
-  for (Int_t iev=0;iev<nevents; iev++) {
-    cout << "****************************************** " << endl;
-    cout << "Working on eventNumber:\t" << iev <<"\tof:\t"<<ntotal<<endl;
-    cout << "*************************1***************** " << endl;
-    chain->Clear();
-    int iret = chain->Make(iev);
-    total++;
-    if (iret) {
-      cout << "Bad return code!" << endl;
-      break;
-    }
-  }
-
-  chain->Finish();
-  cout << "****************************************** " << endl;
-  cout << "total number of events  " << total << endl;
-  cout << "****************************************** " << endl;
+  chain->EventLoop(nevents);
 }
-
-
-
