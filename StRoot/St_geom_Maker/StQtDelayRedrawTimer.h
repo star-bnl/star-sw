00eda328c614e0e969877a048404b61b54cb222e
fine <>
2009-09-17 17:40:49 +0000

    Add full item path tool tip

--- StRoot/St_geom_Maker/StQtDelayRedrawTimer.h
+++ StRoot/St_geom_Maker/StQtDelayRedrawTimer.h
@@ -1,18 +1,25 @@
+// Author: Valeri Fine   15/09/2009
+// ****************************************************************************
+// ** $Id: StQtDelayRedrawTimer.h,v 1.2 2009/09/17 17:40:49 fine Exp $
 #ifndef STAR_STQTDELAYREDRAWTIMER
 #define STAR_STQTDELAYREDRAWTIMER
 
-#include <QtCore/QTimer>
+#include <QtCore/QObject>
 class TObject;
+class QTimer;
 
-class StQtDelayRedrawTimer : public QTimer {
+class StQtDelayRedrawTimer : public QObject {
+   Q_OBJECT
    private:
+     QTimer  *fComplexVolumeTimer;
+     QTimer  *fSingeVolumeTimer;
      TObject *fObj;
-     bool   fExpanded;
    public:
       StQtDelayRedrawTimer(QObject *parent=0);
       virtual ~StQtDelayRedrawTimer() {}
+      virtual bool IsActive(bool expanded) const;
    public slots:
-      void DrawObject(int delay, TObject *obj,bool expanded);
+      void DrawObject(TObject *obj,bool expanded,int depth=3);
       void DrawObject();
    signals:
       void DrawObjectSignal(TObject *obj,bool expanded);
