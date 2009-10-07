006597a204bbe57f0945507b7fad66e9fffa5027
Valeri Fine <val.fine@gmail.com>
2009-10-07 15:50:36 +0000

    fix the comment typos

--- StRoot/macros/graphics/Draw3D.C
+++ StRoot/macros/graphics/Draw3D.C
@@ -1,10 +1,10 @@
 //! \file Draw3D.C 
 /*!
-  \brief Draw3D.CE macro is to test STAR EventDisplay facility
+  \brief Draw3D.C macro is to test STAR EventDisplay facility
  */
 void Draw3D()
 {
    gROOT->Macro("Load.C");  //< Load STAR framework shared libraries
-   gEventDisplay->Draw3DTest(); //< Inloke the built-in rednering test
+   gEventDisplay->Draw3DTest(); //< Invoke the built-in rendering test
    gEventDisplay->Print("Draw3DTest.wrl"); //< Save the 3D scene into the file
 }
