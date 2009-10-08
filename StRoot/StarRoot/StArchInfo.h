9c5f23286c04b546aa2338985ed2d6e14e4561e6
fine <>
2009-10-08 20:04:32 +0000

    Small optimization

--- StRoot/StarRoot/StArchInfo.h
+++ StRoot/StarRoot/StArchInfo.h
@@ -43,25 +43,40 @@ class StArchInfo :public TNamed {
    }
    
 //! This is an overloaded member function, provided for convenience.
-/*!  Returns the number of bytes the type \a T has to be align to 
+/*!
+    Returns the number of bytes the type \a T has to be align to
+    \author Valeri Fine, 2009
+    \sa AligmentTest.C
  */
-   template<typename T> static long align()
+   template<typename T> static int align()
    {
-      struct { char c; T test; } probe;
-      return ((char*)&probe.test)-&probe.c;
+      static int done = -1;
+      if (done<0) {
+         struct { char c; T test; } probe;
+         done =  ((char*)&probe.test)-&probe.c;
+      }
+      return done;
    }
    
 //! Returns the number of bytes to be added to the \a offset of the \a T type to get the \a C type proper aligned 
-   template <typename C, typename T> static long padding(const T&offset)
+/*!
+   \author Valeri Fine, 2009
+   \sa AligmentTest.C
+   \sa http://en.wikipedia.org/wiki/Data_structure_alignment#Computing_padding
+ */
+   template <typename C, typename T> static int padding(const T&offset)
    {
       long offchar = (long)&offset;
       long  alg = align<C>();
-      return long((alg + ((offchar - 1) & ~(alg - 1))) - offchar);
+      return  int((alg + ((offchar - 1) & ~(alg - 1))) - offchar);
    }
-   
+
 //! This is an overloaded member function, provided for convenience.
-/*!  Returns the number of bytes to be added to the \a offset of the \a T type to get the \a C type proper aligned 
- */
+/*!
+   Returns the number of bytes to be added to the \a offset of the \a T type to get the \a C type proper aligned 
+   \author Valeri Fine, 2009
+   \sa AligmentTest.C
+*/
    template <typename T, typename C> static int padding(const T&offset, const C&)
    {
       return padding<C,T>(offset);
