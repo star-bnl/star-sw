f084b8061bcf9a51578ba56c36fc7a0c893bf574
Michael Betancourt <betan@mit.edu>
2009-10-13 21:00:57 +0000

    Loaded libMinuit to improve compatability with ROOT 5.22

--- StRoot/StGammaMaker/macros/loadGammaLibs.C
+++ StRoot/StGammaMaker/macros/loadGammaLibs.C
@@ -4,6 +4,7 @@ void loadGammaLibs()
   gROOT -> LoadMacro("$STAR/StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C");
   loadSharedLibraries();
 
+  gSystem->Load("libMinuit")
   gSystem->Load("StDetectorDbMaker");
   gSystem->Load("StDbUtilities");
   gSystem->Load("StDbLib");
