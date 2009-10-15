a248cafa7df7c2073df2e80315d34400ffbd1299
fine <>
2009-10-15 22:32:39 +0000

    adjust the name of the parameters

--- StRoot/StarRoot/StDraw3D.h
+++ StRoot/StarRoot/StDraw3D.h
@@ -1,6 +1,6 @@
 #ifndef STAR_StDraw3D
 #define STAR_StDraw3D
-// $Id: StDraw3D.h,v 1.31 2009/10/15 22:30:50 fine Exp $
+// $Id: StDraw3D.h,v 1.32 2009/10/15 22:32:39 fine Exp $
 // *-- Author :    Valery Fine(fine@bnl.gov)   27/04/2008
 
 #include "TObject.h"
@@ -150,7 +150,7 @@ public:
    virtual TObject *Line(int n,  const float *xyz
          ,  EDraw3DStyle sty);
 
-   virtual TObject *Trap(float radius, float lamda, float phi, float w, float h, Color_t col,Style_t sty, Size_t siz);
+   virtual TObject *Trap(float radius, float lamda, float phi, float alpha1, float alpha2, Color_t col,Style_t sty, Size_t siz);
 
    virtual void Joint(StDraw3D *dsp);
    virtual void SetModel(TObject *model);
