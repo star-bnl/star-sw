5b74ed1499ac3f078beada2d51530e7b155ecc5c
cvs2git <cvs2git>
2009-11-09 21:26:03 +0000

    This commit was manufactured by cvs2git to create tag 'SL09e'.

---
+++ StRoot/StDetectorDbMaker/St_tpcGainC.h
@@ -0,0 +1,24 @@
+#ifndef St_tpcGainC_h
+#define St_tpcGainC_h
+#include "TChair.h"
+#include "tables/St_tpcGain_Table.h"
+
+class St_tpcGainC : public TChair {
+ public:
+  static St_tpcGainC* 	instance();
+  tpcGain_st  *Struct(Int_t i = 0) {return ((St_tpcGain*) Table())->GetTable()+i;}
+  Float_t   Gain(Int_t sector, Int_t row, Int_t pad) {return Struct(sector-1)->Gain[row-1][pad-1];}
+  Bool_t    livePadrow(Int_t sector, Int_t row) {
+    for (Int_t pad=1; pad<=182; pad++) if (Gain(sector,row,pad)>0) return kTRUE;
+    return kFALSE;
+  }
+ protected:
+  St_tpcGainC(St_tpcGain *table=0) : TChair(table) {}
+  virtual ~St_tpcGainC() {fgInstance = 0;}
+ private:
+  static St_tpcGainC* fgInstance;
+  ClassDefChair(St_tpcGain, tpcGain_st )
+  ClassDef(St_tpcGainC,1) //C++ TChair for tpcGain table class
+};
+
+#endif
