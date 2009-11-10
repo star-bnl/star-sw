9ab41c8eb076203a11b4b9282dce7c09985cc99b
cvs2git <cvs2git>
2009-11-10 18:34:29 +0000

    This commit was manufactured by cvs2git to create tag 'precleanup-20091110'.

--- StRoot/StJetFinder/StJetPars.cxx
+++
@@ -1,4 +0,0 @@
-// $Id: StJetPars.cxx,v 1.1 2008/08/28 04:56:08 tai Exp $
-#include "StJetPars.h"
-
-ClassImp(StJetPars)
--- pams/l3/l3Clufi/trans_table.h
+++
@@ -1,3 +0,0 @@
-#include "StDaqLib/TPC/trans_table.hh"
-inline unsigned char  log10to8_tableTouch(int i){return log10to8_table[i];}
-inline unsigned short log8to10_tableTouch(int i){return log8to10_table[i];}
