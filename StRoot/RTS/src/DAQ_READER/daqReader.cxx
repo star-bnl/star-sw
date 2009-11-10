f0607a2b4115e83ff38a17bc6a8d630e9f3055a6
tonko <>
2009-11-10 13:20:31 +0000

    Added RTS_ONLINE to enable stderr logging

--- StRoot/RTS/src/DAQ_READER/daqReader.cxx
+++ StRoot/RTS/src/DAQ_READER/daqReader.cxx
@@ -46,7 +46,7 @@
 u_int evp_daqbits ;
 
 //Tonko:
-static const char cvs_id_string[] = "$Id: daqReader.cxx,v 1.24 2009/11/09 21:05:29 tonko Exp $" ;
+static const char cvs_id_string[] = "$Id: daqReader.cxx,v 1.25 2009/11/10 13:20:31 tonko Exp $" ;
 
 static int evtwait(int task, ic_msg *m) ;
 static int ask(int desc, ic_msg *m) ;
@@ -136,16 +136,15 @@ daqReader::daqReader(char *name)
 
 void daqReader::init()
 {
-#ifdef __ROOT__
-// Tonko: daqman is hammered with log messages from misc rcas hosts
-// although there's an ifdef __ROOT__ in the rtsLog.h but it doesn't
-// seem to work. So I'm forcing it here to STDERR
+#ifdef RTS_ONLINE
+  // this is only for Offline!
+
   rtsLogLevel(WARN) ;
   rtsLogAddDest("130.199.60.86",RTS_LOG_PORT_READER) ;	// reader.log to daqman
 
   // allow this one message to daqman's reader log...
   rtsLogOutput(RTS_LOG_NET) ;
-  LOG(INFO,"daqReader::init  [%s]",cvs_id_string) ;
+  LOG(INFO,"%s",cvs_id_string) ;
   // back to STDERR
   rtsLogOutput(RTS_LOG_STDERR) ;	// STDERR only!
 
