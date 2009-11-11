863dbb30ec46dfc81e9ee5577c934e31c6ecb1ec
baumgart <>
2009-11-11 14:17:15 +0000

    Hard code masking to OFF until database is fixed

--- StRoot/StSvtSimulationMaker/StSvtOnlineSeqAdjSimMaker.cxx
+++ StRoot/StSvtSimulationMaker/StSvtOnlineSeqAdjSimMaker.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StSvtOnlineSeqAdjSimMaker.cxx,v 1.12 2009/10/08 01:56:28 baumgart Exp $
+ * $Id: StSvtOnlineSeqAdjSimMaker.cxx,v 1.13 2009/11/11 14:17:15 baumgart Exp $
  *
  * Author: Petr Chaloupka
  ***************************************************************************
@@ -499,7 +499,7 @@ void  StSvtOnlineSeqAdjSimMaker::WriteMask()
   
   for(int tim = 0; tim < 128; tim++){
     for(int an = 0; an < 240; an++){
-      if (mMask[an*128 + tim]==kFALSE)	mAdcArray[an*128 + tim]=0;
+      // if (mMask[an*128 + tim]==kFALSE)	mAdcArray[an*128 + tim]=0;
     }
   }
 }
