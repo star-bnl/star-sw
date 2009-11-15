fd06f89c01913e5a2291257db742fc509435a55b
cvs2git <cvs2git>
2009-11-15 15:28:05 +0000

    This commit was manufactured by cvs2git to create tag 'SL09d_SL5'.

--- StRoot/macros/LoadLogger.C
+++ StRoot/macros/LoadLogger.C
@@ -2,7 +2,7 @@
 // ROOT macro to load and initialize the STAR messager with log4cxx implementation
 void LoadLogger() {
    //  gSystem->Load("logger/lib/liblog4cxx.so");
-  gSystem->Load("liblog4cxx.so");
+  //  gSystem->Load("liblog4cxx.so");
   gSystem->Load("St_base.so");
   gSystem->Load("StStarLogger.so");
   StLoggerManager::StarLoggerInit();
