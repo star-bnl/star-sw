1ce696d7b26f55b99e91d4661fa2b133e8dcc6c3
Valeri Fine <val.fine@gmail.com>
2009-11-19 21:42:20 +0000

    move getSpaceChargeCoulombs from the header to cxx

--- StRoot/StDetectorDbMaker/St_spaceChargeCorC.h
+++ StRoot/StDetectorDbMaker/St_spaceChargeCorC.h
@@ -6,6 +6,7 @@
 #include "StDetectorDbRichScalers.h"
 #include "StDetectorDbMagnet.h"
 #include "StChain/StChain.h"
+
 class St_spaceChargeCorC : public TChair {
  public:
   spaceChargeCor_st 	*Struct(Int_t i = 0) 	{return ((St_spaceChargeCor*) Table())->GetTable()+i;}
@@ -30,39 +31,7 @@ class St_spaceChargeCorC : public TChair {
     return value;
   }
   Double_t getSpaceChargeCorrection(){return  getSpaceChargeCorrection(StDetectorDbMagnet::instance()->getScaleFactor());}
-  Double_t getSpaceChargeCoulombs(Double_t scaleFactor){
-    StDetectorDbRichScalers* scalers = StDetectorDbRichScalers::instance();
-    if (! scalers ) return 0;
-    Double_t zf = zeroField(0); // potential validity margin for scalers
-    if (zf>0 && zf<1) scalers->setValidityMargin(zf);
-    Double_t coulombs = 0;
-    bool use_powers = (StMaker::GetChain()->GetDate() > 20090101);
-    for (int row=0;row< (int) getNumRows();row++) {
-      Double_t mult = 0;
-      switch ((int) getSpaceChargeDetector(row)) {
-        case (0) : mult = scalers->getMult(); break;
-        case (1) : mult = scalers->getBBCX(); break;
-        case (2) : mult = scalers->getZDCX(); break;
-        case (3) : mult = scalers->getZDCEast()+scalers->getZDCWest(); break;
-        case (4) : mult = scalers->getBBCEast()+scalers->getBBCWest(); break;
-        case (5) : mult = scalers->getZDCEast(); break;
-        case (6) : mult = scalers->getZDCWest(); break;
-        case (7) : mult = scalers->getBBCEast(); break;
-        case (8) : mult = scalers->getBBCWest(); break;
-        case (9) : mult = scalers->getBBCYellowBkg(); break;
-        case (10): mult = scalers->getBBCBlueBkg(); break;
-        default  : mult = 0.;
-      }
-      Double_t saturation = getSpaceChargeSatRate(row);
-      Double_t correction = getSpaceChargeCorrection(scaleFactor,row);
-      Double_t factor     = getSpaceChargeFactor(row);
-      Double_t offset     = getSpaceChargeOffset(row);
-      Double_t intens = (mult < saturation) ? mult : saturation;
-      if (use_powers) coulombs += ::pow(intens-offset,factor) * correction ;
-      else coulombs += factor * (intens-offset) * correction ;
-    }
-    return coulombs;
-  };
+  Double_t getSpaceChargeCoulombs(Double_t scaleFactor);
   Double_t getSpaceChargeCoulombs(){return getSpaceChargeCoulombs(StDetectorDbMagnet::instance()->getScaleFactor());}
   Double_t getSpaceChargeSatRate(Int_t i = 0) {return satRate(i);}
   Float_t  getSpaceChargeFactor(Int_t i = 0)  {return factor(i);}
