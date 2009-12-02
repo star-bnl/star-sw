9be30c52bcae485db220db5e3dcb0906e9c5bd6d
Oleksandr Grebenyuk <ogrebenyuk@lbl.gov>
2009-12-02 20:47:51 +0000

    Removing the user's analysis code from the BEMC pool

--- StRoot/StEmcPool/StPi0DataMaker/macros/config_simulation_dAu2003_MB_nospreadSMD.C
+++
@@ -1,7 +0,0 @@
-// Configuration to run pi0 reconstruction chain on the dAu2003 MB simulated data w/o SMD calibration spread.
-{
-    gROOT->Macro("config_simulation_dAu2003_nospreadSMD.C"); // load dAu2003 simulation configuration w/o SMD calibration spread
-
-    dataAnalysisSettings.timestampsFilename = "dAu2003Times_MB.txt";
-}
-
