ba53f11304bb6bbbe2283560ad83ec1fde4db728
Ross Corliss <rcorliss@mit.edu>
2009-12-11 20:37:09 +0000

    Updated Z Maker to use track direction rather than tower direction when computing invariant mass.  Added new histograms.

--- StRoot/StSpinPool/StWalgoB2009/St2009Z_histo.cxx
+++ StRoot/StSpinPool/StWalgoB2009/St2009Z_histo.cxx
@@ -29,37 +29,41 @@ St2009ZMaker::initHistos(){
   
   //final selection stuff:
 
-  hA[1]=h=new TH1F(core+"Zmass","  Final Z selection; Invariant Mass (GeV)", 50,0,200);
+  hA[1]=h=new TH1F(core+"Zmass","  Final Z selection; Invariant Mass (GeV)", 100,0,200);
   //  Lx=h->GetListOfFunctions();
   //  ln=new TLine(par_highET,0,par_highET,1.e6);  ln->SetLineColor(kRed);  Lx->Add(ln);
   sprintf(txt,"Reconstructed Charge of each track in candidate  ;First Charge;Second Charge");
   hA[2]=h=new TH2F(core+"charge_v_charge", txt,3,-1.5,1.5,3,-1.5,1.5);
   sprintf(txt,"Product of Reconstructed Charges of each track in candidate");
   hA[3]=h=new TH1F(core+"charge_product", txt,3,-1.5,1.5);
+  hA[4]=h=new TH2F(core+"phi1_v_phi2","Comparison of azimuthal position of first and second track;Phi 1;Phi 2",100,-PI,PI,100,-PI,PI);
   sprintf(txt,"Relative phi between two tracks in Z candidate");
-  hA[4]=h=new TH1F(core+"relative_phi", txt,100,0,PI*2.02);
-  //  Lx=h->GetListOfFunctions();
+  hA[5]=h=new TH1F(core+"relative_phi", txt,100,0,PI*2.02);
+  hA[6]=h=new TH2F(core+"track_mom_v_mass","Q1/pT1*Q2/pT2 versus Invariant Mass;Mass (GeV);Q1/pT1*Q2/pT2",100,0,200,200,-0.1,0.1);
+ sprintf(txt,"Product of Electron Charges vs Reconstructed Mass;mass (GeV);Q1*Q2");
+  hA[7]=h=new TH2F(core+"charge_product_v_mass",txt,50,0,200,3,-1.5,1.5);
+  hA[8]=h=new TH1F(core+"et1","ET of first cluster (after cuts)",100,0,100);
+  hA[9]=h=new TH2F(core+"positron_pos","Location of e+ from Z;eta;phi",10,-1,1,10,-PI,PI);
+  hA[10]=h=new TH2F(core+"electron_pos","Location of e- from Z;eta;phi",10,-1,1,10,-PI,PI);
+  hA[11]=h=new TH2F(core+"fmax_v_fmax","Final Z Selection, Fmax v Fmax;fmax1;fmax2",100,0,1,100,0,1);
+  hA[12]=h=new TH2F(core+"et_v_et","Transverse Energies of the two Clusters;Et1;Et2",100,0,100,100,0,100);
+  hA[13]=h=new TH2F(core+"rel_phi_v_mass","Relative Phi vs Invariant Mass;mass;phi",50,0,200,100,0,PI*2.02);
+  hA[14]=h=new TH1F(core+"ZmassLike","  Final Z selection with Like Charges on both tracks; Invariant Mass (GeV)", 100,0,200);
+  hA[15]=h=new TH1F(core+"ZmassUnlike","  Final Z selection with Unlike Charges on both tracks; Invariant Mass (GeV)", 100,0,200);
+//  Lx=h->GetListOfFunctions();
   //  ln=new TLine(0,0,100,0);  ln->SetLineColor(kMagenta);  Lx->Add(ln);
 
 
   //cut details:
-  hA[5]=h=new TH2F(core+"fmax_v_fmax","Final Z Selection, Fmax v Fmax;fmax1;fmax2",100,0,1,100,0,1);
-  hA[6]=h=new TH2F(core+"et_v_et","Transverse Energies of the two Clusters;Et1;Et2",100,0,100,100,0,100);
-  hA[7]=h=new TH1F(core+"et1before","Transverse Energy of first Cluster (before cuts)",100,0,100);
-  hA[8]=h=new TH2F(core+"rel_phi_v_mass","Relative Phi vs Invariant Mass;mass;phi",50,0,200,100,0,PI*2.02);
+  hA[21]=h=new TH2F(core+"fmax_v_fmaxbefore","Final Z Selection, Fmax v Fmax;fmax1;fmax2",100,0,1,100,0,1);
+  hA[22]=h=new TH2F(core+"et_v_etbefore","Transverse Energies of the two Clusters;Et1;Et2",100,0,100,100,0,100);
+  hA[23]=h=new TH1F(core+"et1before","Transverse Energy of first Cluster (before cuts)",100,0,100);
 
   //event/vertex details:
-  hA[9]=h=new TH1F(core+"nVertices","Number of vertices per event",10,0,10);
-  hA[10]=h=new TH1F(core+"nTracks","Number of tracks per vertex",20,0,20);
+  hA[31]=h=new TH1F(core+"nVertices","Number of vertices per event",10,0,10);
+  hA[32]=h=new TH1F(core+"nTracks","Number of tracks per vertex",20,0,20);
 
- sprintf(txt,"Product of Electron Charges vs Reconstructed Mass;mass (GeV);Q1*Q2");
-  hA[11]=h=new TH2F(core+"charge_product_v_mass",txt,50,0,200,3,-1.5,1.5);
-  hA[12]=h=new TH2F(core+"phi1_v_phi2","Comparison of azimuthal position of first and second track;Phi 1;Phi 2",100,-PI,PI,100,-PI,PI);
-  hA[13]=h=new TH1F(core+"et1","ET of first cluster (after cuts)",100,0,100);
-  hA[14]=h=new TH2F(core+"positron_pos","Location of e+ from Z;eta;phi",10,-1,1,10,-PI,PI);
-  hA[15]=h=new TH2F(core+"electron_pos","Location of e- from Z;eta;phi",10,-1,1,10,-PI,PI);
-
-
+  //evolution of cuts:
   hA[40]=h=new TH1F(core+"Zmass_cut0","Before cuts; Invariant Mass (GeV)", 50,0,200);
   hA[41]=h=new TH1F(core+"Zmass_cut1","after first fmax cut; Invariant Mass (GeV)", 50,0,200);
   hA[42]=h=new TH1F(core+"Zmass_cut2","after both fmax cuts; Invariant Mass (GeV)", 50,0,200);
