9aec5438d510f1e12b8ba5dc5e19f97cb3f9d094
balewski <>
2009-12-30 18:37:08 +0000

    code tagged in the form close to that used for the Fall 2009 DNP preliminary Jacobian peak

--- StRoot/StSpinPool/StWalgoB2009/macros/plWana.C
+++ StRoot/StSpinPool/StWalgoB2009/macros/plWana.C
@@ -2,13 +2,13 @@ TCanvas *can=0;
 
 //=================================================
 plWana(  int page=4,int pl=0, char *core0="R10096140", char *iPath="", char *oPath=""){ //1=gif, 2=ps, 3=both
-  iPath="out-sl09g/";
+  iPath="out-sl09g-Dec27/";
   //iPath="/star/data05/scratch/stevens4/wAnalysis";
   //iPath="/star/data05/scratch/balewski/2009-Wana-out6/data/";
   //core0="R10097000";
   //core0="run9setABCD";
   //core0="run9setP1234";
-  core0="run9setABCD-wEtow-BoflG";
+  core0="run9setABCD-wEtow-simBgain-Dec27";
   //core0="mcSetD1_ppWprod";
   //core0="mcSetD2_ppQCD10_inf_filter";
   //core0="mcSetD1_ppZprod";
@@ -592,6 +592,9 @@ void doAllMC(){
 
 
 // $Log: plWana.C,v $
+// Revision 1.3  2009/12/30 18:37:08  balewski
+// code tagged in the form close to that used for the Fall 2009 DNP preliminary Jacobian peak
+//
 // Revision 1.2  2009/12/08 16:53:01  balewski
 // *** empty log message ***
 //
--- StRoot/StSpinPool/StWalgoB2009/macros/rdMuWana.C
+++ StRoot/StSpinPool/StWalgoB2009/macros/rdMuWana.C
@@ -11,7 +11,7 @@ int rdMuWana(
 	     char* inDir   = "",// make it empty for scheduler 
 	     char* file    = "/star/institutions/mit/balewski/freezer/2009-W-algoVer4.3s-prelim-Jacobian2/fillListA/R10097000_230531_230601.lis",// full fill F10505
 	     int nFiles  = 1000, // max # of muDst files
-	     int isMC=0 // 0=run9-data, 1=Weve, 2=QCDeve, 3=Zeve
+	     int isMC=1 // 0=run9-data, 1=Weve, 2=QCDeve, 3=Zeve
  ) { 
 
 
@@ -256,6 +256,9 @@ int rdMuWana(
 
 
 // $Log: rdMuWana.C,v $
+// Revision 1.5  2009/12/30 18:37:08  balewski
+// code tagged in the form close to that used for the Fall 2009 DNP preliminary Jacobian peak
+//
 // Revision 1.4  2009/12/08 17:00:06  balewski
 // *** empty log message ***
 //
