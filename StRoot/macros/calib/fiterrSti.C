4cbe0e3503157d707673dd11e389ef5ca808d85c
Victor Perevoztchikov <perev@bnl.gov>
2010-01-27 21:39:06 +0000

    Options no only from user

--- StRoot/macros/calib/fiterrSti.C
+++ StRoot/macros/calib/fiterrSti.C
@@ -4,24 +4,9 @@ int ians=0;
 gROOT->ProcessLine(".L bfc.C");
 
 //if(!daqFile) daqFile = "/star/data03/daq/2005/073/st_physics_6073023_raw_1040005.daq";
-TString myFlg(flg);
-TString opt;
-int simu = strstr(daqFile,".fz")!=0;
-int tpcOnly = myFlg.Contains("tpcOnly",TString::kIgnoreCase);
-if (simu) {
-  opt ="trs  srs  fss  y2007  Idst  IAna  l0 ";
-  opt+="tpcI  fcf  ftpc  Tree  logger  ITTF  Sti  genvtx  SvtIt  bbcSim  tofsim ";
-  opt+="tags  emcY2  EEfs  evout  -dstout  IdTruth  geantout  big  fzin";
-
-} else {
-  opt="P2005";
-  for (int god=2001;god <=2009;god++) {
-    TString ts("/"); ts+=god; ts +="/";
-    if (strstr(daqFile,ts.Data())) {opt ="P";opt+=god;break;}
-  }
-  opt +=",MakeEvent,ITTF,OShortR,OSpaceZ2,-dstout";
-  if (!tpcOnly)  opt += ",ssddat,SsdIt,spt,SvtIt,Corr5,KeepSvtHit,hitfilt,skip1row";
-}
+TString opt(flg);
+printf("\n============= runsti: file=%s\n",daqFile);
+printf("============= runsti: Opt =%s\n",opt.Data());
 
 ians = bfc(-1,opt, daqFile,0,"pulls.root");
 if (ians) {printf("ERROR: bfc(-1,...) == %d\n",ians); return iAns;}
@@ -32,8 +17,9 @@ chain->SetAttr(".call","SetActive(0)","outputStream");	//NO Out
 chain->SetAttr(".call","SetActive(0)","kink2");
 chain->SetAttr(".call","SetActive(0)","StTagsMaker::");
 chain->SetAttr("makePulls",1,"Sti");
-if (tpcOnly) chain->SetAttr("useSvt",0,"Sti" );
-if (tpcOnly) chain->SetAttr("useSsd",0,"Sti" );
+chain->SetAttr("maxTotPrims",100000,"Sti");
+//if (tpcOnly) chain->SetAttr("useSvt",0,"Sti" );
+//if (tpcOnly) chain->SetAttr("useSsd",0,"Sti" );
 
 chain->SetAttr(".Privilege",1,"Sti" );
 ians = chain->Init(); 
