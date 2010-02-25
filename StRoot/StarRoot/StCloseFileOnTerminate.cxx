be2d82049276277f666cb553eee5dc7cb94abdc4
Valeri Fine <val.fine@gmail.com>
2010-02-25 20:44:05 +0000

    Class to close all open ROOT files as soon as job is about to be killed by batch

---
+++ StRoot/StarRoot/StCloseFileOnTerminate.cxx
@@ -0,0 +1,31 @@
+#include "StCloseFileOnTerminate.h"
+
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TFile.h"
+
+StCloseFileOnTerminate *StCloseFileOnTerminate::fgCloseFileOnTerminate = 0;
+//_________________________________________________________
+StCloseFileOnTerminate &StCloseFileOnTerminate::Instantiate()
+{
+  if (! fgCloseFileOnTerminate ) {
+     fgCloseFileOnTerminate = new StCloseFileOnTerminate;
+     fgCloseFileOnTerminate->Add(); 
+  }
+  return *fgCloseFileOnTerminate;
+}
+//_________________________________________________________
+Bool_t StCloseFileOnTerminate::Notify() {
+  // close all TFile
+  Error(__FUNCTION__," Closing files   . . . . ");
+  TSeqCollection   *files = gROOT->GetListOfFiles();
+  int count = 0;
+  if (files && files->GetSize() >0 ) {
+     TIter next(files);
+      while( TFile *f = (TFile *)next() ) { f->Close(); ++count; }
+      files->Delete();
+  }
+  if (count) Error(__FUNCTION__, "%d files have been closed", count);
+  else Print(" There was no open file to close");
+  return kTRUE;
+}
