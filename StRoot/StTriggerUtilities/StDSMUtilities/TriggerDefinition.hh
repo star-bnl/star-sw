a4bcce2e8e51a30bd39c05ec418ad9dcaee371ba
Pibero Djawotho <pibero@tamu.edu>
2010-03-01 19:05:15 +0000

    More updates for Run 9

---
+++ StRoot/StTriggerUtilities/StDSMUtilities/TriggerDefinition.hh
@@ -0,0 +1,17 @@
+//
+// Pibero Djawotho <pibero@tamu.edu>
+// Texas A&M University
+// 19 Feb 2010
+//
+
+#ifndef TRIGGER_DEFINITION_HH
+#define TRIGGER_DEFINITION_HH
+
+struct TriggerDefinition {
+  int  idx_trigger;
+  char name[64];
+  int  triggerId;
+  int  physicsBits;
+};
+
+#endif	// TRIGGER_DEFINITION_HH
