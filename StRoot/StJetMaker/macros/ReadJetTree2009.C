11cac4da7fcc71e10dcb7b23047f4ede1770bc58
pibero <>
2010-03-05 21:43:40 +0000

    Example jet tree reader for Run 9

---
+++ StRoot/StJetMaker/macros/ReadJetTree2009.C
@@ -0,0 +1,113 @@
+#include <map>
+
+void ReadJetTree2009(int nentries = 1e6,
+		     const char* jetfile  = "st_physics_adc_10125066_raw_6320001.jets.root",
+		     const char* skimfile = "st_physics_adc_10125066_raw_6320001.skim.root")
+{
+  cout << "nentries = " << nentries << endl;
+  cout << "jetfile  = " << jetfile  << endl;
+  cout << "skimfile = " << skimfile << endl;
+  cout << endl;
+
+  // Load libraries
+  gSystem->Load("StJetEvent");
+  gSystem->Load("StJetSkimEvent");
+
+  // Open jet & skim files
+  TChain* jetChain = new TChain("jet");
+  TChain* skimChain = new TChain("jetSkimTree");
+
+  jetChain->Add(jetfile);
+  skimChain->Add(skimfile);
+
+  // Set jet buffer
+  StJetEvent* jetEvent = 0;
+  jetChain->SetBranchAddress("ConeJets12",&jetEvent);
+
+  // Set skim buffer
+  StJetSkimEvent* skimEvent = 0;
+  skimChain->SetBranchAddress("skimEventBranch",&skimEvent);
+
+  // Event loop
+  for (int iEntry = 0; iEntry < nentries; ++iEntry) {
+    if (jetChain->GetEvent(iEntry) <= 0 || skimChain->GetEvent(iEntry) <= 0) break;
+
+    // Should not be null
+    assert(jetEvent && skimEvent);
+
+    // Enforce event synchronization
+    assert(jetEvent->runId() == skimEvent->runId() && jetEvent->eventId() == skimEvent->eventId());
+
+    if (iEntry % 1000 == 0) cout << iEntry << endl;
+
+    cout << "run: " << jetEvent->runId() << endl;
+    cout << "event: " << jetEvent->eventId() << endl;
+
+    // Print triggers
+    cout << "triggers: ";
+    TIter next(skimEvent->triggers());
+    StJetSkimTrig* trig;
+    while (trig = (StJetSkimTrig*)next()) {
+      cout << trig->trigId() << " ";
+    }
+    cout << endl;
+
+    // Get 2009 pp200 JP1 trigger
+    StJetSkimTrig* trig = skimEvent->trigger(240410);
+    if (!trig) continue;
+    if (!trig->didFire()) continue;
+
+    // Get jet patches above JP1 threshold
+    map<int,int> barrelJetPatches = skimEvent->barrelJetPatchesAboveTh(1);
+    map<int,int> endcapJetPatches = skimEvent->endcapJetPatchesAboveTh(1);
+    map<int,int> overlapJetPatches = skimEvent->overlapJetPatchesAboveTh(1);
+
+    cout << "barrel jet patches above threshold: ";
+    for (map<int,int>::const_iterator it = barrelJetPatches.begin(); it != barrelJetPatches.end(); ++it) {
+      int jp = it->first;
+      int adc = it->second;
+      cout << "JP" << jp << "=" << adc << " ";
+    }
+    cout << endl;
+
+    cout << "endcap jet patches above threshold: ";
+    for (map<int,int>::const_iterator it = endcapJetPatches.begin(); it != endcapJetPatches.end(); ++it) {
+      int jp = it->first;
+      int adc = it->second;
+      cout << "JP" << jp << "=" << adc << " ";
+    }
+    cout << endl;
+
+    cout << "overlap jet patches above threshold: ";
+    for (map<int,int>::const_iterator it = overlapJetPatches.begin(); it !=overlapJetPatches.end(); ++it) {
+      int jp = it->first;
+      int adc = it->second;
+      cout << "JP" << jp << "=" << adc << " ";
+    }
+    cout << endl;
+
+    int bbctimebin = skimEvent->bbcTimeBin() >> 9 & 0xf;
+    cout << "bbctimebin: " << bbctimebin << endl;
+
+    TVector3 vertex(skimEvent->bestVert()->position());
+    cout << "vertex: " << vertex.x() << '\t' << vertex.y() << '\t' << vertex.z() << endl;
+
+    // Jet loop
+    cout << "njets: " << jetEvent->numberOfJets() << endl;
+    for (int iJet = 0; iJet < jetEvent->numberOfJets(); ++iJet) {
+      StJetCandidate* jet = jetEvent->jet(iJet);
+      cout << "Jet #" << iJet << ": pt=" << jet->pt() << " eta=" << jet->eta() << " phi=" << jet->phi() << " rt=" << jet->neutralFraction() << " ntracks=" << jet->numberOfTracks() << " ntowers=" << jet->numberOfTowers() << endl;
+      // Track loop
+      for (int iTrack = 0; iTrack < jet->numberOfTracks(); ++iTrack) {
+	StJetTrack* track = jet->track(iTrack);
+	cout << "Track #" << iTrack << ": id=" << track->id() << " pt=" << track->pt() << " eta=" << track->eta() << " phi=" << track->phi() << endl;
+      }	// End track loop
+      // Tower loop
+      for (int iTower = 0; iTower < jet->numberOfTowers(); ++iTower) {
+	StJetTower* tower = jet->tower(iTower);
+	cout << "Tower #" << iTower << ": id=" << tower->id() << " detid=" << tower->detectorId() << " pt=" << tower->pt() << " eta=" << tower->eta() << " phi=" << tower->phi() << endl;
+      }	// End tower loop
+    } // End jet loop
+    cout << endl;
+  } // End event loop
+}
