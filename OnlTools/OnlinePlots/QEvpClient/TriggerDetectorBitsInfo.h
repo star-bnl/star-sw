96cc8d1243caec92ab9ed03f5b4bcbf101d291d5
fine <>
2010-03-25 17:15:41 +0000

    RT #1792: Make TriggerDetectorBitsInfoUi.ui Qt4 API compliant

--- OnlTools/OnlinePlots/QEvpClient/TriggerDetectorBitsInfo.h
+++ OnlTools/OnlinePlots/QEvpClient/TriggerDetectorBitsInfo.h
@@ -1,34 +1,22 @@
 #ifndef TriggerDetectorBitsInfo_h
 #define TriggerDetectorBitsInfo_h
 
-#if QT_VERSION < 0x40000
-#  include "TriggerDetectorBitsInfoUi.h"
-#else
-#  include "ui_TriggerDetectorBitsInfoUi.h"
-   typedef Ui_TriggerDetectorBitsInfoUi TriggerDetectorBitsInfoUi;
-#endif
+#include "ui_TriggerDetectorBitsInfoUi.h"
+typedef Ui_TriggerDetectorBitsInfoUi TriggerDetectorBitsInfoUi;
 
 using namespace std;
 
 
-class TriggerDetectorBitsInfo :
-#if QT_VERSION > 0x40000
-      public QWidget, 
-#endif 
-      public TriggerDetectorBitsInfoUi {
+class TriggerDetectorBitsInfo :  public QFrame, public TriggerDetectorBitsInfoUi {
  public:
   TriggerDetectorBitsInfo( QWidget* parent = 0, const char* name = 0, Qt::WFlags fl = 0 ) :
-#if QT_VERSION > 0x40000
-       QWidget(parent, fl) {if (name) setName(name); setupUi(this); }
-#else
-       TriggerDetectorBitsInfoUi(parent, name, fl) { /* no-op */}
-#endif
+       QFrame(parent, fl) {if (name) setName(name); setupUi(this); }
   public slots:
 
-  virtual void setTriggerBitsRun(unsigned int v) { RunTrig->setText( utoa(v) ) ;}
-  virtual void setTriggerBits(unsigned int v) { EventTrig->setText( utoa(v) ) ;}
-  virtual void setDetectorBitsRun(unsigned int v) { RunDet->setText( utoa(v) );}
-  virtual void setDetectorBits(unsigned int v) { EventDet->setText( utoa(v) );}
+  virtual void setTriggerBitsRun(unsigned int v)  { RunTrig->setText( utoa(v) ) ;  }
+  virtual void setTriggerBits(unsigned int v)     { EventTrig->setText( utoa(v) ) ;}
+  virtual void setDetectorBitsRun(unsigned int v) { RunDet->setText( utoa(v) );    }
+  virtual void setDetectorBits(unsigned int v)    { EventDet->setText( utoa(v) );  }
 
  private:
     QString utoa(unsigned int t) {
@@ -42,6 +30,4 @@ class TriggerDetectorBitsInfo :
       return bitstring;
     }
 };
-
-
 #endif
