cb20a70a91e0c35270b4034343d482374e6fc3e4
fine <>
2010-04-13 16:11:47 +0000

    script to customize tomcat installation

---
+++ StRoot/StStarLogger/logging/setenv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+###############################################################
+#   It is used to set  the shared lib for all servlets        #
+#   It is required for JNI-based servlets                     #
+#   $Id: setenv.sh,v 1.1 2010/04/13 16:11:47 fine Exp $
+###############################################################
+
+if [ -d $CATALINA_HOME/shared/lib ]; then 
+   LD_LIBRARY_PATH=$CATALINA_HOME/shared/lib
+   export  LD_LIBRARY_PATH=$CATALINA_HOME/shared/lib
+   export JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$CATALINA_HOME/shared/lib"
+else
+   echo "ATTENTION !!! no $CATALINA_HOME/shared/lib is defined yet"
+   echo "Make sure the shared.loader property from $CATALINA_HOME/conf/catalina.properties is set as well" 
+   echo "grep  shared.loader  $CATALINA_HOME/conf/catalina.properties"
+   grep  shared.loader  $CATALINA_HOME/conf/catalina.properties 
+fi
