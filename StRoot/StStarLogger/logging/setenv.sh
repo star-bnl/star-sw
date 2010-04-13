d69bd0537cfcd1dd309e22ef9fb0ba728a9653b2
Valeri Fine <val.fine@gmail.com>
2010-04-13 18:55:48 +0000

    add CLASSPATH to setenv

--- StRoot/StStarLogger/logging/setenv.sh
+++ StRoot/StStarLogger/logging/setenv.sh
@@ -3,12 +3,13 @@
 ###############################################################
 #   It is used to set  the shared lib for all servlets        #
 #   It is required for JNI-based servlets                     #
-#   $Id: setenv.sh,v 1.1 2010/04/13 16:11:47 fine Exp $
+#   $Id: setenv.sh,v 1.2 2010/04/13 18:55:48 fine Exp $
 ###############################################################
 
 if [ -d $CATALINA_HOME/shared/lib ]; then 
    LD_LIBRARY_PATH=$CATALINA_HOME/shared/lib
-   export  LD_LIBRARY_PATH=$CATALINA_HOME/shared/lib
+   export CLASSPATH=$CLASSPATH:$CATALINA_HOME/shared/lib/tx-ucm.jar:$CATALINA_HOME/shared/lib
+   export LD_LIBRARY_PATH=$CATALINA_HOME/shared/lib
    export JAVA_OPTS="$JAVA_OPTS -Djava.library.path=$CATALINA_HOME/shared/lib"
 else
    echo "ATTENTION !!! no $CATALINA_HOME/shared/lib is defined yet"
