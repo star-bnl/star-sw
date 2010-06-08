01406ed13872aad5d17a9f20d467ee319d92d185
Heinrich Schindler <Heinrich.Schindler@cern.ch>
2010-06-08 15:46:48 +0000

    Added TrackSimple class with exponential or equal spacing (really now)

---
+++ Include/TrackSimple.hh
@@ -0,0 +1,45 @@
+
+#ifndef G_TRACK_SIMPLE_H
+#define G_TRACK_SIMPLE_H
+
+#include "Track.hh"
+
+namespace Garfield {
+
+class TrackSimple: public Track {
+
+  public:
+    // Constructor
+    TrackSimple();
+    // Destructor
+    virtual ~TrackSimple() {}
+
+    void SetEqualSpacing() {useEqualSpacing = true;}
+    void SetExponentialSpacing() {useEqualSpacing = false;}
+    void SetClusterDensity(const double d);
+    void SetStoppingPower(const double dedx);
+
+    void NewTrack(const double x0, const double y0, const double z0, const double t0, 
+                  const double dx0, const double dy0, const double dz0);
+    bool GetCluster(double& xcls, double& ycls, double& zcls, double& tcls,
+                    int& n, double& e, double& extra);
+
+  protected:
+
+    bool isReady;
+
+    // Particle position, time and direction
+    double x, y, z, t;
+    double dx, dy, dz;
+    // Mean free path (mean spacing between adjacent clusters)
+    double mfp;
+    // Average energy per cluster
+    double eloss;
+
+    bool useEqualSpacing;
+
+};
+
+}
+
+#endif
---
+++ Source/TrackSimple.cc
@@ -0,0 +1,123 @@
+#include <iostream>
+
+#include "TrackSimple.hh"
+#include "FundamentalConstants.hh"
+#include "Random.hh"
+
+namespace Garfield {
+
+TrackSimple::TrackSimple() :
+  isReady(false),
+  x(0.), y(0.), z(0.), t(0.),
+  dx(0.), dy(0.), dz(1.),
+  mfp(0.04), eloss(2530.), useEqualSpacing(false) {
+
+}
+
+void
+TrackSimple::SetClusterDensity(const double d) {
+
+  if (d < Small) {
+    std::cerr << "TrackSimple::SetClusterDensity:" << std::endl;
+    std::cerr << "    Cluster density (number of clusters per cm) must be positive."
+              << std::endl;
+    return;
+  }
+
+  mfp = 1. / d;
+
+}
+
+void
+TrackSimple::SetStoppingPower(const double dedx) {
+
+  if (dedx < Small) {
+    std::cerr << "TrackSimple::SetStoppingPower:" << std::endl;
+    std::cerr << "    Stopping power (average energy loss [eV] per cm) "
+              << "must be positive." << std::endl;
+    return;
+  }
+
+  eloss = dedx;
+
+}
+
+void
+TrackSimple::NewTrack(
+            const double x0, const double y0, const double z0, const double t0,
+            const double dx0, const double dy0, const double dz0) {
+
+  // Check if a sensor has been defined
+  if (sensor == 0) {
+    std::cerr << "TrackSimple::NewTrack:" << std::endl;
+    std::cerr << "    Sensor is not defined." << std::endl;
+    isReady = false;
+    return;
+  }
+
+  // Make sure we are inside a medium
+  Medium* medium;
+  if (!sensor->GetMedium(x0, y0, z0, medium)) {
+    std::cerr << "TrackSimple::NewTrack:" << std::endl;
+    std::cerr << "    No medium at initial position." << std::endl;
+    isReady = false;
+    return;
+  }
+
+  isReady = true;
+
+  x = x0; y = y0; z = z0; t = t0;
+
+  // Normalise the direction
+  const double d = sqrt(dx0 * dx0 + dy0 * dy0 + dz0 * dz0);
+  if (d < Small) {
+    // Choose random direction
+    double phi = TwoPi * RndmUniform();
+    double ctheta = 1. - 2. * RndmUniform();
+    double stheta = sqrt(1. - ctheta * ctheta);
+    dx = cos(phi) * stheta;
+    dy = sin(phi) * stheta;
+    dz = ctheta;
+  } else {
+   dx = dx0 / d; dy = dy0 / d; dz = dz0 / d;
+  }
+
+}
+
+bool
+TrackSimple::GetCluster(double& xcls, double& ycls, double& zcls, double& tcls,
+                        int& n, double& e, double& extra) {
+
+  if (!isReady) return false;
+
+  if (useEqualSpacing) {
+    x += dx * mfp; 
+    y += dy * mfp;
+    z += dz * mfp;
+  } else {
+    const double d = - mfp * log(RndmUniformPos());
+    x += dx * d;
+    y += dy * d;
+    z += dz * d;
+  }
+ 
+  xcls = x; ycls = y; zcls = z; tcls = t;
+
+  n = 1;
+  e = eloss * mfp;
+ 
+  Medium* medium;
+  if (!sensor->GetMedium(x, y, z, medium)) {
+    isReady = false;
+    if (debug) {
+      std::cout << "TrackSimple::GetCluster:" << std::endl;
+      std::cout << "    Particle left the medium." << std::endl;
+    }
+    return false;
+  }
+
+  return true;
+
+}
+
+}
