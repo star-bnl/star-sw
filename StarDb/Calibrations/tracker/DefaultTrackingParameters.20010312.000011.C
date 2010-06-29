633692f2fc70ef1cc619a00b1cc49f1fb735b42c
perev <>
2010-06-29 16:59:13 +0000

    Testing of decreasing gates

--- StarDb/Calibrations/tracker/DefaultTrackingParameters.20010312.000011.C
+++ StarDb/Calibrations/tracker/DefaultTrackingParameters.20010312.000011.C
@@ -9,10 +9,10 @@ TrackingParameters_st row;
 St_TrackingParameters *tableSet = new St_TrackingParameters("DefaultTrackingParameters",1);
 //
 memset(&row,0,tableSet->GetRowSize());
-    row.minSearch  =    1.5; // Minimum Search Window ;
-    row.maxSearch  =    8.0; // Maximum Search Window ;
-    row.scaling	   =    3.0; // Search Window Scaling ;
-    row.maxChi2	   =   20.0; // Max Chi2 ;
+    row.minSearch  =    0.1; // Minimum Search Window ;
+    row.maxSearch  =    3.0; // Maximum Search Window ;
+    row.scaling	   =    5.0; // Search Window Scaling ;
+    row.maxChi2	   =   33.0; // Max Chi2 ;
 tableSet->AddAt(&row);
 // ----------------- end of code ---------------
  return (TDataSet *)tableSet;
