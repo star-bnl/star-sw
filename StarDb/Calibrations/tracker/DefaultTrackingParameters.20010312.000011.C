4074de6ca1fa19b1d475b494cabadc8549e30cd9
perev <>
2010-06-30 18:48:07 +0000

    max Xi2=20 to compare with original

--- StarDb/Calibrations/tracker/DefaultTrackingParameters.20010312.000011.C
+++ StarDb/Calibrations/tracker/DefaultTrackingParameters.20010312.000011.C
@@ -12,7 +12,7 @@ memset(&row,0,tableSet->GetRowSize());
     row.minSearch  =    0.1; // Minimum Search Window ;
     row.maxSearch  =    3.0; // Maximum Search Window ;
     row.scaling	   =    5.0; // Search Window Scaling ;
-    row.maxChi2	   =   33.0; // Max Chi2 ;
+    row.maxChi2	   =   20.0; // Max Chi2 ;
 tableSet->AddAt(&row);
 // ----------------- end of code ---------------
  return (TDataSet *)tableSet;
