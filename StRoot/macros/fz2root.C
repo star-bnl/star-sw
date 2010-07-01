a9d37f1ea05fea22b63a3bbb91fa647f8895abfa
fine <>
2010-07-01 15:42:58 +0000

    fix typo, remove the redundant parts

--- StRoot/macros/fz2root.C
+++ StRoot/macros/fz2root.C
@@ -1,27 +1,10 @@
-class St_db_Maker;
-St_db_Maker *dbMk =0;
 // Conver Zebra file into ROOT format
 void fz2root(const char *file=
-            "star/rcf/simu/auau200/hijing/b0_20/inverse/year2001/hadronic_on/gstardata/rcf0191_01_380evts.fzd"
-            , unsigned int nEvent=1) 
+            "/star/rcf/simu/auau200/hijing/b0_20/inverse/year2001/hadronic_on/gstardata/rcf0191_01_380evts.fzd"
+            , unsigned int nEvent=2) 
 {
-   gROOT->Macro("Load.C"); 
-   gSystem->Load("StDetectorDbMaker");
    gROOT->LoadMacro("bfc.C");
-   gSystem->Load("StTreeMaker");
-    bfc(0,"fzin",file);
-    // WriteOut 
-     TString basename = gSystem->BaseName(file);
-     basename.ReplaceAll(".fzd","");
-     char *outName  = gSystem->ConcatFileName("t",basename);
-     dbMk = (St_db_Maker *)chain->Maker("db");
-     dbMk->SetDateTime(20090312, 94451);
-
-     StTreeMaker *outMk = new StTreeMaker("EvOut",outName,"bfcTree");
-        outMk->SetIOMode("w");
-        outMk->IntoBranch("geantBranch","geant");
-      outMk->SetDebug();
-      outMk->Init();
-      chain->EventLoop(nEvent);
-      chain->Finish();
+   bfc(0,"fzin, GeantOut sdt20090312",file);
+   chain->EventLoop(nEvent);
+   chain->Finish();
 }
