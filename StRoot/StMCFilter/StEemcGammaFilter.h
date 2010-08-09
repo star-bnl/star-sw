d05fbdcb4a14b855168c8b5badb218e2c037913b
Ilya Selyuzhenkov <ilya.selyuzhenkov@gmail.com>
2010-08-09 21:36:57 +0000

    Updated comment fields

--- StRoot/StMCFilter/StEemcGammaFilter.h
+++ StRoot/StMCFilter/StEemcGammaFilter.h
@@ -1,12 +1,16 @@
-#ifndef _STAREEMCGAMMAFILTER_
-#define _STAREEMCGAMMAFILTER_
+#ifndef STAR_StEemcGammaFilter
+#define STAR_StEemcGammaFilter
 
-//////////////////////////////////////////////////
-//    StEemcGammaFilter                             //
-//                                              //
-//    Description....                           //
-//                                              //
-//////////////////////////////////////////////////
+/*!
+*                                                                     
+* \class  StEemcGammaFilter
+* \author Michael Betancourt, MIT 
+* \author Alice Bridgeman, Argonne Lab
+* \author Ilya Selyuzhenkov, Indina U. CEEM
+* \date   2010/07/21
+* \brief  Pythia level Endcap EMC gamma filter
+*
+*/                                                                      
 
 #include "StMCFilter/StMCFilter.h"
 
@@ -17,30 +21,36 @@ class StGenParticleMaster;
 
 class StEemcGammaFilter : public StMCFilter 
 {
-
- public:
-
-  StEemcGammaFilter();
-  virtual ~StEemcGammaFilter() {};
-       
-  // Reject after vertex sampling
-  int RejectGT(const StGenParticleMaster &ptl) const;
-
- private:
-
-  static const double mConeRadius;
-  static const double mSeedThreshold;
-  static const double mClusterThreshold;
-  static const double mEtaLow;
-  static const double mEtaHigh;
-  static const double mCalDepth;
-  static const double mHadronScale;
-  static const double mMinPartEnergy;
-  static const double mMaxVertex;
-      
-  int    mPrintLevel;
-  int    mFilterMode;
-
+  
+  public:
+    
+    StEemcGammaFilter();
+    virtual ~StEemcGammaFilter() {};
+    
+    // Reject after vertex sampling
+    int RejectGT(const StGenParticleMaster &ptl) const;
+    
+    virtual const char *GetCVS() const
+    {
+      static const char cvs[]="Tag $Name:  $ $Id: StEemcGammaFilter.h,v 1.2 2010/08/09 21:36:57 seluzhen Exp $ built "__DATE__" "__TIME__ ; 
+      return cvs;
+    }
+    
+  private:
+    
+    static const double mConeRadius;
+    static const double mSeedThreshold;
+    static const double mClusterThreshold;
+    static const double mEtaLow;
+    static const double mEtaHigh;
+    static const double mCalDepth;
+    static const double mHadronScale;
+    static const double mMinPartEnergy;
+    static const double mMaxVertex;
+    
+    int    mPrintLevel;
+    int    mFilterMode;
+    
 };
 
 #endif
