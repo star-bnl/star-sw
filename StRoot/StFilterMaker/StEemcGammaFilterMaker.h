e3fd7c71b2e74d067a4ebba13a451dd5a553b5bb
Ilya Selyuzhenkov <ilya.selyuzhenkov@gmail.com>
2010-08-09 21:38:21 +0000

    Gets sampling fraction from Fast simulator, db settings movied to Init

--- StRoot/StFilterMaker/StEemcGammaFilterMaker.h
+++ StRoot/StFilterMaker/StEemcGammaFilterMaker.h
@@ -1,22 +1,16 @@
-////////////////////////////////////////////////////////////
-//                                                        //
-//    StEemcGammaFilterMaker                                  //
-//                                                        //
-//    Michael Betancourt                                  //
-//    Massachusetts Institute of Technology               //
-//                                                        //
-//    Maker designed for use with StEvent to abort        //
-//    events without clusters that would eventually       //
-//    produce StGammaCandidates.  The filter can be       //
-//    run on either the BEMC or the EEMC.                 //
-//                                                        //
-//    To use with the BFC place the maker instantiation   //
-//    before the line "#if 0" in bfc() of the bfc.C.      //
-//                                                        //
-////////////////////////////////////////////////////////////
-
 #ifndef STAR_StEemcGammaFilterMaker
 #define STAR_StEemcGammaFilterMaker
+
+/*!
+*                                                                     
+* \class  StEemcGammaFilterMaker
+* \author Michael Betancourt, MIT 
+* \author Alice Bridgeman, Argonne Lab
+* \author Ilya Selyuzhenkov, Indina U. CEEM
+* \date   2010/07/21
+* \brief  BFC level Endcap EMC gamma filter
+*
+*/                                                                      
 
 #include "StMaker.h"
 
@@ -39,56 +33,57 @@ const unsigned int nEemcTowers = nEemcEtaBins * nEemcPhiBins;
 
 class StEemcGammaFilterMaker: public StMaker
 {
-
-    public:
+  
+  public:
     
-        StEemcGammaFilterMaker();
-        virtual ~StEemcGammaFilterMaker();
-        
-        virtual Int_t Init();
-        virtual Int_t Make();
-        virtual Int_t Finish();
-        virtual void  Clear(const Option_t* = "");
-        
-        virtual const char *GetCVS() const
-        {
-            static const char cvs[]=" StEemcGammaFilterMaker.h, v1.0 2008/06/25 00:00:00 betan";
-            return cvs;
-        }
-
-      
-        void setThresholds(double seed, double cluster);
-        void setMaxVertex(double maxVertex);
-        void setEEMCSamplingFraction(double f=0.05);//change to 5% hard coded
-            
-	Int_t makeEEMC(StEmcCollection *emcCollection, double zVertex);
-        
-	bool getUseDbParams() const {return mUseDbParams;}
-	void setUseDbParams(bool use = true) {mUseDbParams = use;}
-
-    private:
-        // Flags
-	TVector3 *mMomentum;
-
-        // Filter thresholds
-        double mMaxVertex;
-        double mSeedEnergyThreshold;
-        double mClusterEtThreshold;
-        double mEemcSamplingFraction;
-	double mEemcTowerHits[nEemcTowers];
-        
-        EEmcGeomSimple *mEemcGeom;
-        
-        // Filter counters
-        unsigned int mTotal;
-        unsigned int mAccepted;
-        unsigned int mVertexRejected;
-        unsigned int mFilterRejected;
-	bool mFilterMode;
-	bool mUseDbParams; // whether or not read parameters from the database
-	
+    StEemcGammaFilterMaker();
+    virtual ~StEemcGammaFilterMaker();
+    
+    virtual Int_t Init();
+    virtual Int_t Make();
+    virtual Int_t Finish();
+    virtual void  Clear(const Option_t* = "");
+    
+    virtual const char *GetCVS() const
+    {
+      static const char cvs[]="Tag $Name:  $ $Id: StEemcGammaFilterMaker.h,v 1.2 2010/08/09 21:38:21 seluzhen Exp $ built "__DATE__" "__TIME__ ; 
+      return cvs;
+    }
+    
+    void setThresholds(double seed, double cluster);
+    void setMaxVertex(double maxVertex);
+    void setEEMCSamplingFraction(double f=0.048);//change to 4.8% hard coded
+    
+    Int_t makeEEMC(StEmcCollection *emcCollection, double zVertex);
+    
+    bool getUseDbParams() const {return mUseDbParams;}
+    void setUseDbParams(bool use = true) {mUseDbParams = use;}
+    
+  private:
+    TVector3 *mMomentum;
+    double mMaxVertex;
+    double mSeedEnergyThreshold;
+    double mClusterEtThreshold;
+    double mEemcSamplingFraction;
+    double mEemcTowerHits[nEemcTowers];
+    
+    EEmcGeomSimple *mEemcGeom;
+    
+    // Filter counters
+    unsigned int mTotal;
+    unsigned int mAccepted;
+    unsigned int mVertexRejected;
+    unsigned int mFilterRejected;
+    bool mFilterMode;
+    bool mUseDbParams; // whether or not read parameters from the database
+    
     ClassDef(StEemcGammaFilterMaker, 0);
-        
+    
 };
 
 #endif
+
+// $Log: StEemcGammaFilterMaker.h,v $
+// Revision 1.2  2010/08/09 21:38:21  seluzhen
+// Gets sampling fraction from Fast simulator, db settings movied to Init
+//
