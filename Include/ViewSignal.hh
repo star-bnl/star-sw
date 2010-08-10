ce7e98e34980ba3daad7c52b0e7d7edd2037ca87
Heinrich Schindler <Heinrich.Schindler@cern.ch>
2010-08-10 18:04:32 +0000

    Actually added ViewSignal...

---
+++ Include/ViewSignal.hh
@@ -0,0 +1,53 @@
+#ifndef G_VIEW_SIGNAL
+#define G_VIEW_SIGNAL
+
+#include <string>
+
+#include <RQ_OBJECT.h>
+#include <TCanvas.h>
+#include <TH1F.h>
+
+#include "Sensor.hh"
+
+namespace Garfield {
+
+class ViewSignal { 
+
+  RQ_OBJECT("ViewSignal")
+  
+  public:
+    // Constructor
+    ViewSignal();
+    // Destructor
+    ~ViewSignal();
+   
+    void SetSensor(Sensor* s);
+    void SetCanvas(TCanvas* c);
+ 
+    void PlotSignal(const std::string label);
+
+    void EnableDebugging()  {debug = true;}
+    void DisableDebugging() {debug = false;}
+
+  private:
+  
+    // Options
+    bool debug;
+    
+    // Sensor
+    Sensor* sensor;
+
+    // Time window
+    double tmin, tmax;
+    
+    // Canvas
+    TCanvas* canvas;
+    bool hasExternalCanvas;
+
+    // Histogram
+    TH1F* hSignal;
+    
+};
+
+}
+#endif
---
+++ Source/ViewSignal.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+
+#include <TAxis.h> 
+
+#include "ViewSignal.hh"
+#include "Plotting.hh"
+
+namespace Garfield {
+
+ViewSignal::ViewSignal() :
+  debug(false), sensor(0),
+  canvas(0), hasExternalCanvas(false),
+  hSignal(0) {
+
+}
+
+ViewSignal::~ViewSignal() {
+
+  if (!hasExternalCanvas && canvas != 0) delete canvas;
+  if (hSignal != 0) delete hSignal;
+}
+
+void
+ViewSignal::SetSensor(Sensor* s) {
+
+  if (s == 0) {
+    printf("ViewSignal::SetSensor:\n");
+    printf("    Sensor is not defined.\n");
+    return;
+  }
+
+  sensor = s; 
+
+}
+
+void
+ViewSignal::SetCanvas(TCanvas* c) {
+
+  if (c == 0) return;
+  if (!hasExternalCanvas && canvas != 0) {
+    delete canvas;
+    canvas = 0;
+  }
+  canvas = c;
+  hasExternalCanvas = true;
+
+}
+
+void 
+ViewSignal::PlotSignal(const std::string label) {
+
+  if (sensor == 0) {
+    std::cerr << "ViewSignal::PlotSignal:" << std::endl;
+    std::cerr << "    Sensor is not defined." << std::endl;
+    return;
+  }
+  
+  // Setup the canvas
+  if (canvas == 0) {
+    canvas = new TCanvas();
+    canvas->SetTitle("Signal");
+    if (hasExternalCanvas) hasExternalCanvas = false;
+  }
+  canvas->cd();
+
+  int nBins;
+  double t0, dt;
+  sensor->GetTimeWindow(t0, dt, nBins);
+  
+  if (hSignal != 0) {
+    delete hSignal;
+    hSignal = 0;
+  }
+  hSignal = new TH1F("hSignal", label.c_str(), nBins, t0, t0 + nBins * dt);
+  hSignal->GetXaxis()->SetTitle("time [ns]");
+  hSignal->GetYaxis()->SetTitle("signal");
+  
+  double sig = 0.;  
+  for (int i = nBins; i--;) {
+    sig = sensor->GetSignal(label, i);
+    hSignal->SetBinContent(i, sig);
+  }
+  
+  hSignal->Draw();
+  canvas->Update();
+
+}
+
+}
