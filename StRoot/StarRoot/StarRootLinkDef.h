ca3b4aaf34aa43bb7177150b8a8c286f294c1009
Yuri Fisyak <fisyak@bnl.gov>
2010-08-30 13:42:48 +0000

    Make KFParticleBase and KFParticle to be TObject

--- StRoot/StarRoot/StarRootLinkDef.h
+++ StRoot/StarRoot/StarRootLinkDef.h
@@ -61,8 +61,6 @@
 #pragma link C++ enum  EDraw3DStyle;
 //IncFile=TPolynomial.h
 #pragma link C++ namespace TPolynomial;
-#pragma link C++ class KFParticleBase;
-#pragma link C++ class KFParticle;
 #pragma link C++ class MTrack;
 #pragma link C++ class MVertex;
 #endif
