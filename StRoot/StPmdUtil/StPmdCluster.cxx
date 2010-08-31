e832da19dd977801cab245156df7985362bbb826
cvs2git <cvs2git>
2010-08-31 15:19:38 +0000

    This commit was manufactured by cvs2git to create tag 'SL10h_2'.

--- StRoot/StPmdUtil/StPmdCluster.cxx
+++ StRoot/StPmdUtil/StPmdCluster.cxx
@@ -1,6 +1,6 @@
 /************************************************************
  *
- * $Id: StPmdCluster.cxx,v 1.3 2003/10/14 07:28:53 subhasis Exp $
+ * $Id: StPmdCluster.cxx,v 1.4 2010/05/28 17:24:19 rashmi Exp $
  *
  * Author:  Subhasis Chattopadhyay
  ************************************************************
@@ -10,6 +10,9 @@
  ************************************************************
  *
  * $Log: StPmdCluster.cxx,v $
+ * Revision 1.4  2010/05/28 17:24:19  rashmi
+ * Added initialization of mCluSigma in constructor
+ *
  * Revision 1.3  2003/10/14 07:28:53  subhasis
  * CluX, CluY added
  *
@@ -32,8 +35,11 @@ StPmdCluster::StPmdCluster(TArrayI *cluster) : StObject()
 StPmdCluster::StPmdCluster() : StObject() 
 {
   mCluEdep = 0.0; mCluEta = 0.0; mCluPhi = 0.0; 
-  mNumofMems = 0; mModule = 0;
-  mCluX = 0.0;mCluY = 0.0;  
+  mNumofMems = 0.0 ; mModule = 0;
+  mCluSigmaS = 0.0; mCluSigmaL = 0.0;
+  mCluPID=0;  mCluEdepPID=0;  mMcCluPID=0;
+  mCluX = 0.0; mCluY = 0.0;  
+
 }
 //__________________________________________________
 
