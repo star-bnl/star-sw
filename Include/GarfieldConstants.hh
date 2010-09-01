eb421c7e56390300f42ad0c6d8bf11c3c3cb7ddc
Heinrich Schindler <Heinrich.Schindler@cern.ch>
2010-09-01 11:38:49 +0000

    Added file GarfieldConstants.hh

---
+++ Include/GarfieldConstants.hh
@@ -0,0 +1,12 @@
+
+#ifndef G_GARFIELD_CONSTANTS_H
+#define G_GARFIELD_CONSTANTS_H
+
+namespace Garfield {
+
+static const int DxcTypeElectron = -1;
+static const int DxcTypePhoton   =  1;
+
+}
+
+#endif
