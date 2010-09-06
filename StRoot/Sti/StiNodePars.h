37be3caf844953bb2f6f9aa9a357ce9d9fbe7ba6
Yuri Fisyak <fisyak@bnl.gov>
2010-09-06 18:20:17 +0000

    remove GNUC dependence

--- StRoot/Sti/StiNodePars.h
+++ StRoot/Sti/StiNodePars.h
@@ -11,14 +11,14 @@ class StiNodePars {
     /// accessors
   Double_t  operator[](Int_t idx) const {return P[idx];}
   Double_t &operator[](Int_t idx)       {return P[idx];}
-  Double_t x()    const {return P[0];}
-  Double_t y()    const {return P[1];}
-  Double_t z()    const {return P[2];}
-  Double_t eta()  const {return P[3];} 
-  Double_t ptin() const {return P[4];}
-  Double_t tanl() const {return P[5];}
-  Double_t curv() const {return P[6];}
-  Double_t hz()   const {return P[7];}
+  Double_t x()    const {return P[0];} 
+  Double_t y()    const {return P[1];}//  local Y-coordinate of this track (reference plane)           		     
+  Double_t z()    const {return P[2];}//  local Z-coordinate of this track (reference plane)			     
+  Double_t eta()  const {return P[3];}//  (signed curvature)*(local Xc of helix axis - X current point on track)
+  Double_t ptin() const {return P[4];}//  signed invert pt [sign = sign(-qB)]					     
+  Double_t tanl() const {return P[5];}//  tangent of the track momentum dip angle
+  Double_t curv() const {return P[6];}//  signed curvature [sign = sign(-qB)]					     
+  Double_t hz()   const {return P[7];}//  Z component magnetic field in units Pt(Gev) = Hz * RCurv(cm)                  
   Double_t &x()    {return P[0];}
   Double_t &y()    {return P[1];}
   Double_t &z()    {return P[2];}
@@ -37,17 +37,6 @@ class StiNodePars {
     /// sine and cosine of cross angle
   Double_t _cosCA;
   Double_t _sinCA;
-#if __GNUC__ == 4 && __GNUC_MINOR__ >= 5 || __GNUC__ > 4 
   Double_t P[8]; // array of parameters. see below
-#else  /* don't work with gcc451 and optimization, keep for documentation */
-  union{Double_t P[1];Double_t _x;}; 
-  Double_t _y;     //  local Y-coordinate of this track (reference plane)           
-  Double_t _z;     //  local Z-coordinate of this track (reference plane)
-  Double_t _eta;   //  (signed curvature)*(local Xc of helix axis - X current point on track)
-  Double_t _ptin;  //  signed invert pt [sign = sign(-qB)]
-  Double_t _tanl;  //  tangent of the track momentum dip angle
-  Double_t _curv;  //  signed curvature [sign = sign(-qB)]
-  Double_t _hz;    //  Z component magnetic field in units Pt(Gev) = Hz * RCurv(cm)
-#endif
 };
 #endif
