3f17a09eb39f49eaaa02779af92b253c1ef11003
Victor Perevoztchikov <perev@bnl.gov>
2010-11-23 01:52:59 +0000

    Improve track drawing

--- StRoot/Stv/StvDraw.cxx
+++ StRoot/Stv/StvDraw.cxx
@@ -12,6 +12,7 @@ StvDraw *StvDraw::fgStvDraw=0;
 StvDraw::StvDraw(const char *name):StDraw3D(name)
 {
  fgStvDraw = this; mNDoIt=0;mNPow2=1;
+ SetBkColor(kWhite);
 }
 //_____________________________________________________________________________
 TObject *StvDraw::Hits(const std::vector<StvHit*> &hits, EDraw3DStyle sty)
@@ -75,11 +76,20 @@ void  StvDraw::Trak(const StvTrack *tk, EDraw3DStyle sty)
 {
   StvHits myHits;
   StvPoints  myPoits;
+  const StvNode *lNode=0,*rNode;
   for (StvNodeConstIter it = tk->begin();it != tk->end();++it) {
-    const StvNode *node = *it;
-    StvHit  *hit  = node->GetHit();
-    myPoits+=node->GetFP().P;
+    rNode = *it;
+    StvHit  *hit  = rNode->GetHit();
     if (hit) myHits+=hit;
+    if (!lNode) { myPoits+=rNode->GetFP().P;}
+    else        { Join(lNode,rNode,myPoits);}
+    if (hit)    {// make connection to hit
+      const double *P = rNode->GetFP().P;
+      const float  *H = hit->x_g();
+      float con[6] = {H[0],H[1],H[2],P[0],P[1],P[2]};
+      Line (2,con);            
+    }
+    lNode = rNode;
   }
   Hits(myHits,kUsedHit);
   Trak(myPoits,sty);
@@ -112,4 +122,33 @@ void StvDraw::Wait()
     Jnst()->UpdateModified();
     fprintf(stderr,"StvDraw::Waiting...\n");
     while(!ProcessEvents()){gSystem->Sleep(200);}; 
+}
+//_____________________________________________________________________________
+void StvDraw::Join(const StvNode *left,const StvNode *rite,StvPoints &poits)
+{
+static const double maxStep=0.1;
+  double lDir[3],rDir[3];
+  const StvNodePars &lFP = left->GetFP();
+  const StvNodePars &rFP = rite->GetFP();
+  lFP.getDir(lDir);
+  rFP.getDir(rDir);
+  THelixTrack hLeft(lFP.P,lDir,lFP._curv);
+  THelixTrack hRite(rFP.P,rDir,rFP._curv);
+  double len =  hLeft.Path(rFP.P);
+  hRite.Move(-len);
+  int nStep = fabs(len)/maxStep;
+  if (nStep <6) nStep = 6;
+  double step = len/nStep;
+  for (int is=1;is<=nStep;is++) {
+    hLeft.Move(step);
+    double dL = hRite.Path(hLeft.Pos());
+    hRite.Move(dL);
+    double wtL = pow(nStep-is,2);
+    double wtR = pow(      is,2);
+    if (is*2<nStep) {wtL=1;wtR=0;} else {wtL=0;wtR=1;}  
+    double x[3];
+    for (int i=0;i<3;i++) { 
+      x[i] = (hLeft.Pos()[i]*wtL+hRite.Pos()[i]*wtR)/(wtL+wtR);}
+    poits +=x;
+  }
 }
