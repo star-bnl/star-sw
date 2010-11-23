de632e3cb3aaf8eeb91adc3263392580c943a46f
Victor Perevoztchikov <perev@bnl.gov>
2010-11-23 01:51:04 +0000

    Increase default Xi2

--- StRoot/Stv/StvConst.cxx
+++ StRoot/Stv/StvConst.cxx
@@ -12,7 +12,7 @@ StvConst::StvConst(const char *name):TNamed(name,"")
   assert(!mgConst);
   mgConst = this;
 
-  mXi2Hit = 22;		//Xi2 to accept new hit
+  mXi2Hit = 33;		//Xi2 to accept new hit
   mXi2Vtx = 33;		//Xi2 to accept vertex
   mXi2Joi = mXi2Hit*3;	//Xi2 in Refit join left & right subtrack
   mXi2Big = mXi2Hit*3;	//Xi2 in Refit, temporary allowed Xi2.
