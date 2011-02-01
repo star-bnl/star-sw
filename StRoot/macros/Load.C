4099c6eb755dfb58c169d80b01e988c48bcd3815
Gene Van Buren <gene@bnl.gov>
2011-02-01 23:52:53 +0000

    StDetectorDbMaker before StTpcDb

--- StRoot/macros/Load.C
+++ StRoot/macros/Load.C
@@ -1,10 +1,10 @@
-// $Id: Load.C,v 1.20 2011/01/19 20:06:48 fisyak Exp $
+// $Id: Load.C,v 1.21 2011/02/01 23:52:53 genevb Exp $
 //=======================================================================
 // owner: Yuri Fisyak
 // what it does: 
 //=======================================================================
 
-void Load(Char_t *loadList="St_base,St_Tables,StChain,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StDetectorDbMaker,StTriggerDataMaker,StEvent,StEventUtilities,StBFChain"){
+void Load(Char_t *loadList="St_base,St_Tables,StChain,StDetectorDbMaker,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StTriggerDataMaker,StEvent,StEventUtilities,StBFChain"){
   TString opt(loadList);
   TString separator = "[^ ;,]+";
   TObjArray *array = opt.Tokenize(separator);
