7e9c8c7cadd2000e4c5cf5219d197dc7257ea61f
perev <>
2011-02-05 21:57:55 +0000

    Test for +ve error matrix added

--- StRoot/StvMaker/StvStarVertexFinder.cxx
+++ StRoot/StvMaker/StvStarVertexFinder.cxx
@@ -7,6 +7,7 @@
 #include "StGenericVertexMaker/StGenericVertexMaker.h"
 #include "StGenericVertexMaker/StGenericVertexFinder.h"
 #include "StMaker.h"
+#include "StvUtil/StvNodePars.h"
 
 
 //______________________________________________________________________________
@@ -84,6 +85,6 @@ int StvStarVertexFinder::GetVertex(int idx,double x[3],double e[6])
   e[0]=cov[0][0];
   e[1]=cov[1][0];e[2]=cov[1][1];
   e[3]=cov[2][0];e[4]=cov[2][1];e[5]=cov[2][2];
-
+  assert(EmxSign(3,e)>0);
   return 0;
 }
