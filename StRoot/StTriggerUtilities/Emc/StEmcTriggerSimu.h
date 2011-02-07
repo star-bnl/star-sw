96ed0e59c1b3a555a9d4d200a88f012baf0554d5
Pibero Djawotho <pibero@tamu.edu>
2011-02-07 16:25:51 +0000

    More access functions

--- StRoot/StTriggerUtilities/Emc/StEmcTriggerSimu.h
+++ StRoot/StTriggerUtilities/Emc/StEmcTriggerSimu.h
@@ -16,7 +16,6 @@ struct TriggerDefinition;
 class TCU;
 
 #include <set>
-#include <map>
 
 using namespace std;
 
@@ -36,6 +35,7 @@ public:
   int EM201output() const;
 
   // Access to individual bits of EM201 DSM ouput
+  // See http://www.star.bnl.gov/public/trg/run2009/emc_recabling.pdf
   int  BHT() const;		// (Bit 0:3) Barrel HT bits (4 bits)
   int  EHT() const;		// (Bit 4:5) Endcap HT bits (2 bits)
   int  JP1() const;		// (Bit 6) JP1, unified over the BEMC+EEMC (1 bit)
@@ -47,6 +47,15 @@ public:
   int  AJP() const;		// (Bit 12) AJP for BEMC and EEMC but NOT the boundary (1 bit)
   int BAJP() const;		// (Bit 13) BAJP for the BEMC-only patches (1 bit)
   int EAJP() const;		// (Bit 14) EAJP for the EEMC-only patches (1 bit)
+  int  JP0() const;		// (Bit 15) JP0, unified over the BEMC+EEMC
+
+  int BHT0() const;
+  int BHT1() const;
+  int BHT2() const;
+  int BHT3() const;
+
+  int EHT0() const;
+  int EHT1() const;
 
   void setHeadMaker(StMaker*) { /* dummy */ }
 
@@ -65,6 +74,15 @@ public:
   void getOverlapJetPatchAdc(int i, int& jp, int& adc) const;
 
 private:
+  // returns value of bit from x at position pos
+  int btest(int x, int pos) const { return x >> pos & 1; }
+
+  // returns n bits from x starting at position pos
+  int getbits(int x, int pos, int n) const { return x >> pos & ~(~0 << n); }
+
+  // OR x with value starting at position pos
+  void setbits(int& x, int pos, int value) const { x |= value << pos; }
+
   TDatime mDBTime;
   StBemcTriggerSimu* mBemc;
   StEemcTriggerSimu* mEemc;
@@ -72,22 +90,31 @@ private:
   DSMLayer_LD301_2009* mLD301;
   TCU* mTcu;
 
-  ClassDef(StEmcTriggerSimu,1);
+  ClassDef(StEmcTriggerSimu,0);
 };
 
 inline DSMLayer_EM201_2009* StEmcTriggerSimu::get2009_DSMLayer2_Result() { return mEM201; }
 inline DSMLayer_LD301_2009* StEmcTriggerSimu::get2009_DSMLayer3_Result() { return mLD301; }
 
-inline int  StEmcTriggerSimu::BHT() const { return EM201output()       & 0xf; }
-inline int  StEmcTriggerSimu::EHT() const { return EM201output() >>  4 & 0x3; }
-inline int  StEmcTriggerSimu::JP1() const { return EM201output() >>  6 & 0x1; }
-inline int  StEmcTriggerSimu::JP2() const { return EM201output() >>  7 & 0x1; }
-inline int StEmcTriggerSimu::BJP1() const { return EM201output() >>  8 & 0x1; }
-inline int StEmcTriggerSimu::BJP2() const { return EM201output() >>  9 & 0x1; }
-inline int StEmcTriggerSimu::EJP1() const { return EM201output() >> 10 & 0x1; }
-inline int StEmcTriggerSimu::EJP2() const { return EM201output() >> 11 & 0x1; }
-inline int  StEmcTriggerSimu::AJP() const { return EM201output() >> 12 & 0x1; }
-inline int StEmcTriggerSimu::BAJP() const { return EM201output() >> 13 & 0x1; }
-inline int StEmcTriggerSimu::EAJP() const { return EM201output() >> 14 & 0x1; }
+inline int  StEmcTriggerSimu::BHT() const { return getbits(EM201output(),0,4); }
+inline int  StEmcTriggerSimu::EHT() const { return getbits(EM201output(),4,2); }
+inline int  StEmcTriggerSimu::JP1() const { return btest(EM201output(),6); }
+inline int  StEmcTriggerSimu::JP2() const { return btest(EM201output(),7); }
+inline int StEmcTriggerSimu::BJP1() const { return btest(EM201output(),8); }
+inline int StEmcTriggerSimu::BJP2() const { return btest(EM201output(),9); }
+inline int StEmcTriggerSimu::EJP1() const { return btest(EM201output(),10); }
+inline int StEmcTriggerSimu::EJP2() const { return btest(EM201output(),11); }
+inline int  StEmcTriggerSimu::AJP() const { return btest(EM201output(),12); }
+inline int StEmcTriggerSimu::BAJP() const { return btest(EM201output(),13); }
+inline int StEmcTriggerSimu::EAJP() const { return btest(EM201output(),14); }
+inline int  StEmcTriggerSimu::JP0() const { return btest(EM201output(),15); }
+
+inline int StEmcTriggerSimu::BHT0() const { return btest(EM201output(),0); }
+inline int StEmcTriggerSimu::BHT1() const { return btest(EM201output(),1); }
+inline int StEmcTriggerSimu::BHT2() const { return btest(EM201output(),2); }
+inline int StEmcTriggerSimu::BHT3() const { return btest(EM201output(),3); }
+
+inline int StEmcTriggerSimu::EHT0() const { return btest(EM201output(),4); }
+inline int StEmcTriggerSimu::EHT1() const { return btest(EM201output(),5); }
 
 #endif // ST_EMC_TRIGGER_SIMU_H
