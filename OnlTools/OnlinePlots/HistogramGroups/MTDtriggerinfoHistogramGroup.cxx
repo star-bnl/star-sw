eb268da748c341032a9976f83d2e57e02cb8f1fe
Gene Van Buren <gene@bnl.gov>
2011-02-11 14:10:33 +0000

    MTD trigger updates

--- OnlTools/OnlinePlots/HistogramGroups/MTDtriggerinfoHistogramGroup.cxx
+++ OnlTools/OnlinePlots/HistogramGroups/MTDtriggerinfoHistogramGroup.cxx
@@ -34,55 +34,46 @@ MTDtriggerinfoHistogramGroup::MTDtriggerinfoHistogramGroup() {
   // For ROOT I/O
   memset( MTD_adc, 0, sizeof(MTD_adc));
   memset( MTD_tac, 0, sizeof(MTD_tac));
-  MTD_eastTac_vs_westTac = 0;
-  MTD_aveTac_vs_vpd_aveTac = 0;
 }
 
 MTDtriggerinfoHistogramGroup::MTDtriggerinfoHistogramGroup(const char* group, const char* subGroup, const char* trigger, const char* detector)
   : HistogramGroup(group,subGroup,trigger,detector) {
  
   char tmpchr[200];
-  sprintf(tmpchr,"MTD_east_adc");
-  MTD_adc[0]=new TH1F(tmpchr,"MTD ADC (east)",100,0,2048);
-  MTD_adc[0]->SetXTitle("ADC");
-  sprintf(tmpchr,"MTD_west_adc");
-  MTD_adc[1]=new TH1F(tmpchr,"MTD ADC (west)",100,0,2048);
-  MTD_adc[1]->SetXTitle("ADC");
+	  for (int itray=0; itray<=2; itray++){
+  sprintf(tmpchr,"MTD%d_east_adc",itray);
+  MTD_adc[itray][0]=new TH1F(tmpchr,tmpchr,100,0,2048);
+  MTD_adc[itray][0]->SetXTitle("ADC");
+  sprintf(tmpchr,"MTD%d_west_adc",itray);
+  MTD_adc[itray][1]=new TH1F(tmpchr,tmpchr,100,0,2048);
+  MTD_adc[itray][1]->SetXTitle("ADC");
 
-  sprintf(tmpchr,"MTD_east_tac");
-  MTD_tac[0]=new TH1F(tmpchr,"MTD TAC (east)",150,0,3000);
-  MTD_tac[0]->SetXTitle("TAC");
-  sprintf(tmpchr,"MTD_west_tac");
-  MTD_tac[1]=new TH1F(tmpchr,"MTD TAC (west)",150,0,3000);
-  MTD_tac[1]->SetXTitle("TAC");
-
-  sprintf(tmpchr,"MTD_eastTac_vs_westTac");
-  MTD_eastTac_vs_westTac=new TH2F(tmpchr,"MTD eastTac vs westTac",150,0,3000,150,0,3000);
-  MTD_eastTac_vs_westTac->SetXTitle("east TAC");
-  //MTD_eastTac_vs_westTac->SetYTitle("west TAC");
-
-  sprintf(tmpchr,"MTD_aveTac_vs_vpd_aveTac");
-  MTD_aveTac_vs_vpd_aveTac=new TH2F(tmpchr,"MTD aveTac vs vpd aveTac",150,0,3000,100,500,2048);
-  MTD_aveTac_vs_vpd_aveTac->SetXTitle("MTD aveTAC");
+  sprintf(tmpchr,"MTD%d_east_tac",itray);
+  MTD_tac[itray][0]=new TH1F(tmpchr,tmpchr,150,0,3000);
+  MTD_tac[itray][0]->SetXTitle("TAC");
+  sprintf(tmpchr,"MTD%d_west_tac",itray);
+  MTD_tac[itray][1]=new TH1F(tmpchr,tmpchr,150,0,3000);
+  MTD_tac[itray][1]->SetXTitle("TAC");
+	  }//tray loop over
+	  
 
 }
 
 
 MTDtriggerinfoHistogramGroup::~MTDtriggerinfoHistogramGroup() {
 
-  for (int i = 0; i < 2; ++i)delete MTD_adc[i];
-  for (int i = 0; i < 2; ++i)delete MTD_tac[i];
-  delete MTD_eastTac_vs_westTac;
-  delete MTD_aveTac_vs_vpd_aveTac;
+	for (int itray=0;itray<=2;itray++){
+  for (int i = 0; i < 2; ++i)delete MTD_adc[itray][i];
+  for (int i = 0; i < 2; ++i)delete MTD_tac[itray][i];
+	}
 }
 
 
 void MTDtriggerinfoHistogramGroup::reset() {
-
-  for (int i = 0; i < 2; ++i)MTD_adc[i]->Reset();
-  for (int i = 0; i < 2; ++i)MTD_tac[i]->Reset();
-  MTD_eastTac_vs_westTac->Reset();
-  MTD_aveTac_vs_vpd_aveTac->Reset();
+	for (int itray=0;itray<=2;itray++){
+  for (int i = 0; i < 2; ++i)MTD_adc[itray][i]->Reset();
+  for (int i = 0; i < 2; ++i)MTD_tac[itray][i]->Reset();
+	}
 }
 
 
@@ -131,53 +122,44 @@ void MTDtriggerinfoHistogramGroup::draw(TCanvas* cc) {
 
   cc->cd(); cc->SetFillColor(0);
   cc->Clear();
-  cc->Divide(2, 3);
-  cc->cd(1);
+  cc->Divide(2, 6);
+	for(int itray=0;itray<=2;itray++){
+  cc->cd(itray*4+1);
 
-  MTD_adc[0]->SetFillColor(19);
-  MTD_adc[0]->GetYaxis()->SetLabelSize(0.07);
-  MTD_adc[0]->GetXaxis()->SetLabelSize(0.055);
-  gPad->SetLogy(MTD_adc[0]->GetEntries() ? 1 : 0);
-  MTD_adc[0]->Draw();
-  float hmax=MTD_adc[0]->GetMaximum();
+  MTD_adc[itray][0]->SetFillColor(19);
+  MTD_adc[itray][0]->GetYaxis()->SetLabelSize(0.07);
+  MTD_adc[itray][0]->GetXaxis()->SetLabelSize(0.055);
+  gPad->SetLogy(MTD_adc[itray][0]->GetEntries() ? 1 : 0);
+  MTD_adc[itray][0]->Draw();
+  float hmax=MTD_adc[itray][0]->GetMaximum();
   labely.DrawLatex(-200, 0.6*hmax, "Counts");
   
-  cc->cd(2);
-  MTD_adc[1]->SetFillColor(19);
-  MTD_adc[1]->GetYaxis()->SetLabelSize(0.07);
-  MTD_adc[1]->GetXaxis()->SetLabelSize(0.055);
-  gPad->SetLogy(MTD_adc[1]->GetEntries() ? 1 : 0);
-  MTD_adc[1]->Draw();
-  hmax=MTD_adc[1]->GetMaximum();
+  cc->cd(itray*4+2);
+  MTD_adc[itray][1]->SetFillColor(19);
+  MTD_adc[itray][1]->GetYaxis()->SetLabelSize(0.07);
+  MTD_adc[itray][1]->GetXaxis()->SetLabelSize(0.055);
+  gPad->SetLogy(MTD_adc[itray][1]->GetEntries() ? 1 : 0);
+  MTD_adc[itray][1]->Draw();
+  hmax=MTD_adc[itray][1]->GetMaximum();
   labely.DrawLatex(-200, 0.6*hmax, "Counts");
 
-  cc->cd(3);
-  MTD_tac[0]->SetFillColor(19);
-  MTD_tac[0]->GetYaxis()->SetLabelSize(0.07);
-  MTD_tac[0]->GetXaxis()->SetLabelSize(0.055);
-  MTD_tac[0]->Draw();
-  hmax=MTD_tac[0]->GetMaximum();
+  cc->cd(itray*4+3);
+  MTD_tac[itray][0]->SetFillColor(19);
+  MTD_tac[itray][0]->GetYaxis()->SetLabelSize(0.07);
+  MTD_tac[itray][0]->GetXaxis()->SetLabelSize(0.055);
+  MTD_tac[itray][0]->Draw();
+  hmax=MTD_tac[itray][0]->GetMaximum();
   labely.DrawLatex(-300, 0.6*hmax, "Counts");
 
-  cc->cd(4);
-  MTD_tac[1]->SetFillColor(19);
-  MTD_tac[1]->GetYaxis()->SetLabelSize(0.07);
-  MTD_tac[1]->GetXaxis()->SetLabelSize(0.055);
-  MTD_tac[1]->Draw();
-  hmax=MTD_tac[1]->GetMaximum();
+  cc->cd(itray*4+4);
+  MTD_tac[itray][1]->SetFillColor(19);
+  MTD_tac[itray][1]->GetYaxis()->SetLabelSize(0.07);
+  MTD_tac[itray][1]->GetXaxis()->SetLabelSize(0.055);
+  MTD_tac[itray][1]->Draw();
+  hmax=MTD_tac[itray][1]->GetMaximum();
   labely.DrawLatex(-300, 0.6*hmax, "Counts");
+	}
 
-  cc->cd(5);
-  MTD_eastTac_vs_westTac->GetYaxis()->SetLabelSize(0.07);
-  MTD_eastTac_vs_westTac->GetXaxis()->SetLabelSize(0.055);
-  MTD_eastTac_vs_westTac->Draw("col");
-  labely.DrawLatex(-300, 0.6*2048, "west TAC");
-
-  cc->cd(6);
-  MTD_aveTac_vs_vpd_aveTac->GetYaxis()->SetLabelSize(0.07);
-  MTD_aveTac_vs_vpd_aveTac->GetXaxis()->SetLabelSize(0.055);
-  MTD_aveTac_vs_vpd_aveTac->Draw("col");
-  labely.DrawLatex(-300, 600+0.6*1500, "VPD aveTac");
 
   cc->Update();
 
@@ -187,25 +169,50 @@ void MTDtriggerinfoHistogramGroup::draw(TCanvas* cc) {
 bool MTDtriggerinfoHistogramGroup::fill(evpReader* evp, char* datap) { 
   
   StTriggerData* trgd = TriggerData::Instance(datap);
-  if(!trgd) return false;  
+  if(!trgd){ 
+	  cout<<"NO TRIGGER DATA"<<endl;
+	  return false;  
+  }
 
-  float east_adc=trgd->mtdAdc(east,0);
-  float west_adc=trgd->mtdAdc(west,0);
-  float east_tac=trgd->mtdTdc(east,0);
-  float west_tac=trgd->mtdTdc(west,0);
+  //MTD for run 11
+  //0 for MTD-1; 1 for MTD-26, the center tray; 2 for MTD-26, the left and right trays. 
+	float west_adc[3];
+	float west_tac[3];
+	float east_adc[3];
+	float east_tac[3];
+	for(int itray=0;itray<=2;itray++){ 
+		west_adc[itray]=-9999.;
+		west_tac[itray]=-9999.;
+		east_adc[itray]=-9999.;
+		east_tac[itray]=-9999.;
+	}
+	
+	 west_adc[0]= trgd->mtdAtAddress(8, 0);
+   west_tac[0]= trgd->mtdAtAddress(12, 0);
+   east_adc[0]= trgd->mtdAtAddress(9, 0);
+   east_tac[0]= trgd->mtdAtAddress(13, 0);
 
-  if(east_adc>0)MTD_adc[0]->Fill(east_adc);
-  if(west_adc>0)MTD_adc[1]->Fill(west_adc);
-  if(east_tac>0)MTD_tac[0]->Fill(east_tac);
-  if(west_tac>0)MTD_tac[1]->Fill(west_tac);
+   west_adc[1]= trgd->mtdAtAddress(16, 0);
+   west_tac[1]= trgd->mtdAtAddress(20, 0);
+   east_adc[1]= trgd->mtdAtAddress(17, 0);
+   east_tac[1]= trgd->mtdAtAddress(21, 0);
 
-  if((east_tac+west_tac)>0) MTD_eastTac_vs_westTac->Fill(east_tac,west_tac);
+   west_adc[2]= trgd->mtdAtAddress(24, 0);
+   west_tac[2]= trgd->mtdAtAddress(28, 0);
+   east_adc[2]= trgd->mtdAtAddress(25, 0);
+   east_tac[2]= trgd->mtdAtAddress(29, 0);
 
-  int vpd_maxTacEast = trgd->vpdEarliestTDC((StBeamDirection)0);
-  int vpd_maxTacWest = trgd->vpdEarliestTDC((StBeamDirection)1);
+for (int itray=0;itray<=2; itray++){
+  if(east_adc[itray]>0)MTD_adc[itray][0]->Fill(east_adc[itray]);
+  if(west_adc[itray]>0)MTD_adc[itray][1]->Fill(west_adc[itray]);
+  if(east_tac[itray]>0)MTD_tac[itray][0]->Fill(east_tac[itray]);
+  if(west_tac[itray]>0)MTD_tac[itray][1]->Fill(west_tac[itray]);
 
-  //cout<<" eastadc="<<east_adc<<" westadc="<<west_adc<<" easttac="<<east_tac<<" westtac="<<west_tac<<" vpdeast="<<vpd_maxTacEast<<" vpdwest="<<vpd_maxTacWest<<endl;
-  if((east_tac+west_tac)>0)MTD_aveTac_vs_vpd_aveTac->Fill( (east_tac+west_tac)/2.,(vpd_maxTacEast+vpd_maxTacWest)/2.);
+	}
+
+ // for (int itray=0;itray<=2; itray++){cout <<"itray="<<itray<<endl;cout<<"eastadc="<<east_adc[itray]<<" westadc[itray]="<<west_adc[itray]<<" easttac="<<east_tac[itray]<<" westtac0="<<west_tac[itray]<<endl;}
+
+	//if (west_adc[1]>0) cout<<"west_adc[1]="<<west_adc[1]<<"!!!!!!!!!!!<"endl;
 
   return true;
 
