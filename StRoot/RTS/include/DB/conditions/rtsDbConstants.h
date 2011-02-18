d5bf41daebf31fa9092586c8112c5ef5244fcb0a
jml <>
2011-02-18 19:45:53 +0000

    update number of triggers

--- StRoot/RTS/include/DB/conditions/rtsDbConstants.h
+++ StRoot/RTS/include/DB/conditions/rtsDbConstants.h
@@ -4,7 +4,7 @@
 #define DB_MAX_STR_LEN 64
 #define DB_MAX_NODES 200
 #define DB_MAX_DICT_ENTRIES 1500
-#define DB_TRIGGERS_MAX 32
+#define DB_TRIGGERS_MAX 64
 //#define DB_TW_MAX (1<<16)
 //#define DB_PW_MAX (1<<12)
 #define DB_PW_COND_MAX (DB_TRIGGERS_MAX*4)
