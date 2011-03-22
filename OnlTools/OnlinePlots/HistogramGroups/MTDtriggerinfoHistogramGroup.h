f15e3bd73cbb17e62c260f06dbf8da91e845ba70
genevb <>
2011-03-22 18:43:59 +0000

    more unique defined name: ntray -> nMTDtrays

--- OnlTools/OnlinePlots/HistogramGroups/MTDtriggerinfoHistogramGroup.h
+++ OnlTools/OnlinePlots/HistogramGroups/MTDtriggerinfoHistogramGroup.h
@@ -3,7 +3,7 @@
 
 #include "Rtypes.h"
 #include "HistogramGroup.h"
-#define ntray 3
+#define nMTDtrays 3
 
 class MTDtriggerinfoHistogramGroup : public HistogramGroup {
 
@@ -18,8 +18,8 @@ public:
   virtual void draw(TCanvas* cc); 
 
 private:
-  TH1* MTD_adc[ntray][2];// 3 trays; 0 east, 1 east
-  TH1* MTD_tac[ntray][2];
+  TH1* MTD_adc[nMTDtrays][2];// 3 trays; 0 east, 1 west
+  TH1* MTD_tac[nMTDtrays][2];
 
   ClassDef(MTDtriggerinfoHistogramGroup,1) ;
 
