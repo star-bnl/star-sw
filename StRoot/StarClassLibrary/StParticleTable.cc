f922389a49f3c1ac6522daef14ec6e472b2b6293
Jason C. Webb <jwebb@bnl.gov>
2011-03-25 18:17:46 +0000

    Updates to StParticleTable and additions to STAR Class Library
    (1) resolve ticket 2097
    (2) include all particles/deays defined in gstar_part.g
    (3) added few anti-nuclei in anticipation of future needs.
    (3) added the geantino for completeness

--- StRoot/StarClassLibrary/StParticleTable.cc
+++ StRoot/StarClassLibrary/StParticleTable.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StParticleTable.cc,v 1.11 2011/03/15 22:41:43 jwebb Exp $
+ * $Id: StParticleTable.cc,v 1.12 2011/03/25 18:17:46 jwebb Exp $
  *
  * Author: Thomas Ullrich, May 99 (based on Geant4 code, see below) 
  ***************************************************************************
@@ -14,6 +14,14 @@
  ***************************************************************************
  *
  * $Log: StParticleTable.cc,v $
+ * Revision 1.12  2011/03/25 18:17:46  jwebb
+ * Updates to StParticleTable and additions to STAR Class Library
+ *
+ * (1) resolve ticket 2097
+ * (2) include all particles/deays defined in gstar_part.g
+ * (3) added few anti-nuclei in anticipation of future needs.
+ * (3) added the geantino for completeness
+ *
  * Revision 1.11  2011/03/15 22:41:43  jwebb
  * (1) Added Omega+/- and Xi+/- embedding definitions.
  * (2) 1st attempt at improving the Doxygenization of the particle table.
@@ -58,10 +66,14 @@
 #include "StParticleDefinition.hh"
 
 #include "StarPDGEncoding.hh"
+#define kUndefined _undefined_particle_id++
+long _undefined_particle_id = 2000000000;
 
 #if defined (__SUNPRO_CC) && __SUNPRO_CC < 0x500
 #include <ospace/stl/src/treeaux.cpp> // CC4.2 with ObjectSpace only
 #endif
+
+
 
 StParticleTable* StParticleTable::mParticleTable = 0;
 
@@ -84,8 +96,9 @@ StParticleTable::StParticleTable()
     }
 
 
-    ///@addtogroup STANDARD
+    ///@addtogroup GEANT_STANDARD
     ///@{
+    /// Geant3 Standard Particle Defintions
     Geant2Pdg(1, 22, gamma);      
     Geant2Pdg(2, -11, e+);     // e+
     Geant2Pdg(3, 11, e-);      // e-
@@ -130,47 +143,87 @@ StParticleTable::StParticleTable()
     Geant2Pdg(42, 24,    W+ );     // W+  (STAR def.)
     Geant2Pdg(43, -24,   W- );    // W-  (STAR def.)
     Geant2Pdg(44, 23,    Z0 );     // Z0  (STAR def.)
+
+    Geant2Pdg(45, kUndefined, Deuteron ); // The deuteron
+    Geant2Pdg(46, kUndefined, Triton )  ; // The triton
+    Geant2Pdg(47, kUndefined, Alpha )   ; // The alpha
+    Geant2Pdg(48, kUndefined, Geantino ); // The mythical geantino 
+    Geant2Pdg(49, kUndefined, Helium3  ); // Helium3
+    Geant2Pdg(50, 22,         Cerenkov ); // Cerenkov photons
     ///@} 
 
-    Geant2Pdg(52, kHyperTriton, HyperTriton ); // Star def. HyperTriton (fake pdg id)
+    
+    ///@addtogroup DEPRECATED
+    ///@{
+    /// Deprecated definitions.  Please use alternate IDs provided below.
+       Geant2Pdg(52, kHyperTriton, HyperTriton ); // Star def. HyperTriton (fake pdg id)
+    ///@}
 
-    Geant2Pdg( 60, +413, DStar+ ); // D*+
-    Geant2Pdg( 61, -413, DStar- ); // D*-
-    Geant2Pdg( 62, +423, DStar0 ); // D*0
-    Geant2Pdg( 63, -423, DStar0Bar ); // D*0 bar
+    ///@addtogroup STAR_DEFINITIONS
+    ///@{
+    /// STAR Revised Particle Definitions
+       Geant2Pdg( 60, +413, DStar+ ); // D*+
+       Geant2Pdg( 61, -413, DStar- ); // D*-
+       Geant2Pdg( 62, +423, DStar0 ); // D*0
+       Geant2Pdg( 63, -423, DStar0Bar ); // D*0 bar      
 
-    Geant2Pdg(70, +521, B+);    // B+ meson
-    Geant2Pdg(71, -521, B-);    // B- meson
-    Geant2Pdg(72, +511, B0);    // B0 meson
-    Geant2Pdg(73, -511, B0Bar );    // B0-bar meson
+       Geant2Pdg(70, +521, B+);    // B+ meson
+       Geant2Pdg(71, -521, B-);    // B- meson
+       Geant2Pdg(72, +511, B0);    // B0 meson
+       Geant2Pdg(73, -511, B0Bar );    // B0-bar meson
+    ///@}
 
-    Geant2Pdg(150, 223, omega);   // omega meson (STAR def.)
-    Geant2Pdg(151, 333, phi);   // phi meson (STAR def.)
-    Geant2Pdg(152, 113, rho);   // rho meson (STAR def.)
-    Geant2Pdg(153, 213, rho+);   // rho+ meson (STAR def.)
-    Geant2Pdg(154, -213, rho-);  // rho- meson (STAR def.)
-    Geant2Pdg(155, 311, K0);   // K0 (STAR def.)
-    Geant2Pdg(156, -311, K0Bar);  // anti_K0 (STAR def.)
 
-    Geant2Pdg( 160, 443, JPsi );     // JPsi
-    Geant2Pdg( 149, kDalitz, Pi0Dalitz ); // pi0 --> e+ e- gamma
+    ///@addtogroup NONSTANDARD_DECAY_MODES
+    ///@{
+    /// STAR Non-Standard Decay Modes 
+       Geant2Pdg( 97, -3122, LambdaBar --> pbar + pi+ );
+       Geant2Pdg( 98, +3122, Lambda --> p + pi- );       
+    ///@}
 
-    Geant2Pdg( 161,    553, Upsilon1S); // Upsilon(1S)
-    Geant2Pdg( 162, 100553, Upsilon2S); // Upsilon(2S)
-    Geant2Pdg( 163, 200553, Upsilon3S); // Uspilon(3S)
-    Geant2Pdg( 164,    553, Upsilon1S); // Upsilon(1S) -- mu+ mu- channel w/ incorrect partial width
-    /// mu+mu- channel 
-    Geant2Pdg( 165, 100553, Upsilon2S); // Upsilon(2S) -- mu+ mu- channel w/ incorrect partial width
-    /// mu+mu- channel 
-    Geant2Pdg( 166, 200553, Upsilon3S); // Uspilon(3S) -- mu+ mu- channel w/ incorrect partial width
-    /// mu+mu- channel 
+    Geant2Pdg( 149, kDalitz, Pi0 --> e+ e- gamma ); // pi0 --> e+ e- gamma
 
-    Geant2Pdg( 701, +3224, Sigma(1385)+ ); // Sigma 1385 +
-    Geant2Pdg( 702, +3114, Sigma(1385)- ); // Sigma 1385 -
-    Geant2Pdg( 703, -3114, SigmaBar(1385)+ ); // Sigma 1385 plus bar
-    Geant2Pdg( 704, -3224, SigmaBar(1385)- ); // Sigma 1385 minus bar 
+    ///@addtogroup STAR_DEFINITIONS
+    ///@{
+       Geant2Pdg(150, 223, omega);   // omega meson (STAR def.)
+       Geant2Pdg(151, 333, phi);   // phi meson (STAR def.)
+       Geant2Pdg(152, 113, rho);   // rho meson (STAR def.)
+       Geant2Pdg(153, 213, rho+);   // rho+ meson (STAR def.)
+       Geant2Pdg(154, -213, rho-);  // rho- meson (STAR def.)
+       Geant2Pdg(155, 311, K0);   // K0 (STAR def.)
+       Geant2Pdg(156, -311, K0Bar);  // anti_K0 (STAR def.)
+    ///@}
+      
 
-    Geant2Pdg( 707, 100311, K0-->pi+pi- ); // K0 --> pi+ pi- 
+
+    ///@addtogroup DIELECTRONS
+    ///@{
+    /// Quarkonia in dielectron channel
+
+       Geant2Pdg( 160, 443, JPsi );     // JPsi
+    
+       Geant2Pdg( 161,    553, Upsilon1S); // Upsilon(1S)
+       Geant2Pdg( 162, 100553, Upsilon2S); // Upsilon(2S)
+       Geant2Pdg( 163, 200553, Upsilon3S); // Uspilon(3S)
+    ///@}
+
+    ///@addtogroup DIMUONS
+    ///@{
+    /// Quarkonia in dimuon channel
+       Geant2Pdg( 164,    553, Upsilon1S); // Upsilon(1S) -- mu+ mu- channel w/ incorrect partial width
+       Geant2Pdg( 165, 100553, Upsilon2S); // Upsilon(2S) -- mu+ mu- channel w/ incorrect partial width
+       Geant2Pdg( 166, 200553, Upsilon3S); // Uspilon(3S) -- mu+ mu- channel w/ incorrect partial width
+    ///@}
+
+    ///@addtogroup NONSTANDARD_DECAY_MODES
+    ///@{
+       Geant2Pdg( 701, +3224, Sigma(1385)+ ); // Sigma 1385 +
+       Geant2Pdg( 702, +3114, Sigma(1385)- ); // Sigma 1385 -
+       Geant2Pdg( 703, -3114, SigmaBar(1385)+ ); // Sigma 1385 plus bar
+       Geant2Pdg( 704, -3224, SigmaBar(1385)- ); // Sigma 1385 minus bar 
+       Geant2Pdg( 707, 100311, K0-->pi+pi- ); // K0 --> pi+ pi- 
+    ///@}
+
 
     Geant2Pdg( +995, +20003122, Lambda(1520) ); // Lambda 1520 
     Geant2Pdg( +996, -20003122, LamdaBar(1520) ); // Lambda 1520 
@@ -179,31 +232,21 @@ StParticleTable::StParticleTable()
     /// Embedding particle definitions
     ///@{
 
-    Geant2Pdg(40001,  3334, Omega-); 
-    /// \f$ \Omega^{-} \rightarrow \Lambda K^{-} \f$ 100%
-    /// \f$ \Lambda    \rightarrow p \pi^{-} \$ 100%
-    ///
-    Geant2Pdg(40002, -3334, Omega+); 
-    /// \f$ \Omega^{+} \rightarrow \bar{\Lambda} K^{+} \f$ 100%
-    /// \f$ \bar{\Lambda}    \rightarrow \bar{p} \pi^{+} \$ 100%
-    ///
-
-    Geant2Pdg( 40003, +3312, XiMinus );
-    /// \f$ \Xi^{-} \rightarrow \Lambda \pi^{-} \f$ 100%
-    /// \f$ \Lambda    \rightarrow p \pi^{-} \$ 100%
-    ///
-    Geant2Pdg( 40004, -3312, XiPlus  );
-    /// \f$ \Xi^{+} \rightarrow \bar{\Lambda} \pi^{+} \f$ 100%
-    /// \f$ \bar{\Lambda}    \rightarrow \bar{p} \pi^{+} \$ 100%
-    ///
-    Geant2Pdg( 40005, +3322, XiZero );
-    /// \f$ \Xi^{0} \rightarrow \Lambda \pi^{0} \$
-    /// \f$ \Lambda    \rightarrow p \pi^{-} \$ 100%
-    ///
-    Geant2Pdg( 40006, +3322, XiZeroBar );
-    /// \f$ \bar{\Xi}^{0} \rightarrow \bar{\Lambda} \pi^{0} \$
-    /// \f$ \bar{\Lambda}    \rightarrow \bar{p} \pi^{+} \$ 100%
-    ///
+       Geant2Pdg(10017, 221, eta --> e+ e- gamma);
+       Geant2Pdg(10018, 3122, lambda --> p + pi- );
+       Geant2Pdg(10026,-3122, lambdaBar --> pbar + pi+ );
+       Geant2Pdg(10039,  431,  D_s_+ --> phi + pi+ w/ phi --> K+ K- );
+       Geant2Pdg(10040, -431,  D_s_- --> phi + pi- w/ phi --> K+ K- );
+       Geant2Pdg(10150,  223,  omega --> e+ e- );
+       Geant2Pdg(10151,  333,  phi --> K+ K- );
+       Geant2Pdg(11151,  333,  phi --> e+ e- );
+             
+       Geant2Pdg(40001,  3334, Omega-); 
+       Geant2Pdg(40002, -3334, Omega+); 
+       Geant2Pdg( 40003, +3312, XiMinus );       
+       Geant2Pdg( 40004, -3312, XiPlus  );      
+       Geant2Pdg( 40005, +3322, XiZero );       
+       Geant2Pdg( 40006, +3322, XiZeroBar );
 
     ///@}
 
