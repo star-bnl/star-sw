9d403d143e17d8cc89a4212cfad098e9fd74ef99
perev <>
2011-04-01 20:55:08 +0000

    timesUsed logic modified

--- StRoot/Stv/StvHitter.cxx
+++ StRoot/Stv/StvHitter.cxx
@@ -26,9 +26,9 @@ const StvHits *StvHitter::GetHits(const StvNodePars *pars, const float gate[2])
 {
   mHits.clear();
   const StHitPlane *myHitPlane = StTGeoHelper::Inst()->GetCurrentHitPlane();
-  if (!myHitPlane) 		return 0;
-  if (!myHitPlane->GetNHits()) 	return &mHits;
-  if (mHitPlane == myHitPlane)  return 0;
+  if (!myHitPlane) 		return 0;	//no sensitive volume there
+  if (mHitPlane == myHitPlane)  return 0;	//hit plane was already used
+  if (!myHitPlane->GetNHits())	return &mHits;	//it is sensitive but no hits
   mHitPlane = myHitPlane;
   mHitMap  = mHitPlane->GetHitMap();
   float xNode[3]={pars->_x,pars->_y,pars->_z};
@@ -40,6 +40,7 @@ const StvHits *StvHitter::GetHits(const StvNodePars *pars, const float gate[2])
 
   for (int j=0;j<3;j++) {tau+=(mOrg[j]-xNode[j])*ort[j];
                          den+= mom[j]           *ort[j];}
+  if (fabs(den)<1e-2) 		return 0; // track is parallel to hit plane
   tau/=den;
   for (int j=0;j<3;j++) {xNode[j]+=mom[j]*tau;}
   float lNode[3],lim[2][2];
@@ -52,7 +53,7 @@ const StvHits *StvHitter::GetHits(const StvNodePars *pars, const float gate[2])
   for (StMultiKeyNode *node=0;(node = *(*fMultiIter)) ;++(*fMultiIter)) 
   { 
     StvHit *nexHit = (StvHit*)node->GetObj();
-//??????    if (nexHit->timesUsed()) continue;
+    if (nexHit->timesUsed()) continue;
     mHits.push_back(nexHit);
   }
   return &mHits;
