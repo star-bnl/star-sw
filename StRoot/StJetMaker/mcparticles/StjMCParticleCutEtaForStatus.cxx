2413f488069009bb4b028216f4d28ceca64447bc
cvs2git <cvs2git>
2011-04-04 15:44:25 +0000

    This commit was manufactured by cvs2git to create branch 'SL11b_dbpatch'.

--- StRoot/StJetMaker/mcparticles/StjMCParticleCutEtaForStatus.cxx
+++
@@ -1,6 +0,0 @@
-// $Id: StjMCParticleCutEtaForStatus.cxx,v 1.1 2008/11/27 07:40:02 tai Exp $
-// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-#include "StjMCParticleCutEtaForStatus.h"
-
-ClassImp(StjMCParticleCutEtaForStatus)
- 
--- StRoot/StSpectraPool/StRareMaker/StAcceptAllL3Tracks.cxx
+++
@@ -1,7 +0,0 @@
-#include "StAcceptAllL3Tracks.h"
-#include "StEventTypes.h"
-
-ClassImp(StAcceptAllL3Tracks)
-
-int StAcceptAllL3Tracks::Accept(StGlobalTrack* event){return 1;}
-void StAcceptAllL3Tracks::Report(){cout << "StAcceptAllL3Tracks: No track cuts for uDST" << endl;}
