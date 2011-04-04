6e6f39a6f51d2361af636e497fe5d7a3157ee0da
cvs2git <cvs2git>
2011-04-04 15:44:26 +0000

    This commit was manufactured by cvs2git to create branch 'dblib_160patch'.

--- StRoot/StJetMaker/jets/StjJetCutDetectorEta.cxx
+++
@@ -1,6 +0,0 @@
-// $Id: StjJetCutDetectorEta.cxx,v 1.1 2008/09/12 00:32:49 tai Exp $
-// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-#include "StjJetCutDetectorEta.h"
-
-ClassImp(StjJetCutDetectorEta)
- 
--- StRoot/StJetMaker/scratch/cresedscr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while read old new;do
-    echo 's/\([ <:(~]\|^\)'$old'\([ >&*;:(),]\)/\1'$new'\2/g'
-done < cha.txt
-
