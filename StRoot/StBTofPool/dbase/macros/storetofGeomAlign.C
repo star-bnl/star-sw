5f3ad60044e403f3b1da87d4ce120215383df7d8
Frank Geurts <geurts@rice.edu>
2011-08-04 19:15:23 +0000

    Run-11 updates

--- StRoot/StBTofPool/dbase/macros/storetofGeomAlign.C
+++ StRoot/StBTofPool/dbase/macros/storetofGeomAlign.C
@@ -52,13 +52,14 @@ void storetofGeomAlign()
 //  TString ZStoreTime = "2008-11-01 00:00:00";  // initial for tuning
 //  TString ZStoreTime = "2008-11-01 00:00:01";  // y9
 //  TString ZStoreTime = "2008-11-01 00:00:02";  // y9 - OK update on localy offset
-                                               // local z window mean shifted to 0
-//  TString ZStoreTime = "2009-02-01 00:00:00"; // final numbers with RFF low L data
+                                                 // local z window mean shifted to 0
+//  TString ZStoreTime = "2009-02-01 00:00:00";  // final numbers with RFF low L data
 //  TString ZStoreTime = "2009-11-01 00:00:00";  // initial for Run10
 //  TString ZStoreTime = "2009-11-01 00:00:01";  //  Run10 from low luminosity data  
 //  TString ZStoreTime = "2009-12-14 00:00:02";  //  Run10 update with final TPC calib (7GeV based) 
-//    TString ZStoreTime = "2009-12-14 00:00:03";  //  Run10 200GeV update for production (note: TPC sector 20 problem)
-  TString ZStoreTime = "2010-04-08 15:00:00";  //  Run10 update with final TPC calib 7GeV based) 
+//  TString ZStoreTime = "2009-12-14 00:00:03";  //  Run10 200GeV update for production (note: TPC sector 20 problem)
+//  TString ZStoreTime = "2010-04-08 15:00:00";  //  Run10 update with final TPC calib 7GeV based) 
+  TString ZStoreTime = "2010-12-20 00:00:01";  //  Run11 update with final TPC calib (19.6GeV based) 
 
   //-- add table to the container with descriptor given by Database
   StDbTable* tofGeomAlign = configNode->addDbTable("tofGeomAlign");
@@ -66,12 +67,11 @@ void storetofGeomAlign()
   //-- fill structures & store times
   tofGeomAlign_st *tofAlign = new tofGeomAlign_st[NMAX];
 
-// year10
+// year10+
   float shifty[120], shiftz[120], shiftx[120];
   ifstream inData;
-  //inData.open("geomAlign_200GeV_04022010.txt");
-  inData.open("dat/run10/alignment/geomAlign_run10_7GeV_100819.txt");
-  //inData.open("dat/run10/alignment/geomAlign_200GeV_10132010.txt");
+  //inData.open("dat/run10/alignment/geomAlign_run10_7GeV_100819.txt");
+  inData.open("data/run11/alignment/geomAlign_19GeV_110804.txt");
   for(int i=0;i<120;i++) {
     inData >> shifty[i] >> shiftz[i] >> shiftx[i];
   }
