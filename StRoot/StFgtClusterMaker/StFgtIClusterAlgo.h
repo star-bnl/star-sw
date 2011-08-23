27ebcf3b3ff05f991035c4c6de9dc96b91e87e9a
avossen <>
2011-08-23 03:05:31 +0000

    *** empty log message ***

---
+++ StRoot/StFgtClusterMaker/StFgtIClusterAlgo.h
@@ -0,0 +1,15 @@
+// $Id: StFgtIClusterAlgo.h,v 1.1 2011/08/23 03:05:09 avossen Exp $
+// $Log: StFgtIClusterAlgo.h,v $
+// Revision 1.1  2011/08/23 03:05:09  avossen
+// *** empty log message ***
+//
+//
+//author Anselm Vossen
+//
+//abstract base class for cluster algorithm implementation
+//
+//
+class StFgtIClusterAlgo
+{
+  virtual void doClustering()=0;
+}
--- StRoot/StFgtRawMaker/StFgtRawMaker.cxx
+++ StRoot/StFgtRawMaker/StFgtRawMaker.cxx
@@ -3,3 +3,9 @@
 //
 //
 //
+mEvent = (StEvent*)GetInputDS("StEvent");
+mEvent->fgtRawCollection();
+
+StRtsTable* rts_tbl = this->GetNextDaqElement("fgt/adc");
+if(rts_tbl)
+  mFgtRawData=(fgt_adc_t*)*rts_tbl.begin();
