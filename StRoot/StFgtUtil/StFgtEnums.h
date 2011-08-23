13731e818989ad587338bc1587c9cca43b93ee44
wwitzke <>
2011-08-23 13:20:56 +0000

    First attempt to add StFgtGeom.h and .cxx, as well as place holders for StFgtDb.h and .cxx. Also, made modifications to StFgtGeomDefs.h and .cxx so that they would compile. Finally, may have added other things along with these that were not already in the repository.

---
+++ StRoot/StFgtUtil/StFgtEnums.h
@@ -0,0 +1,19 @@
+/*
+ * Some basic enums the FGT
+ *
+ * \author S. Gliske (sgliske@anl.gov)
+ *
+ */
+
+#ifndef _ST_FGT_ENUMS_H_
+#define _ST_FGT_ENUMS_H_
+
+enum StFgtLayer_t { FGT_R_LAYER = 'R', FGT_PHI_LAYER = 'P' };
+
+#endif
+
+/*
+ * $ Id: $
+ * $ Log: $
+ *
+ */
