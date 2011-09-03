5f0d5a9417e7726df3d755d62623a5befdb0598f
pibero <>
2011-09-03 01:00:56 +0000

    Load fastjet shared lib

--- StRoot/StJetMaker/macros/RunJetFinder2009pro.C
+++ StRoot/StJetMaker/macros/RunJetFinder2009pro.C
@@ -36,6 +36,7 @@ void RunJetFinder2009pro(int nevents = 1e6,
   gSystem->Load("StTriggerUtilities");
   gSystem->Load("StMCAsymMaker");
   gSystem->Load("StRandomSelector");
+  gSystem->Load("libfastjet.so");
   gSystem->Load("StJetFinder");
   gSystem->Load("StJetSkimEvent");
   gSystem->Load("StJets");
