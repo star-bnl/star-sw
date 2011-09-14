606bf49633ab6e067d70f524fa7adccb1dd30c6d
cvs2git <cvs2git>
2011-09-14 15:58:13 +0000

    This commit was manufactured by cvs2git to create tag 'Wxsec2011PRD'.

--- StRoot/St_base/St_HP_aCC.h
+++
@@ -1,3 +0,0 @@
-#ifndef St_HP_aCC_hh
-#define St_HP_aCC_hh
-#endif
--- StarVMC/StarAgmlLib/AgSTAR.cxx
+++
@@ -1,8 +0,0 @@
-#include "AgSTAR.h"
-#include "AgBlock.h"
-
-Int_t agexist( TString name )
-{
-  if ( AgBlock::Find(name) ) { return 1; }
-  return 0;
-}
