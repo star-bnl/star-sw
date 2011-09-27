63155638effe939c5152456aa8c6a0bcf5400218
rfatemi <>
2011-09-27 21:42:22 +0000

    only here to support the .h

---
+++ StRoot/StFgtUtil/geometry/StFgtCosmicTestStandGeom.cxx
@@ -0,0 +1,15 @@
+/*  StFgtGeom.cxx
+ *
+ *  FGT geometry class implementation.
+ *
+ *  \author W. Witzke (wowitz0@uky.edu)
+ *
+ */
+
+#include "StFgtCosmicTestStandGeom.h"
+
+/*
+ *  $ Id: $
+ *  $ Log: $
+ *
+ */
