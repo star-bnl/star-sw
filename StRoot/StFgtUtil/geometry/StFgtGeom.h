13a58edaec93fe52fbb3c9e809633ab5e8fc89cd
Jan Balewski <balewski@lbl.gov>
2011-09-28 20:58:32 +0000

    matching geometry to reality

--- StRoot/StFgtUtil/geometry/StFgtGeom.h
+++ StRoot/StFgtUtil/geometry/StFgtGeom.h
@@ -248,6 +248,7 @@ class StFgtGeom
 
 	//  Jan's necessary functions start here.  These were written by Jan,
 	//  modified slightly by me.
+	//  Jan: I have adjusted the dimensions to match FGT as build, September, 2011
 	static double Rin()	{ return kFgtRin; }
 	static double Rmid()	{ return kFgtRmid; }
 	static double Rout()	{ return kFgtRout; }
@@ -270,9 +271,9 @@ class StFgtGeom
 	static int radStripLOCId_number() { return mRadStripLOCId_number; }
 	static int phiStripLOCId_number() { return mPhiStripLOCId_number; }
 
-	static inline double phiQuadXaxis(int iquad);
-	static inline bool inDisc( TVector3 rLab );
-	static inline int getQuad( double phiLab );
+	static  double phiQuadXaxis(int iquad);
+	static  bool inDisc( TVector3 rLab );
+	static  int getQuad( double phiLab );
 
 	//  This is NOT a candidate for inlining.  This returns false if it is
 	//  out of range.
@@ -282,17 +283,19 @@ class StFgtGeom
 	    int dbg=0
 	);
 
-	//  Jan's definitions.
-	static const double kFgtRout		= 37.1;	    //	cm
-	static const double kFgtRmid		= 18.8;	    //	cm
-	static const double kFgtRin		= 11.5;	    //	cm
-	static const double kFgtRadPitch	=  0.08;    //	800 mu
-	static const double kFgtPhiPitch	=  0.06;    //	600 mu
-	static const double kFgtDeadQuadEdge	=  0.1;	    //	cm
+	//  Jan's definitions for the final 400-800 micron pitch design
+	static const double kFgtRout		= 38.25;    //	cm ,
+	static const double kFgtRmid		= 19.125;   //	cm, at Rout/2.
+	static const double kFgtRin		= 11.5;	    //	cm, 
+	static const double kFgtRadPitch	=  0.09538; //	nominal '800 mu pitch'
+	static const double kFgtPhiPitch	=  0.08;    //	800 mu, at outer radi or at Rmid
+	static const double kFgtDeadQuadEdge	=  1.2;	    // (cm) effective dead area along quadrant edges
 
 	//  Standard definitions.
 	static const Int_t kNumStrips = 1440;
 	static const Int_t kNumChannels = 1280;
+	static const Int_t kFgtMxDisk=6;    /* max # of  FGT disks @ STAR */
+	static const Int_t kFgtMxQuad=4;    /* max # of quadrants in single FGT disk */
 
     protected:
 	/*  Not sure that these have a point anymore.
@@ -341,6 +344,47 @@ class StFgtGeom
 };
 
 #endif
+
+/*
+--------- December 18, 2010 ---------
+ Description of quadrant geometry by Doug Hasell , December 18, 2010 :
+
+(Only 400_800 pitch description is left from the original text , Jan.)
+
+	In my coordinate system the X axis points to the right, Y axis points up, and the Z axis comes out of the page towards you.  Thus the beam direction is in the positive Z direction and the beam is at X=0, Y=0.  For the rest of the discussion I don't need the Z coordinate.  So for the following the origin is at ( 0, 0 ) in the XY plane formed by the readout board.
+
+	The FGT quadrants have a support frame.  The outer edges of this frame are defined by three lines and two arcs:
+
+Line 1 has Y = 0.5 mm
+
+Line 2 has X = 0.5 mm
+
+Line 3 is perpendicular to a ray at 31 degrees, 370 mm from the origin
+
+Arc 1 has radius = 103.5 mm
+
+Arc 2 has radius = 394.0 mm
+
+	The frame is everywhere 11.5 mm wide.  With the outer edges defined as above the frame width defines the inner dimensions of the frame or the "active" area of the detector.  (i.e. Y = 12, X = 12, perpendicular to 31 degrees at 358.5 mm, R = 115, and R = 382.5).
+
+	The lines or rather string of pads connected in an arc at constant radius effectively stop at the inner edge of the frame.  Actually they extend slightly beneath the frame but this is no longer in the active area.  
+	The exception to this are the lines of constant radius which intersect the flat in the outer region.  These lines end where they disappear under the flat.  Thus there are two lines at each of these radii readout on either side.  They are not connected together.
+
+	For the 400-800 micron pitch design the lines of constant radius only arc half way across the quadrant.  Thus lines of constant radius arcing from around 0 degrees to almost 45 degrees are readout on the edge near the X axis while those arcing from just over 45 degrees to near 90 degrees are readout on the other side.
+
+	Again R lines intersecting the flat are the exception and are connected and readout as above on one side or the other.
+
+	The R lines have a constant pitch.   For the 400-800 micron design the pitch is 953.8 microns. Remember these are really pads connected together to simulate a line arcing at a constant radius.  This pitch is the pitch in the radial or outward direction.  The pads are actually trapezoids with the other dimensions identical to the width of the neighboring PHI lines which changes pitch with radius. 
+
+-------------
+	PHI lines or lines at a constant angle extend from the outer edge (but the inside edge of the frame) to a radius of 382.5 / 2 = 191.25 mm.  The pitch varies from 800 micron at the outer edge to 400 at half the radius.  Lines which start at the outer edge but which disappear under the frame before reaching R=191.25 are terminated where they disappear under the frame.  So the PHI lines near the frame don't make it all the way in.
+
+	At R = 191.25 half of the PHI lines end and the other half revert to 800 micron pitch and continue on to the inner radius of the frame. Again, where they go beneath the frame they are terminated there.
+ 
+	To determine which PHI lines go from the outer radius to the inner radius look at the angle of the PHI lines connected to connector 0 or 9.  You'll see that these change angle with a step size twice that of the PHI lines in the outer connectors 3-6.  Those PHI lines with integer multiples of this step go from the outer edge all the way to the inner edge.  Those with half integer multiples go from the outer end but end at R = 191.25 mm.
+
+*/
+
 
 /*
  *  $ Id: $
