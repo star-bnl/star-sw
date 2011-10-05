cb11aa8aa68a63a8092f2b1d71bbbb2cb2665721
Victor Perevoztchikov <perev@bnl.gov>
2011-10-05 23:24:11 +0000

    add Check methods

--- StRoot/Stv/StvKalmanTrackFitter.h
+++ StRoot/Stv/StvKalmanTrackFitter.h
@@ -7,6 +7,8 @@
 /// \class StvKalmanTrackFitter
 class StvTrack;
 class StvNode;
+class THelixTrack;
+class THelixFitter;
 
 class StvKalmanTrackFitter : public StvTrackFitter
 {
@@ -17,11 +19,16 @@ public:
   virtual  int Fit(const StvTrack *trak,const StvHit *vtx,StvNode *node);
            int Propagate(StvNode  *node,const StvNode *preNode,int dir);
   virtual  int Helix(StvTrack *trak,int mode);
+  virtual  int Check(StvTrack *trak);
+  virtual  int Check(const StvNodePars &parA,const StvFitErrs &errA,
+		     const StvNodePars &parB,const StvFitErrs &errB);
+  virtual  THelixTrack* GetHelix() const;
   virtual void Clear(const char *opt="");
   
 
 protected:
 char mBeg[1];
+THelixFitter *mHelx;
 char mEnd[1];
 
 ClassDef(StvKalmanTrackFitter,0);
--- StRoot/Stv/StvTrackFitter.h
+++ StRoot/Stv/StvTrackFitter.h
@@ -5,9 +5,12 @@
 #include "TNamed.h"
 
 /// \class StvTrackFitter
+class THelixTrack;
 class StvTrack;
 class StvNode;
 class StvHit;
+class StvNodePars;
+class StvFitErrs;
 
 class StvTrackFitter : public TNamed
 {
@@ -18,6 +21,10 @@ public:
   virtual void Clear(const char *opt="")	{mNDF=0; mXi2=3e33;}
   virtual  int Fit(const StvTrack *trak,const StvHit *vtx,StvNode *node)=0;
   virtual  int Helix(StvTrack *trak,int mode)=0;
+  virtual  int Check(StvTrack *trak) 		{return 0;}
+  virtual  int Check(const StvNodePars &parA,const StvFitErrs &errA,
+		     const StvNodePars &parB,const StvFitErrs &errB) {return 0;}
+  virtual  THelixTrack* GetHelix() const 	{return 0;}
            int GetNDF() const 			{return mNDF;}     
         double GetDca3() const 			{return mDca3;}     
         double GetXi2() const 			{return mXi2;}     
