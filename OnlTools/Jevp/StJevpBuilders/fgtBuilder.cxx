bddfc85fdf838aec57a51df88e18003968f681d1
Anselm Vossen <avossen@indiana.edu>
2011-11-29 16:49:36 +0000

    fgtBuilder

--- OnlTools/Jevp/StJevpBuilders/fgtBuilder.cxx
+++ OnlTools/Jevp/StJevpBuilders/fgtBuilder.cxx
@@ -4,9 +4,11 @@
 #include "Jevp/StJevpPlot/JevpPlotSet.h"
 #include "DAQ_READER/daqReader.h"
 #include "DAQ_READER/daq_dta.h"
+#include <DAQ_READER/daq_dta.h>
 #include "DAQ_READER/daq_det.h"
 #include "StDaqLib/TRG/trgStructures2009.h"
 #include "StEvent/StTriggerData2009.h"
+#include <DAQ_FGT/daq_fgt.h>
 
 #include "Jevp/StJevpPlot/RunStatus.h"
 #include "StEvent/StTriggerData.h"
@@ -28,7 +30,7 @@
 ClassImp(fgtBuilder);
   
 
-fgtBuilder::fgtBuilder() {
+fgtBuilder::fgtBuilder():evtCt(0) {
   plotsetname = (char *)"fgt";
   // start with histograms undefined...
   memset(&contents, 0, sizeof(contents));
@@ -37,27 +39,57 @@ fgtBuilder::fgtBuilder() {
 fgtBuilder::~fgtBuilder() {
 
   // Delete any existing histograms...
-  int n = sizeof(contents) / sizeof(TH1 *);
+
+  int n = sizeof(contents) / sizeof(TH2 *);
+  cout <<" deleting " <<n<<" histos"  << endl;
   for(int i=0;i<n;i++) {
-    if(contents.array[i]) delete contents.array[i];
+        if(contents.array[i]) delete contents.array[i];
   }
+  cout <<"done.." <<endl;
 }
 
 void fgtBuilder::initialize(int argc, char *argv[]) {
 
   // Initialization of histograms.
-  contents.h2_tmp = new TH1F("h2_tmp","tst",50,0,10);
+  //could run a loop...
+  contents.q1=new TH2F("q1","Disc 1, Quad 1",1280,0,1280,2000,0,2000);
+  contents.q2=new TH2F("q2","Disc 1, Quad 2",1280,0,1280,2000,0,2000);
+  contents.q3=new TH2F("q3","Disc 1, Quad 3",1280,0,1280,2000,0,2000);
+  contents.q4=new TH2F("q4","Disc 1, Quad 4",1280,0,1280,2000,0,2000);
+  contents.q5=new TH2F("q5","Disc 2, Quad 1",1280,0,1280,2000,0,2000);
+  contents.q6=new TH2F("q6","Disc 2, Quad 2",1280,0,1280,2000,0,2000);
+  contents.q7=new TH2F("q7","Disc 2, Quad 3",1280,0,1280,2000,0,2000);
+  contents.q8=new TH2F("q8","Disc 2, Quad 4",1280,0,1280,2000,0,2000);
+  contents.q9=new TH2F("q9","Disc 3, Quad 1",1280,0,1280,2000,0,2000);
+  contents.q10=new TH2F("q10","Disc 3, Quad 2",1280,0,1280,2000,0,2000);
+  contents.q11=new TH2F("q11","Disc 3, Quad 3",1280,0,1280,2000,0,2000);
+  contents.q12=new TH2F("q12","Disc 3, Quad 4",1280,0,1280,2000,0,2000);
+  contents.q13=new TH2F("q13","Disc 4, Quad 1",1280,0,1280,2000,0,2000);
+  contents.q14=new TH2F("q14","Disc 4, Quad 2",1280,0,1280,2000,0,2000);
+  contents.q15=new TH2F("q15","Disc 4, Quad 3",1280,0,1280,2000,0,2000);
+  contents.q16=new TH2F("q16","Disc 4, Quad 4",1280,0,1280,2000,0,2000);
+  contents.q17=new TH2F("q17","Disc 5, Quad 1",1280,0,1280,2000,0,2000);
+  contents.q18=new TH2F("q18","Disc 5, Quad 2",1280,0,1280,2000,0,2000);
+  contents.q19=new TH2F("q19","Disc 5, Quad 3",1280,0,1280,2000,0,2000);
+  contents.q20=new TH2F("q20","Disc 5, Quad 4",1280,0,1280,2000,0,2000);
+  contents.q21=new TH2F("q21","Disc 6, Quad 1",1280,0,1280,2000,0,2000);
+  contents.q22=new TH2F("q22","Disc 6, Quad 2",1280,0,1280,2000,0,2000);
+  contents.q23=new TH2F("q23","Disc 6, Quad 3",1280,0,1280,2000,0,2000);
+  contents.q24=new TH2F("q24","Disc 6, Quad 4",1280,0,1280,2000,0,2000);
+  ////
 
   // Add root histograms to Plots
-  int np = sizeof(contents) / sizeof(TH1 *);
+  int np = sizeof(contents) / sizeof(TH2 *);
   JevpPlot *plots[np];
+  cout <<" we have " << np <<endl;
 
-  int n=0;
-  plots[n] = new JevpPlot(contents.h2_tmp);
-  //  plots[++n] = new JevpPlot(contents.h0_evt_size);
+  for(int i=0;i<np;i++)
+    {
+      plots[i] = new JevpPlot(contents.array[i]);
+    }
 
   // Add Plots to plot set...
-  for(int i=0;i<=n;i++) {
+    for(int i=0;i<np;i++) {
     LOG(DBG, "Adding plot %d",i);
     addPlot(plots[i]);
   }
@@ -78,7 +110,42 @@ void fgtBuilder::startrun(daqReader *rdr) {
 
 void fgtBuilder::event(daqReader *rdr)
 {
-  contents.h2_tmp->Fill(tRnd.Rndm(0));
+  //  contents.h2_tmp->Fill(tRnd.Rndm(0));
+  if(!(evtCt %100))
+    cout <<"looking at evt " << evtCt <<endl;
+  daq_dta *dd=rdr->det("fgt")->get("adc");
+  while(dd && dd->iterate()) {
+    fgt_adc_t *f = (fgt_adc_t *) dd->Void ;
+    for(u_int i=0;i<dd->ncontent;i++)
+      {
+
+	//	  printf("FGT ADC: RDO %d, ARM %d, APV %d: %d values\n",dd->rdo,dd->sec,dd->pad,dd->ncontent) ;
+	//	dd->rdo;
+	//the arm
+	//	dd->sec;// two arms per disc
+	//	int disc=dd->rdo*3+dd->sec/2;
+	int quad=(dd->rdo-1)*12+dd->sec*2;
+	if(dd->pad>10)
+	  quad+=1;
+	if(quad<24)
+	contents.array[quad]->Fill(dd->pad*128+f[i].ch,f[i].adc);
+	else
+	  cout <<"quad too large: " << quad <<endl;
+	//	cout <<" filling with : " << dd->pad*128+f[i].ch <<" and " << f[i].adc <<endl;
+	/*if(dd->pad <= APVEnd)
+	  {
+	  value=f[i].adc-pedestals[dd->pad-APVStart][f[i].ch];
+	  vals[dd->pad-APVStart][f[i].ch]+=value;
+	  //timbin
+	  f[i].tb;
+	  }*/
+
+
+	//quad should be 0-23
+      }
+  }
+    evtCt++;
+
   // StTriggerData2009 *trgd2009;
   // int run = rdr->run;
 
@@ -98,12 +165,14 @@ void fgtBuilder::event(daqReader *rdr)
 }
 
 void fgtBuilder::stoprun(daqReader *rdr) {
-  
+  cout <<"stopping run " <<endl;  
 }
 
 void fgtBuilder::main(int argc, char *argv[])
 {
   fgtBuilder me;
+  cout <<"starting main" << endl;
   me.Main(argc, argv);
+  cout <<"ending main" << endl;
 }
 
--- OnlTools/Jevp/StJevpBuilders/fgtBuilder.h
+++ OnlTools/Jevp/StJevpBuilders/fgtBuilder.h
@@ -6,7 +6,7 @@
 #include <TH1F.h>
 #include <TH2F.h>
 #include <TRandom.h>
-
+#include "DAQ_READER/daq_dta.h"
 
 #include <math.h>
 
@@ -27,6 +27,7 @@ public:
 
  private:
   TRandom tRnd;
+  int evtCt;
   int t_2min;
   int t_10min;
   int t_120min;
@@ -35,12 +36,35 @@ public:
   //*** Use the union to be able to treat in bulk
   //*** As well as by name...
   union {
-    TH1 *array[];
+    TH2 *array[];
     struct {
-      TH1 *h2_tmp;
+      TH2* q1;
+      TH2* q2;
+      TH2* q3;
+      TH2* q4;
+      TH2* q5;
+      TH2* q6;
+      TH2* q7;
+      TH2* q8;
+      TH2* q9;
+      TH2* q10;
+      TH2* q11;
+      TH2* q12;
+      TH2* q13;
+      TH2* q14;
+      TH2* q15;
+      TH2* q16;
+      TH2* q17;
+      TH2* q18;
+      TH2* q19;
+      TH2* q20;
+      TH2* q21;
+      TH2* q22;
+      TH2* q23;
+      TH2* q24;
     };
   } contents;
-
+  daq_dta *dd;
   //*** End Histogram Declarations...
 
   ClassDef(fgtBuilder, 1);
