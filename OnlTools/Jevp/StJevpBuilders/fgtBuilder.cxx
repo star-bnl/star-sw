f7d13c018aa70b68da232e27dcaa7375c092574e
avossen <>
2011-12-18 01:26:20 +0000

    changed mapping

--- OnlTools/Jevp/StJevpBuilders/fgtBuilder.cxx
+++ OnlTools/Jevp/StJevpBuilders/fgtBuilder.cxx
@@ -50,30 +50,30 @@ void fgtBuilder::initialize(int argc, char *argv[]) {
   
   // Initialization of histograms.
   //could run a loop...
-  contents.q1=new TH2F("assembly1","Disc 1, Assembly 1",3000,0,3000,100,0,5000);
-  contents.q2=new TH2F("assembly2","Disc 1, Assembly 2",3000,0,3000,100,0,5000);
-  contents.q3=new TH2F("assembly3","Disc 1, Assembly 3",3000,0,3000,100,0,5000);
-  contents.q4=new TH2F("assembly4","Disc 1, Assembly 4",3000,0,3000,100,0,5000);
-  contents.q5=new TH2F("assembly5","Disc 2, Assembly 1",3000,0,3000,100,0,5000);
-  contents.q6=new TH2F("assembly6","Disc 2, Assembly 2",3000,0,3000,100,0,5000);
-  contents.q7=new TH2F("assembly7","Disc 2, Assembly 3",3000,0,3000,100,0,5000);
-  contents.q8=new TH2F("assembly8","Disc 2, Assembly 4",3000,0,3000,100,0,5000);
-  contents.q9=new TH2F("assembly9","Disc 3, Assembly 1",3000,0,3000,100,0,5000);
-  contents.q10=new TH2F("assembly10","Disc 3, Assembly 2",3000,0,3000,100,0,5000);
-  contents.q11=new TH2F("assembly11","Disc 3, Assembly 3",3000,0,3000,100,0,5000);
-  contents.q12=new TH2F("assembly12","Disc 3, Assembly 4",3000,0,3000,100,0,5000);
-  contents.q13=new TH2F("assembly13","Disc 4, Assembly 1",3000,0,3000,100,0,5000);
-  contents.q14=new TH2F("assembly14","Disc 4, Assembly 2",3000,0,3000,100,0,5000);
-  contents.q15=new TH2F("assembly15","Disc 4, Assembly 3",3000,0,3000,100,0,5000);
-  contents.q16=new TH2F("assembly16","Disc 4, Assembly 4",3000,0,3000,100,0,5000);
-  contents.q17=new TH2F("assembly17","Disc 5, Assembly 1",3000,0,3000,100,0,5000);
-  contents.q18=new TH2F("assembly18","Disc 5, Assembly 2",3000,0,3000,100,0,5000);
-  contents.q19=new TH2F("assembly19","Disc 5, Assembly 3",3000,0,3000,100,0,5000);
-  contents.q20=new TH2F("assembly20","Disc 5, Assembly 4",3000,0,3000,100,0,5000);
-  contents.q21=new TH2F("assembly21","Disc 6, Assembly 1",3000,0,3000,100,0,5000);
-  contents.q22=new TH2F("assembly22","Disc 6, Assembly 2",3000,0,3000,100,0,5000);
-  contents.q23=new TH2F("assembly23","Disc 6, Assembly 3",3000,0,3000,100,0,5000);
-  contents.q24=new TH2F("assembly24","Disc 6, Assembly 4",3000,0,3000,100,0,5000);
+  contents.q1=new TH2F("assembly1","Disc 1, Assembly 1",1400,0,1400,100,0,4000);
+  contents.q2=new TH2F("assembly2","Disc 1, Assembly 2",1400,0,1400,100,0,4000);
+  contents.q3=new TH2F("assembly3","Disc 1, Assembly 3",1400,0,1400,100,0,4000);
+  contents.q4=new TH2F("assembly4","Disc 1, Assembly 4",1400,0,1400,100,0,4000);
+  contents.q5=new TH2F("assembly5","Disc 2, Assembly 1",1400,0,1400,100,0,4000);
+  contents.q6=new TH2F("assembly6","Disc 2, Assembly 2",1400,0,1400,100,0,4000);
+  contents.q7=new TH2F("assembly7","Disc 2, Assembly 3",1400,0,1400,100,0,4000);
+  contents.q8=new TH2F("assembly8","Disc 2, Assembly 4",1400,0,1400,100,0,4000);
+  contents.q9=new TH2F("assembly9","Disc 3, Assembly 1",1400,0,1400,100,0,4000);
+  contents.q10=new TH2F("assembly10","Disc 3, Assembly 2",1400,0,1400,100,0,4000);
+  contents.q11=new TH2F("assembly11","Disc 3, Assembly 3",1400,0,1400,100,0,4000);
+  contents.q12=new TH2F("assembly12","Disc 3, Assembly 4",1400,0,1400,100,0,4000);
+  contents.q13=new TH2F("assembly13","Disc 4, Assembly 1",1400,0,1400,100,0,4000);
+  contents.q14=new TH2F("assembly14","Disc 4, Assembly 2",1400,0,1400,100,0,4000);
+  contents.q15=new TH2F("assembly15","Disc 4, Assembly 3",1400,0,1400,100,0,4000);
+  contents.q16=new TH2F("assembly16","Disc 4, Assembly 4",1400,0,1400,100,0,4000);
+  contents.q17=new TH2F("assembly17","Disc 5, Assembly 1",1400,0,1400,100,0,4000);
+  contents.q18=new TH2F("assembly18","Disc 5, Assembly 2",1400,0,1400,100,0,4000);
+  contents.q19=new TH2F("assembly19","Disc 5, Assembly 3",1400,0,1400,100,0,4000);
+  contents.q20=new TH2F("assembly20","Disc 5, Assembly 4",1400,0,1400,100,0,4000);
+  contents.q21=new TH2F("assembly21","Disc 6, Assembly 1",1400,0,1400,100,0,4000);
+  contents.q22=new TH2F("assembly22","Disc 6, Assembly 2",1400,0,1400,100,0,4000);
+  contents.q23=new TH2F("assembly23","Disc 6, Assembly 3",1400,0,1400,100,0,4000);
+  contents.q24=new TH2F("assembly24","Disc 6, Assembly 4",1400,0,1400,100,0,4000);
   ////
 
   // Add root histograms to Plots
@@ -117,8 +117,8 @@ void fgtBuilder::event(daqReader *rdr)
     fgt_adc_t *f = (fgt_adc_t *) dd->Void ;
     for(u_int i=0;i<dd->ncontent;i++)
       {
-
-	//		  printf("FGT ADC: RDO %d, ARM %d, APV %d: %d values\n",dd->rdo,dd->sec,dd->pad,dd->ncontent) ;
+	//	if(evtCt <1000)
+	//	  printf("FGT ADC: RDO %d, ARM %d, APV %d: %d values\n",dd->rdo,dd->sec,dd->pad,dd->ncontent) ;
 	//	dd->rdo;
 	//the arm
 	//	dd->sec;// two arms per disc
@@ -132,7 +132,8 @@ void fgtBuilder::event(daqReader *rdr)
 	    //	    channel=dd->pad*128+f[i].ch;
 	    //	    if(channel>1280)
 	    //	      channel-=(2*128);
-	    channel=(dd->pad%12)+f[i].ch;
+	    channel=(dd->pad%12)*128+f[i].ch;
+	    //apvs go 0-9 then 12-...
 	    contents.array[quad]->Fill(channel,f[i].adc);
 	  }
 	else
