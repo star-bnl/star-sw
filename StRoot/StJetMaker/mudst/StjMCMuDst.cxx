6fa69bfeb5346fb2bc303ba7b5739a95e0e02f8d
pibero <>
2012-01-21 17:36:11 +0000

    *** empty log message ***

--- StRoot/StJetMaker/mudst/StjMCMuDst.cxx
+++ StRoot/StJetMaker/mudst/StjMCMuDst.cxx
@@ -1,17 +1,17 @@
-// $Id: StjMCMuDst.cxx,v 1.11 2012/01/20 21:53:08 pibero Exp $
-
-#include <StjMCMuDst.h>
-
-#include <StMaker.h>
+// $Id: StjMCMuDst.cxx,v 1.12 2012/01/21 17:36:11 pibero Exp $
 
 #include <TDataSet.h>
 #include <TDataSetIter.h>
+#include <TLorentzVector.h>
 
+#include <StMaker.h>
 #include <tables/St_g2t_event_Table.h>
 #include <tables/St_g2t_vertex_Table.h>
+#include <tables/St_g2t_pythia_Table.h>
 #include <tables/St_particle_Table.h>
 
-#include <TLorentzVector.h>
+#include <StjMCParticleCutParton.h>
+#include <StjMCMuDst.h>
 
 StjPrimaryVertex StjMCMuDst::getMCVertex() const
 {
@@ -43,6 +43,14 @@ StjMCParticleList StjMCMuDst::getMCParticleList()
       if (const g2t_event_st* g2t_event_table = g2t_event_descriptor->GetTable()) {
 	runNumber   = g2t_event_table->n_run;
 	eventNumber = g2t_event_table->n_event;
+      }
+    }
+
+    // Get Pythia variables
+    if (St_g2t_pythia* g2t_pythia = (St_g2t_pythia*)geantDstI("g2t_pythia")) {
+      if (g2t_pythia_st* pythiaTable = g2t_pythia->GetTable()) {
+	StjMCParticleCutParton::mstu72 = pythiaTable->mstu72;
+	StjMCParticleCutParton::mstu73 = pythiaTable->mstu73;
       }
     }
 
