8d2cace62146cf00ac285f3429fd2bc8478560bf
perev <>
2012-01-25 17:56:16 +0000

    Cleanup

--- StRoot/StEvent/StEnumerations.cxx
+++ StRoot/StEvent/StEnumerations.cxx
@@ -1,60 +1,57 @@
 #include "StEnumerations.h"
 #include "TString.h"
-
-static const char *detName[] = {
-                  "Unknown",
-                  "Tpc",
-                  "Svt",
-                  "Rich",
-                  "FtpcWest",
-                  "FtpcEast",
-                  "Tof",
-                  "Ctb",
-                  "Ssd",
-                  "BarrelEmcTower",
-                  "BarrelEmcPreShower",
-                  "BarrelSmdEtaStrip",
-                  "BarrelSmdPhiStrip",
-                  "EndcapEmcTower",
-                  "EndcapEmcPreShower",
-                  "EndcapSmdUStrip",
-                  "EndcapSmdVStrip",
-                  "ZdcWest",
-                  "ZdcEast",
-                  "MwpcWest",
-                  "MwpcEast",
-                  "TpcSsd",
-                  "TpcSvt",
-                  "TpcSsdSvt",
-                  "SsdSvt",
-                  "PhmdCpv",
-                  "Phmd",
-                  "Pxl",
-                  "Ist",
-                  "Fgt",
-                  "Etr",
-                  "FpdWest",
-                  "FpdEast",
-                  "Fms",
-                  "Rps",
-                  "Mtd",
-                  "MaxDetector"};
+struct myMap {int id; const char *name;};
+static myMap gMyMap[] = {
+{kUnknownId             ,"Unknown"  },
+{kTpcId       		,"Tpc"      },
+{kSvtId       		,"Svt"      },
+{kRichId      		,"Rich"     },
+{kFtpcWestId  		,"FtpcWest" },
+{kFtpcEastId  		,"FtpcEast" },
+{kTofId       		,"Tof"      },
+{kCtbId       		,"Ctb"      },
+{kSsdId       		,"Ssd"      },
+{kBarrelEmcTowerId     	,"BarrelEmcTower"    },
+{kBarrelEmcPreShowerId 	,"BarrelEmcPreShower"},
+{kBarrelSmdEtaStripId  	,"BarrelSmdEtaStrip" },
+{kBarrelSmdPhiStripId  	,"BarrelSmdPhiStrip" },
+{kEndcapEmcTowerId     	,"EndcapEmcTower"    },
+{kEndcapEmcPreShowerId 	,"EndcapEmcPreShower"},
+{kEndcapSmdUStripId    	,"EndcapSmdUStrip"   },
+{kEndcapSmdVStripId    	,"EndcapSmdVStrip"   },
+{kZdcWestId   		,"ZdcWest"  },
+{kZdcEastId   		,"ZdcEast"  },
+{kMwpcWestId  		,"MwpcWest" },
+{kMwpcEastId  		,"MwpcEast" },
+{kPhmdCpvId   		,"PhmdCpv"  },
+{kPhmdId      		,"Phmd"     },
+{kPxlId       		,"Pxl"      },
+{kIstId       		,"Ist"      },
+{kFgtId       		,"Fgt"      },
+{kEtrId       		,"Etr"      },
+{kFpdWestId   		,"FpdWest"  },
+{kFpdEastId   		,"FpdEast"  },
+{kFmsId       		,"Fms"      },
+{kRpsId       		,"Rps"      },
+{kMtdId       		,"Mtd"      },
+{0,0}};
 //_____________________________________________________________________________
 const char *detectorNameById(StDetectorId id)
 {
-   if (id<0 || id >= kMaxDetectorId) id = kUnknownId;
-   return detName[id];
+ for (int i=0;gMyMap[i].name;i++) { if (gMyMap[i].id==id) return gMyMap[i].name;}
+ return gMyMap[0].name;
 }
 //_____________________________________________________________________________
 StDetectorId detectorIdByName(const char *name)
 {
   int lmin=99,imin=0;
   
-  for (int id =0;id<kMaxDetectorId; id++) {
-    if (!TString(detName[id]).BeginsWith(name)) continue;
-    int l = strlen(detName[id]);
-    if (l>=lmin) continue;
-    lmin = l; imin=id;
+  for (int i =0;gMyMap[i].name; i++) {
+    if (!TString(gMyMap[i].name).BeginsWith(name,TString::kIgnoreCase))
+    			continue;
+    int l = strlen(gMyMap[i].name);
+    if (l>=lmin) 	continue;
+    lmin = l; imin=i;
   }
-  return (StDetectorId)imin;
+  return (StDetectorId)gMyMap[imin].id;
 }
