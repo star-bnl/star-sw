05c2de3e20843cae5177a8f3d86e640adcff693d
Victor Perevoztchikov <perev@bnl.gov>
2012-01-30 17:22:13 +0000

    Cleanup

--- StRoot/StMagF/StMagF.h
+++ StRoot/StMagF/StMagF.h
@@ -2,6 +2,7 @@
 #define StMagF_H
 #include "Rtypes.h"
 #include "TNamed.h"
+#include "StarMagField/StarMagField.h"
 #ifndef __CINT__
 #include "StarCallf77.h" 
 #define gufld  F77_NAME(gufld,GUFLD)
@@ -11,7 +12,13 @@ R__EXTERN  "C" {
 #endif
 class StMagF : public TNamed {
 public:
-  static void Agufld(float *x, float *b) {gufld(x,b);}
-  ClassDef(StMagF,1)
+  static void Agufld(float *x, float *b);
+ClassDef(StMagF,0) //mmmm
 };
+inline void StMagF::Agufld(float *x, float *b) {
+  static StarMagField *mag = StarMagField::Instance();
+  double xx[3]={x[0],x[1],x[2]}, bb[3];
+  mag->BField(xx,bb);
+  b[0]=bb[0]; b[1]=bb[1]; b[2]=bb[2];
+}
 #endif
