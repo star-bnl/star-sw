aa9bd91b7193cb766fb736d95f27ca7cc5d538cf
jml <>
2012-02-07 21:51:50 +0000

    save rootfiles

--- OnlTools/Jevp/StJevpServer/EvpConstants.h
+++ OnlTools/Jevp/StJevpServer/EvpConstants.h
@@ -7,6 +7,7 @@
 #define DEFAULT_REF_PLOT_DIR "/a/jevp/refplots"
 #define DEFAULT_BASEDIR "/RTScache/conf/jevp"
 #define DEFAULT_PDFDIR "/a/jevp/pdf"
+#define DEFAULT_ROOTFILEDIR "/a/jevp/rootfiles"
 #define EVP_SAVEPLOT_DIR "/a/jevp/saveplots"
 
 #endif
