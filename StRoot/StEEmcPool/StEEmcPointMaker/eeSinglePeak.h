0592f4f83c4467651bd3262df057e86ec1bf2830
cvs2git <cvs2git>
2012-02-29 18:23:47 +0000

    This commit was manufactured by cvs2git to create tag 'classic_version'.

--- StRoot/StEEmcPool/StEEmcPointMaker/eeSinglePeak.h
+++
@@ -1,8 +0,0 @@
-#include <Rtypes.h>
-#ifndef __eeSinglePeak_hh__
-#define __eeSinglePeak_hh__
-
-Double_t eeSinglePeak ( Double_t *x, Double_t *p );
-Double_t eeDoublePeak ( Double_t *x, Double_t *p );
-
-#endif
