40e4259272b5442a4bec67b8c386f35ab8f70136
perev <>
2012-03-01 16:47:48 +0000

    Creation of Mtd hit added

--- StRoot/StMcEvent/StMcMtdHit.cc
+++ StRoot/StMcEvent/StMcMtdHit.cc
@@ -9,68 +9,41 @@
 
 
 ClassImp(StMcMtdHit);
-StMcMtdHit::StMcMtdHit()
-  : mBackleg(0),mModule(0),mCell(0),mPathLength(0),mTime(0),mTof(0),mCharge(0)
-{/* noop */   }
-
-StMcMtdHit::StMcMtdHit(int t,int m,int c,float de,float pl,float time,float tof,float q)
-    :mBackleg(t),mModule(m),mCell(c),mPathLength(pl),mTime(time),mTof(tof),mCharge(q)
-{
-  mdE=de;
-}
-
-StMcMtdHit::StMcMtdHit(int t,int m,int c,float de,float pl,float time,float tof,float q,StThreeVectorF& x,StMcTrack* parent)
-    :mBackleg(t),mModule(m),mCell(c),mPathLength(pl),mTime(time),mTof(tof),mCharge(q)
-{ 
-  mdE=de;
-  mPosition = x;
-  mParentTrack=parent;
-}
-
-StMcMtdHit::StMcMtdHit(g2t_mtd_hit_st* pt)
-  : mBackleg(0),mModule(0),mCell(0),mPathLength(pt->ds),mTof(pt->tof),mCharge(0)
-
-{
-  mdE=pt->de;
-  mPosition = StThreeVectorF(pt->x[0], pt->x[1], pt->x[2]);
-//fg	    StThreeVectorF(pt->p[0], pt->p[1], pt->p[2]),
-//fg	    pt->de,
-//fg	    pt->ds,
-//fg	    pt->id,
-//fg	    pt->volume_id,
-//fg	    0), mTof(pt->tof), mBackleg(0), mModule(0),mCell(0)
-}
-
-
-
+//___________________________________________________________________________
+StMcMtdHit::StMcMtdHit(){}
+//___________________________________________________________________________
 StMcMtdHit::~StMcMtdHit() {/* noop */ }
     
-int StMcMtdHit::operator==(const StMcMtdHit& h) const
-{
-	//we have some options here -- include 2nd line or no??
-  // Hits are from the same particle and the same tower(cell)
-    return h.mBackleg == mBackleg && h.mModule == mModule && h.mCell == mCell 
-           && h.mPathLength == mPathLength && h.mTime == mTime 
-	   && h.mTof == mTof && h.mCharge == mCharge 
-	   && h.mPosition == mPosition
-	   && ( (h.mParentTrack == mParentTrack) || (h.mParentTrackId == mParentTrackId) );
+// //___________________________________________________________________________
+// int StMcMtdHit::operator==(const StMcMtdHit& h) const
+// {
+// 	//we have some options here -- include 2nd line or no??
+//   // Hits are from the same particle and the same tower(cell)
+//     return h.mBackleg == mBackleg && h.mModule == mModule && h.mCell == mCell 
+//            && h.mPathLength == mPathLength && h.mTime == mTime 
+// 	   && h.mTof == mTof && h.mCharge == mCharge 
+// 	   && h.mPosition == mPosition
+// 	   && ( (h.mParentTrack == mParentTrack) || (h.mParentTrackId == mParentTrackId) );
+// 
+// }
 
-}
-
+//___________________________________________________________________________
 int StMcMtdHit::operator!=(const StMcMtdHit& h) const
 {
     return !(*this == h);  // use operator==()
 }
 
-void StMcMtdHit::operator+=(const StMcMtdHit& h)
-{
-	//we have some options here
-    //if(*this == h) mdE += h.dE(); // use operator==()
-    //if(*this.sameCell(h)) mdE += h.dE(); 
-     mdE += h.dE();
-     mCharge += h.charge();
-}
+// //___________________________________________________________________________
+// void StMcMtdHit::operator+=(const StMcMtdHit& h)
+// {
+// 	//we have some options here
+//     //if(*this == h) mdE += h.dE(); // use operator==()
+//     //if(*this.sameCell(h)) mdE += h.dE(); 
+//      mdE += h.dE();
+//      mCharge += h.charge();
+// }
 
+//___________________________________________________________________________
 bool StMcMtdHit::sameCell(const StMcMtdHit& h) const
 {
   // Hits are from the same cell(tower)
@@ -78,22 +51,6 @@ bool StMcMtdHit::sameCell(const StMcMtdHit& h) const
     return h.mBackleg == mBackleg && h.mModule == mModule && h.mCell == mCell; 
 }
 
-void StMcMtdHit::setBackleg(int val)    { mBackleg = val; }
-
-void StMcMtdHit::setModule(int val) { mModule = val; }
-
-void StMcMtdHit::setCell(int val)    { mCell = val; }
-
-void StMcMtdHit::setPathLength(float val)   { mPathLength = val; }
-
-void StMcMtdHit::setTime(float val)   { mTime = val; }
-
-void StMcMtdHit::setTof(float val)    { mTof = val; }
-
-void StMcMtdHit::setCharge(float val)   { mCharge = val; }
-
-void StMcMtdHit::setParentTrackId(int val) { mParentTrackId = val; }
-    
     
 ostream& operator<<(ostream& os, const StMcMtdHit & h)
 {
@@ -103,9 +60,7 @@ ostream& operator<<(ostream& os, const StMcMtdHit & h)
      << " cell: " << h.cell()
      << " dE: " << h.dE()
      << " pathL: " << h.pathLength()
-     << " time: " << h.time()
      << " tof (simu): " << h.tof()
-     << " charge: " << h.charge()
      << " position: " << h.position();
     if(h.parentTrack()) {
       StMcTrack* t=h.parentTrack();
@@ -117,3 +72,21 @@ ostream& operator<<(ostream& os, const StMcMtdHit & h)
     else os <<" Parent track undefined ";
     return os;
 }
+//___________________________________________________________________________
+StMcMtdHit::StMcMtdHit(g2t_mtd_hit_st* pt): 
+    StMcHit(StThreeVectorF(pt->x[0], pt->x[1], pt->x[2]),
+	    StThreeVectorF(pt->p[0], pt->p[1], pt->p[2]), 
+	    pt->de, pt->ds, pt->tof, pt->id, pt->volume_id, 0)
+{
+const static float kMtdPadWidth = 3.8 + 0.6;  	//! Pad Width: 38mm padwidth + 6mm innerspacing
+const static   int kNCell       = 12,     	//! 12 cells per box
+
+   mPathLength = pt->s_track;
+   int volume_id = pt->volume_id;
+   mModule = (volume_id/100  )%100;
+   mBackleg =(volume_id/10000)%100;
+   mCell = Int_t((pt->x[1] + kMtdPadWidth * kNCell/2) / kMtdPadWidth) + 1;
+
+
+}        
+    
--- StRoot/StMcEvent/StMcMtdHit.hh
+++ StRoot/StMcEvent/StMcMtdHit.hh
@@ -16,8 +16,6 @@ class g2t_mtd_hit_st;
 class StMcMtdHit : public StMcHit {
 public:
   StMcMtdHit();
-  StMcMtdHit(int, int, int, float, float, float, float, float); //! backleg, module, cell, dE, pathLength, tof_exact, tof_measured, charge
-  StMcMtdHit(int, int, int, float, float, float, float, float, StThreeVectorF&, StMcTrack*);// above + position + partentTrack
   StMcMtdHit(g2t_mtd_hit_st*);
 
   virtual ~StMcMtdHit();
@@ -33,22 +31,10 @@ public:
     virtual int                          module() const;
     virtual int                            cell() const;
     virtual float                    pathLength() const;
-    virtual float                          time() const; //! time of flight geant
-    virtual float                           tof() const; //! tof simulated
-    virtual float                        charge() const; //! collected charge
     virtual int                   parentTrackId() const;	
 
   // "Set" Methods
 
-    virtual void setBackleg(int);
-    virtual void setModule(int);
-    virtual void setCell(int);
-    virtual void setPathLength(float);
-    virtual void setTime(float);
-    virtual void setTof(float);
-    virtual void setCharge(float);
-    virtual void setParentTrackId(int);
-    
   //  void* operator new(size_t)     { return mPool.alloc(); }
   //  void  operator delete(void* p) { mPool.free(p); }
     
@@ -57,9 +43,6 @@ protected:
     int                  mModule;
     int                  mCell;
     float                mPathLength;
-    float                mTime;
-    float                mTof;
-    float                mCharge;
     int			 mParentTrackId;
 
     ClassDef(StMcMtdHit,1)//
@@ -71,9 +54,6 @@ inline   int StMcMtdHit::backleg()	const {return mBackleg; 	}
 inline   int StMcMtdHit::module() 	const {return mModule; 		}
 inline   int StMcMtdHit::cell() 	const {return mCell; 		}
 inline float StMcMtdHit::pathLength()  	const {return mPathLength; 	}
-inline float StMcMtdHit::time()  	const {return mTime; 		}
-inline float StMcMtdHit::tof() 		const {return mTof; 		}
-inline float StMcMtdHit::charge() 	const {return mCharge; 		}
 inline   int StMcMtdHit::parentTrackId()const {return mParentTrackId; 	}
 
 #endif
