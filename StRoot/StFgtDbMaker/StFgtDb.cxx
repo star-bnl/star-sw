eb1b148fe16b139d599f8e0cdc8224ecb7cde506
Renee Fatemi <renee.fatemi@uky.edu>
2012-03-09 12:54:49 +0000

    Removed old methods used in StFgtSlowSimulatorMaker

--- StRoot/StFgtDbMaker/StFgtDb.cxx
+++ StRoot/StFgtDbMaker/StFgtDb.cxx
@@ -2,83 +2,9 @@
 
 #include "StFgtDb.h"
 
-// tmp, methods below whould  be moved to fgt-db-maker
-
-//--------------------------------------------
-//--------------------------------------------
-//--------------------------------------------
-double
-StFgtDb::gridAttenuation(float xLoc, float yLoc){ // range [0,1]
-  double att=1.0 ; // default is no attenuation
-
-  const int nR=4;
-  double rA[nR]={11.5, 20.63,29.15, 38.1};// (cm) - location of inneficiency rings
-
-  double const par_delR=1.; // cm 
-  
-  // R-grid
-  double r=sqrt(xLoc*xLoc + yLoc*yLoc);
-  for(int i=0; i<nR; i++){
-    double eps=(r-rA[i])/par_delR;
-    if(fabs(eps)>3) continue; // skip if too far
-    att*= 1. - exp(-eps*eps/sqrt(2));
-    //printf(" Ri=%d  eps=%f\n", i,eps);
-  }
-  //printf("r=%f att=%f  \n", r,att);
-
-  //Phi edges: X, Y-axis
-  for(int i=0;i<2;i++) {
-    double eps=0;
-    if (i==0) eps=(yLoc- 1.2)/par_delR; // dist to edge of quadrant
-    if (i==1) eps=(xLoc- 1.2)/par_delR;
-    if(fabs(eps)>3.) continue; // skip if too far
-    att*= 1. - exp(-eps*eps/sqrt(2));
-    //printf(" XYi=%d  eps=%f\n", i,eps);
-  }
-
-  
-  //Phi edges: 30, 60 deg
-  for(int i=0;i<2;i++) {
-    double del=0;
-    if(i==0) del=-xLoc*0.5 + yLoc*0.866;  //- x*sin(30) + y*cos(30)
-    if(i==1) del= yLoc*0.5 - xLoc*0.866;  //- x*sin(60) + y*cos(60)
-    double eps=del/par_delR;
-    if(fabs(eps)>3.) continue; // skip if too far
-    att*= 1. - exp(-eps*eps/sqrt(2));
-    //printf(" Gphi=%ddeg  eps=%f\n", 30*(1+i),eps);
-  }
-
-  return att;
-}
-
-//--------------------------------------------
-//--------------------------------------------
-//--------------------------------------------
-double
-StFgtDb::PchargeFraction(float xLoc, float yLoc){ // range [0,1]
-  return 0.45;
-}
-
-//--------------------------------------------
-//--------------------------------------------
-//--------------------------------------------
-double
-StFgtDb::PstripGain(int iStrip, int iQuad, int iDisc){
-  return 3.;
-}
-
-//--------------------------------------------
-//--------------------------------------------
-//--------------------------------------------
-double
-StFgtDb::RstripGain(int iStrip, int iQuad, int iDisc){
-  return 3.;
-}
 
 
-//--------------------------------------------
-//--------------------------------------------
-//--------------------------------------------
+
 void
 StFgtDb::printFgtDumpCSV1(TString fname, int myDate, int myTime) {
   FILE *fd=fopen(fname.Data(),"w"); assert(fd);
