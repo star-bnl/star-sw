b0ca24b6fde70f43a38b0774ecb84480710aa608
Renee Fatemi <renee.fatemi@uky.edu>
2012-03-12 16:02:55 +0000

    return to StFgtDb *fgtDb = this;  way of accessing members

--- StRoot/StFgtDbMaker/StFgtDb.cxx
+++ StRoot/StFgtDbMaker/StFgtDb.cxx
@@ -16,6 +16,9 @@ StFgtDb::printFgtDumpCSV1(TString fname, int myDate, int myTime) {
       fd <<"#  FGT mapping, timeStamp "<< myDate<<" "<<myTime<<"\n";
       fd<<"# electId,geoID,   RDO(1;2),ARM(0-4),APV(0-9;12-21),chan(0-127),  disk(1-6),quad(A-D),layer(P;R),strip(P:0-719;R0-279+400-679), ordinate(rad;cm),lowSpan(cm;rad),upSpan(cm;rad), geoName, stat,ped(ADC),sigPed(ADC)\n";
       
+
+      StFgtDb *fgtDb=this;
+
       int nTry=0, nMap=0;
       for (int rdo=1;rdo<=2;rdo++){// 2 RDOs numbered 1,2
 	for (int arm=0;arm<6;arm++){//6 arms numbered from 0
@@ -25,7 +28,7 @@ StFgtDb::printFgtDumpCSV1(TString fname, int myDate, int myTime) {
 	    for (int channel=0;channel<128;channel++){//128 channels numbered from 0
 	      
 	      nTry++;
-	      int geoId=StFgtDb::getGeoIdFromElecCoord(rdo, arm, apv, channel);
+	      int geoId=fgtDb->getGeoIdFromElecCoord(rdo, arm, apv, channel);
 	      if (geoId<0) continue;
 	      nMap++;
 	      Short_t disk,quad,strip; Char_t layer;
@@ -33,11 +36,11 @@ StFgtDb::printFgtDumpCSV1(TString fname, int myDate, int myTime) {
 	      StFgtGeom::decodeGeoId(geoId,disk,quad,layer,strip);
 	      StFgtGeom::getPhysicalCoordinate(geoId,disk,quad,layer,ordinate,  lowerSpan,  upperSpan);
 	      
-	      double  ped=StFgtDb::getPedestalFromElecCoord(rdo,arm,apv,channel);
-	      double  pedSig=StFgtDb::getPedestalSigmaFromElecCoord(rdo,arm,apv,channel);
-	      Short_t stat=StFgtDb::getStatusFromElecCoord(rdo,arm,apv,channel);	      
+	      double  ped=fgtDb->getPedestalFromElecCoord(rdo,arm,apv,channel);
+	      double  pedSig=fgtDb->getPedestalSigmaFromElecCoord(rdo,arm,apv,channel);
+	      Short_t stat=fgtDb->getStatusFromElecCoord(rdo,arm,apv,channel);	      
 	      int     electId = StFgtGeom::getElectIdFromElecCoord(rdo,arm,apv,channel);
-	      std::string  geoName=StFgtDb::getGeoNameFromElecCoord(rdo,arm,apv,channel);	 
+	      std::string  geoName=fgtDb->getGeoNameFromElecCoord(rdo,arm,apv,channel);	 
 	      
 	      
 	      fd<<electId<<" "<<geoId<<"  "<<rdo<<"  "<<arm<<"  "<<apv<<"  "<<channel<<"  "<<disk+1<<"  "<<quad+'A'<<"  "<<layer<<"  "<<strip<<"  "<<ordinate<<"  "<<lowerSpan<<"  "<<upperSpan<<"  "<<geoName.data()<<"  "<<stat<<"  "<<ped<<"  "<<pedSig<<"\n";
