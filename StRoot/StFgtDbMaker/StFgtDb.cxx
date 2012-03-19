500810bb46c10c608f9d43fe8da58092482410fd
rfatemi <>
2012-03-19 01:19:50 +0000

    Collapsed StFgtDbImpl class into StFgtDb after removing StFgtDbIdealImpl

--- StRoot/StFgtDbMaker/StFgtDb.cxx
+++ StRoot/StFgtDbMaker/StFgtDb.cxx
@@ -1,3 +1,4 @@
+#include "StMessMgr.h"
 #include "StFgtDb.h"
 #include <fstream>
 using namespace std;
@@ -61,3 +62,232 @@ StFgtDb::printFgtDumpCSV1(TString fname, int myDate, int myTime) {
    }
   
 }
+
+
+
+
+Int_t StFgtDb::getElecCoordFromGeoId(
+    Int_t geoId, Int_t& rdo, Int_t& arm, Int_t& apv, Int_t& channel
+)
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getElecCoordFromGeoId." << endm;
+        rdo = kFgtError;
+        arm = kFgtError;
+        apv = kFgtError;
+        channel = kFgtError;
+
+        return kFgtError;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+
+    StFgtGeom::decodeElectronicId( elecId, rdo, arm, apv, channel );
+
+    return 0;
+}
+
+Int_t StFgtDb::getElecCoordFromGeoName(
+    const std::string & geoName,
+    Int_t& rdo, Int_t& arm, Int_t& apv, Int_t& channel
+)
+{
+    Int_t geoId =
+	StFgtGeom::translateGeoNameToGeoId( geoName );
+
+    if ( geoId < 0 )
+    {
+	rdo = kFgtError;
+	arm = kFgtError;
+	apv = kFgtError;
+	channel = kFgtError;
+
+	return kFgtError;
+    }
+
+    getElecCoordFromGeoId( geoId, rdo, arm, apv, channel );
+
+    return 0;
+}
+
+Double_t StFgtDb::getPedestalFromGeoId( Int_t geoId )
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getPedestalFromGeoId." << endm;
+        return kFgtError;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+    return m_pedestal->AdcPedestal[ elecId ];
+}
+
+Double_t StFgtDb::getPedestalFromElecId( Int_t elecId)
+{
+    if ( elecId < 0 || elecId >= kFgtNumElecIds )
+    {
+        LOG_DEBUG << "Electronic ID " << elecId << " out of range in StFgtDb::getPedestalFromElecId." << endm;
+        return kFgtError;
+    }
+
+    return m_pedestal->AdcPedestal[ elecId ];
+}
+
+Double_t StFgtDb::getPedestalSigmaFromGeoId( Int_t geoId )
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getPedestalSigmaFromGeoId." << endm;
+        return kFgtError;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+    return m_pedestal->AdcPedestalRMS[ elecId ];
+}
+
+Double_t StFgtDb::getPedestalSigmaFromElecId( Int_t elecId )
+{
+    if ( elecId < 0 || elecId >= kFgtNumElecIds )
+    {
+        LOG_DEBUG << "Electronic ID " << elecId << " out of range in StFgtDb::getPedestalSigmaFromElecId." << endm;
+        return kFgtError;
+    }
+
+    return m_pedestal->AdcPedestalRMS[ elecId ];
+}
+
+Char_t StFgtDb::getPedestalStatusFromGeoId( Int_t geoId )
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getPedestalStatusFromGeoId." << endm;
+        return kFgtErrorChar;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+    return m_pedestal->Status[ elecId ];
+}
+
+Char_t StFgtDb::getPedestalStatusFromElecId( Int_t elecId )
+{
+    if ( elecId < 0 || elecId >= kFgtNumElecIds )
+    {
+        LOG_DEBUG << "Electronic ID " << elecId << " out of range in StFgtDb::getPedestalStatusFromElecId." << endm;
+        return kFgtErrorChar;
+    }
+
+    return m_pedestal->Status[ elecId ];
+}
+
+Char_t StFgtDb::getStatusFromGeoId( Int_t geoId )
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getStatusFromGeoId." << endm;
+        return kFgtErrorChar;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+    return m_status->Status[ elecId ];
+}
+
+Char_t StFgtDb::getStatusFromElecId( Int_t elecId )
+{
+    if ( elecId < 0 || elecId >= kFgtNumElecIds )
+    {
+        LOG_DEBUG << "Electronic ID " << elecId << " out of range in StFgtDb::getStatusFromElecId." << endm;
+        return kFgtErrorChar;
+    }
+
+    return m_status->Status[ elecId ];
+}
+
+Double_t StFgtDb::getGainFromGeoId( Int_t geoId )
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getGainFromGeoId." << endm;
+        return kFgtError;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+    return m_gain->Gain[ elecId ];
+}
+
+Double_t StFgtDb::getGainFromElecId( Int_t elecId )
+{
+    if ( elecId < 0 || elecId >= kFgtNumElecIds )
+    {
+        LOG_DEBUG << "Electronic ID " << elecId << " out of range in StFgtDb::getGainFromElecId." << endm;
+        return kFgtError;
+    }
+
+    return m_gain->Gain[ elecId ];
+}
+
+Char_t StFgtDb::getGainStatusFromGeoId( Int_t geoId )
+{
+    if ( geoId < 0 || geoId >= kFgtNumGeoIds )
+    {
+        LOG_DEBUG << "GeoId " << geoId << " out of range in StFgtDb::getGainStatusFromGeoId." << endm;
+        return kFgtErrorChar;
+    }
+
+    Int_t elecId = m_rmap->Mapping[ geoId ];
+    return m_gain->Status[ elecId ];
+}
+
+Char_t StFgtDb::getGainStatusFromElecId( Int_t elecId )
+{
+    if ( elecId < 0 || elecId >= kFgtNumElecIds )
+    {
+        LOG_DEBUG << "Electronic ID " << elecId << " out of range in StFgtDb::getGainStatusFromElecId." << endm;
+        return kFgtErrorChar;
+    }
+
+    return m_gain->Status[ elecId ];
+}
+
+Double_t StFgtDb::getMapping(
+    Int_t rdo, Int_t arm, Int_t apv, Int_t channel
+)
+{
+	Short_t disc, quadrant;
+	Char_t layer;
+	Double_t ordinate, lowerSpan, upperSpan;
+
+	if ( getPhysCoordFromElecCoord(
+		rdo, arm, apv, channel,
+		disc, quadrant, layer,
+		ordinate, lowerSpan, upperSpan
+	     ) < 0
+	)
+	    return kFgtError;
+     
+	return ordinate;
+}
+
+//  This, similarly, seems needlessly complicated.
+bool StFgtDb::isR(
+    Int_t rdo, Int_t arm, Int_t apv, Int_t channel
+)
+{
+	Short_t disc, quadrant;
+	Char_t layer;
+	Double_t ordinate, lowerSpan, upperSpan;
+
+	//  Can't do any boundry checking return value thing here. Going to
+	//  have to let the call here handle the warning message.
+	getPhysCoordFromElecCoord(
+	    rdo, arm, apv, channel,
+	    disc, quadrant, layer,
+	    ordinate, lowerSpan, upperSpan
+	);
+     
+	return (layer == 'R');
+}
+
+
+
+
