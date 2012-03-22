aa6a72713a1189677c906e96c8541bb8f18c4429
geurts <>
2012-03-22 19:31:19 +0000

    run-9 reproduction timestamp update

--- StRoot/StBTofPool/dbase/macros/storetofGeomAlign.C
+++ StRoot/StBTofPool/dbase/macros/storetofGeomAlign.C
@@ -1,17 +1,10 @@
-// macro to upload tofr5 INL tables to database
+// Upload BTOF/TPC Alignment to database
 //
 // based on
 //  http://www.star.bnl.gov/STAR/comp/db/StoreDbTable.cc.html
 //
 // Jing Liu, 02/18/2005 
 //
-
- 
-// #include "StDbLib/StDbManager.hh"
-// #include "StDbLib/StDbConfigNode.hh"
-// #include "StDbLib/StDbTable.h"
-// #include "StDbLib/StDbDefs.hh"
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -19,10 +12,7 @@
 using namespace std;
 
 
-void storetofGeomAlign()
-//int main(int argc, char *argv[])
-
-{
+void storetofGeomAlign() {
 
 // year8
 //  const Int_t NTRAY = 5;
@@ -31,22 +21,21 @@ void storetofGeomAlign()
   const Int_t NVPDTRAY = 2;
   const Int_t NMAX = 120;
 
-  //-- load dBase and Table definition libraries
+//-- load dBase and Table definition libraries
   gSystem->Load("St_base");
   gSystem->Load("StChain");
   gSystem->Load("StUtilities");
   gSystem->Load("St_Tables.so");
-
   gSystem->Load("StDbLib.so");
   gSystem->Load("libStDb_Tables.so");
 
-  //-- get the singleton manager
+//-- get the singleton manager
   StDbManager* dbManager = StDbManager::Instance();
 
-  //-- connect to the db & get an empty container
+//-- connect to the db & get an empty container
   StDbConfigNode* configNode = dbManager->initConfig("Calibrations_tof");
 
-  //----------------------------------------
+//----------------------------------------
 // Commit log:
 //  TString ZStoreTime = "2007-11-01 00:00:00";
 //  TString ZStoreTime = "2008-11-01 00:00:00";  // initial for tuning
@@ -59,20 +48,20 @@ void storetofGeomAlign()
 //  TString ZStoreTime = "2009-12-14 00:00:02";  //  Run10 update with final TPC calib (7GeV based) 
 //  TString ZStoreTime = "2009-12-14 00:00:03";  //  Run10 200GeV update for production (note: TPC sector 20 problem)
 //  TString ZStoreTime = "2010-04-08 15:00:00";  //  Run10 update with final TPC calib 7GeV based) 
-  TString ZStoreTime = "2010-12-20 00:00:01";  //  Run11 update with final TPC calib (19.6GeV based) 
+//  TString ZStoreTime = "2010-12-20 00:00:01";  //  Run11 update with final TPC calib (19.6GeV based) 
+  TString ZStoreTime = "2009-02-01 00:00:01";  //  Run9, alignment update following major TPC geometry update
 
-  //-- add table to the container with descriptor given by Database
+//-- add table to the container with descriptor given by Database
   StDbTable* tofGeomAlign = configNode->addDbTable("tofGeomAlign");
 
-  //-- fill structures & store times
+//-- fill structures & store times
   tofGeomAlign_st *tofAlign = new tofGeomAlign_st[NMAX];
 
 // year10+
-  float shifty[120], shiftz[120], shiftx[120];
+  float shifty[NMAX], shiftz[NMAX], shiftx[NMAX];
   ifstream inData;
-  //inData.open("dat/run10/alignment/geomAlign_run10_7GeV_100819.txt");
-  inData.open("data/run11/alignment/geomAlign_19GeV_110804.txt");
-  for(int i=0;i<120;i++) {
+  inData.open("data/run9/alignment/geomAlign_200GeV_120322.txt");
+  for(int i=0;i<NMAX;i++) {
     inData >> shifty[i] >> shiftz[i] >> shiftx[i];
   }
   inData.close();
@@ -87,11 +76,10 @@ void storetofGeomAlign()
   }
 
   tofGeomAlign->SetTable((char*)tofAlign, NMAX);
-  //- set store time
+//- set store time
   dbManager->setStoreTime(ZStoreTime.Data());
-  //- store table in dBase
+//- store table in dBase
   cout<<" here "<<endl;
   dbManager->storeDbTable(tofGeomAlign);
   cout<<"uploaded"<<endl;
-      //    return 0;
 }
