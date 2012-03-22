041984beec63fcd25d660420ba7d72649f2d9fab
Victor Perevoztchikov <perev@bnl.gov>
2012-03-22 00:07:32 +0000

    Section added

--- StRoot/StEvent/StEtrHit.cxx
+++ StRoot/StEvent/StEtrHit.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StEtrHit.cxx,v 2.1 2012/01/24 03:06:12 perev Exp $
+ * $Id: StEtrHit.cxx,v 2.2 2012/03/22 00:07:32 perev Exp $
  *
  * Author: Ming Shao, Jan 2012
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: StEtrHit.cxx,v $
+ * Revision 2.2  2012/03/22 00:07:32  perev
+ * Section added
+ *
  * Revision 2.1  2012/01/24 03:06:12  perev
  * Add Etr
  *
@@ -31,14 +34,19 @@ StEtrHit::StEtrHit()
 
 //______________________________________________________________________________
 StEtrHit::StEtrHit(const StThreeVectorF& p,
-		   int sector, int layer, float charge)
-  : StHit(p, zero3V, sector*10+layer, charge, 0)
-{
-}
+		   int sector, int layer, int section, float charge)
+//          volume_id = section + 100*layer + 10000*sector
+  : StHit(p, zero3V, section+100*(layer+100*sector), charge, 0)
+{}
 
 //______________________________________________________________________________
 StEtrHit::~StEtrHit() {/* noop */}
-
+//______________________________________________________________________________
+int StEtrHit::section() const		{return (hardwarePosition()      )%100;}
+//______________________________________________________________________________
+int StEtrHit::layer()   const		{return (hardwarePosition()/100  )%100;}
+//______________________________________________________________________________
+int StEtrHit::sector()  const		{return (hardwarePosition()/10000)%100;}
 
 
 
--- StRoot/StEvent/StEtrHit.h
+++ StRoot/StEvent/StEtrHit.h
@@ -4,7 +4,7 @@
  */
 /***************************************************************************
  *
- * $Id: StEtrHit.h,v 2.1 2012/01/24 03:06:12 perev Exp $
+ * $Id: StEtrHit.h,v 2.2 2012/03/22 00:07:28 perev Exp $
  *
  * Author: Ming Shao, Jan 2012
  ***************************************************************************
@@ -14,6 +14,9 @@
  ***************************************************************************
  *
  * $Log: StEtrHit.h,v $
+ * Revision 2.2  2012/03/22 00:07:28  perev
+ * Section added
+ *
  * Revision 2.1  2012/01/24 03:06:12  perev
  * Add Etr
  *
@@ -34,18 +37,14 @@ class StEtrHit : public StHit {
 public:
     StEtrHit();
     StEtrHit(const StThreeVectorF&  position,
-             int sector, int layer, float charge);
+              int sector, int layer, int section, float charge);
     ~StEtrHit();
-    StDetectorId detector() const;
+    StDetectorId detector() const {return kEtrId;}
 
-    int sector() const;        // 0-11
-    int layer() const;         // 0-2
+    int section() const;        // 0-29
+    int sector()  const;        // 0-11
+    int layer()   const;        // 0-2
 protected:
     ClassDef(StEtrHit,1)
 };
-
-inline int StEtrHit::sector() const		{return hardwarePosition()/10;}
-inline int StEtrHit::layer()  const		{return hardwarePosition()%10;}
-inline StDetectorId StEtrHit::detector() const 	{return kEtrId;}
-
 #endif
