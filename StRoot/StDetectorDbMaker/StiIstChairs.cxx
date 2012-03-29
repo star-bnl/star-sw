396c58867fafb89277fdb50bfee1fa4ac56ce97c
cvs2git <cvs2git>
2012-03-29 02:04:30 +0000

    This commit was manufactured by cvs2git to create tag 'iTPC_IRAK_20180216'.

--- StRoot/StDetectorDbMaker/StiIstChairs.cxx
+++
@@ -1,8 +0,0 @@
-#include "StarChairDefs.h"
-#include "St_db_Maker/St_db_Maker.h"
-#include "StiIst1HitErrorCalculator.h"
-MakeChairInstance2(HitError,StiIst1HitErrorCalculator,Calibrations/tracker/ist1HitError);
-#include "StiIst2HitErrorCalculator.h"
-MakeChairInstance2(HitError,StiIst2HitErrorCalculator,Calibrations/tracker/ist2HitError);
-#include "StiIst3HitErrorCalculator.h"
-MakeChairInstance2(HitError,StiIst3HitErrorCalculator,Calibrations/tracker/ist3HitError);
--- pams/geometry/pipegeo/pipegeo1.g
+++
@@ -1,6 +0,0 @@
-MODULE pipegeo1 is a dummy module 
-AUTHOR  Jason C. Webb
-CREATED Today
-+cde,gcunit,agecom.
-<W> '-- PipeGeo1 is only implemented in xgeometry.so --'; (A80); STOP;
-END! Module pipegeo1
