11ad23722f7fd375ed88156ece688965e4466d07
pibero <>
2012-04-08 05:16:37 +0000

    Hack to keep around towers killed by hadronic correction

--- StRoot/StJetMaker/towers/StjTowerEnergyCorrectionForTracksFraction.cxx
+++ StRoot/StJetMaker/towers/StjTowerEnergyCorrectionForTracksFraction.cxx
@@ -24,7 +24,18 @@ StjTowerEnergyList StjTowerEnergyCorrectionForTracksFraction::Do(const StjTowerE
       }
     } // End track loop
     // Drop towers without energy
-    if (tower.energy > 0) elist.push_back(tower);
+    //if (tower.energy > 0) elist.push_back(tower);
+
+    // Artificially prevent this tower from contributing to
+    // the jet energy by assigning a really tiny amount (1e-10 GeV)
+    // to the tower energy. Otherwise we would lose the information
+    // about the position of the tower (eta and phi)
+    // if the tower energy was set to zero. This way we retain
+    // the tower ADC that contributes to the jet patch transverse energy
+    // and helps in identifying jets that could have fired the
+    // jet patch trigger.
+    if (tower.energy < 0) tower.energy = 1e-10;
+    elist.push_back(tower);
   } // End tower loop
   return elist;
 }
