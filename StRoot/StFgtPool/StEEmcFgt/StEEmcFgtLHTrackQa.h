a2da88a3aafb6d8d244a047ea8854e0033b455e4
sgliske <>
2012-04-12 17:12:05 +0000

    update to not use A2EMaker but StEEmcRawMaker

--- StRoot/StFgtPool/StEEmcFgt/StEEmcFgtLHTrackQa.h
+++ StRoot/StFgtPool/StEEmcFgt/StEEmcFgtLHTrackQa.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StEEmcFgtLHTrackQa.h,v 1.2 2012/04/11 22:13:24 sgliske Exp $
+ * $Id: StEEmcFgtLHTrackQa.h,v 1.3 2012/04/12 17:12:05 sgliske Exp $
  * Author: S. Gliske, April 2012
  *
  ***************************************************************************
@@ -11,6 +11,9 @@
  ***************************************************************************
  *
  * $Log: StEEmcFgtLHTrackQa.h,v $
+ * Revision 1.3  2012/04/12 17:12:05  sgliske
+ * update to not use A2EMaker but StEEmcRawMaker
+ *
  * Revision 1.2  2012/04/11 22:13:24  sgliske
  * update
  *
@@ -24,12 +27,13 @@
 #define _StEEmcFgtLHTrackQa_H_
 
 #include "StMaker.h"
-class StEEmcA2EMaker;
+class StEEmcRawMapMaker;
+class StFgtLHTracking;
 
 class StEEmcFgtLHTrackQa : public StMaker {
  public:
    // constructors
-   StEEmcFgtLHTrackQa( const Char_t* name = "fgtTracking", const Char_t* a2eMkrName = "EEmcA2EMaker", const Char_t* fgtLHTkrName = "FgtLHTracker" );
+   StEEmcFgtLHTrackQa( const Char_t* name = "fgtTracking", const Char_t* rawMapMkrName = "EEmcRawMapMaker", const Char_t* fgtLHTkrName = "FgtLHTracker" );
 
    // deconstructor
    virtual ~StEEmcFgtLHTrackQa();
@@ -43,7 +47,9 @@ class StEEmcFgtLHTrackQa : public StMaker {
    TH1F* getEnergyPerTrack(){ return mEnergyPerTrack; };
 
  protected:
-   StEEmcA2EMaker *mEEmcA2EMkr;
+   StEEmcRawMapMaker *mEEmcRawMapMkr;
+   StFgtLHTracking *mFgtLHTkr;
+
    TH1F *mEnergyPerTrack;
    TH1F *mEnergy;
 
