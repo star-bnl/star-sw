e893a9fd374d8696af0b076f17bfe49b621f0753
Ramiro Debbe <debbe@bnl.gov>
2012-04-24 14:33:25 +0000

    updated the plots for run 12 U+U

--- OnlTools/Jevp/StJevpBuilders/upcBuilder.cxx
+++ OnlTools/Jevp/StJevpBuilders/upcBuilder.cxx
@@ -42,20 +42,25 @@ upcBuilder::~upcBuilder() {
 
 void upcBuilder::initialize(int argc, char *argv[]) {
 
-  contents.h_zdce_sum = new TH1D( "h_zdce_sum", "ZDC East Unattenuated Sum", 150, -0.5, 149.5);
+  contents.h_zdce_sum = new TH1D( "h_zdce_sum", "ZDC East Unattenuated Sum", 150, -0.5, 1800.5);
   contents.h_zdce_sum->SetXTitle("Sum [ADC Ch.]");
   contents.h_zdce_sum->SetYTitle("Count");
-  contents.h_zdcw_sum = new TH1D( "h_zdcw_sum", "ZDC West Unattenuated Sum", 150, -0.5, 149.5);
+  contents.h_zdcw_sum = new TH1D( "h_zdcw_sum", "ZDC West Unattenuated Sum", 150, -0.5, 1800.5);
   contents.h_zdcw_sum->SetXTitle("Sum [ADC Ch.]");
   contents.h_zdcw_sum->SetYTitle("Count");
-  contents.h_zdce_sum_vs_ctb_sum = new TH2D( "h_zdce_sum_vs_ctb_sum", "ZDC East Unattenuated Sum vs. CTB ADC Sum",
-					      210, -0.5, 209.5, 150, -0.5, 149.5);
-  contents.h_zdce_sum_vs_ctb_sum->SetXTitle("CTB Sum [ADC Ch.]");
-  contents.h_zdce_sum_vs_ctb_sum->SetYTitle("ZDC Sum [ADC Ch.]");
-  contents.h_zdcw_sum_vs_ctb_sum = new TH2D( "h_zdcw_sum_vs_ctb_sum", "ZDC West Unattenuated Sum vs. CTB ADC Sum",
-					      210, -0.5, 209.5, 150, -0.5, 149.5);
-  contents.h_zdcw_sum_vs_ctb_sum->SetXTitle("CTB Sum [ADC Ch.]");
-  contents.h_zdcw_sum_vs_ctb_sum->SetYTitle("ZDC Sum [ADC Ch.]");
+  contents.h_zdce_sum_vs_zdcw_sum = new TH2D( "h_zdce_sum_vs_zdcw_sum", "ZDC East Unattenuated Sum vs. ZDC West Unatt.  ADC Sum",
+					      150, -0.5, 1800.5, 150, -0.5, 1800.5);
+
+
+  contents.h_zdce_sum_vs_zdcw_sum->SetXTitle("ZDC West Sum [ADC Ch.]");
+  contents.h_zdce_sum_vs_zdcw_sum->SetYTitle("ZDC East Sum [ADC Ch.]");
+
+  contents.h_bbce_adc = new TH1D( "h_bbce_adc", "BBC East ADC", 150, -0.5, 600.5);
+  contents.h_bbce_adc->SetXTitle("Sum [ADC Ch.]");
+  contents.h_bbcw_adc = new TH1D( "h_bbcw_adc", "BBC West ADC", 150, -0.5, 600.5);
+  contents.h_bbcw_adc->SetXTitle("Sum [ADC Ch.]");
+
+  contents.upcTOF_L1mult_vs_ZDCadcsum=new TH2F("upcTOF_L1_vs_ZDCadcsum","TOF_L1_vs_ZDCadcsum",144,0.5,1800.5,150,0,3000);
   
 
   // Add root histograms to Plots
@@ -87,12 +92,23 @@ void upcBuilder::event(daqReader *rdr)
   if(trgd){
     unsigned int zdcEastUnattSum = trgd->zdcUnAttenuated(east);
     unsigned int zdcWestUnattSum = trgd->zdcUnAttenuated(west);
-    unsigned int ctbAdcSum       = trgd->lastDSM(3);
+
+    unsigned int bbcADCeast       = trgd->bbcADCSum(east);
+    unsigned int bbcADCwest       = trgd->bbcADCSum(west);
+    unsigned int bbcLargeTileEast = trgd->bbcADCSumLargeTile(east);
+    unsigned int bbcLargeTileWest = trgd->bbcADCSumLargeTile(west);
     
     contents.h_zdce_sum->Fill(zdcEastUnattSum);
     contents.h_zdcw_sum->Fill(zdcWestUnattSum);
-    contents.h_zdce_sum_vs_ctb_sum->Fill(ctbAdcSum, zdcEastUnattSum);
-    contents.h_zdcw_sum_vs_ctb_sum->Fill(ctbAdcSum, zdcWestUnattSum);
+    contents.h_zdce_sum_vs_zdcw_sum->Fill(zdcWestUnattSum, zdcEastUnattSum);
+
+
+    contents.h_bbce_adc->Fill(bbcADCeast);
+    contents.h_bbcw_adc->Fill(bbcADCwest);
+    float TOF_L1mult = (float)trgd->tofMultiplicity(0);
+
+    contents.upcTOF_L1mult_vs_ZDCadcsum->Fill(TOF_L1mult, trgd->zdcHardwareSum());
+
   }
 
   if(trgd) 
