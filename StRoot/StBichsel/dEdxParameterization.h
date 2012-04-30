da8e3dd9c8ca46d4319031aad9d00ab8d598ef60
Yuri Fisyak <fisyak@bnl.gov>
2012-04-30 14:33:35 +0000

    Step back with interpolation update

--- StRoot/StBichsel/dEdxParameterization.h
+++ StRoot/StBichsel/dEdxParameterization.h
@@ -11,7 +11,7 @@
 #include "TProfile2D.h"
 #include "TString.h"
 //#include "Bichsel.h"
-//#define __OWN_INTERPOLATION__
+
 class dEdxParameterization {
  private: 
   TString      fTag;                 //! Tag for  root file (Bichsel or PAI)
@@ -30,12 +30,6 @@ class dEdxParameterization {
   Double_t     fAverageZShift;       //!
   Double_t     fI70Shift;            //!
   Double_t     fI60Shift;            //!
-  Double_t     fbgL10min;
-  Double_t     fbgL10max;
-  Double_t     fdxL2min;
-  Double_t     fdxL2max;
-  Double_t     fzmin;
-  Double_t     fzmax;
  public :
     dEdxParameterization(const Char_t *Tag="p10", Int_t keep3D = 0,
 			 const Double_t MostProbableZShift = 0,
@@ -43,24 +37,32 @@ class dEdxParameterization {
 			 const Double_t I70Shift           = 1,
 			 const Double_t I60Shift           = 1);
   virtual ~dEdxParameterization();
-  Double_t    MostProbableZCorrection(Double_t log10bg);
-  Double_t    I70Correction(Double_t log10bg);
-#ifdef  __OWN_INTERPOLATION__
   Double_t    Interpolation(Int_t Narg, TH1 *hist, Double_t *XYZ, Int_t kase = 0);
   Double_t    Interpolation(TH3 *hist, Double_t X, Double_t Y, Double_t Z, Int_t kase = 0);
   Double_t    Interpolation(TH2 *hist, Double_t X, Double_t Y, Int_t kase = 0);
   Double_t    Interpolation(TH1 *hist, Double_t X, Int_t kase = 0);
+  Double_t    MostProbableZCorrection(Double_t log10bg);
+  Double_t    I70Correction(Double_t log10bg);
   Double_t    GetMostProbableZ(Double_t log10bg, Double_t log2dx, Int_t kase=0) {
     return fMostProbableZShift+Interpolation(fP,log10bg,log2dx,kase);
   }
+  Double_t    GetMostProbableZM(Double_t log10bg, Double_t log2dx, Int_t kase=0) {
+    return MostProbableZCorrection(log10bg)+GetMostProbableZ(log10bg,log2dx,kase);
+  }
   Double_t    GetAverageZ(Double_t log10bg, Double_t log2dx, Int_t kase=0) {
     return fAverageZShift+MostProbableZCorrection(log10bg)+Interpolation(fA,log10bg,log2dx,kase);
+  }
+  Double_t    GetAverageZM(Double_t log10bg, Double_t log2dx, Int_t kase=0) {
+    return MostProbableZCorrection(log10bg)+GetAverageZ(log10bg,log2dx,kase);
   }
   Double_t    GetRmsZ(Double_t log10bg, Double_t log2dx, Int_t kase=0) {
     return Interpolation(fRms,log10bg,log2dx,kase);
   }
   Double_t    GetI70(Double_t log10bg, Double_t log2dx, Int_t kase=0)  {
     return fI70Shift*Interpolation(fI70,log10bg,log2dx,kase);
+  }
+  Double_t    GetI70M(Double_t log10bg, Double_t log2dx, Int_t kase=0)  {
+    return I70Correction(log10bg)*GetI70(log10bg,log2dx,kase);
   }
   Double_t    GetI60(Double_t log10bg, Double_t log2dx, Int_t kase=0)  {
     return fI60Shift*Interpolation(fI60,log10bg,log2dx,kase);
@@ -73,72 +75,6 @@ class dEdxParameterization {
   }
   Double_t    GetProbability(Double_t log10bg, Double_t log2dx, Double_t z, Int_t kase=0) {
     return Interpolation(fPhi,log10bg,log2dx,z,kase);}
-  Double_t    GetMostProbableZM(Double_t log10bg, Double_t log2dx) {
-    return MostProbableZCorrection(log10bg)+GetMostProbableZ(log10bg,log2dx,kase);
-  }
-  Double_t    GetAverageZM(Double_t log10bg, Double_t log2dx, Int_t kase=0) {
-    return MostProbableZCorrection(log10bg)+GetAverageZ(log10bg,log2dx,kase);
-  }
-  Double_t    GetI70M(Double_t log10bg, Double_t log2dx, Int_t kase=0)  {
-    return I70Correction(log10bg)*GetI70(log10bg,log2dx,kase);
-  }
-#else /* !  __OWN_INTERPOLATION__ */ 
-  Double_t    GetMostProbableZ(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fMostProbableZShift+fP->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetAverageZ(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fAverageZShift+MostProbableZCorrection(log10bg)+fA->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetRmsZ(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fRms->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetI70(Double_t log10bg, Double_t log2dx)  {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fI70Shift*fI70->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetI60(Double_t log10bg, Double_t log2dx)  {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fI60Shift*fI60->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetMostProbabledEdx(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fD->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetdEdxWidth(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return fW->Interpolate(log10bg,log2dx);
-  }
-  Double_t    GetProbability(Double_t log10bg, Double_t log2dx, Double_t z) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx  = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    z       = TMath::Max(fzmin, TMath::Min(fzmax,z));
-    return fPhi->Interpolate(log10bg,log2dx,z);}
-  Double_t    GetMostProbableZM(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return MostProbableZCorrection(log10bg)+GetMostProbableZ(log10bg,log2dx);
-  }
-  Double_t    GetAverageZM(Double_t log10bg, Double_t log2dx) {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return MostProbableZCorrection(log10bg)+GetAverageZ(log10bg,log2dx);
-  }
-  Double_t    GetI70M(Double_t log10bg, Double_t log2dx)  {
-    log10bg = TMath::Max(fbgL10min, TMath::Min(fbgL10max,log10bg));
-    log2dx = TMath::Max(fdxL2min, TMath::Min(fdxL2max,log2dx));
-    return I70Correction(log10bg)*GetI70(log10bg,log2dx);
-  }
-#endif /*  __OWN_INTERPOLATION__ */
   void        Print();
   const Char_t      *Tag() const {return    fTag.Data();}   
   const TProfile2D  *P()   const {return     fP;}     
@@ -149,9 +85,7 @@ class dEdxParameterization {
   const TProfile2D  *Rms() const {return   fRms;}   
   const TProfile2D  *W()   const {return     fW;}     
   const TH3D        *Phi() const {return   fPhi;}     
-  Double_t bgL10min() const {return fbgL10min;}
-  Double_t bgL10max() const {return fbgL10max;}
   ClassDef(dEdxParameterization,0)
-};
+    };
 #endif
 
