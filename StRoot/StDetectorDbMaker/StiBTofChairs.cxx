6065ce32b98246a37f1d195aaf77268ea68a3eec
Yuri Fisyak <fisyak@bnl.gov>
2012-05-07 14:18:35 +0000

    Add more chairs

---
+++ StRoot/StDetectorDbMaker/StiBTofChairs.cxx
@@ -0,0 +1,4 @@
+#include "StarChairDefs.h"
+#include "St_db_Maker/St_db_Maker.h"
+#include "StiBTofHitErrorCalculator.h"
+MakeChairInstance2(HitError,StiBTofHitErrorCalculator,Calibrations/tracker/BTofHitError);
