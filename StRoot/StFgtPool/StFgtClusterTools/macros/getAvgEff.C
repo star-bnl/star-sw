a4d0c2709c6a66f7e9273ef5d8f4381d35a7b375
avossen <>
2012-05-30 15:33:14 +0000

    added efficiency computation macro

--- StRoot/StFgtPool/StFgtClusterTools/macros/getAvgEff.C
+++ StRoot/StFgtPool/StFgtClusterTools/macros/getAvgEff.C
@@ -9,21 +9,26 @@ TFile f("signalShapes.root");
 Double_t eff=0;
 Int_t count=0;
 
- Double_t overallCounts=0;
+ Double_t overallErr=0;
 for(Int_t i=1;i<h->GetNbinsX()-1;i++)
   {
     for(Int_t j=1;j<h->GetNbinsY()-1;j++)
       {
-	if(h->GetBinContent(i,j)>0.05)
+	Double_t efficiency=h->GetBinContent(i,j);
+	if(efficiency>0.05)
 	  {
-	    Int_t numCounts=hEff->GetBinContent(i,j)+hNonEff->GetBinContent(i,j);
+	    Int_t numEff=hEff->GetBinContent(i,j);
+	    Int_t numNonEff=hNonEff->GetBinContent(i,j);
+	    Int_t numCounts=numEff+numNonEff;
 	    //
-	    eff+=(h->GetBinContent(i,j))*numCounts;
-	    overallCounts+=numCounts;
+	    Double_t relErr=sqrt((1/(Double_t)(numEff+numNonEff)+1/(Double_t)numEff));
+	    Double_t err=relErr*efficiency;
+	    eff+=efficiency/(err*err);
+	    overallErr+=(1/(err*err));
 	    count++;
 	  }
       }
   }
 //cout <<"avg eff: " << eff/count <<endl;
-cout <<"avg eff: " << eff/overallCounts <<endl;
+cout <<"avg eff: " << eff/overallErr <<endl;
 }
