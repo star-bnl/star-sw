ceb05bbec5c1d35ba5d08b9f27cbe27bdabee019
cvs2git <cvs2git>
2012-06-04 15:14:20 +0000

    This commit was manufactured by cvs2git to create branch 'SL10k_patch2'.

--- StRoot/StJetMaker/towers/StjAbstractTowerEnergyCorrectionForTracks.cxx
+++
@@ -1,9 +0,0 @@
-//
-// Pibero Djawotho <pibero@tamu.edu>
-// Texas A&M University
-// 25 August 2009
-//
-
-#include "StjAbstractTowerEnergyCorrectionForTracks.h"
-
-ClassImp(StjAbstractTowerEnergyCorrectionForTracks);
--- StRoot/StStarLogger/logging/StUcmTask.cxx
+++
@@ -1,9 +0,0 @@
-#include "StUcmTask.h"
-using namespace TxLogging;
-StUcmTask::StUcmTask()  {;}
-StUcmTask::~StUcmTask() {;}
-const RecordList &StUcmTask::getJobs() const
-{
-   return getJobs();
-}
-
