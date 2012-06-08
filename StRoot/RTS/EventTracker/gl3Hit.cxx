1db4492b16fbb060211aad36c368aec08e5be873
Jeffery M Landgraf <jml@bnl.gov>
2012-06-08 14:47:27 +0000

    blah

--- StRoot/RTS/EventTracker/gl3Hit.cxx
+++ StRoot/RTS/EventTracker/gl3Hit.cxx
@@ -4,7 +4,7 @@
 //    Print a gl3Hit 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 void gl3Hit::print (  ){
-   printf ( "%d %7.2f %7.2f %7.2f\n", rowSector, x, y, z );
+  printf ( "%d %d %d %7.2f %7.2f %7.2f %7.2f\n", rowSector, mezzanninneCard, readoutBoard, x, y, z,sqrt(x*x + y*y) );
 }
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //    Set a gl3Hit from a l3_cluster structure
@@ -18,16 +18,22 @@ int gl3Hit::set ( l3CoordinateTransformer* transformer,
    double pad  = double(cluster->pad)/64.;
    double time = double(cluster->time)/64.;
 
+   //printf("pad=%7.2lf time=%7.2lf padrow=%d sector=%d\n",pad,time,cluster->padrow,sector);
+
    readoutBoard    = cluster->RB_MZ / 16 ;
    if ( readoutBoard > 5  && sector%2==1) sector++ ; // when two sectors in one crate
                                       // last 3 readout boards belong to next sector
    mezzanninneCard = cluster->RB_MZ%16 ; 
 
    rowSector  = sector * 100 + cluster->padrow ;
-   
+
+
+   //printf("--->pad=%7.2lf time=%7.2lf padrow=%d sector=%d\n",pad,time,cluster->padrow,sector);
+
+
 // rawToGlobal(sector, (int)cluster->padrow, pad, time, &xx, &yy, &zz);
  
-   PTRS.Setptrs((double)pad, (double)time,(double)(cluster->padrow), (double) sector) ;
+   PTRS.Setptrs((double)pad, (double)time,(double)(cluster->padrow+1), (double) sector) ;
    transformer->raw_to_global(PTRS,XYZ) ;
 
    x = XYZ.Getx();
