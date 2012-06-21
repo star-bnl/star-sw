42d966f2765568645de4b36a3949dfff9dd97ea4
Yuri Fisyak <fisyak@bnl.gov>
2012-06-21 19:00:06 +0000

    Swap StTpcDb and StEvent

--- StRoot/macros/Load.C
+++ StRoot/macros/Load.C
@@ -1,10 +1,10 @@
-// $Id: Load.C,v 1.21 2011/02/01 23:52:53 genevb Exp $
+// $Id: Load.C,v 1.22 2012/06/21 19:00:06 fisyak Exp $
 //=======================================================================
 // owner: Yuri Fisyak
 // what it does: 
 //=======================================================================
 
-void Load(Char_t *loadList="St_base,St_Tables,StChain,StDetectorDbMaker,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StTriggerDataMaker,StEvent,StEventUtilities,StBFChain"){
+void Load(Char_t *loadList="St_base,St_Tables,StChain,StDetectorDbMaker,StEvent,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StTriggerDataMaker,StEventUtilities,StBFChain"){
   TString opt(loadList);
   TString separator = "[^ ;,]+";
   TObjArray *array = opt.Tokenize(separator);
