d4d40e3e5c2da2a13369c6224ae7754e46d277b5
Frank Geurts <geurts@rice.edu>
2012-06-21 20:36:27 +0000

    create board or cell-based zeroed entries

--- StRoot/StBTofPool/dbase/macros/zerotray.sh
+++ StRoot/StBTofPool/dbase/macros/zerotray.sh
@@ -1,18 +1,48 @@
 #!/bin/sh
 # generate zeroed tray entries
 
-for module in `seq 1 32`; do
-  for cell in `seq 1 6`; do
-   printf "%4.3s%4.3s%4.3s\n" $1 $module $cell
-   printf "%4.3s\n" 39
+case "$1" in
 
-   for i in `seq 1 40`; do
-    printf "%16.1f" 0
-   done
-   printf "\n"
-   for i in `seq 1 40`; do
-    printf "%16.1f" 0
-   done
-   printf "\n"
-  done
-done
+[0-9]*)
+     # cell-based calibration parameters
+     if [ $2 == 'c' ]; then
+            for module in `seq 1 32`; do
+              for cell in `seq 1 6`; do
+               printf "%4.3s%4.3s%4.3s\n" $1 $module $cell
+               printf "%4.3s\n" 39
+            
+               for i in `seq 1 40`; do
+                printf "%16.1f" 0
+               done
+               printf "\n"
+               for i in `seq 1 40`; do
+                printf "%16.1f" 0
+               done
+               printf "\n"
+              done
+            done
+
+     # board-based calibration parameters
+     elif [ $2 == 'b' ]; then
+            for board in `seq 1 8`; do
+               printf "%4.3s%4.3s%4.3s\n" $1 $board
+               printf "%4.3s\n" 39
+            
+               for i in `seq 1 40`; do
+                printf "%16.1f" 0
+               done
+               printf "\n"
+               for i in `seq 1 40`; do
+                printf "%16.1f" 0
+               done
+               printf "\n"
+              done
+     fi
+     ;;
+
+*)
+     # Print quick usage report
+     echo "Usage: $0 <tray> <c|b> > <tray>.txt"
+    ;;
+
+esac
