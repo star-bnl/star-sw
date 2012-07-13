81b016dd99065b9af58db2733340bdb7cee7b916
perev <>
2012-07-13 23:14:17 +0000

    Added lane number to Show

--- StRoot/Stv/StvDraw.h
+++ StRoot/Stv/StvDraw.h
@@ -22,8 +22,8 @@ TObject *Hits(const std::vector<const float*> &hits, EDraw3DStyle sty);
 
 TObject *Trak(const THelixTrack &helx,const std::vector<const StvHit*>  &hits, EDraw3DStyle sty=kGlobalTrack);
 TObject *Trak(const THelixTrack &helx,const std::vector<      StvHit*>  &hits, EDraw3DStyle sty=kGlobalTrack);
-TObject *Trak(const std::vector<float> &pnts, EDraw3DStyle sty=kGlobalTrack);
-   void  Trak(const StvTrack *tk, EDraw3DStyle sty=kGlobalTrack);
+TObject *Trak(const std::vector<float> &pnts, EDraw3DStyle sty=kGlobalTrack,Color_t col=kRed);
+   void  Trak(const StvTrack *tk, int dir = 2, EDraw3DStyle sty=kGlobalTrack);
 
    void  Road(const StvTrack *tk, double wide=5,EDraw3DStyle sty=kGlobalTrack);
    void  All(const char *opt);
@@ -33,16 +33,16 @@ static int ProcessEvents();
 static StvDraw *Inst() {if (!fgStvDraw) fgStvDraw=new StvDraw(); return fgStvDraw;}
 static StvDraw *Jnst() {return fgStvDraw;}
 static void Wait();
-static void Show(const StvTrack *tk);
+static void Show(const StvTrack *tk, int dir=0);
 static void Zhow(const StvTrack *tk);
 static void Klear();
 static void KBrowse(const TObject *to);
 private:
-   void Join(const StvNode *left,const StvNode *rite,StvPoints &poits);
+   void Join(const StvNode *left,const StvNode *rite,StvPoints &poits,int dir=2);
 private:
 int mNDoIt;
 int mNPow2;
-
+int mIColor;		//Current color index when it is changing in cycle 
 static StvDraw *fgStvDraw;
 };
 
