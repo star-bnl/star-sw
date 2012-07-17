e5c4386f34e24d6b9c600a653fe811b34d2112d9
Pibero Djawotho <pibero@tamu.edu>
2012-07-17 06:08:14 +0000

    Added functionality for 2012

---
+++ StRoot/StSpinPool/StFmsTriggerMaker/fms_fm001_2012_a.hh
@@ -0,0 +1,14 @@
+//
+// Pibero Djawotho <pibero@tamu.edu>
+// Texas A&M University
+// 11 Feb 2012
+//
+
+#ifndef FMS_FM001_2012_A_HH
+#define FMS_FM001_2012_A_HH
+
+struct Board;
+
+void fms_fm001_2012_a(Board& fm001);
+
+#endif	// FMS_FM001_2012_A_HH
---
+++ StRoot/StSpinPool/StFmsTriggerMaker/fms_fm101_2012_a.hh
@@ -0,0 +1,14 @@
+//
+// Pibero Djawotho <pibero@tamu.edu>
+// Texas A&M University
+// 9 Feb 2012
+//
+
+#ifndef FMS_FM101_2012_A_HH
+#define FMS_FM101_2012_A_HH
+
+struct Board;
+
+void fms_fm101_2012_a(Board& fm101);
+
+#endif	// FMS_FM101_2012_A_HH
