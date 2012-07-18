4f4c588d98111fad5b8dbe21eca43f18dfd0a678
Pibero Djawotho <pibero@tamu.edu>
2012-07-18 22:36:31 +0000

    Run FMS simulator in BFC

---
+++ StRoot/StSpinPool/StFmsSimulatorMaker/macros/bfcFms.C
@@ -0,0 +1,24 @@
+//
+// Pibero Djawotho <pibero@tamu.edu>
+// Texas A&M
+// 18 July 2012
+//
+// Run bfc.C with FMS fast simulator in the chain to import FPD/FMS hits
+// from GEANT to StEvent to MuDst.
+//
+
+class StBFChain;
+StBFChain* chain = 0;
+
+void bfcFms(int nevents = 1000,
+	    const char* fzfile = "test.fzd",
+	    const char* chainopt = "DbV20110923 tpcRS y2011 fmsDb MakeEvent ITTF NoSsdIt NoSvtIt Idst BAna l0 Tree logger Sti VFPPVnoCTB beamLine tpcDB TpcHitMover TpxClu bbcSim btofsim tags emcY2 EEfs evout -dstout IdTruth geantout big fzin MiniMcMk clearmem sdt20110417.193427")
+{
+  gROOT->LoadMacro("bfc.C");
+  bfc(-1,chainopt,fzfile);
+  gSystem->Load("StFmsSimulatorMaker");
+  StFmsFastMaker* fmsFastSim = new StFmsFastMaker;
+  chain->AddAfter("fmsDb",fmsFastSim);
+  chain->Init();
+  chain->EventLoop(nevents);
+}
