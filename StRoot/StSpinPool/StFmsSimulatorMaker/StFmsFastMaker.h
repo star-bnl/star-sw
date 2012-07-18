114c2d7739e2cd9254adf67d46bce7677574e896
Pibero Djawotho <pibero@tamu.edu>
2012-07-18 22:36:07 +0000

    Remove dependency on StMcEvent

--- StRoot/StSpinPool/StFmsSimulatorMaker/StFmsFastMaker.h
+++ StRoot/StSpinPool/StFmsSimulatorMaker/StFmsFastMaker.h
@@ -9,23 +9,22 @@
 #ifndef ST_FMS_FAST_MAKER_H
 #define ST_FMS_FAST_MAKER_H
 
-class StMcEvent;
-class StEvent;
-class StMcCalorimeterHit;
+class St_g2t_emc_hit;
+class g2t_emc_hit_st;
 class StFmsHit;
+class StEvent;
 
 #include "StMaker.h"
 
 class StFmsFastMaker : public StMaker {
 public:
   StFmsFastMaker(const char* name = "fmsfastsimu") : StMaker(name) {}
-
   int Make();
 
 private:
-  StFmsHit* makeFmsHit(const StMcCalorimeterHit* mcHit) const;
-  int getDetectorId(const StMcCalorimeterHit* mcHit) const;
-  void fillStEvent(StMcEvent*, StEvent*);
+  int getDetectorId(int ew, int nstb) const;
+  StFmsHit* makeFmsHit(const g2t_emc_hit_st&) const;
+  void fillStEvent(const St_g2t_emc_hit*, StEvent*);
 
   ClassDef(StFmsFastMaker,0);
 };
