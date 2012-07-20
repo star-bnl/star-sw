b8eadb95e839cbaf7dea3d47f85e0bebcbe5f4d0
pibero <>
2012-07-20 20:37:20 +0000

    Minor updates

--- StRoot/StSpinPool/StFmsSimulatorMaker/StFmsSimulatorMaker.h
+++ StRoot/StSpinPool/StFmsSimulatorMaker/StFmsSimulatorMaker.h
@@ -22,6 +22,7 @@ public:
   int Make();
 
 private:
+  void decodeVolumeId(int volumeId, int& ew, int& nstb, int& channel) const;
   int getDetectorId(int ew, int nstb) const;
   StFmsHit* makeFmsHit(const g2t_emc_hit_st&) const;
   void fillStEvent(const St_g2t_emc_hit*, StEvent*);
