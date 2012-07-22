c46bb9c41bc9f3aed5d3cca0f2fac611193b837e
pibero <>
2012-07-22 19:33:39 +0000

    Use "fmsSim" chain option

--- StRoot/StSpinPool/StFmsSimulatorMaker/macros/bfcFms.C
+++ StRoot/StSpinPool/StFmsSimulatorMaker/macros/bfcFms.C
@@ -6,6 +6,12 @@
 // Run bfc.C with FMS fast simulator in the chain to import FPD/FMS hits
 // from GEANT to StEvent to MuDst.
 //
+// Note, as of July 18, 2012, this way of running the FMS simulator is no
+// longer necessary. Instead, it is recommended to use the "fmsSim" chain option
+// directly with bfc.C:
+//
+// root4star -q -b bfc.C'(1000,"DbV20110923 tpcRS y2011 fmsDb fmsSim MakeEvent ITTF NoSsdIt NoSvtIt Idst BAna l0 Tree logger Sti VFPPVnoCTB beamLine tpcDB TpcHitMover TpxClu bbcSim btofsim tags emcY2 EEfs evout -dstout IdTruth geantout big fzin MiniMcMk clearmem sdt20110417.193427")'
+//
 
 class StBFChain;
 StBFChain* chain = 0;
