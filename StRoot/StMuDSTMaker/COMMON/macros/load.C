e984909f90c7ddff3832bf206c1c17ebf6914b79
cvs2git <cvs2git>
2012-07-31 22:08:53 +0000

    This commit was manufactured by cvs2git to create tag 'SL12a'.

--- StRoot/StMuDSTMaker/COMMON/macros/load.C
+++ StRoot/StMuDSTMaker/COMMON/macros/load.C
@@ -4,18 +4,4 @@ void load() {
   cout << " THIS macro is OBSOLETE and will be later removed  " << endl;
   cout << " Please, use 'loadSharedLibraries.C' instead       " << endl;
   cout << "***************************************************" << endl;
-  gSystem->Load("libTable");
-  gSystem->Load("libPhysics");
-  gSystem->Load("St_base");
-  gSystem->Load("StChain");
-  gSystem->Load("St_Tables");
-  gSystem->Load("StUtilities");         // new addition 22jul99
-  gSystem->Load("StarClassLibrary");
-  gSystem->Load("StTriggerDataMaker");  // new starting from April 2003
-  gSystem->Load("StEvent");
-  gSystem->Load("StEventUtilities");
-  gSystem->Load("StEmcUtil");
-  gSystem->Load("StStrangeMuDstMaker");
-  gSystem->Load("StMuDSTMaker");  
-  cout << " loading of shared libraries done" << endl;
  }
