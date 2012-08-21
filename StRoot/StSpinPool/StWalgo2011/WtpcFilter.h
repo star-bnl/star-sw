9161afff6953dba2a146c10cbbc8458cc48fa4ab
stevens4 <>
2012-08-21 17:40:15 +0000

    Revert to previous version

--- StRoot/StSpinPool/StWalgo2011/WtpcFilter.h
+++ StRoot/StSpinPool/StWalgo2011/WtpcFilter.h
@@ -1,4 +1,4 @@
-// $Id: WtpcFilter.h,v 1.3 2012/08/20 23:43:01 smirnovd Exp $
+// $Id: WtpcFilter.h,v 1.4 2012/08/21 17:40:09 stevens4 Exp $
 //
 //*-- Author : Jan Balewski, MIT
 
@@ -15,28 +15,25 @@ class TObjArray;
 class TH1;
 class StMuTrack ;
 
-class WtpcFilter
-{
-public:
-   WtpcFilter();
-   void init(const char* core, int sec, TObjArray* HListX, bool barrel);
-   void setCuts(int x, float y, float r1, float r2) {
-      par_nFitPts = x; par_nHitFrac = y; par_Rmin = r1; par_Rmax = r2;
-   }
-   static int getTpcSec(float phiRad, float etaDet);
-   bool accept( const StMuTrack*  prMuTrack);
+class WtpcFilter {
+ public:
+  WtpcFilter();
+  void init(const char *core, int sec, TObjArray *HListX, bool barrel);
+  void setCuts(int x, float y, float r1, float r2) {
+    par_nFitPts=x; par_nHitFrac=y; par_Rmin=r1; par_Rmax=r2; }
+  static int getTpcSec(float phiRad, float etaDet);
+  bool accept( const StMuTrack  *prMuTrack);
 
-private:
-   TString name;
-   int secID;
-   int par_nFitPts;
-   float par_nHitFrac, par_Rmin, par_Rmax;
+ private:
+  TString name;
+  int secID;
+  int par_nFitPts;
+  float par_nHitFrac, par_Rmin, par_Rmax;
 
-   // histograms
-   TObjArray* HList;
-   enum {mxHA = 8};
-   TH1* hA[mxHA];
-   void initHistos(bool barrel);
+  // histograms
+  TObjArray *HList;
+  enum {mxHA=8}; TH1 * hA[mxHA];
+  void initHistos(bool barrel);
 
 };
 
@@ -45,8 +42,8 @@ private:
 
 
 // $Log: WtpcFilter.h,v $
-// Revision 1.3  2012/08/20 23:43:01  smirnovd
-// Improved readability
+// Revision 1.4  2012/08/21 17:40:09  stevens4
+// Revert to previous version
 //
 // Revision 1.2  2012/06/18 18:28:01  stevens4
 // Updates for Run 9+11+12 AL analysis
