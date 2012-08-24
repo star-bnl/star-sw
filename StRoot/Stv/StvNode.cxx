a5e203f0a6214be2dbf4434ae73b2a60d72f322c
perev <>
2012-08-24 19:52:31 +0000

    UpdateDca simplified

--- StRoot/Stv/StvNode.cxx
+++ StRoot/Stv/StvNode.cxx
@@ -1,6 +1,6 @@
 //StvKalmanTrack.cxx
 /*
- * $Id: StvNode.cxx,v 1.11 2012/07/21 18:58:32 perev Exp $
+ * $Id: StvNode.cxx,v 1.12 2012/08/24 19:52:16 perev Exp $
  *
  * /author Victor Perev
  */
@@ -53,48 +53,70 @@ double StvNode::GetTime() const
 //________________________________________________________________________________
 void StvNode::Print(const char *opt) const
 {
-static const char *txt = "XYZAPTCHRELK";
-static const char *hhh = "xyzre";
+static const char *txt = "X Y Z A P T C H R E L K U Xi P[ E[ ";
+static const char *hhh = "x y z r e ";
   if (!opt || !opt[0]) opt = "_";
-  TString myOpt(opt);
-  if (myOpt.Contains("_")) myOpt.ReplaceAll("_","2RZErze =0");
-  int idx = myOpt.Index("=");
-  int djr = (idx>=0)? myOpt[idx+1]-'0':0; //mPP[4]==mFP[2]
-  int dir = (djr<=2)? djr+2:djr-3;
-
+  TString myOpt(opt);myOpt+=" ";
+  int dir = myOpt.Index("=");
+  dir = (dir<0)? 2:myOpt[dir+1]-'0';
+  int djr = dir; if (djr>2) djr-=3;
   double val,err[2];
-  const StvNodePars &fp= mPP[dir];
-  const StvFitErrs  &fe= mPE[dir];
+  const StvNodePars &fp= mFP[dir];
+  const StvFitErrs  &fe= mFE[dir];
   StvHit *hit = GetHit();
   TString ts; if (hit) {ts = (mXi2[0]>1e3 && mXi2[1]>1e3)? "h":"H";}
   if (GetType()==kDcaNode ) ts='D';
   if (GetType()==kPrimNode) ts='P';
   printf("%p(%s)",(void*)this,ts.Data());
   printf("\t%s=%g","Xi2",GetXi2(djr));
+  int iopt=0;
+  const char *myopt = myOpt.Data(); char*e;
   for (int i=0;txt[i];i++) {
+    if (txt[i]==' ') continue;
     err[0]=-999;val=-999;
-    if (myOpt.Index(TString(txt[i]))<0) continue;
-    if (txt[i]=='R') 		{val = fp.getRxy();}
-    else if (txt[i]=='P')	{val = fp.getPt() ;}
-    else if (txt[i]=='E')	{err[0] = sqrt(fe.mHH); err[1] = sqrt(fe.mZZ);}
-    else if (txt[i]=='L')	{val = GetLen();}
-    else if (txt[i]=='K')	{val = fe.MaxCorr();}
-    else 			{val = fp[i]      ;}
-    if (abs(val+999)>1e-6) 	{ printf("\t%c=%g",txt[i],val);}
-    if (err[0]>-999)  		{ printf("\tHH=%7.2g ZZ=%7.2g",err[0],err[1]);}
+    if ((iopt=myOpt.Index(TString(txt+i,2)))<0) continue;
+    if (txt[i+1]==' ') {//Single letter request 
+           if (txt[i]=='R') 	{val = fp.getRxy();}
+      else if (txt[i]=='P')	{val = fp.getPt() ;}
+      else if (txt[i]=='E')	{err[0] = sqrt(fe.mHH); err[1] = sqrt(fe.mZZ);}
+      else if (txt[i]=='L')	{val = GetLen();}
+      else if (txt[i]=='K')	{val = fe.MaxCorr();}
+      else if (txt[i]=='U')	{val = fe.mPP;}
+      else if (txt[i]=='H')	{val = fp._hz;}
+      else                      {val = fp[i];}
+      if (abs(val+999)>1e-6) 	{ printf("\t%c=%g",txt[i],val);}
+      if (err[0]>-999)  	{ printf("\tHH=%7.2g ZZ=%7.2g",err[0],err[1]);}
+      } else if (txt[i+1]=='[') {// now print by index
+      int idx = strtol(myopt+iopt+2,&e,10);
+      TString tnam(myopt+iopt,e-(myopt+iopt)+1);
+      if      (txt[i]=='P') 	{val = fp[idx];}
+      else if (txt[i]=='E') 	{val = fe[idx];}
+      else 			continue;
+      printf("\t%s=%g",tnam.Data(),val);
+    } 
   }//end for i
 
   if (hit) {
     for (int i=0;hit && hhh[i];i++) {
        err[0]=-999;val=-999;
-      if (myOpt.Index(TString(hhh[i]))<0) continue;
-      if (hhh[i]=='r') 		{val = hit->getRxy();}
-      else if (hhh[i]=='e')	{err[0] = sqrt(mHrr[0]); err[1] = sqrt(mHrr[2]);}
+      if (hhh[i]==' ') continue;
+      if ((iopt=myOpt.Index(TString(hhh+i,2)))<0) continue;
+      if (hhh[i+1]==' ')  	{//Single letter request 
+        if (hhh[i]=='r')  	{ val = hit->getRxy();}
+        else if (hhh[i]=='e')	{err[0] = sqrt(mHrr[0]); err[1] = sqrt(mHrr[2]);}
       else 			{val = hit->x()[i];} 
       if (abs(val+999)>1e-6) 	{printf("\th%c=%g",hhh[i],val);}
       if (err[0]>-999)  	{printf("\thh=%7.2g zz=%7.2g",err[0],err[1]);}
-    } 
+      } else if (txt[i+1]=='[') {// now print by index
+
+      int idx = strtol(myopt+i+2,&e,10);
+      TString tnam(myopt+i,e-(myopt+i)+1);
+      if      (txt[i]=='e') 	{val = mHrr[idx];}
+      printf("\t%s=%g",tnam.Data(),val);
+      } 
+    }
   }
+
   printf("\n");
   return;
 }    
@@ -117,21 +139,17 @@ void StvNode::SetHit(StvHit *hit)
 //________________________________________________________________________________
 void StvNode::UpdateDca()
 { 
-static double zero[2]={0};
   const StvNodePars &P = mFP[2];
-  double dL;
-  for (int iter=0;iter<4;iter++) {
-    dL =  -( P._x*P._cosCA+P._y*P._sinCA)
+  double dL = -( P._x*P._cosCA+P._y*P._sinCA)
               /(1+(-P._x*P._sinCA+P._y*P._cosCA)*P._curv);
     if (fabs(dL)<1e-6) return;
-    do {
-      if (!iter && fabs(dL*mFP[2]._curv)<1e-1) break;
-      TCircle cirk(&(P._x),&(P._cosCA),P._curv);		//????Error not touched ??
-      dL = cirk.Path(zero);
-    } while(0);
-    for (int i=0;i<5;i++) {mPP[i].move(dL);} 
-  }
-  assert(fabs(dL)<1e-6);
+    THelixTrack hlx;		
+    mFP[2].get(&hlx);
+    mFE[2].Get(&hlx);
+    dL = hlx.Path(0.,0.);
+    hlx.Move(dL);
+    mFP[2].set(&hlx,mFP[2]._hz);
+    mFE[2].Set(&hlx,mFP[2]._hz);
 }
 //________________________________________________________________________________
 int StvNode::Check(const char *tit, int dirs) const
