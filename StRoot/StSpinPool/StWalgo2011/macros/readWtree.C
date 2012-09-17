c0e76565719510a592df7667db9489e7958e5c58
Justin Stevens <jrsteven@mit.edu>
2012-09-17 03:29:48 +0000

    Updates to Endcap algo and Q*ET/PT charge separation

--- StRoot/StSpinPool/StWalgo2011/macros/readWtree.C
+++ StRoot/StSpinPool/StWalgo2011/macros/readWtree.C
@@ -31,16 +31,16 @@ void readWtree(	const Char_t *fileList="./R13104003.lis", int etaBin=8, int maxE
   TObjArray* HListZ=new TObjArray;
   TObjArray* HListEta=new TObjArray;;
   
-  //define eta ranges
-  float etaLow[8] = {-1.0, -0.5, 0.0, 0.5, 1.0, 1.15, 0.7, -1.5};
-  float etaHigh[8] = {-0.5, 0.0, 0.5, 1.0, 1.15, 1.5, 2.5, 2.0};
+  //define eta ranges (skip bin 5+6 and 9 is same as 8 except spin sorting)
+  float etaLow[9] =  {-1.0,-0.5, 0.0, 0.5, 0.0, 0.0, 0.7,-1.5,-1.5};
+  float etaHigh[9] = {-0.5, 0.0, 0.5, 1.0, 0.0, 0.0, 2.5, 2.0, 2.0};
 
   //initiate W maker
   wTreeMk = new St2011WMaker(Form("Eta%d",etaBin));
   wTreeMk->setJetTreeBranch("ConeJets12_100","ConeJets12_100_noEEMC"); //select jet tree braches used
   //set cuts for barrle and endcap algos
   wTreeMk->setWbosonCuts(25., 0.88, 14., etaLow[etaBin-1], etaHigh[etaBin-1]); //highET, nearTotEtFrac, ptBalance, etaLow, etaHigh
-  wTreeMk->setE_WbosonCuts(25., 0.9, 14., etaLow[etaBin-1], etaHigh[etaBin-1]); //highET, nearTotEtFrac, ptBalance, etaLow, etaHigh
+  wTreeMk->setE_WbosonCuts(25., 0.85, 20., etaLow[etaBin-1], etaHigh[etaBin-1]); //highET, nearTotEtFrac, ptBalance, etaLow, etaHigh
   wTreeMk->setHList(HListEta);
   wTreeMk->setMaxDisplayEve(1e6); // only first N events will get displayed 
 
@@ -53,10 +53,10 @@ void readWtree(	const Char_t *fileList="./R13104003.lis", int etaBin=8, int maxE
       lumiMk->attachWalgoMaker(wTreeMk);
     }
  
-    enum {mxSM=1}; // only one for now
+    enum {mxSM=2};
     St2011pubSpinMaker *spinMkA[mxSM];
     for(int kk=0;kk<mxSM;kk++) {
-      char ttx[100]; sprintf(ttx,"%cEta%dspin",'A',etaBin);
+      char ttx[100]; sprintf(ttx,"%cEta%dspin",'A'+kk,etaBin);
       printf("add spinMaker %s %d \n",ttx,kk);
       spinMkA[kk]=new St2011pubSpinMaker(ttx,Form("Eta%d",etaBin)); 
       spinMkA[kk]->attachWalgoMaker(wTreeMk);
@@ -64,6 +64,7 @@ void readWtree(	const Char_t *fileList="./R13104003.lis", int etaBin=8, int maxE
       //assign eta bin same as W maker
       spinMkA[kk]->setEta(etaLow[etaBin-1],etaHigh[etaBin-1]); 
       spinMkA[kk]->setEtaE(etaLow[etaBin-1],etaHigh[etaBin-1]); 
+      if(kk==1) spinMkA[kk]->setQPT(-1,-1); //disable Q/pT cut
 
       //special cases
       if(etaBin==7) {
