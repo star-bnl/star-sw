ed80456c28efc2cba5e1b5fdf9ae1aaa50f6428c
fisyak <>
2012-09-27 16:18:14 +0000

    Adjust bin center position

--- StRoot/StTpcRSMaker/TF1F.cxx
+++ StRoot/StTpcRSMaker/TF1F.cxx
@@ -10,7 +10,7 @@ void TF1F::Save(Double_t xmin, Double_t xmax, Double_t ymin, Double_t ymax, Doub
 Double_t TF1F::GetSaveL(Double_t *xx) {
   // Get value corresponding to X in array of fSave values
   if (xx[0] < fXmin || xx[0]  > fXmax || fdX <= 0) return 0.;
-  Int_t bin     = Int_t((xx[0]-fXmin)/fdX);
+  Int_t bin     = TMath::Nint((xx[0]-fXmin)/fdX);
 #if 1 /* step function */
   return fSave[bin];
 #else
@@ -26,7 +26,7 @@ Double_t TF1F::GetSaveL(Double_t *xx) {
 Double_t TF1F::GetSaveL(Int_t N, Double_t x, Double_t *y) {
   // Get values y[N] corresponding to x+i, i = [0, ..., N-1];
   memset(y, 0, N*sizeof(Double_t));
-  Int_t bin     = Int_t((x-fXmin)/fdX);
+  Int_t bin     = TMath::Nint((x-fXmin)/fdX);
   Int_t i1 = 0;
   while (bin < 0) {i1++; bin += fStep;}
   for (Int_t i = i1; i < N && bin < fNsave - 3; i++, bin += fStep) {
