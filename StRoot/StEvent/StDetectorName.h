c2cbcb9ea379ce911af1df6d66ce80486cba7fea
cvs2git <cvs2git>
2012-09-28 22:38:06 +0000

    This commit was manufactured by cvs2git to create tag 'yyang-2012_11_04'.

--- StRoot/StEvent/StDetectorName.h
+++
@@ -1,5 +0,0 @@
-#ifndef __StDetectorName__
-#define __StDetectorName__
-#include "StEnumerations.h"
-R__EXTERN const Char_t *DetectorName(Int_t /* id */);
-#endif
--- asps/Simulation/starsim/atlroot/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ class  aroot;     // atlsim-root interface class
-#pragma link C++ global kuexec;    // its instance
-#pragma link C++ class  agconvert; // converter class
-#pragma link C++ global Ag;        // its instance
-#endif
