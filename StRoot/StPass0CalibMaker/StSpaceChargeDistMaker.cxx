70fd12a3e5a791f606c252040fba6fe4af8288c6
genevb <>
2012-10-15 17:51:12 +0000

    Include distortion corrections, which must be evenly sampled per event (per hit)

--- StRoot/StPass0CalibMaker/StSpaceChargeDistMaker.cxx
+++ StRoot/StPass0CalibMaker/StSpaceChargeDistMaker.cxx
@@ -13,6 +13,8 @@
 #include "StDetectorDbMaker/St_tpcAnodeHVavgC.h"
 #include "StDetectorDbMaker/StDetectorDbTpcRDOMasks.h"
 #include "StDetectorDbMaker/St_tpcPadPlanesC.h"
+#include "StDbUtilities/StMagUtilities.h"
+#include "StDbUtilities/StTpcCoordinateTransform.hh"
 
 
 #include "TH2.h"
@@ -27,6 +29,7 @@ static const Int_t nph = 12;
 static const Double_t phmin = -TMath::Pi();
 static const Double_t phmax = TMath::Pi();
 static const Double_t PhiMax = TMath::Pi()/12;
+static const Double_t Phi2Max = TMath::Pi()/6;
 static const Double_t ZdcMax = 2e6;
 
 static const Double_t MINGAIN = 0.1;
@@ -41,6 +44,11 @@ StSpaceChargeDistMaker::StSpaceChargeDistMaker(const char *name):StMaker(name),
   run = 0;
   throws = 500000; // default = 500k throws
   trigs.Set(0);
+  memset(Xpads,0,128*sizeof(Float_t));
+  memset(Npads,0,128*sizeof(UShort_t));
+  memset(XMIN,0,128*sizeof(Float_t));
+  memset(YMIN,0,32768*sizeof(Float_t));
+  memset(gainCorr,0,4096*sizeof(Float_t));
 }
 //_____________________________________________________________________________
 StSpaceChargeDistMaker::~StSpaceChargeDistMaker() {
@@ -87,7 +95,7 @@ Int_t StSpaceChargeDistMaker::Finish() {
 }
 //_____________________________________________________________________________
 Int_t StSpaceChargeDistMaker::InitRun(Int_t run) {
-  if (Rhist->GetEntries() < 1) Pads();
+  if (Rhist->GetEntries() < 1) GeomInit();
   return kStOk;
 }
 //_____________________________________________________________________________
@@ -136,6 +144,12 @@ Int_t StSpaceChargeDistMaker::Make() {
     if (!passTrigs) return kStOK;
   }
 
+  if (!gStTpcDb) {
+    LOG_ERROR << "StSpaceChargeDistMaker: no gStTpcDb - should quit!" << endm;
+    return kStFatal;
+  }
+  static StTpcCoordinateTransform transform(gStTpcDb);
+  static StTpcLocalCoordinate  coorLT; // local TPC coordinate
 
   StTpcHitCollection* TpcHitCollection = event->tpcHitCollection();
   if (TpcHitCollection) {
@@ -158,16 +172,33 @@ Int_t StSpaceChargeDistMaker::Make() {
             UInt_t NoHits = rowCollection->hits().size();
             for (UInt_t k = 0; k < NoHits; k++) {
               StTpcHit* tpcHit = TpcHitCollection->sector(i)->padrow(j)->hits().at(k);
+              const StThreeVectorF& positionG = tpcHit->position();
+
+              // Discard post-central-membrane and hist at gated grid opening
+              if (positionG.z() * (((float) i)-11.5) > 0) continue;
+              if (TMath::Abs(positionG.z()) > 180) continue;
+
+              // Discard outermost 3 pads on any row (pad is [1..Npad])
+              if (tpcHit->pad() < 4 || tpcHit->pad() > Npads[j]-3) continue;
+
+              // Want to fill histograms in TPC local, sector-aligned coordinates
+              // Need to convert global coords to TPC local coords
+              StGlobalCoordinate coorG(positionG);
+              transform(coorG,coorLT,i+1,j+1);
+              StThreeVectorD& positionL = coorLT.position();
+
               float charge = tpcHit->charge() * gainCorr[i*128 + j];
-              Space3ChargePRZ->Fill(tpcHit->position().phi(),
-                                    tpcHit->position().perp(),
-                                    tpcHit->position().z(),
+              Space3ChargePRZ->Fill(positionL.phi(),
+                                    positionL.perp(),
+                                    positionL.z(),
                                     charge);
               if (tpcHit->trackReferenceCount() > 0)
-              Space3ChargeU->Fill(tpcHit->position().phi(),
-                                    tpcHit->position().perp(),
-                                    tpcHit->position().z(),
+              Space3ChargeU  ->Fill(positionL.phi(),
+                                    positionL.perp(),
+                                    positionL.z(),
                                     charge);
+              if (throws<1 && k%((int) (1.0/throws))==0)
+                GeomFill(tpcHit);
             }
           }
         }
@@ -177,17 +208,13 @@ Int_t StSpaceChargeDistMaker::Make() {
   return kStOk;
 }
 //_____________________________________________________________________________
-void StSpaceChargeDistMaker::Pads() {
+void StSpaceChargeDistMaker::GeomInit() {
 
-  // Monte Carlo approach to determining acceptance
-  LOG_INFO << "StSpaceChargeDistMaker: Start filling acceptance histograms..." << endm;
+  // Calculated in sector 3 coordinates
 
-  Double_t Xpads[128];
-  Int_t Npads[128];
-
-  const Int_t NP = St_tpcPadPlanesC::instance()->padRows(); // # padrows (45)
-  const Int_t NR = 256; // max # pads/padrow (actually 182)
-  const Int_t NS = NP * NR; // > # pads/sector
+  NP = St_tpcPadPlanesC::instance()->padRows(); // # padrows (45)
+  NR = 256; // max # pads/padrow (actually 182)
+  NS = NP * NR; // > # pads/sector
 
   /*
   Double_t widths[2] = {1.2, 2.}; //{1.15, 1.95};
@@ -207,13 +234,10 @@ void StSpaceChargeDistMaker::Pads() {
     124,126,128,128,130,132,134,136,138,138,140,142,144,144,144,144};
   */
 
-  Double_t XMIN[128];
-  Double_t YMIN[32768]; // 128*256
-  Bool_t PLIVE[1048576]; // 128*256*32
-  Double_t pitch,width;
+  Float_t pitch,width;
   int i,j,k,l;
 
-  LOG_INFO << "StSpaceChargeDistMaker: Now reading live/dead/gains." << endm;
+  LOG_INFO << "StSpaceChargeDistMaker: Now reading live/dead/gains for acceptance" << endm;
   int isec,irow,ipad;
   for (i = 0; i < 24; i++) {
     isec = i + 1;
@@ -237,71 +261,113 @@ void StSpaceChargeDistMaker::Pads() {
         : 0);
 
       for (k=0; k<Npads[j]; k++) {
-        ipad = k + 1;
+        ipad = Npads[j] - k;
         l = k + NR*j + NS*i;
         PLIVE[l] = (liveRow &&
+             k>2 && k<Npads[j]-3 && // exclude outermost 3 pads on any row
              St_tpcPadGainT0BC::instance()->Gain(isec,irow,ipad) > MINGAIN);
         if (i==0) YMIN[l] = pitch * (k - 0.5*Npads[j]);
       }
     }
   }
+  LOG_INFO << "StSpaceChargeDistMaker: will throw approximately 12*" << throws << " hits per TPC hit." << endm;
 
-  LOG_INFO << "StSpaceChargeDistMaker: Now throwing " << throws << " hits." << endm;
-  Double_t phi0,phi,r,x,y,rr,pp,pp0,z;
-  Int_t ix,iy,skip;
-  for (i = 1; i < throws; i++) {
-   r = rmin + gRandom->Rndm()*(rmax - rmin);
-   phi0 = PhiMax*(2*gRandom->Rndm() - 1);
-   x = r*TMath::Cos(phi0);
-   y = r*TMath::Sin(phi0);
-   skip = 0;
-   for (j = 0; j < 24; j++) {
-    if (j < 12) {  // west end
-      z = 1;
-      phi = -phi0 - (j-2)*TMath::Pi()/6.0;
+}
+//_____________________________________________________________________________
+void StSpaceChargeDistMaker::GeomFill(StTpcHit* hit) {
+
+  // Monte Carlo approach to determining geometrical acceptance
+  Float_t pitch,width;
+  int j,k,l,i=1;
+
+  // phi0 and phi3 will be in sector 3 local coordinates
+  // phi and phi2 are in TPC local coordinates
+  Float_t phi0,phi,phi2,phi3,r,r2,x,y,z,zbin;
+  Float_t pos1[3];
+  Float_t pos2[3];
+  Int_t ix,iy,isec;
+
+  z = hit->position().z();
+  isec = hit->sector(); // 1..24
+  // already required z as correct side for sector before GeomFill()
+
+  // must use StEvtTrigDetSumsMaker if reading from event.root
+  StMagUtilities* mExB = StMagUtilities::Instance();
+
+  // for r^-1.71 in r:
+  static const Double_t rdist0 = -1.71 + 1.0;
+  // static const Double_t rdist1 = TMath::Power(rmin,rdist0);
+  // save some CPU by starting at r=49 since acceptance is zero below that...
+  static const Double_t rdist1 = TMath::Power(49,rdist0);
+  static const Double_t rdist2 = TMath::Power(rmax,rdist0) - rdist1;
+  static const Double_t rdist3 = 1.0/rdist0;
+
+  while (i < 12*TMath::Max(1,(int) throws)) {
+    // flat in r:
+    // r = rmin + gRandom->Rndm()*(rmax - rmin);
+
+    // r^-1.71 in r:
+    r = TMath::Power(rdist1 + rdist2*gRandom->Rndm(),rdist3);
+    
+    phi0 = PhiMax*(2*gRandom->Rndm() - 1); //sector 3 coordinates
+    j = (int) (12*gRandom->Rndm());
+    if (j>=12) j-=12;
+    if (isec < 13) {  // west end
+      zbin = 1;
+      k = j; // k = [0..11]
+      phi = phi0 - (k-2)*TMath::Pi()/6.0;
     } else { // east end
-      z = -1;
-      phi = phi0 + (j-20)*TMath::Pi()/6.0;
+      zbin = -1;
+      k = j + 12; // k = [12..23]
+      phi = -phi0 + (k-20)*TMath::Pi()/6.0;
     }
     while (phi>=phmax) phi -= TMath::TwoPi();
     while (phi< phmin) phi += TMath::TwoPi();
     Rhist->Fill(r,0);
     RPhist->Fill(r,phi,0);
     PHhist->Fill(phi,0);
-    Rhist->Fill(r,z);
-    RPhist->Fill(r,phi,z);
-    PHhist->Fill(phi,z);
+    Rhist->Fill(r,zbin);
+    RPhist->Fill(r,phi,zbin);
+    PHhist->Fill(phi,zbin);
 
-    if (skip != 0) continue; // Skip if hit falls on a pad
+    // Apply distortions in TPC local
+    pos1[0] = r*TMath::Cos(phi);
+    pos1[1] = r*TMath::Sin(phi);
+    pos1[2] = z;
+    mExB->DoDistortion(pos1,pos2,k+1);
+    phi2 = TMath::ATan2(pos2[1],pos2[0]);
+    r2 = TMath::Sqrt(pos2[0]*pos2[0]+pos2[1]*pos2[1]);
+
+    // Ignoring misalignment of TPC
+    // but transforming back to sector 3
+    phi3 = phi0 + ((phi2-phi)*(pos2[2] < 0 ? -1.0 : 1.0));
+    x = r2*TMath::Cos(phi3);
+    y = r2*TMath::Sin(phi3);
 
     // Determine if hit falls on a pad
-    if (j==0) {
-     skip = 1;
-     ix = (Int_t) (TMath::BinarySearch(NP,&XMIN[0],x));
-     if (ix < 0 || ix >= NP) continue;
-     width = St_tpcPadPlanesC::instance()->PadLengthAtRow(ix+1);
-     pitch = St_tpcPadPlanesC::instance()->PadPitchAtRow(ix+1);
-     if (x > XMIN[ix] + width) continue;
-     iy = (Int_t) (TMath::BinarySearch(Npads[ix],&YMIN[ix*NR],y));
-     if (iy < 1 || iy >= Npads[ix]-1) continue; // Don't use boundary pads
-     //if (iy < 0 || iy >= Npads[ix]) continue;
-     if (y > YMIN[iy + ix*NR] + pitch) continue;
-     skip = 0;
-     rr = TMath::Sqrt(y*y + Xpads[ix]*Xpads[ix]);
-     pp0 = TMath::ATan2(y,Xpads[ix]);
-    }
+    ix = (Int_t) (TMath::BinarySearch(NP,&XMIN[0],x));
+    if (ix < 0 || ix >= NP) continue;
+    width = St_tpcPadPlanesC::instance()->PadLengthAtRow(ix+1);
+    pitch = St_tpcPadPlanesC::instance()->PadPitchAtRow(ix+1);
+    if (x > XMIN[ix] + width) continue;
+    iy = (Int_t) (TMath::BinarySearch(Npads[ix],&YMIN[ix*NR],y));
+    if (y > YMIN[iy + ix*NR] + pitch) continue;
 
     // Determine if hit falls on an active pad
     // Use gains file:
-    l = iy + ix*NR + j*NS;
+    l = iy + ix*NR + k*NS;
     if (! PLIVE[l]) continue;
 
+    // The need to exclude channels like the following should no
+    // longer be necessary, but I am leaving it commented here
+    // as an example for possible testing purposes. -GVB
+    //
     // Sector 20 RDO problem: account for only 23 sectors active
     // for padrows 9-21.
-    //if (j==19 && ix >=8 && ix <=20) continue;
+    //if (k==19 && ix >=8 && ix <=20) continue;
     // Sector 5 RDO problem: account for only 23 sectors active
     // for padrow 39.
-    //if (j==4 && ix == 38) continue;
+    //if (k==4 && ix == 38) continue;
 
 
     if (Debug()) {
@@ -312,24 +378,23 @@ void StSpaceChargeDistMaker::Pads() {
       LOG_INFO << endm;
     }
 
-    pp = pp0 + (phi-phi0);
-    while (pp>=phmax) pp -= TMath::TwoPi();
-    while (pp< phmin) pp += TMath::TwoPi();
-    Phist->Fill(rr,0);
-    PPhist->Fill(pp,0);
-    RPPhist->Fill(rr,pp,0);
-    Phist->Fill(rr,z);
-    PPhist->Fill(pp,z);
-    RPPhist->Fill(rr,pp,z);
-   }
-  }
-  LOG_INFO << "StSpaceChargeDistMaker: Done filling acceptance histograms." << endm;
+    Phist->Fill(r,0);
+    PPhist->Fill(phi,0);
+    RPPhist->Fill(r,phi,0);
+    Phist->Fill(r,zbin);
+    PPhist->Fill(phi,zbin);
+    RPPhist->Fill(r,phi,zbin);
+    i++;
+  } // i
 }
 
 
 //_____________________________________________________________________________
-// $Id: StSpaceChargeDistMaker.cxx,v 1.2 2012/09/13 20:58:56 fisyak Exp $
+// $Id: StSpaceChargeDistMaker.cxx,v 1.3 2012/10/15 17:51:12 genevb Exp $
 // $Log: StSpaceChargeDistMaker.cxx,v $
+// Revision 1.3  2012/10/15 17:51:12  genevb
+// Include distortion corrections, which must be evenly sampled per event (per hit)
+//
 // Revision 1.2  2012/09/13 20:58:56  fisyak
 // Corrections for iTpx
 //
--- StRoot/StPass0CalibMaker/StSpaceChargeDistMaker.h
+++ StRoot/StPass0CalibMaker/StSpaceChargeDistMaker.h
@@ -18,6 +18,7 @@ class StEvent;
 class TH1D;
 class TH2D;
 class TH3D;
+class StTpcHit;
 
 class StSpaceChargeDistMaker : public StMaker {
  
@@ -29,17 +30,18 @@ public:
   virtual Int_t  Make();
   virtual Int_t  Finish();
   
-  void SetThrows(Int_t N) { throws = N; }
+  void SetThrows(Float_t N) { throws = N; }
 
   void AcceptTrigger(Int_t trig); // negative value accepts all triggers
 
   virtual const char *GetCVS() const
-    {static const char cvs[]="Tag $Name:  $ $Id: StSpaceChargeDistMaker.h,v 1.1 2012/07/06 17:23:00 genevb Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+    {static const char cvs[]="Tag $Name:  $ $Id: StSpaceChargeDistMaker.h,v 1.2 2012/10/15 17:51:12 genevb Exp $ built "__DATE__" "__TIME__ ; return cvs;}
   
 
 protected:
 
-  void   Pads();
+  void   GeomInit();
+  void   GeomFill(StTpcHit* hit);
   StEvent* event;
   TH3D* Space3ChargePRZ;
   TH3D* Space3ChargeU;
@@ -52,8 +54,17 @@ protected:
   TH1D* ZdcC;
   Float_t gainCorr[4096];
   TArrayI trigs;
-  Int_t throws;
+  Float_t throws;
   Int_t run;
+
+  Float_t Xpads[128];
+  UShort_t Npads[128];
+  Int_t NP;
+  Int_t NR;
+  Int_t NS;
+  Float_t XMIN[128];
+  Float_t YMIN[32768]; // 128*256
+  Bool_t PLIVE[1048576]; // 128*256*32
 
   ClassDef(StSpaceChargeDistMaker, 0)
 };
@@ -61,8 +72,11 @@ protected:
 #endif
 
 //_____________________________________________________________________________
-// $Id: StSpaceChargeDistMaker.h,v 1.1 2012/07/06 17:23:00 genevb Exp $
+// $Id: StSpaceChargeDistMaker.h,v 1.2 2012/10/15 17:51:12 genevb Exp $
 // $Log: StSpaceChargeDistMaker.h,v $
+// Revision 1.2  2012/10/15 17:51:12  genevb
+// Include distortion corrections, which must be evenly sampled per event (per hit)
+//
 // Revision 1.1  2012/07/06 17:23:00  genevb
 // Introduce StSpaceChargeDistMaker
 //
