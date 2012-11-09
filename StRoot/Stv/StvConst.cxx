cc3975467373ea41369d026cecba4e677811a506
Victor Perevoztchikov <perev@bnl.gov>
2012-11-09 03:04:19 +0000

    Check for non zeros added

--- StRoot/Stv/StvConst.cxx
+++ StRoot/Stv/StvConst.cxx
@@ -14,5 +14,12 @@ StvConst::StvConst()
   StMaker *mk = StMaker::GetChain();
   St_StvKonst *tb = (St_StvKonst*)mk->GetDataBase("Calibrations/tracker/StvKonst");
   assert(tb);
+  int nCols = tb->GetNumberOfColumns();
+  assert(nCols);
+//	check table for zeros. All the fields either float or int
+  int *row = (int*)tb->GetTable();
+  for (int i=0;i<nCols;i++) {
+    assert(row[i] && "field not filled");
+  }
   *((StvKonst_st*)this) = *(tb->GetTable());
 }
