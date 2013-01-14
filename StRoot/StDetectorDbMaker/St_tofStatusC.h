134c720ef0ce192843f53b3445f71d657e2edf03
Yuri Fisyak <fisyak@bnl.gov>
2013-01-14 23:01:30 +0000

    Add status(Int_t trayId, Int_t moduleId, Int_t cellId)

--- StRoot/StDetectorDbMaker/St_tofStatusC.h
+++ StRoot/StDetectorDbMaker/St_tofStatusC.h
@@ -7,9 +7,17 @@
 class St_tofStatusC : public TChair {
  public:
   static St_tofStatusC* 	instance();
-  tofStatus_st 	*Struct(Int_t i = 0) 	const {return ((St_tofStatus*) Table())->GetTable()+i;}
+  tofStatus_st *Struct(Int_t i = 0) 	const {return ((St_tofStatus*) Table())->GetTable()+i;}
   UInt_t     	getNumRows()                	const {return GetNRows();}
-  unsigned short* 	status(Int_t i = 0) 	const {return Struct(i)->status;}
+  UShort_t     *status(Int_t i = 0) 	const {return Struct(i)->status;}
+  Int_t         status(Int_t trayId, Int_t moduleId, Int_t cellId) {
+    enum {mNModule = 32, mNCell = 6, mNTray=120};
+    if (trayId   < 1 || trayId   > mNTray   ||
+	moduleId < 1 || moduleId > mNModule ||
+	cellId   < 1 || cellId   > mNCell     ) return 0;
+    Int_t i = (cellId -1) + mNCell*((moduleId-1) + mNModule*(trayId-1));
+    return (Int_t) status()[i];
+  }
  protected:
   St_tofStatusC(St_tofStatus *table=0) : TChair(table) {}
   virtual ~St_tofStatusC() {fgInstance = 0;}
