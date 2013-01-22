a1c8f2888f9c1e2da16da050277b68eb6a71015f
Frank Geurts <geurts@rice.edu>
2013-01-22 05:57:40 +0000

    run 13 time stamp

--- StRoot/StBTofPool/dbase/macros/tof_reload.C
+++ StRoot/StBTofPool/dbase/macros/tof_reload.C
@@ -1,5 +1,11 @@
+// $Id: tof_reload.C,v 1.3 2013/01/22 05:57:40 geurts Exp $
 // TOF annual database reload script
 // expects table_reupload.C to be in the same directory
+//
+// $Log: tof_reload.C,v $
+// Revision 1.3  2013/01/22 05:57:40  geurts
+// run 13 time stamp
+//
 
 void tof_reload(){
 
@@ -7,29 +13,29 @@ void tof_reload(){
   gROOT->Macro("LoadLogger.C");
 
   // INL (StBTofINLCorr)
-  //  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTDIGOnTray\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofINLSCorr\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  //  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTDIGOnTray\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofINLSCorr\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 
   // alignment (StBTofGeometry)
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofGeomAlign\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofGeomAlign\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 
   // DAQ Map and Tray Config (StBTofDaqMap, StBTofTables)
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofDaqMap\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTrayConfig\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofDaqMap\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTrayConfig\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 
   // trigger window, vpd delay (StBTofSortRawHit)
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTrgWindow\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"vpdDelay\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTrgWindow\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"vpdDelay\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 
   // tofStatus (StBTofTables)
-  // gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofStatus\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofStatus\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 
   // calibration: T0, Slewing, Zhit (StBTofCalibMaker)
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTotbCorr\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofZbCorr\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTOffset\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTotbCorr\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofZbCorr\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"tofTOffset\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 
   // calibration: start (StVpdCalibMaker)
-  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"vpdTotCorr\", \"ofl\", \"2011-12-19 00:00:00\",\"2011-12-20 00:00:00\"");
+  gROOT->ProcessLine(".x table_reupload.C(\"Calibrations_tof\", \"vpdTotCorr\", \"ofl\", \"2012-12-19 00:00:00\",\"2012-12-20 00:00:00\"");
 }
 
