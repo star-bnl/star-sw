4c904f1a8f7edaa2ef3d2514f4212dcc4984b1f1
Ramiro Debbe <debbe@bnl.gov>
2013-01-24 15:45:04 +0000

    added ZDC shower max information to output tree and bbc small tubes individual ADC. Returns UPC_Main trigger

--- StRoot/StPeCMaker/StPeCTrigger.h
+++ StRoot/StPeCMaker/StPeCTrigger.h
@@ -1,7 +1,10 @@
 //////////////////////////////////////////////////////////////////////
 //
-// $Id: StPeCTrigger.h,v 1.8 2012/07/03 19:37:55 ramdebbe Exp $
+// $Id: StPeCTrigger.h,v 1.9 2013/01/24 15:45:04 ramdebbe Exp $
 // $Log: StPeCTrigger.h,v $
+// Revision 1.9  2013/01/24 15:45:04  ramdebbe
+// added ZDC shower max information to output tree and bbc small tubes individual ADC. Returns UPC_Main trigger
+//
 // Revision 1.8  2012/07/03 19:37:55  ramdebbe
 // raised ClassDef from 1 to 2
 //
@@ -42,6 +45,9 @@
 #include "TClonesArray.h"
 #include "StPeCL0.h"
 #include "StMuDSTMaker/COMMON/StMuEvent.h"
+#include "StMuDSTMaker/COMMON/StMuPrimaryVertex.h"
+#include "StEvent/StTriggerData.h"
+
 class StEvent ;
 
 class StPeCTrigger: public TObject {
@@ -96,6 +102,54 @@ public:
   Float_t   zdcEastUA;
   Float_t   zdcWestUA;
   Float_t   zdcSumUA  ;
+  //zdc TDC information
+  Float_t   zdcEastTDC ;
+  Float_t   zdcWestTDC ;
+  Float_t   zdcTimeDifference  ;
+  //SMD information
+  bool zdcSMDPresent;
+  unsigned short zdcSMDEastH0;
+  unsigned short zdcSMDEastH1;
+  unsigned short zdcSMDEastH2;
+  unsigned short zdcSMDEastH3;
+  unsigned short zdcSMDEastH4;
+  unsigned short zdcSMDEastH5;
+  unsigned short zdcSMDEastH6;
+  unsigned short zdcSMDEastH7;
+
+  unsigned short zdcSMDEastV0;
+  unsigned short zdcSMDEastV1;
+  unsigned short zdcSMDEastV2;
+  unsigned short zdcSMDEastV3;
+  unsigned short zdcSMDEastV4;
+  unsigned short zdcSMDEastV5;
+  unsigned short zdcSMDEastV6;
+  unsigned short zdcSMDEastV7;
+
+  unsigned short zdcSMDWestH0;
+  unsigned short zdcSMDWestH1;
+  unsigned short zdcSMDWestH2;
+  unsigned short zdcSMDWestH3;
+  unsigned short zdcSMDWestH4;
+  unsigned short zdcSMDWestH5;
+  unsigned short zdcSMDWestH6;
+  unsigned short zdcSMDWestH7;
+
+  unsigned short zdcSMDWestV0;
+  unsigned short zdcSMDWestV1;
+  unsigned short zdcSMDWestV2;
+  unsigned short zdcSMDWestV3;
+  unsigned short zdcSMDWestV4;
+  unsigned short zdcSMDWestV5;
+  unsigned short zdcSMDWestV6;
+  unsigned short zdcSMDWestV7;
+
+  unsigned short zdcSMDHighestStripEastH;
+  unsigned short zdcSMDHighestStripEastV;
+  unsigned short zdcSMDHighestStripWestH;
+  unsigned short zdcSMDHighestStripWestV;
+
+
   //trigger word 
   Int_t     tw;
 
@@ -112,6 +166,44 @@ public:
 
   Int_t  bbcTacEast;
   Int_t  bbcTacWest;
+
+  unsigned short bbcADCEastSmall_1;
+  unsigned short bbcADCEastSmall_2;
+  unsigned short bbcADCEastSmall_3;
+  unsigned short bbcADCEastSmall_4;
+  unsigned short bbcADCEastSmall_5;
+  unsigned short bbcADCEastSmall_6;
+  unsigned short bbcADCEastSmall_7;
+  unsigned short bbcADCEastSmall_8;
+  unsigned short bbcADCEastSmall_9;
+  unsigned short bbcADCEastSmall_10;
+  unsigned short bbcADCEastSmall_11;
+  unsigned short bbcADCEastSmall_12;
+  unsigned short bbcADCEastSmall_13;
+  unsigned short bbcADCEastSmall_14;
+  unsigned short bbcADCEastSmall_15;
+  unsigned short bbcADCEastSmall_16;
+  unsigned short bbcADCEastSmall_17;
+  unsigned short bbcADCEastSmall_18;
+
+  unsigned short bbcADCWestSmall_1;
+  unsigned short bbcADCWestSmall_2;
+  unsigned short bbcADCWestSmall_3;
+  unsigned short bbcADCWestSmall_4;
+  unsigned short bbcADCWestSmall_5;
+  unsigned short bbcADCWestSmall_6;
+  unsigned short bbcADCWestSmall_7;
+  unsigned short bbcADCWestSmall_8;
+  unsigned short bbcADCWestSmall_9;
+  unsigned short bbcADCWestSmall_10;
+  unsigned short bbcADCWestSmall_11;
+  unsigned short bbcADCWestSmall_12;
+  unsigned short bbcADCWestSmall_13;
+  unsigned short bbcADCWestSmall_14;
+  unsigned short bbcADCWestSmall_15;
+  unsigned short bbcADCWestSmall_16;
+  unsigned short bbcADCWestSmall_17;
+  unsigned short bbcADCWestSmall_18;
 
   // trigger ids efficiency analysis 
   Int_t trg_3000;   // UPC
