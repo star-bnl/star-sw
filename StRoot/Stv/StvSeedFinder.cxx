cf5243ad8aa3ce1bb09478537e4cbe40651e964c
perev <>
2013-02-16 02:14:25 +0000

    Seed with errors estimation

--- StRoot/Stv/StvSeedFinder.cxx
+++ StRoot/Stv/StvSeedFinder.cxx
@@ -2,6 +2,7 @@
 #include "TSystem.h"
 #include "StvDraw.h"
 #include "StvHit.h"
+#include "StvUtil/StvHitErrCalculator.h"
 #include "vector"
 #include "StarVMC/GeoTestMaker/StTGeoHelper.h"
 
@@ -71,29 +72,52 @@ if(!myCanvas) {
 }
 #endif // APPROX_DEBUG
 
-const double kTpcHitErr = 0.2;
-const double BAD_XI2=333*kTpcHitErr*kTpcHitErr;
+const double BAD_XI2=100;
 const double BAD_RHO=0.1;
 
 //		Loop over nodes and collect global xyz
 
-  THelixFitter &circ = fHelix;
-  circ.Clear();
+  THelixFitter circ;
   int nNode=fSeedHits.size();
   for (int iNode = 0; iNode<nNode;++iNode) {
     const StvHit * hit = fSeedHits[iNode];
     circ.Add(hit->x()[0],hit->x()[1],hit->x()[2]);
   }  
   double Xi2 =circ.Fit();
-  if (Xi2>BAD_XI2) return 0; //Xi2 too bad, no updates
-  if (fabs(circ.GetRho()) >BAD_RHO) return 0;
+  if (Xi2>BAD_XI2*100) 			return 0; //Xi2 too bad, no updates
+  if (fabs(circ.GetRho()) >BAD_RHO) 	return 0; //Too big curvature
   int startSeed = 0;
-  if (fHelix.Pos()[0]*fHelix.Dir()[0]+fHelix.Pos()[1]*fHelix.Dir()[1]>0) {
-    fHelix.Backward(); startSeed = fSeedHits.size()-1;
+  if (circ.Pos()[0]*circ.Dir()[0]+circ.Pos()[1]*circ.Dir()[1]>0) {
+    circ.Backward(); startSeed = nNode-1;
   }
   const float *fx = fSeedHits[startSeed]->x();
   const double dx[3]={fx[0],fx[1],fx[2]};
-  double l = fHelix.Path(dx); fHelix.Move(l*1.2);
+  double l = circ.Path(dx); circ.Move(l*1.2);
+
+//		Now refit with errors
+  int step = (startSeed)? -1:1;
+  fHelix.Clear();
+  for (int iNode = startSeed; (iNode<nNode && iNode>=0) ;iNode+=step) {
+    const StvHit * hit = fSeedHits[iNode];
+    const float *fx = hit->x();
+    const double dx[3]={fx[0],fx[1],fx[2]};
+    double l = circ.Path(dx); circ.Move(l);
+//		Set position for helix
+    fHelix.Add(dx[0],dx[1],dx[2]);
+//		Set position errors for helix
+    const StHitPlane *hp = hit->detector();
+    const Mtx33F_t &hd = hp->GetDir(fx);
+    StvHitErrCalculator* myHitErrCalc = (StvHitErrCalculator*)hp->GetHitErrCalc();
+    myHitErrCalc->SetTrack(circ.Dir());
+    double hRR[3];
+    myHitErrCalc->CalcDcaErrs(fx,hd,hRR);
+    double cos2l = circ.GetCos(); cos2l*=cos2l;
+    fHelix.AddErr( hRR[0],hRR[2]/cos2l);
+  }
+  Xi2 =fHelix.Fit();
+  if (Xi2>BAD_XI2) return 0; 	//Xi2 too bad, no updates
+  fHelix.MakeErrs();
+  l = fHelix.Path(dx); fHelix.Move(l*1.2);
   return &fHelix;
 }    
 //_____________________________________________________________________________
