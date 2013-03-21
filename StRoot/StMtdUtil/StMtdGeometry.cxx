6093c22bf1b913d0e6a175a4c4b415824e436b5c
Jerome Lauret <jeromel@bnl.gov>
2013-03-21 11:23:17 +0000

    Reviewed version 2013/03

---
+++ StRoot/StMtdUtil/StMtdGeometry.cxx
@@ -0,0 +1,11 @@
+#include "StMtdGeometry.h"
+
+//_____________________________________________________________________________
+StMtdGeometry::StMtdGeometry(const char* name, const char* title)
+  : TNamed(name,title)
+{
+   //
+   //
+}
+
+
