3c48f0a83a38d96288c61c519e932e76b2c21b1f
Jerome Lauret <jeromel@bnl.gov>
2013-03-24 19:56:41 +0000

    Previous version nad no valid implementation, somewhat fixed (many params hard-coded need fix)

--- StRoot/StMtdUtil/StMtdGeometry.cxx
+++ StRoot/StMtdUtil/StMtdGeometry.cxx
@@ -1,11 +1,21 @@
 #include "StMtdGeometry.h"
+#include "StMessMgr.h" 
 
-//_____________________________________________________________________________
+StMtdGeometry *gMtdGeometry = 0;
+
+
+// ___________________________________________________________________________
 StMtdGeometry::StMtdGeometry(const char* name, const char* title)
   : TNamed(name,title)
 {
-   //
-   //
+  LOG_INFO << "Noop" << endm;
+  gMtdGeometry = this;
 }
 
 
+StMtdGeometry::~StMtdGeometry()
+{
+  LOG_INFO << "StMtdGeometry at pointer =" << (void*)gMtdGeometry
+	   << " will be deleted" << endm;
+  gMtdGeometry = 0;
+}
