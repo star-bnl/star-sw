f1c8283cc9932e897c93286c38c6dd09f362b269
jeromel <>
2013-04-10 19:28:35 +0000

    Step back to 04/04 version (van aware) - previous changes may be recoverred

--- StRoot/StMuDSTMaker/COMMON/StMuPrimaryTrackCovariance.cxx
+++ StRoot/StMuDSTMaker/COMMON/StMuPrimaryTrackCovariance.cxx
@@ -1,10 +1,8 @@
-#include <assert.h>
 #include "StMuPrimaryTrackCovariance.h"
 #include "TMath.h"
 #include "Stiostream.h"
 #include "TRSymMatrix.h"
-
-static Int_t _debug = 0;
+ClassImp(StMuPrimaryTrackCovariance);
 StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance() {memset(mBeg,0,mEnd-mBeg+1);}
 StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance(StMatrixF cov) {
   memset(mBeg,0,mEnd-mBeg+1);
@@ -17,32 +15,13 @@ StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance(StMatrixF cov) {
       if (j == 1) covv[ij] *= TMath::DegToRad();
     }
   }
-  if (_debug) {
-    TRSymMatrix C(3,covv); cout << "StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance\t" << C << endl;
-  }
-  assert(mTanTan > 0 && mPsiPsi > 0 && mPtiPti > 0);
 }
-
 //________________________________________________________________________________
-StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance(const TArrayF &cov) {
+StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance(const Float_t *cov) {
   memset(mBeg,0,mEnd-mBeg+1);
-  if (cov.GetSize() == 15) {
-    mTanTan = cov[ 9];     
-    mPsiTan = cov[10]*TMath::DegToRad(); mPsiPsi = cov[12]*TMath::DegToRad()*TMath::DegToRad();
-    mPtiTan = cov[11]                  ; mPtiPsi = cov[13]*TMath::DegToRad()                  ; mPtiPti = cov[14];
-  } else {
-    if (cov.GetSize() >= 9) 
-    memcpy(&mTanTan, cov.GetArray()+3, 6*sizeof(Float_t));
-  }
-  if (_debug) {
-    if (cov.GetSize() == 15) {
-      TRSymMatrix C(5,cov.GetArray()); cout << "StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance\t" << C << endl;
-    } else {
-      TRSymMatrix C(3,cov.GetArray()+3); cout << "StMuPrimaryTrackCovariance::StMuPrimaryTrackCovariance\t" << C << endl;
-    }
-    cout << "*this\t" << *this << endl;
-  }
-  assert(mTanTan >= 0 && mPsiPsi >= 0 && mPtiPti >= 0);
+  mTanTan = cov[ 9];                                                                         
+  mPsiTan = cov[10]*TMath::DegToRad(); mPsiPsi = cov[12]*TMath::DegToRad()*TMath::DegToRad();
+  mPtiTan = cov[11]                  ; mPtiPsi = cov[13]*TMath::DegToRad()                  ; mPtiPti = cov[14];
 }
 ostream&  operator<<(ostream& os, const StMuPrimaryTrackCovariance& v) {
   TRSymMatrix cov(3,v.errMatrix());
