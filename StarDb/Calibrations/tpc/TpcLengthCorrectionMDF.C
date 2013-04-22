70d39c2eae43d326d07fb40f9c33440b6a7ffaa9
Yuri Fisyak <fisyak@bnl.gov>
2013-04-22 19:12:36 +0000

    add default

---
+++ StarDb/Calibrations/tpc/TpcLengthCorrectionMDF.C
@@ -0,0 +1,24 @@
+TDataSet *CreateTable() { 
+  if (!gROOT->GetClass("St_MDFCorrection")) return 0;
+  Int_t nrows = 6;
+  MDFCorrection_st row;
+  St_MDFCorrection *tableSet = new St_MDFCorrection("TpcLengthCorrectionMDF",nrows);
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  //  row.nrows      = nrows;
+  //  row.idx        =     1;// 
+  tableSet->AddAt(&row);// 0 -> I70
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);// 1 -> sigma.I70
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);// 2 -> I60
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);// 3 -> sigma.I60 
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);// 4 -> I 
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);// 5 -> sigma.I
+  memset(&row,0,tableSet->GetRowSize()); // 0
+  tableSet->AddAt(&row);
+  // ----------------- end of code ---------------
+  return (TDataSet *)tableSet;
+}
