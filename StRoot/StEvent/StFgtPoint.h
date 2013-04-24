5d60e30bed2f20852d33daedf6cd321f0512375f
Thomas Ullrich <thomas.ullrich@bnl.gov>
2013-04-24 17:35:09 +0000

    Implemented detector() and removed redundant semicolons.

--- StRoot/StEvent/StFgtPoint.h
+++ StRoot/StEvent/StFgtPoint.h
@@ -4,7 +4,7 @@
  */
 /***************************************************************************
  *
- * $Id: StFgtPoint.h,v 2.3 2013/01/08 19:53:15 ullrich Exp $
+ * $Id: StFgtPoint.h,v 2.4 2013/04/24 17:35:09 ullrich Exp $
  * Author: S. Gliske, Oct 2011
  *
  ***************************************************************************
@@ -16,6 +16,9 @@
  ***************************************************************************
  *
  * $Log: StFgtPoint.h,v $
+ * Revision 2.4  2013/04/24 17:35:09  ullrich
+ * Implemented detector() and removed redundant semicolons.
+ *
  * Revision 2.3  2013/01/08 19:53:15  ullrich
  * Added comparison operators.
  *
@@ -57,7 +60,10 @@ public:
     const bool operator <= (const StFgtPoint& rhs) const;
     // deconstructor
     ~StFgtPoint();
-    
+
+    StDetectorId detector() const;
+    void setHardwarePosition(short disc, short quad);
+
     // other accessors
     int getKey();
     int getDisc();
@@ -89,63 +95,68 @@ private:
 
 inline StFgtPoint::StFgtPoint() : StHit(), mHitR(0), mHitPhi(0) { 
     // nothing else
-};
+}
 
 inline int StFgtPoint::getDisc() {
     return static_cast< int >(mHardwarePosition/8);
-};
+}
 
 inline int StFgtPoint::getQuad() {
     return static_cast< int >((mHardwarePosition/2)%4);
-};
+}
+
+inline StDetectorId StFgtPoint::detector() const {return kFgtId;}
 
 inline int StFgtPoint::getKey() {
     return mKey;
-};
+}
 
 inline const StFgtHit* StFgtPoint::getHitR() const {
     return mHitR;
-};
+}
 
 inline const StFgtHit* StFgtPoint::getHitPhi() const {
     return mHitPhi;
-};
+}
 
 inline float StFgtPoint::getPositionR() const {
     return mHitR->getPositionR();
-};
+}
 
 inline float StFgtPoint::getPositionPhi() const {
     return mHitPhi->getPositionPhi();
-};
+}
 
 inline float StFgtPoint::getChargeAsymmetry() const {
     return mChargeAsymmetry;
-};
+}
 
 inline int StFgtPoint::getRank() const {
     return mRank;
-};
+}
 
 inline void StFgtPoint::setRank(int rank) {
     mRank=rank;
-};
+}
 
 inline const bool StFgtPoint::operator >(const StFgtPoint& rhs) const {
     return mRank>rhs.mRank;
-};
+}
 
 inline const bool StFgtPoint::operator >=(const StFgtPoint& rhs) const {
     return mRank>=rhs.mRank;
-};
+}
 
 inline const bool StFgtPoint::operator <(const StFgtPoint& rhs) const {
     return mRank<rhs.mRank;
-};
+}
 
 inline const bool StFgtPoint::operator <=(const StFgtPoint& rhs) const {
     return mRank<=rhs.mRank;
-};
+}
 
+inline void StFgtPoint::setHardwarePosition(short disc, short quad){
+  mHardwarePosition = disc*4+quad+1;
+}
 
 #endif
