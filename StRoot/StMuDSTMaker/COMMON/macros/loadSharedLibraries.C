7e0a0db7cae91652448a7bc934193b54bb05ce8f
fisyak <>
2013-04-29 14:03:01 +0000

    Restore a clumsy version of loadSharedLibraries.C and add lMuDst.C

--- StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C
+++ StRoot/StMuDSTMaker/COMMON/macros/loadSharedLibraries.C
@@ -1,5 +1,30 @@
+#include "TSystem.h"
+
+extern TSystem* gSystem;
+
+
 void loadSharedLibraries() {
-  gROOT->LoadMacro("bfc.C");
-  TString Chain("StEvent,CMuDst,nodefault");
-  bfc(-2,Chain,0,0,0);
+  // Dynamically link needed shared lib
+  gSystem->Load("libStTableUtilities");
+  gSystem->Load("libTable");
+  gSystem->Load("libPhysics");
+  gSystem->Load("St_base");
+  gSystem->Load("StChain");
+  gSystem->Load("St_Tables");
+  gSystem->Load("StUtilities");        // new addition 22jul99
+  gSystem->Load("StTreeMaker");
+  gSystem->Load("StIOMaker");
+  gSystem->Load("StarClassLibrary");
+  gSystem->Load("StTriggerDataMaker"); // new starting from April 2003
+  gSystem->Load("StBichsel");
+  gSystem->Load("StEvent");
+  gSystem->Load("StEventUtilities");
+  gSystem->Load("StDbLib");
+  gSystem->Load("StEmcUtil");
+  gSystem->Load("StTofUtil");
+  gSystem->Load("StPmdUtil");
+  gSystem->Load("StPreEclMaker");
+  gSystem->Load("StStrangeMuDstMaker");
+  gSystem->Load("StMuDSTMaker");  
+  cout << " loading of shared libraries done" << endl;
  }
