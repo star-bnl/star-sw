dd5d225a3217f5b05126ce943fddbc6de42b5087
qiuh <>
2013-05-23 21:28:44 +0000

    *** empty log message ***

---
+++ StRoot/StPxlUtil/StPxlCluster.h
@@ -0,0 +1,43 @@
+/*!
+ * \class StPxlCluster
+ * \author Qiu Hao, Jan 2013, according codes from Xiangming Sun
+ */
+/***************************************************************************
+ * 
+ * $Id: StPxlCluster.h,v 1.5 2013/05/23 21:28:44 qiuh Exp $
+ *
+ * Author: Qiu Hao, Jan 2013, according codes from Xiangming Sun 
+ ***************************************************************************
+ *
+ * Description:
+ * a group of neighboring pixel raw hits
+ *
+ ***************************************************************************
+ *
+ * $Log: StPxlCluster.h,v $
+ * Revision 1.5  2013/05/23 21:28:44  qiuh
+ * *** empty log message ***
+ * 
+ **************************************************************************/
+
+#ifndef StPxlCluster_hh
+#define StPxlCluster_hh
+
+#include "Rtypes.h"
+
+class StPxlCluster{
+public:
+    StPxlCluster();
+    ~StPxlCluster();
+    int nRawHits();
+    void AddRawHit(int column, int row, int idTruthAdd);
+    void Centering();
+    std::vector<int> columnVec; 
+    std::vector<int> rowVec; 
+    float columnCenter;
+    float rowCenter;
+    int idTruth;
+    ClassDef(StPxlCluster,1)
+};
+
+#endif 
---
+++ StRoot/StPxlUtil/StPxlRawHit.h
@@ -0,0 +1,62 @@
+/*!
+ * \class StPxlRawHit 
+ * \author Qiu Hao, Jan 2013
+ */
+/***************************************************************************
+ * 
+ * $Id: StPxlRawHit.h,v 1.5 2013/05/23 21:28:44 qiuh Exp $
+ *
+ * Author: Qiu Hao, Jan 2013 
+ ***************************************************************************
+ *
+ * Description:
+ * pixel raw hit before clustering. One raw hit is one fired pixel.
+ *
+ ***************************************************************************
+ *
+ * $Log: StPxlRawHit.h,v $
+ * Revision 1.5  2013/05/23 21:28:44  qiuh
+ * *** empty log message ***
+ * 
+ **************************************************************************/ 
+
+#ifndef StPxlRawHit_hh
+#define StPxlRawHit_hh
+
+#include "Rtypes.h"
+#include "TObject.h"
+
+class StPxlRawHit: public TObject
+{
+public:
+    StPxlRawHit();
+    StPxlRawHit(int sector, int ladder, int sensor, int row, int column, int idTruth);
+    ~StPxlRawHit();
+
+    int  sector() const;
+    int  ladder() const;
+    int  sensor() const;
+    int  row() const;
+    int  column() const;
+    int  idTruth() const; //!< for embedding, 0 as background
+
+    void setSector(int sector);
+    void setLadder(int ladder);
+    void setSensor(int sensor);
+    void setRow(int row);
+    void setColumn(int column);
+    void setIdTruth(int idTruth);
+
+    void print(); //!< print all information
+
+protected:
+    Char_t mSector; //!< 1-10
+    Char_t mLadder; //!< 1-4
+    Char_t mSensor; //!< 1-10
+    Short_t mRow;
+    Short_t mColumn;
+    UShort_t mIdTruth; //!< for embedding, 0 as background
+
+    ClassDef(StPxlRawHit, 1)
+};
+#endif
