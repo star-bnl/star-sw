1eb1d8478d83b9f1818cf8eaeb85ab96faadad19
bouchet <>
2013-05-29 18:12:16 +0000

    added GetCVS tags

--- StRoot/StPxlDbMaker/StPxlDbMaker.h
+++ StRoot/StPxlDbMaker/StPxlDbMaker.h
@@ -1,5 +1,8 @@
-// $Id: StPxlDbMaker.h,v 1.1 2013/05/24 15:59:53 bouchet Exp $
+// $Id: StPxlDbMaker.h,v 1.2 2013/05/29 18:12:16 bouchet Exp $
 // $Log: StPxlDbMaker.h,v $
+// Revision 1.2  2013/05/29 18:12:16  bouchet
+// added GetCVS tags
+//
 // Revision 1.1  2013/05/24 15:59:53  bouchet
 // first version
 //
@@ -29,7 +32,8 @@ class StPxlDbMaker : public StMaker {
   virtual void   Clear(const char *opt);
   virtual THashList *GetRotations() {return fRotList;}
   virtual Int_t CalculateSensorsPosition();
-
+  virtual const char *GetCVS() const
+  {static const char cvs[]="Tag $Name:  $ $Id: StPxlDbMaker.h,v 1.2 2013/05/29 18:12:16 bouchet Exp $ built "__DATE__" "__TIME__ ; return cvs;}
   ClassDef(StPxlDbMaker,0)   //StAF chain virtual base class for Makers
 };
 // Global pointers:
