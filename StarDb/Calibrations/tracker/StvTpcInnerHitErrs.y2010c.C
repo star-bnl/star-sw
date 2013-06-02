67c80f460e57f78bb957b61280a849acf6916394
perev <>
2013-06-02 02:33:11 +0000

    New fit based 3200 events

--- StarDb/Calibrations/tracker/StvTpcInnerHitErrs.y2010c.C
+++ StarDb/Calibrations/tracker/StvTpcInnerHitErrs.y2010c.C
@@ -9,13 +9,13 @@ StvTpcHitErrs_st row;
 St_StvTpcHitErrs *tableSet = new St_StvTpcHitErrs("StvTpcInnerHitErrs",1);
 //
 memset(&row,0,tableSet->GetRowSize());
-    row.yErr	 = 0.0006155811; // Intrinsic resolution, padrow or Y direction		;
-    row.zErr	 = 0.004761928; // Intrinsic resolution, z direction			;
+    row.yErr	 = 3.775685e-08; // Intrinsic resolution, padrow or Y direction		;
+    row.zErr	 = 1.67014e-06; // Intrinsic resolution, z direction			;
     row.thkDet	 =     1.3225; // detector thickness , not fitted			;
-    row.yyDiff	 = 0.004700078; // Diffusion in XY direction				;
-    row.zzDiff	 = 0.005376144; // Diffusion in Z direction  				;
-    row.yFact	 =  0.5308722; // Error factor in Y-direction 			;
-    row.zFact	 =  0.5356543; // Error factor in Z-direction 			;
+    row.yyDiff	 = 0.007464294; // Diffusion in XY direction				;
+    row.zzDiff	 = 0.01401472; // Diffusion in Z direction  				;
+    row.yFact	 =  0.4522974; // Error factor in Y-direction 			;
+    row.zFact	 =  0.5037845; // Error factor in Z-direction 			;
     row.zAB2	 = 5.0293e-06; // Constant member in Z direction (a*b)**2		;
 tableSet->AddAt(&row);
 // ----------------- end of code ---------------
