5e78e0045af09f2d7f0f3205c3c6e5dbcf250d80
Victor Perevoztchikov <perev@bnl.gov>
2013-06-07 01:26:14 +0000

    Rename to StTGeoProxy.h

--- StarVMC/GeoTestMaker/StTGeoHelper.h
+++
@@ -1,9 +0,0 @@
-// $Id: StTGeoHelper.h,v 1.25 2013/04/20 21:54:57 perev Exp $
-//
-//
-// Class StTGeoProxy
-// ------------------
-// _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ __
-
-typedef StTGeoProxy StTGeoHelper;
-#include "StTGeoProxy.h"
