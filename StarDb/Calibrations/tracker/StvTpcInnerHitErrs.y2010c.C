b1231984710d8dfdd4c9400834847a5d8a53ec0c
Victor Perevoztchikov <perev@bnl.gov>
2013-08-02 19:21:58 +0000

    HitErrs refitted

--- StarDb/Calibrations/tracker/StvTpcInnerHitErrs.y2010c.C
+++ StarDb/Calibrations/tracker/StvTpcInnerHitErrs.y2010c.C
@@ -9,13 +9,13 @@ StvTpcHitErrs_st row;
 St_StvTpcHitErrs *tableSet = new St_StvTpcHitErrs("StvTpcInnerHitErrs",1);
 //
 memset(&row,0,tableSet->GetRowSize());
-    row.yErr	 = 3.775685e-08; // Intrinsic resolution, padrow or Y direction		;
-    row.zErr	 = 1.67014e-06; // Intrinsic resolution, z direction			;
+    row.yErr	 = 0.0003248401; // Intrinsic resolution, padrow or Y direction		;
+    row.zErr	 = 0.003155829; // Intrinsic resolution, z direction			;
     row.thkDet	 =     1.3225; // detector thickness , not fitted			;
-    row.yyDiff	 = 0.007464294; // Diffusion in XY direction				;
-    row.zzDiff	 = 0.01401472; // Diffusion in Z direction  				;
-    row.yFact	 =  0.4522974; // Error factor in Y-direction 			;
-    row.zFact	 =  0.5037845; // Error factor in Z-direction 			;
+    row.yyDiff	 = 0.006162413; // Diffusion in XY direction				;
+    row.zzDiff	 = 0.008600485; // Diffusion in Z direction  				;
+    row.yFact	 =  0.4891122; // Error factor in Y-direction 			;
+    row.zFact	 =  0.5432013; // Error factor in Z-direction 			;
     row.zAB2	 = 5.0293e-06; // Constant member in Z direction (a*b)**2		;
 tableSet->AddAt(&row);
 // ----------------- end of code ---------------
