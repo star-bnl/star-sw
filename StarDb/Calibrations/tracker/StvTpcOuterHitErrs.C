2f4c4b852041e744d19f671da2536cba198f6b0f
perev <>
2013-08-02 19:21:58 +0000

    HitErrs refitted

--- StarDb/Calibrations/tracker/StvTpcOuterHitErrs.C
+++ StarDb/Calibrations/tracker/StvTpcOuterHitErrs.C
@@ -9,13 +9,13 @@ StvTpcHitErrs_st row;
 St_StvTpcHitErrs *tableSet = new St_StvTpcHitErrs("StvTpcOuterHitErrs",1);
 //
 memset(&row,0,tableSet->GetRowSize());
-    row.yErr	 = 0.0005158837; // Intrinsic resolution, padrow or Y direction		;
-    row.zErr	 = 0.0003500886; // Intrinsic resolution, z direction			;
+    row.yErr	 = 0.0006493898; // Intrinsic resolution, padrow or Y direction		;
+    row.zErr	 = 0.003386928; // Intrinsic resolution, z direction			;
     row.thkDet	 =     3.8025; // detector thickness , not fitted			;
-    row.yyDiff	 = 0.00519817; // Diffusion in XY direction				;
-    row.zzDiff	 =  0.0126671; // Diffusion in Z direction  				;
-    row.yFact	 =   0.396071; // Error factor in Y-direction 			;
-    row.zFact	 =  0.4035668; // Error factor in Z-direction 			;
+    row.yyDiff	 = 0.004501354; // Diffusion in XY direction				;
+    row.zzDiff	 = 0.004859645; // Diffusion in Z direction  				;
+    row.yFact	 =  0.4156496; // Error factor in Y-direction 			;
+    row.zFact	 =  0.4108317; // Error factor in Z-direction 			;
     row.zAB2	 = 5.0293e-06; // Constant member in Z direction (a*b)**2		;
 tableSet->AddAt(&row);
 // ----------------- end of code ---------------
