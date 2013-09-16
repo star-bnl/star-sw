bfb7b5014153b5f4fe9d345b14cd73e9a960b9f5
Jonathan Bouchet <bouchet@rcf.rhic.bnl.gov>
2013-09-16 16:45:06 +0000

    clean up

--- StRoot/StPxlDbMaker/StPxlDbMaker.cxx
+++ StRoot/StPxlDbMaker/StPxlDbMaker.cxx
@@ -1,5 +1,8 @@
-// $Id: StPxlDbMaker.cxx,v 1.5 2013/09/14 17:53:01 bouchet Exp $
+// $Id: StPxlDbMaker.cxx,v 1.6 2013/09/16 16:45:06 bouchet Exp $
 // $Log: StPxlDbMaker.cxx,v $
+// Revision 1.6  2013/09/16 16:45:06  bouchet
+// clean up
+//
 // Revision 1.5  2013/09/14 17:53:01  bouchet
 // methods to retrieve PXL masks tables
 //
@@ -29,13 +32,7 @@
   Naming of roatation matrices in this maker :
   PG            = Tpc2Global * GL      *  PSOI * PXOP *    DP              * SD            * LS        *      WLL     *  PS
   
-  note to self : 
-  LS is the old PxlLadderOnSector
-  WLL is the old PxlSensorOnLadder table
-*/
-
-/*
-  new numbering from Hao 
+  numbering : 
   Id  = (sector-1)*40 + (ladder-1)*10 + sensor 
   1<= sector <= 10
   1<= ladder <= 4
@@ -43,6 +40,7 @@
 */
 
 #include "StPxlDbMaker.h"
+#include "StPxlUtil/StPxlConstants.h"
 #include "TDataSetIter.h"
 #include "StMessMgr.h"
 #include "tables/St_Survey_Table.h"
@@ -65,6 +63,9 @@ StPxlDbMaker::~StPxlDbMaker() {gStPxlDbMaker = 0;}
 //_____________________________________________________________________________
 Int_t StPxlDbMaker::Init()
 {
+  LOG_DEBUG << " number of sectors : " << nPxlSectors << endm;
+  LOG_DEBUG << " number of ladders per sector : " << nPxlLaddersPerSector << endm;
+  LOG_DEBUG << " number of sensors per ladder : " << nPxlSensorsPerLadder << endm;
   LOG_DEBUG << "Init done" << endm;
   return StMaker::Init();
 }
@@ -98,10 +99,10 @@ Int_t StPxlDbMaker::Finish()
 //_____________________________________________________________________________
 Int_t StPxlDbMaker::CalculateSensorsPosition(){
   SafeDelete(fRotList);
-  fRotList = new THashList(400,0);
+  fRotList = new THashList(nPxlSectors*nPxlLaddersPerSector*nPxlSensorsPerLadder,0);
   fRotList->SetOwner(kFALSE);
 
-  TGeoHMatrix GL, LS, SD, WGTEST, PSOI , PXOP, DP;
+  TGeoHMatrix GL, LS, SD, PSOI , PXOP, DP;
 
   //get TPC positionement relative to STAR
   assert(gStTpcDb);
@@ -129,7 +130,7 @@ Int_t StPxlDbMaker::CalculateSensorsPosition(){
   if (! pxlSensorOnLadder)  {cout << "pxlSensorOnLadder has not been found"  << endl; return 0;}
 
   //get these tables
-  Survey_st *IdsOnTpc         = idsOnTpc->GetTable();   
+  Survey_st *IdsOnTpc          = idsOnTpc->GetTable();   
   Survey_st *PstOnIds          = pstOnIds->GetTable();   
   Survey_st *PxlOnPst          = pxlOnPst->GetTable();   
   Survey_st *HalfOnPxl         = pxlHalfOnPxl->GetTable();   
@@ -163,36 +164,24 @@ Int_t StPxlDbMaker::CalculateSensorsPosition(){
   PXOP.SetRotation(&PxlOnPst->r00);
   PXOP.SetTranslation(&PxlOnPst->t0);
 
-  LOG_DEBUG << "IDS on TPC :" << endm;
-  //GL.Print();
-  LOG_DEBUG << "PST on IDS :" << endm;
-  //PSOI.Print();
-  LOG_DEBUG << "PXL on PST :" << endm;
-  //PXOP.Print();
-
-  Int_t ladder = 0;
-  Int_t sector = 0;
-  Int_t sensor = 0;
-  Int_t SECTOR = 0;
-  Int_t LADDER = 0;
-  Int_t HALF   = 0;
+  if(Debug()>2){
+    GL.Print();
+    PSOI.Print();
+    PXOP.Print();
+  }
 
   for (Int_t i = 0; i < NoSensors; i++,SensorsOnLadders++) {
     Int_t Id = SensorsOnLadders->Id;
     TGeoHMatrix *comb = (TGeoHMatrix *) fRotList->FindObject(Form("R%03i",Id));
     if (comb) continue;
-    //LOG_DEBUG <<" I'm starting with Id : " << Id << endm;
     comb = new TGeoHMatrix(Form("R%03i",Id));
     // numbering of sensors in the RotMatrices() list is :
     // id = [1-10]*40 + [1-4]*10 + sensor
-    sector = (Id-1)/40 + 1;
-    ladder = (Id-1)%40/10 + 1;
-    sensor = (Id-1)%10 + 1;
-    //LOG_DEBUG <<" Id : " << Id <<" sector : " << sector << " ladder : " << ladder << " sensor :  " << sensor<<endm;
+    Int_t sector = (Id-1)/NoLadders + 1;
+    Int_t ladder = (Id-1)%NoLadders/NoSectors + 1;
     TGeoHMatrix WLL;
     WLL.SetRotation(&SensorsOnLadders->r00);
     WLL.SetTranslation(&SensorsOnLadders->t0); 
-    //cout << "WLL\t"; WLL.Print();
     TGeoHMatrix *WL = (TGeoHMatrix *) fRotList->FindObject(Form("WL%03i",Id));
     if (! WL) {
       WL = new  TGeoHMatrix(Form("WL%03i",Id)); 
@@ -206,74 +195,54 @@ Int_t StPxlDbMaker::CalculateSensorsPosition(){
     }
     LaddersOnSectors = pxlLadderOnSector->GetTable();
     for (Int_t l = 0; l < NoLadders; l++, LaddersOnSectors++) {
-        SECTOR  =  (LaddersOnSectors->Id-1)/4 + 1;
-        LADDER  =  (LaddersOnSectors->Id-1)%4 + 1;
-      LOG_DEBUG <<" from Comb-->sector : " << sector <<"  from tables-->sector : " << SECTOR << endm;
-      LOG_DEBUG <<" from Comb-->ladder : " << ladder <<"  from tables-->ladder : " << LADDER << endm;
+      Int_t SECTOR = (LaddersOnSectors->Id-1)/nPxlLaddersPerSector + 1;
+      Int_t LADDER = (LaddersOnSectors->Id-1)%nPxlLaddersPerSector + 1;
       if ((ladder == LADDER) && (sector == SECTOR)){
 	LS.SetRotation(&LaddersOnSectors->r00);
 	LS.SetTranslation(&LaddersOnSectors->t0);
-	//cout << "LS\t"; LS.Print();
 	break;
       }
     }
-    if (sector <-1  || sector > 11) {cout << "Sector has not been defined" << endl; continue;}
+    if (sector <=0  || sector >=NoSectors) {cout << "Sector has not been defined" << endl; continue;}
     SectorsOnHalf = pxlSectorOnHalf->GetTable();
-    LOG_DEBUG <<" current sector is : " << sector << endm;
     for (Int_t s = 0; s <NoSectors; s++, SectorsOnHalf++) {
-      //cout << "SectorsOnHalf Id\t" << SectorsOnHalf->Id << endl;
-      if (SectorsOnHalf->Id != (SECTOR)) continue; //1<=SectorsOnHalf<=10
+      if (SectorsOnHalf->Id != (sector)) continue; //1<=SectorsOnHalf<=10
       SD.SetRotation(&SectorsOnHalf->r00);
       SD.SetTranslation(&SectorsOnHalf->t0); 
-      //cout << "Sector\t" << Sector << "\tSD\t"; SD.Print();
       break;
     }
 
-    //find the correct half 
     // numbering is :
     //sector 1 to 5  --> half 1
     //sector 6 to 10 --> half 2
     
     HalfOnPxl = pxlHalfOnPxl->GetTable();
-    for (Int_t s = 0; s <NoDShell; s++, HalfOnPxl++) {
-      //cout << "HalfOnPxl Id\t" << HalfOnPxl->Id << endl;
-      (sector>0 && sector<6)?HALF=1:HALF=2;
-      if (HalfOnPxl->Id != HALF) continue; //1<= HalfOnPxl <=2
+    Int_t HALF = 0;
+    for (Int_t t = 0; t <NoDShell; t++, HalfOnPxl++) {
+      (sector>=1 && sector<=NoSectors/2)?HALF=1:HALF=2;
+      if (HalfOnPxl->Id != HALF) continue; 
       DP.SetRotation(&HalfOnPxl->r00);
       DP.SetTranslation(&HalfOnPxl->t0); 
-      //cout << "Half\t" << shalf << "\tDP\t"; DP.Print();
       break;
     }   
-
-    WGTEST = Tpc2Global * GL * PSOI * PXOP * DP * SD * LS * WLL; 
-    //cout << "WGTEST\t"; WGTEST.Print();
     
-    Double_t *r = WGTEST.GetRotationMatrix();
-
-    Double_t norm;    
-    TVector3 d(r[0],r[3],r[6]); norm = 1/d.Mag(); d *= norm;
-    TVector3 t(r[2],r[5],r[8]); norm = 1/t.Mag(); t *= norm;
-    TVector3 n(r[1],r[4],r[7]); //norm = 1/n.Mag(); n *= norm;
-
-    //TVector3 c = d.Cross(t);
-    //Float_t trans[3] = {r[2],r[5],r[8]};
-    //same as StRoot/StSsdDbMaker/StSsdDbMaker.cxx but not sure if used
-    //if (c.Dot(n) < 0) c *= -1;
-    //c.GetXYZ(trans);
-    
-    Double_t *wgtr = WGTEST.GetTranslation();
+    TGeoHMatrix WG = Tpc2Global * GL * PSOI * PXOP * DP * SD * LS * WLL; 
+    Double_t *r    = WG.GetRotationMatrix();
+    Double_t *wgtr = WG.GetTranslation();
     Double_t rot[9] = 
       {
 	r[0], r[1], r[2],
 	r[3], r[4], r[5],
 	r[6] ,r[7], r[8]
       };
-
+    
     Double_t tr[3] = {wgtr[0], wgtr[1], wgtr[2]};
     comb->SetRotation(rot);
     comb->SetTranslation(tr);
     fRotList->Add(comb);
-    comb->Print();
+    if(Debug()>2){
+      comb->Print();
+    }
   }
   return kStOk;
 }
