a913cb223d5dfad50736a48215c2b61d0a36659d
perev <>
2013-12-10 16:34:29 +0000

    string.h include added

--- StRoot/StvUtil/StvKNNUtil.cxx
+++ StRoot/StvUtil/StvKNNUtil.cxx
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 #include <assert.h>
 #include "StvKNNUtil.h"
 //_____________________________________________________________________________
