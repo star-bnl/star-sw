aed11c0f17eace2f05469e64a9bb651a607c7968
Yuri Fisyak <fisyak@bnl.gov>
2013-12-17 15:07:34 +0000

    take out StPidParticleDefinition in a separate h-file

---
+++ StRoot/StEvent/StPidParticleDefinition.h
@@ -0,0 +1,16 @@
+#ifndef __StPidParticleDefinition_h__
+#define __StPidParticleDefinition_h__
+
+enum StPidParticle {
+    kPidElectron ,
+    kPidProton   ,
+    kPidKaon     ,
+    kPidPion     ,
+    kPidMuon     ,
+    kPidDeuteron ,
+    kPidTriton   ,
+    kPidHe3      ,
+    kPidAlpha    ,
+    KPidParticles
+};
+#endif /* __StPidParticleDefinition_h__ */
