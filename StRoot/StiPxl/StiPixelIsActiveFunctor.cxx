217dc7d04b99c15d408a5dc21c536d8d962ee118
smirnovd <>
2014-02-01 19:02:45 +0000

    Renamed StRoot/Hft to StRoot/StiPxl to be consistent with existing convention

---
+++ StRoot/StiPxl/StiPixelIsActiveFunctor.cxx
@@ -0,0 +1,17 @@
+#include "Stiostream.h"
+#include "StiPixelIsActiveFunctor.h"
+
+StiPixelIsActiveFunctor::StiPixelIsActiveFunctor()
+{
+} // StiPixelIsActiveFunctor
+
+StiPixelIsActiveFunctor::~StiPixelIsActiveFunctor()
+{
+} // ~StiPixelIsActiveFunctor
+
+bool StiPixelIsActiveFunctor::operator()(double dYlocal, double dZlocal) const
+{
+   return true;
+   //return false;
+} // operator()
+
