fe72fa632517e2999134e8f52049cd4a5be11359
fisyak <>
2014-02-04 14:48:00 +0000

    Use RHIC clock for y2013

--- StarDb/RunLog/onl/starClockOnl.y2013.C
+++ StarDb/RunLog/onl/starClockOnl.y2013.C
@@ -5,7 +5,7 @@ TDataSet *CreateTable() {
   memset(&row,0,tableSet->GetRowSize());
   row.runNumber	 =          0; // run number  ;
   row.time	 =          0; // unix time of entry  ;
-  row.frequency	 =    9399970; // frequency in Hz, local clock
+  row.frequency	 =    9383160; // frequency in Hz, RHIC clock
   tableSet->AddAt(&row);
  return (TDataSet *)tableSet;
 }
