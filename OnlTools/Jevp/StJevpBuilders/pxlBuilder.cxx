41d60231f5ce7d6d3f0b1daecbdfd534f89af926
martinc <>
2014-02-27 08:38:35 +0000

    *** empty log message ***

--- OnlTools/Jevp/StJevpBuilders/pxlBuilder.cxx
+++ OnlTools/Jevp/StJevpBuilders/pxlBuilder.cxx
@@ -15,7 +15,579 @@
 #include "pxlBuilder.h"
 #include <RTS/include/rtsLog.h>
 
-#include "pxlBuilder_helper_funcs.h"
+//#include "pxlBuilder_helper_funcs.h"
+
+const int NSENSOR = 40;
+const int NCOL = 960;
+const int NROW = 928;
+const int NRDO = 10;
+
+struct _pxlHeaderInfo {
+  unsigned short tcdWord;
+  unsigned int rhicStrobeCtr;
+  unsigned short temperature[4][2];
+};
+
+
+
+
+
+
+
+
+#ifdef PRINT_DEBUG
+#define DEBUGP(...) fprintf(stdout, __VA_ARGS__)
+#else
+#define DEBUGP(...)
+#endif
+
+///////////////////////////////////////////////////////////
+///////////////////// PXL /////////////////////////////////
+///////////////////////////////////////////////////////////
+const int PXLERR_HEADER = 0x1;
+const int PXLERR_HITLEN = 0x2;
+const int PXLERR_SENSORNUM = 0x4;
+const int PXLERR_DUPLICATE = 0x8;
+const int PXLERR_ENDER = 0x10;
+const int PXLERR_RANGE = 0x20;
+const int PXLERR_OUTOFORDER = 0x40;
+const int PXLERR_UNKNOWNROW = 0x80;
+
+
+static int decode16bit(unsigned short val, bool MS16,
+		       int sensor,
+		       int *row,
+		       int *prev_row,
+		       int *prev_col,
+		       int *error_cnt,
+		       int *OVF,
+		       bitset<NCOL> bs[][NROW],
+		       int runlength[][4]
+		       )
+{
+  int ret;
+  bool duplicate;
+  int column, coding;
+
+  ret = 0;
+  if ( (val & 0x1000) == 0x1000 ) { // State0
+    int tmpRow = (val>>2) & 0x3ff;
+    if (tmpRow >= NROW) {
+      DEBUGP("**row %4d", tmpRow);
+      ret |= PXLERR_RANGE; (*error_cnt)++;
+    }
+    else {
+      DEBUGP("row %4d", tmpRow);
+    }
+    if(sensor < NSENSOR) {
+      row[sensor] = tmpRow;
+      if(tmpRow < (prev_row[sensor]&0xFFF)) {
+	if((prev_row[sensor]&0x1000) == 0x1000) {
+	  prev_row[sensor] = tmpRow | 0x1000;
+	  ret |=  PXLERR_OUTOFORDER; (*error_cnt)++;
+	  DEBUGP(" **out-of-order");
+	}
+	else {
+	  prev_row[sensor] = tmpRow | 0x1000;
+	}
+      }
+      else {
+	prev_row[sensor] = tmpRow;
+      }
+    }
+    if( (val & 0x2) == 0x2) {
+      // overflow:
+      DEBUGP(" OVF");
+      if(sensor < NSENSOR) OVF[sensor]++;
+    }
+  }
+  else { // StateN
+    column = (val>>2) & 0x3ff;
+    coding = val & 0x3;
+    if (column+coding >= NCOL) {
+      if (column == 1023)
+	DEBUGP("dummy ");
+      else { 
+	DEBUGP("**col %4d ", column);
+	ret |= PXLERR_RANGE; (*error_cnt)++;
+      }
+    }
+    else {
+      DEBUGP("col %4d ", column);
+    }
+    if(sensor < NSENSOR) {
+      if (MS16) {
+	if (row[sensor] == -1) {
+	  // error: row should already be defined here
+	  DEBUGP("(**row %4d) ", row[sensor]);
+	  ret |= PXLERR_UNKNOWNROW; (*error_cnt)++;
+	}
+	else
+	  DEBUGP("(row %4d) ", row[sensor]);
+      }
+      else
+	  DEBUGP("(row %4d) ", row[sensor]);
+      // valid hits, fill bitset if valid row
+      if((row[sensor]>=0) && (row[sensor]<NROW)) {
+	duplicate = false;
+	runlength[sensor][coding]++;
+	for(int c=0; c<coding+1; c++) {
+	  if((column+c)<NCOL)  {
+	    if (bs[sensor][row[sensor]].test(column+c))
+	      duplicate = true;
+	    bs[sensor][row[sensor]].set(column+c);
+	  }
+	}
+	if(duplicate) {
+	  DEBUGP("**(duplicate) ");
+	  ret |= PXLERR_DUPLICATE; (*error_cnt)++;
+	}
+      }
+    }
+    DEBUGP("coding %d", coding);
+  }
+  return ret;
+}
+
+//**************************************************
+static int pxl_decoder(const u_int *d, const int wordLength,
+		       bitset<NCOL> bs[][NROW],
+		       int *OVF, struct _pxlHeaderInfo *pxlHeaderInfo,
+		       float *ave_runlength,
+		       int *error_cnt)
+{
+  int i,j;
+  int ret; 
+
+  // Sensor variables:
+  int sensor;
+  int row[NSENSOR]; // 40 sensors per RDO
+  unsigned short valLS, valMS;
+  int prev_row[NSENSOR], prev_col[NSENSOR];
+  int runlength[NSENSOR][4];
+  register unsigned int crc = 0xFFFFFFFF ;
+
+  ret = 0; // no error
+
+  // clear the row array
+  for (i=0; i<NSENSOR; i++) {
+    row[i] = -1;
+    prev_row[i] = 0;
+    prev_col[i] = 0;
+    // init the results parameters
+    *error_cnt = 0;
+    OVF[i] = 0;
+    for(j=0; j<NROW; j++)
+      bs[i][j].reset();
+    for(j=0; j<4; j++)
+      runlength[i][j] = 0;
+  }
+
+#ifdef CRC_CALC
+#define	 G_CONST  0x04C11DB7 
+  for(int i=0; i<(wordLength-2); i++) {
+    u_int datum ;
+    
+    datum = d[i] ;
+    register u_int data_j ;
+    register u_int crc_31 ;
+    
+    for(register int j=31;j>=0;j--) {
+      data_j = (datum >> j) & 1 ;
+      crc_31 = (crc & 0x80000000) ? 1 : 0 ;
+      
+      if(crc_31 == data_j) {
+	crc = (crc<<1) ^ G_CONST ;
+      }
+      else {
+	crc = (crc<<1) ;
+      }
+    }
+  }
+#endif
+
+  // dump header
+  DEBUGP("\t%4d: 0x%08X\n",0,d[0]);
+  if (d[0] != 0xAAAAAAAA) {
+    DEBUGP(" *** wrong header token!\n");
+    ret |= PXLERR_HEADER; (*error_cnt)++;
+  }
+
+  pxlHeaderInfo->tcdWord = (unsigned short)(d[1]&0xFFFFF);
+  pxlHeaderInfo->rhicStrobeCtr = d[7];
+  pxlHeaderInfo->temperature[0][0] = (unsigned short)(d[2]&0x3FF);
+  pxlHeaderInfo->temperature[0][1] = (unsigned short)((d[2]&0xFFC00)>>10);
+  pxlHeaderInfo->temperature[1][0] = (unsigned short)((d[2]&0x3FF00000)>>20);
+  pxlHeaderInfo->temperature[1][1] = (unsigned short)(d[3]&0x3FF);
+  pxlHeaderInfo->temperature[2][0] = (unsigned short)((d[3]&0xFFC00)>>10);
+  pxlHeaderInfo->temperature[2][1] = (unsigned short)((d[3]&0x3FF00000)>>20);
+  pxlHeaderInfo->temperature[3][0] = (unsigned short)(d[4]&0x3FF);
+  pxlHeaderInfo->temperature[3][1] = (unsigned short)((d[4]&0xFFC00)>>10);
+
+#ifdef PRINT_DEBUG
+  fprintf(stdout, "\t%4d: 0x%08X",1,d[1]);
+  fprintf(stdout, " - Trigger word: TRGCMD=0x%x, DAQCMD=0x%x, TOKEN=0x%03X\n",
+	 (d[1]&0xf0000)>>16,
+	 (d[1]&0x0f000)>>12,
+	 d[1]&0x00fff);
+
+  fprintf(stdout, "\t%4d: 0x%08X - ",2,d[2]);
+  fprintf(stdout, "L1Temp1 0x%03x L1Temp2 0x%03x L2Temp1 0x%03x\n",
+	  d[2]&0x3FF, (d[2]&0xFFC00)>>10, (d[2]&0x3FF00000)>>20);
+  
+  fprintf(stdout, "\t%4d: 0x%08X - ",3,d[3]);
+  fprintf(stdout, "L2Temp2 0x%03x L3Temp1 0x%03x L3Temp2 0x%03x\n",
+	  d[3]&0x3FF, (d[3]&0xFFC00)>>10, (d[3]&0x3FF00000)>>20);
+  
+  fprintf(stdout, "\t%4d: 0x%08X - ",4,d[4]);
+  fprintf(stdout, "L4Temp1 0x%03x L4Temp2 0x%03x\n",
+	  d[4]&0x3FF, (d[4]&0xFFC00)>>10);
+  
+  fprintf(stdout, "\t%4d: 0x%08X - Firmware version\n",5,d[5]);
+  fprintf(stdout, "\t%4d: 0x%08X - Board Position %d, Serial 0x%x\n",6,d[6],
+	  d[6]&0xF, d[6]>>4);
+  fprintf(stdout, "\t%4d: 0x%08X - RHICstrobe Ctr\n",7,d[7]);
+  for(i=8;i<15;i++) {
+    fprintf(stdout, "\t%4d: 0x%08X - System Status %d\n",i,d[i],i-7);
+  }
+  fprintf(stdout, "\t%4d: 0x%08X - end of header\n",15,d[15]);
+#endif
+
+#ifdef PRINT_STATUS
+  if ((d[8] != 0) || (d[9] != 0) || ((d[10]&0xFFFF) != 0)) {
+    fprintf(stdout, "\theader_err: ");
+    for (i=0; i<16; i++) 
+      if((d[8]& (0x3<<(i*2))) != 0) fprintf(stdout, "%d ", i+1);
+    for (i=0; i<16; i++) 
+      if((d[9]& (0x3<<(i*2))) != 0) fprintf(stdout, "%d ", i+17);
+    for (i=0; i<8; i++) 
+      if((d[10]& (0x3<<(i*2))) != 0) fprintf(stdout, "%d ", i+33);
+    fprintf(stdout, "\n");
+  }
+
+  if (((d[10]&0xFFFF0000) != 0) || ((d[11]&0xFFFFFF) != 0)) {
+      fprintf(stdout, "\tlength_err: ");
+    for (i=0; i<16; i++) 
+      if((d[10]& (0x10000<<i)) != 0) fprintf(stdout, "%d ", i+1);
+    for (i=0; i<24; i++) 
+      if((d[11]& (0x1<<i)) != 0) fprintf(stdout, "%d ", i+17);
+    fprintf(stdout, "\n");
+  }
+
+  if (((d[11]&0xFF000000) != 0) || (d[12] != 0)) {
+      fprintf(stdout, "\tdata_err: ");
+    for (i=0; i<8; i++) 
+      if((d[11]& (0x1000000<<i)) != 0) fprintf(stdout, "%d ", i+1);
+    for (i=0; i<32; i++) 
+      if((d[12]& (0x1<<i)) != 0) fprintf(stdout, "%d ", i+9);
+    fprintf(stdout, "\n");
+  }
+
+  if ((d[13] != 0) || ((d[14]&0xFF) != 0)) {
+      fprintf(stdout, "\ttrailer_err: ");
+    for (i=0; i<32; i++) 
+      if((d[13]& (0x1<<i)) != 0) fprintf(stdout, "%d ", i+1);
+    for (i=0; i<8; i++) 
+      if((d[14]& (0x1<<i)) != 0) fprintf(stdout, "%d ", i+33);
+    fprintf(stdout, "\n");
+  }
+
+  if (((d[14]&0xFFFFFF00) != 0) || ((d[15]&0xFFFF) != 0)) {
+      fprintf(stdout, "\tsensfull_err: ");
+    for (i=0; i<24; i++) 
+      if((d[14]& (0x100<<i)) != 0) fprintf(stdout, "%d ", i+1);
+    for (i=0; i<16; i++) 
+      if((d[15]& (0x1<<i)) != 0) fprintf(stdout, "%d ", i+25);
+    fprintf(stdout, "\n");
+  }
+
+  if ((d[15]&0x100) == 0x100) fprintf(stdout, "\tFIFO_0_FULL ");
+  if ((d[15]&0x200) == 0x200) fprintf(stdout, "\tFIFO_1_FULL");
+  if ((d[15]&0x300) != 0) fprintf(stdout, "\n");
+#endif
+  
+  // hit block length
+  int hitLength = d[16];
+  DEBUGP("\t%4d: 0x%08X - hit block length: %d",16,d[16],d[16]);
+
+  // Now dump hits:
+  int endOfHits;
+  if ( (hitLength+16) < (wordLength-2) ) {
+    endOfHits = hitLength+17;
+  }
+  else {
+    ret |= PXLERR_HITLEN; (*error_cnt)++;
+    DEBUGP(" *** too big!!!");
+    endOfHits = wordLength - 2;
+  }
+  DEBUGP("\n");
+
+  for(i=17; i<endOfHits; i++) {
+    DEBUGP("\t%4d: 0x%08X",i,d[i]);
+    // decode hit data:
+    sensor = ((d[i]>>26) & 0x38) | ((d[i]>>13) & 0x7);
+    DEBUGP(" - Sensor %2d", sensor);
+    // sensor goes  from 1-40, internally use 0-39:
+    sensor -= 1;
+    if((sensor > (NSENSOR-1)) || (sensor < 0)) {
+      ret |= PXLERR_SENSORNUM; (*error_cnt)++;
+      DEBUGP("**invalid sensor\n");
+      continue;
+    }
+
+    valLS = d[i] & 0xffff;
+    valMS = (d[i]>>16) & 0xffff;
+
+    // first decode least significant 16 bits
+    DEBUGP(", LS16: ");
+    ret |= decode16bit(valLS, false, sensor, row, prev_row, prev_col, 
+		       error_cnt, OVF, bs, runlength);
+
+    DEBUGP(", MS16: ");
+    ret |= decode16bit(valMS, true, sensor, row, prev_row, prev_col,
+		       error_cnt, OVF, bs, runlength);
+
+    DEBUGP("\n");
+  }
+	
+#ifdef PRINT_DEBUG
+  // next should be the TCD Info block  
+  if ( endOfHits < (wordLength-2)) {
+    fprintf(stdout, "\t%4d: 0x%08X - start TCD Info\n",endOfHits,d[endOfHits]);
+    for (i=endOfHits+1; i<(wordLength-2); i++) {
+      fprintf(stdout, "\t%4d: 0x%08X\n",i,d[i]);
+    }
+  }
+  else {
+    fprintf(stdout, "\t\tno TCD Info!\n");
+  }
+#endif
+
+#ifdef PRINT_DEBUG
+  // finally, the CRC 
+  fprintf(stdout, "\t%4d: 0x%08X - CRC (calculated 0x%08X)\n",
+	  (wordLength-2),d[wordLength-2],crc);
+  // and Ender
+  fprintf(stdout, "\t%4d: 0x%08X",(wordLength-1),d[wordLength-1]);
+
+#endif
+  if (d[wordLength-1] != 0xBBBBBBBB) {
+    ret |= PXLERR_ENDER; (*error_cnt)++;
+    DEBUGP(" *** wrong ender token!");
+  }
+  DEBUGP(" - Ender\n");
+
+  // now calculate the average runlength (coding+1):
+  for(i=0; i<NSENSOR; i++) {
+    int total=0, N=0;
+    for(j=0; j<4; j++) {
+      total += (j+1)*runlength[i][j];
+      N += runlength[i][j];
+    }
+    if (N>0)
+      ave_runlength[i] = (float)total/((float)N);
+    else
+      ave_runlength[i] = 0.0;
+  }
+
+	
+  return ret;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+_pxlHeaderInfo pxlHeaderInfo;
+
+
+void pxlBuilder::IncrementMultiplicity(int sensor_number,int row_count){
+  (sensor_number < 11) ? multiplicity_inner += row_count : multiplicity_outer += row_count;
+}
+
+int pxlBuilder::WhichLadder(int sector_number,int sensor_number){
+  //sector_number 1 - 10, sensor 1 - 40, returns ladder number 1 - 40
+  int ladder_number = 1+(int)((sensor_number-1)/10)+(4*(sector_number-1));
+  return ladder_number;
+}
+
+void pxlBuilder::UpdateLadderCount(int sector_number,int sensor_number,int sensor_count){
+  int ladder_number = WhichLadder(sector_number,sensor_number);
+
+  if(LadderCount->count(ladder_number) == 0) LadderCount->insert(make_pair(ladder_number,sensor_count));
+  else{
+    LadderCount->find(ladder_number)->second += sensor_count;
+  } 
+}
+
+void pxlBuilder::SetRunLength(int sensor_number,double average_run_length){
+  if(AverageRunLength->count(sensor_number) == 0) AverageRunLength->insert(make_pair(sensor_number,average_run_length));
+  else{
+    //This shouldn't happen
+    //AverageRunLength->find(sensor_number)->second += average_run_length;
+  }
+}
+bool pxlBuilder::ScaleTH1Bin(TH1 *hist,int bin,int scale_factor){
+  int bin_content = 0;
+  bin_content += hist->GetBinContent(bin);
+  
+  int new_content = bin_content/scale_factor;
+  hist->SetBinContent(bin,new_content);
+
+ return (hist->GetBinContent(bin) == new_content) ? true : false;
+}
+
+bool pxlBuilder::UpdateTH1(TH1 *hist,int bin,double value,bool scale,int mod_val){
+  int bin_content = 0;
+  bin_content += hist->GetBinContent(bin);
+  
+  int new_content = bin_content+value;
+  hist->SetBinContent(bin,new_content);
+
+  bool did_it_work = (hist->GetBinContent(bin) == new_content) ? true : false;
+  if(!scale) return did_it_work;
+  else{
+    if(!did_it_work) return false;
+    else{
+      if(mod_val<2) return false;
+      else{
+	if((number_of_events+1 % mod_val) == 0){
+	  return ScaleTH1Bin(hist,bin,mod_val);
+	}
+	else return true;
+      }
+    }
+  }
+}
+
+int pxlBuilder::IncrementArray(const char* name,int x_bin,int y_bin){
+  int count;
+  x_bin--;
+  y_bin--;
+
+  if(strcmp(name,"hits_inner") == 0){
+    count_hits_inner[x_bin][y_bin]++;
+    count = count_hits_inner[x_bin][y_bin];
+  }
+
+  else if(strcmp(name,"hits_outer") == 0){
+    count_hits_outer[x_bin][y_bin]++;
+    count = count_hits_outer[x_bin][y_bin];
+  }
+
+  else if(strcmp(name,"length_inner") == 0){
+    count_length_inner[x_bin][y_bin]++;
+    count = count_length_inner[x_bin][y_bin];
+  }
+
+  else if(strcmp(name,"length_outer") == 0){
+    count_length_outer[x_bin][y_bin]++;
+    count = count_length_outer[x_bin][y_bin];
+  }
+
+  else{
+    cout<<"Error: IncrementArray"<<endl;
+    return 1.0;
+  }
+
+  return count;
+}
+
+bool pxlBuilder::UpdateTH1(TH1 *hist,int bin,double value){
+  double bin_content = 0;
+  bin_content += hist->GetBinContent(bin);
+  ((!bin_content) || (bin_content < 0)) && (bin_content = 0);
+  double new_content = bin_content+value;
+
+  if(new_content <= 0.0) return true;
+  else{
+
+    hist->SetBinContent(bin,new_content);
+    return (hist->GetBinContent(bin) == new_content) ? true : false;
+  }
+}
+
+bool pxlBuilder::UpdateTH2(const char* name,TH1 *hist,int x_bin,int y_bin,double value){
+  if(value <= 0.0) return true;
+  else{
+
+    double bin_content = 0;
+    bin_content += hist->GetBinContent(x_bin,y_bin);
+    ((!bin_content) || (bin_content < 0)) && (bin_content = 0);
+    
+    double new_content;
+    int count = IncrementArray(name,x_bin,y_bin);
+    if(count == 1) new_content = value;
+    else{
+      new_content = ((bin_content*(count-1))+value)/(double)(count);
+    }
+    if(new_content < 0) cout<<"Less than 0!"<<endl;
+    
+    hist->SetBinContent(x_bin,y_bin,new_content);
+    return (hist->GetBinContent(x_bin,y_bin) == new_content) ? true : false;
+  }
+}
+
+bool pxlBuilder::UpdateTH2(TH1 *hist,int x_bin,int y_bin,double value){
+  if(value <= 0.0) return true;
+  else{
+
+    double bin_content = 0;
+    bin_content += hist->GetBinContent(x_bin,y_bin);
+    ((!bin_content) || (bin_content < 0)) && (bin_content = 0);
+    double new_content = bin_content+value;
+    
+    if(new_content <= 0.0) return true;
+    else{
+      hist->SetBinContent(x_bin,y_bin,new_content);
+      return (hist->GetBinContent(x_bin,y_bin) == new_content) ? true : false;
+    }
+  }
+}
+
+void pxlBuilder::SetLadderMap(){
+  for(int i=1; i<41; i++){
+    int value = ((i-1)/4)+1;
+    LadderMap->insert(make_pair(i,value));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ClassImp(pxlBuilder);
