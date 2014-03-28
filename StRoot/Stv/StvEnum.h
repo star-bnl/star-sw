bbfe2f6f738ee559d7946010d07f942f55d0e368
perev <>
2014-03-28 15:31:31 +0000

    More constants moved to this common place

--- StRoot/Stv/StvEnum.h
+++ StRoot/Stv/StvEnum.h
@@ -1,7 +1,7 @@
 #ifndef __StvEnum_h_
 #define __StvEnum_h_
 ///Permanent constants only
-enum StvEnum { kKalmanErrFact=9};
+enum StvEnum { kKalmanErrFact=9, kBigMom=3, kBigMom2=kBigMom*kBigMom};
 
 #endif //__StvEnum_h_
 
