a146ef2a7c0429f0069ed0a195f5351a87465875
cvs2git <cvs2git>
2014-04-15 18:47:06 +0000

    This commit was manufactured by cvs2git to create tag 'hft-dev-0'.

--- StRoot/macros/LoadLogger.C
+++
@@ -1,9 +0,0 @@
-// Author: V.Fine (fine@bnl.gov)
-// ROOT macro to load and initialize the STAR messager with log4cxx implementation
-void LoadLogger() {
-   //  gSystem->Load("logger/lib/liblog4cxx.so");
-  gSystem->Load("liblog4cxx.so");
-  gSystem->Load("St_base.so");
-  gSystem->Load("StStarLogger.so");
-  StLoggerManager::StarLoggerInit();
-}
--- pams/geometry/pixlgeo/PixlGeo5.g
+++
@@ -1,3 +0,0 @@
-Subroutine pixlgeo5 !is a stub for an agml module
-  STOP """ PixlGeo5 is supported in AgML / xgeometry.so """
-END!pixlgeo4
