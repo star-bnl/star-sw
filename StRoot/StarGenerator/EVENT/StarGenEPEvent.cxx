05e5e7145a1ec7bab17482b269546800ceb9f7ae
cvs2git <cvs2git>
2014-04-15 18:47:07 +0000

    This commit was manufactured by cvs2git to create branch 'hft-dev'.

--- StRoot/StarGenerator/EVENT/StarGenEPEvent.cxx
+++
@@ -1,8 +0,0 @@
-#include "StarGenEPEvent.h"
-ClassImp(StarGenEPEvent)
-
-StarGenEPEvent::StarGenEPEvent( const Char_t *name, const Char_t *title )
-  : StarGenEvent(name,title)
-{ 
-  /* nada */ 
-};
