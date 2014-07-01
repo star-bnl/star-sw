04d873e4bd7208741e0db15c102f9a7d76e0270f
fisyak <>
2014-07-01 21:36:36 +0000

    New Tpc Alignment

--- StarDb/Geometry/tpc/TpcPosition.C
+++ StarDb/Geometry/tpc/TpcPosition.C
@@ -1,7 +1,7 @@
 TDataSet *CreateTable() { 
   if (!gROOT->GetClass("St_Survey")) return 0;
   Survey_st row = {
-    //            gamma    -beta   -gamma             alpha     beta   -alpha                 x0        y0        z0
+    //           -gamma     beta    gamma            -alpha    -beta    alpha                 x0        y0        z0
     0, 1.00000, 0.00000, 0.00000, 0.00000, 1.00000, 0.00000, 0.00000, 0.00000, 1.00000,   0.0000,   0.0000,   0.0000,0,0,0,0,0,0,"Ideal Tpc"
   };
   Int_t n = 1;// east = 0, west = 1
--- StarDb/Geometry/tpc/TpcPosition.y2003.C
+++ StarDb/Geometry/tpc/TpcPosition.y2003.C
@@ -1,7 +1,7 @@
 TDataSet *CreateTable() { 
   if (!gROOT->GetClass("St_Survey")) return 0;
   Survey_st row = {
-    //            gamma    -beta   -gamma             alpha     beta   -alpha                 x0        y0        z0
+    //           -gamma     beta    gamma            -alpha    -beta    alpha                 x0        y0        z0
     0, 1.00000,-0.00036,-0.00046, 0.00036, 1.00000, 0.00004, 0.00046,-0.00004, 1.00000,  -0.2686,  -0.1590,  -0.1775,0,0,0,0,0,0,"2003 Tpc"
   };
   Int_t n = 1;// east = 0, west = 1
