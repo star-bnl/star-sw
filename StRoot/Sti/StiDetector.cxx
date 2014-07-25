97283e5a066a804af46deae4719deb3e4587f47d
Gene Van Buren <gene@bnl.gov>
2014-07-25 16:21:31 +0000

    Auto-segmentation of radially oriented volumes (Victor/Dmitri/Gene)

--- StRoot/Sti/StiDetector.cxx
+++ StRoot/Sti/StiDetector.cxx
@@ -4,6 +4,7 @@
 #include <map>
 #include "StiMaterial.h"
 #include "StiShape.h"
+#include "StiPlanarShape.h"
 #include "StiPlacement.h"
 #include "StiDetectorContainer.h"
 #include "StiDetector.h"
@@ -35,6 +36,7 @@ void StiDetector::copy(StiDetector &detector){
   material = detector.getMaterial();
   shape = detector.getShape();
   placement = detector.getPlacement();
+  _elossCalculator = detector.getElossCalculator();
   _cos  = detector._cos;
   _sin  = detector._sin;
   setName(detector.getName());
@@ -57,4 +59,58 @@ ostream& operator<<(ostream& os, const StiDetector& d)
        os << *d.placement;
 
     return os;
+}
+//______________________________________________________________________________
+int StiDetector::splitIt(StiDetVect &vect,double dXdY,int nMax)
+{
+  vect.resize(1);
+  vect[0]=this;
+  assert(shape);
+  if (shape->getShapeCode()!=kPlanar) return 1;
+
+  float deltaX = shape->getThickness();
+  float halfZ  = shape->getHalfDepth();
+  float halfY  = shape->getHalfWidth();
+  int ny = deltaX/(halfY*2*dXdY)+0.5;
+  int nz = deltaX/(halfZ*2*dXdY)+0.5;
+  int nSplit = (ny>nz)? ny:nz;
+  if (nSplit<=1) return 1;
+  if (nSplit>nMax) nSplit=nMax;
+
+//		OK, we must split it.
+
+   vect.clear();
+   float dX = deltaX/nSplit;
+   int N = -1;
+   for (float xc = -deltaX/2 +dX/2; xc<deltaX/2;xc+=dX) {
+     N++;
+//		Create small part of  detector
+     StiDetector *det = new StiDetector;
+     det->copy(*this);
+     TString ts(getName());
+     if (N) { ts+="_"; ts+=N;}
+     det->setName(ts.Data());
+//		Create shape
+     ts = shape->getName();
+     if (N) { ts+="_"; ts+=N;}
+     StiShape *myShape = new StiPlanarShape(ts.Data(),halfZ,dX,halfY);
+     det->setShape(myShape);
+//		Create placement
+
+     StiPlacement *place = new StiPlacement;
+     *place = *placement;
+     float myRadius = placement->getNormalRadius()+xc;
+     place->setNormalRep(placement->getNormalRefAngle()
+                        ,myRadius
+                        ,placement->getNormalYoffset());
+     place->setLayerRadius(myRadius);
+     det->setPlacement(place);
+     vect.push_back(det);
+   }
+   this->copy(*vect[0]); 
+   this->setName(vect[0]->getName());
+   delete vect[0]; vect[0] = this;
+   if (vect.size()>1) {
+     printf("StiDetector::splitIt %s is split into %d peices\n",getName().c_str(),vect.size());}
+   return vect.size();
 }
