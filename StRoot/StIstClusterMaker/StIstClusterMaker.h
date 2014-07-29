08fd66b86ca947a98a5aa7bc40fb0c6f272cf26c
ypwang <>
2014-07-29 20:13:31 +0000

    update the IST DB obtain method

--- StRoot/StIstClusterMaker/StIstClusterMaker.h
+++ StRoot/StIstClusterMaker/StIstClusterMaker.h
@@ -1,6 +1,6 @@
 /***************************************************************************
 *
-* $Id: StIstClusterMaker.h,v 1.5 2014/02/15 20:02:37 ypwang Exp $
+* $Id: StIstClusterMaker.h,v 1.6 2014/07/29 20:13:31 ypwang Exp $
 *
 * Author: Yaping Wang, March 2013
 ****************************************************************************
@@ -9,6 +9,9 @@
 ****************************************************************************
 *
 * $Log: StIstClusterMaker.h,v $
+* Revision 1.6  2014/07/29 20:13:31  ypwang
+* update the IST DB obtain method
+*
 * Revision 1.5  2014/02/15 20:02:37  ypwang
 * Clear() member function added, and mIstCollectionPtr data member defined
 *
@@ -29,8 +32,7 @@
 #include "StIstIClusterAlgo.h"
 
 class StIstCollection;
-class StIstDbMaker;
-class St_istControl;
+class StIstDb;
 
 class StIstClusterMaker : public StMaker
 {
@@ -46,12 +48,12 @@ class StIstClusterMaker : public StMaker
   void setClusterSplitFlag( bool splitFlag = 1);	//cluster splitting switch
 
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StIstClusterMaker.h,v 1.5 2014/02/15 20:02:37 ypwang Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StIstClusterMaker.h,v 1.6 2014/07/29 20:13:31 ypwang Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
  protected:
   StIstCollection *mIstCollectionPtr;
   StIstIClusterAlgo *mClusterAlgoPtr;
-  StIstDbMaker *mIstDbMaker;
+  StIstDb *mIstDb;
 
   UChar_t mTimeBin;
   Bool_t mSplitCluster;
