350c9b1f59ca23258873f40e3e82102f8d45d162
Jerome Lauret <jeromel@bnl.gov>
2014-08-07 21:24:37 +0000

    Added Sst related pragmas

--- StRoot/StSsdDbMaker/StSsdLinkDef.h
+++ StRoot/StSsdDbMaker/StSsdLinkDef.h
@@ -1,6 +1,9 @@
-// $Id: StSsdLinkDef.h,v 1.2 2005/06/20 14:21:38 lmartin Exp $
+// $Id: StSsdLinkDef.h,v 1.3 2014/08/07 21:24:37 jeromel Exp $
 //
 // $Log: StSsdLinkDef.h,v $
+// Revision 1.3  2014/08/07 21:24:37  jeromel
+// Added Sst related pragmas
+//
 // Revision 1.2  2005/06/20 14:21:38  lmartin
 // CVS tags added
 //
@@ -10,6 +13,8 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class StSsdDbMaker;
+#pragma link C++ class StSstDbMaker;
 #pragma link C++ global gStSsdDbMaker;
+#pragma link C++ global gStSstDbMaker;
 
 #endif
