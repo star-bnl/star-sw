6520f252312050fe31f4921cbbfcaa324f20da08
Yaping Wang <wangyaping168@gmail.com>
2014-08-12 23:04:53 +0000

    remove the raw hit number cut per ladder before doing clustering, due to chip occupancy cut was added in raw hit maker which can do the bad column rejection; simplfy the code by removing the InitRun() function

--- StRoot/StIstClusterMaker/StIstClusterMaker.h
+++ StRoot/StIstClusterMaker/StIstClusterMaker.h
@@ -1,6 +1,6 @@
 /***************************************************************************
 *
-* $Id: StIstClusterMaker.h,v 1.6 2014/07/29 20:13:31 ypwang Exp $
+* $Id: StIstClusterMaker.h,v 1.7 2014/08/12 23:04:53 ypwang Exp $
 *
 * Author: Yaping Wang, March 2013
 ****************************************************************************
@@ -9,6 +9,9 @@
 ****************************************************************************
 *
 * $Log: StIstClusterMaker.h,v $
+* Revision 1.7  2014/08/12 23:04:53  ypwang
+* remove the raw hit number cut per ladder before doing clustering, due to chip occupancy cut was added in raw hit maker which can do the bad column rejection; simplfy the code by removing the InitRun() function
+*
 * Revision 1.6  2014/07/29 20:13:31  ypwang
 * update the IST DB obtain method
 *
@@ -32,14 +35,12 @@
 #include "StIstIClusterAlgo.h"
 
 class StIstCollection;
-class StIstDb;
 
 class StIstClusterMaker : public StMaker
 {
  public:
   StIstClusterMaker( const char* name="ist_cluster");
   Int_t Init();
-  Int_t InitRun(Int_t runumber);
   Int_t Make();
   void Clear( Option_t *opts = "" );
 
@@ -48,16 +49,14 @@ class StIstClusterMaker : public StMaker
   void setClusterSplitFlag( bool splitFlag = 1);	//cluster splitting switch
 
   virtual const char *GetCVS() const
-  {static const char cvs[]="Tag $Name:  $ $Id: StIstClusterMaker.h,v 1.6 2014/07/29 20:13:31 ypwang Exp $ built "__DATE__" "__TIME__ ; return cvs;}
+  {static const char cvs[]="Tag $Name:  $ $Id: StIstClusterMaker.h,v 1.7 2014/08/12 23:04:53 ypwang Exp $ built "__DATE__" "__TIME__ ; return cvs;}
 
  protected:
   StIstCollection *mIstCollectionPtr;
   StIstIClusterAlgo *mClusterAlgoPtr;
-  StIstDb *mIstDb;
 
   UChar_t mTimeBin;
   Bool_t mSplitCluster;
-  UShort_t mMinNumOfRawHits, mMaxNumOfRawHits;
 
  private:
   ClassDef(StIstClusterMaker,1);
