7c1302a1075d6d5b8dce62461f574fa3e4ef49f4
Victor Perevoztchikov <perev@bnl.gov>
2014-09-18 18:46:46 +0000

    operator=() added numerical constants ==> enum

--- StRoot/Sti/StiNodePars.h
+++ StRoot/Sti/StiNodePars.h
@@ -3,39 +3,39 @@
 #include "Rtypes.h"
 class StiNodePars {
  public:	
+  enum {kX=0,kY=1,kZ=2,kPhi=3,kPtin=4,kTan=5,kCurv=6,kHz=7};
   void reset(){memset(this,0,sizeof(StiNodePars));_cosCA=1;}
   void ready(){_cosCA=cos(eta());_sinCA=sin(eta()); curv() = hz()*ptin();
     if (fabs(curv()) < 1e-9) curv()=1e-9;}
   StiNodePars &merge(Double_t wt,StiNodePars &other);
-
+  StiNodePars &operator=(const StiNodePars& fr);
     /// accessors
   Double_t  operator[](Int_t idx) const {return P[idx];}
   Double_t &operator[](Int_t idx)       {return P[idx];}
-  Double_t x()    const {return P[0];} 
-  Double_t y()    const {return P[1];}//  local Y-coordinate of this track (reference plane)           		     
-  Double_t z()    const {return P[2];}//  local Z-coordinate of this track (reference plane)			     
-  Double_t eta()  const {return P[3];}//  phi angle
-  Double_t phi()  const {return P[3];}//  phi angle again
-  Double_t ptin() const {return P[4];}//  signed invert pt [sign = sign(-qB)]					     
-  Double_t tanl() const {return P[5];}//  tangent of the track momentum dip angle
-  Double_t curv() const {return P[6];}//  signed curvature [sign = sign(-qB)]					     
-  Double_t rxy()  const {return sqrt(P[0]*P[0]+P[1]*P[1]);}
-  Double_t hz()   const {return P[7];}//  Z component magnetic field in units Pt(Gev) = Hz * RCurv(cm)                  
-  Double_t &x()    {return P[0];}
-  Double_t &y()    {return P[1];}
-  Double_t &z()    {return P[2];}
-  Double_t &eta()  {return P[3];} 
-  Double_t &ptin() {return P[4];}
-  Double_t &tanl() {return P[5];}
-  Double_t &curv() {return P[6];}
-  Double_t &hz()   {return P[7];}
+  Double_t x()    const {return P[kX];} 
+  Double_t y()    const {return P[kY];}//  local Y-coordinate of this track (reference plane)           		     
+  Double_t z()    const {return P[kZ];}//  local Z-coordinate of this track (reference plane)			     
+  Double_t eta()  const {return P[kPhi];}//  phi angle
+  Double_t phi()  const {return P[kPhi];}//  phi angle again
+  Double_t ptin() const {return P[kPtin];}//  signed invert pt [sign = sign(-qB)]					     
+  Double_t tanl() const {return P[kTan];}//  tangent of the track momentum dip angle
+  Double_t curv() const {return P[kCurv];}//  signed curvature [sign = sign(-qB)]					     
+  Double_t rxy2() const {return     (P[kX]*P[kX]+P[kY]*P[kY]);}
+  Double_t rxy()  const {return sqrt(P[kX]*P[kX]+P[kY]*P[kY]);}
+  Double_t hz()   const {return P[kHz];}//  Z component magnetic field in units Pt(Gev) = Hz * RCurv(cm)                  
+  Double_t &x()    	{return P[kX];}
+  Double_t &y()    	{return P[kY];}
+  Double_t &z()    	{return P[kZ];}
+  Double_t &eta()  	{return P[kPhi];} 
+  Double_t &ptin() 	{return P[kPtin];}
+  Double_t &tanl() 	{return P[kTan];}
+  Double_t &curv() 	{return P[kCurv];}
+  Double_t &hz()   	{return P[kHz];}
   Double_t *A(Int_t i = 0)  {return &P[i];}
   
   Int_t     check(const char *pri=0) const;
   void      print() const;
 
-  enum {kX=0,kY,kZ,kEta,kCurv,kTanL};
-  
     /// sine and cosine of cross angle
   Double_t _cosCA;
   Double_t _sinCA;
