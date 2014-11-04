d3706505e1b44f11bb914e0d683842eee55ada99
didenko <>
2014-11-04 01:16:37 +0000

    revert to previous revision

--- StRoot/Sti/StiVertexFinder.cxx
+++ StRoot/Sti/StiVertexFinder.cxx
@@ -1,7 +1,8 @@
 #include "Stiostream.h"
 #include "StiVertexFinder.h"
+#include "StiHit.h"
 #include "StiToolkit.h"
-
+static const double kBigErr2 = 100;
 //______________________________________________________________________________
 StiVertexFinder::StiVertexFinder(const string & name): Named(name)
 {
@@ -18,6 +19,11 @@ const std::vector<StiHit* >* StiVertexFinder::result()
 	
   _result.clear();
   StiHit *hit=0;
-  for (int i=0;(hit=getVertex(i));i++) {_result.push_back(hit);}	
+  for (int i=0;(hit=getVertex(i));i++) {
+    if (hit->sxx() > kBigErr2) continue;
+    if (hit->syy() > kBigErr2) continue;
+    if (hit->szz() > kBigErr2) continue;
+   _result.push_back(hit);
+  }	
   return &_result; 
 }
