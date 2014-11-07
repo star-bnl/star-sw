b0d18141912c042af4db05a411cb5bf6025f07e3
cvs2git <cvs2git>
2014-11-07 19:20:24 +0000

    This commit was manufactured by cvs2git to create branch 'SL15b_1Patch'.

--- StRoot/StMuDSTMaker/COMMON/macros/load.C
+++
@@ -1,7 +0,0 @@
-void load() {
-  // Dynamically link needed shared libs
-  cout << "***************************************************" << endl;
-  cout << " THIS macro is OBSOLETE and will be later removed  " << endl;
-  cout << " Please, use 'loadSharedLibraries.C' instead       " << endl;
-  cout << "***************************************************" << endl;
- }
--- StarDb/Geometry/svt/SvtOnGlobal.y2007.C
+++
@@ -1,7 +0,0 @@
-TDataSet *CreateTable() {
-  if (!gROOT->GetClass("St_Survey")) return 0;
-  Survey_st row = {0, 1,0,0, 0,1,0, 0,0,1, 0,0,0,.1,.1,.1,.1,.1,.1,"ideal position"};
-  St_Survey *tableSet = new St_Survey("SvtOnGlobal",1);
-  tableSet->AddAt(&row.Id, 0);
-  return (TDataSet *)tableSet;
-}
