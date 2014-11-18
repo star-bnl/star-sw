ceb7102c64c190fe883dbf57ce469deb5b27e124
smirnovd <>
2014-11-18 23:13:26 +0000

    StSstDbMaker: Fine tuned the accessor to tranformation matrices for IST sensitive layers

--- StRoot/StSsdDbMaker/StSstDbMaker.cxx
+++ StRoot/StSsdDbMaker/StSstDbMaker.cxx
@@ -219,11 +219,11 @@ St_ssdWafersPosition *StSstDbMaker::CalculateWafersPosition(){
 
 
 /**
- * Returns TGeoHMatrix with complete set of transformation from the sensor local
- * coordinated system to the global one.
+ * Returns TGeoHMatrix with complete set of transformations from the sensor
+ * local coordinate system to the global one.
  */
-TGeoHMatrix* StSstDbMaker::getHMatrixSensorOnGlobal(int ladder, int sensor)
+const TGeoHMatrix* StSstDbMaker::getHMatrixSensorOnGlobal(int ladder, int sensor)
 {
    int id = 7000 + 100*(sensor) + (ladder);
-   return (TGeoHMatrix*) fRotList->FindObject(Form("R%04i", id));
+   return fRotList ? (TGeoHMatrix*) fRotList->FindObject(Form("R%04i", id)) : 0;
 }
