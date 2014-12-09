7a26a65b63cb8cd4d4fc689b50e2eac430f8a9cf
Dmitri Smirnov <dsmirnov@bnl.gov>
2014-12-09 21:52:19 +0000

    StiXxxDetectorBuilder: No point in resizing the detector container (std::vector) manually at the begining because it grows as new detectors are added

--- StRoot/StiPxl/StiPxlDetectorBuilder.cxx
+++ StRoot/StiPxl/StiPxlDetectorBuilder.cxx
@@ -1,4 +1,4 @@
-/* $Id: StiPxlDetectorBuilder.cxx,v 1.78.2.11 2014/12/09 21:52:12 smirnovd Exp $ */
+/* $Id: StiPxlDetectorBuilder.cxx,v 1.78.2.12 2014/12/09 21:52:19 smirnovd Exp $ */
 
 #include <assert.h>
 #include <sstream>
@@ -64,9 +64,6 @@ StiPxlDetectorBuilder::StiPxlDetectorBuilder(bool active, bool buildIdealGeom) :
 void StiPxlDetectorBuilder::buildDetectors(StMaker &source)
 {
    LOG_INFO << "StiPxlDetectorBuilder::buildDetectors() -I- Started" << endm;
-
-   // 2 real rows, but we have detector elements and support elements.
-   setNRows(2);
 
    SetCurrentDetectorBuilder(this);
 
