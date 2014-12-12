70d5baf37ea665607b14dce170938e0cda2ffb57
Dmitri Smirnov <dsmirnov@bnl.gov>
2014-12-12 20:23:35 +0000

    StiIstDetectorBuilder: Added a couple of helper methods to ease manual construction and placement of inactive volumes

--- StRoot/StiIst/StiIstDetectorBuilder.h
+++ StRoot/StiIst/StiIstDetectorBuilder.h
@@ -1,11 +1,17 @@
 #ifndef StiIstDetectorBuilder_h
 #define StiIstDetectorBuilder_h
 
+#include "TGeoMatrix.h"
+#include "TVector3.h"
+
 #include "Sti/StiDetectorBuilder.h"
 
 class StIstDb;
-class StiMaterial;
+class StiDetector;
+class StiIsActiveFunctor;
+class StiShape;
 class StiPlacement;
+class StiMaterial;
 
 
 class StiIstDetectorBuilder : public StiDetectorBuilder
@@ -22,6 +28,11 @@ protected:
 
    bool          mBuildIdealGeom;
    StIstDb      *mIstDb;
+
+private:
+
+   static void setDetectorProperties(StiDetector* detector, std::string name, StiIsActiveFunctor* activeFunctor, StiShape* shape, StiPlacement* placement, StiMaterial* gas, StiMaterial* material);
+   static StiPlacement* createPlacement(const TGeoMatrix& transMatrix, const TVector3& localCenterOffset=TVector3(), const TVector3& normal=TVector3(0, 1, 0));
 };
 
 #endif
