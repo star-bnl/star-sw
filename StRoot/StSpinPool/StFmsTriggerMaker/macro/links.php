5c762e69bee24ed04d5d6339f0011132731a24c5
akio <>
2015-02-25 18:27:08 +0000

    adding README and setup script

---
+++ StRoot/StSpinPool/StFmsTriggerMaker/macro/links.php
@@ -0,0 +1,14 @@
+<?php
+
+function links()
+{
+
+  print("<table><tr>\n");
+  print("<td nowrap><a href='http://www.star.bnl.gov/'>STAR</a> &gt </td>\n");
+  print("<td nowrap><a href='http://online.star.bnl.gov/'>Online</a> &gt</td>\n");
+  print("<td nowrap><a href='http://online.star.bnl.gov/fmsTrg'>FMS trigger Monitor 2015</a> || </td> \n");
+  print("<td nowrap><a href='https://www.star.bnl.gov/protected/spin/akio/fmsps/bitcheck.html'>BitChecking results</a></td> \n");
+  print("</tr></table>\n");
+}
+
+?>
