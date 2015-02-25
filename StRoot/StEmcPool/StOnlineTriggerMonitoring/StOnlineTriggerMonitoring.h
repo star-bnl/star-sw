eca17946626d816511536f3784238ed01a0b4c39
James Kevin Adkins <kevin.adkins@uky.edu>
2015-02-25 21:28:19 +0000

    No longer has methods for updates of offline pedestal and status

--- StRoot/StEmcPool/StOnlineTriggerMonitoring/StOnlineTriggerMonitoring.h
+++ StRoot/StEmcPool/StOnlineTriggerMonitoring/StOnlineTriggerMonitoring.h
@@ -4,22 +4,9 @@
 #include <TObject.h>
 
 class StOnlineTriggerMonitoring {
-public:
-    void saveTrigger(const Char_t *TS = ""
-		    , Bool_t status = true
-		    , Bool_t pedestal = true
-		    , Bool_t lut = true
-		    , Bool_t statusOffline = true
-		    , Bool_t pedestalOffline = true
-		    , Bool_t saveDB = true
-		    , Bool_t saveTables = true
-		    , const Char_t *tables_dir = "tables.emconline_trg"
-		    , const Char_t *saved_dir = "last_conf.emconline_trg"
-		    , const Char_t *bemcStatusCopy = "bemcStatus.txt"
-		    , const Char_t *bceTable = "bce_table.txt"
-		    , const Char_t *bcwTable = "bcw_table.txt"
-		    );
-    ClassDef(StOnlineTriggerMonitoring, 1)
+ public:
+  void saveTrigger(const Char_t *TS = "", Bool_t status = true, Bool_t pedestal = true, Bool_t lut = true, Bool_t saveDB = true, Bool_t saveTables = true, const Char_t *tables_dir = "tables.emconline_trg", const Char_t *saved_dir = "last_conf.emconline_trg", const Char_t *bemcStatusCopy = "bemcStatus.txt", const Char_t *bceTable = "bce_table.txt", const Char_t *bcwTable = "bcw_table.txt");
+  ClassDef(StOnlineTriggerMonitoring, 1);
 };
 
 #endif
