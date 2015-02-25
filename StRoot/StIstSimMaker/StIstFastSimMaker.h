41b342a93a6a70052c34a80106d73546c0d8199e
Dmitri Smirnov <dsmirnov@bnl.gov>
2015-02-25 20:32:05 +0000

    Do not use automatic ROOT I/O as this is a StMaker. Makers are not persistent

--- StRoot/StIstSimMaker/StIstFastSimMaker.h
+++ StRoot/StIstSimMaker/StIstFastSimMaker.h
@@ -1,4 +1,4 @@
-/* $Id: StIstFastSimMaker.h,v 1.4 2015/02/25 20:31:58 smirnovd Exp $ */
+/* $Id: StIstFastSimMaker.h,v 1.5 2015/02/25 20:32:05 smirnovd Exp $ */
 
 #ifndef STAR_StIstFastSimMaker
 #define STAR_StIstFastSimMaker
@@ -41,7 +41,7 @@ class StIstFastSimMaker : public StMaker {
 
   virtual const char *GetCVS() const
   {
-    static const char cvs[]="Tag $Name:  $ $Id: StIstFastSimMaker.h,v 1.4 2015/02/25 20:31:58 smirnovd Exp $ built "__DATE__" "__TIME__ ; 
+    static const char cvs[]="Tag $Name:  $ $Id: StIstFastSimMaker.h,v 1.5 2015/02/25 20:32:05 smirnovd Exp $ built "__DATE__" "__TIME__ ; 
     return cvs;
   }
 
@@ -56,7 +56,7 @@ class StIstFastSimMaker : public StMaker {
   Double_t resZIst1;
   Int_t mSmear; //to turn smearing on and off
 
-  ClassDef(StIstFastSimMaker,1)
+  ClassDef(StIstFastSimMaker, 0)
 };
 
 #endif
@@ -65,6 +65,9 @@ class StIstFastSimMaker : public StMaker {
 /***************************************************************************
 *
 * $Log: StIstFastSimMaker.h,v $
+* Revision 1.5  2015/02/25 20:32:05  smirnovd
+* Do not use automatic ROOT I/O as this is a StMaker. Makers are not persistent
+*
 * Revision 1.4  2015/02/25 20:31:58  smirnovd
 * Removed pointless methods. ::Init() and ::Finish() do not do much. Data members initialized in constructor
 *
