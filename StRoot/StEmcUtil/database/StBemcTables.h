c1cfbbefa1d29da9ee01aa0b86a1cbe59f5270f5
James Kevin Adkins <kevin.adkins@uky.edu>
2015-03-02 20:26:09 +0000

    Updates to hold values for bemcTriggerPed4

--- StRoot/StEmcUtil/database/StBemcTables.h
+++ StRoot/StEmcUtil/database/StBemcTables.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StBemcTables.h,v 1.8 2009/09/25 15:50:29 mattheww Exp $
+ * $Id: StBemcTables.h,v 1.9 2015/03/02 20:26:09 jkadkins Exp $
  * Author:      Alexandre A. P. Suaide
  * Maintainer:  Adam Kocoloski, MIT, kocolosk@mit.edu
  *
@@ -30,6 +30,7 @@ using std::pair;
 #include "tables/St_smdGain_Table.h"
 #include "tables/St_emcTriggerStatus_Table.h"
 #include "tables/St_emcTriggerPed_Table.h"
+#include "tables/St_bemcTriggerPed4_Table.h"
 #include "tables/St_emcTriggerLUT_Table.h"
 
 class StMaker;
@@ -44,25 +45,26 @@ This class handles all the BEMC database requests
 class StBemcTables : public TObject 
 {
 protected: 
-    emcPed_st*              mBtowP;
-    emcPed_st*              mBprsP;
-    smdPed_st*              mSmdeP;
-    smdPed_st*              mSmdpP;
-    emcStatus_st*           mBtowS;
-    emcStatus_st*           mBprsS;
-    smdStatus_st*           mSmdeS;
-    smdStatus_st*           mSmdpS;
-    emcCalib_st*            mBtowC;
-    emcCalib_st*            mBprsC;
-    smdCalib_st*            mSmdeC;
-    smdCalib_st*            mSmdpC;
-    emcGain_st*             mBtowG;
-    emcGain_st*             mBprsG;
-    smdGain_st*             mSmdeG;
-    smdGain_st*             mSmdpG;
-    emcTriggerStatus_st*    mTrigS;
-    emcTriggerPed_st*       mTrigP;
-    emcTriggerLUT_st*       mTrigL;
+    emcPed_st*                mBtowP;
+    emcPed_st*                mBprsP;
+    smdPed_st*                mSmdeP;
+    smdPed_st*                mSmdpP;
+    emcStatus_st*             mBtowS;
+    emcStatus_st*             mBprsS;
+    smdStatus_st*             mSmdeS;
+    smdStatus_st*             mSmdpS;
+    emcCalib_st*              mBtowC;
+    emcCalib_st*              mBprsC;
+    smdCalib_st*              mSmdeC;
+    smdCalib_st*              mSmdpC;
+    emcGain_st*               mBtowG;
+    emcGain_st*               mBprsG;
+    smdGain_st*               mSmdeG;
+    smdGain_st*               mSmdpG;
+    emcTriggerStatus_st*      mTrigS;
+    emcTriggerPed_st*         mTrigP;
+    emcTriggerLUT_st*         mTrigL;
+    bemcTriggerPed4_st*       mTrigP4;
     
     StEmcDecoder*           mDecoder;
     Bool_t                  mBtowMapFix;
@@ -100,6 +102,7 @@ public:
     float   triggerPedestal(int crate, int sequence) const;
     int     triggerPedestalShift() const;
     int     triggerBitConversion(int crate, int patchSequence) const;
+    short   triggerPed4(int softId) const;
     int     triggerFormulaTag(int crate, int patchSequence) const;
     int*    triggerFormulaParameters(int crate, int patchSequence) const;
     
@@ -127,6 +130,7 @@ public:
     void    getTriggerPedestal(Int_t crate, Int_t index, Float_t& pedestal) const; ///< Return tower pedestal loaded in trigger
     void    getTriggerBitConv(Int_t crate, Int_t patch, Int_t& bit) const; ///< Return 6 bits conversion mode
     void    getTriggerPedestalShift(Int_t& pedestalShift) const; ///< Return target pedestal shift
+    void    getTriggerPed4(Int_t softId, Short_t& PED4) const; // Return Ped4 value
     void    getTriggerFormulaTag(Int_t crate, Int_t index, Int_t& formula) const; ///< Return LUT formula
     void    getTriggerFormulaParameters(Int_t crate, Int_t index, Int_t* parameters) const; ///< Return LUT formula parameters
     
@@ -140,6 +144,9 @@ public:
 /***************************************************************************
  *
  * $Log: StBemcTables.h,v $
+ * Revision 1.9  2015/03/02 20:26:09  jkadkins
+ * Updates to hold values for bemcTriggerPed4
+ *
  * Revision 1.8  2009/09/25 15:50:29  mattheww
  * added getter to StBemcTable for StEmcDecoder used
  *
