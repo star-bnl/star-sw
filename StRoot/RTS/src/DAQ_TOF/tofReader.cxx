749d03d1ee499bff6244174a779a69df2eaf6670
cvs2git <cvs2git>
2015-04-13 19:43:44 +0000

    This commit was manufactured by cvs2git to create branch 'SL16d_embed_patch'.

--- StRoot/RTS/src/DAQ_TOF/tofReader.cxx
+++
@@ -1,7 +0,0 @@
-#include <sys/types.h>
-#include "DAQ_READER/daqReader.h"
-#include <DAQ_READER/daq_dta.h>
-#include <DAQ_READER/daq_det.h>
-#include "tofReader.h"
-
-DAQ_LEGACY_DEF(tof);
--- StRoot/StSpectraPool/StRareMaker/StAcceptAllEvents.cxx
+++
@@ -1,5 +0,0 @@
-#include "StAcceptAllEvents.h"
-#include "StEventTypes.h"
-  ClassImp(StAcceptAllEvents)
-int StAcceptAllEvents::Accept(StEvent* event){return 1;}
-void StAcceptAllEvents::Report(){cout << "StAcceptAllEvents: No track cuts for uDST" << endl;}
