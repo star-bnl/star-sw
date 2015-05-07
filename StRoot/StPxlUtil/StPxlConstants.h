9de1f559201a3fc0b6b66d48e1f7e464a8c33c09
smirnovd <>
2015-05-07 20:58:20 +0000

    Defined dimensional constants for PXL sensitive sensor area

--- StRoot/StPxlUtil/StPxlConstants.h
+++ StRoot/StPxlUtil/StPxlConstants.h
@@ -5,7 +5,7 @@
  */
 /***************************************************************************
  *
- * $Id: StPxlConstants.h,v 1.10 2014/01/28 19:29:47 qiuh Exp $
+ * $Id: StPxlConstants.h,v 1.11 2015/05/07 20:58:20 smirnovd Exp $
  *
  * Author: Qiu Hao, March 2013
  ***************************************************************************
@@ -16,6 +16,9 @@
  ***************************************************************************
  *
  * $Log: StPxlConstants.h,v $
+ * Revision 1.11  2015/05/07 20:58:20  smirnovd
+ * Defined dimensional constants for PXL sensitive sensor area
+ *
  * Revision 1.10  2014/01/28 19:29:47  qiuh
  * *** empty log message ***
  *
@@ -30,5 +33,20 @@ const int kNumberOfPxlLaddersPerSector = 4;
 const int kNumberOfPxlSensorsPerLadder = 10;
 const int kNumberOfPxlColumnsOnSensor = 960;
 const int kNumberOfPxlRowsOnSensor = 928;
+
+namespace StPxlConsts
+{
+
+//@{
+/**
+ * Dimensions of PXL sensor active area. E.g. the values are used to restrict
+ * smeared hits to active area see
+ * StPxlSimMaker/doc/PXL_ultimate_sensor_flemming.pdf
+ */
+const double kPxlActiveLengthX = 1.921;   ///< in cm
+const double kPxlActiveLengthY = 1.9872;  ///< in cm
+//@}
+
+}
 
 #endif
