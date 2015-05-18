b79bb45e20411206722f4fc28134fa9e73e4c2d8
Yuri Fisyak <fisyak@bnl.gov>
2015-05-18 14:53:50 -0400

    Merge
    Former-commit-id: ec12e463059a30d4a3b14269deedca04e257f6a0

--- StRoot/StPxlSimMaker/StPxlFastSim.cxx
+++ StRoot/StPxlSimMaker/StPxlFastSim.cxx
@@ -5,6 +5,25 @@
  *
  **********************************************************
  * $Log: StPxlFastSim.cxx,v $
+ * Revision 1.12  2015/05/14 18:57:52  smirnovd
+ * Squashed commit of the following:
+ *
+ * StPxlFastSim: Streamlined creation of PXL hits by making use of StPxlUtil/StPxlDigiHit
+ *
+ * StPxlHitMaker: Updated comments
+ *
+ * StPxlHitMaker: Streamlined creation of PXL hits by making use of StPxlUtil/StPxlDigiHit
+ *
+ * StPxlDigiHit: A helper to manipulate local hit position in StPxlHit
+ *
+ * StPxlConsts: Define constants in namespace
+ *
+ * For safety reasons, the intentions is to move the constants into the namespace
+ * and get rid of those defined in the global space.
+ *
+ * Revision 1.11  2015/05/07 21:24:31  smirnovd
+ * StPxlSimMaker: Switched to using consts from StPxlUtil/
+ *
  * Revision 1.10  2015/03/13 18:45:01  perev
  * Roll back
  *
@@ -46,6 +65,8 @@
 #include "StarClassLibrary/StRandom.hh"
 #include "StThreeVectorF.hh"
 #include "StPxlDbMaker/StPxlDb.h"
+#include "StPxlUtil/StPxlConstants.h"
+#include "StPxlUtil/StPxlDigiHit.h"
 
 #include "TGeoManager.h"
 #include "TGeoMatrix.h"
@@ -165,8 +186,8 @@ Int_t StPxlFastSim::addPxlHits(const StMcPxlHitCollection& mcPxlHitCol,
                LOG_DEBUG << "localPixHitPos = " << localPixHitPos[0] << " " << localPixHitPos[1] << " " << localPixHitPos[2] << endm;
                // please note that what is called local Y in the PXL sensor design
                // is actually called Z in STAR coordinates convention and vice-versa
-               smearedX = distortHit(localPixHitPos[0], mResXPix, PXL_ACTIVE_X_LENGTH / 2.0);
-               smearedZ = distortHit(localPixHitPos[2], mResZPix, PXL_ACTIVE_Y_LENGTH / 2.0);
+               smearedX = distortHit(localPixHitPos[0], mResXPix, StPxlConsts::kPxlActiveLengthX / 2.0);
+               smearedZ = distortHit(localPixHitPos[2], mResZPix, StPxlConsts::kPxlActiveLengthY / 2.0);
                if (mResYPix) smearedY = distortHit(localPixHitPos[1], mResYPix, 0.0020); // Not properly constrained yet
                else smearedY = localPixHitPos[1];
                localPixHitPos[0] = smearedX;
@@ -180,14 +201,12 @@ Int_t StPxlFastSim::addPxlHits(const StMcPxlHitCollection& mcPxlHitCol,
                StThreeVectorF mRndHitError(0., 0., 0.);
 
                UInt_t hw = sector * 10 + ladder; // needs to be updated later after clustering alogrithms are finalized
-               StPxlHit* tempHit = new StPxlHit(gpixpos, mRndHitError, hw, mcPix->dE() , 0);
-               tempHit->setSector(iSec + 1);
-               tempHit->setLadder(mcPix->ladder());
-               tempHit->setSensor(mcPix->sensor());
-               mcPix->parentTrack()? tempHit->setIdTruth(mcPix->parentTrack()->key(), 100): tempHit->setIdTruth(-999);
-               tempHit->setDetectorId(kPxlId);
-               tempHit->setId(mcPix->key());
-               tempHit->setLocalPosition(localPixHitPos[0], localPixHitPos[1], localPixHitPos[2]);
+
+               unsigned short idTruth = mcPix->parentTrack() ? mcPix->parentTrack()->key() : -999;
+               unsigned short quality = mcPix->parentTrack() ? 100 : 0;
+
+               StPxlDigiHit* tempHit = new StPxlDigiHit(localPixHitPos, iSec+1, mcPix->ladder(), mcPix->sensor(),
+                  gpixpos, mRndHitError, hw, mcPix->dE(), 0, idTruth, quality, mcPix->key());
 
                LOG_DEBUG << "key() : " << mcPix->key() - 1 << " idTruth: " << mcPix->parentTrack()->key() << endm;
                LOG_DEBUG << "from StMcPxlHit : x= " << mcPix->position().x() << ";  y= " << mcPix->position().y() << ";  z= " << mcPix->position().z() << endm;
@@ -255,6 +274,25 @@ void StPxlFastSim::localToMatser(Double_t* local,Double_t* master,Int_t sector,I
  *
  **********************************************************
  * $Log: StPxlFastSim.cxx,v $
+ * Revision 1.12  2015/05/14 18:57:52  smirnovd
+ * Squashed commit of the following:
+ *
+ * StPxlFastSim: Streamlined creation of PXL hits by making use of StPxlUtil/StPxlDigiHit
+ *
+ * StPxlHitMaker: Updated comments
+ *
+ * StPxlHitMaker: Streamlined creation of PXL hits by making use of StPxlUtil/StPxlDigiHit
+ *
+ * StPxlDigiHit: A helper to manipulate local hit position in StPxlHit
+ *
+ * StPxlConsts: Define constants in namespace
+ *
+ * For safety reasons, the intentions is to move the constants into the namespace
+ * and get rid of those defined in the global space.
+ *
+ * Revision 1.11  2015/05/07 21:24:31  smirnovd
+ * StPxlSimMaker: Switched to using consts from StPxlUtil/
+ *
  * Revision 1.10  2015/03/13 18:45:01  perev
  * Roll back
  *
