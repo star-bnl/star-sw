16a60992e9885077c9fdc2439d69ae0b5995882a
perev <>
2015-05-19 20:36:18 +0000

    WarnOff

--- StRoot/StarClassLibrary/PhysicalConstants.h
+++ StRoot/StarClassLibrary/PhysicalConstants.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: PhysicalConstants.h,v 1.3 2012/06/11 15:29:26 fisyak Exp $
+ * $Id: PhysicalConstants.h,v 1.4 2015/05/19 20:36:18 perev Exp $
  *
  * Author: CLHEP (see below)
  ***************************************************************************
@@ -10,6 +10,9 @@
  ***************************************************************************
  *
  * $Log: PhysicalConstants.h,v $
+ * Revision 1.4  2015/05/19 20:36:18  perev
+ * WarnOff
+ *
  * Revision 1.3  2012/06/11 15:29:26  fisyak
  * std namespace
  *
@@ -79,7 +82,7 @@ static const double electron_mass_c2 = 0.51099906 * MeV;
 static const double   proton_mass_c2 = 938.27231 * MeV;
 static const double  neutron_mass_c2 = 939.56563 * MeV;
 static const double           amu_c2 = 931.49432 * MeV;
-static const double              amu = amu_c2/c_squared;
+//VP static const double              amu = amu_c2/c_squared; //same name in SystemOfUnits.h
 
 static const double kaon_0_short_mass_c2 = 497.672  * MeV;
 static const double    pion_plus_mass_c2 = 139.5700 * MeV;
@@ -124,6 +127,46 @@ static const double STP_Temperature = 273.15*kelvin;
 static const double STP_Pressure    = 1.*atmosphere;
 static const double kGasThreshold   = 1.e-2*gram/centimeter3;
 #endif /* !__CINT__ */
+inline int dummyPhysicalConstants() 
+{
+return
+pi
++halfpi
++pi2
++Avogadro
++c_light
++c_squared
++h_Planck
++hbar_Planck
++hbarc
++hbarc_squared
++electron_charge
++electron_mass_c2
++proton_mass_c2
++neutron_mass_c2
++amu_c2
++kaon_0_short_mass_c2
++pion_plus_mass_c2
++pion_minus_mass_c2
++lambda_mass_c2
++antilambda_mass_c2
++xi_minus_mass_c2
++mu0
++epsilon0
++elm_coupling
++fine_structure_const
++classic_electr_radius
++electron_Compton_length
++Bohr_radius
++alpha_rcl2
++twopi_mc2_rcl2
++k_Boltzmann
++STP_Temperature
++STP_Pressure
++kGasThreshold;
+}
+
+
 #endif /* HEP_PHYSICAL_CONSTANTS_H */
 
 
