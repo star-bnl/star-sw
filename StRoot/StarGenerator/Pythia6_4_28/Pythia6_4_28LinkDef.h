93b5629291d7baf537c08df344ad6913c760306a
Yuri Fisyak <fisyak@bnl.gov>
2015-07-03 09:38:55 -0400

    Merge
    Former-commit-id: 80b8c499095592704158fda33ae792c0a0a1e3a1

---
+++ StRoot/StarGenerator/Pythia6_4_28/Pythia6_4_28LinkDef.h
@@ -0,0 +1,10 @@
+/* This was generated for version 'DEV' */
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#ifdef __CINT__
+#pragma link C++ class StarPythia6+;
+// Add pythia common blocks
+#pragma link C++ struct PyJets_t+;
+#pragma link C++ struct PySubs_t+;
+#endif
