4b43a607abca362b983b8147bfea5f977e5bebb9
Victor Perevoztchikov <perev@bnl.gov>
2015-07-07 02:39:39 +0000

    Cleanup

--- StRoot/StvSeed/StvKNSeedFinder.cxx
+++ StRoot/StvSeed/StvKNSeedFinder.cxx
@@ -27,6 +27,9 @@ void myBreak(int);
 ClassImp(StvKNSeedFinder)
 #endif
 
+enum {kMinRxy = 50};
+
+
 //_____________________________________________________________________________
 StvKNSeedFinder::StvKNSeedFinder(const char *name):StvSeedFinder(name)
 {
@@ -88,15 +91,12 @@ static int nCall=0; nCall++;
   for (;(*f1stHitMapIter)!=f1stHitMap->end();++(*f1stHitMapIter)) {//1st hit loop
     fstHit = (*(*f1stHitMapIter)).second;
     assert(fstHit);
-    if (fstHit->timesUsed()) continue;
+    if (fstHit->timesUsed()) 		continue;
     fSeedHits.clear();
     const float *hPos = fstHit->x();
     float Rxy2 = hPos[0]*hPos[0]+hPos[1]*hPos[1];
+    if (Rxy2 < kMinRxy*kMinRxy) 	continue;
     const StHitPlane *hp = fstHit->detector();
-    float lay = hp->GetLayer();
-    float hDir[3]; 
-    TCL::ucopy(hp->GetDir(hPos)[0],hDir,3);
-    if (TCL::vdot(hDir,hPos,3)>0) { TCL::vscale(hDir,-1.,hDir,3);}
 //    mRej.Reset(hPos,hDir,lay*kMaxHits*3);
     mRej.Reset(hPos);
     mRej.Prepare();
@@ -113,7 +113,9 @@ static int nCall=0; nCall++;
       if (nexHit->timesUsed()) 		continue;
       if (nexHit->detector()==hp)	continue;
       const float *f = nexHit->x();
-      if (f[0]*f[0]+f[1]*f[1]>=Rxy2)  	continue;
+      float myRxy2 = f[0]*f[0]+f[1]*f[1];
+      if (myRxy2 >=Rxy2)  		continue;
+      if (myRxy2 < kMinRxy*kMinRxy)  	continue;
       nTotHits++;
       
       int ans = mRej.Reject(nexHit->x());
@@ -124,33 +126,6 @@ static int nCall=0; nCall++;
 
     } //endMultiIter loop
 
-#if 0
-    StMultiKeyMapIter myMultiIter(fMultiHits->GetTop(),0,0);
-    int myTotHits = 0,myAccHits=0;
-    for (StMultiKeyNode *node=0;(node = *(myMultiIter)) ;++(myMultiIter)){ 
-      StvHit *nexHit = (StvHit*)node->GetObj();
-      if (nexHit==fstHit)		continue;
-      if (nexHit->timesUsed()) 		continue;
-      if (nexHit->detector()==hp)	continue;;
-      const float *x = nexHit->x();
-      int jk=0;
-      for (int j=0;j<3;j++) {
-        jk = 1;
-        if (x[j]<mRej.mLim[0][j]) break;
-        if (x[j]>mRej.mLim[1][j]) break;
-        jk = 0;
-      }
-      if (jk) continue;
-      myTotHits++;
-      int ans = mRej.Reject(nexHit->x());
-      if (ans) continue;
-      myAccHits++;
-
-    } //endMultiIter loop
-
-    assert (abs(nTotHits-myTotHits)<=1);
-    assert (abs(nAccHits-myAccHits)<=1);
-#endif //0
 
     int nHits = mSel.Select();
     if (nHits < kMinHits) continue;
@@ -160,6 +135,8 @@ if (myShow) mSel.Show();
 }
     fSeedHits.clear();
     fSeedHits+=mSel.Get();
+
+
     const THelixTrack *hel =  Approx();
     if (hel) { fNSeeds[0]++; ++(*f1stHitMapIter); return hel;}	//Good boy
  //		Bad boy
@@ -184,10 +161,23 @@ void StvKNSeedFinder::Show()
 //_____________________________________________________________________________
 void StvKNSeedFinder::FeedBack(int success)  	
 {
-  if (success) {
-    StvDebug::Count("KNDis_MinEig_succ1",sqrt(mSel.mEigen[0])*57,mSel.mKNNDist*57);
+  StvSeedFinder::FeedBack(success);
+
+  double dis = mSel.mKNNDist;
+  double ei0 = sqrt(mSel.mEigen[0]);
+  double ei1 = sqrt(mSel.mEigen[1]);
+
+  double all = log10(fXi2[1]*ei0*ei1);
+
+  if (success>0) {
+    StvDebug::Count("GooEigMax:EigMin",ei0*57,ei1*57);
+    StvDebug::Count("GooKNNDis",dis *57);
+    StvDebug::Count("GooAll",all);
+
   } else {
-    StvDebug::Count("KNDis_MinEig_succ0",sqrt(mSel.mEigen[0])*57,mSel.mKNNDist*57);
+    StvDebug::Count("BadEigMax:EigMin",ei0*57,ei1*57);
+    StvDebug::Count("BadKNNDis",dis *57);
+    StvDebug::Count("BadAll",all);
   }
 }     
   
