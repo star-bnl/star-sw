d9ef0f990ecc618aaa8977145d4ebbb25b1bd5a3
cvs2git <cvs2git>
2015-07-09 17:22:56 +0000

    This commit was manufactured by cvs2git to create branch 'tracker-2016-eval'.

--- StRoot/StDetectorDbMaker/StDetectorDbRichScalers.h
+++
@@ -1,5 +0,0 @@
-#ifndef StDetectorDbRichScalers_h
-#define StDetectorDbRichScalers_h
-#include "St_trigDetSumsC.h"
-typedef St_trigDetSumsC StDetectorDbRichScalers;
-#endif
--- StRoot/StDetectorDbMaker/StiPixelChairs.cxx
+++
@@ -1,6 +0,0 @@
-#include "StarChairDefs.h"
-#include "St_db_Maker/St_db_Maker.h"
-#include "StiPixelHitErrorCalculator.h"
-MakeChairInstance2(HitError,StiPixelHitErrorCalculator,Calibrations/tracker/PixelHitError);
-#include "StiPixelTrackingParameters.h"
-MakeChairInstance2(TrackingParameters,StiPixelTrackingParameters,Calibrations/tracker/PixelTrackingParameters);
