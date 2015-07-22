a0708db386bc9a2a09adc58d1bc599e07667cb7e
jwebb <>
2015-07-22 19:30:02 +0000

    Fix minor compiler warnings.

--- StRoot/StMcEvent/StMcPxlSectorHitCollection.cc
+++ StRoot/StMcEvent/StMcPxlSectorHitCollection.cc
@@ -1,7 +1,10 @@
 /***************************************************************************
  *
- * $Id: StMcPxlSectorHitCollection.cc,v 2.1 2013/03/25 23:50:36 perev Exp $
+ * $Id: StMcPxlSectorHitCollection.cc,v 2.2 2015/07/22 19:30:02 jwebb Exp $
  * $Log: StMcPxlSectorHitCollection.cc,v $
+ * Revision 2.2  2015/07/22 19:30:02  jwebb
+ * Fix minor compiler warnings.
+ *
  * Revision 2.1  2013/03/25 23:50:36  perev
  * Mustafa.Pxl add
  *
@@ -9,7 +12,7 @@
  **************************************************************************/
 #include "StMcPxlSectorHitCollection.hh"
 #include "StMcPxlHit.hh"
-static const char rcsid[] = "$Id: StMcPxlSectorHitCollection.cc,v 2.1 2013/03/25 23:50:36 perev Exp $";
+static const char rcsid[] = "$Id: StMcPxlSectorHitCollection.cc,v 2.2 2015/07/22 19:30:02 jwebb Exp $";
 
 ClassImp(StMcPxlSectorHitCollection)
 
@@ -41,7 +44,7 @@ unsigned int StMcPxlSectorHitCollection::numberOfHits() const
    unsigned int sum = 0;
    for (int iLadder = 0; iLadder < kNumberOfLadders; iLadder++)
    {
-      for (int iSensor = 0; iSensor < mLadders[iSensor].numberOfSensors(); iSensor++)
+      for (unsigned int iSensor = 0; iSensor < mLadders[iSensor].numberOfSensors(); iSensor++)
       {
          sum += mLadders[iLadder].sensor(iSensor)->hits().size();
       }
