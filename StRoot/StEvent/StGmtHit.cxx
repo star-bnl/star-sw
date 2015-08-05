3fe0a9f622657899bfd6c832ba6d12d783aaff5c
Yuri Fisyak <fisyak@bnl.gov>
2015-08-05 14:04:37 -0400

    Revised GmtHit
    Former-commit-id: 7196522d2545a72d1ba1677bd12d53b9e5905034

--- StRoot/StEvent/StGmtHit.cxx
+++ StRoot/StEvent/StGmtHit.cxx
@@ -12,64 +12,14 @@
 #include "StGmtHit.h"
 #include <cmath>
 ClassImp(StGmtHit)
-
-// constructor
-StGmtHit::StGmtHit( int key, short module, int adcX, int adcY,
-	      int tbX, int tbY, double localX, double localY,
-	      double localXErr, double localYErr)
-{
-    StHit::setHardwarePosition(module);
-    StHit::setCharge(0);
-    mKey = key;    
-    mModule = module;    
-    mAdcX = adcX;    
-    mAdcY = adcY;    
-    mTbX = tbX;    
-    mTbY = tbY;    
-    mLocalX = localX; 
-    mLocalY = localY; 
-    mErrLocalX = localXErr;
-    mErrLocalY = localYErr;
-    mChargeUncert = 1000;
-        
-    mPosition.setX( localX ); 
-    mPosition.setY( localY );
-//     mPosition.setZ( zPos );  //////// FIX ME!!!!!!!!!!
-    
-   update2error();
-//    mPositionError.setZ( zErr );  //////// FIX ME!!!!!!!!!!
+//________________________________________________________________________________
+ostream&  operator<<(ostream& os, const StGmtHit& v) {
+  return os << Form("Gmt m %3i ",v.getModule())
+	    << *((StHit *)&v)
+	    << Form(" Adc X/Y  %5.1f/%5.1f locX = %8.3f +/- %7.3f locY = %8.3f +/- %7.3f",
+		    v.getAdcX(), v.getAdcY(), v.getLocalX(), v.getErrorLocalX(), v.getLocalY(), v.getErrorLocalY()); 
 }
-
-// deconstructor
-StGmtHit::~StGmtHit() {
-    // nothing to do
-}
-
-// propagate the uncertainty on phi and r to uncertainty on x and y
-void StGmtHit::update2error(){
-//     float cosPhiSq = cos( mPhi );
-//     cosPhiSq *= cosPhiSq;
-//     float sinPhiSq = 1 - cosPhiSq;
-//     
-//     float mErrRSq = mErrR*mErrR;
-//     float mErrPhiSq = mErrPhi*mErrPhi;
-//     
-//     float xErrSq = mErrRSq*cosPhiSq + mErrPhiSq*sinPhiSq;
-//     float yErrSq = mErrRSq*sinPhiSq + mErrPhiSq*cosPhiSq;
-//     
-//     mPositionError.setX( sqrt( xErrSq ) );
-//     mPositionError.setY( sqrt( yErrSq ) );
-    
-    mPositionError.setX( mErrLocalX );  //////// FIX ME!!!!!!!!!!
-    mPositionError.setY( mErrLocalY );  //////// FIX ME!!!!!!!!!!
-
-}
-ostream&  operator<<(ostream& os, const StGmtHit& v)
-{
-    return os << Form("Gmt m %3i ",v.getModule())
-	      << *((StHit *)&v)
-	      << Form(" Adc X/Y  %4i/%4i t X/Y %4i/%4i locX = %8.3f +/- %7.3f locY = %8.3f +/- %7.3f",
-		      v.getAdcX(), v.getAdcY(), v.getTbX(), v.getTbY(), v.getLocalX(), v.getErrorLocalX(), v.getLocalY(), v.getErrorLocalY()); 
-}
+//________________________________________________________________________________
 void   StGmtHit::Print(Option_t *option) const {cout << *this << endl;}
+//________________________________________________________________________________
 
--- StRoot/StEvent/StGmtHit.h
+++ StRoot/StEvent/StGmtHit.h
@@ -17,194 +17,71 @@
 #include "StHit.h"
 #include "StGmtStrip.h"
 
-// Note: not const StGmtStrip, so the clustering can modify the
-// strips.  Clustering algos require these to be ordered by geoId.
-// Note also, the gmtStripWeightMap_t is only used in memory, and is not
-// streamed.
-typedef map< StGmtStrip*, float, gmtStripPtrLessThan > gmtStripWeightMap_t;
-
 class StGmtHit : public StHit {
 public:
     // constructors
-    StGmtHit( int key = -1, short module = -1, int adcX = 0, int adcY = 0,
-	      int tbX = -1, int tbY = -1, double localX = 0, double localY = 0,
-	      double localXErr = 10000, double localYErr = 10000 );
-    // StGmtHit(const StGmtHit&);             --> use default
-    // StGmtHit& operator=(const StGmtHit&);  --> use default
-    
+    StGmtHit( Int_t key = -1, Int_t module = -1, 
+	      Float_t adcX = 0, Float_t adcY = 0,
+	      Float_t dadcX = 0, Float_t dadcY = 0,
+	      Float_t localX = 0, Float_t localY = 0,
+	      Float_t localXErr = 10000, Float_t localYErr = 10000,
+	      Float_t sigmaX = 0, Float_t sigmaY = 0,
+	      Float_t sigmaXErr = 10000, Float_t sigmaYErr = 10000) : 
+      StHit( StThreeVectorF(localX,localY,0), StThreeVectorF(localXErr,localYErr,0), module+1, 0.), mKey(key), 
+      mAdcX(adcX), mAdcY(adcY), mdAdcX(dadcX), mdAdcY(dadcY), 
+      mSigmaX(sigmaX), mErrSigmaX(sigmaXErr), mSigmaY(sigmaY), mErrSigmaY(sigmaYErr) {}
     // deconstructor
-    ~StGmtHit();
-virtual StDetectorId detector() const 		{return kGmtId;};   
+    ~StGmtHit() {}
+    virtual StDetectorId detector() const 		{return kGmtId;};   
     // accessors/modifiers for the map
-
-    gmtStripWeightMap_t& getStripWeightMap();
-    const gmtStripWeightMap_t& getStripWeightMap() const;
-    
     // modifer
-    void setHardwareId( short module );
-    
     // other accessors
-    int getKey() const;
-    int getModule() const;
-    int getAdcX() const;
-    int getAdcY() const;
-    int getTbX() const;
-    int getTbY() const;
-    double getLocalX() const;
-    double getErrorLocalX() const;
-    double getLocalY() const;
-    double getErrorLocalY() const;
+    Int_t getKey() const {return mKey;}
+    Int_t getModule() const {return hardwarePosition() - 1; }
+    Float_t getAdcX() const {return mAdcX;}
+    Float_t getErrorAdcX() const {return mdAdcX;}
+    Float_t getAdcY() const {return mAdcY;}
+    Float_t getErrorAdcY() const {return mdAdcY;}
+    Float_t getLocalX() const {return position().x();}
+    Float_t getErrorLocalX() const {return positionError().x();}
+    Float_t getLocalY() const {return position().y();}
+    Float_t getErrorLocalY() const {return positionError().y();}
+    Float_t getSigmaX() const {return mSigmaX;}
+    Float_t getErrorSigmaX() const {return mErrSigmaX;}
+    Float_t getSigmaY() const {return mSigmaY;}
+    Float_t getErrorSigmaY() const {return mErrSigmaY;}
+
     // note: no getCharge, as already have charge() defined through parent StHit
-    double getChargeUncert() const;
-//     short getMaxAdc() const;
+//     Short_t getMaxAdc() const;
     
     // modifiers
-    void setAdcX( int adc );
-    void setAdcY( int adc );
-    void setTbX( int tb );
-    void setTbY( int tb );
-    void setLocalX( double position );
-    void setErrorLocalX( double error );
-    void setLocalY( double position );
-    void setErrorLocalY( double error );
-    void setModule( short module );
-    void setChargeUncert( double sigma );
+    void setAdcX( Float_t adc ) {mAdcX = adc;}
+    void setErrorAdcX( Float_t error ) {mdAdcX = error;}
+    void setAdcY( Float_t adc ) {mAdcY = adc;}
+    void setErrorAdcY( Float_t error ) {mdAdcY = error;}
+    void setLocalX( Float_t position ) {mPosition.setX(position);}
+    void setErrorLocalX( Float_t error ) {mPositionError.setX(error);}
+    void setLocalY( Float_t position ) {mPosition.setY(position);}
+    void setErrorLocalY( Float_t error ) {mPositionError.setY(error);}
+    void setSigmaX( Float_t sigma ) {mSigmaX = sigma;}
+    void setErrorSigmaX( Float_t error ) {mErrSigmaX = error;}
+    void setSigmaY( Float_t sigma ) {mSigmaY = sigma;}
+    void setErrorSigmaY( Float_t error ) {mErrSigmaY = error;}
+    void setModule( Short_t module ) {setHardwarePosition(module+1);}
     void     Print(Option_t *option="") const;
-protected:
-    void update2error();                          // set x,y part of inherited mPositionError
-    
-    
 protected:
     // data members
     Int_t   mKey;                                 // unique label
-    Int_t   mModule;                                 // module number
-    Int_t mAdcX, mAdcY;     // Adc counts in X and Y 
-    Int_t mTbX, mTbY;     // time bins of maximum Adc count 
-    Double_t mLocalX, mErrLocalX;     // local x position and error
-    Double_t mLocalY, mErrLocalY;     // local y position and error
+    Float_t mAdcX, mAdcY;     // Adc counts in X and Y 
+    Float_t mdAdcX, mdAdcY;     // Adc counts in X and Y 
+    Float_t mSigmaX, mErrSigmaX;     // local x position and error
+    Float_t mSigmaY, mErrSigmaY;     // local y position and error
+
     Float_t mChargeUncert;                        // uncertanity on the charge
     // for keeping track of which strips constribute to which cluster (not persistant)
-    gmtStripWeightMap_t mStripWeightMap;             //! 
     
 private:   
-    ClassDef(StGmtHit,1)
+    ClassDef(StGmtHit,2)
 }; 
-ostream&              operator<<(ostream& os, StGmtHit const & v);
-
-// inline functions
-
-// inline short StGmtHit::getMaxAdc() const { //////// FIX ME!!!!!!!!!!
-//     short mMaxAdc = -1;
-//     
-//     for(gmtStripWeightMap_t::const_iterator it=mStripWeightMap.begin(); it != mStripWeightMap.end(); it++ ){
-//         short adcVal = it->first->getMaxAdc();
-//         if( adcVal > mMaxAdc ) 
-//             mMaxAdc = adcVal;
-//     };
-// 
-//     return mMaxAdc;
-// }
-
-inline int StGmtHit::getModule() const {            
-    return mModule;  
-};
-inline int StGmtHit::getAdcX() const {            
-    return mAdcX;  
-};
-inline int StGmtHit::getAdcY() const {            
-    return mAdcY;  
-};
-inline int StGmtHit::getTbX() const {            
-    return mTbX;  
-};
-inline int StGmtHit::getTbY() const {            
-    return mTbY;  
-};
-
-
-inline int StGmtHit::getKey() const {
-    return mKey;
-};
-
-inline gmtStripWeightMap_t& StGmtHit::getStripWeightMap() {
-    return mStripWeightMap;
-};
-
-inline const gmtStripWeightMap_t& StGmtHit::getStripWeightMap() const {
-    return mStripWeightMap;
-};
-
-// inline void StGmtHit::setHardwareId( short module ){
-//     mHardwarePosition = module;      //////// FIX ME!!!!!!!!!!
-// };
-
-inline double StGmtHit::getLocalX() const {
-    return mLocalX;
-};
-inline double StGmtHit::getLocalY() const {
-    return mLocalY;
-};
-
-inline double StGmtHit::getErrorLocalX() const {
-    return mErrLocalX;
-};
-inline double StGmtHit::getErrorLocalY() const {
-    return mErrLocalY;
-};
-
-
-
-
-inline void StGmtHit::setAdcX( int adc ){    
-    mAdcX = adc;
-};
-
-inline void StGmtHit::setAdcY( int adc ){    
-    mAdcY = adc;
-};
-
-inline void StGmtHit::setTbX( int tb ){    
-    mTbX = tb;
-};
-
-inline void StGmtHit::setTbY( int tb ){    
-    mTbY = tb;
-};
-
-inline void StGmtHit::setModule( short module ){    
-    mModule = module;
-};
-
-// inline void StGmtHit::setIsY( int isY ){
-//     mIsY = isY;
-//     short module = getModule();
-//     
-//     setHardwareId( module );
-// };
-
-inline void StGmtHit::setLocalX( double position ){ 
-    mLocalX = position;
-    mPosition.setX( mLocalX );
-};
-
-inline void StGmtHit::setLocalY( double position ){ 
-    mLocalY = position;
-    mPosition.setY( mLocalY );
-};
-
-inline void StGmtHit::setErrorLocalX( double error ){
-    mErrLocalX = error;
-    update2error();
-};
-
-inline void StGmtHit::setErrorLocalY( double error ){
-    mErrLocalY = error;
-    update2error();
-};
-
-// charge uncertainty
-
-inline double StGmtHit::getChargeUncert() const { return mChargeUncert; };
-inline void StGmtHit::setChargeUncert( double sigma ){ mChargeUncert = sigma; };
-
+ostream&   operator<<(ostream& os, StGmtHit const & v);
 #endif
