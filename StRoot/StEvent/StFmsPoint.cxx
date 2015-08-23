fa8f17fc549dd94c0e452061032ef32505797e9c
Yuri Fisyak <fisyak@bnl.gov>
2015-08-23 15:41:53 -0400

    Merge
    Former-commit-id: 79014959829dfda1540d0cbc94fa739fc33d3783

--- StRoot/StEvent/StFmsPoint.cxx
+++ StRoot/StEvent/StFmsPoint.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StFmsPoint.cxx,v 2.1 2015/02/14 18:56:00 ullrich Exp $
+ * $Id: StFmsPoint.cxx,v 2.2 2015/08/19 19:22:34 ullrich Exp $
  *
  * Author: Thomas Burton, Yuxi Pan, 2014
  ***************************************************************************
@@ -11,18 +11,19 @@
  ***************************************************************************
  *
  * $Log: StFmsPoint.cxx,v $
- * Revision 2.1  2015/02/14 18:56:00  ullrich
- * Initial Revision.
+ * Revision 2.2  2015/08/19 19:22:34  ullrich
+ * Major update (PID) by Akio.
  *
  *
  ***************************************************************************/
 #include "StFmsPoint.h"
 
-static const char rcsid[] = "$Id: StFmsPoint.cxx,v 2.1 2015/02/14 18:56:00 ullrich Exp $";
+static const char rcsid[] = "$Id: StFmsPoint.cxx,v 2.2 2015/08/19 19:22:34 ullrich Exp $";
 
 StFmsPoint::StFmsPoint()
-    : mDetectorId(0), mEnergy(-1.0), mX(-99.0), mY(-99.0), mId(-1),
-      mParentClusterId(-1), mNParentClusterPhotons(-1), mCluster(0)
-{ /* no op */ }
+    : mDetectorId(0), mEnergy(-1.0), mX(-99.0), mY(-99.0), mId(-1), mParentClusterId(-1), mNParentClusterPhotons(-1), mCluster(0)
+{
+  resetFps(); 
+}
 
 StFmsPoint::~StFmsPoint() { /* no op */ }
