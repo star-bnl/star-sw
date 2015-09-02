e30a0b645c54a3d8775fce4b05722403fee472d7
akio <>
2015-09-02 14:50:10 +0000

    Adding SetReadMuDst() to give optoins for reading back from Mudst Reading hits only and re-run clustering, or reading hit+cluster+point(no new calibration applied).

--- StRoot/StFmsHitMaker/StFmsHitMaker.cxx
+++ StRoot/StFmsHitMaker/StFmsHitMaker.cxx
@@ -1,8 +1,13 @@
-//*-- Author : Jingguo Ma
+// ROOT
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TTree.h"
 
 #include "StMessMgr.h"
 #include "StEventTypes.h"
 #include "StMuDSTMaker/COMMON/StMuTypes.hh"
+#include "StMuDSTMaker/COMMON/StMuFmsUtil.h"
 
 #include "StFmsDbMaker/StFmsDbMaker.h"
 #include "StFmsCollection.h"
@@ -10,135 +15,149 @@
 #include "StFmsHitMaker.h"
 #include "StTriggerData2009.h"
 
+// PSU-FMS package
+//#include "StFmsPointMaker/StFmsClusterFitter.h"
+
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 using namespace std;
 
 ClassImp(StFmsHitMaker);
 
-/// This is StFmsHitMaker constructor, it initializes some variables to NULL.
-StFmsHitMaker::StFmsHitMaker(const char* name) : StMaker(name){
-  mFmsDbMaker=NULL;
-  mFmsCollection = NULL;
-  mMuFmsColl = NULL;
-  LOG_DEBUG << "StFmsHitMaker::constructor." << endm;
+StFmsHitMaker::StFmsHitMaker(const char* name) : StMaker(name), mReadMuDst(0) {
+	mFmsDbMaker = NULL;
+	mFmsCollection = NULL;
+	mMuFmsColl = NULL;
+	LOG_INFO << "StFmsHitMaker initializing...."<<endm;
+	LOG_DEBUG << "StFmsHitMaker::constructor." << endm;
 }
 
-/// This is StFmsHitMaker destructor, it does nothing right now.
 StFmsHitMaker::~StFmsHitMaker(){
-  LOG_DEBUG << "StFmsHitMaker::destructor." << endm;
+  
+  LOG_INFO << "StFmsHitMaker::destructor." << endm;
 }
 
-/// This is StFmsHitMaker Clear()
 void StFmsHitMaker::Clear(Option_t* option){
-  LOG_DEBUG << "StFmsHitMaker::Clear." << endm;
+  LOG_DEBUG << "StFmsHitMaker::Clear()" << endm;
   StMaker::Clear(option);
 }
 
-/// This is StFmsHitMaker Init()
 int StFmsHitMaker::Init() {
-  LOG_DEBUG<<"StFmsHitMaker Init Start"<<endm;
-  return StMaker::Init();
+  
+	LOG_INFO<<"StFmsHitMaker::Init() "<<endm;
+
+	return StMaker::Init();
+
 }
 
-/// This is StFmsHitMaker InitRun, it gets the pointer to the FmsDbMaker.
-int StFmsHitMaker::InitRun(Int_t runNumber) {
-  LOG_DEBUG << "StFmsHitMaker::InitRun - run = " << runNumber << endm;
-  mFmsDbMaker = gStFmsDbMaker;
-  return kStOK;
+int StFmsHitMaker::InitRun(Int_t runNumber) {  
+	LOG_INFO << "StFmsHitMaker::InitRun --run# changed to " << runNumber << endm;
+	mFmsDbMaker = static_cast<StFmsDbMaker*>(GetMaker("fmsDb")); 
+	if(!mFmsDbMaker){
+	  LOG_ERROR  << "StFmsHitMaker::InitRun Failed to get StFmsDbMaker" << endm;
+	  return kStFatal;
+	}
+	mCurrentRunNumber = runNumber; //called by maker's method :InitRun(run); when the run# changes
+	return kStOK;
 }
 
 //// This is StFmsHitMaker Make, it reads in data and make hit and fill StFmsCollection
 int StFmsHitMaker::Make(){
-  LOG_DEBUG<<"StFmsHitMaker::Make start"<<endm;
-  int flag = 0;
-  StTriggerData* triggerData = 0;
-  StFmsTriggerDetector* triggerDetector =0;
 
-  ///first try to get StTriggerData from StTriggerDataMaker and create StFmsCollection
-  TObjectSet *os = (TObjectSet*)GetDataSet("StTriggerData");
-  if (os) {
-    triggerData = (StTriggerData*)os->GetObject();
-    if(triggerData){
-      flag=1;
-      mFmsCollection = new StFmsCollection();
-      LOG_DEBUG<<"StFmsHitMaker::Make Found StTriggerData from StTriggerDataMaker"<<endm;
-    }
-  }
-  
-  ///2nd try to get StTriggerData from StEvent
-  ///but once FMS data is killed in StEvent, this will not work and all you see is empty data
-  StEvent* stEvent = (StEvent*) GetInputDS("StEvent");
-  if(flag==0){
-    if(stEvent){
-      triggerData = stEvent->triggerData();
-      if(triggerData) {
-	flag=2;
-	mFmsCollection = new StFmsCollection();
-	LOG_DEBUG<<"StFmsHitMaker::Make Found StTriggerData from StEvent"<<endm;
-      }
-      else{
-	mFmsCollection = stEvent->fmsCollection();
-	if(mFmsCollection){
-	  flag=3;
-	  LOG_DEBUG<<"StFmsHitMaker::Make Found StFmsCollection from StEvent"<<endm;
-	}
-      }
-    }
-  }
+	LOG_DEBUG<<"StFmsHitMaker::Make start"<<endm;
+	int flag = 0;
+	StTriggerData* triggerData = 0;
+	
+	if(mReadMuDst>0) return readMuDst();
 
-  ///try to read QT data from MuDST (run8)
-  StMuDst* muDst = (StMuDst*)GetInputDS("MuDst");
-  if(flag==0) {
-    if(muDst){
-      triggerDetector = &(StMuDst::event()->fmsTriggerDetector());
-      if(triggerDetector){
-	flag=0; ///temporary
-	mFmsCollection = new StFmsCollection();
-	LOG_DEBUG<<"StFmsHitMaker::Make Found StFmsTriggerDetector in MuDst"<<endm;
-      }
-    }
-  }
-  
-  ///creating StFmsHit and adding it to StFmsCollection
-  if(flag>0){
-    for(unsigned short crt=1; crt<=5; crt++){
-      unsigned short crt2=crt;
-      if(crt==5) crt2=7;
-      for(unsigned short slot=1; slot<=16; slot++){
-	for(unsigned short ch=0; ch<32; ch++){
-	  unsigned short adc=0;
-	  unsigned short tdc=0;
-	  if(flag<3){
-	    adc=triggerData->fmsADC(crt,slot-1,ch);
-	    tdc=triggerData->fmsTDC(crt,slot-1,ch);
-	  }
-	  if(flag==4 && crt<5){
-	    adc=triggerDetector->adc(crt+10,slot-1+16,ch/8,ch%8);
-	    tdc=triggerDetector->tdc(crt+10,slot-1+16,ch/8,ch%8);
-	  }
-	  if(adc>0 || tdc>0){
-	    StFmsHit* hit = new StFmsHit();
-	    if(!hit){
-	      LOG_ERROR <<"Failed to create FMS hit, skip this hit."<<endm;
-	      continue;
-	    }
-	    hit->setDetectorId(0);
-	    hit->setChannel(0);
-	    hit->setQtCrate(crt2);
-	    hit->setQtSlot(slot);
-	    hit->setQtChannel(ch);
-	    hit->setAdc(adc);
-	    hit->setTdc(tdc);
-	    hit->setEnergy(0.0);
-	    mFmsCollection->addHit(hit);
-	    if(GetDebug()>0) hit->print();
-	  }
+	//first try to get StTriggerData from StTriggerDataMaker (works for proudction) and create StFmsCollection
+	TObjectSet *os = (TObjectSet*)GetDataSet("StTriggerData");
+	if (os) {
+		triggerData = (StTriggerData*)os->GetObject();
+		if(triggerData){
+			flag=1;
+			// mFmsCollection = new StFmsCollection();
+			LOG_DEBUG<<"StFmsHitMaker::Make Found StTriggerData from StTriggerDataMaker"<<endm;
+		}
 	}
-      }	
-    }
-  }
-    
+  
+	//2nd try to get StTriggerData from StEvent
+	//but once FMS data is killed in StEvent, this will not work and all you see is empty data
+	StEvent* stEvent = (StEvent*) GetInputDS("StEvent");
+	//StEvent* stEvent = (StEvent*) GetInputDS("StEvent");
+	if(flag==0){
+		if(stEvent){
+			triggerData = stEvent->triggerData();
+			if(triggerData) {
+				flag=2;
+				LOG_DEBUG<<"StFmsHitMaker::Make Found StTriggerData from StEvent"<<endm;
+			}
+			else{
+				mFmsCollection = stEvent->fmsCollection();
+				if(mFmsCollection){
+	  				flag=3;
+	  				LOG_DEBUG<<"StFmsHitMaker::Make Found StFmsCollection from StEvent"<<endm;
+				}
+			}
+		} //found StEvent
+	}
+	LOG_DEBUG<<"before checking MuDst, flag is: "<<flag<<endl;
+  
+	//3rd try to get StTriggerData from StMuEvent, works for produced data (.MuDst.root) --Yuxi
+	StMuDst* muDst = (StMuDst*)GetInputDS("MuDst");
+	if(flag==0){
+		if(muDst){
+    	mCurrentRunNumber = muDst->event()->runNumber();
+			triggerData = (StTriggerData*)StMuDst::event()->triggerData();
+			if(triggerData){
+				flag = 4; //Yuxi
+				LOG_INFO<<"StFmsHitMaker::Make Found StFmsTriggerData in MuDst"<<endm;
+			}
+			else LOG_ERROR << "Finally, no StFmsTriggerData in MuDst " <<endm;
+		}
+	}
+   
+ 
+	LOG_DEBUG<<"after checking MuDst, flag is: "<<flag<<endm;
+	//after this step triggerData is pointing to StTriggerData block of StEvent
+
+	if(flag>0){
+		mFmsCollection = new StFmsCollection();
+		//create StFmsHit and add it to StFmsCollection
+		for(unsigned short crt=1; crt<=4; crt++){
+			for(unsigned short slot=1; slot<=16; slot++){
+				for(unsigned short ch=0; ch<32; ch++){
+	  				unsigned short adc=0;
+	  				unsigned short tdc=0;
+	  				if(flag<=4){ //wont work when flag=3
+	    					adc=triggerData->fmsADC(crt,slot-1,ch);
+	    					tdc=triggerData->fmsTDC(crt,slot-1,ch);
+	  				}
+				
+					if(adc>0 || tdc>0){
+					//	LOG_INFO<<"adc of crt "<<crt<<", slot "<<slot<<", channel "<<ch<<" is: "<<adc<<endm;
+					//	LOG_INFO<<"tdc=====================================================is: "<<tdc<<endm;
+	    					StFmsHit* hit = new StFmsHit();
+	    					if(!hit){
+	      						LOG_ERROR <<"Failed to create FMS hit, skip this hit."<<endm;
+	      						continue;
+	    					}
+						hit->setDetectorId(0);
+						hit->setChannel(0);
+						hit->setQtCrate(crt);
+						hit->setQtSlot(slot);
+						hit->setQtChannel(ch);
+						hit->setAdc(adc);
+						hit->setTdc(tdc);
+						hit->setEnergy(0.0);
+						mFmsCollection->addHit(hit);
+						if(GetDebug()>0) hit->print();
+	  				}
+				}
+      			}	
+    		}
+
 
   /// Read DB and put DetectorId, channeel and apply Calibration to get Energy
   if(flag>0){
@@ -152,9 +171,9 @@ int StFmsHitMaker::Make(){
       mFmsDbMaker->getReverseMap(crt,slot,ch,&d,&c);
       float e=0.0;
       if(d>0 || c>0){
-	float g1=mFmsDbMaker->getGain(d,c);
-	float g2=mFmsDbMaker->getGainCorrection(d,c);
-	e       =adc*g1*g2;
+  float g1=mFmsDbMaker->getGain(d,c);
+  float g2=mFmsDbMaker->getGainCorrection(d,c);
+  e       =adc*g1*g2;
       }
       fmsHit->setDetectorId(d);
       fmsHit->setChannel(c);
@@ -162,35 +181,61 @@ int StFmsHitMaker::Make(){
       if(GetDebug()>0) fmsHit->print();
     }
   }
-  
-  LOG_INFO<<"StFmsHitMaker::Make got "<<mFmsCollection->numberOfHits()<<" hits in StFmsCollection"<<endm;
-    
-  ///try to read StFmsHit in MuDST and overwrite energy with current calibration
-  if(flag==0) {
-    if(muDst){
-      mMuFmsColl = StMuDst::muFmsCollection();
-      if(mMuFmsColl){
-	int nhits = mMuFmsColl->numberOfHits();
-	LOG_DEBUG<<"StFmsHitMaker::Make Found "<<nhits<<" hits in MuDst"<<endm;
-	for(int i=0; i<nhits; i++){
-	  StMuFmsHit* mHit = mMuFmsColl->getHit(i);
-	  if(mHit && GetDebug()>0) mHit->print();
+
+ 	LOG_INFO<<"StFmsHitMaker::Make(): flag = "<<flag<<", got "<<mFmsCollection->numberOfHits()<<" hits in StFmsCollection"<<endm;
+	
+
+						
+
+	}//flag, received fms data from mudst, triggerdata, etc.
+
+/*   
+	//flag = 0; //yuxi debug
+	if(flag==0) { //read hits created during production
+		if(muDst){
+			mMuFmsColl = StMuDst::muFmsCollection();
+			if(mMuFmsColl){
+				int nhits = mMuFmsColl->numberOfHits();
+				LOG_DEBUG<<"StFmsHitMaker::Make Found "<<nhits<<" hits in MuDst"<<endm;
+				cout<<"StFmsHitMaker::Make Found "<<nhits<<" hits in MuDst"<<endl;
+				for(int i=0; i<nhits; i++){
+		  			StMuFmsHit* mHit = mMuFmsColl->getHit(i);
+		  			if(mHit && GetDebug()>0) mHit->print();
+		  			mHit->print();
+				}
+				flag=5;
+			}
+		}
 	}
-	flag=5;
-      }
-    }
-  }
+*/
+	if(stEvent) {
+		//Adding StFmsCollection to StEvent
+		LOG_INFO<<"StFmsHitMaker::Make Adding StFmsCollection to StEvent"<<endm;
+		stEvent->setFmsCollection(mFmsCollection);
+	}
+	else LOG_INFO << "StEvent is empty" << endm;
 
-  if(flag>0 && flag<=2) {
-    ///Adding StFmsCollection to StEvent
-    LOG_DEBUG<<"StFmsHitMaker::Make Adding StFmsCollection as FMSCOLLECTION"<<endm;
-    stEvent->setFmsCollection(mFmsCollection);
-  }
+	return kStOk;
+}
 
+int StFmsHitMaker::Finish(){  
+  LOG_INFO << "StFmsHitMaker::Finish() " << endm;
   return kStOk;
 }
 
-/// This is StFmsHitMaker Finish()
-int StFmsHitMaker::Finish(){
+Int_t StFmsHitMaker::readMuDst(){
+  StEvent* event = (StEvent*)GetInputDS("StEvent");
+  if(!event){LOG_INFO<<"StFmsHitMaker::readMuDst found no StEvent"<<endm; return kStErr;}
+  StFmsCollection* fmsColl = event->fmsCollection();
+  if(!fmsColl){
+    fmsColl=new StFmsCollection;
+    event->setFmsCollection(fmsColl);
+  }
+  StMuDst* mudst = (StMuDst*)GetInputDS("MuDst");
+  if(!mudst){LOG_INFO<<"StFmsHitMaker::readMuDst found no MuDst"<<endm; return kStErr;}
+  StMuFmsCollection* mufmsColl= mudst->muFmsCollection();
+  if(!mufmsColl){LOG_INFO<<"StFmsHitMaker::readMuDst found no MuFmsCollection"<<endm; return kStErr;}
+  StMuFmsUtil util;
+  util.fillFms(fmsColl,mufmsColl); 
   return kStOk;
 }
