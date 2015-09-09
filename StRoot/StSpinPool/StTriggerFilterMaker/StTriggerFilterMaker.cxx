069d9fe0bf45e31f5f357f2a9cd8dcbb5ec708cf
Akio Ogawa <akio@bnl.gov>
2015-09-09 20:29:39 +0000

    Adding Vetoing TriggerId Also adding printing if option is set

--- StRoot/StSpinPool/StTriggerFilterMaker/StTriggerFilterMaker.cxx
+++ StRoot/StSpinPool/StTriggerFilterMaker/StTriggerFilterMaker.cxx
@@ -1,4 +1,4 @@
-// $Id: StTriggerFilterMaker.cxx,v 1.1 2008/01/23 04:45:07 kocolosk Exp $
+// $Id: StTriggerFilterMaker.cxx,v 1.2 2015/09/09 20:29:39 akio Exp $
 
 #include "StTriggerFilterMaker.h"
 #include "StMuDSTMaker/COMMON/StMuDst.h"
@@ -6,10 +6,11 @@
 
 ClassImp(StTriggerFilterMaker)
 
-StTriggerFilterMaker::StTriggerFilterMaker(const char *name) : StMaker(name) { }
+StTriggerFilterMaker::StTriggerFilterMaker(const char *name) : StMaker(name), mPrint(0) { }
 
 StTriggerFilterMaker::~StTriggerFilterMaker() {
     mGoodTriggers.clear();
+    mVetoTriggers.clear();
 }
 
 Int_t StTriggerFilterMaker::Init() {
@@ -19,16 +20,33 @@ Int_t StTriggerFilterMaker::Init() {
 }
 
 Int_t StTriggerFilterMaker::Make() {
-    for(unsigned i=0; i<mGoodTriggers.size(); ++i) {
-        if(StMuDst::event()->triggerIdCollection().nominal().isTrigger(mGoodTriggers[i])) {
-            return kStOk;
-        }
+  if(mPrint){
+    vector<unsigned int> ids = StMuDst::event()->triggerIdCollection().nominal().triggerIds();
+    LOG_INFO << "Offline Trigger Id = ";
+    for(unsigned i=0; i<ids.size(); ++i) LOG_INFO << ids[i] << " ";
+  }
+  for(unsigned i=0; i<mVetoTriggers.size(); ++i) {
+    if(StMuDst::event()->triggerIdCollection().nominal().isTrigger(mVetoTriggers[i])) {
+      if(mPrint)  LOG_INFO << " Veto" << endm;
+      return kStSkip;
     }
-    return kStSkip;
+  }
+  for(unsigned i=0; i<mGoodTriggers.size(); ++i) {
+    if(StMuDst::event()->triggerIdCollection().nominal().isTrigger(mGoodTriggers[i])) {
+      if(mPrint)   LOG_INFO << " Accept" << endm;
+      return kStOk;
+    }
+  }
+  if(mPrint)    LOG_INFO << " Skip" << endm;
+  return kStSkip;
 }
 
 /*****************************************************************************
  * $Log: StTriggerFilterMaker.cxx,v $
+ * Revision 1.2  2015/09/09 20:29:39  akio
+ * Adding Vetoing TriggerId
+ * Also adding printing if option is set
+ *
  * Revision 1.1  2008/01/23 04:45:07  kocolosk
  * Privileged Maker which skips events unless they fired any one of a set of supplied trigIDs
  *
--- StRoot/StSpinPool/StTriggerFilterMaker/StTriggerFilterMaker.h
+++ StRoot/StSpinPool/StTriggerFilterMaker/StTriggerFilterMaker.h
@@ -1,7 +1,7 @@
 #ifndef ST_TRIGGER_FILTER_MAKER_HH
 #define ST_TRIGGER_FILTER_MAKER_HH
 
-// $Id: StTriggerFilterMaker.h,v 1.3 2014/08/06 11:43:40 jeromel Exp $
+// $Id: StTriggerFilterMaker.h,v 1.4 2015/09/09 20:29:39 akio Exp $
 
 /*****************************************************************************
  * @class StTriggerFilterMaker
@@ -29,15 +29,20 @@ public:
     //virtual Int_t Finish();
     
     void addTrigger(unsigned int trigId) { mGoodTriggers.push_back(trigId); }
-    
+    void addVetoTrigger(unsigned int trigId) { mVetoTriggers.push_back(trigId); }
+    void printTriggerId(int v=1) {mPrint=v;}
+
     const vector<unsigned int> getTriggers() const {return mGoodTriggers;}
+    const vector<unsigned int> getVetoTriggers() const {return mGoodTriggers;}
 
     virtual const char* GetCVS() const
-    {static const char cvs[]="Tag $Name:  $ $Id: StTriggerFilterMaker.h,v 1.3 2014/08/06 11:43:40 jeromel Exp $ built " __DATE__ " " __TIME__; return cvs;}
+    {static const char cvs[]="Tag $Name:  $ $Id: StTriggerFilterMaker.h,v 1.4 2015/09/09 20:29:39 akio Exp $ built " __DATE__ " " __TIME__; return cvs;}
     
 private:
     vector<unsigned int> mGoodTriggers; //!
-    
+    vector<unsigned int> mVetoTriggers; //!
+    int mPrint; //!
+
     ClassDef(StTriggerFilterMaker,1)
 };
 
@@ -45,6 +50,10 @@ private:
 
 /*****************************************************************************
  * $Log: StTriggerFilterMaker.h,v $
+ * Revision 1.4  2015/09/09 20:29:39  akio
+ * Adding Vetoing TriggerId
+ * Also adding printing if option is set
+ *
  * Revision 1.3  2014/08/06 11:43:40  jeromel
  * Suffix on literals need to be space (later gcc compiler makes it an error) - first wave of fixes
  *
