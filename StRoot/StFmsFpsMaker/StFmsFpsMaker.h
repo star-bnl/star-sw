a91e6366e514ac28be1b7e9683fd7b42e913dedc
akio <>
2015-09-18 18:50:06 +0000

    cleaning up and adding QA histos

--- StRoot/StFmsFpsMaker/StFmsFpsMaker.h
+++ StRoot/StFmsFpsMaker/StFmsFpsMaker.h
@@ -3,8 +3,11 @@
 //
 //   This is analysis for FMS-FPS correlations.
 // 
-//  $Id: StFmsFpsMaker.h,v 1.1 2015/09/02 14:56:12 akio Exp $
+//  $Id: StFmsFpsMaker.h,v 1.2 2015/09/18 18:50:06 akio Exp $
 //  $Log: StFmsFpsMaker.h,v $
+//  Revision 1.2  2015/09/18 18:50:06  akio
+//  cleaning up and adding QA histos
+//
 //  Revision 1.1  2015/09/02 14:56:12  akio
 //  Initial version of FMS-FPS correation analysis
 //
@@ -29,49 +32,65 @@ public:
 
   void setReadMuDST(int v=1){mReadMuDST=v;}         //Read MuDST hits if available, and update FPS hits in StEvent using current DB values
                                                     //if this is set to 0 (default), use hits as is (new DB values will NOT be applied)
-  void setQA(char* file){mQA=true; mFilename=file;} //enable alighment histograms for QA
 
+  void setQA(char* file){mQA=true; mFilename=file;} //enable alighment histograms for QA
+  void setPrint(int v=1){mPrint=v;}
   void print();
 
 private:
-  enum {NCUT=13};
-
   StFmsDbMaker* mFmsDbMaker;
   StFmsCollection* mFmsColl;
 
   bool mQA;
   char* mFilename;
   TFile* mFile;
-  
+  int mPrint;
+
   int mReadMuDST; 
   void readMuDST();
 
-  //float mHit[NQ][kFpsNLayer][kFpsNSlat];
-  //int   mNHit[NQ][kFpsNLayer];
-  //int   mNFmsPoint[NQ][kFpsNLayer][kFpsNSlat];
-  //int   mFmsPoint[kFpsNQuad][kFpsNLayer][kFpsNSlat][NMAXPOINT];
-  //void makeHitArray(); 
-  
   void corrFmsFps();
   void pid(int opt=0);
+  void isolationCone();
 
-  void fmsFpsAlignment();
-  TH2F* mH2[kFpsNQuad][kFpsNLayer][NCUT];
-  TH1F* mHd[kFpsNQuad][kFpsNLayer][NCUT];
-  TH2F* mHd2[kFpsNQuad][kFpsNLayer][NCUT];
-  TH1F* mNp[NCUT];
-  TH1F* mHene[NCUT];
-  TH1F* mHpt[NCUT];
-  TH2F* mHept[NCUT];
-  TH1F* mHeta[NCUT];
-  TH1F* mHphi[NCUT];
-  TH1F* mHx[NCUT];
-  TH1F* mHy[NCUT];
-  TH2F* mHxy[NCUT];
-  TH1F* mHm1[NCUT];
-  TH1F* mHm2[NCUT];
-  TH1F* mHpid[NCUT];
-  TH1F* mHpid2[NCUT];
+  //QA & alignment related
+  enum {NCUT1=6};  //0:all, 1:E>3GeV, 2:PID=gamma, 3:PID=hadron, 4:PID=electron, 5:others
+  enum {NCUT2=6};  //0:all combo, 1:E1,E2>5GeV, 2:PID=gg, 3:PID=hh, 4:ee, 5:other PID combos
+
+  void fmsFpsAlignment(); 
+  TH2F* mH2[kFpsNQuad][kFpsNLayer][NCUT1];
+  TH1F* mHd[kFpsNQuad][kFpsNLayer][NCUT1];
+  TH2F* mHd2[kFpsNQuad][kFpsNLayer][NCUT1];
+
+  TH1F* mHn[NCUT1];
+  TH1F* mHene[NCUT1];
+  TH1F* mHelo[NCUT1];
+  TH1F* mHpt[NCUT1];
+  TH2F* mHept[NCUT1];
+  TH1F* mHeta[NCUT1];
+  TH1F* mHphi[NCUT1];
+  TH1F* mHx[NCUT1];
+  TH1F* mHy[NCUT1];
+  TH2F* mHxy[NCUT1];
+  TH1F* mHpid[NCUT1];
+  TH1F* mHpid2[NCUT1];
+
+
+  TH1F* mPn[NCUT1];
+  TH1F* mPene[NCUT2];
+  TH1F* mPpt[NCUT2];
+  TH2F* mPept[NCUT2];
+  TH1F* mPeta[NCUT2];
+  TH1F* mPphi[NCUT2];
+  TH2F* mPpid[NCUT2];
+  TH1F* mPm1[NCUT2];
+  TH1F* mPm2[NCUT2];
+  TH1F* mPzgg[NCUT2];
+  TH1F* mPr30[NCUT2];
+  TH1F* mPr100[NCUT2];
+
+  virtual const char *GetCVS() const
+    {static const char cvs[]="Tag $Name:  $ $Id: StFmsFpsMaker.h,v 1.2 2015/09/18 18:50:06 akio Exp $ built " __DATE__ " " __TIME__ ; return cvs;}
 
   ClassDef(StFmsFpsMaker,0);
 };
