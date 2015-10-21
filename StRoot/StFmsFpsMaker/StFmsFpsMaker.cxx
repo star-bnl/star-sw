3594caaaed489c8137632c45fac7d138783dd27a
Akio Ogawa <akio@bnl.gov>
2015-10-21 15:51:01 +0000

    Add more QA for debugging FMS reconstruciton code

--- StRoot/StFmsFpsMaker/StFmsFpsMaker.cxx
+++ StRoot/StFmsFpsMaker/StFmsFpsMaker.cxx
@@ -1,8 +1,11 @@
 // \class StFmsFpsMaker
 // \author Akio Ogawa
 //
-//  $Id: StFmsFpsMaker.cxx,v 1.3 2015/09/18 18:50:06 akio Exp $
+//  $Id: StFmsFpsMaker.cxx,v 1.4 2015/10/21 15:51:01 akio Exp $
 //  $Log: StFmsFpsMaker.cxx,v $
+//  Revision 1.4  2015/10/21 15:51:01  akio
+//  Add more QA for debugging FMS reconstruciton code
+//
 //  Revision 1.3  2015/09/18 18:50:06  akio
 //  cleaning up and adding QA histos
 //
@@ -19,6 +22,7 @@
 #include "Stypes.h"
 
 #include "StFmsDbMaker/StFmsDbMaker.h"
+#include "StEnumerations.h"
 #include "StEventTypes.h"
 #include "StEvent/StEvent.h"
 #include "StEvent/StFmsCollection.h"
@@ -60,7 +64,10 @@ float distance(float x, float y, float x0, float y0, float xw, float yw){
 
 ClassImp(StFmsFpsMaker);
 
-StFmsFpsMaker::StFmsFpsMaker(const Char_t* name):StMaker(name),mQA(false),mFilename((char *)"fmsfps.root"),mPrint(0),mReadMuDST(0){}
+StFmsFpsMaker::StFmsFpsMaker(const Char_t* name):
+    StMaker(name),mQA(false),mFilename((char *)"fmsfps.root"),mPrint(0),mReadMuDST(0),
+    mMaxDistanceToAssociate(2.0),mPidMethod(1)
+{}
 
 StFmsFpsMaker::~StFmsFpsMaker(){}
 
@@ -75,6 +82,32 @@ Int_t StFmsFpsMaker::Init(){
     char c[100];
     mFile=new TFile(mFilename,"RECREATE");
     static const int pm[kFpsNQuad][kFpsNLayer]={{1,1,1},{1,-1,-1},{-1,1,1},{-1,-1,-1}}; //+or- measured coordnates
+    
+    mERatio[0]= new TH1F("ERatioCluster-Hit","ERatioCH",50.0,0.0,1.2);
+    mERatio[1]= new TH1F("ERatioPoint-Cluster","ERatioPC",50.0,0.0,1.2);
+
+    mFmsAdc      = new TH1F("FmsAdc","FmsAdc",4096,0.0,4096.0);
+    mFmsHitLarge = new TH2F("FmsHitLarge","FmsHitLarge",34,-17.0,17.0,34,0.0,34.0);
+    mFmsHitSmall = new TH2F("FmsHitSmall","FmsHitSmall",24,-12.0,12.0,24,0.0,24.0);
+    mFpsMip[0]   = new TH1F("FpsMipL1","FpsMipL1",100,0.0,5.0);
+    mFpsMip[1]   = new TH1F("FpsMipL2","FpsMipL2",100,0.0,5.0);
+    mFpsMip[2]   = new TH1F("FpsMipL3","FpsMipL3",180,0.0,90.0);
+    
+    mNTow[0]  = new TH1F("NTowL","NTowL",50,0.0,50.0);
+    mNTow[1]  = new TH1F("NTowS","NTowS",50,0.0,50.0);
+    mNTowE[0] = new TH2F("NTowEL","NTowEL",50,0.0,50.0,100.0,0.0,100.0);
+    mNTowE[1] = new TH2F("NTowES","NTowES",50,0.0,50.0,100.0,0.0,100.0);
+    mSigmax[0]= new TH1F("SigMaxL","SigMaxL",100.0,0.0,4.0);
+    mSigmax[1]= new TH1F("SigMaxS","SigMaxS",100.0,0.0,4.0);
+    mSigmin[0]= new TH1F("SigMinL","SigMinL",100.0,0.0,4.0);
+    mSigmin[1]= new TH1F("SigMinS","SigMinS",100.0,0.0,4.0);
+    mSigmaxE[0]= new TH2F("SigMaxEL","SigMaxEL",100.0,0.0,100.0,100,0.0,2.0);
+    mSigmaxE[1]= new TH2F("SigMaxES","SigMaxES",100.0,0.0,100.0,100,0.0,2.0);
+    mChi2[0]  = new TH1F("Chi2L","Chi2L",500.0,0.0,2000.0);
+    mChi2[1]  = new TH1F("Chi2S","Chi2S",500.0,0.0,2000.0);
+    mCluXY[0]= new TH2F("CluXYL","CluXYL",500.0,0.0,17*5.812,500,0.0,34.0*5.812);
+    mCluXY[1]= new TH2F("CluXYS","CluXYS",500.0,0.0,24*3.822,500,0.0,12.0*3.875);
+
     for(int cut=0; cut<NCUT1; cut++){
 	for(int q=0; q<kFpsNQuad; q++){
 	    for(int l=0; l<kFpsNLayer; l++){
@@ -98,7 +131,7 @@ Int_t StFmsFpsMaker::Init(){
 	sprintf(c,"phi_c%d",cut);  mHphi[cut]=new TH1F(c,c,100,-3.141592654,3.141592654);
 	sprintf(c,"x_c%d",cut);    mHx  [cut]=new TH1F(c,c,100,-100.0,100.0);
 	sprintf(c,"y_c%d",cut);    mHy  [cut]=new TH1F(c,c,100,-100.0,100.0);
-	sprintf(c,"xy_c%d",cut);   mHxy [cut]=new TH2F(c,c,100,-100.0,100.0,100,-100.0,100.0);
+	sprintf(c,"xy_c%d",cut);   mHxy [cut]=new TH2F(c,c,500,-100.0,100.0,500,-100.0,100.0);
 	sprintf(c,"pid_c%d",cut);  mHpid[cut]=new TH1F(c,c,41,-0.5,40.5);
 	sprintf(c,"pid2_c%d",cut); mHpid2[cut]=new TH1F(c,c,5,-0.5,4.5);
     }//loop over cuts
@@ -114,8 +147,10 @@ Int_t StFmsFpsMaker::Init(){
 	sprintf(c,"p_m1_c%d",cut);   mPm1 [cut]=new TH1F(c,c,100,0.0,1.0);
 	sprintf(c,"p_m2_c%d",cut);   mPm2 [cut]=new TH1F(c,c,100,0.0,5.0);    
 	sprintf(c,"p_zgg_c%d",cut);  mPzgg[cut]=new TH1F(c,c,100,0.0,1.0);    
+	sprintf(c,"p_dgg_c%d",cut);  mPdgg[cut]=new TH1F(c,c,200,0.0,20.0);    
 	sprintf(c,"p_r30_c%d",cut);  mPr30[cut]=new TH1F(c,c,100,0.0,3.0);    
-	sprintf(c,"p_r100_c%d",cut);mPr100[cut]=new TH1F(c,c,100,0.0,2.0);    
+	sprintf(c,"p_r100_c%d",cut); mPr100[cut]=new TH1F(c,c,100,0.0,2.0);    
+	sprintf(c,"p_xy_c%d",cut);   mPxy[cut]=new TH2F(c,c,500,-100.0,100.0,500,-100.0,100.0);    
     }
     return kStOK;
 }
@@ -137,7 +172,7 @@ Int_t StFmsFpsMaker::Make(){
     mFmsColl->fillFpsSlat();        //fill StFpsSlat in StFmsCollection
     corrFmsFps();                   //find FMS-FPS correlation, add fps slats info to StFmsPoint 
     mFmsColl->fillFpsAssociation(); //fill StFpsSlat with association info to FmsPoint
-    pid(0);                         //find PID, add to FMS point
+    pid(mPidMethod);                //find PID, add to FMS point
     mFmsColl->fillFmsPointPair();   //find pairs of points
     isolationCone();                //calc sum energy within cones
     if(mQA) fmsFpsAlignment();  
@@ -167,12 +202,11 @@ void StFmsFpsMaker::corrFmsFps(){
 		    if(slatid<0) continue;
 		    float xyz[3],width[3];
 		    mFmsDbMaker->fpsPosition(slatid,xyz,width);	 
-		    static const float mergine=2.0; //add a mergine to both side to get close hit
 		    static const float zvertex=0.0;
 		    float projx=project(x,z,xyz[2],zvertex);
 		    float projy=project(y,z,xyz[2],zvertex);	
 		    float d = distance(projx,projy,xyz[0],xyz[1],width[0],width[1]); 
-		    if(d<mergine){
+		    if(d<mMaxDistanceToAssociate){
 			float mip=mFmsColl->fps(slatid)->mip();
 			unsigned short status=mFmsDbMaker->fpsStatus(slatid);
 			if(status != 0) mip=-9.0; //check FPS status from DB
@@ -193,7 +227,8 @@ void StFmsFpsMaker::corrFmsFps(){
     }
 }
 
-void StFmsFpsMaker::pid(int opt){  //opt: 0=take closest only, 1=closest + 2nd closest, 2=closest, if mip=0 at closest, take 2nd if available
+//opt: 0=take closest only, 1=sum of all associated, 2=closest, if mip=0 at closest, take 2nd if available
+void StFmsFpsMaker::pid(int opt){  
     int npoint=mFmsColl->numberOfPoints();
     StSPtrVecFmsPoint& points = mFmsColl->points();
     
@@ -205,9 +240,9 @@ void StFmsFpsMaker::pid(int opt){  //opt: 0=take closest only, 1=closest + 2nd c
 	    i2=int(points[i]->fpsMip(2,0) + 0.5); 
 	    i3=int(points[i]->fpsMip(3,0) + 0.5);
 	}else if(opt==1){
-	    i1=int(points[i]->fpsMip(1,kFpsNCandidate)+0.5);
-	    i2=int(points[i]->fpsMip(2,kFpsNCandidate)+0.5);
-	    i3=int(points[i]->fpsMip(3,kFpsNCandidate)+0.5);
+	    i1=int(points[i]->fpsMip(1,kFpsNCandidate+2)+0.5);
+	    i2=int(points[i]->fpsMip(2,kFpsNCandidate+2)+0.5);
+	    i3=int(points[i]->fpsMip(3,kFpsNCandidate+2)+0.5);
 	}
 	if(opt==2){
 	    if(i1<=0) i1=int(points[i]->fpsMip(1,1) + 0.5);
@@ -299,16 +334,100 @@ void StFmsFpsMaker::print(){
 	}
 	printf("\n");
     }
+
+    /*
+    //testing StFmsDbMaker::distanceFromEdge()
+    const int nn=100;
+    const float minx=-10.0, maxx=110.0;
+    for(int i=0; i<nn; i++){
+	float x=minx + i*(maxx-minx)/float(nn);
+	float y=20.0;
+	int edge=0;
+	float d=mFmsDbMaker->distanceFromEdge(8,x,y,edge);
+	printf("EDGE i=%4d det=%2d x=%8.2f y=%8.2f d=%8.2f edge=%1d\n",i,8,x,y,d,edge);
+    }
+    for(int i=0; i<nn; i++){
+	float x=minx + i*(maxx-minx)/float(nn);
+	float y=80.0;
+	int edge=0;
+	float d=mFmsDbMaker->distanceFromEdge(8,x,y,edge);
+	printf("EDGE i=%4d det=%2d x=%8.2f y=%8.2f d=%8.2f edge=%1d\n",i,8,x,y,d,edge);
+    }
+    for(int i=0; i<nn; i++){
+	float x=minx + i*(maxx-minx)/float(nn);
+	float y=10.0;
+	int edge=0;
+	float d=mFmsDbMaker->distanceFromEdge(10,x,y,edge);
+	printf("EDGE i=%4d det=%2d x=%8.2f y=%8.2f d=%8.2f edge=%1d\n",i,10,x,y,d,edge);
+    }
+    for(int i=0; i<nn; i++){
+	float x=minx + i*(maxx-minx)/float(nn);
+	float y=40.0;
+	int edge=0;
+	float d=mFmsDbMaker->distanceFromEdge(10,x,y,edge);
+	printf("EDGE i=%4d det=%2d x=%8.2f y=%8.2f d=%8.2f edge=%1d\n",i,10,x,y,d,edge);
+    }
+    */
 }
 
 //QA for Correlation between FMS and FPS and alignment histograms
 void StFmsFpsMaker::fmsFpsAlignment(){
+    StSPtrVecFmsHit& hits = mFmsColl->hits();
+    StSPtrVecFmsCluster& clusters = mFmsColl->clusters();
     StSPtrVecFmsPoint& points = mFmsColl->points(); 
     StSPtrVecFpsSlat& slats = mFmsColl->fpsSlats(); 
     vector<StFmsPointPair*>& pairs = mFmsColl->pointPairs();
+    int nh=mFmsColl->numberOfHits();
+    int nc=mFmsColl->numberOfClusters();
     int np=mFmsColl->numberOfPoints();
     int ns=slats.size();
     int npair=mFmsColl->numberOfPointPairs();
+    float etot[3]={0.0,0.0,0.0};
+
+    //First fill some hists for hits for just QA
+    for(int i=0; i<nh; i++){
+	StFmsHit* hit=hits[i];
+	int det=hit->detectorId();
+	int ch=hit->channel();
+	if(det>=kFmsNorthLargeDetId && det<=kFmsSouthSmallDetId){
+	    mFmsAdc->Fill(float(hit->adc()));
+	    float row=mFmsDbMaker->getRowNumber(det,ch)-0.5;
+	    float col=mFmsDbMaker->getColumnNumber(det,ch)-0.5;
+	    if(mFmsDbMaker->northSouth(det)==0) col*=-1.0;
+	    if(mFmsDbMaker->largeSmall(det)==0){
+		mFmsHitLarge->Fill(col,row,hit->energy());
+	    }else{
+		mFmsHitSmall->Fill(col,row,hit->energy());
+	    }
+	    etot[0]+=hit->energy();
+	}else if(det==kFpsDetId){
+            int q,l,s;
+            mFmsDbMaker->fpsQLSfromSlatId(ch,&q,&l,&s);
+	    if(l>=1 && l<=3) mFpsMip[l-1]->Fill(hit->energy());
+	}
+    }
+	    
+    //Hists for clusters
+    for(int i=0; i<nc; i++){
+	StFmsCluster* clu=clusters[i];
+	int det=clu->detectorId();
+	int ls=mFmsDbMaker->largeSmall(det);
+	float nt=float(clu->nTowers());
+	mNTow[ls]->Fill(nt);
+	mNTowE[ls]->Fill(nt,clu->energy());
+	mSigmax[ls]->Fill(clu->sigmaMax());
+	mSigmin[ls]->Fill(clu->sigmaMin());
+	mSigmaxE[ls]->Fill(clu->energy(),clu->sigmaMax());
+	mCluXY[ls]->Fill(clu->x(),clu->y());
+	int nphoton=clu->nPhotons();
+	float c2=0.0;
+	if(nphoton==0)      {c2=0.0;}
+	else if(nphoton==1) {c2=clu->chi2Ndf1Photon();}
+	else if(nphoton==2) {c2=clu->chi2Ndf2Photon();}
+	else {LOG_INFO << Form("NPHOTON=%d ???",nphoton) << endm;}
+	mChi2[ls]->Fill(c2);
+	etot[1]+=clu->energy();
+    }
     
     //Counting points and slats per quad
     int npoint[kFpsNQuad], nfps[kFpsNQuad][kFpsNLayer];
@@ -324,6 +443,7 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	else if(x< 0.0 && y>=0.0) {q=3;}
 	else if(x< 0.0 && y< 0.0) {q=4;}
 	npoint[q-1]++;
+	etot[2]+=points[i]->energy();
     }  
     //Get # of FPS slats hitted per quad/layer
     for(int i=0; i<ns; i++) {
@@ -334,7 +454,9 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	    nfps[q-1][l-1]++;
 	}
     }
-    
+    if(etot[0]>0.0) mERatio[0]->Fill(etot[1]/etot[0]);
+    if(etot[1]>0.0) mERatio[1]->Fill(etot[2]/etot[1]);
+
     //Loop over FMS points to set up cuts and counts 
     int cut1[NCUT1], n1[NCUT1];
     memset(n1,0,sizeof(n1));
@@ -353,13 +475,20 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	else if(x< 0.0 && y>=0.0) {q=3;}
 	else if(x< 0.0 && y< 0.0) {q=4;}
 	
-	cut1[0]=1; //all
+	cut1[0]=1; //all	
 	if(e>=EThr){
 	    cut1[1]=1;
-	    if(pid2==1)      {cut1[2]=1;}
-	    else if(pid2==2) {cut1[3]=1;}
-	    else if(pid2==3) {cut1[4]=1;}
-	    else             {cut1[5]=1;}
+	    int edgeType;
+	    float distance=mFmsDbMaker->distanceFromEdge(points[i],edgeType); 
+	    if(distance<-0.5 || (mFmsColl->isMergeSmallToLarge() && edgeType==4)){
+		cut1[2]=1;
+		if(pid2==1)      {cut1[5]=1;}
+		else if(pid2==2) {cut1[6]=1;}
+		else if(pid2==3) {cut1[7]=1;}
+		else             {cut1[8]=1;}
+	    }
+	    if(edgeType==4) cut1[3]=1;
+	    if(edgeType==3) cut1[4]=1;
 	}
 	for(int c=0; c<NCUT1; c++){      
 	    if(cut1[c]==0) continue;
@@ -413,16 +542,27 @@ void StFmsFpsMaker::fmsFpsAlignment(){
     for(int i=0; i<npair; i++) {
 	memset(cut2,0,sizeof(cut2));
 	StFmsPointPair* pair=pairs[i];
-	float e0=pair->point(0)->energy();
-	float e1=pair->point(1)->energy();
+	StFmsPoint* p0=pair->point(0);
+	StFmsPoint* p1=pair->point(1);
+	float e0=p0->energy();
+	float e1=p1->energy();
 	int pid=pair->fpsPid();
 	cut2[0]=1; //all
 	if(e0>=EThr && e1>=EThr && pair->energy()>ETotThr){
 	    cut2[1]=1;    
-	    if(pid==11)        {cut2[2]=1;}
-	    else if(pid==22)   {cut2[3]=1;}
-	    else if(pid==33)   {cut2[4]=1;}
-	    else               {cut2[5]=1;}
+	    int edgeType0, edgeType1;
+	    float d0=mFmsDbMaker->distanceFromEdge(p0,edgeType0); 
+	    float d1=mFmsDbMaker->distanceFromEdge(p1,edgeType1); 
+	    if( (d0<-0.5 || (mFmsColl->isMergeSmallToLarge() && edgeType0==4)) &&
+		(d1<-0.5 || (mFmsColl->isMergeSmallToLarge() && edgeType1==4)) ){
+		cut2[2]=1;
+		if     (pid==11)   {cut2[5]=1;}
+		else if(pid==22)   {cut2[6]=1;}
+		else if(pid==33)   {cut2[7]=1;}
+		else               {cut2[8]=1;}
+	    }
+	    if(edgeType0==4 || edgeType1==4) cut2[3]=1;
+	    if(edgeType0==3 || edgeType1==3) cut2[4]=1;
 	}
 	for(int c=0; c<NCUT2; c++){      
 	    if(cut2[c]==0) continue;
@@ -436,8 +576,10 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	    mPm1[c]->Fill(pair->mass());
 	    mPm2[c]->Fill(pair->mass());
 	    mPzgg[c]->Fill(pair->zgg());
+	    mPdgg[c]->Fill(pair->dgg());
 	    mPr30[c]->Fill(pair->coneEnergyFraction(2));
 	    mPr100[c]->Fill(pair->coneEnergyFraction(0));
+	    mPxy[c]->Fill(pair->x(),pair->y());
 	}//loop over cuts
     }//loop over pairs
     for(int c=0; c<NCUT2; c++) mPn[c]->Fill(float(n2[c]));
@@ -459,7 +601,8 @@ void StFmsFpsMaker::readMuDST(){
     int nhits = muFmsColl->numberOfHits();  
     for(int i=0; i<nhits; i++){
 	StMuFmsHit* h = muFmsColl->getHit(i);
-	if(h->detectorId()==kFpsDetId){ //only updating FPS hits... StFmsHitMaker deal with the rest
+	if(h->detectorId()==15) h->setDetectorId(kFpsDetId); //hack to deal with wrong DetId
+	if(h->detectorId()==kFpsDetId) { //only updating FPS hits... StFmsHitMaker deal with the rest
 	    int flag=0;
 	    int ch=h->channel();	  
 	    float gain=mFmsDbMaker->fpsGain(ch);
