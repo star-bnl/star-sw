f46f11e3f9bbbd09e7ca9d7f7e00d7755b560710
James Daniel Brandenburg <jdb12@rice.edu>
2015-10-23 19:27:45 +0000

    Rongrong's update to the StMuMtdHit::tof() function

--- StRoot/StMuDSTMaker/COMMON/StMuMtdHit.cxx
+++ StRoot/StMuDSTMaker/COMMON/StMuMtdHit.cxx
@@ -6,22 +6,23 @@
 
 ClassImp(StMuMtdHit)
 
-StMuMtdHit::StMuMtdHit(const StMtdHit *hit){
+StMuMtdHit::StMuMtdHit(const StMtdHit *hit)
+{
 
-	mBackLeg = hit->backleg();
-	mModule = hit->module();
-	mCell  = hit->cell();
-    mLeadingEdgeTime = hit->leadingEdgeTime();
-    mTrailingEdgeTime = hit->trailingEdgeTime();
+  mBackLeg = hit->backleg();
+  mModule = hit->module();
+  mCell  = hit->cell();
+  mLeadingEdgeTime = hit->leadingEdgeTime();
+  mTrailingEdgeTime = hit->trailingEdgeTime();
 	
-    mIdTruth  = hit->idTruth();  
-	mQuality  = hit->qaTruth(); 
-	if(hit->associatedTrack()!=0) mTrackKey = hit->associatedTrack()->key();	
-	else mTrackKey = 0;
+  mIdTruth  = hit->idTruth();  
+  mQuality  = hit->qaTruth(); 
+  if(hit->associatedTrack()!=0) mTrackKey = hit->associatedTrack()->key();	
+  else mTrackKey = 0;
 	
-	// rongrong
-	mIndex2Primary = -1;
-	mIndex2Global = -1;
+  // rongrong
+  mIndex2Primary = -1;
+  mIndex2Global = -1;
 }
 
 int
@@ -41,6 +42,9 @@ StMuMtdHit::trailingEdgeTime() const { return mTrailingEdgeTime; }
 
 pair<double,double>
 StMuMtdHit::tot() const { return pair<double,double>(mTrailingEdgeTime.first - mLeadingEdgeTime.first, mTrailingEdgeTime.second - mLeadingEdgeTime.second); }
+
+double 
+StMuMtdHit::tof() const { return 0.5*(mLeadingEdgeTime.first+mLeadingEdgeTime.second); }
 
 short 
 StMuMtdHit::associatedTrackKey() const { return mTrackKey; }
