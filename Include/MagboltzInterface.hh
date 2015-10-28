92f62cfb42dd67df82aefb2a8db316c7be0bfe3d
Heinrich Schindler <Heinrich.Schindler@cern.ch>
2015-10-28 22:22:17 +0000

    Undo changes to MagboltzInterface

--- Include/MagboltzInterface.hh
+++ Include/MagboltzInterface.hh
@@ -105,46 +105,29 @@ void gasmix_(long long* ngs, double* q, double* qin, long long* nin, double* e,
              double* ei, char* name, double* virl, double* eb, double* peqel,
              double* peqin, double* penfra, long long* kel, long long* kin,
              double* qion, double* peqion, double* eion, long long* nion,
-             long long* nc0, double* ec0, double* wk, double* efl, 
-             long long* ng1, double* eg1, long long* ng2, double* eg2,  
              char scrpt[260][50]);
 
 void setup1_();
-void setupt1_();
 
 void mixer_();
-void mixert_();
 
 void elimit_(long long* ielow);
 void elimitb_(long long* ielow);
 void elimitc_(long long* ielow);
-void elimitt_(long long* ielow);
-void elimitbt_(long long* ielow);
-void elimitct_(long long* ielow);
 
 void monte_();
 void montea_();
 void monteb_();
 void montec_();
-void montet_();
-void monteat_();
-void montebt_();
-void montect_();
 
 void alpcalc_();
 void alpclca_();
 void alpclcb_();
 void alpclcc_();
-void alpcalct_();
-void alpclcat_();
-void alpclcbt_();
-void alpclcct_();
 
 void prnter_();
 void output_();
 void output2_();
-void outputt_();
-void output2t_();
 }
 }
 }
