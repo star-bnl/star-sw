6f2a183d8c329dc756a319101eed15200f0bd966
Yuri Fisyak <fisyak@bnl.gov>
2015-11-02 16:51:22 -0500

    Merge
    Former-commit-id: 7b6f0e374ca13865012be995bc9359e725796f7e

---
+++ StarDb/starsim/geometry.y2015b.C
@@ -0,0 +1,9 @@
+
+TDataSet *CreateTable() {
+  if (!gROOT->GetClass("St_geant_Maker")) return 0;
+  St_geant_Maker *geant = (St_geant_Maker *) StMaker::GetChain()->Maker("geant");
+  if (! geant) return 0;
+  TDataSet *geom = new TDataSet("geometry");
+  geant->Do("detp geom y2015b");
+  return geom;
+};
