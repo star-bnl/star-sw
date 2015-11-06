ad7e19e9c4dad08fb2dced549b191c6796dd9869
Yuri Fisyak <fisyak@bnl.gov>
2015-11-06 15:56:51 -0500

    new ast to rz
    Former-commit-id: 64eb30fd2279d23889c537e6eea6f7c0fd461f5e

--- StRoot/macros/Ast2Rz.C
+++ StRoot/macros/Ast2Rz.C
@@ -2,7 +2,8 @@ void Ast2Rz(const Char_t *vers="y2005x", const Char_t *geom = "") {
   gROOT->LoadMacro("bfc.C");
   //  TString cmd("bfc(0,\"gstar,useXgeom,nodefault,");
   //  TString cmd("bfc(0,\"gstar,nodefault,StEvent,");
-  TString cmd("bfc(0,\"gstar,ForceGeometry,nodefault,");
+  //  TString cmd("bfc(0,\"gstar,ForceGeometry,nodefault,");
+  TString cmd("bfc(0,\"geant,nodefault,");
   cmd += vers;
   cmd += ",";
   cmd += geom;
@@ -16,6 +17,8 @@ void Ast2Rz(const Char_t *vers="y2005x", const Char_t *geom = "") {
   geant->InitRun(1);
   TString cmd("grfile"); 
   geant->Do(cmd.Data());
+  //  geant->Do("FORTRAN/CLOSE 30");
+#if 1
   ofstream out;
   TString fOut("Geometry.");
   fOut += vers;
@@ -27,4 +30,6 @@ void Ast2Rz(const Char_t *vers="y2005x", const Char_t *geom = "") {
   out << "  return CreateGeometry(\"" << vers << "\");" << endl;
   out << "}" << endl;
   out.close(); 
+  if (gSystem->Exec(Form("mv geom.rz %s.rz",vers))) gSystem->Exit(1);
+#endif
 }
