e917065d5a02043f3faf761941ed9bbd68f21c3c
Victor Perevoztchikov <perev@bnl.gov>
2015-11-13 00:25:16 +0000

    Added changable constants and FTS

--- StRoot/StMuDSTMaker/COMMON/StMuFilter.cxx
+++ StRoot/StMuDSTMaker/COMMON/StMuFilter.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StMuFilter.cxx,v 1.5 2006/08/28 17:07:15 fisyak Exp $
+ * $Id: StMuFilter.cxx,v 1.6 2015/11/13 00:25:16 perev Exp $
  * Author: Frank Laue, BNL, laue@bnl.gov
  *
  ***************************************************************************/
@@ -12,19 +12,29 @@
 #include "StEvent/StContainers.h"
 #include "StEvent/StDedxPidTraits.h"
 #include "TMath.h"
+#define __MIN_HITS__     11
 #define __MIN_HITS_TPC__ 11
 #define __MIN_HITS_FTPC__ 5
+#define __MIN_HITS_FTS__  3
 
 
 ClassImp(StMuFilter)
+StMuFilter::StMuFilter() 
+{
+  mMinHits 	= __MIN_HITS__;
+  mMinTpcHits 	= __MIN_HITS_TPC__;
+  mMinFTpcHits 	= __MIN_HITS_FTPC__;
+  mMinFtsHits 	= __MIN_HITS_FTS__;
+}
 
-bool StMuFilter::accept( const StEvent* e) { cout << "StMuFilter::accept( const StEvent* e) not overwritten, returning true" << endl; return true;}
-bool StMuFilter::accept( const StV0Vertex* v) { cout << "StMuFilter::accept(const StV0Vertex* v) not overwritten, returning true" << endl; return true;}
-bool StMuFilter::accept( const StXiVertex* x) { cout << "StMuFilter::accept(const StXiVertex* x) not overwritten, returning true" << endl; return true;}
+
+bool StMuFilter::accept( const StEvent*      e) { cout << "StMuFilter::accept( const StEvent* e) not overwritten, returning true" << endl; return true;}
+bool StMuFilter::accept( const StV0Vertex*   v) { cout << "StMuFilter::accept(const StV0Vertex* v) not overwritten, returning true" << endl; return true;}
+bool StMuFilter::accept( const StXiVertex*   x) { cout << "StMuFilter::accept(const StXiVertex* x) not overwritten, returning true" << endl; return true;}
 bool StMuFilter::accept( const StKinkVertex* k) { cout << "StMuFilter::accept(const StKinkVertex* k) not overwritten, returning true" << endl; return true;}
-bool StMuFilter::accept( const StV0MuDst* v) { cout << "StMuFilter::accept(const StV0MuDst* v) not overwritten, returning true" << endl; return true;}
-bool StMuFilter::accept( const StXiMuDst* x) { cout << "StMuFilter::accept(const StXiMuDst* x) not overwritten, returning true" << endl; return true;}
-bool StMuFilter::accept( const StKinkMuDst* k) { cout << "StMuFilter::accept(const StKinkMuDst* k) not overwritten, returning true" << endl; return true;}
+bool StMuFilter::accept( const StV0MuDst*    v) { cout << "StMuFilter::accept(const StV0MuDst* v) not overwritten, returning true" << endl; return true;}
+bool StMuFilter::accept( const StXiMuDst*    x) { cout << "StMuFilter::accept(const StXiMuDst* x) not overwritten, returning true" << endl; return true;}
+bool StMuFilter::accept( const StKinkMuDst*  k) { cout << "StMuFilter::accept(const StKinkMuDst* k) not overwritten, returning true" << endl; return true;}
 
 
 
@@ -44,9 +54,12 @@ bool StMuFilter::accept(const StTrack* track) {
 
   if ( !track->detectorInfo() ) return false;
   if ( TMath::Abs(track->flag())%100 == 11) return true;
-  if ( track->detectorInfo()->numberOfPoints(kTpcId)<__MIN_HITS_TPC__ &&
-       track->detectorInfo()->numberOfPoints(kFtpcWestId)<__MIN_HITS_FTPC__ && 
-       track->detectorInfo()->numberOfPoints(kFtpcEastId)<__MIN_HITS_FTPC__ 
+  if ( track->detectorInfo()->numberOfPoints(kTpcId     )	<mMinTpcHits 	&&
+#ifdef kFtsIdentifier
+       track->detectorInfo()->numberOfPoints(kFtsId     )	<mMinFtsHits 	&& 
+#endif
+       track->detectorInfo()->numberOfPoints(kFtpcWestId)	<mMinFTpcHits 	&& 
+       track->detectorInfo()->numberOfPoints(kFtpcEastId)	<mMinFTpcHits  
        ) return false;
 
   return true;
@@ -56,6 +69,9 @@ bool StMuFilter::accept(const StTrack* track) {
 /***************************************************************************
  *
  * $Log: StMuFilter.cxx,v $
+ * Revision 1.6  2015/11/13 00:25:16  perev
+ * Added changable constants and FTS
+ *
  * Revision 1.5  2006/08/28 17:07:15  fisyak
  * Don't applay no. fit points cut for short tracks pointing to EEMC (+x11)
  *
