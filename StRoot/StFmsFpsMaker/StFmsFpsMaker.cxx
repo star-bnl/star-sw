356c5a7d2e90827c3a55b167205593fad970e67b
Akio Ogawa <akio@bnl.gov>
2015-12-08 17:00:07 +0000

    updates

--- StRoot/StFmsFpsMaker/StFmsFpsMaker.cxx
+++ StRoot/StFmsFpsMaker/StFmsFpsMaker.cxx
@@ -1,8 +1,11 @@
 // \class StFmsFpsMaker
 // \author Akio Ogawa
 //
-//  $Id: StFmsFpsMaker.cxx,v 1.5 2015/10/29 21:22:01 akio Exp $
+//  $Id: StFmsFpsMaker.cxx,v 1.6 2015/12/08 17:00:03 akio Exp $
 //  $Log: StFmsFpsMaker.cxx,v $
+//  Revision 1.6  2015/12/08 17:00:03  akio
+//  updates
+//
 //  Revision 1.5  2015/10/29 21:22:01  akio
 //  more cuts for QA
 //
@@ -38,9 +41,10 @@
 #include "TH1F.h"
 #include "TH2F.h"
 
+//static const float EThr=1.0;
+//static const float ETotThr1=2.0;
 static const float EThr=5.0;
 static const float ETotThr1=20.0;
-static const float ETotThr2=30.0;
 
 inline float project(float x, float z, float zp, float vz){
     return x/(z-vz)*(zp-vz);
@@ -66,6 +70,42 @@ float distance(float x, float y, float x0, float y0, float xw, float yw){
     return sqrt( pow(fabs(xx)-xw/2.0,2.0) + pow(fabs(yy)-yw/2.0,2.0) ); //outside both ways, return dist from corner
 }
 
+static const int NTRG=12;  //123=FMS-sm-bs123,456=FMS-lg-bs123,7=FMS-DiBS,8910=FMS-JP012,11=FMS-DiJP,13=LED
+static const char *CTRG[NTRG] = {"SmBS1","SmBS2","SmBS3",
+                                 "LgBS1","LgBS2","LgBS3","DiBS",
+				 "JP2","JP1","JP0","DiJP","LED"};
+int getFmsTrigId(const StTriggerId& trgid, int print=1){
+    const int TIDBASE=480800;
+    const int NBEAM=4;
+    const int MAXVERSION=3;
+    int trig=0;
+    //    LOG_INFO << trgid <<endm;
+    LOG_INFO << "TRGID = ";
+    for(int k=0; k<NBEAM; k++){
+	for(int j=0; j<MAXVERSION; j++){
+	    for(int i=1; i<=NTRG; i++){
+		int l=i;
+		if(i==12) l=13;
+		int id=TIDBASE + 10000*k + 20*j + l; 
+		if(trgid.isTrigger(id)){
+		    trig |= (1<<(i-1));
+		    LOG_INFO << CTRG[i-1] << " ";
+		}		
+	    }      
+	}
+    }
+    //include none-production id for now.... could be dangerous
+    static int nonProdId[NTRG]={42,43,44,45,46,47,48,49,50,51,52,53};
+    for(int i=0; i<NTRG; i++){
+	if(trgid.isTrigger(nonProdId[i])){
+	    trig |= (1<<i);
+	    LOG_INFO << CTRG[i] << " ";
+	}
+    }
+    LOG_INFO << endm;
+    return trig;
+}
+
 ClassImp(StFmsFpsMaker);
 
 StFmsFpsMaker::StFmsFpsMaker(const Char_t* name):
@@ -89,10 +129,15 @@ Int_t StFmsFpsMaker::Init(){
     
     mERatio[0]= new TH1F("ERatioCluster-Hit","ERatioCH",50.0,0.0,1.2);
     mERatio[1]= new TH1F("ERatioPoint-Cluster","ERatioPC",50.0,0.0,1.2);
+    mBC       = new TH1F("BC","BC",120,0.0,120.0);
+    mTrig[0]  = new TH1F("Trig","Trig",    13,0.0,13.0);
+    mTrig[1]  = new TH1F("TrigAg","TrigAg",13,0.0,13.0);
 
     mFmsAdc      = new TH1F("FmsAdc","FmsAdc",4096,0.0,4096.0);
-    mFmsHitLarge = new TH2F("FmsHitLarge","FmsHitLarge",34,-17.0,17.0,34,0.0,34.0);
-    mFmsHitSmall = new TH2F("FmsHitSmall","FmsHitSmall",24,-12.0,12.0,24,0.0,24.0);
+    mFmsHitLarge[0] = new TH2F("FmsHitLarge","FmsHitLarge",34,-17.0,17.0,34,0.0,34.0);
+    mFmsHitSmall[0] = new TH2F("FmsHitSmall","FmsHitSmall",24,-12.0,12.0,24,0.0,24.0);
+    mFmsHitLarge[1] = new TH2F("FmsHitLargeAg","FmsHitLargeAg",34,-17.0,17.0,34,0.0,34.0);
+    mFmsHitSmall[1] = new TH2F("FmsHitSmallAg","FmsHitSmallAg",24,-12.0,12.0,24,0.0,24.0);
     mFpsMip[0]   = new TH1F("FpsMipL1","FpsMipL1",100,0.0,5.0);
     mFpsMip[1]   = new TH1F("FpsMipL2","FpsMipL2",100,0.0,5.0);
     mFpsMip[2]   = new TH1F("FpsMipL3","FpsMipL3",180,0.0,90.0);
@@ -107,10 +152,18 @@ Int_t StFmsFpsMaker::Init(){
     mSigmin[1]= new TH1F("SigMinS","SigMinS",100.0,0.0,4.0);
     mSigmaxE[0]= new TH2F("SigMaxEL","SigMaxEL",100.0,0.0,100.0,100,0.0,2.0);
     mSigmaxE[1]= new TH2F("SigMaxES","SigMaxES",100.0,0.0,100.0,100,0.0,2.0);
-    mChi2[0]  = new TH1F("Chi2L","Chi2L",100.0,0.0,2000.0);
-    mChi2[1]  = new TH1F("Chi2S","Chi2S",100.0,0.0,2000.0);
-    mCluXY[0]= new TH2F("CluXYL","CluXYL",100.0,0.0,17*5.812,100,0.0,34.0*5.812);
-    mCluXY[1]= new TH2F("CluXYS","CluXYS",100.0,0.0,24*3.822,100,0.0,12.0*3.875);
+    mChi2[0]  = new TH1F("Chi2L","Chi2L",200.0,0.0,200.0);
+    mChi2[1]  = new TH1F("Chi2S","Chi2S",200.0,0.0,200.0);
+    mCluXY[0][0]= new TH2F("CluXYL","CluXYL",100.0,-17.0,17.0,100,0.0,34.0);
+    mCluXY[1][0]= new TH2F("CluXYS","CluXYS",100.0,-12.0,12.0,100,0.0,24.0);
+    mCluXY[0][1]= new TH2F("CluXYLAg","CluXYLAg",100.0,-17.0,17.0,100,0.0,34.0);
+    mCluXY[1][1]= new TH2F("CluXYSAg","CluXYSAg",100.0,-12.0,12.0,100,0.0,24.0);
+    mCluEta[0]=new TH1F("CluEta0","CluEta0",100,2.5,4.5);
+    mCluEta[1]=new TH1F("CluEta1","CluEta1",100,2.5,4.5);
+    mCluEta[2]=new TH1F("CluEta2","CluEta2",100,2.5,4.5);
+    mCluEta[3]=new TH1F("CluEta3","CluEta3",100,2.5,4.5);
+    mCluEta[4]=new TH1F("CluEta4","CluEta4",100,2.5,4.5);
+    mCluEta[5]=new TH1F("CluEta5","CluEta5",100,2.5,4.5);
 
     for(int cut=0; cut<NCUT1; cut++){
 	for(int q=0; q<kFpsNQuad; q++){
@@ -126,7 +179,7 @@ Int_t StFmsFpsMaker::Init(){
 		mHd[q][l][cut]=new TH1F(c,c,100,-50.0,50.0);
 	    }//loop over layer
 	}//loop over quad
-	sprintf(c,"NP_c%d",cut);   mHn  [cut]=new TH1F(c,c, 50,0.0,50.0);
+	sprintf(c,"NP_c%d",cut);   mHn  [cut]=new TH1F(c,c,100,0.0,100.0);
 	sprintf(c,"e_c%d",cut);    mHene[cut]=new TH1F(c,c,100,0.0,100.0);
 	sprintf(c,"elo_c%d",cut);  mHelo[cut]=new TH1F(c,c,100,0.0,4.0);
 	sprintf(c,"pt_c%d",cut);   mHpt [cut]=new TH1F(c,c,100,0.0,10.0);
@@ -135,6 +188,8 @@ Int_t StFmsFpsMaker::Init(){
 	sprintf(c,"phi_c%d",cut);  mHphi[cut]=new TH1F(c,c,100,-3.2,3.2);
 	sprintf(c,"x_c%d",cut);    mHx  [cut]=new TH1F(c,c,100,-100.0,100.0);
 	sprintf(c,"y_c%d",cut);    mHy  [cut]=new TH1F(c,c,100,-100.0,100.0);
+	sprintf(c,"dxl_c%d",cut);  mHdxL[cut]=new TH1F(c,c,120,-0.1,1.1);
+	sprintf(c,"dxs_c%d",cut);  mHdxS[cut]=new TH1F(c,c,120,-0.1,1.1);
 	sprintf(c,"xy_c%d",cut);   mHxy [cut]=new TH2F(c,c,200,-100.0,100.0,200,-100.0,100.0);
 	sprintf(c,"pid_c%d",cut);  mHpid[cut]=new TH1F(c,c,41,-0.5,40.5);
 	sprintf(c,"pid2_c%d",cut); mHpid2[cut]=new TH1F(c,c,5,-0.5,4.5);
@@ -162,6 +217,7 @@ Int_t StFmsFpsMaker::Init(){
 
 Int_t StFmsFpsMaker::Finish(){
     if(mQA){
+	LOG_INFO << Form("Writing and closing %s",mFilename) << endm;
 	mFile->Write();
 	mFile->Close();
     }
@@ -171,6 +227,10 @@ Int_t StFmsFpsMaker::Finish(){
 Int_t StFmsFpsMaker::Make(){
     StEvent* event = (StEvent*)GetInputDS("StEvent");
     if(!event) {LOG_ERROR << "StFmsFpsMaker::Make did not find StEvent"<<endm; return kStErr;}
+    if(event->triggerData()) mBunch = event->triggerData()->bunchId7Bit();
+    if(event->triggerIdCollection() && event->triggerIdCollection()->nominal()){
+	mTrigger = getFmsTrigId(*(event->triggerIdCollection()->nominal()));
+    }
     mFmsColl = event->fmsCollection();
     if(!mFmsColl) {LOG_ERROR << "StFmsFpsMaker::Make did not find StEvent->FmsCollection"<<endm; return kStErr;}
     if(mReadMuDST) readMuDST();
@@ -389,6 +449,20 @@ void StFmsFpsMaker::fmsFpsAlignment(){
     int npair=mFmsColl->numberOfPointPairs();
     float etot[3]={0.0,0.0,0.0};
 
+    //Bunch counter and AG
+    int ag=0;
+    if     (mBunch>=30  && mBunch< 40) {ag=1;}
+    else if(mBunch>=110 && mBunch<120) {ag=2;}
+    mBC->Fill(float(mBunch));
+    if(ag==0) mTrig[0]->Fill(0);
+    if(ag==1) mTrig[1]->Fill(0);
+    for(int i=0; i<NTRG; i++){
+	if(mTrigger & (1<<i)){
+	    if(ag==0) mTrig[0]->Fill(float(i+1));
+	    if(ag==1) mTrig[1]->Fill(float(i+1));
+	}
+    }
+
     //First fill some hists for hits for just QA
     for(int i=0; i<nh; i++){
 	StFmsHit* hit=hits[i];
@@ -400,9 +474,11 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	    float col=mFmsDbMaker->getColumnNumber(det,ch)-0.5;
 	    if(mFmsDbMaker->northSouth(det)==0) col*=-1.0;
 	    if(mFmsDbMaker->largeSmall(det)==0){
-		mFmsHitLarge->Fill(col,row,hit->energy());
+		if(ag==0) mFmsHitLarge[0]->Fill(col,row,hit->energy());		
+		if(ag==1) mFmsHitLarge[1]->Fill(col,row,hit->energy());		
 	    }else{
-		mFmsHitSmall->Fill(col,row,hit->energy());
+		if(ag==0) mFmsHitSmall[0]->Fill(col,row,hit->energy());
+		if(ag==1) mFmsHitSmall[1]->Fill(col,row,hit->energy());
 	    }
 	    etot[0]+=hit->energy();
 	}else if(det==kFpsDetId){
@@ -415,6 +491,8 @@ void StFmsFpsMaker::fmsFpsAlignment(){
     //Hists for clusters
     for(int i=0; i<nc; i++){
 	StFmsCluster* clu=clusters[i];
+	etot[1]+=clu->energy();
+	if(clu->energy()<5.0) continue;
 	int det=clu->detectorId();
 	int ls=mFmsDbMaker->largeSmall(det);
 	float nt=float(clu->nTowers());
@@ -423,7 +501,13 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	mSigmax[ls]->Fill(clu->sigmaMax());
 	mSigmin[ls]->Fill(clu->sigmaMin());
 	mSigmaxE[ls]->Fill(clu->energy(),clu->sigmaMax());
-	mCluXY[ls]->Fill(clu->x(),clu->y());
+	if(det%2==0){
+	    if(ag==0) mCluXY[ls][0]->Fill(-clu->x(),clu->y());	
+	    if(ag==1) mCluXY[ls][1]->Fill(-clu->x(),clu->y());	
+	}else{
+	    if(ag==0) mCluXY[ls][0]->Fill(clu->x(),clu->y());	
+	    if(ag==1) mCluXY[ls][1]->Fill(clu->x(),clu->y());	
+	}	   
 	int nphoton=clu->nPhotons();
 	float c2=0.0;
 	if(nphoton==0)      {c2=0.0;}
@@ -431,7 +515,10 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	else if(nphoton==2) {c2=clu->chi2Ndf2Photon();}
 	else {LOG_INFO << Form("NPHOTON=%d ???",nphoton) << endm;}
 	mChi2[ls]->Fill(c2);
-	etot[1]+=clu->energy();
+	int ctgy=clu->category();
+	int type=(nphoton-1)*3 + ctgy;
+	//cout << Form("NP=%1d Ctgry=%1d Type=%d\n",nphoton,ctgy,type);       
+	mCluEta[type]->Fill(clu->fourMomentum().pseudoRapidity());	
     }
     
     //Counting points and slats per quad
@@ -483,20 +570,28 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	cut1[0]=1; //all	
 	int edgeType;
 	float distance=mFmsDbMaker->distanceFromEdge(points[i],edgeType); 
-	//	if(distance<-0.51 || (mFmsColl->isMergeSmallToLarge() && edgeType==4)){
-	if(distance<-0.51 || edgeType==4){
+	/*
+	if(points[i]->detectorId()<=9)  cut1[1]=1;
+	if(points[i]->detectorId()>=10) cut1[2]=1;
+	if(points[i]->detectorId()<=9 && edgeType==4)  cut1[3]=1;
+	if(points[i]->detectorId()>=10 && edgeType==4)  cut1[4]=1;
+	*/
+	if(e>=EThr){
 	    cut1[1]=1;
-	    if(e>=EThr){
+	    if(distance<-0.51 || edgeType==4){
 		cut1[2]=1;
-		if(pid2==1)      {cut1[5]=1;}
-		else if(pid2==2) {cut1[6]=1;}
-		else if(pid2==3) {cut1[7]=1;}
-		else             {cut1[8]=1;}
+		if(distance<-0.51 || (mFmsColl->isMergeSmallToLarge() && edgeType==4)){
+		    if(ag==0) {
+			cut1[3]=1;
+			if(pid2==1)      {cut1[6]=1;}
+			else if(pid2==2) {cut1[7]=1;}
+			else if(pid2==3) {cut1[8]=1;}
+			else             {cut1[9]=1;}
+		    }
+		    if(ag==1) cut1[5]=1;
+		}
 	    }
-	}
-	if(edgeType==4 && distance>=-0.51){
-	    cut1[3]=1;
-	    if(e>=EThr) cut1[4]=1;
+	    if(edgeType==4 && distance>=-0.51) cut1[4]=1;
 	}
 	for(int c=0; c<NCUT1; c++){      
 	    if(cut1[c]==0) continue;
@@ -510,6 +605,18 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	    mHx[c]->Fill(x);
 	    mHy[c]->Fill(y);
 	    mHxy[c]->Fill(x,y);
+	    int det=points[i]->detectorId();
+	    float localx=points[i]->x()/mFmsDbMaker->getXWidth(det);
+	    float localy=points[i]->y()/mFmsDbMaker->getYWidth(det);
+	    float dlx=localx-int(localx);
+	    float dly=localy-int(localy);
+	    if(points[i]->detectorId()<=9){		
+		mHdxL[c]->Fill(dlx);
+		mHdxL[c]->Fill(dly);
+	    }else{
+		mHdxS[c]->Fill(dlx);
+		mHdxS[c]->Fill(dly);
+	    }
 	    mHpid[c]->Fill(float(pid));
 	    mHpid2[c]->Fill(float(pid2));
 	}//loop over cuts
@@ -559,28 +666,26 @@ void StFmsFpsMaker::fmsFpsAlignment(){
 	int edgeType0, edgeType1;
 	float d0=mFmsDbMaker->distanceFromEdge(p0,edgeType0); 
 	float d1=mFmsDbMaker->distanceFromEdge(p1,edgeType1); 
-	//	if( (d0<-0.51 || (mFmsColl->isMergeSmallToLarge() && edgeType0==4)) &&
-	//          (d1<-0.51 || (mFmsColl->isMergeSmallToLarge() && edgeType1==4)) ){
-	if( (d0<-0.51 || edgeType0==4) &&
-	    (d1<-0.51 || edgeType1==4) ){
+	if(e0>=EThr && e1>=EThr && pair->energy()>ETotThr1){
 	    cut2[1]=1;    
-	    if(e0>=EThr && e1>=EThr && pair->energy()>ETotThr1){
-		LOG_DEBUG << Form("EdgeType=%1d %1d, d=%20.15f %20.15f x=%20.15f %20.15f xx=%20.15f %20.15f\n",
-				  edgeType0, edgeType1,d0,d1,
-				  p0->x(),p1->x(),p0->XYZ().x(),p1->XYZ().x());
+	    if( (d0<-0.51 || edgeType0==4) &&
+		(d1<-0.51 || edgeType1==4) ){
 		cut2[2]=1;
-		if(pair->energy()>ETotThr2) {cut2[9]=1;}
-		if     (pid==11)   {cut2[5]=1;}
-		else if(pid==22)   {cut2[6]=1;}
-		else if(pid==33)   {cut2[7]=1;}
-		else               {cut2[8]=1;}
+		if( (d0<-0.51 || (mFmsColl->isMergeSmallToLarge() && edgeType0==4)) &&
+		    (d1<-0.51 || (mFmsColl->isMergeSmallToLarge() && edgeType1==4)) ){
+		    if(ag==0){
+			cut2[3]=1;
+			if     (pid==11)   {cut2[6]=1;}
+			else if(pid==22)   {cut2[7]=1;}
+			else if(pid==33)   {cut2[8]=1;}
+			else               {cut2[9]=1;}
+		    }
+		    if(ag==1) cut2[5]=1;		    
+		    if(n1[2]==2) cut2[10]=1;
+		}
 	    }
-	}
-	if( (edgeType0==4 && d0>=-0.51) || (edgeType1==4 && d1>=-0.51) ){
-	    cut2[3]=1;
-	    if(e0>=EThr && e1>=EThr && pair->energy()>ETotThr1){
+	    if( (edgeType0==4 && d0>=-0.51) || (edgeType1==4 && d1>=-0.51) ){
 		cut2[4]=1;
-		if(pair->energy()>ETotThr2) cut2[10]=1;
 	    }
 	}
 	for(int c=0; c<NCUT2; c++){      
@@ -614,6 +719,9 @@ void StFmsFpsMaker::readMuDST(){
     }
     StMuDst* mudst = (StMuDst*)GetInputDS("MuDst");
     if(!mudst){ LOG_INFO<<"StFmsFpsMaker::readMuDST found no Mudst"<<endm; return;}
+    mBunch = mudst->event()->triggerData()->bunchId7Bit(); 
+    mTrigger = getFmsTrigId(mudst->event()->triggerIdCollection().nominal());
+    
     StMuFmsCollection* muFmsColl = StMuDst::muFmsCollection();
     if(!muFmsColl){ LOG_INFO<<"StFmsFpsMaker::readMuDST found no StMuFmsCollection in MuDST"<<endm; return;}
     int nfps=0;
