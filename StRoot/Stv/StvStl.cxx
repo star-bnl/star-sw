a6354f53030032cc9b3998e06a2e8a42d08a2df7
perev <>
2015-12-12 01:58:25 +0000

    added idTruth into print method

--- StRoot/Stv/StvStl.cxx
+++ StRoot/Stv/StvStl.cxx
@@ -1,4 +1,4 @@
-// $Id: StvStl.cxx,v 1.5 2015/10/07 02:52:21 perev Exp $
+// $Id: StvStl.cxx,v 1.6 2015/12/12 01:58:25 perev Exp $
 //
 //
 // Class StvStl some stl containers for Stv objects
@@ -14,12 +14,16 @@ StvPoints &StvPoints::operator+=(const double  add[3])
 { push_back(add[0]); push_back(add[1]); push_back(add[2]);return *this;}
 
 //_____________________________________________________________________________
-inline void StvHits::Print(const char *txt)
+void StvHits::Print(const char *txt)
 {
   if (txt && txt[0]) printf("StvHits(%s)\n",txt);
   for (int i=0;i<(int)size();i++) {
-    const float *x = (*this)[i]->x();
+    auto *stvHit   = (*this)[i];
+    const float *x = stvHit->x();
     double r = sqrt(x[0]*x[0]+x[1]*x[1]);
-    printf("%3d - Rxy=%g x=%g y=%g z=%g\n",i,r,x[0],x[1],x[2]);
+    int idTru = stvHit->idTru();
+
+
+    printf("%3d - idTru = %d Rxy=%g x=%g y=%g z=%g\n",i,idTru,r,x[0],x[1],x[2]);
   }
 }
