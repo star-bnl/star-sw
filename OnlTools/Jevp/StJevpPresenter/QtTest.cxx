1e98418c07c409e99991bc7482bc4279348f76f6
evpops <>
2016-01-14 03:38:45 +0000

    updates

--- OnlTools/Jevp/StJevpPresenter/QtTest.cxx
+++ OnlTools/Jevp/StJevpPresenter/QtTest.cxx
@@ -16,107 +16,105 @@ int testCurrLine = 0;
 
 static void sigHandler(int arg, siginfo_t *sig, void *v)
 {
-  static char str[255];
+    static char str[255];
   
-  if(arg == 17) {
-    LOG(DBG, "Got signal 17: ignoring");
-    return;
-  }
+    if(arg == 17) {
+	LOG(DBG, "Got signal 17: ignoring");
+	return;
+    }
 
-  sprintf(str,"Signal %d: shutting down! (file=%s line=%d)",arg,testCurrFile, testCurrLine);
+    sprintf(str,"Signal %d: shutting down! (file=%s line=%d)",arg,testCurrFile, testCurrLine);
 
-  LOG(ERR, "%s", str);
+    LOG(ERR, "%s", str);
 
-  exit(-1);
+    exit(-1);
 }
 
 static void catchSignals(void)
 {
-  int i ;
-  struct sigaction act ;
+    int i ;
+    struct sigaction act ;
   
-  LOG(DBG, "catching signals");
+    LOG(DBG, "catching signals");
 
-  // hook signals to my default...
-  act.sa_sigaction = sigHandler ;
-  act.sa_flags = SA_SIGINFO ;
+    // hook signals to my default...
+    act.sa_sigaction = sigHandler ;
+    act.sa_flags = SA_SIGINFO ;
   
-  for(i=0;i<37;i++) {	// hook'em all!
-    sigaction(i,&act,NULL) ;
-  }
+    for(i=0;i<37;i++) {	// hook'em all!
+	sigaction(i,&act,NULL) ;
+    }
   
-  return ;
+    return ;
 }
 
 int QtTest::main(char *args) 
 {
-  char *argv[50];
-  int argc=0;
+    char *argv[50];
+    int argc=0;
 
-  gSystem->ResetSignal(kSigChild);
-  gSystem->ResetSignal(kSigBus);
-  gSystem->ResetSignal(kSigSegmentationViolation);
-  gSystem->ResetSignal(kSigIllegalInstruction);
-  gSystem->ResetSignal(kSigSystem);
-  gSystem->ResetSignal(kSigPipe);
-  gSystem->ResetSignal(kSigAlarm);
-  gSystem->ResetSignal(kSigUrgent);
-  gSystem->ResetSignal(kSigFloatingException);
-  gSystem->ResetSignal(kSigWindowChanged);
+    gSystem->ResetSignal(kSigChild);
+    gSystem->ResetSignal(kSigBus);
+    gSystem->ResetSignal(kSigSegmentationViolation);
+    gSystem->ResetSignal(kSigIllegalInstruction);
+    gSystem->ResetSignal(kSigSystem);
+    gSystem->ResetSignal(kSigPipe);
+    gSystem->ResetSignal(kSigAlarm);
+    gSystem->ResetSignal(kSigUrgent);
+    gSystem->ResetSignal(kSigFloatingException);
+    gSystem->ResetSignal(kSigWindowChanged);
   
-  catchSignals();
+    catchSignals();
 
-  CP;
+    CP;
   
-  rtsLogOutput(RTS_LOG_STDERR);
-  //rtsLogAddDest((char *)"130.199.60.86", 8004);
+    rtsLogOutput(RTS_LOG_STDERR);
+    //rtsLogAddDest((char *)"130.199.60.86", 8004);
 
-  CP;
+    CP;
 
-  argv[0] = strtok(args, " ");
-  argc++;
-
-  char *next;
-  while((next = strtok(NULL, " "))) {
-    argv[argc] = next;
+    argv[0] = strtok(args, " ");
     argc++;
-  }
+
+    char *next;
+    while((next = strtok(NULL, " "))) {
+	argv[argc] = next;
+	argc++;
+    }
  
-  CP;
-  return _main(argc, argv);
+    CP;
+    return _main(argc, argv);
 }
 
 
 //---- Main program ------------------------------------------------------
 int QtTest::_main(int argc, char **argv )
 {
-  printf("_main\n");
-  qttest = new QtTest();   // store globals here...
-  CP;
+    // Sometimes need this
+    // Sometimes don't
+    // depends on the STAR distribution!@
+    printf("app\n");
+    CP;
+    QApplication app(argc, argv);
+    CP;
+   
 
-  // Sometimes need this
-  // Sometimes don't
-  // depends on the STAR distribution!@
-  printf("app\n");
-  QApplication app(argc, argv);
-  printf("app2\n");
-  
+    printf("Create QtTest\n");
+    qttest = new QtTest();   // store globals here...
+    CP;
 
-  QtTestGui *gui = new QtTestGui();
-  CP;
-  printf("init\n");
-  gui->init();
-  printf("inited\n");
+    printf("Create QtTestGui\n");
+    QtTestGui *gui = new QtTestGui();
+    CP;
+   
+    printf("Init gui\n");
+    gui->init();
+    CP;
 
+    printf("Exec app\n");
+    app.exec();
+    CP;
 
-  printf("arm\n");
-  // This issues an update...to start the update loop...
-  QTimer::singleShot (100,gui,SLOT(UpdatePlots()));
-
-  printf("done\n");
-
-  //sleep(100);
-
-  app.exec();
-  return 0;  // just returns to roo4star...
+    printf("Done with app\n");
+    return 0;  // just returns to roo4star...
 }
--- OnlTools/Jevp/StJevpPresenter/QtTestGui.h
+++ OnlTools/Jevp/StJevpPresenter/QtTestGui.h
@@ -30,34 +30,19 @@
 #  include <q3intdict.h> 
 #  include <q3mainwindow.h>
 #  include <Q3Frame>
-
-class Q3ToolBar;
-class Q3HBox;
-class Q3ProgressBar;
-
-
-class QTimer;
-class QTabWidget ;
-class QPushButton;
-class QButton;
-class TCanvas;
-class QMenuBar;
-class QWidget;
-class QLineEdit;
-class EventInfo;
-class ServerInfo;
-class ServerStatus;
-class TriggerDetectorBitsInfo;
-class TCanvas;
-class TObject;
-class TQtWidget;
-class TQtZoomPadWidget;
-
+#include "q3hbox.h"
 
 using namespace std;
 
-enum { kFileChangeHistogramSet, kFileIgnoreServerTags, kFileChangeToRun, kFileLive, kFilePrint, kFilePrintAll, kHelpAbout, kUpdate, kAutoUpdate, kToolBar, kOnlPrinter2 }; 
 
+//class ScreenWidget : public TQtWidget {
+class ScreenWidget : public QWidget {
+ public:
+    QTabWidget *parentMenu;
+    ScreenWidget(char *name, QTabWidget *menu);
+    void mouseDoubleClickEvent(QMouseEvent *e);
+    void mousePressEvent(QMouseEvent *e);
+};
 
 
 // ************************************************************************
@@ -65,66 +50,27 @@ class QtTestGui : public Q3MainWindow {
      
     Q_OBJECT
 
-
  private:
-    TCanvas* pc_mCanvas;
     QTimer *refreshTimer;
 
     int mWidth;
     int mHight;
   
     Q3HBox *mCentralWidget;
- 
     Int_t   fGuiRefreshRate; // msec.
     
- protected:
-     void StopEventLoop();
-
  public:
-
     QtTestGui();
-    void gui_QtTestGui(QtTestGui *logic, bool isRefWindow = false);
     virtual ~QtTestGui();
-    
-    int GetWidth() { return mWidth;}
-    int GetHight() { return mHight; }
- 
     void init(); 
 
 
  public slots:
-    void ProcessMessage();
-    void jl_ClosePresenter();
-
-    void CloseWindow(void);
-    void DefineLayouts(void);
- 
+    void ClosePresenter();
     void refreshTimerFired();
-    void UpdatePlots();
+    void currentChangedSlot(int);
 
  signals:
-    
-    void jl_setEnabled(bool);
-    void save();
-    void saveAs();
-    void live();
-    void file();
-    void update();
-    void update(TCanvas* cc, const char* );
-    void update(TCanvas* cc, int tab, int subTab);
-    void autoUpdate();
-    void print();
-    void printAll(const char*);
-    void tab(int);
-    void subTab(int);
-    void canvas(TCanvas*);
-    void openReference();
-  
-
-    //////////////////////////////  PresenterConnect
-
-
- 
 
 };
 //************************************************************************
