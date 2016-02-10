8438d85846bca76efc5844a422d9b84cf9bc7199
ullrich <>
2016-02-10 14:58:39 +0000

    Add several missing pragmas, mostly related to trigger.

--- StRoot/StEvent/StEventLinkDef.h
+++ StRoot/StEvent/StEventLinkDef.h
@@ -134,7 +134,7 @@
 #pragma link C++ class FMSBlock2012+;
 #pragma link C++ class TrgOfflen2012+;
 
-//IncFile=StDaqLib/TRG/trgStructures2012.h
+//IncFile=StDaqLib/TRG/trgStructures2013.h
 #pragma link C++ class TriggerDataBlk2013+;
 #pragma link C++ class EvtDescData2013+;
 #pragma link C++ class TrgSumData2013+;
@@ -148,6 +148,21 @@
 #pragma link C++ class BELayerBlock2013+;
 #pragma link C++ class FMSBlock2013+;
 #pragma link C++ class TrgOfflen2013+;
+
+//IncFile=StDaqLib/TRG/trgStructures2016.h
+#pragma link C++ class TriggerDataBlk2016+;
+#pragma link C++ class EvtDescData2016+;
+#pragma link C++ class TrgSumData2016+;
+#pragma link C++ class L1_DSM_Data2016+;
+#pragma link C++ class DataBlock2016+;
+#pragma link C++ class BBCBlock2016+;
+#pragma link C++ class MIXBlock2016+;
+#pragma link C++ class QTBlock2016+;
+#pragma link C++ class BEastBlock2016+;
+#pragma link C++ class BWestBlock2016+;
+#pragma link C++ class BELayerBlock2016+;
+#pragma link C++ class FMSBlock2016+;
+#pragma link C++ class TrgOfflen2016+;
 
 #pragma link C++ function operator<<(ostream&, const StPxlHit&);        
 #pragma link C++ function operator<<(ostream&, const StIstHit&); 
