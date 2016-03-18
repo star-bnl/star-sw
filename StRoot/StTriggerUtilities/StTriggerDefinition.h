044cb7cc4f843ea1dd1f9a5f7d83ad6c727b5f33
zchang <>
2016-03-18 22:42:09 +0000

    remove onbits0/offbits0 to match with what was in the database table

--- StRoot/StTriggerUtilities/StTriggerDefinition.h
+++ StRoot/StTriggerUtilities/StTriggerDefinition.h
@@ -13,11 +13,9 @@ struct StTriggerDefinition : public TObject {
   int triggerId;		// trigger ID
   unsigned int onbits;	        // bits required to be ON at input of TCU
   unsigned int offbits;	        // bits required to be OFF at input of TCU
-  unsigned int onbits0;
   unsigned int onbits1;
   unsigned int onbits2;
   unsigned int onbits3;
-  unsigned int offbits0;
   unsigned int offbits1;
   unsigned int offbits2;
   unsigned int offbits3;
@@ -29,8 +27,8 @@ struct StTriggerDefinition : public TObject {
 
 inline void StTriggerDefinition::print()
 {
-  printf("triggerIndex=%d name=%s triggerId=%d onbits=0x%04x offbits=0x%04x\n",
-	 triggerIndex,name.Data(),triggerId,onbits,offbits);
+  printf("triggerIndex=%d name=%s triggerId=%d onbits=0x%04x offbits=0x%04x onbits1=0x%04x onbits2=0x%04x onbits3=0x%04x, offbits1=0x%04x, offbits2=0x%04x, offbits3=0x%04x\n",
+	 triggerIndex,name.Data(),triggerId,onbits,offbits, onbits1, onbits2, onbits3, offbits1, offbits2, offbits3);
 }
 
 #endif	// StTriggerDefinition_h
