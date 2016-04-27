6ad1e710c37575228498fbbdae26a81873428b86
Zilong Chang <changzl@physics.tamu.edu>
2016-04-27 16:10:41 +0000

    SL13b embedding library for run12 pp500 production with fastjet area calculationsCVS: ----------------------------------------------------------------------

---
+++ StRoot/StJetFinder/StFastJetAreaPars.cxx
@@ -0,0 +1,8 @@
+#include "fastjet/AreaDefinition.hh"
+#include "StjFastJet.h"
+#include "StFastJetAreaPars.h"
+
+const int StFastJetAreaPars::active_area = fastjet::active_area;
+const int StFastJetAreaPars::active_area_explicit_ghosts = fastjet::active_area_explicit_ghosts;
+const int StFastJetAreaPars::one_ghost_passive_area = fastjet::one_ghost_passive_area;
+const int StFastJetAreaPars::passive_area = fastjet::passive_area;
