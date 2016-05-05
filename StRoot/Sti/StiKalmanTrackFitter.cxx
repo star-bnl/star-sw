0b3a2c63a59d4cb7362c4d54fb1473902eefbee1
Dmitri Smirnov <dsmirnov@bnl.gov>
2016-05-05 16:09:25 +0000

    check for targetDet added

--- StRoot/Sti/StiKalmanTrackFitter.cxx
+++ StRoot/Sti/StiKalmanTrackFitter.cxx
@@ -72,7 +72,10 @@ Int_t StiKalmanTrackFitter::fit(StiTrack * stiTrack, Int_t fitDirection) //throw
       }
       else  {
 	if (debug()) {
-	  targetNode->ResetComment(::Form("%30s start refit",targetDet->getName().c_str()));
+          if (targetDet)
+            targetNode->ResetComment(::Form("%30s start refit",targetDet->getName().c_str()));
+          else
+            targetNode->ResetComment(::Form("%30s start refit","Vertex"));
 	  targetNode->PrintpT("S");}
 //        pNode = targetNode;		continue;
         pNode = targetNode;		
