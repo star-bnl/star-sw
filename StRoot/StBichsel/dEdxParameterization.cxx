4ff8ce5653c3e38cf259831d63c7d4e49ff8eaaf
Yuri Fisyak <fisyak@bnl.gov>
2016-06-10 15:58:00 -0400

    Covertry
    Former-commit-id: b2437360fe14d0bc9f87b7fd438ed1c5738303d8

--- StRoot/StBichsel/dEdxParameterization.cxx
+++ StRoot/StBichsel/dEdxParameterization.cxx
@@ -147,8 +147,11 @@ Double_t dEdxParameterization::Get(const TH1D *hist, Double_t log10bg) const {
   if (log10bg > xmax) log10bg = xmax;
   return hsave->Interpolate(log10bg);
 }
-// $Id: dEdxParameterization.cxx,v 1.18 2015/12/24 00:16:26 fisyak Exp $
+// $Id: dEdxParameterization.cxx,v 1.19 2016/06/10 19:56:11 fisyak Exp $
 // $Log: dEdxParameterization.cxx,v $
+// Revision 1.19  2016/06/10 19:56:11  fisyak
+// Fix covertry warning
+//
 // Revision 1.18  2015/12/24 00:16:26  fisyak
 // Add TpcRS model and macros
 //
