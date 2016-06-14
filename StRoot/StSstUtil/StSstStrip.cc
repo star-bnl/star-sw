167e70de5bd7c8d359f0b107542f49afc8144911
Jonathan Bouchet <bouchet@rcf.rhic.bnl.gov>
2016-06-14 19:01:48 +0000

    coverity : UNINIT_CTOR

--- StRoot/StSstUtil/StSstStrip.cc
+++ StRoot/StSstUtil/StSstStrip.cc
@@ -1,6 +1,9 @@
-//$Id: StSstStrip.cc,v 1.1 2015/06/23 16:26:20 jeromel Exp $
+//$Id: StSstStrip.cc,v 1.2 2016/06/14 19:01:48 bouchet Exp $
 //
 //$Log: StSstStrip.cc,v $
+//Revision 1.2  2016/06/14 19:01:48  bouchet
+//coverity : UNINIT_CTOR
+//
 //Revision 1.1  2015/06/23 16:26:20  jeromel
 //First version created from the SSD code and reshaped
 //
@@ -22,8 +25,14 @@ StSstStrip::StSstStrip(Int_t rNStrip, Int_t rDigitSig, Float_t rSigma, Int_t rPe
   mPedestal  = rPedestal;
   for(Int_t e=0;e<5;e++)  {
     mIdMcHit[e] = rIdMcHit[e];  
-    mIdMcTrack[e]  = rIdMcTrack[e];
+    mIdMcTrack[e] = rIdMcTrack[e];
+    mIdHit[e] = 0;
   }
+  mMcStrip = 0;
+  mNHits = 1;
+  mAnalogSig = 0;
+  mPrevStrip = 0;
+  mNextStrip = 0;
 }
 //________________________________________________________________________________
 StSstStrip::StSstStrip(Int_t rNStrip, Int_t rDigitSig, Float_t rSigma, Int_t rPedestal) {
@@ -32,7 +41,16 @@ StSstStrip::StSstStrip(Int_t rNStrip, Int_t rDigitSig, Float_t rSigma, Int_t rPe
   mDigitSig  = rDigitSig;  
   mSigma     = rSigma;
   mPedestal  = rPedestal;
-  for(Int_t e=0;e<5;e++) mIdMcHit[e] = 0;
+  for(Int_t e=0;e<5;e++) {
+    mIdMcHit[e] = 0;
+    mIdMcTrack[e] = 0;
+    mIdHit[e] =0;
+  }
+  mMcStrip = 0;
+  mNHits = 1;
+  mAnalogSig = 0;
+  mPrevStrip = 0;
+  mNextStrip = 0;
 }
 //________________________________________________________________________________
 StSstStrip::StSstStrip(Int_t rNStrip, Int_t rIdHit, Int_t rMcHit, Int_t rMcTrack, Float_t rAnalogSig) {
@@ -41,8 +59,19 @@ StSstStrip::StSstStrip(Int_t rNStrip, Int_t rIdHit, Int_t rMcHit, Int_t rMcTrack
   mIdHit[0]      = rIdHit;
   mIdMcHit[0]    = rMcHit;
   mIdMcTrack[0]  = rMcTrack;
+  for(Int_t e=1;e<5;e++) {
+    mIdMcHit[e] = 0;
+    mIdMcTrack[e] = 0;
+    mIdHit[e] = 0;
+  }
   mNHits         = 1;
   mAnalogSig     = rAnalogSig;
+  mDigitSig      = 0;  
+  mSigma     = 0;
+  mPedestal  = 0;
+  mMcStrip = 0;
+  mPrevStrip =0;
+  mNextStrip = 0;
 }
 //________________________________________________________________________________
 StSstStrip::StSstStrip(Int_t rNStrip, Int_t rMcStrip, Int_t rDigitSig, Float_t rAnalogSig, Int_t *rIdMcHit) {
@@ -52,6 +81,15 @@ StSstStrip::StSstStrip(Int_t rNStrip, Int_t rMcStrip, Int_t rDigitSig, Float_t r
   mDigitSig      = rDigitSig;  
   mAnalogSig     = rAnalogSig;
   for(Int_t e=0;e<5;e++)  mIdMcHit[e] = rIdMcHit[e];
+  for(Int_t e=0;e<5;e++) {
+    mIdMcTrack[e] = 0;
+    mIdHit[e] = 0;
+  }
+  mNHits = 1;
+  mSigma     = 0;
+  mPedestal  = 0;
+  mPrevStrip = 0;
+  mNextStrip = 0;
 }
 //_________________
 StSstStrip::StSstStrip(Int_t rNStrip, Int_t rMcStrip, Int_t rDigitSig, Float_t rAnalogSig, Int_t *rIdMcHit,Int_t *rIdMcTrack) {
@@ -61,9 +99,15 @@ StSstStrip::StSstStrip(Int_t rNStrip, Int_t rMcStrip, Int_t rDigitSig, Float_t r
   mDigitSig      = rDigitSig;  
   mAnalogSig     = rAnalogSig;
   for(Int_t e=0;e<5;e++)  {
+    mIdHit[e] =0;
     mIdMcHit[e]   = rIdMcHit[e];  
     mIdMcTrack[e] = rIdMcTrack[e];
   }
+  mSigma     = 0;
+  mPedestal  = 0;
+  mNHits = 1;
+  mPrevStrip = 0;
+  mNextStrip = 0;
 }
 
 //________________________________________________________________________________
@@ -72,12 +116,35 @@ StSstStrip::StSstStrip(Int_t rNStrip, Int_t rMcStrip, Float_t rAnalogSig) {
   mNStrip        = rNStrip;
   mMcStrip       = rMcStrip;
   mAnalogSig     = rAnalogSig;
+  mDigitSig  = 0;
+  mSigma     = 0;
+  mPedestal  = 0;
+  for(Int_t e=0;e<5;e++)  {
+    mIdMcHit[e] = 0;
+    mIdMcTrack[e] = 0;
+    mIdHit[e] = 0;
+  }
+  mNHits = 1;
+  mPrevStrip = 0;
+  mNextStrip = 0;
 }
 //________________________________________________________________________________
 StSstStrip::StSstStrip(Int_t rNStrip, Int_t rDigitSig) {
   memset (first, 0, last - first);
   mNStrip        = rNStrip;
   mDigitSig      = rDigitSig;  
+  mSigma     = 0;
+  mPedestal  = 0;
+  for(Int_t e=0;e<5;e++)  {
+    mIdMcHit[e] = 0;
+    mIdMcTrack[e] = 0;
+    mIdHit[e] = 0;
+  }
+  mMcStrip = 0;
+  mNHits = 1;
+  mAnalogSig = 0;
+  mPrevStrip = 0;
+  mNextStrip = 0;
 }
 //________________________________________________________________________________
 void StSstStrip::copyTo(StSstStrip *ptrClone) {
