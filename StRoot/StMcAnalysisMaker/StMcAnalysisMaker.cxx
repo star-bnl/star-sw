8139c4952697def9a80c814cb67d4176e9f34455
Jason C. Webb <jwebb@bnl.gov>
2016-06-21 20:39:03 +0000

    Init members.  Protected against div by zero.

--- StRoot/StMcAnalysisMaker/StMcAnalysisMaker.cxx
+++ StRoot/StMcAnalysisMaker/StMcAnalysisMaker.cxx
@@ -1,7 +1,10 @@
 /*************************************************
  *
- * $Id: StMcAnalysisMaker.cxx,v 1.40 2015/03/13 18:44:50 perev Exp $
+ * $Id: StMcAnalysisMaker.cxx,v 1.41 2016/06/21 20:39:03 jwebb Exp $
  * $Log: StMcAnalysisMaker.cxx,v $
+ * Revision 1.41  2016/06/21 20:39:03  jwebb
+ * Init members.  Protected against div by zero.
+ *
  * Revision 1.40  2015/03/13 18:44:50  perev
  * Roll back
  *
@@ -222,7 +225,23 @@ ClassImp(StMcAnalysisMaker)
 
 //_________________________________________________
 StMcAnalysisMaker::StMcAnalysisMaker(const char *name, const char *title):
-  StMaker(name,title), mNtupleFile(0)
+  StMaker(name,title), 
+  currentChain(0),
+  mMomResolution(0),
+  mHitResolution(0),
+  mSvtHitResolution(0),
+  mSsdHitResolution(0),
+  coordRec(0),
+  coordMcPartner(0),
+  mNtupleFile(0),
+  mTrackNtuple(0),
+  mTpcHitNtuple(0),
+  mSvtHitNtuple(0),
+  mSsdHitNtuple(0),
+  mAssociationCanvas(0),
+  mPadColumns(0),
+  mPadRows(0),
+  drawinit(0)
 {
     //  StMcAnalysisMaker Constructor
     // - zero all pointers defined in the header file
@@ -856,7 +875,7 @@ Int_t StMcAnalysisMaker::Make()
     for (multimap<int,float>::iterator mi=mostCommRange.first; mi!=mostCommRange.second; ++mi) {
 	idQuality+=mi->second;
     }
-    idQuality/=cachedNHitsIdTruth;
+    if (cachedNHitsIdTruth) { idQuality/=cachedNHitsIdTruth; }
     
     values[13] = mostCommonIdTruth;
     values[14] = cachedNHitsIdTruth;
