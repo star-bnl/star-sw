375b178002e14de6d379e70ff1303f32df025523
jwebb <>
2016-06-21 15:11:40 +0000

    Facepalm.  Code only deferences a NULL pointer.  Really?

--- asps/Simulation/starsim/deccc/avost.c
+++ asps/Simulation/starsim/deccc/avost.c
@@ -1,6 +1,9 @@
 /*
- * $Id: avost.c,v 1.3 2004/06/26 00:14:39 potekhin Exp $ 
+ * $Id: avost.c,v 1.4 2016/06/21 15:11:40 jwebb Exp $ 
  * $Log: avost.c,v $
+ * Revision 1.4  2016/06/21 15:11:40  jwebb
+ * Facepalm.  Code only deferences a NULL pointer.  Really?
+ *
  * Revision 1.3  2004/06/26 00:14:39  potekhin
  * Second attempt to reformat the CVS comments on top and
  * prevent it from flipping over
@@ -8,5 +11,5 @@
  */
 
 
-void avost_() { int *p=0; int q=*p; }
+//void avost_() { int *p=0; int q=*p; }
 
