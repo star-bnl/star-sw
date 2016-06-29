1349694c25aaaf5e88a5173a679707fd6ed35d60
Victor Perevoztchikov <perev@bnl.gov>
2016-06-29 19:43:54 +0000

    1. StiCATpcSeedFinder inherited now from StiTrackFinder 2. For StiCATpcSeedFinder assigned name "CASeedFinder" 3. Method startEvent added 4. Method findTrack added which is used for all seed finders 5. findTpcTracks replaced by findTrack 6. Auxiliary class StiCALoader added to create CA seed finder via rootcint

--- StRoot/StiCA/StiCATpcSeedFinder.cxx
+++ StRoot/StiCA/StiCATpcSeedFinder.cxx
@@ -1,12 +1,12 @@
-#include "StiCA/StiCATpcSeedFinder.h"
+#include <assert.h>
+#include <string.h>
 #include "Sti/StiToolkit.h"
 #include "Sti/StiHit.h"
-#include "St_base/StMessMgr.h"
+#include "StMessMgr.h"
 #include "StEvent/StTpcHit.h"
-#include "StiCA/StiCAKalmanTrack.h"
-#include "StiCA/StiCAKalmanTrackFinder.h"
+#include "Sti/StiKalmanTrack.h"
 
-#include "StiCA/StiCATpcTrackerInterface.h"
+#include "StiCATpcTrackerInterface.h"
 //#define PRINT_SEED_STATISTIC
 //#define PRINT_FIT_ERR_STATISTIC
 #ifdef PRINT_FIT_ERR_STATISTIC
@@ -16,74 +16,54 @@
 //#define KINK_REJECTION  
 #define OVERLAP_REJECTION
 //________________________________________________________________________________
-Bool_t StiCATpcSeedFinder::SeedsCompareStatus(const Seed_t a, const Seed_t b){
+Bool_t StiCATpcSeedFinder::SeedsCompareStatus(const Seed_t a, const Seed_t b)
+{
   return (a.total_hits < b.total_hits);
 }
 //________________________________________________________________________________
-void StiCATpcSeedFinder::findTpcTracks(StiCATpcTrackerInterface &caTrackerInt) {
-#ifdef PRINT_FIT_ERR_STATISTIC
-   static std::map<int,unsigned int> statusMap;
-#endif // PRINT_FIT_ERR_STATISTIC
+StiTrack *StiCATpcSeedFinder::findTrack(double rMin)
+{ 
+  static StiCATpcTrackerInterface& caTrackerInt = StiCATpcTrackerInterface::Instance();
+  if (!mSeeds || mSeeds->size()==0) {
+    if (mEnded) return 0;
+    caTrackerInt.SetNewEvent();
+    // zero all banks before filling !!! 
+    auto *map =  &StiToolkit::instance()->getHitContainer()->hits();
 
+
+    // Run reconstruction by the CA Tracker
+    caTrackerInt.SetHits(*map);
+    caTrackerInt.Run();
+    mSeeds = &caTrackerInt.GetSeeds();
+    if (!mSeeds->size()) { mEnded = 2; return 0;}
+    sort(mSeeds->begin(), mSeeds->end(),SeedsCompareStatus );
+  }
+  int begEndFail=0;
+  while (mSeeds->size()) {
   
-  // zero all banks before filling !!! 
-  HitMapToVectorAndEndType& map =  StiToolkit::instance()->getHitContainer()->hits();
-
-
-  // Run reconstruction by the CA Tracker
-  caTrackerInt.SetHits(map);
-  caTrackerInt.Run();
-  vector<Seed_t> &seeds = caTrackerInt.GetSeeds();
-
-
-  sort(seeds.begin(), seeds.end(),SeedsCompareStatus );
-#ifdef PRINT_SEED_STATISTIC
-  Int_t nSeed = 0;
-#endif // PRINT_SEED_STATISTIC
-  while (! seeds.empty()) {
-    Seed_t &aSeed = seeds.back();
+    Seed_t &aSeed = mSeeds->back();
     vector<StiHit*>        _seedHits;
-    for (vector<SeedHit_t *>::iterator hitb = aSeed.vhit.begin(); hitb != aSeed.vhit.end(); hitb++) {
-      StiHit *hit = (*hitb)->hit;
-      if (!hit || hit->timesUsed()) continue;
+    int nHits = aSeed.vhit.size();
+    for (int iHit=0;iHit<nHits;iHit++) 
+    {
+      StiHit *hit = aSeed.vhit[iHit]->hit;
+      if (!hit || hit->timesUsed()) {
+        if (!iHit) begEndFail++;
+        continue;
+      }
       _seedHits.push_back(hit);
     }
-    seeds.pop_back();
-#ifdef PRINT_SEED_STATISTIC
-    cout<< "seed: " << nSeed++ << "\t" <<aSeed.total_hits<<" total hits "; //<<aSeed.used_per_total;
-    cout << " no. unused hits " << _seedHits.size();
-#endif // PRINT_SEED_STATISTIC
-    if (_seedHits.size() < 4) {
-#ifdef PRINT_SEED_STATISTIC
-      cout << endl; 
-#endif // PRINT_SEED_STATISTIC
-      continue;
-    }
-    StiCAKalmanTrack* track = static_cast<StiCAKalmanTrack*>(StiToolkit::instance()->getTrackFactory()->getInstance());
-    
-//   if (track->initialize(_seedHits)) {cout << " initialization failed" << endl; continue;} // use helix approximation
-   track->initialize0(_seedHits, &aSeed.firstNodePars, &aSeed.lastNodePars/*, &aSeed.firstNodeErrs, &aSeed.lastNodeErrs*/ ); // use CATracker parameters. P.S errors should not be copied, they'd be initialized.
-   StiCAKalmanTrackFinder *finderTmp = (StiCAKalmanTrackFinder *)track->getTrackFinder();
-   int status = finderTmp->Fit(track);
-#ifdef PRINT_FIT_ERR_STATISTIC
-   StiCAKalmanTrackFinder::PrintFitStatus(status,track);
+    mSeeds->pop_back(); mEnded = !mSeeds->size(); if (mEnded) return 0;
+    if (_seedHits.size() < 4) continue;
 
-   if ( statusMap.find(status) != statusMap.end() ) statusMap[status]++;
-   else statusMap[status] = 1;
-#endif // PRINT_FIT_ERR_STATISTIC
-   
-   if (status){
-     BFactory::Free(track); // delete not fitted track
-     continue;
-   }
-   track->setSeedHitCount(track->getSeedHitCount()+100);
-  } // while (! seeds.empty())
-
-#ifdef PRINT_FIT_ERR_STATISTIC
-   cout << " ---- Fit status statistic.  ---- " << endl;
-   for ( std::map<int, unsigned int>::iterator it = statusMap.begin(); it != statusMap.end(); it++) {
-     cout << (*it).second << " entries for:" << endl;
-     StiCAKalmanTrackFinder::PrintFitStatus((*it).first,0);
-   }
-#endif // PRINT_FIT_ERR_STATISTIC
+    StiKalmanTrack* track = static_cast<StiKalmanTrack*>(StiToolkit::instance()->getTrackFactory()->getInstance());
+    if (1 || !begEndFail)
+      track->initialize0(_seedHits, &aSeed.firstNodePars, &aSeed.lastNodePars/*, &aSeed.firstNodeErrs, &aSeed.lastNodeErrs*/ ); // use CATracker parameters. P.S errors should not be copied, they'd be initialized.
+    else 
+      track->initialize0(_seedHits); 
+    return track;
+  }
+  mEnded = 3; return 0;
 }
+//________________________________________________________________________________
+StiCATpcSeedFinder* StiCALoader::New() { return new StiCATpcSeedFinder;}
