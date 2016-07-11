1c18ff86cf9fb2f898bdfc26c97ebaaa8d118d92
Yuri Fisyak <fisyak@bnl.gov>
2016-07-11 12:28:31 -0400

    Merge
    Former-commit-id: 73d229e1bd4ac6c31ec896d2a2df174be69b2207

--- StRoot/RTS/src/RTS_EXAMPLE/daqFileChopper.C
+++ StRoot/RTS/src/RTS_EXAMPLE/daqFileChopper.C
@@ -16,6 +16,10 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+
 #include <rtsLog.h>	// for my LOG() call
 #include <rtsSystems.h>
 
@@ -29,6 +33,7 @@ void displayHelp()
     LOG(ERR,"        filterlist -->  -trg xxx yyy zzz...  // list of offline trigger bits");
     LOG(ERR,"                        -dtrg xxx yyy zzz... // list of daq trigger bits");
     LOG(ERR,"                        -eventnum xxx yyy... // list of event numbers");
+    LOG(ERR,"                        -chop xxx            // chop into files of xxx events");
 }
 
 // Example event filter...
@@ -74,10 +79,13 @@ int FilterEvent(daqReader *rdr, int nargs, char *argv[])
   return 0;
 }
 
+void doChopInstead(int argc, char *argv[]);
+
+
 int main(int argc, char *argv[])
 {
   rtsLogOutput(RTS_LOG_STDERR) ;
-  rtsLogLevel(WARN) ;
+  rtsLogLevel((char *)WARN) ;
 
   if(argc < 3) {
     displayHelp();
@@ -88,6 +96,18 @@ int main(int argc, char *argv[])
   int filterArgc = argc-2;
   char **filterArgv = &argv[2];
   
+
+  // This is a very ugly way to do this, but chopping the file into separate bits is very different from
+  // The general problem of filtering in semantics
+  // Yet, the utility program is similar in the minds of users, so I simply treat this as
+  // a separate program run from the same entry point...
+  if(strcmp(argv[2], "-chop") == 0) {
+      doChopInstead(argc, argv);
+      return 0;
+  }
+
+
+
   daqReader *evp;
   evp = new daqReader(argv[1]) ;	// create it with the filename argument..
 
@@ -136,4 +156,106 @@ int main(int argc, char *argv[])
   
   delete evp ; 
   return 0 ;
+}
+
+
+
+void doChopInstead(int argc, char *argv[]) {
+    if(argc < 4) {
+	printf("-chop argument requires number of events per file...\n");
+	return;
+    }
+
+    int nevts_perfile = atoi(argv[3]);
+    int fidx = 0;
+    int nevts_thisfile = 0;
+    int fd = -1;
+
+    char basename[256];
+
+    int i=0;
+    for(i=0;i<256;i++) {
+	if(argv[1][i] == '\0') break;
+	if(argv[1][i] == '.') break;
+	
+	basename[i] = argv[1][i];
+    }
+
+    basename[i] = '\0';
+
+    char currname[256];
+    
+    daqReader *evp;
+    evp = new daqReader(argv[1]) ;	// create it with the filename argument..
+
+    int good=0;
+    int bad=0;
+
+    for(;;) {
+	char *ret = evp->get(0,EVP_TYPE_ANY);
+    
+	if(ret) {
+	    if(evp->status) {
+		LOG(ERR,"evp status is non-null [0x08X, %d dec]",evp->status,evp->status) ;
+		continue ;
+	    }
+	    good++;
+	}
+	else {    // something other than an event, check what.
+	    switch(evp->status) {
+	    case EVP_STAT_OK:   // just a burp!
+		continue;
+	    case EVP_STAT_EOR:
+		LOG(OPER, "Done after scanning %d events (%d bad)",good,bad);
+		break;        // file, we're done...
+	    case EVP_STAT_EVT:
+		bad++;
+		LOG(WARN, "Problem getting event - skipping [good %d, bad %d]",good,bad);
+		continue;
+	    case EVP_STAT_CRIT:
+		LOG(CRIT,"evp->status CRITICAL (?)") ;
+		return ;
+	    }
+	}
+    		
+	if(evp->status == EVP_STAT_EOR) {
+	    if(fd >= 0) {
+		LOG(INFO, "Wrote %d evts to file: %s", nevts_thisfile, currname);
+		close(fd);
+	    }
+
+	    LOG(INFO,"Done after scanning %d events (%d bad)",good,bad) ;
+
+	 
+	    break; 
+	}
+
+	nevts_thisfile++;
+	
+	if(fd == -1) {
+	    fidx++;
+	    sprintf(currname, "%s-%d.daq", basename, fidx);
+	    fd = open(currname, O_WRONLY | O_CREAT, 0666);
+	    if(fd < 0) {
+		LOG(INFO, "Error opening file %s: (%s)", currname, strerror(errno));
+		return;
+	    }
+	}
+	    
+	LOG(DBG, "Writing event #%d (ptr=0x%x sz=%d) [%d in file %s]",evp->seq,evp->memmap->mem,evp->event_size, nevts_thisfile, currname);
+	
+	write(fd, evp->memmap->mem, evp->event_size);
+
+	if(nevts_thisfile >= nevts_perfile) {
+	    LOG(INFO, "Wrote %d evts to file: %s", nevts_thisfile, currname);
+	    nevts_thisfile = 0;
+	    close(fd);
+	    fd = -1;
+	}
+    }
+  
+    delete evp ; 
+    return;
+    
+    
 }
