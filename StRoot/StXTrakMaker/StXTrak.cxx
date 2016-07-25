443cea2607b394f1c328ea95c9ec2e6b4e895779
Yuri Fisyak <fisyak@bnl.gov>
2016-07-25 09:48:26 -0400

    Merge
    Former-commit-id: 53bcc1979168eb543c57b02de0d9e0d1c49337fb

--- StRoot/StXTrakMaker/StXTrak.cxx
+++ StRoot/StXTrakMaker/StXTrak.cxx
@@ -1,4 +1,4 @@
-// $Id: StXTrak.cxx,v 1.2 2016/06/01 01:06:33 perev Exp $
+// $Id: StXTrak.cxx,v 1.3 2016/07/22 19:00:20 perev Exp $
 /// \File StXTrak.cxx
 /// \author V.Perev 2016
 //
@@ -18,6 +18,7 @@ Main tasks:
 #include <string.h>
 #include <assert.h>
 #include "TCernLib.h"
+#include "TVector3.h"
 #include "StXTrak.h"
 #include "TGeoManager.h"
 #include "TGeoSwim.h"
@@ -28,24 +29,123 @@ Main tasks:
 #include "TGeoMaterial.h"
 #include "StThreeVectorD.hh"
 #include "THelixTrack.h"
+#include "StiUtilities/StiDebug.h"
 
 //#include "Sti/StiElossCalculator.h"
 #include "StvUtil/StvELossTrak.h"
 #include "StiUtilities/StiDebug.h"
 
 //_____________________________________________________________________________
-StXTrak::StXTrak(MyMag *myMag,StvELossTrak* eLoss) 
+StXTrak::StXTrak(MyMag *myMag,MyLoss* eLoss,TGeoSwimEnd* myEnd) 
 {
-mELoss=eLoss;		//ELoss calculator
-mMyMag=myMag;		//Mag field calculator
+  mMyLoss= eLoss;		//ELoss calculator
+  if (!mMyLoss) mMyLoss = new MyLoss();
+
+  mMyMag = myMag;		//Mag field calculator
+  if (!mMyMag) mMyMag = new MyMag();
+
+
+  mMyEnd = myEnd; 	//End condition
+
+  static const char* farDets[]={
+  "BTOF","BBCA","CALB","ECAL","ETTV",
+  "FGTM","FBOX","FSCE","HCMO","MUTD",
+  "PHMD","VPDD","ZCAL",
+  0};
+
+  if (!mMyEnd ) mMyEnd = new TGeoSwimDefaultEnd(TGeoSwimEnd::kNameVolu,farDets);
+                                                
+  mSwim = new TGeoSwim();
+  mSwim->Set(mMyMag,mMyLoss,mMyEnd);
+  mSwim->Set(500.,-400.,400.,5.);
+}
+
+//_____________________________________________________________________________
+void  StXTrak::Clear() 
+{
+ memset(mBeg,0,mEnd-mBeg+1);
+}
+
+//_____________________________________________________________________________
+void StXTrak::Set1stPoint(int charge1st,double pos1st[3],double mom1st[3])
+{
+static const double kHftRad = 3;
+enum {kUndef, kPrimary, kDca,k1stPoint};
+
+  memcpy(m1stTk.mPos,pos1st,sizeof(m1stTk.mPos));
+  memcpy(m1stTk.mMom,mom1st,sizeof(m1stTk.mMom));
+  m1stTk.mCosLam = sin(TVector3(m1stTk.mMom).Theta());
+  m1stTk.mCharge=charge1st;
+  TVector3 vMom(m1stTk.mMom),vPos(m1stTk.mPos);
+  double B[3];
+  (*mMyMag)(m1stTk.mPos,B);
+  m1stTk.mCurv=B[2]*m1stTk.mCharge/vMom.Perp();
+  m1stTk.mP = vMom.Mag();;
+  m1stTk.mPti = m1stTk.mCharge/vMom.Perp();
+  m1stTk.mPt  = 1./(fabs(m1stTk.mPti)+1e-10);
+  m1stTk.mLen= 0;
+  double dis = vPos.Perp();
+  mFlag1st=k1stPoint; 		//0=undefined, 1=primary, 2=dca,3=1st point
+  if (dis<kHftRad) mFlag1st=kPrimary;
+  if (vMom.Dot(vPos)<1e-3)  mFlag1st=kDca;
+
+  if (mFlag1st==k1stPoint) {
+    double sinAlf = dis*m1stTk.mCurv/2;
+    m1stTk.mLen = (fabs(sinAlf)>1e-3)? asin(sinAlf)*2./m1stTk.mCurv:dis;
+    m1stTk.mLen/= m1stTk.mCosLam;
+  }
 }
 
 
 
 //_____________________________________________________________________________
-double StXTrak::Path(double posp[3],double momp[3], int charge)
+void StXTrak::Set2ndPoint(int charge2nd,double pos2nd[3],double mom2nd[3])
 {
-return 0;
+  memcpy(m2ndTk.mPos,pos2nd,sizeof(m2ndTk.mPos));
+  memcpy(m2ndTk.mMom,mom2nd,sizeof(m2ndTk.mMom));
+  m2ndTk.mCosLam = sin(TVector3(m2ndTk.mMom).Theta());
+  m2ndTk.mCharge=charge2nd;
+  TVector3 vPos(m2ndTk.mPos);
+  TVector3 vMom(m2ndTk.mMom);
+  TVector3 dPos = (vPos - TVector3(m1stTk.mPos));
+  TVector3 aMom = (vMom + TVector3(m1stTk.mMom))*0.5;
+
+  
+  double B[3];
+  (*mMyMag)(m2ndTk.mPos,B);
+  m2ndTk.mCurv=B[2]*m2ndTk.mCharge/vMom.Perp();
+  m2ndTk.mP = vMom.Mag();;
+  m2ndTk.mPti = m2ndTk.mCharge/vMom.Perp();
+  m2ndTk.mPt  = 1./(fabs(m2ndTk.mPti)+1e-10);
+  m2ndTk.mLen= 0;
+  double curv = (m1stTk.mCurv+m2ndTk.mCurv)*0.5;
+  double dis = dPos.Perp();
+  double sinAlf = dis*curv/2;
+  m2ndTk.mLen = (fabs(sinAlf)>1e-3)? asin(sinAlf)*2./curv:dis;
+  m2ndTk.mLen/= (m1stTk.mCosLam+m2ndTk.mCosLam)*0.5;
+  m2ndTk.mLen+=m1stTk.mLen;
+
+  mCurTk = m2ndTk;
+}
+
+//_____________________________________________________________________________
+int StXTrak::Next()
+{
+
+  mSwim->Set(mCurTk.mPos,mCurTk.mMom,mCurTk.mCurv);
+  auto ans = mSwim->Swim(500);
+
+  memcpy(mCurTk.mPos,mSwim->GetPos(),sizeof(mCurTk.mPos));
+  memcpy(mCurTk.mMom,mSwim->GetDir(),sizeof(mCurTk.mMom));
+  mCurTk.mCurv = mSwim->GetCurv();
+  mCurTk.mP   = mSwim->GetP();
+  mCurTk.mPt  = mSwim->GetPt();
+  mCurTk.mPti = mSwim->GetPti();
+  mCurTk.mLen+= mSwim->GetLen();
+  mCurTk.mPLoss   = mSwim->GetPLoss();
+  double d = mSwim->GetLen(1)-mSwim->GetLen(0);
+  StiDebug::Count("Thick",d);
+  return ans;
 }
 #include "StarMagField/StarMagField.h"
 //______________________________________________________________________________
--- StRoot/StXTrakMaker/TGeoSwim.cxx
+++ StRoot/StXTrakMaker/TGeoSwim.cxx
@@ -1,5 +1,5 @@
 
-// $Id: TGeoSwim.cxx,v 1.3 2016/06/01 01:05:27 perev Exp $
+// $Id: TGeoSwim.cxx,v 1.4 2016/07/22 19:00:20 perev Exp $
 //
 //
 // Class StTGeoHelper
@@ -34,8 +34,8 @@ TGeoSwim::TGeoSwim(const char *name):TNamed(name,"TGeoSwim")
   fRmax = 1000;
   fZmin =-1000;
   fZmax = 1000;
-  fHelx[0] = 0;
-  fHelx[1] = 0;
+  fHelx[0] = new THelixTrack();
+  fHelx[1] = new THelixTrack();
   fNode[0] = 0;
   fNode[1] = 0;
   fMag = 0;  
@@ -58,37 +58,35 @@ void TGeoSwim::Set(double Rmax,double Zmin,double zMax,double sMax)
   fZmax = zMax;
   fSmax = sMax;
 }
-//_____________________________________________________________________________
-int TGeoSwim::Set(THelixTrack *inHelx,THelixTrack *otHelx)
-{
- fHelx[0]=inHelx; fHelx[1]=otHelx;
- gGeoManager->SetCurrentPoint    (fHelx[0]->Pos());
- gGeoManager->SetCurrentDirection(fHelx[0]->Dir());
- fNode[0] = gGeoManager->FindNode();
- if (!fNode[0]) return 1;
- return 0;
-}
 
 //_____________________________________________________________________________
 int TGeoSwim::Set(const double* pos,const double* dir, double curv)
 {
+THelixTrack **helx = fHelx;
+double *B = fB;
+double *inOut = fInOutLen;
+  if (fEnd) fEnd->Reset();
   fPt=0;fP=0;fPLoss=0;fTimeFly=0; memset(fB,0,sizeof(fB));
   fC = curv;
-  int ans = Set(new THelixTrack(pos,dir,curv),new THelixTrack()); 
+  fHelx[0]->Set(pos,dir,curv);
   if (fMag) {
     (*fMag)(pos,fB);
-    double Pti = fHelx[0]->GetRho()*fB[2];
-    fPt = (fabs(Pti)>1e-6) ? 1./Pti:1E6;
-    fP  = fabs(fPt/fHelx[0]->GetCos());
+    fPti = (fabs(fB[2])>1e-6) ? fC/fB[2]:1e-3;
+    fPt  = fabs(1./fPti);
+    fP   = fabs(fPt/fHelx[0]->GetCos());
   }
-  return ans;
+  gGeoManager->SetCurrentPoint    (fHelx[0]->Pos());
+  gGeoManager->SetCurrentDirection(fHelx[0]->Dir());
+  fNode[0] = gGeoManager->FindNode();
+  if (!fNode[0]) return 13;
+  return 0;
 }
 //_____________________________________________________________________________
 const double *TGeoSwim::GetPos  (int idx) const      {return fHelx[idx]->Pos();}
 //_____________________________________________________________________________
 const double *TGeoSwim::GetDir  (int idx) const      {return fHelx[idx]->Dir();}
 //_____________________________________________________________________________
-const char *TGeoSwim::GetPath  () const      {return gGeoManager->GetPath();}
+const char   *TGeoSwim::GetPath ()        const      {return gGeoManager->GetPath();}
 //_____________________________________________________________________________
 int TGeoSwim::OutScene(const double *x) const
 {
@@ -106,28 +104,34 @@ const TGeoMaterial *TGeoSwim::GetMate () const
 //_____________________________________________________________________________
 int TGeoSwim::Swim(double maxLenP)
 {
+static const double kMaxLoss = 0.1;// Max momentum loss 
+
 enum {kMaxIter=100};
 enum {kInside  = 1, kOutside  = 0};
-
-  const TGeoNode **nodes=fNode;
-  double   *inout= fInOutLen;
-  THelixTrack** th=fHelx;
-
-  const double *poz = fHelx[0]->Pos();
-  double range = fabs(poz[0])+fabs(poz[1])+fabs(poz[2]);
-  double myMicron = 1e-4+range*1e-4;
-
-  double dP,dC,dPt,pos[3],dir[3],cosTh2,cosTh,lenxy2,lenxy;
+THelixTrack **helx = fHelx;
+double *B = fB;
+double *inOut = fInOutLen;
 
   *fHelx[1] = *fHelx[0];
-  fNode[0] = gGeoManager->GetCurrentNode();
-  double myRad =1./(fabs(fHelx[0]->GetRho())+1e-10)/fHelx[0]->GetCos();
-  double maxLen = (maxLenP>3*myRad)? 3*myRad: maxLenP;
-  fInOutLen[0]=0; fInOutLen[1]=maxLen;
-  double step=0,maxStep=0;
+  double maxLen = maxLenP;
+  double losLen = 1e11;
   while(1) {
+    const double *poz = fHelx[0]->Pos();
+    double range = fabs(poz[0])+fabs(poz[1])+fabs(poz[2]);
+    double myMicron = 1e-4+range*1e-4;
+
+    double dP,dC,dPt,pos[3],dir[3],cosTh2,cosTh,lenxy2,lenxy;
+
+    fNode[0] = gGeoManager->GetCurrentNode();
+    double myRad =1./(fabs(fHelx[0]->GetRho())+1e-10)/fHelx[0]->GetCos();
+    double maxStep = fSmax;
+    if (maxStep>losLen) maxStep=losLen;
+    losLen = 1e11;
+    if (maxStep>maxLen) maxStep=maxLen;
+    if (maxStep>myRad) maxStep = myRad;
+    fInOutLen[0]=0; fInOutLen[1]=maxStep;
+    double step=0,myLen=0;
     const TGeoMaterial *gmate = GetMate();
-    double myLen = fInOutLen[0];
     int kase = kInside;  	
     for (int iter=0;iter<kMaxIter; iter++)  {
       switch (kase) {
@@ -139,8 +143,6 @@ enum {kInside  = 1, kOutside  = 0};
             gGeoManager->SetCurrentDirection(fHelx[1]->Dir());
             fNode[1] = gGeoManager->FindNode();
           }
-          maxStep = fInOutLen[1]-fInOutLen[0];
-	  if (maxStep>0.3*myRad) maxStep=0.3*myRad;
           if ( fInOutLen[0]+maxStep > maxLen) maxStep = maxLen-fInOutLen[0];
           if (maxStep<=0) break;
 	  gGeoManager->FindNextBoundary(maxStep);
@@ -154,66 +156,115 @@ enum {kInside  = 1, kOutside  = 0};
 	  else 				   { step = (fInOutLen[1]-fInOutLen[0])*0.5;}
 	  break;}
 
-	default: assert(0 || "Wrong case");
+	default: assert(0 && "Wrong case");
 
       }
-      if (maxStep<=0) 			return 1;
       range = fInOutLen[1]-fInOutLen[0];
       if (range<myMicron)	break;
-      if (step<myMicron) 	{ 	//if step is tiny try to change upper limit
+      if (step <myMicron) 	{ 	//if step is tiny try to change upper limit
 	step = myMicron;
 	if (step> range/2) step = range/2;			
+        if (losLen<1e10) {fInOutLen[1] = fInOutLen[0]; break;}
       }
       fHelx[1]->Eval(step,pos,dir); 
       kase = (gGeoManager->IsSameLocation(pos[0],pos[1],pos[2]))? kInside:kOutside;
     }
 
     range = fInOutLen[1]-fInOutLen[0];
-    if (range>myMicron) 		return 13; 	//no convergency at all
+    if (range>myMicron) 		return kNoConv; 	//no convergency at all
     fHelx[1]->Move(range);
 
-    if (OutScene(fHelx[1]->Pos()))	return 2;
-    myLen = fInOutLen[1]-myLen;
+    if (OutScene(fHelx[1]->Pos()))	return kOutScene;
+    myLen = fInOutLen[1];
     if (fLoss) { // Account of energy loss
 
-      dP = (*fLoss)(gmate,fP,myLen,0);
+      dP = -(*fLoss)(gmate,fP,myLen,0);
 //    ================================
 
+      if (fabs(dP) > kMaxLoss*fP) { // Cut step, too big eloss
+         losLen = fInOutLen[1]/2;
+         *fHelx[1]=*fHelx[0];
+         gGeoManager->SetCurrentPoint    (fHelx[1]->Pos());
+         gGeoManager->SetCurrentDirection(fHelx[1]->Dir());
+         fNode[1] = gGeoManager->FindNode();
+	 continue;
+      }
       memcpy(pos,fHelx[1]->Pos(),sizeof(pos));
       memcpy(dir,fHelx[1]->Dir(),sizeof(dir));
       cosTh2 = (1.-dir[2])*(1.+dir[2]);
       cosTh  = sqrt(cosTh2);
       for (int j=0;j<3;j++){ dir[j]/=cosTh;}
       dPt = fPt/fP*dP;
-      fP -= dP; fPt-= dPt;
-      if (fabs(fPt)<1e-2) { return 1313; }
-      fPLoss += dP;
+      double dPti = -fPti*(dPt/fPt);
+      fP += dP; fPt+= dPt; fPti+= dPti;
+      if (fabs(fPt)<kMinMom) { return kBadMom; }
+      fPLoss -= dP;
       lenxy2 = cosTh2*myLen*myLen;
       lenxy  = cosTh*myLen;
 
+      double B2old = fB[2];//????
       if (fMag) { //We have mag field 
 	(*fMag)(pos,fB);
 //      ===============
-        dC  = 1./fPt/fB[2] - fC;
+        dC  = fPti*fB[2] - fC;
       } else {
-	 dC = -fC*dPt/fPt;
+	dC = fC*dPti/fPti;
       }
-      fC -=dC;
+      fC +=dC;
       double dPhi = dC*lenxy /2.;
       double dH   = dC*lenxy2/6.;
-      pos[0]+=-dir[1]*dH;
-      pos[1]+= dir[0]*dH;
-      dir[0] += -dir[1]*dPhi;
-      dir[1] +=  dir[0]*dPhi;
+      pos[0]+= -dir[1]*dH;
+      pos[1]+=  dir[0]*dH;
+      dir[0]+= -dir[1]*dPhi;
+      dir[1]+=  dir[0]*dPhi;
       fHelx[1]->Set(pos,dir,fC);       
     }
     gGeoManager->SetCurrentPoint    ((double*)fHelx[1]->Pos());
     gGeoManager->SetCurrentDirection((double*)fHelx[1]->Dir());
     fNode[1] = gGeoManager->FindNode();
-    if (!fNode[1]) 		return 99;
-    if (!fEnd || (*fEnd)())	return 0;
-
+    if (!fNode[1]) 		return kOutScene;
+    if (!fEnd || (*fEnd)())	return kNormal;
     fNode[0] = fNode[1];
+    *fHelx[0] = *fHelx[1];
+    maxLen -= fInOutLen[1];
+    if (maxLen<=0) 		return kEndRange;
   }
-  return 131313;
+  return kFailed;
+}
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+int TGeoSwimDefaultEnd::operator()()
+{
+  mTit = 0;
+  mEnd = mKase;
+  switch(mKase) {
+  
+    case kDefault: return kDefault;		
+
+    case kNewVolu: {
+      TString myPath(gGeoManager->GetPath());
+      if (myPath == mPrevPath) {mEnd = kUndef;return kUndef;}
+      mPrevPath = myPath;      return kNewVolu;
+    }
+
+    case kNameVolu:{
+      mEnd = kUndef;
+      TString myPath(gGeoManager->GetPath());
+      if (myPath == mPrevPath) {return kUndef;};
+      mPrevPath = myPath;
+      
+      for (int j=0;mTits[j];j++) {
+        if (myPath.Index(mTits[j])<0) 	continue;
+        if (j == mJit) 			break;
+        mJit = j;
+        mTit = mTits[j];
+        mEnd = kNameVolu; break;
+      }
+      return mEnd;
+    }
+
+    default: assert(0 && "Wrong TGeoSwimEnd kase");
+
+  }
+  return kUndef;
 }
