4e7d427db6c1ea9c4a6a43830694343b1d9340b8
Yuri Fisyak <fisyak@bnl.gov>
2016-09-15 15:09:44 -0400

    STAR LIB built platforms
    Former-commit-id: 37a1459a37042e2012a0e3d6b7ce83ebdbb7af94

---
+++ bin/BuildList.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+use File::Basename;
+use Cwd;
+use Env;
+my @list = qw(
+	       gcc482.DEV2
+	       gcc482.DEV2.NODEBUG
+	       gcc482.NODEBUG.x8664
+	       gcc482.x8664
+	       gcc492.DEV2
+	       gcc492.DEV2.NODEBUG
+	       gcc492.DEV2.NODEBUG.x8664
+	       gcc492.DEV2.x8664
+	       gcc521.DEV2.NODEBUG.x8664
+	       gcc521.DEV2.x8664
+	       gcc620.DEV2.NODEBUG.x8664
+	       gcc620.DEV2.x8664
+	    );
+foreach my $item (@list) {
+	my $log = $item . ".log";
+	if (-r $log) {next;}
+   print "string:$item\n";
+}
