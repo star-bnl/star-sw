8baf6e39e90cf6876673f3281a449bf88c688116
Dmitri Smirnov <dsmirnov@bnl.gov>
2016-09-21 15:52:10 +0000

    picoDst v1.0: First release for STAR CVS

---
+++ StRoot/StPicoDstMaker/StPicoDst.cxx
@@ -0,0 +1,191 @@
+#include <map>
+#include "StPicoEvent/StPicoEvent.h"
+#include "StPicoEvent/StPicoTrack.h"
+#include "StPicoEvent/StPicoEmcTrigger.h"
+#include "StPicoEvent/StPicoBTowHit.h"
+#include "StPicoEvent/StPicoBTofHit.h"
+#include "StPicoEvent/StPicoMtdHit.h"
+#include "StPicoEvent/StPicoBEmcPidTraits.h"
+#include "StPicoEvent/StPicoBTofPidTraits.h"
+#include "StPicoEvent/StPicoMtdPidTraits.h"
+#include "StPicoDstMaker/StPicoDst.h"
+#include "StPicoDstMaker/StPicoDstMaker.h"
+
+TClonesArray** StPicoDst::picoArrays       = 0;
+
+//-----------------------------------------------------------------------
+void StPicoDst::unset()
+{
+  picoArrays   = 0;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::set(TClonesArray** thePicoArrays)
+{
+  picoArrays    = thePicoArrays;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::print() const
+{
+  LOG_INFO << "\n=========== event header =============\n\n";
+  LOG_INFO << " fill/run/event Id = " << event()->fillId() << "/" << event()->runId() << "/" << event()->eventId() << "\n";
+  LOG_INFO << " vertex = " << event()->primaryVertex() << "\n";
+  LOG_INFO << " refMult = " << event()->refMult() << " refMultFtpc = " << event()->refMultFtpc() << "\n";
+  LOG_INFO << " nVpdHits e/w = " << event()->nVpdHitsEast() << "/" << event()->nVpdHitsWest() << " nTofT0 = " << event()->nTofT0() << " vzVpd = " << event()->vzVpd() << "\n";
+  LOG_INFO << "=====================================\n\n";
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printTracks()
+{
+  if (numberOfTracks() == 0)
+  {
+    LOG_INFO << "No tracks found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ track list ( " << numberOfTracks() << " entries )\n\n";
+  for (UInt_t i_trk = 0; i_trk < numberOfTracks(); i_trk++)
+  {
+    LOG_INFO << "+++ track " << i_trk << "\n";
+    track(i_trk)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printTriggers()
+{
+
+  LOG_INFO << "=== EMC triggers ===" << "\n";
+
+  if (numberOfEmcTriggers() == 0)
+  {
+    LOG_INFO << "No triggers found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n\n+++++++++ trigger list ( " << numberOfEmcTriggers() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfEmcTriggers(); i_t++)
+  {
+    LOG_INFO << "+++ trigger " << i_t << "\n";
+    emcTrigger(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printBTOWHits()
+{
+  if (numberOfBTOWHits() == 0)
+  {
+    LOG_INFO << "No BTOWHit found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ BTOWHit list ( " << numberOfBTOWHits() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfBTOWHits(); i_t++)
+  {
+    LOG_INFO << "+++ btowHit " << i_t << "\n";
+    btowHit(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printBTofHits()
+{
+  if (numberOfBTofHits() == 0)
+  {
+    LOG_INFO << "No BTofHit found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ BTof list ( " << numberOfBTofHits() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfBTofHits(); i_t++)
+  {
+    LOG_INFO << "+++ btofHit " << i_t << "\n";
+    btofHit(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printMtdHits()
+{
+  if (numberOfMtdHits() == 0)
+  {
+    LOG_INFO << "No MtdHit found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ Mtd list ( " << numberOfMtdHits() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfMtdHits(); i_t++)
+  {
+    LOG_INFO << "+++ mtdHit " << i_t << "\n";
+    mtdHit(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printBEmcPidTraits()
+{
+  if (numberOfBEmcPidTraits() == 0)
+  {
+    LOG_INFO << "No BEmc pidTraits found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ BEmc pidTraits list ( " << numberOfBEmcPidTraits() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfBEmcPidTraits(); i_t++)
+  {
+    LOG_INFO << "+++ BEmcPidTraits " << i_t << "\n";
+    bemcPidTraits(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printBTofPidTraits()
+{
+  if (numberOfBTofPidTraits() == 0)
+  {
+    LOG_INFO << "No BTof pidTraits found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ BTof pidTraits list ( " << numberOfBTofPidTraits() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfBTofPidTraits(); i_t++)
+  {
+    LOG_INFO << "+++ EmcPidTraits " << i_t << "\n";
+    btofPidTraits(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+//-----------------------------------------------------------------------
+void StPicoDst::printMtdPidTraits()
+{
+  if (numberOfMtdPidTraits() == 0)
+  {
+    LOG_INFO << "No Mtd pidTraits found!" << endm;
+    return;
+  }
+
+  LOG_INFO << "\n+++++++++ Mtd pidTraits list ( " << numberOfMtdPidTraits() << " entries )\n\n";
+  for (UInt_t i_t = 0; i_t < numberOfMtdPidTraits(); i_t++)
+  {
+    LOG_INFO << "+++ mtdPidTraits " << i_t << "\n";
+    mtdPidTraits(i_t)->Print();
+    LOG_INFO << "\n";
+  }
+
+  LOG_INFO << endm;
+}
+
---
+++ StRoot/StPicoDstMaker/StPicoDstMaker.h
@@ -0,0 +1,159 @@
+#ifndef StPicoDstMaker_h
+#define StPicoDstMaker_h
+
+#include <vector>
+#include <utility>
+
+#include "TClonesArray.h"
+
+#include "StChain/StMaker.h"
+#include "StPicoDstMaker/StPicoEnumerations.h"
+#include "StPicoDstMaker/StPicoArrays.h"
+
+class TFile;
+class TTree;
+class StMuDst;
+class StMuTrack;
+class StEmcCollection;
+class StEmcPosition;
+class StEmcGeom;
+class StEmcRawHit;
+class StPicoDst;
+class StPicoEvent;
+
+
+
+class StPicoDstMaker : public StMaker
+{
+public:
+  StPicoDstMaker(char const* name = "PicoDst");
+  StPicoDstMaker(int mode, char const* fileName = "", char const* name = "PicoDst");
+  virtual ~StPicoDstMaker();
+
+  virtual Int_t Init();
+  virtual Int_t InitRun(Int_t const runnumber);
+  virtual Int_t Make();
+  virtual void  Clear(Option_t* option = "");
+  virtual Int_t Finish();
+
+  void printArrays();
+  void SetStatus(char const* arrType, int status);
+
+  /// Returns null pointer if no StPicoDst
+  StPicoDst* picoDst();
+  /// In read mode, returns pointer to the chain of .picoDst.root files
+  TChain* chain();
+  /// Returns pointer to the current TTree, the top level io structure
+  TTree* tree();
+
+  /// Sets the split level for the file and all branches. Please refer to the ROOT manual (http://root.cern.ch) for more info
+  void setSplit(int = 99);
+  /// Sets the buffer size for all branches.
+  void setBufferSize(int = 65536 * 4);
+  /// Sets the compression level for the file and all branches. 0 means no compression, 9 is the higher compression level.
+  void setCompression(int comp = 9);
+
+  void setVtxMode(int);
+
+protected:
+
+  void streamerOff();
+
+  void openWrite();
+  void write();
+  void closeWrite();
+  Int_t openRead();
+  void  read();
+  void setBranchAddresses();
+  void closeRead();
+  void setBranchAddresses(TChain*);
+
+  void buildEmcIndex();
+  void initEmc();
+  void finishEmc();
+
+  Bool_t initMtd(Int_t const runnumber);
+
+  void clearArrays();
+  void createArrays();
+
+  Int_t MakeRead();
+  Int_t MakeWrite();
+
+  void fillTracks();
+  void fillEvent();
+  void fillEmcTrigger();
+  void fillMtdTrigger();
+  void fillBTOWHits();
+  void fillBTofHits();
+  void fillMtdHits();
+
+ /**
+  * Returns various measurements by the BEMC and BSMD detectors corresponding to
+  * a given global track.
+  *
+  * param[in]   t        A global track
+  * param[out]  id >= 0  Indicates that a BEMC tower matching track t has been found
+  * param[out]  adc      The largest ADC value of a tower in the BEMC cluster matching track t
+  * param[out]  ene[0]   The highest energy tower in the BEMC cluster matching track t
+  * param[out]  ene[1]   The total energy of the BEMC cluster containing the matching tower
+  * param[out]  ene[2]   The energy deposited in the (closest) BEMC tower matching track t
+  * param[out]  ene[3]   The energy deposited in the second closest BEMC tower matching track t
+  * param[out]  ene[4]   The energy deposited in the third closest BEMC tower matching track t
+  * param[out]  d[0]     The distance [cm] along z from the track t projection onto BSMD to the BEMC cluster matching track t
+  * param[out]  d[1]     The distance [rad] along phi similar to d[0]
+  * param[out]  d[2]     The distance [rad] along eta between the track t projection onto BEMC and the matched BEMC tower center
+  * param[out]  d[3]     The distance [rad] along phi similar to d[2]
+  * param[out]  nep[0]   The number of eta strips in the BSMD cluster corresponding to the BEMC cluster matching track t
+  * param[out]  nep[1]   The number of phi strips in the BSMD cluster corresponding to the BEMC cluster matching track t
+  * param[out]  towid[]  Unique ids of the three BEMC towers identified for ene[2], ene[3], and ene[4]
+  */
+  bool getBEMC(StMuTrack* t, int* id, int* adc, float* ene, float* d, int* nep, int* towid);
+  bool selectVertex();
+
+  enum ioMode {ioRead, ioWrite};
+
+  StMuDst*   mMuDst;
+  StEmcCollection* mEmcCollection;
+  StEmcPosition*   mEmcPosition;
+  StEmcGeom*       mEmcGeom[4];
+  StEmcRawHit*     mEmcIndex[4800];
+  StPicoDst* mPicoDst;
+  Float_t    mBField;
+
+  Int_t      mIoMode;         //! I/O mode:  0: - read,   1: - write
+  Int_t      mVtxMode;
+
+  TString   mInputFileName;        //! *.list - MuDst or picoDst
+  TString   mOutputFileName;       //! FileName
+  TFile*    mOutputFile;
+
+  TChain*   mChain;
+  TTree*    mTTree;
+
+  int mEventCounter;
+  int mSplit;
+  int mCompression;
+  int mBufferSize;
+
+  // MTD map from backleg to QT
+  Int_t  mModuleToQT[30][5];        // Map from module to QT board index
+  Int_t  mModuleToQTPos[30][5];     // Map from module to the position on QA board
+  Int_t  mQTtoModule[8][8];         // Map from QT board to module
+  Int_t  mQTSlewBinEdge[8][16][8];  // Bin Edge for QT slewing correction
+  Int_t  mQTSlewCorr[8][16][8];     // QT Slewing correction
+
+  TClonesArray*   mPicoArrays[StPicoArrays::NAllPicoArrays];
+  char            mStatusArrays[StPicoArrays::NAllPicoArrays];
+
+  ClassDef(StPicoDstMaker, 0)
+};
+
+inline StPicoDst* StPicoDstMaker::picoDst() { return mPicoDst; }
+inline TChain* StPicoDstMaker::chain() { return mChain; }
+inline TTree* StPicoDstMaker::tree() { return mTTree; }
+inline void StPicoDstMaker::setSplit(int split) { mSplit = split; }
+inline void StPicoDstMaker::setCompression(int comp) { mCompression = comp; }
+inline void StPicoDstMaker::setBufferSize(int buf) { mBufferSize = buf; }
+inline void StPicoDstMaker::setVtxMode(int const vtxMode) { mVtxMode = vtxMode; }
+#endif
