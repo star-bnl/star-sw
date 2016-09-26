c8f6c6a00aeebcce5d1e4e5b09b364a39620f87e
mstftsm <>
2016-09-26 19:53:44 +0000

    Add StPicoTrack::dNdx()

--- StRoot/StPicoEvent/StPicoTrack.cxx
+++ StRoot/StPicoEvent/StPicoTrack.cxx
@@ -12,7 +12,7 @@ StPicoTrack::StPicoTrack() : TObject(),
   mId(0),
   mChi2(std::numeric_limits<unsigned short>::max()),
   mPMomentum(0., 0., 0.), mGMomentum(0., 0., 0.), mOrigin(0., 0., 0.),
-  mDedx(0.), mNHitsFit(0), mNHitsMax(0), mNHitsDedx(0), mCharge(0),
+  mDedx(0.), mDnDx(0.), mNHitsFit(0), mNHitsMax(0), mNHitsDedx(0), mCharge(0),
   mNSigmaPion(std::numeric_limits<short>::max()),
   mNSigmaKaon(std::numeric_limits<short>::max()),
   mNSigmaProton(std::numeric_limits<short>::max()),
@@ -46,6 +46,7 @@ StPicoTrack::StPicoTrack(StMuTrack const* const gTrk, StMuTrack const* const pTr
     mOrigin = gHelix.origin();
 
     mDedx      = gTrk->dEdx();
+    mDnDx      = gTrk->probPidTraits().dNdxFit();
     int flag = gTrk->flag();
     if (flag / 100 < 7) // TPC tracks
     {
