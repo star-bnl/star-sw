e74b759486591c54749fb07211704d32beefdcd8
cvs2git <cvs2git>
2016-10-19 14:54:36 +0000

    This commit was manufactured by cvs2git to create tag 'SL16j'.

--- StRoot/macros/test/test7.C
+++
@@ -1,8 +0,0 @@
-{
-St_XDFFile  xdf("/star/mds/data/SD98/auau200/evg/central/hijing/set0001/regular/auau_ce_b0-2_4801_5000.xdf");
-// StXDFFile  xdf("e:/temp/auau_ce_b0-2_4801_5000.xdf");
- St_Event *event = xdf.NextEvent();
- St_DataSet *set=0;
- St_Table *table=0;
-}
-
