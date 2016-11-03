c3d081dbf89d43391220c0c310ded2c6be9596bd
Jason C. Webb <jwebb@bnl.gov>
2016-11-03 13:49:01 +0000

    Integrate EPD into framework.

--- StRoot/St_geant_Maker/St_geant_Maker.cxx
+++ StRoot/St_geant_Maker/St_geant_Maker.cxx
@@ -1,5 +1,8 @@
-// $Id: St_geant_Maker.cxx,v 1.162 2016/06/21 16:12:24 jwebb Exp $
+// $Id: St_geant_Maker.cxx,v 1.163 2016/11/03 13:49:01 jwebb Exp $
 // $Log: St_geant_Maker.cxx,v $
+// Revision 1.163  2016/11/03 13:49:01  jwebb
+// Integrate EPD into framework.
+//
 // Revision 1.162  2016/06/21 16:12:24  jwebb
 // Compiler warning: nin set but not used.
 //
@@ -604,6 +607,7 @@
 #include "g2t/St_g2t_etr_Module.h"
 #include "g2t/St_g2t_hca_Module.h"
 #include "g2t/St_g2t_fts_Module.h"
+#include "g2t/St_g2t_epd_Module.h"
 
 #include "St_db_Maker/St_db_Maker.h"
 #include "TUnixTime.h"
@@ -1515,6 +1519,15 @@ Int_t St_geant_Maker::Make() {
     St_g2t_fts_hit *g2t_fts_hit = new St_g2t_fts_hit("g2t_fts_hit",nhits);
     m_DataSet->Add(  g2t_fts_hit );
     /*iRes =*/ g2t_fts( g2t_track, g2t_fts_hit );
+  };
+
+  nhits=0;
+
+  geant3 -> Gfnhit("EPDH","EPAD", nhits); 
+  if ( nhits > 0 ) {
+    St_g2t_epd_hit *g2t_epd_hit = new St_g2t_epd_hit("g2t_epd_hit",nhits);
+    m_DataSet->Add(  g2t_epd_hit );
+    /*iRes =*/ g2t_epd( g2t_track, g2t_epd_hit );
   };
 
 
