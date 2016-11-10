950db1b4cfae07a6afc794a1b478d9516b226518
perev <>
2016-11-10 02:14:22 +0000

    Increase accuracy of digitalization of layer radius

--- StRoot/Sti/StiPlacement.cxx
+++ StRoot/Sti/StiPlacement.cxx
@@ -126,7 +126,7 @@ ostream& operator<<(ostream& os, const StiPlacement& p)
 void StiPlacement::setLayerRadius(float radius_)
 {
 static const double kMinRad=0.1,kMaxRad=200;
-static const double kLogStep=(log(kMaxRad)-log(kMinRad))/1000;
+static const double kLogStep=(log(kMaxRad)-log(kMinRad))/10000;
    int nStp = int(log(radius_+kMinRad)/kLogStep);
    layerRadius = exp(nStp*kLogStep)-kMinRad; 
 }
