68fecd277e414f099fe2eb0ba3d7da90ac93c9c3
Victor Perevoztchikov <perev@bnl.gov>
2016-11-29 16:56:18 +0000

    Rename Fts files for future

--- StRoot/StvUtil/StvFtsHitErrCalculator.h.C
+++ StRoot/StvUtil/StvFtsHitErrCalculator.h.C
@@ -1,6 +1,8 @@
 #ifndef __StvFtsHitErrCalculatorulator_h_
 #define __StvFtsHitErrCalculatorulator_h_
+#include "StEvent/StEnumerations.h"
 #include "StvUtil/StvHitErrCalculator.h"
+
 class StvHit;
 
 class StvFtsHitErrCalculator : public StvHitErrCalculator {
