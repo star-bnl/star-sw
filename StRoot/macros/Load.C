f153726a9e728be8a5b956b59ddc8d482659e821
Jerome Lauret <jeromel@bnl.gov>
2016-12-01 15:10:44 +0000

    StEvent depends on StBichsel - fixed

--- StRoot/macros/Load.C
+++ StRoot/macros/Load.C
@@ -1,10 +1,10 @@
-// $Id: Load.C,v 1.22 2012/06/21 19:00:06 fisyak Exp $
+// $Id: Load.C,v 1.23 2016/12/01 15:10:44 jeromel Exp $
 //=======================================================================
 // owner: Yuri Fisyak
 // what it does: 
 //=======================================================================
 
-void Load(Char_t *loadList="St_base,St_Tables,StChain,StDetectorDbMaker,StEvent,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StTriggerDataMaker,StEventUtilities,StBFChain"){
+void Load(Char_t *loadList="St_base,St_Tables,StChain,StDetectorDbMaker,StBichsel,StEvent,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StTriggerDataMaker,StEventUtilities,StBFChain"){
   TString opt(loadList);
   TString separator = "[^ ;,]+";
   TObjArray *array = opt.Tokenize(separator);
