9b200a5f76e237f33511881debcf77183c602d69
Yuri Fisyak <fisyak@bnl.gov>
2016-12-05 11:46:23 -0500

    Merge
    Former-commit-id: 42aa8dd7e3ed68778efc7848ee3945f0bf7d68fb

--- StRoot/macros/Load.C
+++ StRoot/macros/Load.C
@@ -1,11 +1,11 @@
-// $Id: Load.C,v 1.22 2012/06/21 19:00:06 fisyak Exp $
+// $Id: Load.C,v 1.23 2016/12/01 15:10:44 jeromel Exp $
 //=======================================================================
 // owner: Yuri Fisyak
 // what it does: 
 //=======================================================================
 
-void Load(Char_t *loadList="St_base,StChain"){
- TString opt(loadList);
+void Load(Char_t *loadList="St_base,St_Tables,StChain,StDetectorDbMaker,StBichsel,StEvent,StTpcDb,StUtilities,StDbLib,StDbBroker,St_db_Maker,StTriggerDataMaker,StEventUtilities,StBFChain"){
+  TString opt(loadList);
   TString separator = "[^ ;,]+";
   TObjArray *array = opt.Tokenize(separator);
   TIter next(array);
