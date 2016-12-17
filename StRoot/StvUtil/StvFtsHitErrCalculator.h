68d82150cf83f1b143a6a0c760a33760da65c2e0
Victor Perevoztchikov <perev@bnl.gov>
2016-12-17 01:00:16 +0000

    Simplification. used StvHitRr class now

--- StRoot/StvUtil/StvFtsHitErrCalculator.h
+++ StRoot/StvUtil/StvFtsHitErrCalculator.h
@@ -5,24 +5,12 @@
 
 class StvHit;
 
-class StvFtsHitErrCalculator : public StvHitErrCalculator {
+class StvFtsHitErrCalculator : public StvHitRrCalculator {
 
 public:	
-  StvFtsHitErrCalculator(const char *name="FtsHitErrs"):StvHitErrCalculator(name,2){};
-int CalcDcaErrs(const StvHit* hit,double hRR[3]);
-int CalcDetErrs(const float hiPos[3],const float hiDir[3][3],double hRR[3]);
-int CalcDcaErrs(const float hiPos[3],const float hiDir[3][3],double hRR[3])
-   {return StvHitErrCalculator::CalcDcaErrs(hiPos,hiDir,hRR);}
+  StvFtsHitErrCalculator(const char *name="FtsHitErrs"):StvHitRrCalculator(name){};
 
 protected:
-enum {
-kRxyErr  	=0, 	/* Intrinsic resolution, padrow or rxy direction		*/
-kPhiErr  	=1}; 	/* Intrinsic resolution, Phi direction			*/
-
-static double mgRPhiErrs[2]; 	//these errors are used only for CalcDetErrs
-				//when the concrete hit is not defined
-
-
 ClassDef(StvFtsHitErrCalculator,0)
 };
 
