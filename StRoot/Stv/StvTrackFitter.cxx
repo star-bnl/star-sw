ae9774fb86e574e0cca20de5b3efe44ab10c7521
Victor Perevoztchikov <perev@bnl.gov>
2016-12-17 01:15:37 +0000

    Static member fgInst removed

--- StRoot/Stv/StvTrackFitter.cxx
+++ StRoot/Stv/StvTrackFitter.cxx
@@ -2,17 +2,14 @@
 #include <assert.h>
 #include "StvTrackFitter.h"
 ClassImp(StvTrackFitter)
-StvTrackFitter *StvTrackFitter::fgInst=0;
 //______________________________________________________________________________
 StvTrackFitter::StvTrackFitter(const char *name):TNamed(name,"")
 {
-  assert(!fgInst);
-  fgInst=this;
   Clear();
 }
 //______________________________________________________________________________
 StvTrackFitter::~StvTrackFitter()
-{assert(this==fgInst); fgInst=0;}
+{;}
 
 //______________________________________________________________________________
 void StvTrackFitter::Clear(const char *)
