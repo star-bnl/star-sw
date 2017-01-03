28a094c30fe3e0a62c086bac0ba06c71a04f0092
Victor Perevoztchikov <perev@bnl.gov>
2017-01-03 17:38:13 +0000

    1. mPos and mLim now 3dim instead of 5 (x+y & x-y dimensions removed. No gain) 2  mDir now is (3x3) Orthogonal to track directions vectors added 3. Methot TestIt added. It is called at the beginning to test geo selection

--- StRoot/StvSeed/StvConeRejector.cxx
+++ StRoot/StvSeed/StvConeRejector.cxx
@@ -33,6 +33,20 @@ inline static void Sub(float A[3],const float B[3],const float C[3])
    A[0]=B[0]-C[0]; A[1]=B[1]-C[1]; A[2]=B[2]-C[2];
 }
 //_____________________________________________________________________________
+inline static void Add(float A[3],const float B[3],const float C[3])
+{   
+   A[0]=B[0]+C[0]; A[1]=B[1]+C[1]; A[2]=B[2]+C[2];
+}
+//_____________________________________________________________________________
+inline static void Cro(float A[3],const float B[3],const float C[3])
+{   
+// TVector3(fY*p.fZ-p.fY*fZ, fZ*p.fX-p.fZ*fX, fX*p.fY-p.fX*fY);
+   A[0] = B[1]*C[2]-C[1]*B[2];
+   A[1] = B[2]*C[0]-C[2]*B[0];
+   A[2] = B[0]*C[1]-C[0]*B[1];
+
+}
+//_____________________________________________________________________________
 StvConeRejector::StvConeRejector()
 {
   memset(mBeg,0,mEnd-mBeg+1);
@@ -49,31 +63,32 @@ void StvConeRejector::Reset(const float pos[3],const float dir[3]
   mR2   = mRxy2 + mPos[2]*mPos[2];
   mRxy  = sqrt(mRxy2);
   mErr = SEED_ERR(mRxy);
+  double norL=0,norR=0;
 
   if (dir) { 	//Direction defined
-    Cop(mDir,dir);
+    Cop(mDir[0],dir);
     mThet = (theta) ? theta : kFstAng*M_PI/180;
-  } else   {	//Estimate dir as direction to 0,0,0
+  } else   {	//Estimate dir as median direction to (0,0,-kZRange) and (0,0,kZRange)
 
-    double norL = 1./sqrt(mRxy2+pow(mPos[2]-kZRange,2)); 
-    double norR = 1./sqrt(mRxy2+pow(mPos[2]+kZRange,2)); 
-    double norQ = (norL+norR);
+    norL = sqrt(mRxy2+pow(mPos[2]-kZRange,2)); 
+    norR = sqrt(mRxy2+pow(mPos[2]+kZRange,2)); 
+    double norQ = (1./norL+1./norR);
 
-    mDir[0]= mPos[0]*norQ;
-    mDir[1]= mPos[1]*norQ;
-    mDir[2]= mPos[2]*norQ +kZRange*(norR-norL);
-    norQ = 1./sqrt(mDir[0]*mDir[0]+mDir[1]*mDir[1]+mDir[2]*mDir[2]);
-    for (int i=0;i<3;i++) {mDir[i]*=(-norQ);}
-    mThet = (mR2 -(kZRange*kZRange))*norL*norR;
+    mDir[0][0]= mPos[0]*norQ;
+    mDir[0][1]= mPos[1]*norQ;
+    mDir[0][2]= mPos[2]*norQ +kZRange*(1./norR-1./norL);
+    norQ = 1./sqrt(Dot(mDir[0],mDir[0]));
+    Mul(mDir[0],-norQ,mDir[0]);
+    mThet = (mR2 -(kZRange*kZRange))/(norL*norR);
     mThet = 0.5*acos(mThet);
 
     assert(mThet< M_PI/2);
   }
   if (rad ) { mOutRad = rad;}	//rad is defined, use it
   else      { 			//rad is no defined, estimate it
-    mOutRad = sqrt(mR2)/kDivLen;
+    mOutRad = (norL>norR) ? norL:norR;
+    mOutRad/= kDivLen;
     if (mOutRad<kMinLen) mOutRad=kMinLen;
-//??    if (mOutRad>kMaxLen) mOutRad=kMaxLen;
   }
   mOutRad2 = (mOutRad+mErr)*(mOutRad+mErr);
   mSin = sin(mThet);
@@ -89,17 +104,24 @@ void StvConeRejector::Prepare()
 static const double kSqrHlf = sqrt(0.5);
   double along = mOutRad*mCos;
   double ortho = mOutRad*mSin;
-  mPos[3] = ( mPos[0] + mPos[1])*kSqrHlf;
-  mPos[4] = (-mPos[0] + mPos[1])*kSqrHlf;
-  mDir[3] = ( mDir[0] + mDir[1])*kSqrHlf;
-  mDir[4] = (-mDir[0] + mDir[1])*kSqrHlf;
+  mDir[1][0]= -mDir[0][1];
+  mDir[1][1]=  mDir[0][0];
+  mDir[1][2]= 0;
+  double qwe = sqrt(Dot(mDir[1],mDir[1]));
+  if (qwe<1e-11) { //Strictly along Z?
+    mDir[1][0]=1; mDir[1][1]=0; mDir[1][2]=0;
+  } else {
+    Mul(mDir[1],1/qwe,mDir[1]);
+  }
+  Cro(mDir[2],mDir[0],mDir[1]);
 
   
-  for (int i=0;i<5;i++) {
+  for (int i=0;i<3;i++) {
     mLim[0][i] = -mErr;
     mLim[1][i] =  mErr;
-    float qwe = along*mDir[i];
-    float nor = sqrt(fabs(1-mDir[i]*mDir[i]));
+    float qwe = along*mDir[0][i];			//Shift to the base of cone
+//	Norm sqrt(Bx*Bx+Cx*Cx)==sqrt(1-Ax*Ax)
+    float nor = sqrt(fabs(1-mDir[0][i]*mDir[0][i])); 	//Norm 
     float asd = ortho*nor;
     float lim = qwe - asd - mErr;
     if (mLim[0][i]>lim)	mLim[0][i] = lim;
@@ -107,8 +129,8 @@ static const double kSqrHlf = sqrt(0.5);
     if (mLim[1][i]<lim)	mLim[1][i] = lim;
 
 		// if spheric part of "cone" is important
-    if      (mDir[i]> mCos) {mLim[1][i] =  mOutRad;} 
-    else if (mDir[i]<-mCos) {mLim[0][i] = -mOutRad;}
+    if      (mDir[0][i]> mCos) {mLim[1][i] =  mOutRad;} 
+    else if (mDir[0][i]<-mCos) {mLim[0][i] = -mOutRad;}
       
       		// Move to global system 
     mLim[0][i]+= mPos[i];
@@ -116,15 +138,12 @@ static const double kSqrHlf = sqrt(0.5);
   }
 static int mytimes = 20;
   if (mytimes >0) {
+    TestIt();
     mytimes--;
     double fullVol = M_PI*200*200*400/100;
     double vol1=1/fullVol; for (int i=0;i<3;i++) 	{ vol1*=mLim[1][i]-mLim[0][i];}
-    double vol2=1/fullVol; for (int i: {2,3,4}  ) 	{ vol2*=mLim[1][i]-mLim[0][i];}
-    printf (" vol1 = %g Vol2 = %g\n",vol1,vol2);
+    printf (" vol1 = %g \n",vol1);
   }
-
-
-
 }   
 //_____________________________________________________________________________
 int StvConeRejector::Reject(const float x[3]) const
@@ -132,11 +151,37 @@ int StvConeRejector::Reject(const float x[3]) const
   float xx[3];
   Sub(xx,x,mPos);
   float r2 = xx[0]*xx[0]+xx[1]*xx[1]+xx[2]*xx[2];
-  if (r2>mOutRad2)	return 1;
-  float myX = Dot(xx,mDir);
-  if (myX <0) 	return 2;
+  if (r2>mOutRad2)		return 1;
+  float myX = Dot(xx,mDir[0]);
+  if (myX <0) 			return 2;
 
   float myY2 = (r2-myX*myX);
-  if (myY2 >myX*myX*mTan2) return 3;
+  if (myY2 >myX*myX*mTan2) 	return 3;
   return 0;
 }
+#include "TRandom.h"
+//_____________________________________________________________________________
+int StvConeRejector::TestIt() const
+{
+  float x[3];
+  
+  for (int i=0;i<20;i++) {
+  
+    int ifail = 0;
+    double r = (mOutRad*1.5)*gRandom->Rndm();
+    if (r >mOutRad) ifail+=1;
+    double sThet = mSin*1.1; if(sThet>=0.99) sThet=0.99;
+    sThet = sThet*gRandom->Rndm();
+    if (sThet>mSin) ifail+=2;
+    double cThet = sqrt(1.-sThet*sThet);
+    double alfa = M_PI*2*gRandom->Rndm();
+    for (int j = 0;j<3;j++) {
+      x[j] = r*(mDir[0][j]*cThet + sThet*(mDir[1][j]*cos(alfa)+mDir[2][j]*sin(alfa)));
+    }
+    if (Dot(x,x)>mOutRad*mOutRad)  ifail+=4;
+    Add(x,x,mPos);
+    int irej = Reject(x);
+    assert((!!irej) == (!!ifail));
+   }    
+   return 0;
+} 
