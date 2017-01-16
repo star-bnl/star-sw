d517930f35d4f76f215f3abdecf56cdf87d7d551
Christopher Dilks <cjd5150@psu.edu>
2017-01-16 21:39:13 +0000

    added hit map to FMS Jevp plots

--- OnlTools/Jevp/StJevpBuilders/fmsBuilder.h
+++ OnlTools/Jevp/StJevpBuilders/fmsBuilder.h
@@ -4,12 +4,34 @@
 #include <list>
 #include <map>
 #include <math.h>
+#include <stdio.h>
+#include <memory>
+#include <sstream>
+#include <utility>
 
+#include "StEvent/StTriggerData.h"
+
+#include "DAQ_READER/daqReader.h"
+#include "DAQ_READER/daq_dta.h"
+#include "DAQ_READER/daq_det.h"
+
+#include <TH1F.h>
+#include <TH2F.h>
+
+#include "Jevp/StJevpPlot/JevpPlot.h"
 #include "JevpBuilder.h"
+
+//#include "StRoot/StFmsDbMaker/StFmsDbMaker.h" // disabled
+
+
 
 class TH1;
 class daqReader;
 class JevpPlot;
+//class StFmsDbMaker; // disabled
+
+const Int_t N_QTCR = 5;
+const Int_t N_QTIDX = 353;
 
 /**
  Builder creating and filling plots for FMS in Jevp.
@@ -47,9 +69,16 @@ public:
    Called once per event
    */
   void event(daqReader*);
-  
+
   static void main(int argc, char* argv[]);
+
+  // fms cell --> map coordinates -- DEPRECATED
+  //Float_t xPos(Int_t nstb_,Int_t row_,Int_t col_);
+  //Float_t yPos(Int_t nstb_,Int_t row_,Int_t col_);
   
+  void initMaps();
+
+
 protected:
 
   // The collection of JevpPlots to fill.
@@ -60,6 +89,23 @@ protected:
   // Histograms are owned by a corresponding JevpPlot in mPlots.
   typedef std::map<int, TH1*> TH1PtrMap;
   TH1PtrMap mHists;
+
+  // channel maps
+  Int_t nstb_map[N_QTCR][N_QTIDX];
+  Int_t chan_map[N_QTCR][N_QTIDX];
+  Int_t row_map[N_QTCR][N_QTIDX];
+  Int_t col_map[N_QTCR][N_QTIDX];
+
+
+  // hit map
+  //StFmsDbMaker * fmsdb; // disabled
+  TH2D * hitmap[2][2]; // [kLarge/kSmall] [kADC/kLED]
+  //TH2D * fmschan[2]; // deprecated
+  int crate;
+  int index;
+  unsigned short adc;
+  Int_t detid,nstb,cellchan;
+  Int_t row,col;
 
   ClassDef(fmsBuilder, 1);
 };
