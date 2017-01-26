a08233135879814f6d6806be6b16329b61df61b3
Yuri Fisyak <fisyak@bnl.gov>
2017-01-26 18:42:15 -0500

    Adjust for VMC
    Former-commit-id: ff5597d60974f6293a49c653913e72db6681a65f

--- macros/Run1Ev.C
+++ macros/Run1Ev.C
@@ -1,8 +1,8 @@
 void Run1Ev(Int_t NEvents=1, Int_t iD = 5, 
 	    Double_t pTlow=1,Double_t pThigh=1,
-	    Double_t Ylow=0.1, Double_t Yhigh=0.1,
-	    Double_t Philow=0.0, Double_t Phihigh=0.0,
-	    Double_t Zlow=2, Double_t Zhigh=2, Int_t Npart = 1) {
+	    Double_t Ylow=-0.1, Double_t Yhigh=0.1,
+	    Double_t Philow=-.1, Double_t Phihigh=0.1,
+	    Double_t Zlow=0, Double_t Zhigh=0, Int_t Npart = 1) {
   if ( gClassTable->GetID("TGiant3") >= 0) { // root4star
     if (gClassTable->GetID("St_geant_Maker") < 0) {
       cout << "You have to use root4star with St_geant_Maker already loaded" << endl; 
@@ -28,6 +28,22 @@ void Run1Ev(Int_t NEvents=1, Int_t iD = 5,
     //                          Do("CUTS     1e-5   1e-5   1e-3  1e-14   1e-3  1e-3  1e-3  1e-3  1e-3   1e-3     10");
     //                          Do("CUTS     1e-5   1e-5   1e-3  1e-14   1e-3  1e-3  1e-3  1e-3  1e-3   1e-3     1e-3");
     St_geant_Maker::instance()->Do("CUTS     1e-5   1e-5   1e-3  1e-14   1e-3  1e-3  1e-3  1e-3  1e-3   1e-3     1e3");
+    St_geant_Maker::instance()->Do("DCAY 0");
+    St_geant_Maker::instance()->Do("ANNI 0");
+    St_geant_Maker::instance()->Do("BREM 0");
+    St_geant_Maker::instance()->Do("COMP 0");
+    St_geant_Maker::instance()->Do("HADR 0");
+    St_geant_Maker::instance()->Do("MUNU 0");
+    St_geant_Maker::instance()->Do("PAIR 0");
+    St_geant_Maker::instance()->Do("PFIS 0");
+    St_geant_Maker::instance()->Do("PHOT 0");
+    St_geant_Maker::instance()->Do("RAYL 0");
+    St_geant_Maker::instance()->Do("LOSS 4"); // no fluctuations 
+      //    St_geant_Maker::instance()->Do("LOSS 1"); // with delta electron above dcute
+    St_geant_Maker::instance()->Do("DRAY 0");
+    St_geant_Maker::instance()->Do("MULS 0");
+    St_geant_Maker::instance()->Do("STRA 0");
+
     St_geant_Maker::instance()->Do("physi");
 
     /*
@@ -54,13 +70,6 @@ void Run1Ev(Int_t NEvents=1, Int_t iD = 5,
      BACK  (0)             - number of pile-up bunchs to select (relative to the trigger one)
      
      To change default values use GSFLAG command.  */
-#if 0
-    St_geant_Maker::instance()->Do("mode  svtt simu 2");
-    St_geant_Maker::instance()->Do("mode  tpce simu 2");
-    St_geant_Maker::instance()->Do("mode  ftpc simu 2");
-    St_geant_Maker::instance()->Do("detp  trac DCAY 210 210 0.1 0.01");
-    St_geant_Maker::instance()->Do("mode  g2tm prin 1;");
-#endif
 #if 1
     St_geant_Maker::instance()->Do("debug on");
     St_geant_Maker::instance()->Do("swit 1 2");
@@ -71,14 +80,6 @@ void Run1Ev(Int_t NEvents=1, Int_t iD = 5,
     StVMCMaker *geant = chain->Maker("geant");
     geant->SetDebug(1);
     // geant->SetDebug(2); // Print StarVMCApplication::MisalignGeometry()
-#if 0
-    StMaker *svtDb = chain->Maker("svtDb");
-    if (svtDb) svtDb->SetDebug(2);
-    StMaker *ssdDb = chain->Maker("ssdDb");
-    if (ssdDb) ssdDb->SetDebug(2);
-#endif
-    chain->SetFlavor("simu","svtRDOstripped"); // disable time depended SVT activity status
-#if 1
     if (StarVMCApplication::Instance()) {
       StarMCSimplePrimaryGenerator *gener = (StarMCSimplePrimaryGenerator *) StarVMCApplication::Instance()->GetPrimaryGenerator();
       if ( gener && ! gener->IsA()->InheritsFrom( "StarMCSimplePrimaryGenerator" ) ) {
@@ -92,7 +93,32 @@ void Run1Ev(Int_t NEvents=1, Int_t iD = 5,
       cout << "Set StarMCSimplePrimaryGenerator" << endl;
       //      StarVMCApplication::Instance()->DoMisAlignment(kFALSE);
       //      geant->SetSetAttr("phys_off",kTRUE); // physics off
-      geant->SetMode(100); // physics off
+      gMC->SetProcess("DCAY", 0);
+      gMC->SetProcess("ANNI", 0);
+      gMC->SetProcess("BREM", 0);
+      gMC->SetProcess("COMP", 0);
+      gMC->SetProcess("HADR", 0);
+      gMC->SetProcess("MUNU", 0);
+      gMC->SetProcess("PAIR", 0);
+      gMC->SetProcess("PFIS", 0);
+      gMC->SetProcess("PHOT", 0);
+      gMC->SetProcess("RAYL", 0);
+      gMC->SetProcess("LOSS", 4); // no fluctuations 
+      gMC->SetProcess("DRAY", 0);
+      gMC->SetProcess("MULS", 0);
+      gMC->SetProcess("STRA", 0);
+      gMC->SetCut("CUTGAM",	1e-5  );
+      gMC->SetCut("CUTELE", 	1e-5  );
+      gMC->SetCut("CUTHAD", 	1e-3  );
+      gMC->SetCut("CUTNEU", 	1e-14 );
+      gMC->SetCut("CUTMUO", 	1e-3  );
+      gMC->SetCut("BCUTE", 	1e-3  );
+      gMC->SetCut("BCUTM", 	1e-3  );
+      gMC->SetCut("DCUTE", 	1e-3  );
+      gMC->SetCut("DCUTM", 	1e-3  );
+      gMC->SetCut("PPCUTM", 	1e-3  );
+      gMC->SetCut("TOFMAX", 	1e3);
+      gMC->BuildPhysics();
       if (gMC && gMC->IsA()->InheritsFrom("TGeant3TGeo")) {
 	TGeant3TGeo *geant3 = (TGeant3TGeo *)gMC;
 	Gcflag_t* cflag = geant3->Gcflag();
@@ -102,16 +128,10 @@ void Run1Ev(Int_t NEvents=1, Int_t iD = 5,
 	cflag->iswit[1] = 2;
 	cflag->iswit[2] = 2; 
 	StGeanePropagator::instance()->SetDebug(1);
-#if 0
-	StiKalmanTrackNode::SetDebug(8);
-	StiKalmanTrack::SetDebug(2);
-#endif
       }
     } else {
       cout << "You have to use root4star with St_geant_Maker already loaded" << endl;
     }
-#else
-#endif
   }
 #if 1
   // Old Sti
@@ -120,10 +140,6 @@ void Run1Ev(Int_t NEvents=1, Int_t iD = 5,
   StiKalmanTrackFitter::setDebug(1);
   StiKalmanTrack::setDebug(2);
   StiTrackNodeHelper::setDebug(8);
-#endif
-#if 0
-  if (chain->Maker("svt_hits")) chain->Maker("svt_hits")->SetDebug(2);
-  if (chain->Maker("SsdFastSim")) chain->Maker("SsdFastSim")->SetDebug(2);
 #endif
   chain->EventLoop(NEvents);
 }
