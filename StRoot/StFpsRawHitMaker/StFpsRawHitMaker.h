35cda81fdb16362a6f706d6eedcef5986fb3acb8
Akio Ogawa <akio@bnl.gov>
2017-01-30 17:49:28 +0000

    adding FPost

--- StRoot/StFpsRawHitMaker/StFpsRawHitMaker.h
+++ StRoot/StFpsRawHitMaker/StFpsRawHitMaker.h
@@ -4,8 +4,11 @@
 //   This is the hit maker for the FPS data. 
 //   It makes use of its base class functions to read daq files into the StEvent Data structure.
 // 
-//  $Id: StFpsRawHitMaker.h,v 1.2 2015/09/02 14:55:18 akio Exp $
+//  $Id: StFpsRawHitMaker.h,v 1.3 2017/01/30 17:49:28 akio Exp $
 //  $Log: StFpsRawHitMaker.h,v $
+//  Revision 1.3  2017/01/30 17:49:28  akio
+//  adding FPost
+//
 //  Revision 1.2  2015/09/02 14:55:18  akio
 //  Modified to work with StFmsDbMaker
 //
@@ -26,7 +29,7 @@ public:
   void setPrePost(int v) {mPrePost=v;} //if 0 (default) takes only triggered crossing. none-0 accept prepost as well,
   
 private:
-  enum {kFpsDetId=14, kFpsQtCrate=8};
+  enum {kFpsQtCrate=8, kFpostQtCrate=9};
 
   int mPrePost;
   Int_t prepareEnvironment();
