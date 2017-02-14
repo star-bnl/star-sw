e38fa5e73ac4f8b24090e7b9aaff5bc86a645138
cvs2git <cvs2git>
2017-02-14 23:40:37 +0000

    This commit was manufactured by cvs2git to create branch 'SL17d_v1Patch'.

--- StRoot/StJetMaker/towers/StjBEMCNull.cxx
+++
@@ -1,6 +0,0 @@
-// $Id: StjBEMCNull.cxx,v 1.1 2008/11/27 07:35:21 tai Exp $
-// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-#include "StjBEMCNull.h"
-
-ClassImp(StjBEMCNull)
- 
--- StRoot/Stl3Util/gl3/gl3True.cxx
+++
@@ -1,9 +0,0 @@
-#include "gl3True.h"
-
-
-int gl3True::decide()
-{
-
-  return 1; // always true
-}
-
