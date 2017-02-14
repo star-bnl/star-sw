fcc6043f67a7dbf36f5d5ac506aa20cab99a1507
cvs2git <cvs2git>
2017-02-14 23:40:36 +0000

    This commit was manufactured by cvs2git to create branch 'SL17d_emb'.

--- StRoot/StDetectorDbMaker/StDetectorDbFTPCVoltageStatus.h
+++
@@ -1,5 +0,0 @@
-#ifndef StDetectorDbFTPCVoltageStatus_h
-#define StDetectorDbFTPCVoltageStatus_h
-#include "St_ftpcVoltageStatusC.h"
-typedef St_ftpcVoltageStatusC StDetectorDbFTPCVoltageStatus;
-#endif
