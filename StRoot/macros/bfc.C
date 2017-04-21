0856a259d96e2ff931f67e6994f4514de0bd2ae8
Yuri Fisyak <fisyak@bnl.gov>
2017-04-21 19:37:02 -0400

    Freeze
    Former-commit-id: 91a085f7dc04f05aa63d01948ad4305fce978d5d

--- StRoot/macros/bfc.C
+++ StRoot/macros/bfc.C
@@ -178,10 +178,13 @@ void bfc(Int_t First, Int_t Last,
   if (tChain == "") {
   if (Last == -2 && tChain.CompareTo("ittf",TString::kIgnoreCase)) Usage();
   tChain += "MC.2016a,StiCA,-hitfilt,KFVertex,StiHftC,geantOut,";
+  //  if ( TString(gProgName) == "root4star") tChain += "gstar,HijingAuAu200Z6cm,Corr4,OSpaceZ2,OGridLeak3D,useXgeom";
   if ( TString(gProgName) == "root4star") tChain += "gstar,20Muons,Corr4,OSpaceZ2,OGridLeak3D,useXgeom";
-  //    else                                    tChain += "vmc,20Muons,VMCAlignment,CorrX,OSpaceZ2,OGridLeak3D";
-  //  else                                    tChain += "vmc,20Muons,VMCAlignment,sdt20160301,CorrX,OSpaceZ2,OGridLeak3D";
-  else                                    tChain += "vmc,Lc3pi,Rung.1,VMCAlignment,sdt20160301,CorrX,OSpaceZ2,OGridLeak3D,-useXgeom";
+  //    else                                    tChain += "vmc,20Muons,VMCAlignment,CorrX,OSpaceZ2,OGridLeak3D,RunG.1";
+    else                                    tChain += "vmc,20Muons,VMCAlignment,sdt20160301,CorrX,OSpaceZ2,OGridLeak3D,RunG.1";
+  //  else                                    tChain += "vmc,20Muons,VMCAlignment,CorrX,OSpaceZ2,OGridLeak3D,RunG.1"; // Ideal
+  // else                                    tChain += "vmc,HijingAuAu200Z6cm,Rung.1,VMCAlignment,sdt20160301,CorrX,OSpaceZ2,OGridLeak3D,-useXgeom";
+  //  else                                    tChain += "vmc,Lc3pi,Rung.1,VMCAlignment,sdt20160301,CorrX,OSpaceZ2,OGridLeak3D,-useXgeom";
   //  else                                    tChain += "vmc,LcKppi,VMCAlignment,sdt20160301,CorrX,OSpaceZ2,OGridLeak3D,-useXgeom";
   //                                             "test.RC.AuAu200.y2016,StiCA,KFVertex";
   }
