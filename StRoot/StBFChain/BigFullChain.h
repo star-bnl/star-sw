fdb0d32900770dd90041889c4cbeab5c2391e335
Yuri Fisyak <fisyak@bnl.gov>
2017-05-02 12:03:42 -0400

    Load AgStar after gean
    Former-commit-id: 4d81e1525c376b8d9214f507a204cc9c6cc16ebb

--- StRoot/StBFChain/BigFullChain.h
+++ StRoot/StBFChain/BigFullChain.h
@@ -1427,8 +1427,8 @@ Bfc_st BFC[] = { // standard chains
    ,                             "StarGenEventReader","StarGenEventReader","Read event from TTree", kFALSE},
   {"GenFILT", "GenFILT","Generators","Generators"
    ,                                 "StarFilterMaker","StarGeneratorFilt","Filt generated events", kFALSE},
-  {"AgStar","","","Generators,geantL"                ,"","AgStar","Utility to feed St_geant_Maker", kFALSE},
   {"geant"       ,"geant","","geantL"                          ,"St_geant_Maker","","passive GEANT",kFALSE},
+  {"AgStar","","","Generators,geant"                 ,"","AgStar","Utility to feed St_geant_Maker", kFALSE},
   {"geantEmb"    ,"geant","","geantL"                   ,"St_geant_Maker","","GEANT embedding mode",kFALSE},
   {"RootVMC","","","-geant,-fzin,-ntin,StarMagField,-geantL,-geometry,-geomNoField,geant3","","","",kFALSE},
   {"VMCAppl"     ,"","","geomT,gen_t,sim_T,tpcDB,RootVMC","","minicern,MathMore,StarGeneratorUtil"
