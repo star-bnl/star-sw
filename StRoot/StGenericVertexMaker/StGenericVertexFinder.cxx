d2ca2b22bc5eea9ffad9fa15121ffd229f174c2b
Dmitri Smirnov <dsmirnov@bnl.gov>
2017-05-09 12:30:09 +0000

    StGenericVertexFinder: Don't pollute log files with pointless messages

--- StRoot/StGenericVertexMaker/StGenericVertexFinder.cxx
+++ StRoot/StGenericVertexMaker/StGenericVertexFinder.cxx
@@ -1,5 +1,5 @@
 /***************************************************************************
- * $Id: StGenericVertexFinder.cxx,v 1.51 2017/05/09 12:29:41 smirnovd Exp $
+ * $Id: StGenericVertexFinder.cxx,v 1.52 2017/05/09 12:30:09 smirnovd Exp $
  *
  * Author: Lee Barnby, April 2003
  *
@@ -97,9 +97,10 @@ void StGenericVertexFinder::FillStEvent(StEvent* event)
     //allocates new memory for each vertex
     StPrimaryVertex* primV = new StPrimaryVertex(mVertexList[i]); 
     event->addPrimaryVertex(primV,mVertexOrderMethod);
-    LOG_INFO << "StGenericVertexFinder::FillStEvent: Added " <<i+1 
-		     <<" primary vertex (" << mVertexOrderMethod << ")" << endm;
   }
+
+  LOG_INFO << "StGenericVertexFinder::FillStEvent: Added " << mVertexList.size()
+           <<" primary vertices using ordering method: " << mVertexOrderMethod << endm;
 
   // Use StEvent's ordering
   // (might be undesirable for some debugging)
