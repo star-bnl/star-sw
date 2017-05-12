8b9c44c2161f9deef379e0f90864bca60d769df4
Yuri Fisyak <fisyak@bnl.gov>
2017-05-12 13:52:39 +0000

    Add default MC version of the correction

--- StarDb/Calibrations/tpc/TpcResponseSimulator.y2016.C
+++ StarDb/Calibrations/tpc/TpcResponseSimulator.y2016.C
@@ -1,5 +1,8 @@
-// $Id: TpcResponseSimulator.y2016.C,v 1.1 2016/09/19 12:25:07 fisyak Exp $
+// $Id: TpcResponseSimulator.y2016.C,v 1.2 2017/05/12 13:52:39 fisyak Exp $
 // $Log: TpcResponseSimulator.y2016.C,v $
+// Revision 1.2  2017/05/12 13:52:39  fisyak
+// Add default MC version of the correction
+//
 // Revision 1.1  2016/09/19 12:25:07  fisyak
 // Set/ReSet default values
 //
@@ -94,8 +97,10 @@ TDataSet *CreateTable() {
   row.OmegaTauScaleO        = 1.8  *1.201;  //HC 1.;// 1.8  *1.201;  //i 1.8  *1.1;    //h 1.8;    //ad 1.8  *1.25;  //b effective reduction of OmegaTau near Outer sector anode wire
   // Inner_wire_to_plane_coupling ( 0.533 ) * Inner_wire_to_plane_couplingScale ( 0.843485 )
   // Outer_wire_to_plane_coupling ( 0.512 ) * Outer_wire_to_plane_couplingScale ( 0.725267 )
-  row.SecRowCorIW[0] = row.SecRowCorIE[0] = 6.99114715017355337e-01;//- TMath::Log(0.533*0.843485) -5.84129e-01 + 4.52885e-01 + 3.09117e-02;
-  row.SecRowCorOW[0] = row.SecRowCorOE[0] = 9.79357470004933006e-01;//- TMath::Log(0.512*0.725267) -5.47141e-01 + 5.23937e-01 + 1.19154e-02;
+  row.SecRowCorIW[0] = row.SecRowCorIE[0] = 4.39226e-01 + 2.61137e-01 + 1.90407e-02; //SecRow3CGFdEdx + Hijing.AuAu200.VMCE-bbc2/dEdx/SecRow3CGFdEdx
+  row.SecRowCorIW[1] = row.SecRowCorIE[1] = 1.76094e-02 - 1.30348e-02; //
+  row.SecRowCorOW[0] = row.SecRowCorOE[0] = 1.00611e+00 + 1.52493e-01 + 4.52381e-03; //
+  row.SecRowCorOW[1] = row.SecRowCorOE[1] = 1.33791e-03 - 1.33425e-03; //
   // SecRow3CGFdaq_2011_pp500LowLum => Inner: 3.26428e-01 - -5.01720e-04*y; Outer: 2.68883e-01 + 1.23403e-04*y
   //                                          3.22907e-01                          2.72715e-01
   // SecRow3CGFTpcRS_2011_pp500LowLum_f     : 3.09711e-01                          2.65342e-01
