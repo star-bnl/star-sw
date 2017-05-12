fb588eb4c3ff4d7d498771a0843b1577a75c6638
Yuri Fisyak <fisyak@bnl.gov>
2017-05-12 09:50:36 -0400

    replace find by loop
    Former-commit-id: c46169f3f7e0e262670a1e5e724e925f61167f7d

--- StRoot/StTableUtilities/StG2TrackVertexMap.h
+++ StRoot/StTableUtilities/StG2TrackVertexMap.h
@@ -1,18 +1,24 @@
 #ifndef __StG2TrackVertexMap_h__
 #define __StG2TrackVertexMap_h__
 #include <map>
-#include "Rtypes.h"
+#include "TObject.h"
 #include "tables/St_g2t_track_Table.h"
 #include "tables/St_g2t_vertex_Table.h"
-class StG2TrackVertexMap {
+class StG2TrackVertexMap : public TObject {
  public:
   static  StG2TrackVertexMap* instance(St_g2t_track *track=0, St_g2t_vertex *vertex = 0);
   void    Reset(St_g2t_track *track, St_g2t_vertex *vertex);
   virtual ~StG2TrackVertexMap() {fgInstance = 0;}
-  Int_t   IdVertex(Int_t IdTrack) {return fTrack2Vertex.empty() ? -1 : fTrack2Vertex.find(IdTrack)->second;}
-  Int_t   IdParentTrack(Int_t IdVertex) {return fVertex2ParentTrack.empty() ? -1 : fVertex2ParentTrack.find(IdVertex)->second;}
+  Int_t   IdVertex(Int_t IdTrack) const {return fTrack2Vertex.empty() ? -1 : fTrack2Vertex.find(IdTrack)->second;}
+  Int_t   IdParentTrack(Int_t IdVertex) const {
+    if (fVertex2ParentTrack.empty()) return -1; 
+    auto x = fVertex2ParentTrack.find(IdVertex);
+    if (x == fVertex2ParentTrack.end()) return -1;
+    return x->second;
+  }
   const   St_g2t_track  *Tracks()   const {return fTrack;}
   const   St_g2t_vertex *Vertices() const {return fVertex;}
+  void    Print(Option_t *option="") const;
  private:
   StG2TrackVertexMap(St_g2t_track *track=0, St_g2t_vertex *vertex = 0) : fTrack(track), fVertex(vertex) {Reset(track,vertex);}
   St_g2t_track *fTrack;
@@ -20,5 +26,6 @@ class StG2TrackVertexMap {
   std::map<Int_t,Int_t> fTrack2Vertex;
   std::map<Int_t,Int_t> fVertex2ParentTrack;
   static StG2TrackVertexMap* fgInstance;
+  ClassDef(StG2TrackVertexMap, 0)
 };
 #endif
