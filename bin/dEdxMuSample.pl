02a8b231faa2c02954a6a55a214e045ff6bb5cec
Yuri Fisyak <fisyak@bnl.gov>
2017-06-01 11:56:08 -0400

    Freeze
    Former-commit-id: 4c4de2bd7f0777a2dc1dfe9137f4f3396c2a9e7f

---
+++ bin/dEdxMuSample.pl
@@ -0,0 +1,11 @@
+#! /usr/bin/env perl
+use File::Basename;
+use Cwd;
+my $glob = "../*.MuDst.root";
+my $pwd = cwd();
+if ($#ARGV >= 0) {$glob = $ARGV[0];}
+my @Files = glob $glob;
+foreach my $file (@Files) {
+  my $string = "string:" . $pwd . "/" . $file;
+  print "$string\n";
+}
