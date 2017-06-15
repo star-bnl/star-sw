bd59d4fea5da0f7c38528a6563e1a38a1d45d44d
Yuri Fisyak <fisyak@bnl.gov>
2017-06-15 14:35:28 -0400

    ROOT6
    Former-commit-id: a14957a3712dea2aadf75294bc532b75964a8310

--- StRoot/StTpcRSMaker/TF1F.cxx
+++ StRoot/StTpcRSMaker/TF1F.cxx
@@ -29,7 +29,7 @@ Double_t TF1F::GetSaveL(Int_t N, Double_t x, Double_t *y) {
   Int_t bin     = TMath::Nint((x-fXmin)/fdX);
   Int_t i1 = 0;
   while (bin < 0) {i1++; bin += fStep;}
-  for (Int_t i = i1; i < N && bin < fNsave - 3; i++, bin += fStep) {
+  for (Int_t i = i1; i < N && bin < GetNpx() - 3; i++, bin += fStep) {
     y[i] = fSave[bin];
   }
   return y[0];
@@ -38,12 +38,23 @@ Double_t TF1F::GetSaveL(Int_t N, Double_t x, Double_t *y) {
 Double_t TF1F::GetSaveL(Int_t N, Double_t *x, Double_t *y) {
   // Get values y[N] corresponding to x[N] in array of fSave values
   memset(y, 0, N*sizeof(Double_t));
-  if (fNsave <= 0) return 0.;
+  if (GetNpx() <= 0) return 0.;
   for (Int_t i = 0; i < N; i++) {
     if (x[i] > fXmin) {
       Int_t bin     = Int_t((x[i]-fXmin)/fdX);
-      if (bin < fNsave - 3) y[i] = fSave[bin];
+      if (bin < GetNpx() - 3) y[i] = fSave[bin];
     }
   }
   return y[0];
 }
+#if ROOT_VERSION_CODE >= 393216 /* = ROOT_VERSION(6,0,0) */
+TF1F::TF1F(): TF1(){fNpx = 200;}
+TF1F::TF1F(const char *name,const char *formula, Double_t xmin, Double_t xmax)
+      :TF1(name,formula,xmax,xmin) {fNpx = 200;}
+TF1F::TF1F(const char *name, Double_t (*fcn)(Double_t *, Double_t *), Double_t xmin, Double_t xmax, Int_t npar, Int_t ndim)
+      : TF1(name, fcn, xmin, xmax, npar,ndim) {fNpx = 200;}
+#if 0
+TF1F::TF1F(const char *name, ROOT::Math::ParamFunctor f, Double_t xmin, Double_t xmax, Double_t ymin, Double_t ymax, Int_t npar, Int_t ndim)
+      : TF1(name, f, xmin, xmax, npar,ndim) {fNpx = 200;}
+#endif
+#endif /* ROOT 6 */
