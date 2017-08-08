357dcff0dedf1096d38a709899a67ebcaff5e2ed
Yuri Fisyak <fisyak@bnl.gov>
2017-08-08 11:20:37 -0400

    Freeze
    Former-commit-id: 774a303be4a03cbee55ed3d892334046ba7f1d73

--- macros/geant.C
+++ macros/geant.C
@@ -1,4 +1,5 @@
 void geant() {
   gROOT->LoadMacro("bfc.C");
-  bfc(0,"VMC,VMCAlignment,mysql,sdt20160301,RunG.1,sim_T,nodefault,quiet",0);
+  bfc(0,"VMC,VMCAlignment,mysql,sdt20160301,pxlDb,sstDb,istDb,RunG.1,sim_T,nodefault,quiet",0);
+  //  bfc(0,"VMC,VMCAlignment,mysql,y2016a,pxlDb,sstDb,istDb,RunG.1,sim_T,nodefault,quiet",0);
 }
