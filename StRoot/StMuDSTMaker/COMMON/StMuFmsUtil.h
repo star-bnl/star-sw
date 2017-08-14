dbdf96032f1a417f5bd90485d4f121224e859088
smirnovd <>
2017-08-14 16:22:36 +0000

    Recover FMS hits using StTriggerData
    commit 6d7358f4c86a15edd0671326580d291a9843aec9
    Date:   Tue Aug 8 23:42:41 2017 -0400
    
        StMuFmsUtil: Recover FMS hits using StTriggerData
    
    commit 556d07cb8fd87cb62e4ac674226423671c94917d
    Date:   Tue Aug 8 23:42:34 2017 -0400
    
        StMuFmsUtil: Added func to fill StMuFmsCollection with FMS hits from StTriggerData in StMuEvent
    
    commit c355529c1ee401849b2b81d74df8d452886593d1
    Date:   Tue Aug 8 23:42:19 2017 -0400
    
        [Cosmetic] Changes in whitespace
    
    commit 67fdc1b348bebbfbfb137b726ee9c455a7d8be37
    Date:   Mon Jun 5 12:00:24 2017 -0400
    
        StMuFmsCollection::addHit() Return pointer to just added default FMS hit object

--- StRoot/StMuDSTMaker/COMMON/StMuFmsUtil.h
+++ StRoot/StMuDSTMaker/COMMON/StMuFmsUtil.h
@@ -1,6 +1,6 @@
  /***************************************************************************
   *
-  * $Id: StMuFmsUtil.h,v 1.2 2015/08/28 18:36:04 jdb Exp $
+  * $Id: StMuFmsUtil.h,v 1.3 2017/08/14 16:22:36 smirnovd Exp $
   *
   * Author: Jingguo Ma, Jan 2010
   ***************************************************************************
@@ -10,6 +10,29 @@
   ***************************************************************************
   *
   * $Log: StMuFmsUtil.h,v $
+  * Revision 1.3  2017/08/14 16:22:36  smirnovd
+  * Recover FMS hits using StTriggerData
+  *
+  * commit 6d7358f4c86a15edd0671326580d291a9843aec9
+  * Date:   Tue Aug 8 23:42:41 2017 -0400
+  *
+  *     StMuFmsUtil: Recover FMS hits using StTriggerData
+  *
+  * commit 556d07cb8fd87cb62e4ac674226423671c94917d
+  * Date:   Tue Aug 8 23:42:34 2017 -0400
+  *
+  *     StMuFmsUtil: Added func to fill StMuFmsCollection with FMS hits from StTriggerData in StMuEvent
+  *
+  * commit c355529c1ee401849b2b81d74df8d452886593d1
+  * Date:   Tue Aug 8 23:42:19 2017 -0400
+  *
+  *     [Cosmetic] Changes in whitespace
+  *
+  * commit 67fdc1b348bebbfbfb137b726ee9c455a7d8be37
+  * Date:   Mon Jun 5 12:00:24 2017 -0400
+  *
+  *     StMuFmsCollection::addHit() Return pointer to just added default FMS hit object
+  *
   * Revision 1.2  2015/08/28 18:36:04  jdb
   * Added Akios FMS codes
   *
@@ -23,20 +46,35 @@
 
  class StMuFmsCollection;
  class StFmsCollection;
+ class StFmsDbMaker;
+ class StTriggerData;
+ class StMuDst;
 
- class StMuFmsUtil : public TObject
- {
-   protected:
 
-   public:
-			StMuFmsUtil();
-			~StMuFmsUtil();
-     StMuFmsCollection* getMuFms(StFmsCollection*);
-     StFmsCollection*   getFms(StMuFmsCollection*);
-     void               fillMuFms(StMuFmsCollection*,StFmsCollection*);
-     void               fillFms(StFmsCollection*,StMuFmsCollection*);
+class StMuFmsUtil : public TObject
+{
+public:
+  StMuFmsUtil();
+  ~StMuFmsUtil();
+  StMuFmsCollection* getMuFms(StFmsCollection*);
+  StFmsCollection*   getFms(StMuFmsCollection*);
+  void               fillMuFms(StMuFmsCollection*,StFmsCollection*);
+  void               fillFms(StFmsCollection*,StMuFmsCollection*);
 
-   private:
+  /**
+   * Creates `StMuFmsHit`s from the StTriggerData object and appends them to the
+   * StMuFmsCollection.
+   */
+  static void fillMuFmsHits(StMuFmsCollection&, const StTriggerData&, const StFmsDbMaker* = nullptr);
+
+  /**
+   * In the provided `muDst` object fills StMuFmsCollection with FMS hits
+   * extracted from the StTriggerData block in the same `muDst`.
+   */
+  static void recoverMuFmsCollection(StMuDst& muDst, const StFmsDbMaker* = nullptr);
+
+private:
+
   /** Create StMuFmsHits from StFmsHits and fill StMuFmsCollection */
   void fillMuFmsHits(StMuFmsCollection*, StFmsCollection*);
   /** Create StMuFmsClusters from StFmsClusters and fill StMuFmsCollection */
