80040f8c126450ce7c3fb7dcfdcdb99336b6c601
Yuri Fisyak <fisyak@bnl.gov>
2017-08-29 14:34:49 -0400

    Fix double definition of rnorm_improved
    Former-commit-id: b46d501fe42686bc1f3e5bd1414776af14b4ceef

--- garfield/Heed/wcpplib/random/rnorm.h
+++ garfield/Heed/wcpplib/random/rnorm.h
@@ -8,7 +8,7 @@
 
 namespace Heed {
 
-double rnorm_improved(void) { return Garfield::RndmGaussian(); }
+  double rnorm_improved(void);
 
 void rnorm_double(const double r1, const double r2, // flat random numbers
                   double &x1, double &x2);          // results
