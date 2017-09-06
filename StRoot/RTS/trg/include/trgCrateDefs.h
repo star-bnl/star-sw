470bb8052b994226fd9aafdd7c8ebbb344f37dad
Jeffery M Landgraf <jml@bnl.gov>
2017-09-06 14:42:21 +0000

    update

--- StRoot/RTS/trg/include/trgCrateDefs.h
+++ StRoot/RTS/trg/include/trgCrateDefs.h
@@ -16,6 +16,50 @@ typedef struct {
   unsigned int data[10];                      /* 40 bytes available for use */
 } INFBlock; 
 
+typedef struct {
+  char name[4];                               /* Contains BBC */
+  int length;                                 /* Byte count of data that follows */
+  unsigned short BBClayer1[16];               /* This is the layer1 DSM that feeds the VTX DSM */
+  unsigned short ZDClayer1[8];                /* This is the new layer1 ZDC DSM that also feeds the VTX DSM */
+  unsigned short VPD[8];                      /* ADC & TAC values for VPD detectors*/
+} BBCBlock;
+
+typedef struct {
+  char name[4];                               /* Contains MIX */
+  int length;                                 /* Byte count of data that follows */
+  unsigned short FPDEastNSLayer1[8];          /* FPD east north/south layer 1  */  
+  unsigned char  MTD_P2PLayer1[16];           /* Data from MTD and PP2PP */
+  unsigned short TOFLayer1[8];                /* This is TOF Layer 1 */
+  unsigned short TOF[48];                     /* TOF data */
+  unsigned short TPCpreMask[24];              /* EMC, MTD, & TOF TPC Grid Masks */
+} MIXBlock;
+
+typedef struct {
+  char name[4];
+  int length;
+  unsigned char BEMCEast[240];                /* 15 DSMs covering the East half of BEMC */
+} BEastBlock; 
+
+typedef struct {
+  char name[4];
+  int length;
+  unsigned char BEMCWest[240];                /* 15 DSMs covering the West half of BEMC */
+} BWestBlock;
+
+typedef struct {
+  char name[4];
+  int length;
+  unsigned short BEMClayer1[48];              /* 6 DSMs for BEMC at layer1 */
+  unsigned short EEMClayer1[16];              /* 2 DSMs for EEMC at layer1 */
+  unsigned char  EEMC[144];                   /* 9 DSMs for EEMC at layer0 */
+} BELayerBlock;
+
+typedef struct {
+  char name[4];
+  int length;
+  unsigned char FMS[256];                     /* 16 DSMs for FMS */
+} FMSBlock;
+
 typedef struct  {
   char name[4];
   int length;                                 /* Byte count of data that follows */
