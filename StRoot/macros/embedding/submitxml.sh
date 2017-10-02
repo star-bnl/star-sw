0868fa86ac116d4feb5918f0c16e098602e7b776
Xianglei Zhu <zhux@tsinghua.edu.cn>
2017-10-02 09:41:26 +0000

    shell script to submit the embedding xml jobs for one FSET

---
+++ StRoot/macros/embedding/submitxml.sh
@@ -0,0 +1,16 @@
+#!/bin/csh
+
+if ( $#argv != 2 ) then
+  echo ""
+  echo " Usage : $0 [embedding template xml file] [FSET number, 109 or something like that]"
+  echo ""
+  echo ""
+  exit
+endif
+
+set template = "$1"
+
+set FSET = "$2"
+
+#star-submit-template-beta -template ${template} -entities FSET=$FSET -- -p bnl_condor_production
+star-submit-template -template ${template} -entities FSET=$FSET
