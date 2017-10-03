bcfa72ae49ecfd1a3195ff67896aaae8671936ad
cvs2git <cvs2git>
2017-10-03 20:43:27 +0000

    This commit was manufactured by cvs2git to create branch 'AgML100417b'.

--- StRoot/StDetectorDbMaker/StiIstChairs.cxx
+++
@@ -1,4 +0,0 @@
-#include "StarChairDefs.h"
-#include "St_db_Maker/St_db_Maker.h"
-#include "StiIst1HitErrorCalculator.h"
-MakeChairInstance2(HitError,StiIst1HitErrorCalculator,Calibrations/tracker/ist1HitError);
