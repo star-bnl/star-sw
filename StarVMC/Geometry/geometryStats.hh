e5ab315e7672f9f7f629aa6886735d5e3433b6ca
Jason C. Webb <jwebb@bnl.gov>
2017-10-04 19:22:02 +0000

    Add back the agml 1 steering codes

---
+++ StarVMC/Geometry/geometryStats.hh
@@ -0,0 +1,10 @@
+#ifndef __geometryStats_hh__
+#define __geometryStats_hh__
+
+void geometryStats( const Char_t *name = "BBCM" );
+
+Int_t getNumberOfNodes();
+Int_t getNumberOfVolumes();
+
+
+#endif
