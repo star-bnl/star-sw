55cfc6850193e058fb27e2195a5b4d7623d6cce1
Yuri Fisyak <fisyak@bnl.gov>
2017-10-05 18:44:55 -0400

    Freeze
    Former-commit-id: a495434f016327cf7713f563312501212fe8bd41

--- bin/mount.pl
+++ bin/mount.pl
@@ -30,8 +30,8 @@ if (! -r $disk ) {
 }
 my $myVolName = $mount; #File::Basename::basename($mount);
 # fusermount -u /direct/gpfs01
-#$cmd = "sshfs fisyak\@rftpexp02.rhic.bnl.gov:$DISK $disk";
-$cmd = "sshfs fisyak\@rftpexp01.rhic.bnl.gov:$DISK $disk";
+$cmd = "sshfs fisyak\@rftpexp02.rhic.bnl.gov:$DISK $disk";
+#$cmd = "sshfs fisyak\@rftpexp01.rhic.bnl.gov:$DISK $disk";
 print "$cmd";
 $flag = system($cmd);
 if ( $flag) {print " === failed";}
