9975d283facd8cfcffbf0ff60cd8cb1d472812c4
cvs2git <cvs2git>
2017-10-23 11:25:50 +0000

    This commit was manufactured by cvs2git to create branch 'SL17ip1'.

--- StRoot/StDaqLib/EVP/daqFormatX.h
+++
@@ -1,7 +0,0 @@
-#ifndef _DAQ_FORMATX_H
-#define _DAQ_FORMATX_H
-// eXtended definitions of DAQ structures for offline
-#include "daqFormats.h"
-
-#endif
-
--- StRoot/StDetectorDbMaker/StDetectorDbTpcOmegaTau.h
+++
@@ -1,5 +0,0 @@
-#ifndef StDetectorDbTpcOmegaTau_h
-#define StDetectorDbTpcOmegaTau_h
-#include "St_tpcOmegaTauC.h"
-typedef St_tpcOmegaTauC StDetectorDbTpcOmegaTau;
-#endif
