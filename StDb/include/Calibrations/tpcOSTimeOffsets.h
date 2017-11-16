b20b7e069328b1f171b9adfaa094fc8a41d99569
Yuri Fisyak <fisyak@bnl.gov>
2017-11-16 18:42:26 -0500

    synch. with CVS
    Former-commit-id: ff349844daef20266c6a2b129118826f73c9b945

---
+++ StDb/include/Calibrations/tpcOSTimeOffsets.h
@@ -0,0 +1,20 @@
+//:Description: OuterSector Pad level time-bucket corrections
+//:Synonyms::::
+//:Source:
+//:Update:
+//:Update frequncy:
+//:Reminder:
+//:Recall frequency:
+//:Size of Data:
+//:Pointer to data::
+
+#ifndef TPC_OUTERSECTORTIMEOFFSETS_H
+#define TPC_OUTERSECTORTIMEOFFSETS_H
+
+struct tpcOSTimeOffsets {
+
+  float offset[3942]; // pad-to-pad time offset corrections
+
+};
+
+#endif
