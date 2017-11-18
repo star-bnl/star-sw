a5e4fd4ceaa813e88a14bf2ebaabb8f14f766d7b
Xianglei Zhu <zhux@tsinghua.edu.cn>
2017-11-18 09:43:39 +0000

    code upgraded. now show fully diagonisis information of several fsets.

--- StRoot/macros/embedding/fsetstat.sh
+++ StRoot/macros/embedding/fsetstat.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
-#show statistics info of one Fset, applicable at both PDSF and Cori
+#show statistics info of each Fset, applicable at both PDSF and Cori
 
-if [ ! $# -eq 1 ] ; then
+if [[ $# -eq 0 || $# -gt 2 ]] ; then
    echo ""
-   echo " Usage : $0 [Fset #] "
+   echo " Usage : $0 [Fset#] "
+   echo "      or $0 [Fset_begin#] [Fset_end#] "
    echo ""
    echo ""
    exit
+fi
+
+begin=$1
+if [ $# -eq 1 ] ; then
+   end=$(($1+1))
+else
+   if [ $1 -gt $2 ] ; then
+	echo "wrong parameters! the first fset# should not be larger than the second! "
+	exit
+   fi
+   end=$(($2+1))
 fi
 
 if [[ $HOST =~ "cori" ]] ; then
@@ -20,18 +32,26 @@ particle=`echo $part`
 trg=`grep "\-trg" $PWD/preparexmlslr.sh | awk -F"-trg |-production" '{print $2}'`
 trgset=`echo $trg`
 reqid=`grep "\-r" $PWD/preparexmlslr.sh | awk '{print $2}'`
+daqdir=`grep "\-daq" $PWD/preparexmlslr.sh | awk '{print $2}'`
+ndaq=`find $daqdir/*.daq | wc -l`
 
-fsetp=$outp/$trgset/${particle}_${1}_${reqid}
+for (( ifset=$begin ; $ifset - $end ; ifset++ ))
+do
 
+fsetp=$outp/$trgset/${particle}_${ifset}_${reqid}
+
+echo ""
 if [ ! -d "$fsetp" ] ; then
-   echo Fset# $1 directory can not be found in $outp/$trgset 
-   exit
+   echo CAUTION: Fset# $ifset directory can not be found in $outp/$trgset 
+   if [ $ifset -eq $(($end-1)) ] ; then
+	echo ""
+   fi
+   continue
 fi
-echo ""
-echo "Fset# $1 data is found in $fsetp"
-echo "Now start to scan ..."
-echo ""
-echo "-------------------------------------"
+echo "Fset# $ifset is found at $fsetp"
+echo "The directory size is "`du -sh $fsetp | awk '{print $1}'`
+echo "Now start to scan the log files..."
+
 
 total=0
 count=0
@@ -40,23 +60,42 @@ storedevts=0
 
 for i in `find $fsetp/ -name "*.log.gz"`
 do
-echo $i
-jtime=`zcat $i| tail -n 1000 | grep "StChain::Embedding" | awk -F '=' '{print $2}' | awk '{print $1}'`
+#echo $i
+if [ -f tmplog.txt ] ; then
+   rm -f tmplog.txt
+fi
+gzip -cd $i | tail -n 1000 > tmplog.txt
+jtime=`grep "StChain::Embedding" tmplog.txt | awk -F '=' '{print $2}' | awk '{print $1}'`
 if [ -z $jtime ] ; then
    continue
 fi
 count=$(($count+1))
 total=`echo "$total+$jtime" |bc -l`
-nevents=`zcat $i| tail -n 1000 | grep "Total events processed" | awk -F ':' '{print $4}' | awk '{print $1}'`
-echo $nevents events in this daq file
-nanaevents=`zcat $i| tail -n 1000 | grep "StAnalysisMaker::Finish" | awk '{print $5}'`
-echo $nanaevents events accepted for embedding in this daq file
+nevents=`grep "Total events processed" tmplog.txt | awk -F ':' '{print $4}' | awk '{print $1}'`
+#echo $nevents events in this daq file
+nanaevents=`grep "StAnalysisMaker::Finish" tmplog.txt | awk '{print $5}'`
+#echo $nanaevents events accepted for embedding in this daq file
 allevts=`echo "$allevts+$nevents" |bc -l`
 storedevts=`echo "$storedevts+$nanaevents" |bc -l`
 done
+if [ -f tmplog.txt ] ; then
+   rm -f tmplog.txt
+fi
 
-echo ""
-echo "-----------DONE------------"
-echo "Scanned Fset# $1 data in $fsetp" 
-echo "total # of log files:" $count "; total # of daq events:" $allevts "; total # of embedded events:" $storedevts "; total CPU*Hours:" `echo "$total/3600+0.5" | bc -l | cut -d. -f1` "; average CPU*Hours per file:" `echo "$total/$count/3600+0.5" | bc -l | cut -d. -f1`
-echo ""
+echo "Scanned Fset# $ifset data:" 
+echo "total # of daq files:" $ndaq "; total # of log files:" $count "; total # of daq events:" $allevts
+echo "total # of embedded events:" $storedevts "; total CPU*Hours:" `echo "$total/3600+0.5" | bc -l | cut -d. -f1` "; average CPU*Hours per daq file:" `echo "$total/$count/3600+0.5" | bc -l | cut -d. -f1`
+if [ $count -lt $ndaq ] ; then
+   echo CAUTION: $(($ndaq-$count))/$ndaq daq files have not been processed properly!
+   if [[ $HOST =~ "cori" ]] ; then
+	echo Please use \'resumecori.sh\' to do some further check.
+   else
+	echo Please use \'findfailed.sh\' to do some further check.
+   fi
+fi
+
+if [ $ifset -eq $(($end-1)) ] ; then
+   echo ""
+fi
+
+done
