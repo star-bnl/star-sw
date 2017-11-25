deceaa7e90e9397d5c0cec34aab1e704b1551a56
Yuri Fisyak <fisyak@bnl.gov>
2017-11-25 14:26:57 -0500

    Freeze
    Former-commit-id: e3e9a41b4729bd879f178aec560fe0c59740b298

--- bin/2016AuAu200MuDst.pl
+++ bin/2016AuAu200MuDst.pl
@@ -1,1 +1,62 @@
-get_file_list.pl -keys path,filename -cond production=P16ij,trgsetupname=AuAu_200_production_2016,filetype=daq_reco_MuDst,filename~st_physics,storage=nfs -limit 0
+#! /usr/bin/env perl
+#print "$#ARGV\n";
+if ($#ARGV < 0) {
+  print "Usage : $0 day\n";
+  exit 0;
+}
+my $day = $ARGV[0];
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename,events' -cond 'filetype=online_daq,filename~st_laser,tpx=1,sanity=1,runnumber>15000000,events>10' -limit 0`;
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'filetype=online_daq,filename~st_physics_15,tpx=1,sanity=1,runnumber>15000000,events>10' -limit 0`;
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'filetype=online_daq,filename~st_,tpx=1,sanity=1,events>10,runnumber>16140032,trgsetupname=fixedTarget2015' -limit 0`; 
+#get_file_list.pl -keys path,filename -cond production=P16ij,trgsetupname=AuAu_200_production_2016,filetype=daq_reco_MuDst,filename~st_physics,storage=nfs -limit 0
+#my  @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'production=P16ij,trgsetupname=AuAu_200_production_2016,filetype=daq_reco_MuDst,filename~st_physics,tpx=1,pxl=1,ist=1,sanity=1,runnumber>17124000,events>10' -limit 0`;
+my $cmd = "get_file_list.pl -delim '/' -keys 'path,filename' -cond 'production=P16ij,trgsetupname=AuAu_200_production_2016,filetype=daq_reco_MuDst,filename~st_physics,tpx=1,pxl=1,ist=1,sanity=1,events>10,runnumber>17000000' -limit 0";# print "$cmd\n";
+my  @list = `$cmd`;
+my @HLT = qw(17125034 17127006 17127007 17127008 17127009 17127010 17127011 17127012 17127013 17127014
+	     17127016 17127017 17127026 17128002 17128003 17128006 17128007 17128008 17128009 17128010);
+my $HLT = join '|', @HLT;
+my $i = 1;   
+my $OldRun = 0;
+foreach my $file (@list) { 
+  chomp($file);
+  my @words = split('/',$file);
+  my $year = int $words[7];
+  my $dayr = int $words[8]; 
+  if ($dayr ne $day) {next;}
+  my $run =  int $words[9]; 
+  if ($run =~ $HLT) {next;}
+#  my $year = int $run/1000000;
+#  my $day = int $run/1000 - 1000 * $year;
+#  print "run = $run year = $year day = $day \n";
+#  die;
+#  print "run = $run\n";
+  if ($run != $OldRun) {$i = 1; $OldRun = $run;}
+  else                 {$i++;}
+#  if ($i%20 != 1) {next;}
+  my $basename = $file;
+  $basename =~ s#home/starreco/reco/AuAu_200_production_2016/ReversedFullField/P16ij/##;
+#  print "$file = > $basename\n";
+  my $found = 0;
+  my $dir;
+  my @dirs = qw( /gpfs02/eic/ayk/STAR/reco/MuDst /gpfs01/star/pwg/fisyak/MuDst );
+  foreach $dir (@dirs) {
+    my $fullpath = $dir . $basename;
+#    print "$i : $fullpath ===========";
+    if (-f $fullpath) {
+      $found = 1; 
+#      print "found\n"; 
+      last;
+    }
+#    print "not found\n";
+  }
+  if (! $found) {
+    my $piconame = $basename;
+    $piconame =~ s/MuDst/picoDst/;
+    my $pico = "/gpfs01/star/pwg_tasks/hf03/Pico" . $piconame;# print "$pico\n";
+    if (-r $pico) {
+  #    print "$pico is Done\n"; 
+      next;
+    }
+    print "$file $dirs[0]$basename\n";
+  }
+}
--- bin/PicoDST.pl
+++ bin/PicoDST.pl
@@ -6,14 +6,15 @@ my $pwd = cwd();
 my $day = File::Basename::basename(File::Basename::dirname($pwd));
 my $run =  File::Basename::basename($pwd);
 #my $dir = "/star/subsys/tpc/fisyak/reco/2014/50M/SL15StiCAKFV/" . $day . "/" . $run;
-my $dir = "/net/l401/data/scratch2/fisyak/MuDst/2016/" . $day . "/" . $run;
+#my $dir = "/net/l401/data/scratch2/fisyak/MuDst/2016/" . $day . "/" . $run;
 my $debug = 0;
+my $dir = "/gpfs01/star/pwg/fisyak/MuDst/2016/" . $day . "/" . $run; print "dir = $dir\n" if ($debug);
 my %Hash = ();
 my @list = glob "$dir" . "/*.MuDst.root";
 my $NJobs = 0;
 my ($st,$physcs,$adc,$r,$raw,$p,$f1,$l1);
 my $file;
-foreach my $line (@list) {
+foreach my $line (@list) { print "$line\n" if ($debug);
   my $b = File::Basename::basename($line,".MuDst.root");
 #  $b =~ s/_adc//;  print "$b\n" if ($debug);
   if ($b =~ /_adc/) {
@@ -24,7 +25,13 @@ foreach my $line (@list) {
     $file = $st . "_" . $physics  . "_" . $r . "_" . $raw . "_" . $p; print "file = $file \n" if ($debug);
   }
   if ($run ne $r) {die "run != r";}
-#  my ($dev,$ino,$mode,$nlink,$uid,$gid,$dev, $size, $atime, $mtim, $ctime, $blksize,$blocks) = stat($line);
+#  my ($dev,$ino,$mode,$nlink,$uid,$gid,$dev, $size, $atime, $mtim, $ctime, $blksize,$blocks) = File::stat($line);
+  $st = stat($line);			   
+#  print "$dev,$ino,$mode,$nlink,$uid,$gid,$dev, $size, $atime, $mtim, $ctime, $blksize,$blocks\n";
+  my $size = $st->size;
+  print "$line size = $size\n" if ($debug);
+#  die;			   
+  if ($size < 10000) {next;}
   my $inode = stat($line);
   my $ctime = $inode->ctime;
   
