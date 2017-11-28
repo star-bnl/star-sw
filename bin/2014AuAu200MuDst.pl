7e1391172b93d65a9dd9147937bea33495c6733a
Yuri Fisyak <fisyak@bnl.gov>
2017-11-28 16:25:13 -0500

    Freeze 2016 Pico
    Former-commit-id: 5c836a65c775c7745067beeb244a44d02f5fdeb3

--- bin/2014AuAu200MuDst.pl
+++ bin/2014AuAu200MuDst.pl
@@ -6,7 +6,7 @@ if ($#ARGV < 0) {
 }
 my $debug = 0;
 my $day = $ARGV[0];
-my $trigger = "AuAu_200_production_2016";
+my $trigger = "AuAu_200_production_mid_2014";
 #my @list = `get_file_list.pl -delim '/' -keys 'path,filename,events' -cond 'filetype=online_daq,filename~st_laser,tpx=1,sanity=1,runnumber>15000000,events>10' -limit 0`;
 #my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'filetype=online_daq,filename~st_physics_15,tpx=1,sanity=1,runnumber>15000000,events>10' -limit 0`;
 #my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'filetype=online_daq,filename~st_,tpx=1,sanity=1,events>10,runnumber>16140032,trgsetupname=fixedTarget2015' -limit 0`; 
@@ -15,7 +15,7 @@ my $trigger = "AuAu_200_production_2016";
 #my $cmd = "get_file_list.pl -delim '/' -keys 'path,filename' -cond 'production=P16ij,trgsetupname=AuAu_200_production_2016,filetype=daq_reco_MuDst,tpx=1,pxl=1,ist=1,sanity=1,events>10,runnumber>17000000' -limit 0"; print "$cmd\n" if ($debug);
 my $TriggerFile = $trigger . ".list"; print "Trigger file = $TriggerFile\n" if ($debug);
 if (! -r $TriggerFile) {
-  my $cmd = "get_file_list.pl -delim '/' -keys 'path,filename' -cond 'production=P16ij,trgsetupname=" . $trigger . ",filetype=daq_reco_MuDst,filename~st_physics,tpx=1,pxl=1,ist=1,sanity=1,events>10,runnumber>17000000' -limit 0"; 
+  my $cmd = "get_file_list.pl -delim '/' -keys 'path,filename' -cond 'production=P16id,trgsetupname=" . $trigger . ",filetype=daq_reco_MuDst,filename~st_physics,tpx=1,pxl=1,ist=1,sanity=1,events>10,runnumber>15000000' -limit 0"; 
   print "$cmd\n" if ($debug);
   my  @list = `$cmd`;
   open(Out, ">$TrigerFile") or die "Can't open $TriggerFile";
@@ -24,9 +24,9 @@ if (! -r $TriggerFile) {
   }
   close (Out);
 }
-my @HLT = qw(17125034 17127006 17127007 17127008 17127009 17127010 17127011 17127012 17127013 17127014
-	     17127016 17127017 17127026 17128002 17128003 17128006 17128007 17128008 17128009 17128010);
-my $HLT = join '|', @HLT;
+#my @HLT = qw(17125034 17127006 17127007 17127008 17127009 17127010 17127011 17127012 17127013 17127014
+#	     17127016 17127017 17127026 17128002 17128003 17128006 17128007 17128008 17128009 17128010);
+#my $HLT = join '|', @HLT;
 my $i = 1;   
 my $OldRun = 0;
 open(In, "$TriggerFile") or die "Can't open $TriggerFile";
@@ -37,7 +37,7 @@ while (my $file = <In>) {
   my $dayr = int $words[8]; 
   if ($dayr ne $day) {next;}
   my $run =  int $words[9]; 
-  if ($run =~ $HLT) {next;}
+#  if ($run =~ $HLT) {next;}
 #  my $year = int $run/1000000;
 #  my $day = int $run/1000 - 1000 * $year;
   print "run = $run year = $year day = $day \n" if ($debug);
@@ -48,6 +48,7 @@ while (my $file = <In>) {
 #  if ($i%20 != 1) {next;}
   my $basename = $file;
   $basename =~ s#home/starreco/reco/AuAu_200_production_2016/ReversedFullField/P16ij/##;
+  $basename =~ s#home/starreco/reco/AuAu_200_production_mid_2014/ReversedFullField/P16id/##;
 #  print "$file = > $basename\n";
   my $found = 0;
   my $dir;
