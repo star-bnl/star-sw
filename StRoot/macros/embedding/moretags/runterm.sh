0cb5fe91b1998c0382939cccbc7037f22a437d7e
zhux <>
2017-12-05 04:57:06 +0000

    initial version.

---
+++ StRoot/macros/embedding/moretags/runterm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#how to run:
+#if the scheduler job doesn't run, scancel the job, then run this on terminal (15 tasks in parallel)
+#%find sums/sched9EC71*csh > tasks.csh
+#%./runterm.sh tasks.csh
+
+echo $1
+
+for i in `cat $1`
+do
+   ls -l $i
+   bn=`basename $i .csh`
+   tag=`echo $bn | awk -F'_' '{print $1}'`
+
+   count=`ps aux | grep $tag |wc -l`
+   while [ $count -gt 15 ]; do
+	sleep 2
+	count=`ps aux | grep $tag |wc -l`
+   done
+   if [ -f logs/${bn}.log ] ; then
+	rm -f logs/${bn}.log
+   fi
+   if [ -f logs/${bn}.err ] ; then
+	rm -f logs/${bn}.err
+   fi
+   ./$i 1> logs/${bn}.log 2> logs/${bn}.err &
+done
+
