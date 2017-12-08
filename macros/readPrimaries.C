98571bea91ba66b59a069f361cf9af1481801a1f
Yuri Fisyak <fisyak@bnl.gov>
2017-12-08 12:21:14 -0500

    switch to lMuDst
    Former-commit-id: 5dd7436c2a2873f05273c804f1666004e8bec833

---
+++ macros/readPrimaries.C
@@ -0,0 +1,115 @@
+/*
+   root.exe -q -b -x  'lMuDst.C(0,"*.MuDst.root")' readPrimaries.C
+ */
+#include "TH1.h"
+#include "TH2.h"
+#include "TStopwatch.h"
+
+// #include "StMuDSTMaker/COMMON/StMuDstMaker.h"
+
+const double C_C_LIGHT = 29.9792458; // [cm/ns]
+
+void readPrimaries(Long64_t nevent=1000) {
+  
+  TH1::SetDefaultSumw2(1);
+  
+  TH1D* hRcPt  = new TH1D("hRcPt", "", 500, 0, 5);
+  TH1D* hRcEta = new TH1D("hRcEta", "", 400, -2, 2);
+  TH2D* hOneOverBetaP = new TH2D("hOneOverBetaP", "", 300, 0, 3, 300, 0, 3);
+  TH2D* hBTofHitLTTray = new TH2D("hBTofHitLTTray", "", 150, 0, 150, 100, 0, 100);
+  TH1D* hBTofBeta = new TH1D("hBTofBeta", "", 200, 0, 1);
+  
+  TH2D* hBTofHitLETvsTof = new TH2D("hBTofHitLETvsTof", "", 100, 0, 100, 100, 0, 100);
+  
+  TH1D* hMcPt  = new TH1D("hMcPt", "", 500, 0, 5);
+  TH1D* hMcEta = new TH1D("hMcEta", "", 400, -2, 2);
+  
+  TH1D* hEffPt = new TH1D("hEffPt", "", 500, 0, 5);
+  TH1D* hEffEta = new TH1D("hEffEta", "", 400, -2, 2);
+  
+  TStopwatch time_all;
+  // Chain
+  StBFChain *chain = (StBFChain *) StMaker::GetTopChain();
+  StMuDstMaker* maker = (StMuDstMaker *) chain->Maker("MuDst");
+  maker->SetStatus("*",0);
+  const Char_t *ActiveBranches[] = {"MuEvent"
+				    ,"PrimaryVertices"
+				    ,"PrimaryTracks"
+				    ,"GlobalTracks"
+				    ,"CovGlobTrack"
+				    ,"BTofHit"
+#if 1
+				    ,"StStMuMcVertex"
+				    ,"StStMuMcTrack"
+#endif
+  };
+  Int_t Nb = sizeof(ActiveBranches)/sizeof(Char_t *);
+  for (Int_t i = 0; i < Nb; i++) maker->SetStatus(ActiveBranches[i],1); // Set Active braches
+  StMuDebug::setLevel(0);  
+  TChain *tree = StMuDstMaker::instance()->chain();
+  if (! tree) return;
+  Long64_t nentries = tree->GetEntries();
+  nevent = TMath::Min(nevent,nentries);
+  cout << nentries << " events in chain " << nevent << " will be read." << endl;
+  //  if (nentries < 100) return;
+  tree->SetCacheSize(-1);        //by setting the read cache to -1 we set it to the AutoFlush value when writing
+  tree->SetCacheLearnEntries(1); //one entry is sufficient to learn
+  tree->SetCacheEntryRange(0,nevent);
+  for (Long64_t i_evt = 0; i_evt < nevent; i_evt++) {
+    if (chain->MakeEvent()) break;
+    StMuDst *mudst = StMuDst::instance();
+    if (! mudst) {
+      cout << "No event" << endl;
+      continue;
+    }
+#if 0
+    cout << "Event " << mudst->event()->eventId() << ": "
+	 << mudst->numberOfPrimaryTracks() << " primary tracks, "
+	 << mudst->numberOfGlobalTracks() << " global tracks" << endl;
+#endif    
+    for (int iPTrk = 0; iPTrk < mudst->numberOfPrimaryTracks(); ++iPTrk) {
+      StMuTrack *pTrk = mudst->primaryTracks(iPTrk);
+      if (!pTrk) continue;
+      hRcPt->Fill(pTrk->pt());
+      StMuBTofHit* tofHit = pTrk->tofHit();
+      // int idxTofHit = pTrk->index2BTofHit();
+      // StMuBTofHit* tofHit = mudst->btofHit(idxTofHit);
+      if (tofHit) {
+	// hOneOverBetaP->Fill(pTrk->p().mag(), tofHit->leadingEdgeTime()/pTrk->btofPidTraits().pathLength());
+	//Hongwei	double tofBeta = pTrk->btofPidTraits().pathLength() / ((tofHit->leadingEdgeTime()-19) * C_C_LIGHT);
+	double tofBeta = -1.;
+	if (pTrk->btofPidTraits().beta() > 0) tofBeta = pTrk->btofPidTraits().beta();
+	// double tofBeta = pTrk->btofPidTraits().pathLength() / ((pTrk->btofPidTraits().timeOfFlight()-19) * C_C_LIGHT);
+	hBTofBeta->Fill(tofBeta);
+	hOneOverBetaP->Fill(pTrk->p().mag(), 1.0/tofBeta);
+	hBTofHitLTTray->Fill(tofHit->tray(), tofHit->leadingEdgeTime());
+	
+	hBTofHitLETvsTof->Fill(tofHit->leadingEdgeTime()-19, pTrk->btofPidTraits().timeOfFlight());
+      }
+    }
+    
+    TClonesArray* MCTracks = StMuDst::mcArray(1);
+    for (int iMCTrk = 0; iMCTrk < MCTracks->GetSize(); ++iMCTrk) {
+      StMuMcTrack* mcTrk = (StMuMcTrack*)MCTracks->UncheckedAt(iMCTrk);
+      // StMuMcTrack* mcTrk = mudst-><MCtrack(iMCTrk);
+      if (!mcTrk) continue;
+      // cout << Form("%15.6f%15.6f%15.6f", mcTrk.pT(), mcTrk.Pxyz().x(), mcTrk.Pxyz().y()) << endl;
+      Int_t IdVx = mcTrk->IdVx();
+      if (IdVx != 1) continue; // original vertex 
+      if (TMath::Abs(mcTrk->Eta()) > 1) continue;
+      if (!mcTrk->Charge()) continue;
+      hMcPt->Fill(mcTrk->pT());
+    }
+    
+  } // event loop end
+  
+  cout << hRcPt->GetEntries() << "\t" << hMcPt->GetEntries() << endl;
+  
+  hEffPt->Divide(hRcPt, hMcPt);
+  
+  TCanvas* c1 = new TCanvas("c1", "", 700, 600);
+  c1->DrawFrame(0, 0, 5, 1);
+  hEffPt->Draw("same");
+  
+  hOneOverBetaP->Draw("colz");
+}
