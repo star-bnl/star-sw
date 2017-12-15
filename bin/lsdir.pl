e184270a5a3ecbb7ba335b01b8420759257d9935
Yuri Fisyak <fisyak@bnl.gov>
2017-12-15 11:50:43 -0500

    Freeze
    Former-commit-id: 85850cd766728f75709f63531c50315a913d14ee

--- bin/lsdir.pl
+++ bin/lsdir.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/env perl
 use File::Basename;
-my $glob = "/gpfs02/eic/ayk/STAR/reco/MuDst/2010/*/11*";
+#my $glob = "/gpfs02/eic/ayk/STAR/reco/MuDst/2010/*/11*";
+my $glob = "/gpfs02/eic/ayk/STAR/reco/2010AuAu11/MuDst/2010/*/*";
 foreach my $file (glob $glob) {
   my $f = File::Basename::basename($file);
   my $pico = $f . ".picoDst.root";
