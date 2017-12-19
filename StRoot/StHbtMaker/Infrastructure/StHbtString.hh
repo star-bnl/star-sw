0c4ef1678e75b399f68c8c7094cc2f05bb12bb0c
Yuri Fisyak <fisyak@bnl.gov>
2017-12-19 11:56:38 -0500

    cling
    Former-commit-id: 3269bc9f92aa85bfe1c95d0b384a4a2efb228c42

--- StRoot/StHbtMaker/Infrastructure/StHbtString.hh
+++ StRoot/StHbtMaker/Infrastructure/StHbtString.hh
@@ -1,10 +1,4 @@
 #ifndef StHbtString_hh
-#define StHbtString_hh
-
-#if !defined(__CINT__) && !defined(__CLING__)
-
-#ifndef StHbtString_noCint
-#define StHbtString_noCint
 #include <string>
 
 #if !defined(ST_NO_NAMESPACES)
@@ -12,15 +6,5 @@ using std::string;
 #endif
 
 typedef string StHbtString; //!
-#endif
-
-#else
-
-#ifndef StHbtString_yesCint
-#define StHbtString_yesCint
-class StHbtString; //!
-#endif
-
-#endif
 
 #endif
