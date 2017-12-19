66ab43b2d672b36e44b952872fbb6d9e2591916e
Yuri Fisyak <fisyak@bnl.gov>
2017-12-19 11:58:37 -0500

    warn off
    Former-commit-id: 5f441512d494d1b25e42c3bfe3d8e693f3694f8d

--- StRoot/StSpinPool/StFmsTriggerMaker/StFmsTriggerMaker.cxx
+++ StRoot/StSpinPool/StFmsTriggerMaker/StFmsTriggerMaker.cxx
@@ -712,12 +712,12 @@ void StFmsTriggerMaker::writeFmsLayer1ToFpdLayer2(Crate& sim, int t)
 int StFmsTriggerMaker::loadRegisters(int runNumber)
 {
   MYSQL mysql;
-  char* host1 = "dbbak.starp.bnl.gov";
-  char* host2 = "onldb2.starp.bnl.gov";
+  const char* host1 = "dbbak.starp.bnl.gov";
+  const char* host2 = "onldb2.starp.bnl.gov";
   const char* user = "";
   const char* pass = "";
   // See http://drupal.star.bnl.gov/STAR/comp/db/onlinedb/online-sever-port-map
-  char* host = host1;
+  const char* host = host1;
   int year;
   if(mForceRun==0){ 
     mDBTime=GetDBTime(); 
