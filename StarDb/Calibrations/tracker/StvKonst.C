bf9fc6e23cc9153425d0827d16d19d6983f89ea8
perev <>
2018-01-09 16:48:54 +0000

    Tuning

--- StarDb/Calibrations/tracker/StvKonst.C
+++ StarDb/Calibrations/tracker/StvKonst.C
@@ -9,7 +9,7 @@ StvKonst_st row;
 St_StvKonst *tableSet = new St_StvKonst("StvKonst",1);
 //
 memset(&row,0,tableSet->GetRowSize());
-  double myProb = TMath::Prob(30,2);
+  double myProb = TMath::Prob(30*3,2);
   double myFaktor = 3;
   row.mXi2Hit = ROOT::Math::chisquared_quantile_c(myProb,2);		//Xi2 to accept new hit
   row.mXi2Trk = ROOT::Math::chisquared_quantile_c(myProb,30)/30*myFaktor;//Xi2 to accept new track
@@ -18,9 +18,9 @@ memset(&row,0,tableSet->GetRowSize());
   row.mXi2Hlx = row.mXi2Hit*9;  //Xi2 in Helix, .
   row.mDca2dZeroXY = 6.;	//max 2d dca to X=Y=0  for primary track
   row.mDca3dVertex = 3.;	//max 3d dca to vertex for primary track
-  row.mMaxCurv     = 0.2;	//Maximal allowed curvature(5cm radius)
+  row.mMaxCurv     = 1./33;	//Maximal allowed curvature(33cm radius)
   row.mMinP2  = 0.003*0.003;	//Geant3 cut for too small momentum**2	
-  row.mMaxPti   = 200;		/*Maximal allowed 1/pt */
+  row.mMaxPti   =  20;		/*Maximal allowed 1/pt or Pt = 50MeV*/
   row.mMaxRes	= 0.5;		/*Maximal allowed residual */
   row.mCoeWindow= 5.;		/*Maximal window to search hits*/
   row.mMaxWindow= 10.;		/*Maximal window to search hits*/
@@ -35,8 +35,9 @@ memset(&row,0,tableSet->GetRowSize());
   row.mMinTotHits =3;       	/*Min number hits for track*/
   row.mMinGoodHits=3;       	/*Min number good hits for track*/
   row.mMinContHits=3;       	/*Min length of good hit sequence*/
-  row.mMaxContNits=11;      	/*Max length of acceptable non hit sequence*/
-  row.mMaxTotNits =20;      	/*Max number of acceptable non hits*/
+  row.mMaxContNits=5;      	/*Max length of acceptable non hit sequence*/
+  row.mMaxTotNits =10;      	/*Max number of acceptable non hits*/
+  row.mMaxTotNits =10;      	/*Max number of acceptable non hits*/
 
   row.mRxyMax = 207;		//Max radius for tracking
   row.mZMax   = 220;		//Max Z      for tracking
