0b95a4425bfe621d1df259f00e35d57fba95c072
lisa <>
2018-01-11 19:16:27 +0000

    Small changes.  Use basic c-types.  Make non-persistent so remove ClassImp.  Use gRandom by default.

--- StRoot/StEpdUtil/StEpdGeom.cxx
+++ StRoot/StEpdUtil/StEpdGeom.cxx
@@ -12,36 +12,39 @@
  *************************************/
 
 #include "StEpdGeom.h"
-// #include "StEvent/StEpdHit.h"  no StObject dependence
+#include "TRandom.h"
 #include "TRandom3.h"
 #include "TMath.h"
 
-ClassImp(StEpdGeom);
-
 StEpdGeom::StEpdGeom() : mPP(0), mTT(0), mEW(0){
-  pRan = new TRandom3;
-  pRan->SetSeed();
+  if ( gRandom ) {
+    pRan = (TRandom3*)gRandom;
+  }
+  else {
+    pRan = new TRandom3();
+    pRan -> SetSeed();
+  }
 }
 
 StEpdGeom::~StEpdGeom(){
-  delete pRan;
+  if ( pRan && pRan != gRandom ) delete pRan;
 }
 
-void StEpdGeom::SetPpTtEw(Short_t uniqueID){
+void StEpdGeom::SetPpTtEw(short uniqueID){
   mPP = std::abs(uniqueID/100);
   mTT = std::abs(uniqueID % 100);
   mEW = (uniqueID>0)?+1:-1;
 }
 
-Double_t StEpdGeom::GetZwheel(){
+double StEpdGeom::GetZwheel(){
   return 375.0*mEW;
 }
 
-Double_t StEpdGeom::GetPhiCenter(){
-  Double_t phiCenter;
-  Double_t DeltaPhiSS = 30.0*TMath::Pi()/180.0;  // 30 degree supersectors
+double StEpdGeom::GetPhiCenter(){
+  double phiCenter;
+  double DeltaPhiSS = 30.0*TMath::Pi()/180.0;  // 30 degree supersectors
   if (mEW<0){    // east
-    Double_t phiSS = TMath::Pi()/2.0 - (mPP-0.5)*DeltaPhiSS;
+    double phiSS = TMath::Pi()/2.0 - (mPP-0.5)*DeltaPhiSS;
     if (phiSS<0.0) phiSS += 2.0*TMath::Pi();
     if (mTT==1){phiCenter = phiSS;}  // tile 1
     else{
@@ -50,7 +53,7 @@ Double_t StEpdGeom::GetPhiCenter(){
     }
   }
   else{  // west
-    Double_t phiSS = TMath::Pi()/2.0 + (mPP-0.5)*DeltaPhiSS;
+    double phiSS = TMath::Pi()/2.0 + (mPP-0.5)*DeltaPhiSS;
     if (phiSS>2.0*TMath::Pi()) phiSS -= 2.0*TMath::Pi();
     if (mTT==1){phiCenter = phiSS;}  // tile 1
     else{
@@ -61,50 +64,50 @@ Double_t StEpdGeom::GetPhiCenter(){
   return phiCenter;
 }
 
-void StEpdGeom::GetRminRmax(Double_t *Rmin, Double_t *Rmax){
-  Double_t RowHeight[16]={4.4, 4.4, 4.4, 5.53, 5.53, 5.53,
+void StEpdGeom::GetRminRmax(double *Rmin, double *Rmax){
+  double RowHeight[16]={4.4, 4.4, 4.4, 5.53, 5.53, 5.53,
 			  5.53, 5.53, 5.53, 5.53, 5.53, 5.53, 5.53, 5.53, 5.53, 5.53};
-  Double_t Rminimum = 4.6;  // the distance from beamline to the inner edge of tile 1
-  Double_t Rlimit[17];
+  double Rminimum = 4.6;  // the distance from beamline to the inner edge of tile 1
+  double Rlimit[17];
   Rlimit[0] = Rminimum;
-  for (Int_t iRow=0; iRow<16; iRow++){
+  for (int iRow=0; iRow<16; iRow++){
     Rlimit[iRow+1] = Rlimit[iRow] + RowHeight[iRow];
   }
-  Int_t Row = this->Row();
+  int Row = this->Row();
   *Rmin = Rlimit[Row-1];
   *Rmax = Rlimit[Row];
 }
 
 //------------------------------------------------
-Short_t StEpdGeom::Row(Short_t uniqueID){
+short StEpdGeom::Row(short uniqueID){
   SetPpTtEw(uniqueID);
   return this->Row();
 }
-Short_t StEpdGeom::Row(Short_t PP, Short_t TT, Short_t EW){
+short StEpdGeom::Row(short PP, short TT, short EW){
   mPP = PP;
   mTT = TT;
   mEW = EW;
   return this->Row();
 }
-Short_t StEpdGeom::Row(){
+short StEpdGeom::Row(){
   return mTT/2 + 1;
 }
 
 //------------------------------------------------
-TVector3 StEpdGeom::TileCenter(Short_t uniqueID){
+TVector3 StEpdGeom::TileCenter(short uniqueID){
   SetPpTtEw(uniqueID);
   return this->TileCenter();
 }
-TVector3 StEpdGeom::TileCenter(Short_t PP, Short_t TT, Short_t EW){
+TVector3 StEpdGeom::TileCenter(short PP, short TT, short EW){
   mPP = PP;  
   mTT = TT;  
   mEW = EW;  
   return this->TileCenter();
 }
 TVector3 StEpdGeom::TileCenter(){
-  Double_t Rmin,Rmax;
+  double Rmin,Rmax;
   GetRminRmax(&Rmin,&Rmax);
-  Double_t ZZ = this->GetZwheel();
+  double ZZ = this->GetZwheel();
   TVector3 cent;
   cent.SetXYZ(0.5*(Rmin+Rmax),0.0,ZZ);
   cent.RotateZ(this->GetPhiCenter());
@@ -112,11 +115,11 @@ TVector3 StEpdGeom::TileCenter(){
 }
 
 //-----------------------------------------------------
-TVector3 StEpdGeom::RandomPointOnTile(Short_t uniqueID){
+TVector3 StEpdGeom::RandomPointOnTile(short uniqueID){
   SetPpTtEw(uniqueID);
   return this->RandomPointOnTile();
 }
-TVector3 StEpdGeom::RandomPointOnTile(Short_t PP, Short_t TT, Short_t EW){
+TVector3 StEpdGeom::RandomPointOnTile(short PP, short TT, short EW){
   mPP = PP;  
   mTT = TT;  
   mEW = EW;  
@@ -124,16 +127,16 @@ TVector3 StEpdGeom::RandomPointOnTile(Short_t PP, Short_t TT, Short_t EW){
 }
 TVector3 StEpdGeom::RandomPointOnTile(){
 
-  Double_t GapWidth = 0.08;  // one half of the glue gap width
-  Double_t Aparam = 2.0*tan(7.5*TMath::Pi()/180.0);
-  Double_t Bparam = -2.0*GapWidth;
+  double GapWidth = 0.08;  // one half of the glue gap width
+  double Aparam = 2.0*tan(7.5*TMath::Pi()/180.0);
+  double Bparam = -2.0*GapWidth;
 
-  Double_t ZZ = this->GetZwheel();
-  Double_t Rmin,Rmax;
+  double ZZ = this->GetZwheel();
+  double Rmin,Rmax;
   GetRminRmax(&Rmin,&Rmax);
-  Short_t RR=this->Row();
-  Double_t Xmin = Rmin + GapWidth;
-  Double_t Xmax = Rmax - GapWidth;
+  short RR=this->Row();
+  double Xmin = Rmin + GapWidth;
+  double Xmax = Rmax - GapWidth;
   if (RR==1) Xmin -= 2.0*GapWidth;  // no glue on the "inside" of tile 1
   if (RR==16) Xmax += GapWidth; // no glue on "outside" of TT30,31
 
@@ -143,17 +146,17 @@ TVector3 StEpdGeom::RandomPointOnTile(){
   // it doesn't fit in the tile, I try again until it's finally there.
   if (RR==1) Xmax += GapWidth;
 
-  Double_t A = Aparam;
+  double A = Aparam;
   if (1==RR) A*= 2.0;
 
-  Double_t gamma = 0.5*A*pow(Xmin,2)+Bparam*Xmin;
-  Double_t alpha = 0.5*A*pow(Xmax,2)+Bparam*Xmax-gamma;
+  double gamma = 0.5*A*pow(Xmin,2)+Bparam*Xmin;
+  double alpha = 0.5*A*pow(Xmax,2)+Bparam*Xmax-gamma;
 
-  Double_t q = pRan->Rndm();
-  Double_t XX = (sqrt(pow(Bparam,2)+2.0*A*(alpha*q+gamma)) - Bparam)/A;
+  double q = pRan->Rndm();
+  double XX = (sqrt(pow(Bparam,2)+2.0*A*(alpha*q+gamma)) - Bparam)/A;
   q = pRan->Rndm();
-  Double_t DeltaY = A*XX+Bparam;
-  Double_t YY = (q-0.5)*DeltaY;
+  double DeltaY = A*XX+Bparam;
+  double YY = (q-0.5)*DeltaY;
 
   TVector3 Point(XX,YY,ZZ);
   Point.RotateZ(this->GetPhiCenter());
@@ -169,35 +172,35 @@ TVector3 StEpdGeom::RandomPointOnTile(){
 }
 
 //----------------------------------------------------
-void StEpdGeom::GetCorners(Short_t uniqueID,
-			   Int_t* nCorners, Double_t* x, Double_t* y){
+void StEpdGeom::GetCorners(short uniqueID,
+			   int* nCorners, double* x, double* y){
   SetPpTtEw(uniqueID);
   GetCorners(nCorners,x,y);
 }
-void StEpdGeom::GetCorners(Short_t position, Short_t tilenumber, Short_t eastwest,
-		Int_t* nCorners, Double_t* x, Double_t* y){
+void StEpdGeom::GetCorners(short position, short tilenumber, short eastwest,
+		int* nCorners, double* x, double* y){
   mPP = position;
   mTT = tilenumber;
   mEW = eastwest;
   GetCorners(nCorners,x,y);
 }
-void StEpdGeom::GetCorners(Int_t* nCorners, Double_t* xc, Double_t* yc){
-  Double_t x[5];
-  Double_t y[5];
+void StEpdGeom::GetCorners(int* nCorners, double* xc, double* yc){
+  double x[5];
+  double y[5];
   // we provde the user five corners.  For tiles 2-31, the fifth "corner" is junk.
   // only tile 1 is a pentagon
-  Double_t OpeningAngle = 7.5*TMath::Pi()/180.0;
-  Double_t GapWidth = 0.08;  // gap between tiles / 2
-  Short_t RR = this->Row();
-  Double_t Rmin,Rmax;
+  double OpeningAngle = 7.5*TMath::Pi()/180.0;
+  double GapWidth = 0.08;  // gap between tiles / 2
+  short RR = this->Row();
+  double Rmin,Rmax;
   GetRminRmax(&Rmin,&Rmax);
   if (1==RR){
     *nCorners=5;
-    Double_t xtmp[3], ytmp[3];
+    double xtmp[3], ytmp[3];
     xtmp[0] = Rmin;  ytmp[0] = +Rmin*tan(OpeningAngle);
     xtmp[1] = Rmax;  ytmp[1] = +Rmax*tan(OpeningAngle);
     xtmp[2] = Rmax;  ytmp[2] = -Rmax*tan(OpeningAngle);
-    for (Int_t ic=0; ic<3; ic++){
+    for (int ic=0; ic<3; ic++){
       x[ic] =  xtmp[ic]*cos(OpeningAngle) - ytmp[ic]*sin(OpeningAngle);
       y[ic] = +xtmp[ic]*sin(OpeningAngle) + ytmp[ic]*cos(OpeningAngle);
     }
@@ -221,30 +224,30 @@ void StEpdGeom::GetCorners(Int_t* nCorners, Double_t* xc, Double_t* yc){
       x[2] += GapWidth;
     }
   }
-  Double_t phi = this->GetPhiCenter();
-  for (Int_t icorn=0; icorn<(*nCorners); icorn++){
+  double phi = this->GetPhiCenter();
+  for (int icorn=0; icorn<(*nCorners); icorn++){
     xc[icorn] = +x[icorn]*cos(phi) - y[icorn]*sin(phi);
     yc[icorn] = +x[icorn]*sin(phi) + y[icorn]*cos(phi);
   }
 }
 
 //---------------------------------------------------------------------  
-Bool_t StEpdGeom::IsInTile(Short_t uniqueID,
-			   Double_t x, Double_t y){
+bool StEpdGeom::IsInTile(short uniqueID,
+			   double x, double y){
   SetPpTtEw(uniqueID);
   return this->IsInTile(x,y);
 }
-Bool_t StEpdGeom::IsInTile(Short_t position, Short_t tilenumber, Short_t eastwest,
-			   Double_t x, Double_t y){
+bool StEpdGeom::IsInTile(short position, short tilenumber, short eastwest,
+			   double x, double y){
   mPP = position;
   mTT = tilenumber;
   mEW = eastwest;
   return this->IsInTile(x,y);
 }
-Bool_t StEpdGeom::IsInTile(Double_t x, Double_t y){
-  Double_t PolygonX[6];
-  Double_t PolygonY[6];
-  Int_t numberOfCorners;
+bool StEpdGeom::IsInTile(double x, double y){
+  double PolygonX[6];
+  double PolygonY[6];
+  int numberOfCorners;
   this->GetCorners(&numberOfCorners,PolygonX,PolygonY);
   PolygonX[numberOfCorners] = PolygonX[0];     PolygonY[numberOfCorners] = PolygonY[0];
   return TMath::IsInside(x,y,numberOfCorners+1,PolygonX,PolygonY);  
