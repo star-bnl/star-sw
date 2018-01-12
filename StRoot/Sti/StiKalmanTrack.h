cea47d1f1af83a6cafac214c693b8d3f9d265362
Dmitri Smirnov <dsmirnov@bnl.gov>
2018-01-12 23:17:09 +0000

    Removed declared but undefined functions

--- StRoot/Sti/StiKalmanTrack.h
+++ StRoot/Sti/StiKalmanTrack.h
@@ -212,11 +212,7 @@ class StiKalmanTrack : public StiTrack
    int isPrimary() const	{return mVertex;}
    void setPrimary(int vertex) 	{mVertex=vertex;}
 
-	double calculateTrackLength() const;
-	double calculateTrackSegmentLength(const StiKalmanTrackNode &p1, const StiKalmanTrackNode &p2) const;
 	double getTrackRadLength() const;
-	int calculatePointCount() const;
-	int calculateMaxPointCount() const;
 
   double getTpcDedx() const;
   double getSvtDedx() const;
