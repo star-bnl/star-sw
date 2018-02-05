eba0c6fea57ee1b67d66a13adff5ac98b0271f33
Yuri Fisyak <fisyak@bnl.gov>
2018-02-05 12:46:18 -0500

    Merge with Maksym
    Former-commit-id: 053c6c7075b7cff6d78908b1837066c5d8fa4b19

---
+++ StRoot/StKFParticleInterface/StKFParticleInterface.h
@@ -0,0 +1,105 @@
+#ifndef StKFParticleInterface_H
+#define StKFParticleInterface_H
+#include <vector>
+
+#include "KFParticle/KFParticle.h"
+#include "TObject.h"
+
+
+class KFParticleTopoReconstructor;
+class KFTopoPerformance;
+class KFVertex;
+class StPicoDst;
+class StMuDst;
+class StDcaGeometry;
+class KFPTrack;
+
+class StKFParticleInterface: public TObject
+{
+ public:
+   
+  StKFParticleInterface();
+  ~StKFParticleInterface();
+  
+  void InitParticles();
+  void ReconstructParticles();
+  void ReconstructTopology();
+  
+  const std::vector<KFParticle> &GetParticles() const;
+  const std::vector<KFParticle>* GetSecondaryCandidates() const;                      // Get secondary particles with the mass constraint
+  const std::vector<KFParticle>& GetSecondaryK0()         const;
+  const std::vector<KFParticle>& GetSecondaryLambda()     const;
+  const std::vector<KFParticle>& GetSecondaryAntiLambda() const;
+  const std::vector<KFParticle>& GetSecondaryGamma()      const;
+  const std::vector<KFParticle>& GetSecondaryPi0()        const;
+  const std::vector< std::vector<KFParticle> >* GetPrimaryCandidates() const;         // Get primary particles with the mass constraint
+  const std::vector< std::vector<KFParticle> >* GetPrimaryTopoCandidates() const;     // Get primary particles with the topologigal constraint
+  const std::vector< std::vector<KFParticle> >* GetPrimaryTopoMassCandidates() const; // Get primary particles with the topologigal and mass constraint
+  void SetParticles(std::vector<KFParticle>& particles)
+  {
+    fParticles = particles;
+    if(fParticlesPdg.size() != fParticles.size())
+    {
+      fParticlesPdg.clear();
+      fParticlesPdg.resize(fParticles.size(), -1);
+    }
+  }
+  void SetParticlesPdg(std::vector<int>& pdg) { fParticlesPdg = pdg;}
+  void SetHftHits(std::vector<int>& nHftHits) { fNHftHits = nHftHits; }
+  
+  void SetField(float field);
+  void SetBeamLine(KFParticle& p);
+  
+  void CleanPV();
+  void AddPV(const KFVertex &pv, const std::vector<int> &tracks);
+  void AddPV(const KFVertex &pv);
+  void AddParticle(const KFParticle& p);
+  void AddCandidate(const KFParticle& candidate, int iPV = -1);
+  
+  const KFParticleTopoReconstructor* GetTopoReconstructor() { return fKFParticleTopoReconstructor; }
+  
+  void SetPrimaryProbCut(float prob);
+  
+  bool ProcessEvent(StPicoDst* picoDst, std::vector<int>& goodTracks);
+//   bool ProcessEvent(StMuDst* muDst);
+  bool OpenCharmTrigger();
+  void OpenCharmTriggerCompression(int nTracksTriggered, int nTracksInEvent, bool triggerDMesons);
+  
+  
+  //PID cuts 
+  void SetStrictTofPidMode() { fStrictTofPID = true;  }
+  void SetSoftTofPidMode()   { fStrictTofPID = false; }
+  void SetSoftKaonPIDMode()  { fCleanKaonsWitTof = false; }
+  void UseCorrecteddEdX()    { fdEdXMode = 2; }
+  void SetTriggerMode()      { fTriggerMode = true; }
+  //KF Particle Finder cuts
+  void SetChiPrimaryCut(float cut)  { fChiPrimaryCut = cut; }
+  
+ private:
+  
+  double InversedChi2Prob(double p, int ndf) const;
+  bool IsGoodPV(const KFVertex& pv);
+  bool GetTrack(const StDcaGeometry& dcaG, KFPTrack& track, int q, int index);
+  std::vector<int> GetTofPID(double m2, double p, int q);
+  std::vector<int> GetPID(double m2, double p, int q, float dEdXPull[7], bool isTofm2);
+  void AddTrackToParticleList(const KFPTrack& track, int nHftHitsInTrack, int index, const std::vector<int>& totalPDG, KFVertex& pv, std::vector<int>& primaryTrackList,
+                              std::vector<int>& nHftHits, std::vector<int>& particlesPdg, std::vector<KFParticle>& particles, int& nPartSaved);
+  
+  KFParticleTopoReconstructor* fKFParticleTopoReconstructor;
+  std::vector<KFParticle> fParticles;
+  std::vector<int> fParticlesPdg;
+  std::vector<int> fNHftHits;
+  
+  //PID cuts
+  bool fStrictTofPID;
+  bool fCleanKaonsWitTof;
+  int fdEdXMode;
+  //trigger cuts
+  bool fTriggerMode;
+  //KF Particle Finder cuts
+  float fChiPrimaryCut;
+  
+  ClassDef(StKFParticleInterface,1)
+};
+
+#endif //#ifndef StKFParticleInterface_H
---
+++ StRoot/StKFParticleInterface/StKFParticlePerformanceInterface.cxx
@@ -0,0 +1,111 @@
+#include "StKFParticlePerformanceInterface.h"
+
+#include "KFParticle/KFParticleTopoReconstructor.h"
+#include "KFParticlePerformance/KFTopoPerformance.h"
+#include "TFile.h"
+#include "TDirectory.h"
+
+ClassImp(StKFParticlePerformanceInterface)
+
+StKFParticlePerformanceInterface::StKFParticlePerformanceInterface(const KFParticleTopoReconstructor* tr, bool storeMCHistograms, TString outFileName): 
+  fOutFileName(outFileName), fOutFile(0), fEfffileName("Efficiency.txt"),fKFTopoPerformance(0), fMCTracks(0), fMCIndexes(0)
+{
+  fKFTopoPerformance = new KFTopoPerformance();
+  fKFTopoPerformance->SetTopoReconstructor(tr);
+  
+  TFile* curFile = TFile::CurrentFile();
+  TDirectory* curDirectory = TDirectory::CurrentDirectory();
+  if (fOutFileName == "")  fOutFileName = "StKFParticleFinderQA.root";
+  if(! curFile)     fOutFile = new TFile(fOutFileName.Data(),"RECREATE");
+  TDirectory *dir = TDirectory::CurrentDirectory();
+  if(!storeMCHistograms)
+    fKFTopoPerformance->DoNotStoreMCHistograms();
+  fKFTopoPerformance->CreateHistos("",dir);
+  
+  TFile::CurrentFile() = curFile;
+  TDirectory::CurrentDirectory() = curDirectory;
+}
+
+StKFParticlePerformanceInterface::~StKFParticlePerformanceInterface()
+{
+  if(fKFTopoPerformance)
+  {
+#if 1
+    TDirectory *curr = TDirectory::CurrentDirectory();
+    TFile *currentFile = TFile::CurrentFile();
+    if (currentFile && currentFile->IsWritable()) {
+      fOutFile->cd();
+      if (fKFTopoPerformance->GetHistosDirectory()) {
+	WriteHistosCurFile(fKFTopoPerformance->GetHistosDirectory());
+      }
+      if(!(fOutFileName == ""))
+	{   
+	  fOutFile->Close();
+	  fOutFile->Delete();
+	}
+    }
+    TFile::CurrentFile() = currentFile;
+    TDirectory::CurrentDirectory() = curr;
+#else
+    if (fOutFile && fOutFile->IsWritable()) fOutFile->Write();
+#endif
+    
+    std::fstream eff(fEfffileName.Data(),std::fstream::out);
+    eff << fKFTopoPerformance->fParteff;
+    eff.close();
+  }
+  SafeDelete(fKFTopoPerformance);
+}
+
+void StKFParticlePerformanceInterface::PerformanceAnalysis()
+{
+  if ( fKFTopoPerformance )
+  {
+    fKFTopoPerformance->SetMCTracks(fMCTracks);
+    fKFTopoPerformance->SetTrackMatch(fMCIndexes);
+  
+    fKFTopoPerformance->CheckMCTracks();
+    fKFTopoPerformance->MatchTracks();
+    fKFTopoPerformance->FillHistos();
+  }
+}
+#if 1
+void StKFParticlePerformanceInterface::WriteHistosCurFile( TObject *obj ){
+  
+  if( !obj->IsFolder() ) obj->Write();
+  else{
+    TDirectory *cur = TDirectory::CurrentDirectory();
+    TFile *currentFile = TFile::CurrentFile();
+
+    TDirectory *sub = cur->GetDirectory(obj->GetName());
+    sub->cd();
+    TList *listSub = (static_cast<TDirectory*>(obj))->GetList();
+    TIter it(listSub);
+    while( TObject *obj1=it() ) WriteHistosCurFile(obj1);
+    cur->cd();
+    TFile::CurrentFile() = currentFile;
+    TDirectory::CurrentDirectory() = cur;
+  }
+}
+#endif
+void StKFParticlePerformanceInterface::SetPrintEffFrequency(Int_t n)
+{ 
+  fKFTopoPerformance->SetPrintEffFrequency(n); 
+}
+
+int StKFParticlePerformanceInterface::GetNReconstructedParticles()
+{
+  return fKFTopoPerformance->GetTopoReconstructor()->GetParticles().size();
+}
+
+bool StKFParticlePerformanceInterface::GetParticle(KFParticle& particle, const int iParticle)
+{
+  if(iParticle<0 || iParticle>=fKFTopoPerformance->GetTopoReconstructor()->GetParticles().size())
+  {
+    std::cout << "Error!!!    StKFParticlePerformanceInterface::GetParticle()   iParticle " << iParticle << " size " << GetNReconstructedParticles() << std::endl;
+    return 0;
+  }
+  particle = fKFTopoPerformance->GetTopoReconstructor()->GetParticles()[iParticle];
+  bool isMatched = fKFTopoPerformance->ParticlesMatch()[iParticle].IsMatchedWithPdg();
+  return isMatched;
+}
