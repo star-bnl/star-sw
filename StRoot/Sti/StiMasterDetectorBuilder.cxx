f5904b41aba5739f4802addf081e837847ea1c30
Victor Perevoztchikov <perev@bnl.gov>
2018-02-17 01:42:17 +0000

    iTPC

--- StRoot/Sti/StiMasterDetectorBuilder.cxx
+++ StRoot/Sti/StiMasterDetectorBuilder.cxx
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include "Riostream.h"
 #include "StiMasterDetectorBuilder.h"
 #include "TGeoManager.h"
 #include "StiDetector.h"
@@ -22,6 +24,8 @@ void StiMasterDetectorBuilder::reset()
  */
 void StiMasterDetectorBuilder::build(StMaker&source)
 {
+  //#define __Physics_Node_Clear__
+#ifndef __Physics_Node_Clear__
   if (! gGeoManager) StiVMCToolKit::GetVMC();
 
   TGeoManager *gGeoManagerSV = gGeoManager; gGeoManager = 0;
@@ -30,7 +34,7 @@ void StiMasterDetectorBuilder::build(StMaker&source)
   LOG_INFO << "We are StiMasterDetectorBuilder::build() and will clone the geometry if exists" << endm;
   if ( chain ){
     TDatime t = chain->StMaker::GetDBTime();
-    LOG_INFO << "Timestanp check - Date/Time from chain is " << t.GetDate() << "/" << t.GetTime() << endm;
+    LOG_INFO << "Timestamp check - Date/Time from chain is " << t.GetDate() << "/" << t.GetTime() << endm;
   }
 
 #if 1
@@ -42,65 +46,114 @@ void StiMasterDetectorBuilder::build(StMaker&source)
   }
 #else
   if ( chain ) {
-    cout << "StiMasterDetectorBuilder::build() -I- Create clone of VmcGeometry by reinitialization for reconstruction" <<endl;
+    LOG_INFO << "StiMasterDetectorBuilder::build() -I- Create clone of VmcGeometry by reinitialization for reconstruction" <<endm;
     TDataSet *set = chain->GetDataBase("VmcGeometry/Geometry", &chain->StMaker::GetDBTime());
     delete set;
   }
 #endif
-  cout << "StiMasterDetectorBuilder::build() -I- Started"<<endl;
+#endif /* ! __Physics_Node_Clear__ */
+  LOG_INFO << "StiMasterDetectorBuilder::build() -I- Started"<<endm;
   vector<StiDetectorBuilder*>::iterator iter;
-  unsigned int nRows=0;
-  for (iter=begin();
-       iter!=end();
-       iter++)
-    {
-      cout << "StiMasterDetectorBuilder::build() -I- Calling Group Builder named:" << (*iter)->getName()<<endl;
-      if (!*iter) cout <<"   pointer is corrupted!!!!!!!!!!!!!!!!!!!"<<endl;
-      (*iter)->build(source);
-      nRows+=(*iter)->getNRows();
-    }
-  cout << "StiMasterDetectorBuilder::build() -I- Will build local array"<<endl;
+  UInt_t nRows=0;
+  for (iter=begin();       iter!=end();       iter++)    {
+    LOG_INFO << "StiMasterDetectorBuilder::build() -I- Calling Group Builder named:" << (*iter)->getName()<<endm;
+    if (!*iter) {LOG_INFO <<"   pointer is corrupted!!!!!!!!!!!!!!!!!!!"<<endm;}
+    (*iter)->build(source);
+    nRows+=(*iter)->getNRows();
+  }
+  LOG_INFO << "StiMasterDetectorBuilder::build() -I- Will build local array"<<endm;
   setNRows(nRows);
-  unsigned int row=0;
-  for (iter=begin();
-       iter!=end();
-       iter++)
-    {
-      cout << "StiMasterDetectorBuilder::build() -I- Builder:"<<(*iter)->getName()<<endl;
-      for (unsigned int i=0;i<(*iter)->getNRows();i++)
-	{
-	  cout << "StiMasterDetectorBuilder::build() -I- row:"<<row;
-	  unsigned int nSectors = (*iter)->getNSectors(i);
-	  setNSectors(row,nSectors);
-	  Int_t ifrow = 0;
-	  for (unsigned int sector=0;sector<nSectors;sector++)
-	    {
-	      StiDetector *detector = (*iter)->getDetector(i,sector);
-	      if (!ifrow && detector) {cout << "\t" << detector->getName(); ifrow++;}
-	      setDetector(row,sector,detector);
-	    }
-	  cout << endl;
-	  row++;
+  UInt_t row=0;
+  for (iter=begin();       iter!=end();       iter++)    {
+    for (UInt_t i=0;i<(*iter)->getNRows();i++)	{
+      UInt_t nSectors = (*iter)->getNSectors(i);
+      setNSectors(row,nSectors);
+      for (UInt_t sector=0;sector<nSectors;sector++)	    {
+	StiDetector *detector = (*iter)->getDetector(i,sector);
+	if (detector && detector->isActive()) {
+	  //	  if (! sector) {
+	    LOG_INFO << "StiMasterDetectorBuilder::build() -I- row:"<<row << "\t" << *detector << endm;
+	    //	  }
+	  setDetector(row,sector,detector);
 	}
+      }
+      row++;
     }
+  }
+#ifndef __Physics_Node_Clear__
   if (gGeoManagerSV) {
-    //VP    SafeDelete(gGeoManager); 
+    //#define __CHECK_SHARED_OBJECTS__
+#ifdef __CHECK_SHARED_OBJECTS__
+    // Clean up shared objects 
+    enum {NoLists = 12};
+    TSeqCollection *listSV[NoLists] = {
+      gGeoManagerSV->GetListOfMaterials(),      // TList                 
+      gGeoManagerSV->GetListOfMedia(),	        // TList                 
+      gGeoManagerSV->GetListOfNodes(),          // TObjArray             
+      gGeoManagerSV->GetListOfPhysicalNodes(),  // TObjArray             
+      gGeoManagerSV->GetListOfOverlaps(),	// TObjArray             
+      gGeoManagerSV->GetListOfMatrices(),       // TObjArray             
+      gGeoManagerSV->GetListOfVolumes(),        // TObjArray             
+      gGeoManagerSV->GetListOfGVolumes(),       // TObjArray             
+      gGeoManagerSV->GetListOfShapes(),	        // TObjArray             
+      gGeoManagerSV->GetListOfGShapes(),	// TObjArray             
+      gGeoManagerSV->GetListOfUVolumes(),       // TObjArray             
+      gGeoManagerSV->GetListOfTracks()};        // TObjArray             
+    TSeqCollection *list[NoLists] = {
+      gGeoManager->GetListOfMaterials(),      // TList                 
+      gGeoManager->GetListOfMedia(),	      // TList                 
+      gGeoManager->GetListOfNodes(),          // TObjArray             
+      gGeoManager->GetListOfPhysicalNodes(),  // TObjArray             
+      gGeoManager->GetListOfOverlaps(),       // TObjArray             
+      gGeoManager->GetListOfMatrices(),       // TObjArray             
+      gGeoManager->GetListOfVolumes(),        // TObjArray             
+      gGeoManager->GetListOfGVolumes(),       // TObjArray             
+      gGeoManager->GetListOfShapes(),	      // TObjArray             
+      gGeoManager->GetListOfGShapes(),	      // TObjArray             
+      gGeoManager->GetListOfUVolumes(),       // TObjArray             
+      gGeoManager->GetListOfTracks()};	      // TObjArray             
+    for (Int_t l = 0; l < NoLists; l++) {
+      if (listSV[l] && list[l]) {
+	TIter nextSV(listSV[l]);
+	TObject *oSV = 0;
+	while ((oSV = nextSV())) {
+	  TIter next(list[l]);
+	  TObject *o = 0;
+	  while ((o = next())) {
+	    if (o == oSV) {
+	      LOG_INFO << "Duplicate object " << o->GetName() << "\t" << o->GetTitle() << endm;
+	      Int_t indx = list[l]->IndexOf(o);
+	      if (indx >= 0) {
+		list[l]->RemoveAt(indx);
+	      }
+	    }
+	  }
+	}
+      }
+    }
+    //    delete gGeoManager;
+#endif /* __CHECK_SHARED_OBJECTS__ */
     gGeoManager = gGeoManagerSV;
   }
-  cout << "StiMasterDetectorBuilder::build() -I- Done"<<endl;
+#else /* __Physics_Node_Clear__ */
+  LOG_INFO << "StiMasterDetectorBuilder::build() -I- ClearPhysicalNodes"<<endm;
+  gGeoManager->ClearPhysicalNodes(kTRUE);
+#undef __Physics_Node_Clear__
+#endif /* !  __Physics_Node_Clear__ */
+  LOG_INFO << "StiMasterDetectorBuilder::build() -I- Done"<<endm;
 }
 
 /*! Return true if this builder has not served all detector objects currently registered with it.
  */
 bool StiMasterDetectorBuilder::hasMore() const
 { 
-  //cout << "StiMasterDetectorBuilder::build() -I- Started"<<endl;
+  //LOG_INFO << "StiMasterDetectorBuilder::build() -I- Started"<<endm;
   vector<StiDetectorBuilder*>::const_iterator iter;
   for (iter=begin();
        iter!=end();
        iter++)
     {
-      //cout << "StiMasterDetectorBuilder::hasMore() -I- Calling Group Builder named:" << (*iter)->getName()<<endl;
+      //LOG_INFO << "StiMasterDetectorBuilder::hasMore() -I- Calling Group Builder named:" << (*iter)->getName()<<endm;
       if((*iter)->hasMore()) return true;
     }
   return false;
@@ -111,7 +164,7 @@ no detector object left to server.
  */
 StiDetector * StiMasterDetectorBuilder::next()
 {
-  //cout << "StiMasterDetectorBuilder::next() -I- Started"<<endl;
+  //LOG_INFO << "StiMasterDetectorBuilder::next() -I- Started"<<endm;
   vector<StiDetectorBuilder*>::const_iterator iter;
   for (iter=begin();
        iter!=end();
@@ -135,8 +188,13 @@ StiDetectorBuilder * StiMasterDetectorBuilder::get(const string & name)
        iter!=end();
        iter++)
     {
-      //cout << "StiMasterDetectorBuilder::next() -I- Calling Group Builder named:" << (*iter)->getName()<<endl;
+      //LOG_INFO << "StiMasterDetectorBuilder::next() -I- Calling Group Builder named:" << (*iter)->getName()<<endm;
       if((*iter)->isName(name)) return *iter;
     }
+#if 0
 	throw runtime_error("StiMasterDetectorBuilder::get(const string & name) -E- Requested object not found");
+#else
+	cout << "StiMasterDetectorBuilder::get(const string & name) -E- Requested object not found" << endl;
+	assert(0);
+#endif
 }
