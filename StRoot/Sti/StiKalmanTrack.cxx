0e6021e4292467ee1cba8d033d8bb6c449d05c3b
Victor Perevoztchikov <perev@bnl.gov>
2018-03-06 17:51:56 +0000

    Method legal() forced to return always 1 (temporary) This method not always correct, whih keads to dfiifetences between DEV and iTPC versions for non iTPC case.Later it will be fixed

--- StRoot/Sti/StiKalmanTrack.cxx
+++ StRoot/Sti/StiKalmanTrack.cxx
@@ -1,11 +1,16 @@
 //StiKalmanTrack.cxx
 /*
- * $Id: StiKalmanTrack.cxx,v 2.148.2.2 2018/02/28 01:47:37 perev Exp $
- * $Id: StiKalmanTrack.cxx,v 2.148.2.2 2018/02/28 01:47:37 perev Exp $
+ * $Id: StiKalmanTrack.cxx,v 2.148.2.3 2018/03/06 17:51:56 perev Exp $
+ * $Id: StiKalmanTrack.cxx,v 2.148.2.3 2018/03/06 17:51:56 perev Exp $
  *
  * /author Claude Pruneau
  *
  * $Log: StiKalmanTrack.cxx,v $
+ * Revision 2.148.2.3  2018/03/06 17:51:56  perev
+ * Method legal() forced to return always 1 (temporary)
+ * This method not always correct, whih keads to dfiifetences
+ * between DEV and iTPC versions for non iTPC case.Later it will be fixed
+ *
  * Revision 2.148.2.2  2018/02/28 01:47:37  perev
  * Debug--
  *
@@ -1938,17 +1943,66 @@ int StiKalmanTrack::idTruth(int *qa) const
 //_____________________________________________________________________________
 int StiKalmanTrack::legal(const StiHit *hit)  const
 {
-  enum {kNear= 40};
-
+  enum {kNear= 50};
+  int ans[]={
+/* 0 00000 G G+ G+*/ 1
+/* 1 00001 G G+ G-*/,1
+/* 2 00010 G G+ B+*/,0
+/* 3 00011 G G+ B-*/,1
+/* 4 00100 G G- G+*/,0
+/* 5 00101 G G- G-*/,1
+/* 6 00110 G G- B+*/,0
+/* 7 00111 G G- B-*/,1
+/* 8 01000 G B+ G+*/,0
+/* 9 01001 G B+ G-*/,0
+/*10 01010 G B+ B+*/,0
+/*11 01011 G B- B-*/,1
+/*12 01100 G B- G+*/,0
+/*13 01101 G B- G-*/,0
+/*14 01110 G B- B+*/,0
+/*15 01111 G B- B-*/,1
+/*16 10000 B G+ G+*/,0
+/*17 10001 B G+ G-*/,0
+/*18 10010 B G+ B+*/,0
+/*19 10011 B G+ B-*/,0
+/*20 10100 B G+ B+*/,0
+/*21 10101 B G- G-*/,1
+/*22 10110 B G- B+*/,0
+/*23 10111 B G- B-*/,0
+/*24 11000 B B+ G+*/,0
+/*25 11001 B B+ G-*/,1
+/*26 11010 B B+ B+*/,1
+/*27 11011 B B+ B-*/,0
+/*28 11100 B B- G+*/,0
+/*29 11101 B B- G-*/,0
+/*30 11110 B B- B+*/,0
+/*31 11111 B B- B-*/,0
+};  
   double zH = hit->z(); if (fabs(zH)>kNear)	return 1; //Too far from memb
   double vH = hit->vz();if (fabs(vH)<=   0) 	return 1; //Non TPC hit
-  const auto fstHit = getInnerMostHitNode(1)->getHit();
-  const auto lstHit = getOuterMostHitNode(1)->getHit();
+//		Hit info
+  int qH = (zH*vH<0); //hit quality
 
-  auto *tkHit = (fabs(fstHit->z()-hit->z()) < 
-                 fabs(lstHit->z()-hit->z()))
-                ? fstHit:lstHit;
-  if (tkHit->vz()*vH >= 0) return 1;	//same sign of drift velosity
-  if (tkHit->z() *zH <= 0) return 1;	//but Z sign are different
-  return 0;				//wrong hit
+  const auto *fstHit = getInnerMostHitNode(1)->getHit();
+  const auto *lstHit = getOuterMostHitNode(1)->getHit();
+  double zF = fstHit->z();
+  double zL = lstHit->z();
+
+  if (fabs(zF-zH) < fabs(zL-zH)) {
+    const auto *swap = fstHit; fstHit = lstHit; lstHit = swap;
+          auto  zwap = zF;     zF     = zL;     zL = zwap;
+  }
+  
+  double vF = fstHit->vz(); int qF = (zF*vF<0); //first hit quality
+  double vL = lstHit->vz(); int qL = (zL*vL<0); // last hit quality
+  if (fabs(vF*vL)<=0) return 1;
+  int kase = 0;
+  if (zH*zF<0) kase|= 1; 	//new hit in opposite side
+  if (qH     ) kase|= 2;	//new hit is bad
+  if (zL*zF<0) kase|= 4; 	//lst hit in opposite side
+  if (qL     ) kase|= 8;	//lst hit is bad
+  if (qF     ) kase|=16;	//fst hit is bad
+  if (!ans[kase]) StiDebug::Count("legal0",kase);
+  return 1; ///????????????????????????????????
+  return ans[kase];
 }
