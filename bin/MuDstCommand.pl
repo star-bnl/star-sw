77fe05d7a8cf51360e5686f80cbfe1b8b5bf687e
Yuri Fisyak <fisyak@bnl.gov>
2018-03-11 23:38:44 -0400

    Freeze
    Former-commit-id: 64ffe40876cf6d44111bdbca5e6a4535a4fb63f0

--- bin/MuDstCommand.pl
+++ bin/MuDstCommand.pl
@@ -3,12 +3,21 @@ use File::Basename;
 #print "ARGV[0] = $ARGV[0]\n";
 #my @list = glob $ARGV[0]; 
 #my @list = @ARGV; 
-my @list = glob "/net/l404/data/fisyak/reco/2016/AuAu200_adc/*.MuDst.root";
+my @list = ();
+my $MUDST = "/net/l404/data/fisyak/reco/2016/AuAu200_adc";
+if (-d $MUDST) {
+  @list = glob $MUDST . "/*.MuDst.root";
+} else {
+  $MUDST = "/gpfs02/eic/ayk/STAR/reco/MuDst/AuAu_200_production_2016/ReversedFullField/P16ij/2016";
+  if (-d $MUDST) {
+    @list = glob $MUDST . "/*/*/*.MuDst.root";
+  }
+}
 my $no = 0;
 foreach my $line (@list) {
   my $mudst = File::Basename::basename($line);
   if (-r $mudst) {next;}
   print "string:$line\n";
   $no++;
-#  if ($no >= 400) {last;}
+  if ($no >= 10) {last;}
 }
