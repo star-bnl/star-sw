dfc8e45b84b9d529751a364f5c5d4dd7a884e493
Yuri Fisyak <fisyak@bnl.gov>
2018-03-21 10:57:19 -0400

    Merge
    Former-commit-id: f01a3b74d99411b030ddadc3679a9faab9b317b8

--- StRoot/St_geant_Maker/St_geant_Maker.cxx
+++ StRoot/St_geant_Maker/St_geant_Maker.cxx
@@ -1,5 +1,13 @@
-// $Id: St_geant_Maker.cxx,v 1.168 2017/12/28 19:10:33 jwebb Exp $
+// $Id: St_geant_Maker.cxx,v 1.170 2018/03/20 20:46:26 jwebb Exp $
 // $Log: St_geant_Maker.cxx,v $
+// Revision 1.170  2018/03/20 20:46:26  jwebb
+//
+// Re-enable the FTSA (fts "active") volume.  (Temporarily lost when I during
+// integration of the FtsdGeo1 model).
+//
+// Revision 1.169  2018/03/19 17:50:42  jwebb
+// Setup support for 3 planes of Si tracking
+//
 // Revision 1.168  2017/12/28 19:10:33  jwebb
 //
 // Update sensitive volume names in St_geant_Maker and g2t_epd.F for interface
@@ -1535,9 +1543,11 @@ Int_t St_geant_Maker::Make() {
     iRes = g2t_hca( g2t_track, g2t_hca_hit);
   } 
 
-  nhits=0;
-
-  geant3 -> Gfnhit("FTSH","FTSA", nhits); 
+  nhits=nhit1=0;
+  geant3 -> Gfnhit("FTSH","FTSA", nhit1); nhits+=nhit1; nhit1=0;
+  geant3 -> Gfnhit("FTSH","FSIA", nhit1); nhits+=nhit1; nhit1=0;
+  geant3 -> Gfnhit("FTSH","FSIB", nhit2); nhits+=nhit1; nhit1=0;
+  geant3 -> Gfnhit("FTSH","FSIC", nhit3); nhits+=nhit1; nhit1=0;
   if ( nhits > 0 ) {
     St_g2t_fts_hit *g2t_fts_hit = new St_g2t_fts_hit("g2t_fts_hit",nhits);
     AddData(  g2t_fts_hit );
