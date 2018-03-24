e8c7645a6f2ae7030841996baa158fad47057b8b
cvs2git <cvs2git>
2018-03-24 20:10:42 +0000

    This commit was manufactured by cvs2git to create branch 'SL16d_embpatch'.

--- StRoot/StarGenerator/EVENT/StarGenAAEvent.cxx
+++
@@ -1,8 +0,0 @@
-#include "StarGenAAEvent.h"
-ClassImp(StarGenAAEvent);
-
-StarGenAAEvent::StarGenAAEvent( const Char_t *name, const Char_t *title )
-  : StarGenEvent(name,title)
-{ 
-  /* nada */ 
-};
--- StRoot/StarGenerator/EVENT/StarGenPPEvent.cxx
+++
@@ -1,8 +0,0 @@
-#include "StarGenPPEvent.h"
-ClassImp(StarGenPPEvent);
-
-StarGenPPEvent::StarGenPPEvent( const Char_t *name, const Char_t *title )
-  : StarGenEvent(name,title)
-{ 
-  /* nada */ 
-};
