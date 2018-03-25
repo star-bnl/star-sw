e071deefb99469d7387d9cf31b0fccf4d268ec74
Yuri Fisyak <fisyak@bnl.gov>
2018-03-25 14:56:38 -0400

    Freeze
    Former-commit-id: ce67c702f4e1db0a145d27bcbaccf5acef0e7237

--- bin/Lana2018.pl
+++ bin/Lana2018.pl
@@ -2,11 +2,19 @@
 use File::Basename;
 use Cwd;
 #my @list = glob "/gpfs01/star/scratch/fisyak/daq/2017/*/*/st_laser*.daq";
-my @list = glob "/star/data03/daq/2018/*/*/st_laser*.daq";
+my @list = ();
+if (-d "/net/l401/data/scratch1/fisyak/") {
+  @list = glob "/net/l401/data/scratch1/daq/2018/*/*/*laser*.daq";
+} else {
+  @list = glob "/star/data03/daq/2018/*/*/st_laser*.daq";
+}
 foreach my $fullpath (@list) {
   my $file = File::Basename::basename($fullpath,".daq");
-  my $rootfile = $file . ".laser.root";
+  my $rootfile = "./" . $file . ".laser.root";
   if (-r $rootfile) {next;}
+  my $glob = "./*/" . $file . ".laser.root";
+  my @rootfiles = glob $glob;# print "glob = $glob rootfiles = @rootfiles\n";
+  if ($#rootfiles >= 0) {next;}
   $rootfile = $file;
   $rootfile =~ s/_adc//;
   $rootfile .= "_adc.laser.root";
