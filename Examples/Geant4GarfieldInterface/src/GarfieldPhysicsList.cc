570f3f93b31e666fb27d2ff7e42919274d9cedd0
Heinrich Schindler <Heinrich.Schindler@cern.ch>
2018-04-06 09:25:43 +0000

    G4 example: add auto the ParticleIterator = GetParticleIterator()

--- Examples/Geant4GarfieldInterface/src/GarfieldPhysicsList.cc
+++ Examples/Geant4GarfieldInterface/src/GarfieldPhysicsList.cc
@@ -78,6 +78,7 @@ void GarfieldPhysicsList::AddParameterisation() {
 	G4FastSimulationManagerProcess* fastSimProcess_garfield =
 			new G4FastSimulationManagerProcess("G4FSMP_garfield");
 
+        auto theParticleIterator = GetParticleIterator();
 	theParticleIterator->reset();
 	while ((*theParticleIterator)()) {
 
