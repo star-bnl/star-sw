a8e70bbb7b6cbc352109c2a887e76d4c0d9d9f20
Florian Seck <f.seck@gsi.de>
2018-04-12 11:31:59 +0000

    fixed the number of messages to read in  &  added new hitMap

--- OnlTools/Jevp/StJevpBuilders/etofBuilder.cxx
+++ OnlTools/Jevp/StJevpBuilders/etofBuilder.cxx
@@ -69,15 +69,18 @@ void etofBuilder::initialize(int argc, char *argv[]) {
 		LOG( INFO, "%hu = %#06x", kv.second , kv.first );
 		rocrMap[ kv.second ] = kv.first;
 	}
+	
+	contents.nHits            = new TH1D( "nHits",            "ETOF # hits; # hits",                                     300,  0, 300 );
+        contents.nEpochMismatch   = new TH1D( "nEpochMismatch",   "ETOF # of Epoch Mismatch / roc; roc; # Epoch Mismatch",     5,  0,   5 );
+        contents.nEpochLoss       = new TH1D( "nEpochLoss",       "ETOF # of Epoch loss / roc; roc; # Epoch Lost",             5,  0,   5 );
+        contents.nEpochSync       = new TH1D( "nEpochSync",       "ETOF # of Epoch Sync / roc; roc; # Epoch Sync",             5,  0,   5 );
+        contents.nEpochDataLoss   = new TH1D( "nEpochDataLoss",   "ETOF # of Epoch Data loss / roc; roc; # Epoch Data Lost",   5,  0,   5 );
+        contents.totAll           = new TH1D( "totAll",           "ETOF ToT of all hits; ToT",                                60,  0,  60 );
+        contents.hitTimeToTrigger = new TH1D( "hitTimeToTrigger", "ETOF hit time to Trigger; t_{hit} - t_{trigger} (#mus)",  500, -5,   5 );
+        contents.hitMap           = new TH2D( "hitMap",           "ETOF hit map; roc; get4Id",                                 5,  0,   5, 32, 0, 32 );
+        contents.hitMapChannelId  = new TH2D( "hitMapChannelId",  "ETOF hit map channel; roc; channelId",                      5,  0,   5,  4, 0,  4 );
 
-	contents.nHits = new TH1F( "nHits", "ETOF # hits; # hits", 250, 0, 250 );
-	contents.nEpochMismatch = new TH1F( "nEpochMismatch", "ETOF # of Epoch Mismatch / roc; roc;# Epoch Mismatch", 5, 0, 5 );
-	contents.nEpochLoss = new TH1F( "nEpochLoss", "ETOF # of Epoch loss / roc; roc;# Epoch Lost", 5, 0, 5 );
-	contents.nEpochSync = new TH1F( "nEpochSync", "ETOF # of Epoch Sync / roc; roc;# Epoch Sync", 5, 0, 5 );
-	contents.nEpochDataLoss = new TH1F( "nEpochDataLoss", "ETOF # of Epoch Data loss / roc; roc;# Epoch Data Lost", 5, 0, 5 );
-	contents.totAll = new TH1F( "totAll", "ETOF ToT of all hits; ToT", 60, 0, 60 );
-	contents.hitMap = new TH2F( "hitMap", "ETOF hit map;roc;channel", 5, 0, 5, 4, 0, 4 );
-	for ( size_t i = 0; i < 20; i++ ){
+	for ( size_t i = 0; i < 20; i++ ) {
 		int roc = i / 4;
 		int chan = i % 4;
 		contents.fineTime[i] = new TH1F( TString::Format("fineTime_%lu", (long unsigned int) i ), TString::Format( "ETOF Fine Time ROC %#06x, chan %d", rocrMap[ roc ], chan ), 130, 0, 130 );
@@ -89,11 +92,14 @@ void etofBuilder::initialize(int argc, char *argv[]) {
 		char buf[50];
 		LOG( INFO, "%#06x", rocrMap[i] );
 		sprintf( buf, "%#06x", rocrMap[i] );
-		contents.hitMap->GetXaxis()->SetBinLabel( i+1, buf );
-		contents.nEpochMismatch->GetXaxis()->SetBinLabel( i+1, buf );
-		contents.nEpochLoss->GetXaxis()->SetBinLabel( i+1, buf );
-		contents.nEpochSync->GetXaxis()->SetBinLabel( i+1, buf );
-		contents.nEpochDataLoss->GetXaxis()->SetBinLabel( i+1, buf );
+                
+		contents.hitMap         ->GetXaxis()->SetBinLabel( i+1, buf );
+                contents.hitMapChannelId->GetXaxis()->SetBinLabel( i+1, buf );
+                
+		contents.nEpochMismatch ->GetXaxis()->SetBinLabel( i+1, buf );
+                contents.nEpochLoss     ->GetXaxis()->SetBinLabel( i+1, buf );
+                contents.nEpochSync     ->GetXaxis()->SetBinLabel( i+1, buf );
+                contents.nEpochDataLoss ->GetXaxis()->SetBinLabel( i+1, buf );
 	}
 	
 	int np = sizeof(contents) / sizeof(TH1 *);
@@ -110,7 +116,7 @@ void etofBuilder::initialize(int argc, char *argv[]) {
 
 		JevpPlot *jp =  new JevpPlot( contents.array[i] );
 		jp->logy = 0;
-		if (contents.array[i] == contents.nHits || contents.array[i] == contents.totAll)
+		if (contents.array[i] == contents.nHits || contents.array[i] == contents.hitTimeToTrigger || contents.array[i] == contents.totAll)
 			jp->logy = 1;
 		
 		addPlot( jp );
@@ -149,49 +155,65 @@ void etofBuilder::event(daqReader *rdr) {
 		LOG(DBG, "iInputSizeBytes=%d", iInputSizeBytes );
 		LOG(DBG, "iInputSzLg=%d = iInputSizeBytes / %d", iInputSzLg, sizeof( ULong64_t ) );
 
-		ULong64_t * pulLongBuff = static_cast< ULong64_t * >(dd->Void);
-		ULong64_t ulTrgGdpbFullTs = pulLongBuff[0];
-		ULong64_t ulTrgStarFullTs = pulLongBuff[1];
-		UInt_t    uStarToken      = (pulLongBuff[2] >> 32) & 0xFFF;
-		UInt_t    uStarDaqCmdIn   = (pulLongBuff[2] >> 16) & 0x00F;
-		UInt_t    uStarTrigCmdIn  = (pulLongBuff[2]      ) & 0x00F;
-		ULong64_t fullEventStatusFlag = pulLongBuff[3];
+		ULong64_t * pulLongBuff      = static_cast< ULong64_t * >(dd->Void);
+		ULong64_t ulTrgGdpbFullTs    = pulLongBuff[0];
+		ULong64_t ulTrgStarFullTs    = pulLongBuff[1];
+		UInt_t    uStarToken         = (pulLongBuff[2] >> 32) & 0xFFF;
+		UInt_t    uStarDaqCmdIn      = (pulLongBuff[2] >> 16) & 0x00F;
+		UInt_t    uStarTrigCmdIn     = (pulLongBuff[2]      ) & 0x00F;
+		ULong64_t fulEventStatusFlag = pulLongBuff[3];
 
-		LOG(DBG, "ulTrgGdpbFullTs=%lu", ulTrgGdpbFullTs );
-		LOG(DBG, "ulTrgStarFullTs=%lu", ulTrgStarFullTs );
-		LOG(DBG, "uStarToken=%lu", uStarToken );
-		LOG(DBG, "uStarDaqCmdIn=%lu", uStarDaqCmdIn );
-		LOG(DBG, "uStarTrigCmdIn=%lu", uStarTrigCmdIn );
-		LOG(DBG, "fullEventStatusFlag=%lu", fullEventStatusFlag );
+		LOG(DBG, "ulTrgGdpbFullTs=%lu",    ulTrgGdpbFullTs );
+		LOG(DBG, "ulTrgStarFullTs=%lu",    ulTrgStarFullTs );
+		LOG(DBG, "uStarToken=%lu",         uStarToken );
+		LOG(DBG, "uStarDaqCmdIn=%lu",      uStarDaqCmdIn );
+		LOG(DBG, "uStarTrigCmdIn=%lu",     uStarTrigCmdIn );
+		LOG(DBG, "fulEventStatusFlag=%lu", fulEventStatusFlag );
 
 		if ( iInputSzLg > 4){
 			UInt_t uMsgsToRead = iInputSzLg - 4;
-			for( UInt_t uMsgIdx = 0; uMsgIdx < uMsgsToRead; uMsgIdx++)
+			for( UInt_t uMsgIdx = 0; uMsgIdx < uMsgsToRead / 2; uMsgIdx++)
 			{
 				gdpb::FullMessage mess( pulLongBuff[4 + 2 * uMsgIdx], pulLongBuff[4 + 2 * uMsgIdx + 1] );
 				// mess.PrintMessage( gdpb::msg_print_Prefix | gdpb::msg_print_Data);
 
 				if ( mess.isGet4Hit32Msg() ){
 					// roc.insert( mess.getRocNumber() );
-					contents.totAll->Fill( mess.getGdpbHit32Tot() );
-					nHits ++;
+				
+					unsigned int rocId      = mess.getRocNumber();
+                                        unsigned int get4ChipId = mess.getGdpbGenChipId();
+                                        unsigned int get4ChanId = mess.getGdpbHitChanId();
 
-					if ( rocMap.count( mess.getRocNumber() ) > 0 ){
-						int rocN = rocMap[mess.getRocNumber()];
-						contents.hitMap->Fill( rocN, mess.getGdpbHitChanId() );
-						contents.fineTime[ rocN * 4 + mess.getGdpbHitChanId() ]->Fill( mess.getGdpbHitFineTs() );
+                                        // check if the reported rocId, ChipId and ChannelId make sense
+                                        if( rocMap.count( rocId ) > 0 && get4ChipId < 32 && get4ChanId < 4 ) {
+						nHits ++;
+
+						contents.totAll->Fill( mess.getGdpbHit32Tot() );
+						
+						double dTriggerTime = static_cast< double_t > ( ulTrgGdpbFullTs );
+                                                double dMessageFullTime = mess.GetFullTimeNs();
+                                                double dTimeToTrigg = ( dMessageFullTime - dTriggerTime ) / 1000.;
+
+                                                contents.hitTimeToTrigger->Fill( dTimeToTrigg );
+
+						contents.hitMap         ->Fill( rocMap[ rocId ], get4ChipId );
+                                                contents.hitMapChannelId->Fill( rocMap[ rocId ], get4ChanId );
+                                                contents.fineTime[ rocMap[ rocId ] * 4 + get4ChanId ]->Fill( mess.getGdpbHitFineTs() );
+					} 
+					else {
+						LOG( DBG, "some id is out of range: rocId %d    get4ChipId %d    get4ChannelId %d", rocId, get4ChipId, get4ChanId );
+						//mess.PrintMessage( gdpb::msg_print_Prefix | gdpb::msg_print_Data);
 					}
-
-				}
+				} // end of things to do with hit message
 
 				if ( mess.isEpoch2Msg() ){
 				  // JML  Messages every event kills the logs!
 
 
 				  //LOG( INFO, "getEpoch2EpochLost=%d", mess.getEpoch2EpochLost() );
-				  //	LOG( INFO, "getEpoch2DataLost=%d", mess.getEpoch2DataLost() );
-				  //	LOG( INFO, "getEpoch2Sync=%d", mess.getEpoch2Sync() );
-				  //	LOG(INFO, "roc=%08x, chan=%d", mess.getRocNumber(), mess.getGdpbHitChanId());
+				  //LOG( INFO, "getEpoch2DataLost=%d", mess.getEpoch2DataLost() );
+				  //LOG( INFO, "getEpoch2Sync=%d", mess.getEpoch2Sync() );
+				  //LOG( INFO, "roc=%08x, chan=%d", mess.getRocNumber(), mess.getGdpbHitChanId());
 
 
 					int rocN = rocMap[mess.getRocNumber()];
@@ -204,9 +226,9 @@ void etofBuilder::event(daqReader *rdr) {
 					if ( mess.getEpoch2Sync() )
 						contents.nEpochSync->Fill( rocN );
 
-				}
+				} // end of things to do with epoch2 message
 
-			} // message loop
+			} // end message loop
 		} // check if there are messages
 	} // dd->iterate
 		
