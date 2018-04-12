77f420efbf4a7d0aa6f8e9887baba6329ab6c73f
Dmitri Smirnov <dsmirnov@bnl.gov>
2018-04-12 18:43:33 +0000

    Add convenience method to identify iTPC padrows

--- StRoot/StDetectorDbMaker/St_tpcPadConfigC.h
+++ StRoot/StDetectorDbMaker/St_tpcPadConfigC.h
@@ -57,6 +57,7 @@ class St_tpcPadConfigC : public TChair {
   Double_t 	   PadPitchAtRow(Int_t sector, Int_t row);		   
   Double_t 	   RowPitchAtRow(Int_t sector, Int_t row);		   
   Int_t            indexForRowPad(Int_t sector, Int_t row, Int_t pad);
+  UChar_t          isiTpcPadRow(Int_t sector, Int_t row) { return iTpc(sector) && row >= 1 && row <= numberOfInnerRows(sector); }
  protected:
   St_tpcPadConfigC(St_tpcPadConfig *table=0) : TChair(table) {}
   virtual ~St_tpcPadConfigC() {fgInstance = 0;}
