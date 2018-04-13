0fb326322e766ba636fd30db9c1f3f1c810266af
cvs2git <cvs2git>
2018-04-13 23:59:21 +0000

    This commit was manufactured by cvs2git to create branch 'SL16j_embpatch2'.

--- StRoot/RTS/src/DAQ_ITPC/itpcCore.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ITPC_CORE_H_
-#define _ITPC_CORE_H_
-
-extern void itpc_sampa_to_rowpad(int id, int sampa, int ch, int &row, int &pad) ;
-extern void itpc_rowpad_to_id(int row, int pad, int &id, int &pin)  ;
-extern  int itpc_altro_to_ifee(int altro) ;
-extern void itpc_altro_to_rowpad(int altro, int ch, int odd, int &row, int &pad) ;
-
-#endif
--- StRoot/StStarLogger/mysql/CleanJobTable.sql
+++
@@ -1,8 +0,0 @@
-# $Id: CleanJobTable.sql,v 1.2 2006/01/27 00:55:35 fine Exp $
-# Author: Valeri Fine (fine@bnl.gov) 26.01.2006
-# Clear the job description table
-
-use logger;
-SHOW tables;
-DELETE FROM JobDescription;
-DESCRIBE  JobDescription;
