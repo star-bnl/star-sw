1e9e3b164cd0167df0a465055fda832e0d23b3cb
smirnovd <>
2018-04-30 23:18:37 +0000

    [Cosmetic] Changes in white space only

--- StRoot/StiTpc/StiTpcDetectorBuilder.cxx
+++ StRoot/StiTpc/StiTpcDetectorBuilder.cxx
@@ -133,116 +133,116 @@ StiDetector* StiTpcDetectorBuilder::constructTpcPadrowDetector(StiLayer stiLayer
   StMatrixD  unit(3,3,1);
   Double_t dZ = pShape->getHalfDepth()*NoStiSectors/24.;
 
-      //Retrieve position and orientation of the TPC pad rows from the database.
-      StTpcLocalSectorDirection  dirLS[3];
-      dirLS[0] = StTpcLocalSectorDirection(1.,0.,0.,tpc_sector_id,tpc_padrow_id);
-      dirLS[1] = StTpcLocalSectorDirection(0.,1.,0.,tpc_sector_id,tpc_padrow_id);
-      dirLS[2] = StTpcLocalSectorDirection(0.,0.,1.,tpc_sector_id,tpc_padrow_id);
-      local2GlobalRotation = unit;
-      for (Int_t i = 0; i < 3; i++) {
-	//	if (debug>1) cout << "dirLS\t" << dirLS[i] << endl;
+  //Retrieve position and orientation of the TPC pad rows from the database.
+  StTpcLocalSectorDirection  dirLS[3];
+  dirLS[0] = StTpcLocalSectorDirection(1.,0.,0.,tpc_sector_id,tpc_padrow_id);
+  dirLS[1] = StTpcLocalSectorDirection(0.,1.,0.,tpc_sector_id,tpc_padrow_id);
+  dirLS[2] = StTpcLocalSectorDirection(0.,0.,1.,tpc_sector_id,tpc_padrow_id);
+  local2GlobalRotation = unit;
+  for (Int_t i = 0; i < 3; i++) {
+    // if (debug>1) cout << "dirLS\t" << dirLS[i] << endl;
 #ifndef TPC_IDEAL_GEOM
-	StTpcLocalDirection        dirL;
-	StTpcLocalSectorAlignedDirection  dirLSA;
-	transform(dirLS[i],dirLSA);//   if (debug>1) cout << "dirLSA\t" << dirLSA << endl;
-	transform(dirLSA,dirL);    //   if (debug>1) cout << "dirL\t" << dirL << endl;
-	StGlobalDirection          dirG;
-	transform(dirL,dirG);//      if (debug>1) cout << "dirG\t" << dirG << endl;
+    StTpcLocalDirection        dirL;
+    StTpcLocalSectorAlignedDirection  dirLSA;
+    transform(dirLS[i],dirLSA);//   if (debug>1) cout << "dirLSA\t" << dirLSA << endl;
+    transform(dirLSA,dirL);    //   if (debug>1) cout << "dirL\t" << dirL << endl;
+    StGlobalDirection          dirG;
+    transform(dirL,dirG);//      if (debug>1) cout << "dirG\t" << dirG << endl;
 #else
-	StTpcLocalDirection  dirG;
-	transform(dirLS[i],dirG);
+    StTpcLocalDirection  dirG;
+    transform(dirLS[i],dirG);
 #endif
-	local2GlobalRotation(i+1,1) = dirG.position().x();
-	local2GlobalRotation(i+1,2) = dirG.position().y();
-	local2GlobalRotation(i+1,3) = dirG.position().z();
-      }
-      //      if (debug>1) cout << "Local2GlobalRotation = " << local2GlobalRotation << endl;
-      Double_t y  = transform.yFromRow(tpc_sector_id, tpc_padrow_id);
-      StTpcLocalSectorCoordinate  lsCoord(0., y, dZ, tpc_sector_id, tpc_padrow_id);// if (debug>1) cout << lsCoord << endl;
+    local2GlobalRotation(i+1,1) = dirG.position().x();
+    local2GlobalRotation(i+1,2) = dirG.position().y();
+    local2GlobalRotation(i+1,3) = dirG.position().z();
+  }
+  //      if (debug>1) cout << "Local2GlobalRotation = " << local2GlobalRotation << endl;
+  Double_t y  = transform.yFromRow(tpc_sector_id, tpc_padrow_id);
+  StTpcLocalSectorCoordinate  lsCoord(0., y, dZ, tpc_sector_id, tpc_padrow_id);// if (debug>1) cout << lsCoord << endl;
 #ifndef TPC_IDEAL_GEOM
-      StTpcLocalSectorAlignedCoordinate lsCoordA;
-      transform(lsCoord,lsCoordA);//                       if (debug>1) cout << lsCoordA << endl;
-      StGlobalCoordinate  gCoord;
-      transform(lsCoordA, gCoord);//                       if (debug>1) cout << gCoord << endl;
+  StTpcLocalSectorAlignedCoordinate lsCoordA;
+  transform(lsCoord,lsCoordA);//                       if (debug>1) cout << lsCoordA << endl;
+  StGlobalCoordinate  gCoord;
+  transform(lsCoordA, gCoord);//                       if (debug>1) cout << gCoord << endl;
 #else  // Ideal geom
-      StTpcLocalCoordinate gCoord;
-      transform(lsCoord, gCoord);
+  StTpcLocalCoordinate gCoord;
+  transform(lsCoord, gCoord);
 #endif
-      //unit vector normal to the pad plane
-      StThreeVectorD centerVector(gCoord.position().x(),gCoord.position().y(),gCoord.position().z());
-      StThreeVectorD normalVector(local2GlobalRotation(2,1),
-				  local2GlobalRotation(2,2),
-				  local2GlobalRotation(2,3));
-      Double_t prod = centerVector*normalVector;
-      if (prod < 0) normalVector *= -1;
-      Double_t phi  = centerVector.phi();
-      Double_t phiD = normalVector.phi();
-      Double_t r = centerVector.perp();
-      StiPlacement *pPlacement = new StiPlacement;
-      Double_t zc = 0;
-      if (NoStiSectors != 12) zc = centerVector.z();
+  // unit vector normal to the pad plane
+  StThreeVectorD centerVector(gCoord.position().x(),gCoord.position().y(),gCoord.position().z());
+  StThreeVectorD normalVector(local2GlobalRotation(2,1),
+                              local2GlobalRotation(2,2),
+                              local2GlobalRotation(2,3));
+  Double_t prod = centerVector*normalVector;
+  if (prod < 0) normalVector *= -1;
+  Double_t phi  = centerVector.phi();
+  Double_t phiD = normalVector.phi();
+  Double_t r = centerVector.perp();
+  StiPlacement *pPlacement = new StiPlacement;
+  Double_t zc = 0;
+  if (NoStiSectors != 12) zc = centerVector.z();
 
-      if ( stiLayer.tpc_sector_id[StiLayer::East] < 0 ) zc =  2*dZ;
-      if ( stiLayer.tpc_sector_id[StiLayer::West] < 0 ) zc = -2*dZ;
+  if ( stiLayer.tpc_sector_id[StiLayer::East] < 0 ) zc =  2*dZ;
+  if ( stiLayer.tpc_sector_id[StiLayer::West] < 0 ) zc = -2*dZ;
 
-      pPlacement->setZcenter(zc);
-      pPlacement->setLayerRadius(fRadius);
-      pPlacement->setLayerAngle(phi);
-      pPlacement->setRegion(StiPlacement::kMidRapidity);
-      pPlacement->setNormalRep(phiD, r*TMath::Cos(phi-phiD), r*TMath::Sin(phi-phiD));
-      TString name = Form("Tpc/Padrow_%d/Sector_%d", stiLayer.sti_padrow_id, stiLayer.sti_sector_id);
-      // fill in the detector object and save it in our vector
-      StiDetector *pDetector = _detectorFactory->getInstance();
-      pDetector->setName(name.Data());
-      pDetector->setIsOn(kTRUE);
-      Bool_t west = kTRUE;
-      Bool_t east = kTRUE;
-      if (nRows == 45) { // ! iTpx
+  pPlacement->setZcenter(zc);
+  pPlacement->setLayerRadius(fRadius);
+  pPlacement->setLayerAngle(phi);
+  pPlacement->setRegion(StiPlacement::kMidRapidity);
+  pPlacement->setNormalRep(phiD, r*TMath::Cos(phi-phiD), r*TMath::Sin(phi-phiD));
+  TString name = Form("Tpc/Padrow_%d/Sector_%d", stiLayer.sti_padrow_id, stiLayer.sti_sector_id);
+  // fill in the detector object and save it in our vector
+  StiDetector *pDetector = _detectorFactory->getInstance();
+  pDetector->setName(name.Data());
+  pDetector->setIsOn(kTRUE);
+  Bool_t west = kTRUE;
+  Bool_t east = kTRUE;
+  if (nRows == 45) { // ! iTpx
 
-        int tpc_sector_id_west = stiLayer.tpc_sector(StiLayer::West);
-        int tpc_sector_id_east = stiLayer.tpc_sector(StiLayer::East);
+    int tpc_sector_id_west = stiLayer.tpc_sector(StiLayer::West);
+    int tpc_sector_id_east = stiLayer.tpc_sector(StiLayer::East);
 
-	Int_t iRdo  = s_pRdoMasks->rdoForPadrow(tpc_padrow_id);
-	Bool_t west = tpc_sector_id_west > 0 && s_pRdoMasks->isOn(tpc_sector_id_west, iRdo);
-	Bool_t east = tpc_sector_id_east > 0 && s_pRdoMasks->isOn(tpc_sector_id_east, iRdo);
-	
-	if (west) {
-	  west = St_tpcAnodeHVavgC::instance()->livePadrow(tpc_sector_id_west,tpc_padrow_id) &&
-	         St_tpcPadGainT0BC::instance()->livePadrow(tpc_sector_id_west,tpc_padrow_id);
-	}
-	if (east) {
-	  east = St_tpcAnodeHVavgC::instance()->livePadrow(tpc_sector_id_east,tpc_padrow_id) &&
-	         St_tpcPadGainT0BC::instance()->livePadrow(tpc_sector_id_east,tpc_padrow_id);
-	}
-      }
+    Int_t iRdo  = s_pRdoMasks->rdoForPadrow(tpc_padrow_id);
+    Bool_t west = tpc_sector_id_west > 0 && s_pRdoMasks->isOn(tpc_sector_id_west, iRdo);
+    Bool_t east = tpc_sector_id_east > 0 && s_pRdoMasks->isOn(tpc_sector_id_east, iRdo);
 
-      StiIsActiveFunctor* activator = nullptr;
+    if (west) {
+      west = St_tpcAnodeHVavgC::instance()->livePadrow(tpc_sector_id_west,tpc_padrow_id) &&
+             St_tpcPadGainT0BC::instance()->livePadrow(tpc_sector_id_west,tpc_padrow_id);
+    }
+    if (east) {
+      east = St_tpcAnodeHVavgC::instance()->livePadrow(tpc_sector_id_east,tpc_padrow_id) &&
+             St_tpcPadGainT0BC::instance()->livePadrow(tpc_sector_id_east,tpc_padrow_id);
+    }
+  }
 
-      if ( St_tpcPadConfigC::instance()->isiTpcPadRow(tpc_sector_id, tpc_padrow_id) ) {
-        pDetector->setGroupId(kiTpcId);
-        activator = _active_iTpc ? new StiTpcIsActiveFunctor(true,west,east) :
-                                   new StiTpcIsActiveFunctor(false,west,east);
-      }
-      else {
-        pDetector->setGroupId(kTpcId);
-        activator = new StiTpcIsActiveFunctor(_active,west,east);
-      }
+  StiIsActiveFunctor* activator = nullptr;
 
-      pDetector->setIsActive(activator);
-      pDetector->setIsContinuousMedium(kTRUE);
-      pDetector->setIsDiscreteScatterer(kFALSE);
-      pDetector->setMaterial(_gasMat);
-      pDetector->setGas(_gasMat);
-      pDetector->setShape(pShape);
-      pDetector->setPlacement(pPlacement);
+  if ( St_tpcPadConfigC::instance()->isiTpcPadRow(tpc_sector_id, tpc_padrow_id) ) {
+    pDetector->setGroupId(kiTpcId);
+    activator = _active_iTpc ? new StiTpcIsActiveFunctor(true,west,east) :
+                               new StiTpcIsActiveFunctor(false,west,east);
+  }
+  else {
+    pDetector->setGroupId(kTpcId);
+    activator = new StiTpcIsActiveFunctor(_active,west,east);
+  }
 
-      if (tpc_padrow_id <= nInnerPadrows)
-	pDetector->setHitErrorCalculator(StiTpcInnerHitErrorCalculator::instance());
-      else
-	pDetector->setHitErrorCalculator(StiTpcOuterHitErrorCalculator::instance());
+  pDetector->setIsActive(activator);
+  pDetector->setIsContinuousMedium(kTRUE);
+  pDetector->setIsDiscreteScatterer(kFALSE);
+  pDetector->setMaterial(_gasMat);
+  pDetector->setGas(_gasMat);
+  pDetector->setShape(pShape);
+  pDetector->setPlacement(pPlacement);
 
-      pDetector->setKey(1,stiLayer.sti_padrow_id);
-      pDetector->setKey(2,stiLayer.sti_sector_id);
+  if (tpc_padrow_id <= nInnerPadrows)
+    pDetector->setHitErrorCalculator(StiTpcInnerHitErrorCalculator::instance());
+  else
+    pDetector->setHitErrorCalculator(StiTpcOuterHitErrorCalculator::instance());
+
+  pDetector->setKey(1,stiLayer.sti_padrow_id);
+  pDetector->setKey(2,stiLayer.sti_sector_id);
 
   return pDetector;
 }
@@ -256,32 +256,32 @@ StiPlanarShape* StiTpcDetectorBuilder::constructTpcPadrowShape(StiLayer stiLayer
   Int_t NoStiSectors = 12;
   UInt_t nInnerPadrows = St_tpcPadConfigC::instance()->numberOfInnerRows(tpc_sector_id);
 
-    TString name = Form("Tpc/Padrow_%d/Sector_%d", stiLayer.sti_padrow_id, stiLayer.sti_sector_id);
-    StiPlanarShape* pShape = new StiPlanarShape;
-    if (!pShape)
-      throw runtime_error("StiTpcDetectorBuilder::buildDetectors() - FATAL - pShape==0||ofcShape==0");
-    Double_t dZ = 0;
-    if(tpc_padrow_id <= nInnerPadrows) {
-      pShape->setThickness(St_tpcPadConfigC::instance()->innerSectorPadLength(tpc_sector_id));
-      dZ = St_tpcPadConfigC::instance()->innerSectorPadPlaneZ(tpc_sector_id);
-    }
-    else {
-      pShape->setThickness(St_tpcPadConfigC::instance()->outerSectorPadLength(tpc_sector_id));
-      dZ = St_tpcPadConfigC::instance()->outerSectorPadPlaneZ(tpc_sector_id);
-    }
+  TString name = Form("Tpc/Padrow_%d/Sector_%d", stiLayer.sti_padrow_id, stiLayer.sti_sector_id);
+  StiPlanarShape* pShape = new StiPlanarShape;
+  if (!pShape)
+    throw runtime_error("StiTpcDetectorBuilder::buildDetectors() - FATAL - pShape==0||ofcShape==0");
+  Double_t dZ = 0;
+  if(tpc_padrow_id <= nInnerPadrows) {
+    pShape->setThickness(St_tpcPadConfigC::instance()->innerSectorPadLength(tpc_sector_id));
+    dZ = St_tpcPadConfigC::instance()->innerSectorPadPlaneZ(tpc_sector_id);
+  }
+  else {
+    pShape->setThickness(St_tpcPadConfigC::instance()->outerSectorPadLength(tpc_sector_id));
+    dZ = St_tpcPadConfigC::instance()->outerSectorPadPlaneZ(tpc_sector_id);
+  }
 
-    // Check if stiLayer represents only one half of TPC layer
-    if ( stiLayer.tpc_sector_id[StiLayer::West] < 0 ||
-         stiLayer.tpc_sector_id[StiLayer::East] < 0 )
-    {
-      dZ *= 0.5;
-    }
+  // Check if stiLayer represents only one half of TPC layer
+  if ( stiLayer.tpc_sector_id[StiLayer::West] < 0 ||
+       stiLayer.tpc_sector_id[StiLayer::East] < 0 )
+  {
+    dZ *= 0.5;
+  }
 
-    pShape->setHalfDepth(dZ*24/NoStiSectors);
-    pShape->setHalfWidth(St_tpcPadConfigC::instance()->PadPitchAtRow(tpc_sector_id, tpc_padrow_id) * St_tpcPadConfigC::instance()->numberOfPadsAtRow(tpc_sector_id, tpc_padrow_id) / 2.);
-    pShape->setName(name.Data()); if (StiVMCToolKit::Debug()>1) cout << *pShape << endl;
+  pShape->setHalfDepth(dZ*24/NoStiSectors);
+  pShape->setHalfWidth(St_tpcPadConfigC::instance()->PadPitchAtRow(tpc_sector_id, tpc_padrow_id) * St_tpcPadConfigC::instance()->numberOfPadsAtRow(tpc_sector_id, tpc_padrow_id) / 2.);
+  pShape->setName(name.Data()); if (StiVMCToolKit::Debug()>1) cout << *pShape << endl;
 
-    return pShape;
+  return pShape;
 }
 
 
@@ -297,7 +297,7 @@ bool StiTpcDetectorBuilder::StiLayer::operator< (const StiLayer& other) const
            padCfg.radialDistanceAtRow(other.tpc_sector(), other.tpc_padrow()) &&
            sti_sector_id < other.sti_sector_id );
 
-   return result;
+  return result;
 }
 
 
