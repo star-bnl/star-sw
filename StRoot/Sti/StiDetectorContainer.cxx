8f7a93aec54412fe9bc325618b5e1deaa09fe7d4
Victor Perevoztchikov <perev@bnl.gov>
2018-05-02 15:35:24 +0000

    assert()

--- StRoot/Sti/StiDetectorContainer.cxx
+++ StRoot/Sti/StiDetectorContainer.cxx
@@ -72,7 +72,7 @@ void StiDetectorContainer::initialize()
       for (int sector=0; sector<nSectors; sector++)
         {
         StiDetector* detector = (*bIter)->getDetector(row,sector);
-	if (! detector) continue;
+	assert(detector);
         if (detector->isActive()) 
 	  add(detector);
         else 
