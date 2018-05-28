7d5599cd6e1f1f45788e60c5010780588d68ed50
Victor Perevoztchikov <perev@bnl.gov>
2018-05-28 23:19:34 +0000

    isActive(int,int) added

--- StRoot/StiTpc/StiTpcDetectorBuilder.h
+++ StRoot/StiTpc/StiTpcDetectorBuilder.h
@@ -12,6 +12,7 @@ public:
     virtual void buildDetectors(StMaker&s);
     /// returns the azimuthal angle [-pi, pi) for tpc sector [1-24]
     void         useVMCGeometry();		
+int isActive(int sector,int row) const;
 
 static int   sector(double ang,int east=0);
 static double angle(int sec);
