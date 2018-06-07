31577e3320b92139b0cbcfef04f885760ab51fb5
Yuri Fisyak <fisyak@bnl.gov>
2018-06-07 19:08:43 -0400

    Freeze
    Former-commit-id: 98095ba6dc44b4b95dfcd7a3925ab05dd87471d2

--- StRoot/StMuDSTMaker/COMMON/macros/lMuDst.C
+++ StRoot/StMuDSTMaker/COMMON/macros/lMuDst.C
@@ -1,8 +1,17 @@
-void lMuDst() {
+#include "Riostream.h"
+void lMuDst(Int_t opt = -2, const Char_t *input = "", const Char_t *copt = "StEvent,Stu,RMuDst,picoEvt,mysql,tpcDb,magF,nodefault,CorrX,quiet",const Char_t *tfile =  0,const Char_t *ofile =  0) {
+#if !defined(__CINT__)
+  std::cout << "This code cannot be compiled" << std::endl;
+#else
   gROOT->LoadMacro("bfc.C");
-  TString Chain("StEvent,RMuDst,nodefault");
-  bfc(-2,Chain,0,0,0);
+  TString Chain(copt);
+  if (TString(gSystem->Getenv("STAR_VERSION")) == ".DEV2") Chain += ",TMVARank";
+  bfc(opt,Chain,input,ofile,tfile);
   //  gROOT->LoadMacro("FitP_t.h+");
   gSystem->Load("libEG");
-  //  gSystem->Load("libStKFVertex");
+#if 0
+  gSystem->Load("libKFParticlePerformance");
+  gSystem->AddIncludePath(" -I$ROOTROOT/root/tmva/test");
+#endif
+#endif
 }
