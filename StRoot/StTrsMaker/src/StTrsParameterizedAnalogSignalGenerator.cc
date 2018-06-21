802b4c3f8a00dcf66cffb6fc607bb224351c7044
perev <>
2018-06-21 22:23:08 +0000

    TpcGroup fixes

--- StRoot/StTrsMaker/src/StTrsParameterizedAnalogSignalGenerator.cc
+++ StRoot/StTrsMaker/src/StTrsParameterizedAnalogSignalGenerator.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StTrsParameterizedAnalogSignalGenerator.cc,v 1.42 2018/02/20 22:45:53 smirnovd Exp $
+ * $Id: StTrsParameterizedAnalogSignalGenerator.cc,v 1.43 2018/06/21 22:23:08 perev Exp $
  *
  * Author: Hui Long
  ***************************************************************************
@@ -11,6 +11,9 @@
  *
  *
  * $Log: StTrsParameterizedAnalogSignalGenerator.cc,v $
+ * Revision 1.43  2018/06/21 22:23:08  perev
+ * TpcGroup fixes
+ *
  * Revision 1.42  2018/02/20 22:45:53  smirnovd
  * Revert "Changes from Irakli's directory to make the code compile"
  *
@@ -381,9 +384,9 @@ void StTrsParameterizedAnalogSignalGenerator::localArrayBuilder()
   for(row=0;row<rows;row++)
      {int max_pads=mGeomDb->numberOfPadsAtRow(row+1);
       mPadsAtRow[row]=max_pads;
-      yCentroid[row]=transformer.yFromRow(row+1);
+      yCentroid[row]=transformer.yFromRow(20,row+1);
       for(pad=0;pad<max_pads;pad++)
-         { xCentroid[row][pad]=transformer.xFromPad(row+1,pad+1);     
+	{ xCentroid[row][pad]=transformer.xFromPad(20,row+1,pad+1);     
 	 // gain[row][pad]=1.0+(StTrsRandom::inst().Rndm()-0.5)*0.20;
               }
         
@@ -651,7 +654,7 @@ void StTrsParameterizedAnalogSignalGenerator::inducedChargeOnPad(StTrsWireHistog
 	       
                 if(Repeat<0.5){ 
                   
-		  mCentralPad = (Int_t) transformer.padFromLocal(iter->position(),irow2+1)-1;
+		  mCentralPad = (Int_t) transformer.padFromLocal(iter->position(),sector,irow2+1)-1;
 		   if(mCentralPad>  PadsAtRow)mCentralPad= PadsAtRow;//upper limit boundary check.
 
                  
