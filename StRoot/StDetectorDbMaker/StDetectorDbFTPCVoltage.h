af2e3188611cd0ab691d6f328a90cf943bf09a3a
cvs2git <cvs2git>
2018-06-29 17:35:04 +0000

    This commit was manufactured by cvs2git to create tag 'iTPC_IRAK_20180216b_1'.

--- StRoot/StDetectorDbMaker/StDetectorDbFTPCVoltage.h
+++
@@ -1,5 +0,0 @@
-#ifndef StDetectorDbFTPCVoltage_h
-#define StDetectorDbFTPCVoltage_h
-#include "St_ftpcVoltageC.h"
-typedef St_ftpcVoltageC StDetectorDbFTPCVoltage;
-#endif
--- StRoot/StJetMaker/dijets/StjDijetCutAsymmetricPt.cxx
+++
@@ -1,5 +0,0 @@
-// $Id: StjDijetCutAsymmetricPt.cxx,v 1.1 2008/09/11 23:34:45 tai Exp $
-// Copyright (C) 2008 Tai Sakuma <sakuma@bnl.gov>
-#include "StjDijetCutAsymmetricPt.h"
-
-ClassImp(StjDijetCutAsymmetricPt)
