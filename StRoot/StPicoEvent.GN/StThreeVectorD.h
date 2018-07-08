0b0666f95b228611b14471c1a0777d80ca8aed6d
Yuri Fisyak <fisyak@bnl.gov>
2018-07-08 11:28:54 -0400

    Merge with Grisha
    Former-commit-id: df0e9eccc4cf2d4d0f3a73f2dc6ff89c9ec0f3f7

---
+++ StRoot/StPicoEvent.GN/StThreeVectorD.h
@@ -0,0 +1,13 @@
+#ifndef ST_THREE_VECTOR_D_H
+#define ST_THREE_VECTOR_D_H
+
+#ifdef _VANILLA_ROOT_
+
+/// PicoDst headers
+#include "StThreeVector.h"
+
+typedef StThreeVector<double> StThreeVectorD;
+
+#endif
+
+#endif //#define ST_THREE_VECTOR_D_H
---
+++ StRoot/StPicoEvent.GN/StThreeVectorF.h
@@ -0,0 +1,13 @@
+#ifndef ST_THREE_VECTOR_F_H
+#define ST_THREE_VECTOR_F_H
+
+#ifdef _VANILLA_ROOT_
+
+/// PicoDst headers
+#include "StThreeVector.h"
+
+typedef StThreeVector<float> StThreeVectorF;
+
+#endif //ifdef _VANILLA_ROOT_
+
+#endif //#define ST_THREE_VECTOR_F_H
