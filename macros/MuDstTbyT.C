03baef9bcda2799d322462f7213eb24b827b368b
Yuri Fisyak <fisyak@bnl.gov>
2018-08-15 09:49:49 -0400

    update subsys/tpc
    Former-commit-id: cf710463c1f8f0f94cd8accd6172de3b3b06d714

--- macros/MuDstTbyT.C
+++ macros/MuDstTbyT.C
@@ -14,8 +14,8 @@ void MuDstTbyT(
 	       const Char_t *oldf="/gpfs02/eic/ayk/STAR/reco/MuDst/AuAu_200_production_2016/ReversedFullField/P16ij/2016/125/17125034/st_physics_adc_17125034_raw_1000007.MuDst.root", 
 	       const Char_t *newf="/gpfs01/star/pwg/fisyak/Embedding/2016/piNmTsq5PerCentZ6cm/st_physics_adc_17125034_raw_1000007.MuDst.root",
 #else
-	       const Char_t *oldf="/star/subsys/tpc/fisyak/reco/2018/EastOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
-	       const Char_t *newf="/star/subsys/tpc/fisyak/reco/2018/WestOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
+	       const Char_t *oldf="/gpfs01/star/subsys-tpc/fisyak/reco/2018/EastOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
+	       const Char_t *newf="/gpfs01/star/subsys-tpc/fisyak/reco/2018/WestOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
 #endif
 	       const Char_t *out = "") {
   TString fileO(out);
