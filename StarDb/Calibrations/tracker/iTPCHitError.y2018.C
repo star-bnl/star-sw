c5351d237b102b51b005604f9ab82bc61ca074dc
Yuri Fisyak <fisyak@bnl.gov>
2018-09-10 17:24:21 -0400

    Add scale for iTPC hit errors
    Former-commit-id: d8583890093e3ed6a7449763af683a51645dbd77

--- StarDb/Calibrations/tracker/iTPCHitError.y2018.C
+++ StarDb/Calibrations/tracker/iTPCHitError.y2018.C
@@ -8,10 +8,11 @@ TDataSet *CreateTable() {
 HitError_st row;
 St_HitError *tableSet = new St_HitError("iTPCHitError",1);
 //
+ Double_t scale = 0.5/0.335; // strip widths
 memset(&row,0,tableSet->GetRowSize());
-    row.coeff[0]	 =     0.0004; // *;
-    row.coeff[1]	 = 0.002045093;
-    row.coeff[2]	 =  0.0355285;
+    row.coeff[0]	 =     0.0004 *scale*scale; // *;
+    row.coeff[1]	 = 0.002045093*scale*scale;
+    row.coeff[2]	 =  0.0355285 *scale*scale;
     row.coeff[3]	 = 0.00122247;
     row.coeff[4]	 = 0.005828129;
     row.coeff[5]	 = 0.02526806;
