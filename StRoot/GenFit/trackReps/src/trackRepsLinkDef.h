93f8045169e1930532e696bd260ad5531b8fedbf
Yuri Fisyak <fisyak@bnl.gov>
2018-09-11 09:10:30 -0400

    Merge with THLT
    Former-commit-id: 095f67300d98bef830db93e642a12361e0726329

---
+++ StRoot/GenFit/trackReps/src/trackRepsLinkDef.h
@@ -0,0 +1,10 @@
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ namespace genfit;
+
+#pragma link C++ class genfit::RKTrackRep-;
