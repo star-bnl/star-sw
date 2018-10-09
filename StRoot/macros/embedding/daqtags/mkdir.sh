1d8b61d47f1c7207ea2957c990d6fa830b33ec3d
zhux <>
2018-10-09 04:03:34 +0000

    *** empty log message ***

---
+++ StRoot/macros/embedding/daqtags/mkdir.sh
@@ -0,0 +1,6 @@
+mkdir /star/embed/daq/2016/auau200_hftphys
+mkdir /star/embed/reco/2016/auau200_hftphys
+mkdir /star/embed/tags/2016/auau200_hftphys
+ln -s /star/embed/daq/2016/auau200_hftphys daq
+ln -s /star/embed/reco/2016/auau200_hftphys reco
+ln -s /star/embed/tags/2016/auau200_hftphys tags
