5e3f070ab1328445c4d0856e4a605ede424ef4db
zhux <>
2018-10-09 02:06:34 +0000

    *** empty log message ***

---
+++ StRoot/macros/embedding/daqtags/keepgood.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in `cat $1`
+do
+   bname=`basename $i`
+   #echo $bname
+   mystr=`echo $bname | cut -d"_" -f4 -`
+   #echo $mystr
+   teststr=`grep $mystr goodruns.txt`
+   #echo $teststr
+   if [ ! -z "$teststr" ] ; then
+
+	echo $i >> good_$1
+
+   fi
+done
+
