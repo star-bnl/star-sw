d83cd5407623bc6ad50c2888ce4d32b368884fb6
zhux <>
2018-10-14 03:44:38 +0000

    *** empty log message ***

--- StRoot/macros/embedding/moretags/getfsetstat.sh
+++ StRoot/macros/embedding/moretags/getfsetstat.sh
@@ -5,12 +5,19 @@ if [[ ! $HOST =~ "rcas" ]] ; then
    exit
 fi
 
-if [[ ! $# -eq 2 ]] ; then
+if [[ $# -eq 0 || $# -gt 2 ]] ; then
    echo ""
-   echo " Usage : $0 [good_daqfilelist.500.daq.list] [nevents]"
-   echo "       [nevents] is the maximum # of events used in each daq file"
+   echo " Usage : $0 [good_xxx.500.Pxxiy.mudst.list]"
+   echo "      or $0 [good_xxx.500.Pxxiy.mudst.list] [nevents]"
+   echo "       [nevents] is the maximum # of events used in each file (default is 1000)"
    echo ""
    exit
+fi
+
+if [ $# -eq 1 ] ; then
+   neventscut=1000
+else
+   neventscut=$2
 fi
 
 totall=0
@@ -18,13 +25,18 @@ tot=0
 min=10000
 max=0
 ndaq=0
+neffdaq=0
 for i in `cat $1`
 do
-   bname=`basename $i .daq`
+   bname=`basename $i .MuDst.root`
    #echo $bname
    mystr=`echo $bname`
    #echo $mystr
    teststr=`find output -name "${bname}*.chopper.txt"`
+   teststr2=`find output -name "${bname}*.moretags.root"`
+   if [[ ! -z "$teststr" && ! -z "$teststr2" ]] ; then
+	neffdaq=$(($neffdaq+1))
+   fi
    evts=`wc -l $teststr | awk '{print $1}' `
    ndaq=$(($ndaq+1))
    totall=$(($totall+$evts))
@@ -34,12 +46,19 @@ do
    if [ $evts -gt $max ] ; then
 	max=$evts
    fi
-   if [ $evts -lt $2 ] ; then
+   if [ $evts -lt $neventscut ] ; then
 	tot=$(($tot+$evts))
    else
-	tot=$(($tot+$2))
+	tot=$(($tot+$neventscut))
    fi
 done
 
-echo "# of daq files: $ndaq, total events in daq files: $totall, average # in one daq file: "$(($totall/$ndaq))", maximum # of events per file: $max, minimum # of events per file: $min, nevents cut value: $2, total events per fset: $tot"
+echo "# of daq/MuDst files (in the input file list): $ndaq"
+echo "# of effective daq/MuDst files (with both .chopper.txt and .moretags.root produced in output/): $neffdaq"
+echo "total effective events (passed event cuts): $totall"
+echo "average # of effective events in each file: "$(($totall/$ndaq))
+echo "the maximum # of effective events per file: $max"
+echo "the minimum # of effective events per file: $min"
+echo "assigned nevents cut value: $neventscut"
+echo "total events per fset: $tot"
 
