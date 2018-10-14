7581279567d37fcd7739f158d58ac94fb0d8a049
Yuri Fisyak <fisyak@bnl.gov>
2018-10-14 08:48:40 -0400

    Merge
    Former-commit-id: 015346e7553ed05e888ab454cf7e9335f787047a

---
+++ StRoot/macros/embedding/daqtags/tagAdder.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Hadds all tag files of a given runNumber
+tagDir=$1
+outDir=$2
+
+filetype="st_physics_adc"
+tagtype="tags"
+#tagtype="moretags"
+
+#Get a list of all the Unique Run Numbers
+uniqueRunNumber=$(ls $tagDir | cut -d"_" -f4 | sort | uniq)
+nRunNumbers=$(ls $tagDir | cut -d"_" -f4 | sort | uniq | wc -l)
+
+echo "Number of Unique Runs:" $nRunNumbers
+
+for i in `seq 1 $nRunNumbers`;
+do 
+
+    runNumber=$(echo $uniqueRunNumber | awk -vv=$i '{ print $v }')
+    echo $runNumber
+
+    combrun=`grep $runNumber combined_runs.list`
+    if [ -z "$combrun" ]; then
+	 #echo no change
+	 cp -p $tagDir/*$runNumber* $outDir/
+    else
+	 newFileName=$outDir/"${filetype}_"$runNumber"_raw_0000000.${tagtype}.root"
+	 echo $newFileName
+
+	 hadd $newFileName $tagDir/*$runNumber*.${tagtype}.root
+    fi
+done
+
