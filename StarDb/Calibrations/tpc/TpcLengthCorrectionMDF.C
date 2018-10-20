eddab0a265acc6ab8017e32ca4c2219d957a782a
Yuri Fisyak <fisyak@bnl.gov>
2018-10-20 10:22:56 -0400

    Adjust comments
    Former-commit-id: 38b36108675f0ad77674b1be15cd3480389095dc

--- StarDb/Calibrations/tpc/TpcLengthCorrectionMDF.C
+++ StarDb/Calibrations/tpc/TpcLengthCorrectionMDF.C
@@ -3,11 +3,11 @@ TDataSet *CreateTable() {
   Int_t nrows = 6;
   MDFCorrection_st row;
   St_MDFCorrection *tableSet = new St_MDFCorrection("TpcLengthCorrectionMDF",nrows);
-  memset(&row,0,tableSet->GetRowSize()); //  TPoints70BUGPRunXVII14; h2mdf("mu",5,1,20);        
+  memset(&row,0,tableSet->GetRowSize()); //  TPoints70UGPRunXVII14; h2mdf("mu",5,1,20);        
   row.nrows      = nrows;
   row.idx        =     1;// 
   tableSet->AddAt(&row);// 0 -> I70                                                            
-  memset(&row,0,tableSet->GetRowSize()); //  TPoints70BUGPRunXVII14; h2mdf("sigma",5,1,20)     
+  memset(&row,0,tableSet->GetRowSize()); //  TPoints70UGPRunXVII14; h2mdf("sigma",5,1,20)     
   row.nrows      = nrows;
   row.idx        =     2;// 
   tableSet->AddAt(&row);// 1 -> sigma.I70   						       
@@ -19,11 +19,11 @@ TDataSet *CreateTable() {
   row.nrows      = nrows;
   row.idx        =     4;// 
   tableSet->AddAt(&row);// 3 -> sigma.dN/dx   						       
-  memset(&row,0,tableSet->GetRowSize()); //  TPointsBUGPRunXVII14; h2mdf("mu",5,1,20); 	       
+  memset(&row,0,tableSet->GetRowSize()); //  TPointsFUGPRunXVII14; h2mdf("mu",5,1,20); 	       
   row.nrows      = nrows;
   row.idx        =     5;// 
   tableSet->AddAt(&row);// 4 -> Ifit 	     
-  memset(&row,0,tableSet->GetRowSize()); //  TPointsBUGPRunXVII14; h2mdf("sigma",5,1,20);      
+  memset(&row,0,tableSet->GetRowSize()); //  TPointsFUGPRunXVII14; h2mdf("sigma",5,1,20);      
   row.nrows      = nrows;
   row.idx        =     6;// 
   tableSet->AddAt(&row);// 5 -> sigma.Ifit	     
