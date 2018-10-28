590cc2894a0992a43bdf5444025767dc83c73084
Yuri Fisyak <fisyak@bnl.gov>
2018-10-28 10:51:55 -0400

    Freeze
    Former-commit-id: 159e3e068265b9b78636df73eb67884f48a43d78

--- macros/KFVDebug.C
+++ macros/KFVDebug.C
@@ -4,5 +4,9 @@ void KFVDebug() {
   kfv->SetDebug(3);
   TCanvas *c1 = new TCanvas("c1","c1",1400,500);
   kfv->SetCanvas(c1);
+#if 0
+  StMaker *stx = chain->Maker("Stx");
+  if (stx) stx->SetDebug(13);
+#endif
   chain->MakeEvent();
 }
