0c4a3ac9b047293318de6f920c9d58de6c81254f
Yuri Fisyak <fisyak@bnl.gov>
2018-11-20 19:51:15 +0000

    Temporarely disable __STOPPED_ELECTRONS__ to check effect of this on no. of primary tracks for 2010 AuAu200 sample

--- StRoot/StTpcRSMaker/StTpcRSMaker.cxx
+++ StRoot/StTpcRSMaker/StTpcRSMaker.cxx
@@ -59,15 +59,15 @@
 #include "tables/St_g2t_vertex_Table.h" 
 //#define ElectronHack
 //#define __LASERINO__
-//#define Old_dNdx_Table
-#define __STOPPED_ELECTRONS__
-#define __DEBUG__
+#define Old_dNdx_Table
+//#define __STOPPED_ELECTRONS__
+//#define __DEBUG__
 #if defined(__DEBUG__)
 #define PrPP(A,B) if (Debug()%10 > 2) {LOG_INFO << "StTpcRSMaker::" << (#A) << "\t" << (#B) << " = \t" << (B) << endm;}
 #else
 #define PrPP(A,B)
 #endif
-static const char rcsid[] = "$Id: StTpcRSMaker.cxx,v 1.82 2018/11/05 01:05:19 fisyak Exp $";
+static const char rcsid[] = "$Id: StTpcRSMaker.cxx,v 1.83 2018/11/20 19:51:15 fisyak Exp $";
 #define __ClusterProfile__
 static Bool_t ClusterProfile = kFALSE;
 #define Laserino 170
@@ -587,8 +587,8 @@ Int_t StTpcRSMaker::Make(){  //  PrintInfo();
   static Int_t minRow    = IAttr("minRow");
   static Int_t maxRow    = IAttr("maxRow");
   // constants
-#ifdef __DEBUG__
   static Int_t iBreak = 0;
+#ifdef __DEBUG__
   if (Debug()%10) {
     gBenchmark->Reset();
     gBenchmark->Start("TpcRS");
@@ -2053,8 +2053,11 @@ typedef struct {
 }
 #undef PrPP
 //________________________________________________________________________________
-// $Id: StTpcRSMaker.cxx,v 1.82 2018/11/05 01:05:19 fisyak Exp $
+// $Id: StTpcRSMaker.cxx,v 1.83 2018/11/20 19:51:15 fisyak Exp $
 // $Log: StTpcRSMaker.cxx,v $
+// Revision 1.83  2018/11/20 19:51:15  fisyak
+// Temporarely disable __STOPPED_ELECTRONS__ to check effect of this on no. of primary tracks for 2010 AuAu200 sample
+//
 // Revision 1.82  2018/11/05 01:05:19  fisyak
 // Replace assert to error message
 //
