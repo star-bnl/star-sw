328117fb5600f1472ae53c0ea7fdda24d45c0bc4
Yuri Fisyak <fisyak@bnl.gov>
2018-12-05 09:57:15 -0500

    Fix a bug with radial row position
    Former-commit-id: 47815e8b158d1e3e0862a21edba526f5add62ecb

--- StRoot/StDbUtilities/StTpcCoordinateTransform.cc
+++ StRoot/StDbUtilities/StTpcCoordinateTransform.cc
@@ -1,6 +1,6 @@
 /***********************************************************************
  *
- * $Id: StTpcCoordinateTransform.cc,v 1.50 2018/10/17 20:45:24 fisyak Exp $
+ * $Id: StTpcCoordinateTransform.cc,v 1.52 2018/12/05 14:56:28 fisyak Exp $
  *
  * Author: brian Feb 6, 1998
  *
@@ -16,6 +16,12 @@
  ***********************************************************************
  *
  * $Log: StTpcCoordinateTransform.cc,v $
+ * Revision 1.52  2018/12/05 14:56:28  fisyak
+ * Fix a bug with radial row position
+ *
+ * Revision 1.51  2018/12/04 19:38:07  fisyak
+ * Add J.Thomas fix for iTPC survey
+ *
  * Revision 1.50  2018/10/17 20:45:24  fisyak
  * Restore update for Run XVIII dE/dx calibration removed by Gene on 08/07/2018
  *
@@ -464,13 +470,13 @@ Int_t StTpcCoordinateTransform::rowFromLocalY(Double_t y, Int_t sector) {
 		       - St_tpcPadConfigC::instance()->radialDistanceAtRow(sector,i-2))/2;
       } else {
 	Radii[i-1] = (St_tpcPadConfigC::instance()->radialDistanceAtRow(sector,i-1) +
-		      St_tpcPadConfigC::instance()->radialDistanceAtRow(sector,i-1))/2;
+		      St_tpcPadConfigC::instance()->radialDistanceAtRow(sector,i))/2;
       }
     }
   }
-  if (y < Radii[1]) return 1;
-  if (y > Radii[Nrows]) return Nrows;
-  Long64_t row = TMath::BinarySearch(Nrows+1, Radii, y);
+  Long64_t row = TMath::BinarySearch(Nrows+1, Radii, y) + 1;
+  if (row <= 0) row = 1;
+  if (row > Nrows) row = Nrows;
   return row;
 #else
   if (! Nrows) {
