7a8c87e21dae1dd7be75eaefced82e0abd0c5788
cvs2git <cvs2git>
2019-01-08 21:03:12 +0000

    This commit was manufactured by cvs2git to create tag 'V20190109'.

--- StRoot/macros/embedding/daqtags/mkdir.sh
+++
@@ -1,6 +0,0 @@
-mkdir /star/embed/daq/2016/auau200_hftphys
-mkdir /star/embed/reco/2016/auau200_hftphys
-mkdir /star/embed/tags/2016/auau200_hftphys
-ln -s /star/embed/daq/2016/auau200_hftphys daq
-ln -s /star/embed/reco/2016/auau200_hftphys reco
-ln -s /star/embed/tags/2016/auau200_hftphys tags
