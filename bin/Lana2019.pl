3cf4cb5fcd6d446559f8f66c70e2e2b5e61ca622
Yuri Fisyak <fisyak@bnl.gov>
2019-01-16 11:15:50 -0500

    Freeze
    Former-commit-id: ef2f0514d873a73e0d3da990e44ae44d1bb66c42

---
+++ bin/Lana2019.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+use File::Basename;
+use Cwd;
+#my @list = glob "/gpfs01/star/scratch/fisyak/daq/2017/*/*/st_laser*.daq";
+my @list = ();
+if (-d "/net/l401/data/scratch1/daq/2019/") {
+  @list = glob "/net/l401/data/scratch1/daq/2019/*/*/*laser*.daq";
+} else {
+  @list = glob "/star/data03/daq/2019/*/*/st_laser*.daq";
+}
+foreach my $fullpath (@list) {
+  my $file = File::Basename::basename($fullpath,".daq");
+  my $rootfile = "./" . $file . ".laser.root";
+  if (-r $rootfile) {next;}
+  my $glob = "./*/" . $file . ".laser.root";
+  my @rootfiles = glob $glob;# print "glob = $glob rootfiles = @rootfiles\n";
+  if ($#rootfiles >= 0) {next;}
+  $rootfile = $file;
+  $rootfile =~ s/_adc//;
+  $rootfile .= "_adc.laser.root";
+  if (-r $rootfile) {next;}
+  print "string:$fullpath\n";
+}
