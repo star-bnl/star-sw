2abff8485c62d07eec27b20653793e893816c47e
Yuri Fisyak <fisyak@bnl.gov>
2019-01-17 12:04:32 -0500

    Add Maxim's pallete
    Former-commit-id: bb1feb105ed57472eb2d64b41d8fdc1bdeb9854b

--- StRoot/macros/rootlogon.C
+++ StRoot/macros/rootlogon.C
@@ -68,9 +68,17 @@
   //  Set date/time for plot
   gStyle->SetOptDate(1);
   //  gStyle->SetPalette(1);
-  gStyle->SetPalette(55);
+  //  gStyle->SetPalette(55);
+  // Maxim's pallete
+  const Int_t NRGBs = 5;
+  const Int_t NCont = 255;
+  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+  Double_t red[NRGBs] = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+  Double_t blue[NRGBs] = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  gStyle->SetNumberContours(NCont);
   gStyle->SetMarkerStyle(20);
-
   
   // Redefine prompt
   TString gPrompt =  gSystem->BaseName(gROOT->GetApplication()->Argv(0));
