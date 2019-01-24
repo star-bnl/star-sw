d871e0b804f248c40bc9d3bbbe0a20b75bb938d1
Yuri Fisyak <fisyak@bnl.gov>
2019-01-24 11:57:19 -0500

    Freeze
    Former-commit-id: e29e6280d2218b9975aeb23ad0b7c938a012fc03

---
+++ bin/2019Cosmic.pl
@@ -0,0 +1,38 @@
+#! /usr/bin/env perl
+use File::Basename;
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename,events' -cond 'filetype=online_daq,filename~st_laser,tpx=1,sanity=1,runnumber>15000000,events>10' -limit 0`;
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'filetype=online_daq,filename~st_laser,tpx=1,sanity=1,runnumber>16161045,events>10' -limit 0`;
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'magscale=FullField,filetype=online_daq,trgsetupname=CosmicLocalClock,tpx=1,pxl=1,ist=1,gmt=1,sanity=1,events>10000' -limit 50`;
+#my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'magscale=ReversedFullField,filetype=online_daq,trgsetupname=CosmicLocalClock,tpx=1,pxl=1,ist=1,gmt=1,sanity=1,events>10000' -limit 50`;
+# if ($#ARGV < 0) {
+#   print "Usage $0 ZF, FF or RF\n";
+#   exit 0;
+# }
+# 20018043
+# my $field = "";
+# my $Field = $ARGV[0];
+# print "$Field\n";
+# if    ($Field eq "FF") {$field = "FullField";}
+# elsif ($Field eq "RF") {$field = "ReversedFullField";}
+# elsif ($Field eq "ZF") {$field = "FieldOff"; }
+# print "$Field => $field\n";
+# if ($field eq "") {die "$Field has not defined";}
+# my $cmd = "get_file_list.pl -delim '/' -keys 'path,filename' -cond 'magscale=" . $field . ",filetype=online_daq,filename~st,trgsetupname=CosmicLocalClock,tpx=1,gmt=1,sanity=1,runnumber>19000000' -limit 0"; 
+# print "$cmd\n";
+# my @list = `$cmd`;
+# #my @list = `get_file_list.pl -delim '/' -keys 'path,filename' -cond 'magscale=ReversedFullField,filetype=online_daq,filename~st_gmt,trgsetupname=CosmicLocalClock,tpx=1,gmt=1,sanity=1,events>10000' -limit 50`;
+my @list = glob "/net/l401/data/scratch1/daq/2019/*/*/st_cosmic_*.daq";
+foreach my $file (@list) {
+  chomp($file);
+  my $bf = File::Basename::basename($file);
+  my $f = File::Basename::dirname($file);
+  my $run = File::Basename::basename($f);
+#  print "file = $file, bf = $bf, f = $f, run = $run\n";
+  if ($run < 20018043) {next;}
+  my $root = $bf . ".MuDst.root";
+  if (-r $root) {next;}
+  print "string:$file\n";
+#  last;
+}
+
+	
---
+++ bin/rsynch.csh
@@ -0,0 +1,6 @@
+#! /usr/local/bin/tcsh -f
+rsync -avz -h                        \
+    --include='*.root'                  \
+    --exclude='*.log' --exclude='*.dat' \
+    ./ rftpexp01.rhic.bnl.gov:/gpfs01/star/subsys-tpc/fisyak/Tpc/Current/2019/
+
