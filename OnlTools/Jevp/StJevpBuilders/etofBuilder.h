67d1cda5fa572a95f68779404951e886a840d0ea
Florian Seck <f.seck@gsi.de>
2019-02-04 13:16:34 +0000

    added plots monitoring the trigger time differences between AFCK cards

--- OnlTools/Jevp/StJevpBuilders/etofBuilder.h
+++ OnlTools/Jevp/StJevpBuilders/etofBuilder.h
@@ -5,10 +5,13 @@
 #include "DAQ_READER/daqReader.h"
 #include <TH1F.h>
 #include <TH2F.h>
+#include <TProfile.h>
 
 #include <math.h>
 #include <map>
 #include <vector>
+
+namespace gdpbv100 { class FullMessage; };
 
 
 class etofBuilder : public JevpBuilder {
@@ -16,27 +19,33 @@ class etofBuilder : public JevpBuilder {
     //RunStatus status;
     int run;
 
-    etofBuilder(JevpServer *parent=NULL); 
+    etofBuilder( JevpServer* parent=NULL ); 
     ~etofBuilder();
 
 
-    void initialize(int argc, char *argv[]);
-    void startrun(daqReader *rdr);
-    void stoprun(daqReader *rdr);
-    void event(daqReader *rdr);
+    void initialize( int argc, char* argv[] );
+    void startrun( daqReader* rdr );
+    void stoprun( daqReader* rdr );
+    void event( daqReader* rdr );
 
-    static void main(int argc, char *argv[]);
+    static void main( int argc, char* argv[] );
 
 
   private:
     // -------------------------------
-    void processMessages( uint64_t* messageBuffer, size_t nFullMessagesToRead, map< unsigned int, unsigned short >& nDigisInGdpb );
+    void processMessages( uint64_t* messageBuffer, size_t nFullMessagesToRead, vector < vector < gdpbv100::FullMessage > >& triggerMessages, map< unsigned int, unsigned short >& nDigisInGdpb );
 
     unsigned int hardwareMapElinkGet4Id( const unsigned int& chip );
     unsigned int hardwareMapChannelNumberInFee( const unsigned int& chip, const unsigned int& chan );
     unsigned int hardwareMapFeeNumber( const unsigned int& chip );
     unsigned int hardwareMapChannelNumber( const unsigned int& chip, const unsigned int& chan );
-    
+
+    unsigned int hardwareMapSector(  const unsigned int& gdpb );
+    unsigned int hardwareMapModule(  const unsigned int& mappedChannelNr );
+    unsigned int hardwareMapCounter( const unsigned int& mappedChannelNr );
+    unsigned int hardwareMapSide(    const unsigned int& mappedChannelNr );
+    unsigned int hardwareMapStrip(   const unsigned int& mappedChannelNr );
+
     void processMessages2018( uint64_t* messageBuffer, size_t nFullMessagesToRead, map< unsigned int, unsigned short >& nDigisInGdpb );
     // --------------------------------
 
@@ -54,26 +63,14 @@ class etofBuilder : public JevpBuilder {
         TH1* digiFineTs;
         TH1* digiCoarseTs;
 
-
         TH1* nDigisPerGdpb[ 12 ];
         TH1* digiTotPerGdpb[ 12 ];
         TH1* digiMappedChannelNumberPerGdpb[ 12 ];
-        
-        /*
-        TH1* hitMap;
-        TH1* hitMapChannelId;
-        TH1* hitMapCounter[9];
-        TH1* fineTimeAll;
-        TH1* coarseTimeAll;
-        TH1* fineTimeGet4card[18];
-        TH1* coarseTimeGet4card[18];
-        TH1* fineTime[20];
 
-        TH1* nEpochMismatch;
-        TH1* nEpochLoss;
-        TH1* nEpochDataLoss;
-        TH1* nEpochSync;
-        */
+        TH1* digiDensityAllChannels;
+
+        TH1* triggerTimeDiffSectors[ 11 ];
+        TH1* missingTriggerTs;
       };
     } contents;
 
@@ -100,6 +97,7 @@ class etofBuilder : public JevpBuilder {
     unsigned int nrOfChannelsPerGet4;
 
     unsigned int nrOfChannelsPerFee;
+    unsigned int nrOfChannelsPerGbtx;
     unsigned int nrOfGet4PerGbtx;
     unsigned int nrOfChannelsPerGdpb;
 
