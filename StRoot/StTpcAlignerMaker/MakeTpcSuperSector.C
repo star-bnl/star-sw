50d3b265ddb0730c517d3fee34893ddbb264ecf5
Yuri Fisyak <fisyak@bnl.gov>
2019-03-22 10:31:57 -0400

    Freeze intermidiate Tpc Alignment for Run XIX
    Former-commit-id: f26230e68e85a529c03a9b5912fe0286a71cfc2a

--- StRoot/StTpcAlignerMaker/MakeTpcSuperSector.C
+++ StRoot/StTpcAlignerMaker/MakeTpcSuperSector.C
@@ -31,69 +31,220 @@
 #include "StChain.h"
 #include "tables/St_Survey_Table.h"
 #include "TGeoMatrix.h"
+#include "StTpcDb/StTpcDb.h"
+#include "StEvent/StEnumerations.h"
+#include "StDetectorDbMaker/StTpcSurveyC.h"
 #endif
 TCanvas *c1 = 0;
-THStack *hs[6];
-TLegend *leg[6];
+static THStack *hs[6];
+static TLegend *leg[6];
+static TGeoHMatrix RAv[3], RAvI[3];
+static TGeoHMatrix dR[24], dRC[24];
+static TGeoHMatrix Rideal[24], RidealI[24];
+static const Double_t scale = 1.0;
+//________________________________________________________________________________
+void RemoveOverAllShift(SurveyPass_t &Pass) {
+  /* Euler angles: a = phi, b = theta, g = psi
+                  (  1 -(a+g)  0)   
+     R(a,b,g) =   (a+g      1 -b) = 
+                  (  0      b  1)
+   ( cospsi*cosphi - costhe*sinphi*sinpsi;   -sinpsi*cosphi - costhe*sinphi*cospsi;    sinthe*sinphi;)
+   ( cospsi*sinphi + costhe*cosphi*sinpsi;   -sinpsi*sinphi + costhe*cosphi*cospsi;   -sinthe*cosphi;) =
+   (                        sinpsi*sinthe;                           cospsi*sinthe;           costhe;) 
+
+   (       1; -psi-phi;    0;
+   ( phi+psi;        1; -the;) =  psi = 0
+   (       0;      the;    1;) 
+
+
+   (   1;  -(a+g);   0;)
+   ( a+g;      1;   -b;) =
+   (   0;;     b;    1;) 
+
+   Tait–Bryan angles:      dR.RotateX(a); dR.RotateY(b); dR.Rotate(g);
+
+                            ( 1  -g  b dx)       (nx)              (   nx +g*ny -b*nz)        (x)           (  x - g*y )
+      dR(a,b,g,dx,dy,dz) =  ( g   1 -a dy); n =  (ny);   dR x n =  (-g*nx +  ny +a*nz);   X = (y); dR x X = (dy)
+                            (-b   a  1 dz)       (nz)		   ( b*nx -a*ny +  nz)        (z)           (dz)
+		            ( 0   0  0  1)       ( 0) 		   ( 0               )        (1)           ( 1)
+
+			    Euler <=> Tait–Bryan : a+g <=> g; b <=> a; 0 <=> b;
+Euler Rotation in Tait-Bryan angles
+
+   (   1;  -g;   0;)
+   (   g;   1;  -a;) =
+   (   0;;  a;   1;) 
+=========              T = Sup2TPC; dRi = I, Rold 
+    dRAv = <dTPCreal * dTPCideal^-1> = <T * dRreal * T^-1> = <T *dRr * T^-1> = dRAv; 
+    Rtpc * dRAv^-1 = T * dRr * dRAv^-1
+    T^-1 * dRAv * T= dRr * dRi^-1 
+========
+    A    = <T * dR * C * T^-1>; dR * C = 
+    D    =  T * dR;  D * A^-1 = T * dR * A^-1 = T * dR *  
+   */
+  Int_t date = Pass.date;
+  Int_t time = Pass.time;
+  StChain *chain = (StChain *) StChain::GetChain();
+  StMaker *db = chain->Maker("db");
+  if (! db) return;
+  db->SetDebug(1);
+  db->SetDateTime(date,time);
+  static Bool_t InitDone = kFALSE;
+  if (! InitDone) {
+    chain->Init();
+    InitDone = kTRUE;
+  }
+  chain->MakeEvent();
+  // Calculate average shift and rotation for each half of TPC and overall TPC
+  Double_t phi[24] = {0};
+  Double_t theta[24] = {0};
+  Double_t psi[24] = {0};
+  Double_t tra[24][3] = {0};
+  for (Int_t i = 0; i < 24; i++) {
+    /*
+      dR.RotateX(a); dR.RotateY(b); dR.Rotate(g);
+
+                            ( 1  -g  b dx)       (nx)              (   nx +g*ny -b*nz)        (x)           (  x - g*y )
+      dR(a,b,g,dx,dy,dz) =  ( g   1 -a dy); n =  (ny);   dR x n =  (-g*nx +  ny +a*nz);   X = (y); dR x X = (dy)
+                            (-b   a  1 dz)       (nz)		   ( b*nx -a*ny +  nz)        (z)           (dz)
+		            ( 0   0  0  1)       ( 0) 		   ( 0               )        (1)           ( 1)
+     */
+    StBeamDirection part = east;
+    if (i < 12) part = west;
+    Double_t xyz[3];
+    xyz[0] = 1e-4*Pass.Data[i].x*scale;
+    xyz[1] = 1e-4*Pass.Data[i].y*scale;
+    xyz[2] = 1e-4*Pass.Data[i].z*scale;
+    //#define __SWAP_SIGNS__
+#ifdef __SWAP_SIGNS__
+    Pass.Data[i].alpha *= -1.;
+    Pass.Data[i].beta  *= -1.;
+    Pass.Data[i].gamma *= -1.;
+#endif
+    dR[i].RotateX( TMath::RadToDeg()*Pass.Data[i].alpha*1e-3*scale);
+#if 0
+    dR[i].RotateY( TMath::RadToDeg()*Pass.Data[i].beta*1e-3*scale); 
+#endif
+    dR[i].RotateZ( TMath::RadToDeg()*Pass.Data[i].gamma*1e-3*scale);  // swap sign 03/13/19
+    dR[i].SetTranslation(xyz);
+    Rideal[i]     = StTpcDb::instance()->SupS2Glob(i+1); cout << "Ideal:"; Rideal[i].Print();
+    RidealI[i]    = Rideal[i].Inverse(); cout << "Inverse:"; RidealI[i].Print();
+    TGeoHMatrix Rreal      = StTpcDb::instance()->SupS2Glob(i+1)*dR[i]; cout << "Real:"; Rreal.Print();
+    TGeoHMatrix R = Rreal * Rideal[i].Inverse(); cout <<"Diff:"; R.Print();
+    const Double_t *xyzR = R.GetTranslation();
+    for (Int_t j = 0; j < 3; j++) tra[i][j] = xyzR[j];
+#if 0
+    TGeoRotation RR(R);
+    RR.GetAngles(phi[i],theta[i],psi[i]); 
+#else
+    Double_t *m = R.GetRotationMatrix();
+    theta[i] = TMath::ACos(m[8])*TMath::RadToDeg();
+    phi[i]   = TMath::ATan2(-m[8]*m[1],m[0])*TMath::RadToDeg();
+    psi[i]   = 0.; // convention, phi+psi matters
+#endif
+    cout << " sec " << i+1 << " phi " << phi[i] << " theta " << theta[i] << " psi " << psi[i] << endl;
+  }
+  Double_t dphi[3] = {0}, dtheta[3] = {0}, dpsi[3] = {0}, dxyz[3][3] = {0};
+  for (Int_t l = 0; l < 3; l++) {
+    Int_t i1 = 0, i2 = 24;
+    if      (l == 0) {i2 = 12;}
+    else if (l == 1) {i1 = 12;}
+    Int_t N = i2 - i1;
+    for (Int_t i = i1; i < i2; i++) {
+      dphi[l]   += phi[i]/N;
+      dtheta[l] += theta[i]/N;
+      dpsi[l]   += psi[i]/N;
+      for (Int_t j = 0; j < 3; j++)dxyz[l][j] += tra[i][j]/N;
+    }
+    TGeoRotation RR;
+    RR.SetAngles(dphi[l], dtheta[l], dpsi[l]);
+    RAv[l].SetRotation(RR.GetRotationMatrix());
+    RAv[l].SetTranslation(dxyz[l]);
+    if (l == east) cout << "east:";
+    else if (l == west) cout << "west:";
+    else                cout << "Tpc:";
+    RAv[l].Print();
+    RAvI[l] = RAv[l].Inverse(); cout << "Inverse:"; RAvI[l].Print("");
+  }
+  for (Int_t i = 0; i < 24; i++) {
+    StBeamDirection part = east;
+    if (i < 12) part = west;
+    //    cout << "dR" << i << ":"; dR[i].Print("");
+    /*
+      RAV = <Rideal*dR*Rideal^1>; dRAv = Rideal
+      RA  = Rideal*dR
+      RA*dRAV^-1   = Rideal*dR*<Rideal*dR*Rideal^1>^-1 = Rideal*dR *(
+      T*dR * T^-1 * RAv^-1 * T
+
+      g = T * l;
+      A = g * T^-1 = T * l * T^-1
+      A = g * C^-1; g = A * C = 
+
+      
+     */
+#ifdef __ACCOUNT_OFFSET__
+    TGeoHMatrix A = RidealI[i] * RAvI[part] * Rideal[i]; cout << "A\t:"; A.Print();
+    dRC[i] = dR[i]*A ; //cout << "dRC:"; dRC[i].Print("");
+    cout << "sec = " << i+1 << " before:"; Pass.Data[i].Print();
+#else
+    dRC[i] = dR[i];
+#endif
+    Pass.Data[i].x = dRC[i].GetTranslation()[0]*1e4/scale;
+    Pass.Data[i].y = dRC[i].GetTranslation()[1]*1e4/scale;
+    Pass.Data[i].z = dRC[i].GetTranslation()[2]*1e4/scale;
+    Pass.Data[i].alpha = -dRC[i].GetRotationMatrix()[5]*1e3/scale;
+    //    Pass.Data[i].beta  = 0;
+    Pass.Data[i].gamma = -dRC[i].GetRotationMatrix()[1]*1e3/scale;
+    cout << "sec = " << i+1 << " after :"; Pass.Data[i].Print();
+    cout << "================================================================================" << endl;
+  }
+}
 //________________________________________________________________________________
 void MakeSuperSectorPositionB(){
   const Char_t *plots[3] = {"dXS","dYS","dZS"};
   for (Int_t j = 1; j <= 24; j++) {
     cout << Form("%2i ",j); Passes[NP-1].Data[j-1].Print();
   }
-  Int_t date = Passes[NP-1].date;
-  Int_t time = Passes[NP-1].time;
-#if 0
-  gROOT->LoadMacro("bfc.C");
-  bfc(0,"mysql,tpcDb,MagF,nodefault");
-#endif
-  StChain *chain = (StChain *) StChain::GetChain();
-  StMaker *db = chain->Maker("db");
-  if (! db) return;
-  db->SetDebug(1);
-  db->SetDateTime(date,time);
-  chain->Init();
-  chain->MakeEvent();
-  St_Survey *TpcSuperSectorPositionB = (St_Survey *) chain->GetDataBase("Geometry/tpc/TpcSuperSectorPositionB");
+  //  St_Survey *TpcSuperSectorPositionB = (St_Survey *) chain->GetDataBase("Geometry/tpc/TpcSuperSectorPositionB");
+  St_Survey *TpcSuperSectorPositionB = (St_Survey *) StTpcSuperSectorPosition::instance()->Table(); //chain->GetDataBase("Geometry/tpc/TpcSuperSectorPositionB");
   if (! TpcSuperSectorPositionB)  {cout << "TpcSuperSectorPositionB has not been found"  << endl; return;}
   TpcSuperSectorPositionB->Print(0,24);
   TGeoHMatrix GL;
   Survey_st *TpcSuperSectorOld         = TpcSuperSectorPositionB->GetTable();        
   Survey_st row[24];
+  /* 
+     Rav = R * dR => R^-1 * Rav =  dR
+     
+   */
   for (Int_t i = 0; i < 24; i++) {
+    StBeamDirection part = east;
+    if (i < 12) part = west;
     row[i] = TpcSuperSectorOld[i];
     GL.SetRotation(&TpcSuperSectorOld[i].r00);
     GL.SetTranslation(&TpcSuperSectorOld[i].t0); cout << "s: " << i+1 << " GL\t"; GL.Print();
-    TGeoHMatrix dR;
-    dR.RotateX(-TMath::RadToDeg()*Passes[NP-1].Data[i].alpha*1e-3*1.0);
-#if 0
-    dR.RotateY(-TMath::RadToDeg()*Passes[NP-1].Data[i].beta*1e-3*1.0); 
-#endif
-    dR.RotateZ(TMath::RadToDeg()*Passes[NP-1].Data[i].gamma*1e-3*1.0);
-    Double_t xyz[3];
     Double_t dxyz[3], drot[3];
-    xyz[0] = 1e-4*Passes[NP-1].Data[i].x*1.0;
-    xyz[1] = 1e-4*Passes[NP-1].Data[i].y*1.0;
-    xyz[2] = 1e-4*Passes[NP-1].Data[i].z*1.0;
     dxyz[0] = 1e-4*Passes[NP-1].Data[i].Dx;
     dxyz[1] = 1e-4*Passes[NP-1].Data[i].Dy;
     dxyz[2] = 1e-4*Passes[NP-1].Data[i].Dz;
     drot[0] = Passes[NP-1].Data[i].Dalpha*1e-3;
     drot[1] = Passes[NP-1].Data[i].Dbeta*1e-3; 
     drot[2] = Passes[NP-1].Data[i].Dgamma*1e-3;
-    dR.SetTranslation(xyz);
     memcpy(&row[i].sigmaRotX, drot, 3*sizeof(Double_t));
     memcpy(&row[i].sigmaTrX, dxyz, 3*sizeof(Double_t));
-    cout << "Additional rotation for Inner Sector\t"; dR.Print();
-    //    TGeoHMatrix GLnew = GL * dR;  cout << "GLnew\t"; GLnew.Print();
-    //    TGeoHMatrix GLnew = StTpcDb::instance()->Flip().Inverse()*dR*StTpcDb::instance()->Flip()*GL; GLnew.Print();
-    TGeoHMatrix GLnew = GL*dR; GLnew.Print();
-    //    TGeoHMatrix GLnew = dR * GL; GLnew.Print();
+    cout << "Additional rotation for Inner Sector\t"; dR[i].Print();
+    cout << "dR" << i << ":"; dR[i].Print("");
+    //    TGeoHMatrix dRC = dR[i]*RAvI[part]; cout << "dRC:"; dRC.Print("");
+    //    TGeoHMatrix GLnew = StTpcDb::instance()->Flip().Inverse()*dR[i]*StTpcDb::instance()->Flip()*GL; GLnew.Print(); // Flip 03/14/19
+    //    TGeoHMatrix GLnew = GL*dR[i]; GLnew.Print(); // used till 03/13/19 and after 03/15/19
+    TGeoHMatrix GLnew = GL*dRC[i]; GLnew.Print(); // used till 03/13/19 and after 03/15/19
+    //    TGeoHMatrix GLnew = dR[i] * GL; GLnew.Print();
     Double_t *R = GLnew.GetRotationMatrix();
     memcpy(&row[i].r00, R, 9*sizeof(Double_t));
     Double_t *tr = GLnew.GetTranslation();
     memcpy(&row[i].t0, tr, 3*sizeof(Double_t));
   }
+  Int_t date = Passes[NP-1].date;
+  Int_t time = Passes[NP-1].time;
   TString fOut =  Form("TpcSuperSectorPositionB.%8i.%06i.C", date, time);
   ofstream out;
   cout << "Create " << fOut.Data() << endl;
@@ -105,9 +256,9 @@ void MakeSuperSectorPositionB(){
   for (Int_t i = 0; i < 24; i++) {
     out << "    {" << Form("%2i",i+1) << ","; 
     Double_t *r = &(row[i].r00);
-    cout << " ";
+    //    cout << " ";
     for (Int_t j =  0; j <  9; j++) out << Form("%8.5f,",r[j]);
-    cout << " ";
+    //    cout << " ";
     for (Int_t j =  9; j < 12; j++) out << Form("%8.4f,",r[j]); 
     cout << " ";
     for (Int_t j = 12; j < 18; j++) out << Form("%8.5f,",r[j]);
@@ -115,14 +266,14 @@ void MakeSuperSectorPositionB(){
     if (i != 23) out << ",";
     out << endl;
   }
+  cout << endl;
   out << "  };" << endl;
   out << "  St_Survey *tableSet = new St_Survey(\"TpcSuperSectorPositionB\",24);" << endl; 
   out << "  for (Int_t i = 0; i < 24; i++) tableSet->AddAt(&row[i].Id);" << endl;
   out << "  return (TDataSet *)tableSet;" << endl;
   out << "}" << endl;
   out.close(); 
-} 
-
+}
 //________________________________________________________________________________
 void MakeTpcSuperSector(const Char_t *opt="") {
   gStyle->SetMarkerStyle(20);
@@ -142,16 +293,19 @@ void MakeTpcSuperSector(const Char_t *opt="") {
     else       hs[i]->SetYTitle(Form("%s (mrad)",names[i]));
     hs[i]->SetXTitle("sector");
 #endif
-    Double_t ymin =  1e10;
-    Double_t ymax = -1e10;
-    TString Name;
-    TString Title;
     if (! i)     leg[i] = new TLegend(0.10,0.65,0.30,0.90);
     else         leg[i] = 0;
-    TString same("e");
-    Int_t color = 1;
-    TH1::SetDefaultSumw2(kTRUE);
-    for (Int_t k = 0; k < NP; k++) {
+  }
+  TString same("e");
+  TH1::SetDefaultSumw2(kTRUE);
+  for (Int_t k = 0; k < NP; k++) {
+    RemoveOverAllShift(Passes[k]);
+    for (Int_t i = 0; i < 6; i++) {
+      Int_t color = k+1;
+      TString Name;
+      TString Title;
+      Double_t ymin =  1e10;
+      Double_t ymax = -1e10;
       if (k < NP) {
 	if (i == 0 && k < NP) Passes[k].Print();
 	Name = Form("%s%s",Passes[k].PassName,nameK[i]);
@@ -171,7 +325,6 @@ void MakeTpcSuperSector(const Char_t *opt="") {
       //      cout << "Create: " << dath[k][i]->GetName() << "\t" << dath[k][i]->GetTitle() << endl;
       dath[k][i]->SetMarkerColor(color);
       dath[k][i]->SetLineColor(color);
-      color++;
       dath[k][i]->SetXTitle("sector");
       if (i < 3) dath[k][i]->SetYTitle(Form("%s (#mum)",names[i]));
       else       dath[k][i]->SetYTitle(Form("%s (mrad)",names[i]));
@@ -236,4 +389,5 @@ void MakeTpcSuperSector(const Char_t *opt="") {
     if (leg[i]) leg[i]->Draw();
   }
   c1->Update();
+  MakeSuperSectorPositionB();
 }
