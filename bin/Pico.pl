1769df2a7eca146f066c16a7c5e04e2a0761a339
Yuri Fisyak <fisyak@bnl.gov>
2019-05-08 11:53:03 -0400

    Freeze
    Former-commit-id: 680d8ed4590b7935829c2b1b81de380a61667d63

---
+++ bin/Pico.pl
@@ -0,0 +1,16 @@
+#! /usr/bin/env perl
+use File::Basename;
+#my $glob =  "/net/l404/data/fisyak/Pico/BES-I/AuAu19_production/2011/???/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/???/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/TFG19d/???/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/14GeV_2019_StiCA/0??/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/14GeV_2019/0??/*";
+my $glob = "/gpfs01/star/data*/reco/production_14p5GeV_2019/ReversedFullField/dev/2019/*/*";
+foreach my $file (glob $glob) {
+  my $f = File::Basename::basename($file);
+  my @words = split('/',$file);# print "disk = $words[3]\n";
+  my $pico = $f . $words[3] . ".root";# print "$pico\n";
+  if ( -r $pico) {next};
+  print "string:$file\n";
+#  last;
+}
