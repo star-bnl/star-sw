577904dcb388756aff835dd5b01a7c5cda6c8897
Yuri Fisyak <fisyak@bnl.gov>
2019-05-09 14:13:33 -0400

    Add switch between StiKFVertex and KFVertex
    Former-commit-id: 8169c03697beb2388e928f0a3792ce622b3bb54d

--- macros/KFVDebug.C
+++ macros/KFVDebug.C
@@ -1,6 +1,9 @@
 void KFVDebug() {
   StKFVertexMaker *kfv = (StKFVertexMaker *) chain->Maker("KFVertex");
-  if (! kfv) return;
+  if (! kfv) {
+    kfv = (StKFVertexMaker *) chain->Maker("StiKFVertex");
+    if (!kfv) return;
+  }
   kfv->SetDebug(3);
   TCanvas *c1 = new TCanvas("c1","c1",1400,500);
   kfv->SetCanvas(c1);
