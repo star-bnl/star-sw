f838c31b21855298b8726ce720085546a17959e5
Yuri Fisyak <fisyak@bnl.gov>
2019-05-18 02:13:32 -0400

    Reduce NCont 255 => 100
    Former-commit-id: 05caf33d0b6f2d79cdc56ecd45606970d0330ef7

--- StRoot/macros/rootlogon.C
+++ StRoot/macros/rootlogon.C
@@ -71,7 +71,7 @@
   //  gStyle->SetPalette(55);
   // Maxim's pallete
   const Int_t NRGBs = 5;
-  const Int_t NCont = 255;
+  const Int_t NCont = 100; // 255;
   Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
   Double_t red[NRGBs] = { 0.00, 0.00, 0.87, 1.00, 0.51 };
   Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
