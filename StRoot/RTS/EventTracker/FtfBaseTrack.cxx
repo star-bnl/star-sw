1d93cb7517f81d731a82f640faeaa7b9c12ea5c3
evpdaq <>
2019-06-17 14:58:22 +0000

    update

--- StRoot/RTS/EventTracker/FtfBaseTrack.cxx
+++ StRoot/RTS/EventTracker/FtfBaseTrack.cxx
@@ -287,7 +287,7 @@ int FtfBaseTrack::fitCircle (  )
   double h24   = yrrav ;
   double h34   = 1.0   + 2.0*lamda ;
   if ( h11 == 0.0 || h22 == 0.0 ){
-	  LOG(ERR, " Problems fitting a circle " ) ;
+    //LOG(ERR, " Problems fitting a circle " ) ;
 	  return 1 ;
   }
   double rootsq = (h14*h14)/(h11*h11) + 4.0*h34 ;
