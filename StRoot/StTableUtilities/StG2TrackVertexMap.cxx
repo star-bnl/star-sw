c2273b197814f53bf8760bfcfb3ed1af81533df8
Yuri Fisyak <fisyak@bnl.gov>
2019-07-15 14:59:45 -0400

    comment out assert
    Former-commit-id: 6a0ba8ae968f10a01d867b065444905b573432e2

--- StRoot/StTableUtilities/StG2TrackVertexMap.cxx
+++ StRoot/StTableUtilities/StG2TrackVertexMap.cxx
@@ -2,6 +2,7 @@
 #include "Riostream.h"
 #include "StG2TrackVertexMap.h"
 #include "TVector3.h"
+#include "StMessMgr.h"
 StG2TrackVertexMap *StG2TrackVertexMap::fgInstance = 0;
 using namespace std;
 static Int_t _debug = 0;
@@ -27,19 +28,26 @@ void StG2TrackVertexMap::Reset(St_g2t_track *track, St_g2t_vertex *vertex) {
   for (Int_t i = 0; i < Nt; i++) {
     Int_t IdT = t[i].id - 1;                 
     if (! (IdT >= 0 && IdT < Nt)) {
-      cout << " StG2TrackVertexMap::Reset Illegal IdT = " << IdT + 1 << " and Nt = " << Nt << endl;
-      assert(IdT >= 0 && IdT < Nt);
+      LOG_ERROR << " StG2TrackVertexMap::Reset Illegal IdT = " << IdT + 1 << " and Nt = " << Nt << endm;
+      //      assert(IdT >= 0 && IdT < Nt);
+      continue;
     }
     Int_t IdV = t[i].start_vertex_p - 1;     
     if (! (IdV >= 0 && IdV < NV)) {
-      cout << " StG2TrackVertexMap::Reset Illegal IdV = " << IdV + 1 << " and NV = " << NV << endl;
-      assert(IdV >= 0 && IdV < NV);
+      LOG_ERROR << " StG2TrackVertexMap::Reset Illegal IdV = " << IdV + 1 << " and NV = " << NV << endm;
+      //      assert(IdV >= 0 && IdV < NV);
+      continue;
     }
     TVector3 Vx1(v[IdV].ge_x);
     Float_t tof1 = v[IdV].ge_tof;
     Int_t IdP = v[IdV].parent_p - 1;         
     if (IdP >= 0 && IdP < Nt) {
-      Int_t IdVxP = t[IdP].start_vertex_p - 1; assert(IdVxP >= 0 && IdVxP < NV);
+      Int_t IdVxP = t[IdP].start_vertex_p - 1; 
+      if (! (IdVxP >= 0 && IdVxP < NV)) {
+	LOG_ERROR << " StG2TrackVertexMap::Reset Illegal IdVxP = " << IdV + 1 << " and NV = " << NV << endm;
+	//      assert(IdVxP >= 0 && IdVxP < NV);
+	continue;
+      }
       TVector3 Vx2(v[IdVxP].ge_x);
       Vx2 -= Vx1;
       while ((IdVxP > 0 && IdV != IdVxP && Vx2.Mag() < 10e-4 && TMath::Abs(tof1-v[IdVxP].ge_tof) < 1e-9)) {
@@ -61,24 +69,24 @@ void StG2TrackVertexMap::Reset(St_g2t_track *track, St_g2t_vertex *vertex) {
 }
 //________________________________________________________________________________
 void StG2TrackVertexMap::Print(Option_t *option) const {
-  cout << "StG2TrackVertexMap:";
-  if (fVertex) cout << " No.g2tVertices = " << fVertex->GetNRows();
-  if (fTrack)  cout << " No.g2tTracks = " << fTrack->GetNRows();
-  cout << endl;
-  cout << "Track to Vertex map" << endl;
+  LOG_INFO << "StG2TrackVertexMap:";
+  if (fVertex) LOG_INFO << " No.g2tVertices = " << fVertex->GetNRows();
+  if (fTrack)  LOG_INFO << " No.g2tTracks = " << fTrack->GetNRows();
+  LOG_INFO << endm;
+  LOG_INFO << "Track to Vertex map" << endm;
   Int_t t = 0;
   for (auto x : fTrack2Vertex) {
-    if (t > 0 && t%5 == 0) cout << endl;
-    cout << "V[" << x.first << "] = " << x.second << "\t" << IdVertex(x.first) << "\t";
+    if (t > 0 && t%5 == 0) LOG_INFO << endm;
+    LOG_INFO << "V[" << x.first << "] = " << x.second << "\t" << IdVertex(x.first) << "\t";
     t++;
   }
-  cout << endl;
-  cout << "Vertex to Parent track map" << endl;
+  LOG_INFO << endm;
+  LOG_INFO << "Vertex to Parent track map" << endm;
   t = 0;
   for (auto x : fVertex2ParentTrack) {
-    if (t > 0 && t%5 == 0) cout << endl;
-    cout << "T[" << x.first << "] = " << x.second << "\t" << IdParentTrack(x.first) << "\t";
+    if (t > 0 && t%5 == 0) LOG_INFO << endm;
+    LOG_INFO << "T[" << x.first << "] = " << x.second << "\t" << IdParentTrack(x.first) << "\t";
     t++;
   }
-  cout << endl;
+  LOG_INFO << endm;
 }
