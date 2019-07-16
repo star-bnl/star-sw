1edfe11d34e1e4d1da1d04c8c4d1c3dbb45e9ab3
smirnovd <>
2019-07-16 21:32:34 +0000

    Introduce a C++(11) alternative to global Form() function in ROOT

--- StRoot/St_base/Stsstream.h
+++ StRoot/St_base/Stsstream.h
@@ -35,4 +35,29 @@ istrstream(const char *init):std::istringstream(std::string(init)){}
 #else
 #endif 
 
+// Hide the function from `rootcint` as it cannot process variadic template parameters
+#ifndef __CINT__
+
+#include <iostream>
+#include <cstdio>
+#include <memory>
+#include <string>
+
+/**
+ * This is an alternative to the ROOT's global function Form() defined in
+ * TString.cxx. The implementation of this function is based on the answer to
+ * https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+ *
+ * Returns an std::string
+ */
+template<typename ... Args>
+std::string FormString(const std::string& format, Args ... args)
+{
+    size_t cstr_size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
+    std::unique_ptr<char[]> buf(new char[cstr_size]);
+    snprintf(buf.get(), cstr_size, format.c_str(), args ...);
+    return std::string(buf.get(), buf.get() + cstr_size - 1); // We don't want the '\0' inside
+}
+#endif
+
 #endif 
