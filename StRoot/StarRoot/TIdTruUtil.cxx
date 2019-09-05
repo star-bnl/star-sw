01446e76bf64f564766f931661eb0e0365df76f5
Victor Perevoztchikov <perev@bnl.gov>
2019-09-05 18:34:48 +0000

    Handling 0 idTruth

--- StRoot/StarRoot/TIdTruUtil.cxx
+++ StRoot/StarRoot/TIdTruUtil.cxx
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: TIdTruUtil.cxx,v 1.1 2016/02/16 16:58:28 perev Exp $
+ * $Id: TIdTruUtil.cxx,v 1.2 2019/09/05 18:34:48 perev Exp $
  *
  ***************************************************************************
  *
@@ -36,20 +36,26 @@ mDetWt.clear();
 void TIdTruUtil::Eval()
 {
   mEvalted = 1;  
+  mIdTru   = 0;
+  mQua     = 0;
   double totWt=0;
   std::multimap <double,int> myWtDet;
   for (auto it = mDetWt.begin(); it!= mDetWt.end(); ++it) {
     int idTru = (*it).first;
+    if (!idTru) continue;
     double wt = (*it).second;
     totWt+=wt;
     myWtDet.insert( std::pair<double,int>(-wt,idTru));
   }
+  if (!myWtDet.size()) return;
   mQua   = fabs((*myWtDet.begin()).first/(totWt+1e-11));
   mIdTru = (*myWtDet.begin()).second;
 }
 //______________________________________________________________________________
 void TIdTruUtil::Add(int idTru,int qa)
 {
+   mEvalted = 0;  
+   if (!idTru) return;
    mSize++;
    mDetWt[idTru]+=qa;
 }
