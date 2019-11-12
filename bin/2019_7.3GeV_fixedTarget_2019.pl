7860cb14da62a5a11948a2b809b3832e5e843d84
Yuri Fisyak <fisyak@bnl.gov>
2019-11-12 10:20:50 -0500

    Freeze
    Former-commit-id: b74e9615ed9a91b3f4c2d17d75cdec87891eb721

---
+++ bin/2019_7.3GeV_fixedTarget_2019.pl
@@ -0,0 +1,29 @@
+#! /usr/bin/env perl
+my $cmd = "get_file_list.pl -delim '/' -keys 'path,filename' -cond 'filetype=online_daq,filename~st,trgsetupname= production_7.3GeV_fixedTarget_2019,tpx=1,itpc=1,sanity=1,runnumber>20000000' -limit 0";  
+
+print "$cmd\n";
+my @list = `$cmd`;
+
+foreach my $file (@list) {
+  chomp($file);
+  my $basename = $file;
+  $basename =~ s#/home/starsink/raw##;
+#  print "$file = > $basename\n";
+  my $found = 0;
+  my $dir;
+  my @dirs = qw(/star/data03 /gpfs01/star/scratch/fisyak);
+  foreach $dir (@dirs) {
+    my $fullpath = $dir . $basename;
+#    print "$fullpath ===========";
+    if (-r $fullpath) {
+      $found = 1; 
+#      print "found\n"; 
+      last;
+    }
+#    print "not found\n";
+  }
+  if (! $found) {
+    print "$file $dirs[0]$basename\n";
+  }
+}
+	
