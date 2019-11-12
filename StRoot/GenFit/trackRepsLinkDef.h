d09d1bf8b1c47112fa363e813dc03c66951deb82
Yuri Fisyak <fisyak@bnl.gov>
2019-11-12 09:30:37 -0500

    Merge
    Former-commit-id: 26771dee2b3f70398523ee8db36f99fd8cefac1b

--- StRoot/GenFit/trackRepsLinkDef.h
+++ StRoot/GenFit/trackRepsLinkDef.h
@@ -1,16 +1,11 @@
-#ifdef __CINT__
-
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-#ifdef __CLING__
 #pragma link C++ nestedclass;
 #pragma link C++ nestedtypedef;
 
 #pragma link C++ namespace genfit;
-#endif
 
 #pragma link C++ class genfit::RKTrackRep-;
-
-#endif
+#pragma link C++ class genfit::MplTrackRep-;
