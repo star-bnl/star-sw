1338aef4d69c043f7ba922431581c06c35e1ec31
Yuri Fisyak <fisyak@bnl.gov>
2019-11-22 18:30:48 -0500

    Add geometry of Run XIX as Run XX
    Former-commit-id: 4197048553cdd818a368aefe5a31e9379469ebbd

---
+++ StarDb/AgiGeometry/Geometry.y2020.C
@@ -0,0 +1,36 @@
+#include "CreateGeometry.h"
+TDataSet *CreateTable() {
+  TEnv *configGeom = new TEnv("configGeom");
+ configGeom ->SetValue("isvt",-1);
+ configGeom ->SetValue("itpc",4);
+ configGeom ->SetValue("ibtf",8);
+ configGeom ->SetValue("ical",12);
+ configGeom ->SetValue("ivpd",-1);
+ configGeom ->SetValue("istb",-1);
+ configGeom ->SetValue("ifpd",-1);
+ configGeom ->SetValue("ifms",-1);
+ configGeom ->SetValue("ifsc",-1);
+ configGeom ->SetValue("tpcg_version",5);
+ configGeom ->SetValue("tpcg_tpadconfig",8);
+ configGeom ->SetValue("btog_version",6);
+ configGeom ->SetValue("btog_choice",13);
+ configGeom ->SetValue("btog_posit1(1)",32);
+ configGeom ->SetValue("btog_posit1(2)",33);
+ configGeom ->SetValue("btog_posit2",23);
+ configGeom ->SetValue("btog_posit3",33);
+ configGeom ->SetValue("calg_version",3);
+ configGeom ->SetValue("calg_nmodule(1)",60);
+ configGeom ->SetValue("calg_nmodule(2)",60);
+ configGeom ->SetValue("calg_netaT",20);
+ configGeom ->SetValue("calg_maxmodule",60);
+ configGeom ->SetValue("calg_nsub",2);
+ configGeom ->SetValue("calg_netasmdp",10);
+ configGeom ->SetValue("calg_nphistr",15);
+ configGeom ->SetValue("calg_netfirst",75);
+ configGeom ->SetValue("calg_netsecon",75);
+ configGeom ->SetValue("emcg_version",6.1);
+ configGeom ->SetValue("emcg_onoff",1);
+ configGeom ->SetValue("emcg_fillmode",3);
+ configGeom ->SetValue("mtdg_version",1.1);
+  return CreateGeometry("y2019",configGeom);
+}
