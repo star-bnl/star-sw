4a345b9a5cd3191a632e585afb95dbc017fe5ab8
Yuri Fisyak <fisyak@bnl.gov>
2019-12-10 16:49:55 -0500

    Apply Run19 tpc Geometry for Run 20
    Former-commit-id: 711d72358ca50a9f50a46559997b8df87533bb31

--- StarDb/Geometry/tpc/TpcSuperSectorPositionB.y2020.C
+++ StarDb/Geometry/tpc/TpcSuperSectorPositionB.y2020.C
@@ -4,7 +4,7 @@ TDataSet *CreateTable() {
     //             -gamma      beta     gamma              -alpha     -beta     alpha                 x0       y0       z0
     1, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000,  0.0000,  0.0000,  0.0000,0,0,0,0,0,0,"Ideal"
   };
-  St_Survey *tableSet = new St_Survey("TpcOuterSectorPositionB",24);
-  for (Int_t i = 0; i < 24; i++) {row.Id = i+1; tableSet->AddAt(&row.Id, i);}
+  St_Survey *tableSet = new St_Survey("TpcSuperSectorPositionB",24);
+  for (Int_t i = 0; i < 24; i++) { row.Id = i+1; tableSet->AddAt(&row.Id, i);}
   return (TDataSet *)tableSet;
 }
