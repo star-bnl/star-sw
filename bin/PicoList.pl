256e6d20860303a5b211b037ae59947ee557f469
Yuri Fisyak <fisyak@bnl.gov>
2019-12-30 16:12:22 -0500

    cond.pl
    Former-commit-id: 52c5dd17b0baae5ca0459de8f54fdeba57434215

---
+++ bin/PicoList.pl
@@ -0,0 +1,14 @@
+#! /usr/bin/env perl
+use File::Basename;
+#my $glob =  "/net/l404/data/fisyak/Pico/BES-I/AuAu19_production/2011/???/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/???/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/TFG19d/???/*";
+#my $glob =  "/net/l401/data/scratch2/kehw/reco/2019/14GeV_2019_StiCA/0??/*";
+my $glob =  "./Pico/???/*/*.picoDst.root";
+foreach my $file (glob $glob) {
+  my $f = File::Basename::basename($file);
+  my $pico = $f . ".root";
+  if ( -r $pico) {next};
+  print "string:$file\n";
+#  last;
+}
