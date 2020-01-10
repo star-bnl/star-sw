71176dc6c6583cf5955fc8f887c67bd10964d142
Yuri Fisyak <fisyak@bnl.gov>
2020-01-10 14:30:38 -0500

    add 2020 to the list
    Former-commit-id: 582b0a194ea94abf08ef0c3dbb28131f66b92e7a

--- bin/CleanHLTdaq.pl
+++ bin/CleanHLTdaq.pl
@@ -3,29 +3,37 @@ use File::Basename;
 use Cwd;
 my $daq = "";
 my @List = glob "*/*/Done";
+my $debug = 0;
 foreach my $file (@List) {
   my $Dir = File::Basename::dirname($file);
-#  print "Dir = $Dir\n";
+  print "Dir = $Dir\n" if ($debug);
   my $glob = $Dir . "/*picoDst.root";
-  my @PicoList = glob $glob; # print "glob = $glob, PicoList = @PicoList\n";
+  my @PicoList = glob $glob;  print "glob = $glob, PicoList = @PicoList\n" if ($debug);
   my $globEv = $Dir . "/*event.root"; 
   my @EventList = glob $globEv;
-  my $count = $#EventList; # print "$globEv = > EventList = @EventList\n";
+  my $count = $#EventList;  print "$globEv = > EventList = @EventList, count = $count\n" if ($debug);
   foreach my $pico (@PicoList) {
     my $tag = File::Basename::basename($pico,".picoDst.root");
     my $event = $Dir . "/" . $tag . ".event.root";
-#    print "tag = $tag, event = $event\n";
-#    if (-r $event) {
-#      print "$pico => $event\n";
-#    }
-    if (-r $event or $count == 0) {
-#      if (-r $event) { print "$count : $pico => $event\n";}
-#      else           { print "$count : $pico\n";}
+    print "tag = $tag\n" if ($debug);
+    if (-r $event and $debug) {
+      print "$pico => $event\n";
+    }
+    if (-r $event or $count == -1) {
+      if ($debug) {
+	if (-r $event) { print "$count : $pico => $event\n";}
+	else           { print "$count : $pico\n";}
+      }
       $count++;
       next;
     }
     my $daq = "/hlt/cephfs/daq/2019/" . $Dir . "/" . $tag . ".daq";
-    if (! -r $daq) {next;}
+    if (! -r $daq) {
+      $daq = "/hlt/cephfs/daq/2020/" . $Dir . "/" . $tag . ".daq";
+      if (! -r $daq) {
+	next;
+      }
+    }
     print "rm $daq\n";
   }
 }
