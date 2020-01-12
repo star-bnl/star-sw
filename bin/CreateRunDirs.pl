e39c3059da0eedbb77bcffd240003eb196bc7bb2
Yuri Fisyak <fisyak@bnl.gov>
2020-01-12 13:59:07 -0500

    Update
    Former-commit-id: ffabd860e2311e7d9f3212d11ef95f190d338bb0

---
+++ bin/CreateRunDirs.pl
@@ -0,0 +1,68 @@
+#! /usr/bin/env perl
+use File::Basename;
+use Cwd;
+use Env;
+use lib "/net/l402/data/fisyak/STAR/packages/.DEV2/bin";#$ENV{ConstructLocation}; 
+use RunXXDefs;
+my $pwd = cwd();
+#my $day = File::Basename::basename(File::Basename::dirname($pwd));
+#my $run =  File::Basename::basename($pwd);
+#my @globs = ("/hlt/cephfs/daq/2020/" . $day . "/" . $run . "*/hlt*.daq");#  print "globs = @globs\n";
+my $debug = 0;
+my $fNo = 0;
+# foreach my $glob (@globs) {
+#   my @files = glob $glob;
+#   foreach my $file (@files) {# print "file = $file\n";
+#     my $b = File::Basename::basename($file,".daq");
+#     print "$b\n" if ($debug);
+#     my $mufile = $b . ".MuDst.root";
+#     if (-r $mufile) {next;}
+#     print "string:$file\n";
+#     $fNo++;
+#   }
+# }
+#____________________________________________________________
+sub PrintHash($$) {
+  my $env = shift; # print "Call PrintHash\n";
+  my $prefix = shift;
+  foreach my $key (sort keys %$env ) {
+    print "{ $key }\t=> {'$env->{$key}->{trig}', \tfield=>`$env->{$key}->{field}',\tfirst=>'$env->{$key}->{first}', \tlast=>'$env->{$key}->{last}', \tbeginTime=>'$env->{$key}->{beginTime}'\n";
+  }
+}
+sub GoodRun($$) {
+  my $env = shift;
+  my $run = shift;
+  print "GoodRun:: run = $run" if $debug;
+  foreach my $key (sort keys %$env ) {
+    print "$pwd, trig = $env->{$key}->{trig}, field = $env->{$key}->{field}; first = $env->{$key}->{first}, last = $env->{$key}->{last}" if ($debug);
+    if ($pwd !~ /$env->{$key}->{trig}/)  {print ", rejected by trig\n"  if ($debug); next;}
+    if ($pwd !~ /$env->{$key}->{field}/) {print ", rejected by field\n" if ($debug); next;}
+    if ($run < $env->{$key}->{first})    {print ", rejected by first\n" if ($debug); next;}
+    if ($run > $env->{$key}->{last})     {print ", rejected by last\n"  if ($debug); next;}
+    print " accepted\n" if ($debug);
+    return $run;
+  }
+  print " rejected\n" if ($debug);
+  return -1;
+}
+my $def = {@Runs};# print "Runs = @Runs\n";
+#PrintHash($def,"Runs") if ($debug);
+#die;
+my  @runs  = glob "/hlt/cephfs/daq/2019/???/* /hlt/cephfs/daq/2020/???/*";  print "runs = @runs\n" if ($debug);
+#my  @runs  = glob "/hlt/cephfs/daq/2020/012/2101202?";  print "runs = @runs\n" if ($debug);
+foreach my $run (@runs) {
+  my $r = File::Basename::basename($run);
+  if (GoodRun($def,$r) < 0) {next;}
+  my $glob = $run . "/hlt*.daq";
+  my @daqfiles = glob $glob;
+  if ($#daqfiles < 0) {next;}
+  my $day = sprintf("%03i",(int ($r/1000))%1000); print "ru = $r => day = $day\n" if ($debug);
+  my $dir = $day . "/" . $r;
+  if (-d $dir) {next;}
+  my $cmd = "mkdir -p $dir"; print "cmd = $cmd\n";
+  my $flag = system($cmd);
+  if ($flag) {last;}
+}
+
+
+
---
+++ bin/JobSubmitcron.csh
@@ -0,0 +1,26 @@
+#! /bin/tcsh -f
+cd ~/bin/
+RunXX
+git diff .
+git ci -m "Update" .
+cd ~/reco/2020/TFG19m/RF/11p5GeV.B
+CreateRunDirs.pl
+foreach d (`ls -1d */2*`)
+  cd $d;
+  if (-r Done) then
+    cd -
+    continue
+  endif
+  daq_2020dR.pl
+  if ($?) then
+    ls -1d *bla.root
+    if ($?) then
+      rm sched* *xml;
+      touch Done
+    endif
+    cd -;
+    continue;
+  endif
+  lsf63 ~/xml/daq_2020StiCA.Minuit.TFG19m.xml
+  cd -
+end
