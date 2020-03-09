b33a00e922c9e3ff21ebd7022853a765f4dd0e20
Yuri Fisyak <fisyak@bnl.gov>
2020-03-09 14:32:48 -0400

    Merge
    Former-commit-id: 5cf004dba66699010e893ea90e4edd0ab54c4bc7

--- StRoot/StarRoot/THelixTrack.cxx
+++ StRoot/StarRoot/THelixTrack.cxx
@@ -3747,7 +3747,7 @@ double EmxSign(int n,const double *e)
 //______________________________________________________________________________
 /***************************************************************************
  *
- * $Id: THelixTrack.cxx,v 1.80 2020/01/15 19:18:00 perev Exp $
+ * $Id: THelixTrack.cxx,v 1.82 2020/03/09 17:56:16 perev Exp $
  *
  * Author: Victor Perev, Mar 2006
  * Rewritten Thomas version. Error hangling added
@@ -3763,8 +3763,8 @@ double EmxSign(int n,const double *e)
  ***************************************************************************
  *
  * $Log: THelixTrack.cxx,v $
- * Revision 1.80  2020/01/15 19:18:00  perev
- * BACKWARD
+ * Revision 1.82  2020/03/09 17:56:16  perev
+ * Out old version back
  *
  * Revision 1.78  2017/06/27 23:45:39  perev
  * Coverity
