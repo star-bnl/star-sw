053431465b4b6ea80e30693e1e7f6b5ac276b10c
Yuri Fisyak <fisyak@bnl.gov>
2020-04-18 09:31:19 -0400

    new ADC correction for Run XX
    Former-commit-id: 6bf974558ca613cc5547115075b0f3aadbce07f1

--- StarDb/Calibrations/tpc/TpcAdcCorrectionB.r2020.C
+++ StarDb/Calibrations/tpc/TpcAdcCorrectionB.r2020.C
@@ -6,36 +6,11 @@ TDataSet *CreateTable() {
   // ------  Test whether this table share library was loaded ------
   if (!TClass::GetClass("St_tpcCorrection")) return 0;
   tpcCorrection_st row;
-  St_tpcCorrection *tableSet = new St_tpcCorrection("TpcAdcCorrectionB",3);
+  Int_t nrows = 3;
+  St_tpcCorrection *tableSet = new St_tpcCorrection("TpcAdcCorrectionB",nrows);
   //
   memset(&row,0,tableSet->GetRowSize());
-  row.type	 =         10; // ;
-  row.idx	 =          1; // ;
-  row.nrows	 =          2; // ;
-  row.npar	 =          4; // ;
-  row.OffSet	 =          0; // ;
-  row.min	 =          3; // ;
-  row.max	 =          9; // ;
-  row.a[0]	 =   5.262643; // ;
-  row.a[1]	 =   -1.85861;
-  row.a[2]	 =   0.279676;
-  row.a[3]	 = -0.0145679;
-  tableSet->AddAt(&row);
-  memset(&row,0,tableSet->GetRowSize());
-  row.type	 =         10; // ;
-  row.idx	 =          2; // ;
-  row.nrows	 =          2; // ;
-  row.npar	 =          4; // ;
-  row.OffSet	 =          0; // ;
-  row.min	 =          3; // ;
-  row.max	 =          9; // ;
-  row.a[0]	 =   5.087566; // ;
-  row.a[1]	 =   -1.91954;
-  row.a[2]	 =   0.293305;
-  row.a[3]	 = -0.0153733;
-  tableSet->AddAt(&row);
-  memset(&row,0,tableSet->GetRowSize());
-  for (Int_t i = 2; i < 50; i++) {
+  for (Int_t i = 0; i < nrows; i++) {
     tableSet->AddAt(&row);
   }
   // ----------------- end of code ---------------
