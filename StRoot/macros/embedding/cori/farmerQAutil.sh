982a411e800c3494bfec40b660b287e52d80534c
Yuri Fisyak <fisyak@bnl.gov>
2020-04-21 09:54:00 -0400

    Merge
    Former-commit-id: bc63ba2606e770b3d113c6cfbf0fa671e4a53723

--- StRoot/macros/embedding/cori/farmerQAutil.sh
+++ StRoot/macros/embedding/cori/farmerQAutil.sh
@@ -17,41 +17,35 @@ echo Q-$NN  nHypCpu=$nCpu  and top
 free -g
 top ibn1
 
+# --- function rewritten for localDb model
+
 function queryDB {
-    dbName=$1
-    #echo query DB=$dbName
 
-    if [[ $SLURM_CLUSTER_NAME == "edison" ]] ; then
-	echo "no mysql on Edison, skip DB connection monitor"
-	return
+    targetDb=`grep "port=" dbLoadBalLocConf.xml | sed 's/\"/ /g' | awk '{print "-h "$3" -P"$5}'`
+
+    if [[ $targetDb =~ "db_server_ip" ]]
+    then
+        echo "db server not ready"
+    else
+        line=`mysql $targetDb  -e '  show processlist; '| wc -l`
+        nCon=$[ $line - 2 ]
+        echo "N connections = $nCon"
+        totDbCon=$[ $totDbCon + $nCon ]
     fi
-
-    targetDb=" -h $dbName -u$my_mstardb_name -p$my_mstardb_pass -P3316 "
-    #echo cerds=$targetDb
-    line=`mysql $targetDb  -e ' \! w |head -n1'`   
-    
-    #echo w-line=$line
-    #w1=`echo $line |head -1 | cut -f4 -d \, | cut -f2 -d:`
-
-    line=`mysql $targetDb  -e '  show processlist; '| wc -l`
-    nCon=$[ $line - 2 ]
-    echo $dbName,$nCon
-
-    totDbCon=$[ $totDbCon + $nCon ]
-
-    # max of 2 floats is more tricky
-    #if (( $(echo "$w1 > $topDbLoad" |bc -l) )); then
-   #	topDbLoad=$w1
-   #   fi
-
 }
 
+
+# --- obsolete in localDB model ----
+#
 # get credentials to STAR
-source ~/.ssh/mstardb-root.pass
+#source ~/.ssh/mstardb-root.pass
+#
+#echo Q-$NN test connection to DB-server 
+#
+#queryDB mstardb02
+#
+#-------- end obsolete ------
 
-echo Q-$NN test connection to DB-server 
-
-queryDB mstardb02
 
 nSleep=5
 totSec=0
@@ -75,12 +69,8 @@ while true ; do
     sleep $nSleep
 
     totDbCon=0
-   
-    for dbId in 02 03 ; do
-	name=mstardb$dbId
-	#echo $name
-	queryDB $name
-    done
+
+    queryDB
     echo  Q-$NN end of query over Dbs totDbCon=$totDbCon
     pwd
     ls -l
--- StRoot/macros/embedding/cori/starLocalDb.sh
+++ StRoot/macros/embedding/cori/starLocalDb.sh
@@ -2,8 +2,8 @@
 set -u ;  # exit  if you try to use an uninitialized variable
 
 NN=`hostname -f`
-line=`ifconfig | grep 'inet addr:10'`=
-myIP=`echo $line | cut -f2 -d: | cut -f1 -d\ `
+line=`/sbin/ifconfig | grep 'inet 10'`=
+myIP=`echo $line | cut -d ' ' -f2`
 echo D inShifter:`env|grep  SHIFTER_RUNTIME`
 
 echo  D-$NN myIP=$myIP   
@@ -16,11 +16,14 @@ echo  D-$NN myIP=$myIP
 # keep DB deamon alive until SLURM job dies
 du -hs  /mysqlVault 
 
+
 echo 'D-$NN  starting mysqld_safe ...'
-/usr/bin/mysqld_safe  --defaults-file=/mysqlVault/my.cnf &
+export LD_LIBRARY_PATH=/opt/rh/rh-mysql57/root/usr/lib64
+/opt/rh/rh-mysql57/root/usr/libexec/mysqld --defaults-file=/mysqlVault/my.cnf --secure-file-priv=$SCRATCH --tmpdir=/mysqlVault/tmp &
+
 
 echo "DB-$NN started mysqld_safe  "`date` 
-#ls -l /mysqlVault 
+ls -l /mysqlVault 
 
 sleep 10
 ps
@@ -32,21 +35,10 @@ echo D-$NN after IP was added
 cat  ${DBCONF_PATH}
 
 echo  D-$NN   list_mysql users
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'SELECT user, host FROM mysql.user;' 
+mysql --socket=/mysqlVault/mysql.sock -e 'SELECT user, host FROM mysql.user;' 
 
 echo  D-$NN   list_mysql tables
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'show databases'
-
-echo  D-$NN   mysql optimizing tables
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'OPTIMIZE TABLE `Calibrations_tpc`.`tpcAnodeHVavg`'
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'OPTIMIZE TABLE `Calibrations_tof`.`tofINLSCorr`'
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'OPTIMIZE TABLE `Calibrations_tof`.`tofTDIGOnTray`'
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'OPTIMIZE TABLE `Calibrations_tof`.`tofTotbCorr`'
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'OPTIMIZE TABLE `Calibrations_tof`.`tofZbCorr`'
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e 'OPTIMIZE TABLE `Calibrations_tof`.`tofTOffset`'
-
-echo  D-$NN check permission for the load balancing process
-mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan   -e 'show grants for "loadbalancer"@"%";'
+mysql --socket=/mysqlVault/mysql.sock -e 'show databases'
  
 echo  D-$NN   starDB stay alive forever or main program completes
 nSleep=30
@@ -55,7 +47,7 @@ while true ; do
     #ps -ef |grep mysqld
     sleep $nSleep
 
-    NL=`  mysql -u balewski --socket=/mysqlVault/mysql.sock -pjan -e ' show processlist;' | wc -l`
+    NL=`  mysql --socket=/mysqlVault/mysql.sock -e ' show processlist;' | wc -l`
     NC=$[ $NL - 2 ]
     echo  D-$NN   $totSec sec, nDbProc:   $NC  " "`date`
     totSec=$[ $totSec + $nSleep ]
