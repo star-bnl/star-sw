c2f536b52d2b571254463da9d5c6021c0f927806
Jason C. Webb <jwebb@bnl.gov>
2020-04-29 14:05:10 +0000

    Enable y2018b geometry in BFC

--- StRoot/StChain/GeometryDbAliases.h
+++ StRoot/StChain/GeometryDbAliases.h
@@ -142,6 +142,7 @@ static const DbAlias_t fDbAlias[] = {// geometry  Comment            old
   {"y2018x",      20171215,     0, "y2018x",   "y2018x experimental TPC geometry, AgML,xgeometry"},   
   {"y2018",       20171215,     2, "y2018",    "y2018 development geometry, AgML,xgeometry"},   
   {"y2018a",      20171215,     3, "y2018a",   "y2018a physics production geometry version A, AgML,xgeometry"},   
+  {"y2018b",      20171215,     4, "y2018b",   "y2018b physics production geometry version B, AgML,xgeometry"},   
 
   {"y2019",       20181215,     0, "y2019",    "y2019 development geometry, AgML,xgeometry"},   
   {"y2019a",      20181215,     1, "y2019a",   "y2019a first production release geometry, AgML,xgeometry"},   
