64150c1ccd617c5c27d55d5b830b387612f760eb
Yuri Fisyak <fisyak@bnl.gov>
2020-05-01 08:38:49 -0400

    Merge
    Former-commit-id: 305de7f877a4b6c0e58261b7616e080564f74406

--- StRoot/StChain/GeometryDbAliases.h
+++ StRoot/StChain/GeometryDbAliases.h
@@ -180,6 +180,8 @@ static const DbAlias_t fDbAlias[] = {// geometry  Comment            old
 //{"y2018a",      20171215,     3, "y2018a",   "y2018a physics production geometry version A, AgML,xgeometry"},//  not read yet
 //{"r2018",       20171220,     0, "y2018a",   "y2018a, no HFT,xgeometry"}, // 
   {"r2018",       20171220,     0, "y2018",    "y2018, no HFT,xgeometry"}, // 
+  {"y2018a",      20171215,     3, "y2018",    "y2018a physics production geometry version A, AgML,xgeometry"},   
+  {"y2018b",      20171215,     4, "y2018",    "y2018b physics production geometry version B, AgML,xgeometry"},   
   {"y2019",       20181210,     0, "y2019",    "y2019, iTPC, Jim Thomas 05/31/16, no HFT,xgeometry"}, // 
   {"y2019",       20181215,     0, "y2019",    "y2019 development geometry, AgML,xgeometry"},   
   {"y2019a",      20181215,     1, "y2019",    "y2019a first production release geometry, AgML,xgeometry"},   
