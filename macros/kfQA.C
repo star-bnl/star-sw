76018c99963a030af413296857b9744fef5d0b1c
Yuri Fisyak <fisyak@bnl.gov>
2020-07-08 11:04:51 -0400

    Add merging histograms from file with the run
    Former-commit-id: 5e9154c0e55e15c290c68c98995e991a953509b8

--- macros/kfQA.C
+++ macros/kfQA.C
@@ -1,6 +1,6 @@
 /* 
    FPE_OFF
-   root.exe -q -b lBichsel.C dEdxFit.C+  'kfQA.C+("J/dEdx/*.root","kfQAdEdx.J.root")'
+   root.exe -q -b lBichsel.C dEdxFit.C+  'kfQA.C+("J/dev","kfQAdev.P.root")'
 */
 #if !defined(__CINT__)
 // code that should be seen ONLY by the compiler
@@ -15,6 +15,8 @@
 //#define __DRAW__
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include "Riostream.h"
+#include <map>
+#include <vector>
 #include <stdio.h>
 #include "TROOT.h"
 #include "TSystem.h"
@@ -31,7 +33,9 @@
 #include "TFile.h"
 #include "TNtuple.h"
 #include "TCanvas.h"
-#include "TDirIter.h"
+//#include "TDirIter.h"
+#include "TSystemDirectory.h"
+#include "TSystemFile.h"
 #include "TObjArray.h"
 #include "brtw.C"
 #endif
@@ -80,7 +84,7 @@ static const Hist_t Histos[] = {
 
 }; 
 //________________________________________________________________________________
-void kfQA(const Char_t *files = "/gpfs01/star/subsys-tpc/fisyak/kfp/2020/11p5GeV.C/I/dEdx/21042030_1.root", const Char_t *Out = 0){
+void kfQA(const Char_t *path2dir = "/gpfs01/star/subsys-tpc/fisyak/kfp/2020/9p2GeVc/J/dev", const Char_t *Out = 0){
   const Int_t N = sizeof(Histos)/sizeof(Hist_t); cout << " N " << N << endl;
 #if 0
   if (gClassTable->GetID("StBichsel") < 0) {
@@ -126,8 +130,6 @@ void kfQA(const Char_t *files = "/gpfs01/star/subsys-tpc/fisyak/kfp/2020/11p5GeV
   Int_t Run = 0;
   Int_t nFile = 0;
   TString title;
-  TDirIter Dir(files);
-  const Char_t *file = 0;
   TF1 *gaus = 0;
   TF1 *pol4 = 0;
   TF1 *gp = 0;
@@ -135,129 +137,175 @@ void kfQA(const Char_t *files = "/gpfs01/star/subsys-tpc/fisyak/kfp/2020/11p5GeV
   TH1 *proj = 0;
   TH2 *h2 = 0;
   Int_t no = 0;
-  while ( (file = Dir.NextFile()) ) {
-    TString File = file;
-    cout << "File::Name:" << File.Data() << endl;
+  Int_t noRuns = 0;
+  //  TDirIter Dir(files);
+  //  const Char_t *file = Dir.NextFile();
+  TSystemDirectory Dir("",path2dir);
+  TList *files = Dir.GetListOfFiles();
+  if (! files) return;
+  TIter next(files);
+  TSystemFile *file = 0;
+  multimap<Int_t,TString> fileMap;
+  while ((file = (TSystemFile *) next())) {
+    TString File(file->GetName());
     if (! File.EndsWith(".root")) continue;
+    //    cout << file->GetName() << "\t" << file->GetTitle() << endl;
+    sscanf(File.Data(),"%i_",&run); 
+    TString path( file->GetTitle());
+    path += "/";
+    path += file->GetName();
+    fileMap.insert(pair<Int_t,TString>(run,path));
+    nFile++;
+  }
+#if 0
+  for (std::multimap<Int_t,TString>::iterator it=fileMap.begin(); it!=fileMap.end(); ++it) {
+    std::cout << (*it).first << " => " << (*it).second.Data() << '\n';
+  }
+#endif
+  // Unique keys
+  for( auto it = fileMap.begin(), end = fileMap.end();
+       it != end;
+       it = fileMap.upper_bound(it->first)) {
+    run = it->first;
+    cout << run << ' ' << it->second.Data() << endl;
+    std::pair <std::multimap<Int_t,TString>::iterator, std::multimap<Int_t,TString>::iterator> ret;
+    ret = fileMap.equal_range(run);
+    std::cout << run << " =>";
+    vector<TFile *> TFiles;
+    for (std::multimap<Int_t,TString>::iterator itt=ret.first; itt!=ret.second; ++itt) {
+      std::cout << ' ' << itt->second.Data();
+      cout << "\tOpen " <<  itt->second.Data() << endl;
+      TFile *f = new TFile(itt->second.Data());
+      if (f) TFiles.push_back(f);
+      else {cout << "\t====================== failed " << endl;}
+    }
+    //    std::cout << '\n';
+    if (! TFiles.size()) continue;
     no++;
-    cout << "Open " <<  File;
-    TFile *f = new TFile(File.Data());
-    if (! f) {cout << "====================== failed " << endl; return; continue;}
-    f->cd();
-    TString F = gSystem->BaseName(File);
-    sscanf(F.Data(),"%i_",&run); 
     Run = run%1000000;
-    cout << " for Run " << run << "/" << Run << "\t" << File << "\t" << nFile++ << endl;
     Float_t params[100] = {0};
     params[0] = run;
     params[1] = Run;
     params[2] = no;
-    TH1F *hist = (TH1F *) gDirectory->Get(Histos[2].Path);
-    if (hist) {
+    TH1F *hist = 0;
+    for (Int_t i = 0; i < N; i++) {
+      hist = 0;
+      for (auto f : TFiles) {
+	if (! hist) {
+	  hist = (TH1F *) f->Get(Histos[i].Path);
+	  hist->SetDirectory(0);
+	} else {
+	  TH1F *h = (TH1F *) f->Get(Histos[i].Path);
+	  if (h) hist->Add(h);
+	}
+      } 
+      Int_t p = 0;
+      TObjArray* arr = 0;
+      Double_t binWidth;
+      Double_t S;
+      if (! hist) goto CLOSEFILES;
       params[3] = hist->GetEntries();
-      if (params[3] > 100) {
-	Int_t p = 4;
-	for (Int_t i = 0; i < N; i++) {
-	  TH1F *hist = (TH1F *) gDirectory->Get(Histos[i].Path);
-	  if (! hist) {
-	    cout << "Histograms " << Histos[i].Path << " is not found." << endl;
-	  }
-	  for (Int_t j = 0; j < NTVar; j++) {
-	    if ((1 << j) &  Histos[i].opt) {
-	      params[p] = 0;
-	      params[p+1] = 0;
-	      if (hist && hist->GetEntries() > 0) {
-		switch (j+1) {
-		case 1: // mean
-		  params[p] = hist->GetMean(); 
-		  break;
-		case 2: // RMS
-		  params[p] = hist->GetRMS();
-		  break;
-		case 3: // mu
-		case 4: // sigma
-		  hist->Fit("gaus");
-		  gaus = (TF1 *) hist->GetListOfFunctions()->FindObject("gaus");
-		  if (gaus) {
-		    params[p] = gaus->GetParameter(1);
-		    params[++p] = gaus->GetParameter(2);
+      if (params[3] < 100) goto CLOSEFILES;
+      p = 4;
+      for (Int_t j = 0; j < NTVar; j++) {
+	if ((1 << j) &  Histos[i].opt) {
+	  params[p] = 0;
+	  params[p+1] = 0;
+	  if (hist && hist->GetEntries() > 0) {
+	    switch (j+1) {
+	    case 1: // mean
+	      params[p] = hist->GetMean(); 
+	      break;
+	    case 2: // RMS
+	      params[p] = hist->GetRMS();
+	      break;
+	    case 3: // mu
+	    case 4: // sigma
+	      hist->Fit("gaus");
+	      gaus = (TF1 *) hist->GetListOfFunctions()->FindObject("gaus");
+	      if (gaus) {
+		params[p] = gaus->GetParameter(1);
+		params[++p] = gaus->GetParameter(2);
 #ifdef __DRAW__
-		    TCanvas *c = new TCanvas(Histos[i].Path,Histos[i].Path);
-		    hist->Draw();
-		    c->Update();
+		TCanvas *c = new TCanvas(Histos[i].Path,Histos[i].Path);
+		hist->Draw();
+		c->Update();
 #endif
-		  } else {
-		    ++p;
-		  }
-		  j++; 
-		  break;
-		case 5:  // sg10
-		case 6:  // sg100
-		  if (hist->GetDimension() == 2) {
-		    h2 = (TH2 *) hist;
-		    TObjArray* arr =  new TObjArray(4);
-		    h2->FitSlicesY(0, 0, -1, 0, "QNR", arr);
-		    TH1* mu = (TH1 *) (*arr)[1];
-		    mu->Fit("pol4");
-		    pol4 = (TF1 *) mu->GetListOfFunctions()->FindObject("pol4");
-		    if (pol4) {
-		      params[p] = pol4->Eval(1);
-		      params[++p] = pol4->Eval(2);
-		    } else {
-		      ++p;
-		    }
-		    delete arr;
-		    j++;
-		  }
-		  break;
-		case 7: // muGF
-		case 8: // sigmaGP
-		  h2 = (TH2 *) hist;
-		  proj = h2->ProjectionY();
-		  gp = FitGP(proj);
-		  if (gp) {
-#ifdef __DRAW__
-		    TCanvas *c = new TCanvas(Histos[i].Path,Histos[i].Path);
-		    proj->Draw();
-		    c->Update();
-#endif
-		    params[p] = gp->GetParameter(1);
-		    params[++p] = gp->GetParameter(2);
-		  }
-		  j++;
-		  break;
-		case 9: // M
-		case 10: // Gamma
-		  BW = K0BW(hist);
-		  if (BW) {
-		    params[p] = BW->GetParameter(1);
-		    params[p+1] = BW->GetParameter(2);
-		    params[p+2] = 1./BW->GetParError(0);
-		    Double_t binWidth = hist->GetBinWidth(1);
-		    Double_t S = BW->Integral(params[p]-3*params[p+1],params[p]+3*params[p+1])/binWidth;
-		    params[p+3] = S/params[3];
-		    p += 3;
-		    j += 3;
-#ifdef __DRAW__
-		    TCanvas *c = new TCanvas(Histos[i].Path,Histos[i].Path);
-		    hist->Draw();
-		    c->Update();
-#endif
-		  }
-		  break;
-		default: 
-		  break;
-		}
-		p++;
+	      } else {
+		++p;
 	      }
+	      j++; 
+	      break;
+	    case 5:  // sg10
+	    case 6:  // sg100
+	      if (hist->GetDimension() == 2) {
+		h2 = (TH2 *) hist;
+		arr =  new TObjArray(4);
+		h2->FitSlicesY(0, 0, -1, 0, "QNR", arr);
+		TH1* mu = (TH1 *) (*arr)[1];
+		mu->Fit("pol4");
+		pol4 = (TF1 *) mu->GetListOfFunctions()->FindObject("pol4");
+		if (pol4) {
+		  params[p] = pol4->Eval(1);
+		  params[++p] = pol4->Eval(2);
+		} else {
+		  ++p;
+		}
+		delete arr;
+		j++;
+	      }
+	      break;
+	    case 7: // muGF
+	    case 8: // sigmaGP
+	      h2 = (TH2 *) hist;
+	      proj = h2->ProjectionY();
+	      gp = FitGP(proj);
+	      if (gp) {
+#ifdef __DRAW__
+		TCanvas *c = new TCanvas(Histos[i].Path,Histos[i].Path);
+		proj->Draw();
+		c->Update();
+#endif
+		params[p] = gp->GetParameter(1);
+		params[++p] = gp->GetParameter(2);
+	      }
+	      j++;
+	      break;
+	    case 9: // M
+	    case 10: // Gamma
+	      BW = K0BW(hist);
+	      if (BW) {
+		params[p] = BW->GetParameter(1);
+		params[p+1] = BW->GetParameter(2);
+		params[p+2] = 1./BW->GetParError(0);
+		binWidth = hist->GetBinWidth(1);
+		S = BW->Integral(params[p]-3*params[p+1],params[p]+3*params[p+1])/binWidth;
+		params[p+3] = S/params[3];
+		p += 3;
+		j += 3;
+#ifdef __DRAW__
+		TCanvas *c = new TCanvas(Histos[i].Path,Histos[i].Path);
+		hist->Draw();
+		c->Update();
+#endif
+	      }
+	      break;
+	    default: 
+	      break;
 	    }
+	    p++;
 	  }
 	}
       }
     }
     QA->Fill(params);
+  CLOSEFILES:
+    for (auto f : TFiles) {
+      delete f;
+    }
 #ifndef __DRAW__
-    delete f;
+    delete hist;
+    hist = 0;
 #endif
   }
   fout->cd();
