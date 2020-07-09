2f95cb3084dbc495f65789c7351da1a74a27c60e
cvs2git <cvs2git>
2020-07-09 19:44:08 +0000

    This commit was manufactured by cvs2git to create tag 'Comis64_0'.

--- StarDb/Calibrations/tpc/tpcPadrowT0.C
+++
@@ -1,8 +0,0 @@
-TDataSet *CreateTable() { 
-  if (!gROOT->GetClass("St_tpcPadrowT0")) return 0;
-  tpcPadrowT0_st row;
-  St_tpcPadrowT0 *tableSet = new St_tpcPadrowT0("tpcPadrowT0",24);
-  memset(&row,0,tableSet->GetRowSize());
-  for (Int_t s = 1; s <= 24; s++) tableSet->AddAt(&row);
-  return (TDataSet *)tableSet;
-}
--- StarVMC/StarAgmlLib/AgMLExtension.cxx
+++
@@ -1,6 +0,0 @@
-#include "AgMLExtension.h"
-
-AgMLExtension::AgMLExtension() : TGeoRCExtension(), mModuleName("none"), mFamilyName("none"), mVolumeName("none"), mSensitive(0), mTracking(0)
-{
-  Grab();
-}
