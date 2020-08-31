01814a55dd2fa306a158e48ce7d40d7c78968426
Yuri Fisyak <fisyak@bnl.gov>
2020-08-31 16:14:42 -0400

    Clean up
    Former-commit-id: fd738ca27a9230ae8538e42a6a6b0e0e26ab8083

--- StRoot/StarClassLibrary/StHelix.cc
+++ StRoot/StarClassLibrary/StHelix.cc
@@ -477,7 +477,7 @@ double StHelix::pathLength(const StThreeVector<double>& r,
 	
 	double a, f, fp;
 	double sOld = s = 0;  
-	double shiftOld = 0;
+	//	double shiftOld = 0;
 	double shift;
 //		(cos(angMax)-1)/angMax = 0.1
         const double angMax = 0.21;
@@ -507,7 +507,7 @@ double StHelix::pathLength(const StThreeVector<double>& r,
                shift = f/fp;
             }
 	    s -= shift;
-	    shiftOld = shift;
+	    //	    shiftOld = shift;
 	    if (fabs(sOld-s) < MaxPrecisionNeeded) break;
 	    sOld = s;
 	}
