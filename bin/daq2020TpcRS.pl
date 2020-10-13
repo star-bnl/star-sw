88d12006fdecea6f3a6cbd20df4aad2789bcf62f
Yuri Fisyak <fisyak@bnl.gov>
2020-10-13 16:23:31 -0400

    Freeze
    Former-commit-id: fa3d00c44be3cfffbb9fcde605868f6817daf9b6

--- bin/daq2020TpcRS.pl
+++ bin/daq2020TpcRS.pl
@@ -8,7 +8,7 @@ my $pwd = cwd();
 #my $Trigger =  File::Basename::basename($pwd); $Trigger =~ s/TpcRS_/daq_/; $Trigger =~ s/\..*//; print "Trigger = $Trigger\n" if ($debug);
 #my $glob = "/net/l401/data/scratch1/daq/2020/" . $Trigger . "/st_physics_adc*.daq";  print "glob = $glob\n" if ($debug);
 #my $glob = "/net/l401/data/scratch1/fisyak/Tpc/TpcRS/" . $Trigger . "/st_physics_adc*.MuDst.root";  print "glob = $glob\n" if ($debug);
-my $Trigger =  File::Basename::basename($pwd); $Trigger =~ s/TpcRS_//; print "Trigger = $Trigger\n" if ($debug);
+my $Trigger =  File::Basename::basename($pwd); $Trigger =~ s/TpcRS_//; $Trigger =~ s/\..*$//;print "Trigger = $Trigger\n" if ($debug);
 my $fNo = 0;
 my $glob = "../" . $Trigger . "/*.MuDst.root"; print "glob = $glob\n" if ($debug);
 my @globs = glob $glob; print "globs = @globs\n" if ($debug);
