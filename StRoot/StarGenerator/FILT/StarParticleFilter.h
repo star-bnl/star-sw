c19d7dc1005810aa122c0117cb6803e950fc4c15
Jason C. Webb <jwebb@bnl.gov>
2020-11-20 18:08:12 +0000

    Limit consideration to final state particles, and allow user to specify parent of trigger particle(s).

--- StRoot/StarGenerator/FILT/StarParticleFilter.h
+++ StRoot/StarGenerator/FILT/StarParticleFilter.h
@@ -25,7 +25,8 @@ public:
   /// @param _ptmx Maximum pT of particle [GeV].  If < _ptmn, no maximum is set.
   /// @param _etamn Minimum eta of particle
   /// @param _etamx Maximum eta of particle 
-  void AddTrigger( int _pdgid, double _ptmn=0, double _ptmx=-1, double _etamn=-1, double _etamx=1 );
+  /// @param _pdgidParent The PDG id of the parent particle (0 accepts any)
+  void AddTrigger( int _pdgid, double _ptmn=0, double _ptmx=-1, double _etamn=-1, double _etamx=1, int _pdgidParent=0 );
 
 private:
 protected:
@@ -36,27 +37,29 @@ protected:
     double ptmx;
     double etamn;
     double etamx;
+    int    pdgid_parent;
     Trigger_t() :
       pdgid(0), 
       ptmn (0.), 
       ptmx (0.), 
       etamn(0.), 
-      etamx(0.) 
+      etamx(0.),
+      pdgid_parent(0)
     { };
-    Trigger_t( int _pdgid, double _ptmn, double _ptmx, double _etamn, double _etamx ) : 
+    Trigger_t( int _pdgid, double _ptmn, double _ptmx, double _etamn, double _etamx, int _pdgid_parent ) : 
       pdgid(_pdgid), 
       ptmn (_ptmn), 
       ptmx (_ptmx), 
       etamn(_etamn), 
-      etamx(_etamx) 
+      etamx(_etamx),
+      pdgid_parent(_pdgid_parent)
     {
     };
   };
   vector<Trigger_t> mTriggers;
 
-
-#if 0 // we dont really need, but this triggers cons to create dictionary
-  ClassDef(StarParticleFilter,1);
+#if 1 // we dont really need, but this triggers cons to create dictionary
+  ClassDef(StarParticleFilter,0);
 #endif
 
 };
