894bad14127b9fa80f071e0610da9340de510dfe
Jason C. Webb <jwebb@bnl.gov>
2020-11-30 18:21:58 +0000

    Improve volume/shape sanity check... no need to check shapeless assembly volumes.

--- StarVMC/StarAgmlLib/StarTGeoStacker.cxx
+++ StarVMC/StarAgmlLib/StarTGeoStacker.cxx
@@ -838,22 +838,33 @@ TGeoVolume *makeCopyVolume( TGeoVolume *org, AgShape shape, Bool_t copyDaughters
 //
 Bool_t sanityCheck( TGeoVolume *volume )
 {
-  
-  TGeoShape *shape = volume->GetShape();
-  
-  // Shape with volume=0 is probably an error
-  if ( shape->Capacity() <= 0. )
-    { Int_t old=gErrorIgnoreLevel;    
-      gErrorIgnoreLevel=1; // Some warnings will not be supressed
-      AgBlock::module()->Warning(AgModule::module()->GetName(),Form("Volume %s shape parameters invalid.  NULL bounding box capacity.",volume->GetName()));
-      //      volume -> Print();
-      //      volume -> InspectShape();
-      gErrorIgnoreLevel=old;
-      return false;
+
+  bool result = false;
+
+  if ( volume->IsAssembly() ) {
+    result = true;
+  } 
+  else { 
+
+    TGeoShape *shape = volume->GetShape();
+    if ( 0==shape ) { 
+      result = false;
     }
+    else { 
+      if ( shape->Capacity() > 0 ) {
+	result = true;
+      }
+      else {
+	result = false;
+      }
+    }
+  }
 
+  if ( result == false ) {
+    LOG_WARN << "Invalid shape for volume " << volume->GetName() << endm;
+  }
 
-  return true;
+  return result;
 }
 
 
