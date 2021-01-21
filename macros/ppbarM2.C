4cb2581c0ab3f26d9a8e251e380965d7ed78b615
Yuri Fisyak <fisyak@bnl.gov>
2021-01-21 09:46:08 -0500

    use ratio to M2
    Former-commit-id: 4e46155bf4c938bf04df2ce340bb9aff8b84c3f5

--- macros/ppbarM2.C
+++ macros/ppbarM2.C
@@ -14,6 +14,10 @@
 #endif
 void ppbarM2() {
   //  gStyle->SetOptStat(0);
+  TF1 *func = new TF1("func","[0]*(1+[1]+[2]*x)",-0.5,0.5);
+  Double_t m2P = 0.9382723*0.9382723;
+  func->SetParameters(0,0,0);
+  func->FixParameter(0,m2P);
   TLegend *l = new TLegend(0.2,0.2,0.6,0.4);
   TH2* p2 = (TH2 *) gDirectory->Get("/Tracks/p/hTofPID");
   if (! p2) return;
@@ -22,10 +26,10 @@ void ppbarM2() {
   TH1D *m2p = (TH1D *) (*arr)[1]; m2p->SetMarkerColor(1); m2p->SetMarkerStyle(20);
   m2p->SetName("m2p");
   m2p->SetStats(0);
-  m2p->Fit("pol1");
-  TF1 *pol1 = (TF1*) m2p->GetListOfFunctions()->FindObject("pol1");
-  if (pol1) l->AddEntry(m2p,Form("p  : %6.4f + %6.4f log_{10}P",pol1->GetParameter(0), pol1->GetParameter(1)));
-  else      l->AddEntry(m2p,"p");
+  m2p->Fit("func");
+  //  TF1 *func = (TF1*) m2p->GetListOfFunctions()->FindObject("func");
+  TString Title = Form("p   : %6.4f + %6.4f log_{10}P(%%)",100*func->GetParameter(1), 100*func->GetParameter(2));
+  l->AddEntry(m2p,Title);
 #if 1
   TH2* pbar2 = (TH2* )gDirectory->Get("/Tracks/p-/hTofPID");
   TObjArray* arr2 =  new TObjArray(4);
@@ -33,11 +37,12 @@ void ppbarM2() {
   TH1D *m2pbar = (TH1D *) (*arr2)[1]; m2pbar->SetMarkerColor(2); m2pbar->SetMarkerStyle(20);
   m2pbar->SetName("m2pbar");
   m2pbar->SetStats(0);
-  m2pbar->Fit("pol1");
-  pol1 = (TF1*) m2pbar->GetListOfFunctions()->FindObject("pol1");
-  if (pol1) l->AddEntry(m2pbar,Form("pbar: %6.4f + %6.4f log_{10}P",pol1->GetParameter(0), pol1->GetParameter(1)));
-  else      l->AddEntry(m2pbar,"pbar");
-  Double_t m2P = 0.9382723*0.9382723;
+  m2pbar->Fit("func");
+  //  func = (TF1*) m2pbar->GetListOfFunctions()->FindObject("func");
+  cout << Title.Data() << endl;
+  Title = Form("pbar: %6.4f + %6.4f log_{10}P(%%)",100*func->GetParameter(1), 100*func->GetParameter(2));
+  cout << Title.Data() << endl;
+  l->AddEntry(m2pbar,Title);
   TLine *line = new TLine(-0.5, m2P, 0.5, m2P);
 #endif
   TCanvas *c1 = (TCanvas *) gROOT->GetListOfCanvases()->FindObject("c1");
