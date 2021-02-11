282a6e8a83132fd18c6bf231794d92a56aa50b89
evpops <>
2021-02-11 04:31:12 +0000

    add fit to VertexXZ and VertexYZ plots

--- OnlTools/Jevp/StJevpBuilders/l4Builder.cxx
+++ OnlTools/Jevp/StJevpBuilders/l4Builder.cxx
@@ -491,6 +491,36 @@ void l4Builder::stoprun(daqReader *rdr)
 
 	hHLTGood2VertexZ->Fit("gaus", "", "", -150, 150);
 
+        // 47
+	TProfile* hVertexXZ_pfx = hVertexXZ->ProfileX("_pfx", 41, 60);
+        HltPlots[47]->addHisto(hVertexXZ_pfx);
+        TF1* hVertexXZ_pfx_fitfunc = new TF1("hVertexXZ_pfx_fitfunc", "pol1", -200, 200);
+        hVertexXZ_pfx_fitfunc->SetLineColor(kRed);
+        hVertexXZ_pfx_fitfunc->SetLineWidth(0.5);
+        hVertexXZ_pfx->Fit(hVertexXZ_pfx_fitfunc);
+
+        JLatex* hVertexXZ_pfx_fit_res = new JLatex(-150, 2, "");
+	hVertexXZ_pfx_fit_res->SetTextSize(0.05);
+	hVertexXZ_pfx_fit_res->SetText(TString::Format("Vx = %f + %f Vz",
+                                                 hVertexXZ_pfx_fitfunc->GetParameter(0),
+                                                 hVertexXZ_pfx_fitfunc->GetParameter(1)).Data());
+	HltPlots[47]->addElement(hVertexXZ_pfx_fit_res);
+
+        // 48
+	TProfile* hVertexYZ_pfx = hVertexYZ->ProfileX("_pfx", 41, 60);
+        HltPlots[48]->addHisto(hVertexYZ_pfx);
+        TF1* hVertexYZ_pfx_fitfunc = new TF1("hVertexYZ_pfx_fitfunc", "pol1", -200, 200);
+        hVertexYZ_pfx_fitfunc->SetLineColor(kRed);
+        hVertexYZ_pfx_fitfunc->SetLineWidth(0.5);
+        hVertexYZ_pfx->Fit(hVertexYZ_pfx_fitfunc);
+
+        JLatex* hVertexYZ_pfx_fit_res = new JLatex(-150, 2, "");
+	hVertexYZ_pfx_fit_res->SetTextSize(0.05);
+	hVertexYZ_pfx_fit_res->SetText(TString::Format("Vy = %f + %f Vz",
+                                                 hVertexYZ_pfx_fitfunc->GetParameter(0),
+                                                 hVertexYZ_pfx_fitfunc->GetParameter(1)).Data());
+	HltPlots[48]->addElement(hVertexYZ_pfx_fit_res);
+
 	float low = -13.12;
 	float high = -12.8;
 	TF1 *fit = new TF1("fit", "gaus", low, high);
