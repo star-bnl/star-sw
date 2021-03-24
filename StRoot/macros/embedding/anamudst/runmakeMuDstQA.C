4c1f03a8d0766192ae97541dc915ca764f5dd483
cvs2git <cvs2git>
2021-03-24 10:30:19 +0000

    This commit was manufactured by cvs2git to create tag 'SL21b'.

--- StRoot/macros/embedding/anamudst/runmakeMuDstQA.C
+++
@@ -1,9 +0,0 @@
-//wrapper to makeMuDstQA.C
-//for ACLiC mode
-void runmakeMuDstQA(TString InputFileList,Int_t nfiles,Int_t nevents,TString OutputFile)
-{
-   gROOT->Reset();
-   gROOT->Macro("loadMuDst.C");
-   gROOT->LoadMacro("makeMuDstQA.C+");
-   makeMuDstQA(InputFileList,nfiles,nevents,OutputFile); 
-}
