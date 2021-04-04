26466d213811cc2e6c9db76d5867da7bf693a401
Yuri Fisyak <fisyak@bnl.gov>
2021-04-04 13:19:07 -0400

    Freeze
    Former-commit-id: dc4ed2ceaf11b253f7cd19ffa06fe26db68f3880

---
+++ StRoot/StDetectorDbMaker/St_starTriggerDelayC.h
@@ -0,0 +1,22 @@
+#ifndef St_starTriggerDelayC_h
+#define St_starTriggerDelayC_h
+
+#include "TChair.h"
+#include "tables/St_starTriggerDelay_Table.h"
+
+class St_starTriggerDelayC : public TChair {
+ public:
+  static St_starTriggerDelayC* 	instance();
+  starTriggerDelay_st 	*Struct(Int_t i = 0) 	const {return ((St_starTriggerDelay*) Table())->GetTable()+i;}
+  UInt_t     	getNumRows()                	const {return GetNRows();}
+  Float_t 	clocks(Int_t i = 0) 	const {return Struct(i)->clocks;}
+  Float_t 	tZero(Int_t i = 0) 	const {return Struct(i)->tZero;}
+ protected:
+  St_starTriggerDelayC(St_starTriggerDelay *table=0) : TChair(table) {}
+  virtual ~St_starTriggerDelayC() {if (Table()->IsMarked()) delete GetThisTable(); fgInstance = 0;}
+ private:
+  static St_starTriggerDelayC* fgInstance;
+  ClassDefChair(St_starTriggerDelay, starTriggerDelay_st )
+  ClassDef(St_starTriggerDelayC,1) //C++ TChair for starTriggerDelay table class
+};
+#endif
