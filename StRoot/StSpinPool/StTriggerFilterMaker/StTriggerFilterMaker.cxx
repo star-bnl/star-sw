5bf41552475f89a5a6bae685c6531fa6d5085ea7
Akio Ogawa <akio@bnl.gov>
2021-04-19 18:49:54 +0000

    Getting info from StEvent as well when not reading from Mudst file

--- StRoot/StSpinPool/StTriggerFilterMaker/StTriggerFilterMaker.cxx
+++ StRoot/StSpinPool/StTriggerFilterMaker/StTriggerFilterMaker.cxx
@@ -1,8 +1,9 @@
-// $Id: StTriggerFilterMaker.cxx,v 1.2 2015/09/09 20:29:39 akio Exp $
+// $Id: StTriggerFilterMaker.cxx,v 1.3 2021/04/19 18:49:54 akio Exp $
 
 #include "StTriggerFilterMaker.h"
 #include "StMuDSTMaker/COMMON/StMuDst.h"
 #include "StMuDSTMaker/COMMON/StMuEvent.h"
+#include "StRoot/StEvent/StEvent.h"
 
 ClassImp(StTriggerFilterMaker)
 
@@ -20,29 +21,46 @@ Int_t StTriggerFilterMaker::Init() {
 }
 
 Int_t StTriggerFilterMaker::Make() {
-  if(mPrint){
-    vector<unsigned int> ids = StMuDst::event()->triggerIdCollection().nominal().triggerIds();
-    LOG_INFO << "Offline Trigger Id = ";
-    for(unsigned i=0; i<ids.size(); ++i) LOG_INFO << ids[i] << " ";
-  }
-  for(unsigned i=0; i<mVetoTriggers.size(); ++i) {
-    if(StMuDst::event()->triggerIdCollection().nominal().isTrigger(mVetoTriggers[i])) {
-      if(mPrint)  LOG_INFO << " Veto" << endm;
-      return kStSkip;
+    StTriggerIdCollection* trgIdColl;
+    StEvent* event= (StEvent*)GetInputDS("StEvent");
+    if(event){
+	trgIdColl = event->triggerIdCollection();
+    }else{
+	if(StMuDst::event()){
+	    trgIdColl = event->triggerIdCollection();
+	}else{
+	    LOG_INFO << "No StEvent nor Mudst" << endm;
+	}	
     }
-  }
-  for(unsigned i=0; i<mGoodTriggers.size(); ++i) {
-    if(StMuDst::event()->triggerIdCollection().nominal().isTrigger(mGoodTriggers[i])) {
-      if(mPrint)   LOG_INFO << " Accept" << endm;
-      return kStOk;
+    if(trgIdColl){
+	if(mPrint){
+	    vector<unsigned int> ids = trgIdColl->nominal()->triggerIds();
+	    LOG_INFO << "Offline Trigger Id = ";
+	    for(unsigned i=0; i<ids.size(); ++i) LOG_INFO << ids[i] << " ";
+	}
+	for(unsigned i=0; i<mVetoTriggers.size(); ++i) {
+	    if(trgIdColl->nominal()->isTrigger(mVetoTriggers[i])) {
+		if(mPrint)  LOG_INFO << " Veto" << endm;
+		return kStSkip;
+	    }
+	}
+	for(unsigned i=0; i<mGoodTriggers.size(); ++i) {
+	    if(trgIdColl->nominal()->isTrigger(mGoodTriggers[i])) {
+		if(mPrint)   LOG_INFO << " Accept" << endm;
+		return kStOk;
+	    }
+	}
+
     }
-  }
-  if(mPrint)    LOG_INFO << " Skip" << endm;
-  return kStSkip;
+    if(mPrint) LOG_INFO << "Skip" << endm;
+    return kStSkip;
 }
 
 /*****************************************************************************
  * $Log: StTriggerFilterMaker.cxx,v $
+ * Revision 1.3  2021/04/19 18:49:54  akio
+ * Getting info from StEvent as well when not reading from Mudst file
+ *
  * Revision 1.2  2015/09/09 20:29:39  akio
  * Adding Vetoing TriggerId
  * Also adding printing if option is set
