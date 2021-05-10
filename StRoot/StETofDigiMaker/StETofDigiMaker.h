f569c93316feb2e90d79dc6d740b5cc129273772
weidenkaff <>
2021-05-10 10:45:30 +0000

    Added unpacking of status pattern to be written to Event header. PW.StETofDigiMaker.cxx

--- StRoot/StETofDigiMaker/StETofDigiMaker.h
+++ StRoot/StETofDigiMaker/StETofDigiMaker.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * $Id: StETofDigiMaker.h,v 1.3 2019/02/19 20:32:05 fseck Exp $
+ * $Id: StETofDigiMaker.h,v 1.4 2021/05/10 10:45:13 weidenkaff Exp $
  *
  * Author: Florian Seck, April 2018
  ***************************************************************************
@@ -11,6 +11,9 @@
  ***************************************************************************
  *
  * $Log: StETofDigiMaker.h,v $
+ * Revision 1.4  2021/05/10 10:45:13  weidenkaff
+ * Added unpacking of status pattern to be written to Event header. PW.StETofDigiMaker.cxx
+ *
  * Revision 1.3  2019/02/19 20:32:05  fseck
  * update for unpacking year 2019+ daq files
  *
@@ -26,11 +29,14 @@
 #define STETOFDIGIMAKER_H
 
 #include <stdint.h>
+#include <string>
+#include <vector>
 #include "StRTSBaseMaker.h"
 
 class StEvent;
 class StETofCollection;
 class StETofDigi;
+class StETofHardwareMap;
 
 namespace gdpb { class FullMessage; };
 namespace gdpbv100 { class FullMessage; };
@@ -66,6 +72,8 @@ public:
     void   fillETofHeader( uint64_t* messageBuff, vector< gdpbv100::FullMessage >& triggerMessages );
     void   fillETofDigi( gdpbv100::FullMessage& mess );
 
+    void setFileNameElectronicsMap(     const char* fileName );
+
     // methods for processing the data in 2018
     void   processEvent2018( uint64_t* messageBuffer, size_t nFullMessagesToRead );
 
@@ -81,11 +89,17 @@ private:
     StEvent*             mEvent;
     StETofCollection*    mETofCollection;
     Int_t                mRunYear;
+
+	 vector < Bool_t > 			 mMissMatchFlagVec;
+    StETofHardwareMap*   		 mHwMap;         // electronic channel to hardware/geometry map
+    std::string   				 mFileNameElectronicsMap;      // name of parameter file for electronics-to-hardware map
+    std::map< Int_t, Bool_t >  mGet4ActiveMap;               // map of get4Id that are actually mapped to existing channels.
+
     Bool_t               mDebug;
 
     virtual const Char_t *GetCVS() const { static const char cvs[]="Tag $Name:  $Id: built " __DATE__ " " __TIME__ ; return cvs; }
 
-    ClassDef( StETofDigiMaker, 0 )
+    ClassDef( StETofDigiMaker, 1 )
 };
 
 inline void StETofDigiMaker::setDebug( const bool debug )  { mDebug = debug; }
