9e58d6b25cfb2dd5f37e522cdd626b8ecdab74d1
Yuri Fisyak <fisyak@bnl.gov>
2021-05-12 15:21:09 -0400

    Freeze
    Former-commit-id: e7a52234cc1f03ca45cda2847fa8121207296b77

--- bin/PicoL.pl
+++ bin/PicoL.pl
@@ -138,6 +138,10 @@ if ($pwd =~ /dev/ or $pwd  =~ /DEV/ or $pwd =~ /P20i/ or $pwd =~ /P21i/) {
     elsif ($pwd =~ /7p7GeV_2021/)             {$glob = "/2021/RF/TFG21c.B/7p7GeV_2021";}
   }
   elsif ($pwd =~ /2021\/3p85GeV_fixedTarget_2021/) {$glob = "/2021/RF/TFG21e/3p85GeV_fixedTarget_2021";}
+  elsif ($pwd =~ /2021\/100GeV_fixedTarget_2021/) {$glob = "/2021/RF/TFG21e/100GeV_fixedTarget_2021";}
+  elsif ($pwd =~ /2021\/44p5GeV_fixedTarget_2021/) {$glob = "/2021/RF/TFG21e/44p5GeV_fixedTarget_2021";}
+  elsif ($pwd =~ /2021\/70GeV_fixedTarget_2021/) {$glob = "/2021/RF/TFG21e/70GeV_fixedTarget_2021";}
+  elsif ($pwd =~ /2021\/OO_200GeV_2021/) {$glob = "/2021/RF/TFG21e/OO_200GeV_2021";}
 }
 print "PICOPATH = $PICOPATH; days = $dayMin  - $dayMax : glob = $glob\n" if ($debug);
 #if (! $glob) {die "glob = $glob";}
