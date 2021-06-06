0afcce6f1fd10e7d1a97b3d1d59d302117a29eb8
Yuri Fisyak <fisyak@bnl.gov>
2021-06-06 09:49:43 -0400

    Merge
    Former-commit-id: a319126bcad6aa46087504e451f6a1f62906de6e

--- StRoot/StRefMultCorr/StRefMultCorr.h
+++ StRoot/StRefMultCorr/StRefMultCorr.h
@@ -88,140 +88,139 @@
 
 //______________________________________________________________________________
 // Class to correct z-vertex dependence, luminosity dependence of multiplicity
-class StRefMultCorr 
-{
-	public:
-		// Specify the type of multiplicity (default is refmult)
-		// "refmult"   - reference multiplicity defined in |eta|<0.5
-		// "refmult2"  - reference multiplicity defined in 0.5<|eta|<1.0
-		// "refmult3"  - reference multiplicity defined in |eta|<0.5 without protons
-		// "toftray"   - TOF tray multiplicity
-		// "grefmult"  - global reference multiplicity defined in |eta|<0.5,dca<3,nHitsFit>10
-		// Specify the type of data sets (in case there are multiple prameters/definitions in the same runs)
-		// "Def"
-		// "VpdMB5"
-		// "VpdMB30"
-		// "VpdMBnoVtx"
-		StRefMultCorr(const TString name="refmult", const TString subname="Def", const TString libname="Def");
-		virtual ~StRefMultCorr(); /// Default destructor
+class StRefMultCorr {
+ public:
+  // Specify the type of multiplicity (default is refmult)
+  // "refmult"   - reference multiplicity defined in |eta|<0.5
+  // "refmult2"  - reference multiplicity defined in 0.5<|eta|<1.0
+  // "refmult3"  - reference multiplicity defined in |eta|<0.5 without protons
+  // "toftray"   - TOF tray multiplicity
+  // "grefmult"  - global reference multiplicity defined in |eta|<0.5,dca<3,nHitsFit>10
+  // Specify the type of data sets (in case there are multiple prameters/definitions in the same runs)
+  // "Def"
+  // "VpdMB5"
+  // "VpdMB30"
+  // "VpdMBnoVtx"
+  StRefMultCorr(const TString name="refmult", const TString subname="Def", const TString libname="Def");
+  virtual ~StRefMultCorr(); /// Default destructor
+  
+  // Bad run rejection
+  Bool_t isBadRun(const Int_t RunId) ;
 
-		// Bad run rejection
-		Bool_t isBadRun(const Int_t RunId) ;
+  // Event-by-event initialization. Call this function event-by-event
+  // * Default ZDC coincidence rate = 0 to make the function backward compatible 
+  // --> i.e. no correction will be applied unless users set the values for 200 GeV
+  void initEvent(const UShort_t RefMult, const Double_t z, const Double_t zdcCoincidenceRate=0.0) ; // Set multiplicity, vz and zdc coincidence rate
 
-		// Event-by-event initialization. Call this function event-by-event
-		// * Default ZDC coincidence rate = 0 to make the function backward compatible 
-		// --> i.e. no correction will be applied unless users set the values for 200 GeV
-		void initEvent(const UShort_t RefMult, const Double_t z, const Double_t zdcCoincidenceRate=0.0) ; // Set multiplicity, vz and zdc coincidence rate
+  Bool_t passnTofMatchRefmultCut(Double_t refmult, Double_t ntofmatch); //remove the pile up event 
 
-		Bool_t passnTofMatchRefmultCut(Double_t refmult, Double_t ntofmatch); //remove the pile up event 
+  /// Get corrected multiplicity, correction as a function of primary z-vertex
+  Double_t getRefMultCorr() const;
+  // Corrected multiplity, flag=0:  Luminosity only, flag=1:  z-vertex only, flag=2:  full correction (default)
+  Double_t getRefMultCorr(const UShort_t RefMult, const Double_t z, const Double_t zdcCoincidenceRate, const UInt_t flag=2) const ;
 
-		/// Get corrected multiplicity, correction as a function of primary z-vertex
-		Double_t getRefMultCorr() const;
-		// Corrected multiplity, flag=0:  Luminosity only, flag=1:  z-vertex only, flag=2:  full correction (default)
-		Double_t getRefMultCorr(const UShort_t RefMult, const Double_t z, const Double_t zdcCoincidenceRate, const UInt_t flag=2) const ;
+  /// Get 16 centrality bins (5% increment, 0-5, 5-10, ..., 75-80)
+  Int_t getCentralityBin16() const;
 
-		/// Get 16 centrality bins (5% increment, 0-5, 5-10, ..., 75-80)
-		Int_t getCentralityBin16() const;
-
-		/// Get 9 centrality bins (10% increment except for 0-5 and 5-10)
-		Int_t getCentralityBin9() const;
+  /// Get 9 centrality bins (10% increment except for 0-5 and 5-10)
+  Int_t getCentralityBin9() const;
 		
-		/// Re-weighting correction, correction is only applied up to mNormalize_step (energy dependent)
-		//Double_t getWeight() const;
-		Double_t getWeight();
+  /// Re-weighting correction, correction is only applied up to mNormalize_step (energy dependent)
+  //Double_t getWeight() const;
+  Double_t getWeight();
 
-		// Initialization of centrality bins etc
-		void init(const Int_t RunId);
+  // Initialization of centrality bins etc
+  void init(const Int_t RunId);
 
-		// Read scale factor from text file
-		void setVzForWeight(const Int_t nbin, const Double_t min, const Double_t max) ;
-		void readScaleForWeight(const Char_t* input) ;
+  // Read scale factor from text file
+  void setVzForWeight(const Int_t nbin, const Double_t min, const Double_t max) ;
+  void readScaleForWeight(const Char_t* input) ;
 
-		// Return begin/end run from energy and year
-		Int_t getBeginRun(const Double_t energy, const Int_t year) ;
-		Int_t getEndRun(  const Double_t energy, const Int_t year) ;
+  // Return begin/end run from energy and year
+  Int_t getBeginRun(const Double_t energy, const Int_t year) ;
+  Int_t getEndRun(  const Double_t energy, const Int_t year) ;
 
-		// Print all parameters
-		void print(const Option_t* option="") const ;
+  // Print all parameters
+  void print(const Option_t* option="") const ;
 
-	private:
-		const TString mName ; // refmult, refmult2, refmult3 or toftray (case insensitive)
-		const TString mSubName ; // specify triggers, in case there are multiple parameters/definitions in the same runs 
-		const TString mLibName ; // specify library, in case the centrality definition depends on productions 
+ private:
+  const TString mName ; // refmult, refmult2, refmult3 or toftray (case insensitive)
+  const TString mSubName ; // specify triggers, in case there are multiple parameters/definitions in the same runs 
+  const TString mLibName ; // specify library, in case the centrality definition depends on productions 
 
-		// Functions
-		void clear() ; /// Clear all arrays
-		Bool_t isIndexOk() const ; /// 0 <= mParameterIndex < maxArraySize
-		Bool_t isZvertexOk() const ; /// mStart_zvertex < z < mStop_zvertex
-		Bool_t isRefMultOk() const ; /// 0-80%, (corrected multiplicity) > mCentrality_bins[0]
-		Bool_t isCentralityOk(const Int_t icent) const ; /// centrality bin check
-		Int_t setParameterIndex(const Int_t RunId) ; /// Parameter index from run id (return mParameterIndex)
-		// For isobar
-		Bool_t IsZr;
-		Bool_t IsRu;
+  // Functions
+  void clear() ; /// Clear all arrays
+  Bool_t isIndexOk() const ; /// 0 <= mParameterIndex < maxArraySize
+  Bool_t isZvertexOk() const ; /// mStart_zvertex < z < mStop_zvertex
+  Bool_t isRefMultOk() const ; /// 0-80%, (corrected multiplicity) > mCentrality_bins[0]
+  Bool_t isCentralityOk(const Int_t icent) const ; /// centrality bin check
+  Int_t setParameterIndex(const Int_t RunId) ; /// Parameter index from run id (return mParameterIndex)
+  // For isobar
+  Bool_t IsZr;
+  Bool_t IsRu;
 
-		// Special scale factor for Run14 to take into account the weight
-		// between different triggers
-		//  - return 1 for all the other runs
-		Double_t getScaleForWeight() const ;
+  // Special scale factor for Run14 to take into account the weight
+  // between different triggers
+  //  - return 1 for all the other runs
+  Double_t getScaleForWeight() const ;
 
-		// Data members
-		enum 
-		{
-			mNCentrality   = 16, /// 16 centrality bins, starting from 75-80% with 5% bin width
-			mNPar_z_vertex = 8,
-			mNPar_weight   = 8,
-			mNPar_luminosity = 2
-		};
+  // Data members
+  enum 
+  {
+    mNCentrality   = 16, /// 16 centrality bins, starting from 75-80% with 5% bin width
+    mNPar_z_vertex = 8,
+    mNPar_weight   = 8,
+    mNPar_luminosity = 2
+  };
 
-		// Use these variables to avoid varying the corrected multiplicity
-		// in the same event by random numbers
-		UShort_t mRefMult ;     /// Current multiplicity
-		Double_t mVz ;          /// Current primary z-vertex
-		Double_t mZdcCoincidenceRate ; /// Current ZDC coincidence rate
-		Double_t mRefMult_corr; /// Corrected refmult
+  // Use these variables to avoid varying the corrected multiplicity
+  // in the same event by random numbers
+  UShort_t mRefMult ;     /// Current multiplicity
+  Double_t mVz ;          /// Current primary z-vertex
+  Double_t mZdcCoincidenceRate ; /// Current ZDC coincidence rate
+  Double_t mRefMult_corr; /// Corrected refmult
 
-		std::vector<Int_t> mYear              ; /// Year
-		std::vector<Int_t> mStart_runId       ; /// Start run id
-		std::vector<Int_t> mStop_runId        ; /// Stop run id
-		std::vector<Double_t> mStart_zvertex  ; /// Start z-vertex (cm)
-		std::vector<Double_t> mStop_zvertex   ; /// Stop z-vertex (cm)
-		std::vector<Double_t> mNormalize_stop ; /// Normalization between MC and data (normalized in refmult>mNormalize_stop)
-		std::vector<Int_t>    mCentrality_bins[mNCentrality+1] ;  /// Centrality bins (last value is set to 5000)
-		std::vector<Double_t> mPar_z_vertex[mNPar_z_vertex] ;     /// parameters for z-vertex correction
-		std::vector<Double_t> mPar_weight[mNPar_weight] ;         /// parameters for weight correction
-		std::vector<Double_t> mPar_luminosity[mNPar_luminosity] ; /// parameters for luminosity correction (valid only for 200 GeV)
-		Int_t mParameterIndex; /// Index of correction parameters
+  std::vector<Int_t> mYear              ; /// Year
+  std::vector<Int_t> mStart_runId       ; /// Start run id
+  std::vector<Int_t> mStop_runId        ; /// Stop run id
+  std::vector<Double_t> mStart_zvertex  ; /// Start z-vertex (cm)
+  std::vector<Double_t> mStop_zvertex   ; /// Stop z-vertex (cm)
+  std::vector<Double_t> mNormalize_stop ; /// Normalization between MC and data (normalized in refmult>mNormalize_stop)
+  std::vector<Int_t>    mCentrality_bins[mNCentrality+1] ;  /// Centrality bins (last value is set to 5000)
+  std::vector<Double_t> mPar_z_vertex[mNPar_z_vertex] ;     /// parameters for z-vertex correction
+  std::vector<Double_t> mPar_weight[mNPar_weight] ;         /// parameters for weight correction
+  std::vector<Double_t> mPar_luminosity[mNPar_luminosity] ; /// parameters for luminosity correction (valid only for 200 GeV)
+  Int_t mParameterIndex; /// Index of correction parameters
 
-		std::multimap<std::pair<Double_t, Int_t>, Int_t> mBeginRun ; /// Begin run number for a given (energy, year)
-		std::multimap<std::pair<Double_t, Int_t>, Int_t> mEndRun   ; /// End run number for a given (energy, year)
-		std::vector<Int_t> mBadRun ; /// Bad run number list
+  std::multimap<std::pair<Double_t, Int_t>, Int_t> mBeginRun ; /// Begin run number for a given (energy, year)
+  std::multimap<std::pair<Double_t, Int_t>, Int_t> mEndRun   ; /// End run number for a given (energy, year)
+  std::vector<Int_t> mBadRun ; /// Bad run number list
 
-		// [6][680];
-		Int_t mnVzBinForWeight ; /// vz bin size for scale factor
-		std::vector<Double_t> mVzEdgeForWeight ; /// vz edge value
-		std::vector<Double_t> mgRefMultTriggerCorrDiffVzScaleRatio ; /// Scale factor for global refmult
+  // [6][680];
+  Int_t mnVzBinForWeight ; /// vz bin size for scale factor
+  std::vector<Double_t> mVzEdgeForWeight ; /// vz edge value
+  std::vector<Double_t> mgRefMultTriggerCorrDiffVzScaleRatio ; /// Scale factor for global refmult
 
-		/////// Added from official package in StRoot/StRefMultCorr 
-		const Int_t getRefX() const;  /// X= 1 (normal RefMult), 2, 3, 4
-		const Int_t getNumberOfDatasets() const; /// Number of definitions for each X
-		void readHeaderFile();   //// alternative of read() in rev<=1.9 
-		void readBadRunsFromHeaderFile();  /// alternative of readBadRuns() in rev<=1.9 
-		//// Multiplicity dependent centrality definitions
-		Int_t getVzWindowForVzDepCentDef() const;
-		Int_t getCentralityBin9VzDep() const;
-		Int_t getCentralityBin16VzDep() const;
-		std::vector<std::string> StringSplit( const std::string str, const char sep );
+  /////// Added from official package in StRoot/StRefMultCorr 
+  const Int_t getRefX() const;  /// X= 1 (normal RefMult), 2, 3, 4
+  const Int_t getNumberOfDatasets() const; /// Number of definitions for each X
+  void readHeaderFile();   //// alternative of read() in rev<=1.9 
+  void readBadRunsFromHeaderFile();  /// alternative of readBadRuns() in rev<=1.9 
+  //// Multiplicity dependent centrality definitions
+  Int_t getVzWindowForVzDepCentDef() const;
+  Int_t getCentralityBin9VzDep() const;
+  Int_t getCentralityBin16VzDep() const;
+  std::vector<std::string> StringSplit( const std::string str, const char sep );
 
-		//---------------------------------------------------------
-		//weight refmult shape in each Vz bin to what in the center (|Vz|<10cm), add by Zaochen
-		Double_t getShapeWeight_SubVz2Center() ;
-		//---------------------------------------------------------
-		//
-		// Read scale factor from header file
-		void readScaleForWeight(const Int_t nRefmultBin, const Double_t *weight) ;
+  //---------------------------------------------------------
+  //weight refmult shape in each Vz bin to what in the center (|Vz|<10cm), add by Zaochen
+  Double_t getShapeWeight_SubVz2Center() ;
+  //---------------------------------------------------------
+  //
+  // Read scale factor from header file
+  void readScaleForWeight(const Int_t nRefmultBin, const Double_t *weight) ;
 
-		ClassDef(StRefMultCorr, 0)
+  ClassDef(StRefMultCorr, 0)
 };
 #endif
 
