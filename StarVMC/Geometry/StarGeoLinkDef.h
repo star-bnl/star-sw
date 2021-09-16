5505fa2cd2669ba3ce5094e99cd9ec006b06a595
Dmitri Smirnov <dmixsmi@gmail.com>
2021-09-16 17:56:39 -0400

    cons: Generate ROOT dictionary for generated geometry sources (8378339d)
    Since this is done manually for the the generated classes (`StarGeometry` and
    `Geometry`), we need to turn off the automatic dictionary generation in
    `StarVMC/Geometry` triggered otherwise by the `StarGeoLinkDef.h` file.
    
    cherry-pick 8378339d

---
+++ StarVMC/Geometry/StarGeoLinkDef.h
@@ -0,0 +1,8 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class StarGeometry;
+#pragma link C++ class Geometry;
+#endif
