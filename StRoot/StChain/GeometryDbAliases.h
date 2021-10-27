6d7e41ecc1c46f4077d6b46c1f36a48f7919b60c
Yuri Fisyak <fisyak@bnl.gov>
2021-10-27 10:52:33 -0400

    Add y2022

--- StRoot/StChain/GeometryDbAliases.h
+++ StRoot/StChain/GeometryDbAliases.h
@@ -193,6 +193,8 @@ static const DbAlias_t fDbAlias[] = {// geometry  Comment            old
   {"r2021",       20201220,     0, "y2019",    "y2019, iTPC, Jim Thomas 05/31/16, no HFT,xgeometry"}, // 
   {"r2021a",      20201220,     1, "y2019",    "y2019, iTPC, Jim Thomas 05/31/16, no HFT,xgeometry"}, // 
 //{"dev2022",     20211210,     1, "dev2022",  "development geometry for 2022+ forward program,AgML,xgeometry"},
+  {"y2022",       20211015,     0, "y2019",    "y2019 development geometry, AgML,xgeometry"},   
+  {"r2022",       20211025,     0, "y2019",    "y2019, iTPC, Jim Thomas 05/31/16, no HFT,xgeometry"}, // 
   {"athena",      20301220,     1, "athena",    "athena"}, // 
   {0,                    0,     0,        0,    0}
 };
