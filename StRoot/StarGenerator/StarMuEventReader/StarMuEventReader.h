f26347cd9aba8280662e637ef25a9a0a66d98c15
Yuri Fisyak <fisyak@bnl.gov>
2021-11-09 17:35:31 -0500

    Add clear

--- StRoot/StarGenerator/StarMuEventReader/StarMuEventReader.h
+++ StRoot/StarGenerator/StarMuEventReader/StarMuEventReader.h
@@ -17,6 +17,8 @@ public:
 
   Int_t Init();
   Int_t Generate();
+ // Clear the event
+  virtual void Clear( const Option_t *opts="" ) {mEvent->Clear();}
   Int_t Skip(Int_t Nskip);
   Int_t ReadEvent(Int_t N = 0);
   StarGenParticle *AddParticle();
