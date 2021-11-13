5aee02a610dd73c4699d8188b0d6f01db461ad60
Xu Sun <sunxuhit@gmail.com>
2021-11-12 20:09:45 -0500

    forward declaration daqReader

--- OnlTools/Jevp/StJevpBuilders/fstBuilder.h
+++ OnlTools/Jevp/StJevpBuilders/fstBuilder.h
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 #include "JevpBuilder.h"
-#include "DAQ_READER/daqReader.h"
+// #include "DAQ_READER/daqReader.h"
+class daqReader;
 #include <TH1F.h>
 #include <TH2F.h>
 #include <TH1S.h>
