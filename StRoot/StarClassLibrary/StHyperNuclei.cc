1e501917467d25d114e40f15a24139db46810f41
klendathu2k <56083924+klendathu2k@users.noreply.github.com>
2021-11-16 09:08:18 -0500

    Sl20c embedding patch (#196)
    * Add two pentaquark states
    
    * Add two pentaquark states... small hack for single decay mode.
    
    * Pentaquark decay chain, all daughters decay to charged states
    
    * Fix cut-n-paste error on K0short definition
    
    * Update to gstar_part.g and StarClassLibrary to support simulation of
    H4 Lambda , He4 Lambda  and He5 Lambda hypernuclei.
    
    * Remove the STOP, as we may be passed stable nuclei here
    
    * Add particle definitions to support H3lambda quasi two body decay
    
    * Tweaks to mass, lifetime requested as part of the quasi two body decay
    
    * Use the eta lifetime, so that energy smearing in gdecay does not (completely) break energy conservation
    
    * Definitions for H4-Lambda, He4-Lambda and He5-Lambda hypernuclei.
    
    * Ensure that an instance of the particle data is available.
    
    * Add Anti-H4 Lambda decay to Anti-He4 + pion- (#176)
    
    * Add H3LambdaBar decay to gstar library
    
    * Add H4LambdaBar, He4LambdaBar & He5LambdaBar hypernuclei to StarClassLibrary.
    Add only H4LambdaBar to the particle table (others are placeholders assuming
    a request is made at some point).

---
+++ StRoot/StarClassLibrary/StHyperNuclei.cc
@@ -0,0 +1,9 @@
+#include "StHyperNuclei.hh"
+
+StH4Lambda  StH4Lambda::mH4Lambda;
+StHe4Lambda StHe4Lambda::mHe4Lambda;
+StHe5Lambda StHe5Lambda::mHe5Lambda;
+
+StH4LambdaBar  StH4LambdaBar::mH4LambdaBar;
+StHe4LambdaBar StHe4LambdaBar::mHe4LambdaBar;
+StHe5LambdaBar StHe5LambdaBar::mHe5LambdaBar;
