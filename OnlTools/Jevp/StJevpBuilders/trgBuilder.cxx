60b8ec4c8ef870ae85bb08c21b7a846abc345ede
Hongwei Ke <kehw@bnl.gov>
2021-12-20 16:13:47 -0500

    bug fix

--- OnlTools/Jevp/StJevpBuilders/trgBuilder.cxx
+++ OnlTools/Jevp/StJevpBuilders/trgBuilder.cxx
@@ -642,19 +642,19 @@ void trgBuilder::event(daqReader *rdr)
     */
     
     // zdcsmd...
-    TH1D *zdcsmd[8];
-    zdcsmd[0] = (TH1D*)contents.h329_zdcsmd_w_v_N;
-    zdcsmd[1] = (TH1D*)contents.h330_zdcsmd_w_h_N;
-    zdcsmd[2] = (TH1D*)contents.h331_zdcsmd_e_v_N;
-    zdcsmd[3] = (TH1D*)contents.h332_zdcsmd_e_h_N;
-    zdcsmd[4] = (TH1D*)contents.h333_zdcsmd_w_v_A;
-    zdcsmd[5] = (TH1D*)contents.h334_zdcsmd_w_h_A;
-    zdcsmd[6] = (TH1D*)contents.h335_zdcsmd_e_v_A;
-    zdcsmd[7] = (TH1D*)contents.h336_zdcsmd_e_h_A;
-    zdcsmd[8] = (TH2D*)contents.h337_zdcsmd_w_v_A_2D;
-    zdcsmd[9] = (TH2D*)contents.h338_zdcsmd_w_h_A_2D;
-    zdcsmd[10] = (TH2D*)contents.h339_zdcsmd_e_v_A_2D;
-    zdcsmd[11] = (TH2D*)contents.h340_zdcsmd_e_h_A_2D;
+    TH1 *zdcsmd[12];
+    zdcsmd[0] = contents.h329_zdcsmd_w_v_N;
+    zdcsmd[1] = contents.h330_zdcsmd_w_h_N;
+    zdcsmd[2] = contents.h331_zdcsmd_e_v_N;
+    zdcsmd[3] = contents.h332_zdcsmd_e_h_N;
+    zdcsmd[4] = contents.h333_zdcsmd_w_v_A;
+    zdcsmd[5] = contents.h334_zdcsmd_w_h_A;
+    zdcsmd[6] = contents.h335_zdcsmd_e_v_A;
+    zdcsmd[7] = contents.h336_zdcsmd_e_h_A;
+    zdcsmd[8] = contents.h337_zdcsmd_w_v_A_2D;
+    zdcsmd[9] = contents.h338_zdcsmd_w_h_A_2D;
+    zdcsmd[10] = contents.h339_zdcsmd_e_v_A_2D;
+    zdcsmd[11] = contents.h340_zdcsmd_e_h_A_2D;
     
     const int ZDCSMDTHR = 80;
     for(int i=0; i<2; i++){
