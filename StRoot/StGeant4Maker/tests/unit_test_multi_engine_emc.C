4bab5bf1aff506eb5247bc6754746a132f25ca35
klendathu2k <jwebb@bnl.gov>
2021-12-29 16:01:26 -0500

    Sampling fraction histograms

--- StRoot/StGeant4Maker/tests/unit_test_multi_engine_emc.C
+++ StRoot/StGeant4Maker/tests/unit_test_multi_engine_emc.C
@@ -4,6 +4,8 @@
 #include "TMath.h"
 #include "TProfile.h"
 
+
+
 #ifndef __CINT__
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics/stats.hpp>
@@ -40,9 +42,22 @@ const double phi0 = 0.0; // not really...
 
 
 //___________________________________________________________________
+
+// Obtain a track pointer from the given hit
+template<typename Hit> const g2t_track_st* get_track( Hit* hit ) {
+  int index = hit->track_p - 1;
+  if ( index < 0 || index > track_table->GetNRows() ) {
+    return 0;
+  } 
+  else {
+    return static_cast<const g2t_track_st*>( track_table -> At( index ) );
+  }
+}
+
+//___________________________________________________________________
 std::vector<double> mean_, median_, min_, max_, error_of_mean_, sum_;
 std::vector<int> nhits_;
-void eventSums() {
+void trackLoop() {
 
   auto* chain = StMaker::GetChain();
   vertex_table = dynamic_cast<TTable*>( chain->GetDataSet("g2t_vertex")  );
@@ -50,6 +65,8 @@ void eventSums() {
   hit_table    = dynamic_cast<TTable*>( chain->GetDataSet("g2t_emc_hit") ) ;
 
   Accumulator_t edep; // Energy deposition
+
+  std::map< int, double > edep_per_track;
 
   // Loop on all hits and accumulate all energy depositions
   int nh = hit_table->GetNRows();
@@ -62,7 +79,33 @@ void eventSums() {
       // accumulate energy deposition
       edep( hit->de );
 
+       // accumulate sampling fraction
+       auto* track = get_track( hit );
+       if ( track ) {
+       	double pt = track->pt;
+       	double eta = track->eta;
+       	if ( pt > 0.100 && pt < 10.0 ) {
+       	  edep_per_track[ track->id ] += hit->de; // running sum
+       	}	
+       }
+
   }
+
+   // Fill histograms
+   for ( int i=0;i<track_table->GetNRows(); i++ ) {
+     const g2t_track_st* track = static_cast<const g2t_track_st*>( track_table->At(i) );
+     if ( track ) {
+       double pt = track->pt;
+       double eta = track->eta;
+       // todo phi
+       double E  = track->e + 1.0e-12; // prevent div by zero
+       if ( pt > 0.100 && pt < 10.0 && eta > -0.95 && eta < 0.95 ) {
+   	sampling_fraction_vs_pt.back()->Fill( pt, edep_per_track[ track->id ] / E );
+   	sampling_fraction_vs_eta.back()->Fill( eta, edep_per_track[ track->id ] / E );
+       }
+     }
+   }
+
 
   double _sum           = boost::accumulators::sum(edep);
   double _mean          = boost::accumulators::mean(edep);
@@ -79,9 +122,22 @@ void eventSums() {
   error_of_mean_.push_back(_error_of_mean);
 
 }
-
 //___________________________________________________________________
-void unit_test_multi_engine_emc( int ntracks = 500, const char* part = "pi+" ) {
+void book_histograms() {
+
+  const char* engines[] = { "geant3", "geant4" };
+  assert(sampling_fraction_vs_pt.size() < sizeof(engines)/sizeof(const char*) );
+
+  TString base  = Form( "%s_sampling_fraction_vs_",    engines[ sampling_fraction_vs_pt.size() ] );
+  TString title = Form( "EMC Sampling fraction [%s]", engines[ sampling_fraction_vs_pt.size() ] );
+
+  sampling_fraction_vs_pt.push_back ( new TProfile(base+"pt",  title+";p_{T}", 50,  0., 10.0, 0.0, 1.0 ) );
+  sampling_fraction_vs_eta.push_back( new TProfile(base+"eta", title+";#eta",  40, -0.95, 0.95, 0.0, 1.0 ) );
+  sampling_fraction_vs_phi.push_back( new TProfile(base+"phi", title+";#phi",  60, 0., TMath::TwoPi(), 0.0, 1.0 ) );
+
+}
+//___________________________________________________________________
+void unit_test_multi_engine_emc( int ntracks = 5, const char* part = "gamma" ) {
 
   gROOT->ProcessLine("initChain();");
 
@@ -99,8 +155,9 @@ void unit_test_multi_engine_emc( int ntracks = 500, const char* part = "pi+" ) {
   auto* gm = dynamic_cast<StGeant4Maker*>( StMaker::GetChain()->GetMaker("geant4star") );
 
   gm->SetEngineForModule( "CALB", 0 ); 
-  throw_particle(ntracks, part, 0.99995, 1.00005, -0.95, 0.95, 0., TMath::TwoPi() );
-  eventSums();
+  book_histograms();
+  throw_particle(ntracks, part, 0.099995, 10.00005, -0.95, 0.95, 0., TMath::TwoPi() );
+  trackLoop();
 
   LOG_TEST << "=======================================================" << std::endl;
   LOG_TEST << "Multi-engine testing of EMC" << std::endl;
@@ -109,8 +166,9 @@ void unit_test_multi_engine_emc( int ntracks = 500, const char* part = "pi+" ) {
   LOG_TEST << "=======================================================" << std::endl;
 
   gm->SetEngineForModule( "CALB", 1 );
+  book_histograms();
   throw_particle(ntracks, part, 0.99995, 1.00005, -0.95, 0.95, 0., TMath::TwoPi() );
-  eventSums();
+  trackLoop();
 
   LOG_TEST << "=======================================================" << std::endl;
   LOG_TEST << "Multi-engine testing of EMC" << std::endl;
