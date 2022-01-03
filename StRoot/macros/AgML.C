fbd14d14b87bfaa1b44739bc55f770dffeac15c8
Yuri Fisyak <fisyak@bnl.gov>
2022-01-02 19:03:46 -0500

    Activate

--- StRoot/macros/AgML.C
+++ StRoot/macros/AgML.C
@@ -6,19 +6,24 @@ void AgML(const Char_t *tag="y2013_2", const Char_t *geom="") {
     exit(1);
   }
   if (Geom == "") Geom = Tag;
-#if 1
-  gSystem->Load("St_base");
-  gSystem->Load("StChain");
-  if (gSystem->Load("liblog4cxx.so") >=  0) {             //  StMemStat::PrintMem("load log4cxx");
-    cout << " + liblog4cxx.so";
-    if(gSystem->Load("libStStarLogger.so") >= 0) {              //  StMemStat::PrintMem("load log4cxx");
-      cout << " + libStStarLogger.so";
+  
+  gSystem->Load("libSt_base");                                        //  StMemStat::PrintMem("load St_base");
+  if (gSystem->Load("liblog4cxx") >=  0) {             //  StMemStat::PrintMem("load log4cxx");
+    cout << " + liblog4cxx";
+    if(gSystem->Load("libStStarLogger") >= 0) {              //  StMemStat::PrintMem("load log4cxx");
+      cout << " + libStStarLogger";
+      //      gROOT->ProcessLine("StLoggerManager::StarLoggerInit();"); 
+      StLoggerManager::StarLoggerInit();
+      if (gROOT->IsBatch())  StLoggerManager::setColorEnabled(kFALSE);
     }
+    cout << endl;
   }
+  gSystem->Load("libStChain");                                        //  StMemStat::PrintMem("load StChain");
+  gSystem->Load("libStUtilities");                                    //  StMemStat::PrintMem("load StUtilities");
   gSystem->Load("StarAgmlUtil");
   gSystem->Load("StarAgmlLib");
-  //  gSystem->Load("StarGeometry");
-  gSystem->Load("libGeometry");
+  gSystem->Load("Geometry");
+  gSystem->Load("StarGeometry");
   gSystem->AddIncludePath(" -IStRoot -Igeom -IStarVMC -IStarVMC/Geometry/macros -I$STAR/StRoot -Igeom -I$STAR/StarVMC -I$STAR/StarVMC/Geometry/macros ");
   gErrorIgnoreLevel=9999;                        // Silence ROOT warnings for now
   gGeoManager = new TGeoManager(Geom.Data(),Form("%s/AgML",Geom.Data()));
@@ -26,10 +31,6 @@ void AgML(const Char_t *tag="y2013_2", const Char_t *geom="") {
   Geometry *build = new Geometry();                        // Instantiate the geometry
   build -> ConstructGeometry ( Geom.Data() );            
 
-#else
-  gROOT->LoadMacro("./StarVMC/Geometry/macros/loadStarGeometry.C");
-  loadStarGeometry(geom);
-#endif
   gGeoManager->CloseGeometry();
   //  gGeoManager->Export(Form("%s.root",Geom.Data()));
   TObjectSet *geomOS = new TObjectSet("Geometry",gGeoManager,kFALSE);
