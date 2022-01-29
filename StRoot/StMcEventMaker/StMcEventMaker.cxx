9c37558fcd135540dde1185c1211f0cfb1e96674
Xin Dong <XDong@lbl.gov>
2022-01-29 09:45:13 -0800

    Update StMcEventMaker.cxx
    Remove "#if 0" that commented out some subsystem hits

--- StRoot/StMcEventMaker/StMcEventMaker.cxx
+++ StRoot/StMcEventMaker/StMcEventMaker.cxx
@@ -625,7 +625,7 @@ Int_t StMcEventMaker::Make()
 	    mtdHitTable = g2t_mtd_hitTablePointer->GetTable();
 	else
 	    if (Debug()) cerr << "Table g2t_mtd_hit Not found in Dataset " << geantDstI.Pwd()->GetName() << endl;
-#if 0
+
 	//
 	// BEMC and BPRS Hit Table
 	//
@@ -679,7 +679,7 @@ Int_t StMcEventMaker::Make()
 	  fscHitTable = g2t_fsc_hitTablePointer->GetTable();
 	else
 	  if (Debug()) cout << "Table g2t_fsc_hit Not found in Dataset " << geantDstI.Pwd()->GetName() << endl;
-#endif
+
 	//	
 	// Pxl Hit Table
 	//
