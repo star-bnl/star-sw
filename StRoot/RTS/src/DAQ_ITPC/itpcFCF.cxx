star-bnl/star-sw

commit a4551293d054a64e83ebd34a1dc2ca81401858ad

Sync with RTS code (#288)

StRoot/RTS/src/DAQ_ITPC/itpcFCF.cxx
@@ -1005,6 +1005,12 @@ void itpc_fcf_c::run_start()
 
 	memset(&f_stat,0,sizeof(f_stat)) ;
 
+	for(int r=0;r<=max_slice;r++) {
+	for(int p=0;p<=max_x;p++) {
+		get_row_pad(r,p)->s1_len = 0 ;
+	}
+	}
+
 
 }
 
@@ -1694,22 +1700,24 @@ int itpc_fcf_c::do_blobs_stage3(int row)
 			if(cha > 0x7FFF) cha = 0x8000 | (cha/1024) ;
 
 			if(flags & 3) tmp_fl |= 0x8000 ;	// ROW_EDGE
+			// NEW 11-Jan-2022
+			else {
 
+				*obuff++ = (time_c << 16) | pad_c ;
+				*obuff++ = (cha << 16) | tmp_fl ;
 
-			*obuff++ = (time_c << 16) | pad_c ;
-			*obuff++ = (cha << 16) | tmp_fl ;
-
-			if(words_per_cluster>2) {
-				*obuff++ = (quality<<16)|track_id ;
-			}
-			if(words_per_cluster>3) {
-				*obuff++ = (pixels<<16)|adc_max ;
-			}
+				if(words_per_cluster>2) {
+					*obuff++ = (quality<<16)|track_id ;
+				}
+				if(words_per_cluster>3) {
+					*obuff++ = (pixels<<16)|adc_max ;
+				}
 
 #ifdef DO_DBG1
 //			LOG(TERR,"**** S %d: row %d: %f %f %f",clusters_cou,row,f_p_ave,f_t_ave,f_charge) ;
 #endif
-			clusters_cou++ ;
+				clusters_cou++ ;
+			}
 		}
 		else {	// multiple peak hauristics
 			int ip1, ip2 ;
@@ -1827,24 +1835,26 @@ int itpc_fcf_c::do_blobs_stage3(int row)
 				// add flags here
 				pad_c |= 0x8000 ;					// merged flag
 				if(flags & 3) tmp_fl |= 0x8000 ;			// ROW_EDGE
+				// NEW 11-Jan-2022
+				else {
 
 
+					*obuff++ = (time_c << 16) | pad_c ;
+					*obuff++ = (cha << 16) | tmp_fl ;
 
-				*obuff++ = (time_c << 16) | pad_c ;
-				*obuff++ = (cha << 16) | tmp_fl ;
-
-				if(words_per_cluster > 2) {
-					*obuff++ = (quality<<16)|track_id ;
-				}
-				if(words_per_cluster > 3) {
-					*obuff++ = (pixels<<16)|adc_max ;
-				}
+					if(words_per_cluster > 2) {
+						*obuff++ = (quality<<16)|track_id ;
+					}
+					if(words_per_cluster > 3) {
+						*obuff++ = (pixels<<16)|adc_max ;
+					}
 				
 #ifdef DO_DBG1
 //				LOG(TERR,"0x%X 0x%X 0x%X 0x%X - 0x%X 0x%X",pad_c,time_c,cha,tmp_fl, (time_c<16)|pad_c,(cha<<16)|tmp_fl) ;
 //				LOG(TERR,"**** D %d: %f %f %f",clusters_cou,f_p_ave,f_t_ave,f_charge) ;
 #endif				
-				clusters_cou++ ;
+					clusters_cou++ ;
+				}
 			}
 		}
 

StRoot/RTS/src/DAQ_ITPC/itpcInterpreter.cxx
@@ -1666,6 +1666,7 @@ int itpcInterpreter::ana_triggered(u_int *data, u_int *data_end)
 	u_int err = 0 ;
 	u_int soft_err = 0 ;
 	int fee_cou = 0 ;
+	u_int evt_status = 0 ;
 
 	int expect_fee_cou = itpc_config[sector_id].rdo[rdo_id-1].fee_count ;
 
@@ -2050,11 +2051,13 @@ int itpcInterpreter::ana_triggered(u_int *data, u_int *data_end)
 		LOG(ERR,"%d:#%02d(id %d,cou %d) evt %d: error 0x%X 0x%X",
 		    rdo_id,fee_port,fee_id,fee_cou,evt_ix,err,soft_err) ;
 
-		for(int i=-4;i<8;i++) {
-			LOG(ERR,".... %d = 0x%08X",i,data[i]) ;	
+		if(dbg_level>=1) {
+			for(int i=-4;i<8;i++) {
+				LOG(ERR,".... %d = 0x%08X",i,data[i]) ;	
+			}
 		}
-		
-		if(err || soft_err) return -1 ;
+
+		if(err || soft_err) return -1 ;	// this is a bit harsh to return already here???
 	}
 
 	// this only works online
@@ -2072,8 +2075,10 @@ int itpcInterpreter::ana_triggered(u_int *data, u_int *data_end)
 
 	switch(data[0]) {
 	case 0x98001000 :	// start of trailer
-		if(data[1]!=0) {	// event status
+		evt_status = data[1] ;
+		if(evt_status!=0) {	// event status
 			run_err_add(rdo_id,8) ;
+
 			LOG(ERR,"RDO %d: bad event status 0x%08X",rdo_id,data[1]) ;
 		}
 
@@ -2129,6 +2134,10 @@ int itpcInterpreter::ana_triggered(u_int *data, u_int *data_end)
 			data++ ;
 		}
 		break ;
+	}
+
+	if(evt_status) {
+		LOG(ERR,"%d: evt_status 0x%08X",rdo_id,evt_status) ;
 	}
 
 	return 0 ;
@@ -2213,6 +2222,7 @@ int itpcInterpreter::rdo_scan_top(u_int *data, int words)
 	sampa_bx = -1 ;
 	ascii_cou = 0 ;
 	memset(evt_err,0,sizeof(evt_err)) ;
+	evt_status = 0 ;
 
 	// move forward until I hit start-comma
 	int w_cou = (words<16)?words:16 ;
@@ -2247,6 +2257,67 @@ int itpcInterpreter::rdo_scan_top(u_int *data, int words)
 		return -1 ;
 	}
 
+
+	int no_fees = 0 ;
+	//ds is of the form 0x98000014
+
+	if((data[0]&0xFF00000F)==0x98000004) {
+		rdo_version = (data[0]>>4)&0xFF ;	// aka 1
+	}
+	else {
+		no_fees = 1 ;	// not a triggered event - no FEEs
+	}
+
+	if(dbg_level>1) LOG(TERR,"%d: ds 0x%08X, 0x%X %d, words %d,%d",rdo_id,data[0],rdo_version,no_fees,words,w_cou) ;
+
+
+	// I need the event status ala get_l2
+
+	int trl_ix = -1 ;
+	int trl_stop_ix = -1 ;
+	
+
+//	for(int i=(words-6);i>=0;i--) {
+	for(int i=(words-12);i>=0;i--) {
+		if(dbg_level>1) LOG(TERR,"%d: 0x%08X",i,data[i]) ;
+
+		if(data[i]==0x98001000) {
+			trl_ix = i ;
+			break ;
+		}
+		if(data[i]==0x58001001) {
+			trl_stop_ix = i ;
+		}
+	}
+
+	if(no_fees==0) {	// triggered event with FEEs
+		if(trl_ix < 0) {
+			LOG(ERR,"%d: no trailer found, trl_stop_ix %d",rdo_id,trl_stop_ix) ;
+		}
+		else {
+			trl_ix++ ;
+
+			evt_status = (data[trl_ix++]) ;
+			int trg_cou = (data[trl_ix++]) & 0xFFFF ;
+
+			//if(dbg_level>1) LOG(TERR,"%d: evt_status 0x%08X, trg_cou %d, words %d",rdo_id,evt_status,trg_cou,words) ;
+			if(evt_status != 0 || trl_stop_ix<0) {	// FEE timeout
+				LOG(ERR,"%d: evt_status 0x%08X, trg_cou %d, words %d, trl_ix %d, trl_stop_ix %d",rdo_id,evt_status,trg_cou,words,trl_ix,trl_stop_ix) ;
+
+#if 0
+				for(int i=0;i<8;i++) {
+					LOG(TERR,"%d = 0x%08X",i,data[i]) ;
+				}
+				for(int i=(words-32);i<words;i++) {
+					LOG(TERR,"%d = 0x%08X",i,data[i]) ;
+				}
+#endif
+
+				//return -1 ;
+			}
+		}
+	}
+	
 	
 //	for(int i=0;i<16;i++) {
 //		LOG(TERR,"%d/%d = 0x%08X",i,words,data[i]) ;
@@ -2294,6 +2365,8 @@ int itpcInterpreter::rdo_scan_top(u_int *data, int words)
 			}
 			else {
 				ret = ana_triggered(data,data_end) ;
+
+				
 			}
 			break ;
 		}

StRoot/RTS/EventTracker/FtfSl3.cxx
@@ -901,7 +901,7 @@ void FtfSl3::setClustersFromGl3Event(gl3Event *event, int sector) {
     
     for(int i=event->sectorFirstHit[sector]; i < event->nHits; i++) {
 	if(i >= maxHits) {
-	    LOG(ERR, "Too many hits for sector %d, truncating", sector);
+	    LOG(ERR, "Too many hits for sector %d, (%d/%d) truncating", sector, i, maxHits);
 	    break;
 	}
 

StRoot/RTS/EventTracker/Makefile
@@ -10,12 +10,12 @@ include ../src/rtsplus.def
 DEFINES += -DRTS_LOG_COLORED
 
 VOBJS += libbsmd.a libbtow.a libemc.a libesmd.a libfpd.a libftp.a libl3.a libpmd.a libpp2pp.a \
-	libric.a libsc.a libssd.a libsvt.a libtof.a libtpc.a libtpcfcf.a libtpx.a libtrg.a libetow.a \
+	libric.a libsc.a libssd.a libsvt.a libtof.a libtpc.a libtpcfcf.a libtpx.a libitpc.a libtrg.a libetow.a \
 	libdaqReader.a libsfs.a librtslog.a
 
 VPATH += ../src/DAQ_BSMD ../src/DAQ_BTOW ../src/DAQ_EMC ../src/DAQ_ESMD ../src/DAQ_FPD ../src/DAQ_FTP ../src/DAQ_L3 ../src/DAQ_PMD \
 	../src/DAQ_PP2PP ../src/DAQ_RIC ../src/DAQ_SC ../src/DAQ_SSD ../src/DAQ_SVT ../src/DAQ_TOF ../src/DAQ_TPC ../src/FCF ../src/DAQ_ETOW \
-	../src/DAQ_TPX ../src/DAQ_TRG \
+	../src/DAQ_TPX ../src/DAQ_ITPC ../src/DAQ_TRG \
 	../src/DAQ_READER  ../src/SFS ../src/LOG 
 
 

StRoot/RTS/EventTracker/sizes.h
@@ -10,7 +10,7 @@
 #define szGL3_default_mHits    1000000    // global
 #define szGL3_default_mTracks  15000      // global
 
-#define szSL3_default_mHits    100000     // 1 sector
+#define szSL3_default_mHits    250000     // 1 sector
 #define szSL3_default_mTracks  1500       // 1 sector
 
 #define szSECP_max             75000

StRoot/RTS/include/RC/RC_Msg.hh
@@ -82,6 +82,7 @@ public:
     rc_daq_run_pause        daq_run_pause;
     rc_daq_run_resume       daq_run_resume;
     rc_daq_send_config      daq_send_config;
+    rc_daq_send_config      daq_send_reconfig;
     rc_daq_set_busy         daq_set_busy;
     rc_daq_release_busy     daq_release_busy;
     rc_daq_flush_tokens     daq_flush_tokens;

StRoot/RTS/include/SUNRT/clock.h
@@ -42,6 +42,7 @@ class RtsTimer
   timespec ts_new;
 #endif
   double t;
+  int ref_cnt;
 
   RtsTimer() {
     reset();
@@ -54,6 +55,7 @@ class RtsTimer
     clock_gettime(CLOCK_REALTIME, &ts_old);
 #endif
     ts_last = ts_old;
+    ref_cnt = 0;
   }
 
   double currtime() {
@@ -126,6 +128,20 @@ class RtsTimer
       stopwatchcounting = 1;
       return stopwatch;
   }
+
+  double rt_start() {
+      ref_cnt++;
+      if(ref_cnt == 1) return record_time();
+      return 0.0;
+  }
+  
+  double rt_stop() {
+      ref_cnt--;
+      if(ref_cnt < 0) ref_cnt = 0;
+      if(ref_cnt == 0) return record_time();
+      return 0.0;
+  }
+      
 };
 
 // Returns medium resolution time  (~1ms)

StRoot/RTS/include/cmds.h
@@ -66,6 +66,9 @@
 #define DAQ_QUERY_TOKENS               0x28
 #define RTS_QUERY_TOKENS               DAQ_QUERY_TOKENS
 
+#define DAQ_SEND_RECONFIG              0x26
+#define RTS_SEND_RECONFIG              DAQ_SEND_RECONFIG
+
 #define RTS_FORCE_STOP		       0x46 
 #define RTS_RECONNECT                  0x48
 #endif

commit 343a795335a421350567649c404fc5caa7640e85

fix for offline flag in itpc reader (#296)
Successfully resolves a memory error when reconstructing iTPC clusters that was introduced in PR #288 .

StRoot/RTS/src/DAQ_ITPC/itpcFCF.cxx
@@ -60,7 +60,7 @@ itpc_fcf_c::itpc_fcf_c()
 
         version = VERSION ;
 	sector_id = 0 ;		// 0 is ALL sectors
-	offline = 0 ;		// not Offline!
+	offline = 1 ;		// new: offline is default
 
 
 	det_type = 1 ;		// ITPC
@@ -293,7 +293,7 @@ struct itpc_fcf_c::rp_t *itpc_fcf_c::get_row_pad(int row, int pad)
 {
 	int max_pad_all = max_x + 1 ;
 
-	if(offline) s1_data_length = 1 + max_y * 2 ;	// need more for track_id
+	if(offline) s1_data_length = (1 + max_y) * 2 ;	// need more for track_id
 	else s1_data_length = 1 + max_y ;
 
 	if(row_pad_store==0) {	// initialize on first use...
@@ -738,14 +738,15 @@ int itpc_fcf_c::do_ch(int fee_id, int fee_ch, u_int *data, int words)
 		int t_start = tb_buff[i++] ;
 		int t_stop = t_start + t_cou - 1 ;
 
+		// happens too often, get rid of ERR
 		if(t_start <= t_stop_last) {
-			LOG(ERR,"%d:#%d: FEE %d:%d, words %d: %d <= %d",rdo,port,fee_id,fee_ch,words,t_start,t_stop_last) ;
+			LOG(NOTE,"%d:#%d: FEE %d:%d, words %d: %d <= %d",rdo,port,fee_id,fee_ch,words,t_start,t_stop_last) ;
 			seq_cou = 0 ;
 			err |= 4 ;
 			goto err_ret ;
 		}
 		if(t_stop > 511) {
-			LOG(ERR,"%d:#%d: FEE %d:%d, words %d: %d > 511",rdo,port,fee_id,fee_ch,words,t_stop) ;
+			LOG(NOTE,"%d:#%d: FEE %d:%d, words %d: %d > 511",rdo,port,fee_id,fee_ch,words,t_stop) ;
 			seq_cou = 0 ;
 			err |= 4 ;
 			goto err_ret ;
@@ -984,7 +985,11 @@ int itpc_fcf_c::do_fcf(void *v_store, int bytes)
 			out_store = row_store ;	// rewind 
 		}
 
-		if((out_store-store_start)>(max_out_bytes/4-1000)) {
+		int ints_used = out_store - store_start ;
+		//int ints_needed = (max_out_bytes/4)-1000 ;
+		int ints_needed = (max_out_bytes/4)-400 ;
+
+		if(ints_used && (ints_used>ints_needed)) {
 			LOG(ERR,"not enough ints %d vs %d",out_store-store_start,max_out_bytes/4) ;
 			break ;
 		}		

StRoot/RTS/src/DAQ_ITPC/itpcInterpreter.cxx
@@ -1897,7 +1897,8 @@ int itpcInterpreter::ana_triggered(u_int *data, u_int *data_end)
 				}
 			}
 #endif
-			//soft_err |= 0x100 ;
+			// 26-Jan-2022: enabling
+			soft_err |= 0x100 ;
 			//err |= 0x400 ;
 			//goto done ;
 		}