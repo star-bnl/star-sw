641d48e8d34417a2b91e52be3ba64d386baaaa79
starsdong <XDong@lbl.gov>
2022-02-07 21:17:38 -0800

    picoWrite option now includes trgSimu to ensure StTriggerSimuMaker included

--- StRoot/StBFChain/BigFullChain.h
+++ StRoot/StBFChain/BigFullChain.h
@@ -1951,7 +1951,7 @@ Bfc_st BFC[] = { // standard chains
 
   {"trgSimu"        ,"","",""       ,"StTriggerSimuMaker","StTriggerUtilities","trigger simu maker",kFALSE},
 
-  {"picoWrite" ,"","PicoChain","picoDst","StPicoDstMaker",""               ,"Writes picoDST format",kFALSE},
+  {"picoWrite" ,"","PicoChain","trgSimu,picoDst","StPicoDstMaker",""       ,"Writes picoDST format",kFALSE},
   {"picoRead"  ,"","PicoChain","picoDst","StPicoDstMaker",""           ,"WritesRead picoDST format",kFALSE},
   {"PicoVtxDefault" ,"","",""                                       ,"" ,"","pico Vtx default mode",kFALSE},
   {"PicoVtxVpd"     ,"","","-PicoVtxDefault"             ,"" ,"","pico Vtx cut on Tof and VPD mode",kFALSE},
