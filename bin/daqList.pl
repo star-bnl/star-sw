105a21685d1cd405d961052a8c7c4d8f39c0109c
Yuri Fisyak <fisyak@bnl.gov>
2022-02-16 15:52:55 -0500

    Update

--- bin/daqList.pl
+++ bin/daqList.pl
@@ -1,18 +1,29 @@
 #! /usr/bin/env perl
 use File::Basename;
 use Cwd;
-my $glob = "/net/l404/data/fisyak/daq/2016/1\*\*/\*/\*adc\*\.daq";# $ARGV[0];
-my @list = glob $glob;# print "$glob => @list";
+my $debug = 0;
+#my $glob = "/net/l404/data/fisyak/daq/2016/1\*\*/\*/\*adc\*\.daq";# $ARGV[0];
+my $glob = $ARGV[0]; print "glob = $glob\n" if ($debug);
+my @list = glob $glob; print "$glob => @list\n" if ($debug);
 foreach my $fullpath (@list) {
    my $base = File::Basename::basename($fullpath,".daq");
-#   print "$fullpath => $base\n";
+   print "$fullpath => $base => " if ($debug);
  #  if ($rootfile =~ m/$BadFiles/) {next;}
    my $rootfile = $base . ".event.root";
-   if (-r $rootfile) {next;}
+   if (-r $rootfile) {
+     print "$rootfile exists\n" if ($debug);
+     next;
+   }
    my $mufile   = $base . ".MuDst.root";
-   if (-r $mufile) {next;}
+   if (-r $mufile) {
+     print "$mufile exists\n" if ($debug);
+     next;
+   }
    my $logfile  = $base . "B.log";
-   if (-r $logfile) {next;}
-  print "string:$fullpath\n";
+   if (-r $logfile) {
+     print "$logfile exists\n" if ($debug);
+     next;
+   }
+   print "string:$fullpath\n";
 #  last;
 }
