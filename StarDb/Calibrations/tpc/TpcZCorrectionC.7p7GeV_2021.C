25648c180e9ac4c9c3b0680a60729fac8f15ee56
Yuri Fisyak <fisyak@bnl.gov>
2022-05-16 17:46:14 -0400

    Update from last calibration production

--- StarDb/Calibrations/tpc/TpcZCorrectionC.7p7GeV_2021.C
+++ StarDb/Calibrations/tpc/TpcZCorrectionC.7p7GeV_2021.C
@@ -9,9 +9,9 @@ TDataSet *CreateTable() {
   row.min =  16.5;
   row.max = 208.0;
   row.npar =            3;// 7p7GeV_2021
-  row.a[0] =     0.014847;
-  row.a[1] =   1.0452e-05;
-  row.a[2] =  -9.3952e-07;
+  row.a[0] =      0.01731;
+  row.a[1] =   -8.492e-06;
+  row.a[2] =  -9.5361e-07;
   tableSet->AddAt(&row);
   memset(&row,0,tableSet->GetRowSize());
   row.idx   = 2;
@@ -19,12 +19,12 @@ TDataSet *CreateTable() {
   row.min =  16.5;
   row.max = 208.0;
   row.npar =            6;// 7p7GeV_2021
-  row.a[0] =     -0.01594;
-  row.a[1] =    0.0034435;
-  row.a[2] =  -5.4775e-05;
-  row.a[3] =   3.3478e-07;
-  row.a[4] =  -1.0058e-09;
-  row.a[5] =   1.2267e-12;
+  row.a[0] =    0.0048486;
+  row.a[1] =     0.003154;
+  row.a[2] =  -5.3261e-05;
+  row.a[3] =   3.2283e-07;
+  row.a[4] =  -9.4024e-10;
+  row.a[5] =   1.1097e-12;
   tableSet->AddAt(&row);
   memset(&row,0,tableSet->GetRowSize());
   row.idx   = 3;
@@ -32,9 +32,9 @@ TDataSet *CreateTable() {
   row.min =  16.5;
   row.max = 208.0;
   row.npar =            3;// 7p7GeV_2021
-  row.a[0] =     0.017326;
-  row.a[1] =     -2.1e-05;
-  row.a[2] =  -8.4514e-07;
+  row.a[0] =     0.019485;
+  row.a[1] =  -3.3934e-05;
+  row.a[2] =  -8.7877e-07;
   tableSet->AddAt(&row);
   memset(&row,0,tableSet->GetRowSize());
   row.idx   = 4;
@@ -42,12 +42,12 @@ TDataSet *CreateTable() {
   row.min =  16.5;
   row.max = 208.0;
   row.npar =            6;// 7p7GeV_2021
-  row.a[0] =    -0.020817;
-  row.a[1] =    0.0037993;
-  row.a[2] =  -5.6193e-05;
-  row.a[3] =   2.9104e-07;
-  row.a[4] =  -6.3872e-10;
-  row.a[5] =   4.4486e-13;
+  row.a[0] =     0.015399;
+  row.a[1] =    0.0026149;
+  row.a[2] =  -3.6692e-05;
+  row.a[3] =    1.148e-07;
+  row.a[4] =   1.2642e-10;
+  row.a[5] =  -8.0028e-13;
   tableSet->AddAt(&row);
   return (TDataSet *)tableSet;
 }
