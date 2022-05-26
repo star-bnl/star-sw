2f01c35b2867dc0a34ef5acdd0c43d235e05a1c8
klendathu2k <jwebb@bnl.gov>
2022-05-26 16:00:45 -0400

    Cleanup linkdef

--- StarVMC/StarAgmlLib/StarAgmlLibLinkDef.h
+++ StarVMC/StarAgmlLib/StarAgmlLibLinkDef.h
@@ -1,20 +1,10 @@
-/* This was generated for version 'SL10k' */
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #ifdef __CINT__
-//#pragma link C++ class AgAttribute+;
-//#pragma link C++ class AgBlock+;
-//#pragma link C++ class AgCreate+;
-//#pragma link C++ class AgDetp+;
-//#pragma link C++ class AgMaterial+;
-//#pragma link C++ class AgMedium+;
-//#pragma link C++ class AgModule+;
-//#pragma link C++ class AgPosition+;
-//#pragma link C++ class AgShape+;
-//#pragma link C++ class AgStructure+;
-//#pragma link C++ class AgVolume+;
 #pragma link C++ class StarAgmlStacker+;
 #pragma link C++ class StarTGeoStacker+;
 #pragma link C++ class AgMLExtension+;
 #endif
+
+
