46e493549596998f1b699c19a1412203ae321b56
Philipp Weidenkaff <weidenkaff@rcas6009.rcf.bnl.gov>
2022-05-30 10:37:42 -0400

    added alignment readout from database

--- StRoot/StETofUtil/StETofGeometry.h
+++ StRoot/StETofUtil/StETofGeometry.h
@@ -47,6 +47,8 @@
 
 #include "StETofUtil/StETofConstants.h"
 
+#include "StMaker.h"
+
 class TNamed;
 
 class StETofHit;
@@ -288,9 +290,9 @@ public:
     StETofGeometry( const char* name = "etofGeo", const char* title = "simplified ETOF Geometry" );
     ~StETofGeometry();
 
-    void init( TGeoManager* geoManager );
-    void init( TGeoManager* geoManager, const double* safetyMargins );
-    void init( TGeoManager* geoManager, const double* safetyMargins, const bool& useHelixSwimmer );
+    void init( TGeoManager* geoManager ); //does not load alignment from local file or database
+    void init( TGeoManager* geoManager, const double* safetyMargins ); //does not load alignment from local file or database
+    void init( StMaker* maker, TGeoManager* geoManager, const double* safetyMargins, const bool& useHelixSwimmer );
 
     void reset();
 
@@ -363,6 +365,7 @@ public:
     unsigned int      nValidModules() const;
 
 	 void readAlignmentParameters();
+	 void readAlignmentDatabase(StMaker* maker); 
 	 void setFileNameAlignParam(std::string FileNameAlignParam);
 
     void debugOn();
