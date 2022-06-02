d67a1048ff9a22a7bdd7911f0a3ce6f095dddd46
Yuri Fisyak <fisyak@bnl.gov>
2022-06-02 11:08:13 -0400

    Freeze

--- bin/MakedEdx.pl
+++ bin/MakedEdx.pl
@@ -2427,7 +2427,8 @@ my $step = 0;
 #$hist = "RunXIX_XXII_05"; $NEvents = 1000; $disk = "/hlt/cephfs/";  $RECO = "reco/20*/*F"; $Production = "/TFG22b"; $year = "/*GeV*/*/*/*"; $FILE = ""; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";#  05/10/22 new TpcSecRow & TpcLengthCorrectionMDN
 #$hist = "RunXIX_XXII_06"; $NEvents = 1000; $disk = "/hlt/cephfs/";  $RECO = "reco/20*/*F"; $Production = "/TFG22b"; $year = "/*GeV*/*/*/*"; $FILE = ""; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";#  05/16/22 MySQL versions
 #$hist = "RunXIX_XXII_06"; $NEvents = 1000; $disk = "/hlt/cephfs/";  $RECO = "reco/20*/*F"; $Production = "/TFG22b"; $year = "/*AuAu200*/*/*/*"; $FILE = ""; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";#  05/16/22 MySQL versions
-$hist = "RunXIX_XXII_07"; $NEvents = 1000; $disk = "/hlt/cephfs/";  $RECO = "reco/20*/*F"; $Production = "/TFG22b"; $year = "/*GeV*/*/*/*"; $FILE = ""; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";#  05/24/22 more MySQL versions
+#$hist = "RunXIX_XXII_07"; $NEvents = 1000; $disk = "/hlt/cephfs/";  $RECO = "reco/20*/*F"; $Production = "/TFG22b"; $year = "/*GeV*/*/*/*"; $FILE = ""; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";#  05/24/22 more MySQL versions
+$hist = "RunXIX_XXII_08"; $NEvents = 1000; $disk = "/hlt/cephfs/";  $RECO = "reco/20*/*F"; $Production = "/TFG22b"; $year = "/*GeV*/*/*/*"; $FILE = ""; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";#  05/26/22 Check after fix with SecRow3 +/- and MIP selection
 
 my $Year = $year;
 if ($Year eq "/") {$Year = "2020";}
