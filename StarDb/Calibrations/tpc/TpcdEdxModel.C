bde5603beabac98f0fade14ea3756b4a6b2d1a29
Yuri Fisyak <fisyak@bnl.gov>
2022-06-29 10:18:45 -0400

    reeze with scew normal distribution

--- StarDb/Calibrations/tpc/TpcdEdxModel.C
+++ StarDb/Calibrations/tpc/TpcdEdxModel.C
@@ -2,8 +2,9 @@ TDataSet *CreateTable() {
 // ------  Test whether this table share library was loaded ------
   if (!gROOT->GetClass("St_tpcCorrection")) return 0;
   tpcCorrection_st row;
-  Int_t nrows = 2;
+  Int_t nrows = 3;
   St_tpcCorrection *tableSet = new St_tpcCorrection("TpcdEdxModel",nrows);
+#if 0
   //    root.exe */lnePI+lnePOGPAdcSparseD4.root 'Chain.C("FitP")'
   /*    z = log(log(ne/nP)) versus log(nP), z is supposed to be Gaussian with mu and sigma depnedending on log(nP) 
     tChain->Draw("mu:x","i&&j&&dmu<0.01&&dsigma<0.01&&abs(mu)<0.4","profs")
@@ -53,6 +54,57 @@ TDataSet *CreateTable() {
   row.a[1] =  3.17166e-02;
   row.a[2] = -1.57127e-02;
   tableSet->AddAt(&row); // sigma versus log(nP)
+#else
+  /* 
+     /star/u/fisyak/work/Tpc/TpcRS/2019/TpcRS_COL2/Fit?
+     root.exe ne*.root 'Chain.C("FitP")
+
+    mu =  -2.08815e+01 + xx*(  2.57192e+01 + xx*( -1.17647e+01 + xx*(  2.59742e+00 + xx*( -2.77836e-01 + xx*  1.15642e-02))));
+Fit: tChain->Draw("sigma:x>>sigma(40,3,7,100,0.1,0.6)","i&&j&&dmu<0.01&&a0>-0.1&&a0<3.9&&mu>0.65&&sigma>0.1","")
+    sigma =        1.10043e+00   + xx * (-2.14927e-01   + xx * 9.77528e-03);
+    alpha =    -22.6936   + xx*(    16.2998   + xx*(   -3.28817   + xx*   0.209148 ));
+   */
+
+  memset(&row,0,tableSet->GetRowSize());
+  row.idx   = 1;
+  row.type  = 0;
+  row.nrows = nrows;
+  row.min =     3.1;
+  row.max =     6.5;
+  row.npar =           6;// 
+  row.a[0] =  -2.08815e+01;
+  row.a[1] =   2.57192e+01;
+  row.a[2] =  -1.17647e+01;
+  row.a[3] =   2.59742e+00;
+  row.a[4] =  -2.77836e-01;
+  row.a[5] =   1.15642e-02;
+  tableSet->AddAt(&row); // mu 
+
+  memset(&row,0,tableSet->GetRowSize());
+  row.idx   = 2;
+  row.type  = 0;
+  row.nrows = nrows;
+  row.min =     3.1;
+  row.max =     6.5;
+  row.npar =           3;// 
+  row.a[0] =   1.10043e+00;
+  row.a[1] =  -2.14927e-01;
+  row.a[2] =   9.77528e-03;
+  tableSet->AddAt(&row); // sigma
+
+  memset(&row,0,tableSet->GetRowSize());
+  row.idx   = 3;
+  row.type  = 0;
+  row.nrows = nrows;
+  row.min =     3.1;
+  row.max =     6.5;
+  row.npar =           4;// 
+  row.a[0] =  -22.6936;
+  row.a[1] =   16.2998;
+  row.a[2] =  -3.28817;
+  row.a[3] =   0.209148;
+  tableSet->AddAt(&row); // alpha
+#endif
   // ----------------- end of code ---------------
   return (TDataSet *)tableSet;
 }
