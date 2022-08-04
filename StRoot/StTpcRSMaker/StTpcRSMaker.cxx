7e052daf22bf42e12f11a7b4a99c1aae5e02cf47
Yuri Fisyak <fisyak@bnl.gov>
2022-08-04 09:21:16 -0400

    fix typo

--- StRoot/StTpcRSMaker/StTpcRSMaker.cxx
+++ StRoot/StTpcRSMaker/StTpcRSMaker.cxx
@@ -1379,7 +1379,7 @@ Double_t StTpcRSMaker::GetNoPrimaryClusters(Double_t betaGamma, Int_t charge) {
 #endif
   Double_t Q_eff = TMath::Abs(charge%100);
   if (Q_eff > 1)   {
-    // Effective charge from GEANT ghion.F
+    // Effective charge from GEANT gthion.F
     Double_t w1 = 1.034 - 0.1777*TMath::Exp(-0.08114*Q_eff);
     Double_t w2 = beta*TMath::Power(Q_eff,-2./3.);
     Double_t w3 = 121.4139*w2 + 0.0378*TMath::Sin(190.7165*w2);
