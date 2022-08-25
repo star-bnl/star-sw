4a51699f2acde63d20c1c94858dad9f914ef5a8b
nigmatkulov <nigmatkulov@gmail.com>
2022-08-25 19:01:36 +0300

    Add official centrality calibration for 14.6 GeV Run 19 (#384)
    * General update of the StRefMultCorr package with 14.6 GeV centrality
    - Add centrality for 14.6 GeV year 2019.
    - Fix parameter swap for trigger efficiency calculation.
    - Add Param.cxx (in order to compile in a standalone mode)
    - Add Makefile to build standalone library (works only with ROOT6)
    
    * Shape correction for 135<=Vz (cm)<=145 added
    
    * Update shape parameters
    
    * Update trigger efficiency parameters for 14.6 GeV from Run19.
    
    * Weird missing comma was added.
    
    * Added Zachary Sweger as a co-maintainer of StRefMultCorr package.
    
    * Add Rtypes to define Int_t and so on.
    
    * Size of mParamStr_ref1 was incremented.
    
    * Fix stupid typo.
    
    * Try to avoid ROOT data types in Param.
    
    * Param.h(cxx) is based on pure C++ data types
    
    * Edited typo in event weight paramters
    
    Co-authored-by: Grigory Nigmatkulov <nigmaktulov@gmail.com>
    Co-authored-by: Zachary W Sweger <sweger@rcas6008.rcf.bnl.gov>

--- StRoot/StRefMultCorr/StRefMultCorr.h
+++ StRoot/StRefMultCorr/StRefMultCorr.h
@@ -35,6 +35,7 @@
 #include <map>
 
 // ROOT headers
+#include "Rtypes.h"
 #include "TString.h"
 #include "BadRun.h"
 
@@ -197,7 +198,17 @@ class StRefMultCorr {
   std::vector<std::string> StringSplit( const std::string str, const char sep ) const;
 
   // Read scale factor from header file
-  void readScaleForWeight(const Int_t nRefmultBin, const Double_t *weight) ;
+  void readScaleForWeight(const Int_t nRefmultBin, const Double_t *weight);
+
+  /// Calculate maximal or minimal refMult value for pile-up 
+  /// rejection (b parameters for refMultMax, c parameters for refMultMin)
+  Double_t calcPileUpRefMult(Double_t ntofmatch, Double_t x0, Double_t x1, 
+                             Double_t x2, Double_t x3, Double_t x4) const;
+  /// Check if refMult is between refMultLow and refMultHi values (for pile-up rejection)
+  Bool_t isInPileUpRefMultLimits(Double_t refMult, Double_t low, Double_t hi) const 
+  { return ( low < refMult && refMult < hi); }
+
+
 
   ClassDef(StRefMultCorr, 0)
 };
