f657b15b0667f16d1c7e09347d1963beaa210e84
Grigory Nigmatkulov <nigmatkulov@gmail.com>
2022-08-27 20:39:39 +0300

    Add README.md and LinkDef file (needed for standalone compilation)

---
+++ StRoot/StRefMultCorr/StRefMultCorrLinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// Main PicoDst classes
+#pragma link C++ class StRefMultCorr+;
+#pragma link C++ class StCentralityMaker+;
+
+#endif
