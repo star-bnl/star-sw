58000fc0de58faad8eca66f6f9e144762bee8ee8
Yuri Fisyak <fisyak@bnl.gov>
2022-09-04 16:34:52 -0400

    comment out D__TFG__VERSION__

--- StRoot/StMuDSTMaker/COMMON/macros/lMuDst.C
+++ StRoot/StMuDSTMaker/COMMON/macros/lMuDst.C
@@ -1,5 +1,5 @@
 #include "Riostream.h"
-void lMuDst(Int_t opt = -2, const Char_t *input = "", const Char_t *copt = "StEvent,Stu,RMuDst,picoEvt,mysql,tpcDb,magF,nodefault,quiet",const Char_t *tfile =  0,const Char_t *ofile =  0) {
+void lMuDst(Int_t opt = -2, const Char_t *input = "", const Char_t *copt = "StEvent,Stu,RMuDst,picoEvt,mysql,tpcDb,magF,nodefault",const Char_t *tfile =  0,const Char_t *ofile =  0) {
 #if !defined(__CINT__)
   std::cout << "This code cannot be compiled" << std::endl;
 #else
@@ -7,8 +7,8 @@ void lMuDst(Int_t opt = -2, const Char_t *input = "", const Char_t *copt = "StEv
   TString Chain(copt);
   TString STAR_VERSION(gSystem->Getenv("STAR_VERSION"));
   if (STAR_VERSION == ".DEV2" || STAR_VERSION.Contains("TFG")) {
-    Chain += ",TMVARank";
-    gSystem->AddIncludePath(" -D__TFG__VERSION__");
+    Chain += ",quiet,TMVARank";
+    //    gSystem->AddIncludePath(" -D__TFG__VERSION__");
   }
   bfc(opt,Chain,input,ofile,tfile);
   //  gROOT->LoadMacro("FitP_t.h+");
