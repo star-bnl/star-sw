156f48b4a436349757fc69fef6a4bb30c1ce2c51
Yuri Fisyak <fisyak@bnl.gov>
2022-09-26 19:05:28 -0400

    expand particle list

--- StRoot/StEvent/StPidParticleDefinition.h
+++ StRoot/StEvent/StPidParticleDefinition.h
@@ -11,6 +11,14 @@ enum StPidParticle {
     kPidTriton   ,
     kPidHe3      ,
     kPidAlpha    ,
+    kPidHe6      ,
+    kPidLi5      ,
+    kPidLi6      ,
+    kPidLi7      ,
+    kPidBe7      ,
+    kPidBe9      ,
+    kPidBe10     ,
+    kPidB11      ,
     KPidParticles
 };
 #endif /* __StPidParticleDefinition_h__ */
