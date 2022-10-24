star-bnl/star-sw

commit 17852935822c9aee06f78fe6f8c374722ee9fbe5

Add good event flag to ETOF headers and StPicoEvent (#160)
* fixed 64b integer conversion in getField() method and added additional setters from etofIn001 version of this file. Needed to unpack Get4Status bit into event files

* merged with group development to have support for local run-by-run calibrations in the repository version. Nessecary only for creating calibrations, but should be available in the offical code. Added calculation of goodEventFlag for every counter from Get4 missmatch bits and pulser digis.

* merged with group development to have support for local run-by-run calibrations in the repository version. Nessecary only for creating calibrations, but should be available in the offical code. Added calculation of goodEventFlag for every counter from Get4 missmatch bits and pulser digis.

* <strstring.h> header include needed to be added for local compilation (?)

* added eTofGoodEventFlag to mark counters which are good foer analysis in each event

* moved struct to public to avoid root6 issue. Changed class dev version back to 0

* added kStFatal return in ::init if calib histo files are corrupt

* Changed class dev version back to 0

* Trying to fix merger

* Trying to fix merger

* added eTofGoodEventFlag to mark counters which are good foer analysis in each event

* moved struct to public to avoid root6 issue. Changed class dev version back to 0

* added kStFatal return in ::init if calib histo files are corrupt

* Changed class dev version back to 0

* decremented classDef version of StEtofCollection. Makers compile correct header version after rebase without increment

* removed double line in MuEtofHeader constructor

* included eTOF constants header and moved constants here

* included eTOF constants header and moved constants here

* added constant nbGet4s here for use in event headers

* Removed strstr include in StMuDstMaker.cxx. Now indentical to repository version

StRoot/StPicoEvent/StPicoEvent.cxx
@@ -36,7 +36,7 @@ StPicoEvent::StPicoEvent(): TObject(),
   mZdcSumAdcEast(0), mZdcSumAdcWest(0),
   mZdcSmdEastHorizontal{}, mZdcSmdEastVertical{}, mZdcSmdWestHorizontal{}, mZdcSmdWestVertical{},
   mBbcAdcEast{}, mBbcAdcWest{}, mHighTowerThreshold{}, mJetPatchThreshold{},
-  mETofHitMultiplicity(0), mETofDigiMultiplicity(0), mNumberOfPrimaryTracks(0), mZdcUnAttenuated{} {
+  mETofHitMultiplicity(0), mETofDigiMultiplicity(0), mETofGoodEventFlag{}, mNumberOfPrimaryTracks(0), mZdcUnAttenuated{} {
 
   // Default constructor
   if( !mTriggerIds.empty() ) {
@@ -145,6 +145,10 @@ StPicoEvent::StPicoEvent(const StPicoEvent &event) : TObject() {
   for(int iIter=0; iIter<4; iIter++) {
     mHighTowerThreshold[iIter] = event.mHighTowerThreshold[iIter];
     mJetPatchThreshold[iIter] = event.mJetPatchThreshold[iIter];
+  }
+  
+  for(int iIter=0; iIter<108; iIter++) {
+    mETofGoodEventFlag[iIter] = event.mETofGoodEventFlag[iIter];
   }
 }
 
@@ -332,4 +336,32 @@ void StPicoEvent::setBunchId(Int_t id) {
 		      std::numeric_limits<decltype(mBunchCrossId)>::max() :
 		      (UChar_t)id );
   }
+}
+
+//_________________
+bool StPicoEvent::eTofGoodEventFlag( UShort_t iSector, UShort_t iModule,  UShort_t iCounter ) const {
+	if( iSector < 13 || iSector > 24 ){
+		    LOG_INFO << "StPicoEvent::eTofGoodEventFlag() - non-existing sector id " << iSector <<"  -> return false"<< endm;
+		    return false;
+	}
+	if( iModule < 1 || iModule > 3 ){
+		    LOG_INFO << "StPicoEvent::eTofGoodEventFlag() - non-existing module id " << iModule <<"  -> return false"<< endm;
+		    return false;
+	}
+	if( iCounter < 1 || iCounter > 3 ){
+	    LOG_INFO << "StPicoEvent::eTofGoodEventFlag() - non-existing counter id " << iCounter <<"  -> return false"<< endm;
+	    return false;
+	}
+
+	return (bool) mETofGoodEventFlag[ 9*(iSector-13) + 3*(iModule-1) + (iCounter-1) ];
+}
+//_________________
+void StPicoEvent::setETofGoodEventFlag( std::vector<bool> flagVec ) {
+	if( flagVec.size() != 108 ){
+	    LOG_INFO << "StPicoEvent::setETofGoodEventFlag() - eTof flag vector wrong size " << flagVec.size() <<" / 108"<< endm;
+	}else{
+		for( auto iterCounter : flagVec ){
+			 mETofGoodEventFlag[iterCounter] = flagVec[iterCounter];
+		}
+	}
 }

StRoot/StETofCalibMaker/StETofCalibMaker.h
@@ -80,13 +80,26 @@ public:
     void setFileNameTimingWindow(       const char* fileName );
     void setFileNameSignalVelocity(     const char* fileName );
     void setFileNameCalibHistograms(    const char* fileName );
+    void setFileNameOffsetHistograms(   const char* fileName );
     void setFileNameResetTimeCorr(      const char* fileName );
     void setFileNamePulserTotPeak(      const char* fileName );
     void setFileNamePulserTimeDiffGbtx( const char* fileName );
 
     void setDoQA(  const bool doQA  );
     void setDebug( const bool debug );
+    void setStrictPulserHandling( const bool debug );
     void setReferencePulserIndex( const int index );
+    
+    //moved to public to avoid problem with root6
+    struct StructStuckFwDigi{
+		  Int_t     geomId;
+		  Double_t  time;
+		  Double_t  tot;
+
+		  bool operator==( const StructStuckFwDigi& r ) const {
+		      return geomId == r.geomId && fabs( time - r.time ) < 1.e-5 && fabs( tot - r.tot ) < 1.e-5;
+		  }
+    };
 
 
 private:
@@ -136,6 +149,7 @@ private:
     std::string   mFileNameTimingWindow;        // name of parameter file for timing window
     std::string   mFileNameSignalVelocity;      // name of parameter file for signal velocity
     std::string   mFileNameCalibHistograms;     // name of parameter file for calibration histograms (output of QA maker)
+    std::string   mFileNameOffsetHistograms;    // name of parameter file for run by run offsets histograms (output of QA maker)
     std::string   mFileNameResetTimeCorr;       // name of parameter file for reset time correction
     std::string   mFileNamePulserTotPeak;       // name of parameter file for pulser peak tot
     std::string   mFileNamePulserTimeDiffGbtx;  // name of parameter file for pulser time diff
@@ -146,7 +160,8 @@ private:
     Float_t       mResetTimeCorr;           // additional offset for the whole system in case reset time was not saved correctly
 
     Double_t      mTriggerTime;             // trigger time in ns
-    Double_t      mResetTime;               // reset time in ns 
+    Double_t      mResetTime;               // reset time in ns
+    Long64_t      mResetTs;                 // reset time stamp in clock ticks
 
     Float_t       mPulserPeakTime;          // pulser peak time relative to the trigger time in ns
     Int_t         mReferencePulserIndex;    // index of reference pulser used in the pulser correction to correct time offset between different Gbtx
@@ -162,15 +177,26 @@ private:
 
     std::map< UInt_t, Float_t >   mPulserPeakTot;       // TOT of pulsers on each side of the RPC counters (as key)
     std::map< UInt_t, Double_t >  mPulserTimeDiff;      // pulser time difference with respect to the reference pulser for each detector side as key
-    std::map< UInt_t, Double_t >  mPulserTimeDiffGbtx;  // pulser time difference with inside a Gbtx with respect to counter 1
+    std::map< UInt_t, Double_t >  mPulserTimeDiffGbtx;  // pulser time difference with inside a Gbtx with respect to counter 1    
+    std::map< UInt_t, UInt_t >   	 mNPulsersCounter;     // number of found pulsers on each counter. Has to be 2 for good the counter to be considered good in this event. 
+    std::map< UInt_t, UInt_t >   	 mNStatusBitsCounter;  // number of Get4 status bits on each counter. Has to be 0 for good the counter to be considered good in this event.     
+    std::map< UInt_t, Bool_t >    mPulserPresent;       // map of present pulsers for each event (by counter side)
 
-    std::map< UInt_t, UInt_t >    mJumpingPulsers;      // flag jumping pulsers
+    std::map< UInt_t, Int_t >     mJumpingPulsers;      // flag jumping pulsers
 
+    std::map< UInt_t, Int_t >     mUnlockPulserState;   // map that counts pulser offsets to avoid locking into the wrong pulser offset state
+
+    std::vector< StructStuckFwDigi > mStuckFwDigi; // list of digis to ignore for the rest of the run due to stuck firmware
+
+	 Bool_t 			mStrictPulserHandling;
     Bool_t        mUsePulserGbtxDiff;
     Bool_t        mDoQA;
     Bool_t        mDebug;
     std::string                    mHistFileName;
     std::map< std::string, TH1* >  mHistograms;
+
+
+
 
 
     virtual const Char_t *GetCVS() const { static const char cvs[]="Tag $Name:  $Id: built " __DATE__ " " __TIME__ ; return cvs; }
@@ -184,6 +210,7 @@ inline void StETofCalibMaker::setFileNameStatusMap(          const char* fileNam
 inline void StETofCalibMaker::setFileNameTimingWindow(       const char* fileName )     { mFileNameTimingWindow       = fileName; }
 inline void StETofCalibMaker::setFileNameSignalVelocity(     const char* fileName )     { mFileNameSignalVelocity     = fileName; }
 inline void StETofCalibMaker::setFileNameCalibHistograms(    const char* fileName )     { mFileNameCalibHistograms    = fileName; }
+inline void StETofCalibMaker::setFileNameOffsetHistograms(   const char* fileName )     { mFileNameOffsetHistograms   = fileName; } 
 inline void StETofCalibMaker::setFileNameResetTimeCorr(      const char* fileName )     { mFileNameResetTimeCorr      = fileName; }
 inline void StETofCalibMaker::setFileNamePulserTotPeak(      const char* fileName )     { mFileNamePulserTotPeak      = fileName; }
 inline void StETofCalibMaker::setFileNamePulserTimeDiffGbtx( const char* fileName )     { mFileNamePulserTimeDiffGbtx = fileName; }
@@ -191,6 +218,7 @@ inline void StETofCalibMaker::setFileNamePulserTimeDiffGbtx( const char* fileNam
 
 inline double StETofCalibMaker::resetTimeCorr() const { return mResetTimeCorr; }
 
+inline void StETofCalibMaker::setStrictPulserHandling(  const bool StrictPulserHandling  ) { mStrictPulserHandling  = StrictPulserHandling;  }
 inline void StETofCalibMaker::setDoQA(  const bool doQA  ) { mDoQA  = doQA;  }
 inline void StETofCalibMaker::setDebug( const bool debug ) { mDebug = debug; }
 inline void StETofCalibMaker::setReferencePulserIndex( const int index ) { mReferencePulserIndex = index; }

StRoot/StETofUtil/StETofConstants.h
@@ -57,6 +57,7 @@ namespace eTofConst{
     const int nChannelsPerSector  = nChannelsPerModule  * nPlanes;
     const int nChannelsInSystem   = nChannelsPerSector  * nSectors;
 
+    const int nGet4sInSystem   	 = nChannelsInSystem / 4;
 
     // clock related constants
     const double coarseClockCycle  = 6.25;   // [ns]  -- needs to be double otherwise strange things happen ...

StRoot/StEvent/StETofHeader.cxx
@@ -24,6 +24,8 @@
  *
  ***************************************************************************/
 #include "StETofHeader.h"
+#include "StETofUtil/StETofConstants.h"
+
 #include <map>
 #include <vector>
 #include <iostream>
@@ -34,7 +36,9 @@ StETofHeader::StETofHeader()
   mStarToken( 0 ),
   mStarDaqCmdIn( 0 ),
   mStarTrgCmdIn( 0 ),
-  mEventStatusFlag( 0 )
+  mEventStatusFlag( 0 ),
+  mMissMatchFlagVec( eTofConst::nGet4sInSystem, false ),
+  mGoodEventFlagVec( eTofConst::nCountersInSystem, false )
 {
     mRocGdpbTs.clear();
     mRocStarTs.clear();
@@ -50,34 +54,46 @@ StETofHeader::StETofHeader( const double& trgGdpbTime, const double& trgStarTime
   mStarToken( starToken ),
   mStarDaqCmdIn( starDaqCmdIn ),
   mStarTrgCmdIn( starTrgCmdIn ),
-  mEventStatusFlag( eventStatusFlag )
+  mEventStatusFlag( eventStatusFlag ),
+  mMissMatchFlagVec( eTofConst::nGet4sInSystem, false ),
+  mGoodEventFlagVec( eTofConst::nCountersInSystem, false )
 {
     setRocGdpbTs( gdpbTs );
     setRocStarTs( starTs );
-    const size_t kNbGet4sInSystem = 1728;
-    mMissMatchFlagVec = vector<bool>( kNbGet4sInSystem, false ); 
 }
 
 StETofHeader::StETofHeader( const double& trgGdpbTime, const double& trgStarTime,
                             const map< unsigned int, uint64_t >& gdpbTs, const map< unsigned int, uint64_t >& starTs,
                             const unsigned int& starToken, const unsigned int& starDaqCmdIn, const unsigned int& starTrgCmdIn,
-                            const uint64_t& eventStatusFlag, const vector<bool>& MissMatchFlagVec )
+                            const uint64_t& eventStatusFlag, const std::vector<bool>& MissMatchFlagVec )
 : mTrgGdpbFullTime( trgGdpbTime ),
   mTrgStarFullTime( trgStarTime ),
   mStarToken( starToken ),
   mStarDaqCmdIn( starDaqCmdIn ),
   mStarTrgCmdIn( starTrgCmdIn ),
   mEventStatusFlag( eventStatusFlag ),
-  mMissMatchFlagVec( MissMatchFlagVec )
+  mMissMatchFlagVec( MissMatchFlagVec ),
+  mGoodEventFlagVec( eTofConst::nCountersInSystem, false )
 {
     setRocGdpbTs( gdpbTs );
     setRocStarTs( starTs );
-    //const size_t kNbGet4sInSystem = 1728;
-    //mMissMatchFlagVec.resize( kNbGet4sInSystem );
-    //for( auto mapcheck : mMissMatchFlagVec ){
-    //	cout << mapcheck << endl;
-    // }
+}
 
+StETofHeader::StETofHeader( const double& trgGdpbTime, const double& trgStarTime,
+                            const map< unsigned int, uint64_t >& gdpbTs, const map< unsigned int, uint64_t >& starTs,
+                            const unsigned int& starToken, const unsigned int& starDaqCmdIn, const unsigned int& starTrgCmdIn,
+                            const uint64_t& eventStatusFlag, const std::vector<bool>& MissMatchFlagVec, const std::vector<bool>& GoodEventFlagVec  )
+: mTrgGdpbFullTime( trgGdpbTime ),
+  mTrgStarFullTime( trgStarTime ),
+  mStarToken( starToken ),
+  mStarDaqCmdIn( starDaqCmdIn ),
+  mStarTrgCmdIn( starTrgCmdIn ),
+  mEventStatusFlag( eventStatusFlag ),
+  mMissMatchFlagVec( MissMatchFlagVec ),
+  mGoodEventFlagVec( GoodEventFlagVec )
+{
+    setRocGdpbTs( gdpbTs );
+    setRocStarTs( starTs ); 
 }
 
 
@@ -142,10 +158,16 @@ StETofHeader::eventStatusFlag() const
     return mEventStatusFlag;
 }
 
-vector <bool>
+std::vector <bool>
 StETofHeader::missMatchFlagVec() const
 {
     return mMissMatchFlagVec;
+}
+
+std::vector <bool>
+StETofHeader::goodEventFlagVec() const
+{
+    return mGoodEventFlagVec;
 }
 
 void
@@ -201,4 +223,10 @@ void
 StETofHeader::setEventStatusFlag( const uint64_t& statusFlag )
 {
     mEventStatusFlag = statusFlag;
+}
+
+void    
+StETofHeader::setGoodEventFlagVec( const std::vector<bool>& FlagVec )
+{
+    mGoodEventFlagVec = FlagVec;
 }

StRoot/StEvent/StETofHeader.h
@@ -41,10 +41,21 @@ class StETofHeader : public StObject {
 public:
 
     StETofHeader();
+    /** 
+     ** @brief default constructor for pre-2020 data. No missmatch information available. Used in StEtofDigiMaker to initialise the header.
+     **/
     StETofHeader( const double&, const double&, const map< unsigned int, uint64_t >&, const map< unsigned int, uint64_t >& ,
                   const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t& );
+    /** 
+     ** @brief default constructor for post-2020 data. Include missmatch information from FEE. Used in StEtofDigiMaker to initialise the header.
+     **/              
     StETofHeader( const double&, const double&, const map< unsigned int, uint64_t >&, const map< unsigned int, uint64_t >& ,
-                  const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t&, const vector<bool>& );
+                  const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t&, const std::vector<bool>&  );
+    /** 
+     ** @brief Full constructor including goodEventFlag, which is normally set in calibrations only.
+     **/                
+    StETofHeader( const double&, const double&, const map< unsigned int, uint64_t >&, const map< unsigned int, uint64_t >& ,
+                  const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t&, const std::vector<bool>&, const std::vector<bool>&  );              
 
     ~StETofHeader();
 
@@ -57,9 +68,15 @@ public:
     unsigned int      starToken()         const;
     unsigned int      starDaqCmdIn()      const;
     unsigned int      starTrgCmdIn()      const;
-    uint64_t          eventStatusFlag()   const;
-    
-    vector<bool>       missMatchFlagVec()  const;
+    uint64_t          eventStatusFlag()   const;    
+    /** 
+     ** @brief Flag for each Get4 TDC to mark if it is available in this event.
+     **/     
+    std::vector<bool>       missMatchFlagVec()  const;
+    /** 
+     ** @brief Flag to mark if the event is good for physics analysis for each counter. A counter is considered good in each event when there are zero missmatch flags set and pulser digis on both sides are found. In this case, the counter should perform at its best. Counter efficiency should be constant between good events. 
+     **/      
+    std::vector<bool>       goodEventFlagVec()  const;
 
 
     void    setTrgGdpbFullTime( const double& gdpbFullTime );
@@ -73,6 +90,9 @@ public:
     void    setStarTrgCmdIn( const unsigned int& trgCmdIn );
 
     void    setEventStatusFlag( const uint64_t& statusFlag );
+    void    setGoodEventFlagVec( const std::vector<bool>& FlagVec );
+    void    setGoodEventFlagVec( int blubb ) {return;}
+    //    void    setGoodEventFlagVec( const std::vector<bool>& FlagVec );
 
 private:
     Double_t    mTrgGdpbFullTime;
@@ -87,9 +107,10 @@ private:
     
     ULong64_t   mEventStatusFlag;
 
-    vector< Bool_t > mMissMatchFlagVec; 
+    std::vector< Bool_t > mMissMatchFlagVec;
+    std::vector< Bool_t > mGoodEventFlagVec; 
 
-    ClassDef( StETofHeader, 2 )
+    ClassDef( StETofHeader, 3 )
 };
 
 #endif // STETOFHEADER_H

StRoot/StMuDSTMaker/COMMON/StMuETofHeader.cxx
@@ -22,6 +22,7 @@
  ***************************************************************************/
 #include "StMuETofHeader.h"
 #include "StETofHeader.h"
+#include "StETofUtil/StETofConstants.h"
 
 
 StMuETofHeader::StMuETofHeader()
@@ -30,7 +31,9 @@ StMuETofHeader::StMuETofHeader()
   mStarToken( 0 ),
   mStarDaqCmdIn( 0 ),
   mStarTrgCmdIn( 0 ),
-  mEventStatusFlag( 0 )
+  mEventStatusFlag( 0 ),
+  mMissMatchFlagVec( eTofConst::nGet4sInSystem, false ),
+  mGoodEventFlagVec( eTofConst::nCountersInSystem, false )
 {
   mRocGdpbTs.clear();
   mRocStarTs.clear();
@@ -46,28 +49,46 @@ StMuETofHeader::StMuETofHeader( const double& trgGdpbTime, const double& trgStar
   mStarToken( starToken ),
   mStarDaqCmdIn( starDaqCmdIn ),
   mStarTrgCmdIn( starTrgCmdIn ),
-  mEventStatusFlag( eventStatusFlag )
+  mEventStatusFlag( eventStatusFlag ),
+  mMissMatchFlagVec( eTofConst::nGet4sInSystem, false ),
+  mGoodEventFlagVec( eTofConst::nCountersInSystem, false )
 {
     setRocGdpbTs( gdpbTs );
     setRocStarTs( starTs );
-	 const size_t kNbGet4sInSystem = 1728;
-    mMissMatchFlagVec = std::vector< Bool_t >( kNbGet4sInSystem, false ); 
 }
 
 StMuETofHeader::StMuETofHeader( const double& trgGdpbTime, const double& trgStarTime,
                             const map< unsigned int, uint64_t >& gdpbTs, const map< unsigned int, uint64_t >& starTs,
                             const unsigned int& starToken, const unsigned int& starDaqCmdIn, const unsigned int& starTrgCmdIn,
-                            const uint64_t& eventStatusFlag, const vector< Bool_t >& MissMatchFlagVec  )
+                            const uint64_t& eventStatusFlag, const std::vector< Bool_t >& MissMatchFlagVec  )
 : mTrgGdpbFullTime( trgGdpbTime ),
   mTrgStarFullTime( trgStarTime ),
   mStarToken( starToken ),
   mStarDaqCmdIn( starDaqCmdIn ),
   mStarTrgCmdIn( starTrgCmdIn ),
   mEventStatusFlag( eventStatusFlag ),
-  mMissMatchFlagVec( MissMatchFlagVec )
+  mMissMatchFlagVec( MissMatchFlagVec ),
+  mGoodEventFlagVec( eTofConst::nCountersInSystem, false )
 {
     setRocGdpbTs( gdpbTs );
     setRocStarTs( starTs );
+}
+
+StMuETofHeader::StMuETofHeader( const double& trgGdpbTime, const double& trgStarTime,
+                            const map< unsigned int, uint64_t >& gdpbTs, const map< unsigned int, uint64_t >& starTs,
+                            const unsigned int& starToken, const unsigned int& starDaqCmdIn, const unsigned int& starTrgCmdIn,
+                            const uint64_t& eventStatusFlag, const std::vector<bool>& MissMatchFlagVec, const std::vector<bool>& GoodEventFlagVec  )
+: mTrgGdpbFullTime( trgGdpbTime ),
+  mTrgStarFullTime( trgStarTime ),
+  mStarToken( starToken ),
+  mStarDaqCmdIn( starDaqCmdIn ),
+  mStarTrgCmdIn( starTrgCmdIn ),
+  mEventStatusFlag( eventStatusFlag ),
+  mMissMatchFlagVec( MissMatchFlagVec ),
+  mGoodEventFlagVec( GoodEventFlagVec )
+{
+    setRocGdpbTs( gdpbTs );
+    setRocStarTs( starTs ); 
 }
 
 StMuETofHeader::StMuETofHeader( const StETofHeader* header )
@@ -77,7 +98,8 @@ StMuETofHeader::StMuETofHeader( const StETofHeader* header )
   mStarDaqCmdIn(    header->starDaqCmdIn()    ),
   mStarTrgCmdIn(    header->starTrgCmdIn()    ),
   mEventStatusFlag( header->eventStatusFlag() ),
-  mMissMatchFlagVec(header->missMatchFlagVec())
+  mMissMatchFlagVec(header->missMatchFlagVec()),
+  mGoodEventFlagVec(header->goodEventFlagVec())
 {
     setRocGdpbTs( header->rocGdpbTs() );
     setRocStarTs( header->rocStarTs() );
@@ -151,6 +173,12 @@ StMuETofHeader::missMatchFlagVec() const
     return mMissMatchFlagVec;
 }
 
+std::vector <bool>
+StMuETofHeader::goodEventFlagVec() const
+{
+    return mGoodEventFlagVec;
+}
+
 
 void
 StMuETofHeader::setTrgGdpbFullTime( const double& gdpbFullTime )
@@ -205,4 +233,10 @@ void
 StMuETofHeader::setEventStatusFlag( const uint64_t& statusFlag )
 {
     mEventStatusFlag = statusFlag;
+}
+
+void    
+StMuETofHeader::setGoodEventFlagVec( const std::vector<bool>& FlagVec )
+{
+    mGoodEventFlagVec = FlagVec;
 }

StRoot/StMuDSTMaker/COMMON/StMuETofHeader.h
@@ -35,11 +35,26 @@ class StMuETofHeader : public TObject {
 public:
 
     StMuETofHeader();
+     /** 
+     ** @brief Constructor for conversion from StEvent header.
+     **/    
     StMuETofHeader( const StETofHeader* header );
+     /** 
+     ** @brief default constructor for pre-2020 data. No missmatch information available. Used in StEtofDigiMaker to initialise the header.
+     **/
     StMuETofHeader( const double&, const double&, const std::map< unsigned int, uint64_t >&, const std::map< unsigned int, uint64_t >& ,
                     const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t& );
+     /** 
+     ** @brief default constructor for post-2020 data. Include missmatch information from FEE. Used in StEtofDigiMaker to initialise the header.
+     **/                
     StMuETofHeader( const double&, const double&, const std::map< unsigned int, uint64_t >&, const std::map< unsigned int, uint64_t >& ,
                     const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t&, const std::vector< Bool_t >& );
+     /** 
+     ** @brief Full constructor including goodEventFlag, which is normally set in calibrations only.
+     **/                
+    StMuETofHeader( const double&, const double&, const std::map< unsigned int, uint64_t >&, const std::map< unsigned int, uint64_t >& ,
+                  const unsigned int&, const unsigned int&, const unsigned int&, const uint64_t&, const std::vector<bool>&, 
+                  const std::vector<bool>&  );                     
 
     ~StMuETofHeader();
 
@@ -53,7 +68,14 @@ public:
     unsigned int      starDaqCmdIn()      const;
     unsigned int      starTrgCmdIn()      const;
     uint64_t          eventStatusFlag()   const;
-	 std::vector< Bool_t >  missMatchFlagVec()  const;
+    /** 
+     ** @brief Flag for each Get4 TDC to mark if it is available in this event.
+     **/     
+    std::vector<bool>       missMatchFlagVec()  const;
+    /** 
+     ** @brief Flag to mark if the event is good for physics analysis for each counter. A counter is considered good in each event when there are zero missmatch flags set and pulser digis on both sides are found. In this case, the counter should perform at its best. Counter efficiency should be constant between good events. 
+     **/      
+    std::vector<bool>       goodEventFlagVec()  const;
 
 
     void    setTrgGdpbFullTime( const double& gdpbFullTime );
@@ -67,6 +89,7 @@ public:
     void    setStarTrgCmdIn( const unsigned int& trgCmdIn );
 
     void    setEventStatusFlag( const uint64_t& statusFlag );
+    void    setGoodEventFlagVec( const std::vector<bool>& FlagVec );
 
 private:
     Double_t    mTrgGdpbFullTime;
@@ -81,9 +104,10 @@ private:
     
     ULong64_t   mEventStatusFlag;
 
-	 std::vector< Bool_t > mMissMatchFlagVec; 
+	 std::vector< Bool_t > mMissMatchFlagVec;
+	 std::vector< Bool_t > mGoodEventFlagVec;  
 
-    ClassDef( StMuETofHeader, 2 )
+    ClassDef( StMuETofHeader, 3 )
 };
 
 #endif // STMUETOFHEADER_H

commit 84803c6a7983b0ece7080f0fa022b06a51f616d1

Fix copy of eTOF good channel flags to PicoEvent (#416)
Fixes bug introduced in #160

Co-authored-by: PhilippWeidenkaff <weidenkaff@physi.uni-heildeberg.de>
Co-authored-by: Dmitri Smirnov <dmixsmi@gmail.com>

StRoot/StPicoEvent/StPicoEvent.cxx
@@ -360,8 +360,6 @@ void StPicoEvent::setETofGoodEventFlag( std::vector<bool> flagVec ) {
 	if( flagVec.size() != 108 ){
 	    LOG_INFO << "StPicoEvent::setETofGoodEventFlag() - eTof flag vector wrong size " << flagVec.size() <<" / 108"<< endm;
 	}else{
-		for( auto iterCounter : flagVec ){
-			 mETofGoodEventFlag[iterCounter] = flagVec[iterCounter];
-		}
+	    std::copy(flagVec.begin(), flagVec.end(), mETofGoodEventFlag);
 	}
 }