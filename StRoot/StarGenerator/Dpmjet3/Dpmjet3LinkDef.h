b18eb11fe8c1a1cb2c7c8f8d1d612e37fbad0bb2
Yuri Fisyak <fisyak@bnl.gov>
2022-12-22 15:57:04 -0500

    Remove generators

--- StRoot/StarGenerator/Dpmjet3/Dpmjet3LinkDef.h
+++
@@ -1,9 +0,0 @@
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-
-#ifdef __CINT__
-#pragma link C++ class StarDpmjet3+;
-#pragma link C++ struct DtEvt1_t+;
-#pragma link C++ struct DtGlcp_t+;
-#endif
