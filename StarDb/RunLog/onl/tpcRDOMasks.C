e7fe531ba159606656c108bab7f3aad1ea944026
Yuri Fisyak <fisyak@bnl.gov>
2023-01-06 17:28:32 -0500

    Back to star-sw version

--- StarDb/RunLog/onl/tpcRDOMasks.C
+++ StarDb/RunLog/onl/tpcRDOMasks.C
@@ -1,20 +1,14 @@
 TDataSet *CreateTable() { 
-// -----------------------------------------------------------------
-// bfc/.make/db/.const/StarDb/RunLog/onl/.tpcRDOMasks/tpcRDOMasks Allocated rows: 12  Used rows: 12  Row size: 12 bytes
-//  Table: tpcRDOMasks_st[0]--> tpcRDOMasks_st[11]
-// ====================================================================
-// ------  Test whether this table share library was loaded ------
   if (!TClass::GetClass("St_tpcRDOMasks")) return 0;
-tpcRDOMasks_st row;
-St_tpcRDOMasks *tableSet = new St_tpcRDOMasks("tpcRDOMasks",12);
-//
- for (Int_t s = 1; s <= 24; s += 2) {
-   memset(&row,0,tableSet->GetRowSize());
-   row.runNumber	 =   0; // run number  ;
-   row.sector	         =   s; // sector  ;
-   row.mask	         = 4095; // enable mask  ;
-   tableSet->AddAt(&row);
- }
-// ----------------- end of code ---------------
- return (TDataSet *)tableSet;
+  tpcRDOMasks_st row;
+  Int_t nrows = 12;
+  St_tpcRDOMasks *tableSet = new St_tpcRDOMasks("tpcRDOMasks",nrows);
+  memset(&row,0,tableSet->GetRowSize());
+  row.runNumber	 =    8178047; // run number  ;
+  row.mask	 =       4095; // enable mask  ;
+  for (Int_t i = 0; i < nrows; i++) {
+    row.sector	 =          2*i+1; // sector  ;
+    tableSet->AddAt(&row);
+  }
+  return (TDataSet *)tableSet;
 }
