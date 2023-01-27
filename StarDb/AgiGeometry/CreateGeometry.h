c9f1764a7e2894dfe630f07c08e0e864bcee7501
klendathu2k <jwebb@bnl.gov>
2023-01-26 20:53:40 -0500

    [root6] Fix cling warning: ISO C++11 does not allow conversion from string literal to 'Char_t *' (aka 'char *') [-Wwritable-strings]

--- StarDb/AgiGeometry/CreateGeometry.h
+++ StarDb/AgiGeometry/CreateGeometry.h
@@ -5,10 +5,10 @@ TDataSet *CreateGeometry(const Char_t *name="y2005") {
 	 << name << " ! " << endl;
     return geom;
   }
-  Char_t *path  = ".:./StarDb/AgiGeometry:$STAR/StarDb/AgiGeometry";
+  TString path  = ".:./StarDb/AgiGeometry:$STAR/StarDb/AgiGeometry";
   TString geomF(name); geomF += ".h";
-  Char_t *file = gSystem->Which(path,geomF,kReadPermission);
-  if (! file) Fatal("CreateGeometry","File %s has not found in path %s",geomF.Data(),path);
+  Char_t *file = gSystem->Which(path.Data(),geomF,kReadPermission);
+  if (! file) Fatal("CreateGeometry","File %s has not found in path %s",geomF.Data(),path.Data());
   else        Warning("CreateGeometry","File %s has been found as %s",geomF.Data(),file);
   TString command = ".L "; command += file;
   gInterpreter->ProcessLine(command);
