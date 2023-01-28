ac6f998040e301a0754fb8efdf21efca8f2895e6
Yuri Fisyak <fisyak@bnl.gov>
2023-01-28 18:00:01 -0500

    Freeze

--- StRoot/StTpcMcAnalysisMaker/FitTpcT.C
+++ StRoot/StTpcMcAnalysisMaker/FitTpcT.C
@@ -1,9 +1,14 @@
 /*
    root.exe -q *.H.root FitTpcT.C
- */
+*/
+#include "Riostream.h"
 void FitTpcT() {
+#if defined(__CINT__) && ! defined(__MAKECINT__)
   gROOT->LoadMacro("lBichsel.C");
   lBichsel();
   gROOT->LoadMacro("TpcT.C+");
   Fits();
+#else
+  cout << " FitTpcT.C does not work with AClick" << endl;
+#endif
 }
