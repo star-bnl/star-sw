64cac71fdb5ff80c03f328a3c96445fc2957b69e
klendathu2k <56083924+klendathu2k@users.noreply.github.com>
2023-01-30 09:52:39 -0500

    [root6] fixes to the legacy StarDb/AgiGeometry (#485)
    Fixes to the old AgiGeometry... needed to run, e.g., nightly test job 11
    (/star/u/starreco/devtest/sampleList_opt.txt)
    
    ---------
    
    Co-authored-by: Dmitri Smirnov <dmixsmi@gmail.com>

--- StarDb/AgiGeometry/CreateGeometry.h
+++ StarDb/AgiGeometry/CreateGeometry.h
@@ -3,12 +3,12 @@ TDataSet *CreateGeometry(const Char_t *name="y2005") {
   if (gGeoManager) {
     cout << "VMC geometry " << gGeoManager->GetName() << " has beed created. Ignore request for " 
 	 << name << " ! " << endl;
-    return geom;
+    return (TDataSet*)geom;
   }
-  Char_t *path  = ".:./StarDb/AgiGeometry:$STAR/StarDb/AgiGeometry";
+  TString path  = ".:./StarDb/AgiGeometry:$STAR/StarDb/AgiGeometry";
   TString geomF(name); geomF += ".h";
-  Char_t *file = gSystem->Which(path,geomF,kReadPermission);
-  if (! file) Fatal("CreateGeometry","File %s has not found in path %s",geomF.Data(),path);
+  Char_t *file = gSystem->Which(path.Data(),geomF,kReadPermission);
+  if (! file) Fatal("CreateGeometry","File %s has not found in path %s",geomF.Data(),path.Data());
   else        Warning("CreateGeometry","File %s has been found as %s",geomF.Data(),file);
   TString command = ".L "; command += file;
   gInterpreter->ProcessLine(command);
