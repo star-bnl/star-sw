46de69f1ad10c8bf747b49cc35133a297176cd20
Yuri Fisyak <fisyak@bnl.gov>
2023-06-24 11:39:52 -0400

    Reset modified tables in StDetectorDbMaker::InitRun, intialize rows = 0 in St_tpcRDOMasksC::instance

--- StRoot/StDetectorDbMaker/St_tpcMaxHitsC.h
+++ StRoot/StDetectorDbMaker/St_tpcMaxHitsC.h
@@ -2,12 +2,14 @@
 #define St_tpcMaxHitsC_h
 #include "TChair.h"
 #include "tables/St_tpcMaxHits_Table.h"
+class TBrowser;
 class St_tpcMaxHitsC : public TChair {
  public:
   static St_tpcMaxHitsC* 	instance();
   tpcMaxHits_st *Struct(Int_t i = 0) {return ((St_tpcMaxHits*) Table())->GetTable()+i;}
   Int_t maxSectorHits() {return Struct()->maxSectorHits;}
   Int_t maxBinZeroHits() {return Struct()->maxBinZeroHits;}
+  void        Browse(TBrowser *b) {}
  protected:
   St_tpcMaxHitsC(St_tpcMaxHits *table=0) : TChair(table) {}
   virtual ~St_tpcMaxHitsC() {fgInstance = 0;}
