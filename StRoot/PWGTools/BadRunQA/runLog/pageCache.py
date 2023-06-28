09be1af22b268dacefc2f0ad87e8adb2495b8e5b
ssedd1123 <tommytsang3108@hotmail.com.hk>
2023-06-28 11:10:21 -0400

    Default range of global rejection is modified according to QA meetings on 6/16 (#558)
    I only changed python script in BadRunQA. No changes are made to the
    makers in StRoot.
    
    ---------
    
    Co-authored-by: Chun Yuen Tsang <ctsang@rcas6013.rcf.bnl.gov>
    Co-authored-by: Chun Yuen Tsang <ctsang@rcas6012.rcf.bnl.gov>
    Co-authored-by: surface <ctsang@bnl.gov>

--- StRoot/PWGTools/BadRunQA/runLog/pageCache.py
+++ StRoot/PWGTools/BadRunQA/runLog/pageCache.py
@@ -43,7 +43,7 @@ class PageCache:
         if 'error' in driver.title.lower() or 'unauthorize' in driver.title.lower():
             raise RuntimeError('Cannot load shift log')
 
-        with open(os.path.join(self._dir, filename + '.html'), 'w') as f:
+        with open(os.path.join(self._dir, filename + '.html'), 'w', encoding='utf-8') as f:
             f.write('<!-- ' + url + '-->\n' + driver.page_source)
         time.sleep(self._timeSep)
         return driver.page_source
