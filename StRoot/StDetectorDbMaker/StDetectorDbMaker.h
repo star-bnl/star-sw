7fd7f97ac66d61744abe7c674de6fdee277c4743
Yuri Fisyak <fisyak@bnl.gov>
2023-07-21 16:32:35 -0400

    Reset modified tables in StDetectorDbMaker::InitRun, intialize rows =… (#560)
    Reset modified tables in StDetectorDbMaker::InitRun, intialize rows = 0
    in St_tpcRDOMasksC::instance

--- StRoot/StDetectorDbMaker/StDetectorDbMaker.h
+++ StRoot/StDetectorDbMaker/StDetectorDbMaker.h
@@ -7,13 +7,16 @@
 
 class StDetectorDbMaker : public StMaker {
  public: 
-  StDetectorDbMaker(const char *name="DetectorDb") : StMaker(name) {}
-  virtual       ~StDetectorDbMaker() {}
+ StDetectorDbMaker(const char *name="DetectorDb") : StMaker(name) {fgStDetectorDbMaker = this; }
+  virtual       ~StDetectorDbMaker() {fgStDetectorDbMaker = 0;}
+  virtual Int_t        InitRun(Int_t runumber);
   virtual Int_t  Make();
+  static StDetectorDbMaker *instance() {return fgStDetectorDbMaker;}
+ private:
   static  Int_t  _debug;  
   virtual const char *GetCVS() const
   {static const char cvs[]="Tag $Name:  $ $Id: StDetectorDbMaker.h,v 1.5 2015/12/23 23:34:09 fisyak Exp $ built " __DATE__ " " __TIME__ ; return cvs;}
-  
+  static StDetectorDbMaker *fgStDetectorDbMaker;
   ClassDef(StDetectorDbMaker,0)   //StAF chain virtual base class for Makers
 };
 
