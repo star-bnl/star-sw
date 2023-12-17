f1db5e8216b89fde404bc0830c566f03389920b5
Daniel Brandenburg <danbburg@gmail.com>
2023-12-17 14:29:28 -0500

    basic QA for StEvent and MuDst FwdTracks

--- StRoot/StFwdUtils/StFwdAnalysisMaker.h
+++ StRoot/StFwdUtils/StFwdAnalysisMaker.h
@@ -1,22 +1,13 @@
 #ifndef ST_FWD_ANALYSIS_MAKER_H
 #define ST_FWD_ANALYSIS_MAKER_H
 
+#include <map>
+
 #include "StChain/StMaker.h"
-#include "TVector3.h"
-// ROOT includes
-#include "TNtuple.h"
-#include "TTree.h"
-// STL includes
-#include <vector>
-#include <memory>
-
-class StFwdTrack;
 
 
-class StFwdAnalysisMaker : public StMaker {
-
-    ClassDef(StFwdAnalysisMaker, 0);
-
+class StFwdAnalysisMaker : public StMaker
+{
   public:
     StFwdAnalysisMaker();
     ~StFwdAnalysisMaker(){/* nada */};
@@ -27,6 +18,26 @@ class StFwdAnalysisMaker : public StMaker {
     void Clear(const Option_t *opts = "");
     void ProcessFwdTracks();
     void ProcessFwdMuTracks();
+
+    // StEvent analyzed by default
+    // call this to analyze the MuDst instead
+    void setMuDstInput() { mAnalyzeMuDst = true; }
+
+  protected:
+
+    std::map<TString, TH1*> mHists;
+    TH1 * addHist( TH1 * h1){
+      mHists[h1->GetName()] = h1;
+    }
+    TH1* getHist( TString n ){
+      if (mHists.count(n))
+        return mHists[n];
+      LOG_ERROR << "Attempting to access non-existing histogram" << endm;
+      return new TH1F( "NULL", "NULL", 1, 0, 1 ); // returning nullptr can lead to seg fault, this fails softly
+    }
+    bool mAnalyzeMuDst = false;
+
+  ClassDef(StFwdAnalysisMaker, 0);
 };
 
 #endif
