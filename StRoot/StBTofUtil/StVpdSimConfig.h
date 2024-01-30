98e90ea4167402fcd1da57e41789f85ea83c7b44
Daniel Torres Valladares <dt36@star3.rice.edu>
2024-01-30 11:31:59 -0600

    VPD start mode added and mVpdResConfigs->ingleTubeRes function fixed.

--- StRoot/StBTofUtil/StVpdSimConfig.h
+++ StRoot/StBTofUtil/StVpdSimConfig.h
@@ -34,16 +34,29 @@ public:
         int tubeTriggerFlag;    //!< Status flag for whether tube was triggered on (1) or not (0)
 	};
 
-/// calculate correct resolution based on those tubes that were used
-double singleTubeRes(UInt_t mVPDHitPatternEast, UInt_t mVPDHitPatternWest){
-	double vpdResSumSqr(0.), vpdresolution(0.);
-	for (int i=0; i<19; i++){
-		if (1 << i && mVPDHitPatternEast) vpdResSumSqr += (mSimParams[i].singleTubeRes)*(mSimParams[i].singleTubeRes);
-		if (1 << i && mVPDHitPatternWest) vpdResSumSqr += (mSimParams[i+19].singleTubeRes)*(mSimParams[i+19].singleTubeRes);
+	/**
+	 * @brief Calculate correct resolution based on those tubes that were used.
+	 * 
+	 * @param mVPDHitPatternEast 9 digit binary number specifying hit pattern of east VPD tubes.
+	 * @param mVPDHitPatternWest 9 digit binary number specifying hit pattern of west VPD tubes.
+	 * @return double vpd resolution.
+	 */
+	double singleTubeRes(UInt_t mVPDHitPatternEast, UInt_t mVPDHitPatternWest){
+		double vpdResSumSqr(0.), vpdresolution(0.);
+		int total_vpd_hits = 0; //Total number of vpd tubes used.
+		for (int i=0; i<19; i++){
+			if (1 << i & mVPDHitPatternEast) {
+				vpdResSumSqr += 1;
+				total_vpd_hits += 1;
+			}
+			if (1 << i & mVPDHitPatternWest) {
+				vpdResSumSqr += 1;
+				total_vpd_hits += 1;
+			}
 		}
-  	vpdresolution = sqrt(vpdResSumSqr);
-	return vpdresolution;
-}
+		vpdresolution = sqrt(vpdResSumSqr)/total_vpd_hits;
+		return vpdresolution;
+	}
 
     /**
     * Calculates the average resolution across all 38 tubes (discounts inactive tubes)
