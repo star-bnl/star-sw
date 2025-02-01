fcacdad4c5e9fc0c3f695c2ba49a39fbb5bb37d8
Daniel Brandenburg <danbburg@gmail.com>
2025-01-31 20:39:52 -0500

    fix incorrect merge, duplicate symbol

--- StRoot/StEvent/StFwdTrack.cxx
+++ StRoot/StEvent/StFwdTrack.cxx
@@ -6,12 +6,6 @@ StFwdTrack::StFwdTrack() {}
 
 StFwdTrack::~StFwdTrack() {}
 
-StFwdTrack::~StFwdTrack() { 
-   mEcalClusters.clear();
-   mHcalClusters.clear();
-}
-
-
 /* momentum
  * get the track momentum at the first point (PV if included)
  */
