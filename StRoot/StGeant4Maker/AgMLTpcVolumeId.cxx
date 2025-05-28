5747a5c2c3bfa06c5d639f6a255f48f65f3af75b
klendathu2k <jwebb@bnl.gov>
2025-05-28 13:48:32 -0400

    Handle inner TPC padrows from before the iTPC, during the single sector 20 test, and the full install.

--- StRoot/StGeant4Maker/AgMLTpcVolumeId.cxx
+++ StRoot/StGeant4Maker/AgMLTpcVolumeId.cxx
@@ -1,45 +1,24 @@
 #include <AgMLTpcVolumeId.h>
+
+#include <StarVMC/StarGeometry/StarGeo.h>
+
 AgMLTpcVolumeId::AgMLTpcVolumeId() : 
   AgMLVolumeId(),
-  tpads{  
-    1,  1,  2,  3,  4,  5,  6,  7,  8,  9,
-    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-    40, 40, 41, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-    70, 71, 72, 72,
-    1,  1,  2,  3,  4,  5,  6,  7,  8,  9,
-    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-    40, 40, 41, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-    50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-    60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-    70, 71, 72, 72
- },
-                      
-  isdet{  
-      1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  2,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  2,
-      1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  2,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  2
-      }   
+  mode(1)
 {
+  if ( StarGeometry::HasDetector( "TPCEv6"  ) || StarGeometry::HasDetector( "TPCEv61" ) ) {
+    LOG_INFO << "TPC volume IDs configured for full iTPC" << endm;
+    mode=3;
+  }
+  else if ( StarGeometry::HasDetector( "TPCE32" ) ) {
+    LOG_INFO << "TPC volume IDs configured for testbed iTPC / sector 20 inner will be mapped out of range for tpcrs." << endm;
+    mode=2;
+  }
+  else {
+    LOG_INFO << "TPC volume IDs configured for classic inner detector" << endm;
+    mode=1;
+  }
+};
 
-}
 				     
 
