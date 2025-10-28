42480171234771e192bd3a794a3f26a160ab6b28
klendathu2k <jwebb@bnl.gov>
2025-10-28 17:36:15 -0400

    Avoid TVector3 ctor

--- StRoot/StTableUtilities/StG2TrackVertexMap.cxx
+++ StRoot/StTableUtilities/StG2TrackVertexMap.cxx
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include "StG2TrackVertexMap.h"
 #include "TVector3.h"
+#include "StMessMgr.h"
+
 StG2TrackVertexMap *StG2TrackVertexMap::fgInstance = 0;
 //________________________________________________________________________________
 StG2TrackVertexMap *StG2TrackVertexMap::instance(St_g2t_track *track, St_g2t_vertex *vertex) {
@@ -12,34 +14,41 @@ StG2TrackVertexMap *StG2TrackVertexMap::instance(St_g2t_track *track, St_g2t_ver
 }
 //________________________________________________________________________________
 void StG2TrackVertexMap::Reset(St_g2t_track *track, St_g2t_vertex *vertex) {
+  // Clear the maps
   fTrack2Vertex.clear();
   fVertex2ParentTrack.clear();
+  // Return if there is no track or vertex table
   if (! track ||  ! vertex) return;
-  Int_t Nt = track->GetNRows();
-  Int_t NV = vertex->GetNRows();
+  // Number of tracks and vertices
+  int Nt = track->GetNRows();
+  int NV = vertex->GetNRows();
   g2t_track_st *t = track->GetTable();
   g2t_vertex_st *v = vertex->GetTable();
-  for (Int_t i = 0; i < Nt; i++) {
-    Int_t IdT = t[i].id - 1;                 assert(IdT >= 0 && IdT < Nt);
-    Int_t IdV = t[i].start_vertex_p - 1;     assert(IdV >= 0 && IdV < NV);
-    TVector3 Vx1(v[IdV].ge_x);
-    Float_t tof1 = v[IdV].ge_tof;
-    Int_t IdP = v[IdV].parent_p - 1;         
+  static TVector3 Vx1;
+  static TVector3 Vx2;
+  // Loop over all tracks
+  for (int i = 0; i < Nt; i++) {
+    int IdT = t[i].id - 1;                 assert(IdT >= 0 && IdT < Nt);
+    int IdV = t[i].start_vertex_p - 1;     assert(IdV >= 0 && IdV < NV);
+    Vx1.SetXYZ( v[IdV].ge_x[0], v[IdV].ge_x[1], v[IdV].ge_x[2] );
+    float tof1 = v[IdV].ge_tof;
+    int IdP = v[IdV].parent_p - 1;         
     if (IdP >= 0 && IdP < Nt) {
-      Int_t IdVxP = t[IdP].start_vertex_p - 1; assert(IdVxP >= 0 && IdVxP < NV);
-      TVector3 Vx2(v[IdVxP].ge_x);
+      int IdVxP = t[IdP].start_vertex_p - 1; assert(IdVxP >= 0 && IdVxP < NV);
+      Vx2.SetXYZ( v[IdVxP].ge_x[0], v[IdVxP].ge_x[1], v[IdVxP].ge_x[2] );
       Vx2 -= Vx1;
+      // Merge short lived decays with any (grand) parent vertex 
       while ((Vx2.Mag() < 10e-4 && TMath::Abs(tof1-v[IdVxP].ge_tof) < 1e-9)) {
 	IdV = IdVxP;
 	IdP = v[IdV].parent_p - 1; if (IdP < 0) break;
 	IdVxP = t[IdP].start_vertex_p - 1;  if (IdVxP < 0) break;
-	Vx2 = TVector3(v[IdVxP].ge_x);
+	Vx2.SetXYZ( v[IdVxP].ge_x[0], v[IdVxP].ge_x[1], v[IdVxP].ge_x[2] );
 	Vx2 -= Vx1;
       }
     }
     fTrack2Vertex[IdT+1] = IdV+1;
   }
-  for (Int_t i = 0; i < NV; i++) {
+  for (int i = 0; i < NV; i++) {
     fVertex2ParentTrack[v[i].id] = v[i].parent_p;
   }
 }
