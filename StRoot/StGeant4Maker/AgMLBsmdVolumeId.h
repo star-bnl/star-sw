af26e934a7556385169fd3ef4f0087c37638950a
klendathu2k <jwebb@bnl.gov>
2026-01-19 16:35:22 -0500

    BSMD volume IDs with strip decoding via (hardcoded) geometry parameters.

--- StRoot/StGeant4Maker/AgMLBsmdVolumeId.h
+++ StRoot/StGeant4Maker/AgMLBsmdVolumeId.h
@@ -1,88 +1,145 @@
 #ifndef __AgMLBsmdVolumeId_h__
 #define __AgMLBsmdVolumeId_h__
 
+#include <StarVMC/StarAgmlLib/AgMLExtension.h>
 #include <TVirtualMC.h>
-#include <TVector3.h>
-#include <TLorentzVector.h>
 #include <TMath.h>
-#include <iostream>
-#include <iomanip>
+#include <TLorentzVector.h>
+#include <StMessMgr.h>
+#include <TGeoManager.h>
+#include <TGeoMatrix.h>
 
 class AgMLBsmdVolumeId : public AgMLVolumeId {
+
+  const double width_eta1 {1.53668}; // 2 * (0.7277 + 0.04064)
+  const double width_eta2 {1.96088}; // 2 * (0.9398 + 0.04064)
+  const double width_phi  {1.49348}; // 2 * (0.6680 + 0.07874)
+
+  const int n_strip_eta1 {75};
+  const int n_strip_eta2 {75};
+  const int n_strip_phi  {15};
+
 public:
-    AgMLBsmdVolumeId() : AgMLVolumeId() { }
+  
+  AgMLBsmdVolumeId() : AgMLVolumeId() { };
 
-    virtual int id( int* numbv ) const {
-      
+  virtual int id( int* numbv ) const { 
+    
 
-      int rl      = numbv[0]; // Side
-      int phiMod  = numbv[1]; // Module
-      int section = numbv[3]; // Section/Layer
-      int strip   = numbv[4]; // Strip
-
-      int type = 1; // 1=BSMDE, 3=BSMDP
-
-      // Deep hierarchy fallback? important?
-      if (strip == 0 && numbv[5] > 0) strip = numbv[5];
-
-      int offset = 0;
-
-      TLorentzVector _direction(0,0,0,0);
-      TVirtualMC::GetMC()->TrackPosition( _direction );
-
-      double xg[4], xl[4];
-      _direction.GetXYZT( xg );
-      double pseudoRapidity = TMath::Abs(_direction.Eta());        
-
-      // Obtain local coordinates from global coordinates
-      TVirtualMC::GetMC()->Gmtod( xg, xl, 1 );
-
-      if ( rl==1 ) {
-        phiMod = 60 - phiMod + 1;
-      }
-      else {
-        phiMod = 60+phiMod;
-      }
-
-      // very important 
-      // Section 1 = Low Eta, Section 3 = High Eta (+75), Others = Phi
-      // check ClabGeo2.xml
-      if (section == 1) {type=1; offset = 0;}
-      else if (section == 3) {type=1; offset = 75;}
-      else {type=3; offset = 0;} // BSMDP
-
-      int finalStrip = strip + offset;
-
-      int etaBin = 1;
-
-      if (type == 1) { 
-        // BSMDE (Eta)
-        etaBin = static_cast<int>((finalStrip - 1) / 7.5) + 1;
-      } 
-      else { 
-        // BSMDP (Phi)
-        // have to get it from the position
-        // used similar logic in AgMLEmcVolumeId
-        // have to check if it is correct but it looks fine
-        double x, y, z;
-        TVirtualMC::GetMC()->TrackPosition(x, y, z);
-        TVector3 pos(x, y, z);
-        
-        if (pos.Mag() > 0) {
-          double etaVal = TMath::Abs(pos.Eta());
-          //this is the most important line in phi calculation
-          // phi is segmented into 10 bins per 1 unit of eta
-          // this meas that each bin is 0.1 in eta
-          etaBin = static_cast<int>(etaVal / 0.1) + 1;
-        }
-      }
-
-      int volumeid =  (rl     * 100000000) + //not the same ocd (check AgMLEsmdVolumeId)
-                      (etaBin * 1000000  ) + 
-                      (phiMod * 1000     ) + 
-                      (type   * 100      ) + 
-                      (finalStrip        ) ;
-      return volumeid;
+    int rileft    = numbv[0]; // 1 west, 2 east
+    int phi_mod   = numbv[1]; // 1-60 phi module
+    int layer     = numbv[3]; // smd layer 1-4
+    //Note: in the g2t_volume_id.g layer is numbv(3), which is numbv[2] in C++
+    // but for some reason, numbv[2] is always 2. I think geant4 treats the mother 
+    // volume CSDA as a separate volume, so the layer info is shifted by 1.
+    int phi_encoded {0};
+    if (rileft == 1) {
+      phi_encoded = 60 - phi_mod + 1;
+    } else {
+      //Note: StEmcGeom does this differently than g2t_volume_id.g
+      //StEmcGeom: phi    = smdChid[2];  // module phi [1,60]
+      //g2t_volume_id.g
+      /*
+        If (rileft==1) then
+          phi=60-phi+1
+        else
+          phi=60+phi
+        endif     
+      */
+     //keeping the StEmcGeom logic because we are using
+     //it to generate bsmd cells in gen_bsmd_cells.C
+      phi_encoded = phi_mod;
     }
+
+    TLorentzVector _direction(0,0,0,0);
+    TVirtualMC::GetMC()->TrackPosition( _direction );
+
+    double xg[4], xl[4];
+    _direction.GetXYZT( xg );
+    TVirtualMC::GetMC()->Gmtod( xg, xl, 1 );
+
+
+    int strip     {  0  };
+    int eta_bin   {  0  };
+    int forw_back {layer};
+
+    if (forw_back == 4) forw_back = 3;  //from g2t_volume_id.g
+
+    // the SMD-Eta is treated as one big gas volume (by the geometry definition)
+    // so we have to segment it ourself from the local z position of the hit.
+    if (forw_back <= 2) { //smde
+
+      double start_z_pos {0.0};
+      double width       {0.0};
+      int    max_strip   { 0 };
+      
+      if (forw_back == 1) {
+        max_strip = n_strip_eta1;//same for both layers
+        width     = width_eta1;
+      } else {
+        max_strip = n_strip_eta2;//same for both layers
+        width     = width_eta2;
+      }
+
+      // Center alignment
+      const double total_width {max_strip * width};
+      start_z_pos = -total_width / 2.0;
+
+      // Strip = (LocalZ - Start) / Width
+      strip = static_cast<int>(floor( (xl[2] - start_z_pos) / width )) + 1;
+
+      // In East (rileft=2), the module is rotated 180 degrees.
+      // Strip 1 is still at Eta=0 (which is now Max Local Z).
+      // So we must invert the calculated strip index.
+      if (rileft == 2) {
+        strip = max_strip - strip + 1;
+      }
+      //for smde, calculate eta bin from strip
+      int global_strip {strip};
+      if (forw_back == 2) global_strip += 75; 
+      eta_bin = (global_strip - 1) / 15 + 1;
+    }
+    else { //smdp
+      
+      TGeoNavigator* nav = gGeoManager->GetCurrentNavigator();
+      nav->CdUp();//go to csme
+      nav->CdUp();//go to csda
+      
+      double masterPos[3] = {xg[0], xg[1], xg[2]}; //global hit position
+      double localPos[3];
+      //smdp is rotated 180 in phi 90 theta wrt tower
+      //we have to un-rotate to get local coordinates
+      nav->MasterToLocal(masterPos, localPos);
+      
+      double total_width {n_strip_phi * width_phi}; // ~22.4022
+      double start_y     {  -total_width / 2.0   };
+
+      strip = static_cast<int>(floor( (localPos[1] - start_y) / width_phi ) ) + 1;      
+      eta_bin = static_cast<int>( TMath::Abs(_direction.Eta()) * 10.0) + 1;
+    }
+
+    int volume_id = 100000000 * rileft
+                  + 1000000   * eta_bin
+                  + 1000      * phi_encoded
+                  + 100       * forw_back
+                  + strip;
+    // if(forw_back == 3)
+    // LOG_INFO << "BSMDP VolumeId: rileft=" << rileft
+    //          << ", phi_mod=" << phi_mod
+    //          << ", layer=" << layer
+    //          << ", xg[0]=" << xg[0]
+    //          << ", xg[1]=" << xg[1]
+    //          << ", xg[2]=" << xg[2]
+    //          << ", xl[0]=" << xl[0]
+    //          << ", xl[1]=" << xl[1]
+    //          << ", xl[2]=" << xl[2]
+    //          << ", strip=" << strip
+    //          << ", eta_bin=" << eta_bin
+    //          << ", volume_id=" << volume_id
+    //          << endm;
+
+    return volume_id;
+  };
 };
+
 #endif
