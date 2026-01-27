665d38366ac447af1c39693612fd7cbf2412524f
klendathu2k <jwebb@bnl.gov>
2026-01-27 13:45:02 -0500

    VpD appears to only have the single production version... and HasDetector does not seem to be working correctly, so hardcode the production version.

--- StRoot/StGeant4Maker/AgMLVpdVolumeId.h
+++ StRoot/StGeant4Maker/AgMLVpdVolumeId.h
@@ -4,6 +4,7 @@
 #include <StarVMC/StarAgmlLib/AgMLExtension.h>
 #include <StarVMC/StarGeometry/StarGeo.h>
 #include <StMessMgr.h>
+#include <string>
 
 /**
  * @class AgMLVpdVolumeId
@@ -19,9 +20,13 @@ public:
   
   AgMLVpdVolumeId()
   {
-    version = 
-      StarGeometry::HasDetector( "VPDD07" ) ||
-      StarGeometry::HasDetector( "VPDD08" ) 
+    version = 1;
+    if      ( StarGeometry::HasDetector( "VPDD07" ) ) version=7;
+    else if ( StarGeometry::HasDetector( "VPDD08" ) ) version=7;
+    else if ( StarGeometry::HasDetector( "VPDD02" ) ) version=2;
+    else if ( StarGeometry::HasDetector( "VPDD03" ) ) version=3;
+    else if ( StarGeometry::HasDetector( "VPDD04" ) ) version=4;
+    LOG_INFO << "VPD Volume ID: version=" << version << endm;
       ;
   }
 
@@ -32,13 +37,27 @@ public:
   
   virtual int id( int* numbv ) const { 
 
-    int idx = 0;
-
-    int rileft =                numbv[idx++];
-    int inout  = version ? numbv[idx++] : 0;
-    int sector =                numbv[idx++];
+    
+    int rileft = numbv[0];
+    int inout  = 0; // n.b. earlier R&D version appears to have had inner vs outer ring
+    int sector = numbv[1];
 
     int _id = 1000 * rileft + 100*inout + sector;
+
+#if 0
+    LOG_INFO << "VPD volume ID : "
+	     << std::to_string(numbv[0]) << " " 
+	     << std::to_string(numbv[1]) << " "  
+	     << std::to_string(numbv[2]) << " " << " | " 
+	     << std::to_string(numbv[3]) << " " << " | " 
+	     << std::to_string(rileft) << " " 
+	     << std::to_string(inout) << " "
+	     << std::to_string(sector) << " | " 
+	     << _id 
+	     << endm;
+#endif
+    
+
 
     return _id;
 
