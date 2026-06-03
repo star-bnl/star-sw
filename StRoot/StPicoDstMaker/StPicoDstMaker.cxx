fece0c7d4e1da26a73441fef6f1c77db1c434c49
Gene Van Buren <gene@bnl.gov>
2026-06-03 15:52:17 -0400

    Allow for different FXT vertex z location

--- StRoot/StPicoDstMaker/StPicoDstMaker.cxx
+++ StRoot/StPicoDstMaker/StPicoDstMaker.cxx
@@ -2740,6 +2740,10 @@ bool StPicoDstMaker::selectVertex() {
   }
   else if ( mVtxMode == PicoVtxMode::FXT ) {
     
+    // Target z at west-side location until the final OO200 dataset
+    float z_tgt = (mMuDst->event()->runNumber() < 27032000 ? 200.0 : -180.0);
+    float z_tgtTolerance = 2.0;
+
     // Loop over primary vertices
     for (unsigned int iVtx = 0; iVtx < mMuDst->numberOfPrimaryVertices(); ++iVtx) {
       
@@ -2748,7 +2752,7 @@ bool StPicoDstMaker::selectVertex() {
       if (!vtx) continue;
       
       // Check TpcVz and VpdVz difference
-      if (vtx->position().z() > 198. && vtx->position().z() < 202.) {
+      if (TMath::Abs(vtx->position().z() - z_tgt) < z_tgtTolerance) {
 	// Reset vertex index
 	mMuDst->setVertexIndex( iVtx );
 	// Reset pointer to the primary vertex
